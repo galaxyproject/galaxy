5a4c943fda37485c43c83300573b5a319b7fe4c8
Daniel Blankenberg <dan@bx.psu.edu>
2007-01-11 16:28:14 +0000

    Fix for Biomart test. Why are there two biomart tests?

--- test/functional/test_3A_GetData.py
+++ test/functional/test_3A_GetData.py
@@ -30,18 +30,23 @@ class UcscTests(TwillTestCase):
 
     def test_30_Biomart_Uniprot(self):
         """3A_GetData: Connection to Biomart"""
+        #All this test does, is check to see if biomart is accessible through Galaxy. A dataset is never retrieved or checked.
 
         # some button indices are hardcoded (twill limitation)
         self.run_tool('biomart')
         
-        self.submit_form(form='settings', button='stage_filter', 
-            database='UNIPROTPROTOTYPE4-5(EBI)__default', dataset='uniprot'
+        self.submit_form(form='mainform', button='get_count_button', 
+            dataBase='default____UNIPROT PROTOTYPE 4-5  (EBI)', dataset='uniprot'
         )
         
-        self.submit_form(form='settings', button=6, 
-            uniprot_collection_start=1, uniprot_collection_end=1,
-            uniprot_component_start=1000, uniprot_component_end=2000
-        )
+        #self.submit_form(form='settings', button='stage_filter', 
+        #    database='UNIPROTPROTOTYPE4-5(EBI)__default', dataset='uniprot'
+        #)
+        
+        #self.submit_form(form='settings', button=6, 
+        #    uniprot_collection_start=1, uniprot_collection_end=1,
+        #    uniprot_component_start=1000, uniprot_component_end=2000
+        #)
         
         #self.submit_form(form='settings', button=5, 
         #    outtype='Features', outformat='tsv'
