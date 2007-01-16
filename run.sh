547ecf09608bf23eba59266dae5700d528334835
Daniel Blankenberg <dan@bx.psu.edu>
2007-01-16 17:05:33 +0000

    Removing arch and module directories.  Removing old operations (and tests), since they require arch; updated tool_conf.xml.sample. Removed non-arch specific cheetah egg, since compiled c code (once located in arch) is included in arch specific eggs. Removed a tool called basicStats2, which used arch, but hasn't been in tool_conf.xml for some time.

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ ARCH="$KERNEL-$MACHTYPE"
 echo "Architecture appears to be $ARCH"
 
 export UNIVERSE_HOME=`pwd`
-export PATH=$PATH:$UNIVERSE_HOME/arch/$ARCH/bin:/depot/apps/$MACHTYPE/bin:/home/universe/$ARCH/EMBOSS/EMBOSS-3.0.0/bin:/home/universe/$ARCH/EMBOSS/bin:/home/universe/$ARCH/ImageMagick/bin:/home/universe/$ARCH/PAML/paml3.15/bin
-export PYTHONPATH=$UNIVERSE_HOME/lib:$UNIVERSE_HOME/modules:$UNIVERSE_HOME/eggs:$UNIVERSE_HOME/arch/$ARCH/lib/python
+export PATH=$PATH:/depot/apps/$MACHTYPE/bin:/home/universe/$ARCH/EMBOSS/EMBOSS-3.0.0/bin:/home/universe/$ARCH/EMBOSS/bin:/home/universe/$ARCH/ImageMagick/bin:/home/universe/$ARCH/PAML/paml3.15/bin
+export PYTHONPATH=$UNIVERSE_HOME/lib:$UNIVERSE_HOME/eggs
 
 echo "python path: $PYTHONPATH"
 
