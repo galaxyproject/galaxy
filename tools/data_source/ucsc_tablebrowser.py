412b9a96779444afbce8ff80d6ad4d8a36da4307
Daniel Blankenberg <dan@bx.psu.edu>
2007-02-28 20:46:06 +0000

    Add UCSC Table Browser direct connection tool.

---
+++ tools/data_source/ucsc_tablebrowser.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2.4
+#Retreives data from UCSC and stores in a file. UCSC parameters are provided in the input/output file.
+import urllib, sys
+
+def __main__():
+    filename = sys.argv[1]
+    params = {}
+    
+    for line in open(filename, 'r'):
+        try:
+            line = line.strip()
+            fields = line.split('\t')
+            params[fields[0]] = fields[1]
+        except:
+            continue
+    
+    URL = params.get('URL',None)
+    if not URL:
+        open(filename, 'w').write("")
+        #raise Exception('Datasource has not sent back a URL parameter')
+        print >> sys.stderr, 'Datasource has not sent back a URL parameter'
+        sys.exit(0)
+    
+    out = open(filename, 'w')
+    
+    CHUNK_SIZE = 2**20 # 1Mb 
+    try:
+        page = urllib.urlopen(URL, urllib.urlencode(params))
+    except Exception, exc:
+        #raise Exception('Problems connecting to %s (%s)' % (URL, exc) )
+        print >> sys.stderr, 'Problems connecting to %s (%s)' % (URL, exc)
+        sys.exit(0)
+    
+    while 1:
+        chunk = page.read(CHUNK_SIZE)
+        if not chunk:
+            break
+        out.write(chunk)
+    
+    out.close()
+    
+    
+if __name__ == "__main__": __main__()
---
+++ tools/data_source/ucsc_tablebrowser_code.py
@@ -0,0 +1,44 @@
+#Code for direct connection to UCSC
+from galaxy import datatypes
+
+def exec_before_job( trans, inp_data, out_data, param_dict, tool=None):
+    """Sets the name of the data"""
+    outputType = param_dict.get( 'hgta_outputType', "interval" ) #assume all data is interval, we will fix later if not the case
+    #list for converting ucsc to galaxy exts, if not in here, use raw reported value
+    outputType_to_ext = {'wigData':'wig','tab':'interval'}
+    items = out_data.items()
+    description = param_dict.get('hgta_regionType',"")
+    organism = param_dict.get('org',"unkown species")
+    table = param_dict.get('hgta_track',"")
+    if description == 'range':
+        try:
+            description = param_dict.get('position',"")
+        except:
+            description = "unkown position"
+    for name, data in items:
+        data.name  = "%s on %s: %s (%s)" % (data.name, organism, table, description)
+        data.dbkey = param_dict.get('db', '?')
+        ext = outputType
+        try: ext = outputType_to_ext[outputType]
+        except: pass
+        
+        data = datatypes.change_datatype(data, ext)
+        
+        #store ucsc parameters temporarily in output file
+        out = open(data.file_name,'w')
+        for key, value in param_dict.items():
+            print >> out, "%s\t%s" % (key,value)
+        out.close()
+        
+        out_data[name] = data
+
+def exec_after_process(app, inp_data, out_data, param_dict, tool=None, stdout=None, stderr=None):
+    """Verifies the datatype after the run"""
+    name, data = out_data.items()[0]
+    if data.state == data.states.OK: data.info = data.name
+    
+    if not isinstance(data.datatype, datatypes.interval.Bed) and isinstance(data.datatype, datatypes.interval.Interval):
+        data.set_meta()
+        if data.missing_meta(): data = datatypes.change_datatype(data, 'tabular')
+    data.set_peek()
+    data.flush()
