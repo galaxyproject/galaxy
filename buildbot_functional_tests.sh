a70768b25597910ebfcea7c8dc7db397263e0b51
James Taylor <james@jamestaylor.org>
2007-03-08 18:27:00 +0000

    Buildbot functional test script updates.

--- buildbot_functional_tests.sh
+++ buildbot_functional_tests.sh
@@ -5,4 +5,8 @@ createdb buildbot_galaxy_functional_tests
 
 export GALAXY_TEST_DBURI=postgres:///buildbot_galaxy_functional_tests
 
-./nosetests.sh -v --with-nosehtml --html-report-file buildbot_functional_tests.html --with-doctest --exclude="^get" galaxy.test.functional
+./nosetests.sh -v -w test \
+	--with-nosehtml \
+	--html-report-file buildbot_functional_tests.html \
+	-- exclude="^get" \
+	functional
