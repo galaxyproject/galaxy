cc4d6934bdadad2126a3034540566e4af19348ad
Daniel Blankenberg <dan@bx.psu.edu>
2007-03-15 20:31:09 +0000

    Random Intervals update.

--- tools/encode/random_intervals_no_bits.py
+++ tools/encode/random_intervals_no_bits.py
@@ -1,38 +1,61 @@
 #!/usr/bin/env python2.4
+#Dan Blankenberg
 #%prog bounding_region_file mask_intervals_file intervals_to_mimic_file out_file mask_chr mask_start mask_end interval_chr interval_start interval_end interval_strand use_mask allow_strand_overlaps
 import sys, random
+from copy import deepcopy
 
-max_iters = 100000
+import pkg_resources
+pkg_resources.require( "bx-python" )
+import bx.intervals.io
+import bx.intervals.intersection
+import psyco_full
+
+max_iters = 5
+
 
 #Try to add a random region
-def add_random_region(region, b_chr, b_start, b_end, exist_regions, mask, overlaps):
-    rand_region = region.copy()
-    for iter in range(max_iters):
-        rand_region['start'] = random.randint(b_start, b_end - rand_region['length'])
-        rand_region['end'] = rand_region['start'] + rand_region['length']
+def add_random_region(mimic_region, bound, exist_regions, plus_mask, minus_mask, overlaps):
+    region_length, region_strand = mimic_region
+    plus_count = plus_mask.count_range()
+    minus_count = minus_mask.count_range()
+    
+    gaps = []
+    if region_strand == "-": gaps = minus_mask.get_gaps(region_length)
+    else: gaps = plus_mask.get_gaps(region_length)
+    
+    while True:
+        try: gap_length, gap_start, gap_end = gaps.pop(random.randint(0,len(gaps)-1))
+        except: break
+        start = random.randint(bound.start+gap_start, bound.start+gap_end-region_length-1)
+        end = start + region_length
         
-        if overlaps == "all":
-            exist_regions.append(rand_region)
-            return exist_regions, True
+        try_plus_mask = plus_mask.copy()
+        try_minus_mask = minus_mask.copy()
         
-        found_overlap = False
-        for region in exist_regions:
-            if not (rand_region['end'] <= region['start'] or  region['end'] <= rand_region['start']):
-                if overlaps=="none" or rand_region['strand'] == region['strand']:
-                    found_overlap = True
-            
+        if region_strand == "-":
+            try_minus_mask.set_range(start-bound.start, end-bound.start)
+        else:
+            try_plus_mask.set_range(start-bound.start, end-bound.start)
         
-        if not found_overlap:
-            for region in mask:
-	            if region['chr'] != rand_region['chr']:
-	                continue
-	            if not (rand_region['end'] <= region['start'] or  region['end'] <= rand_region['start']):
-	                found_overlap = True
-                
-        if not found_overlap:
-            exist_regions.append(rand_region)
-            return exist_regions, True
-    return exist_regions, False
+        rand_region = bx.intervals.io.GenomicInterval( None, [bound.chrom,start,end,region_strand], 0, 1, 2, 3, "+", fix_strand=True )
+        
+        if try_plus_mask.count_range() == plus_count + region_length or try_minus_mask.count_range() == minus_count + region_length:
+            if overlaps in ["strand", "all"]: #overlaps allowed across strands
+                exist_regions.append(rand_region)
+                if overlaps == "strand":
+                    return exist_regions, True, try_plus_mask, try_minus_mask
+                else: #overlaps allowed everywhere
+                    return exist_regions, True, plus_mask, minus_mask
+            else: #no overlapping anywhere
+                exist_regions.append(rand_region)
+                if region_strand == "-": return exist_regions, True, try_minus_mask.copy(), try_minus_mask
+                else: return exist_regions, True, try_plus_mask, try_plus_mask.copy()
+        
+        
+    return exist_regions, False, plus_mask, minus_mask
+
+
+
 
 def main():
     region_uid = sys.argv[1]
@@ -51,10 +74,10 @@ def main():
     
     
     available_regions = {}
-
-    loc_file = "/depot/data2/galaxy/regions.loc"
-
-
+    
+    loc_file = "/depot/data2/galaxy/regions.loc" #"/home/dan/galaxy/universe/svnroot/galaxy/trunk/regions.loc"
+    
+    
     try:
         for line in open( loc_file ):
             if line[0:1] == "#" : continue
@@ -65,7 +88,7 @@ def main():
                 build = fields[0]
                 uid = fields[1]
                 description = fields[2]
-                filepath =fields[3].replace("\n","").replace("\r","")
+                filepath = fields[3].replace("\n","").replace("\r","")
                 available_regions[uid]=filepath
             except:
                 continue
@@ -79,102 +102,114 @@ def main():
     region_fname = available_regions[region_uid]
 
     
+    #set up bounding regions to hold random intervals
     bounds = []
-    for line in open(region_fname):
-        try:
-	        if line[0:1] == "#":
-	            continue
-	        fields = line.split("\t")
-	        b_dict = {}
-	        b_dict['chr'] = fields[0]
-	        b_dict['start'] = int(fields[1])
-	        b_dict['end'] = int(fields[2].replace("\n","").replace("\r",""))
-	        bounds.append(b_dict)
-        except:
-            continue
+    for bound in bx.intervals.io.GenomicIntervalReader( open( region_fname, 'r' ), chrom_col=0, start_col=1, end_col=2, fix_strand=True): bounds.append(bound)
     
-    regions = []
-    for i in range(len(bounds)):
-        regions.append([])
-        
-    for line in open(intervals_fname):
-        try:
-	        if line[0:1] == "#":
-	            continue
-	        fields = line.split("\t")
-	        r_dict = {}
-	        r_dict['chr'] = fields[interval_chr].replace("\n","").replace("\r","")
-	        r_dict['start'] = int(fields[interval_start].replace("\n","").replace("\r",""))
-	        r_dict['end'] = int(fields[interval_end].replace("\n","").replace("\r",""))
-	        if interval_strand < 0:
-	        	r_dict['strand'] = "+"
-	        else:
-	            try:
-	                r_dict['strand'] = fields[interval_strand].replace("\n","").replace("\r","")
-	            except:
-	                r_dict['strand'] = "+"
-	        r_dict['length'] = r_dict['end'] - r_dict['start']
-	        
-	        #loop through bounds, find first proper bounds then add in parrallel to regions
-	        #if an interval crosses bounds, it will be added to the first bound
-	        for i in range(len(bounds)):
-	            b_chr = bounds[i]['chr']
-	            if b_chr != r_dict["chr"]:
-	                continue
-	            b_start = bounds[i]['start']
-	            b_end = bounds[i]['end']
-	            
-	            if (r_dict['start'] >= b_start and r_dict['start'] <= b_end) or (r_dict['end'] >= b_start and r_dict['end'] <= b_end):
-	                regions[i].append(r_dict)
-	                break
-        except:
-            continue    
+    #set up length and number of regions to mimic
+    regions = [ [] for i in range(len(bounds)) ]
     
+    for region in bx.intervals.io.GenomicIntervalReader( open(intervals_fname, 'r' ), chrom_col=interval_chr, start_col=interval_start, end_col=interval_end, strand_col=interval_strand, fix_strand=True):
+        #loop through bounds, find first proper bounds then add
+        #if an interval crosses bounds, it will be added to the first bound
+        for i in range(len(bounds)):
+            if bounds[i].chrom != region.chrom:
+                continue
+            intersecter = bx.intervals.intersection.Intersecter()
+            intersecter.add_interval( bounds[i] )
+            if len(intersecter.find(region.start, region.end))>0:
+                regions[i].append( (region.end-region.start, region.strand) ) #add region to proper bound and go to next region
+                break
+    for region in regions:
+        region.sort()
+        region.reverse()
+    
+    #read mask file
     mask = []
     if use_mask != "no_mask":
-        for line in open(mask_fname):
-	        try:
-		        if line[0:1] == "#":
-		            continue
-		        fields = line.split("\t")
-		        m_dict = {}
-		        m_dict['chr'] = fields[mask_chr].replace("\n","").replace("\r","")
-		        m_dict['start'] = int(fields[mask_start].replace("\n","").replace("\r",""))
-		        m_dict['end'] = int(fields[mask_end].replace("\n","").replace("\r",""))
-		        mask.append(m_dict)
-	        except:
-	            continue
-
-
+        for region in bx.intervals.io.GenomicIntervalReader( open(mask_fname, 'r' ), chrom_col=mask_chr, start_col=mask_start, end_col=mask_end, fix_strand=True): mask.append(region)
     
     out_file = open (out_fname, "w") or die ("Can not open output file")
+    
     i = 0
     i_iters = 0
-    region_count = 1
-    while i < (len(bounds)):
+    region_count = 0
+    best_regions = []
+    num_fail = 0
+    while i < len(bounds):
         i_iters += 1
+        
+        #order regions to mimic
+        regions_to_mimic = regions[i][0:]
+        if len(regions_to_mimic) < 1: #if no regions to mimic, skip
+            i+=1
+            i_iters = 0
+            continue 
+        
+        #set up region mask
+        plus_mask = Region(bounds[i].end-bounds[i].start)
+        for region in mask:
+            if region.chrom != bounds[i].chrom: continue
+            mask_start = region.start - bounds[i].start
+            mask_end = region.end - bounds[i].start
+            if mask_start >= 0 and mask_end > 0:
+                plus_mask.set_range(mask_start, mask_end)
+        minus_mask = plus_mask.copy()
+        
         random_regions = []
-        added = True
+        num_added = 0
         for j in range(len(regions[i])):
-            random_regions, added = add_random_region(regions[i][j], bounds[i]['chr'], bounds[i]['start'], bounds[i]['end'], random_regions, mask, overlaps)
-            if added == False:
-                if i_iters < max_iters:
-                    i-=1
-                    break
-                else:
-                    added = True
-                    i_iters = 0
-                    print "After",str(max_iters),"x",str(max_iters),"iterations, a region could not be added."
-                    if use_mask == "use_mask":
-                        print "The mask you have provided may be too restrictive."
-                
-        if added == True:
-	        i_iters = 0
-	        for region in random_regions:
-	            print >>out_file, "%s\t%d\t%d\t%s\t%s\t%s" % ( region['chr'], region['start'], region['end'], "region_"+str(region_count), "0", region['strand'] )
-	            region_count +=1
+            random_regions, added, plus_mask, minus_mask = add_random_region(regions_to_mimic[j], bounds[i], random_regions, plus_mask, minus_mask, overlaps)
+            if added: num_added += 1
+        
+        if num_added == len(regions_to_mimic) or i_iters >= max_iters:
+            if len(best_regions) > len(random_regions): random_regions = best_regions.copy()
+            num_fail += (len(regions_to_mimic)-len(random_regions))
+            i_iters = 0
+            best_regions = []
+            for region in random_regions:
+                print >>out_file, "%s\t%d\t%d\t%s\t%s\t%s" % ( region.chrom, region.start, region.end, "region_"+str(region_count), "0", region.strand )
+                region_count += 1
+        else:
+            i-=1
+            if len(best_regions) < len(random_regions): best_regions = random_regions[:]
         
         i+=1
     
+    out_file.close()
+    if num_fail:
+        print "After %i iterations, %i regions could not be added." % (max_iters, num_fail)
+        if use_mask == "use_mask": print "The mask you have provided may be too restrictive."
+
+
+class Region( list ):
+    """
+    A list for on/off regions
+    """
+    def __init__( self, size=0 ):
+        for i in range(size): self.append(False)
+    def copy(self):
+        return deepcopy(self)
+    def set_range(self, start=0, end=None):
+        if start < 0: start = 0
+        if (not end and end != 0) or end > len(self): end = len(self)
+        for i in range(start,end): self[i]=True
+    def count_range(self, start=0, end=None):
+        if start < 0: start = 0
+        if (not end and end != 0) or end > len(self): end = len(self)
+        return self[start:end].count(True)
+    def get_gaps(self, min_size = 0):
+        gaps = []
+        start = end = 0
+        while True:
+            try: start = self[end:].index(False) + end
+            except: break
+            try: end = self[start:].index(True) + start
+            except: end = len(self)
+            if end > start and end-start >= min_size: gaps.append( ( end-start, start, end ) )
+        gaps.sort()
+        gaps.reverse()
+        return gaps
     
+
 if __name__ == "__main__": main()
