968668253686b5110989ef29e16789428438cbc2
Guruprasad Anada <gua110@bx.psu.edu>
2007-03-30 14:21:34 +0000

    Modified get_flanks.py: Changed the approach for generating output.

--- tools/new_operations/get_flanks.py
+++ tools/new_operations/get_flanks.py
@@ -26,7 +26,7 @@ size = int(sys.argv[3])
 direction = sys.argv[4]
 
 """
-Determine the number of columns in the input file and the data type for each
+Determine the number of columns in the input file
 """
 elems = []
 if os.path.exists( inp_file ):
@@ -36,96 +36,70 @@ if os.path.exists( inp_file ):
             elems = line.split( '\t' )
             break
 else:
-    stop_err('The input data file "%s" does not exist.' % inp_file)
+    print 'The input file does not exist.'
+    sys.exit()
 
 if not elems:
-    stop_err('No non-blank or non-comment lines in input data file "%s"' % inp_file)    
-
+    print 'No non-blank or non-comment lines in the input data file'
+    sys.exit()
+    
 if len(elems) < 3:
     if len(line.split()) != 1:
-        stop_err('This tool can only be run on tab delimited files')
-
-
-
-cols = []
-for ind, elem in enumerate(elems):
-    name = 'c%d' % ( ind + 1 )
-    cols.append(name)
-    
-col = ', '.join(cols)
-assign = "%s = line.split('\\t')" % col
+        print 'This tool can only be run on tab delimited data'
+        sys.exit()
 
 if len(elems) < 6:
     strand = '+'
 else:
     strand = elems[5]
 
-elems = []
-newline=''
-newline1=''
-newline2=''
-
-#newline = newline + elems[0] + '\\t' + str(elems[1]) + '\\t' + str(elems[2]) + '\\t' + elems[3] + '\\t' + elems[4] + '\\t' + elems[5] + '\\n'
-#newline = newline + elems[0] + '\\t' + elems[1] + '\\t' + str(elems[2]) + '\\t' + elems[3] + '\\t' + elems[4] + '\\t' + elems[5] + '\\n'
-            
-code = """
-for line in open ( inp_file ):
-    line = line.strip()
-    if line and not line.startswith( '#' ):
-        elems = line.split( '\\t' )
-        if direction == 'up':
-            if strand == '+':
-                elems[2] = int(elems[1])
-                elems[1] = int(elems[1]) - size
-            elif strand == '-':
-                elems[1] = elems[2]
-                elems[2] = int(elems[2]) + size
-            for elem in elems:
-                newline = newline + str(elem) + '\\t'
-            newline = newline.strip() + '\\n'
-        elif direction == 'down':
-            if strand == '-':
-                elems[2] = int(elems[1])
-                elems[1] = int(elems[1]) - size
-            elif strand == '+':
-                elems[1] = elems[2]
-                elems[2] = int(elems[2]) + size
-            for elem in elems:
-                newline = newline + str(elem) + '\\t'
-            newline = newline.strip() + '\\n'
-    	elif direction == 'both':
-            newelem1 = int(elems[1]) - size
-            newelem2 = elems[1]
-            newelem3 = elems[2]
-            newelem4 = int(elems[2]) + size
-            elems[1]=newelem1
-            elems[2]=newelem2
-            newline1=''
-            for elem in elems:
-                newline1 = newline1 + str(elem) + '\\t' #elems[0] + '\\t' + str(newelem1) + '\\t' + newelem2 + '\\t' + elems[3] + '\\t' + elems[4] + '\\t' + elems[5] + '\\n'
-            elems[1]=newelem3
-            elems[2]=newelem4
-            newline2=''
-            for elem in elems:
-                newline2 = newline2 + str(elem) + '\\t' #newline2 = elems[0] + '\\t' + newelem3 + '\\t' + str(newelem4) + '\\t' + elems[3] + '\\t' + elems[4] + '\\t' + elems[5] + '\\n'
-            newline = newline + newline1.strip() + '\\n' + newline2.strip() + '\\n'  
-	
-    elif line == '':
-        break
-"""
-
-exec code
-
-#
-# create output
-#
 try:
     fo = open(out_file,'w')
 except:
     print >> sys.stderr, "Unable to open output file"
-    sys.exit(0)
-fo.write(newline)
+    sys.exit()
+
+
+elems = []
+for line in open ( inp_file ):
+    line = line.strip()
+    if line and (not line.startswith( '#' )) and line != '':
+        elems = line.split( '\t' )
+        if direction == 'up':
+            if strand == '+':
+                elems[2] = elems[1]
+                elems[1] = str(int(elems[1]) - size)
+            elif strand == '-':
+                elems[1] = elems[2]
+                elems[2] = str(int(elems[2]) + size)
+            print >>fo, '\t'.join(elems)
+            
+        elif direction == 'down':
+            if strand == '-':
+                elems[2] = elems[1]
+                elems[1] = str(int(elems[1]) - size)
+            elif strand == '+':
+                elems[1] = elems[2]
+                elems[2] = str(int(elems[2]) + size)
+            print >>fo, '\t'.join(elems)
+            
+    	elif direction == 'both':
+            newelem1 = str(int(elems[1]) - size)
+            newelem2 = elems[1]
+            newelem3 = elems[2]
+            newelem4 = str(int(elems[2]) + size)
+            elems[1]=newelem1
+            elems[2]=newelem2
+            print >>fo, '\t'.join(elems)
+            elems[1]=newelem3
+            elems[2]=newelem4
+            print >>fo, '\t'.join(elems)
+
 fo.close()
+
+
+
+
 
 
 
