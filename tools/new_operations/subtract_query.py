39f811eff7f29b42b397d44fa24fbc8b5f69d610
Greg Von Kuster <greg@bx.psu.edu>
2007-05-07 18:59:15 +0000

    Added new Subtract Whole Query tool.  Also some miscellaneous code clean-up.

---
+++ tools/new_operations/subtract_query.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2.4
+# Greg Von Kuster
+
+"""
+Subtract an entire query from another query
+usage: %prog in_file_1 in_file_2 out_file
+"""
+
+import sys, sets
+import cookbook.doc_optparse
+
+
+def get_lines(fname):
+    lines = set([])
+    for i, line in enumerate(file(fname)):
+        line = line.strip()
+        lines.add( line )
+    return lines
+
+def main():   
+    # Parsing Command Line here
+    options, args = cookbook.doc_optparse.parse( __doc__ )
+    
+    try:
+        inp1_file, inp2_file, out_file = args
+    except:
+        cookbook.doc_optparse.exception()
+
+    try:
+        fo = open(out_file,'w')
+    except:
+        print >> sys.stderr, "Unable to open output file"
+        sys.exit()
+
+    lines1 = get_lines(inp1_file)
+    lines2 = get_lines(inp2_file)
+    lines1.difference_update(lines2)
+    
+    for line in lines1:
+        print >> fo, line
+
+    fo.close()
+    
+if __name__ == "__main__":
+    main()
