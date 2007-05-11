41edf2c852096c11b18c119b66529652bfd572b0
Daniel Blankenberg <dan@bx.psu.edu>
2007-05-11 16:39:15 +0000

    Can now delete abandoned histories via the admin interface.

--- lib/galaxy/interfaces/admin.py
+++ lib/galaxy/interfaces/admin.py
@@ -17,6 +17,8 @@ class Admin( common.Root ):
                 msg = self.tool_reload( **kwd )
             elif kwd['action'] == "purge":
                 msg = self.purge( **kwd )
+            elif kwd['action'] == "abandon":
+                msg = self.abandon( **kwd )
         
         users, data = [], []
         for row in trans.model.User.table.select().execute():
@@ -27,6 +29,47 @@ class Admin( common.Root ):
         qsize = self.app.job_queue.queue.qsize() 
         return trans.fill_template('admin_main.tmpl', toolbox=self.app.toolbox, users=users, data=data, qsize=qsize,msg=msg)
         
+    
+    def abandon( self, **kwd ):
+        params = util.Params(kwd)
+        msg = ''
+        if params.days:
+            if params.passwd==self.app.config.admin_pass:
+                days = int(params.abandon_days)
+                
+                histories_no_users = []
+                history_count = 0
+                for row in self.app.model.History.table.select().execute():
+                    try:
+                        int(row.user_id)
+                    except:
+                        history = self.app.model.History.get(row.id)
+                        now  = time.time()
+                        last = time.mktime( time.strptime( history.update_time.strftime('%a %b %d %H:%M:%S %Y') )) 
+                        diff = (now - last) /3600/24 # days
+                        if diff>days:
+                            histories_no_users.append(row.id)
+                            if history.deleted: continue #we don't add to history delete count, but we do want to be able to make sure datasets associated with deleted histories are really deleted
+                            history.deleted = True
+                            history_count += 1
+                
+                dataset_count = 0
+                for row in self.app.model.Dataset.table.select().execute():
+                    if row.history_id in histories_no_users:
+                        #delete dataset
+                        data = self.app.model.Dataset.get(row.id)
+                        if data.deleted: continue
+                        data.deleted = True
+                        dataset_count += 1
+                try:
+                    self.app.model.flush()
+                except:
+                    pass
+                msg = 'deleted %d abandoned histories with %d total datasets' % ( history_count, dataset_count )
+            else:
+                msg = 'Invalid password'
+        return msg
+    
     def purge( self, **kwd ):
         """ Purges deleted datasets older than specified number of days """
         params = util.Params(kwd)
