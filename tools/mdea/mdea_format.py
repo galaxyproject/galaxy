a768803972a5156f8142227a77a9f23490208166
Ian Schenck <ian@bx.psu.edu>
2007-05-21 16:27:07 +0000

    Baseml booted.  Goodbye baseml.

--- tools/mdea/mdea_format.py
+++
@@ -1,8 +0,0 @@
-from galaxy import datatypes
-def exec_after_process(app, inp_data, out_data, param_dict,tool, stdout, stderr):
-    ext   = param_dict.get('out_format', 'html')
-    items = out_data.items()   
-    for name, data in items: 
-        data =  datatypes.change_datatype(data, ext)
-        data.flush()
-    app.model.flush()
