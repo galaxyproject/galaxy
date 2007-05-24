03ac41cfaa309e2e8ccd3116cc195680771d1da1
Ian Schenck <ian@bx.psu.edu>
2007-05-24 20:48:08 +0000

    The source of the ever elusive NoneType has been found.  If value is never set, then it never exists, and str( value ) simply returns nothing at all, which is different than returning None?  I don't know, but wrapping value as str( value ) doesn't work while repr( value ) does.  This works as well, and avoids using repr which...might be a bad idea?  I'm in over my head when it comes to repr() and I would prefer to familiarize myself with it another time.  In short, those responsible for the sacking have been sacked.

--- lib/galaxy/tools/parameters.py
+++ lib/galaxy/tools/parameters.py
@@ -441,6 +441,7 @@ class SelectToolParameter( ToolParameter ):
                 assert self.multiple, "Multiple values provided but parameter is not expecting multiple values"
             return self.separator.join( value )
         else:
+            if not value: return "None"
             return value
     def value_to_basic( self, value, app ):
         return value
