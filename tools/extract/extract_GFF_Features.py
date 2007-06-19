4919d2d04815c8a26baa24e992801ed69f23cce5
Guruprasad Anada <gua110@bx.psu.edu>
2007-06-19 14:04:41 +0000

    Adding new tool "Extract features from GFF file" under Fetch Sequences and Alignments section. Have not been able to get this tool to pass functional test as test support with the new conditional constructs is yet to be worked out.

---
+++ tools/extract/extract_GFF_Features.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2.4
+
+"""
+Extract features from GFF file.
+
+usage: %prog input1 out_file1 column features out_format
+"""
+
+import sys, os
+import cookbook.doc_optparse
+
+def main():   
+    # Parsing Command Line here
+    options, args = cookbook.doc_optparse.parse( __doc__ )
+    
+    try:
+        inp_file, out_file, column, features = args
+        column = int(column)
+    except:
+        cookbook.doc_optparse.exception()
+    
+    if features == None:
+        print "Column %d has no features to display. Please select another column." %(column+1)
+        sys.exit()
+
+    try:
+        fo=open(out_file,'w')
+        for line in open(inp_file):
+            if line[0] == '#' or not(line) or line == "":
+                print >>fo, line.strip()
+                continue
+            try:
+                if line.split('\t')[column] in features.split(','):
+                    print >>fo, line.strip()
+            except:
+                pass
+        fo.close()
+    except Exception, exc:
+        print >> sys.stderr, exc
+            
+    print 'Field = Column %d and Features = %s' %(column+1, features)
+
+if __name__ == "__main__":
+    main()       
