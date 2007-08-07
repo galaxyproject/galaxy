ca2dfc6109ff3b0ec838547683fe095cabf0972e
Greg Von Kuster <greg@bx.psu.edu>
2007-08-07 13:17:51 +0000

    Better implementation of column restriction in subtract whole query tool.  Eliminated old show_in_ucsc tool from tool_conf.xml.sample.

--- tools/new_operations/subtract_query.py
+++ tools/new_operations/subtract_query.py
@@ -4,7 +4,6 @@
 """
 Subtract an entire query from another query
 usage: %prog in_file_1 in_file_2 begin_col end_col output 
-    -n, --num_cols=N,N: Number of columns in tab-delimited files
 """
 
 import sys, sets, re
@@ -32,101 +31,36 @@ def main():
     
     # Parsing Command Line here
     options, args = doc_optparse.parse( __doc__ )
-    
-    try:
-        num_columns1, num_columns2 = options.num_cols.split(',')
-    except:
-        num_columns1 = num_columns2 = ''
+
     try:
         inp1_file, inp2_file, begin_col, end_col, out_file = args
     except:
-        try:
-            inp1_file, inp2_file, out_file = args
-            begin_col = end_col = ''
-        except:
-            doc_optparse.exception()
+        doc_optparse.exception()
     
-    """
-    If we are to restrict to specific columns, make sure input datasets are tabular.
-    We'll allow default values for both begin_col and end_col.  If the user enters a
-    begin_col, but not an end_col, end_col will be set to input1_columns.  If the 
-    user enters an end_col but not a begin_col, begin_col will be set to 1.
-    """
-    begin_col_is_valid = end_col_is_valid = False
-    if begin_col or end_col:
+    begin_col = begin_col.strip()
+    end_col = end_col.strip()
+    
+    if begin_col != 'None' or end_col != 'None':
         """
-        First we'll determine if we have tabular queries.
+        The user selected columns for restriction.  We'll allow default
+        values for both begin_col and end_col as long as the user selected
+        at least one of them for restriction.
         """
-        is_tabular1 = sniff.is_column_based(inp1_file, sep='\t')
-        is_tabular2 = sniff.is_column_based(inp2_file, sep='\t')
-        if is_tabular1 and is_tabular2:
-            try:
-                num_columns1 = int(num_columns1)
-                num_columns2 = int(num_columns2)
-                are_tabular = True
-            except:
-                are_tabular = False
-        else:
-            are_tabular = False
-
-        if are_tabular:
-            """
-            Next we'll ensure that the user entered valid begin_col and end_col
-            values for the first query.
-            """
-            if begin_col and re.compile('c[0-9]+').findall(begin_col):
-                try:
-                    begin_col = int(begin_col[1:])
-                    if begin_col > 0 and begin_col <= num_columns1:
-                        begin_col_is_valid = True
-                    else:
-                        begin_col_is_valid = False
-                except:
-                    begin_col_is_valid = False
-            else:
-                begin_col_is_valid = False
-            if end_col and re.compile('c[0-9]+').findall(end_col):
-                try:
-                    end_col = int(end_col[1:])
-                    if end_col > 1 and end_col <= num_columns1:
-                        end_col_is_valid = True
-                    else:
-                        end_col_is_valid = False
-                except:
-                    end_col_is_valid = False
-            else:
-                end_col_is_valid = False
-            """
-            Next we'll set defaults for begin_col or end_col if the user
-            left one (but not both) of them blank.
-            """
-            if begin_col_is_valid and not end_col:
-                end_col = num_columns1
-                end_col_is_valid = True
-            elif not begin_col and end_col_is_valid:
-                begin_col = 1
-                begin_col_is_valid = True
-            """
-            Next we want to make sure that begin_col <= end_col
-            """
-            if begin_col > end_col:
-                begin_col_is_valid = end_col_is_valid = False
-            """
-            Next we'll ensure that begin_col and end_col are valid
-            for the second query.
-            """
-            if begin_col_is_valid and end_col_is_valid:
-                if begin_col > num_columns2:
-                    begin_col_is_valid = False
-                if end_col > num_columns2:
-                    end_col_is_valid = False
-            """
-            Finally, if all is not well, we'll blank out begin_col and end_col.
-            """
-            if not (begin_col_is_valid and end_col_is_valid):
-                begin_col = end_col = ''
-        else:
-            begin_col = end_col = ''
+        if begin_col == 'None':
+            begin_col = end_col
+        elif end_col == 'None':
+            end_col = begin_col
+        begin_col = int(begin_col)
+        end_col = int(end_col)
+        """
+        Make sure that begin_col <= end_col (switch if not)
+        """
+        if begin_col > end_col:
+            tmp_col = end_col
+            end_col = begin_col
+            begin_col = tmp_col
+    else:
+        begin_col = end_col = ''
 
     try:
         fo = open(out_file,'w')
---
+++ tools/new_operations/subtract_query_code.py
@@ -0,0 +1,16 @@
+from galaxy.datatypes import *
+
+#return set of columns contained in both input datasets
+def get_columns( input1, input2 ):
+    columns = []
+    """
+    Placing a '?' in the first option will keep 'c1' from being automatically
+    selected if the user does nothing.  Not sure why this is the behavior...
+    """
+    columns.append(('?','None',False))
+    if isinstance(input1.datatype, interval.Tabular().__class__) and isinstance(input2.datatype, interval.Tabular().__class__):
+        num_columns = min(input1.metadata.columns, input2.metadata.columns)
+        for col in range(1, num_columns+1):
+            option = "c" + str(col)
+            columns.append((option,str(col),False))
+    return columns
