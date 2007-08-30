eb580d6ec05b359af920fbf14662897316146a62
Daniel Blankenberg <dan@bx.psu.edu>
2007-08-30 18:44:51 +0000

    Bug fix for thread for species.

--- tools/filters/maf/maf_thread_for_species.py
+++ tools/filters/maf/maf_thread_for_species.py
@@ -13,36 +13,35 @@ import sys
 import pkg_resources; pkg_resources.require( "bx-python" )
 import bx.align.maf
 
-import bx.align.tools.thread
-import bx.align.tools.fuse
+from bx.align.tools.thread import *
+from bx.align.tools.fuse import *
 
 def main():
 
     input_file = sys.argv.pop(1)
     output_file = sys.argv.pop(1)
     species = sys.argv.pop(1).split(',')
-
     
     try:
         maf_reader = bx.align.maf.Reader( open(input_file) )
     except:
-        print sys.stderr, "Unable to open source MAF file"
+        print >> sys.stderr, "Unable to open source MAF file"
         sys.exit()
     try:
-        maf_writer = bx.align.tools.fuse.FusingAlignmentWriter( bx.align.maf.Writer( open(output_file, 'w') ) )
-    except:
-        print sys.stderr, "Unable to open output file"
+        maf_writer = FusingAlignmentWriter( bx.align.maf.Writer( open(output_file, 'w') ) )
+    except Exception, e:
+        print >> sys.stderr, "Unable to open output file", e
         sys.exit()
     try:
         for m in maf_reader:            
-            new_components = bx.align.tools.thread.get_components_for_species( m, species )
+            new_components = get_components_for_species( m, species )
             if new_components: 
-                bx.align.tools.thread.remove_all_gap_columns( new_components )
+                remove_all_gap_columns( new_components )
                 m.components = new_components
                 m.score = 0.0 
                 maf_writer.write( m )
     except:
-        print sys.stderr, "Error steping through MAF File"
+        print >> sys.stderr, "Error steping through MAF File"
         sys.exit()
     maf_reader.close()
     maf_writer.close()
