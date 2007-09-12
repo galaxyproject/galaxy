20244dabf6b7fdc9c7505d7cd785aa4d27d94a78
Guruprasad Anada <gua110@bx.psu.edu>
2007-09-12 18:54:10 +0000

    Modified "UCSC gene BED to Exon/intron BED" tool to include an option for extracting CODONS.

--- tools/filters/ucsc_gene_bed_to_exon_bed.py
+++ tools/filters/ucsc_gene_bed_to_exon_bed.py
@@ -9,7 +9,7 @@ usage: ucsc_gene_table_to_intervals.py [options]
 options:
   -h, --help                  show this help message and exit
   -rREGION, --region=REGION
-                              Limit to region: one of coding, utr3, utr5, transcribed [default]
+                              Limit to region: one of coding, utr3, utr5, codon, intron, transcribed [default]
   -e, --exons                 Only print intervals overlapping an exon
   -i, --input=inputfile       input file
   -o, --output=outputfile     output file
@@ -32,7 +32,7 @@ def main():
     parser.add_option( "-o", "--output", dest="output", default=None,
                        help="Output file" )
     options, args = parser.parse_args()
-    assert options.region in ( 'coding', 'utr3', 'utr5', 'transcribed', 'intron' ), "Invalid region argument"
+    assert options.region in ( 'coding', 'utr3', 'utr5', 'transcribed', 'intron', 'codon' ), "Invalid region argument"
     
     try:
         out_file = open (options.output,"w")
@@ -55,9 +55,7 @@ def main():
     
     # Read table and handle each gene
     for line in in_file:
-	
         try:
-	    
             if line[0:1] == "#":
                 continue
             # Parse fields from gene tabls
@@ -69,8 +67,7 @@ def main():
             strand    = fields[5].replace(" ","_")
             cds_start = int( fields[6] )
             cds_end   = int( fields[7] )
-	    
-	    	
+
             # Determine the subset of the transcribed region we are interested in
             if options.region == 'utr3':
                 if strand == '-': region_start, region_end = tx_start, cds_start
@@ -78,39 +75,67 @@ def main():
             elif options.region == 'utr5':
                 if strand == '-': region_start, region_end = cds_end, tx_end
                 else: region_start, region_end = tx_start, cds_start
-            elif options.region == 'coding':
+            elif options.region == 'coding' or options.region == 'codon':
                 region_start, region_end = cds_start, cds_end
             else:
                 region_start, region_end = tx_start, tx_end
 
             # If only interested in exons, print the portion of each exon overlapping
             # the region of interest, otherwise print the span of the region
-	    # options.exons is always TRUE
+        # options.exons is always TRUE
             if options.exons:
                 exon_starts = map( int, fields[11].rstrip( ',\n' ).split( ',' ) )
                 exon_starts = map((lambda x: x + tx_start ), exon_starts)
                 exon_ends = map( int, fields[10].rstrip( ',\n' ).split( ',' ) )
                 exon_ends = map((lambda x, y: x + y ), exon_starts, exon_ends);
-		#for Intron regions:
-		if options.region == 'intron':
-			i=0
-	    		while i < len(exon_starts)-1:
-            			intron_starts = exon_ends[i]
-				intron_ends = exon_starts[i+1]
-				if strand: print_tab_sep(out_file, chrom, intron_starts, intron_ends, name, "0", strand )
-                		else: print_tab_sep(out_file, chrom, intron_starts, intron_ends )
-				i+=1
-		#for non-intron regions:
-		else:
-                	for start, end in zip( exon_starts, exon_ends ):
-                    		start = max( start, region_start )
-                    		end = min( end, region_end )
-                    		if start < end:
-                        		if strand: print_tab_sep(out_file, chrom, start, end, name, "0", strand )
-                        		else: print_tab_sep(out_file, chrom, start, end )
+
+        #for Intron regions:
+            if options.region == 'intron':
+                i=0
+                while i < len(exon_starts)-1:
+                    intron_starts = exon_ends[i]
+                    intron_ends = exon_starts[i+1]
+                    if strand: print_tab_sep(out_file, chrom, intron_starts, intron_ends, name, "0", strand )
+                    else: print_tab_sep(out_file, chrom, intron_starts, intron_ends )
+                    i+=1
+        #for non-intron regions:
             else:
-                if strand: print_tab_sep(out_file, chrom, region_start, region_end, name, "0", strand )
-                else: print_tab_sep(out_file, chrom, region_start, region_end )
+                for start, end in zip( exon_starts, exon_ends ):
+                    start = max( start, region_start )
+                    end = min( end, region_end )
+                    if start < end:
+                        if options.region == 'codon':
+                            c_start = start
+                            while c_start+3 <= end:
+                                if strand:
+                                    print_tab_sep(out_file, chrom, c_start, c_start+3, name, "0", strand )
+                                else:
+                                    print_tab_sep(out_file, chrom, c_start, c_start+3)
+                                c_start += 3
+                        else:
+                            if strand:
+                                print_tab_sep(out_file, chrom, start, end, name, "0", strand )
+                            else: 
+                                print_tab_sep(out_file, chrom, start, end )
+                    else:
+                        if options.region == 'codon':
+                            c_start = region_start
+                            c_end = region_end
+                            if c_start > c_end:
+                                t = c_start
+                                c_start = c_end
+                                c_end = t
+                            while c_start+3 <= c_end:
+                                if strand:
+                                    print_tab_sep(out_file, chrom, c_start, c_start+3, name, "0", strand )
+                                else:
+                                    print_tab_sep(out_file, chrom, c_start, c_start+3)
+                                c_start += 3
+                        else:
+                            if strand:
+                                print_tab_sep(out_file, chrom, region_start, region_end, name, "0", strand )
+                            else: 
+                                print_tab_sep(out_file, chrom, region_start, region_end )
         except:
             continue
 
