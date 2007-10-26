276c63fe18a841f4b100a807fdd0fae41acc4e0f
Greg Von Kuster <greg@bx.psu.edu>
2007-10-26 12:44:13 +0000

    Modified some of the maf (~/filters) tools to use new dynamic options functionality.

--- tools/filters/maf/maf_by_block_number_code.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python2.4
-#Dan Blankenberg
-"""
-Returns valid column numbers, defaulting to 1 for text files
-"""
-
-def get_columns(dataset):
-    if not dataset.metadata.columns: return [("1", "1", True)]
-    return map(lambda col: (str(col), str(col), False), range(1, dataset.metadata.columns+1))
--- tools/filters/maf/maf_to_bed_code.py
+++ tools/filters/maf/maf_to_bed_code.py
@@ -1,54 +1,8 @@
 import pkg_resources; pkg_resources.require( "bx-python" )
 from bx.align import maf
-# No initialization required.
-
-    
-#return lists of species available, showing gapped and ungapped base counts
-def get_available_species( dbkey, input_filename ):
-    try:
-        rval = []
-        species={}
-        
-        
-        try:
-            file_in = open(input_filename, 'r')
-            maf_reader = maf.Reader( file_in )
-            
-            
-            for i, m in enumerate( maf_reader ):
-                l = m.components
-                for c in l:
-                    spec,chrom = maf.src_split( c.src )
-                    if not spec or not chrom:
-                        spec = chrom = c.src
-                    if spec not in species:
-                        species[spec]={"bases":0,"nongaps":0}
-                    species[spec]["bases"] = species[spec]["bases"] + c.size + c.text.count("-")
-                    species[spec]["nongaps"] = species[spec]["nongaps"] + c.size 
-            
-            file_in.close()
-        except:
-            return [("There is a problem with your MAF file",'None',True)]
-        species_names = species.keys()
-        species_names.sort()
-        
-        for spec in species_names:
-            #species_sequence[spec] = "".join(species_sequence[spec])
-            
-            if spec == dbkey:
-                display = "<b>%s: %i nongap, %i total bases</b>" % (spec, species[spec]["nongaps"], species[spec]["bases"] )
-                rval.append( ( display,spec,True) )
-            else:
-                display = "%s: %i nongap, %i total bases" % (spec, species[spec]["nongaps"], species[spec]["bases"] )
-                rval.append( ( display,spec,False) )
-            
-        return rval
-    except:
-        return [("Include all species.",'None',True)]
-
-
 from galaxy import datatypes, config, jobs 
 from shutil import move
+
 def exec_after_process(app, inp_data, out_data, param_dict, tool, stdout, stderr):
     output_data = out_data.items()[0][1]
     history = output_data.history
