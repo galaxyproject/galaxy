cd58c911040fe4952f71b0731d90a7ff439f204b
Jianbin He <jbhe@bx.psu.edu>
2007-10-31 19:12:00 +0000

    add test section option

--- run_functional_tests.sh
+++ run_functional_tests.sh
@@ -3,23 +3,27 @@
 # A good place to look for nose info: http://somethingaboutorange.com/mrl/projects/nose/
 
 rm -f run_functional_tests.log 
-echo "'run_functional_tests.sh help'                  for help"
 
 if [ ! $1 ]; then
 	./nosetests.sh -v -w test --with-nosehtml --html-report-file run_functional_tests.html --exclude="^get" functional
 elif [ $1 = 'help' ]; then
 	echo "'run_functional_tests.sh'                       for testing all the tools in functional directory"
-	echo "'run_functional_tests.sh aaa'                   for testing one test case, 'aaa'"
-	echo "'run_functional_tests.sh -id bbb'               for testing one tool with id 'bbb'"
+	echo "'run_functional_tests.sh aaa'                   for testing one test case of 'aaa' ('aaa' is the file name with path)"
+	echo "'run_functional_tests.sh -id bbb'               for testing one tool with id 'bbb' ('bbb' is the tool id)"
+	echo "'run_functional_tests.sh -sid ccc'              for testing one section with sid 'ccc' ('ccc' is the string after 'section::')"
 	echo "'run_functional_tests.sh -list'                 for listing all the tool ids"
 elif [ $1 = '-id' ]; then
 	./nosetests.sh -v -w test functional.test_toolbox:TestForTool_$2 --with-nosehtml --html-report-file run_functional_tests.html
+elif [ $1 = '-sid' ]; then
+        ./nosetests.sh --with-nosehtml --html-report-file run_functional_tests.html -v -w test `python tool_list.py $2`
 elif [ $1 = '-list' ]; then
-    python tool_list.py
+        python tool_list.py
 	echo "==========================================================================================================================================="
-    echo "'run_functional_tests.sh help'                  for help"
-	echo "'run_functional_tests.sh -id bbb'               for testing one tool with id 'bbb'"
+	echo "'run_functional_tests.sh -id bbb'               for testing one tool with id 'bbb' ('bbb' is the tool id)"
+	echo "'run_functional_tests.sh -sid ccc'              for testing one section with sid 'ccc' ('ccc' is the string after 'section::')"
 else
 	./nosetests.sh -v -w test --with-nosehtml --html-report-file run_functional_tests.html $1
 fi
 
+echo "'run_functional_tests.sh help'                  for help"
+echo "Note: galaxy test framework uses tool_conf.xml.sample, not tool_conf.xml"
--- tool_list.py
+++ tool_list.py
@@ -1,9 +1,9 @@
-import os
+import os,sys
 
-#--------read tool_conf.xml to get all the tool xml file names-----------
+#--------read tool_conf.xml.sample to get all the tool xml file names-----------
 onoff = 1
 tool_list = []
-for line in open("tool_conf.xml", "r"):
+for line in open("tool_conf.xml.sample", "r"):
    if line.find("<!--") != -1: 
       onoff = 0
    if line.find("file") != -1 and onoff==1: 
@@ -17,7 +17,7 @@ for line in open("tool_conf.xml", "r"):
          if keys[n].find("id") != -1 : strtmp = strtmp + keys[n+1]  
          if keys[n].find("name") != -1 : strtmp = strtmp + keys[n+1] + "-" 
 	 n = n + 1
-      tool_list.append(strtmp)
+      tool_list.append(strtmp.replace(' ', '_'))
    if line.find("-->") != -1:
       onoff =1
 
@@ -45,13 +45,24 @@ for tool in tool_list :
 		n = n + 1
 	     tool_infos.append(tool_info)
 	     break
-for tool_info in tool_infos: 
-   if tool_info["id"].find("section") != -1 : 
-      print "==========================================================================================================================================="
-      print "%-45s\t%-40s\t%s" % ("id", "name", tool_info["id"])
-      print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-   else :
-      print "%-45s\t%-40s" % (tool_info["id"], tool_info["name"])
+
+flag=0
+if len(sys.argv) == 1 : 
+   for tool_info in tool_infos: 
+      if tool_info["id"].find("section") != -1 : 
+         print "==========================================================================================================================================="
+         print "%-45s\t%-40s\t%s" % ("id", "name", tool_info["id"])
+         print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+      else :
+         print "%-45s\t%-40s" % (tool_info["id"], tool_info["name"])
+else: 
+   for tool_info in tool_infos: 
+      if tool_info["id"].find("section") != -1 :
+         flag=0
+      elif flag==1:
+         print " functional.test_toolbox:TestForTool_%s" % tool_info["id"],
+      if tool_info["id"].replace('section::', '')==sys.argv[1]:  
+         flag=1
 
 #for key in tool_infos.keys():
 #   print tool_infos[key]["id"], "\t", tool_infos[key]["name"], "\t", tool_infos[key]["desc"]
