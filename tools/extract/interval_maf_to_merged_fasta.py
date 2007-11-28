83d2926577b8fe0f22f9fefbb9dba3fe5569ee55
Daniel Blankenberg <dan@bx.psu.edu>
2007-11-28 18:12:03 +0000

    Enhance interval_maf stitching tool to work when no species are selected ==> defaults to providing all species available.

--- tools/extract/interval_maf_to_merged_fasta.py
+++ tools/extract/interval_maf_to_merged_fasta.py
@@ -38,6 +38,15 @@ class RegionAlignment( object ):
         self.sequences[species] = { 'file':os.fdopen( fd, 'w+' ), 'path':file_path }
         self.sequences[species]['file'].write( "-" * self.size )
     
+    #returns the names for species found in alignment, skipping names as requested
+    def get_species_names( self, skip = [] ):
+        if not isinstance( skip, list ): skip = [skip]
+        names = self.sequences.keys()
+        for name in skip:
+            try: names.remove( name )
+            except: pass
+        return names
+    
     #returns the sequence for a species
     def get_sequence( self, species ):
         self.sequences[species]['file'].seek( 0 )
@@ -53,6 +62,7 @@ class RegionAlignment( object ):
     def set_position( self, index, species, base ):
         if index >= self.size or index < 0: raise "Your index (%i) is out of range (0 - %i)." % ( index, self.size - 1 )
         if len(base) != 1: raise "A genomic position can only have a length of 1."
+        if species not in self.sequences.keys(): self.add_species( species )
         self.sequences[species]['file'].seek( index )
         self.sequences[species]['file'].write( base )
         
@@ -114,8 +124,11 @@ def __main__():
     primary_species = sys.argv.pop( 1 )
     secondary_species = sys.argv.pop( 1 ).split( "," )
     include_primary = True
-    try: secondary_species.remove( primary_species )
-    except: include_primary = False
+    if secondary_species == ["None"]:
+        secondary_species = None
+    else:
+        try: secondary_species.remove( primary_species )
+        except: include_primary = False
     maf_identifier = sys.argv.pop( 1 )
     interval_file = sys.argv.pop( 1 )
     output_file = sys.argv.pop( 1 )
@@ -140,7 +153,7 @@ def __main__():
     if maf_source_type.lower() in ["cached"]:
         index = maf_index_by_uid( maf_identifier )
         if index is None:
-            print >> sys.stderr, "The MAF source specified (%s) appears to be invalid." % ( maf_uid )
+            print >> sys.stderr, "The MAF source specified (%s) appears to be invalid." % ( maf_identifier )
             sys.exit()
     elif maf_source_type.lower() in ["user"]:
         #index maf for use here, need to remove index_file when finished
@@ -160,7 +173,8 @@ def __main__():
     line_count = 0
     for line_count, region in enumerate( bx.intervals.io.NiceReaderWrapper( open(interval_file, 'r' ), chrom_col = chr_col, start_col = start_col, end_col = end_col, strand_col = strand_col, fix_strand = True, return_header = False, return_comments = False ) ):
         #create alignment object
-        alignment = RegionAlignment( region.end - region.start, [primary_species] + secondary_species )
+        if secondary_species is not None: alignment = RegionAlignment( region.end - region.start, [primary_species] + secondary_species )
+        else: alignment = RegionAlignment( region.end - region.start, primary_species )
         primary_src = "%s.%s" % ( primary_species, region.chrom )
         
         #Get blocks overlaping this position
@@ -180,7 +194,8 @@ def __main__():
             #Get maf block
             maf = blocks[block_index]
             #Limit maf block to desired species
-            maf = maf.limit_to_species( [primary_species] + secondary_species )
+            if secondary_species is not None:
+                maf = maf.limit_to_species( [primary_species] + secondary_species )
             #Colapse extraneous gap columns
             maf.remove_all_gap_columns()
             #Positions and strand are in reference to ref
@@ -206,10 +221,8 @@ def __main__():
                 if ref.text[i] in ["-"]:
                     num_gaps += 1
                     continue
-                #Set base for primary species
-                alignment.set_position( start_offset + i - num_gaps, primary_species, ref.text[i] )
-                #Set base for secondary species
-                for spec in secondary_species:
+                #Set base for all species
+                for spec in [ c.src.split( '.' )[0] for c in maf.components ]:
                     try:
                         #NB: If a gap appears in higher scoring secondary species block,
                         #it will overwrite any bases that have been set by lower scoring blocks
@@ -229,7 +242,7 @@ def __main__():
                 output.write( alignment.get_sequence( primary_species ) )
             output.write( "\n" )
         #Output all remainging species
-        for spec in secondary_species:
+        for spec in secondary_species or alignment.get_species_names( skip = primary_species ):
             output.write( ">%s\n" % ( spec ) )
             if region.strand == "-":
                 output.write( alignment.get_sequence_reverse_complement( spec ) )
