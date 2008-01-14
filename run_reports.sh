3b1ba684da99536f5ef263b302b6d679b6c4f543
Greg Von Kuster <greg@bx.psu.edu>
2008-01-14 16:29:27 +0000

    Fixed command line and port for reports.

--- run_reports.sh
+++ run_reports.sh
@@ -2,4 +2,4 @@
 
 source setup_paths.sh
 
-python2.4 ./scripts/paster.py serve reports_wsgi.ini $@
+python2.4 ./scripts/paster.py serve reports_wsgi.ini --pid-file=reports_webapp.pid --log-file=reports_webapp.log $@
