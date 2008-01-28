32ccd3570c5e1e5a58d9be63b8d6a9de35f7793b
Greg Von Kuster <greg@bx.psu.edu>
2008-01-28 21:25:33 +0000

    Fixes for tabular_to_fasta and fasta_to_tabular tools - requires update to datatype_converters_conf.xml - Use ColumnListParameter rather than TextToolParameter - Use better naming convertions for code and test-data files - Added fasta_to_tabular converter to datatype_converters_conf.xml - Fixed tool-conf so the converters are displayed in the Converters tool section - Other miscellaneous cleanup.

---
+++ tools/fasta_tools/fasta_to_tabular.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python
+# This code exists in 2 places: ~/datatypes/converters and ~/tools/fasta_tools
+"""
+Input: fasta, minimal length, maximal length
+Output: fasta
+Return sequences whose lengths are within the range.
+Wen-Yu Chung
+"""
+
+import sys, os
+
+seq_hash = {}
+
+def __main__():
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+    title = ''
+    sequence = ''
+    sequence_count = 0
+    for i, line in enumerate( open( infile ) ):
+        line = line.rstrip( '\r\n' )
+        if line.startswith( '>' ):
+            if sequence:
+                sequence_count += 1
+                seq_hash[( sequence_count, title )] = sequence
+            title = line
+            sequence = ''
+        else:
+            sequence += line
+            if line.split()[0].isdigit():
+                sequence += ' '
+    if sequence:
+        seq_hash[( sequence_count, title )] = sequence
+    # return only those lengths are in the range
+    out = open( outfile, 'w' )
+    title_keys = seq_hash.keys()
+    title_keys.sort()
+    for i, fasta_title in title_keys:
+        sequence = seq_hash[( i, fasta_title )]
+        print >> out, "%s\t%s" %( fasta_title, sequence )
+    out.close()
+
+if __name__ == "__main__" : __main__()
---
+++ tools/fasta_tools/tabular_to_fasta.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python
+"""
+Input: fasta, minimal length, maximal length
+Output: fasta
+Return sequences whose lengths are within the range.
+Wen-Yu Chung
+"""
+
+import sys, os
+
+seq_hash = {}
+
+def stop_err( msg ):
+    sys.stderr.write( msg )
+    sys.exit()
+
+def __main__():
+    infile = sys.argv[1]
+    title_col = sys.argv[2]
+    seq_col = sys.argv[3]
+    outfile = sys.argv[4]        
+
+    if title_col == None or title_col == 'None' or seq_col == None or seq_col == 'None':
+        stop_err( "Columns not specified." )
+    try:
+        seq_col = int( seq_col ) - 1
+    except:
+        stop_err( "Invalid Sequence Column: %s." %seq_col )
+
+    title_col_list = title_col.split( ',' )
+    out = open( outfile, 'w' )
+
+    for i, line in enumerate( open( infile ) ):
+        line = line.strip( '\r\n' )
+        fields = line.split()
+        fasta_title = []
+        for j in title_col_list:
+            j = int( j ) - 1
+            fasta_title.append( fields[j] )
+        fasta_seq = fields[seq_col]
+        if ( fasta_title[0].startswith(">") ):
+            fasta_title[0] = fasta_title[0][1:]
+        print >> out, ">%s\n%s" % ( "_".join( fasta_title ), fasta_seq )
+    out.close()    
+
+if __name__ == "__main__" : __main__()
