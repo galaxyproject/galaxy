ee85154a155dee09256f90939b9be76c5a5c9319
Guruprasad Anada <gua110@bx.psu.edu>
2008-02-05 17:00:40 +0000

    Modified  error handling in 'Extract GFF features' tool. The tool will display a simple error message in place of stack trace.

--- tools/extract/extract_GFF_Features.py
+++ tools/extract/extract_GFF_Features.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2.4
-
+#Guruprasad Ananda
 """
 Extract features from GFF file.
 
-usage: %prog input1 out_file1 column features out_format
+usage: %prog input1 out_file1 column features
 """
 
 import sys, os
@@ -11,19 +11,26 @@ import sys, os
 import pkg_resources; pkg_resources.require( "bx-python" )
 from bx.cookbook import doc_optparse
 
+def stop_err( msg ):
+    sys.stderr.write( msg )
+    sys.exit()
+
 def main():   
     # Parsing Command Line here
     options, args = doc_optparse.parse( __doc__ )
     
     try:
         inp_file, out_file, column, features = args
+    except:
+        stop_err("One or more arguments is missing or invalid.\nUsage: prog input output column features")
+    
+    try:
         column = int(column)
     except:
-        doc_optparse.exit()
+        stop_err("%s is an invalid column." %(column))
     
     if features == None:
-        print "Column %d has no features to display. Please select another column." %(column+1)
-        sys.exit()
+        stop_err( "Column %d has no features to display. Please select another column." %(column+1))
 
     try:
         fo=open(out_file,'w')
