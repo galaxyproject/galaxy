737f5b49cdec929dcaa32cd288b7aff4e3d7d4de
Wen-Yu Chung <wychung@bx.psu.edu>
2008-02-25 17:29:21 +0000

    new quality score datatype.

---
+++ lib/galaxy/datatypes/qualityscore.py
@@ -0,0 +1,19 @@
+"""
+Qualityscore class
+"""
+
+import data
+import logging
+from galaxy.datatypes.sniff import *
+
+log = logging.getLogger(__name__)
+
+class QualityScore ( data.Text ):
+    """
+    until we know more about quality score formats
+    """
+    file_ext = "qual"
+    def set_peek( self, dataset ):
+        dataset.peek  = data.get_file_peek( dataset.file_name )
+        dataset.blurb = "%s lines, Quality score file" %( data.get_line_count( dataset.file_name ) )
+      
