e0457a67106bb3c006a989b7d8e5fd6bb39829b1
Greg Von Kuster <greg@bx.psu.edu>
2008-02-26 01:13:34 +0000

    Fixes for tabular_to_fasta.py, better exception handling.

--- tools/fasta_tools/tab2fasta/1.0.0/tabular_to_fasta.py
+++ tools/fasta_tools/tab2fasta/1.0.0/tabular_to_fasta.py
@@ -1,13 +1,10 @@
-#! /usr/bin/python
+#!/usr/bin/env python2.4
 """
 Input: fasta, minimal length, maximal length
 Output: fasta
 Return sequences whose lengths are within the range.
 """
-
 import sys, os
-
-seq_hash = {}
 
 def stop_err( msg ):
     sys.stderr.write( msg )
@@ -24,23 +21,43 @@ def __main__():
     try:
         seq_col = int( seq_col ) - 1
     except:
-        stop_err( "Invalid Sequence Column: %s." %seq_col )
+        stop_err( "Invalid Sequence Column: %s." %str( seq_col ) )
 
     title_col_list = title_col.split( ',' )
     out = open( outfile, 'w' )
-
+    skipped_lines = 0
+    first_invalid_line = 0
+    invalid_line = ""
+    i = 0
+    
     for i, line in enumerate( open( infile ) ):
+        error = False
         line = line.rstrip( '\r\n' )
-        fields = line.split()
+        fields = line.split( '\t' )
         fasta_title = []
         for j in title_col_list:
             j = int( j ) - 1
-            if ( j < len( fields ) ):
+            try:
                 fasta_title.append( fields[j] )
-        fasta_seq = fields[seq_col]
-        if ( fasta_title[0].startswith(">") ):
-            fasta_title[0] = fasta_title[0][1:]
-        print >> out, ">%s\n%s" % ( "_".join( fasta_title ), fasta_seq )
+            except:
+                error = True
+                break
+        if not error:
+            try:
+                fasta_seq = fields[seq_col]
+                if fasta_title[0].startswith( ">" ):
+                    fasta_title[0] = fasta_title[0][1:]
+                print >> out, ">%s\n%s" % ( "_".join( fasta_title ), fasta_seq )
+            except:
+                error = True
+        if error:
+            skipped_lines += 1
+            if not invalid_line:
+                first_invalid_line = i + 1
+                invalid_line = line
     out.close()    
+
+    if skipped_lines > 0:
+        print 'Data issue: skipped %d blank or invalid lines starting at line #%d: "%s"' % ( skipped_lines, first_invalid_line, invalid_line )
 
 if __name__ == "__main__" : __main__()
