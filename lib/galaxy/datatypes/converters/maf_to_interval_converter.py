d7cd152cfac13b000e240921932093098cc4c7d5
Daniel Blankenberg <dan@bx.psu.edu>
2008-03-17 15:52:09 +0000

    Fix for maf to interval coverter on negative stranded input.

--- lib/galaxy/datatypes/converters/maf_to_interval_converter.py
+++ lib/galaxy/datatypes/converters/maf_to_interval_converter.py
@@ -16,7 +16,7 @@ def __main__():
     for maf in bx.align.maf.Reader( open(input_name, 'r') ):
         c = maf.get_component_by_src_start(species)
         if c is not None:
-            out.write( "%s\t%i\t%i\t%s\n" % (bx.align.src_split(c.src)[-1], c.start, c.end, c.strand) )
+            out.write( "%s\t%i\t%i\t%s\n" % (bx.align.src_split(c.src)[-1], c.get_forward_strand_start(), c.get_forward_strand_end(), c.strand) )
             count += 1
     out.close()
     print "%i MAF blocks converted to Genomic Intervals for species %s." % (count, species)
