67986d25f60394f720f69a67bd0dff992cfe245a
Wen-Yu Chung <wychung@bx.psu.edu>
2008-03-31 19:29:00 +0000

    sorry, hit the wrong button (want to click cancel but click ok instead). add two tools and functional test data for short reads. update tool_conf.xml.sample.

---
+++ tools/metag_tools/blat_mapping.py
@@ -0,0 +1,98 @@
+#! /usr/bin/python
+
+import os, sys
+
+assert sys.version_info[:2] >= (2.4)
+
+def reverse_complement(s):
+
+    complement_dna = {"A":"T", "T":"A", "C":"G", "G":"C", "a":"t", "t":"a", "c":"g", "g":"c", "N":"N", "n":"n" , ".":"."}
+    reversed_s = []
+    for i in s:
+        reversed_s.append(complement_dna[i])
+    reversed_s.reverse()
+    
+    return "".join(reversed_s)
+
+    
+def __main__():
+    
+    nuc_index = {'a':0,'t':1,'c':2,'g':3,'n':4}
+    coverage = {}        # key = (chrom, index)
+
+    invalid_lines = 0
+    invalid_chrom = 0
+    
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+
+    for i, line in enumerate(open(infile)):
+
+        line = line.rstrip('\r\n')
+        fields = line.split()
+        
+        if line.startswith('#'): continue
+        if not line: continue
+        
+        if (len(fields) < 21):                # standard number of pslx columns
+            invalid_lines += 1
+            continue 
+        if (not fields[0].isdigit()):
+            invalid_lines += 1
+            continue
+        
+        chrom = fields[13]
+        
+        try:
+            assert chrom.startswith('chr') is True
+        except:
+            invalid_chrom += 1
+            continue
+            
+        try:
+            block_count = int(fields[17])
+        except:
+            invalid_lines += 1
+            continue
+        
+        block_size = fields[18].split(',')
+        chrom_start = fields[20].split(',')
+    
+        
+        for j in range(block_count):
+            
+            try:
+                this_block_size = int(block_size[j])
+                this_chrom_start = int(chrom_start[j])
+            except:
+                continue
+            
+            # brut force coverage                
+            for k in range(this_block_size):
+                cur_index = this_chrom_start+k
+                if coverage.has_key((chrom,cur_index)):
+                    coverage[(chrom, cur_index)] += 1
+                else:
+                    coverage[(chrom, cur_index)] = 1
+                
+    # generate a index file
+    outputfh = open(outfile, 'w')
+    keys = coverage.keys()
+    keys.sort()
+    previous_chrom = ''
+    for i in keys:
+        (chrom, location) = i
+        sum = coverage[(i)]
+        if (chrom != previous_chrom):
+            print >> outputfh, 'variableStep chrom=%s' %(chrom)
+            previous_chrom = chrom
+        print >> outputfh, location, sum
+    outputfh.close()
+    
+    if invalid_lines:
+        print >> sys.stdout, "Skip %d invalid lines. These lines could be headers or have fewer columns than standard output." %(invalid_lines)
+    
+    if invalid_chrom:
+        print >> sys.stdout, "Skip %d invalid lines with errors in chromosome id. The chromosome id must begin with \'chr\' to be correctly mapped to ucsc genome browser."
+        
+if __name__ == '__main__': __main__()
---
+++ tools/metag_tools/convert_SOLiD_color2nuc.py
@@ -0,0 +1,92 @@
+#! /usr/bin/python
+"""
+convert SOLiD calor-base data to nucleotide sequence
+example: T011213122200221123032111221021210131332222101
+         TTGTCATGAGAAAGACAGCCGACACTCAAGTCAACGTATCTCTGGT
+"""
+
+import sys, os
+
+assert sys.version_info[:2] >= (2.4)
+
+def stop_err(msg):
+    
+    sys.stderr.write(msg)
+    sys.stderr.write('\n')
+    sys.exit()
+    
+def color2base(color_seq):
+
+    first_nuc = ['A','C','G','T']
+    code_matrix = {}
+    code_matrix['0'] = ['A','C','G','T']
+    code_matrix['1'] = ['C','A','T','G']
+    code_matrix['2'] = ['G','T','A','C']
+    code_matrix['3'] = ['T','G','C','A']
+
+    overlap_nuc = ''
+    nuc_seq = ''
+    
+    seq_prefix = prefix = color_seq[0].upper()
+    color_seq = color_seq[1:]
+                
+    try:
+        assert (seq_prefix in first_nuc) is True
+    except:
+        stop_err('The leading nucleotide is invalid. Must be one of the four nucleotides: A, T, C, G.\nThe file contains a %s' %seq_prefix )
+
+    for code in color_seq:
+        
+        try:
+            assert (code in ['0','1','2','3']) is True
+        except:
+            stop_err('Expect digits (0, 1, 2, 3) in the color-cading data. File contains numbers other than the set.\nThe file contains a %s' %code)
+        
+        second_nuc = code_matrix[code]
+        overlap_nuc = second_nuc[first_nuc.index(prefix)]
+        nuc_seq += overlap_nuc
+        prefix = overlap_nuc
+
+    return seq_prefix, nuc_seq
+
+def __main__():
+
+    infilename = sys.argv[1]
+    keep_prefix = sys.argv[2].lower()
+    outfilename = sys.argv[3]
+
+    outfile = open(outfilename,'w')
+    prefix = ''
+    color_seq = ''
+    for i, line in enumerate(file(infilename)):
+        line = line.rstrip('\r\n')
+        if not line: continue
+        if line.startswith("#"): continue
+        if line.startswith(">"):
+            
+            if color_seq:
+                prefix, nuc_seq = color2base(color_seq)
+                
+                if keep_prefix == 'yes':
+                    nuc_seq = prefix + nuc_seq
+                
+                print >> outfile, title
+                print >> outfile, nuc_seq
+                
+            title = line
+            color_seq = ''
+        else:
+            color_seq += line
+            
+    if color_seq:
+        prefix, nuc_seq = color2base(color_seq)
+                
+        if keep_prefix == 'yes':
+            nuc_seq = prefix + nuc_seq
+
+        print >> outfile, title
+        print >> outfile, nuc_seq
+            
+    outfile.close()
+    
+if __name__=='__main__': __main__()
