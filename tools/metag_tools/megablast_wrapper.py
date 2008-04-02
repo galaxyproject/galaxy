743cbffd7515fd2ad271aee97e4ab3d4700dead1
Wen-Yu Chung <wychung@bx.psu.edu>
2008-04-02 18:45:53 +0000

    make changes of the suggestions to megablast wrapper.

--- tools/metag_tools/megablast_wrapper.py
+++ tools/metag_tools/megablast_wrapper.py
@@ -3,8 +3,8 @@
 run megablast for metagenomics data
 """
 
-import sys, os, tempfile, subprocess
-#from megablast_xml_parser import *
+import sys, os
+
 
 def stop_err(msg):
     
@@ -44,44 +44,37 @@ def __main__():
     
     GALAXY_DATA_INDEX_DIR = sys.argv[9]
     DB_LOC = "%s/blastdb.loc" % GALAXY_DATA_INDEX_DIR
-    
-    output_file = open(output_filename, 'w')
-    
+        
     # prepare the database
     db = {}
     for i, line in enumerate(file(DB_LOC)):
         line = line.rstrip('\r\n')
+        if not line: continue
         if line.startswith('#'): continue
         fields = line.split()
-        db[(fields[0])] = []
-        for j in xrange(1, len(fields)):
-            db[(fields[0])].append(fields[j])
-    
-    
-    try:    
-        assert db.has_key(db_build) is True
-    except:
+        if len(fields) == 2:
+            db[(fields[0])] = fields[1]
+            
+    if not db.has_key(db_build):
         stop_err('Cannot locate the target database. Please check your location file.')
+    
+    # arguments for megablast    
+    chunk = db[(db_build)]
+    megablast_arguments = ["megablast", "-d", chunk, "-i", query_filename, "-o", output_filename]
+    megablast_parameters = ["-m", "8", "-a", "8"]
+    megablast_user_inputs = ["-W", mega_word_size, "-p", mega_iden_cutoff, "-t", mega_disc_word, "-N", mega_disc_type, "-F", mega_filter]
+    megablast_command = " ".join(megablast_arguments) + " " + " ".join(megablast_parameters) + " " + " ".join(megablast_user_inputs) + " 2>&1" 
         
-    for chunk in db[(db_build)]:
-        megablast_arguments = ["megablast", "-d", chunk, "-i", query_filename]
-        megablast_parameters = ["-m", "8", "-a", "8"]
-        megablast_user_inputs = ["-W", mega_word_size, "-p", mega_iden_cutoff, "-t", mega_disc_word, "-N", mega_disc_type, "-F", mega_filter]
-        megablast_command = " ".join(megablast_arguments) + " " + " ".join(megablast_parameters) + " " + " ".join(megablast_user_inputs) + " 2>./error.log" 
-        
-        megablast_output = os.popen(megablast_command)
-        # to avoid reading whole file into memory
-        for i, line in enumerate(megablast_output):
-            line = line.rstrip('\r\n')
-            print >> output_file, line 
-        
-    output_file.close()
+    try:
+        os.system(megablast_command)
+    except:
+        stop_err('Unable to run megablast. Please make sure megablast is installed and available in the search path.')
     
     # megablast generates a file called error.log, if empty, delete it, if not, show the contents
     if os.path.exists('./error.log'):
         for i, line in enumerate( file('./error.log') ):
             line = line.rstrip('\r\n')
-            print >> sys.stdout, line
+            print line
         os.remove('./error.log')
     
 if __name__ == "__main__" : __main__()
