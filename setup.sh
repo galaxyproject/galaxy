d9e3ba2b016b692d5378ccd199e158ad7552f1c6
Nate Coraor <nate@bx.psu.edu>
2008-04-02 20:57:10 +0000

    setup.sh now copies the tool-data samples as well.

--- setup.sh
+++ setup.sh
@@ -7,10 +7,22 @@ datatype_converters_conf.xml.sample
 reports_wsgi.ini.sample
 tool_conf.xml.sample
 universe_wsgi.ini.sample
+tool-data/alignseq.loc.sample
+tool-data/binned_scores.loc.sample
+tool-data/blastdb.loc.sample
+tool-data/encode_datasets.loc.sample
+tool-data/liftOver.loc.sample
+tool-data/maf_index.loc.sample
+tool-data/maf_pairwise.loc.sample
+tool-data/microbial_data.loc.sample
+tool-data/phastOdds.loc.sample
+tool-data/quality_scores.loc.sample
+tool-data/regions.loc.sample
+tool-data/twobit.loc.sample
 "
 
 for sample in $SAMPLES; do
-    file=`basename $sample .sample`
+    file=`echo $sample | sed -e 's/\.sample$//'`
     if [ -f $file ]; then
         echo "Not overwriting existing $file"
     else
