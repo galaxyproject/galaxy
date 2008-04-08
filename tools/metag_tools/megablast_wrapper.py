70f2cecb147fabbc33e1bcdfe5078c834ec65f0c
Wen-Yu Chung <wychung@bx.psu.edu>
2008-04-08 18:28:39 +0000

    update megablast output format. now the gi column is separated to two columns: gi and its length.

--- tools/metag_tools/megablast_wrapper.py
+++ tools/metag_tools/megablast_wrapper.py
@@ -3,7 +3,7 @@
 run megablast for metagenomics data
 """
 
-import sys, os
+import sys, os, tempfile
 
 
 def stop_err(msg):
@@ -22,6 +22,8 @@ def __main__():
     mega_word_size = sys.argv[4]        # -W
     mega_iden_cutoff = sys.argv[5]      # -p
     mega_disc_word = sys.argv[6]        # -t
+    
+    mega_temp_output = tempfile.NamedTemporaryFile().name
     
     # megablast parameters
     try:
@@ -62,7 +64,7 @@ def __main__():
     
     # arguments for megablast    
     chunk = db[(db_build)]
-    megablast_arguments = ["megablast", "-d", chunk, "-i", query_filename, "-o", output_filename]
+    megablast_arguments = ["megablast", "-d", chunk, "-i", query_filename, "-o", mega_temp_output]
     megablast_parameters = ["-m", "8", "-a", "8"]
     megablast_user_inputs = ["-W", mega_word_size, "-p", mega_iden_cutoff, "-t", mega_disc_word, "-N", mega_disc_type, "-F", mega_filter]
     megablast_command = " ".join(megablast_arguments) + " " + " ".join(megablast_parameters) + " " + " ".join(megablast_user_inputs) + " 2>&1" 
@@ -72,6 +74,28 @@ def __main__():
     except:
         stop_err('Unable to run megablast. Please make sure megablast is installed and available in the search path.')
     
+    # parse target id to two separate columns
+    output = open(output_filename,'w')
+    
+    invalid_lines = 0
+    for i, line in enumerate(file(mega_temp_output)):
+        line = line.rstrip('\r\n')
+        fields = line.split()
+        
+        try:
+            gi, gi_len = fields[1].split('_')
+            new_line = fields[0]+'\t'+gi+'\t'+gi_len+'\t'.join(fields[2:])
+        except:
+            invalid_lines += 1
+            new_line = line
+        
+        output.write(new_line+'\n')
+        
+    output.close()
+    
+    if invalid_lines:
+        print "Skipped %d lines that was unable to parse gi id column." % invalid_lines
+        
     # megablast generates a file called error.log, if empty, delete it, if not, show the contents
     if os.path.exists('./error.log'):
         for i, line in enumerate( file('./error.log') ):
