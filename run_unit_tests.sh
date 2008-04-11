45f198363d42195e5a7389951bc404571ecd98d3
Nate Coraor <nate@bx.psu.edu>
2008-04-11 20:14:32 +0000

    Fix the unit tests.

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./nosetests.sh -v -w lib \
+python -ES ./scripts/nosetests.py -v -w lib \
     --with-nosehtml --html-report-file run_unit_tests.html \
     --with-doctest --exclude=functional --exclude="^get"
