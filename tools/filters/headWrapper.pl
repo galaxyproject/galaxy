058455861ddcf5e51d828904d8f7d53ebd207e8c
Nate Coraor <nate@bx.psu.edu>
2008-04-22 18:11:59 +0000

    Fixed head/tail wrapper for Mac.

--- tools/filters/headWrapper.pl
+++ tools/filters/headWrapper.pl
@@ -10,7 +10,7 @@ die "Check arguments" unless @ARGV == 3;
 die "Line number must be an integer\n" unless $ARGV[1]=~ m/^\d+$/;
 
 open (OUT, ">$ARGV[2]") or die "Cannot create $ARGV[2]:$!\n";
-open (HEAD, "head $ARGV[0] -n $ARGV[1]|") or die "Cannot run head:$!\n";
+open (HEAD, "head -n $ARGV[1] $ARGV[0]|") or die "Cannot run head:$!\n";
 while (<HEAD>) {
     print OUT;
 }
--- tools/filters/tailWrapper.pl
+++ tools/filters/tailWrapper.pl
@@ -10,7 +10,7 @@ die "Check arguments" unless @ARGV == 3;
 die "Line number should be an integer\n" unless $ARGV[1]=~ m/^\d+$/;
 
 open (OUT, ">$ARGV[2]") or die "Cannot create $ARGV[2]:$!\n";
-open (TAIL, "tail $ARGV[0] -n $ARGV[1]|") or die "Cannot run tail:$!\n";
+open (TAIL, "tail -n $ARGV[1] $ARGV[0]|") or die "Cannot run tail:$!\n";
 while (<TAIL>) {
     print OUT;
 }
