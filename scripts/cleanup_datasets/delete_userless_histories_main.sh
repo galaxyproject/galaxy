3f01a6dd2a3b2db38b955b64ae0e830ff59d51a6
Greg Von Kuster <greg@bx.psu.edu>
2008-04-24 20:31:27 +0000

    Necessary cron changes for cleanup dataset scripts for main.

--- scripts/cleanup_datasets/delete_userless_histories_main.sh
+++ scripts/cleanup_datasets/delete_userless_histories_main.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-# This script must be executed from the $UNIVERSE_HOME directory
-# e.g., sh ./scripts/cleanup_datasets/delete_userless_histories_main.sh
-
-. ./scripts/get_python.sh
-. ./setup_paths.sh
-
-$GALAXY_PYTHON ./scripts/cleanup_datasets/cleanup_datasets.py ./universe_wsgi.ini -d 60 -2 $@ >> ./scripts/cleanup_datasets/delete_userless_histories_main.log
+cd `dirname $0`/../..
+python ./scripts/cleanup_datasets/cleanup_datasets.py ./universe_wsgi.ini -d 60 -2 $@ >> ./scripts/cleanup_datasets/delete_userless_histories.log
