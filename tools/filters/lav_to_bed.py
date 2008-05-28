57fe2ba4b0fc7523b4656d73d1aced2efa5fcf91
Greg Von Kuster <greg@bx.psu.edu>
2008-05-28 19:26:42 +0000

    Cleaned up lav_to_bed a bit, added functional test.  Removed old, unused tools from the ~/tools/filter directory.

--- tools/filters/lav_to_bed.py
+++ tools/filters/lav_to_bed.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 #Reads a LAV file and writes two BED files.
-
 import sys
 from galaxy import eggs
 import pkg_resources
@@ -9,35 +8,37 @@ import bx.align.lav
 
 assert sys.version_info[:2] >= ( 2, 4 )
 
-def main():
+def stop_err( msg ):
+    sys.stderr.write( msg )
+    sys.exit()
 
+def main():
     try:
         lav_file = open(sys.argv[1],'r')
         bed_file1 = open(sys.argv[2],'w')
         bed_file2 = open(sys.argv[3],'w')
-    except:
-        print >>sys.stderr,"Error with provided arguments"
-        print >>sys.stderr,"Usage: python %s input_lav output_bed1 output_bed2" % sys.argv[0]
-        sys.exit(0)
+    except Exception, e:
+        stop_err( str( e ) )
         
-    lavsRead = bedsWritten = 0
+    lavsRead = 0
+    bedsWritten = 0
     species = {}
-    for lavBlock in bx.align.lav.Reader(lav_file):
+    # TODO: this is really bad since everything is read into memory.  Can we eliminate this tool?
+    for lavBlock in bx.align.lav.Reader( lav_file ):
         lavsRead += 1
-        
         for c in lavBlock.components:
-            spec,chrom = bx.align.lav.src_split( c.src )
+            spec, chrom = bx.align.lav.src_split( c.src )
             if bedsWritten < 1:
-                if len(species)==0:
+                if len( species )==0:
                     species[spec]=bed_file1
-                elif len(species)==1:
+                elif len( species )==1:
                     species[spec]=bed_file2
                 else:
                     continue #this is a pairwise alignment...
             if spec in species:
-                species[spec].write("%s\t%i\t%i\t%s\t%i\t%s\n" % (chrom,c.start,c.end,spec+"_"+str(bedsWritten),0,c.strand))
+                species[spec].write( "%s\t%i\t%i\t%s_%s\t%i\t%s\n" % ( chrom, c.start, c.end, spec, str( bedsWritten ), 0, c.strand ) )
         bedsWritten += 1
-
+        
 
     for spec,file in species.items():
         print "#FILE\t%s\t%s" % (file.name, spec)
--- tools/filters/lav_to_bed_code.py
+++ tools/filters/lav_to_bed_code.py
@@ -7,12 +7,12 @@ def exec_after_process(app, inp_data, out_data, param_dict, tool, stdout, stderr
             fields = line.split("\t")
             filename_to_build[fields[1]]=fields[2].strip()
         else:
-            new_stdout = new_stdout + line
+            new_stdout = "%s%s" % ( new_stdout, line )
     for name,data in out_data.items():
         try:
-            data.info = new_stdout +"\n"+ stderr
+            data.info = "%s\n%s" % ( new_stdout, stderr )
             data.dbkey = filename_to_build[data.file_name]
-            data.name = data.name + " ("+data.dbkey+")"
+            data.name = "%s (%s)" % ( data.name, data.dbkey )
             data.flush()
         except:
             continue
