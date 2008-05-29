b1308a3c620098c0faef3ca3caaddce9caaa4e96
Greg Von Kuster <greg@bx.psu.edu>
2008-05-29 20:41:14 +0000

    Some clarification / fixes for FastqSolexa - our convert tool currently only supports the Solexa variant if the quality scores are integer ( not ASCII ).  This probably needs to be looked at!

--- tools/metag_tools/fastq_to_fasta_qual.py
+++ tools/metag_tools/fastq_to_fasta_qual.py
@@ -25,12 +25,18 @@ def stop_err( msg ):
     sys.exit()
 
 def __main__():
+    # TODO: This tool currently only works for Standard Fastq or Solexa Fastq where the 
+    # quality scores are integers.  This tool will not produce the correct result if the 
+    # input format is fastqsolexa and the data includes quality scores that are ASCII..  Our
+    # fastqsolexa sniffer currently will not sniff data as fastqsolexa when the scores are ASCII,
+    # but a user can manually change the data type in whcih case this tool will break.
     infile_name = sys.argv[1]
     outfile_seq = open( sys.argv[2], 'w' )
-    outfile_score = open( sys.argv[3], 'w' )    
+    outfile_score = open( sys.argv[3], 'w' )
+    datatype = sys.argv[4]
     seq_title_startswith = ''
     qual_title_startswith = ''
-    default_coding_value = 64
+    default_coding_value = 33
     fastq_block_lines = 0
     
     for i, line in enumerate( file( infile_name ) ):
@@ -44,6 +50,8 @@ def __main__():
             if not seq_title_startswith:
                 seq_title_startswith = line_startswith
             if line_startswith != seq_title_startswith:
+                outfile_seq.close()
+                outfile_score.close()
                 stop_err( 'Invalid fastq format at line %d: %s.' % ( i + 1, line ) )
             read_title = line[1:]
             outfile_seq.write( '>%s\n' % line[1:] )
@@ -56,9 +64,13 @@ def __main__():
             if not qual_title_startswith:
                 qual_title_startswith = line_startswith
             if line_startswith != qual_title_startswith:
+                outfile_seq.close()
+                outfile_score.close()
                 stop_err( 'Invalid fastq format at line %d: %s.' % ( i + 1, line ) )    
             quality_title = line[1:]
             if quality_title and read_title != quality_title:
+                outfile_seq.close()
+                outfile_score.close()
                 stop_err( 'Invalid fastq format at line %d: sequence title "%s" differes from score title "%s".' % ( i + 1, read_title, quality_title ) )
             if not quality_title:
                 outfile_score.write( '>%s\n' % read_title )
@@ -73,6 +85,10 @@ def __main__():
                 # digits
                 qual = line
             else:
+                if datatype == 'fastqsolexa':
+                    outfile_seq.close()
+                    outfile_score.close()
+                    stop_err( "This tool currently only works with the fastq solexa variant if the socres are integers, not ascii." )
                 # ascii
                 quality_score_length = len( line )
                 if quality_score_length == read_length + 1:
