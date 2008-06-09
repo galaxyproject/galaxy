44c68cfe2fb6082f72b7886d8269d0834f4dffd2
Nate Coraor <nate@bx.psu.edu>
2008-06-09 21:20:20 +0000

    Since it's more complex now, a script for buildbot to run to set up stuff that needs to exist for tests to run.

---
+++ buildbot_setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+LINKS="
+/depot/data2/galaxy/alignseq.loc
+/depot/data2/galaxy/binned_scores.loc
+/depot/data2/galaxy/encode_datasets.loc
+/home/universe/linux-i686/HYPHY
+/depot/data2/galaxy/liftOver.loc
+/depot/data2/galaxy/microbes/microbial_data.loc
+/depot/data2/galaxy/phastOdds.loc
+/depot/data2/galaxy/quality_scores.loc
+/depot/data2/galaxy/regions.loc
+/depot/data2/galaxy/taxonomy
+/depot/data2/galaxy/twobit.loc
+"
+
+SAMPLES="
+datatypes_conf.xml.sample
+"
+
+# temp
+ln -sf /depot/data2/galaxy/maf_index_new.loc tool-data/maf_index.loc
+ln -sf /depot/data2/galaxy/maf_pairwise_new.loc tool-data/maf_pairwise.loc
+
+for link in $LINKS; do
+    echo "Linking $link"
+    ln -sf $link tool-data
+done
+
+for sample in $SAMPLES; do
+    file=`echo $sample | sed -e 's/\.sample$//'`
+    echo "Copying $sample to $file"
+    cp $sample $file
+done
+
+python ./scripts/fetch_eggs.py all
