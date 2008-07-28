633a218fceb9a52658582c61e70aba9592b28c72
Daniel Blankenberg <dan@bx.psu.edu>
2008-07-28 13:01:18 -0400

    Removing newly created history controller, and moving history_set_permissions into the root controller with the rest of the history methods.

--- lib/galaxy/web/controllers/root.py
+++ lib/galaxy/web/controllers/root.py
@@ -621,6 +621,42 @@ class RootController( BaseController ):
             return trans.show_error_message("Adding File to History has Failed")
 
     @web.expose
+    def history_set_default_permissions( self, trans, **kwd ):
+        """Sets the user's default permissions for the current history"""
+        #TODO: allow changing of default roles associated with history
+        if trans.user:
+            if 'set_permissions' in kwd:
+                """The user clicked the set_permissions button on the set_permissions form"""
+                history = trans.get_history()
+                group_in = []
+                group_out = []
+                #collect groups as entered by user
+                for name, value in kwd.items():
+                    if name.startswith( "group_" ):
+                        group = trans.app.model.GalaxyGroup.get( name.replace( "group_", "", 1 ) )
+                        if not group:
+                            return trans.show_error_message( 'You have specified an invalid group.' )
+                        if value == 'in':
+                            group_in.append( group )
+                        else:
+                            group_out.append( group )
+                if not group_in:
+                    return trans.show_error_message( "You must specify at least one default group." )
+                cur_groups = [ assoc.group for assoc in history.default_groups ]
+                group_in.sort()
+                cur_groups.sort()
+                if cur_groups != group_in:
+                    history.set_default_access( groups = group_in )
+                    return trans.show_ok_message( 'Default history permissions have been changed.' )
+                else:
+                    return trans.show_error_message( "You did not specify any changes to this history's default permissions." )
+            return trans.fill_template( 'history/permissions.mako' )
+        else:
+            #user not logged in, history group must be only public
+            return trans.show_error_message( "You must be logged in to change a history's default permissions." )
+
+
+    @web.expose
     def dataset_make_primary( self, trans, id=None):
         """Copies a dataset and makes primary"""
         try:
