c546f50963b64cc23e382b0232c230e426f68657
Daniel Blankenberg <dan@bx.psu.edu>
2008-08-06 15:41:38 -0400

    Properly set permissions on tools that create new datasets in their post job hooks (this is deprecated).

--- tools/maf/maf_to_bed_code.py
+++ tools/maf/maf_to_bed_code.py
@@ -27,12 +27,13 @@ def exec_after_process(app, inp_data, out_data, param_dict, tool, stdout, stderr
             fields = line.split("\t")
             dbkey = fields[1]
             filepath = fields[2]
-
             newdata = app.model.HistoryDatasetAssociation( create_dataset = True )
             newdata.extension = "bed"
             newdata.name = basic_name + " (" + dbkey + ")"
             newdata.flush()
             history.add_dataset( newdata )
+            app.security_agent.set_dataset_groups( newdata.dataset, output_data.dataset.groups )
+            app.security_agent.set_dataset_roles( newdata.dataset, output_data.dataset.roles )
             newdata.flush()
             history.flush()
             app.model.flush()
