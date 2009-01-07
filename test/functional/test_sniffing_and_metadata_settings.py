ad33e0bc5a27e50f54afdd76612a84a6b74ad68a
Greg Von Kuster <greg@bx.psu.edu>
2009-01-07 15:33:35 -0500

    Fix for my last commit.

--- test/functional/test_sniffing_and_metadata_settings.py
+++ test/functional/test_sniffing_and_metadata_settings.py
@@ -110,8 +110,7 @@ class SniffingAndMetaDataSettings( TwillTestCase ):
         self.delete_history_item( 1 )
     def test_60_blastxml_datatype( self ):
         """Testing correctly sniffing blastxml data type upon upload"""
-        self.upload_file( 'megablast_xml_parser_test1.blastxml' )
-        self.verify_dataset_correctness( 'megablast_xml_parser_test1.blastxml' )
+        self.upload_file( 'megablast_xml_parser_test1.gz' )
         self.check_history_for_string( 'NCBI Blast XML data' )
         self.check_history_for_string( 'format: <span class="blastxml">blastxml</span>' )
         self.delete_history_item( 1 )
