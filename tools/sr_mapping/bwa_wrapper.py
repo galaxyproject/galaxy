45201c6054416c6a5e93e50a0a8ded3289b92090
Kelly Vincent <kpvincent@bx.psu.edu>
2009-09-16 11:14:39 -0400

    Updated BWA wrapper tool: replaced threads option to hard-coded value, changed output and test file output datatype to sam, changed test file names to standard, better cleanup of temp files, added dbkey parameter and exec_before_job method to set it. Also changed name of a FASTQ Conversions tool test file

--- tools/sr_mapping/bwa_wrapper.py
+++ tools/sr_mapping/bwa_wrapper.py
@@ -3,8 +3,6 @@
 """
 Runs BWA on single-end or paired-end data.
 Produces a SAM file containing the mappings.
-
-usage: python bwa_wrapper.py reference_sequence indexing_algorithm(is_or_bwtsw) forward_fastq_file reverse_fastq_file(or_None) output alignment_type(single_or_paired) parameters(pre_set_or_full) file_type(solexa_or_solid) file_source(indexed_or_history) maxEditDist fracMissingAligns maxGapOpens maxGapExtens disallowLongDel disallowIndel seed maxEditDistSeed numThreads mismatchPenalty gapOpenPenalty gapExtensPenalty colorSpaceRev suboptAlign noIterSearch outputTopN maxInsertSize maxOccurPairing\nThe last eighteen need to all be specified, or all be None
 """
 
 import optparse, os, sys, tempfile
@@ -16,6 +14,7 @@ def stop_err( msg ):
 def __main__():
     #Parse Command Line
     parser = optparse.OptionParser()
+    parser.add_option('', '--threads', dest='threads', help='The number of threads to use')
     parser.add_option('', '--ref', dest='ref', help='The reference genome to use or index')
     parser.add_option('', '--fastq', dest='fastq', help='The (forward) fastq file to use for the mapping')
     parser.add_option('', '--rfastq', dest='rfastq', help='The reverse fastq file to use for mapping if paired-end data')
@@ -32,7 +31,6 @@ def __main__():
     parser.add_option('-i', '--disallowIndel', dest='disallowIndel', help='Disallow indel within specified bps')
     parser.add_option('-l', '--seed', dest='seed', help='Take the first specified subsequences')
     parser.add_option('-k', '--maxEditDistSeed', dest='maxEditDistSeed', help='Maximum edit distance to the seed')
-    parser.add_option('-t', '--numThreads', dest='numThreads', help='Number of threads')
     parser.add_option('-M', '--mismatchPenalty', dest='mismatchPenalty', help='Mismatch penalty')
     parser.add_option('-O', '--gapOpenPenalty', dest='gapOpenPenalty', help='Gap open penalty')
     parser.add_option('-E', '--gapExtensPenalty', dest='gapExtensPenalty', help='Gap extension penalty')
@@ -42,8 +40,9 @@ def __main__():
     parser.add_option('', '--outputTopN', dest='outputTopN', help='Output top specified hits')
     parser.add_option('', '--maxInsertSize', dest='maxInsertSize', help='Maximum insert size for a read pair to be considered mapped good')
     parser.add_option('', '--maxOccurPairing', dest='maxOccurPairing', help='Maximum occurrences of a read for pairings')
+    parser.add_option('', '--dbkey', dest='dbkey', help='')
     (options, args) = parser.parse_args()
-        
+       
     # index if necessary
     if options.fileSource == 'history':
         # make temp directory for placement of indices and copy reference file there
@@ -65,46 +64,42 @@ def __main__():
         else:
             indexing_cmds = '-a %s' % indexingAlg
         options.ref = os.path.join(tmp_dir,os.path.split(options.ref)[1])
-        cmd1 = 'bwa index %s %s 2> /dev/null' % (indexing_cmds, options.ref)
+        cmd1 = 'cd %s; bwa index %s %s 2> /dev/null' % (tmp_dir, indexing_cmds, options.ref)
         try:
             os.system(cmd1)
         except Exception, erf:
             stop_err('Error indexing reference sequence\n' + str(erf))
-    
     # set up aligning and generate aligning command options
     if options.params == 'pre_set':
         if options.fileType == 'solid':
-            aligning_cmds = '-c'
+            aligning_cmds = '-c -t %s' % options.threads
         else:
-            aligning_cmds = ''
+            aligning_cmds = '-t %s' % options.threads
         gen_alignment_cmds = ''
     else:
         aligning_cmds = '-n %s -o %s -e %s -d %s -i %s %s -k %s -t %s -M %s -O %s -E %s %s %s %s' % \
                         ((options.fracMissingAligns, options.maxEditDist)[options.maxEditDist != '0'], 
                          options.maxGapOpens, options.maxGapExtens, options.disallowLongDel, 
-                         options.disallowIndel, ('',' -l %s'%options.seed)[options.seed!=-1],
-                         options.maxEditDistSeed, options.numThreads, options.mismatchPenalty, 
+                         options.disallowIndel, ('',' -l %s'%options.seed)[options.seed!='-1'],
+                         options.maxEditDistSeed, options.threads, options.mismatchPenalty, 
                          options.gapOpenPenalty, options.gapExtensPenalty, ('',' -c')[options.colorSpaceRev=='true'], 
                          ('',' -R')[options.suboptAlign=='true'], ('',' -N')[options.noIterSearch=='true'])
         if options.genAlignType == 'single':
             gen_alignment_cmds = '-n %s' % options.outputTopN
         elif options.genAlignType == 'paired':
             gen_alignment_cmds = '-a %s -o %s' % (options.maxInsertSize, options.maxOccurPairing)
-            
-    # set up output file
-    file(options.output,'w').write('QNAME\tFLAG\tRNAME\tPOS\tMAPQ\tCIGAR\tMRNM\tMPOS\tISIZE\tSEQ\tQUAL\tOPT\n')
+    # set up output files
     tmp_align_out = tempfile.NamedTemporaryFile()
+    tmp_align_out2 = tempfile.NamedTemporaryFile()
     
     # prepare actual aligning and generate aligning commands
     cmd2 = 'bwa aln %s %s %s > %s 2> /dev/null' % (aligning_cmds, options.ref, options.fastq, tmp_align_out.name)
     cmd2b = ''
     if options.genAlignType == 'paired':
-        tmp_align_out2 = tempfile.NamedTemporaryFile()
         cmd2b = 'bwa aln %s %s %s > %s 2> /dev/null' % (aligning_cmds, options.ref, options.rfastq, tmp_align_out2.name)
         cmd3 = 'bwa sampe %s %s %s %s %s %s >> %s 2> /dev/null' % (gen_alignment_cmds, options.ref, tmp_align_out.name, tmp_align_out2.name, options.fastq, options.rfastq, options.output)
     else:
         cmd3 = 'bwa samse %s %s %s %s >> %s 2> /dev/null' % (gen_alignment_cmds, options.ref, tmp_align_out.name, options.fastq, options.output) 
-
     # align
     try:
         os.system(cmd2)
@@ -116,11 +111,13 @@ def __main__():
             os.system(cmd2b)
     except Exception, erf:
         stop_err("Error aligning second sequence\n" + str(erf))
-
     # generate align
     try:
         os.system(cmd3)
     except Exception, erf:
         stop_err("Error sequence aligning sequence\n" + str(erf))
-
+    # clean up temp files
+    tmp_align_out.close()
+    tmp_align_out2.close()
+    
 if __name__=="__main__": __main__()
---
+++ tools/sr_mapping/bwa_wrapper_code.py
@@ -0,0 +1,8 @@
+import os
+
+def exec_before_job(app, inp_data, out_data, param_dict, tool):
+    try:
+        refFile = param_dict['solidOrSolexa']['solidRefGenomeSource']['indices'].value
+        out_data['output'].set_dbkey(os.path.split(refFile)[1].split('.')[0])
+    except Exception, eq:
+        out_data['output'].set_dbkey(param_dict['dbkey'])     
