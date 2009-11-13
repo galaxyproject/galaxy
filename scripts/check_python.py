531e8043ad8467848dfe0417bce0e9aba4163a96
Nate Coraor <nate@bx.psu.edu>
2009-11-13 10:39:15 -0500

    Officially support Python 2.6.

--- scripts/check_python.py
+++ scripts/check_python.py
@@ -1,19 +1,16 @@
 import os, sys
 
+msg = """ERROR: Your Python version is: %s
+Galaxy is currently supported on Python 2.4, 2.5 and 2.6.  To run Galaxy,
+please download and install a supported version from python.org.  If a
+supported version is installed but is not your default, getgalaxy.org
+contains instructions on how to force Galaxy to use a different version.""" % sys.version[:3]
+
 def check_python():
-    return
     try:
-        assert sys.version_info[:2] >= ( 2, 4 ) and sys.version_info[:2] <= ( 2, 5 )
+        assert sys.version_info[:2] >= ( 2, 4 ) and sys.version_info[:2] <= ( 2, 6 )
     except AssertionError:
-        print >>sys.stderr, "ERROR: Your Python version is:", sys.version.split( ' ', 1 )[0]
-        print >>sys.stderr, "Galaxy is currently only supported on Python 2.4 and Python 2.5."
-        if sys.version_info[:2] < ( 2, 4 ):
-            print >>sys.stderr, "To run Galaxy, please download and install Python 2.5 from http://python.org"
-        else:
-            print >>sys.stderr, "To track the progress of Python 2.6 support, please see:"
-            print >>sys.stderr, "  http://bitbucket.org/galaxy/galaxy-central/issue/76/support-python-26"
-        print >>sys.stderr, "For hints on how to direct Galaxy to use a different python installation, see:"
-        print >>sys.stderr, "  http://bitbucket.org/galaxy/galaxy-central/wiki/GetGalaxy"
+        print >>sys.stderr, msg
         raise
 
 if __name__ == '__main__':
