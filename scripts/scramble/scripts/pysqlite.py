588ea5e038ba639e60a7998039a12b33b2669b3a
Nate Coraor <nate@bx.psu.edu>
2009-11-13 17:08:12 -0500

    Update PySQLite to latest version.  This resolves bitbucket issue #101

--- scripts/scramble/scripts/pysqlite.py
+++ scripts/scramble/scripts/pysqlite.py
@@ -1,18 +1,4 @@
-import os, sys, subprocess, tarfile, zipfile, shutil
-
-def unpack_sqlite_source():
-    print "unpack_sqlite_source(): Found a previously downloaded sqlite source."
-    print "unpack_sqlite_source(): To force a new download, remove the archive:"
-    print " ", SQLITE_ARCHIVE
-    os.makedirs( "sqlite" )
-    z = zipfile.ZipFile( SQLITE_ARCHIVE, "r" )
-    for fn in z.namelist():
-        if fn == "tclsqlite.c" or fn == "shell.c" or fn == "icu.c":
-            continue
-        o = open( os.path.join( "sqlite", fn ), "wb" )
-        o.write( z.read( fn ) )
-        o.close()
-    z.close()
+import os, sys, shutil
 
 # change back to the build dir
 if os.path.dirname( sys.argv[0] ) != "":
@@ -38,38 +24,11 @@ if os.access( ".galaxy_tag", os.F_OK ):
 else:
     tag = None
 
-SQLITE_VERSION = ( tag.split( "_" ) )[1]
-SQLITE_ARCHIVE = os.path.abspath( os.path.join( "..", "..", "..", "archives", "sqlite-source-%s.zip" %SQLITE_VERSION.replace( ".", "_" ) ) )
-
 # clean, in case you're running this by hand from a dirty module source dir
 for dir in [ "build", "dist", "sqlite" ]:
     if os.access( dir, os.F_OK ):
         print "scramble_it.py: removing dir:", dir
         shutil.rmtree( dir )
-
-# build/unpack SQLite
-unpack_sqlite_source()
-
-# changes to setup.py
-file = "setup.py"
-print "build(): Patching", file
-if not os.access( "%s.orig" %file, os.F_OK ):
-    shutil.copyfile( file, "%s.orig" %file )
-i = open( "%s.orig" %file, "r" )
-o = open( file, "w" )
-for line in i.readlines():
-    if line == '           "src/util.c", "src/row.c"]\n':
-        line += 'sources += glob.glob("./sqlite/*.c")\n'
-    if line == "include_dirs = []\n":
-        line = "include_dirs = [ './sqlite' ]\n"
-    if line == "define_macros = []\n":
-        line = "define_macros = [ ('THREADSAFE','1') ]\n"
-    print >>o, line,
-i.close()
-o.close()
-
-# don't want setup.cfg
-os.rename( "setup.cfg", "setup.cfg.orig" )
 
 # tag
 me = sys.argv[0]
@@ -77,6 +36,7 @@ sys.argv = [ me ]
 if tag is not None:
     sys.argv.append( "egg_info" )
     sys.argv.append( "--tag-build=%s" %tag )
+sys.argv.append( "build_static" )
 sys.argv.append( "bdist_egg" )
 
 # go
