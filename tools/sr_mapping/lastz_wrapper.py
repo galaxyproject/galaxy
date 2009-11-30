1fcafa5a86cd5f36386271c78f8c01d0521c8460
Kelly Vincent <kpvincent@bx.psu.edu>
2009-11-30 09:05:28 -0500

    Added usage information, added/updated relevant test files, and got all tests working for Lastz

--- tools/sr_mapping/lastz_wrapper.py
+++ tools/sr_mapping/lastz_wrapper.py
@@ -1,6 +1,35 @@
 #! /usr/bin/python
 
 """
+Runs Lastz
+Written for Lastz v. 1.01.86.
+
+usage: lastz_wrapper.py [options]
+    --ref_name: The reference name to append to all output matches
+    --ref_source: Whether the reference is cached or from the history
+    --source_select: Whether to used pre-set or cached reference file
+    --input1: The name of the reference file if using history or reference base name if using cached
+    --input2: The reads file to align 
+    --ref_sequences: The number of sequences in the reference file if using one from history 
+    --pre_set_options: Which of the pre set options to use, if using pre-sets
+    --strand: Which strand of the read to search, if specifying all parameters
+    --seed: Seeding settings, if specifying all parameters
+    --gfextend: Whether to perform gap-free extension of seed hits to HSPs (high scoring segment pairs), if specifying all parameters
+    --chain: Whether to perform chaining of HSPs, if specifying all parameters
+    --transition: Number of transitions to allow in each seed hit, if specifying all parameters
+    --O: Gap opening penalty, if specifying all parameters
+    --E: Gap extension penalty, if specifying all parameters
+    --X: X-drop threshold, if specifying all parameters
+    --Y: Y-drop threshold, if specifying all parameters
+    --K: Threshold for HSPs, if specifying all parameters
+    --L: Threshold for gapped alignments, if specifying all parameters
+    --entropy: Whether to involve entropy when filtering HSPs, if specifying all parameters
+    --identity_min: Minimum identity (don't report matches under this identity)
+    --identity_max: Maximum identity (don't report matches above this identity)
+    --coverage: The minimum coverage value (don't report matches covering less than this) 
+    --out_format: The format of the output file (sam, diffs, or tabular (general))
+    --output: The name of the output file
+    --num_threads: The number of threads to run
     --lastzSeqsFileDir: Directory of local lastz_seqs.loc file
 """
 import optparse, os, subprocess, shutil, sys, tempfile, threading
@@ -45,36 +74,31 @@ class LastzJobRunner( object ):
 def __main__():
     #Parse Command Line
     parser = optparse.OptionParser()
-    parser.add_option( '', '--ref_name', dest='ref_name', help='' )
-    parser.add_option( '', '--ref_source', dest='ref_source', help='' )
+    parser.add_option( '', '--ref_name', dest='ref_name', help='The reference name to append to all output matches' )
+    parser.add_option( '', '--ref_source', dest='ref_source', help='Whether the reference is cached or from the history' )
     parser.add_option( '', '--ref_sequences', dest='ref_sequences', help='Number of sequences in the reference dataset' )
-    parser.add_option( '', '--source_select', dest='source_select', help='' )
-    parser.add_option( '', '--out_format', dest='out_format', help='' )
-    parser.add_option( '', '--input1', dest='input1', help='' )
-    parser.add_option( '', '--input2', dest='input2', help='' )
-    parser.add_option( '', '--pre_set_options', dest='pre_set_options', help='' )
-    parser.add_option( '', '--strand', dest='strand', help='' )
-    parser.add_option( '', '--seed', dest='seed', help='' )
-    parser.add_option( '', '--transition', dest='transition', help='' )
-    parser.add_option( '', '--gfextend', dest='gfextend', help='' )
-    parser.add_option( '', '--nogfextend', dest='nogfextend', help='' )
-    parser.add_option( '', '--chain', dest='chain', help='' )
-    parser.add_option( '', '--nochain', dest='nochain', help='' )
-    parser.add_option( '', '--gapped', dest='gapped', help='' )
-    parser.add_option( '', '--nogapped', dest='nogapped', help='' )
-    parser.add_option( '', '--O', dest='O', help='' )
-    parser.add_option( '', '--E', dest='E', help='' )
-    parser.add_option( '', '--X', dest='X', help='' )
-    parser.add_option( '', '--Y', dest='Y', help='' )
-    parser.add_option( '', '--K', dest='K', help='' )
-    parser.add_option( '', '--L', dest='L', help='' )
-    parser.add_option( '', '--entropy', dest='entropy', help='' )
-    parser.add_option( '', '--identity_min', dest='identity_min', help='' )
-    parser.add_option( '', '--identity_max', dest='identity_max', help='' )
-    parser.add_option( '', '--coverage', dest='coverage', help='' )
-    parser.add_option( '', '--format', dest='format', help='' )
+    parser.add_option( '', '--source_select', dest='source_select', help='Whether to used pre-set or cached reference file' )
+    parser.add_option( '', '--input1', dest='input1', help='The name of the reference file if using history or reference base name if using cached' )
+    parser.add_option( '', '--input2', dest='input2', help='The reads file to align' )
+    parser.add_option( '', '--pre_set_options', dest='pre_set_options', help='Which of the pre set options to use, if using pre-sets' )
+    parser.add_option( '', '--strand', dest='strand', help='Which strand of the read to search, if specifying all parameters' )
+    parser.add_option( '', '--seed', dest='seed', help='Seeding settings, if specifying all parameters' )
+    parser.add_option( '', '--transition', dest='transition', help='Number of transitions to allow in each seed hit, if specifying all parameters' )
+    parser.add_option( '', '--gfextend', dest='gfextend', help='Whether to perform gap-free extension of seed hits to HSPs (high scoring segment pairs), if specifying all parameters' )
+    parser.add_option( '', '--chain', dest='chain', help='Whether to perform chaining of HSPs, if specifying all parameters' )
+    parser.add_option( '', '--O', dest='O', help='Gap opening penalty, if specifying all parameters' )
+    parser.add_option( '', '--E', dest='E', help='Gap extension penalty, if specifying all parameters' )
+    parser.add_option( '', '--X', dest='X', help='X-drop threshold, if specifying all parameters' )
+    parser.add_option( '', '--Y', dest='Y', help='Y-drop threshold, if specifying all parameters' )
+    parser.add_option( '', '--K', dest='K', help='Threshold for HSPs, if specifying all parameters' )
+    parser.add_option( '', '--L', dest='L', help='Threshold for gapped alignments, if specifying all parameters' )
+    parser.add_option( '', '--entropy', dest='entropy', help='Whether to involve entropy when filtering HSPs, if specifying all parameters' )
+    parser.add_option( '', '--identity_min', dest='identity_min', help="Minimum identity (don't report matches under this identity)" )
+    parser.add_option( '', '--identity_max', dest='identity_max', help="Maximum identity (don't report matches above this identity)" )
+    parser.add_option( '', '--coverage', dest='coverage', help="The minimum coverage value (don't report matches covering less than this)" )
+    parser.add_option( '', '--out_format', dest='format', help='The format of the output file (sam, diffs, or tabular (general))' )
     parser.add_option( '', '--output', dest='output', help='The output file' )
-    parser.add_option( '', '--num_threads', dest='num_threads', help='' )
+    parser.add_option( '', '--num_threads', dest='num_threads', help='The number of threads to run' )
     parser.add_option( '', '--lastzSeqsFileDir', dest='lastzSeqsFileDir', help='Directory of local lastz_seqs.loc file' )
     ( options, args ) = parser.parse_args()
 
@@ -88,9 +112,9 @@ def __main__():
         set_options = '--%s' % options.pre_set_options
     # Prepare for user-specified options
     else:
-        set_options = '%s %s --%s --%s --%s --%s O=%s E=%s X=%s Y=%s K=%s L=%s %s' % \
-                    ( options.gfextend, options.chain, options.gapped, options.strand, 
-                      options.seed, options.transition, options.O, options.E, options.X, 
+        set_options = '--%s --%s --gapped --%s --%s --%s O=%s E=%s X=%s Y=%s K=%s L=%s --%s' % \
+                    ( options.gfextend, options.chain, options.strand, options.seed, 
+                      options.transition, options.O, options.E, options.X, 
                       options.Y, options.K, options.L, options.entropy )
     # Specify input2 and add [fullnames] modifier if output format is diffs
     if options.format == 'diffs':
@@ -107,18 +131,21 @@ def __main__():
     if options.ref_source == 'history':
         # Reference is a fasta dataset from the history, so split job across number of
         # sequences in the dataset
-        try:
-            error_msg = "The reference dataset is missing metadata, click the pencil icon in the history item and 'auto-detect' the metadata attributes."
-            ref_sequences = int( options.ref_sequences )
-            if ref_sequences < 1:
-                stop_err( error_msg )
-        except:
-            stop_err( error_msg )
-        for seq in range( ref_sequences ):
+#        try:
+#            error_msg = "The reference dataset is missing metadata, click the pencil icon in the history item and 'auto-detect' the metadata attributes."
+#            ref_sequences = int( options.ref_sequences )
+#            if ref_sequences < 1:
+#                stop_err( error_msg )
+#        except:
+#            stop_err( error_msg )
+        # Currently set up to work only for a fasta file with a single sequence
+        for seq in range(1):
+#        for seq in range( ref_sequences ):
             command = 'lastz %s%s %s %s --ambiguousn --nolaj --identity=%s..%s --coverage=%s --format=%s%s >> %s' % \
                 ( ref_name, options.input1, input2, set_options, options.identity_min, 
                   options.identity_max, options.coverage, format, tabular_fields, options.output )
             commands.append( command )
+            print command
     else:
         # Reference is a locally cached 2bit file, split job across number of chroms in 2bit file
         tbf = TwoBitFile( open( options.input1, 'r' ) )
@@ -127,7 +154,6 @@ def __main__():
                 ( ref_name, options.input1, chrom, input2, set_options, options.identity_min, 
                   options.identity_max, options.coverage, format, tabular_fields, options.output )
             commands.append( command )
-        tbf.close()
     job_runner = LastzJobRunner( int( options.num_threads ), commands )
 
 if __name__=="__main__": __main__()
