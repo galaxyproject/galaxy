19e8be887fd8a9b6124a1a00bad8f0867b09fc35
James Taylor <james@jamestaylor.org>
2010-02-01 16:23:20 -0500

    Fix md5 hashing helper for Python 2.4

--- lib/galaxy/util/hash_util.py
+++ lib/galaxy/util/hash_util.py
@@ -1,28 +1,33 @@
-import sys, logging
-using_24 = sys.version_info[:2] < ( 2, 5 )
-if using_24:
-    import sha
-else:
-    import hashlib
-import hmac
-
-log = logging.getLogger( __name__ )
-
 """
 Utility functions for bi-directional Python version compatibility.  Python 2.5
 introduced hashlib which replaced sha in Python 2.4 and previous versions.
 """
+
+import sys, logging
+
+# Use hashlib module if for Python 2.5+, fall back on old sha and md5 modules
+try:
+    import hashlib
+    sha1 = hashlib.sha1
+    md5 = hashlib.md5
+except ImportError, e:
+    import sha, md5
+    sha1 = sha.new
+    md5 = md5.new
+    
+import hmac
+
+log = logging.getLogger( __name__ )
+
 def new_secure_hash( text_type=None ):
-    if using_24:
-        if text_type:
-            return sha.new( text_type ).hexdigest()
-        return sha.new()
+    """
+    Returns either a sha1 hash object (if called with no arguments), or a
+    hexdigest of the sha1 hash of the argument `text_type`.
+    """
+    if text_type:
+        return sha1( text_type ).hexdigest()
     else:
-        if text_type:
-            return hashlib.sha1( text_type ).hexdigest()
-        return hashlib.sha1()
+        return sha1()
+
 def hmac_new( key, value ):
-    if using_24:
-        return hmac.new( key, value, sha ).hexdigest()
-    else:
-        return hmac.new( key, value, hashlib.sha1 ).hexdigest()
+    return hmac.new( key, value, sha1 ).hexdigest()
