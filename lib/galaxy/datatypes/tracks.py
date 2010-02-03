5a0b072e568cacf0294d489859af220857507b57
Daniel Blankenberg <dan@bx.psu.edu>
2010-02-03 16:33:25 -0500

    Make the GeneTrack datatype binary, containing only the 'indexed' data needed by the GeneTrack server. Better creation of the URL for running the peak predictor tool. \nStill some cleanup/decoupling possible.

--- lib/galaxy/datatypes/tracks.py
+++ lib/galaxy/datatypes/tracks.py
@@ -2,41 +2,36 @@
 Datatype classes for tracks/track views within galaxy.
 """
 
-import data
-import tabular, binascii, logging
-from galaxy.datatypes.metadata import MetadataElement
-from galaxy.datatypes import metadata
-import galaxy.model
+import binary, binascii, logging
 from galaxy import util
 from galaxy.web import url_for
-from sniff import *
-from galaxy.util.hash_util import *
+from galaxy.util.hash_util import hmac_new
+import urllib
 
 log = logging.getLogger(__name__)
 
-class GeneTrack( tabular.Tabular ):
+class GeneTrack( binary.Binary ):
     file_ext = "genetrack"
-    
-    MetadataElement( name="genetrack", default="data.genetrack", desc="HDF index", readonly=True, visible=True, no_value=0 )
-    MetadataElement( name="label", default="Custom", desc="Track Label", readonly=True, visible=True, no_value="Custom" )
     
     def __init__(self, **kwargs):
         super( GeneTrack, self ).__init__( **kwargs )
         self.add_display_app( 'genetrack', 'View in', '', 'genetrack_link' )
-    def get_display_links( self, dataset, type, app, base_url, target_frame='galaxy_main', **kwd ):
-        return data.Data.get_display_links( self, dataset, type, app, base_url, target_frame=target_frame, **kwd )
+    def get_display_links( self, dataset, type, app, base_url, target_frame='galaxy_main', **kwd ): #Force target_frame to be 'galaxy_main'
+        return binary.Binary.get_display_links( self, dataset, type, app, base_url, target_frame=target_frame, **kwd )
     def genetrack_link( self, hda, type, app, base_url ):
         ret_val = []
         if hda.has_data:
             # Get the disk file name and data id
             file_name = hda.dataset.get_file_name()
-            data_id  = hda.id
+            data_id  = urllib.quote_plus( str( hda.id ) ) #can we name this 'input' in the passed params instead of 'id' to prevent GT from having to map 'id' to 'input'?
+            galaxy_url = urllib.quote_plus( "%s%s" % ( base_url, url_for( controller = 'tool_runner' ) ) )
+            tool_id = urllib.quote_plus( 'predict2genetrack' )
             # Make it secure
-            hashkey = hmac_new( app.config.tool_secret, file_name )
-            encoded = binascii.hexlify( file_name )
+            hashkey = urllib.quote_plus( hmac_new( app.config.tool_secret, file_name ) )
+            encoded = urllib.quote_plus( binascii.hexlify( file_name ) )
             for name, url in util.get_genetrack_sites():
                 if name.lower() in app.config.genetrack_display_sites:
                     # send both  parameters filename and hashkey
-                    link = "%s?filename=%s&hashkey=%s&id=%s&GALAXY_URL=%s" % ( url, encoded, hashkey, data_id, base_url )
+                    link = "%s?filename=%s&hashkey=%s&id=%s&GALAXY_URL=%s&tool_id=%s" % ( url, encoded, hashkey, data_id, galaxy_url, tool_id )
                     ret_val.append( ( name, link ) )
             return ret_val
--- tools/visualization/genetrack_indexer.py
+++ tools/visualization/genetrack_indexer.py
@@ -37,14 +37,3 @@ if __name__ == "__main__":
     else:
         tabs2genetrack.transform(inpname=options.inpname, outname=options.outname,\
             format=options.format, shift=options.shift, index=options.index, options=options)
-
-    #HACK ALERT
-    #output created in job working directory has a different name than the final destination.
-    #GeneTrack uses dataset_name.hdf for indexes
-    #This will fix the different name problem, but this is not a secure solution
-    #This is Exceptionally Fragile as any change to the way that temporary output names are created could break 
-    #GeneTrack should allow explicit definition of the index file, not filename.hdf
-    if options.workdir and os.path.isdir( options.workdir ):
-        for filename in os.listdir( options.workdir ):
-            if filename.endswith( '.dat.hdf' ) and filename.startswith( 'galaxy_dataset_' ):
-                shutil.move( os.path.join( options.workdir, filename ), os.path.join( options.workdir, filename[ len( 'galaxy_' ): ] ) )
