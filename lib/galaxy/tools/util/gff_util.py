eed5d297c383e531b043c16c25f8667c238b42b8
Jeremy Goecks <jeremy.goecks@emory.edu>
2010-06-07 16:28:46 -0400

    Add GFF support to GOPS subtract tool and updated functional tests. Using the GFF-->BED converter is lossy as GFF has more attributes/features than BED, so it's necessary to directly support GFF. Basic approach used is to modify interval coordinates during reading and writing while not changing the middle, computational steps; this approach can be used to provide GFF support for any tool that uses bx-python to operate on intervals.

---
+++ lib/galaxy/tools/util/gff_util.py
@@ -0,0 +1,28 @@
+"""
+Provides utilities for working with GFF files.
+"""
+
+from bx.intervals.io import NiceReaderWrapper, GenomicInterval
+
+class GFFReaderWrapper( NiceReaderWrapper ):
+    """
+    Reader wrapper converts GFF format--starting and ending coordinates are 1-based, closed--to the 'traditional' interval format--0 based, 
+    half-open. This is useful when using GFF files as inputs to tools that expect traditional interval format.
+    """
+    def parse_row( self, line ):
+        interval = GenomicInterval( self, line.split( "\t" ), self.chrom_col, self.start_col, self.end_col, self.strand_col, self.default_strand, fix_strand=self.fix_strand )
+        # Change from 1-based to 0-based format.
+        interval.start -= 1
+        # Add 1 to end to move from open to closed format for end coordinate.
+        interval.end += 1
+        return interval
+        
+def convert_to_gff_coordinates( interval ):
+    """
+    Converts a GenomicInterval's coordinates to GFF format.
+    """
+    if type( interval ) is GenomicInterval:
+        interval.start += 1
+        interval.end -= 1
+        return interval
+    return interval
