1bac7400730f8277742104d068cf8b051a9d8620
Ramkrishna Chakrabarty <rc@bx.psu.edu>
2010-09-20 12:07:09 -0400

    mutation viz tool - now the tool requires the user to enter the specific columns indicating ref base, position, and start of a sample - added an interactive zoom option. when this option is selected, the image can be zoomed in or out using the mouse wheel. - fixed the firefox & opera problems in displaying the svg image

--- tools/mutation/visualize.py
+++ tools/mutation/visualize.py
@@ -27,37 +27,32 @@ colors = {'A':'blue', 'C':'green', 'G':'orange', 'T':'red'}
 bases = ['A', 'C', 'G', 'T' ]
 
 
-def mainsvg(opts):
-    s = svg.SVG('g', transform="translate(20,0)")
+def mainsvg(opts, args):
+    s = svg.SVG('g', id='viewport')
     
     # display legend
     for i, b in enumerate( bases ):
-        bt = svg.SVG("tspan", b, style="font-family:Verdana;font-size:3")
-        s.append(svg.SVG("text", bt, x=12+(i*10), y=8, stroke="none", fill="black"))
-        s.append(svg.SVG("rect", x=13+(i*10), y=5, width=4, height=3, 
+        bt = svg.SVG("tspan", b, style="font-family:Verdana;font-size:20%")
+        s.append(svg.SVG("text", bt, x=12+(i*10), y=4, stroke="none", fill="black"))
+        s.append(svg.SVG("rect", x=14+(i*10), y=1, width=4, height=3, 
                          stroke="none", fill=colors[b], fill_opacity=0.5))
-    
-    # display labels
-    bt = svg.SVG("tspan", "1's", style="font-family:Verdana;font-size:3")
-    s.append(svg.SVG("text", bt, x=60, y=8, stroke="none", fill="black"))
-    s.append(svg.SVG("rect", x=62, y=5, width=4, height=3, 
-                     stroke="none", fill='grey', fill_opacity=0.1))
-    bt = svg.SVG("tspan", "2's", style="font-family:Verdana;font-size:3")
-    s.append(svg.SVG("text", bt, x=70, y=8, stroke="none", fill="black"))
-    s.append(svg.SVG("rect", x=72, y=5, width=4, height=3, 
-                     stroke="none", fill='grey', fill_opacity=0.2))
 
-    
     reader = open(opts.input_file, 'U')
 
-    headers = []
-    if opts.header_row:
-        headers = [yy for yy in reader.readline().split('\t') if yy.strip()]
-    
-    for i, h in enumerate(headers):
-        x = 20+(i*10)
+    samples = []
+    for i in range(int(len(args)/3)):
+        index = i*3
+        samples.append(dict(name=args[index],
+                            a_col=args[index+1],
+                            totals_col=args[index+2]))
+
+    if opts.zoom == 'interactive':
+        y = 35
+    else:
         y = 25
-        t = svg.SVG("text", svg.SVG("tspan", h, style="font-family:Verdana;font-size:3"), 
+    for i, sample in enumerate(samples):
+        x = 23+(i*(WIDTH+GAP))
+        t = svg.SVG("text", svg.SVG("tspan", sample['name'], style="font-family:Verdana;font-size:25%"), 
                     x=x, y=y, transform="rotate(-90 %i,%i)" % (x, y), stroke="none", fill="black")
         s.append(t)
     
@@ -66,25 +61,29 @@ def mainsvg(opts):
         row = line.split('\t')
         highlighted_position = False
         show_pos = True
-        position = int(row[1])
-        ref = row[3]
-        #print 'position', position, ref, count
+        position = row[int(opts.position_col)-1]
+        ref = row[int(opts.ref_col)-1]
+        print 'position', position
+        print 'ref', ref
+        print 'count', count
         
         # display positions
-        bt = svg.SVG("tspan", str(position), style="font-family:Verdana;font-size:4")
-        s.append(svg.SVG("text", bt, x=7, y=34+(count*(HEIGHT+GAP)), stroke="none", fill="black"))
+        if opts.zoom == 'interactive':
+            textx = 0
+        else:
+            textx = 7
+        bt = svg.SVG("tspan", str(position), style="font-family:Verdana;font-size:25%")
+        s.append(svg.SVG("text", bt, x=textx, y=34+(count*(HEIGHT+GAP)), stroke="none", fill="black"))
         s.append(svg.SVG("rect", x=0, y=30+(count*(HEIGHT+GAP)), width=14, height=HEIGHT, 
                          stroke='none', fill=colors[ref.upper()], fill_opacity=0.2))
         
-        for sample_index in range(int((len(row)-HEADER_COLS)/COLS_PER_SAMPLE)):
-            start_col = HEADER_COLS+(COLS_PER_SAMPLE*sample_index)
-            n_a = int(row[start_col])
-            n_c = int(row[start_col+1])
-            n_g = int(row[start_col+2])
-            n_t = int(row[start_col+3])
-            total = int(row[start_col+4])
-            diff = int(row[start_col+5])
-            imp = int(row[start_col+6])
+        for sample_index, sample in enumerate(samples):
+            n_a = int(row[int(sample['a_col'])-1])
+            n_c = int(row[int(sample['a_col'])+1-1])
+            n_g = int(row[int(sample['a_col'])+2-1])
+            n_t = int(row[int(sample['a_col'])+3-1])
+            total = int(row[int(sample['totals_col'])-1])
+            imp = 1 #int(row[start_col+6])
             if total:
                 x = 16+(sample_index*(WIDTH+GAP))
                 y = 30+(count*(HEIGHT+GAP))
@@ -97,37 +96,56 @@ def mainsvg(opts):
                     fill_opacity = 0.2
                 else:
                     fill_opacity = 0.3
+
+                if count%2:
+                    s.append(svg.SVG("rect", x=x, y=y, width=width, height=height, 
+                                     stroke='none', fill='grey', fill_opacity=0.25))
+                else:
+                    s.append(svg.SVG("rect", x=x, y=y, width=width, height=height, 
+                                     stroke='none', fill='grey', fill_opacity=0.25))
                 
-                s.append(svg.SVG("rect", x=x, y=y, width=width, height=height, 
-                                 stroke='none', fill='grey', fill_opacity=fill_opacity))
                 for base, value in enumerate([n_a, n_c, n_g, n_t]):
                     width = int(math.ceil(value / total * WIDTH))
                     s.append(svg.SVG("rect", x=x, y=y, width=width, height=BAR_WIDTH, 
                                      stroke='none', fill=colors[bases[base]], fill_opacity=0.6))
                     y = y + BAR_WIDTH
-                    #print base, value, total, x, y, width
-                    
-
 
         count=count+1
         
-    #print x, y
-    
-    zoom = int(opts.zoom)
-    w = "%ipx" % (x*(10+zoom))
-    h = "%ipx" % (y*(2+zoom))
-    canv = svg.canvas(s, width=w, height=h, viewBox="0 0 %i %i" %(x+100, y+100))
-    canv.save(opts.output_file)
+    if opts.zoom == 'interactive':
+        canv = svg.canvas(s)
+        canv.save(opts.output_file)
+        import fileinput
+        flag = False
+        for line in fileinput.input(opts.output_file, inplace=1):
+            if line.startswith('<svg'):
+                print '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">'
+                flag = True
+                continue
+            else:
+                if flag:
+                    print '<script xlink:href="/static/scripts/SVGPan.js"/>\r\n<defs id="defs4" />\r\n<script xlink:href="/static/scripts/SVGPan.js"/>'
+                flag = False
+            print line,
+    else:
+        zoom = int(opts.zoom)
+        w = "%ipx" % (x*(10+zoom))
+        h = "%ipx" % (y*(2+zoom))
+        canv = svg.canvas(s, width=w, height=h, viewBox="0 0 %i %i" %(x+100, y+100)) 
+        canv.save(opts.output_file)
 
-    
 if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option('-i', '--input-file', dest='input_file', action='store')
     parser.add_option('-o', '--output-file', dest='output_file', action='store')
     parser.add_option('-z', '--zoom', dest='zoom', action='store', default='1')
-    parser.add_option('-n', '--noheaders', dest='header_row', action='store_false', default=True)
+    parser.add_option('-p', '--position_col', dest='position_col', action='store', default='c0')
+    parser.add_option('-r', '--ref_col', dest='ref_col', action='store', default='c1')
+    #parser.add_option('-n', '--interactive', dest='interactive', action='store_false', default='True')
     (opts, args) = parser.parse_args()
-    mainsvg(opts)
+    print opts
+    print args
+    mainsvg(opts, args)
     sys.exit(1)
 
     
