9bcbbeccffcf131536d3d80aa87d8136ebc0f768
Peter Cock <p.j.a.cock@googlemail.com>
2010-09-30 20:00:15 +0100

    Allow filter to pick ID column

--- tools/ncbi_blast_plus/blast_filter_fasta.py
+++ tools/ncbi_blast_plus/blast_filter_fasta.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
-"""Filter a FASTA file using tabular BLAST output.
+"""Filter a FASTA file using tabular output, e.g. from BLAST.
 
-Takes four command line options, tabular BLAST filename, input FASTA filename,
-and two output FASTA filenames (for records with and without any BLAST hits).
+Takes five command line options, tabular BLAST filename, ID column number
+(using one based counting), input FASTA filename, and two output FASTA
+filenames (for records with and without any BLAST hits).
+
+In the default NCBI BLAST+ tabular output, the query sequence ID is in column
+one, and the ID of the match from the database is in column two.
 """
-#TODO - Option to define which column to use for ID?
-
 import sys
 from galaxy_utils.sequence.fasta import fastaReader, fastaWriter
 
 #Parse Command Line
-blast_file, in_file, out_positive_file, out_negative_file = sys.argv[1:]
+blast_file, blast_col, in_file, out_positive_file, out_negative_file = sys.argv[1:]
+blast_col = int(blast_col)-1
+assert blast_col >= 0
 
 #Read tabular BLAST file and record all queries with hit(s)
 ids = set()
 blast_handle = open(blast_file, "rU")  
 for line in blast_handle:
-    ids.add(line.split("\t")[0])
+    ids.add(line.split("\t")[blast_col])
 blast_handle.close()
 
 #Write filtered FASTA file based on IDs from BLAST file
