7ca626b9f145b0739d011e611d3aae44547e2edb
Ramkrishna Chakrabarty <rc@bx.psu.edu>
2010-10-26 23:04:22 -0400

    Added setup script to configure RabbitMQ

---
+++ scripts/galaxy_messaging/server/setup_rabbitmq.py
@@ -0,0 +1,34 @@
+#
+# Configuration script for RabbitMQ in Galaxy
+#
+# Requirements:
+# - set the rabbitmqctl_path variable in the 'galaxy_amqp' section in Galaxy config file
+#
+# Usage:
+# $ python setup_rabbitmq.py <Galaxy config file>
+#
+
+import os, sys, csv, ConfigParser
+
+def main( config_file ):
+    config = ConfigParser.ConfigParser()
+    config.read( config_file )
+    rabbitmqctl_path = config.get( 'galaxy_amqp', 'rabbitmqctl_path' )
+    username = config.get( 'galaxy_amqp', 'userid' )
+    password = config.get( 'galaxy_amqp', 'password' )
+    virtual_host = config.get( 'galaxy_amqp', 'virtual_host' )
+    
+    cmd_list = [
+                'add_user %s %s' % ( username, password ),
+                'add_vhost %s' % config.get( 'galaxy_amqp', 'virtual_host' ),
+                'set_permissions -p %s %s "%s.*" ".*" ".*"' % ( virtual_host, username, username )
+               ]
+    
+    for cmd in cmd_list:
+        retval = os.system( rabbitmqctl_path + ' ' + cmd )
+        if retval:
+            print "Failed command: %s" % cmd
+            sys.exit(1)
+    
+if __name__ == '__main__':
+    main( sys.argv[1] )
