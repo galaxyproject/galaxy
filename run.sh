d0f5f965fa70adc2079b2f5c126c71df9bdf52b6
Nate Coraor <nate@bx.psu.edu>
2010-11-05 15:13:06 -0400

    Automatically find new .sample files in tool-data/.

--- run.sh
+++ run.sh
@@ -5,47 +5,22 @@ cd `dirname $0`
 python ./scripts/check_python.py
 [ $? -ne 0 ] && exit 1
 
-FROM_SAMPLE="
-    datatypes_conf.xml
-    reports_wsgi.ini
-    tool_conf.xml
-    tool_data_table_conf.xml
-    universe_wsgi.ini
-    tool-data/add_scores.loc
-    tool-data/alignseq.loc
-    tool-data/annotation_profiler_options.xml
-    tool-data/annotation_profiler_valid_builds.txt
-    tool-data/bfast_indexes.loc
-    tool-data/binned_scores.loc
-    tool-data/blastdb.loc
-    tool-data/blastdb_p.loc
-    tool-data/bowtie_indices.loc
-    tool-data/bowtie_indices_color.loc
-    tool-data/codingSnps.loc
-    tool-data/encode_datasets.loc
-    tool-data/funDo.loc
-    tool-data/lastz_seqs.loc
-    tool-data/liftOver.loc
-    tool-data/maf_index.loc
-    tool-data/maf_pairwise.loc
-    tool-data/microbial_data.loc
-    tool-data/phastOdds.loc
-    tool-data/perm_base_index.loc
-    tool-data/perm_color_index.loc
-    tool-data/quality_scores.loc
-    tool-data/regions.loc
-    tool-data/sam_fa_indices.loc
-    tool-data/sift_db.loc
-    tool-data/srma_index.loc
-    tool-data/twobit.loc
-    tool-data/shared/ucsc/builds.txt
+SAMPLES="
+    datatypes_conf.xml.sample
+    reports_wsgi.ini.sample
+    tool_conf.xml.sample
+    tool_data_table_conf.xml.sample
+    universe_wsgi.ini.sample
+    tool-data/shared/ucsc/builds.txt.sample
+    tool-data/*.sample
 "
 
 # Create any missing config/location files
-for file in $FROM_SAMPLE; do
-    if [ ! -f "$file" -a -f "$file.sample" ]; then
-        echo "Initializing $file from `basename $file`.sample"
-        cp $file.sample $file
+for sample in $SAMPLES; do
+    file=`echo $sample | sed -e 's/\.sample$//'`
+    if [ ! -f "$file" -a -f "$sample" ]; then
+        echo "Initializing $file from `basename $sample`"
+        cp $sample $file
     fi
 done
 
