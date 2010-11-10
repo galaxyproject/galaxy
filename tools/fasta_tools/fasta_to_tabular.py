9a806235dffe20d9076c8493abff316a02cd55c1
Peter Cock <p.j.a.cock@googlemail.com>
2010-11-10 11:36:05 +0000

    Extend fasta_to_tabular converter to offer more than 2 column output

--- tools/fasta_tools/fasta_to_tabular.py
+++ tools/fasta_tools/fasta_to_tabular.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
 # This code exists in 2 places: ~/datatypes/converters and ~/tools/fasta_tools
 """
-Input: fasta, int
+Input: fasta (input file), tabular (output file), int (truncation of id), int (columns from description)
 Output: tabular
 format convert: fasta to tabular
 """
 
 import sys, os
 
+def stop_err( msg ):
+    sys.stderr.write( msg )
+    sys.exit()
+
 def __main__():
+    if len(sys.argv) != 5:
+        stop_err("Wrong number of argument. Expect four (fasta, tabular, truncation, columns)")
     infile = sys.argv[1]
     outfile = sys.argv[2]
     keep_first = int( sys.argv[3] )
+    descr_split = int( sys.argv[4] )
     fasta_title = fasta_seq = ''
     if keep_first == 0:
         keep_first = None
-    else:
+    elif descr_split == 1:
+        #Added one for the ">" character
+        #(which is removed if using descr_split > 1)
         keep_first += 1
+    if descr_split < 1:
+        stop_err("Bad description split value (should be 1 or more)")
     out = open( outfile, 'w' )
     for i, line in enumerate( open( infile ) ):
         line = line.rstrip( '\r\n' )
@@ -25,7 +36,16 @@ def __main__():
         if line.startswith( '>' ):
             if i > 0:
                 out.write('\n')
-            out.write(line[1:keep_first])
+            if descr_split == 1:
+                out.write(line[1:keep_first])
+            else:
+                #Truningn columns
+                words = line[1:].split(None, descr_split-1)
+                #apply any truncation to first word (the id)
+                words[0] = words[0][0:keep_first]
+                #pad with empty columns if required
+                words += [""]*(descr_split-len(words))
+                out.write("\t".join(words))
             out.write('\t')
         else:
             out.write(line)
@@ -34,4 +54,3 @@ def __main__():
     out.close()
 
 if __name__ == "__main__" : __main__()
-    
