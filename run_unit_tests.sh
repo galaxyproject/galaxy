86990b25381a24653c26ccd05a5c00b69b939d10
Nate Coraor <nate@bx.psu.edu>
2011-02-16 13:48:14 -0500

    Fix unit tests failing on missing dependencies in job runners

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 ## Excluding controllers due to the problematic genetrack dependency
+## Excluding job runners due to various external dependencies
 
 python ./scripts/nosetests.py -v -w lib \
     --with-nosehtml --html-report-file run_unit_tests.html \
-    --with-doctest --exclude=functional --exclude="^get" --exclude=controllers
+    --with-doctest --exclude=functional --exclude="^get" \
+    --exclude=controllers --exclude=runners
