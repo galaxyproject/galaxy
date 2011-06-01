51aeadf8e5e3db41e652442c02d563841c233b4b
Nate Coraor <nate@bx.psu.edu>
2011-06-01 15:12:12 -0400

    Fix the user create/login links in the FTP upload form

--- lib/galaxy/web/form_builder.py
+++ lib/galaxy/web/form_builder.py
@@ -185,7 +185,7 @@ class FTPFileField(BaseField):
     def get_html( self, prefix="" ):
         rval = FTPFileField.thead
         if self.dir is None:
-            rval += '<tr><td colspan="3"><em>Please <a href="%s">create</a> or <a href="%s">log in to</a> a Galaxy account to view files uploaded via FTP.</em></td></tr>' % ( url_for( controller='user', action='create', referer=url_for( controller='root' ) ), url_for( controller='user', action='login', referer=url_for( controller='root' ) ) )
+            rval += '<tr><td colspan="3"><em>Please <a href="%s">create</a> or <a href="%s">log in to</a> a Galaxy account to view files uploaded via FTP.</em></td></tr>' % ( url_for( controller='user', action='create', cntrller='user', referer=url_for( controller='root' ) ), url_for( controller='user', action='login', cntrller='user', referer=url_for( controller='root' ) ) )
         elif not os.path.exists( self.dir ):
             rval += '<tr><td colspan="3"><em>Your FTP upload directory contains no files.</em></td></tr>'
         else:
