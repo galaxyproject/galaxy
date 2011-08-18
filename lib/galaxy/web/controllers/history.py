6f9701bb413962b97a404fcba4e5a52ce8832b42
Nate Coraor <nate@bx.psu.edu>
2011-08-18 13:08:39 -0400

    Decrease disk usage and actually remove datasets (if applicable) when purging a history.

--- lib/galaxy/web/controllers/history.py
+++ lib/galaxy/web/controllers/history.py
@@ -271,9 +271,12 @@ class HistoryController( BaseController, Sharable, UsesAnnotations, UsesItemRati
                 n_deleted += 1
             if purge and trans.app.config.allow_user_dataset_purge:
                 for hda in history.datasets:
+                    if trans.user:
+                        trans.user.total_disk_usage -= hda.quota_amount( trans.user )
                     hda.purged = True
                     trans.sa_session.add( hda )
                     trans.log_event( "HDA id %s has been purged" % hda.id )
+                    trans.sa_session.flush()
                     if hda.dataset.user_can_purge:
                         try:
                             hda.dataset.full_delete()
