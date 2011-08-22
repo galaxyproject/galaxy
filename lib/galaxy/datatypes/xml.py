8768351b991ca024c475b579534a320fdcdf47db
Daniel Blankenberg <dan@bx.psu.edu>
2011-08-22 14:52:11 -0400

    Add FIMO tool from MEME suite.

--- lib/galaxy/datatypes/xml.py
+++ lib/galaxy/datatypes/xml.py
@@ -102,3 +102,18 @@ class MEMEXml( GenericXml ):
             dataset.blurb = 'file purged from disk'
     def sniff( self, filename ):
         return False
+
+class CisML( GenericXml ):
+    """CisML XML data""" #see: http://www.ncbi.nlm.nih.gov/pubmed/15001475
+    file_ext = "cisml"
+
+    def set_peek( self, dataset, is_multi_byte=False ):
+        """Set the peek and blurb text"""
+        if not dataset.dataset.purged:
+            dataset.peek = data.get_file_peek( dataset.file_name, is_multi_byte=is_multi_byte )
+            dataset.blurb = 'CisML data'
+        else:
+            dataset.peek = 'file does not exist'
+            dataset.blurb = 'file purged from disk'
+    def sniff( self, filename ):
+        return False
