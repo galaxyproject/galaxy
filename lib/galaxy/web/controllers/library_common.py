874cc077b452da4263f1acd27668ce812b8a3746
Nate Coraor <nate@bx.psu.edu>
2011-08-29 10:57:08 -0400

    Properly handle error conditions in library_common.upload_dataset when uploading via the API.

--- lib/galaxy/web/controllers/library_common.py
+++ lib/galaxy/web/controllers/library_common.py
@@ -852,6 +852,8 @@ class LibraryCommon( BaseController, UsesFormDefinitions ):
                         # created_outputs_dict can be a string only if cntrller == 'api'
                         if type( created_outputs_dict ) == str:
                             return 400, created_outputs_dict
+                        elif type( created_outputs_dict ) == tuple:
+                            return created_outputs_dict[0], created_outputs_dict[1]
                         return 200, created_outputs_dict
                     total_added = len( created_outputs_dict.keys() )
                     ldda_id_list = [ str( v.id ) for k, v in created_outputs_dict.items() ]
