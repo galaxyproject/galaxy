cbe4b3759d7b23218edbc237f1e69474bc57332f
Jeremy Goecks <jeremy.goecks@emory.edu>
2011-09-30 18:38:30 -0400

    Fix problems with computing indices for GFF/GTF datasets.

--- lib/galaxy/datatypes/util/gff_util.py
+++ lib/galaxy/datatypes/util/gff_util.py
@@ -133,6 +133,7 @@ class GFFReaderWrapper( NiceReaderWrapper ):
         self.last_line = None
         self.cur_offset = 0
         self.seed_interval = None
+        self.seed_interval_line_len = 0
     
     def parse_row( self, line ):
         interval = GFFInterval( self, line.split( "\t" ), self.chrom_col, self.feature_col, \
@@ -160,12 +161,12 @@ class GFFReaderWrapper( NiceReaderWrapper ):
             # For debugging, uncomment this to propogate parsing exceptions up. 
             # I.e. the underlying reason for an unexpected StopIteration exception 
             # can be found by uncommenting this. 
-            #raise e
+            # raise e
                
         #
         # Get next GFFFeature
         #
-        raw_size = 0 
+        raw_size = self.seed_interval_line_len
 
         # If there is no seed interval, set one. Also, if there are no more 
         # intervals to read, this is where iterator dies.
@@ -184,8 +185,9 @@ class GFFReaderWrapper( NiceReaderWrapper ):
             return_val = self.seed_interval
             return_val.raw_size = len( self.current_line )
             self.seed_interval = None
+            self.seed_interval_line_len = 0
             return return_val
-    
+            
         # Initialize feature identifier from seed.
         feature_group = self.seed_interval.attributes.get( 'group', None ) # For GFF
         # For GFF3
@@ -220,21 +222,28 @@ class GFFReaderWrapper( NiceReaderWrapper ):
             if isinstance( interval, Comment ):
                 continue
             
-            # If interval not associated with feature, break.
+            # Determine if interval is part of feature.
+            part_of = True
             group = interval.attributes.get( 'group', None )
             # GFF test:
             if group and feature_group != group:
-                break
+                part_of = False
             # GFF3 test:
             parent_id = interval.attributes.get( 'Parent', None )
             cur_id = interval.attributes.get( 'ID', None )
             if ( cur_id and cur_id != feature_id ) or ( parent_id and parent_id != feature_id ):
-                break
+                part_of = False
             # GTF test:
             gene_id = interval.attributes.get( 'gene_id', None )
             transcript_id = interval.attributes.get( 'transcript_id', None )
             if ( transcript_id and transcript_id != feature_transcript_id ) or \
                ( gene_id and gene_id != feature_gene_id ):
+                part_of = False
+                
+            # If interval is not part of feature, clean up and break.
+            if not part_of:
+                # Adjust raw size because current line is not part of feature.
+                raw_size -= len( self.current_line )
                 break
     
             # Interval associated with feature.
@@ -242,6 +251,7 @@ class GFFReaderWrapper( NiceReaderWrapper ):
    
         # Last interval read is the seed for the next interval.
         self.seed_interval = interval
+        self.seed_interval_line_len = len( self.current_line )
         
         # Return feature.
         feature = GFFFeature( self, self.chrom_col, self.feature_col, self.start_col, \
@@ -255,7 +265,6 @@ class GFFReaderWrapper( NiceReaderWrapper ):
 
         return feature
         
-
 def convert_bed_coords_to_gff( interval ):
     """
     Converts an interval object's coordinates from BED format to GFF format. 
