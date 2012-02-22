f13eb27286b0cddb6b94f334331f30ee05e2d9f5
Jeremy Goecks <jeremy.goecks@emory.edu>
2012-02-22 13:57:17 -0500

    Enable interval files to be converted to interval_index and summary_tree datatypes.

--- lib/galaxy/datatypes/converters/interval_to_interval_index_converter.py
+++ lib/galaxy/datatypes/converters/interval_to_interval_index_converter.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python
 
 """
-Convert from interval file to interval index file. Default input file format is BED (0-based, half-open intervals).
+Convert from interval file to interval index file.
 
-usage: %prog in_file out_file
-    -G, --gff: input is GFF format, meaning start and end coordinates are 1-based, closed interval
+usage: %prog <options> in_file out_file
+    -c, --chr-col: chromosome column, default=1
+    -s, --start-col: start column, default=2
+    -e, --end-col: end column, default=3
 """
 
 from __future__ import division
 
-import sys, fileinput
+import sys, fileinput, optparse
 from galaxy import eggs
 import pkg_resources; pkg_resources.require( "bx-python" )
 from galaxy.visualization.tracks.summary import *
-from bx.cookbook import doc_optparse
 from galaxy.datatypes.util.gff_util import convert_gff_coords_to_bed
 from bx.interval_index_file import Indexes
 
 def main():
     
     # Read options, args.
-    options, args = doc_optparse.parse( __doc__ )
-    try:
-        gff_format = bool( options.gff )
-        input_fname, out_fname = args
-    except:
-        doc_optparse.exception()
+    parser = optparse.OptionParser()
+    parser.add_option( '-c', '--chr-col', type='int', dest='chrom_col', default=1 )
+    parser.add_option( '-s', '--start-col', type='int', dest='start_col', default=2 )
+    parser.add_option( '-e', '--end-col', type='int', dest='end_col', default=3 )
+    (options, args) = parser.parse_args()
+    input_fname, output_fname = args
+    
+    # Make column indices 0-based.
+    options.chrom_col -= 1
+    options.start_col -= 1
+    options.end_col -= 1
     
     # Do conversion.
-    # TODO: take column numbers from command line.
-    if gff_format:
-        chr_col, start_col, end_col = ( 0, 3, 4 )
-    else:
-        chr_col, start_col, end_col = ( 0, 1, 2 )
     index = Indexes()
     offset = 0
     for line in open(input_fname, "r"):
@@ -40,15 +41,13 @@ def main():
         if not feature or feature[0].startswith("track") or feature[0].startswith("#"):
             offset += len(line)
             continue
-        chrom = feature[ chr_col ]
-        chrom_start = int( feature[ start_col ] )
-        chrom_end = int( feature[ end_col ] )
-        if gff_format:
-            chrom_start, chrom_end = convert_gff_coords_to_bed( [chrom_start, chrom_end ] )
+        chrom = feature[ options.chrom_col ]
+        chrom_start = int( feature[ options.start_col ] )
+        chrom_end = int( feature[ options.end_col ] )
         index.add( chrom, chrom_start, chrom_end, offset )
         offset += len(line)
             
-    index.write( open(out_fname, "w") )
+    index.write( open(output_fname, "w") )
 
 if __name__ == "__main__": 
     main()
--- lib/galaxy/datatypes/converters/interval_to_summary_tree_converter.py
+++ lib/galaxy/datatypes/converters/interval_to_summary_tree_converter.py
@@ -3,36 +3,46 @@
 """
 Convert from interval file to summary tree file. Default input file format is BED (0-based, half-open intervals).
 
-usage: %prog in_file out_file
+usage: %prog <options> in_file out_file
+    -c, --chr-col: chromosome column, default=1
+    -s, --start-col: start column, default=2
+    -e, --end-col: end column, default=3
+    -t, --strand-col: strand column, default=6
     -G, --gff: input is GFF format, meaning start and end coordinates are 1-based, closed interval
 """
 from __future__ import division
 
-import sys, fileinput
+import sys, fileinput, optparse
 from galaxy import eggs
 import pkg_resources; pkg_resources.require( "bx-python" )
 from galaxy.visualization.tracks.summary import *
 from bx.intervals.io import *
-from bx.cookbook import doc_optparse
 from galaxy.datatypes.util.gff_util import *
 
 def main():
     # Read options, args.
-    options, args = doc_optparse.parse( __doc__ )
-    try:
-        gff_format = bool( options.gff )
-        input_fname, out_fname = args
-    except:
-        doc_optparse.exception()
+    parser = optparse.OptionParser()
+    parser.add_option( '-c', '--chr-col', type='int', dest='chrom_col', default=1 )
+    parser.add_option( '-s', '--start-col', type='int', dest='start_col', default=2 )
+    parser.add_option( '-e', '--end-col', type='int', dest='end_col', default=3 )
+    parser.add_option( '-t', '--strand-col', type='int', dest='strand_col', default=6 )
+    parser.add_option( '-G', '--gff', dest="gff_format", action="store_true" )
+    (options, args) = parser.parse_args()
+    input_fname, output_fname = args
+    
+    # Convert column indices to 0-based.
+    options.chrom_col -= 1
+    options.start_col -= 1
+    options.end_col -= 1
+    options.strand_col -= 1
         
     # Do conversion.
-    # TODO: take column numbers from command line.
-    if gff_format:
+    if options.gff_format:
         reader_wrapper_class = GFFReaderWrapper
         chr_col, start_col, end_col, strand_col = ( 0, 3, 4, 6 )
     else:
         reader_wrapper_class = NiceReaderWrapper
-        chr_col, start_col, end_col, strand_col = ( 0, 1, 2, 5 )
+        chr_col, start_col, end_col, strand_col = ( options.chrom_col, options.start_col, options.end_col, options.strand_col )
     reader_wrapper = reader_wrapper_class( fileinput.FileInput( input_fname ),
                                             chrom_col=chr_col,
                                             start_col=start_col,
@@ -47,7 +57,7 @@ def main():
                 convert_gff_coords_to_bed( feature )
             st.insert_range( feature.chrom, long( feature.start ), long( feature.end ) )
     
-    st.write(out_fname)
+    st.write( output_fname )
 
 if __name__ == "__main__": 
     main()
