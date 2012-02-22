f926c93b5be5dea592bf0255b4e82fcb42ebd797
Jeremy Goecks <jeremy.goecks@emory.edu>
2012-02-22 15:23:44 -0500

    Enable interval files to be converted to tabix.

--- lib/galaxy/datatypes/converters/bgzip.py
+++ lib/galaxy/datatypes/converters/bgzip.py
@@ -8,19 +8,32 @@ usage: %prog in_file out_file
 
 from galaxy import eggs
 import pkg_resources; pkg_resources.require( "pysam" )
-import ctabix, subprocess, tempfile, sys
+import ctabix, subprocess, tempfile, sys, optparse
 
 def main():
-    filetype, input_fname, out_fname = sys.argv[1:]
+    # Read options, args.
+    parser = optparse.OptionParser()
+    parser.add_option( '-c', '--chr-col', type='int', dest='chrom_col' )
+    parser.add_option( '-s', '--start-col', type='int', dest='start_col' )
+    parser.add_option( '-e', '--end-col', type='int', dest='end_col' )
+    parser.add_option( '-P', '--preset', dest='preset' )
+    (options, args) = parser.parse_args()
+    input_fname, output_fname = args
     
     tmpfile = tempfile.NamedTemporaryFile()
     sort_params = None
     
-    if filetype == "bed":
+    if options.chrom_col and options.start_col and options.end_col:
+        sort_params = ["sort", 
+                        "-k%(i)s,%(i)s" % { 'i': options.chrom_col }, 
+                        "-k%(i)i,%(i)sn" % { 'i': options.start_col },
+                        "-k%(i)i,%(i)sn" % { 'i': options.end_col }
+                      ]
+    elif options.preset == "bed":
         sort_params = ["sort", "-k1,1", "-k2,2n", "-k3,3n"]
-    elif filetype == "vcf":
+    elif options.preset == "vcf":
         sort_params = ["sort", "-k1,1", "-k2,2n"]
-    elif filetype == "gff":
+    elif options.preset == "gff":
         sort_params = ["sort", "-s", "-k1,1", "-k4,4n"] # stable sort on start column
     # Skip any lines starting with "#" and "track"
     grepped = subprocess.Popen(["grep", "-e", "^\"#\"", "-e", "^track", "-v", input_fname], stderr=subprocess.PIPE, stdout=subprocess.PIPE )
@@ -28,7 +41,7 @@ def main():
     grepped.stdout.close()
     output, err = after_sort.communicate()
     
-    ctabix.tabix_compress(tmpfile.name, out_fname, force=True)
+    ctabix.tabix_compress(tmpfile.name, output_fname, force=True)
     
 if __name__ == "__main__": 
     main()
--- lib/galaxy/datatypes/converters/interval_to_tabix_converter.py
+++ lib/galaxy/datatypes/converters/interval_to_tabix_converter.py
@@ -9,13 +9,28 @@ usage: %prog in_file out_file
 
 from galaxy import eggs
 import pkg_resources; pkg_resources.require( "pysam" )
-import ctabix, subprocess, tempfile, sys, os
+import ctabix, subprocess, tempfile, sys, os, optparse
 
 def main():
     # Read options, args.
-    filetype, input_fname, index_fname, out_fname = sys.argv[1:]
+    parser = optparse.OptionParser()
+    parser.add_option( '-c', '--chr-col', type='int', dest='chrom_col' )
+    parser.add_option( '-s', '--start-col', type='int', dest='start_col' )
+    parser.add_option( '-e', '--end-col', type='int', dest='end_col' )
+    parser.add_option( '-P', '--preset', dest='preset' )
+    (options, args) = parser.parse_args()
+    input_fname, index_fname, out_fname = args
     
-    ctabix.tabix_index(filename=index_fname, preset=filetype, keep_original=True, already_compressed=True, index_filename=out_fname)
+    # Create index.
+    if options.preset:
+        # Preset type.
+        ctabix.tabix_index(filename=index_fname, preset=options.preset, keep_original=True, 
+                           already_compressed=True, index_filename=out_fname)
+    else:
+        # For interval files.
+        ctabix.tabix_index(filename=index_fname, seq_col=options.chrom_col, 
+                           start_col=options.start_col, end_col=options.end_col, 
+                           keep_original=True, already_compressed=True, index_filename=out_fname)
     if os.path.getsize(index_fname) == 0:
         sys.stderr.write("The converted tabix index file is empty, meaning the input data is invalid.")
     
