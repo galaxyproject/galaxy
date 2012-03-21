d6302da05ccf252a3a63b1fda05c9e15f0b02a8e
Greg Von Kuster <greg@bx.psu.edu>
2012-03-21 16:46:39 -0400

    Eliminte the emboss_5 tools.

--- tools/emboss_5/emboss_cpgplot_wrapper.pl
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/perl -w
-use strict;
-use File::Copy;
-
-my $cmd_string = join (" ",@ARGV);
-my $results = `$cmd_string`;
-my @files = split("\n",$results);
-my $fileNameOut = $ARGV[14];
-move($fileNameOut.".1.png",$fileNameOut);
