0a0a5a4b7489be85aad9887df0073f5f3da776b5
Nate Coraor <nate@bx.psu.edu>
2012-04-16 16:26:10 -0400

    Allow for starting/stopping multiple servers directly in run.sh.

--- run.sh
+++ run.sh
@@ -52,4 +52,17 @@ if [ $FETCH_EGGS -eq 1 ]; then
     fi
 fi
 
-python ./scripts/paster.py serve universe_wsgi.ini $@
+if [ -n "$GALAXY_RUN_ALL" ]; then
+    servers=`sed -n 's/^\[server:\(.*\)\]/\1/  p' universe_wsgi.ini | xargs echo`
+    daemon=`echo "$@" | grep -q daemon`
+    if [ $? -ne 0 ]; then
+        echo 'ERROR: $GALAXY_RUN_ALL cannot be used without the `--daemon` or `--stop-daemon` arguments to run.sh'
+        exit 1
+    fi
+    for server in $servers; do
+        echo "Handling $server with log file $server.log..."
+        python ./scripts/paster.py serve universe_wsgi.ini --server-name=$server --pid-file=$server.pid --log-file=$server.log $@
+    done
+else
+    python ./scripts/paster.py serve universe_wsgi.ini $@
+fi
