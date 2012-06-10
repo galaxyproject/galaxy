339303b7cd69a2bac4a9557a83792af54004f28f
John Chilton <jmchilton@gmail.com>
2012-06-09 23:00:16 -0500

    Rework dynamic job runner config so that instead using a rules.py file for storing rules, they should be placed in lib/galaxy/jobs/rules/. The rules submodules are "searched" in lexiographically allowing for hierarchical configuration overrides (e.g. naming schemes like: 000_galaxy_rules.py, 100_site_rules.py, 200_instance_rules.py)

--- lib/galaxy/jobs/mapper.py
+++ lib/galaxy/jobs/mapper.py
@@ -1,13 +1,50 @@
-import inspect, sys
+import logging
+import inspect
+import os 
+
+log = logging.getLogger( __name__ )
 
 import galaxy.jobs.rules
 
 DYNAMIC_RUNNER_PREFIX = "dynamic:///"
 
 class JobRunnerMapper( object ):
-    
+    """
+    This class is responsible to managing the mapping of jobs
+    (in the form of job_wrappers) to job runner strings.
+    """
+
     def __init__( self, job_wrapper ):
         self.job_wrapper = job_wrapper
+        self.rule_modules = self.__get_rule_modules( )
+
+    def __get_rule_modules( self ):
+        unsorted_module_names = self.__get_rule_module_names( )
+        ## Load modules in reverse order to allow hierarchical overrides
+        ## i.e. 000_galaxy_rules.py, 100_site_rules.py, 200_instance_rules.py
+        module_names = sorted( unsorted_module_names, reverse=True )
+        modules = []
+        for rule_module_name in module_names:
+            try:
+                module = __import__( rule_module_name )
+                for comp in rule_module_name.split( "." )[1:]:
+                    module = getattr( module, comp )
+                modules.append( module )
+            except BaseException, exception:
+                exception_str = str( exception )
+                message = "%s rule module could not be loaded: %s" % ( rule_module_name, exception_str )
+                log.debug( message )
+                continue
+        return modules
+
+    def __get_rule_module_names( self ):
+        rules_dir = galaxy.jobs.rules.__path__[0]
+        names = []
+        for fname in os.listdir( rules_dir ):
+            if not( fname.startswith( "_" ) ) and fname.endswith( ".py" ):
+                rule_module_name = "galaxy.jobs.rules.%s" % fname[:-len(".py")]
+                names.append( rule_module_name )
+        return names
     
     def __invoke_expand_function( self, expand_function ):
         function_arg_names = inspect.getargspec( expand_function ).args
@@ -15,7 +52,7 @@ class JobRunnerMapper( object ):
         possible_args = { "job_id" : self.job_wrapper.job_id, 
                           "tool" : self.job_wrapper.tool,
                           "tool_id" : self.job_wrapper.tool.id,
-                          "job_wrapper" : self.job_wrapper, 
+                          "job_wrapper" : self.job_wrapper,
                           "app" : self.job_wrapper.app }
         
         actual_args = {}
@@ -25,7 +62,7 @@ class JobRunnerMapper( object ):
             if possible_arg_name in function_arg_names:
                 actual_args[ possible_arg_name ] = possible_args[ possible_arg_name ]
 
-        # Don't hit the DB to load the job object is not needed
+        # Don't hit the DB to load the job object if not needed
         if "job" in function_arg_names or "user" in function_arg_names or "user_email" in function_arg_names:
             job = self.job_wrapper.get_job()
             history = job.history
@@ -51,13 +88,21 @@ class JobRunnerMapper( object ):
         return expand_function_name
 
     def __get_expand_function( self, expand_function_name ):
-        rules_module = sys.modules[ "galaxy.jobs.rules" ]
-        if hasattr( rules_module, expand_function_name ):
-            expand_function = getattr( rules_module, expand_function_name )
+        matching_rule_module = self.__last_rule_module_with_function( expand_function_name )
+        if matching_rule_module:
+            expand_function = getattr( matching_rule_module, expand_function_name )
             return expand_function
         else:
             raise Exception( "Dynamic job runner cannot find function to expand job runner type - %s" % expand_function_name )
-        
+
+    def __last_rule_module_with_function( self, function_name ):
+        # self.rule_modules is sorted in reverse order, so find first
+        # wiht function
+        for rule_module in self.rule_modules:
+            if hasattr( rule_module, function_name ):
+                return rule_module
+        return None
+                
     def __expand_dynamic_job_runner( self, options_str ):
         option_parts = options_str.split( '/' )
         expand_type = option_parts[ 0 ]
@@ -77,6 +122,9 @@ class JobRunnerMapper( object ):
         self.cached_job_runner = job_runner
 
     def get_job_runner( self, params ):
+        """
+        Cache the job_runner string to avoid recalculation.
+        """
         if not hasattr( self, 'cached_job_runner' ):
             self.__cache_job_runner( params )
         return self.cached_job_runner
--- lib/galaxy/jobs/rules.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-log = logging.getLogger( __name__ )
-
-# Add functions to dynamically map job descriptions to job runners in
-# this file. These functions can optionally take in any of the
-# following arguments - job_wrapper, app, user_email, job, tool,
-# email, tool_id, and job_id.
-
