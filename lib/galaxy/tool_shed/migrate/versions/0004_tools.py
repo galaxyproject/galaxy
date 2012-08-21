8faf5cc089501b2c179c64dbd8f5659420873970
Dannon Baker <dannon.baker@gmail.com>
2012-08-21 10:57:52 -0400

    Rewrap text in blast tool migration script.

--- lib/galaxy/tool_shed/migrate/versions/0004_tools.py
+++ lib/galaxy/tool_shed/migrate/versions/0004_tools.py
@@ -1,6 +1,8 @@
 """
-The NCBI BLAST+ tools have been eliminated from the distribution.  The tools and datatypes are are now available in repositories named ncbi_blast_plus
-and blast_datatypes, respectively, from the main Galaxy tool shed at http://toolshed.g2.bx.psu.edu will be installed into your local Galaxy instance
+The NCBI BLAST+ tools have been eliminated from the distribution.  The tools and
+datatypes are are now available in repositories named ncbi_blast_plus and
+blast_datatypes, respectively, from the main Galaxy tool shed at
+http://toolshed.g2.bx.psu.edu will be installed into your local Galaxy instance
 at the location discussed above by running the following command.
 """
 
