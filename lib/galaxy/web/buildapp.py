31167665e3b853c4c8bc2a590005969026480f49
James Taylor <james@jamestaylor.org>
2012-09-28 15:15:28 -0400

    Main webapp needs to be called 'galaxy' since that is what is used in templates currently

--- lib/galaxy/web/buildapp.py
+++ lib/galaxy/web/buildapp.py
@@ -1,3 +1,3 @@
 """For backward compatibility only, pulls app_factor from galaxy.webapps.main"""
 
-from galaxy.webapps.main.buildapp import app_factory
+from galaxy.webapps.galaxy.buildapp import app_factory
