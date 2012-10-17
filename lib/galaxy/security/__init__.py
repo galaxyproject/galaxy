cac074b786bc2897e2dd6bb08bae37eb450cafcd
Daniel Blankenberg <dan@bx.psu.edu>
2012-10-17 12:32:35 -0400

    Fix for python2.5.

--- lib/galaxy/security/__init__.py
+++ lib/galaxy/security/__init__.py
@@ -318,7 +318,7 @@ class GalaxyRBACAgent( RBACAgent ):
                                  % ( item.library_dataset_id, len( base_result ), 
                                      len( new_result ) ) )
                 log.debug( "get_actions_for_items: Test end" )
-            except Exception as e:
+            except Exception, e:
                 log.debug( "Exception in test code: %s" % e )
 
         return ret_permissions
