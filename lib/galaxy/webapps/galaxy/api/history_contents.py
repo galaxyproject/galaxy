0adb4444e8b7674b4012dea7635f9f7f22df0f74
Carl Eberhard <carlfeberhard@gmail.com>
2012-10-30 16:55:23 -0400

    api/history_contents: (1) index: if passed ids=<id|csv id list> in query string, provide full data (as history_contents.show) for each id listed (if no 'ids' - use previous summary behaviour), return error string; (2) show: return 'accessible', 'api_type' (consistent with summary style in index), 'display_types', 'display_apps', 'file_ext', 'hid', 'history_id', 'meta_files' (file types of associated files for generating download urls, eg. myBam.meta_files: [{ 'file_type' : 'bam_index' }]), 'visualizations' (list of visualization names appr. for this hda), 'peek' (data.peek as used in the history panel), return error string;

--- lib/galaxy/webapps/galaxy/api/history_contents.py
+++ lib/galaxy/webapps/galaxy/api/history_contents.py
@@ -2,8 +2,17 @@
 API operations on the contents of a history.
 """
 import logging
+import urllib
+from gettext import gettext
+
 from galaxy import web
-from galaxy.web.base.controller import BaseAPIController, url_for, UsesHistoryDatasetAssociationMixin, UsesHistoryMixin, UsesLibraryMixin, UsesLibraryMixinItems
+from galaxy.web.base.controller import BaseAPIController, url_for
+from galaxy.web.base.controller import UsesHistoryDatasetAssociationMixin, UsesHistoryMixin
+from galaxy.web.base.controller import UsesLibraryMixin, UsesLibraryMixinItems
+
+from galaxy.web.framework.helpers import to_unicode
+from galaxy.datatypes.display_applications import util
+from galaxy.datatypes.metadata import FileParameter
 
 import pkg_resources
 pkg_resources.require( "Routes" )
@@ -11,27 +20,39 @@ import routes
 
 log = logging.getLogger( __name__ )
 
-class HistoryContentsController( BaseAPIController, UsesHistoryDatasetAssociationMixin, UsesHistoryMixin, UsesLibraryMixin, UsesLibraryMixinItems ):
-
+class HistoryContentsController( BaseAPIController, UsesHistoryDatasetAssociationMixin, UsesHistoryMixin,
+                                 UsesLibraryMixin, UsesLibraryMixinItems ):
     @web.expose_api
-    def index( self, trans, history_id, **kwd ):
+    def index( self, trans, history_id, ids=None, **kwd ):
         """
         GET /api/histories/{encoded_history_id}/contents
         Displays a collection (list) of history contents
         """
-        try:
-            history = self.get_history( trans, history_id, check_ownership=True, check_accessible=True )
-        except Exception, e:
-            return str( e )
         rval = []
         try:
-            for dataset in history.datasets:
-                api_type = "file"
-                encoded_id = trans.security.encode_id( dataset.id )
-                rval.append( dict( id = encoded_id,
-                                   type = api_type,
-                                   name = dataset.name,
-                                   url = url_for( 'history_content', history_id=history_id, id=encoded_id, ) ) )
+            history = self.get_history( trans, history_id, check_ownership=True, check_accessible=True )
+
+            # if no ids passed, return a _SUMMARY_ of _all_ datasets in the history
+            if not ids:
+                for dataset in history.datasets:
+                    api_type = "file"
+                    encoded_id = trans.security.encode_id( dataset.id )
+                    # build the summary
+                    rval.append( dict( id = encoded_id,
+                                       type = api_type,
+                                       name = dataset.name,
+                                       url = url_for( 'history_content', history_id=history_id, id=encoded_id, ) ) )
+
+            # if ids, return _FULL_ data (as show) for each id passed
+            #NOTE: this might not be the best form (passing all info),
+            #   but we(I?) need an hda collection with full data somewhere
+            else:
+                ids = ids.split( ',' )
+                for id in ids:
+                    hda = self.get_history_dataset_association( trans, history, id,
+                        check_ownership=True, check_accessible=True, check_state=False )
+                    rval.append( get_hda_dict( trans, history, hda, for_editing=True ) )
+
         except Exception, e:
             rval = "Error in history API at listing contents"
             log.error( rval + ": %s" % str(e) )
@@ -44,30 +65,21 @@ class HistoryContentsController( BaseAPIController, UsesHistoryDatasetAssociatio
         GET /api/histories/{encoded_history_id}/contents/{encoded_content_id}
         Displays information about a history content (dataset).
         """
-        content_id = id
+        hda_dict = {}
         try:
-            # get the history just for the access checks
-            history = self.get_history( trans, history_id, check_ownership=True, check_accessible=True, deleted=False )
-            content = self.get_history_dataset_association( trans, history, content_id, check_ownership=True, check_accessible=True )
+            history = self.get_history( trans, history_id,
+                check_ownership=True, check_accessible=True, deleted=False )
+            hda = self.get_history_dataset_association( trans, history, id,
+                check_ownership=True, check_accessible=True )
+            hda_dict = get_hda_dict( trans, history, hda, for_editing=True )
+
         except Exception, e:
-            return str( e )
-        try:
-            item = content.get_api_value( view='element' )
-            if trans.user_is_admin() or trans.app.config.expose_dataset_path:
-                 item['file_name'] = content.file_name
-            if not item['deleted']:
-                # Problem: Method url_for cannot use the dataset controller
-                # Get the environment from DefaultWebTransaction and use default webapp mapper instead of webapp API mapper
-                url = routes.URLGenerator(trans.webapp.mapper, trans.environ)
-                # http://routes.groovie.org/generating.html
-                # url_for is being phased out, so new applications should use url
-                item['download_url'] = url(controller='dataset', action='display', dataset_id=trans.security.encode_id(content.id), to_ext=content.ext)
-                item = self.encode_all_ids( trans, item )
-        except Exception, e:
-            item = "Error in history API at listing dataset"
-            log.error( item + ": %s" % str(e) )
+            msg = "Error in history API at listing dataset: %s" % ( str(e) )
+            log.error( msg, exc_info=True )
             trans.response.status = 500
-        return item
+            return msg
+
+        return hda_dict
 
     @web.expose_api
     def create( self, trans, history_id, payload, **kwd ):
@@ -99,3 +111,69 @@ class HistoryContentsController( BaseAPIController, UsesHistoryDatasetAssociatio
             trans.response.status = 403
             return "Not implemented."
 
+
+# move these into model?? hell if I know...doesn't seem like the urls should go here
+def get_hda_dict( trans, history, hda, for_editing ):
+    hda_dict = hda.get_api_value( view='element' )
+
+    hda_dict[ 'id' ] = trans.security.encode_id( hda.id )
+    hda_dict[ 'history_id' ] = trans.security.encode_id( history.id )
+    hda_dict[ 'hid' ] = hda.hid
+
+    hda_dict[ 'file_ext' ] = hda.ext
+    if trans.user_is_admin() or trans.app.config.expose_dataset_path:
+        hda_dict[ 'file_name' ] = hda.file_name
+
+    if not hda_dict[ 'deleted' ]:
+        # Problem: Method url_for cannot use the dataset controller
+        # Get the environment from DefaultWebTransaction
+        #   and use default webapp mapper instead of webapp API mapper
+        web_url_for = routes.URLGenerator( trans.webapp.mapper, trans.environ )
+        # http://routes.groovie.org/generating.html
+        # url_for is being phased out, so new applications should use url
+        hda_dict[ 'download_url' ] = web_url_for( controller='dataset', action='display',
+            dataset_id=trans.security.encode_id( hda.id ), to_ext=hda.ext )
+
+    can_access_hda = trans.app.security_agent.can_access_dataset( trans.get_current_user_roles(), hda.dataset )
+    hda_dict[ 'accessible' ] = ( trans.user_is_admin() or can_access_hda )
+    hda_dict[ 'api_type' ] = "file"
+
+    if not( hda.purged or hda.deleted or hda.dataset.purged ):
+        meta_files = []
+        for meta_type in hda.metadata.spec.keys():
+            if isinstance( hda.metadata.spec[ meta_type ].param, FileParameter ):
+                meta_files.append( dict( file_type=meta_type ) )
+        if meta_files:
+            hda_dict[ 'meta_files' ] = meta_files
+
+    #hda_dict[ 'display_apps' ] = get_display_apps( trans, hda )
+    hda_dict[ 'visualizations' ] = hda.get_visualizations()
+    hda_dict[ 'peek' ] = to_unicode( hda.display_peek() )
+
+    return hda_dict
+
+def get_display_apps( trans, hda ):
+    display_apps = []
+
+    def get_display_app_url( display_app_link, hda, trans ):
+        web_url_for = routes.URLGenerator( trans.webapp.mapper, trans.environ )
+        dataset_hash, user_hash = util.encode_dataset_user( trans, hda, None )
+        return web_url_for( controller='/dataset',
+                        action="display_application",
+                        dataset_id=dataset_hash,
+                        user_id=user_hash,
+                        app_name=urllib.quote_plus( display_app_link.display_application.id ),
+                        link_name=urllib.quote_plus( display_app_link.id ) )
+
+
+    for display_app in hda.get_display_applications( trans ).itervalues():
+        app_links = []
+        for display_app_link in display_app.links.itervalues():
+            app_links.append({
+                'target' : display_app_link.url.get( 'target_frame', '_blank' ),
+                'href' : get_display_app_url( display_app_link, hda, trans ),
+                'text' : gettext( display_app_link.name )
+            })
+        display_apps.append( dict( label=display_app.name, links=app_links ) )
+
+    return display_apps
