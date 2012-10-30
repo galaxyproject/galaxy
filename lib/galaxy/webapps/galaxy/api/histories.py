2eb951587dab4345ea3dc9970f5ae308864ff3f6
Carl Eberhard <carlfeberhard@gmail.com>
2012-10-30 16:53:10 -0400

    api/histories.show: add 'nice_size', 'annotation', 'state_ids' (map of possible hda states : lists of hda ids in those states ), reorder history state from child hda states (running/queued higher priority than error), return error string;

--- lib/galaxy/webapps/galaxy/api/histories.py
+++ lib/galaxy/webapps/galaxy/api/histories.py
@@ -10,6 +10,10 @@ from galaxy.model.orm import *
 import galaxy.datatypes
 from galaxy.util.bunch import Bunch
 
+import pkg_resources
+pkg_resources.require( "Routes" )
+import routes
+
 log = logging.getLogger( __name__ )
 
 class HistoriesController( BaseAPIController, UsesHistoryMixin ):
@@ -31,12 +35,14 @@ class HistoriesController( BaseAPIController, UsesHistoryMixin ):
                     item = history.get_api_value(value_mapper={'id':trans.security.encode_id})
                     item['url'] = url_for( 'history', id=trans.security.encode_id( history.id ) )
                     rval.append( item )
+
             elif trans.galaxy_session.current_history:
                 #No user, this must be session authentication with an anonymous user.
                 history = trans.galaxy_session.current_history
                 item = history.get_api_value(value_mapper={'id':trans.security.encode_id})
                 item['url'] = url_for( 'history', id=trans.security.encode_id( history.id ) )
                 rval.append(item)
+
         except Exception, e:
             rval = "Error in history API"
             log.error( rval + ": %s" % str(e) )
@@ -55,52 +61,90 @@ class HistoriesController( BaseAPIController, UsesHistoryMixin ):
         params = util.Params( kwd )
         deleted = util.string_as_bool( deleted )
 
-        def traverse( datasets ):
-            rval = {}
-            states = trans.app.model.Dataset.states
+        states = trans.app.model.Dataset.states
+
+        def get_dataset_state_summaries( datasets ):
+            # cycles through the history's datasets, building counts and id lists for each possible ds state
+            state_counts = {}
+            state_ids = {}
+
+            # init counts, ids for each state
             for key, state in states.items():
-                rval[state] = 0
+                state_counts[state] = 0
+                state_ids[state] = []
+
+            # cycle through datasets saving each ds' state
             for dataset in datasets:
-                item = dataset.get_api_value( view='element' )
-                if not item['deleted']:
-                    rval[item['state']] = rval[item['state']] + 1
-            return rval
+                dataset_dict = dataset.get_api_value( view='element' )
+                item_state = dataset_dict[ 'state' ]
+
+                if not dataset_dict['deleted']:
+                    state_counts[ item_state ] = state_counts[ item_state ] + 1
+                    state_ids[ item_state ].append( trans.security.encode_id( dataset_dict[ 'id' ] ) )
+
+            return ( state_counts, state_ids )
+
+        # try to load the history, by most_recently_used or the given id
         try:
             if history_id == "most_recently_used":
-                if trans.user and len(trans.user.galaxy_sessions) > 0:
+                if trans.user and len( trans.user.galaxy_sessions ) > 0:
                     # Most recent active history for user sessions, not deleted
                     history = trans.user.galaxy_sessions[0].histories[-1].history
                 else:
-                    history = None
+                    return None
             else:
-                history = self.get_history( trans, history_id, check_ownership=True, check_accessible=True, deleted=deleted )
-        except Exception, e:
-            return str( e )
-        try:
-            item = history.get_api_value(view='element', value_mapper={'id':trans.security.encode_id})
-            num_sets = len( [hda.id for hda in history.datasets if not hda.deleted] )
-            states = trans.app.model.Dataset.states
+                history = self.get_history( trans, history_id, check_ownership=False,
+                                            check_accessible=True, deleted=deleted )
+
+            history_data = history.get_api_value( view='element', value_mapper={'id':trans.security.encode_id} )
+            history_data[ 'nice_size' ] = history.get_disk_size( nice_size=True )
+
+            #TODO: separate, move to annotation api, fill on the client
+            history_data[ 'annotation' ] = history.get_item_annotation_str( trans.sa_session, trans.user, history )
+            if not history_data[ 'annotation' ]:
+                history_data[ 'annotation' ] = ''
+
+            # get the history state using the state summaries of it's datasets (default to ERROR)
+            num_sets = len([ hda.id for hda in history.datasets if not hda.deleted ])
             state = states.ERROR
+
+            ( state_counts, state_ids ) = get_dataset_state_summaries( history.datasets )
+
             if num_sets == 0:
                 state = states.NEW
+
             else:
-                summary = traverse(history.datasets)
-                if summary[states.ERROR] > 0 or summary[states.FAILED_METADATA] > 0:
-                    state = states.ERROR
-                elif summary[states.RUNNING] > 0 or summary[states.SETTING_METADATA] > 0:
+
+                if( ( state_counts[ states.RUNNING ] > 0 )
+                or    ( state_counts[ states.SETTING_METADATA ] > 0 )
+                or    ( state_counts[ states.UPLOAD ] > 0 ) ):
                     state = states.RUNNING
-                elif summary[states.QUEUED] > 0:
+
+                elif state_counts[ states.QUEUED ] > 0:
                     state = states.QUEUED
-                elif summary[states.OK] == num_sets:
+
+                elif( ( state_counts[ states.ERROR ] > 0 )
+                or  ( state_counts[ states.FAILED_METADATA ] > 0 ) ):
+                    state = states.ERROR
+
+                elif state_counts[ states.OK ] == num_sets:
                     state = states.OK
-                item['contents_url'] = url_for( 'history_contents', history_id=history_id )
-                item['state_details'] = summary
-            item['state'] = state
+
+            history_data[ 'state' ] = state
+
+            history_data[ 'state_details' ] = state_counts
+            history_data[ 'state_ids' ] = state_ids
+
+            history_data[ 'contents_url' ] = url_for( 'history_contents', history_id=history_id )
+
+
         except Exception, e:
-            item = "Error in history API at showing history detail"
-            log.error(item + ": %s" % str(e))
+            msg = "Error in history API at showing history detail: %s" % ( str( e ) )
+            log.error( msg, exc_info=True )
             trans.response.status = 500
-        return item
+            return msg
+
+        return history_data
 
     @web.expose_api
     def create( self, trans, payload, **kwd ):
