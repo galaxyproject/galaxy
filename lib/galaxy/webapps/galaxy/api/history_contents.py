fffde1f49e6e6ed3cdabfb0a328e5e5917c110db
Carl Eberhard <carlfeberhard@gmail.com>
2012-11-05 16:49:58 -0500

    api/history_contents: allow anon-user to query own, current history; improve doc string

--- lib/galaxy/webapps/galaxy/api/history_contents.py
+++ lib/galaxy/webapps/galaxy/api/history_contents.py
@@ -26,51 +26,100 @@ class HistoryContentsController( BaseAPIController, UsesHistoryDatasetAssociatio
     def index( self, trans, history_id, ids=None, **kwd ):
         """
         GET /api/histories/{encoded_history_id}/contents
-        Displays a collection (list) of history contents
+        Displays a collection (list) of history contents (HDAs)
+
+        :param history_id: an encoded id string of the `History` to search
+        :param ids: (optional) a comma separated list of encoded `HDA` ids
+
+        If Ids is not given, index returns a list of *summary* json objects for
+        every `HDA` associated with the given `history_id`.
+        See _summary_hda_dict.
+
+        If ids is given, index returns a *more complete* json object for each
+        HDA in the ids list.
+
+        Note: Anonymous users are allowed to get their current history contents
+        (generally useful for browser UI access of the api)
         """
         rval = []
         try:
-            history = self.get_history( trans, history_id, check_ownership=True, check_accessible=True )
+            # get the history, if anon user and requesting current history - allow it
+            if( ( trans.user == None )
+            and ( history_id == trans.security.encode_id( trans.history.id ) ) ):
+                #TODO:?? is secure?
+                history = trans.history
 
-            # if no ids passed, return a _SUMMARY_ of _all_ datasets in the history
-            if not ids:
-                for dataset in history.datasets:
-                    api_type = "file"
-                    encoded_id = trans.security.encode_id( dataset.id )
-                    # build the summary
-                    rval.append( dict( id = encoded_id,
-                                       type = api_type,
-                                       name = dataset.name,
-                                       url = url_for( 'history_content', history_id=history_id, id=encoded_id, ) ) )
-
-            # if ids, return _FULL_ data (as show) for each id passed
-            #NOTE: this might not be the best form (passing all info),
-            #   but we(I?) need an hda collection with full data somewhere
+            # otherwise, check permissions for the history first
             else:
+                history = self.get_history( trans, history_id, check_ownership=True, check_accessible=True )
+
+            # build the return hda data list
+            if ids:
+                # if ids, return _FULL_ data (as show) for each id passed
+                #NOTE: this might not be the best form (passing all info),
+                #   but we(I?) need an hda collection with full data somewhere
                 ids = ids.split( ',' )
-                for id in ids:
-                    hda = self.get_history_dataset_association( trans, history, id,
-                        check_ownership=True, check_accessible=True, check_state=False )
-                    rval.append( get_hda_dict( trans, history, hda, for_editing=True ) )
+                for hda in history.datasets:
+                    if trans.security.encode_id( hda.id ) in ids:
+                        rval.append( get_hda_dict( trans, history, hda, for_editing=True ) )
+
+            else:
+                # if no ids passed, return a _SUMMARY_ of _all_ datasets in the history
+                for hda in history.datasets:
+                    rval.append( self._summary_hda_dict( trans, history_id, hda ) )
 
         except Exception, e:
             rval = "Error in history API at listing contents"
-            log.error( rval + ": %s" % str(e) )
+            log.error( rval + ": %s, %s" % ( type( e ), str( e ) ) )
             trans.response.status = 500
+
         return rval
+
+    def _summary_hda_dict( self, trans, history_id, hda ):
+        """
+        Returns a dictionary based on the HDA in .. _summary form::
+        {
+            'id'    : < the encoded dataset id >,
+            'name'  : < currently only returns 'file' >,
+            'type'  : < name of the dataset >,
+            'url'   : < api url to retrieve this datasets full data >,
+        }
+        """
+        api_type = "file"
+        encoded_id = trans.security.encode_id( hda.id )
+        return {
+            'id'    : encoded_id,
+            'name'  : hda.name,
+            'type'  : api_type,
+            'url'   : url_for( 'history_content', history_id=history_id, id=encoded_id, ),
+        }
 
     @web.expose_api
     def show( self, trans, id, history_id, **kwd ):
         """
         GET /api/histories/{encoded_history_id}/contents/{encoded_content_id}
         Displays information about a history content (dataset).
+
+        
         """
         hda_dict = {}
         try:
-            history = self.get_history( trans, history_id,
-                check_ownership=True, check_accessible=True, deleted=False )
-            hda = self.get_history_dataset_association( trans, history, id,
-                check_ownership=True, check_accessible=True )
+            # for anon users:
+            #TODO: check login_required?
+            #TODO: this isn't actually most_recently_used (as defined in histories)
+            if( ( trans.user == None )
+            and ( history_id == trans.security.encode_id( trans.history.id ) ) ):
+                history = trans.history
+                #TODO: dataset/hda by id (from history) OR check_ownership for anon user
+                hda = self.get_history_dataset_association( trans, history, id,
+                    check_ownership=False, check_accessible=True )
+
+            else:
+                history = self.get_history( trans, history_id,
+                    check_ownership=True, check_accessible=True, deleted=False )
+                hda = self.get_history_dataset_association( trans, history, id,
+                    check_ownership=True, check_accessible=True )
+
             hda_dict = get_hda_dict( trans, history, hda, for_editing=True )
 
         except Exception, e:
@@ -112,7 +161,7 @@ class HistoryContentsController( BaseAPIController, UsesHistoryDatasetAssociatio
             return "Not implemented."
 
 
-# move these into model?? hell if I know...doesn't seem like the urls should go here
+#TODO: move these into model
 def get_hda_dict( trans, history, hda, for_editing ):
     hda_dict = hda.get_api_value( view='element' )
 
@@ -146,13 +195,14 @@ def get_hda_dict( trans, history, hda, for_editing ):
         if meta_files:
             hda_dict[ 'meta_files' ] = meta_files
 
-    #hda_dict[ 'display_apps' ] = get_display_apps( trans, hda )
+    hda_dict[ 'display_apps' ] = get_display_apps( trans, hda )
     hda_dict[ 'visualizations' ] = hda.get_visualizations()
     hda_dict[ 'peek' ] = to_unicode( hda.display_peek() )
 
     return hda_dict
 
 def get_display_apps( trans, hda ):
+    #TODO: make more straightforward (somehow)
     display_apps = []
 
     def get_display_app_url( display_app_link, hda, trans ):
@@ -164,7 +214,6 @@ def get_display_apps( trans, hda ):
                         user_id=user_hash,
                         app_name=urllib.quote_plus( display_app_link.display_application.id ),
                         link_name=urllib.quote_plus( display_app_link.id ) )
-
 
     for display_app in hda.get_display_applications( trans ).itervalues():
         app_links = []
