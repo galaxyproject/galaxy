a5215522eb16ac7e11e6bbd490c230969d3d4c2e
John Chilton <jmchilton@gmail.com>
2012-11-14 16:10:31 -0600

    Allow output data attributes `format` and `metadata_source` to work with multiple input data parameters - the first item selected will be used as the source. This decision was discussed with Dannon in IRC and he thought it was an acceptable approach.

--- lib/galaxy/tools/actions/__init__.py
+++ lib/galaxy/tools/actions/__init__.py
@@ -62,7 +62,11 @@ class DefaultToolAction( object ):
                     # If there are multiple inputs with the same name, they
                     # are stored as name1, name2, ...
                     for i, v in enumerate( value ):
-                        input_datasets[ prefix + input.name + str( i + 1 ) ] = process_dataset( v )
+                        processed_dataset = process_dataset( v )
+                        if i == 0:
+                            # Allow copying metadata to output, first item will be source.
+                            input_datasets[ prefix + input.name ] = processed_dataset
+                        input_datasets[ prefix + input.name + str( i + 1 ) ] = processed_dataset
                         conversions = []
                         for conversion_name, conversion_extensions, conversion_datatypes in input.conversions:
                             new_data = process_dataset( input_datasets[ prefix + input.name + str( i + 1 ) ], conversion_datatypes )
