31181c10a4e6a1a9d169450cb25d2077c9404df3
Carl Eberhard <carlfeberhard@gmail.com>
2012-11-15 13:11:21 -0500

    history panel: fix to template name

---
+++ static/scripts/templates/compiled/template-hda-failedMetadata.js
@@ -0,0 +1,33 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['template-hda-failedMetadata'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  helpers = helpers || Handlebars.helpers;
+  var stack1, foundHelper, self=this, functionType="function", blockHelperMissing=helpers.blockHelperMissing, escapeExpression=this.escapeExpression;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1, foundHelper;
+  buffer += "\n";
+  foundHelper = helpers.local;
+  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{},inverse:self.noop,fn:self.program(2, program2, data)}); }
+  else { stack1 = depth0.local; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
+  if (!helpers.local) { stack1 = blockHelperMissing.call(depth0, stack1, {hash:{},inverse:self.noop,fn:self.program(2, program2, data)}); }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\nYou may be able to <a href=\"";
+  stack1 = depth0.urls;
+  stack1 = stack1 == null || stack1 === false ? stack1 : stack1.edit;
+  stack1 = typeof stack1 === functionType ? stack1() : stack1;
+  buffer += escapeExpression(stack1) + "\" target=\"galaxy_main\">set it manually or retry auto-detection</a>.\n";
+  return buffer;}
+function program2(depth0,data) {
+  
+  
+  return "An error occurred setting the metadata for this dataset.";}
+
+  foundHelper = helpers.warningmessagesmall;
+  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{},inverse:self.noop,fn:self.program(1, program1, data)}); }
+  else { stack1 = depth0.warningmessagesmall; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
+  if (!helpers.warningmessagesmall) { stack1 = blockHelperMissing.call(depth0, stack1, {hash:{},inverse:self.noop,fn:self.program(1, program1, data)}); }
+  if(stack1 || stack1 === 0) { return stack1; }
+  else { return ''; }});
+})();
