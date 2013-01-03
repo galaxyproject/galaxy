c6f1df0597ccbcf476c63816c259a49dc175aee8
Jeremy Goecks <jeremy.goecks@emory.edu>
2013-01-03 15:12:51 -0500

    Add missing import to visualization controller.

--- lib/galaxy/webapps/galaxy/controllers/visualization.py
+++ lib/galaxy/webapps/galaxy/controllers/visualization.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from sqlalchemy import desc, or_, and_
 from galaxy import model, web
 from galaxy.model.item_attrs import UsesAnnotations, UsesItemRatings
-from galaxy.web.base.controller import BaseUIController, SharableMixin, UsesVisualizationMixin
+from galaxy.web.base.controller import BaseUIController, SharableMixin, UsesVisualizationMixin, VALID_SLUG_RE
 from galaxy.web.framework.helpers import time_ago, grids, iff
 from galaxy.util.json import from_json_string
 from galaxy.util.sanitize_html import sanitize_html
