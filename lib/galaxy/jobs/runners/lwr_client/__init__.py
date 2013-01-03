45b733a6660054457f9f77039a09f5e9aea7e2c7
Nate Coraor <nate@bx.psu.edu>
2013-01-03 16:30:21 -0500

    backout 6265bf3f27ad

--- lib/galaxy/jobs/runners/lwr_client/__init__.py
+++ lib/galaxy/jobs/runners/lwr_client/__init__.py
@@ -15,99 +15,6 @@ import urllib2
 import simplejson
 
 
-class JobInputs(object):
-    """
-    Abstractions over dynamic inputs created for a given job (namely the command to
-    execute and created configfiles).
-
-    **Parameters**
-
-    command_line : str
-        Local command to execute for this job. (To be rewritten.)
-    config_files : str
-        Config files created for this job. (To be rewritten.)
-
-
-    >>> import tempfile
-    >>> tf = tempfile.NamedTemporaryFile()
-    >>> def setup_inputs(tf):
-    ...     open(tf.name, "w").write("world /path/to/input the rest")
-    ...     inputs = JobInputs("hello /path/to/input", [tf.name])
-    ...     return inputs
-    >>> inputs = setup_inputs(tf)
-    >>> inputs.rewrite_paths("/path/to/input", 'C:\\input')
-    >>> inputs.rewritten_command_line
-    'hello C:\\\\input'
-    >>> inputs.rewritten_config_files[tf.name]
-    'world C:\\\\input the rest'
-    >>> tf.close()
-    >>> tf = tempfile.NamedTemporaryFile()
-    >>> inputs = setup_inputs(tf)
-    >>> inputs.find_referenced_subfiles('/path/to')
-    ['/path/to/input']
-    >>> inputs.path_referenced('/path/to')
-    True
-    >>> inputs.path_referenced('/path/to/input')
-    True
-    >>> inputs.path_referenced('/path/to/notinput')
-    False
-    >>> tf.close()
-    """
-
-    def __init__(self, command_line, config_files):
-        self.rewritten_command_line = command_line
-        self.rewritten_config_files = {}
-        for config_file in config_files or []:
-            config_contents = _read(config_file)
-            self.rewritten_config_files[config_file] = config_contents
-
-    def find_referenced_subfiles(self, directory):
-        """
-        Return list of files below specified `directory` in job inputs. Could
-        use more sophisticated logic (match quotes to handle spaces, handle
-        subdirectories, etc...).
-
-        **Parameters**
-
-        directory : str
-            Full path to directory to search.
-
-        """
-        pattern = r"(%s%s\S+)" % (directory, os.sep)
-        referenced_files = set()
-        for input_contents in self.__items():
-            referenced_files.update(re.findall(pattern, input_contents))
-        return list(referenced_files)
-
-    def path_referenced(self, path):
-        pattern = r"%s" % path
-        found = False
-        for input_contents in self.__items():
-            if re.findall(pattern, input_contents):
-                found = True
-                break
-        return found
-
-    def rewrite_paths(self, local_path, remote_path):
-        """
-        Rewrite references to `local_path` with  `remote_path` in job inputs.
-        """
-        self.__rewrite_command_line(local_path, remote_path)
-        self.__rewrite_config_files(local_path, remote_path)
-
-    def __rewrite_command_line(self, local_path, remote_path):
-        self.rewritten_command_line = self.rewritten_command_line.replace(local_path, remote_path)
-
-    def __rewrite_config_files(self, local_path, remote_path):
-        for config_file, rewritten_contents in self.rewritten_config_files.iteritems():
-            self.rewritten_config_files[config_file] = rewritten_contents.replace(local_path, remote_path)
-
-    def __items(self):
-        items = [self.rewritten_command_line]
-        items.extend(self.rewritten_config_files.values())
-        return items
-
-
 class FileStager(object):
     """
     Objects of the FileStager class interact with an LWR client object to
@@ -143,10 +50,6 @@ class FileStager(object):
         self.tool_dir = os.path.abspath(tool_dir)
         self.working_directory = working_directory
 
-        # Setup job inputs, these will need to be rewritten before
-        # shipping off to remote LWR server.
-        self.job_inputs = JobInputs(self.command_line, self.config_files)
-
         self.file_renames = {}
 
         job_config = client.setup()
@@ -162,11 +65,17 @@ class FileStager(object):
         self.__initialize_output_file_renames()
         self.__initialize_task_output_file_renames()
         self.__initialize_config_file_renames()
-        self.__handle_rewrites()
-        self.__upload_rewritten_config_files()
+        self.__rewrite_and_upload_config_files()
+        self.__rewrite_command_line()
 
     def __initialize_referenced_tool_files(self):
-        self.referenced_tool_files = self.job_inputs.find_referenced_subfiles(self.tool_dir)
+        pattern = r"(%s%s\S+)" % (self.tool_dir, os.sep)
+        referenced_tool_files = []
+        referenced_tool_files += re.findall(pattern, self.command_line)
+        if self.config_files != None:
+            for config_file in self.config_files:
+                referenced_tool_files += re.findall(pattern, self.__read(config_file))
+        self.referenced_tool_files = referenced_tool_files
 
     def __upload_tool_files(self):
         for referenced_tool_file in self.referenced_tool_files:
@@ -175,25 +84,18 @@ class FileStager(object):
 
     def __upload_input_files(self):
         for input_file in self.input_files:
-            self.__upload_input_file(input_file)
-            self.__upload_input_extra_files(input_file)
-
-    def __upload_input_file(self, input_file):
-        if self.job_inputs.path_referenced(input_file):
             input_upload_response = self.client.upload_input(input_file)
             self.file_renames[input_file] = input_upload_response['path']
-
-    def __upload_input_extra_files(self, input_file):
-        # TODO: Determine if this is object store safe and what needs to be
-        # done if it is not.
-        files_path = "%s_files" % input_file[0:-len(".dat")]
-        if os.path.exists(files_path) and self.job_inputs.path_referenced(files_path):
-            for extra_file in os.listdir(files_path):
-                extra_file_path = os.path.join(files_path, extra_file)
-                relative_path = os.path.basename(files_path)
-                extra_file_relative_path = os.path.join(relative_path, extra_file)
-                response = self.client.upload_extra_input(extra_file_path, extra_file_relative_path)
-                self.file_renames[extra_file_path] = response['path']
+            # TODO: Determine if this is object store safe and what needs to be
+            # done if it is not.
+            files_path = "%s_files" % input_file[0:-len(".dat")]
+            if os.path.exists(files_path):
+                for extra_file in os.listdir(files_path):
+                    extra_file_path = os.path.join(files_path, extra_file)
+                    relative_path = os.path.basename(files_path)
+                    extra_file_relative_path = os.path.join(relative_path, extra_file)
+                    response = self.client.upload_extra_input(extra_file_path, extra_file_relative_path)
+                    self.file_renames[extra_file_path] = response['path']
 
     def __upload_working_directory_files(self):
         # Task manager stages files into working directory, these need to be
@@ -228,20 +130,28 @@ class FileStager(object):
             new_contents = new_contents.replace(local_path, remote_path)
         return new_contents
 
-    def __handle_rewrites(self):
-        for local_path, remote_path in self.file_renames.iteritems():
-            self.job_inputs.rewrite_paths(local_path, remote_path)
-
-    def __upload_rewritten_config_files(self):
-        for config_file, new_config_contents in self.job_inputs.rewritten_config_files.iteritems():
+    def __rewrite_and_upload_config_files(self):
+        for config_file in self.config_files:
+            config_contents = self.__read(config_file)
+            new_config_contents = self.__rewrite_paths(config_contents)
             self.client.upload_config_file(config_file, new_config_contents)
+
+    def __rewrite_command_line(self):
+        self.rewritten_command_line = self.__rewrite_paths(self.command_line)
 
     def get_rewritten_command_line(self):
         """
         Returns the rewritten version of the command line to execute suitable
         for remote host.
         """
-        return self.job_inputs.rewritten_command_line
+        return self.rewritten_command_line
+
+    def __read(self, path):
+        input = open(path, "r")
+        try:
+            return input.read()
+        finally:
+            input.close()
 
 
 class Client(object):
@@ -477,15 +387,3 @@ class Client(object):
         Setup remote LWR server to run this job.
         """
         return self.__raw_execute_and_parse("setup", {"job_id": self.job_id})
-
-
-def _read(path):
-    """
-    Utility method to quickly read small files (config files and tool
-    wrappers) into memory as strings.
-    """
-    input = open(path, "r")
-    try:
-        return input.read()
-    finally:
-        input.close()
