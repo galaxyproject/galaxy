b4b23c8fa2fa2f90f33b23c5031fc3a9c716317d
John Chilton <jmchilton@gmail.com>
2013-01-11 10:53:05 -0600

    Implement new job runner super class ClusterJobRunner intended to reduce amount of duplicated code between drmaa, pbs, and lwr job runners (also I guess cli and condor classes seem like they could benefit from this as well). This super class will manage the monitor and worker threads and queues. I am submitting only changes to the LWR that use this class, but I would encourage the Galaxy team to refactor the drmaa and pbs runners to use this class as well (or I would be happy to make these changes if given access or a promise the changes will be accepted quickly).
    A variant of the drmaa runner that has been refactored to use this class can be found here: https://bitbucket.org/jmchilton/galaxy-central-lwr-enhancement-1/src/tip/lib/galaxy/jobs/runners/drmaa.py from the now defunct pull request 80.

--- lib/galaxy/jobs/runners/__init__.py
+++ lib/galaxy/jobs/runners/__init__.py
@@ -1,5 +1,10 @@
 import os, logging, os.path
 
+from galaxy import model
+from Queue import Queue, Empty
+import time
+import threading
+
 log = logging.getLogger( __name__ )
 
 class BaseJobRunner( object ):
@@ -90,3 +95,158 @@ class BaseJobRunner( object ):
                             set_extension = False,
                             kwds = { 'overwrite' : False } ) 
         return commands
+
+class ClusterJobState( object ):
+    """
+    Encapsulate the state of a cluster job, this should be subclassed as
+    needed for various job runners to capture additional information needed
+    to communicate with cluster job manager.
+    """
+
+    def __init__( self ):
+        self.job_wrapper = None
+        self.job_id = None
+        self.old_state = None
+        self.running = False
+        self.runner_url = None
+
+STOP_SIGNAL = object()
+
+JOB_STATUS_QUEUED = 'queue'
+JOB_STATUS_FAILED = 'fail'
+JOB_STATUS_FINISHED = 'finish'
+
+class ClusterJobRunner( BaseJobRunner ):
+    """
+    Not sure this is the best name for this class, but there is common code
+    shared between sge, pbs, drmaa, etc...
+    """
+
+    def __init__( self, app ):
+        self.app = app
+        self.sa_session = app.model.context
+        # 'watched' and 'queue' are both used to keep track of jobs to watch.
+        # 'queue' is used to add new watched jobs, and can be called from
+        # any thread (usually by the 'queue_job' method). 'watched' must only
+        # be modified by the monitor thread, which will move items from 'queue'
+        # to 'watched' and then manage the watched jobs.
+        self.watched = []
+        self.monitor_queue = Queue()
+
+    def _init_monitor_thread(self):
+        self.monitor_thread = threading.Thread( name="%s.monitor_thread" % self.runner_name, target=self.monitor )
+        self.monitor_thread.setDaemon( True )
+        self.monitor_thread.start()
+
+    def _init_worker_threads(self):
+        self.work_queue = Queue()
+        self.work_threads = []
+        nworkers = self.app.config.cluster_job_queue_workers
+        for i in range( nworkers ):
+            worker = threading.Thread( name="%s.work_thread-%d" % (self.runner_name, i), target=self.run_next )
+            worker.start()
+            self.work_threads.append( worker )
+
+    def handle_stop(self):
+        # DRMAA and SGE runners should override this and disconnect.
+        pass
+
+    def monitor( self ):
+        """
+        Watches jobs currently in the cluster queue and deals with state changes
+        (queued to running) and job completion
+        """
+        while 1:
+            # Take any new watched jobs and put them on the monitor list
+            try:
+                while 1: 
+                    cluster_job_state = self.monitor_queue.get_nowait()
+                    if cluster_job_state is STOP_SIGNAL:
+                        # TODO: This is where any cleanup would occur
+                        self.handle_stop()
+                        return
+                    self.watched.append( cluster_job_state )
+            except Empty:
+                pass
+            # Iterate over the list of watched jobs and check state
+            self.check_watched_items()
+            # Sleep a bit before the next state check
+            time.sleep( 1 )
+
+    def run_next( self ):
+        """
+        Run the next item in the queue (a job waiting to run or finish )
+        """
+        while 1:
+            ( op, obj ) = self.work_queue.get()
+            if op is STOP_SIGNAL:
+                return
+            try:
+                if op == JOB_STATUS_QUEUED:
+                    # If the next item is to be run, then only run it if the
+                    # job state is "queued". Otherwise the next item was either
+                    # cancelled or one of its siblings encountered an error.
+                    job_state = obj.get_state()
+                    if model.Job.states.QUEUED == job_state:
+                        self.queue_job( obj )
+                    else:
+                        log.debug( "Not executing job %d in state %s"  % ( obj.get_id_tag(), job_state ) ) 
+                elif op == JOB_STATUS_FINISHED:
+                    self.finish_job( obj )
+                elif op == JOB_STATUS_FAILED:
+                    self.fail_job( obj )
+            except:
+                log.exception( "Uncaught exception %sing job" % op )
+
+    def monitor_job(self, job_state):
+        self.monitor_queue.put( job_state )
+
+    def put( self, job_wrapper ):
+        """Add a job to the queue (by job identifier)"""
+        # Change to queued state before handing to worker thread so the runner won't pick it up again
+        job_wrapper.change_state( model.Job.states.QUEUED )
+        self.mark_as_queued(job_wrapper)
+
+    def shutdown( self ):
+        """Attempts to gracefully shut down the monitor thread"""
+        log.info( "sending stop signal to worker threads" )
+        self.monitor_queue.put( STOP_SIGNAL )
+        for i in range( len( self.work_threads ) ):
+            self.work_queue.put( ( STOP_SIGNAL, None ) )
+
+    def check_watched_items(self):
+        """
+        This method is responsible for iterating over self.watched and handling
+        state changes and updating self.watched with a new list of watched job
+        states. Subclasses can opt to override this directly (as older job runners will
+        initially) or just override check_watched_item and allow the list processing to
+        reuse the logic here.
+        """
+        new_watched = []
+        for cluster_job_state in self.watched:
+            new_cluster_job_state = self.check_watched_item(cluster_job_state)
+            if new_cluster_job_state:
+                new_watched.append(new_cluster_job_state)
+        self.watched = new_watched
+
+    # Subclasses should implement this unless they override check_watched_items all together.
+    def check_watched_item(self):
+        raise NotImplementedError()
+
+    def queue_job(self, job_wrapper):
+        raise NotImplementedError()
+
+    def finish_job(self, job_state):
+        raise NotImplementedError()
+
+    def fail_job(self, job_state):
+        raise NotImplementedError()
+
+    def mark_as_finished(self, job_state):
+        self.work_queue.put( ( JOB_STATUS_FINISHED, job_state ) )
+
+    def mark_as_failed(self, job_state):
+        self.work_queue.put( ( JOB_STATUS_FAILED, job_state ) )
+
+    def mark_as_queued(self, job_wrapper):
+        self.work_queue.put( ( JOB_STATUS_QUEUED, job_wrapper ) )
