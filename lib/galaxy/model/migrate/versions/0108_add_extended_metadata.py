265d857f0d3cc11e371af7264264db1693944ed6
Jeremy Goecks <jeremy.goecks@emory.edu>
2013-01-17 13:56:26 -0500

    Comment and naming fix for migration script 108.

--- lib/galaxy/model/migrate/versions/0108_add_extended_metadata.py
+++ lib/galaxy/model/migrate/versions/0108_add_extended_metadata.py
@@ -71,10 +71,10 @@ def downgrade():
     except Exception, e:
         log.debug( "Dropping 'extended_metadata' table failed: %s" % ( str( e ) ) )
     
-    # Drop the Job table's exit_code column. 
+    # Drop the LDDA table's extended metadata ID column. 
     try:
-        job_table = Table( "library_dataset_dataset_association", metadata, autoload=True )
-        extended_metadata_id = job_table.c.extended_metadata_id
+        ldda_table = Table( "library_dataset_dataset_association", metadata, autoload=True )
+        extended_metadata_id = ldda_table.c.extended_metadata_id
         extended_metadata_id.drop()
     except Exception, e:
         log.debug( "Dropping 'extended_metadata_id' column from library_dataset_dataset_association table failed: %s" % ( str( e ) ) )
