290cd3c35fcde82ef54b6e79cb4cfa840fc0010d
Jeremy Goecks <jeremy.goecks@emory.edu>
2013-02-26 16:42:11 -0500

    Add missing import.

--- lib/galaxy/webapps/galaxy/controllers/visualization.py
+++ lib/galaxy/webapps/galaxy/controllers/visualization.py
@@ -6,6 +6,7 @@ from galaxy.model.item_attrs import UsesAnnotations, UsesItemRatings
 from galaxy.web.base.controller import BaseUIController, SharableMixin, UsesVisualizationMixin
 from galaxy.web.framework.helpers import time_ago, grids
 from galaxy import util
+from galaxy.datatypes.interval import Bed
 from galaxy.util.json import from_json_string
 from galaxy.util.sanitize_html import sanitize_html
 from galaxy.visualization.genomes import decode_dbkey
