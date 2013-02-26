7b13383121cb14b7891a827ed392fb2aef1f350a
Dave Bouvier <dave@bx.psu.edu>
2013-02-26 16:59:17 -0500

    Added name attribute to MigrateToolsApplication to generate a correct metadata dict for migrated tools.

--- lib/tool_shed/galaxy_install/migrate/common.py
+++ lib/tool_shed/galaxy_install/migrate/common.py
@@ -14,6 +14,7 @@ class MigrateToolsApplication( object ):
     def __init__( self, tools_migration_config ):
         install_dependencies = 'install_dependencies' in sys.argv
         galaxy_config_file = 'universe_wsgi.ini'
+        self.name = 'galaxy'
         if '-c' in sys.argv:
             pos = sys.argv.index( '-c' )
             sys.argv.pop( pos )
