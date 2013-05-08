930f938d3ca389f845381d3432ddfa912f0bbdad
Daniel Blankenberg <dan@bx.psu.edu>
2013-05-08 14:22:34 -0400

    Display error message when trying to view / download purged datasets.

--- lib/galaxy/webapps/galaxy/controllers/dataset.py
+++ lib/galaxy/webapps/galaxy/controllers/dataset.py
@@ -290,6 +290,8 @@ class DatasetInterface( BaseUIController, UsesAnnotations, UsesHistoryMixin, Use
             raise paste.httpexceptions.HTTPRequestRangeNotSatisfiable( "Invalid reference dataset id: %s." % str( hda_id ) )
         if not self._can_access_dataset( trans, data ):
             return trans.show_error_message( "You are not allowed to access this dataset" )
+        if data.purged:
+            return trans.show_error_message( "The dataset you are attempting to view has been purged." )
         if data.deleted and not ( trans.user_is_admin() or ( data.history and trans.get_user() == data.history.user ) ):
             return trans.show_error_message( "The dataset you are attempting to view has been deleted." )
         if data.state == trans.model.Dataset.states.UPLOAD:
