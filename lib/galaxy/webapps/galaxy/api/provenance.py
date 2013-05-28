2df81e59de1687dd54b2a9a8896315c34f9d5db6
Kyle Ellrott <kellrott@gmail.com>
2013-05-28 00:05:35 -0700

    Adding source following in provenance tracing

--- lib/galaxy/webapps/galaxy/api/provenance.py
+++ lib/galaxy/webapps/galaxy/api/provenance.py
@@ -14,12 +14,14 @@ class BaseProvenanceController( BaseAPIController ):
     """
     @web.expose_api
     def index( self, trans, **kwd ):
-        value = self._get_provenance( trans, self.provenance_item_class, kwd[self.provenance_item_id] )
+        follow = kwd.get('follow', False)
+        value = self._get_provenance( trans, self.provenance_item_class, kwd[self.provenance_item_id], follow )
         return value
 
     @web.expose_api
     def show( self, trans, elem_name, **kwd ):
-        value = self._get_provenance( trans, self.provenance_item_class, kwd[self.provenance_item_id] )
+        follow = kwd.get('follow', False)
+        value = self._get_provenance( trans, self.provenance_item_class, kwd[self.provenance_item_id], follow )
         return value
 
     @web.expose_api
@@ -31,50 +33,35 @@ class BaseProvenanceController( BaseAPIController ):
         raise HTTPBadRequest("Cannot Delete Provenance")
 
     def _get_provenance(self, trans, item_class_name, item_id, follow=True):
-        out = []
         provenance_item = self.get_object( trans, item_id, item_class_name, check_ownership=False, check_accessible=True )
-        out.append(
-            {
-                "dataset" : {
-                    "id" : trans.security.encode_id(provenance_item.id),
-                    "uuid" : provenance_item.dataset.uuid
-                },
-                "tool_id" : provenance_item.creating_job.tool_id,
-                "parameters" : self._get_job_record(trans, provenance_item.creating_job)
-            }
-        )
-        if follow:
-            depends = self._get_dataset_depends(provenance_item)
-            recorded = {}
-            for rec in depends:
-                if rec.id not in recorded:
-                    recorded[rec.id] = True
-                    out.append( 
-                        {
-                            "dataset" : {
-                                "id" : trans.security.encode_id(rec.id),
-                                "uuid" : rec.dataset.uuid
-                            },
-                            "tool_id" : rec.creating_job.tool_id,
-                            "parameters" : self._get_job_record(trans, rec.creating_job)
-                        }
-                    )
+        out = self._get_record(trans, provenance_item, follow)
         return out
 
-    def _get_dataset_depends(self, item):
-        out = []
-        for d in item.creating_job.input_datasets:
-            out.append(d.dataset)
-            for c in self._get_dataset_depends(d.dataset):
-                out.append(c)
-        return out
+    def _get_record(self, trans, item, follow):
+        if item is not None:
+            if follow:
+                if item.copied_from_library_dataset_dataset_association:
+                    item = item.copied_from_library_dataset_dataset_association
+                return {
+                    "id" : trans.security.encode_id(item.id),
+                    "uuid" : item.dataset.uuid,
+                    "tool_id" : item.creating_job.tool_id,
+                    "parameters" : self._get_job_record(trans, item.creating_job, follow)
+                }
+            else:
+                return {
+                    "id" : trans.security.encode_id(item.id),
+                    "uuid" : item.dataset.uuid
+                }
+        return None
 
-    def _get_job_record(self, trans, job):
+    def _get_job_record(self, trans, job, follow):
         out = {}
         for p in job.parameters:
             out[p.name] = p.value
         for in_d in job.input_datasets:
-            out[in_d.name] = { "dataset_id" : trans.security.encode_id(in_d.dataset.id), "uuid" : in_d.dataset.dataset.uuid }
+            if follow:
+                out[in_d.name] = self._get_record(trans, in_d.dataset, follow)
         return out
 
 
