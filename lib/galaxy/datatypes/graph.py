d877d508e590e2619dec5cc8405b474f25afb195
Carl Eberhard <carlfeberhard@gmail.com>
2013-08-08 15:04:53 -0400

    Graph datatype: add data providers for SIF & XGMML; simple graph data structure to util

--- lib/galaxy/datatypes/graph.py
+++ lib/galaxy/datatypes/graph.py
@@ -2,12 +2,18 @@
 Graph content classes.
 """
 
-import data, tabular, xml
+import data
+import tabular
+import xml
+
+import dataproviders
+from galaxy.util import simplegraph
 
 import logging
 log = logging.getLogger( __name__ )
 
 
+@dataproviders.decorators.has_dataproviders
 class Xgmml( xml.GenericXml ):
     """
     XGMML graph format
@@ -48,7 +54,13 @@ class Xgmml( xml.GenericXml ):
         #For one file only, use base class method (move/copy)
         data.Text.merge( split_files, output_file )
 
+    @dataproviders.decorators.dataprovider_factory( 'node-edge', dataproviders.hierarchy.XMLDataProvider.settings )
+    def node_edge_dataprovider( self, dataset, **settings ):
+        dataset_source = dataproviders.dataset.DatasetDataProvider( dataset )
+        return XGMMLGraphDataProvider( dataset_source, **settings )
 
+
+@dataproviders.decorators.has_dataproviders
 class Sif( tabular.Tabular ):
     """
     SIF graph format
@@ -75,7 +87,6 @@ class Sif( tabular.Tabular ):
         """
         Determines whether the file is SIF
         """
-        print '---------------------------------------- sniffing Siffing'
         line = ''
         with open( filename ) as infile:
             correct = True
@@ -91,6 +102,11 @@ class Sif( tabular.Tabular ):
     @staticmethod
     def merge( split_files, output_file ):
         data.Text.merge( split_files, output_file )
+
+    @dataproviders.decorators.dataprovider_factory( 'node-edge', dataproviders.column.ColumnarDataProvider.settings )
+    def node_edge_dataprovider( self, dataset, **settings ):
+        dataset_source = dataproviders.dataset.DatasetDataProvider( dataset )
+        return SIFGraphDataProvider( dataset_source, **settings )
 
 
 #TODO: we might want to look at rdflib or a similar, larger lib/egg
@@ -108,3 +124,75 @@ class Rdf( xml.GenericXml ):
         else:
             dataset.peek = 'file does not exist'
             dataset.blurb = 'file purged from disk'
+
+    #TODO: won't be as simple
+    #@dataproviders.decorators.dataprovider_factory( 'node-edge', dataproviders.column.ColumnarDataProvider.settings )
+    #def node_edge_dataprovider( self, dataset, **settings ):
+    #    dataset_source = dataproviders.dataset.DatasetDataProvider( dataset )
+    #    return None
+
+
+# ----------------------------------------------------------------------------- graph specific data providers
+class XGMMLGraphDataProvider( dataproviders.hierarchy.XMLDataProvider ):
+    """
+    Provide two lists: nodes, edges::
+
+        'nodes': contains objects of the form:
+            { 'id' : <some string id>, 'data': <any extra data> }
+        'edges': contains objects of the form:
+            { 'source' : <an index into nodes>, 'target': <an index into nodes>, 'data': <any extra data> }
+    """
+    def __iter__( self ):
+        # use simple graph to store nodes and links, later providing them as a dict
+        #   essentially this is a form of aggregation
+        graph = simplegraph.SimpleGraph()
+
+        parent_gen = super( XGMMLGraphDataProvider, self ).__iter__()
+        for graph_elem in parent_gen:
+            if 'children' not in graph_elem:
+                continue
+            for elem in graph_elem[ 'children' ]:
+                # use endswith to work around Elementtree namespaces
+                if elem[ 'tag' ].endswith( 'node' ):
+                    node_id = elem[ 'attrib' ][ 'id' ]
+                    # pass the entire, parsed xml element as the data
+                    graph.add_node( node_id, **elem )
+
+                elif elem[ 'tag' ].endswith( 'edge' ):
+                    source_id = elem[ 'attrib' ][ 'source' ]
+                    target_id = elem[ 'attrib' ][ 'target' ]
+                    graph.add_edge( source_id, target_id, **elem )
+
+        yield graph.as_dict()
+
+
+class SIFGraphDataProvider( dataproviders.column.ColumnarDataProvider ):
+    """
+    Provide two lists: nodes, edges::
+
+        'nodes': contains objects of the form:
+            { 'id' : <some string id>, 'data': <any extra data> }
+        'edges': contains objects of the form:
+            { 'source' : <an index into nodes>, 'target': <an index into nodes>, 'data': <any extra data> }
+    """
+    def __iter__( self ):
+        # use simple graph to store nodes and links, later providing them as a dict
+        #   essentially this is a form of aggregation
+        graph = simplegraph.SimpleGraph()
+        # SIF is tabular with the source, link-type, and all targets in the columns
+        parent_gen = super( SIFGraphDataProvider, self ).__iter__()
+        for columns in parent_gen:
+            if columns:
+                source_id = columns[0]
+                # there's no extra data for nodes (or links) in the examples I've seen
+                graph.add_node( source_id )
+
+                # targets are the (variadic) remaining columns
+                if len( columns ) >= 3:
+                    relation = columns[1]
+                    targets = columns[2:]
+                    for target_id in targets:
+                        graph.add_node( target_id )
+                        graph.add_edge( source_id, target_id, type=relation )
+
+        yield graph.as_dict()
