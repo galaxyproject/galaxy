d516842c0807c44b6f766d13168615fcf77d983a
Greg Von Kuster <greg@bx.psu.edu>
2013-10-07 10:32:33 -0400

    Fix setting mimetype for image files in tool shed repositories.

--- lib/galaxy/webapps/tool_shed/controllers/repository.py
+++ lib/galaxy/webapps/tool_shed/controllers/repository.py
@@ -1038,9 +1038,14 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
                 path_to_file = suc.get_absolute_path_to_file_in_repository( repo_files_dir, relative_path_to_image_file )
                 if os.path.exists( path_to_file ):
                     file_name = os.path.basename( relative_path_to_image_file )
-                    mimetype = trans.app.datatypes_registry.get_mimetype_by_extension( file_name, None )
-                    if mimetype:
-                        trans.response.set_content_type( mimetype )
+                    try:
+                        extension = file_name.split( '.' )[ -1 ]
+                    except Exception, e:
+                        extension = None
+                    if extension:
+                        mimetype = trans.app.datatypes_registry.get_mimetype_by_extension( extension )
+                        if mimetype:
+                            trans.response.set_content_type( mimetype )
                     return open( path_to_file, 'r' )
         return None
 
