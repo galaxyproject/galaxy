86b7dd2b3de9aea5f4199393245fadc2b76234ac
Dannon Baker <dannon.baker@gmail.com>
2013-10-21 16:16:01 -0400

    Datatypes controller should allow anonymous access.

--- lib/galaxy/webapps/galaxy/api/datatypes.py
+++ lib/galaxy/webapps/galaxy/api/datatypes.py
@@ -4,13 +4,12 @@ API operations allowing clients to determine datatype supported by Galaxy.
 
 from galaxy import web
 from galaxy.web.base.controller import BaseAPIController
-from galaxy.datatypes.registry import Registry
 
 import logging
 log = logging.getLogger( __name__ )
 
 class DatatypesController( BaseAPIController ):
-    @web.expose_api
+    @web.expose_api_anonymous
     def index( self, trans, **kwd ):
         """
         GET /api/datatypes
@@ -18,7 +17,6 @@ class DatatypesController( BaseAPIController ):
         """
         try:
             return trans.app.datatypes_registry.upload_file_formats
-            
         except Exception, exception:
             log.error( 'could not get datatypes: %s', str( exception ), exc_info=True )
             trans.response.status = 500
