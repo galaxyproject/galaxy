9ef20f5269a33ec57f00feba2ab660b2734a9ac1
John Chilton <jmchilton@gmail.com>
2013-10-30 00:01:15 -0500

    PEP-8 fixes and style touch-ups for test/functional/test_toolbox.py.

--- test/functional/test_toolbox.py
+++ test/functional/test_toolbox.py
@@ -1,6 +1,6 @@
-import sys, new
+import sys
+import new
 from galaxy.tools.parameters import grouping
-from galaxy.tools.parameters import basic
 from base.twilltestcase import TwillTestCase
 import galaxy.model
 from galaxy.model.orm import *
@@ -8,8 +8,10 @@ from galaxy.model.mapping import context as sa_session
 
 toolbox = None
 
+
 class ToolTestCase( TwillTestCase ):
     """Abstract test case that runs tests based on a `galaxy.tools.test.ToolTest`"""
+
     def do_it( self, testdef, shed_tool_id=None ):
         # If the test generation had an error, raise
         if testdef.error:
@@ -17,22 +19,23 @@ class ToolTestCase( TwillTestCase ):
                 raise testdef.exception
             else:
                 raise Exception( "Test parse failure" )
+
         # Start with a new history
         self.logout()
         self.login( email='test@bx.psu.edu' )
-        admin_user = sa_session.query( galaxy.model.User ).filter( galaxy.model.User.table.c.email=='test@bx.psu.edu' ).one()
+        admin_user = sa_session.query( galaxy.model.User ).filter( galaxy.model.User.table.c.email == 'test@bx.psu.edu' ).one()
         self.new_history()
         latest_history = sa_session.query( galaxy.model.History ) \
-                                   .filter( and_( galaxy.model.History.table.c.deleted==False,
-                                                  galaxy.model.History.table.c.user_id==admin_user.id ) ) \
+                                   .filter( and_( galaxy.model.History.table.c.deleted == False,
+                                                  galaxy.model.History.table.c.user_id == admin_user.id ) ) \
                                    .order_by( desc( galaxy.model.History.table.c.create_time ) ) \
                                    .first()
         assert latest_history is not None, "Problem retrieving latest_history from database"
         if len( self.get_history_as_data_list() ) > 0:
             raise AssertionError("ToolTestCase.do_it failed")
+
         # Upload any needed files
         for fname, extra in testdef.required_files:
-            children = extra.get( 'children', [] )
             metadata = extra.get( 'metadata', [] )
             composite_data = extra.get( 'composite_data', [] )
             self.upload_file( fname,
@@ -51,29 +54,34 @@ class ToolTestCase( TwillTestCase ):
                     assert new_name, 'You must supply the new dataset name as the value tag of the edit_attributes tag'
                     hda_id = self.get_history_as_data_list()[-1].get( 'id' )
                     try:
-                        self.edit_hda_attribute_info( hda_id = str(hda_id), new_name = new_name )
+                        self.edit_hda_attribute_info( hda_id=str(hda_id), new_name=new_name )
                     except:
-                        print "### call to edit_hda failed for hda_id %s, new_name=%s" % (hda_id,new_name)
+                        print "### call to edit_hda failed for hda_id %s, new_name=%s" % (hda_id, new_name)
                 else:
                     raise Exception( 'edit_attributes type (%s) is unimplemented' % edit_att.get( 'type', None ) )
+
         # We need to handle the case where we've uploaded a valid compressed file since the upload
         # tool will have uncompressed it on the fly.
         all_inputs = {}
         for name, value, _ in testdef.inputs:
             all_inputs[ name ] = value
+
         # See if we have a grouping.Repeat element
         repeat_name = None
         for input_name, input_value in testdef.tool.inputs_by_page[0].items():
-            if isinstance( input_value, grouping.Repeat ) and all_inputs.get( input_name, 1 ) not in [ 0, "0" ]: #default behavior is to test 1 repeat, for backwards compatibility
+            if isinstance( input_value, grouping.Repeat ) and all_inputs.get( input_name, 1 ) not in [ 0, "0" ]:  # default behavior is to test 1 repeat, for backwards compatibility
                 repeat_name = input_name
                 break
+
         #check if we need to verify number of outputs created dynamically by tool
         if testdef.tool.force_history_refresh:
             job_finish_by_output_count = len( self.get_history_as_data_list() )
         else:
             job_finish_by_output_count = False
+
         # Do the first page
-        page_inputs =  self.__expand_grouping(testdef.tool.inputs_by_page[0], all_inputs)
+        page_inputs = self.__expand_grouping(testdef.tool.inputs_by_page[0], all_inputs)
+
         # Run the tool
         self.run_tool( testdef.tool.id, repeat_name=repeat_name, **page_inputs )
         print "page_inputs (0)", page_inputs
@@ -82,6 +90,7 @@ class ToolTestCase( TwillTestCase ):
             page_inputs = self.__expand_grouping(testdef.tool.inputs_by_page[i], all_inputs)
             self.submit_form( **page_inputs )
             print "page_inputs (%i)" % i, page_inputs
+
         # Check the results ( handles single or multiple tool outputs ).  Make sure to pass the correct hid.
         # The output datasets from the tool should be in the same order as the testdef.outputs.
         data_list = None
@@ -100,7 +109,7 @@ class ToolTestCase( TwillTestCase ):
             elem_index += 1
             try:
                 self.verify_dataset_correctness( outfile, hid=elem_hid, maxseconds=testdef.maxseconds, attributes=attributes, shed_tool_id=shed_tool_id )
-            except Exception, e:
+            except Exception:
                 print >>sys.stderr, self.get_job_stdout( elem.get( 'id' ), format=True )
                 print >>sys.stderr, self.get_job_stderr( elem.get( 'id' ), format=True )
                 raise
@@ -121,14 +130,14 @@ class ToolTestCase( TwillTestCase ):
                         else:
                             expanded_inputs[ "%s|%s" % ( new_prefix, value.test_param.name ) ] = case.value
                         for input_name, input_value in case.inputs.items():
-                            expanded_inputs.update( self.__expand_grouping( { input_name:input_value }, declared_inputs, prefix = new_prefix ) )
+                            expanded_inputs.update( self.__expand_grouping( { input_name: input_value }, declared_inputs, prefix=new_prefix ) )
             elif isinstance( value, grouping.Repeat ):
-                for repeat_index in xrange( 0, 1 ): #need to allow for and figure out how many repeats we have
+                for repeat_index in xrange( 0, 1 ):  # need to allow for and figure out how many repeats we have
                     for r_name, r_value in value.inputs.iteritems():
-	                    new_prefix = "%s_%d" % ( value.name, repeat_index )
-	                    if prefix:
-	                        new_prefix = "%s|%s" % ( prefix, new_prefix )
-	                    expanded_inputs.update( self.__expand_grouping( { new_prefix : r_value }, declared_inputs, prefix = new_prefix ) )
+                        new_prefix = "%s_%d" % ( value.name, repeat_index )
+                        if prefix:
+                            new_prefix = "%s|%s" % ( prefix, new_prefix )
+                        expanded_inputs.update( self.__expand_grouping( { new_prefix : r_value }, declared_inputs, prefix=new_prefix ) )
             elif value.name not in declared_inputs:
                 print "%s not declared in tool test, will not change default value." % value.name
             elif isinstance(declared_inputs[value.name], str):
@@ -143,6 +152,7 @@ class ToolTestCase( TwillTestCase ):
                     expanded_inputs[value.name] = declared_inputs[value.name]
         return expanded_inputs
 
+
 def build_tests( testing_shed_tools=False ):
     """
     If the module level variable `toolbox` is set, generate `ToolTestCase`
@@ -151,12 +161,15 @@ def build_tests( testing_shed_tools=False ):
     """
     if toolbox is None:
         return
+
     # Push all the toolbox tests to module level
     G = globals()
+
     # Eliminate all previous tests from G.
     for key, val in G.items():
         if key.startswith( 'TestForTool_' ):
             del G[ key ]
+
     for i, tool_id in enumerate( toolbox.tools_by_id ):
         tool = toolbox.get_tool( tool_id )
         if tool.tests:
