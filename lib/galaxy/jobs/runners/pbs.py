ab6d5fb183020c50ae8cb07d33f09621a5c12756
John Chilton <jmchilton@gmail.com>
2013-11-05 20:47:21 -0600

    Attept to improve user feedback for pbs jobs which hit max walltime.
    Working with Dan Sullivan on this.

--- lib/galaxy/jobs/runners/pbs.py
+++ lib/galaxy/jobs/runners/pbs.py
@@ -34,6 +34,8 @@ log = logging.getLogger( __name__ )
 
 __all__ = [ 'PBSJobRunner' ]
 
+CLUSTER_ERROR_MESSAGE = "Job cannot be completed due to a cluster error, please retry it later: %s"
+
 # The last two lines execute the command and then retrieve the command's
 # exit code ($?) and write it to a file.
 pbs_symlink_template = """
@@ -80,7 +82,7 @@ JOB_EXIT_STATUS = {
     -6: "job aborted on MOM init, chkpt, ok migrate",
     -7: "job restart failed",
     -8: "exec() of user command failed",
-    -11: "job maximum walltime exceeded",
+    -11: "job maximum walltime exceeded",  # Added by John, not from job.h.
 }
 
 
@@ -413,8 +415,10 @@ class PBSJobRunner( AsynchronousJobRunner ):
                     assert int( status.exit_status ) == 0
                     log.debug("(%s/%s) PBS job has completed successfully" % ( galaxy_job_id, job_id ) )
                 except AssertionError:
-                    pbs_job_state.fail_message = 'Job cannot be completed due to a cluster error, please retry it later'
-                    log.error( '(%s/%s) PBS job failed: %s' % ( galaxy_job_id, job_id, JOB_EXIT_STATUS.get( int( status.exit_status ), 'Unknown error: %s' % status.exit_status ) ) )
+                    exit_status = int( status.exit_status )
+                    error_message = JOB_EXIT_STATUS.get( exit_status, 'Unknown error: %s' % status.exit_status )
+                    pbs_job_state.fail_message = CLUSTER_ERROR_MESSAGE % error_message
+                    log.error( '(%s/%s) PBS job failed: %s' % ( galaxy_job_id, job_id, error_message ) )
                     pbs_job_state.stop_job = False
                     self.work_queue.put( ( self.fail_job, pbs_job_state ) )
                     continue
