14db913c4df1cf9ee411060686f51b4551816858
John Chilton <jmchilton@gmail.com>
2013-11-11 01:45:32 -0600

    Allow raw display of data via API. Matching functionality available with sessions at /display in root controller. The test framework uses this functionality in root controller to read test result data so an API driven approach to testing will need to have similar functionality available. I am not sure I am enitrely comfortable with serving raw data this way, but I guess it is no worse than the existing /display route. This is marked as beta and there is a note that it may be dropped at some point in the future.

--- lib/galaxy/webapps/galaxy/api/datasets.py
+++ lib/galaxy/webapps/galaxy/api/datasets.py
@@ -7,7 +7,7 @@ from galaxy.web.base.controller import BaseAPIController, UsesVisualizationMixin
 from galaxy.web.base.controller import UsesHistoryMixin
 from galaxy.web.framework.helpers import is_true
 from galaxy.datatypes import dataproviders
-
+from galaxy.util import string_as_bool_or_none
 import logging
 log = logging.getLogger( __name__ )
 
@@ -245,11 +245,16 @@ class DatasetsController( BaseAPIController, UsesVisualizationMixin, UsesHistory
 
     @web.expose_api_raw_anonymous
     def display( self, trans, history_content_id, history_id,
-                 preview=False, filename=None, to_ext=None, chunk=None, **kwd ):
+                 preview=False, filename=None, to_ext=None, chunk=None, raw=False, **kwd ):
         """
         GET /api/histories/{encoded_history_id}/contents/{encoded_content_id}/display
         Displays history content (dataset).
+
+        The query parameter 'raw' should be considered experimental and may be dropped at
+        some point in the future without warning. Generally, data should be processed by its
+        datatype prior to display (the defult if raw is unspecified or explicitly false.
         """
+        raw = string_as_bool_or_none( raw )
         # Huge amount of code overlap with lib/galaxy/webapps/galaxy/api/history_content:show here.
         rval = ''
         try:
@@ -274,8 +279,10 @@ class DatasetsController( BaseAPIController, UsesVisualizationMixin, UsesHistory
                 del display_kwd["key"]
             except KeyError:
                 pass
-            rval = hda.datatype.display_data( trans, hda, preview, filename, to_ext, chunk, **display_kwd )
-
+            if raw:
+                rval = open( hda.file_name )
+            else:
+                rval = hda.datatype.display_data( trans, hda, preview, filename, to_ext, chunk, **display_kwd )
 
         except Exception, exception:
             log.error( "Error getting display data for dataset (%s) from history (%s): %s",
