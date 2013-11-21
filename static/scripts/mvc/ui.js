2c7f48b07932e312e36caf9bba331eeb8361e777
Carl Eberhard <carlfeberhard@gmail.com>
2013-11-21 16:22:58 -0500

    UI: move loading indicator into ui.js, add cover option

--- static/scripts/mvc/ui.js
+++ static/scripts/mvc/ui.js
@@ -465,8 +465,10 @@ PopupMenu.make_popup_menus = function( parent, menuSelector, buttonSelectorBuild
     return popupMenusCreated;
 };
 
+
+//==============================================================================
 var faIconButton = function( options ){
-    //TODO: move out of global
+//TODO: move out of global
     options = options || {};
     options.tooltipConfig = options.tooltipConfig || { placement: 'bottom' };
 
@@ -491,6 +493,8 @@ var faIconButton = function( options ){
     return $button;
 };
 
+
+//==============================================================================
 var searchInput = function( options ){
 //TODO: move out of global
 //TODO: consolidate with tool menu functionality, use there
@@ -589,3 +593,88 @@ var searchInput = function( options ){
     }
     return $searchInput.append([ $input(), $clearBtn() ]);
 };
+
+
+//==============================================================================
+function LoadingIndicator( $where, options ){
+//TODO: move out of global
+//TODO: too specific to history panel
+
+    var self = this;
+    // defaults
+    options = jQuery.extend({
+        cover       : false
+    }, options || {} );
+
+    function render(){
+        var html = [
+            '<div class="loading-indicator">',
+                '<div class="loading-indicator-text">',
+                    '<span class="fa fa-spinner fa-spin fa-lg"></span>',
+                    '<span class="loading-indicator-message">loading...</span>',
+                '</div>',
+            '</div>'
+        ].join( '\n' );
+
+        var $indicator = $( html ).hide().css( options.css || {
+                position    : 'fixed'
+            }),
+            $text = $indicator.children( '.loading-indicator-text' );
+
+        if( options.cover ){
+            $indicator.css({
+                'z-index'   : 2,
+                top         : $where.css( 'top' ),
+                bottom      : $where.css( 'bottom' ),
+                left        : $where.css( 'left' ),
+                right       : $where.css( 'right' ),
+                opacity     : 0.5,
+                'background-color': 'white',
+                'text-align': 'center'
+            });
+            $text = $indicator.children( '.loading-indicator-text' ).css({
+                'margin-top'        : '20px'
+            });
+
+        } else {
+            $text = $indicator.children( '.loading-indicator-text' ).css({
+                margin              : '12px 0px 0px 10px',
+                opacity             : '0.85',
+                color               : 'grey'
+            });
+            $text.children( '.loading-indicator-message' ).css({
+                margin          : '0px 8px 0px 0px',
+                'font-style'    : 'italic'
+            });
+        }
+        return $indicator;
+    }
+
+    self.show = function( msg, speed, callback ){
+        msg = msg || 'loading...';
+        speed = speed || 'fast';
+        // since position is fixed - we insert as sibling
+        self.$indicator = render().insertBefore( $where );
+        self.message( msg );
+        self.$indicator.fadeIn( speed, callback );
+        return self;
+    };
+
+    self.message = function( msg ){
+        self.$indicator.find( 'i' ).text( msg );
+    };
+
+    self.hide = function( speed, callback ){
+        speed = speed || 'fast';
+        if( self.$indicator && self.$indicator.size() ){
+            self.$indicator.fadeOut( speed, function(){
+                self.$indicator.remove();
+                if( callback ){ callback(); }
+            });
+        } else {
+            if( callback ){ callback(); }
+        }
+        return self;
+    };
+    return self;
+}
