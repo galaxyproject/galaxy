b006e1c330165caf03d9c052e474a9b7b77ad010
Dave Bouvier <dave@bx.psu.edu>
2013-11-25 13:50:32 -0500

    Fix the update manager functional test.

--- test/tool_shed/functional/test_1410_update_manager.py
+++ test/tool_shed/functional/test_1410_update_manager.py
@@ -123,6 +123,12 @@ class TestUpdateManager( ShedTwillTestCase ):
         self.galaxy_login( email=common.admin_email, username=common.admin_username )
         repository = test_db_util.get_repository_by_name_and_owner( repository_name, common.test_user_1_name )
         self.update_tool_shed_status()
-        strings_displayed = [ '"%s"><img src="/static/images/icon_warning_sml.gif"' % self.security.encode_id( repository.id ) ]
+        ok_icon = '/static/june_2007_style/blue/ok_small.png'
+        ok_title = 'This is the latest installable revision of this repository'
+        updates_icon = '/static/images/icon_warning_sml.gif'
+        repository_id = self.security.encode_id( repository.id )
+        html = '<label id="%s" for="%s"><img src="%s" class="icon-button" title="%s"/><img src="%s' % \
+            ( repository_id, repository_id, ok_icon, ok_title, updates_icon )
+        strings_displayed = [ html ]
         self.display_galaxy_browse_repositories_page( strings_displayed=strings_displayed )
         
