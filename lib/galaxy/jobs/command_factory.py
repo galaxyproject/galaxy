dd794a526ca33891e9209d1c16253e4c32b2cf22
John Chilton <jmchilton@gmail.com>
2013-12-11 10:26:15 -0600

    command_factory cleanup - split big build_command method into smaller well-named pieces.

--- lib/galaxy/jobs/command_factory.py
+++ lib/galaxy/jobs/command_factory.py
@@ -19,59 +19,77 @@ def build_command( runner, job_wrapper, include_metadata=False, include_work_dir
     if not commands_builder.commands:
         return None
 
+    __handle_version_command(commands_builder, job_wrapper)
+    __handle_task_splitting(commands_builder, job_wrapper)
+    __handle_dependency_resolution(commands_builder, job_wrapper, remote_command_params)
+
+    if include_work_dir_outputs:
+        __handle_work_dir_outputs(commands_builder, job_wrapper, runner, remote_command_params)
+
+    if include_metadata and job_wrapper.requires_setting_metadata:
+        __handle_metadata(commands_builder, job_wrapper, runner, remote_command_params)
+
+    return commands_builder.build()
+
+
+def __handle_version_command(commands_builder, job_wrapper):
     # Prepend version string
     if job_wrapper.version_string_cmd:
         version_command = "%s &> %s" % ( job_wrapper.version_string_cmd, job_wrapper.get_version_string_path() )
         commands_builder.prepend_command(version_command)
 
+
+def __handle_task_splitting(commands_builder, job_wrapper):
     # prepend getting input files (if defined)
-    if hasattr(job_wrapper, 'prepare_input_files_cmds') and job_wrapper.prepare_input_files_cmds is not None:
+    if getattr(job_wrapper, 'prepare_input_files_cmds', None):
         commands_builder.prepend_commands(job_wrapper.prepare_input_files_cmds)
 
+
+def __handle_dependency_resolution(commands_builder, job_wrapper, remote_command_params):
     local_dependency_resolution = "dependency_resolution" not in remote_command_params or (remote_command_params["dependency_resolution"] == "local")
     # Prepend dependency injection
     if job_wrapper.dependency_shell_commands and local_dependency_resolution:
         commands_builder.prepend_commands(job_wrapper.dependency_shell_commands)
 
-    # Append commands to copy job outputs based on from_work_dir attribute.
-    if include_work_dir_outputs:
-        work_dir_outputs_kwds = {}
-        if 'working_directory' in remote_command_params:
-            work_dir_outputs_kwds['job_working_directory'] = remote_command_params['working_directory']
-        work_dir_outputs = runner.get_work_dir_outputs( job_wrapper, **work_dir_outputs_kwds )
-        if work_dir_outputs:
-            commands_builder.capture_return_code()
-            commands_builder.append_commands([ "if [ -f %s ] ; then cp %s %s ; fi" %
-                ( source_file, source_file, destination ) for ( source_file, destination ) in work_dir_outputs ])
 
+def __handle_work_dir_outputs(commands_builder, job_wrapper, runner, remote_command_params):
+    # Append commands to copy job outputs based on from_work_dir attribute.
+    work_dir_outputs_kwds = {}
+    if 'working_directory' in remote_command_params:
+        work_dir_outputs_kwds['job_working_directory'] = remote_command_params['working_directory']
+    work_dir_outputs = runner.get_work_dir_outputs( job_wrapper, **work_dir_outputs_kwds )
+    if work_dir_outputs:
+        commands_builder.capture_return_code()
+        commands_builder.append_commands([ "if [ -f %s ] ; then cp %s %s ; fi" %
+            ( source_file, source_file, destination ) for ( source_file, destination ) in work_dir_outputs ])
+
+
+def __handle_metadata(commands_builder, job_wrapper, runner, remote_command_params):
     # Append metadata setting commands, we don't want to overwrite metadata
     # that was copied over in init_meta(), as per established behavior
-    if include_metadata and job_wrapper.requires_setting_metadata:
-        metadata_kwds = remote_command_params.get('metadata_kwds', {})
-        exec_dir = metadata_kwds.get( 'exec_dir', abspath( getcwd() ) )
-        tmp_dir = metadata_kwds.get( 'tmp_dir', job_wrapper.working_directory )
-        dataset_files_path = metadata_kwds.get( 'dataset_files_path', runner.app.model.Dataset.file_path )
-        output_fnames = metadata_kwds.get( 'output_fnames', job_wrapper.get_output_fnames() )
-        config_root = metadata_kwds.get( 'config_root', None )
-        config_file = metadata_kwds.get( 'config_file', None )
-        datatypes_config = metadata_kwds.get( 'datatypes_config', None )
-        metadata_command = job_wrapper.setup_external_metadata(
-            exec_dir=exec_dir,
-            tmp_dir=tmp_dir,
-            dataset_files_path=dataset_files_path,
-            output_fnames=output_fnames,
-            set_extension=False,
-            config_root=config_root,
-            config_file=config_file,
-            datatypes_config=datatypes_config,
-            kwds={ 'overwrite' : False }
-        ) or ''
-        metadata_command = metadata_command.strip()
-        if metadata_command:
-            commands_builder.capture_return_code()
-            commands_builder.append_command("cd %s; %s" % (exec_dir, metadata_command))
-
-    return commands_builder.build()
+    metadata_kwds = remote_command_params.get('metadata_kwds', {})
+    exec_dir = metadata_kwds.get( 'exec_dir', abspath( getcwd() ) )
+    tmp_dir = metadata_kwds.get( 'tmp_dir', job_wrapper.working_directory )
+    dataset_files_path = metadata_kwds.get( 'dataset_files_path', runner.app.model.Dataset.file_path )
+    output_fnames = metadata_kwds.get( 'output_fnames', job_wrapper.get_output_fnames() )
+    config_root = metadata_kwds.get( 'config_root', None )
+    config_file = metadata_kwds.get( 'config_file', None )
+    datatypes_config = metadata_kwds.get( 'datatypes_config', None )
+    metadata_command = job_wrapper.setup_external_metadata(
+        exec_dir=exec_dir,
+        tmp_dir=tmp_dir,
+        dataset_files_path=dataset_files_path,
+        output_fnames=output_fnames,
+        set_extension=False,
+        config_root=config_root,
+        config_file=config_file,
+        datatypes_config=datatypes_config,
+        kwds={ 'overwrite' : False }
+    ) or ''
+    metadata_command = metadata_command.strip()
+    if metadata_command:
+        commands_builder.capture_return_code()
+        commands_builder.append_command("cd %s; %s" % (exec_dir, metadata_command))
 
 
 class CommandsBuilder(object):
