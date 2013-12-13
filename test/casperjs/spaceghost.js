b8e7f2791490f7818790d3734ba3e43590547e37
John Chilton <jmchilton@gmail.com>
2013-12-12 23:25:25 -0600

    casperjs: Fix user login, registration, logout for recent masthead changes... They are generated in javascript instead of HTML now so I don't think they are available when page loads. My fix is to wait for the User menu to appear before attempting these operations.

--- test/casperjs/spaceghost.js
+++ test/casperjs/spaceghost.js
@@ -1103,6 +1103,10 @@ SpaceGhost.prototype.getUniverseSetting = function getUniverseSetting( iniKey ){
     return match[2];
 };
 
+SpaceGhost.prototype.waitForMasthead = function wait( then ) {
+    return this.waitForText( this.data.labels.masthead.menus.user, then );
+}
+
 
 // =================================================================== TEST DATA
 /** General use selectors, labels, and text. Kept here to allow a centralized location.
