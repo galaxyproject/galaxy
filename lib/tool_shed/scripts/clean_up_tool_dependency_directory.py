20425d57a20159a525913cc878290a69f532abe9
Dave Bouvier <dave@bx.psu.edu>
2013-12-18 13:14:24 -0500

    Another bugfix to the tool dependency cleaning script.

--- lib/tool_shed/scripts/clean_up_tool_dependency_directory.py
+++ lib/tool_shed/scripts/clean_up_tool_dependency_directory.py
@@ -11,10 +11,11 @@ def main( args ):
     else:
         for content in os.listdir( args.tool_dependency_dir ):
             print 'Deleting directory %s from %s.' % ( content, args.tool_dependency_dir )
-            if os.path.isdir( content ):
-                shutil.rmtree( os.path.join( args.tool_dependency_dir, content ) )
+            full_path = os.path.join( args.tool_dependency_dir, content )
+            if os.path.isdir( full_path ):
+                shutil.rmtree( full_path )
             else:
-                os.remove( content )
+                os.remove( full_path )
 
 if __name__ == '__main__':
     description = 'Clean out the configured tool dependency path, creating it if it does not exist.'
