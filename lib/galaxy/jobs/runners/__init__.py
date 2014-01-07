458df6faf2f33f4c4260f47e4c06be4c87cdd826
Nate Coraor <nate@bx.psu.edu>
2014-01-07 15:18:11 -0500

    Allow runners plugins to accept params, provide some simple load-time validation logic for params.

--- lib/galaxy/jobs/runners/__init__.py
+++ lib/galaxy/jobs/runners/__init__.py
@@ -22,13 +22,39 @@ log = logging.getLogger( __name__ )
 
 STOP_SIGNAL = object()
 
+
+class RunnerParams( object ):
+
+    def __init__( self, specs = None, params = None ):
+        self.specs = specs or dict()
+        self.params = params or dict()
+        for name, value in self.params.items():
+            assert name in self.specs, 'Invalid job runner parameter for this plugin: %s' % name
+            if 'map' in self.specs[ name ]:
+                try:
+                    self.params[ name ] = self.specs[ name ][ 'map' ]( value )
+                except Exception, e:
+                    raise Exception( 'Job runner parameter "%s" value "%s" could not be converted to the correct type: %s' % ( name, value, e ) )
+            if 'valid' in self.specs[ name ]:
+                assert self.specs[ name ][ 'valid' ]( value ), 'Job runner parameter %s failed validation' % name
+
+    def __getattr__( self, name ):
+        return self.params.get( name, self.specs[ name ][ 'default' ] )
+
+
 class BaseJobRunner( object ):
-    def __init__( self, app, nworkers ):
+    def __init__( self, app, nworkers, **kwargs ):
         """Start the job runner
         """
         self.app = app
         self.sa_session = app.model.context
         self.nworkers = nworkers
+        runner_param_specs = dict( recheck_missing_job_retries = dict( map = int, valid = lambda x: x >= 0, default = 0 ) )
+        if 'runner_param_specs' in kwargs:
+            runner_param_specs.update( kwargs.pop( 'runner_param_specs' ) )
+        if kwargs:
+            log.debug( 'Loading %s with params: %s', self.runner_name, kwargs )
+        self.runner_params = RunnerParams( specs = runner_param_specs, params = kwargs )
 
     def _init_worker_threads(self):
         """Start ``nworkers`` worker threads.
@@ -115,7 +141,7 @@ class BaseJobRunner( object ):
                 job_wrapper.cleanup()
             return False
         elif job_state != model.Job.states.QUEUED:
-            log.info( "(%d) Job is in state %s, skipping execution"  % ( job_id, job_state ) )
+            log.info( "(%s) Job is in state %s, skipping execution"  % ( job_id, job_state ) )
             # cleanup may not be safe in all states
             return False
 
@@ -287,8 +313,8 @@ class AsynchronousJobRunner( BaseJobRunner ):
     to the correct methods (queue, finish, cleanup) at appropriate times..
     """
 
-    def __init__( self, app, nworkers ):
-        super( AsynchronousJobRunner, self ).__init__( app, nworkers )
+    def __init__( self, app, nworkers, **kwargs ):
+        super( AsynchronousJobRunner, self ).__init__( app, nworkers, **kwargs )
         # 'watched' and 'queue' are both used to keep track of jobs to watch.
         # 'queue' is used to add new watched jobs, and can be called from
         # any thread (usually by the 'queue_job' method). 'watched' must only
