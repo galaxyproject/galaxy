6d0a3a2292cd180934d36a0a5f0d1729feb679f6
Saket Choudhary <saketkc@gmail.com>
2014-01-21 00:46:41 +0530

    Formatting changes

--- lib/tool_shed/util/common_util.py
+++ lib/tool_shed/util/common_util.py
@@ -183,7 +183,7 @@ def parse_repository_dependency_tuple( repository_dependency_tuple, contains_err
 def tool_shed_get( app, tool_shed_url, uri ):
     """Make contact with the tool shed via the uri provided."""
     registry = app.tool_shed_registry
-    # urllib2 auto-detects system proxies, when passed a Proxyhandler
+    # urllib2 auto-detects system proxies, when passed a Proxyhandler.
     # Refer: http://docs.python.org/2/howto/urllib2.html#proxies
     proxy = urllib2.ProxyHandler()
     urlopener = urllib2.build_opener( proxy )
