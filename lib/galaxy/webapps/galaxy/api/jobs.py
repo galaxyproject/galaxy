50cf4fcf0e7d794431dfb91cb1b23b75765e5231
John Chilton <jmchilton@gmail.com>
2014-01-28 23:29:15 -0600

    PEP-8 and other style adjustments for new jobs API.

--- lib/galaxy/webapps/galaxy/api/jobs.py
+++ lib/galaxy/webapps/galaxy/api/jobs.py
@@ -4,25 +4,19 @@ API operations on a jobs.
 .. seealso:: :class:`galaxy.model.Jobs`
 """
 
-import pkg_resources
-pkg_resources.require( "Paste" )
-from paste.httpexceptions import HTTPBadRequest, HTTPForbidden, HTTPInternalServerError, HTTPException
-
 from sqlalchemy import or_, and_
 from sqlalchemy.orm import aliased
 import json
 from galaxy import web
 from galaxy.web import _future_expose_api as expose_api
-from galaxy.web import _future_expose_api_anonymous as expose_api_anonymous
-from galaxy.util import string_as_bool, restore_text
-from galaxy.util.sanitize_html import sanitize_html
-from galaxy.web.base.controller import BaseAPIController, UsesHistoryDatasetAssociationMixin, UsesLibraryMixinItems
-from galaxy.web import url_for
-from galaxy.model.orm import desc
+from galaxy.web.base.controller import BaseAPIController
+from galaxy.web.base.controller import UsesHistoryDatasetAssociationMixin
+from galaxy.web.base.controller import UsesLibraryMixinItems
 from galaxy import exceptions
 
 import logging
 log = logging.getLogger( __name__ )
+
 
 class JobController( BaseAPIController, UsesHistoryDatasetAssociationMixin, UsesLibraryMixinItems ):
 
@@ -42,13 +36,14 @@ class JobController( BaseAPIController, UsesHistoryDatasetAssociationMixin, Uses
         :returns:   list of dictionaries containing summary job information
         """
 
-        state = kwd.get('state', None)
-        query = trans.sa_session.query(trans.app.model.Job).filter( 
-            trans.app.model.Job.user == trans.user )
+        state = kwd.get( 'state', None )
+        query = trans.sa_session.query( trans.app.model.Job ).filter(
+            trans.app.model.Job.user == trans.user
+        )
         if state is not None:
-            if isinstance(state, basestring):
+            if isinstance( state, basestring ):
                 query = query.filter( trans.app.model.Job.state == state )
-            elif isinstance(state, list):
+            elif isinstance( state, list ):
                 t = []
                 for s in state:
                     t.append(  trans.app.model.Job.state == s )
@@ -58,7 +53,7 @@ class JobController( BaseAPIController, UsesHistoryDatasetAssociationMixin, Uses
         for job in query.order_by(
                 trans.app.model.Job.update_time.desc()
             ).all():
-            out.append( self.encode_all_ids( trans, job.to_dict('collection'), True) ) 
+            out.append( self.encode_all_ids( trans, job.to_dict( 'collection' ), True ) )
         return out
 
     @web.expose_api
@@ -78,20 +73,21 @@ class JobController( BaseAPIController, UsesHistoryDatasetAssociationMixin, Uses
             decoded_job_id = trans.security.decode_id(id)
         except:
             raise exceptions.ObjectAttributeInvalidException()
-        query = trans.sa_session.query(trans.app.model.Job).filter( 
+        query = trans.sa_session.query( trans.app.model.Job ).filter(
             trans.app.model.Job.user == trans.user,
-            trans.app.model.Job.id == decoded_job_id)
+            trans.app.model.Job.id == decoded_job_id
+        )
         job = query.first()
         if job is None:
             raise exceptions.ObjectNotFound()
-        return self.encode_all_ids( trans, job.to_dict('element'), True)
+        return self.encode_all_ids( trans, job.to_dict( 'element' ), True )
 
     @expose_api
     def create( self, trans, payload, **kwd ):
         raise NotImplementedError()
 
     @expose_api
-    def search(self, trans, payload, **kwd):
+    def search( self, trans, payload, **kwd ):
         """
         search( trans, payload )
         * POST /api/jobs/search:
@@ -104,96 +100,97 @@ class JobController( BaseAPIController, UsesHistoryDatasetAssociationMixin, Uses
         :rtype:     list
         :returns:   list of dictionaries containing summary job information of the jobs that match the requested job run
 
-        This method is designed to scan the list of previously run jobs and find records of jobs that had 
-        the exact some input parameters and datasets. This can be used to minimize the amount of repeated work, and simply 
+        This method is designed to scan the list of previously run jobs and find records of jobs that had
+        the exact some input parameters and datasets. This can be used to minimize the amount of repeated work, and simply
         recycle the old results.
         """
 
         tool_id = None
         if 'tool_id' in payload:
-            tool_id = payload.get('tool_id')
+            tool_id = payload.get( 'tool_id' )
         if tool_id is None:
-            raise exceptions.ObjectAttributeMissingException("No tool id")
+            raise exceptions.ObjectAttributeMissingException( "No tool id" )
 
         tool = trans.app.toolbox.get_tool( tool_id )
         if tool is None:
             raise exceptions.ObjectNotFound( "Requested tool not found" )
         if 'inputs' not in payload:
-            raise exceptions.ObjectAttributeMissingException("No inputs defined")
+            raise exceptions.ObjectAttributeMissingException( "No inputs defined" )
 
-        inputs = payload['inputs']
+        inputs = payload[ 'inputs' ]
 
         input_data = {}
         input_param = {}
         for k, v in inputs.items():
-            if isinstance(v,dict):
+            if isinstance( v, dict ):
                 if 'id' in v:
                     try:
-                        if 'src' not in v or v['src'] == 'hda':
+                        if 'src' not in v or v[ 'src' ] == 'hda':
                             dataset = self.get_dataset( trans, v['id'], check_ownership=False, check_accessible=True )
                         else:
                             dataset = self.get_library_dataset_dataset_association( trans, v['id'] )
                     except Exception, e:
                         return { "error" : str( e ) }
                     if dataset is None:
-                        raise exceptions.ObjectNotFound("Dataset %s not found" % (v['id']))
+                        raise exceptions.ObjectNotFound( "Dataset %s not found" % ( v[ 'id' ] ) )
                     input_data[k] = dataset.dataset_id
             else:
-                input_param[k] = json.dumps(v)
+                input_param[k] = json.dumps( v )
 
-        query = trans.sa_session.query( trans.app.model.Job ).filter( 
-            trans.app.model.Job.tool_id == tool_id, 
+        query = trans.sa_session.query( trans.app.model.Job ).filter(
+            trans.app.model.Job.tool_id == tool_id,
             trans.app.model.Job.user == trans.user
         )
 
         if 'state' not in payload:
             query = query.filter(
-                or_( 
-                    trans.app.model.Job.state == 'running', 
-                    trans.app.model.Job.state == 'queued', 
-                    trans.app.model.Job.state == 'waiting', 
+                or_(
                     trans.app.model.Job.state == 'running',
-                    trans.app.model.Job.state == 'ok',                  
+                    trans.app.model.Job.state == 'queued',
+                    trans.app.model.Job.state == 'waiting',
+                    trans.app.model.Job.state == 'running',
+                    trans.app.model.Job.state == 'ok',
                 )
             )
         else:
-            if isinstance(payload['state'], basestring):
-                query = query.filter( trans.app.model.Job.state == payload['state'] )
-            elif isinstance(payload['state'], list):
+            if isinstance( payload[ 'state' ], basestring ):
+                query = query.filter( trans.app.model.Job.state == payload[ 'state' ] )
+            elif isinstance( payload[ 'state' ], list ):
                 o = []
-                for s in payload['state']:
+                for s in payload[ 'state' ]:
                     o.append( trans.app.model.Job.state == s )
                 query = query.filter(
-                    or_(*o)
+                    or_( *o )
                 )
 
-        for k,v in input_param.items():
-            a = aliased(trans.app.model.JobParameter)
+        for k, v in input_param.items():
+            a = aliased( trans.app.model.JobParameter )
             query = query.filter( and_(
                 trans.app.model.Job.id == a.job_id,
                 a.name == k,
                 a.value == v
-            ))
+            ) )
 
-        for k,v in input_data.items():
+        for k, v in input_data.items():
             """
             Here we are attempting to link the inputs to the underlying dataset (not the dataset association)
             This way, if the calulation was done using a copied HDA (copied from the library or another history)
             the search will still find the job
             """
-            a = aliased(trans.app.model.JobToInputDatasetAssociation)
-            b = aliased(trans.app.model.HistoryDatasetAssociation)
+            a = aliased( trans.app.model.JobToInputDatasetAssociation )
+            b = aliased( trans.app.model.HistoryDatasetAssociation )
             query = query.filter( and_(
                 trans.app.model.Job.id == a.job_id,
                 a.dataset_id == b.id,
                 b.deleted == False,
                 b.dataset_id == v
-            ))
+            ) )
+
         out = []
         for job in query.all():
             """
             check to make sure none of the output files have been deleted
             """
-            if all(list(a.dataset.deleted == False for a in job.output_datasets)):
-                out.append( self.encode_all_ids( trans, job.to_dict('element'), True) )
+            if all( list( a.dataset.deleted == False for a in job.output_datasets ) ):
+                out.append( self.encode_all_ids( trans, job.to_dict( 'element' ), True ) )
         return out
