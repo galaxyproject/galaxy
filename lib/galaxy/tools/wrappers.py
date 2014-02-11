c87a0d7f4b89394b4f46171179b6d4ad32011695
John Chilton <jmchilton@gmail.com>
2014-02-10 21:34:42 -0600

    Move cheetah wrappers into own module... For clarity - more work toward reduces tools/__init__.py to a more managable size. This also includes an initial suite of test cases for these wrappers - testing simple select wrapper, select wrapper with file options, select wrapper with drilldown widget, raw object wrapper, input value wrapper, and the dataset file name wrapper with and without false paths.

---
+++ lib/galaxy/tools/wrappers.py
@@ -0,0 +1,207 @@
+import pipes
+from galaxy.util.none_like import NoneDataset
+
+
+class ToolParameterValueWrapper( object ):
+    """
+    Base class for object that Wraps a Tool Parameter and Value.
+    """
+
+    def __nonzero__( self ):
+        return bool( self.value )
+
+    def get_display_text( self, quote=True ):
+        """
+        Returns a string containing the value that would be displayed to the user in the tool interface.
+        When quote is True (default), the string is escaped for e.g. command-line usage.
+        """
+        rval = self.input.value_to_display_text( self.value, self.input.tool.app ) or ''
+        if quote:
+            return pipes.quote( rval ) or "''"  # pipes.quote in Python < 2.7 returns an empty string instead of the expected quoted empty string
+        return rval
+
+
+class RawObjectWrapper( ToolParameterValueWrapper ):
+    """
+    Wraps an object so that __str__ returns module_name:class_name.
+    """
+    def __init__( self, obj ):
+        self.obj = obj
+
+    def __nonzero__( self ):
+        return bool( self.obj )  # FIXME: would it be safe/backwards compatible to rename .obj to .value, so that we can just inherit this method?
+
+    def __str__( self ):
+        try:
+            return "%s:%s" % (self.obj.__module__, self.obj.__class__.__name__)
+        except:
+            #Most likely None, which lacks __module__.
+            return str( self.obj )
+
+    def __getattr__( self, key ):
+        return getattr( self.obj, key )
+
+
+class LibraryDatasetValueWrapper( ToolParameterValueWrapper ):
+    """
+    Wraps an input so that __str__ gives the "param_dict" representation.
+    """
+    def __init__( self, input, value, other_values={} ):
+        self.input = input
+        self.value = value
+        self._other_values = other_values
+        self.counter = 0
+
+    def __str__( self ):
+        return self.value
+
+    def __iter__( self ):
+        return self
+
+    def next( self ):
+        if self.counter >= len(self.value):
+            raise StopIteration
+        self.counter += 1
+        return self.value[ self.counter - 1 ]
+
+    def __getattr__( self, key ):
+        return getattr( self.value, key )
+
+
+class InputValueWrapper( ToolParameterValueWrapper ):
+    """
+    Wraps an input so that __str__ gives the "param_dict" representation.
+    """
+    def __init__( self, input, value, other_values={} ):
+        self.input = input
+        self.value = value
+        self._other_values = other_values
+
+    def __str__( self ):
+        return self.input.to_param_dict_string( self.value, self._other_values )
+
+    def __getattr__( self, key ):
+        return getattr( self.value, key )
+
+
+class SelectToolParameterWrapper( ToolParameterValueWrapper ):
+    """
+    Wraps a SelectTooParameter so that __str__ returns the selected value, but all other
+    attributes are accessible.
+    """
+
+    class SelectToolParameterFieldWrapper:
+        """
+        Provide access to any field by name or index for this particular value.
+        Only applicable for dynamic_options selects, which have more than simple 'options' defined (name, value, selected).
+        """
+        def __init__( self, input, value, other_values ):
+            self._input = input
+            self._value = value
+            self._other_values = other_values
+            self._fields = {}
+
+        def __getattr__( self, name ):
+            if name not in self._fields:
+                self._fields[ name ] = self._input.options.get_field_by_name_for_value( name, self._value, None, self._other_values )
+            return self._input.separator.join( map( str, self._fields[ name ] ) )
+
+    def __init__( self, input, value, app, other_values={} ):
+        self.input = input
+        self.value = value
+        self.input.value_label = input.value_to_display_text( value, app )
+        self._other_values = other_values
+        self.fields = self.SelectToolParameterFieldWrapper( input, value, other_values )
+
+    def __str__( self ):
+        return self.input.to_param_dict_string( self.value, other_values=self._other_values )
+
+    def __getattr__( self, key ):
+        return getattr( self.input, key )
+
+
+class DatasetFilenameWrapper( ToolParameterValueWrapper ):
+    """
+    Wraps a dataset so that __str__ returns the filename, but all other
+    attributes are accessible.
+    """
+
+    class MetadataWrapper:
+        """
+        Wraps a Metadata Collection to return MetadataParameters wrapped
+        according to the metadata spec. Methods implemented to match behavior
+        of a Metadata Collection.
+        """
+        def __init__( self, metadata ):
+            self.metadata = metadata
+
+        def __getattr__( self, name ):
+            rval = self.metadata.get( name, None )
+            if name in self.metadata.spec:
+                if rval is None:
+                    rval = self.metadata.spec[name].no_value
+                rval = self.metadata.spec[name].param.to_string( rval )
+                # Store this value, so we don't need to recalculate if needed
+                # again
+                setattr( self, name, rval )
+            return rval
+
+        def __nonzero__( self ):
+            return self.metadata.__nonzero__()
+
+        def __iter__( self ):
+            return self.metadata.__iter__()
+
+        def get( self, key, default=None ):
+            try:
+                return getattr( self, key )
+            except:
+                return default
+
+        def items( self ):
+            return iter( [ ( k, self.get( k ) ) for k, v in self.metadata.items() ] )
+
+    def __init__( self, dataset, datatypes_registry=None, tool=None, name=None, false_path=None ):
+        if not dataset:
+            try:
+                # TODO: allow this to work when working with grouping
+                ext = tool.inputs[name].extensions[0]
+            except:
+                ext = 'data'
+            self.dataset = NoneDataset( datatypes_registry=datatypes_registry, ext=ext )
+        else:
+            self.dataset = dataset
+            self.metadata = self.MetadataWrapper( dataset.metadata )
+        self.false_path = false_path
+
+    def __str__( self ):
+        if self.false_path is not None:
+            return self.false_path
+        else:
+            return self.dataset.file_name
+
+    def __getattr__( self, key ):
+        if self.false_path is not None and key == 'file_name':
+            return self.false_path
+        else:
+            return getattr( self.dataset, key )
+
+    def __nonzero__( self ):
+        return bool( self.dataset )
+
+
+class DatasetListWrapper( list ):
+    """
+    """
+    def __init__( self, datasets, false_paths=[], **kwargs ):
+        if not isinstance(datasets, list):
+            datasets = [datasets]
+
+        def to_wrapper( dataset ):
+            real_path = dataset.file_name
+            wrapper_kwds = kwargs.copy()
+            if real_path in false_paths:
+                wrapper_kwds[ "false_path" ] = false_paths[ real_path ]
+            return DatasetFilenameWrapper( dataset, **wrapper_kwds )
+
+        list.__init__( self, map( to_wrapper, datasets ) )
---
+++ test/unit/tools/test_wrappers.py
@@ -0,0 +1,155 @@
+import tempfile
+import os
+
+from galaxy.tools.parameters.basic import SelectToolParameter
+from galaxy.tools.parameters.basic import DrillDownSelectToolParameter
+from galaxy.tools.parameters.basic import IntegerToolParameter
+from galaxy.tools.wrappers import RawObjectWrapper
+from galaxy.tools.wrappers import SelectToolParameterWrapper
+from galaxy.tools.wrappers import InputValueWrapper
+from galaxy.tools.wrappers import DatasetFilenameWrapper
+from galaxy.util.bunch import Bunch
+from elementtree.ElementTree import XML
+from galaxy.datatypes.metadata import MetadataSpecCollection
+
+
+def with_mock_tool(func):
+    def call():
+        test_directory = tempfile.mkdtemp()
+        app = MockApp(test_directory)
+        tool = MockTool(app)
+        return func(tool)
+    call.__name__ = func.__name__
+    return call
+
+
+@with_mock_tool
+def test_select_wrapper_simple_options(tool):
+    xml = XML('''<param name="blah" type="select">
+        <option value="x">I am X</option>
+        <option value="y" selected="true">I am Y</option>
+        <option value="z">I am Z</option>
+    </param>''')
+    parameter = SelectToolParameter( tool, xml )
+    wrapper = SelectToolParameterWrapper( parameter, "x", tool.app )
+    assert str(wrapper) == "x"
+    assert wrapper.name == "blah"
+    assert wrapper.value_label == "I am X"
+
+
+@with_mock_tool
+def test_select_wrapper_with_drilldown(tool):
+    parameter = _drilldown_parameter(tool)
+    wrapper = SelectToolParameterWrapper( parameter, ["option3"], tool.app )
+    assert str(wrapper) == "option3", str(wrapper)
+
+
+@with_mock_tool
+def test_select_wrapper_option_file(tool):
+    parameter = _setup_blast_tool(tool)
+    wrapper = SelectToolParameterWrapper( parameter, "val2", tool.app )
+    assert str(wrapper) == "val2"
+    assert wrapper.fields.name == "name2"
+    assert wrapper.fields.path == "path2"
+
+
+@with_mock_tool
+def test_select_wrapper_multiple(tool):
+    parameter = _setup_blast_tool(tool, multiple=True)
+    wrapper = SelectToolParameterWrapper( parameter, ["val1", "val2"], tool.app )
+    assert str(wrapper) == "val1,val2"
+    assert wrapper.fields.name == "name1,name2"
+
+
+def test_raw_object_wrapper():
+    obj = Bunch(x=4)
+    wrapper = RawObjectWrapper(obj)
+    assert wrapper.x == 4
+    assert wrapper
+
+    false_wrapper = RawObjectWrapper(False)
+    assert not false_wrapper
+
+
+@with_mock_tool
+def test_input_value_wrapper(tool):
+    parameter = IntegerToolParameter( tool, XML( '<param name="blah" type="integer" size="4" value="10" min="0" />' ) )
+    wrapper = InputValueWrapper( parameter, "5" )
+    assert str( wrapper ) == "5"
+
+
+def test_dataset_wrapper():
+    dataset = MockDataset()
+    wrapper = DatasetFilenameWrapper(dataset)
+    assert str( wrapper ) == MOCK_DATASET_PATH
+    assert wrapper.file_name == MOCK_DATASET_PATH
+
+    assert wrapper.ext == MOCK_DATASET_EXT
+
+
+def test_dataset_wrapper_false_path():
+    dataset = MockDataset()
+    new_path = "/new/path/dataset_123.dat"
+    wrapper = DatasetFilenameWrapper(dataset, false_path=new_path)
+    assert str( wrapper ) == new_path
+    assert wrapper.file_name == new_path
+
+
+def _drilldown_parameter(tool):
+    xml = XML( '''<param name="some_name" type="drill_down" display="checkbox" hierarchy="recurse" multiple="true">
+        <options>
+            <option name="Heading 1" value="heading1">
+                <option name="Option 1" value="option1"/>
+                <option name="Option 2" value="option2"/>
+                <option name="Heading 1" value="heading1">
+                    <option name="Option 3" value="option3"/>
+                    <option name="Option 4" value="option4"/>
+               </option>
+            </option>
+           <option name="Option 5" value="option5"/>
+      </options>
+    </param>''' )
+    parameter = DrillDownSelectToolParameter( tool, xml )
+    return parameter
+
+
+def _setup_blast_tool(tool, multiple=False):
+    tool.app.write_test_tool_data("blastdb.loc", "val1\tname1\tpath1\nval2\tname2\tpath2\n")
+    xml = XML( '''<param name="database" type="select" label="Nucleotide BLAST database" multiple="%s">
+        <options from_file="blastdb.loc">
+            <column name="value" index="0"/>
+            <column name="name" index="1"/>
+            <column name="path" index="2"/>
+        </options>
+    </param>''' % multiple )
+    parameter = SelectToolParameter( tool, xml )
+    return parameter
+
+
+MOCK_DATASET_PATH = "/galaxy/database/files/001/dataset_123.dat"
+MOCK_DATASET_EXT = "bam"
+
+
+class MockDataset(object):
+
+    def __init__(self):
+        self.metadata = MetadataSpecCollection({})
+        self.file_name = MOCK_DATASET_PATH
+        self.ext = MOCK_DATASET_EXT
+
+
+class MockTool(object):
+
+    def __init__(self, app):
+        self.app = app
+        self.options = Bunch(sanitize=False)
+
+
+class MockApp(object):
+
+    def __init__(self, test_directory):
+        self.config = Bunch(tool_data_path=test_directory)
+
+    def write_test_tool_data(self, name, contents):
+        path = os.path.join(self.config.tool_data_path, name)
+        open(path, "w").write(contents)
