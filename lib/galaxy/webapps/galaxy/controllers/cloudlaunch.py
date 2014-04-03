0a47048a496487a5e39be760229b370c177f4541
Dannon Baker <dannon.baker@gmail.com>
2014-04-03 15:23:52 -0400

    Incremental work / shifting changes to bioblend but committed here for posterity.

--- lib/galaxy/webapps/galaxy/controllers/cloudlaunch.py
+++ lib/galaxy/webapps/galaxy/controllers/cloudlaunch.py
@@ -3,10 +3,8 @@ Cloud Controller: handles all cloud interactions.
 
 Adapted from Brad Chapman and Enis Afgan's BioCloudCentral
 BioCloudCentral Source: https://github.com/chapmanb/biocloudcentral
-
 """
 
-import datetime
 import logging
 import os
 import tempfile
@@ -19,14 +17,19 @@ from galaxy.util.json import to_json_string
 
 eggs.require('boto')
 import boto
+import boto.vpc
 from boto.ec2.regioninfo import RegionInfo
 from boto.exception import EC2ResponseError, S3ResponseError
 from boto.s3.connection import OrdinaryCallingFormat, S3Connection
 
 log = logging.getLogger(__name__)
 
+logging.getLogger('boto').setLevel(logging.INFO)
+
+
 PKEY_PREFIX = 'gxy_pkey'
 DEFAULT_KEYPAIR = 'cloudman_keypair'
+CLOUDMAN_TAG_KEY = 'galaxy:cloudman'
 
 
 class CloudController(BaseUIController):
@@ -51,7 +54,6 @@ class CloudController(BaseUIController):
         """
         Get EC2 Account Info
         """
-        #Keypairs
         account_info = {}
         try:
             ec2_conn = connect_ec2(key_id, secret)
@@ -79,19 +81,7 @@ class CloudController(BaseUIController):
                 # This will also allow for much more sophisticated rendering of existing clusters
                 # Currently, this zone detection is a hack.
                 pd_contents = pd.get_contents_as_string()
-                zone = ''
-                try:
-                    for line in pd_contents.split('\n'):
-                        if 'vol_id' in line:
-                            vol_id = line.split(':')[1].strip()
-                            v = ec2_conn.get_all_volumes(volume_ids=[vol_id])
-                            if v:
-                                zone = v[0].zone
-                            else:
-                                zone = ''
-                except:
-                    #If anything goes wrong with zone detection, use the default selection.
-                    zone = ''
+                zone = get_zone_from_pd(pd_contents, ec2_conn)
                 for key in bucket.list():
                     if key.name.endswith('.clusterName'):
                         clusters.append({'name': key.name.split('.clusterName')[0],
@@ -107,60 +97,51 @@ class CloudController(BaseUIController):
         try:
             # Create security group & key pair used when starting an instance
             ec2_conn = connect_ec2(key_id, secret)
-            sg_name = create_cm_security_group(ec2_conn)
+            sg = get_or_create_cm_security_group(ec2_conn)
             kp_name, kp_material = create_key_pair(ec2_conn, key_name=keypair)
         except EC2ResponseError, err:
             ec2_error = err.error_message
         if ec2_error:
             trans.response.status = 400
             return ec2_error
+        #Get an elastic ip, add to UserData.
+        user_provided_data = _format_user_data(cluster_name, key_id, secret, password, share_string, bucket_default)
+        if not ami:
+            ami = trans.app.config.cloudlaunch_default_ami
+        rs = _run_instance(ec2_conn=ec2_conn,
+                           image_id=ami,
+                           user_provided_data=user_provided_data,
+                           key_name=kp_name,
+                           security_group_ids=[sg.id],
+                           placement=zone,
+                           instance_type=instance_type,
+                           subnet_id=get_or_create_cm_subnet(key_id, secret, zone).id)
+        if not rs:
+            trans.response.status = 400
+            return "Instance failure, but no specific error was detected.  Please check your AWS Console."
+        instance = rs.instances[0]
+        while not instance.public_dns_name:
+            try:
+                instance.update()
+            except EC2ResponseError:
+                # This can happen when update is invoked before the instance is fully registered.
+                pass
+            time.sleep(1)
+        if kp_material:
+            #We have created a keypair.  Save to tempfile for one time retrieval.
+            (fd, fname) = tempfile.mkstemp(prefix=PKEY_PREFIX, dir=trans.app.config.new_file_path)
+            f = os.fdopen(fd, 'wt')
+            f.write(kp_material)
+            f.close()
+            kp_material_tag = fname[fname.rfind(PKEY_PREFIX) + len(PKEY_PREFIX):]
         else:
-            user_provided_data = {'cluster_name': cluster_name,
-                                  'access_key': key_id,
-                                  'secret_key': secret,
-                                  'instance_type': instance_type}
-            if password:
-                user_provided_data['password'] = password
-            if share_string:
-                user_provided_data['share_string'] = share_string
-            if bucket_default:
-                user_provided_data['bucket_default'] = bucket_default
-            if not ami:
-                ami = trans.app.config.cloudlaunch_default_ami
-            rs = run_instance(ec2_conn=ec2_conn,
-                              image_id=ami,
-                              user_provided_data=user_provided_data,
-                              key_name=kp_name,
-                              security_groups=[sg_name],
-                              placement=zone)
-            if rs:
-                instance = rs.instances[0]
-                while not instance.public_dns_name:
-                    try:
-                        instance.update()
-                    except EC2ResponseError:
-                        #This can happen when update is invoked before the instance is fully registered.
-                        #Prevent failure, wait it out.
-                        pass
-                    time.sleep(1)
-                if kp_material:
-                    #We have created a keypair.  Save to tempfile for one time retrieval.
-                    (fd, fname) = tempfile.mkstemp(prefix=PKEY_PREFIX, dir=trans.app.config.new_file_path)
-                    f = os.fdopen(fd, 'wt')
-                    f.write(kp_material)
-                    f.close()
-                    kp_material_tag = fname[fname.rfind(PKEY_PREFIX) + len(PKEY_PREFIX):]
-                else:
-                    kp_material_tag = None
-                return to_json_string({'cluster_name': cluster_name,
-                                       'instance_id': rs.instances[0].id,
-                                       'image_id': rs.instances[0].image_id,
-                                       'public_dns_name': rs.instances[0].public_dns_name,
-                                       'kp_name': kp_name,
-                                       'kp_material_tag': kp_material_tag})
-            else:
-                trans.response.status = 400
-                return "Instance failure, but no specific error was detected.  Please check your AWS Console."
+            kp_material_tag = None
+        return to_json_string({'cluster_name': cluster_name,
+                               'instance_id': rs.instances[0].id,
+                               'image_id': rs.instances[0].image_id,
+                               'public_dns_name': rs.instances[0].public_dns_name,
+                               'kp_name': kp_name,
+                               'kp_material_tag': kp_material_tag})
 
     @web.expose
     def get_pkey(self, trans, kp_material_tag=None):
@@ -179,6 +160,21 @@ class CloudController(BaseUIController):
         return "Invalid identifier"
 
 
+def get_zone_from_pd(pd_contents, ec2_conn):
+    try:
+        for line in pd_contents.split('\n'):
+            if 'vol_id' in line:
+                vol_id = line.split(':')[1].strip()
+                v = ec2_conn.get_all_volumes(volume_ids=[vol_id])
+                if v:
+                    return v[0].zone
+    except:
+        # Things can go wrong with Zone detection outside of our control.  Use
+        # the default zone if this happens.
+        pass
+    return ''
+
+
 def connect_ec2(a_key, s_key):
     """
     Create and return an EC2 connection object.
@@ -193,7 +189,6 @@ def connect_ec2(a_key, s_key):
     r = RegionInfo(name=region_name, endpoint=region_endpoint)
     ec2_conn = boto.connect_ec2(aws_access_key_id=a_key,
                                 aws_secret_access_key=s_key,
-                                api_version='2011-11-01',  # needed for availability zone support
                                 is_secure=is_secure,
                                 region=r,
                                 port=ec2_port,
@@ -201,23 +196,22 @@ def connect_ec2(a_key, s_key):
     return ec2_conn
 
 
-def create_cm_security_group(ec2_conn, sg_name='CloudMan'):
-    """ Create a security group with all authorizations required to run CloudMan.
-        If the group already exists, check its rules and add the missing ones.
-        Return the name of the created security group.
+def get_or_create_cm_security_group(ec2_conn, sg_name='CloudManVPC'):
     """
-    cmsg = None
-    # Check if this security group already exists
-    sgs = ec2_conn.get_all_security_groups()
-    for sg in sgs:
-        if sg.name == sg_name:
-            cmsg = sg
-            log.debug("Security group '%s' already exists; will add authorizations next." % sg_name)
-            break
-    # If it does not exist, create security group
-    if cmsg is None:
+    Create a security group with all authorizations required to run CloudMan.
+    If the group already exists, check its rules and add the missing ones.
+    Return the name of the created security group.
+    """
+    cm_sg = None
+    # Check if this security group already exists.  We can't use the built-in
+    # filtering here, because VPC security groups can't be filtered by name.
+    sg = ec2_conn.get_all_security_groups(filters={'group_name': sg_name})
+    if sg:
+        cm_sg = sg[0]
+    else:
+        # If it does not exist, create security group
         log.debug("Creating Security Group %s" % sg_name)
-        cmsg = ec2_conn.create_security_group(sg_name, 'A security group for CloudMan')
+        cm_sg = ec2_conn.create_security_group(sg_name, 'A security group for CloudMan', vpc_id=get_or_create_cm_vpc(ec2_conn.access_key, ec2_conn.secret_key).id)
     # Add appropriate authorization rules
     # If these rules already exist, nothing will be changed in the SG
     ports = (('80', '80'),  # Web UI
@@ -226,29 +220,68 @@ def create_cm_security_group(ec2_conn, sg_name='CloudMan'):
              ('30000', '30100'),  # FTP transfer
              ('42284', '42284'))  # CloudMan UI
     for port in ports:
-        try:
-            if not rule_exists(cmsg.rules, from_port=port[0], to_port=port[1]):
-                cmsg.authorize(ip_protocol='tcp', from_port=port[0], to_port=port[1], cidr_ip='0.0.0.0/0')
-            else:
-                log.debug("Rule (%s:%s) already exists in the SG" % (port[0], port[1]))
-        except EC2ResponseError, e:
-            log.error("A problem with security group authorizations: %s" % e)
+        if not rule_exists(cm_sg.rules, from_port=port[0], to_port=port[1]):
+            cm_sg.authorize(ip_protocol='tcp', from_port=port[0], to_port=port[1], cidr_ip='0.0.0.0/0')
+        else:
+            log.debug("Rule (%s:%s) already exists in the SG" % (port[0], port[1]))
     # Add rule that allows communication between instances in the same SG
     g_rule_exists = False  # Flag to indicate if group rule already exists
-    for rule in cmsg.rules:
-        for grant in rule.grants:
-            if grant.name == cmsg.name:
+    for rule in cm_sg.rules:
+        for grant in rule.grants:#[g for g in rule.grants if g.name == cm_sg.name]:
+            print grant.name
+            if grant.name == cm_sg.name:
                 g_rule_exists = True
                 log.debug("Group rule already exists in the SG")
         if g_rule_exists:
             break
-    if g_rule_exists is False:
-        try:
-            cmsg.authorize(src_group=cmsg)
-        except EC2ResponseError, e:
-            log.error("A problem w/ security group authorization: %s" % e)
-    log.info("Done configuring '%s' security group" % cmsg.name)
-    return cmsg.name
+    #if g_rule_exists is False:
+    #    cm_sg.authorize(src_group=cm_sg)
+    log.info("Done configuring '%s' security group" % cm_sg.name)
+    return cm_sg
+
+
+
+def get_or_create_cm_vpc(a_key, s_key):
+    vpc_conn = boto.vpc.VPCConnection(a_key, s_key)
+    vpcs = vpc_conn.get_all_vpcs()
+    cloudman_vpc = None
+    for vpc in vpcs:
+        if CLOUDMAN_TAG_KEY in vpc.tags:
+            cloudman_vpc = vpc
+    if not cloudman_vpc:
+        #create a new one.
+        cloudman_vpc = vpc_conn.create_vpc('10.0.0.0/16')
+        cloudman_vpc.add_tag(CLOUDMAN_TAG_KEY, '')
+    return cloudman_vpc
+
+
+def get_or_create_cm_subnet(a_key, s_key, availability_zone):
+    log.debug("GETTING SUBNET in %s" % availability_zone)
+    vpc_conn = boto.vpc.VPCConnection(a_key, s_key)
+    subnets = vpc_conn.get_all_subnets()
+    cloudman_subnet = None
+    for subnet in subnets:
+        log.debug("SN: %s, SNAZ:AZ: %s:%s:%s" % (subnet, subnet.availability_zone, availability_zone, subnet.availability_zone == availability_zone))
+        if (not availability_zone or subnet.availability_zone == availability_zone) and CLOUDMAN_TAG_KEY in subnet.tags:
+            cloudman_subnet = subnet
+    if not cloudman_subnet:
+        #create a new one.
+        cloudman_subnet = vpc_conn.create_subnet(get_or_create_cm_vpc(a_key, s_key).id, '10.0.0.0/24', availability_zone)
+        cloudman_subnet.add_tag(CLOUDMAN_TAG_KEY, '')
+    return cloudman_subnet
+
+
+def _format_user_data(cluster_name, key_id, secret, password, share_string, bucket_default):
+    user_provided_data = {'cluster_name': cluster_name,
+                          'access_key': key_id,
+                          'secret_key': secret}
+    if password:
+        user_provided_data['password'] = password
+    if share_string:
+        user_provided_data['share_string'] = share_string
+    if bucket_default:
+        user_provided_data['bucket_default'] = bucket_default
+    return "\n".join(['%s: %s' % (key, value) for key, value in user_provided_data.iteritems() if key != 'kp_material'])
 
 
 def rule_exists(rules, from_port, to_port, ip_protocol='tcp', cidr_ip='0.0.0.0/0'):
@@ -282,24 +315,22 @@ def create_key_pair(ec2_conn, key_name=DEFAULT_KEYPAIR):
     return kp.name, kp.material
 
 
-def run_instance(ec2_conn, user_provided_data, image_id=None,
-                 kernel_id=None, ramdisk_id=None, key_name=DEFAULT_KEYPAIR,
-                 placement=None, security_groups=['CloudMan']):
-    """ Start an instance. If instance start was OK, return the ResultSet object
-        else return None.
+def _run_instance(ec2_conn, user_provided_data, instance_type, image_id=None,
+                  kernel_id=None, ramdisk_id=None, key_name=DEFAULT_KEYPAIR,
+                  placement=None, security_group_ids=None, subnet_id=None):
+    """
+    Start an instance. If instance start was OK, return the ResultSet object
+    else return None.
     """
     rs = None
-    instance_type = user_provided_data['instance_type']
-    # Remove 'instance_type' key from the dict before creating user data
-    del user_provided_data['instance_type']
-    ud = "\n".join(['%s: %s' % (key, value) for key, value in user_provided_data.iteritems() if key != 'kp_material'])
     try:
         rs = ec2_conn.run_instances(image_id=image_id,
                                     instance_type=instance_type,
                                     key_name=key_name,
-                                    security_groups=security_groups,
-                                    user_data=ud,
+                                    security_group_ids=security_group_ids,
+                                    user_data=user_provided_data,
                                     kernel_id=kernel_id,
+                                    subnet_id=subnet_id,
                                     ramdisk_id=ramdisk_id,
                                     placement=placement)
     except EC2ResponseError, e:
@@ -312,23 +343,3 @@ def run_instance(ec2_conn, user_provided_data, image_id=None,
     else:
         log.warning("Problem starting an instance?")
     return rs
-
-
-def _find_placement(ec2_conn, instance_type):
-    """Find a region zone that supports our requested instance type.
-
-    We need to check spot prices in the potential availability zones
-    for support before deciding on a region:
-
-    http://blog.piefox.com/2011/07/ec2-availability-zones-and-instance.html
-    """
-    base = ec2_conn.region.name
-    yesterday = datetime.datetime.now() - datetime.timedelta(1)
-    for loc_choice in ["b", "a", "c", "d"]:
-        cur_loc = "{base}{ext}".format(base=base, ext=loc_choice)
-        if len(ec2_conn.get_spot_price_history(instance_type=instance_type,
-                                               end_time=yesterday.isoformat(),
-                                               availability_zone=cur_loc)) > 0:
-            return cur_loc
-    log.error("Did not find availabilty zone in {0} for {1}".format(base, instance_type))
-    return None
