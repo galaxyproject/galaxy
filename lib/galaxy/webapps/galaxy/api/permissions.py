54aac81f4a77000e3817570d5e6c5a65d1499e46
John Chilton <jmchilton@gmail.com>
2014-04-17 13:46:57 -0500

    Fix library permissions API controller.

--- lib/galaxy/webapps/galaxy/api/permissions.py
+++ lib/galaxy/webapps/galaxy/api/permissions.py
@@ -47,6 +47,6 @@ class PermissionsController( BaseAPIController ):
         trans.app.security_agent.copy_library_permissions( trans, library, library.root_folder )
         message = "Permissions updated for library '%s'." % library.name
 
-        item = library.to_dict( view='element' )
+        item = library.to_dict( view='element', value_mapper={ 'id' : trans.security.encode_id , 'root_folder_id' : trans.security.encode_id } )
         return item
 
