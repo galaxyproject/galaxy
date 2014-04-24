24e3e410de8b8320cb50a8c2d8a0aa983c80693f
John Chilton <jmchilton@gmail.com>
2014-04-23 20:33:41 -0500

    Fix Python 3 incompat in galaxy.jobs.runners.util.
    Module shared with LWR so hoping for Python 3 compat.

--- lib/galaxy/jobs/runners/util/__init__.py
+++ lib/galaxy/jobs/runners/util/__init__.py
@@ -3,7 +3,5 @@ This module and its submodules contains utilities for running external
 processes and interfacing with job managers. This module should contain
 functionality shared between Galaxy and the LWR.
 """
-try:
-    from galaxy.util.bunch import Bunch
-except ImportError:
-    from lwr.util import Bunch
+
+from galaxy.util.bunch import Bunch
