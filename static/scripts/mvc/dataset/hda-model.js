e6e10e46d83241bbc1bbd8ee05e3e732ba16b9c8
John Chilton <jmchilton@gmail.com>
2014-05-06 08:54:31 -0500

    Dataset collections - backbone model for history content.

--- static/scripts/mvc/dataset/hda-model.js
+++ static/scripts/mvc/dataset/hda-model.js
@@ -447,6 +447,8 @@ var HDACollection = Backbone.Collection.extend( baseMVC.LoggableMixin ).extend(
     model : function( attrs, options ) {
         if( attrs.history_content_type == "dataset" ) {
             return new HistoryDatasetAssociation( attrs, options );
+        } else if( attrs.history_content_type == "dataset_collection" ) {
+            return new HistoryDatasetCollectionAssociation( attrs, options );
         } else {
             // TODO: Handle unknown history_content_type...
         }
@@ -696,6 +698,57 @@ var HDACollection = Backbone.Collection.extend( baseMVC.LoggableMixin ).extend(
          return ([ 'HDACollection(', [ this.historyId, this.length ].join(), ')' ].join( '' ));
     }
 });
+
+var HistoryDatasetCollectionAssociation = HistoryContent.extend(
+/** @lends HistoryDatasetCollectionAssociation.prototype */{
+    /** default attributes for a model */
+    defaults : {
+        // parent (containing) history
+        history_id          : null,
+        // often used with tagging
+        model_class         : 'HistoryDatasetCollectionAssociation',
+        history_content_type : 'dataset_collection',
+        hid                 : 0,
+        
+        id                  : null,
+        name                : '(unnamed dataset collection)',
+        // one of HistoryDatasetAssociation.STATES, calling them all 'ok' for now.
+        state               : 'ok',
+
+        accessible          : true,
+        deleted             : false,
+        visible             : true,
+
+        purged              : false, // Purged doesn't make sense for collections - at least right now.
+
+        tags                : [],
+        annotation          : ''
+    },
+    urls : function(){
+    },
+
+    inReadyState : function(){
+        return true; // TODO
+    },
+
+    // ........................................................................ search
+    /** what attributes of an collection will be used in a text search */
+    searchAttributes : [
+        'name'
+    ],
+
+    /** our attr keys don't often match the labels we display to the user - so, when using
+     *      attribute specifiers ('name="bler"') in a term, allow passing in aliases for the
+     *      following attr keys.
+     */
+    searchAliases : {
+        title       : 'name'
+        // TODO: Add tag...
+    },
+
+});
+
+
 
 //==============================================================================
 return {
