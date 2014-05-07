4ad5b570a1f8f8be0b253130fa808a11f4682326
Martin Cech <cech.marten@gmail.com>
2014-05-07 14:23:15 -0400

    allow JS modal to execute callbacks on hide()

--- static/scripts/mvc/ui/ui-modal.js
+++ static/scripts/mvc/ui/ui-modal.js
@@ -7,12 +7,13 @@ var View = Backbone.View.extend({
     
     // defaults options
     optionsDefault: {
-        title           : 'ui-modal',
-        body            : '',
-        backdrop        : true,
-        height          : null,
-        width           : null,
-        closing_events  : false
+        title            : 'ui-modal',
+        body             : '',
+        backdrop         : true,
+        height           : null,
+        width            : null,
+        closing_events   : false,
+        closing_callback : null
     },
 
     // button list
@@ -58,6 +59,9 @@ var View = Backbone.View.extend({
     hide: function() {
         this.visible = false;
         this.$el.fadeOut('fast');
+        if (this.options.closing_callback){
+            this.options.closing_callback();
+        }
     },
 
     // enable buttons
