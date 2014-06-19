c549cee921efec488f74c1350580b08c868b90d5
John Chilton <jmchilton@gmail.com>
2014-06-19 12:36:15 -0500

    LWR client updates - mostly MQ improvements. Updated LWR runner with spec parsing for new connection "ensure" parameters.

--- lib/galaxy/jobs/runners/lwr_client/amqp_exchange.py
+++ lib/galaxy/jobs/runners/lwr_client/amqp_exchange.py
@@ -19,6 +19,9 @@ DEFAULT_EXCHANGE_TYPE = "direct"
 DEFAULT_TIMEOUT = 0.2
 DEFAULT_HEARTBEAT = 580
 
+DEFAULT_RECONNECT_CONSUMER_WAIT = 1
+DEFAULT_HEARTBEAT_WAIT = 1
+
 
 class LwrExchange(object):
     """ Utility for publishing and consuming structured LWR queues using kombu.
@@ -51,6 +54,8 @@ class LwrExchange(object):
         self.__exchange = kombu.Exchange(DEFAULT_EXCHANGE_NAME, DEFAULT_EXCHANGE_TYPE)
         self.__timeout = timeout
         self.__publish_kwds = publish_kwds
+        if "retry_policy" in self.__publish_kwds:
+            self.__publish_kwds["retry_policy"]["errback"] = self.__publish_errback
 
     @property
     def url(self):
@@ -75,12 +80,13 @@ class LwrExchange(object):
                 log.warning('Got %s, will retry: %s', exc.__class__.__name__, exc)
                 if heartbeat_thread:
                     heartbeat_thread.join()
+                sleep(DEFAULT_RECONNECT_CONSUMER_WAIT)
 
     def heartbeat(self, connection):
         log.debug('AMQP heartbeat thread alive')
         while connection.connected:
             connection.heartbeat_check()
-            sleep(1)
+            sleep(DEFAULT_HEARTBEAT_WAIT)
         log.debug('AMQP heartbeat thread exiting')
 
     def publish(self, name, payload):
@@ -95,6 +101,10 @@ class LwrExchange(object):
                     routing_key=key,
                     **self.__publish_kwds
                 )
+
+    def __publish_errback(self, exc, interval):
+        log.error("Connection error while publishing: %r", exc, exc_info=1)
+        log.info("Retrying in %s seconds", interval)
 
     def connection(self, connection_string, **kwargs):
         if "ssl" not in kwargs:
