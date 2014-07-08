d3119bc4bb67fa7b92638c84f643621b6b26c712
Jeremy Goecks <jgoecks@gwu.edu>
2014-07-08 17:10:58 -0400

    Trackster: move icon rendering into Backbone view.

--- static/scripts/viz/trackster/tracks.js
+++ static/scripts/viz/trackster/tracks.js
@@ -194,10 +194,82 @@ var TrackHeaderView = Backbone.View.extend({
         this.$el.append($("<div/>").addClass(this.model.drag_handle_class));
         this.$el.append($("<div/>").addClass("track-name")
                                    .text(this.model.config.get_value('name')));
+
+        // Icons container.
+        this.action_icons = {};
+        this.render_action_icons();
+                
+        this.$el.append( $("<div style='clear: both'/>") );
+        
+        // Suppress double clicks in header so that they do not impact viz under header.
+        this.$el.dblclick( function(e) { e.stopPropagation(); } );
+        
+        // Needed for floating elts in header.
+        $("<div style='clear: both'/>").appendTo(this.container_div);
     },
 
     update_name: function() {
         this.$el.find('.track-name').text(this.model.config.get_value('name'));
+    },
+
+    render_action_icons: function() {
+        var self = this;
+        this.icons_div = $("<div/>").addClass('track-icons').hide().appendTo(this.$el);
+        _.each(this.model.action_icons_def, function(icon_dict) {
+            self.add_action_icon(icon_dict.name, icon_dict.title, icon_dict.css_class, 
+                                 icon_dict.on_click_fn, icon_dict.prepend, icon_dict.hide);
+        });
+        
+        // Set up behavior for modes popup.
+        this.set_display_modes(this.model.display_modes);
+    },
+
+    /**
+     * Add an action icon to this object. Appends icon unless prepend flag is specified.
+     */
+    add_action_icon: function(name, title, css_class, on_click_fn, prepend, hide) {
+        var self = this;
+        this.action_icons[name] = $("<a/>").attr("title", title)
+                                           .addClass("icon-button").addClass(css_class).tooltip()
+                                           .click( function() { on_click_fn(self.model); } )
+                                           .appendTo(this.icons_div);
+        if (hide) {
+            this.action_icons[name].hide();
+        }
+    },
+
+    /**
+     * Set track's modes and update mode icon popup.
+     */
+    set_display_modes: function(new_modes, init_mode) {
+        if (!new_modes) { return; }
+
+        // HACK: move this out of view and into track.
+
+        // Set modes, init mode.
+        this.model.display_modes = new_modes;
+        this.model.mode = (init_mode || this.model.config.get_value('mode') || this.model.display_modes[0]);
+        
+        this.action_icons.mode_icon.attr("title", "Set display mode (now: " + this.mode + ")");
+
+        // Setup popup menu for changing modes.
+        var self = this,
+            track = this.model,
+            mode_mapping = {};
+        for (var i = 0, len = track.display_modes.length; i < len; i++) {
+            var mode = track.display_modes[i];
+            mode_mapping[mode] = function(mode) {
+                return function() { 
+                    track.change_mode(mode);
+                    // HACK: the popup menu messes with the track's hover event, so manually show/hide
+                    // icons div for now.
+                    //self.icons_div.show(); 
+                    //track.container_div.mouseleave(function() { track.icons_div.hide(); } ); 
+                };
+            }(mode);
+        }
+
+        make_popupmenu(this.action_icons.mode_icon, mode_mapping);
     }
 });
 
@@ -254,24 +326,13 @@ var Drawable = function(view, container, obj_dict) {
             
         this.header_div = header_view.$el;
         this.container_div.append(this.header_div);
-        
-        // Icons container.
-        this.icons_div = $("<div/>").addClass('track-icons').hide().appendTo(this.header_div);
-        this.build_action_icons(this.action_icons_def);
-                
-        this.header_div.append( $("<div style='clear: both'/>") );
-        
-        // Suppress double clicks in header so that they do not impact viz.
-        this.header_div.dblclick( function(e) { e.stopPropagation(); } );
-        
+
         // Show icons when users is hovering over track.
-        var drawable = this;
+        var icons_div = header_view.icons_div;
+        this.action_icons = header_view.action_icons;
         this.container_div.hover(
-            function() { drawable.icons_div.show(); }, function() { drawable.icons_div.hide(); }
+            function() { icons_div.show(); }, function() { icons_div.hide(); }
         );
-       
-        // Needed for floating elts in header.
-        $("<div style='clear: both'/>").appendTo(this.container_div);
     }
 };
 
@@ -389,33 +450,6 @@ extend(Drawable.prototype, {
      * Build drawable's container div; this is the parent div for all drawable's elements.
      */
     build_container_div: function() {},
-
-    /**
-     * Add an action icon to this object. Appends icon unless prepend flag is specified.
-     */
-    add_action_icon: function(name, title, css_class, on_click_fn, prepend, hide) {
-        var drawable = this;
-        this.action_icons[name] = $("<a/>").attr("title", title)
-                                           .addClass("icon-button").addClass(css_class).tooltip()
-                                           .click( function() { on_click_fn(drawable); } )
-                                           .appendTo(this.icons_div);
-        if (hide) {
-            this.action_icons[name].hide();
-        }
-    },
-
-    /**
-     * Build drawable's icons div from object's icons_dict.
-     */
-    build_action_icons: function(action_icons_def) {        
-        // Create icons.
-        var icon_dict;
-        for (var i = 0; i < action_icons_def.length; i++) {
-            icon_dict = action_icons_def[i];
-            this.add_action_icon(icon_dict.name, icon_dict.title, icon_dict.css_class, 
-                                 icon_dict.on_click_fn, icon_dict.prepend, icon_dict.hide);
-        }
-    },
     
     /**
      * Update icons.
@@ -2392,46 +2426,6 @@ extend(Track.prototype, Drawable.prototype, {
             track.config.set_value('height', track.visible_height_px);
             track.changed();
         }).appendTo(track.container_div);
-    },
-
-    /**
-     * Set track's modes and update mode icon popup.
-     */
-    set_display_modes: function(new_modes, init_mode) {
-        // Set modes, init mode.
-        this.display_modes = new_modes;
-        this.mode = (init_mode ? init_mode :
-                     (this.config && this.config.get_value('mode') ?
-                      this.config.get_value('mode') : this.display_modes[0])
-                    );
-        
-        this.action_icons.mode_icon.attr("title", "Set display mode (now: " + this.mode + ")");
-
-        // Setup popup menu for changing modes.
-        var track = this,
-            mode_mapping = {};
-        for (var i = 0, len = track.display_modes.length; i < len; i++) {
-            var mode = track.display_modes[i];
-            mode_mapping[mode] = function(mode) {
-                return function() { 
-                    track.change_mode(mode);
-                    // HACK: the popup menu messes with the track's hover event, so manually show/hide
-                    // icons div for now.
-                    track.icons_div.show(); 
-                    track.container_div.mouseleave(function() { track.icons_div.hide(); } ); };
-            }(mode);
-        }
-
-        make_popupmenu(this.action_icons.mode_icon, mode_mapping);
-    },
-
-    build_action_icons: function() {
-        Drawable.prototype.build_action_icons.call(this, this.action_icons_def);
-        
-        // Set up behavior for modes popup.
-        if (this.display_modes !== undefined) {
-            this.set_display_modes(this.display_modes);
-        }
     },
 
     /**
