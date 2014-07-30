ba16d0cf5ef1aa75c7ba96c8523634f412afb3bc
Ira Cooke <iracooke@gmail.com>
2014-07-31 09:59:53 +1000

    Fallback for sql whitelisting when sqlparse is not available

--- lib/galaxy/util/sqlite.py
+++ lib/galaxy/util/sqlite.py
@@ -1,6 +1,27 @@
 import sqlite3
-import sqlparse
 import re
+
+try:
+	import sqlparse
+	def is_read_only_query(query):
+		statements = sqlparse.parse(query)
+		for statement in statements:
+			if statement.get_type() != "SELECT":
+				return False
+		return True
+
+except ImportError:
+	# Without sqlparse we use a very weak regex check
+	def is_read_only_query(query):
+		if re.match("select ", query, re.IGNORECASE):
+			if re.search("^([^\"]|\"[^\"]*\")*?;", query) or re.search("^([^\']|\'[^\']*\')*?;", query):
+				return False
+			else:
+				return True
+		return False
+
+
+
 
 
 def connect(path):
@@ -9,9 +30,3 @@ def connect(path):
     return connection
 
 
-def is_read_only_query(query):
-	statements = sqlparse.parse(query)
-	for statement in statements:
-		if statement.get_type() != "SELECT":
-			return False
-	return True
