4d5f0deebf22413508afad62a5f4f63f86e6869e
John Chilton <jmchilton@gmail.com>
2014-08-07 09:35:43 -0400

    Relax BibTeX parsing and accept header to handle figshare DOIs. For instance 10.6084/m9.figshare.979190 reported by Michael Crusoe. JavaScript parser didn't like @data entries produced by figshare and when requesting the entry dx.doi.org returns a 403 response for type accept type text/bibliography, also allowing application/x-bibtex fixes this. Interestingly dx.doi.org throws 500 exceptions if application/x-bibtex is the only acceptable type or appears first for some other DOIs.

--- static/scripts/libs/bibtex.js
+++ static/scripts/libs/bibtex.js
@@ -93,6 +93,7 @@ function BibtexParser(arg0) {
     'booklet'           : 14,
     'masterthesis'      : 15,
     'conference'        : 16
+    ,'data'             : 999 // Galaxy MOD: Handle @data citations coming from figshare.
   }
   /** @private */ this.MACROS_        = {
     'jan'               : 'January',
