707f180cdc55f77d70431ead67e0333eb38d5f90
Aysam Guerler <aysam.guerler@gmail.com>
2014-08-22 14:32:07 -0400

    ToolForm: Implements conditionals

--- static/scripts/mvc/tools/tools-form.js
+++ static/scripts/mvc/tools/tools-form.js
@@ -103,8 +103,11 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
             // reset table
             this.table.delAll();
             
-            // reset list
-            this.list = [];
+            // reset field list
+            this.field_list = {};
+            
+            // reset sequential input definition list
+            this.inputs_sequential = {};
             
             // model
             var data = new Backbone.Model();
@@ -115,65 +118,93 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
             }
             
             // trigger change
-            for (var id in this.list) {
-                this.list[id].trigger('change');
+            for (var id in this.field_list) {
+                this.field_list[id].trigger('change');
             }
         },
         
         // add table row
         _add: function(inputs_def, data) {
-            // link this
-            var self = this;
-            
+            // identify field type
+            var type = inputs_def.type;
+            switch(type) {
+                // conditional field
+                case 'conditional':
+                    // add label to input definition root
+                    inputs_def.label = inputs_def.test_param.label;
+                
+                    // add id to input definition root
+                    inputs_def.name = inputs_def.test_param.name;
+                
+                    // build options field
+                    this._addRow('conditional', inputs_def, data);
+                    
+                    // add fields
+                    for (var i in inputs_def.cases) {
+                        var cases = inputs_def.cases[i];
+                        for (var j in cases.inputs) {
+                            // add element to form
+                            this._add(cases.inputs[j], data);
+                        }
+                    }
+                    break;
+                
+                // default single element row
+                default:
+                    this._addRow(type, inputs_def, data);
+            }
+        },
+        
+        // add table row
+        _addRow: function(field_type, inputs_def, data) {
             // get id
             var id = inputs_def.name;
             
             // field wrapper
             var field = null;
-            console.log(inputs_def);
-            // create select field
-            var type = inputs_def.type;
-            switch(type) {
+            
+            // identify field type
+            switch(field_type) {
                 // text input field
                 case 'text' :
-                    field = this.field_text(inputs_def, data);
+                    field = this._field_text(inputs_def, data);
                     break;
                     
                 // select field
                 case 'select' :
-                    field = this.field_select(inputs_def, data);
+                    field = this._field_select(inputs_def, data);
                     break;
                     
                 // radiobox field
                 case 'radiobutton' :
-                    field = this.field_radio(inputs_def, data);
+                    field = this._field_radio(inputs_def, data);
                     break;
                 
                 // dataset
                 case 'data':
-                    field = this.field_data(inputs_def, data);
+                    field = this._field_data(inputs_def, data);
                     break;
                 
                 // dataset column
                 case 'data_column':
-                    field = this.field_column(inputs_def, data);
+                    field = this._field_column(inputs_def, data);
                     break;
                 
                 // text area field
                 case 'textarea' :
-                    field = this.field_textarea(inputs_def, data);
+                    field = this._field_textarea(inputs_def, data);
                     break;
-
-                // default
-                default:
-                    field = new Ui.Input({
-                        id          : 'field-' + id,
-                        placeholder : inputs_def.placeholder,
-                        type        : inputs_def.type,
-                        onchange    : function() {
-                            data.set(id, field.value());
-                        }
-                    });
+                    
+                // conditional select field
+                case 'conditional':
+                    field = this._field_conditional(inputs_def, data);
+                    break;
+            }
+            
+            // check if field type was detected
+            if (!field) {
+                console.debug('tools-form::_addRow() : Unmatched field type (' + field_type + ').');
+                return;
             }
             
             // set value
@@ -182,8 +213,11 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
             }
             field.value(data.get(id));
             
-            // add list
-            this.list[id] = field;
+            // add to field list
+            this.field_list[id] = field;
+            
+            // add to input definition into sequential list
+            this.inputs_sequential[id] = inputs_def;
             
             // combine field and info
             var $input = $('<div/>');
@@ -206,7 +240,7 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
         },
         
         // text input field
-        field_text : function(inputs_def, data) {
+        _field_text : function(inputs_def, data) {
             var id = inputs_def.name;
             return new Ui.Input({
                 id          : 'field-' + id,
@@ -218,7 +252,7 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
         },
         
         // text area
-        field_textarea : function(inputs_def, data) {
+        _field_textarea : function(inputs_def, data) {
             var id = inputs_def.name;
             return new Ui.Textarea({
                 id          : 'field-' + id,
@@ -228,8 +262,72 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
             });
         },
         
+        // conditional input field
+        _field_conditional : function(inputs_def, data) {
+            // link this
+            var self = this;
+
+            // configure options fields
+            var options = [];
+            for (var i in inputs_def.test_param.options) {
+                var option = inputs_def.test_param.options[i];
+                options.push({
+                    label: option[0],
+                    value: option[1]
+                });
+            }
+            
+            // select field
+            var id = inputs_def.name;
+            return new Ui.Select.View({
+                id          : 'field-' + id,
+                data        : options,
+                value       : data.get(id),
+                onchange    : function(value) {
+                    // update value
+                    data.set(id, value);
+                    
+                    // recursively hide/show options
+                    function _updateVisibility(root, show) {
+                        // loop through all inputs
+                        for (var i in root.inputs) {
+                            // get input element
+                            var element = root.inputs[i];
+                            
+                            // identify element name
+                            var element_id = element.name;
+                            
+                            // identify row
+                            var element_row = self.table.get(element_id);
+                            
+                            // show/hide row
+                            if (show) {
+                                element_row.fadeIn();
+                            } else {
+                                element_row.hide();
+                            }
+                            
+                            // check if its a nested conditional
+                            if (element.type == 'conditional') {
+                                // identify all cases
+                                for (var j in element.cases) {
+                                    _updateVisibility(element.cases[j], show);
+                                }
+                            }
+                        }
+                    }
+                    
+                    // check value in order to hide/show options
+                    for (var i in inputs_def.cases) {
+                        var case_def = inputs_def.cases[i];
+                        _updateVisibility(case_def, case_def.value == value);
+                    }
+                }
+            });
+        },
+        
         // data input field
-        field_data : function(inputs_def, data) {
+        _field_data : function(inputs_def, data) {
             // link this
             var self = this;
             
@@ -248,12 +346,6 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
                 });
             }
             
-            // find referenced columns
-            var column_list = _.where(this.inputs, {
-                data_ref    : id,
-                type        : 'data_column'
-            });
-            
             // select field
             return new Ui.Select.View({
                 id          : 'field-' + id,
@@ -262,6 +354,12 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
                 onchange    : function(value) {
                     // update value
                     data.set(id, value);
+                    
+                    // find referenced columns
+                    var column_list = _.where(self.inputs_sequential, {
+                        data_ref    : id,
+                        type        : 'data_column'
+                    });
                     
                     // find selected dataset
                     var dataset = self.datasets.filter(value);
@@ -291,7 +389,7 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
                 
                         // update referenced columns
                         for (var i in column_list) {
-                            var column_field = self.list[column_list[i].name]
+                            var column_field = self.field_list[column_list[i].name]
                             if (column_field) {
                                 column_field.update(columns);
                                 column_field.value(column_field.first());
@@ -306,7 +404,7 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
         },
         
         // select field
-        field_column : function (inputs_def, data) {
+        _field_column : function (inputs_def, data) {
             // configure options fields
             var options = [];
             for (var i in inputs_def.options) {
@@ -330,7 +428,7 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
         },
 
         // select field
-        field_select : function (inputs_def, data) {
+        _field_select : function (inputs_def, data) {
             // configure options fields
             var options = [];
             for (var i in inputs_def.options) {
@@ -360,7 +458,7 @@ define(['mvc/ui/ui-portlet', 'mvc/ui/ui-table', 'mvc/ui/ui-misc',
         },
         
         // radio field
-        field_radio : function(inputs_def, data) {
+        _field_radio : function(inputs_def, data) {
             var id = inputs_def.name;
             return new Ui.RadioButton({
                 id          : 'field-' + id,
