c7d947d5f362a75abd90abbe58ece62cc88bd037
John Chilton <jmchilton@gmail.com>
2014-09-11 16:01:08 -0400

    Refactor generic controller mixin code for security out for reuse in managers.

--- lib/galaxy/managers/base.py
+++ lib/galaxy/managers/base.py
@@ -1,7 +1,41 @@
+from galaxy import exceptions
 
 
 class ModelManager( object ):
     pass
 
+
 class ModelSerializer( object ):
     pass
+
+
+def security_check( trans, item, check_ownership=False, check_accessible=False ):
+    """ Security checks for an item: checks if (a) user owns item or (b) item
+    is accessible to user. This is a generic method for dealing with objects
+    uniformly from the older controller mixin code - however whenever possible
+    the managers for a particular model should be used to perform security
+    checks.
+    """
+
+    # all items are accessible to an admin
+    if trans.user_is_admin():
+        return item
+
+    # Verify ownership: there is a current user and that user is the same as the item's
+    if check_ownership:
+        if not trans.user:
+            raise exceptions.ItemOwnershipException( "Must be logged in to manage Galaxy items", type='error' )
+        if item.user != trans.user:
+            raise exceptions.ItemOwnershipException( "%s is not owned by the current user" % item.__class__.__name__, type='error' )
+
+    # Verify accessible:
+    #   if it's part of a lib - can they access via security
+    #   if it's something else (sharable) have they been added to the item's users_shared_with_dot_users
+    if check_accessible:
+        if type( item ) in ( trans.app.model.LibraryFolder, trans.app.model.LibraryDatasetDatasetAssociation, trans.app.model.LibraryDataset ):
+            if not trans.app.security_agent.can_access_library_item( trans.get_current_user_roles(), item, trans.user ):
+                raise exceptions.ItemAccessibilityException( "%s is not accessible to the current user" % item.__class__.__name__, type='error' )
+        else:
+            if ( item.user != trans.user ) and ( not item.importable ) and ( trans.user not in item.users_shared_with_dot_users ):
+                raise exceptions.ItemAccessibilityException( "%s is not accessible to the current user" % item.__class__.__name__, type='error' )
+    return item
--- lib/galaxy/web/base/controller.py
+++ lib/galaxy/web/base/controller.py
@@ -39,6 +39,7 @@ from galaxy.model import ExtendedMetadata, ExtendedMetadataIndex, LibraryDataset
 
 from galaxy.managers import api_keys
 from galaxy.managers import tags
+from galaxy.managers import base as managers_base
 from galaxy.datatypes.metadata import FileParameter
 from galaxy.tools.parameters import RuntimeValue, visit_input_values
 from galaxy.tools.parameters.basic import DataToolParameter
@@ -301,28 +302,7 @@ class SharableItemSecurityMixin:
 
     def security_check( self, trans, item, check_ownership=False, check_accessible=False ):
         """ Security checks for an item: checks if (a) user owns item or (b) item is accessible to user. """
-        # all items are accessible to an admin
-        if trans.user_is_admin():
-            return item
-
-        # Verify ownership: there is a current user and that user is the same as the item's
-        if check_ownership:
-            if not trans.user:
-                raise ItemOwnershipException( "Must be logged in to manage Galaxy items", type='error' )
-            if item.user != trans.user:
-                raise ItemOwnershipException( "%s is not owned by the current user" % item.__class__.__name__, type='error' )
-
-        # Verify accessible:
-        #   if it's part of a lib - can they access via security
-        #   if it's something else (sharable) have they been added to the item's users_shared_with_dot_users
-        if check_accessible:
-            if type( item ) in ( trans.app.model.LibraryFolder, trans.app.model.LibraryDatasetDatasetAssociation, trans.app.model.LibraryDataset ):
-                if not trans.app.security_agent.can_access_library_item( trans.get_current_user_roles(), item, trans.user ):
-                    raise ItemAccessibilityException( "%s is not accessible to the current user" % item.__class__.__name__, type='error' )
-            else:
-                if ( item.user != trans.user ) and ( not item.importable ) and ( trans.user not in item.users_shared_with_dot_users ):
-                    raise ItemAccessibilityException( "%s is not accessible to the current user" % item.__class__.__name__, type='error' )
-        return item
+        return managers_base.security_check( trans, item, check_ownership=check_ownership, check_accessible=check_accessible )
 
 
 class UsesHistoryMixin( SharableItemSecurityMixin ):
