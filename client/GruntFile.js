4753dd3b49c365e4add16193d94985e007c4f773
Carl Eberhard <carlfeberhard@gmail.com>
2014-09-17 11:28:16 -0400

    Client build system, pack_scripts: the build system will now call pack_scripts on all files in static/scripts when called via 'grunt' or 'grunt pack', and pack_scripts automatically only on those that have changed when using 'grunt watch'; add README.txt

--- client/GruntFile.js
+++ client/GruntFile.js
@@ -19,11 +19,31 @@ module.exports = function(grunt) {
       }
     },
 
+    // use 'grunt pack' to call pack_scripts.py to pack all or selected files in static/scripts
+    exec: {
+      packScripts: {
+        cwd: '../static/scripts',
+        target: [],
+        cmd: function(){
+          var targets = grunt.config( 'exec.packScripts.target' );
+          // if nothing was passed in pack all scripts
+          if( !targets.length ){
+            return './pack_scripts.py';
+          }
+
+          grunt.log.write( 'packing: ' + targets + '\n' );
+          return targets.map( function( target ){
+            return './pack_scripts.py ' + target;
+          }).join( '; ' );
+        }
+      }
+    },
+
     // use 'grunt watch' (from a new tab in your terminal) to have grunt re-copy changed files automatically
     watch: {
         // watch for changes in the src dir
         files: [ 'galaxy/scripts/**' ],
-        tasks: [ 'copy' ],
+        tasks: [ 'copy', 'pack' ],
         options: {
           spawn: false
         }
@@ -32,11 +52,20 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks( 'grunt-contrib-watch' );
   grunt.loadNpmTasks( 'grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-exec');
 
+  grunt.registerTask( 'pack', [ 'exec' ] );
+  grunt.registerTask( 'default', [ 'copy', 'pack' ] );
+
+  // -------------------------------------------------------------------------- copy,pack only those changed
   // adapted from grunt-contrib-watch jslint example
   //TODO: a bit hacky and there's prob. a better way
   //NOTE: copy will fail silently if a file isn't found
+
+  // outer scope variable for the event handler and onChange fn - begin with empty hash
   var changedFiles = Object.create(null);
+
+  // when files are changed, set the copy src and packScripts target to the filenames of the updated files
   var onChange = grunt.util._.debounce(function() {
     grunt.config( 'copy.main.files', [{
       expand: true,
@@ -44,8 +73,10 @@ module.exports = function(grunt) {
       src: Object.keys( changedFiles ),
       dest: '../static/scripts/'
     }]);
+    grunt.config( 'exec.packScripts.target', Object.keys( changedFiles ) );
     changedFiles = Object.create(null);
   }, 200);
+
   grunt.event.on('watch', function(action, filepath) {
     // store each filepath in a Files obj, the debounced fn above will use it as an aggregate list for copying
     // we need to take galaxy/scripts out of the filepath or it will be copied to the wrong loc
@@ -54,5 +85,4 @@ module.exports = function(grunt) {
     onChange();
   });
 
-  grunt.registerTask( 'default', [ 'copy' ] );
 };
