2035a1e92220054ee7f088b8c944e0931aa6cda1
Carl Eberhard <carlfeberhard@gmail.com>
2014-09-17 10:10:13 -0400

    Client build system, copy task: make much more efficient by calling 'copy' with just those files that have changed

--- client/GruntFile.js
+++ client/GruntFile.js
@@ -8,10 +8,14 @@ module.exports = function(grunt) {
     // use 'grunt copy' to copy the entire <galaxy>/client/galaxy/scripts dir into <galaxy>/static/scripts
     copy: {
       main: {
-        expand: true,
-        cwd: 'galaxy/scripts/',
-        src: '**',
-        dest: '../static/scripts/'
+        files: [
+          {
+            expand : true,
+            cwd : 'galaxy/scripts/',
+            src : '**',
+            dest : '../static/scripts'
+          }
+        ]
       }
     },
 
@@ -19,12 +23,36 @@ module.exports = function(grunt) {
     watch: {
         // watch for changes in the src dir
         files: [ 'galaxy/scripts/**' ],
-        tasks: [ 'default' ]
+        tasks: [ 'copy' ],
+        options: {
+          spawn: false
+        }
     }
   });
 
   grunt.loadNpmTasks( 'grunt-contrib-watch' );
   grunt.loadNpmTasks( 'grunt-contrib-copy');
+
+  // adapted from grunt-contrib-watch jslint example
+  //TODO: a bit hacky and there's prob. a better way
+  //NOTE: copy will fail silently if a file isn't found
+  var changedFiles = Object.create(null);
+  var onChange = grunt.util._.debounce(function() {
+    grunt.config( 'copy.main.files', [{
+      expand: true,
+      cwd: 'galaxy/scripts',
+      src: Object.keys( changedFiles ),
+      dest: '../static/scripts/'
+    }]);
+    changedFiles = Object.create(null);
+  }, 200);
+  grunt.event.on('watch', function(action, filepath) {
+    // store each filepath in a Files obj, the debounced fn above will use it as an aggregate list for copying
+    // we need to take galaxy/scripts out of the filepath or it will be copied to the wrong loc
+    filepath = filepath.replace( /galaxy\/scripts\//, '' );
+    changedFiles[filepath] = action;
+    onChange();
+  });
 
   grunt.registerTask( 'default', [ 'copy' ] );
 };
