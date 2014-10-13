3e5fab3bff94458b21c661d584c48d81c6118240
Dannon Baker <dannon.baker@gmail.com>
2014-10-13 12:55:31 -0400

    Fix Xslx binary (bad merge?).

--- lib/galaxy/datatypes/binary.py
+++ lib/galaxy/datatypes/binary.py
@@ -281,22 +281,6 @@ class Bam( Binary ):
             return "Binary bam alignments file (%s)" % ( data.nice_size( dataset.get_size() ) )
 
 
-class Xlsx(Binary):
-    """Class for Excel 2007 (xlsx) files"""
-    file_ext="xlsx"
-    def sniff( self, filename ):
-        # Xlsx is compressed in zip format and must not be uncompressed in Galaxy.
-        try:
-            if zipfile.is_zipfile( filename ):
-                tempzip = zipfile.ZipFile( filename )
-                if "[Content_Types].xml" in tempzip.namelist() and tempzip.read("[Content_Types].xml").find(b'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml') != -1:
-                    return True
-            return False
-        except:
-            return False
-
-Binary.register_sniffable_binary_format("xlsx", "xlsx", Xlsx)
-
 
     # ------------- Dataproviders
     # pipe through samtools view
@@ -638,4 +622,20 @@ class SQlite ( Binary ):
 
 
 Binary.register_sniffable_binary_format("sqlite", "sqlite", SQlite)
+
+class Xlsx(Binary):
+    """Class for Excel 2007 (xlsx) files"""
+    file_ext="xlsx"
+    def sniff( self, filename ):
+        # Xlsx is compressed in zip format and must not be uncompressed in Galaxy.
+        try:
+            if zipfile.is_zipfile( filename ):
+                tempzip = zipfile.ZipFile( filename )
+                if "[Content_Types].xml" in tempzip.namelist() and tempzip.read("[Content_Types].xml").find(b'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml') != -1:
+                    return True
+            return False
+        except:
+            return False
+
+Binary.register_sniffable_binary_format("xlsx", "xlsx", Xlsx)
 
