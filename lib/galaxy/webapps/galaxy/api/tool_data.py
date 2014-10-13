4848c980718356e55f8c7a9633064cc1e0734328
Kyle Ellrott <kellrott@gmail.com>
2014-10-13 15:48:50 -0700

    Removing unneeded imports and limiting access to admin for ToolData api

--- lib/galaxy/webapps/galaxy/api/tool_data.py
+++ lib/galaxy/webapps/galaxy/api/tool_data.py
@@ -1,16 +1,6 @@
 
-from galaxy import exceptions
 from galaxy import web, util
-from galaxy.web import _future_expose_api_anonymous
-from galaxy.web import _future_expose_api
 from galaxy.web.base.controller import BaseAPIController
-from galaxy.web.base.controller import UsesVisualizationMixin
-from galaxy.web.base.controller import UsesHistoryMixin
-from galaxy.visualization.genomes import GenomeRegion
-from galaxy.util.json import dumps
-from galaxy.visualization.data_providers.genome import *
-
-from galaxy.managers.collections_util import dictify_dataset_collection_instance
 
 
 class ToolData( BaseAPIController ):
@@ -18,6 +8,7 @@ class ToolData( BaseAPIController ):
     RESTful controller for interactions with tools.
     """
 
+    @web.require_admin
     @web.expose_api
     def index( self, trans, **kwds ):
         """
@@ -26,6 +17,7 @@ class ToolData( BaseAPIController ):
         """
         return list( a.to_dict() for a in trans.app.tool_data_tables.data_tables.values() )
 
+    @web.require_admin
     @web.expose_api
     def show( self, trans, id, **kwds ):
         return trans.app.tool_data_tables.data_tables[id].to_dict(view='element')
