6062f625ff5d1b3247a834d2c5fbb48f34576b1d
Aysam Guerler <aysam.guerler@gmail.com>
2014-11-24 13:36:24 -0500

    ToolForm: Adjust batch mode submission

--- client/galaxy/scripts/mvc/tools/tools-select-content.js
+++ client/galaxy/scripts/mvc/tools/tools-select-content.js
@@ -190,7 +190,7 @@ var View = Backbone.View.extend({
         
         // prepare result dict
         var result = {
-            batch   : !this.options.multiple,
+            batch   : !this.options.multiple && id_list.length > 1,
             values  : []
         }
         
