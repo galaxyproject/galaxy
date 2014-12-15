8e285594d743835acc0dbc558e25ff6107664e18
John Chilton <jmchilton@gmail.com>
2014-12-15 00:20:35 -0500

    Small improvements to tool data API. Update tool data API to use newer API decorator for better error handling, create some abstractions for reuse down stream, etc...

--- lib/galaxy/webapps/galaxy/api/tool_data.py
+++ lib/galaxy/webapps/galaxy/api/tool_data.py
@@ -1,4 +1,6 @@
+from galaxy import exceptions
 from galaxy import web
+from galaxy.web import _future_expose_api as expose_api
 from galaxy.web.base.controller import BaseAPIController
 
 
@@ -8,21 +10,21 @@ class ToolData( BaseAPIController ):
     """
 
     @web.require_admin
-    @web.expose_api
+    @expose_api
     def index( self, trans, **kwds ):
         """
         GET /api/tool_data: returns a list tool_data tables::
 
         """
-        return list( a.to_dict() for a in trans.app.tool_data_tables.data_tables.values() )
+        return list( a.to_dict() for a in self._data_tables.values() )
 
     @web.require_admin
-    @web.expose_api
+    @expose_api
     def show( self, trans, id, **kwds ):
-        return trans.app.tool_data_tables.data_tables[id].to_dict(view='element')
+        return self._data_table(id).to_dict(view='element')
 
     @web.require_admin
-    @web.expose_api
+    @expose_api
     def delete( self, trans, id, **kwd ):
         """
         DELETE /api/tool_data/{id}
@@ -61,3 +63,13 @@ class ToolData( BaseAPIController ):
             return "Invalid data table item ( %s ) specified. Wrong number of columns (%s given, %s required)." % ( str( values ), str(len(split_values)), str(len(data_table.get_column_name_list())))
 
         return data_table.remove_entry(split_values)
+
+    def _data_table( self, id ):
+        try:
+            return self._data_tables[id]
+        except IndexError:
+            raise exceptions.ObjectNotFound("No such data table %s" % id)
+
+    @property
+    def _data_tables( self ):
+        return self.app.tool_data_tables.data_tables
