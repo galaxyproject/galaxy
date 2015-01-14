91feab47e722927039603c86ce2ad64004ab63cc
Carl Eberhard <carlfeberhard@gmail.com>
2015-01-14 17:31:05 -0500

    Fix to 14796ac: for history-panel-tests.js

--- test/casperjs/history-panel-tests.js
+++ test/casperjs/history-panel-tests.js
@@ -99,7 +99,6 @@ spaceghost.test.begin( 'Testing the form of the main/current history panel', 0, 
         this.page.sendEvent( 'keypress', this.page.event.key.Enter );
         // wait for send and re-render name
         this.wait( 1000, function(){
-            this.capture( 'name.png' );
             this.test.assertSelectorHasText( nameSelector, newHistoryName, 'History name is ' + newHistoryName );
             this.test.assertDoesntExist( editableTextInput, "Input disappears after pressing enter" );
         });
@@ -113,158 +112,157 @@ spaceghost.test.begin( 'Testing the form of the main/current history panel', 0, 
 
         this.page.sendEvent( 'mousedown', -1, -1 );
         this.wait( 1000, function(){
-            this.debug( 'waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaat' );
             this.test.assertSelectorHasText( nameSelector, newHistoryName, 'History name is STILL ' + newHistoryName );
             this.test.assertDoesntExist( editableTextInput, "Input disappears after clicking away" );
         });
     });
 
-    //spaceghost.then( function(){
-    //    this.test.comment( 'name should revert if user hits ESC while editing' );
-    //
-    //    this.click( nameSelector );
-    //    this.page.sendEvent( 'keypress', "Arsenic Bacteria" );
-    //
-    //    this.page.sendEvent( 'keypress', this.page.event.key.Escape );
-    //    this.wait( 1000, function(){
-    //        this.test.assertSelectorHasText( nameSelector, newHistoryName, 'History name is STILL ' + newHistoryName );
-    //        this.test.assertDoesntExist( editableTextInput, "Input disappears after hitting ESC" );
-    //    });
-    //});
-    //
-    //// ------------------------------------------------------------------- check structure of NON empty history
-    //// upload file: 1.txt
-    //spaceghost.api.tools.thenUploadToCurrent({ filepath: filepathToUpload }, function uploadCallback( id, json ){
-    //    uploadId = id;
-    //});
-    //
-    //spaceghost.openHomePage().then( function checkPanelStructure(){
-    //    this.test.comment( 'checking structure of non-empty panel' );
-    //
-    //    this.test.comment( "history name should exist, be visible, and have text " + unnamedName );
-    //    this.test.assertExists( nameSelector, nameSelector + ' exists' );
-    //    this.test.assertVisible( nameSelector, 'History name is visible' );
-    //    this.test.assertSelectorHasText( nameSelector, newHistoryName, 'History name is ' + newHistoryName );
-    //
-    //    var onetxtFilesize = require( 'fs' ).size( filepathToUpload ),
-    //        expectedSize = onetxtFilesize + ' bytes';
-    //    this.test.comment( "history should display size and size should be " + onetxtFilesize + " bytes" );
-    //    this.test.assertExists( sizeSelector, 'Found ' + sizeSelector );
-    //    this.test.assertVisible( sizeSelector, 'History size is visible' );
-    //    this.test.assertSelectorHasText( sizeSelector, expectedSize,
-    //        'History size has "' + expectedSize + '": ' + this.fetchText( sizeSelector ).trim() );
-    //
-    //    this.test.comment( "tags and annotation icons should be available" );
-    //    this.test.assertExists( tagIconSelector,  'Tag icon button found' );
-    //    this.test.assertExists( annoIconSelector, 'Annotation icon button found' );
-    //
-    //    this.test.comment( "A message about the current history being empty should NOT be displayed" );
-    //    this.test.assertExists( emptyMsgSelector, emptyMsgSelector + ' exists' );
-    //    this.test.assertNotVisible( emptyMsgSelector, 'Empty history message is NOT visible' );
-    //});
-    //
-    //// ------------------------------------------------------------------- tags
-    //// keeping this light here - better for its own test file
-    ////TODO: check tooltips
-    //spaceghost.then( function openTags(){
-    //    this.test.comment( 'tag area should open when the history panel tag icon is clicked' );
-    //
-    //    this.click( tagIconSelector );
-    //    this.wait( 1000, function(){
-    //        this.test.assertVisible( tagAreaSelector, 'Tag area is now displayed' );
-    //    });
-    //});
-    //spaceghost.then( function closeAnnotation(){
-    //    this.test.comment( 'annotation area should close when the history panel tag icon is clicked again' );
-    //
-    //    this.click( tagIconSelector );
-    //    this.wait( 1000, function(){
-    //        this.test.assertNotVisible( tagAreaSelector, 'Tag area is now hidden' );
-    //    });
-    //});
-    //
-    //// ------------------------------------------------------------------- annotation
-    //// keeping this light here - better for its own test file
-    ////TODO: check tooltips
-    //spaceghost.then( function openAnnotation(){
-    //    this.test.comment( 'annotation area should open when the history panel annotation icon is clicked' );
-    //
-    //    this.click( annoIconSelector );
-    //    this.wait( 1000, function(){
-    //        this.test.assertVisible( annoAreaSelector, 'Annotation area is now displayed' );
-    //    });
-    //});
-    //spaceghost.then( function closeAnnotation(){
-    //    this.test.comment( 'annotation area should close when the history panel tag icon is clicked again' );
-    //
-    //    this.click( annoIconSelector );
-    //    this.wait( 1000, function(){
-    //        this.test.assertNotVisible( annoAreaSelector, 'Annotation area is now hidden' );
-    //    });
-    //});
-    //
-    //// ------------------------------------------------------------------- refresh button
-    //spaceghost.then( function refreshButton(){
-    //    this.test.comment( 'History panel should refresh when the history refresh icon is clicked' );
-    //
-    //    this.test.assertExists(  refreshButtonSelector, "Found refresh button" );
-    //    this.test.assertVisible( refreshButtonSelector, "Refresh button is visible" );
-    //    this.test.assertVisible( refreshButtonSelector + ' ' + refreshButtonIconSelector, "Refresh icon is visible" );
-    //
-    //    //this.assertNavigationRequested( refreshButtonHref, "History refreshed when clicking refresh icon", function(){
-    //    //    this.click( refreshButtonSelector );
-    //    //});
-    //});
-    //
-    //// ------------------------------------------------------------------- hdas can be expanded by clicking on the hda
-    //// broken in webkit w/ jq 1.7
-    //spaceghost.historypanel.waitForHdas( function(){
-    //    this.test.comment( 'HDAs can be expanded by clicking on the name' );
-    //    var uploadedSelector = '#dataset-' + uploadId;
-    //
-    //    this.click( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.title );
-    //    this.wait( 1000, function(){
-    //        this.test.assertVisible( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.body,
-    //            "Body for uploaded file is visible" );
-    //    });
-    //});
-    //
-    //// ------------------------------------------------------------------- expanded hdas still expanded after refresh
-    //spaceghost.then( function(){
-    //    this.test.comment( 'Expanded hdas are still expanded after a refresh' );
-    //    var uploadedSelector = '#dataset-' + uploadId;
-    //
-    //    this.click( refreshButtonSelector );
-    //    this.historypanel.waitForHdas( function(){
-    //        this.test.assertVisible( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.body,
-    //            "Body for uploaded file is visible" );
-    //    });
-    //    // this will break: webkit + jq 1.7
-    //});
-    //
-    //// ------------------------------------------------------------------- expanded hdas collapse by clicking name again
-    //spaceghost.then( function(){
-    //    this.test.comment( 'Expanded hdas collapse by clicking name again' );
-    //    var uploadedSelector = '#dataset-' + uploadId;
-    //
-    //    this.click( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.title );
-    //    this.wait( 500, function(){
-    //        this.test.assertNotVisible( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.body,
-    //            "Body for uploaded file is not visible" );
-    //    });
-    //});
-    //
-    //// ------------------------------------------------------------------- collapsed hdas still collapsed after refresh
-    //spaceghost.then( function(){
-    //    this.test.comment( 'collapsed hdas still collapsed after a refresh' );
-    //    var uploadedSelector = '#dataset-' + uploadId;
-    //
-    //    this.click( refreshButtonSelector );
-    //    this.historypanel.waitForHdas( function(){
-    //        this.test.assertNotVisible( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.body,
-    //            "Body for uploaded file is not visible" );
-    //    });
-    //});
+    spaceghost.then( function(){
+        this.test.comment( 'name should revert if user hits ESC while editing' );
+
+        this.click( nameSelector );
+        this.page.sendEvent( 'keypress', "Arsenic Bacteria" );
+
+        this.page.sendEvent( 'keypress', this.page.event.key.Escape );
+        this.wait( 1000, function(){
+            this.test.assertSelectorHasText( nameSelector, newHistoryName, 'History name is STILL ' + newHistoryName );
+            this.test.assertDoesntExist( editableTextInput, "Input disappears after hitting ESC" );
+        });
+    });
+
+    // ------------------------------------------------------------------- check structure of NON empty history
+    // upload file: 1.txt
+    spaceghost.api.tools.thenUploadToCurrent({ filepath: filepathToUpload }, function uploadCallback( id, json ){
+        uploadId = id;
+    });
+
+    spaceghost.openHomePage().then( function checkPanelStructure(){
+        this.test.comment( 'checking structure of non-empty panel' );
+
+        this.test.comment( "history name should exist, be visible, and have text " + unnamedName );
+        this.test.assertExists( nameSelector, nameSelector + ' exists' );
+        this.test.assertVisible( nameSelector, 'History name is visible' );
+        this.test.assertSelectorHasText( nameSelector, newHistoryName, 'History name is ' + newHistoryName );
+
+        var onetxtFilesize = require( 'fs' ).size( filepathToUpload ),
+            expectedSize = onetxtFilesize + ' bytes';
+        this.test.comment( "history should display size and size should be " + onetxtFilesize + " bytes" );
+        this.test.assertExists( sizeSelector, 'Found ' + sizeSelector );
+        this.test.assertVisible( sizeSelector, 'History size is visible' );
+        this.test.assertSelectorHasText( sizeSelector, expectedSize,
+            'History size has "' + expectedSize + '": ' + this.fetchText( sizeSelector ).trim() );
+
+        this.test.comment( "tags and annotation icons should be available" );
+        this.test.assertExists( tagIconSelector,  'Tag icon button found' );
+        this.test.assertExists( annoIconSelector, 'Annotation icon button found' );
+
+        this.test.comment( "A message about the current history being empty should NOT be displayed" );
+        this.test.assertExists( emptyMsgSelector, emptyMsgSelector + ' exists' );
+        this.test.assertNotVisible( emptyMsgSelector, 'Empty history message is NOT visible' );
+    });
+
+    // ------------------------------------------------------------------- tags
+    // keeping this light here - better for its own test file
+    //TODO: check tooltips
+    spaceghost.then( function openTags(){
+        this.test.comment( 'tag area should open when the history panel tag icon is clicked' );
+
+        this.click( tagIconSelector );
+        this.wait( 1000, function(){
+            this.test.assertVisible( tagAreaSelector, 'Tag area is now displayed' );
+        });
+    });
+    spaceghost.then( function closeAnnotation(){
+        this.test.comment( 'annotation area should close when the history panel tag icon is clicked again' );
+
+        this.click( tagIconSelector );
+        this.wait( 1000, function(){
+            this.test.assertNotVisible( tagAreaSelector, 'Tag area is now hidden' );
+        });
+    });
+
+    // ------------------------------------------------------------------- annotation
+    // keeping this light here - better for its own test file
+    //TODO: check tooltips
+    spaceghost.then( function openAnnotation(){
+        this.test.comment( 'annotation area should open when the history panel annotation icon is clicked' );
+
+        this.click( annoIconSelector );
+        this.wait( 1000, function(){
+            this.test.assertVisible( annoAreaSelector, 'Annotation area is now displayed' );
+        });
+    });
+    spaceghost.then( function closeAnnotation(){
+        this.test.comment( 'annotation area should close when the history panel tag icon is clicked again' );
+
+        this.click( annoIconSelector );
+        this.wait( 1000, function(){
+            this.test.assertNotVisible( annoAreaSelector, 'Annotation area is now hidden' );
+        });
+    });
+
+    // ------------------------------------------------------------------- refresh button
+    spaceghost.then( function refreshButton(){
+        this.test.comment( 'History panel should refresh when the history refresh icon is clicked' );
+
+        this.test.assertExists(  refreshButtonSelector, "Found refresh button" );
+        this.test.assertVisible( refreshButtonSelector, "Refresh button is visible" );
+        this.test.assertVisible( refreshButtonSelector + ' ' + refreshButtonIconSelector, "Refresh icon is visible" );
+
+        //this.assertNavigationRequested( refreshButtonHref, "History refreshed when clicking refresh icon", function(){
+        //    this.click( refreshButtonSelector );
+        //});
+    });
+
+    // ------------------------------------------------------------------- hdas can be expanded by clicking on the hda
+    // broken in webkit w/ jq 1.7
+    spaceghost.historypanel.waitForHdas( function(){
+        this.test.comment( 'HDAs can be expanded by clicking on the name' );
+        var uploadedSelector = '#dataset-' + uploadId;
+
+        this.click( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.title );
+        this.wait( 1000, function(){
+            this.test.assertVisible( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.body,
+                "Body for uploaded file is visible" );
+        });
+    });
+
+    // ------------------------------------------------------------------- expanded hdas still expanded after refresh
+    spaceghost.then( function(){
+        this.test.comment( 'Expanded hdas are still expanded after a refresh' );
+        var uploadedSelector = '#dataset-' + uploadId;
+
+        this.click( refreshButtonSelector );
+        this.historypanel.waitForHdas( function(){
+            this.test.assertVisible( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.body,
+                "Body for uploaded file is visible" );
+        });
+        // this will break: webkit + jq 1.7
+    });
+
+    // ------------------------------------------------------------------- expanded hdas collapse by clicking name again
+    spaceghost.then( function(){
+        this.test.comment( 'Expanded hdas collapse by clicking name again' );
+        var uploadedSelector = '#dataset-' + uploadId;
+
+        this.click( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.title );
+        this.wait( 500, function(){
+            this.test.assertNotVisible( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.body,
+                "Body for uploaded file is not visible" );
+        });
+    });
+
+    // ------------------------------------------------------------------- collapsed hdas still collapsed after refresh
+    spaceghost.then( function(){
+        this.test.comment( 'collapsed hdas still collapsed after a refresh' );
+        var uploadedSelector = '#dataset-' + uploadId;
+
+        this.click( refreshButtonSelector );
+        this.historypanel.waitForHdas( function(){
+            this.test.assertNotVisible( uploadedSelector + ' ' + this.historypanel.data.selectors.hda.body,
+                "Body for uploaded file is not visible" );
+        });
+    });
     /*
     */
     // ===================================================================
