3f1d9b0286db048aac5f28689cdfeb6d982d12e1
John Chilton <jmchilton@gmail.com>
2015-01-15 13:20:34 -0500

    Skip extra wrapping around template-style macros. Cheetah wants these to be strings.

--- lib/galaxy/tools/evaluation.py
+++ lib/galaxy/tools/evaluation.py
@@ -348,7 +348,7 @@ class ToolEvaluator( object ):
         Note: this method follows the style of the similar populate calls, in that param_dict is modified in-place.
         """
         # chromInfo is a filename, do not sanitize it.
-        skip = [ 'chromInfo' ]
+        skip = [ 'chromInfo' ] + self.tool.template_macro_params.keys()
         if not self.tool or not self.tool.options or self.tool.options.sanitize:
             for key, value in param_dict.items():
                 if key not in skip:
