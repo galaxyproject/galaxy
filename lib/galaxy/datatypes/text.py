7b1798349673fa0d16a305b010e944a272024328
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-01-16 21:38:18 +0100

    add SnpEffDb datatype

--- lib/galaxy/datatypes/text.py
+++ lib/galaxy/datatypes/text.py
@@ -18,7 +18,6 @@ import re
 import logging
 log = logging.getLogger(__name__)
 
-
 class Json( Text ):
     file_ext = "json"
 
@@ -258,4 +257,53 @@ class Arff( Text ):
                         data_block = True
         dataset.metadata.comment_lines = comment_lines
         dataset.metadata.columns = column_count
+
+
+class SnpEffDb( Text ):
+    """Class describing a SnpEff genome build"""
+    file_ext = "snpeffdb"
+    MetadataElement( name="genome_version", default=None, desc="Genome Version", readonly=True, visible=True, no_value=None )
+    MetadataElement( name="regulation", default=[], desc="Regulation Names", readonly=True, visible=True, no_value=[], optional=True)
+    MetadataElement( name="annotation", default=[], desc="Annotation Names", readonly=True, visible=True, no_value=[], optional=True)
+
+    def __init__( self, **kwd ):
+        Text.__init__( self, **kwd )
+
+    def set_meta( self, dataset, **kwd ):
+        Text.set_meta(self, dataset, **kwd )
+        data_dir = dataset.extra_files_path
+        ## search data_dir/genome_version for files
+        regulation_pattern = 'regulation_(.+).bin'
+        #  annotation files that are included in snpEff by a flag
+        annotations_dict = {'nextProt.bin' : '-nextprot','motif.bin': '-motif'}
+        regulations = []
+        annotations = []
+        if data_dir and os.path.isdir(data_dir):
+            for root, dirs, files in os.walk(data_dir):
+                for fname in files:
+                    if fname.startswith('snpEffectPredictor'):
+                        # if snpEffectPredictor.bin download succeeded
+                        genome_version = os.path.basename(root)
+                        dataset.metadata.genome_version = genome_version
+                    else:
+                        m = re.match(regulation_pattern,fname)
+                        if m:
+                            name = m.groups()[0]
+                            regulations.append(name)
+                        elif fname in annotations_dict:
+                            value = annotations_dict[fname]
+                            name = value.lstrip('-')
+                            annotations.append(name)
+            dataset.metadata.regulation = regulations
+            dataset.metadata.annotation = annotations
+            try:
+                fh = file(dataset.file_name,'w')
+                fh.write("%s\n" % genome_version)
+                if annotations:
+                    fh.write("annotations: %s\n" % ','.join(annotations))
+                if regulations:
+                    fh.write("regulations: %s\n" % ','.join(regulations))
+                fh.close()
+            except:
+                pass
 
