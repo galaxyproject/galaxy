05cdaadfb99b09326e75e7b18f0902ab4cb8168d
Dave Bouvier <dave@bx.psu.edu>
2015-01-28 11:50:31 -0500

    Allow a Galaxy admin to configure environment variables (e.g. $PATH) for their local Galaxy installation without altering the environment for their shell.

--- run.sh
+++ run.sh
@@ -10,6 +10,18 @@ then
     . .venv/bin/activate
 fi
 
+# If there is a file that defines a shell environment specific to this
+# instance of Galaxy, source the file.
+if [ -z "$GALAXY_LOCAL_ENV_FILE" ];
+then
+    GALAXY_LOCAL_ENV_FILE='./config/local_env.sh'
+fi
+
+if [ -f $GALAXY_LOCAL_ENV_FILE ];
+then
+    . $GALAXY_LOCAL_ENV_FILE
+fi
+
 python ./scripts/check_python.py
 [ $? -ne 0 ] && exit 1
 
