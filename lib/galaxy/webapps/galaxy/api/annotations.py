91beddc6f9422ab5b8e720e967bc9f7798b52373
Carl Eberhard <carlfeberhard@gmail.com>
2015-02-02 17:08:17 -0500

    API, tags & annotations: use _future_expose_api and minor clean up

--- lib/galaxy/webapps/galaxy/api/annotations.py
+++ lib/galaxy/webapps/galaxy/api/annotations.py
@@ -1,27 +1,31 @@
 """
 API operations on annotations.
 """
-import logging
-from galaxy import web
+
+from galaxy.web.base.controller import BaseAPIController
+from galaxy.web.base.controller import UsesStoredWorkflowMixin
 from galaxy.model.item_attrs import UsesAnnotations
-from galaxy.util.sanitize_html import sanitize_html
-from galaxy.web.base.controller import BaseAPIController, HTTPNotImplemented, UsesStoredWorkflowMixin
 
 from galaxy import managers
 
+from galaxy import exceptions
+from galaxy.web import _future_expose_api as expose_api
+from galaxy.util import sanitize_html
+
+import logging
 log = logging.getLogger( __name__ )
 
 
 class BaseAnnotationsController( BaseAPIController, UsesStoredWorkflowMixin, UsesAnnotations ):
 
-    @web.expose_api
+    @expose_api
     def index( self, trans, **kwd ):
         idnum = kwd[self.tagged_item_id]
         item = self._get_item_from_id(trans, idnum)
         if item is not None:
             return self.get_item_annotation_str( trans.sa_session, trans.get_user(), item )
 
-    @web.expose_api
+    @expose_api
     def create( self, trans, payload, **kwd ):
         if "text" not in payload:
             return ""
@@ -29,24 +33,24 @@ class BaseAnnotationsController( BaseAPIController, UsesStoredWorkflowMixin, Use
         item = self._get_item_from_id(trans, idnum)
         if item is not None:
             new_annotation = payload.get("text")
-            # Sanitize annotation before adding it.
-            new_annotation = sanitize_html( new_annotation, 'utf-8', 'text/html' )
+            #TODO: sanitize on display not entry
+            new_annotation = sanitize_html.sanitize_html( new_annotation, 'utf-8', 'text/html' )
 
             self.add_item_annotation( trans.sa_session, trans.get_user(), item, new_annotation )
             trans.sa_session.flush()
             return new_annotation
         return ""
 
-    @web.expose_api
+    @expose_api
     def delete( self, trans, **kwd ):
         idnum = kwd[self.tagged_item_id]
         item = self._get_item_from_id(trans, idnum)
         if item is not None:
             return self.delete_item_annotation( trans.sa_session, trans.get_user(), item )
 
-    @web.expose_api
+    @expose_api
     def undelete( self, trans, **kwd ):
-        raise HTTPNotImplemented()
+        raise exceptions.NotImplemented()
 
 
 class HistoryAnnotationsController(BaseAnnotationsController):
@@ -63,7 +67,7 @@ class HistoryAnnotationsController(BaseAnnotationsController):
         return history
 
 
-class HistoryContentAnnotationsController(BaseAnnotationsController):
+class HistoryContentAnnotationsController( BaseAnnotationsController ):
     controller_name = "history_content_annotations"
     tagged_item_id = "history_content_id"
 
@@ -78,10 +82,10 @@ class HistoryContentAnnotationsController(BaseAnnotationsController):
         return hda
 
 
-class WorkflowAnnotationsController(BaseAnnotationsController):
+class WorkflowAnnotationsController( BaseAnnotationsController ):
     controller_name = "workflow_annotations"
     tagged_item_id = "workflow_id"
 
     def _get_item_from_id(self, trans, idstr):
-        hda = self.get_stored_workflow(trans, idstr)
+        hda = self.get_stored_workflow( trans, idstr )
         return hda
