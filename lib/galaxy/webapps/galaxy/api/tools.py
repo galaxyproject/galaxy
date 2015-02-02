5b7ee3fdf8349b1854bbccc0c12e0db0c6bb869a
Daniel Blankenberg <dan@bx.psu.edu>
2015-02-02 15:57:18 -0500

    Fix for preventing non-admins from running data managers via the api.

--- lib/galaxy/webapps/galaxy/api/tools.py
+++ lib/galaxy/webapps/galaxy/api/tools.py
@@ -67,7 +67,7 @@ class ToolsController( BaseAPIController, UsesVisualizationMixin ):
         """
         io_details = util.string_as_bool( kwd.get( 'io_details', False ) )
         link_details = util.string_as_bool( kwd.get( 'link_details', False ) )
-        tool = self._get_tool( id )
+        tool = self._get_tool( id, user=trans.user )
         return tool.to_dict( trans, io_details=io_details, link_details=link_details )
 
     @_future_expose_api_anonymous
@@ -98,7 +98,7 @@ class ToolsController( BaseAPIController, UsesVisualizationMixin ):
 
     @_future_expose_api_anonymous
     def citations( self, trans, id, **kwds ):
-        tool = self._get_tool( id )
+        tool = self._get_tool( id, user=trans.user )
         rval = []
         for citation in tool.citations:
             rval.append( citation.to_dict( 'bibtex' ) )
@@ -132,7 +132,7 @@ class ToolsController( BaseAPIController, UsesVisualizationMixin ):
         # Get tool.
         tool_version = payload.get( 'tool_version', None )
         tool = trans.app.toolbox.get_tool( payload[ 'tool_id' ] , tool_version ) if 'tool_id' in payload else None
-        if not tool:
+        if not tool or not tool.allow_user_access( trans.user ):
             trans.response.status = 404
             return { "message": { "type": "error", "text" : trans.app.model.Dataset.conversion_messages.NO_TOOL } }
 
@@ -230,10 +230,10 @@ class ToolsController( BaseAPIController, UsesVisualizationMixin ):
     #
     # -- Helper methods --
     #
-    def _get_tool( self, id, tool_version=None ):
+    def _get_tool( self, id, tool_version=None, user=None ):
         id = urllib.unquote_plus( id )
         tool = self.app.toolbox.get_tool( id, tool_version )
-        if not tool:
+        if not tool or not tool.allow_user_access( user ):
             raise exceptions.ObjectNotFound("Could not find tool with id '%s'" % id)
         return tool
 
@@ -300,7 +300,7 @@ class ToolsController( BaseAPIController, UsesVisualizationMixin ):
         #
         original_job = self.hda_manager.creating_job( original_dataset )
         tool = trans.app.toolbox.get_tool( original_job.tool_id )
-        if not tool:
+        if not tool or not tool.allow_user_access( trans.user ):
             return trans.app.model.Dataset.conversion_messages.NO_TOOL
         tool_params = dict( [ ( p.name, p.value ) for p in original_job.parameters ] )
 
