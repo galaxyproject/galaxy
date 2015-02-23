3c0275ae52013ca056aea2f31a43a657ca737c45
guerler <aysam.guerler@gmail.com>
2015-02-23 18:04:02 -0500

    Fix job ids in success message

--- client/galaxy/scripts/mvc/tools/tools-template.js
+++ client/galaxy/scripts/mvc/tools/tools-template.js
@@ -10,17 +10,17 @@ return {
                     '</div>' +
                 '</div>';
     },
-    
+
     success: function(response) {
         // check
         if (!response.jobs || !response.jobs.length) {
             console.debug('tools-template::success() - Failed jobs.');
             return;
         }
-       
+
         // number of jobs
         var njobs = response.jobs.length;
-        
+
         // job count info text
         var njobs_text = '';
         if (njobs == 1) {
@@ -28,20 +28,20 @@ return {
         } else {
             njobs_text = njobs + ' jobs have';
         }
-       
+
         // create template string
         var tmpl =  '<div class="donemessagelarge">' +
                         '<p>' + njobs_text + ' been successfully added to the queue - resulting in the following datasets:</p>';
         for (var i in response.outputs) {
-            tmpl +=     '<p style="padding: 10px 20px;"><b>' + (parseInt(i)+1) + ': ' + response.outputs[i].name + '</b></p>';
+            tmpl +=     '<p style="padding: 10px 20px;"><b>' + response.outputs[i].hid + ': ' + response.outputs[i].name + '</b></p>';
         }
         tmpl +=         '<p>You can check the status of queued jobs and view the resulting data by refreshing the History pane. When the job has been run the status will change from \'running\' to \'finished\' if completed successfully or \'error\' if problems were encountered.</p>' +
                     '</div>';
-       
+
         // return success message element
         return tmpl;
     },
-    
+
     error: function(job_def) {
         return  '<div>' +
                     '<p>' +
@@ -52,14 +52,14 @@ return {
                     '</textarea>' +
                 '</div>';
     },
-    
+
     batchMode: function() {
         return  '<div class="ui-table-form-info">' +
                     '<i class="fa fa-sitemap" style="font-size: 1.2em; padding: 2px 5px;"/>' +
                     'This is a batch mode input field. A separate job will be triggered for each dataset.' +
                 '</div>';
     },
-    
+
     requirements: function(options) {
         var requirements_message = 'This tool requires ';
         for (var i in options.requirements) {
