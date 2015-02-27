galaxyproject/galaxy

commit ba825fc5531e52ed6a9a8a74212803a4ca7bd86a

Fix reports webapp for MySQL. Big thanks for Eric Enns and Fernandez Edgar for contributing patches to the galaxy-dev list.

lib/galaxy/webapps/reports/app.py
@@ -23,6 +23,7 @@ class UniverseApplication( object ):
                                                 db_url,
                                                 self.config.database_engine_options,
                                                 create_tables=True )
+        self.targets_mysql = 'mysql' in self.config.database_connection
         # Security helper
         self.security = security.SecurityHelper( id_secret=self.config.id_secret )
 

lib/galaxy/webapps/reports/controllers/jobs.py
@@ -7,6 +7,8 @@ from galaxy.model.orm import and_, not_, or_
 import pkg_resources
 pkg_resources.require( "SQLAlchemy >= 0.4" )
 import sqlalchemy as sa
+from galaxy.webapps.reports.controllers.query import ReportQueryBuilder
+
 import logging
 log = logging.getLogger( __name__ )
 
@@ -135,7 +137,7 @@ class SpecifiedDateListGrid( grids.Grid ):
                                .enable_eagerloads( False )
 
 
-class Jobs( BaseUIController ):
+class Jobs( BaseUIController, ReportQueryBuilder ):
 
     """
     Class contains functions for querying data requested by user via the webapp. It exposes the functions and
@@ -297,11 +299,11 @@ class Jobs( BaseUIController ):
         # In case we don't know which is the monitor user we will query for all jobs
         monitor_user_id = get_monitor_id( trans, monitor_email )
 
-        jobs_by_month = sa.select( ( sa.func.date_trunc( 'month', model.Job.table.c.create_time ).label( 'date' ),
+        jobs_by_month = sa.select( ( self.select_month( model.Job.table.c.create_time ).label( 'date' ),
                                      sa.func.count( model.Job.table.c.id ).label( 'total_jobs' ) ),
                                    whereclause=model.Job.table.c.user_id != monitor_user_id,
                                    from_obj=[ model.Job.table ],
-                                   group_by=[ 'date' ],
+                                   group_by=self.group_by_month( model.Job.table.c.create_time ),
                                    order_by=[ sa.desc( 'date' ) ] )
 
         jobs = []
@@ -331,12 +333,12 @@ class Jobs( BaseUIController ):
         # In case we don't know which is the monitor user we will query for all jobs
         monitor_user_id = get_monitor_id( trans, monitor_email )
 
-        jobs_in_error_by_month = sa.select( ( sa.func.date_trunc( 'month', sa.func.date( model.Job.table.c.create_time ) ).label( 'date' ),
+        jobs_in_error_by_month = sa.select( ( self.select_month( model.Job.table.c.create_time ).label( 'date' ),
                                               sa.func.count( model.Job.table.c.id ).label( 'total_jobs' ) ),
                                             whereclause=sa.and_( model.Job.table.c.state == 'error',
                                                                  model.Job.table.c.user_id != monitor_user_id ),
                                             from_obj=[ model.Job.table ],
-                                            group_by=[ sa.func.date_trunc( 'month', sa.func.date( model.Job.table.c.create_time ) ) ],
+                                            group_by=self.group_by_month( model.Job.table.c.create_time ),
                                             order_by=[ sa.desc( 'date' ) ] )
 
         jobs = []
@@ -380,14 +382,14 @@ class Jobs( BaseUIController ):
         params = util.Params( kwd )
         message = ''
         email = util.restore_text( params.get( 'email', '' ) )
-        q = sa.select( ( sa.func.date_trunc( 'month', sa.func.date( model.Job.table.c.create_time ) ).label( 'date' ),
+        q = sa.select( ( self.select_month( model.Job.table.c.create_time ).label( 'date' ),
                          sa.func.count( model.Job.table.c.id ).label( 'total_jobs' ) ),
                        whereclause=sa.and_( model.Job.table.c.session_id == model.GalaxySession.table.c.id,
                                             model.GalaxySession.table.c.user_id == model.User.table.c.id,
                                             model.User.table.c.email == email
                                             ),
                        from_obj=[ sa.join( model.Job.table, model.User.table ) ],
-                       group_by=[ sa.func.date_trunc( 'month', sa.func.date( model.Job.table.c.create_time ) ) ],
+                       group_by=self.group_by_month( model.Job.table.c.create_time ),
                        order_by=[ sa.desc( 'date' ) ] )
         jobs = []
         for row in q.execute():
@@ -436,12 +438,12 @@ class Jobs( BaseUIController ):
 
         tool_id = params.get( 'tool_id', 'Add a column1' )
         specified_date = params.get( 'specified_date', datetime.utcnow().strftime( "%Y-%m-%d" ) )
-        q = sa.select( ( sa.func.date_trunc( 'month', sa.func.date( model.Job.table.c.create_time ) ).label( 'date' ),
+        q = sa.select( ( self.select_month( model.Job.table.c.create_time ).label( 'date' ),
                          sa.func.count( model.Job.table.c.id ).label( 'total_jobs' ) ),
                        whereclause=sa.and_( model.Job.table.c.tool_id == tool_id,
                                             model.Job.table.c.user_id != monitor_user_id ),
                        from_obj=[ model.Job.table ],
-                       group_by=[ sa.func.date_trunc( 'month', sa.func.date( model.Job.table.c.create_time ) ) ],
+                       group_by=self.group_by_month( model.Job.table.c.create_time ),
                        order_by=[ sa.desc( 'date' ) ] )
         jobs = []
         for row in q.execute():

lib/galaxy/webapps/reports/controllers/query.py
@@ -1,0 +1,32 @@
+""" Mixin to help build advanced queries for reports interface.
+"""
+import pkg_resources
+pkg_resources.require( "SQLAlchemy >= 0.4" )
+import sqlalchemy as sa
+
+
+class ReportQueryBuilder:
+
+    def group_by_month(self, column):
+        if self.app.targets_mysql:
+            return [ sa.func.year( column ), sa.func.month( sa.func.date( column ) ) ]
+        else:
+            return [ sa.func.date_trunc( 'month', sa.func.date( column ) ) ]
+
+    def select_month(self, column):
+        if self.app.targets_mysql:
+            return sa.func.date( column )
+        else:
+            return sa.func.date_trunc( 'month', sa.func.date( column ) )
+
+    def group_by_day(self, column):
+        if self.app.targets_mysql:
+            return [ sa.func.day( sa.func.date( column ) ) ]
+        else:
+            return [ sa.func.date_trunc( 'day', sa.func.date( column ) ) ]
+
+    def select_day(self, column):
+        if self.app.targets_mysql:
+            return sa.func.date( column )
+        else:
+            return sa.func.date_trunc( 'day', sa.func.date( column ) )

lib/galaxy/webapps/reports/controllers/sample_tracking.py
@@ -7,6 +7,8 @@ from galaxy.model.orm import and_
 import pkg_resources
 pkg_resources.require( "SQLAlchemy >= 0.4" )
 import sqlalchemy as sa
+from galaxy.webapps.reports.controllers.query import ReportQueryBuilder
+
 import logging
 log = logging.getLogger( __name__ )
 
@@ -105,7 +107,7 @@ class SpecifiedDateListGrid( grids.Grid ):
                                .enable_eagerloads( False )
 
 
-class SampleTracking( BaseUIController ):
+class SampleTracking( BaseUIController, ReportQueryBuilder ):
 
     specified_date_list_grid = SpecifiedDateListGrid()
 
@@ -144,9 +146,10 @@ class SampleTracking( BaseUIController ):
     @web.expose
     def per_month_all( self, trans, **kwd ):
         message = ''
-        q = sa.select( ( sa.func.date_trunc( 'month', sa.func.date( model.Request.table.c.create_time ) ).label( 'date' ), sa.func.count( model.Request.table.c.id ).label( 'total' ) ),
+        q = sa.select( ( self.select_month( model.Request.table.c.create_time ).label( 'date' ),
+                         sa.func.count( model.Request.table.c.id ).label( 'total' ) ),
                        from_obj=[ sa.outerjoin( model.Request.table, model.User.table ) ],
-                       group_by=[ sa.func.date_trunc( 'month', sa.func.date( model.Request.table.c.create_time ) ) ],
+                       group_by=self.group_by_month( model.Request.table.c.create_time ),
                        order_by=[ sa.desc( 'date' ) ] )
         requests = []
         for row in q.execute():
@@ -178,11 +181,11 @@ class SampleTracking( BaseUIController ):
         message = ''
         email = util.restore_text( params.get( 'email', '' ) )
         user_id = trans.security.decode_id( params.get( 'id', '' ) )
-        q = sa.select( ( sa.func.date_trunc( 'month', sa.func.date( model.Request.table.c.create_time ) ).label( 'date' ),
+        q = sa.select( ( self.select_month( model.Request.table.c.create_time ).label( 'date' ),
                          sa.func.count( model.Request.table.c.id ).label( 'total' ) ),
                        whereclause=model.Request.table.c.user_id == user_id,
                        from_obj=[ model.Request.table ],
-                       group_by=[ sa.func.date_trunc( 'month', sa.func.date( model.Request.table.c.create_time ) ) ],
+                       group_by=self.group_by_month( model.Request.table.c.create_time ),
                        order_by=[ sa.desc( 'date' ) ] )
         requests = []
         for row in q.execute():

lib/galaxy/webapps/reports/controllers/users.py
@@ -7,13 +7,14 @@ from galaxy import util
 import pkg_resources
 pkg_resources.require( "SQLAlchemy >= 0.4" )
 import sqlalchemy as sa
+from galaxy.webapps.reports.controllers.query import ReportQueryBuilder
 import logging
 from markupsafe import escape
 
 log = logging.getLogger( __name__ )
 
 
-class Users( BaseUIController ):
+class Users( BaseUIController, ReportQueryBuilder ):
 
     @web.expose
     def registered_users( self, trans, **kwd ):
@@ -24,10 +25,10 @@ class Users( BaseUIController ):
     @web.expose
     def registered_users_per_month( self, trans, **kwd ):
         message = escape( util.restore_text( kwd.get( 'message', '' ) ) )
-        q = sa.select( ( sa.func.date_trunc( 'month', sa.func.date( galaxy.model.User.table.c.create_time ) ).label( 'date' ),
+        q = sa.select( ( self.select_month( galaxy.model.User.table.c.create_time ).label( 'date' ),
                          sa.func.count( galaxy.model.User.table.c.id ).label( 'num_users' ) ),
                        from_obj=[ galaxy.model.User.table ],
-                       group_by=[ sa.func.date_trunc( 'month', sa.func.date( galaxy.model.User.table.c.create_time ) ) ],
+                       group_by=self.group_by_month( galaxy.model.User.table.c.create_time ),
                        order_by=[ sa.desc( 'date' ) ] )
         users = []
         for row in q.execute():
@@ -50,12 +51,12 @@ class Users( BaseUIController ):
         end_date = start_date + timedelta( days=calendar.monthrange( year, month )[1] )
         month_label = start_date.strftime( "%B" )
         year_label = start_date.strftime( "%Y" )
-        q = sa.select( ( sa.func.date_trunc( 'day', sa.func.date( galaxy.model.User.table.c.create_time ) ).label( 'date' ),
+        q = sa.select( ( self.select_day( galaxy.model.User.table.c.create_time ).label( 'date' ),
                          sa.func.count( galaxy.model.User.table.c.id ).label( 'num_users' ) ),
                        whereclause=sa.and_( galaxy.model.User.table.c.create_time >= start_date,
                                             galaxy.model.User.table.c.create_time < end_date ),
                        from_obj=[ galaxy.model.User.table ],
-                       group_by=[ sa.func.date_trunc( 'day', sa.func.date( galaxy.model.User.table.c.create_time ) ) ],
+                       group_by=self.group_by_day( galaxy.model.User.table.c.create_time ),
                        order_by=[ sa.desc( 'date' ) ] )
         users = []
         for row in q.execute():
@@ -82,7 +83,7 @@ class Users( BaseUIController ):
         day_label = start_date.strftime( "%A" )
         month_label = start_date.strftime( "%B" )
         year_label = start_date.strftime( "%Y" )
-        q = sa.select( ( sa.func.date_trunc( 'day', sa.func.date( galaxy.model.User.table.c.create_time ) ).label( 'date' ),
+        q = sa.select( ( self.select_day( galaxy.model.User.table.c.create_time ).label( 'date' ),
                          galaxy.model.User.table.c.email ),
                        whereclause=sa.and_( galaxy.model.User.table.c.create_time >= start_date,
                                             galaxy.model.User.table.c.create_time < end_date ),

lib/galaxy/webapps/reports/controllers/workflows.py
@@ -7,6 +7,7 @@ from galaxy.web.base.controller import BaseUIController, web
 from galaxy.web.framework.helpers import grids
 eggs.require( "SQLAlchemy >= 0.4" )
 import sqlalchemy as sa
+from galaxy.webapps.reports.controllers.query import ReportQueryBuilder
 
 import logging
 log = logging.getLogger( __name__ )
@@ -106,7 +107,7 @@ class SpecifiedDateListGrid( grids.Grid ):
                                .enable_eagerloads( False )
 
 
-class Workflows( BaseUIController ):
+class Workflows( BaseUIController, ReportQueryBuilder ):
 
     specified_date_list_grid = SpecifiedDateListGrid()
 
@@ -145,9 +146,9 @@ class Workflows( BaseUIController ):
     @web.expose
     def per_month_all( self, trans, **kwd ):
         message = ''
-        q = sa.select( ( sa.func.date_trunc( 'month', sa.func.date( model.StoredWorkflow.table.c.create_time ) ).label( 'date' ), sa.func.count( model.StoredWorkflow.table.c.id ).label( 'total_workflows' ) ),
+        q = sa.select( ( self.select_month( model.StoredWorkflow.table.c.create_time ).label( 'date' ), sa.func.count( model.StoredWorkflow.table.c.id ).label( 'total_workflows' ) ),
                        from_obj=[ sa.outerjoin( model.StoredWorkflow.table, model.User.table ) ],
-                       group_by=[ sa.func.date_trunc( 'month', sa.func.date( model.StoredWorkflow.table.c.create_time ) ) ],
+                       group_by=self.group_by_month( model.StoredWorkflow.table.c.create_time ),
                        order_by=[ sa.desc( 'date' ) ] )
         workflows = []
         for row in q.execute():
@@ -179,11 +180,11 @@ class Workflows( BaseUIController ):
         message = ''
         email = util.restore_text( params.get( 'email', '' ) )
         user_id = trans.security.decode_id( params.get( 'id', '' ) )
-        q = sa.select( ( sa.func.date_trunc( 'month', sa.func.date( model.StoredWorkflow.table.c.create_time ) ).label( 'date' ),
+        q = sa.select( ( self.select_month( model.StoredWorkflow.table.c.create_time ).label( 'date' ),
                          sa.func.count( model.StoredWorkflow.table.c.id ).label( 'total_workflows' ) ),
                        whereclause=model.StoredWorkflow.table.c.user_id == user_id,
                        from_obj=[ model.StoredWorkflow.table ],
-                       group_by=[ sa.func.date_trunc( 'month', sa.func.date( model.StoredWorkflow.table.c.create_time ) ) ],
+                       group_by=self.group_by_month( model.StoredWorkflow.table.c.create_time ),
                        order_by=[ sa.desc( 'date' ) ] )
         workflows = []
         for row in q.execute():

commit 674360510253eccfc13478d89237726327d36f51

Fix reports webapp under default database broken in ba825fc5

lib/galaxy/webapps/reports/app.py
@@ -25,7 +25,10 @@ class UniverseApplication( object ):
                                                 db_url,
                                                 self.config.database_engine_options,
                                                 create_tables=True )
-        self.targets_mysql = 'mysql' in self.config.database_connection
+        if not self.config.database_connection:
+            self.targets_mysql = False
+        else:
+            self.targets_mysql = 'mysql' in self.config.database_connection
         # Security helper
         self.security = security.SecurityHelper( id_secret=self.config.id_secret )
         # used for cachebusting -- refactor this into a *SINGLE* UniverseApplication base.