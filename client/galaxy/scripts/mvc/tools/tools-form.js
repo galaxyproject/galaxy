bf3c2c10dcb6a6a49b1bb8ec0b486139a52b7c3f
guerler <aysam.guerler@gmail.com>
2015-03-06 18:56:57 -0500

    ToolForm: Add remap options

--- client/galaxy/scripts/mvc/tools/tools-form.js
+++ client/galaxy/scripts/mvc/tools/tools-form.js
@@ -8,6 +8,19 @@ define(['utils/utils', 'mvc/ui/ui-misc', 'mvc/tools/tools-form-base', 'mvc/tools
     var View = ToolFormBase.extend({
         // initialize
         initialize: function(options) {
+            // add remap button
+            if (options.job_id && options.job_remap) {
+                options.inputs['rerun_remap_job_id'] = {
+                    label       : 'Resume dependencies from this job',
+                    name        : 'rerun_remap_job_id',
+                    type        : 'select',
+                    display     : 'radio',
+                    ignore      : '__ignore__',
+                    value       : '__ignore__',
+                    options     : [['Yes', options.job_id], ['No', '__ignore__']],
+                    help        : 'The previous run of this tool failed and other tools were waiting for it to finish successfully, use this option to resume those tools using the outputs of this tool run.'
+                }
+            }
             // create options dictionary
             var self = this;
             options = Utils.merge(options, {
