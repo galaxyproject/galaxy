11fb091ccf83d89660859f9a8bd17c8270bb3dde
peterjc <p.j.a.cock@googlemail.com>
2015-03-09 16:49:12 +0000

    Add sorting option to Count1 (tools/filters/uniq.xml)
    When using this tool to produce a tally table, often want to
    have the most common entries listed first - rather than the
    sort order coming from the values being counted.

--- tools/filters/uniq.py
+++ tools/filters/uniq.py
@@ -50,6 +50,7 @@ def main():
         print "                     P   Pipe"
         print "                     Dt  Dot"
         print "                     Sp  Space"
+        print " -s        Sorting: value (default), largest, or smallest"
         return 0
 
     outputfile = opts.get("-o")
@@ -71,6 +72,13 @@ def main():
     if columns == None or columns == 'None':
         print "Columns not specified."
         return -4
+
+    sorting = opts.get("-s")
+    if sorting is None:
+        sorting = "value"
+    if sorting not in ["value", "largest", "smallest"]:
+        print "Unknown sorting option %r" % sorting
+        return -5
 
     # All inputs have been specified at this point, now validate.
     fileRegEx = re.compile("^[A-Za-z0-9./\-_]+$")
@@ -106,10 +114,16 @@ def main():
 
     # set columns
     commandline += "-f " + columns
+    commandline += " " + inputfile + " | sed 's/[ \t]*$//' | sort | uniq -c"
+    # uniq -C produces lines with leading spaces, so we can sort by value
+    if sorting == "largest":
+        commandline += " | sort -r"
+    elif sorting == "smallest":
+        commandline += " | sort"
     # uniq -C produces lines with leading spaces, use sed to remove that
     # uniq -C puts a space between the count and the field, want a tab.
     # To replace just first tab, use sed again with 1 as the index
-    commandline += " " + inputfile + " | sed 's/[ \t]*$//' | sort | uniq -c | sed 's/^\ *//' | sed 's/ /\t/1' > " + outputfile
+    commandline += " | sed 's/^\ *//' | sed 's/ /\t/1' > " + outputfile
     errorcode, stdout = commands.getstatusoutput(commandline)
     
     print "Count of unique values in " + columns_for_display
