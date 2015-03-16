e2118e5bfc3dd28775810aa770d3fe31ff101ecd
dannon <dannon.baker@gmail.com>
2015-03-16 17:21:08 -0400

    Add information about making history accessible to initial export archive message.

--- lib/galaxy/webapps/galaxy/controllers/history.py
+++ lib/galaxy/webapps/galaxy/controllers/history.py
@@ -1233,33 +1233,28 @@ class HistoryController( BaseUIController, SharableMixin, UsesAnnotations, UsesI
             # Use current history.
             history = trans.history
             id = trans.security.encode_id( history.id )
-
         if not history:
             return trans.show_error_message( "This history does not exist or you cannot export this history." )
-
-        #
         # If history has already been exported and it has not changed since export, stream it.
-        #
         jeha = history.latest_export
         if jeha and jeha.up_to_date:
             if jeha.ready:
                 if preview:
                     url = url_for( controller='history', action="export_archive", id=id, qualified=True )
-                    return trans.show_message( "History Ready: '%(n)s'. Use this link to download \
-                                                the archive or import it to another Galaxy server: \
-                                                <a href='%(u)s'>%(u)s</a>" % ( { 'n' : history.name, 'u' : url } ) )
+                    return trans.show_message( "History Ready: '%(n)s'. Use this link to download "
+                                               "the archive or import it to another Galaxy server: "
+                                               "<a href='%(u)s'>%(u)s</a>" % ( { 'n': history.name, 'u': url } ) )
                 else:
                     return self.serve_ready_history_export( trans, jeha )
             elif jeha.preparing:
-                return trans.show_message( "Still exporting history %(n)s; please check back soon. Link: <a href='%(s)s'>%(s)s</a>" \
-                        % ( { 'n' : history.name, 's' : url_for( controller='history', action="export_archive", id=id, qualified=True ) } ) )
-
+                return trans.show_message( "Still exporting history %(n)s; please check back soon. Link: <a href='%(s)s'>%(s)s</a>"
+                                           % ( { 'n': history.name, 's': url_for( controller='history', action="export_archive", id=id, qualified=True ) } ) )
         self.queue_history_export( trans, history, gzip=gzip, include_hidden=include_hidden, include_deleted=include_deleted )
         url = url_for( controller='history', action="export_archive", id=id, qualified=True )
-        return trans.show_message( "Exporting History '%(n)s'. Use this link to download \
-                                    the archive or import it to another Galaxy server: \
-                                    <a href='%(u)s'>%(u)s</a>" % ( { 'n' : history.name, 'u' : url } ) )
-        #TODO: used in this file and index.mako
+        return trans.show_message( "Exporting History '%(n)s'. You will need to <a href='%(share)s'>make this history 'accessible'</a> in order to import this to another galaxy sever. <br/>"
+                                   "Use this link to download the archive or import it to another Galaxy server: "
+                                   "<a href='%(u)s'>%(u)s</a>" % ( { 'share': url_for(controller='history', action='sharing'), 'n': history.name, 'u': url } ) )
+        # TODO: used in this file and index.mako
 
     @web.expose
     @web.json
