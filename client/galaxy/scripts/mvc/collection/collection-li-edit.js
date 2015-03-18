c0e55095da250efcc0f21759fb50131b3f85fb7f
Carl Eberhard <carlfeberhard@gmail.com>
2015-03-18 11:40:18 -0400

    UI, Collections: adding editable collection panels and lis

--- client/galaxy/scripts/mvc/collection/collection-li-edit.js
+++ client/galaxy/scripts/mvc/collection/collection-li-edit.js
@@ -1,34 +1,156 @@
 define([
     "mvc/collection/collection-li",
+    "mvc/dataset/dataset-li-edit",
+    "mvc/base-mvc",
     "utils/localization"
-], function( DC_LI, _l ){
+], function( DC_LI, DATASET_LI_EDIT, BASE_MVC, _l ){
+/* global Backbone, LoggableMixin */
 //==============================================================================
-/*
-
-NOTE: not much going on here. Until we find out what operations a user will need
-    to perform on their own DC's, we'll leave this empty.
-
-*/
-//==============================================================================
-var _super = DC_LI.DCListItemView;
-/** @class Editing view for DatasetCollectionElement.
+var DCListItemView = DC_LI.DCListItemView;
+/** @class Edit view for DatasetCollection.
  */
-var DCEListItemEdit = _super.extend(
-/** @lends DCEListItemEdit.prototype */{
+var DCListItemEdit = DCListItemView.extend(
+/** @lends DCListItemEdit.prototype */{
+//TODO: may not be needed
 
     /** logger used to record this.log messages, commonly set to console */
     //logger              : console,
 
+    /** override to add linkTarget */
+    initialize : function( attributes ){
+        DCListItemView.prototype.initialize.call( this, attributes );
+    },
+
     // ......................................................................... misc
-    /** string rep */
+    /** String representation */
+    toString : function(){
+        var modelString = ( this.model )?( this.model + '' ):( '(no model)' );
+        return 'DCListItemEdit(' + modelString + ')';
+    }
+});
+
+// ............................................................................ TEMPLATES
+/** underscore templates */
+// DCListItemEdit.prototype.templates = (function(){
+
+//     // use element identifier
+//     var titleBarTemplate = BASE_MVC.wrapTemplate([
+//         '<div class="title-bar clear" tabindex="0">',
+//             '<div class="title">',
+//                 '<span class="name"><%- collection.element_identifier || collection.name %></span>',
+//             '</div>',
+//             '<div class="subtitle"></div>',
+//         '</div>'
+//     ], 'collection' );
+
+//     return _.extend( {}, DCListItemView.prototype.templates, {
+//         titleBar : titleBarTemplate
+//     });
+// }());
+
+
+//==============================================================================
+var DCEListItemView = DC_LI.DCEListItemView;
+/** @class Read only view for DatasetCollectionElement.
+ */
+var DCEListItemEdit = DCEListItemView.extend(
+/** @lends DCEListItemEdit.prototype */{
+//TODO: this might be expendable - compacted with HDAListItemView
+
+    /** logger used to record this.log messages, commonly set to console */
+    //logger              : console,
+
+    /** set up */
+    initialize  : function( attributes ){
+        DCEListItemView.prototype.initialize.call( this, attributes );
+    },
+
+    // ......................................................................... misc
+    /** String representation */
     toString : function(){
         var modelString = ( this.model )?( this.model + '' ):( '(no model)' );
         return 'DCEListItemEdit(' + modelString + ')';
     }
 });
 
+// ............................................................................ TEMPLATES
+/** underscore templates */
+// DCEListItemEdit.prototype.templates = (function(){
+
+//     // use the element identifier here - since that will persist and the user will need it
+//     var titleBarTemplate = BASE_MVC.wrapTemplate([
+//         '<div class="title-bar clear" tabindex="0">',
+//             '<div class="title">',
+//                 '<span class="name"><%- element.element_identifier %></span>',
+//             '</div>',
+//             '<div class="subtitle"></div>',
+//         '</div>'
+//     ], 'element' );
+
+//     return _.extend( {}, DCEListItemView.prototype.templates, {
+//         titleBar : titleBarTemplate
+//     });
+// }());
+
+
+//==============================================================================
+/** @class Editable view for a DatasetCollectionElement that is also an DatasetAssociation
+ *      (a dataset contained in a dataset collection).
+ */
+var DatasetDCEListItemEdit = DATASET_LI_EDIT.DatasetListItemEdit.extend(
+/** @lends DatasetDCEListItemEdit.prototype */{
+
+    /** logger used to record this.log messages, commonly set to console */
+    //logger              : console,
+
+    /** set up */
+    initialize  : function( attributes ){
+        DATASET_LI_EDIT.DatasetListItemEdit.prototype.initialize.call( this, attributes );
+    },
+
+    // ......................................................................... misc
+    /** String representation */
+    toString : function(){
+        var modelString = ( this.model )?( this.model + '' ):( '(no model)' );
+        return 'DatasetDCEListItemEdit(' + modelString + ')';
+    }
+});
+
+// ............................................................................ TEMPLATES
+/** underscore templates */
+DatasetDCEListItemEdit.prototype.templates = (function(){
+
+    return _.extend( {}, DATASET_LI_EDIT.DatasetListItemEdit.prototype.templates, {
+        titleBar : DC_LI.DatasetDCEListItemView.prototype.templates.titleBar
+    });
+}());
+
+
+//==============================================================================
+/** @class Read only view for a DatasetCollectionElement that is also a DatasetCollection
+ *      (a nested DC).
+ */
+var NestedDCDCEListItemEdit = DC_LI.NestedDCDCEListItemView.extend(
+/** @lends NestedDCDCEListItemEdit.prototype */{
+
+    /** logger used to record this.log messages, commonly set to console */
+    // comment this out to suppress log output
+    //logger              : console,
+
+    // ......................................................................... misc
+    /** String representation */
+    toString : function(){
+        var modelString = ( this.model )?( this.model + '' ):( '(no model)' );
+        return 'NestedDCDCEListItemEdit(' + modelString + ')';
+    }
+});
+
+
 //==============================================================================
     return {
-        DCEListItemEdit : DCEListItemEdit
+        DCListItemEdit          : DCListItemEdit,
+        DCEListItemEdit         : DCEListItemEdit,
+        DatasetDCEListItemEdit  : DatasetDCEListItemEdit,
+        NestedDCDCEListItemEdit : NestedDCDCEListItemEdit
     };
 });
