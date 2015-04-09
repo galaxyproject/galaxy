8254caba549ddb39474827fe2f8de323d7b2d1d7
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2015-04-09 20:37:03 +0100

    Update many links which redirect to https.

--- lib/galaxy/config.py
+++ lib/galaxy/config.py
@@ -262,7 +262,7 @@ class Configuration( object ):
         self.message_box_content = kwargs.get( 'message_box_content', None )
         self.message_box_class = kwargs.get( 'message_box_class', 'info' )
         self.support_url = kwargs.get( 'support_url', 'https://wiki.galaxyproject.org/Support' )
-        self.wiki_url = kwargs.get( 'wiki_url', 'http://wiki.galaxyproject.org/' )
+        self.wiki_url = kwargs.get( 'wiki_url', 'https://wiki.galaxyproject.org/' )
         self.blog_url = kwargs.get( 'blog_url', None )
         self.screencasts_url = kwargs.get( 'screencasts_url', None )
         self.library_import_dir = kwargs.get( 'library_import_dir', None )
