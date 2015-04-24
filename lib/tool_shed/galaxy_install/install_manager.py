48cfa2b8aacbb55e19b90b985582b810c82095a7
Dannon Baker <dannon.baker@gmail.com>
2015-04-24 08:02:25 -0400

    Logging fix in toolshed install manager.

--- lib/tool_shed/galaxy_install/install_manager.py
+++ lib/tool_shed/galaxy_install/install_manager.py
@@ -1026,6 +1026,6 @@ def fetch_tool_versions( app, tool_shed_repository ):
             failed_to_fetch = True
     except Exception:
         failed_to_fetch = True
-    if failed_to_fetch:
         log.exception("Failed to fetch tool shed repository verion information.")
+    if failed_to_fetch:
         return FAILED_TO_FETCH_VERSIONS
