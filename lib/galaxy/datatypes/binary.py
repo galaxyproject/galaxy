bfe1e40480a61029652ecb0b5391b7b71ca5640c
anton <anton@nekrut.org>
2015-04-24 16:18:20 -0400

    Added default indexing for bcc files

--- lib/galaxy/datatypes/binary.py
+++ lib/galaxy/datatypes/binary.py
@@ -423,10 +423,11 @@ class Bam( Binary ):
 
 Binary.register_sniffable_binary_format("bam", "bam", Bam)
 
-
 class Bcf( Binary):
     """Class describing a BCF file"""
     file_ext = "bcf"
+
+    MetadataElement( name="bcf_index", desc="BCF Index File", param=metadata.FileParameter, file_ext="csi", readonly=True, no_value=None, visible=False, optional=True )
 
     def sniff( self, filename ):
         # BCF is compressed in the BGZF format, and must not be uncompressed in Galaxy.
@@ -438,6 +439,38 @@ class Bcf( Binary):
             return False
         except:
             return False
+
+    def set_meta( self, dataset, overwrite = True, **kwd ):
+        """ Creates the index for the BCF file. """
+        # These metadata values are not accessible by users, always overwrite
+        index_file = dataset.metadata.bcf_index
+        if not index_file:
+            index_file = dataset.metadata.spec['bcf_index'].param.new_file( dataset = dataset )
+        # Create the bcf index
+        ##$ bcftools index
+        ##Usage: bcftools index <in.bcf>
+
+        dataset_symlink = os.path.join( os.path.dirname( index_file.file_name ),
+                    '__dataset_%d_%s' % ( dataset.id, os.path.basename( index_file.file_name ) ) )
+        os.symlink( dataset.file_name, dataset_symlink )
+           
+        stderr_name = tempfile.NamedTemporaryFile( prefix = "bcf_index_stderr" ).name
+        command = [ 'bcftools', 'index', dataset_symlink ]
+        proc = subprocess.Popen( args=command, stderr=open( stderr_name, 'wb' ) )
+        exit_code = proc.wait()
+        shutil.move( dataset_symlink + '.csi', index_file.file_name )
+
+        stderr = open( stderr_name ).read().strip()
+        if stderr:
+            if exit_code != 0:
+                os.unlink( stderr_name ) #clean up
+                raise Exception, "Error Setting BCF Metadata: %s" % stderr
+            else:
+                print stderr
+        dataset.metadata.bcf_index = index_file
+        # Remove temp file
+        os.unlink( stderr_name )
+
 
 Binary.register_sniffable_binary_format("bcf", "bcf", Bcf)
 
