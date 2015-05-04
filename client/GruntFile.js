2c964227cbb8560130a92b7ae123d16861b6671a
Carl Eberhard <carlfeberhard@gmail.com>
2015-05-04 11:34:09 -0400

    Client build, styles: remove style build system from static/style

--- client/GruntFile.js
+++ client/GruntFile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
     "use strict";
 
+    // see the sub directory grunt-tasks/ for individual task definitions
     grunt.loadTasks( 'grunt-tasks' );
     grunt.registerTask( 'default', [ 'uglify' ] );
 };
