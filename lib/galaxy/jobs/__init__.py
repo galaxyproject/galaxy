galaxyproject/galaxy

commit af846d7b3923a5ef5c1ced7723ac6385e119a1df

Bugfix: History import when 'cleanup_job=never'.

lib/galaxy/jobs/__init__.py
@@ -789,8 +789,8 @@ class JobWrapper( object ):
         # If the job was deleted, call tool specific fail actions (used for e.g. external metadata) and clean up
         if self.tool:
             self.tool.job_failed( self, message, exception )
-        if self.app.config.cleanup_job == 'always' or (self.app.config.cleanup_job == 'onsuccess' and job.state == job.states.DELETED):
-            self.cleanup()
+        delete_files = self.app.config.cleanup_job == 'always' or (self.app.config.cleanup_job == 'onsuccess' and job.state == job.states.DELETED)
+        self.cleanup( delete_files=delete_files )
 
     def pause( self, job=None, message=None ):
         if job is None:
@@ -1067,22 +1067,27 @@ class JobWrapper( object ):
         self.sa_session.flush()
 
         log.debug( 'job %d ended' % self.job_id )
-        if self.app.config.cleanup_job == 'always' or ( not stderr and self.app.config.cleanup_job == 'onsuccess' ):
-            self.cleanup()
+        # TODO: Also fix "not stderr" below, use final_job_state.
+        delete_files = self.app.config.cleanup_job == 'always' or ( not stderr and self.app.config.cleanup_job == 'onsuccess' )
+        self.cleanup( delete_files=delete_files )
 
     def check_tool_output( self, stdout, stderr, tool_exit_code, job ):
         return check_output( self.tool, stdout, stderr, tool_exit_code, job )
 
-    def cleanup( self ):
-        # remove temporary files
+    def cleanup( self, delete_files=True ):
+        # At least one of these tool cleanup actions (job import), is needed
+        # for thetool to work properly, that is why one might want to run
+        # cleanup but not delete files.
         try:
-            for fname in self.extra_filenames:
-                os.remove( fname )
-            self.external_output_metadata.cleanup_external_metadata( self.sa_session )
+            if delete_files:
+                for fname in self.extra_filenames:
+                    os.remove( fname )
+                self.external_output_metadata.cleanup_external_metadata( self.sa_session )
             galaxy.tools.imp_exp.JobExportHistoryArchiveWrapper( self.job_id ).cleanup_after_job( self.sa_session )
             galaxy.tools.imp_exp.JobImportHistoryArchiveWrapper( self.app, self.job_id ).cleanup_after_job()
             galaxy.tools.genome_index.GenomeIndexToolWrapper( self.job_id ).postprocessing( self.sa_session, self.app )
-            self.app.object_store.delete(self.get_job(), base_dir='job_work', entire_dir=True, dir_only=True, extra_dir=str(self.job_id))
+            if delete_files:
+                self.app.object_store.delete(self.get_job(), base_dir='job_work', entire_dir=True, dir_only=True, extra_dir=str(self.job_id))
         except:
             log.exception( "Unable to cleanup job %d" % self.job_id )
 
@@ -1520,8 +1525,8 @@ class TaskWrapper(JobWrapper):
         # if the job was deleted, don't finish it
         if task.state == task.states.DELETED:
             # Job was deleted by an administrator
-            if self.app.config.cleanup_job in ( 'always', 'onsuccess' ):
-                self.cleanup()
+            delete_files = self.app.config.cleanup_job in ( 'always', 'onsuccess' )
+            self.cleanup( delete_files=delete_files )
             return
         elif task.state == task.states.ERROR:
             self.fail( task.info )

commit 0e49e3a00cd0081781694d4c4f962168e97e0b94

Fix scary traceback when using task splitting jobs fail.
Regression probably introduced in af846d7b3923a5ef5c1ced7723ac6385e119a1df, must not be to harmful since noone has noticed it though.

lib/galaxy/jobs/__init__.py
@@ -1801,7 +1801,7 @@ class TaskWrapper(JobWrapper):
         task.command_line = self.command_line
         self.sa_session.flush()
 
-    def cleanup( self ):
+    def cleanup( self, delete_files=True ):
         # There is no task cleanup.  The job cleans up for all tasks.
         pass
 