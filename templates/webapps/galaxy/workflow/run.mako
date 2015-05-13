galaxyproject/galaxy

commit c8a71c7e8988d80bf406f11bfb9b8d3ed815e97e

Sanitize display of workflow parameters in workflow run.mako.

templates/webapps/galaxy/workflow/run.mako
@@ -574,6 +574,7 @@ if wf_parms:
                     <%
                     pja_ss_all = []
                     for pja_ss in [ActionBox.get_short_str(pja) for pja in step.post_job_actions]:
+                        pja_ss = h.escape( pja_ss )
                         for rematch in re.findall('\$\{.+?\}', pja_ss):
                             pja_ss = pja_ss.replace(rematch, '<span style="background-color:%s" class="wfpspan wf_parm__%s pja_wfp">%s</span>' % (wf_parms[rematch[2:-1]], rematch[2:-1], rematch[2:-1]))
                         pja_ss_all.append(pja_ss)

commit 30557c0705d27c87912debcc690650fe3bc06ef8

Fix display regression from overzealous sanitzation in c8a71c7e8988d80bf406f11bfb9b8d3ed815e97e

templates/webapps/galaxy/workflow/run.mako
@@ -583,9 +583,10 @@ if wf_parms:
                     <%
                     pja_ss_all = []
                     for pja_ss in [ActionBox.get_short_str(pja) for pja in step.post_job_actions]:
-                        pja_ss = h.escape( pja_ss )
                         for rematch in re.findall('\$\{.+?\}', pja_ss):
-                            pja_ss = pja_ss.replace(rematch, '<span style="background-color:%s" class="wfpspan wf_parm__%s pja_wfp">%s</span>' % (wf_parms[rematch[2:-1]], rematch[2:-1], rematch[2:-1]))
+                            pja_ss = pja_ss.replace(rematch, '<span style="background-color:%s" class="wfpspan wf_parm__%s pja_wfp">%s</span>' % (h.escape(wf_parms[rematch[2:-1]]),
+                                                                                                                                                  h.escape(rematch[2:-1]),
+                                                                                                                                                  h.escape(rematch[2:-1])))
                         pja_ss_all.append(pja_ss)
                     %>
                     ${'<br/>'.join(pja_ss_all)}