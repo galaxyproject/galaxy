b20af47cbbeceac991d0e082e2ee099f0d0969f5
Jaclyn Smith <jacmarjorie@gmail.com>
2015-05-20 16:47:57 -0700

    multi-fastqc tool ready

--- tools/multi_fastqc/multi_fastqc.py
+++ tools/multi_fastqc/multi_fastqc.py
@@ -1,4 +1,5 @@
-# wrapper for RNASeq Explorer
+# Visualize the output of FastQC for multiple samples
+
 import os
 from argparse import ArgumentParser
 import pandas as pd
@@ -12,8 +13,8 @@ def explorer_argparse():
     
     parser = ArgumentParser(description='RNASeq Explorer: Gene Expression vs Drug Resistance')
     parser.add_argument("-i", "--input", dest = "input", nargs = "+", type=str, required=True, default=None, help="List of fastqc_data.txt files.")
+    parser.add_argument("-n", "--names", dest = "names", nargs = "+", type=str, required=True, default=None, help="Names to be displayed on legend.")
     parser.add_argument("-o1", "--output1", dest = "output1", required=True, type=str, default=None, help="Label Output File One")
-    # parser.add_argument("-o2", "--output2", dest = "output2", required=True, type=str, default=None, help="Label Output File Two")
 
     args = parser.parse_args()
     
@@ -35,24 +36,27 @@ def get_modules(sing_file):
             elif (line[:2] != ">>" and line[:1] != "#"): # grab details under each module
                 cols = line.split('\t')
                 file_dict[this_mod].append(cols)
-                #file_dict[this_mod] = file_dict[this_mod] + line + '\n'
     return file_dict
 
-def plot_per_seq_quality(in_files, output):
+def plot_per_seq_quality(in_files, output, file_names):
     with PdfPages(output) as pdf:
-        for input_f in in_files:
-            list_ = input_f.split('/')
-            leg_name = list_[len(list_)-1][:-4]
-            f_dict = get_modules(input_f)
+
+        for i in range(0, len(in_files)):
+            list_ = in_files[i].split('/')
+            leg_name = file_names[i]
+            f_dict = get_modules(in_files[i])
             arr_ = np.array(f_dict['Per sequence quality scores'])
             plt.plot(arr_[:,0], arr_[:,1], label=leg_name)
-        plt.legend(loc='upper left')
         plt.ylabel('Count')
         plt.xlabel('Mean Sequence Quality')
         plt.title('Per Sequence Quality Scores')
+        ax = plt.subplot(111)
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         pdf.savefig()
         plt.close()
 
 if __name__ == "__main__":
     args = explorer_argparse()
-    plot_per_seq_quality(vars(args)['input'], vars(args)['output1'])
+    plot_per_seq_quality(vars(args)['input'], vars(args)['output1'], vars(args)['names'])
