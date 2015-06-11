7bea3621b6afd533dbb59eb5272c16976a1c837e
Carl Eberhard <carlfeberhard@gmail.com>
2015-06-11 09:33:53 -0400

    Client build, toolshed: allow passing an 'app' option to the client/GruntFile, use 'grunt --app=toolshed' to watch for changes in toolshed scripts, duplicate our (unusual) galaxy scripts directories for the toolshed app (test-file.js to be removed).

--- client/GruntFile.js
+++ client/GruntFile.js
@@ -1,7 +1,26 @@
 module.exports = function(grunt) {
     "use strict";
 
+    var GALAXY_PATHS = {
+            dist        : '../static/scripts',
+            maps        : '../static/maps',
+            // this symlink allows us to serve uncompressed scripts in DEV_PATH for use with sourcemaps
+            srcSymlink  : '../static/src',
+        },
+        TOOLSHED_PATHS = {
+            dist        : '../static/toolshed/scripts',
+            maps        : '../static/toolshed/maps',
+            srcSymlink  : '../static/toolshed/src',
+        };
+
+    grunt.config.set( 'app', 'galaxy' );
+    grunt.config.set( 'paths', GALAXY_PATHS );
+    if( grunt.option( 'app' ) === 'toolshed' ){
+	    grunt.config.set( 'app', grunt.option( 'app' ) );
+	    grunt.config.set( 'paths', TOOLSHED_PATHS );
+    }
+
     // see the sub directory grunt-tasks/ for individual task definitions
     grunt.loadTasks( 'grunt-tasks' );
-    grunt.registerTask( 'default', [ 'uglify' ] );
+    grunt.registerTask( 'default', [ 'uglify:galaxy' ] );
 };
