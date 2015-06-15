db5c81acd78728dee37173aaf034024af2e5f302
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2015-06-15 20:04:06 +0100

    Simplify strict_min strict_max logic.

--- lib/galaxy/tools/parameters/validation.py
+++ lib/galaxy/tools/parameters/validation.py
@@ -146,12 +146,9 @@ class InRangeValidator( Validator ):
         self_max_str = str( self.max ).rstrip( '0' ).rstrip( '.' )
         op1 = '>='
         op2 = '<='
-        if self.strict_min and self.strict_max:
-            op1 = '>'
-            op2 = '<'
         if self.strict_min:
             op1 = '>'
-        elif self.strict_max:
+        if self.strict_max:
             op2 = '<'
         self.message = message or "Value must be %s %s and %s %s" % ( op1, self_min_str, op2, self_max_str )
 
