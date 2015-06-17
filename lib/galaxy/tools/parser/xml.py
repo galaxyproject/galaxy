galaxyproject/galaxy

commit 3c40f32fa2c6024716d3fcb0e6b5a66a7ef3dd3a

PEP-8 and pylint fixes.

lib/galaxy/tools/parser/xml.py
@@ -236,7 +236,7 @@ class XmlToolSource(ToolSource):
         output.tool = tool
         output.from_work_dir = data_elem.get("from_work_dir", None)
         output.hidden = string_as_bool( data_elem.get("hidden", "") )
-        output.actions = galaxy.tools.ToolOutputActionGroup( output, data_elem.find( 'actions' ) )
+        output.actions = galaxy.tools.parameters.output.ToolOutputActionGroup( output, data_elem.find( 'actions' ) )
         output.dataset_collectors = output_collect.dataset_collectors_from_elem( data_elem )
         return output
 

lib/galaxy/tools/parser/yaml.py
@@ -87,7 +87,7 @@ class YamlToolSource(ToolSource):
         output.tool = tool
         output.from_work_dir = output_dict.get("from_work_dir", None)
         output.hidden = output_dict.get("hidden", "")
-        output.actions = galaxy.tools.ToolOutputActionGroup( output, None )
+        output.actions = galaxy.tools.parameters.output.ToolOutputActionGroup( output, None )
         discover_datasets_dicts = output_dict.get( "discover_datasets", [] )
         if isinstance( discover_datasets_dicts, dict ):
             discover_datasets_dicts = [ discover_datasets_dicts ]

lib/galaxy/tools/actions/__init__.py
@@ -1,8 +1,7 @@
 from galaxy.exceptions import ObjectInvalid
 from galaxy.model import LibraryDatasetDatasetAssociation
 from galaxy import model
-from galaxy.tools.parameters import DataToolParameter
-from galaxy.tools.parameters import DataCollectionToolParameter
+from galaxy.tools.parameters.basic import DataCollectionToolParameter, DataToolParameter
 from galaxy.tools.parameters.wrapped import WrappedParameters
 from galaxy.util import ExecutionTimer
 from galaxy.util.json import dumps
@@ -51,7 +50,7 @@ class DefaultToolAction( object ):
                             data = converted_dataset
                         else:
                             # FIXME: merge with hda.get_converted_dataset() mode as it's nearly identical.
-                            #run converter here
+                            # run converter here
                             new_data = data.datatype.convert_dataset( trans, data, target_ext, return_output=True, visible=False ).values()[0]
                             new_data.hid = data.hid
                             new_data.name = data.name
@@ -85,12 +84,12 @@ class DefaultToolAction( object ):
                         if parent:
                             parent[input.name][i] = input_datasets[ prefix + input.name + str( i + 1 ) ]
                             for conversion_name, conversion_data in conversions:
-                                #allow explicit conversion to be stored in job_parameter table
+                                # allow explicit conversion to be stored in job_parameter table
                                 parent[ conversion_name ][i] = conversion_data.id  # a more robust way to determine JSONable value is desired
                         else:
                             param_values[input.name][i] = input_datasets[ prefix + input.name + str( i + 1 ) ]
                             for conversion_name, conversion_data in conversions:
-                                #allow explicit conversion to be stored in job_parameter table
+                                # allow explicit conversion to be stored in job_parameter table
                                 param_values[ conversion_name ][i] = conversion_data.id  # a more robust way to determine JSONable value is desired
                 else:
                     input_datasets[ prefix + input.name ] = process_dataset( value )
@@ -107,7 +106,7 @@ class DefaultToolAction( object ):
                         target_dict = param_values
                     target_dict[ input.name ] = input_datasets[ prefix + input.name ]
                     for conversion_name, conversion_data in conversions:
-                        #allow explicit conversion to be stored in job_parameter table
+                        # allow explicit conversion to be stored in job_parameter table
                         target_dict[ conversion_name ] = conversion_data.id  # a more robust way to determine JSONable value is desired
             elif isinstance( input, DataCollectionToolParameter ):
                 if not value:
@@ -121,7 +120,7 @@ class DefaultToolAction( object ):
                     # some point and figure out if implicitly converting a
                     # dataset collection makes senese.
 
-                    #if i == 0:
+                    # if i == 0:
                     #    # Allow copying metadata to output, first item will be source.
                     #    input_datasets[ prefix + input.name ] = data.dataset_instance
                     input_datasets[ prefix + input.name + str( i + 1 ) ] = data
@@ -229,8 +228,8 @@ class DefaultToolAction( object ):
             if output.parent:
                 parent_to_child_pairs.append( ( output.parent, name ) )
                 child_dataset_names.add( name )
-            ## What is the following hack for? Need to document under what
-            ## conditions can the following occur? (james@bx.psu.edu)
+            # What is the following hack for? Need to document under what
+            # conditions can the following occur? (james@bx.psu.edu)
             # HACK: the output data has already been created
             #      this happens i.e. as a result of the async controller
             if name in incoming:
@@ -276,7 +275,7 @@ class DefaultToolAction( object ):
             # Store output
             out_data[ name ] = data
             if output.actions:
-                #Apply pre-job tool-output-dataset actions; e.g. setting metadata, changing format
+                # Apply pre-job tool-output-dataset actions; e.g. setting metadata, changing format
                 output_action_params = dict( out_data )
                 output_action_params.update( incoming )
                 output.actions.apply_action( data, output_action_params )
@@ -386,7 +385,7 @@ class DefaultToolAction( object ):
         for name, dataset in inp_data.iteritems():
             if dataset:
                 if not trans.app.security_agent.can_access_dataset( current_user_roles, dataset.dataset ):
-                    raise "User does not have permission to use a dataset (%s) provided for input." % data.id
+                    raise Exception("User does not have permission to use a dataset (%s) provided for input." % data.id)
                 job.add_input_dataset( name, dataset )
             else:
                 job.add_input_dataset( name, None )
@@ -434,7 +433,7 @@ class DefaultToolAction( object ):
                             trans.sa_session.add(jtid)
                     jtod.dataset.visible = False
                     trans.sa_session.add(jtod)
-            except Exception, e:
+            except Exception:
                 log.exception('Cannot remap rerun dependencies.')
         trans.sa_session.flush()
         # Some tools are not really executable, but jobs are still created for them ( for record keeping ).
@@ -554,7 +553,7 @@ def determine_output_format(output, parameter_context, input_datasets, random_in
         except Exception:
             pass
 
-    #process change_format tags
+    # process change_format tags
     if output.change_format is not None:
         new_format_set = False
         for change_elem in output.change_format:
@@ -563,7 +562,7 @@ def determine_output_format(output, parameter_context, input_datasets, random_in
                 if check is not None:
                     try:
                         if '$' not in check:
-                            #allow a simple name or more complex specifications
+                            # allow a simple name or more complex specifications
                             check = '${%s}' % check
                         if str( fill_template( check, context=parameter_context ) ) == when_elem.get( 'value', None ):
                             ext = when_elem.get( 'format', ext )

lib/galaxy/tools/actions/data_manager.py
@@ -3,6 +3,7 @@ from __init__ import DefaultToolAction
 import logging
 log = logging.getLogger( __name__ )
 
+
 class DataManagerToolAction( DefaultToolAction ):
     """Tool action used for Data Manager Tools"""
 

lib/galaxy/tools/actions/data_source.py
@@ -3,6 +3,7 @@ from __init__ import DefaultToolAction
 import logging
 log = logging.getLogger( __name__ )
 
+
 class DataSourceToolAction( DefaultToolAction ):
     """Tool action used for Data Source Tools"""
 

lib/galaxy/tools/actions/history_imp_exp.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 
 from galaxy.tools.actions import ToolAction
-from galaxy.tools.imp_exp import JobExportHistoryArchiveWrapper, JobImportHistoryArchiveWrapper
+from galaxy.tools.imp_exp import JobExportHistoryArchiveWrapper
 from galaxy.util.odict import odict
 
 import logging

lib/galaxy/tools/actions/metadata.py
@@ -6,7 +6,6 @@ from galaxy.util.odict import odict
 from galaxy.util.json import dumps
 from galaxy.jobs.datasets import DatasetPath
 
-
 log = logging.getLogger( __name__ )
 
 
@@ -25,7 +24,7 @@ class SetMetadataToolAction( ToolAction ):
         return job, odict
 
     def execute_via_app( self, tool, app, session_id, history_id, user=None,
-                         incoming = {}, set_output_hid = False, overwrite = True,
+                         incoming={}, set_output_hid=False, overwrite=True,
                          history=None, job_params=None ):
         """
         Execute using application.
@@ -55,18 +54,18 @@ class SetMetadataToolAction( ToolAction ):
             job.user_id = user.id
         if job_params:
             job.params = dumps( job_params )
-        start_job_state = job.state #should be job.states.NEW
+        start_job_state = job.state  # should be job.states.NEW
         try:
             # For backward compatibility, some tools may not have versions yet.
             job.tool_version = tool.version
         except:
             job.tool_version = "1.0.1"
-        job.state = job.states.WAITING #we need to set job state to something other than NEW, or else when tracking jobs in db it will be picked up before we have added input / output parameters
+        job.state = job.states.WAITING  # we need to set job state to something other than NEW, or else when tracking jobs in db it will be picked up before we have added input / output parameters
         job.set_handler(tool.get_job_handler( job_params ))
         sa_session.add( job )
-        sa_session.flush() #ensure job.id is available
+        sa_session.flush()  # ensure job.id is available
 
-        #add parameters to job_parameter table
+        # add parameters to job_parameter table
         # Store original dataset state, so we can restore it. A separate table might be better (no chance of 'losing' the original state)?
         incoming[ '__ORIGINAL_DATASET_STATE__' ] = dataset.state
         input_paths = [DatasetPath( dataset.id, real_path=dataset.file_name, mutable=False )]
@@ -75,36 +74,36 @@ class SetMetadataToolAction( ToolAction ):
         external_metadata_wrapper = JobExternalOutputMetadataWrapper( job )
         cmd_line = external_metadata_wrapper.setup_external_metadata( dataset,
                                                                       sa_session,
-                                                                      exec_dir = None,
-                                                                      tmp_dir = job_working_dir,
-                                                                      dataset_files_path = app.model.Dataset.file_path,
-                                                                      output_fnames = input_paths,
-                                                                      config_root = app.config.root,
-                                                                      config_file = app.config.config_file,
-                                                                      datatypes_config = app.datatypes_registry.integrated_datatypes_configs,
-                                                                      job_metadata = None,
-                                                                      include_command = False,
-                                                                      kwds = { 'overwrite' : overwrite } )
+                                                                      exec_dir=None,
+                                                                      tmp_dir=job_working_dir,
+                                                                      dataset_files_path=app.model.Dataset.file_path,
+                                                                      output_fnames=input_paths,
+                                                                      config_root=app.config.root,
+                                                                      config_file=app.config.config_file,
+                                                                      datatypes_config=app.datatypes_registry.integrated_datatypes_configs,
+                                                                      job_metadata=None,
+                                                                      include_command=False,
+                                                                      kwds={ 'overwrite' : overwrite } )
         incoming[ '__SET_EXTERNAL_METADATA_COMMAND_LINE__' ] = cmd_line
         for name, value in tool.params_to_strings( incoming, app ).iteritems():
             job.add_parameter( name, value )
-        #add the dataset to job_to_input_dataset table
+        # add the dataset to job_to_input_dataset table
         if type == 'hda':
             job.add_input_dataset( dataset_name, dataset )
         elif type == 'ldda':
             job.add_input_library_dataset( dataset_name, dataset )
-        #Need a special state here to show that metadata is being set and also allow the job to run
-        #   i.e. if state was set to 'running' the set metadata job would never run, as it would wait for input (the dataset to set metadata on) to be in a ready state
+        # Need a special state here to show that metadata is being set and also allow the job to run
+        # i.e. if state was set to 'running' the set metadata job would never run, as it would wait for input (the dataset to set metadata on) to be in a ready state
         dataset._state = dataset.states.SETTING_METADATA
-        job.state = start_job_state #job inputs have been configured, restore initial job state
+        job.state = start_job_state  # job inputs have been configured, restore initial job state
         sa_session.flush()
 
         # Queue the job for execution
         app.job_queue.put( job.id, tool.id )
         # FIXME: need to add event logging to app and log events there rather than trans.
-        #trans.log_event( "Added set external metadata job to the job queue, id: %s" % str(job.id), tool_id=job.tool_id )
+        # trans.log_event( "Added set external metadata job to the job queue, id: %s" % str(job.id), tool_id=job.tool_id )
 
-        #clear e.g. converted files
+        # clear e.g. converted files
         dataset.datatype.before_setting_metadata( dataset )
 
         return job, odict()

commit 10fc5196335530699d7a9f414d2789d1adc79ff2

Fix bug introduced in 3c40f32fa2c6024716d3fcb0e6b5a66a7ef3dd3a .

lib/galaxy/tools/parser/xml.py
@@ -25,6 +25,7 @@ from galaxy.tools.deps import requirements
 import galaxy.tools
 from galaxy.tools.parameters import output_collect
 from galaxy.tools.parameters import dynamic_options
+from galaxy.tools.parameters.output import ToolOutputActionGroup
 
 log = logging.getLogger( __name__ )
 
@@ -236,7 +237,7 @@ class XmlToolSource(ToolSource):
         output.tool = tool
         output.from_work_dir = data_elem.get("from_work_dir", None)
         output.hidden = string_as_bool( data_elem.get("hidden", "") )
-        output.actions = galaxy.tools.parameters.output.ToolOutputActionGroup( output, data_elem.find( 'actions' ) )
+        output.actions = ToolOutputActionGroup( output, data_elem.find( 'actions' ) )
         output.dataset_collectors = output_collect.dataset_collectors_from_elem( data_elem )
         return output
 

lib/galaxy/tools/parser/yaml.py
@@ -6,6 +6,7 @@ from .util import error_on_exit_code
 
 from galaxy.tools.deps import requirements
 from galaxy.tools.parameters import output_collect
+from galaxy.tools.parameters.output import ToolOutputActionGroup
 from galaxy.util.odict import odict
 import galaxy.tools
 
@@ -87,7 +88,7 @@ class YamlToolSource(ToolSource):
         output.tool = tool
         output.from_work_dir = output_dict.get("from_work_dir", None)
         output.hidden = output_dict.get("hidden", "")
-        output.actions = galaxy.tools.parameters.output.ToolOutputActionGroup( output, None )
+        output.actions = ToolOutputActionGroup( output, None )
         discover_datasets_dicts = output_dict.get( "discover_datasets", [] )
         if isinstance( discover_datasets_dicts, dict ):
             discover_datasets_dicts = [ discover_datasets_dicts ]