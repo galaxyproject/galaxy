7830bc62e09bd5928b08f2cde93bae8815a6ba45
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2015-06-17 19:21:13 +0100

    Another fix for 3c40f32fa2c6024716d3fcb0e6b5a66a7ef3dd3a .

--- tools/data_source/upload.py
+++ tools/data_source/upload.py
@@ -14,6 +14,8 @@ import urllib
 import zipfile
 
 from galaxy import util
+# need to import model before sniff to resolve a circular import dependency
+import galaxy.model  # noqa
 from galaxy.datatypes import sniff
 from galaxy.datatypes.binary import Binary
 from galaxy.datatypes.checkers import check_binary, check_bz2, check_gzip, check_html, check_image, check_zip
