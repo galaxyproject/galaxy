f90e2e12f63225e81f48be8236f6f5bc5dd9a60c
John Chilton <jmchilton@gmail.com>
2015-06-22 14:32:02 -0400

    Fix stray print noticed by @nsoranzo.
    And add that file to pep8 sources.

--- test/unit/jobs/test_job_configuration.py
+++ test/unit/jobs/test_job_configuration.py
@@ -131,7 +131,6 @@ class JobConfXmlParserTestCase( unittest.TestCase ):
     def test_macro_expansion( self ):
         self.__with_advanced_config()
         for name in ["foo_small", "foo_medium", "foo_large", "foo_longrunning"]:
-            print self.job_config.destinations.keys()
             assert self.job_config.destinations[ name ]
 
     # TODO: Add job metrics parsing test.
