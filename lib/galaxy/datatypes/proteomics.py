98edbadf685adcb252d31e4de40c6e5e328e85e3
Ira Cooke <iracooke@gmail.com>
2015-06-24 09:54:30 +1000

    Syntax fixes

--- lib/galaxy/datatypes/proteomics.py
+++ lib/galaxy/datatypes/proteomics.py
@@ -288,7 +288,7 @@ class Msp(Text):
         """ Determines whether the file is a NIST MSP output file."""
         with open(filename, 'r') as f:
             begin_contents = f.read(1024)
-            if not in "\n" begin_contents:
+            if begin_contents not in "\n":
                 return False
             lines = begin_contents.splitlines()
             if len(lines) < 2:
