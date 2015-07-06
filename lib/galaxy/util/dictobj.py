880f8c509c69312bfb980b3a659b602f1dcacb2f
Remi Marenco <remi.marenco@gmail.com>
2015-07-06 18:20:44 +0100

    Fixed an indentation in dictobj

--- lib/galaxy/util/dictobj.py
+++ lib/galaxy/util/dictobj.py
@@ -3,38 +3,38 @@ import pickle
 
 class DictionaryObject(object):
     """
-  Copyright 2012 "Grim Apps"
+    Copyright 2012 "Grim Apps"
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
        http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 
-  Helper class written by William Grim - grimwm
-  Original repo: https://github.com/grimwm/py-dictobj
-  Thank you!
-  ************************
-  A class that has all the functionality of a normal Python dictionary, except
-  for the fact it is itself immutable.  It also has the added feature of
-  being able to lookup values by using keys as attributes.
+    Helper class written by William Grim - grimwm
+    Original repo: https://github.com/grimwm/py-dictobj
+    Thank you!
+    ************************
+    A class that has all the functionality of a normal Python dictionary, except
+    for the fact it is itself immutable.  It also has the added feature of
+    being able to lookup values by using keys as attributes.
 
-  The reason for the class being immutable by default is to help make it a
-  little easier to use in multiprocessing situations.  Granted, the underlying
-  values themselves are not deeply copied, but the aim is to enforce some
-  ensurances of immutability on the container class.
+    The reason for the class being immutable by default is to help make it a
+    little easier to use in multiprocessing situations.  Granted, the underlying
+    values themselves are not deeply copied, but the aim is to enforce some
+    ensurances of immutability on the container class.
 
-  When using positional arguments, the first argument must always be something
-  that would be a valid argument for a dict().  However, a second, optional
-  argument may be passed to create a default value when keys are not found.
+    When using positional arguments, the first argument must always be something
+    that would be a valid argument for a dict().  However, a second, optional
+    argument may be passed to create a default value when keys are not found.
 
-  Examples:
+    Examples:
     >>> d = DictionaryObject({'a':1, 'b':True, 3:'x'})
     >>> print d.a, d.b, d[3]
     1 True x
