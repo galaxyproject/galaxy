f623e28ae9342ca30750c9c0b08120a0f24e2b4e
Dannon Baker <dannon.baker@gmail.com>
2015-07-13 12:37:51 -0400

    Fix alignment in model

--- lib/galaxy/model/__init__.py
+++ lib/galaxy/model/__init__.py
@@ -1218,8 +1218,8 @@ class History( object, Dictifiable, UsesAnnotations, HasName ):
             func.sum( db_session.query( HistoryDatasetAssociation.dataset_id, Dataset.total_size ).join( Dataset )
                     .filter( HistoryDatasetAssociation.table.c.history_id == self.id )
                     .filter( HistoryDatasetAssociation.purged != true() )
-                                            .filter( Dataset.purged != true() )
-                                            .distinct().subquery().c.total_size ) ).first()[0]
+                    .filter( Dataset.purged != true() )
+                    .distinct().subquery().c.total_size ) ).first()[0]
         if rval is None:
             rval = 0
         if nice_size:
