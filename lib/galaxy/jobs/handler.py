b0c6512c5b61e9e948d9d8304973b03d07b8d1c7
Dannon Baker <dannon.baker@gmail.com>
2015-07-20 15:36:59 -0400

    Fix more sqlalchemy comparisons broken during flake8'ing.

--- lib/galaxy/jobs/handler.py
+++ lib/galaxy/jobs/handler.py
@@ -112,7 +112,7 @@ class JobHandlerQueue( object ):
                     .outerjoin( model.User ) \
                     .filter( model.Job.state.in_( in_list ) &
                              ( model.Job.handler == self.app.config.server_name ) &
-                             or_( ( model.Job.user_id is None ), ( model.User.active == true() ) ) ).all()
+                             or_( ( model.Job.user_id == null() ), ( model.User.active == true() ) ) ).all()
         else:
             jobs_at_startup = self.sa_session.query( model.Job ).enable_eagerloads( False ) \
                 .filter( model.Job.state.in_( in_list ) &
@@ -220,7 +220,7 @@ class JobHandlerQueue( object ):
                 jobs_to_check = self.sa_session.query(model.Job).enable_eagerloads(False) \
                     .outerjoin( model.User ) \
                     .filter(and_((model.Job.state == model.Job.states.NEW),
-                                 or_((model.Job.user_id is None), (model.User.active == true())),
+                                 or_((model.Job.user_id == null()), (model.User.active == true())),
                                  (model.Job.handler == self.app.config.server_name),
                                  ~model.Job.table.c.id.in_(hda_not_ready),
                                  ~model.Job.table.c.id.in_(ldda_not_ready))) \
