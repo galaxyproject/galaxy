ea89dd8f149778b6c2f0f3f4a34c8b21f7033af7
Vimalkumar Velayudhan <vimalkumarvelayudhan@gmail.com>
2015-07-20 12:30:58 +0100

    Use object store for imported history datasets and included files

--- lib/galaxy/tools/imp_exp/__init__.py
+++ lib/galaxy/tools/imp_exp/__init__.py
@@ -196,25 +196,22 @@ class JobImportHistoryArchiveWrapper( object, UsesAnnotations ):
                         if not file_in_dir( temp_dataset_file_name, os.path.join( archive_dir, "datasets" ) ):
                             raise Exception( "Invalid dataset path: %s" % temp_dataset_file_name )
                         if datasets_usage_counts[ temp_dataset_file_name ] == 1:
-                            shutil.move( temp_dataset_file_name, hda.file_name )
+                            self.app.object_store.update_from_file( hda.dataset, file_name=temp_dataset_file_name, create=True )
 
                             # Import additional files if present. Histories exported previously might not have this attribute set.
-                            dataset_extra_files_path = ''
-                            try:
-                                dataset_extra_files_path = dataset_attrs[ 'extra_files_path' ]
-                            except KeyError:
-                                pass
-
+                            dataset_extra_files_path = dataset_attrs.get( 'extra_files_path', None )
                             if dataset_extra_files_path:
                                 try:
                                     file_list = os.listdir( os.path.join( archive_dir, dataset_extra_files_path ) )
                                 except OSError:
                                     file_list = []
 
-                                if len( file_list ):
-                                    os.mkdir( hda.extra_files_path )
+                                if file_list:
                                     for extra_file in file_list:
-                                        shutil.move( os.path.join( archive_dir, dataset_extra_files_path, extra_file ), hda.extra_files_path )
+                                        self.app.object_store.update_from_file(
+                                            hda.dataset, extra_dir='dataset_%s_files' % hda.dataset.id,
+                                            alt_name=extra_file, file_name=os.path.join( archive_dir, dataset_extra_files_path, extra_file ),
+                                            create=True )
                         else:
                             datasets_usage_counts[ temp_dataset_file_name ] -= 1
                             shutil.copyfile( temp_dataset_file_name, hda.file_name )
