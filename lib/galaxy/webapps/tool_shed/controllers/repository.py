galaxyproject/galaxy

commit 0f2e2152ee17532488a8231cf1220ed9ddddb3aa

Cleanup of the toolshed's url handling methods.

lib/galaxy/webapps/tool_shed/controllers/repository.py
@@ -902,13 +902,13 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
             no_update = 'false'
         elif galaxy_url:
             # Start building up the url to redirect back to the calling Galaxy instance.
-            params = '?tool_shed_url=%s&name=%s&owner=%s&changeset_revision=%s&latest_changeset_revision=' % \
-                ( web.url_for( '/', qualified=True ),
-                  str( repository.name ),
-                  str( repository.user.username ),
-                  changeset_revision )
-            url = common_util.url_join( galaxy_url,
-                                        'admin_toolshed/update_to_changeset_revision%s' % params )
+            params = dict( tool_shed_url=web.url_for( '/', qualified=True ),
+                           name=str( repository.name ),
+                           owner=str( repository.user.username ),
+                           changeset_revision=changeset_revision,
+                           latest_changeset_revision=latest_changeset_revision )
+            pathspec = [ 'admin_toolshed', 'update_to_changeset_revision' ]
+            url = common_util.url_join( galaxy_url, pathspec=pathspec, params=params )
         else:
             message = 'Unable to check for updates due to an invalid Galaxy URL: <b>%s</b>.  ' % galaxy_url
             message += 'You may need to enable third-party cookies in your browser.  '
@@ -1219,12 +1219,8 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
         trans.sa_session.add( repository )
         trans.sa_session.flush()
         tool_shed_url = web.url_for( '/', qualified=True )
-        download_url = common_util.url_join( tool_shed_url,
-                                             'repos',
-                                             str( repository.user.username ),
-                                             str( repository.name ),
-                                             'archive',
-                                             file_type_str )
+        pathspec = [ 'repos', str( repository.user.username ), str( repository.name ), 'archive', file_type_str ]
+        download_url = common_util.url_join( tool_shed_url, pathspec=pathspec )
         return trans.response.send_redirect( download_url )
 
     @web.expose
@@ -1692,11 +1688,8 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
                     else:
                         time_tested = repository_metadata.time_last_tested.strftime( '%a, %d %b %Y %H:%M:%S UT' )
                     # Generate a citable URL for this repository with owner and changeset revision.
-                    repository_citable_url = common_util.url_join( tool_shed_url,
-                                                                   'view',
-                                                                   str( user.username ),
-                                                                   str( repository.name ),
-                                                                   str( repository_metadata.changeset_revision ) )
+                    pathspec = [ 'view', str( user.username ), str( repository.name ), str( repository_metadata.changeset_revision ) ]
+                    repository_citable_url = common_util.url_join( tool_shed_url, pathspec=pathspec )
                     passed_tests = len( tool_test_results.get( 'passed_tests', [] ) )
                     failed_tests = len( tool_test_results.get( 'failed_tests', [] ) )
                     missing_test_components = len( tool_test_results.get( 'missing_test_components', [] ) )
@@ -2132,12 +2125,11 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
         galaxy_url = common_util.handle_galaxy_url( trans, **kwd )
         if galaxy_url:
             # Redirect back to local Galaxy to perform install.
-            params = '?tool_shed_url=%s&repository_ids=%s&changeset_revisions=%s' % \
-                ( web.url_for( '/', qualified=True ),
-                  ','.join( util.listify( repository_ids ) ),
-                  ','.join( util.listify( changeset_revisions ) ) )
-            url = common_util.url_join( galaxy_url,
-                                        'admin_toolshed/prepare_for_install%s' % params )
+            params = dict( tool_shed_url=web.url_for( '/', qualified=True ),
+                           repository_ids=','.join( util.listify( repository_ids ) ),
+                           changeset_revisions=','.join( util.listify( changeset_revisions ) ) )
+            pathspec = [ 'admin_toolshed', 'prepare_for_install' ]
+            url = common_util.url_join( galaxy_url, pathspec=pathspec, params=params )
             return trans.response.send_redirect( url )
         else:
             message = 'Repository installation is not possible due to an invalid Galaxy URL: <b>%s</b>.  ' % galaxy_url

lib/galaxy/webapps/galaxy/controllers/admin.py
@@ -779,9 +779,9 @@ class AdminGalaxy( BaseUIController, Admin, AdminActions, UsesQuotaMixin, QuotaP
                     tool_dependencies_dict = {}
                     repository_name = elem.get( 'name' )
                     changeset_revision = elem.get( 'changeset_revision' )
-                    url = '%s/repository/get_tool_dependencies?name=%s&owner=devteam&changeset_revision=%s' % \
-                        ( tool_shed_url, repository_name, changeset_revision )
-                    text = common_util.tool_shed_get( trans.app, tool_shed_url, url )
+                    params = dict( name=repository_name, owner='devteam', changeset_revision=changeset_revision )
+                    pathspec = [ 'repository', 'get_tool_dependencies' ]
+                    text = common_util.tool_shed_get( trans.app, tool_shed_url, pathspec=pathspec, params=params )
                     if text:
                         tool_dependencies_dict = encoding_util.tool_shed_decode( text )
                         for dependency_key, requirements_dict in tool_dependencies_dict.items():

lib/galaxy/webapps/galaxy/controllers/admin_toolshed.py
@@ -189,8 +189,8 @@ class AdminToolshed( AdminGalaxy ):
     def browse_tool_shed( self, trans, **kwd ):
         tool_shed_url = kwd.get( 'tool_shed_url', '' )
         tool_shed_url = common_util.get_tool_shed_url_from_tool_shed_registry( trans.app, tool_shed_url )
-        galaxy_url = web.url_for( '/', qualified=True )
-        url = common_util.url_join( tool_shed_url, 'repository/browse_valid_categories?galaxy_url=%s' % ( galaxy_url ) )
+        params=dict( galaxy_url=web.url_for( '/', qualified=True ) )
+        url = common_util.url_join( tool_shed_url, pathspec=[ 'repository', 'browse_valid_categories' ], params=params )
         return trans.response.send_redirect( url )
 
     @web.expose
@@ -208,12 +208,12 @@ class AdminToolshed( AdminGalaxy ):
         repository_id = kwd.get( 'id', None )
         repository = repository_util.get_installed_tool_shed_repository( trans.app, repository_id )
         tool_shed_url = common_util.get_tool_shed_url_from_tool_shed_registry( trans.app, str( repository.tool_shed ) )
-        params = '?galaxy_url=%s&name=%s&owner=%s&changeset_revision=%s' % \
-            ( web.url_for( '/', qualified=True ),
-              str( repository.name ),
-              str( repository.owner ),
-              str( repository.changeset_revision ) )
-        url = common_util.url_join( tool_shed_url, 'repository/check_for_updates%s' % params )
+        params = dict( galaxy_url=web.url_for( '/', qualified=True ),
+                       name=str( repository.name ),
+                       owner=str( repository.owner ),
+                       changeset_revision=str( repository.changeset_revision ) )
+        pathspec = [ 'repository', 'check_for_updates' ]
+        url = common_util.url_join( tool_shed_url, pathspec=pathspec, params=params )
         return trans.response.send_redirect( url )
 
     @web.expose
@@ -369,8 +369,8 @@ class AdminToolshed( AdminGalaxy ):
     def find_tools_in_tool_shed( self, trans, **kwd ):
         tool_shed_url = kwd.get( 'tool_shed_url', '' )
         tool_shed_url = common_util.get_tool_shed_url_from_tool_shed_registry( trans.app, tool_shed_url )
-        galaxy_url = web.url_for( '/', qualified=True )
-        url = common_util.url_join( tool_shed_url, 'repository/find_tools?galaxy_url=%s' % galaxy_url )
+        params = dict( galaxy_url=web.url_for( '/', qualified=True ) )
+        url = common_util.url_join( tool_shed_url, pathspec=[ 'repository', 'find_tools' ], params=params )
         return trans.response.send_redirect( url )
 
     @web.expose
@@ -378,8 +378,8 @@ class AdminToolshed( AdminGalaxy ):
     def find_workflows_in_tool_shed( self, trans, **kwd ):
         tool_shed_url = kwd.get( 'tool_shed_url', '' )
         tool_shed_url = common_util.get_tool_shed_url_from_tool_shed_registry( trans.app, tool_shed_url )
-        galaxy_url = web.url_for( '/', qualified=True )
-        url = common_util.url_join( tool_shed_url, 'repository/find_workflows?galaxy_url=%s' % galaxy_url )
+        params = dict( galaxy_url=web.url_for( '/', qualified=True ) )
+        url = common_util.url_join( tool_shed_url, pathspec=[ 'repository', 'find_workflows' ], params=params )
         return trans.response.send_redirect( url )
 
     @web.expose
@@ -412,10 +412,9 @@ class AdminToolshed( AdminGalaxy ):
             message += "parameters is None: tool_shed_url: %s, repository_name: %s, repository_owner: %s, changeset_revision: %s " % \
                 ( str( tool_shed_url ), str( repository_name ), str( repository_owner ), str( changeset_revision ) )
             raise Exception( message )
-        params = '?name=%s&owner=%s&changeset_revision=%s' % ( repository_name, repository_owner, changeset_revision )
-        url = common_util.url_join( tool_shed_url,
-                                    'repository/get_tool_dependencies%s' % params )
-        raw_text = common_util.tool_shed_get( trans.app, tool_shed_url, url )
+        params = dict( name=repository_name, owner=repository_owner, changeset_revision=changeset_revision )
+        pathspec = [ 'repository', 'get_tool_dependencies' ]
+        raw_text = common_util.tool_shed_get( trans.app, tool_shed_url, pathspec=pathspec, params=params )
         if len( raw_text ) > 2:
             encoded_text = json.loads( raw_text )
             text = encoding_util.tool_shed_decode( encoded_text )
@@ -437,12 +436,11 @@ class AdminToolshed( AdminGalaxy ):
             message += "required parameters is None: tool_shed_url: %s, repository_name: %s, repository_owner: %s, changeset_revision: %s " % \
                 ( str( tool_shed_url ), str( repository_name ), str( repository_owner ), str( changeset_revision ) )
             raise Exception( message )
-        params = '?name=%s&owner=%s&changeset_revision=%s' % ( str( repository_name ),
-                                                               str( repository_owner ),
-                                                               changeset_revision )
-        url = common_util.url_join( tool_shed_url,
-                                    'repository/get_updated_repository_information%s' % params )
-        raw_text = common_util.tool_shed_get( trans.app, tool_shed_url, url )
+        params = dict( name=str( repository_name ),
+                       owner=str( repository_owner ),
+                       changeset_revision=changeset_revision )
+        pathspec = [ 'repository', 'get_updated_repository_information' ]
+        raw_text = common_util.tool_shed_get( trans.app, tool_shed_url, pathspec=pathspec, params=params )
         repo_information_dict = json.loads( raw_text )
         return repo_information_dict
 
@@ -528,10 +526,11 @@ class AdminToolshed( AdminGalaxy ):
                 tool_shed_url = common_util.get_tool_shed_url_from_tool_shed_registry( trans.app, str( repository.tool_shed ) )
                 name = str( repository.name )
                 owner = str( repository.owner )
-                params = '?galaxy_url=%s&name=%s&owner=%s' % ( web.url_for( '/', qualified=True ), name, owner )
-                url = common_util.url_join( tool_shed_url,
-                                            'repository/get_latest_downloadable_changeset_revision%s' % params )
-                raw_text = common_util.tool_shed_get( trans.app, tool_shed_url, url )
+                params = dict( galaxy_url=web.url_for( '/', qualified=True ),
+                               name=name,
+                               owner=owner )
+                pathspec = [ 'repository', 'get_latest_downloadable_changeset_revision' ]
+                raw_text = common_util.tool_shed_get( trans.app, tool_shed_url, pathspec=pathspec, params=params )
                 latest_downloadable_revision = json.loads( raw_text )
                 if latest_downloadable_revision == hg_util.INITIAL_CHANGELOG_HASH:
                     message = 'Error retrieving the latest downloadable revision for this repository via the url <b>%s</b>.' % url
@@ -542,7 +541,7 @@ class AdminToolshed( AdminGalaxy ):
                     # appropriate repository revision if one exists.  We need to create a temporary repo_info_tuple
                     # with the following entries to handle this.
                     # ( description, clone_url, changeset_revision, ctx_rev, owner, repository_dependencies, tool_dependencies )
-                    tmp_clone_url = common_util.url_join( tool_shed_url, 'repos', owner, name )
+                    tmp_clone_url = common_util.url_join( tool_shed_url, pathspec=[ 'repos', owner, name ] )
                     tmp_repo_info_tuple = ( None, tmp_clone_url, latest_downloadable_revision, None, owner, None, None )
                     installed_repository, installed_changeset_revision = \
                         suc.repository_was_previously_installed( trans.app, tool_shed_url, name, tmp_repo_info_tuple, from_tip=False )
@@ -555,12 +554,11 @@ class AdminToolshed( AdminGalaxy ):
                         status = 'error'
                     else:
                         # Install the latest downloadable revision of the repository.
-                        params = '?name=%s&owner=%s&changeset_revisions=%s&galaxy_url=%s' % ( name,
-                                                                                              owner,
-                                                                                              str( latest_downloadable_revision ),
-                                                                                              web.url_for( '/', qualified=True ) )
-                        url = common_util.url_join( tool_shed_url,
-                                                    'repository/install_repositories_by_revision%s' % params )
+                        params = dict( name=name,
+                                       owner=owner,
+                                       changeset_revisions=str( latest_downloadable_revision ),
+                                       galaxy_url=web.url_for( '/', qualified=True ) )
+                        url = common_util.url_join( tool_shed_url, pathspec=pathspec, params=params )
                         return trans.response.send_redirect( url )
             else:
                 message = 'Cannot locate installed tool shed repository with encoded id <b>%s</b>.' % str( repository_id )
@@ -723,12 +721,12 @@ class AdminToolshed( AdminGalaxy ):
                                                               tool_shed_repository_ids=tool_shed_repository_ids ) )
         if repository.can_install and operation == 'install':
             # Send a request to the tool shed to install the repository.
-            params = '?name=%s&owner=%s&changeset_revisions=%s&galaxy_url=%s' % ( name,
-                                                                                  owner,
-                                                                                  installed_changeset_revision,
-                                                                                  web.url_for( '/', qualified=True ) )
-            url = common_util.url_join( tool_shed_url,
-                                        'repository/install_repositories_by_revision%s' % params )
+            params = dict( name=name,
+                           owner=owner,
+                           changeset_revisions=installed_changeset_revision,
+                           galaxy_url=web.url_for( '/', qualified=True ) )
+            pathspec = [ 'repository', 'install_repositories_by_revision' ]
+            url = common_util.url_join( tool_shed_url, pathspec=pathspec, params=params )
             return trans.response.send_redirect( url )
         description = kwd.get( 'description', repository.description )
         shed_tool_conf, tool_path, relative_install_dir = suc.get_tool_panel_config_tool_path_install_dir( trans.app, repository )
@@ -981,10 +979,10 @@ class AdminToolshed( AdminGalaxy ):
                 repository = suc.get_tool_shed_repository_by_id( trans.app, updating_repository_id )
                 # For backward compatibility to the 12/20/12 Galaxy release.
                 try:
-                    params = '?name=%s&owner=%s' % ( str( repository.name ), str( repository.owner ) )
-                    url = common_util.url_join( tool_shed_url,
-                                                'repository/get_repository_id%s' % params )
-                    repository_ids = common_util.tool_shed_get( trans.app, tool_shed_url, url )
+                    params = dict( name=str( repository.name ), owner=str( repository.owner ) )
+                    # params = '?name=%s&owner=%s' % ( str( repository.name ), str( repository.owner ) )
+                    pathspec = [ 'repository', 'get_repository_id' ]
+                    repository_ids = common_util.tool_shed_get( trans.app, tool_shed_url, pathspec=pathspec, params=params )
                 except Exception, e:
                     # The Tool Shed cannot handle the get_repository_id request, so the code must be older than the
                     # 04/2014 Galaxy release when it was introduced.  It will be safest to error out and let the
@@ -1002,10 +1000,9 @@ class AdminToolshed( AdminGalaxy ):
             else:
                 changeset_revisions = kwd.get( 'changeset_revisions', None )
             # Get the information necessary to install each repository.
-            params = '?repository_ids=%s&changeset_revisions=%s' % ( str( repository_ids ), str( changeset_revisions ) )
-            url = common_util.url_join( tool_shed_url,
-                                        'repository/get_repository_information%s' % params )
-            raw_text = common_util.tool_shed_get( trans.app, tool_shed_url, url )
+            params = dict( repository_ids=str( repository_ids ), changeset_revisions=str( changeset_revisions ) )
+            pathspec = [ 'repository', 'get_repository_information' ]
+            raw_text = common_util.tool_shed_get( trans.app, tool_shed_url, pathspec=pathspec, params=params )
             repo_information_dict = json.loads( raw_text )
             for encoded_repo_info_dict in repo_information_dict.get( 'repo_info_dicts', [] ):
                 decoded_repo_info_dict = encoding_util.tool_shed_decode( encoded_repo_info_dict )
@@ -1534,12 +1531,11 @@ class AdminToolshed( AdminGalaxy ):
                 if 'workflows' in metadata:
                     includes_workflows = True
                 # Since we're reinstalling, we need to send a request to the tool shed to get the README files.
-                params = '?name=%s&owner=%s&changeset_revision=%s' % ( str( tool_shed_repository.name ),
-                                                                       str( tool_shed_repository.owner ),
-                                                                       str( tool_shed_repository.installed_changeset_revision ) )
-                url = common_util.url_join( tool_shed_url,
-                                            'repository/get_readme_files%s' % params )
-                raw_text = common_util.tool_shed_get( trans.app, tool_shed_url, url )
+                params = dict( name=tool_shed_repository.name, 
+                               owner=tool_shed_repository.owner,
+                               changeset_revision=tool_shed_repository.installed_changeset_revision )
+                pathspec = [ 'repository', 'get_readme_files' ]
+                raw_text = common_util.tool_shed_get( trans.app, tool_shed_url, pathspec=pathspec, params=params )
                 readme_files_dict = json.loads( raw_text )
                 tool_dependencies = metadata.get( 'tool_dependencies', None )
             rdim = repository_dependency_manager.RepositoryDependencyInstallManager( trans.app )
@@ -1738,12 +1734,9 @@ class AdminToolshed( AdminGalaxy ):
         """
         repository = repository_util.get_installed_tool_shed_repository( trans.app, kwd[ 'id' ] )
         tool_shed_url = common_util.get_tool_shed_url_from_tool_shed_registry( trans.app, str( repository.tool_shed ) )
-        params = '?name=%s&owner=%s&changeset_revision=%s' % ( str( repository.name ),
-                                                               str( repository.owner ),
-                                                               str( repository.changeset_revision ) )
-        url = common_util.url_join( tool_shed_url,
-                                    'repository/get_tool_versions%s' % params )
-        text = common_util.tool_shed_get( trans.app, tool_shed_url, url )
+        params = dict( name=repository.name, owner=repository.owner, changeset_revision=repository.changeset_revision )
+        pathspec = [ 'repository', 'get_tool_versions' ]
+        text = common_util.tool_shed_get( trans.app, tool_shed_url, pathspec=pathspec, params=params )
         if text:
             tool_version_dicts = json.loads( text )
             tvm = tool_version_manager.ToolVersionManager( trans.app )

lib/galaxy/webapps/galaxy/controllers/workflow.py
@@ -867,9 +867,11 @@ class WorkflowController( BaseUIController, SharableMixin, UsesStoredWorkflowMix
             import_button = True
         if tool_shed_url and not import_button:
             # Use urllib (send another request to the tool shed) to retrieve the workflow.
-            workflow_url = '%s/workflow/import_workflow?repository_metadata_id=%s&workflow_name=%s&open_for_url=true' % \
-                ( tool_shed_url, repository_metadata_id, encoding_util.tool_shed_encode( workflow_name ) )
-            workflow_text = common_util.tool_shed_get( trans.app, tool_shed_url, workflow_url )
+            params = dict( repository_metadata_id=repository_metadata_id, 
+                           workflow_name=encoding_util.tool_shed_encode( workflow_name ),
+                           open_for_url=True )
+            pathspec = [ 'workflow', 'import_workflow' ]
+            workflow_text = common_util.tool_shed_get( trans.app, tool_shed_url, pathspec=pathspec, params=params )
             import_button = True
         if import_button:
             workflow_data = None

lib/tool_shed/capsule/capsule_manager.py
@@ -117,9 +117,10 @@ class ExportRepositoryManager( object ):
             repositories_archive.close()
         if self.using_api:
             encoded_repositories_archive_name = encoding_util.tool_shed_encode( repositories_archive_filename )
-            params = '?encoded_repositories_archive_name=%s' % encoded_repositories_archive_name
-            download_url = common_util.url_join( web.url_for( '/', qualified=True ),
-                                                'repository/export_via_api%s' % params )
+            params = dict( encoded_repositories_archive_name=encoded_repositories_archive_name )
+            pathspec = [ 'repository', 'export_via_api' ]
+            tool_shed_url = web.url_for( '/', qualified=True )
+            download_url = common_util.url_join( tool_shed_url, pathspec=pathspec, params=params )
             return dict( download_url=download_url, error_messages=error_messages )
         return repositories_archive, error_messages
 

lib/tool_shed/galaxy_install/dependency_display.py
@@ -461,12 +461,11 @@ class DependencyDisplayer( object ):
                       self.app.install_model.ToolShedRepository.installation_status.INSTALLED ]:
                     # Since we're reinstalling, we need to send a request to the tool shed to get the README files.
                     tool_shed_url = common_util.get_tool_shed_url_from_tool_shed_registry( self.app, tool_shed_url )
-                    params = '?name=%s&owner=%s&changeset_revision=%s' % ( str( repository.name ),
-                                                                           str( repository.owner ),
-                                                                           str( repository.installed_changeset_revision ) )
-                    url = common_util.url_join( tool_shed_url,
-                                                'repository/get_readme_files%s' % params )
-                    raw_text = common_util.tool_shed_get( self.app, tool_shed_url, url )
+                    params = dict( name=str( repository.name ),
+                                   owner=str( repository.owner ),
+                                   changeset_revision=str( repository.installed_changeset_revision ) )
+                    pathspec = [ 'repository', 'get_readme_files' ]
+                    raw_text = common_util.tool_shed_get( self.app, tool_shed_url, pathspec=pathspec, params=params )
                     readme_files_dict = json.loads( raw_text )
                 else:
                     readme_files_dict = readme_util.build_readme_files_dict( self.app,

commit 6ca86b5586dea7ae88249cf66183bb44a1c9e96a

Fix regression introduced in 0f2e2152ee17532488a8231cf1220ed9ddddb3aa .

lib/galaxy/webapps/tool_shed/controllers/repository.py
@@ -905,10 +905,8 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
             params = dict( tool_shed_url=web.url_for( '/', qualified=True ),
                            name=str( repository.name ),
                            owner=str( repository.user.username ),
-                           changeset_revision=changeset_revision,
-                           latest_changeset_revision=latest_changeset_revision )
+                           changeset_revision=changeset_revision )
             pathspec = [ 'admin_toolshed', 'update_to_changeset_revision' ]
-            url = common_util.url_join( galaxy_url, pathspec=pathspec, params=params )
         else:
             message = 'Unable to check for updates due to an invalid Galaxy URL: <b>%s</b>.  ' % galaxy_url
             message += 'You may need to enable third-party cookies in your browser.  '
@@ -918,7 +916,6 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
             if from_update_manager:
                 return no_update
             # Return the same value for changeset_revision and latest_changeset_revision.
-            url += latest_changeset_revision
         else:
             repository_metadata = suc.get_repository_metadata_by_changeset_revision( trans.app,
                                                                                      trans.security.encode_id( repository.id ),
@@ -928,9 +925,7 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
                 # additional updates.
                 if from_update_manager:
                     return no_update
-                else:
-                    # Return the same value for changeset_revision and latest_changeset_revision.
-                    url += latest_changeset_revision
+                # Return the same value for changeset_revision and latest_changeset_revision.
             else:
                 # The changeset_revision column in the repository_metadata table has been updated with a new
                 # changeset_revision value since the repository was installed.  We need to find the changeset_revision
@@ -963,8 +958,9 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
                     if latest_changeset_revision == changeset_revision:
                         return no_update
                     return update
-                url += str( latest_changeset_revision )
-        url += '&latest_ctx_rev=%s' % str( update_to_ctx.rev() )
+        params['latest_changeset_revision'] = str( latest_changeset_revision )
+        params['latest_ctx_rev'] = str( update_to_ctx.rev() )
+        url = common_util.url_join( galaxy_url, pathspec=pathspec, params=params )
         return trans.response.send_redirect( url )
 
     @web.expose