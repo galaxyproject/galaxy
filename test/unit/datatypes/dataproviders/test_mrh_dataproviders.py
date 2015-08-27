9b396e8e439ee43db0d0c23eae3165df9cbfe795
Michael Sauria <crockopotamus@gmail.com>
2015-08-27 13:48:51 -0400

    Updated mrh dataprovider unit test.

--- test/unit/datatypes/dataproviders/test_mrh_dataproviders.py
+++ test/unit/datatypes/dataproviders/test_mrh_dataproviders.py
@@ -26,7 +26,6 @@ class Test_BaseDataProvider( BaseTestCase ):
         with open( TEST_FILE, 'rb' ) as source:
             provider = self.provider_class( source )
             data = list( provider )
-            # print 'data: %s' % str( data )
             self.assertEqual( data, [] )
 
     def test_chroms( self ):
@@ -35,7 +34,6 @@ class Test_BaseDataProvider( BaseTestCase ):
                 chromosomes=True,
             )
             data = list( provider )
-            # print 'data: %s' % str( data )
             self.assertEqual( data, [{'chromosomes': ['19', '18'], 'includes_trans': True}] )
 
     def test_header( self ):
@@ -46,10 +44,7 @@ class Test_BaseDataProvider( BaseTestCase ):
                 chrom2='19',
             )
             data = list( provider )
-            # print 'len data[0]: %s' % str( len( data[0] ) )
-            self.assertEqual( len(data[0]), 17 )
-            # print "data[0]['maxscore']]: %s" % str( data[0]['maxscore'] )
-            self.assertEqual( data[0]['maxscore'], -12.772971153259277 )
+            self.assertEqual( data, [{'shape_bins': 75, 'start1': -3114508, 'hres': 5000000, 'start2': 2217738, 'stop1': 96885492, 'minscore': -14.89890193939209, 'index_bins': 75, 'm': 3, 'lres': 20000000, 'n': 5, 'data_bins': 291, 'total_bytes': 1614, 'offset': 1540, 'stop2': 62217738, 'maxscore': -12.311820030212402, 'total_bins': 441}] )
 
     def test_small_cis_window( self ):
         with open( TEST_FILE, 'rb' ) as source:
@@ -57,17 +52,14 @@ class Test_BaseDataProvider( BaseTestCase ):
                 chrom1='18',
                 chrom2='18',
                 start1=20000000,
-                start2=20000000,
-                stop1=22560000,
-                stop2=22560000,
-                min_resolution=1280000,
-                max_resolution=320000,
+                start2=60000000,
+                stop1=40000000,
+                stop2=80000000,
+                minresolution=20000000,
+                maxresolution=10000000,
             )
             data = list( provider )
-            # print 'len data: %s' % str( len( data ) )
-            self.assertEqual( len( data ), 64 )
-            # print data[1]['value']
-            self.assertEqual( "-2.98051", "%0.5f" % data[1]['value'])
+            self.assertEqual( data, [[20000000, 6885492, 60000000, 6885492, -10.190299034118652, 1], [20000000, 6885492, 66885492, 10000000, -10.598214149475098, 1], [26885492, 10000000, 60000000, 6885492, -9.802485466003418, 1], [26885492, 10000000, 66885492, 10000000, -10.095721244812012, 1], [20000000, 6885492, 76885492, 3114508, -11.09201717376709, 1], [26885492, 10000000, 76885492, 3114508, -10.56174373626709, 1], [36885492, 3114508, 60000000, 6885492, -9.686873435974121, 1], [36885492, 3114508, 66885492, 10000000, -9.738481521606445, 1], [36885492, 3114508, 76885492, 3114508, -10.048870086669922, 1]] )
 
     def test_small_trans_window( self ):
         with open( TEST_FILE, 'rb' ) as source:
@@ -75,17 +67,14 @@ class Test_BaseDataProvider( BaseTestCase ):
                 chrom1='18',
                 chrom2='19',
                 start1=20000000,
-                start2=20000000,
-                stop1=22560000,
-                stop2=22560000,
-                min_resolution=1280000,
-                max_resolution=320000,
+                start2=60000000,
+                stop1=40000000,
+                stop2=80000000,
+                minresolution=20000000,
+                maxresolution=10000000,
             )
             data = list( provider )
-            # print 'len data: %s' % str( len( data ) )
-            self.assertEqual( len( data ), 7 )
-            # print data[1]['value']
-            self.assertEqual( "-13.46063", "%0.5f" % data[1]['value'])
+            self.assertEqual( data, [[20000000, 6885492, 60000000, 2217738, -14.142205238342285, 1], [26885492, 10000000, 60000000, 2217738, -13.74052619934082, 1], [36885492, 3114508, 60000000, 2217738, -14.021368026733398, 1]] )
 
 if __name__ == '__main__':
     unittest.main()
