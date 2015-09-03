b99b8f2853c49f432eb6e400c734ef087ce36af2
guerler <aysam.guerler@gmail.com>
2015-09-03 03:38:42 -0400

    Remove unnecessary div

--- client/galaxy/scripts/mvc/app/app-view.js
+++ client/galaxy/scripts/mvc/app/app-view.js
@@ -6,18 +6,9 @@ define(['utils/utils', 'mvc/ui/ui-portlet', 'mvc/ui/ui-misc',
         'mvc/user/user-model','mvc/user/user-quotameter',
         'mvc/app/app-panel-history', 'mvc/app/app-panel-tool', 'mvc/app/app-panel-center'],
     function( Utils, Portlet, Ui, Masthead, Menu, Modal, Frame, User, QuotaMeter, HistoryPanel, ToolPanel, CenterPanel) {
-
-    // create form view
     return Backbone.View.extend({
-        // initialize
         initialize: function( options ) {
-            // configure options
             this.options = Utils.merge( options, {} );
-
-            // log options
-            console.debug( this.options );
-
-            // set element
             this.setElement( this._template( options ) );
             ensure_dd_helper();
             $('body').append( this.$el );
@@ -102,7 +93,7 @@ define(['utils/utils', 'mvc/ui/ui-portlet', 'mvc/ui/ui-misc',
         _template: function() {
             return  '<div id="everything" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">' +
                         //'<div id="background"/>' +
-                        '<div id="masthead" class="navbar navbar-fixed-top navbar-inverse"/>' +
+                        //'<div id="masthead" class="navbar navbar-fixed-top navbar-inverse"/>' +
                         //'<div id="messagebox"/>' +
                         //'<div id="inactivebox" class="panel-warning-message"/>' +
                         '<div id="center" class="inbound"/>' +
