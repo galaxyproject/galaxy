c948bc70caa9f241c56df0a0e1ff7e542bdabee3
guerler <aysam.guerler@gmail.com>
2015-09-04 15:30:41 -0400

    Remove gap

--- client/galaxy/scripts/mvc/app/app-analysis.js
+++ client/galaxy/scripts/mvc/app/app-analysis.js
@@ -15,7 +15,6 @@ define(['utils/utils', 'mvc/tools', 'mvc/upload/upload-view', 'mvc/ui/ui-misc', 
                 'root/welcome'
             ));
         },
-
         _template: function() {
             return  '<div style="position: absolute; width: 100%; height: 100%">' +
                         '<iframe name="galaxy_main" id="galaxy_main" frameborder="0" style="position: absolute; width: 100%; height: 100%;"/>' +
