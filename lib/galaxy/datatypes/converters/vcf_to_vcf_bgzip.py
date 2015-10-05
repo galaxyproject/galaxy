54410d7d70c13c76ac370e83c12db7810192fc71
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2015-10-05 12:03:33 +0100

    Use eggs.require() instead of pkg_resources in lib/galaxy/datatypes/ .
    Fix https://github.com/galaxyproject/galaxy/issues/809#issuecomment-145107037 .

--- lib/galaxy/datatypes/converters/vcf_to_vcf_bgzip.py
+++ lib/galaxy/datatypes/converters/vcf_to_vcf_bgzip.py
@@ -7,10 +7,11 @@ Original ordering, which may be specifically needed  by tools or external displa
 
 usage: %prog in_file out_file
 """
-import pkg_resources
-pkg_resources.require( "pysam" )
-import ctabix
 import optparse
+
+from galaxy import eggs
+eggs.require( "pysam" )
+import ctabix
 
 
 def main():
--- lib/galaxy/datatypes/display_applications/util.py
+++ lib/galaxy/datatypes/display_applications/util.py
@@ -1,5 +1,5 @@
-import pkg_resources
-pkg_resources.require( "pycrypto" )
+from galaxy import eggs
+eggs.require( "pycrypto" )
 from Crypto.Cipher import Blowfish
 
 
