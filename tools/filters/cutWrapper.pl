76d900791b379fb9f0e46d6aed0a501f19ec6c83
Daniel Blankenberg <dan.blankenberg@gmail.com>
2015-11-30 11:41:00 -0500

    Enhance cut tool to allow use of step size within ranges, specifiying the fill char, and allow handling headers in 3 different ways.

--- tools/filters/cutWrapper.pl
+++ tools/filters/cutWrapper.pl
@@ -11,12 +11,15 @@ my $command = "";
 my $field = 0;
 my $start = 0;
 my $end = 0;
+my $step = 0;
 my $i = 0;
+my $fill_char = "";
+my $keep_header = 0;
 
 # a wrapper for cut for use in galaxy
-# cutWrapper.pl [filename] [columns] [delim] [output]
+# cutWrapper.pl [filename] [columns] [delim] [output] [fill-char] [keep-header]
 
-die "Check arguments\n" unless @ARGV == 4;
+die "Check arguments\n" unless @ARGV == 6;
 
 $ARGV[1] =~ s/\s+//g;
 foreach ( split /,/, $ARGV[1] ) {
@@ -28,8 +31,18 @@ foreach ( split /,/, $ARGV[1] ) {
     $start =~ s/c//ig;
     $end =~ s/c//ig;
     for $i ($start .. $end) {
-       push (@columns, $i); 
+       push (@columns, $i);
     }
+  } elsif (m/^c\d{1,}-c\d{1,}\|\d{1,}$/i) {
+    ($start, $end)  = split(/-/, $_);
+    $start =~ s/c//ig;
+    ($end, $step) = split(/\|/, $end);
+    $end =~ s/c//ig;
+    for ($i=$start;$i<=$end;$i+=$step) {
+       push (@columns, $i);
+    }
+  } else {
+  print "Skipping invalid column assignment: \"", $_, "\"\n";
   }
 }
 
@@ -50,19 +63,36 @@ $del = $column_delimiters_href->{$ARGV[2]};
 open (OUT, ">$ARGV[3]") or die "Cannot create $ARGV[2]:$!\n";
 open (IN,  "<$ARGV[0]") or die "Cannot open $ARGV[0]:$!\n";
 
+$fill_char = $ARGV[4];
+
+$keep_header = int($ARGV[5]);
+
+
 while (my $line=<IN>) {
-   if ($line =~ /^#/) {
+   my $is_header = ($line =~ /^#/);
+   if ($is_header && $keep_header==0) {
      #Ignore comment lines
    } else {
      chop($line);
      @in = split(/$del/, $line);
-     foreach $field (@columns) {
-       if (defined($in[$field-1])) {
-         push(@out, $in[$field-1]);
-       } else {
-         push(@out, ".");
-       }
-     }    
+     if ($is_header && $keep_header == 2){
+         $in[0] =~ s/^#+//;
+     }
+     if ($keep_header == 2 or !$is_header){
+         foreach $field (@columns) {
+           if (defined($in[$field-1])) {
+             push(@out, $in[$field-1]);
+           } else {
+             push(@out, $fill_char);
+           }
+         }
+     }
+     elsif ($is_header){
+         @out = ($line);
+     }
+     if ($is_header && $keep_header == 2){
+         print OUT "#";
+     }
      print OUT join("\t",@out), "\n";
      @out = ();
    }
