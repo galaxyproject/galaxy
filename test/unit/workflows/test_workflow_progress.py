7974c1f33486c0a66803004c5d7885a9d91b9c70
John Chilton <jmchilton@gmail.com>
2015-12-01 21:54:45 +0000

    Add unit tests for workflow progress initialization.

--- test/unit/workflows/test_workflow_progress.py
+++ test/unit/workflows/test_workflow_progress.py
@@ -23,7 +23,15 @@ steps:
     -  input_name: "input1"
        "@output_step": 0
        output_name: "output"
+  - type: "tool"
+    tool_id: "cat1"
+    input_connections:
+    -  input_name: "input1"
+       "@output_step": 2
+       output_name: "out_file1"
 """
+
+UNSCHEDULED_STEP = object()
 
 
 class WorkflowProgressTestCase( unittest.TestCase ):
@@ -32,6 +40,7 @@ class WorkflowProgressTestCase( unittest.TestCase ):
         self.app = TestApp()
         self.inputs_by_step_id = {}
         self.invocation = model.WorkflowInvocation()
+        self.progress = {}
 
     def _setup_workflow(self, workflow_yaml):
         workflow = yaml_to_model(TEST_WORKFLOW_YAML)
@@ -39,8 +48,22 @@ class WorkflowProgressTestCase( unittest.TestCase ):
 
     def _new_workflow_progress( self ):
         return WorkflowProgress(
-            self.invocation, self.inputs_by_step_id, MockModuleInjector()
+            self.invocation, self.inputs_by_step_id, MockModuleInjector(self.progress)
         )
+
+    def _set_previous_progress(self, outputs_dict):
+        for step_id, step_value in outputs_dict.iteritems():
+            if step_value is not UNSCHEDULED_STEP:
+                self.progress[step_id] = step_value
+
+                workflow_invocation_step = model.WorkflowInvocationStep()
+                workflow_invocation_step.workflow_step_id = step_id
+                self.invocation.steps.append(workflow_invocation_step)
+
+            workflow_invocation_step_state = model.WorkflowRequestStepState()
+            workflow_invocation_step_state.workflow_step_id = step_id
+            workflow_invocation_step_state.value = True
+            self.invocation.step_states.append(workflow_invocation_step_state)
 
     def _step(self, index):
         return self.invocation.workflow.steps[index]
@@ -81,6 +104,20 @@ class WorkflowProgressTestCase( unittest.TestCase ):
         conn.output_step = self._step(2)
         assert progress.replacement_for_connection(conn) is hda
 
+    def test_remaining_steps_with_progress(self):
+        self._setup_workflow(TEST_WORKFLOW_YAML)
+        self._set_previous_progress({
+            100: {"output": model.HistoryDatasetAssociation()},
+            101: {"output": model.HistoryDatasetAssociation()},
+            102: {"out_file1": model.HistoryDatasetAssociation()},
+            103: {"out_file1": model.HistoryDatasetAssociation()},
+            104: UNSCHEDULED_STEP,
+        })
+        progress = self._new_workflow_progress()
+        steps = progress.remaining_steps()
+        assert len(steps) == 1
+        assert steps[0] is self.invocation.workflow.steps[4]
+
 
 class MockInput(object):
 
@@ -90,5 +127,22 @@ class MockInput(object):
 
 class MockModuleInjector(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, progress):
+        self.progress = progress
+
+    def inject(self, step):
+        step.module = MockModule(self.progress)
+
+
+class MockModule(object):
+
+    def __init__(self, progress):
+        self.progress = progress
+
+    def recover_runtime_state(self, runtime_state):
+        return True
+
+    def recover_mapping(self, step, step_invocations, progress):
+        step_id = step.id
+        if step_id in self.progress:
+            progress.set_step_outputs(step, self.progress[step_id])
