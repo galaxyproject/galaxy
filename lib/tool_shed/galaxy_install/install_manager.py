f974b0f740d4ed8c44ebafde74908cdea1748a1a
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2015-12-03 15:58:11 +0000

    Extend the logging of tool dependency status changes.
    Also merge handle_tool_dependency_installation_error() in
    set_tool_dependency_attributes() and consolidate its use.

--- lib/tool_shed/galaxy_install/install_manager.py
+++ lib/tool_shed/galaxy_install/install_manager.py
@@ -126,10 +126,10 @@ class InstallToolDependencyManager( object ):
             # Since there was an installation error, update the tool dependency status to Error. The remove_installation_path option must
             # be left False here.
             error_message = '%s\n%s' % ( self.format_traceback(), str( e ) )
-            tool_dependency = tool_dependency_util.handle_tool_dependency_installation_error( self.app,
-                                                                                              tool_dependency,
-                                                                                              error_message,
-                                                                                              remove_installation_path=False )
+            tool_dependency = tool_dependency_util.set_tool_dependency_attributes(self.app,
+                                                                                  tool_dependency=tool_dependency,
+                                                                                  status=self.app.install_model.ToolDependency.installation_status.ERROR,
+                                                                                  error_message=error_message)
         tool_dependency = self.mark_tool_dependency_installed( tool_dependency )
         return tool_dependency
 
@@ -204,10 +204,10 @@ class InstallToolDependencyManager( object ):
                                 # Since there was an installation error, update the tool dependency status to Error. The
                                 # remove_installation_path option must be left False here.
                                 tool_dependency = \
-                                    tool_dependency_util.handle_tool_dependency_installation_error( self.app,
-                                                                                                    tool_dependency,
-                                                                                                    error_message,
-                                                                                                    remove_installation_path=False )
+                                    tool_dependency_util.set_tool_dependency_attributes(self.app,
+                                                                                        tool_dependency=tool_dependency,
+                                                                                        status=self.app.install_model.ToolDependency.installation_status.ERROR,
+                                                                                        error_message=error_message)
                         if tool_dependency and tool_dependency.status in [ self.install_model.ToolDependency.installation_status.INSTALLED,
                                                                            self.install_model.ToolDependency.installation_status.ERROR ]:
                             installed_packages.append( tool_dependency )
@@ -422,9 +422,7 @@ class InstallToolDependencyManager( object ):
             status = self.install_model.ToolDependency.installation_status.INSTALLED
             tool_dependency = tool_dependency_util.set_tool_dependency_attributes( self.app,
                                                                                    tool_dependency=tool_dependency,
-                                                                                   status=status,
-                                                                                   error_message=None,
-                                                                                   remove_from_disk=False )
+                                                                                   status=status )
         return tool_dependency
 
 
