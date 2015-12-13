4d51832edec9166ec48fc7a29d0275868e49bf13
John Chilton <jmchilton@gmail.com>
2015-12-13 10:40:26 +0000

    Library methods for dealing with sandboxed JavaScript expressions.
    Based on work by Peter Amstutz in cwltool (https://github.com/common-workflow-language/cwltool).

---
+++ lib/galaxy/tools/expressions/__init__.py
@@ -0,0 +1,11 @@
+from .evaluation import evaluate
+from .sandbox import execjs, interpolate
+from .util import jshead
+
+
+__all__ = [
+    'evaluate',
+    'execjs',
+    'jshead',
+    'interpolate',
+]
---
+++ test/unit/tools/test_expression_basics.py
@@ -0,0 +1,8 @@
+from galaxy.tools.expressions import evaluate
+
+
+def test_evaluate():
+    input = {
+        "script": "{return 5;}"
+    }
+    assert evaluate(None, input) == 5
