c4b532303576b02cffbf8ee8df63e086686d551b
guerler <aysam.guerler@gmail.com>
2016-01-12 14:05:18 -0500

    Remove parenthesis

--- client/galaxy/scripts/utils/utils.js
+++ client/galaxy/scripts/utils/utils.js
@@ -124,16 +124,12 @@ function request (options) {
         url         : options.url
     }
     // encode data into url
-    if ( (ajaxConfig.type == 'GET' || ajaxConfig.type == 'DELETE' ) ) {
-        if ( !$.isEmptyObject(ajaxConfig.data)) {
-            if (ajaxConfig.url.indexOf('?') == -1) {
-                ajaxConfig.url += '?';
-            } else {
-                ajaxConfig.url += '&';
-            }
-            ajaxConfig.url      = ajaxConfig.url + $.param(ajaxConfig.data, true);
+    if ( ajaxConfig.type == 'GET' || ajaxConfig.type == 'DELETE' ) {
+        if ( !$.isEmptyObject(ajaxConfig.data) ) {
+            ajaxConfig.url += ajaxConfig.url.indexOf('?') == -1 ? '?' : '&';
+            ajaxConfig.url += $.param(ajaxConfig.data, true);
         }
-        ajaxConfig.data     = null;
+        ajaxConfig.data = null;
     } else {
         ajaxConfig.dataType = 'json';
         ajaxConfig.url      = ajaxConfig.url;
