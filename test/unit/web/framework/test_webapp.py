0d399f6919cc24fc0e3e288fa21784ea544edb29
carlfeberhard <carlfeberhard@gmail.com>
2016-01-14 15:32:57 -0500

    CORS: cache and restore tempfile.tempdir when using test Configuration instances as it persists across tests and fails any tempfile related tests that follow, move test_config tests into test_webapp (cohesive subject v. ortho file struct)

--- test/unit/web/framework/test_webapp.py
+++ test/unit/web/framework/test_webapp.py
@@ -12,6 +12,8 @@ test_utils = imp.load_source( 'test_utils',
     os.path.join( os.path.dirname( __file__), '../../unittest_utils/utility.py' ) )
 import galaxy_mock
 
+import re
+import tempfile
 from galaxy.web.framework import webapp as Webapp
 import galaxy.config
 
@@ -22,6 +24,15 @@ class StubGalaxyWebTransaction( Webapp.GalaxyWebTransaction ):
 
 
 class GalaxyWebTransaction_Headers_TestCase( test_utils.unittest.TestCase ):
+
+    def setUp( self ):
+        # galaxy uses the tempfile.tempdir interface of the tempfile module, changing this value when a Configuration
+        # is created. This change persists across tests (since it's scoped to the module) and breaks following tests.
+        # TODO: most likely this should be changed at the core level to remove tempfile.tempdir - then remove this
+        self._saved_tempfile_tempdir = tempfile.tempdir
+
+    def teardown( self ):
+        tempfile.tempdir = self._saved_tempfile_tempdir
 
     def _new_trans( self, allowed_origin_hostnames=None ):
         app = galaxy_mock.MockApp()
@@ -38,6 +49,29 @@ class GalaxyWebTransaction_Headers_TestCase( test_utils.unittest.TestCase ):
 
     def assert_cors_header_missing( self, headers ):
         self.assertFalse( 'access-control-allow-origin' in headers )
+
+    def test_default_allowed_origin_hostnames( self ):
+        """Shouldn't have any allowed"""
+        config = galaxy.config.Configuration()
+        self.assertTrue( isinstance( config, galaxy.config.Configuration ) )
+        self.assertEqual( config.allowed_origin_hostnames, None )
+
+    def test_parse_allowed_origin_hostnames( self ):
+        """Should return a list of (possibly) mixed strings and regexps"""
+        config = galaxy.config.Configuration()
+
+        # falsy listify value should return None
+        self.assertEqual( config._parse_allowed_origin_hostnames({
+            "allowed_origin_hostnames" : ""
+        }), None )
+
+        # should parse regex if using fwd slashes, string otherwise
+        hostnames = config._parse_allowed_origin_hostnames({
+            "allowed_origin_hostnames" : "/host\d{2}/,geocities.com,miskatonic.edu"
+        })
+        self.assertTrue( isinstance( hostnames[0], re._pattern_type ) )
+        self.assertTrue( isinstance( hostnames[1], str ) )
+        self.assertTrue( isinstance( hostnames[2], str ) )
 
     def test_default_set_cors_headers( self ):
         """No CORS headers should be set (or even checked) by default"""
