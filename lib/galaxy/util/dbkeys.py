7cc505019d2b2af938b61efe0e00bd25a13af91b
guerler <aysam.guerler@gmail.com>
2016-01-24 14:24:33 -0500

    Use sa_session of workcontext

--- lib/galaxy/util/dbkeys.py
+++ lib/galaxy/util/dbkeys.py
@@ -33,7 +33,7 @@ class GenomeBuilds( object ):
                 # It does allow one-off, history specific dbkeys to be created by a user. But we are not filtering,
                 # so a len file will be listed twice (as the build name and again as dataset name),
                 # if custom dbkey creation/conversion occurred within the current history.
-                datasets = self._app.model.context.query( self._app.model.HistoryDatasetAssociation ) \
+                datasets = trans.sa_session.query( self._app.model.HistoryDatasetAssociation ) \
                                 .filter_by( deleted=False, history_id=trans.history.id, extension="len" )
                 for dataset in datasets:
                     rval.append( ( dataset.dbkey, "%s (%s) [History]" % ( dataset.name, dataset.dbkey ) ) )
@@ -72,11 +72,11 @@ class GenomeBuilds( object ):
                     # fasta-to-len converter.
                     if 'fasta' in custom_build_dict and custom_build_hack_get_len_from_fasta_conversion:
                         # Build is defined by fasta; get len file, which is obtained from converting fasta.
-                        build_fasta_dataset = self._app.model.context.query( self._app.model.HistoryDatasetAssociation ).get( custom_build_dict[ 'fasta' ] )
+                        build_fasta_dataset = trans.sa_session.query( self._app.model.HistoryDatasetAssociation ).get( custom_build_dict[ 'fasta' ] )
                         chrom_info = build_fasta_dataset.get_converted_dataset( trans, 'len' ).file_name
                     elif 'len' in custom_build_dict:
                         # Build is defined by len file, so use it.
-                        chrom_info = self._app.model.context.query( self._app.model.HistoryDatasetAssociation ).get( custom_build_dict[ 'len' ] ).file_name
+                        chrom_info = trans.sa_session.query( self._app.model.HistoryDatasetAssociation ).get( custom_build_dict[ 'len' ] ).file_name
         # Check Data table
         if not chrom_info:
             dbkey_table = self._app.tool_data_tables.get( self._data_table_name, None )
