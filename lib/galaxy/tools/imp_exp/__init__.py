88277c9723c558e2b2003f1b81c7f2d16143fcc6
Nate Coraor <nate@bx.psu.edu>
2016-02-24 11:18:19 -0500

    Security fixes for history imports

--- lib/galaxy/tools/imp_exp/__init__.py
+++ lib/galaxy/tools/imp_exp/__init__.py
@@ -5,6 +5,7 @@ import tempfile
 import json
 import datetime
 from galaxy import model
+from galaxy.exceptions import MalformedContents
 from galaxy.tools.parameters.basic import UnvalidatedValue
 from galaxy.web.framework.helpers import to_unicode
 from galaxy.model.item_attrs import UsesAnnotations
@@ -187,9 +188,9 @@ class JobImportHistoryArchiveWrapper( object, UsesHistoryMixin, UsesAnnotations 
                     if dataset_attrs.get('exported', True) == True:
                         # Do security check and move/copy dataset data.
                         temp_dataset_file_name = \
-                            os.path.abspath( os.path.join( archive_dir, dataset_attrs['file_name'] ) )
+                            os.path.realpath( os.path.abspath( os.path.join( archive_dir, dataset_attrs['file_name'] ) ) )
                         if not file_in_dir( temp_dataset_file_name, os.path.join( archive_dir, "datasets" ) ):
-                            raise Exception( "Invalid dataset path: %s" % temp_dataset_file_name )
+                            raise MalformedContents( "Invalid dataset path: %s" % temp_dataset_file_name )
                         if datasets_usage_counts[ temp_dataset_file_name ] == 1:
                             shutil.move( temp_dataset_file_name, hda.file_name )
                         else:
@@ -313,6 +314,7 @@ class JobImportHistoryArchiveWrapper( object, UsesHistoryMixin, UsesAnnotations 
             except Exception, e:
                 jiha.job.stderr += "Error cleaning up history import job: %s" % e
                 self.sa_session.flush()
+                raise
 
 
 class JobExportHistoryArchiveWrapper( object, UsesHistoryMixin, UsesAnnotations ):
