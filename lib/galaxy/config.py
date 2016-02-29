16280c63236621082ce4c3b3d93eb679294cd515
Dannon Baker <dannon.baker@gmail.com>
2016-02-29 11:20:15 -0500

    Only attempt to read/load sanitize_whitelist_file when starting an actual galaxy application.

--- lib/galaxy/config.py
+++ lib/galaxy/config.py
@@ -259,7 +259,6 @@ class Configuration( object ):
         self.log_events = string_as_bool( kwargs.get( 'log_events', 'False' ) )
         self.sanitize_all_html = string_as_bool( kwargs.get( 'sanitize_all_html', True ) )
         self.sanitize_whitelist_file = resolve_path( kwargs.get( 'sanitize_whitelist_file', "config/sanitize_whitelist.txt" ), self.root )
-        self.reload_sanitize_whitelist(explicit='sanitize_whitelist_file' in kwargs)
         self.serve_xss_vulnerable_mimetypes = string_as_bool( kwargs.get( 'serve_xss_vulnerable_mimetypes', False ) )
         self.trust_ipython_notebook_conversion = string_as_bool( kwargs.get( 'trust_ipython_notebook_conversion', False ) )
         self.enable_old_display_applications = string_as_bool( kwargs.get( "enable_old_display_applications", "True" ) )
