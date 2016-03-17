galaxyproject/galaxy

commit 6e13c3999a6d54b40b2910d5ce95da724f133122

Cherry-picked changes from pull request #1392.

lib/galaxy/webapps/tool_shed/model/__init__.py
@@ -8,8 +8,8 @@ from galaxy.util.bunch import Bunch
 from galaxy.util.hash_util import new_secure_hash
 from galaxy.util.dictifiable import Dictifiable
 import tool_shed.repository_types.util as rt_util
-import tool_shed.util.shed_util_common as suc
 from tool_shed.dependencies.repository import relation_builder
+from tool_shed.util import shed_util_common as suc
 
 from mercurial import hg
 from mercurial import ui
@@ -253,13 +253,13 @@ class Repository( object, Dictifiable ):
     def installable_revisions( self, app ):
         return suc.get_metadata_revisions( self, hg.repository( ui.ui(), self.repo_path( app ) ), sort_revisions=True )
 
-    def ordered_installable_revisions( self, app ):
-        return suc.get_ordered_metadata_changeset_revisions( self, hg.repository( ui.ui(), self.repo_path( app ) ), downloadable=True )
-
     def is_new( self, app ):
         repo = hg.repository( ui.ui(), self.repo_path( app ) )
         tip_ctx = repo.changectx( repo.changelog.tip() )
         return tip_ctx.rev() < 0
+
+    def ordered_installable_revisions( self, app ):
+        return suc.get_ordered_metadata_changeset_revisions( self, hg.repository( ui.ui(), self.repo_path( app ) ), downloadable=True )
 
     def repo_path( self, app ):
         return app.hgweb_config_manager.get_entry( os.path.join( "repos", self.user.username, self.name ) )

lib/galaxy/web/form_builder.py
@@ -288,8 +288,9 @@ class SelectField(BaseField):
     <div><input type="checkbox" name="bar" value="3" id="bar|3"><label class="inline" for="bar|3">automatic</label></div>
     <div><input type="checkbox" name="bar" value="4" id="bar|4" checked='checked'><label class="inline" for="bar|4">bazooty</label></div>
     """
-    def __init__( self, name, multiple=None, display=None, refresh_on_change=False, refresh_on_change_values=None, size=None ):
+    def __init__( self, name, multiple=None, display=None, refresh_on_change=False, refresh_on_change_values=None, size=None, field_id=None ):
         self.name = name
+        self.field_id = field_id
         self.multiple = multiple or False
         self.size = size
         self.options = list()
@@ -312,7 +313,11 @@ class SelectField(BaseField):
     def add_option( self, text, value, selected=False ):
         self.options.append( ( text, value, selected ) )
 
-    def get_html( self, prefix="", disabled=False ):
+    def get_html( self, prefix="", disabled=False, extra_attr=None ):
+        if extra_attr is not None:
+            self.extra_attributes = ' %s' % ' '.join( [ '%s="%s"' % ( k, escape( v ) ) for k, v in extra_attr.items() ] )
+        else:
+            self.extra_attributes = ''
         if self.display == "checkboxes":
             return self.get_html_checkboxes( prefix, disabled )
         elif self.display == "radio":
@@ -335,8 +340,8 @@ class SelectField(BaseField):
             selected_text = ""
             if selected:
                 selected_text = " checked='checked'"
-            rval.append( '<div%s><input type="checkbox" name="%s%s" value="%s" id="%s"%s%s><label class="inline" for="%s">%s</label></div>'
-                         % ( style, prefix, self.name, escaped_value, uniq_id, selected_text, self.get_disabled_str( disabled ), uniq_id, escape( text, quote=True ) ) )
+            rval.append( '<div%s><input type="checkbox" name="%s%s" value="%s" id="%s"%s%s%s><label class="inline" for="%s">%s</label></div>'
+                         % ( style, prefix, self.name, escaped_value, uniq_id, selected_text, self.get_disabled_str( disabled ), self.extra_attributes, uniq_id, escape( text, quote=True ) ) )
             ctr += 1
         return unicodify( "\n".join( rval ) )
 
@@ -352,7 +357,7 @@ class SelectField(BaseField):
             selected_text = ""
             if selected:
                 selected_text = " checked='checked'"
-            rval.append( '<div%s><input type="radio" name="%s%s"%s value="%s" id="%s"%s%s><label class="inline" for="%s">%s</label></div>'
+            rval.append( '<div%s><input type="radio" name="%s%s"%s value="%s" id="%s"%s%s%s><label class="inline" for="%s">%s</label></div>'
                          % ( style,
                              prefix,
                              self.name,
@@ -361,6 +366,7 @@ class SelectField(BaseField):
                              uniq_id,
                              selected_text,
                              self.get_disabled_str( disabled ),
+                             self.extra_attributes,
                              uniq_id,
                              text ) )
             ctr += 1
@@ -388,8 +394,12 @@ class SelectField(BaseField):
             rval.append( '<option value="%s"%s>%s</option>' % ( escape( unicodify( value ), quote=True ), selected_text, escape( unicodify( text ), quote=True ) ) )
         if last_selected_value:
             last_selected_value = ' last_selected_value="%s"' % escape( unicodify( last_selected_value ), quote=True )
-        rval.insert( 0, '<select name="%s%s"%s%s%s%s%s>'
-                     % ( prefix, self.name, multiple, size, self.refresh_on_change_text, last_selected_value, self.get_disabled_str( disabled ) ) )
+        if self.field_id is not None:
+            id_string = ' id="%s"' % self.field_id
+        else:
+            id_string = ''
+        rval.insert( 0, '<select name="%s%s"%s%s%s%s%s%s%s>'
+                     % ( prefix, self.name, multiple, size, self.refresh_on_change_text, last_selected_value, self.get_disabled_str( disabled ), id_string, self.extra_attributes ) )
         rval.append( '</select>' )
         return unicodify( "\n".join( rval ) )
 

lib/galaxy/webapps/tool_shed/api/categories.py
@@ -15,8 +15,11 @@ log = logging.getLogger( __name__ )
 class CategoriesController( BaseAPIController ):
     """RESTful controller for interactions with categories in the Tool Shed."""
 
+    def __get_repository_count( self, trans, category_name ):
+        return self.app.repository_registry.viewable_repositories_and_suites_by_category.get( category_name, 0 )
+
     def __get_value_mapper( self, trans ):
-        value_mapper = { 'id' : trans.security.encode_id }
+        value_mapper = { 'id': trans.security.encode_id }
         return value_mapper
 
     @expose_api
@@ -41,11 +44,11 @@ class CategoriesController( BaseAPIController ):
             if not description:
                 # Default the description to the name.
                 description = name
-            if suc.get_category_by_name( trans.app, name ):
+            if suc.get_category_by_name( self.app, name ):
                 raise exceptions.Conflict( 'A category with that name already exists.' )
             else:
                 # Create the category
-                category = trans.app.model.Category( name=name, description=description )
+                category = self.app.model.Category( name=name, description=description )
                 trans.sa_session.add( category )
                 trans.sa_session.flush()
                 category_dict = category.to_dict( view='element',
@@ -56,6 +59,29 @@ class CategoriesController( BaseAPIController ):
                                                       id=trans.security.encode_id( category.id ) )
         else:
             raise exceptions.RequestParameterMissingException( 'Missing required parameter "name".' )
+        return category_dict
+
+    @expose_api_anonymous_and_sessionless
+    def get_repositories( self, trans, category_id, **kwd ):
+        """
+        GET /api/categories/{encoded_category_id}/repositories
+        Return information about the provided category and the repositories in that category.
+
+        :param id: the encoded id of the Category object
+
+        Example: GET localhost:9009/api/categories/f9cad7b01a472135/repositories
+        """
+        category = suc.get_category( self.app, category_id )
+        if category is None:
+            category_dict = dict( message='Unable to locate category record for id %s.' % ( str( id ) ),
+                                  status='error' )
+            return category_dict
+        category_dict = category.to_dict( view='element',
+                                          value_mapper=self.__get_value_mapper( trans ) )
+        category_dict[ 'url' ] = web.url_for( controller='categories',
+                                              action='show',
+                                              id=trans.security.encode_id( category.id ) )
+        category_dict[ 'repositories' ] = suc.get_repositories_by_category( self.app, category.id )
         return category_dict
 
     @expose_api_anonymous_and_sessionless
@@ -72,14 +98,15 @@ class CategoriesController( BaseAPIController ):
         deleted = util.asbool( deleted )
         if deleted and not trans.user_is_admin():
             raise exceptions.AdminRequiredException( 'Only administrators can query deleted categories.' )
-        for category in trans.sa_session.query( trans.app.model.Category ) \
-                                        .filter( trans.app.model.Category.table.c.deleted == deleted ) \
-                                        .order_by( trans.app.model.Category.table.c.name ):
+        for category in trans.sa_session.query( self.app.model.Category ) \
+                                        .filter( self.app.model.Category.table.c.deleted == deleted ) \
+                                        .order_by( self.app.model.Category.table.c.name ):
             category_dict = category.to_dict( view='collection',
                                               value_mapper=self.__get_value_mapper( trans ) )
             category_dict[ 'url' ] = web.url_for( controller='categories',
                                                   action='show',
                                                   id=trans.security.encode_id( category.id ) )
+            category_dict[ 'repositories' ] = self.app.repository_registry.viewable_repositories_and_suites_by_category.get( category.name, 0 )
             category_dicts.append( category_dict )
         return category_dicts
 
@@ -93,7 +120,7 @@ class CategoriesController( BaseAPIController ):
 
         Example: GET localhost:9009/api/categories/f9cad7b01a472135
         """
-        category = suc.get_category( trans.app, id )
+        category = suc.get_category( self.app, id )
         if category is None:
             category_dict = dict( message='Unable to locate category record for id %s.' % ( str( id ) ),
                                   status='error' )

lib/galaxy/webapps/tool_shed/buildapp.py
@@ -109,12 +109,18 @@ def app_factory( global_conf, **kwargs ):
                             name_prefix='category_',
                             path_prefix='/api',
                             parent_resources=dict( member_name='category', collection_name='categories' ) )
+    webapp.mapper.connect( 'repositories_in_category',
+                           '/api/categories/{category_id}/repositories',
+                           controller='categories',
+                           action='get_repositories',
+                           conditions=dict( method=[ "GET" ] ) )
     webapp.mapper.resource( 'repository',
                             'repositories',
                             controller='repositories',
                             collection={ 'add_repository_registry_entry': 'POST',
                                          'get_repository_revision_install_info': 'GET',
                                          'get_ordered_installable_revisions': 'GET',
+                                         'get_installable_revisions': 'GET',
                                          'remove_repository_registry_entry': 'POST',
                                          'repository_ids_for_setting_metadata': 'GET',
                                          'reset_metadata_on_repositories': 'POST',
@@ -151,6 +157,11 @@ def app_factory( global_conf, **kwargs ):
                            '/api/repositories/{id}/metadata',
                            controller='repositories',
                            action='metadata',
+                           conditions=dict( method=[ "GET" ] ) )
+    webapp.mapper.connect( 'repository_show_tools',
+                           '/api/repositories/{id}/{changeset}/show_tools',
+                           controller='repositories',
+                           action='show_tools',
                            conditions=dict( method=[ "GET" ] ) )
     webapp.mapper.connect( 'create_repository',
                            '/api/repositories',

lib/galaxy/webapps/tool_shed/controllers/repository.py
@@ -1618,22 +1618,10 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
         owner = kwd.get( 'owner', None )
         changeset_revision = kwd.get( 'changeset_revision', None )
         repository = suc.get_repository_by_name_and_owner( trans.app, name, owner )
-        # We aren't concerned with repositories of type tool_dependency_definition here if a
-        # repository_metadata record is not returned because repositories of this type will never
-        # have repository dependencies. However, if a readme file is uploaded, or some other change
-        # is made that does not create a new downloadable changeset revision but updates the existing
-        # one, we still want to be able to get repository dependencies.
-        repository_metadata = suc.get_current_repository_metadata_for_changeset_revision( trans.app,
-                                                                                          repository,
-                                                                                          changeset_revision )
-        if repository_metadata:
-            metadata = repository_metadata.metadata
-            if metadata:
-                toolshed_base_url = str( web.url_for( '/', qualified=True ) ).rstrip( '/' )
-                rb = relation_builder.RelationBuilder( trans.app, repository, repository_metadata, toolshed_base_url )
-                repository_dependencies = rb.get_repository_dependencies_for_changeset_revision()
-                if repository_dependencies:
-                    return encoding_util.tool_shed_encode( repository_dependencies )
+        # get_repository_dependencies( self, app, changeset, toolshed_url )
+        dependencies = repository.get_repository_dependencies( trans.app, changeset_revision, web.url_for( '/', qualified=True ) )
+        if dependencies:
+            return encoding_util.tool_shed_encode( dependencies )
         return ''
 
     @web.expose
@@ -1724,13 +1712,9 @@ class RepositoryController( BaseUIController, ratings_util.ItemRatings ):
         owner = kwd.get( 'owner', None )
         changeset_revision = kwd.get( 'changeset_revision', None )
         repository = suc.get_repository_by_name_and_owner( trans.app, name, owner )
-        for downloadable_revision in repository.downloadable_revisions:
-            if downloadable_revision.changeset_revision == changeset_revision:
-                break
-        metadata = downloadable_revision.metadata
-        tool_dependencies = metadata.get( 'tool_dependencies', '' )
-        if tool_dependencies:
-            return encoding_util.tool_shed_encode( tool_dependencies )
+        dependencies = repository.get_tool_dependencies( changeset_revision )
+        if dependencies is not None:
+            return encoding_util.tool_shed_encode( dependencies )
         return ''
 
     @web.expose

lib/galaxy/webapps/tool_shed/model/mapping.py
@@ -201,7 +201,7 @@ Category.table = Table( "category", metadata,
                         Column( "create_time", DateTime, default=now ),
                         Column( "update_time", DateTime, default=now, onupdate=now ),
                         Column( "name", TrimmedString( 255 ), index=True, unique=True ),
-                        Column( "description" , TEXT ),
+                        Column( "description", TEXT ),
                         Column( "deleted", Boolean, index=True, default=False ) )
 
 Tag.table = Table( "tag", metadata,

lib/tool_shed/util/common_util.py
@@ -341,7 +341,7 @@ def remove_protocol_from_tool_shed_url( tool_shed_url ):
     return util.remove_protocol_from_url( tool_shed_url )
 
 
-def tool_shed_get( app, base_url, pathspec=[], params={} ):
+def tool_shed_get( app, base_url, pathspec=None, params=None ):
     """Make contact with the tool shed via the uri provided."""
     registry = app.tool_shed_registry
     # urllib2 auto-detects system proxies, when passed a Proxyhandler.

commit 80562acfe8979433620da5ad416705c7c845aad5

Fix API method broken in 6e13c3999.

lib/galaxy/webapps/tool_shed/api/repositories.py
@@ -88,7 +88,7 @@ class RepositoriesController( BaseAPIController ):
         return response_dict
 
     @web.expose_api_anonymous
-    def get_ordered_installable_revisions( self, trans, **kwd ):
+    def get_ordered_installable_revisions( self, trans, name, owner, **kwd ):
         """
         GET /api/repositories/get_ordered_installable_revisions
 
@@ -99,8 +99,10 @@ class RepositoriesController( BaseAPIController ):
         As in the changelog, the list is ordered oldest to newest.
         """
         # Example URL: http://localhost:9009/api/repositories/get_ordered_installable_revisions?name=add_column&owner=test
-        name = kwd.get( 'name', None )
-        owner = kwd.get( 'owner', None )
+        if name is None:
+            name = kwd.get( 'name', None )
+        if owner is None:
+            owner = kwd.get( 'owner', None )
         tsr_id = kwd.get( 'tsr_id', None )
         if None not in [ name, owner ]:
             # Get the repository information.
@@ -109,7 +111,7 @@ class RepositoriesController( BaseAPIController ):
             repository = suc.get_repository_in_tool_shed( self.app, tsr_id )
         else:
             error_message = "Error in the Tool Shed repositories API in get_ordered_installable_revisions: "
-            error_message += "invalid parameters received." % ( str( name ), str( owner ) )
+            error_message += "invalid parameters received."
             log.debug( error_message )
             return []
         return repository.ordered_installable_revisions( self.app )

lib/galaxy/webapps/tool_shed/model/__init__.py
@@ -259,7 +259,7 @@ class Repository( object, Dictifiable ):
         return tip_ctx.rev() < 0
 
     def ordered_installable_revisions( self, app ):
-        return suc.get_ordered_metadata_changeset_revisions( self, hg.repository( ui.ui(), self.repo_path( app ) ), downloadable=True )
+        return [ revision[ 1 ] for revision in suc.get_metadata_revisions( self, hg.repository( ui.ui(), self.repo_path( app ) ), downloadable=True ) ]
 
     def repo_path( self, app ):
         return app.hgweb_config_manager.get_entry( os.path.join( "repos", self.user.username, self.name ) )