3240c20a4b5cd29a1f2e3764a8aa6cf84ca1e48d
Dannon Baker <dannon.baker@gmail.com>
2016-03-25 17:02:47 -0400

    Swap analyse to analyze to be consistent.

--- client/galaxy/scripts/mvc/library/library-foldertoolbar-view.js
+++ client/galaxy/scripts/mvc/library/library-foldertoolbar-view.js
@@ -760,7 +760,7 @@ var FolderToolbarView = Backbone.View.extend({
     var popped_item = history_item_set.pop();
     if ( typeof popped_item == "undefined" ) {
       if ( this.options.chain_call_control.failed_number === 0 ){
-        mod_toastr.success( 'Selected datasets imported into history. Click this to start analysing it.', '', { onclick: function() { window.location='/' } } );
+        mod_toastr.success( 'Selected datasets imported into history. Click this to start analyzing it.', '', { onclick: function() { window.location='/' } } );
       } else if ( this.options.chain_call_control.failed_number === this.options.chain_call_control.total_number ){
         mod_toastr.error( 'There was an error and no datasets were imported into history.' );
       } else if ( this.options.chain_call_control.failed_number < this.options.chain_call_control.total_number ){
