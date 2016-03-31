827d175e431b9f2344293d3de47ac663cb56f17b
nitesh1989 <nitesh.turaga@gmail.com>
2016-03-31 13:55:03 -0400

    removed commeted code

--- lib/galaxy/webapps/galaxy/controllers/workflow.py
+++ lib/galaxy/webapps/galaxy/controllers/workflow.py
@@ -343,16 +343,6 @@ class WorkflowController( BaseUIController, SharableMixin, UsesStoredWorkflowMix
                                     email=email,
                                     use_panels=use_panels )
 
-    # @web.expose
-    # @web.require_login( "use workflows" )
-    # def export( self, trans, id=None, **kwd ):
-        # """
-        # Handles download/export workflow command.
-        # """
-        # stored = self.get_stored_workflow( trans, id, check_ownership=False, check_accessible=True )
-
-        # return trans.fill_template( "/workflow/export.mako", item=stored, use_panels=True )
-
     @web.expose
     @web.require_login( "Share or export Galaxy workflows" )
     def sharing( self, trans, id, **kwargs ):
