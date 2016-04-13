b361ffa1c217a7e2c57a4d770b100ec718ca22b8
anuprulez <anup.rulez@gmail.com>
2016-04-13 22:58:14 +0200

    removing old code from _getIndex method

--- client/galaxy/scripts/mvc/ui/ui-select.js
+++ client/galaxy/scripts/mvc/ui/ui-select.js
@@ -184,15 +184,6 @@ var View = Backbone.View.extend(
         // replaced the index search code 
         // by UnderscoreJS method to make it faster and compact
         _.findIndex(this.select_data, {id: value});
-        
-	// Old code
-        //for (var key in this.select_data) {
-        //    if (this.select_data[key].id == value) {
-        //        return key;
-        //    }
-        //}
-        // not found
-        //return -1;
     },
     
     // get value
