galaxyproject/galaxy

commit 7347d75a4a36d42f8060e11d708291d796b266a5

Added ignore lines starting with specific characters to group tool

tools/stats/grouping.py
@@ -37,7 +37,22 @@ def main():
     round_val = []
     data_ary = []
     
-    for var in sys.argv[5:]:
+    if sys.argv[5] != "None":
+        oldfile = open(inputfile,'r')
+        oldfilelines = oldfile.readlines()
+        newinputfile = inputfile+'2'
+        newfile = open(newinputfile,'w')
+        asciitodelete = sys.argv[5].split(',')
+        for i in range(len(asciitodelete)):
+            asciitodelete[i] = chr(int(asciitodelete[i]))
+        for line in oldfilelines:
+            if line[0] not in asciitodelete:
+                newfile.write(line)
+        oldfile.close()
+        newfile.close()
+        inputfile = newinputfile
+
+    for var in sys.argv[6:]:
         op, col, do_round = var.split()
         ops.append(op)
         cols.append(col)

tools/stats/grouping.xml
@@ -6,6 +6,7 @@
       $input1
       $groupcol
       $ignorecase
+      $ignorelines
       #for $op in $operations
        '${op.optype}
         ${op.opcol}
@@ -17,6 +18,24 @@
     <param name="groupcol" label="Group by column" type="data_column" data_ref="input1" />
     <param name="ignorecase" type="boolean" truevalue="1" falsevalue="0">
       <label>Ignore case while grouping?</label>
+    </param>
+    <param name="ignorelines" type="select" display="checkboxes" multiple="True" label="Ignore lines beginning with these characters" help="lines beginning with these are not grouped">
+            <option value="62">&gt;</option>
+            <option value="64">@</option>
+            <option value="43">+</option>
+            <option value="60">&lt;</option>
+            <option value="42">*</option>
+            <option value="45">-</option>
+            <option value="61">=</option>
+            <option value="124">|</option>
+            <option value="63">?</option>
+            <option value="36">$</option>
+            <option value="46">.</option>
+            <option value="58">:</option>
+            <option value="38">&amp;</option>
+            <option value="37">%</option>
+            <option value="94">^</option>
+            <option value="35">&#35;</option>
     </param>
     <repeat name="operations" title="Operation">
       <param name="optype" type="select" label="Type">

commit 34900f87f5bc52bc84efe866371588004a049ebf

Fix grouping tool broken in 7347d75a4a36d42f8060e11d708291d796b266a5 / fd85979a8651e0907bebce05cdfb59d77f0d0f64, and enhance performance when removing lines.

tools/stats/grouping.py
@@ -38,19 +38,20 @@ def main():
     round_val = []
 
     if sys.argv[5] != "None":
-        oldfile = open(inputfile, 'r')
-        oldfilelines = oldfile.readlines()
-        newinputfile = "input_cleaned.tsv"
-        newfile = open(newinputfile, 'w')
-        asciitodelete = sys.argv[5].split(',')
-        for i in range(len(asciitodelete)):
-            asciitodelete[i] = chr(int(asciitodelete[i]))
-        for line in oldfilelines:
-            if line[0] not in asciitodelete:
-                newfile.write(line)
-        oldfile.close()
-        newfile.close()
-        inputfile = newinputfile
+        asciitodelete = sys.argv[5]
+        if asciitodelete:
+            oldfile = open(inputfile, 'r')
+            newinputfile = "input_cleaned.tsv"
+            newfile = open(newinputfile, 'w')
+            asciitodelete = asciitodelete.split(',')
+            for i in range(len(asciitodelete)):
+                asciitodelete[i] = chr(int(asciitodelete[i]))
+            for line in oldfile:
+                if line[0] not in asciitodelete:
+                    newfile.write(line)
+            oldfile.close()
+            newfile.close()
+            inputfile = newinputfile
 
     for var in sys.argv[6:]:
         op, col, do_round = var.split()

tools/stats/grouping.xml
@@ -1,12 +1,12 @@
-<tool id="Grouping1" name="Group" version="2.1.0">
+<tool id="Grouping1" name="Group" version="2.1.1">
   <description>data by a column and perform aggregate operation on other columns.</description>
   <command interpreter="python">
     grouping.py 
-      $out_file1
-      $input1
-      $groupcol
-      $ignorecase
-      $ignorelines
+      "${out_file1}"
+      "${input1}"
+      "${groupcol}"
+      "${ignorecase}"
+      "${ignorelines}"
       #for $op in $operations
        '${op.optype}
         ${op.opcol}