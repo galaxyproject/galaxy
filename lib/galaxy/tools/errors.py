c93f1964933fb1a8596a958511b93908b3746f23
Eric Rasche <rasche.eric@gmail.com>
2016-04-26 21:44:54 +0000

    Cast everything to a string
    cgi.escape doesn't work on non-strings

--- lib/galaxy/tools/errors.py
+++ lib/galaxy/tools/errors.py
@@ -208,7 +208,7 @@ class ErrorReporter( object ):
 
         # Escape all of the content  for use in the HTML report
         for parameter in report_variables.keys():
-            report_variables[parameter] = cgi.escape(report_variables[parameter])
+            report_variables[parameter] = cgi.escape(str(report_variables[parameter]))
 
         self.html_report = string.Template( error_report_template_html ).safe_substitute( report_variables )
 
