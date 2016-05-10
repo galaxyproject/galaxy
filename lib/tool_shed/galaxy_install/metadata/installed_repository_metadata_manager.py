9f9e008d9f63d6fa19255a52122c6cf6e374058c
Dave Bouvier <dave@bx.psu.edu>
2016-05-10 12:38:35 -0400

    Remove another unused import.

--- lib/tool_shed/galaxy_install/metadata/installed_repository_metadata_manager.py
+++ lib/tool_shed/galaxy_install/metadata/installed_repository_metadata_manager.py
@@ -10,7 +10,6 @@ from tool_shed.galaxy_install.tools import tool_panel_manager
 from tool_shed.metadata import metadata_generator
 from tool_shed.util import common_util
 from tool_shed.util import repository_util
-from tool_shed.util import shed_util_common as suc
 from tool_shed.util import tool_util
 from tool_shed.util import xml_util
 
