galaxyproject/galaxy

commit d38936bd8d9b3612dc0718859067e299651e6c86

Move the whole directory instead of element by element
Fix #1947.

lib/tool_shed/galaxy_install/tool_dependencies/recipe/step_handler.py
@@ -848,22 +848,10 @@ class MoveDirectoryFiles( RecipeStep ):
     def move_directory_files( self, current_dir, source_dir, destination_dir ):
         source_directory = os.path.abspath( os.path.join( current_dir, source_dir ) )
         destination_directory = os.path.join( destination_dir )
-        if not os.path.isdir( destination_directory ):
-            os.makedirs( destination_directory )
-        symlinks = []
-        regular_files = []
-        for file_name in os.listdir( source_directory ):
-            source_file = os.path.join( source_directory, file_name )
-            destination_file = os.path.join( destination_directory, file_name )
-            files_tuple = ( source_file, destination_file )
-            if os.path.islink( source_file ):
-                symlinks.append( files_tuple )
-            else:
-                regular_files.append( files_tuple )
-        for source_file, destination_file in symlinks:
-            shutil.move( source_file, destination_file )
-        for source_file, destination_file in regular_files:
-            shutil.move( source_file, destination_file )
+        destination_parent_directory = os.path.dirname(destination_directory)
+        if not os.path.isdir( destination_parent_directory ):
+            os.makedirs( destination_parent_directory )
+        shutil.move( source_directory, destination_directory )
 
     def prepare_step( self, tool_dependency, action_elem, action_dict, install_environment, is_binary_download ):
         # <action type="move_directory_files">

commit 89890d4f60452a9fc55d924fd960f4bd72ddbb12

Fix move_directory_files tool dependency action when destination is not empty
Historically this action moves the contents of the source directory to the
destination directory. Commit d38936bd8d9b3612dc0718859067e299651e6c86
broke the install when e.g. the destination is $INSTALL_DIR, which is
created by Galaxy and is not empty (contains INSTALLATION.log after first
action).

lib/tool_shed/galaxy_install/tool_dependencies/recipe/step_handler.py
@@ -847,11 +847,17 @@ class MoveDirectoryFiles( RecipeStep ):
 
     def move_directory_files( self, current_dir, source_dir, destination_dir ):
         source_directory = os.path.abspath( os.path.join( current_dir, source_dir ) )
-        destination_directory = os.path.join( destination_dir )
-        destination_parent_directory = os.path.dirname(destination_directory)
-        if not os.path.isdir( destination_parent_directory ):
-            os.makedirs( destination_parent_directory )
-        shutil.move( source_directory, destination_directory )
+        destination_directory = os.path.abspath(os.path.join(destination_dir))
+        if not os.path.isdir(destination_directory):
+            os.makedirs(destination_directory)
+        for dir_entry in os.listdir(source_directory):
+            source_entry = os.path.join(source_directory, dir_entry)
+            if os.path.islink(source_entry):
+                destination_entry = os.path.join(destination_directory, dir_entry)
+                os.symlink(os.readlink(source_entry), destination_entry)
+                os.remove(source_entry)
+            else:
+                shutil.move(source_entry, destination_directory)
 
     def prepare_step( self, tool_dependency, action_elem, action_dict, install_environment, is_binary_download ):
         # <action type="move_directory_files">