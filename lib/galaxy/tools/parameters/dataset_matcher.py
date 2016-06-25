galaxyproject/galaxy

commit 80ac816af94d43d7214671582db127483baea2d3

More general handling of what tool inputs are required in what state.

lib/galaxy/tools/parameters/dataset_matcher.py
@@ -35,7 +35,7 @@ class DatasetMatcher( object ):
         accessible to user.
         """
         dataset = hda.dataset
-        state_valid = dataset.state not in INVALID_STATES
+        state_valid = dataset.state in self.tool.valid_input_states
         return state_valid and ( not check_security or self.__can_access_dataset( dataset ) )
 
     def valid_hda_match( self, hda, check_implicit_conversions=True, check_security=False ):

lib/galaxy/model/__init__.py
@@ -1647,6 +1647,16 @@ class Dataset( StorableObject ):
         states.SETTING_METADATA
     )
     ready_states = tuple( set( states.__dict__.values() ) - set( non_ready_states ) )
+    valid_input_states = tuple(
+        set( states.__dict__.values() ) - set( [states.ERROR, states.DISCARDED] )
+    )
+    terminal_states = (
+        states.OK,
+        states.EMPTY,
+        states.ERROR,
+        states.DISCARDED,
+        states.FAILED_METADATA,
+    )
 
     conversion_messages = Bunch( PENDING="pending",
                                  NO_DATA="no data",

lib/galaxy/tools/__init__.py
@@ -364,6 +364,10 @@ class Tool( object, Dictifiable ):
 
         return any( map( output_is_dynamic, self.outputs.values() ) )
 
+    @property
+    def valid_input_states( self ):
+        return model.Dataset.valid_input_states
+
     def __get_job_tool_configuration(self, job_params=None):
         """Generalized method for getting this tool's job configuration.
 
@@ -2187,6 +2191,17 @@ class DataManagerTool( OutputParameterJSONTool ):
 class DatabaseOperationTool( Tool ):
     default_tool_action = ModelOperationToolAction
     require_dataset_ok = True
+
+    @property
+    def valid_input_states( self ):
+        if self.require_dataset_ok:
+            return (model.Dataset.states.OK,)
+        else:
+            return model.Dataset.terminal_states
+
+    @property
+    def allow_errored_inputs( self ):
+        return not self.require_dataset_ok
 
     def check_inputs_ready( self, input_datasets, input_dataset_collections ):
         def check_dataset_instance( input_dataset ):

commit 861740bd6080bf0ab280594d24460143cbee1033

Fix dataset matcher when no tool available.
Broken with 80ac816af94d43d7214671582db127483baea2d3.

lib/galaxy/tools/parameters/dataset_matcher.py
@@ -35,7 +35,12 @@ class DatasetMatcher( object ):
         accessible to user.
         """
         dataset = hda.dataset
-        state_valid = dataset.state in self.tool.valid_input_states
+        has_tool = self.tool
+        if has_tool:
+            valid_input_states = self.tool.valid_input_states
+        else:
+            valid_input_states = galaxy.model.Dataset.valid_input_states
+        state_valid = dataset.state in valid_input_states
         return state_valid and ( not check_security or self.__can_access_dataset( dataset ) )
 
     def valid_hda_match( self, hda, check_implicit_conversions=True, check_security=False ):