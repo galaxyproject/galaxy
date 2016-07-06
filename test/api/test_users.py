54e07906f51f58c1580b1bec64cdfc1c74ff8779
carlfeberhard <carlfeberhard@gmail.com>
2016-07-06 14:10:14 -0400

    User API tests: add update test

--- test/api/test_users.py
+++ test/api/test_users.py
@@ -1,4 +1,9 @@
+import logging
+import json
+from requests import put
 from base import api
+
+log = logging.getLogger(__name__)
 
 TEST_USER_EMAIL = "user_for_users_index_test@bx.psu.edu"
 
@@ -30,10 +35,25 @@ class UsersApiTestCase( api.ApiTestCase ):
             self._assert_status_code_is( show_response, 200 )
             self.__assert_matches_user( user, show_response.json() )
 
+    def test_update( self ):
+        user = self._setup_user( TEST_USER_EMAIL )
+        with self._different_user( email=TEST_USER_EMAIL ):
+            update_response = self.__update( user, username='noob' )
+            self._assert_status_code_is( update_response, 200 )
+            update_json = update_response.json()
+            log.info( update_json )
+            assert update_json[ 'username' ] == 'noob'
+
     def __show( self, user ):
         return self._get( "users/%s" % ( user[ 'id' ] ) )
 
+    def __update( self, user, **new_data ):
+        update_url = self._api_url( "users/%s" % ( user[ "id" ] ), use_key=True )
+        # TODO: Awkward json.dumps required here because of https://trello.com/c/CQwmCeG6
+        body = json.dumps( new_data )
+        return put( update_url, data=body )
+
     def __assert_matches_user( self, userA, userB ):
-        self._assert_has_keys( userB, "id", "username" )
+        self._assert_has_keys( userB, "id", "username", "total_disk_usage" )
         assert userA[ "id" ] == userB[ "id" ]
         assert userA[ "username" ] == userB[ "username" ]
