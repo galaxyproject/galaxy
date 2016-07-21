7dcc6374ccf9ce9903cc82f11f146a1f5aefdc87
Alexander Lenail <alexander.lenail@tufts.edu>
2016-07-21 15:39:21 -0400

    oops =)

--- lib/galaxy/dependencies/__init__.py
+++ lib/galaxy/dependencies/__init__.py
@@ -77,8 +77,8 @@ class ConditionalDependencies( object ):
         return self.check_weberror()
 
     def check_azure_storage( self ):
-        return self.config.get("azure_account_name", None) is not None and
-               self.config.get("azure_account_key", None) is not None
+        return (self.config.get("azure_account_name", None) is not None and
+                self.config.get("azure_account_key", None) is not None)
 
 def optional( config_file ):
     rval = []
