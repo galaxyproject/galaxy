9e82e59baff3ae23fe356aa6ad145324dcd30bf9
guerler <aysam.guerler@gmail.com>
2016-07-21 13:26:06 -0400

    Adjust menu button

--- client/galaxy/scripts/mvc/ui/ui-buttons.js
+++ client/galaxy/scripts/mvc/ui/ui-buttons.js
@@ -1,4 +1,4 @@
-/** This class contains all button views. */
+/** This module contains all button views. */
 define(['utils/utils'], function( Utils ) {
     /** This renders the default button which is used e.g. at the bottom of the upload modal. */
     var ButtonDefault = Backbone.View.extend({
@@ -43,6 +43,16 @@ define(['utils/utils'], function( Utils ) {
                 this.$icon.removeClass( options.icon ).addClass( 'fa-spinner fa-spin' );
                 this.$title.html( options.wait_text );
             }
+        },
+
+        /** Show button */
+        show: function() {
+            this.$el.show();
+        },
+
+        /** Hide button */
+        hide: function() {
+            this.$el.hide();
         },
 
         /** Disable button */
@@ -171,8 +181,7 @@ define(['utils/utils'], function( Utils ) {
                     .addClass( options.disabled && 'disabled' )
                     .attr( 'disabled', options.disabled )
                     .attr( 'id', options.id )
-                    .css( { float : options.floating,
-                            width : options.title && 'auto' } )
+                    .css( 'float', options.floating )
                     .off( 'click' ).on( 'click', function() {
                         $( '.tooltip' ).hide();
                         !options.disabled && options.onclick && options.onclick();
@@ -183,18 +192,11 @@ define(['utils/utils'], function( Utils ) {
         }
     });
 
-    /** This class creates a button with dropdown menu. It extends the functionality of the Ui.ButtonIcon class.
-        TODO: Consolidate class, use common base class
-    */
-    var ButtonMenu = Backbone.View.extend({
-        // optional sub menu
+    /** This class creates a button with dropdown menu. */
+    var ButtonMenu = ButtonDefault.extend({
         $menu: null,
-
-        // initialize
         initialize: function ( options ) {
-            // get options
-            this.options = Utils.merge( options, {
-                // same as Ui.ButtonIcon
+            this.model = options && options.model || new Backbone.Model({
                 id              : '',
                 title           : '',
                 floating        : 'right',
@@ -203,58 +205,42 @@ define(['utils/utils'], function( Utils ) {
                 onclick         : null,
                 cls             : 'ui-button-icon ui-button-menu',
                 tooltip         : '',
-                
-                // additional options
                 target          : '',
                 href            : '',
                 onunload        : null,
                 visible         : true,
                 tag             : ''
-            } );
+            }).set( options );
+            this.setElement( $( '<div/>' ).append( this.$root = $( '<div/>' ).append( this.$icon  = $( '<i/>' ) )
+                                                                             .append( this.$title = $( '<span/>' ) ) ) );
+            this.listenTo( this.model, 'change', this.render, this );
+            this.render();
+        },
 
-            // add template for tab
-            this.setElement( $( this._template( this.options ) ) );
-
-            // find root
-            var $root = $( this.el ).find( '.root' );
-
-            // link head
+        render: function() {
             var self = this;
-            $root.on( 'click', function( e ) {
-                // hide all tooltips
-                $( '.tooltip' ).hide();
-
-                // prevent default
-                e.preventDefault();
-
-                // add click event
-                if( self.options.onclick ) {
-                    self.options.onclick();
-                }
-            });
-
-            // visiblity
-            if ( !this.options.visible )
-                this.hide();
-
-            // add tooltip
-            $root.tooltip( { title: options.tooltip, placement: 'bottom' } );
+            var options = this.model.attributes;
+            this.$el.removeClass()
+                    .addClass( 'dropdown' )
+                    .addClass( options.cls )
+                    .attr( 'id', options.id )
+                    .css( { float   : options.floating,
+                            display : options.visible ? 'block' : 'none' } );
+            this.$root.addClass( 'root button dropdown-toggle' )
+                      .attr( 'data-toggle', 'dropdown' )
+                      .tooltip( { title: options.tooltip, placement: 'bottom' } )
+                      .off( 'click' ).on( 'click', function( e ) {
+                            $( '.tooltip' ).hide();
+                            e.preventDefault();
+                            options.onclick && options.onclick();
+                      } );
+            this.$icon.removeClass().addClass( 'icon fa' ).addClass( options.icon );
+            options.title && this.$title.removeClass().addClass( 'title' ).html( options.title );
         },
 
-        // show
-        show: function() {
-            $( this.el ).show();
-        },
-
-        // hide
-        hide: function() {
-            $( this.el ).hide();
-        },
-
-        // add menu item
+        /** Add a new menu item */
         addMenu: function ( options ) {
-            // menu option defaults
-            var menuOptions = {
+            var options = Utils.merge( options, {
                 title       : '',
                 target      : '',
                 href        : '',
@@ -262,85 +248,29 @@ define(['utils/utils'], function( Utils ) {
                 divider     : false,
                 icon        : null,
                 cls         : 'button-menu btn-group'
-            }
-
-            // get options
-            menuOptions = Utils.merge( options, menuOptions );
-
-            // check if submenu element is available
-            if ( !this.$menu ) {
-                // insert submenu element into root
-                $( this.el ).append( this._templateMenu() );
-
-                // update element link
-                this.$menu = $( this.el ).find( '.menu' );
-            }
-
-            // create
-            var $item = $( this._templateMenuItem( menuOptions ) );
-
-            // add events
-            $item.on( 'click', function( e ) {
-                if( menuOptions.onclick ) {
-                    e.preventDefault();
-                    menuOptions.onclick();
-                }
             });
-
-            // append menu
-            this.$menu.append( $item );
-
-            // append divider
-            if ( menuOptions.divider ) {
-                this.$menu.append( $( this._templateDivider() ) );
+            if ( !this.$menu ) {
+                this.$menu = $( '<ul/>' ).addClass( 'menu dropdown-menu' )
+                                         .addClass( 'pull-' + this.model.get( 'pull' ) )
+                                         .attr( 'role', 'menu' );
+                this.$el.append( this.$menu );
             }
-        },
-
-        // fill template header
-        _templateMenuItem: function ( options ) {
-            var tmpl =  '<li>' +
-                            '<a class="dropdown-item" href="' + options.href + '" target="' + options.target + '" ';
-            if ( options.download ) {
-                tmpl +=         'download="' + options.download + '"';
-            }
-            tmpl +=         '>';
-            if ( options.icon ) {
-                tmpl +=         '<i class="fa ' + options.icon + '"/>';
-            }
-            tmpl +=             ' ' + options.title +
-                            '</a>' +
-                        '</li>';
-            return tmpl;
-        },
-
-        // fill template header
-        _templateMenu: function () {
-            return '<ul class="menu dropdown-menu pull-' + this.options.pull + '" role="menu"/>';
-        },
-
-        _templateDivider: function() {
-            return '<li class="divider"/>';
-        },
-
-        // element
-        _template: function(options) {
-            // TODO: width/margin should be set in css
-            var width = '';
-            var margin = '';
-            if ( options.title ) {
-                width = 'width: auto;';
-            } else {
-                margin = 'margin: 0px;';
-            }
-            var str =   '<div id="' + options.id + '" style="float: ' + options.floating + '; ' + width + '" class="dropdown ' + options.cls + '">' +
-                            '<div class="root button dropdown-toggle" data-toggle="dropdown" style="' + margin + '">' +
-                                '<i class="icon fa ' + options.icon + '"/>';
-            if ( options.title ) {
-                str +=          '&nbsp;<span class="title">' + options.title + '</span>';
-            }
-            str +=          '</div>' +
-                        '</div>';
-            return str;
+            var $link = $( '<a/>' ).addClass( 'dropdown-item' )
+                                   .attr( { href        : options.href,
+                                            target      : options.target,
+                                            download    : options.download } )
+                                   .append( $( '<i/>' ).addClass( 'fa' )
+                                                       .addClass( options.icon )
+                                                       .css( 'display', options.icon ? 'inline-block' : 'none' ) )
+                                   .append( options.title )
+                                   .on( 'click', function( e ) {
+                                        if ( options.onclick ) {
+                                            e.preventDefault();
+                                            options.onclick();
+                                        }
+                                   } );
+            this.$menu.append( $( '<li/>' ).append( $link ) );
+            options.divider && this.$menu.append( $( '<li/>' ).addClass( 'divider' ) );
         }
     });
 
