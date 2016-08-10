e35c6295694c4065fc880e00fabe43f0c1905748
Marius van den Beek <m.vandenbeek@gmail.com>
2016-08-10 19:23:17 +0200

    Extend startup time

--- .ci/first_startup.sh
+++ .ci/first_startup.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+TRIES=120
+URL=http://localhost:8080
 echo "Testing for correct startup:"
-bash run.sh --daemon && sleep 30s && curl -I localhost:8080
-EXIT_CODE=$?
+bash run.sh --daemon && \
+while [[ $i -le $TRIES ]]
+do
+    curl "$URL" && EXIT_CODE=0 && break
+    sleep 1
+    EXIT_CODE=1
+    ((i = i + 1))
+done
 echo "exit code:$EXIT_CODE, showing startup log:"
 cat paster.log
 exit $EXIT_CODE
