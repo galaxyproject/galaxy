86690af610a98a60da09a759bf7c3b5d0ab94441
John Chilton <jmchilton@gmail.com>
2016-08-16 16:03:57 -0400

    Remove nodejs dynamic proxy and supporting infrastructure.
    Possibly the most widely hated thing I ever added to Galaxy?

--- lib/galaxy/web/proxy/__init__.py
+++ lib/galaxy/web/proxy/__init__.py
@@ -4,7 +4,6 @@ import json
 
 from .filelock import FileLock
 from galaxy.util import sockets
-from galaxy.util.lazy_process import LazyProcess, NoOpLazyProcess
 from galaxy.util import sqlite
 from galaxy.util import unique_id
 import urllib2
