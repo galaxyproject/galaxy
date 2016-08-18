galaxyproject/galaxy

commit d5c52cadfd0007338d4c89f505ff272e9acae9f9

Fixing error in run.sh script

run.sh
@@ -34,7 +34,7 @@ do
             shift
             ;;
         --daemon|--restart|restart)
-            if [ $1 == "--restart" ]
+            if [ "$1"=="--restart" ]
             then
                 paster_args="$paster_args restart"
             else

commit 1f69bf32e8cf3c073804d2cdbb3823e14f1a8ce5

Fix run.sh --daemon
Broken in commit d5c52cadfd0007338d4c89f505ff272e9acae9f9 .
Partial backport from #2765 .
Close #2724.

run.sh
@@ -34,7 +34,7 @@ do
             shift
             ;;
         --daemon|--restart|restart)
-            if [ "$1"=="--restart" ]
+            if [ "$1" == "--restart" ]
             then
                 paster_args="$paster_args restart"
             else