600a4177f00799ff6578005a5a8ae00427b45c21
guerler <aysam.guerler@gmail.com>
2016-08-19 10:35:59 -0400

    Add description for jqplots

--- config/plugins/visualizations/charts/static/views/types.js
+++ config/plugins/visualizations/charts/static/views/types.js
@@ -11,7 +11,7 @@ define( [ 'utils/utils', 'mvc/ui/ui-misc' ], function( Utils, Ui ) {
                 { label: 'NVD3',    value: 'nvd3',
                   help: 'NVD3 is a popular d3-based library hosted at <a href="http://www.nvd3.org">www.nvd3.org<a/>. Most visualization types support intuitive drag-and-drop zooming and scaling. NVD3 generates high quality vector graphics. As with all vector based plugins, you may experience a slow down in browser responsiveness if visualizing thousands of data points.' },
                 { label: 'jqPlot',  value: 'jqplot',
-                  help: 'jqPlot is a canvas-based library hosted at www.jqplot.com.' },
+                  help: 'jqPlot is a canvas-based library hosted at <a href="http://www.jqplot.com">www.jqplot.com<a/>. It is a plotting and charting plugin for the jQuery Javascript framework. Since based on canvas it is capable of rendering many thousand data points without adverse effects.' },
                 { label: 'Others',  value: 'others',
                   help: 'These visualizations were developed by the Galaxy team.' } ],
 
