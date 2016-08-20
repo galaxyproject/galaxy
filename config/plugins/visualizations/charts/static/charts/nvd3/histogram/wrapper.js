13d35ba8eae6674f496c0abe8bad0fe8993d1576
guerler <aysam.guerler@gmail.com>
2016-08-19 21:19:25 -0400

    Revise histogram wrapper

--- config/plugins/visualizations/charts/static/charts/nvd3/histogram/wrapper.js
+++ config/plugins/visualizations/charts/static/charts/nvd3/histogram/wrapper.js
@@ -2,21 +2,12 @@ define( [ 'plugin/charts/utilities', 'plugin/library/jobs', 'plugin/charts/nvd3/
     return Backbone.Model.extend({
         initialize: function( app, options ) {
             Jobs.request( app, 'histogram', function( dataset ) {
-                var request_dictionary = Utilities.tabularRequestDictionary( app.chart, dataset.id );
+                options.request_dictionary = Utilities.tabularRequestDictionary( app.chart, dataset.id );
                 var index = 1;
-                for ( var i in request_dictionary.groups ) {
-                    var group = request_dictionary.groups[ i ];
-                    group.columns = {
-                        x: {
-                            index       : 0,
-                            is_numeric  : true
-                        },
-                        y: {
-                            index       : index++
-                        }
-                    }
-                }
-                options.request_dictionary = request_dictionary;
+                _.each( options.request_dictionary.groups, function( group ) {
+                    group.columns = { x : { index : 0, is_numeric : true },
+                                      y : { index : index++ } }
+                });
                 options.type = 'multiBarChart';
                 options.makeConfig = function( nvd3_model ) {
                     nvd3_model.options( { showControls: true } );
