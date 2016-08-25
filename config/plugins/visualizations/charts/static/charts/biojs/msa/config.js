f98ade125f0fbe5d90ca0f005a84f8f7d91fb3d5
guerler <aysam.guerler@gmail.com>
2016-08-25 16:25:39 -0400

    Add basic description for msa viewer

--- config/plugins/visualizations/charts/static/charts/biojs/msa/config.js
+++ config/plugins/visualizations/charts/static/charts/biojs/msa/config.js
@@ -5,6 +5,7 @@ define( [], function() {
         library     : 'BioJS',
         datatype    : 'txt',
         keywords    : 'biojs',
+        description : 'The MSA viewer is a modular, reusable component to visualize large MSAs interactively on the web.',
         settings    : {
             conserv: { label :'Show conservation', help : 'Do you want to display a bar diagram indicating sequence conservation?', type : 'boolean', value: 'true' },
             overviewbox: { label :'Show overview box', help : 'Do you want to display the overview box below the sequence alignments?', type : 'boolean', value: 'true' },
