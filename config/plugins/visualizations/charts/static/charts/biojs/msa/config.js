6a450b9a06a81a58875297af7832f1d5a2cab8a5
guerler <aysam.guerler@gmail.com>
2016-08-24 22:42:07 -0400

    Add basic options to configure msa viewer

--- config/plugins/visualizations/charts/static/charts/biojs/msa/config.js
+++ config/plugins/visualizations/charts/static/charts/biojs/msa/config.js
@@ -4,6 +4,11 @@ define( [], function() {
         category    : 'Sequence',
         library     : 'BioJS',
         datatype    : 'txt',
-        keywords    : 'biojs'
+        keywords    : 'biojs',
+        settings    : {
+            conserv: { label :'Show conservation', help : 'Do you want to display a bar diagram indicating sequence conservation?', type : 'boolean', value: 'true' },
+            overviewbox: { label :'Show overview box', help : 'Do you want to display the overview box below the sequence alignments?', type : 'boolean', value: 'true' },
+            menu: { label :'Show interactive menu', help : 'Do you want to show a menu for interactive configuration?', type : 'boolean', value: 'true' }
+        }
     }
 });
--- config/plugins/visualizations/charts/static/charts/biojs/msa/wrapper.js
+++ config/plugins/visualizations/charts/static/charts/biojs/msa/wrapper.js
@@ -2,11 +2,13 @@ define( [ 'utils/utils' ], function( Utils ) {
     return Backbone.Model.extend({
         initialize: function( app, options ) {
             var chart = app.chart;
+            var settings = chart.settings;
             var m = new msa.msa({
                 el: $( '#'  + options.canvas_list[ 0 ] ),
-                vis: { conserv: false, overviewbox: false },
+                vis: {  conserv: settings.get( 'conserv' ) == 'true',
+                        overviewbox: settings.get( 'overviewbox' ) == 'true' },
                 menu: 'small',
-                bootstrapMenu: true
+                bootstrapMenu: settings.get( 'menu' ) == 'true'
             });
             m.u.file.importURL( options.dataset.download_url, function() {
                 m.render();
