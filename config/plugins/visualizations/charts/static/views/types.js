f03a06488feb0e1c471c92d11cff14c6bf5c0cf1
guerler <aysam.guerler@gmail.com>
2016-08-30 21:48:36 -0400

    Limit title length for thumbnails

--- config/plugins/visualizations/charts/static/views/types.js
+++ config/plugins/visualizations/charts/static/views/types.js
@@ -16,6 +16,7 @@ define( [ 'utils/utils', 'mvc/ui/ui-misc', 'mvc/ui/ui-tabs' ], function( Utils, 
         },
 
         render: function() {
+            this.first = null;
             this.tabs.delAll();
             this._renderDefault();
             this._renderList();
@@ -24,14 +25,15 @@ define( [ 'utils/utils', 'mvc/ui/ui-misc', 'mvc/ui/ui-tabs' ], function( Utils, 
         _renderDefault: function() {
             var self = this;
             var index = [];
-            this.first = null;
+            var title_length = 20;
             var $el = $( '<div/>' ).addClass( 'charts-grid' );
             _.each( this.app.types, function( type, type_id ) {
                 if ( !type.datatypes || type.datatypes.indexOf( self.app.dataset.file_ext ) != -1  ) {
                     if ( type.keywords.indexOf( 'default' ) !== -1 ) {
+                        var title = type.title.length < title_length ? type.title : type.title.substr( 0, title_length ) + '...';
                         $el.append( $( self._templateThumbnailItem( {
                             id      : type_id,
-                            title   : ( type.zoomable ? '<span class="fa fa-search-plus"/>' : '' ) + type.title + ' (' + type.library + ')',
+                            title   : ( type.zoomable ? '<span class="fa fa-search-plus"/>' : '' ) + title + ' (' + type.library + ')',
                             url     : remote_root + 'src/visualizations/' + self.app.split( type_id ) + '/logo.png'
                         })).tooltip( { title: type.description, placement: 'bottom' } ) );
                     }
