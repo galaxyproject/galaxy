5d780d68d1a2ec5d1928a0156637a1d522f5b743
guerler <aysam.guerler@gmail.com>
2016-08-31 12:58:37 -0400

    Revise exporting functions, increase png quality, remove unused libraries

--- config/plugins/visualizations/charts/static/components/screenshot.js
+++ config/plugins/visualizations/charts/static/components/screenshot.js
@@ -1,171 +1,82 @@
-/**
- *  This class enables users to export/download a chart as PNG, SVG or PDF.
- */
-define(["libs/underscore"], function(_) {
-    //
-    // PNG export
-    //
+/** This class enables users to export/download a chart as PNG, SVG or PDF. */
+define( [ 'libs/underscore' ], function( _ ) {
+    /** PNG export */
     function createPNG (options) {
-        if (options.$el.find('svg').length > 0) {
-            _fromSVGtoPNG (options);
+        if ( options.$el.find( 'svg' ).length > 0 ) {
+            _svg2png( options );
         } else {
-            _fromCanvas (options);
+            _canvas2png( options.$el.find( '.charts-viewport-canvas' ) );
         }
     };
-    
-    // from jqplot
-    function _fromCanvas (options) {
+
+    /** Convert canvas to png */
+    function _canvas2png( $canvas ) {
         try {
-            var $canvas = options.$el.find('.jqplot-target');
-            var imgData = $canvas.jqplotToImageStr({});
-            if (imgData) {
-                window.location.href = imgData.replace('image/png', 'image/octet-stream');
-            }
-        } catch (err) {
-            console.debug('FAILED - Screenshot::_fromCanvas() - ' + err);
-            if (options.error) {
-                options.error('Please reduce your chart to a single panel and try again.');
-            }
-        }
-    };
-
-    // from svg
-    function _fromSVGtoPNG (options) {
-       
-        // get parameters
-        var $el = options.$el;
-        var screenshot_url = options.url;
-        var name = options.name;
-       
-        // serialize svg
-        var serializer = new XMLSerializer();
-       
-        // create canvas
-        var canvas = document.createElement('canvas');
-        var $canvas = $(canvas);
-       
-        // get svg dimensions
-        var nsvgs  = $el.find('svg').length;
-        var first = $el.find('svg').first();
-        var height = parseInt(first.css('height'));
-        var width  = parseInt(first.css('width'));
-       
-        // set canvas dimensions
-        $canvas.attr('width', width*nsvgs);
-        $canvas.attr('height', height);
-       
-        // check context support
-        if (!canvas.getContext || !canvas.getContext('2d')) {
-            alert ("Your browser doesn't support this feature, please use a modern browser");
-        }
-       
-        // context
-        var ctx = canvas.getContext('2d');
-       
-        // write all svgs
-        var offsetX = 0;
-        $el.find('svg').each(function() {
-            // get svg element
-            var $svg = $(this);
-            
-            // configure svg
-            $svg.attr({
-                version : '1.1',
-                xmlns   : 'http://www.w3.org/2000/svg',
-                width   : width,
-                height  : height
-            });
-
-            // create xml string
-            var xmlString = serializer.serializeToString(this);
-            
-            // draw into canvas
-            ctx.drawSvg(xmlString, offsetX, 0, width, height)
-            
-            // shift offset for multipanel charts
-            offsetX += width;
-        });
-       
-        // post image
-        window.location.href = _canvasToImage(canvas, canvas.getContext('2d'), 'white').replace('image/png', 'image/octet-stream');
-    };
-    
-    // create image
-    function _canvasToImage(canvas, context, backgroundColor) {
-        // cache height and width
-        var w = canvas.width;
-        var h = canvas.height;
-        var data;
-     
-        if(backgroundColor) {
-            // get the current ImageData for the canvas.
-            data = context.getImageData(0, 0, w, h);
-     
-            // store the current globalCompositeOperation
-            var compositeOperation = context.globalCompositeOperation;
-     
-            // set to draw behind current content
-            context.globalCompositeOperation = "destination-over";
-     
-            // set background color
-            context.fillStyle = backgroundColor;
-     
-            // draw background / rect on entire canvas
-            context.fillRect(0,0,w,h);
-        }
-     
-        // get the image data from the canvas
-        var imageData = canvas.toDataURL("image/png");
-     
-        if(backgroundColor) {
-            // clear the canvas
-            context.clearRect (0,0,w,h);
-     
-            // restore it with original / cached ImageData
-            context.putImageData(data, 0,0);
-     
-            // reset the globalCompositeOperation to what it was
-            context.globalCompositeOperation = compositeOperation;
-        }
-     
-        // return the Base64 encoded data url string
-        return imageData;
-    };
-
-    //
-    // SVG export
-    //
-    function createSVG (options) {
-        window.location.href = 'data:none/none;base64,' + btoa(createXML(options).string);
-    };
-    
-    // css inliner
-    function _inline ($target) {
-        for (var sheet_id in document.styleSheets) {
-            var sheet = document.styleSheets[sheet_id];
-            var rules = sheet.cssRules;
-            if (rules) {
-                for (var idx = 0, len = rules.length; idx < len; idx++) {
-                    try {
-                        $target.find(rules[idx].selectorText).each(function (i, elem) {
-                            elem.style.cssText += rules[idx].style.cssText;
-                        });
-                    } catch(err) {
+            if ( $canvas.width() !== 0 && $canvas.height() !== 0) {
+                var newCanvas = document.createElement( 'canvas' );
+                newCanvas.width = $canvas.outerWidth( true );
+                newCanvas.height = $canvas.outerHeight( true );
+                var newContext = newCanvas.getContext( '2d' );
+                newContext.save();
+                newContext.fillStyle = 'rgb(255,255,255)';
+                newContext.fillRect( 0,0, newCanvas.width, newCanvas.height );
+                newContext.restore();
+                newContext.translate( 0, 0 );
+                newContext.textAlign = 'left';
+                newContext.textBaseline = 'top';
+                function _toImage( $el, x_offset, y_offset ) {
+                    var tagname = $el.prop( 'tagName' ).toLowerCase();
+                    var p = $el.position();
+                    var left = x_offset + p.left + parseInt( $el.css( 'marginLeft' ), 10 ) + parseInt( $el.css( 'borderLeftWidth' ), 10 ) + parseInt( $el.css( 'paddingLeft' ), 10 );
+                    var top = y_offset + p.top + parseInt( $el.css( 'marginTop' ), 10 ) + parseInt( $el.css( 'borderTopWidth' ), 10 ) + parseInt( $el.css( 'paddingTop' ), 10 );
+                    var w = newCanvas.width;
+                    if ( tagname == 'div' || tagname == 'span' ) {
+                        $el.children().each( function() { _toImage( $( this ), left, top ) } );
+                    } else if ( tagname == 'canvas' ) {
+                        newContext.drawImage( $el[ 0 ], left, top );
                     }
                 }
+                $canvas.children().each( function() { _toImage( $( this ), 0, 0 ) } );
+                imgData = newCanvas.toDataURL( 'image/png' );
+                if ( imgData ) {
+                    window.location.href = imgData.replace( 'image/png', 'image/octet-stream' );
+                }
+            }
+        } catch ( err ) {
+            console.debug( 'FAILED - Screenshot::_canvas2png() - ' + err );
+            if (options.error) {
+                options.error( 'Please reduce your chart to a single panel and try again.' );
             }
         }
     };
-    
-    //
-    // PDF export
-    //
-    function createPDF (options) {
-    
-        // do the post
-        var xml = createXML(options);
-       
-        // post data
+
+    /** Convert svg to png */
+    function _svg2png ( options ) {
+        var scale = 5;
+        var xml = toXML( options );
+        var canvas = document.createElement( 'canvas' );
+        var context = canvas.getContext( '2d' );
+        var source = new Image();
+        var $container = $( '<div style="display:none;"/>' ).append( $ ( canvas ) );
+        $( 'body' ).append( $container );
+        canvas.width = xml.width * scale;
+        canvas.height = xml.height * scale;
+        source.src = 'data:image/svg+xml;base64,' + btoa( xml.string );
+        source.onload = function() {
+            context.drawImage( source, 0, 0, canvas.width, canvas.height );
+            window.location.href = canvas.toDataURL( 'image/png' ).replace( 'image/png', 'image/octet-stream' );
+            $container.remove();
+        }
+    };
+
+    /** SVG export */
+    function createSVG (options) {
+        window.location.href = 'data:none/none;base64,' + btoa(toXML(options).string);
+    };
+
+    /** PDF export */
+    function createPDF( options ) {
+        var xml = toXML( options );
         var data = {
             filename    : name || 'chart',
             type        : 'application/pdf',
@@ -174,106 +85,85 @@ define(["libs/underscore"], function(_) {
             scale       : 2,
             svg         : xml.string
         };
-
-       
-        // create the form
-        var $el = $('body');
-        var form = $el.find('#viewport-form');
-        if (form.length === 0) {
-            form = $('<form>', {
+        var $el = $( 'body' );
+        var form = $el.find( '#viewport-form' );
+        if ( form.length === 0 ) {
+            form = $( '<form>', {
                 id      : 'viewport-form',
                 method  : 'post',
                 action  : 'http://export.highcharts.com/',
                 display : 'none'
             });
-            $el.append(form);
+            $el.append( form );
         }
-
-        // reset form
         form.empty();
-
-        // add the data
-        for (name in data) {
-            var input = $('<input/>', {
+        for ( name in data ) {
+            var input = $( '<input/>', {
                 type    : 'hidden',
                 name    : name,
-                value   : data[name]
+                value   : data[ name ]
             });
-            form.append(input);
+            form.append( input );
         }
-
-        // submit
         try {
             form.submit();
-        } catch(err) {
-            console.log(err);
+        } catch( err ) {
+            console.log( err );
         }
     };
-    
-    //
-    // XML export
-    //
-    function createXML (options) {
-        if (options.$el.find('svg').length == 0) {
-            if (options.error) {
-                options.error('No SVG found. This chart type does not support SVG/PDF export.');
+
+    /** XML export */
+    function toXML( options ) {
+        var $svg = options.$el.find( 'svg' );
+        if ( $svg.length == 0 ) {
+            if ( options.error ) {
+                options.error( 'No SVG found. This chart type does not support SVG/PDF export.' );
                 return;
             }
         }
-       
-        // get parameters
         var $el = options.$el;
-       
-        // get svg dimensions
-        var nsvgs  = $el.find('svg').length;
-        var height = parseInt($el.find('svg').first().css('height'));
-        var width  = parseInt($el.find('svg').first().css('width'));
-       
-        // serialize svg
+        var nsvgs  = $svg.length;
+        var height = parseInt( $svg.first().css( 'height' ) );
+        var width  = parseInt( $svg.first().css( 'width' ) );
         var serializer = new XMLSerializer();
-
-        // get svg element
-        var $composite = $('<svg/>');
-        
-        // configure svg
-        $composite.attr({
-            version : '1.1',
-            xmlns   : 'http://www.w3.org/2000/svg',
-            width   : width*nsvgs,
-            height  : height
-        });
-   
-        // write all svgs
-        var xmlString   = '';
-        var offsetX     = 0;
-        $el.find('svg').each(function() {
-            var $svg = $(this).clone();
-            
-            // inline css
-            _inline($svg);
-            
-            // create group and append to composite svg
-            var $g = $('<g transform="translate(' + offsetX + ', 0)">');
-            $g.append($svg.find('g').first());
-            $composite.append($g);
-            
-            // shift offset for multipanel charts
+        var $composite = $( '<svg/>' ).attr( { version: '1.1', xmlns: 'http://www.w3.org/2000/svg', width: width * nsvgs, height: height } );
+        var xmlString = '';
+        var offsetX = 0;
+        $svg.each(function() {
+            var $svg = $( this ).clone();
+            _inline( $svg );
+            var $g = $( '<g transform="translate(' + offsetX + ', 0)">' ).attr( 'xmlns', 'http://www.w3.org/2000/svg' );
+            $g.append( $svg.find( 'g' ).first() );
+            $composite.append( $g );
             offsetX += width;
         });
-       
-        // create xml string
         return {
-            string  : serializer.serializeToString($composite[0]),
+            string  : serializer.serializeToString( $composite[ 0 ] ),
             height  : height,
             width   : width
         }
     };
-    
-// return
-return {
-    createPNG: createPNG,
-    createSVG: createSVG,
-    createPDF: createPDF
-};
 
+    /** inlines CSS code */
+    function _inline( $target ) {
+        for ( var sheet_id in document.styleSheets ) {
+            var sheet = document.styleSheets[ sheet_id ];
+            var rules = sheet.cssRules;
+            if ( rules ) {
+                for ( var idx = 0, len = rules.length; idx < len; idx++ ) {
+                    try {
+                        $target.find( rules[ idx ].selectorText ).each( function ( i, elem ) {
+                            elem.style.cssText += rules[idx].style.cssText;
+                        });
+                    } catch( err ) {}
+                }
+            }
+        }
+    };
+
+    return {
+        createPNG: createPNG,
+        createSVG: createSVG,
+        createPDF: createPDF
+    };
 });
