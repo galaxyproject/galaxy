2fa6ac76203dc69bd4975b12c2f3c9c0a5d70fac
Scott Szakonyi <sszakony@nd.edu>
2016-09-01 11:04:34 -0400

    Fixed first suggestion on 2694.

--- client/galaxy/scripts/mvc/dataset/data.js
+++ client/galaxy/scripts/mvc/dataset/data.js
@@ -276,13 +276,16 @@ var TabularDatasetChunkedView = Backbone.View.extend({
     },
 
     _renderChunk: function(chunk) {
-        var data_table = this.$el.find('table');
-        _.each(chunk.ck_data.split('\n'), function(line, index) {
-//            if (line !== ''){
-            data_table.append(this._renderRow(line));
-//            }
-        }, this);
-    }
+        // Revised for 2694 by Scott Szakonyi
+        // Previous code using for each structure caused the addition of a line to the output
+        // when blank lines were not supressed. Switched to a for loop to allow the added line
+        // to be stripped from the output.
+        var data_table = this.$el.find('table'); 
+        var lines = chunk.ck_data.split('\n'); 
+        var numlines = lines.length - 1; 
+	for (i = 0; i < numlines; i++){ 
+            data_table.append(this._renderRow(lines[i]));
+    }},
 });
 
 /**
