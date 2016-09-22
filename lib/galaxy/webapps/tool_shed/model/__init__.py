c06f8c29968971c6c1e0dfc6847b3baf755f3a96
Martin Cech <cech.marten@gmail.com>
2016-09-22 16:00:51 -0400

    correct the method call

--- lib/galaxy/webapps/tool_shed/model/__init__.py
+++ lib/galaxy/webapps/tool_shed/model/__init__.py
@@ -246,7 +246,7 @@ class Repository( object, Dictifiable ):
 
     def get_tool_dependencies( self, app, changeset_revision ):
         repo = hg.repository( ui.ui(), self.repo_path( app ) )
-        changeset_revision = metadata_util.get_next_downloadable_changeset_revision( self, repo, changeset_revision )
+        changeset_revision = suc.get_next_downloadable_changeset_revision( self, repo, changeset_revision )
         for downloadable_revision in self.downloadable_revisions:
             if downloadable_revision.changeset_revision == changeset_revision:
                 return downloadable_revision.metadata.get( 'tool_dependencies', {} )
