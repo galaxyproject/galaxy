2c8a5d9d58c49c662aed4193f71caaf3fafe5651
Dave Bouvier <dave@bx.psu.edu>
2016-09-23 12:13:10 -0400

    Correct import order.

--- scripts/tool_shed/build_ts_whoosh_index.py
+++ scripts/tool_shed/build_ts_whoosh_index.py
@@ -23,9 +23,8 @@ sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pa
 
 import galaxy.webapps.tool_shed.model.mapping
 from galaxy.tools.loader_directory import load_tool_elements_from_path
-from galaxy.util import pretty_print_time_interval
+from galaxy.util import directory_hash_id, pretty_print_time_interval
 from galaxy.webapps.tool_shed import config, model
-from galaxy.util import directory_hash_id
 
 repo_schema = Schema(
     id=STORED,
