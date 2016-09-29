a552edcb6a98257b2fceabc476270f8b807fb986
John Chilton <jmchilton@gmail.com>
2016-09-29 11:52:52 -0400

    Add script to run mulled from directories of and single TSV files.
    This should be able to serve as the CI target for communities to be able to turn Github PRs into mulled images.

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ ENTRY_POINTS = '''
         mulled-build-channel=galaxy.tools.deps.mulled.mulled_build_channel:main
         mulled-search=galaxy.tools.deps.mulled.mulled_search:main
         mulled-build-tool=galaxy.tools.deps.mulled.mulled_build_tool:main
+        mulled-build-files=galaxy.tools.deps.mulled.mulled_build_files:main 
 '''
 PACKAGE_DATA = {
     # Be sure to update MANIFEST.in for source dist.
