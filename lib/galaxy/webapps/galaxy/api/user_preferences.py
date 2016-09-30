c7f21301ddc3b5d95e3085bffd3e39f0b14924f0
guerler <aysam.guerler@gmail.com>
2016-09-30 01:53:21 -0400

    Fix tabs

--- lib/galaxy/webapps/galaxy/api/user_preferences.py
+++ lib/galaxy/webapps/galaxy/api/user_preferences.py
@@ -73,17 +73,18 @@ class UserPrefAPIController( BaseAPIController, BaseUIController, UsesTagsMixin,
                     inputs.append( { 'type': 'section', 'title': 'Custom options', 'inputs': custom_form_model.to_dict() } )
             info_form_models = self.get_all_forms( trans, filter=dict( deleted=False ), form_type=trans.app.model.FormDefinition.types.USER_INFO )
             info_forms = [ f.to_dict() for f in info_form_models ]
-            info_field = { 'type'   : 'conditional',
-                           'name'   : 'user_info',
-                           'cases'  : [],
-                           'test_param' : {
-                                'name'  : 'selected',
-                                'label' : 'User information',
-                                'type'  : 'select',
-                                'help'  : '',
-                                'data'  : []
-                            }
-                         }
+            info_field = {
+                'type'   : 'conditional',
+                'name'   : 'user_info',
+                'cases'  : [],
+                'test_param' : {
+                    'name'  : 'selected',
+                    'label' : 'User information',
+                    'type'  : 'select',
+                    'help'  : '',
+                    'data'  : []
+                }
+            }
             for i, d in enumerate( info_forms ):
                 info_field[ 'test_param' ][ 'data' ].append( { 'label' : d[ 'name' ], 'value': i } )
                 info_field[ 'cases' ].append( { 'value': i, 'inputs' : d[ 'inputs' ] } )
