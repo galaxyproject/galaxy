galaxyproject/galaxy

commit 7bee2651aa8f08ea0495d167e33d317a22ae3ef9

string_as_bool for message_box_visible on webapps

lib/galaxy/webapps/reports/config.py
@@ -40,7 +40,7 @@ class Configuration( object ):
         self.log_actions = string_as_bool( kwargs.get( 'log_actions', 'False' ) )
         self.brand = kwargs.get( 'brand', None )
         # Configuration for the message box directly below the masthead.
-        self.message_box_visible = kwargs.get( 'message_box_visible', False )
+        self.message_box_visible =string_as_bool( kwargs.get( 'message_box_visible', False ) )
         self.message_box_content = kwargs.get( 'message_box_content', None )
         self.message_box_class = kwargs.get( 'message_box_class', 'info' )
         self.wiki_url = kwargs.get( 'wiki_url', 'https://wiki.galaxyproject.org/' )

lib/galaxy/webapps/tool_shed/config.py
@@ -110,7 +110,7 @@ class Configuration( object ):
         self.brand = kwargs.get( 'brand', None )
         self.pretty_datetime_format = expand_pretty_datetime_format( kwargs.get( 'pretty_datetime_format', '$locale (UTC)' ) )
         # Configuration for the message box directly below the masthead.
-        self.message_box_visible = kwargs.get( 'message_box_visible', False )
+        self.message_box_visible = string_as_bool( kwargs.get( 'message_box_visible', False ) )
         self.message_box_content = kwargs.get( 'message_box_content', None )
         self.message_box_class = kwargs.get( 'message_box_class', 'info' )
         self.support_url = kwargs.get( 'support_url', 'https://wiki.galaxyproject.org/Support' )

commit 7f630ea2ddb279ce963070d87573bac4c66d5027

Fix flake8 linting broken in 7bee2651aa8f08ea0495d167e33d317a22ae3ef9
Also fix import order and Python3 compatibility.

lib/galaxy/webapps/reports/config.py
@@ -1,8 +1,10 @@
 """Universe configuration builder."""
-import sys
-import os
 import logging
-import ConfigParser
+import os
+import sys
+
+from six.moves import configparser
+
 from galaxy.util import string_as_bool
 
 log = logging.getLogger( __name__ )
@@ -40,7 +42,7 @@ class Configuration( object ):
         self.log_actions = string_as_bool( kwargs.get( 'log_actions', 'False' ) )
         self.brand = kwargs.get( 'brand', None )
         # Configuration for the message box directly below the masthead.
-        self.message_box_visible =string_as_bool( kwargs.get( 'message_box_visible', False ) )
+        self.message_box_visible = string_as_bool( kwargs.get( 'message_box_visible', False ) )
         self.message_box_content = kwargs.get( 'message_box_content', None )
         self.message_box_class = kwargs.get( 'message_box_class', 'info' )
         self.wiki_url = kwargs.get( 'wiki_url', 'https://wiki.galaxyproject.org/' )
@@ -52,7 +54,7 @@ class Configuration( object ):
         self.sentry_dsn = kwargs.get( 'sentry_dsn', None )
         # Parse global_conf
         global_conf = kwargs.get( 'global_conf', None )
-        global_conf_parser = ConfigParser.ConfigParser()
+        global_conf_parser = configparser.ConfigParser()
         if global_conf and "__file__" in global_conf:
             global_conf_parser.read(global_conf['__file__'])
 
@@ -84,7 +86,7 @@ def get_database_engine_options( kwargs ):
     prefix = "database_engine_option_"
     prefix_len = len( prefix )
     rval = {}
-    for key, value in kwargs.iteritems():
+    for key, value in kwargs.items():
         if key.startswith( prefix ):
             key = key[prefix_len:]
             if key in conversions: