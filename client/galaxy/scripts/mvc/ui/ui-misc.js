eb45e1dedeb0d8fedb701093e59d2a7fb786e892
guerler <aysam.guerler@gmail.com>
2016-11-15 11:26:56 -0500

    Prepare password handler for merge with regular input field handler

--- client/galaxy/scripts/mvc/ui/ui-misc.js
+++ client/galaxy/scripts/mvc/ui/ui-misc.js
@@ -78,56 +78,6 @@ define(['utils/utils',
         }
     });
 
-
-    	var Password = Backbone.View.extend({
-		initialize: function(options){
-		this.model = options && options.model || new Backbone.Model({
-			type		: 'password',
-			placeholder	: '',
-			disabled	: false,
-			visible		: true,
-			cls		: '',
-			area		: false,
-			color		: null,
-			style		: null
-		}).set(options);
-		this.tagName='input';
-		this.setElement($( '<' + this.tagName + '/>' ));
-		this.listenTo(this.model, 'change', this.render, this);
-		this.render();
-	},
-	events: {
-		'input': '_onchange'
-	},
-	value: function(new_val) {
-		new_val !== undefined && this.model.set('value', typeof new_val === 'string' ? new_val : '');
-		
-		return this.model.get('value');
-	},
-	render: function() {
-		this.$el.removeClass()
-			.addClass('ui-' + this.tagName )
-			.addClass(this.model.get('cls'))
-			.addClass(this.model.get('style'))
-			.attr('id',this.model.id)
-			.attr('type',this.model.get('type'))
-			.attr('placeholder',this.model.get('placeholder'))
-			.css('color', this.model.get('color') || '' )
-			.css('border-color',this.model.get('color') || '');
-		if (this.model.get('value') !== this.$el.val() ) {
-			this.$el.val(this.model.get('value'));
-		}
-		this.model.get('disabled') ? this.$el.attr('disabled',true) : this.$el.removeAttr( 'disabled' );
-		this.$el[this.model.get('visible') ? 'show' : 'hide' ] ();
-		return this;
-	},
-	_onchange: function(){
-		this.value( this.$el.val());
-		this.model.get('onchange') && this.model.get('onchange')(this.model.get('value'));
-	}
-	});
-
-
     /** Renders an input element used e.g. in the tool form */
     var Input = Backbone.View.extend({
         initialize: function( options ) {
@@ -203,8 +153,7 @@ define(['utils/utils',
         ButtonCheck : Buttons.ButtonCheck,
         ButtonMenu  : Buttons.ButtonMenu,
         ButtonLink  : Buttons.ButtonLink,
-        Password    : Password,
-	Input       : Input,
+        Input       : Input,
         Label       : Label,
         Message     : Message,
         Modal       : Modal,
