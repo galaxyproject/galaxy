dd55776a423cc4d8b0222e873c96e7fa41e7c5f2
Thomas Wollmann <thomas.s.wollmann@gmail.com>
2016-11-15 12:39:37 +0100

    cleanup of dependencies

--- lib/galaxy/datatypes/constructive_solid_geometry.py
+++ lib/galaxy/datatypes/constructive_solid_geometry.py
@@ -8,9 +8,6 @@ from galaxy.datatypes.data import nice_size
 from galaxy.datatypes.metadata import MetadataElement
 from galaxy import util
 
-from stl import mesh
-import numpy as np
-
 MAX_HEADER_LINES = 500
 MAX_LINE_LEN = 2000
 COLOR_OPTS = ['COLOR_SCALARS', 'red', 'green', 'blue']
