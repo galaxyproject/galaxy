efde00d80a916afc20869608a7a1922249581a62
mvdbeek <m.vandenbeek@gmail.com>
2016-11-16 21:10:17 +0100

    Handle samtools 0.x

--- lib/galaxy/datatypes/converters/sam_to_bam.py
+++ lib/galaxy/datatypes/converters/sam_to_bam.py
@@ -21,15 +21,38 @@ def cleanup_before_exit( tmp_dir ):
         shutil.rmtree( tmp_dir )
 
 
-def getSamtoolsVersion():
-    cmd = 'samtools --version'
-    proc = subprocess.Popen( args=cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True )
-    stdout, stderr = proc.communicate()
-    if proc.returncode:
-        sys.stderr.write(stderr)
-        return None
-    else:
-        return stdout.split("\n")[0].split()[-1]
+def cmd_exists(cmd):
+    # http://stackoverflow.com/questions/5226958/which-equivalent-function-in-python
+    for path in os.environ["PATH"].split(":"):
+        if os.path.exists(os.path.join(path, cmd)):
+            return True
+    return False
+
+
+def _get_samtools_version():
+    version = '0.0.0'
+    if not cmd_exists('samtools'):
+        raise Exception('This tool needs samtools, but it is not on PATH.')
+    # Get the version of samtools via --version-only, if available
+    p = subprocess.Popen( ['samtools', '--version-only'],
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE)
+    output, error = p.communicate()
+
+    # --version-only is available
+    # Format is <version x.y.z>+htslib-<a.b.c>
+    if p.returncode == 0:
+        version = output.split('+')[0]
+        return version
+
+    output = subprocess.Popen( [ 'samtools' ], stderr=subprocess.PIPE, stdout=subprocess.PIPE ).communicate()[1]
+    lines = output.split( '\n' )
+    for line in lines:
+        if line.lower().startswith( 'version' ):
+            # Assuming line looks something like: version: 0.1.12a (r862)
+            version = line.split()[1]
+            break
+    return version
 
 
 def __main__():
@@ -65,8 +88,8 @@ def __main__():
     sorted_stderr_filename = os.path.join( tmp_dir, 'sorted.stderr' )
     sorting_prefix = os.path.join( tmp_dir, 'sorted_bam' )
     # samtools changed sort command arguments (starting from version 1.3)
-    samtools_version = LooseVersion(getSamtoolsVersion())
-    if samtools_version < LooseVersion('1.3'):
+    samtools_version = LooseVersion(_get_samtools_version())
+    if samtools_version < LooseVersion('1.0'):
         cmd = 'samtools sort -o "%s" "%s" > "%s"' % ( unsorted_bam_filename, sorting_prefix, output_filename )
     else:
         cmd = 'samtools sort -T "%s" "%s" > "%s"' % ( sorting_prefix, unsorted_bam_filename, output_filename )
