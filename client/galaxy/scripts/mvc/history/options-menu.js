galaxyproject/galaxy

commit 1cc57b1eb2c295dbd68837c864740b6ff079b83d

Add icons and reorganise history menu

client/galaxy/scripts/mvc/history/options-menu.js
@@ -11,7 +11,7 @@ var menu = [
     },
     {
         html    : _l( 'Saved Histories' ),
-        href    : 'history/list'
+        href    : 'history/list',
     },
     {
         html    : _l( 'Histories Shared with Me' ),
@@ -19,7 +19,7 @@ var menu = [
     },
 
     {
-        html    : _l( 'Current History' ),
+        html    : _l( 'History Actions' ),
         header  : true,
         anon    : true
     },
@@ -29,32 +29,69 @@ var menu = [
             if( Galaxy && Galaxy.currHistoryPanel ){
                 Galaxy.currHistoryPanel.createNewHistory();
             }
-        }
+        },
+        icon : 'plus'
     },
     {
         html    : _l( 'Copy History' ),
-        href    : 'history/copy'
-    },
-    {
-        html    : _l( 'Copy Datasets' ),
-        href    : 'dataset/copy_datasets'
+        href    : 'history/copy',
+        // TODO: I really want the clone icon here.
+        // http://fortawesome.github.io/Font-Awesome/icon/clone/
+        // missing??
+        icon    : 'files-o'
     },
     {
         html    : _l( 'Share or Publish' ),
-        href    : 'history/sharing'
+        href    : 'history/sharing',
+        icon    : 'share'
+    },
+    {
+        html    : _l( 'Show Structure' ),
+        href    : 'history/display_structured',
+        anon    : true,
+        icon    : 'binoculars'
+    },
+    {
+        html    : _l( 'Delete' ),
+        confirm : _l( 'Really delete the current history?' ),
+        href    : 'history/delete_current',
+        icon    : 'trash-o'
+    },
+    {
+        html    : _l( 'Delete Permanently' ),
+        confirm : _l( 'Really delete the current history permanently? This cannot be undone.' ),
+        href    : 'history/delete_current?purge=True',
+        purge   : true,
+        anon    : true,
+        icon    : 'trash'
+    },
+
+
+    {
+        html    : _l( 'Dataset Actions' ),
+        header  : true,
+        anon    : true
+    },
+    {
+        html    : _l( 'Copy Datasets' ),
+        href    : 'dataset/copy_datasets',
+        icon    : 'files-o'
     },
     {
         html    : _l( 'Extract Workflow' ),
-        href    : 'workflow/build_from_current_history'
+        href    : 'workflow/build_from_current_history',
+        icon    : 'puzzle-piece'  // or cubes, object-group, binoculars
     },
     {
         html    : _l( 'Dataset Security' ),
-        href    : 'root/history_set_default_permissions'
+        href    : 'root/history_set_default_permissions',
+        icon    : 'cog'
     },
     {
         html    : _l( 'Resume Paused Jobs' ),
         href    : 'history/resume_paused_jobs?current=True',
-        anon    : true
+        anon    : true,
+        icon    : 'play'
     },
     {
         html    : _l( 'Collapse Expanded Datasets' ),
@@ -62,7 +99,8 @@ var menu = [
             if( Galaxy && Galaxy.currHistoryPanel ){
                 Galaxy.currHistoryPanel.collapseAll();
             }
-        }
+        },
+        icon    : 'compress'
     },
     {
         html    : _l( 'Unhide Hidden Datasets' ),
@@ -80,7 +118,8 @@ var menu = [
                         console.error( arguments );
                     });
             }
-        }
+        },
+        icon    : 'eye'
     },
     {
         html    : _l( 'Delete Hidden Datasets' ),
@@ -99,41 +138,33 @@ var menu = [
                         console.error( arguments );
                     });
             }
-        }
+        },
+        icon    : 'trash',
     },
     {
         html    : _l( 'Purge Deleted Datasets' ),
         confirm : _l( 'Really delete all deleted datasets permanently? This cannot be undone.' ),
         href    : 'history/purge_deleted_datasets',
         purge   : true,
-        anon    : true
+        anon    : true,
     },
+
+
     {
-        html    : _l( 'Show Structure' ),
-        href    : 'history/display_structured',
-        anon    : true
+        html    : _l( 'Downloads' ),
+        header  : true
     },
     {
         html    : _l( 'Export Citations' ),
         href    : 'history/citations',
-        anon    : true
+        anon    : true,
+        icon    : 'download'
     },
     {
         html    : _l( 'Export to File' ),
         href    : 'history/export_archive?preview=True',
-        anon    : true
-    },
-    {
-        html    : _l( 'Delete' ),
-        confirm : _l( 'Really delete the current history?' ),
-        href    : 'history/delete_current'
-    },
-    {
-        html    : _l( 'Delete Permanently' ),
-        confirm : _l( 'Really delete the current history permanently? This cannot be undone.' ),
-        href    : 'history/delete_current?purge=True',
-        purge   : true,
-        anon    : true
+        anon    : true,
+        icon    : 'download'
     },
 
     {
@@ -142,7 +173,8 @@ var menu = [
     },
     {
         html    : _l( 'Import from File' ),
-        href    : 'history/import_archive'
+        href    : 'history/import_archive',
+        icon    : 'upload'
     }
 ];
 
@@ -160,6 +192,13 @@ function buildMenu( isAnon, purgeAllowed, urlRoot ){
             menuOption.href = urlRoot + menuOption.href;
             menuOption.target = 'galaxy_main';
         }
+
+        if( menuOption.icon ){
+            menuOption.html = '<span class="fa fa-' + menuOption.icon + '"></span> ' + menuOption.html
+        } else {
+            menuOption.html = '<span class="fa"></span> ' + menuOption.html
+        }
+
         if( menuOption.confirm ){
             menuOption.func = function(){
                 if( confirm( menuOption.confirm ) ){
@@ -177,7 +216,7 @@ var create = function( $button, options ){
         purgeAllowed = options.purgeAllowed || false,
         root = options.root || ( ( Galaxy && Galaxy.options )? Galaxy.options.root: '/' ),
         menu = buildMenu( isAnon, purgeAllowed, root );
-    //console.debug( 'menu:', menu );
+    console.debug( 'menu:', menu );
     return new PopupMenu( $button, menu );
 };
 

static/scripts/mvc/history/options-menu.js
@@ -1,2 +1,2 @@
-define(["mvc/ui/popup-menu","mvc/base-mvc","utils/localization"],function(a,b,c){function d(a,b,c){return _.clone(e).filter(function(d){return a&&!d.anon?!1:!b&&d.purge?!1:(d.href&&(d.href=c+d.href,d.target="galaxy_main"),d.confirm&&(d.func=function(){confirm(d.confirm)&&(galaxy_main.location=d.href)}),!0)})}var e=[{html:c("History Lists"),header:!0},{html:c("Saved Histories"),href:"history/list"},{html:c("Histories Shared with Me"),href:"history/list_shared"},{html:c("Current History"),header:!0,anon:!0},{html:c("Create New"),func:function(){Galaxy&&Galaxy.currHistoryPanel&&Galaxy.currHistoryPanel.createNewHistory()}},{html:c("Copy History"),href:"history/copy"},{html:c("Copy Datasets"),href:"dataset/copy_datasets"},{html:c("Share or Publish"),href:"history/sharing"},{html:c("Extract Workflow"),href:"workflow/build_from_current_history"},{html:c("Dataset Security"),href:"root/history_set_default_permissions"},{html:c("Resume Paused Jobs"),href:"history/resume_paused_jobs?current=True",anon:!0},{html:c("Collapse Expanded Datasets"),func:function(){Galaxy&&Galaxy.currHistoryPanel&&Galaxy.currHistoryPanel.collapseAll()}},{html:c("Unhide Hidden Datasets"),anon:!0,func:function(){if(Galaxy&&Galaxy.currHistoryPanel&&confirm(c("Really unhide all hidden datasets?"))){var a=Galaxy.currHistoryPanel.model.contents.hidden();a.ajaxQueue(Backbone.Model.prototype.save,{visible:!0}).done(function(){Galaxy.currHistoryPanel.renderItems()}).fail(function(){alert("There was an error unhiding the datasets"),console.error(arguments)})}}},{html:c("Delete Hidden Datasets"),anon:!0,func:function(){if(Galaxy&&Galaxy.currHistoryPanel&&confirm(c("Really delete all hidden datasets?"))){var a=Galaxy.currHistoryPanel.model.contents.hidden();a.ajaxQueue(Backbone.Model.prototype.save,{deleted:!0,visible:!0}).done(function(){Galaxy.currHistoryPanel.renderItems()}).fail(function(){alert("There was an error deleting the datasets"),console.error(arguments)})}}},{html:c("Purge Deleted Datasets"),confirm:c("Really delete all deleted datasets permanently? This cannot be undone."),href:"history/purge_deleted_datasets",purge:!0,anon:!0},{html:c("Show Structure"),href:"history/display_structured",anon:!0},{html:c("Export Citations"),href:"history/citations",anon:!0},{html:c("Export to File"),href:"history/export_archive?preview=True",anon:!0},{html:c("Delete"),confirm:c("Really delete the current history?"),href:"history/delete_current"},{html:c("Delete Permanently"),confirm:c("Really delete the current history permanently? This cannot be undone."),href:"history/delete_current?purge=True",purge:!0,anon:!0},{html:c("Other Actions"),header:!0},{html:c("Import from File"),href:"history/import_archive"}],f=function(b,c){c=c||{};var e=void 0===c.anonymous?!0:c.anonymous,f=c.purgeAllowed||!1,g=c.root||(Galaxy&&Galaxy.options?Galaxy.options.root:"/"),h=d(e,f,g);return new a(b,h)};return f});
+define(["mvc/ui/popup-menu","mvc/base-mvc","utils/localization"],function(a,b,c){function d(a,b,c){return _.clone(e).filter(function(d){return a&&!d.anon?!1:!b&&d.purge?!1:(d.href&&(d.href=c+d.href,d.target="galaxy_main"),d.html=d.icon?'<span class="fa fa-'+d.icon+'"></span> '+d.html:'<span class="fa"></span> '+d.html,d.confirm&&(d.func=function(){confirm(d.confirm)&&(galaxy_main.location=d.href)}),!0)})}var e=[{html:c("History Lists"),header:!0},{html:c("Saved Histories"),href:"history/list"},{html:c("Histories Shared with Me"),href:"history/list_shared"},{html:c("History Actions"),header:!0,anon:!0},{html:c("Create New"),func:function(){Galaxy&&Galaxy.currHistoryPanel&&Galaxy.currHistoryPanel.createNewHistory()},icon:"plus"},{html:c("Copy History"),href:"history/copy",icon:"files-o"},{html:c("Share or Publish"),href:"history/sharing",icon:"share"},{html:c("Show Structure"),href:"history/display_structured",anon:!0,icon:"binoculars"},{html:c("Delete"),confirm:c("Really delete the current history?"),href:"history/delete_current",icon:"trash-o"},{html:c("Delete Permanently"),confirm:c("Really delete the current history permanently? This cannot be undone."),href:"history/delete_current?purge=True",purge:!0,anon:!0,icon:"trash"},{html:c("Dataset Actions"),header:!0,anon:!0},{html:c("Copy Datasets"),href:"dataset/copy_datasets",icon:"files-o"},{html:c("Extract Workflow"),href:"workflow/build_from_current_history",icon:"puzzle-piece"},{html:c("Dataset Security"),href:"root/history_set_default_permissions",icon:"cog"},{html:c("Resume Paused Jobs"),href:"history/resume_paused_jobs?current=True",anon:!0,icon:"play"},{html:c("Collapse Expanded Datasets"),func:function(){Galaxy&&Galaxy.currHistoryPanel&&Galaxy.currHistoryPanel.collapseAll()},icon:"compress"},{html:c("Unhide Hidden Datasets"),anon:!0,func:function(){if(Galaxy&&Galaxy.currHistoryPanel&&confirm(c("Really unhide all hidden datasets?"))){var a=Galaxy.currHistoryPanel.model.contents.hidden();a.ajaxQueue(Backbone.Model.prototype.save,{visible:!0}).done(function(){Galaxy.currHistoryPanel.renderItems()}).fail(function(){alert("There was an error unhiding the datasets"),console.error(arguments)})}},icon:"eye"},{html:c("Delete Hidden Datasets"),anon:!0,func:function(){if(Galaxy&&Galaxy.currHistoryPanel&&confirm(c("Really delete all hidden datasets?"))){var a=Galaxy.currHistoryPanel.model.contents.hidden();a.ajaxQueue(Backbone.Model.prototype.save,{deleted:!0,visible:!0}).done(function(){Galaxy.currHistoryPanel.renderItems()}).fail(function(){alert("There was an error deleting the datasets"),console.error(arguments)})}},icon:"trash"},{html:c("Purge Deleted Datasets"),confirm:c("Really delete all deleted datasets permanently? This cannot be undone."),href:"history/purge_deleted_datasets",purge:!0,anon:!0},{html:c("Downloads"),header:!0},{html:c("Export Citations"),href:"history/citations",anon:!0,icon:"download"},{html:c("Export to File"),href:"history/export_archive?preview=True",anon:!0,icon:"download"},{html:c("Other Actions"),header:!0},{html:c("Import from File"),href:"history/import_archive",icon:"upload"}],f=function(b,c){c=c||{};var e=void 0===c.anonymous?!0:c.anonymous,f=c.purgeAllowed||!1,g=c.root||(Galaxy&&Galaxy.options?Galaxy.options.root:"/"),h=d(e,f,g);return console.debug("menu:",h),new a(b,h)};return f});
 //# sourceMappingURL=../../../maps/mvc/history/options-menu.js.map

static/maps/mvc/history/options-menu.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"options-menu.js","sources":["../../../src/mvc/history/options-menu.js"],"names":["define","PopupMenu","BASE_MVC","_l","buildMenu","isAnon","purgeAllowed","urlRoot","_","clone","menu","filter","menuOption","anon","purge","href","target","confirm","func","galaxy_main","location","html","header","Galaxy","currHistoryPanel","createNewHistory","collapseAll","filtered","model","contents","hidden","ajaxQueue","Backbone","Model","prototype","save","visible","done","renderItems","fail","alert","console","error","arguments","deleted","create","$button","options","undefined","anonymous","root"],"mappings":"AAAAA,QACI,oBACA,eACA,sBACD,SAAUC,EAAWC,EAAUC,GAgJlC,QAASC,GAAWC,EAAQC,EAAcC,GACtC,MAAOC,GAAEC,MAAOC,GAAOC,OAAQ,SAAUC,GACrC,MAAIP,KAAWO,EAAWC,MACf,GAENP,GAAgBM,EAAWE,OACrB,GAIPF,EAAWG,OACXH,EAAWG,KAAOR,EAAUK,EAAWG,KACvCH,EAAWI,OAAS,eAEpBJ,EAAWK,UACXL,EAAWM,KAAO,WACVD,QAASL,EAAWK,WACpBE,YAAYC,SAAWR,EAAWG,SAIvC,KAnKf,GAAIL,KAEIW,KAAUlB,EAAI,iBACdmB,QAAU,IAGVD,KAAUlB,EAAI,mBACdY,KAAU,iBAGVM,KAAUlB,EAAI,4BACdY,KAAU,wBAIVM,KAAUlB,EAAI,mBACdmB,QAAU,EACVT,MAAU,IAGVQ,KAAUlB,EAAI,cACde,KAAU,WACFK,QAAUA,OAAOC,kBACjBD,OAAOC,iBAAiBC,sBAKhCJ,KAAUlB,EAAI,gBACdY,KAAU,iBAGVM,KAAUlB,EAAI,iBACdY,KAAU,0BAGVM,KAAUlB,EAAI,oBACdY,KAAU,oBAGVM,KAAUlB,EAAI,oBACdY,KAAU,wCAGVM,KAAUlB,EAAI,oBACdY,KAAU,yCAGVM,KAAUlB,EAAI,sBACdY,KAAU,0CACVF,MAAU,IAGVQ,KAAUlB,EAAI,8BACde,KAAU,WACFK,QAAUA,OAAOC,kBACjBD,OAAOC,iBAAiBE,iBAKhCL,KAAUlB,EAAI,0BACdU,MAAU,EACVK,KAAU,WACN,GAAIK,QAAUA,OAAOC,kBAAoBP,QAASd,EAAI,uCAA0C,CAC5F,GAAIwB,GAAWJ,OAAOC,iBAAiBI,MAAMC,SAASC,QAEtDH,GAASI,UAAWC,SAASC,MAAMC,UAAUC,MAAQC,SAAU,IAC1DC,KAAM,WACHd,OAAOC,iBAAiBc,gBAE3BC,KAAM,WACHC,MAAO,4CACPC,QAAQC,MAAOC,iBAM/BtB,KAAUlB,EAAI,0BACdU,MAAU,EACVK,KAAU,WACN,GAAIK,QAAUA,OAAOC,kBAAoBP,QAASd,EAAI,uCAA0C,CAC5F,GAAIwB,GAAWJ,OAAOC,iBAAiBI,MAAMC,SAASC,QAGtDH,GAASI,UAAWC,SAASC,MAAMC,UAAUC,MAAQS,SAAU,EAAMR,SAAS,IACzEC,KAAM,WACHd,OAAOC,iBAAiBc,gBAE3BC,KAAM,WACHC,MAAO,4CACPC,QAAQC,MAAOC,iBAM/BtB,KAAUlB,EAAI,0BACdc,QAAUd,EAAI,0EACdY,KAAU,iCACVD,OAAU,EACVD,MAAU,IAGVQ,KAAUlB,EAAI,kBACdY,KAAU,6BACVF,MAAU,IAGVQ,KAAUlB,EAAI,oBACdY,KAAU,oBACVF,MAAU,IAGVQ,KAAUlB,EAAI,kBACdY,KAAU,sCACVF,MAAU,IAGVQ,KAAUlB,EAAI,UACdc,QAAUd,EAAI,sCACdY,KAAU,2BAGVM,KAAUlB,EAAI,sBACdc,QAAUd,EAAI,yEACdY,KAAU,oCACVD,OAAU,EACVD,MAAU,IAIVQ,KAAUlB,EAAI,iBACdmB,QAAU,IAGVD,KAAUlB,EAAI,oBACdY,KAAU,2BA6Bd8B,EAAS,SAAUC,EAASC,GAC5BA,EAAUA,KACV,IAAI1C,GAA+B2C,SAAtBD,EAAQE,WAAyB,EAAOF,EAAQE,UACzD3C,EAAeyC,EAAQzC,eAAgB,EACvC4C,EAAOH,EAAQG,OAAY3B,QAAUA,OAAOwB,QAAWxB,OAAOwB,QAAQG,KAAM,KAC5ExC,EAAON,EAAWC,EAAQC,EAAc4C,EAE5C,OAAO,IAAIjD,GAAW6C,EAASpC,GAK/B,OAAOmC"}
+{"version":3,"file":"options-menu.js","sources":["../../../src/mvc/history/options-menu.js"],"names":["define","PopupMenu","BASE_MVC","_l","buildMenu","isAnon","purgeAllowed","urlRoot","_","clone","menu","filter","menuOption","anon","purge","href","target","html","icon","confirm","func","galaxy_main","location","header","Galaxy","currHistoryPanel","createNewHistory","collapseAll","filtered","model","contents","hidden","ajaxQueue","Backbone","Model","prototype","save","visible","done","renderItems","fail","alert","console","error","arguments","deleted","create","$button","options","undefined","anonymous","root","debug"],"mappings":"AAAAA,QACI,oBACA,eACA,sBACD,SAAUC,EAAWC,EAAUC,GAgLlC,QAASC,GAAWC,EAAQC,EAAcC,GACtC,MAAOC,GAAEC,MAAOC,GAAOC,OAAQ,SAAUC,GACrC,MAAIP,KAAWO,EAAWC,MACf,GAENP,GAAgBM,EAAWE,OACrB,GAIPF,EAAWG,OACXH,EAAWG,KAAOR,EAAUK,EAAWG,KACvCH,EAAWI,OAAS,eAIpBJ,EAAWK,KADXL,EAAWM,KACO,sBAAwBN,EAAWM,KAAO,aAAeN,EAAWK,KAEpE,4BAA8BL,EAAWK,KAG3DL,EAAWO,UACXP,EAAWQ,KAAO,WACVD,QAASP,EAAWO,WACpBE,YAAYC,SAAWV,EAAWG,SAIvC,KA1Mf,GAAIL,KAEIO,KAAUd,EAAI,iBACdoB,QAAU,IAGVN,KAAUd,EAAI,mBACdY,KAAU,iBAGVE,KAAUd,EAAI,4BACdY,KAAU,wBAIVE,KAAUd,EAAI,mBACdoB,QAAU,EACVV,MAAU,IAGVI,KAAUd,EAAI,cACdiB,KAAU,WACFI,QAAUA,OAAOC,kBACjBD,OAAOC,iBAAiBC,oBAGhCR,KAAO,SAGPD,KAAUd,EAAI,gBACdY,KAAU,eAIVG,KAAU,YAGVD,KAAUd,EAAI,oBACdY,KAAU,kBACVG,KAAU,UAGVD,KAAUd,EAAI,kBACdY,KAAU,6BACVF,MAAU,EACVK,KAAU,eAGVD,KAAUd,EAAI,UACdgB,QAAUhB,EAAI,sCACdY,KAAU,yBACVG,KAAU,YAGVD,KAAUd,EAAI,sBACdgB,QAAUhB,EAAI,yEACdY,KAAU,oCACVD,OAAU,EACVD,MAAU,EACVK,KAAU,UAKVD,KAAUd,EAAI,mBACdoB,QAAU,EACVV,MAAU,IAGVI,KAAUd,EAAI,iBACdY,KAAU,wBACVG,KAAU,YAGVD,KAAUd,EAAI,oBACdY,KAAU,sCACVG,KAAU,iBAGVD,KAAUd,EAAI,oBACdY,KAAU,uCACVG,KAAU,QAGVD,KAAUd,EAAI,sBACdY,KAAU,0CACVF,MAAU,EACVK,KAAU,SAGVD,KAAUd,EAAI,8BACdiB,KAAU,WACFI,QAAUA,OAAOC,kBACjBD,OAAOC,iBAAiBE,eAGhCT,KAAU,aAGVD,KAAUd,EAAI,0BACdU,MAAU,EACVO,KAAU,WACN,GAAII,QAAUA,OAAOC,kBAAoBN,QAAShB,EAAI,uCAA0C,CAC5F,GAAIyB,GAAWJ,OAAOC,iBAAiBI,MAAMC,SAASC,QAEtDH,GAASI,UAAWC,SAASC,MAAMC,UAAUC,MAAQC,SAAU,IAC1DC,KAAM,WACHd,OAAOC,iBAAiBc,gBAE3BC,KAAM,WACHC,MAAO,4CACPC,QAAQC,MAAOC,eAI/B1B,KAAU,QAGVD,KAAUd,EAAI,0BACdU,MAAU,EACVO,KAAU,WACN,GAAII,QAAUA,OAAOC,kBAAoBN,QAAShB,EAAI,uCAA0C,CAC5F,GAAIyB,GAAWJ,OAAOC,iBAAiBI,MAAMC,SAASC,QAGtDH,GAASI,UAAWC,SAASC,MAAMC,UAAUC,MAAQS,SAAU,EAAMR,SAAS,IACzEC,KAAM,WACHd,OAAOC,iBAAiBc,gBAE3BC,KAAM,WACHC,MAAO,4CACPC,QAAQC,MAAOC,eAI/B1B,KAAU,UAGVD,KAAUd,EAAI,0BACdgB,QAAUhB,EAAI,0EACdY,KAAU,iCACVD,OAAU,EACVD,MAAU,IAKVI,KAAUd,EAAI,aACdoB,QAAU,IAGVN,KAAUd,EAAI,oBACdY,KAAU,oBACVF,MAAU,EACVK,KAAU,aAGVD,KAAUd,EAAI,kBACdY,KAAU,sCACVF,MAAU,EACVK,KAAU,aAIVD,KAAUd,EAAI,iBACdoB,QAAU,IAGVN,KAAUd,EAAI,oBACdY,KAAU,yBACVG,KAAU,WAoCd4B,EAAS,SAAUC,EAASC,GAC5BA,EAAUA,KACV,IAAI3C,GAA+B4C,SAAtBD,EAAQE,WAAyB,EAAOF,EAAQE,UACzD5C,EAAe0C,EAAQ1C,eAAgB,EACvC6C,EAAOH,EAAQG,OAAY3B,QAAUA,OAAOwB,QAAWxB,OAAOwB,QAAQG,KAAM,KAC5EzC,EAAON,EAAWC,EAAQC,EAAc6C,EAE5C,OADAT,SAAQU,MAAO,QAAS1C,GACjB,GAAIT,GAAW8C,EAASrC,GAK/B,OAAOoC"}

commit 548d3e6f05b722301deefada71d5dafda81a1881

Fixes for deleting histories broken in 1cc57b1eb2c295dbd68837c864740b6ff079b83d

client/galaxy/scripts/mvc/history/options-menu.js
@@ -60,15 +60,23 @@ var menu = [
     },
     {
         html    : _l( 'Delete' ),
-        confirm : _l( 'Really delete the current history?' ),
-        href    : 'history/delete_current',
+        anon    : true,
+        func    : function() {
+            if( Galaxy && Galaxy.currHistoryPanel && confirm( _l( 'Really delete the current history?' ) ) ){
+                galaxy_main.window.location.href = 'history/delete?id=' + Galaxy.currHistoryPanel.model.id;
+            }
+        },
     },
     {
         html    : _l( 'Delete Permanently' ),
-        confirm : _l( 'Really delete the current history permanently? This cannot be undone.' ),
-        href    : 'history/delete_current?purge=True',
         purge   : true,
         anon    : true,
+        func    : function() {
+            if( Galaxy && Galaxy.currHistoryPanel
+            &&  confirm( _l( 'Really delete the current history permanently? This cannot be undone.' ) ) ){
+                galaxy_main.window.location.href = 'history/delete?purge=True&id=' + Galaxy.currHistoryPanel.model.id;
+            }
+        },
     },
 
 