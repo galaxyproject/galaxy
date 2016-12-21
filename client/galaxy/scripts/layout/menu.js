02f3a2737dd1c48574df9bf8e51ce43e6789bf1e
anuprulez <anup.rulez@gmail.com>
2016-12-21 12:46:14 +0100

    Bug fix for masthead webhook if Galaxy.page is undefined for workflow, data libraries etc pages

--- client/galaxy/scripts/layout/menu.js
+++ client/galaxy/scripts/layout/menu.js
@@ -117,13 +117,25 @@ var Collection = Backbone.Collection.extend({
                     $.each(webhooks.models, function(index, model) {
                         var webhook = model.toJSON();
                         if (webhook.activate) {
-                            Galaxy.page.masthead.collection.add({
-                                id      : webhook.name,
-                                icon    : webhook.config.icon,
-                                url     : webhook.config.url,
-                                tooltip : webhook.config.tooltip,
-                                onclick : webhook.config.function && new Function(webhook.config.function),
-                            });
+                            // Galaxy.page is undefined for data libraries, workflows pages
+                            if( Galaxy.page ) {
+                                Galaxy.page.masthead.collection.add({
+                                    id      : webhook.name,
+                                    icon    : webhook.config.icon,
+                                    url     : webhook.config.url,
+                                    tooltip : webhook.config.tooltip,
+                                    onclick : webhook.config.function && new Function(webhook.config.function),
+                                });
+                            }
+                            else if( Galaxy.masthead ) {
+                                Galaxy.masthead.collection.add({
+                                    id      : webhook.name,
+                                    icon    : webhook.config.icon,
+                                    url     : webhook.config.url,
+                                    tooltip : webhook.config.tooltip,
+                                    onclick : webhook.config.function && new Function(webhook.config.function),
+                                });
+                            }
                         }
                     });
                 });
