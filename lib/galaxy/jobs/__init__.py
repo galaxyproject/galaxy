galaxyproject/galaxy

commit 34f0b0fe8911c01562c7dfc0e83e0d56df50f66c

Implement a Galaxy config option for default job re-submission conditions.

lib/galaxy/jobs/__init__.py
@@ -138,6 +138,17 @@ class JobConfiguration( object ):
         self.resource_parameters = {}
         self.limits = Bunch()
 
+        default_resubmits = []
+        default_resubmit_condition = self.app.config.default_job_resubmission_condition
+        if default_resubmit_condition:
+            default_resubmits.append(dict(
+                destination=None,
+                condition=default_resubmit_condition,
+                handler=None,
+                delay=None,
+            ))
+        self.default_resubmits = default_resubmits
+
         self.__parse_resource_parameters()
         # Initialize the config
         job_config_file = self.app.config.job_config_file
@@ -230,7 +241,13 @@ class JobConfiguration( object ):
             job_destination = JobDestination(**dict(destination.items()))
             job_destination['params'] = self.__get_params(destination)
             job_destination['env'] = self.__get_envs(destination)
-            job_destination['resubmit'] = self.__get_resubmits(destination)
+            destination_resubmits = self.__get_resubmits(destination)
+            if destination_resubmits:
+                resubmits = self.default_resubmits
+            else:
+                resubmits = self.default_resubmits
+            job_destination["resubmit"] = resubmits
+
             self.destinations[id] = (job_destination,)
             if job_destination.tags is not None:
                 for tag in job_destination.tags:

config/galaxy.ini.sample
@@ -1148,6 +1148,20 @@ use_interactive = True
 # be configured through the job configuration file.
 #job_config_file = config/job_conf.xml
 
+# When jobs fail due to job runner problems, Galaxy can be configured to retry
+# these or reroute the jobs to new destinations. Very fine control of this is
+# available with resubmit declarations in job_conf.xml. For simple deployments
+# of Galaxy though, the following attribute can define resubmission conditions
+# for all job destinations. If any job destination defines even one
+# resubmission condition explicitly in job_conf.xml - the condition described
+# by this option will not apply to that destination. For instance, the condition:
+# 'attempt < 3 and unknown_error and (time_running < 300 or time_since_queued < 300)'
+# would retry up to two times jobs that didn't fail due to detected memory or
+# walltime limits but did fail quickly (either while queueing or running). The
+# commented out default below results in no default job resubmission condition,
+# failing jobs are just failed outright.
+#default_job_resubmission_condition = 
+
 # In multiprocess configurations, notification between processes about new jobs
 # must be done via the database.  In single process configurations, this can be
 # done in memory, which is a bit quicker.

lib/galaxy/config.py
@@ -356,6 +356,11 @@ class Configuration( object ):
         self.involucro_path = resolve_path(involucro_path, self.root)
         self.involucro_auto_init = string_as_bool(kwargs.get( 'involucro_auto_init', True))
 
+        default_job_resubmission_condition = kwargs.get( 'default_job_resubmission_condition', '')
+        if not default_job_resubmission_condition.strip():
+            default_job_resubmission_condition = None
+        self.default_job_resubmission_condition = default_job_resubmission_condition
+
         # Configuration options for taking advantage of nginx features
         self.upstream_gzip = string_as_bool( kwargs.get( 'upstream_gzip', False ) )
         self.apache_xsendfile = string_as_bool( kwargs.get( 'apache_xsendfile', False ) )

test/integration/resubmission_default_job_conf.xml
@@ -1,0 +1,43 @@
+<?xml version="1.0"?>
+<!-- 
+    Slimmed down resubmission_job_conf.xml for testing default resubmission rules.
+-->
+<job_conf>
+    <plugins>
+        <plugin id="local" type="runner" load="galaxy.jobs.runners.local:LocalJobRunner" workers="2"/>
+        <plugin id="first_failure_runner" type="runner" load="integration.resubmission_runners:FailOnlyFirstJobRunner" workers="2">
+        </plugin>
+        <plugin id="dynamic" type="runner">
+            <param id="rules_module">integration.resubmission_rules</param>
+        </plugin>
+    </plugins>
+
+    <handlers>
+        <handler id="main"/>
+    </handlers>
+
+    <destinations default="initial_destination">
+        <destination id="initial_destination" runner="dynamic">
+            <param id="type">python</param>
+            <param id="function">initial_destination</param>
+        </destination>
+
+        <destination id="fail_first_try" runner="first_failure_runner">
+        </destination>
+
+        <!-- Upload destination. -->
+        <destination id="local" runner="local">
+        </destination>
+
+    </destinations>
+
+    <resources default="test">
+      <group id="upload"></group>
+      <group id="test">test_name,failure_state,initial_destination,run_for</group>
+    </resources>
+
+    <tools>
+        <tool id="upload1" destination="local" resources="upload" />
+    </tools>
+
+</job_conf>

test/integration/resubmission_runners.py
@@ -63,4 +63,24 @@ class AssertionJobRunner(LocalJobRunner):
         super(AssertionJobRunner, self).queue_job(job_wrapper)
 
 
+class FailOnlyFirstJobRunner(LocalJobRunner):
+    """Job runner that knows about test cases and checks final state assumptions."""
+
+    tests_seen = []
+
+    def queue_job(self, job_wrapper):
+        resource_parameters = job_wrapper.get_resource_parameters()
+        try:
+            test_name = resource_parameters["test_name"]
+        except KeyError:
+            job_wrapper.fail("Job resource parameter test_name not set as required for this job runner.")
+            return
+
+        if test_name in self.tests_seen:
+            super(FailOnlyFirstJobRunner, self).queue_job(job_wrapper)
+        else:
+            self.tests_seen.append(test_name)
+            self._fail_job_local(job_wrapper, "Failing first attempt")
+
+
 __all__ = ('FailsJobRunner', 'AssertionJobRunner')

test/integration/test_job_resubmission.py
@@ -6,10 +6,27 @@ from base import integration_util
 
 SCRIPT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 JOB_RESUBMISSION_JOB_CONFIG_FILE = os.path.join(SCRIPT_DIRECTORY, "resubmission_job_conf.xml")
+JOB_RESUBMISSION_DEFAULT_JOB_CONFIG_FILE = os.path.join(SCRIPT_DIRECTORY, "resubmission_default_job_conf.xml")
 JOB_RESUBMISSION_JOB_RESOURCES_CONFIG_FILE = os.path.join(SCRIPT_DIRECTORY, "resubmission_job_resource_parameters_conf.xml")
 
 
-class JobResubmissionIntegrationTestCase(integration_util.IntegrationTestCase):
+class _BaseResubmissionIntegerationTestCase(integration_util.IntegrationTestCase):
+    framework_tool_and_types = True
+
+    def _assert_job_passes(self, resource_parameters):
+        self._run_tool_test("simple_constructs", resource_parameters=resource_parameters)
+
+    def _assert_job_fails(self, resource_parameters):
+        exception_thrown = False
+        try:
+            self._run_tool_test("simple_constructs", resource_parameters=resource_parameters)
+        except Exception:
+            exception_thrown = True
+
+        assert exception_thrown
+
+
+class JobResubmissionIntegrationTestCase(_BaseResubmissionIntegerationTestCase):
 
     framework_tool_and_types = True
 
@@ -85,14 +102,16 @@ class JobResubmissionIntegrationTestCase(integration_util.IntegrationTestCase):
                                                      "initial_destination": "resubmit_after_two_delays",
                                                      "failure_state": "unknown_error"})
 
-    def _assert_job_passes(self, resource_parameters):
-        self._run_tool_test("simple_constructs", resource_parameters=resource_parameters)
 
-    def _assert_job_fails(self, resource_parameters):
-        exception_thrown = False
-        try:
-            self._run_tool_test("simple_constructs", resource_parameters=resource_parameters)
-        except Exception:
-            exception_thrown = True
+class JobResubmissionDefaultIntegrationTestCase(_BaseResubmissionIntegerationTestCase):
 
-        assert exception_thrown
+    framework_tool_and_types = True
+
+    @classmethod
+    def handle_galaxy_config_kwds(cls, config):
+        config["default_job_resubmission_condition"] = "attempt < 2"
+        config["job_config_file"] = JOB_RESUBMISSION_DEFAULT_JOB_CONFIG_FILE
+        config["job_resource_params_file"] = JOB_RESUBMISSION_JOB_RESOURCES_CONFIG_FILE
+
+    def test_default_resubmission(self):
+        self._assert_job_passes(resource_parameters={"test_name": "test_default_resubmission"})

test/unit/jobs/test_job_configuration.py
@@ -22,6 +22,7 @@ class JobConfXmlParserTestCase( unittest.TestCase ):
             use_tasked_jobs=False,
             job_resource_params_file="/tmp/fake_absent_path",
             config_dict={},
+            default_job_resubmission_condition="",
         )
         self.__write_config_from( SIMPLE_JOB_CONF )
         self.app = bunch.Bunch( config=self.config, job_metrics=MockJobMetrics() )

commit 9373b69f8b6dd93f747d5b12bea7e4f75aef3add

Fix bug with job re-submission introduced in 34f0b0fe8911c01562c7dfc0e83e0d56df50f66c.

lib/galaxy/jobs/__init__.py
@@ -243,7 +243,7 @@ class JobConfiguration( object ):
             job_destination['env'] = self.__get_envs(destination)
             destination_resubmits = self.__get_resubmits(destination)
             if destination_resubmits:
-                resubmits = self.default_resubmits
+                resubmits = destination_resubmits
             else:
                 resubmits = self.default_resubmits
             job_destination["resubmit"] = resubmits