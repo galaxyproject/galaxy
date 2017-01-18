81d71d2e740ee07754785306e4448f8425f890bc
John Chilton <jmchilton@gmail.com>
2017-01-17 20:01:37 -0500

    Allow finer grain specification of requirements.
    This adapts CWL-style ``SoftwareRequirement`` ``specs`` to solve #1927. Here I'm trying to implement the CWL specification in a way that helps enable the feasibility of Conda packaging in Galaxy. It is a delicate balancing act aimed to upset as many interested parties as I can.
    
    To understand the problem - consider the ``blast+`` requirement found in the Galaxy wrappers. It looks something like this:
    
    ```
    <requirement type="package" version="2.2.31" name="blast+">
    ```
    
    Great, that works for Galaxy and Tool Shed packages. It doesn't work for bioconda at all. I think this problem is fairly uncommon - most packages have simple names shared across Debian, Brew, Conda, etc... - but it does happen in some cases that there are inconsistencies. Some people have taken to duplicating the requirement - this is bad and should not be done since they are mutually exclusive and Galaxy will attempt to resolve both.
    
    This introduces the following syntax for tools with profile >= 16.10:
    
    ```
    <requirement type="package" version="2.2.31" name="blast+">
        <specification uri="https://anaconda.org/bioconda/blast" />
        <specification uri="https://packages.debian.org/sid/ncbi-blast+" version="2.2.31-3" />
    </requirement>
    ```
    
    This allows finer grain resolution of the requirement without sacrificing the abstract name at the top. It allows the name and the version to be adapted by resolvers as needed (hopefully rarely so).
    
    This syntax is the future facing one, but obviously this tool would not work on older Galaxy versions. To remedy this - an alternative syntax can be used for tools targetting Galaxy verions pre-16.10:
    
    ```
    <requirement type="package" version="2.2" specification_uris="https://anaconda.org/bioconda/blast@2.2.31,https://packages.debian.org/jessie/ncbi-blast+@2.2.29-3">blast+</requirement>
    ```
    
    This syntax sucks - but it does give newer Galaxies the ability to resolve the specifications without breaking the more simple functionality for older Galaxies.
    
    For more information on the CWL side of this - checkout the discussion on https://github.com/common-workflow-language/cwltool/pull/214. The CWL specification information is defined at http://www.commonwl.org/v1.0/CommandLineTool.html#SoftwarePackage.
    
    Conflicts:
    	lib/galaxy/tools/deps/__init__.py
    	test/functional/tools/samples_tool_conf.xml

--- lib/galaxy/tools/deps/__init__.py
+++ lib/galaxy/tools/deps/__init__.py
@@ -143,6 +143,7 @@ class DependencyManager( object ):
             # Check requirements all at once
             all_unmet = len(requirement_to_dependency) == 0
             if all_unmet and hasattr(resolver, "resolve_all"):
+                # TODO: Handle specs.
                 dependencies = resolver.resolve_all(resolvable_requirements, **kwds)
                 if dependencies:
                     assert len(dependencies) == len(resolvable_requirements)
@@ -158,7 +159,17 @@ class DependencyManager( object ):
                     continue
 
                 if requirement.type in ['package', 'set_environment']:
-                    dependency = resolver.resolve( requirement.name, requirement.version, requirement.type, **kwds )
+                    name = requirement.name
+                    version = requirement.version
+                    specs = requirement.specs
+
+                    if hasattr(resolver, "find_specification"):
+                        spec = resolver.find_specification(specs)
+                        if spec is not None:
+                            name = spec.short_name
+                            version = spec.version or version
+
+                    dependency = resolver.resolve( name, version, requirement.type, **kwds )
                     if require_exact and not dependency.exact:
                         continue
 
