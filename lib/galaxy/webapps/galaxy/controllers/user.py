4689cf16bf77b0e7b0d22d193b695d7d5df24d6c
Dannon Baker <dannon.baker@gmail.com>
2017-02-12 13:59:59 -0500

    Recalculate user disk usage on logout.  Not as ideal as just having it be accurate all the time, but it 1) avoids adding yet another control people need to dig for when they need to recalculate, and 2) makes things right when following 'turn it off and on again'

--- lib/galaxy/webapps/galaxy/controllers/user.py
+++ lib/galaxy/webapps/galaxy/controllers/user.py
@@ -624,6 +624,8 @@ class User( BaseUIController, UsesFormDefinitionsMixin, CreatesUsersMixin, Creat
                 refresh_frames = [ 'masthead', 'history', 'tools' ]
             else:
                 refresh_frames = [ 'masthead', 'history' ]
+            # Recalculate user disk usage.
+            trans.user.calculate_disk_usage()
             # Since logging an event requires a session, we'll log prior to ending the session
             trans.log_event( "User logged out" )
         else:
