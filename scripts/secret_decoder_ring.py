fb2450ff33ebdbdeb3f1205bdc58c38519921e7a
Eric Rasche <rasche.eric@gmail.com>
2017-02-15 21:31:22 +0000

    document scriptg

--- scripts/secret_decoder_ring.py
+++ scripts/secret_decoder_ring.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+"""
+Script to encode/decode the IDs that galaxy exposes to users and admins.
+"""
 import os
 import sys
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'lib')))
