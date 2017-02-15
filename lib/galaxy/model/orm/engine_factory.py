28fe37c977b18d83d91e7a1d604112f62857073f
Dannon Baker <dannon.baker@gmail.com>
2017-02-15 11:47:17 -0500

    Style tweaks.  My flake8-in-editor had silently died and was not warning me anymore, super.

--- lib/galaxy/model/orm/engine_factory.py
+++ lib/galaxy/model/orm/engine_factory.py
@@ -5,6 +5,7 @@ from sqlalchemy.engine import Engine
 
 log = logging.getLogger( __name__ )
 
+
 def build_engine(url, engine_options, database_query_profiling_proxy=False, trace_logger=None, slow_query_log_threshold=0):
     # Should we use the logging proxy?
     if database_query_profiling_proxy:
@@ -19,12 +20,12 @@ def build_engine(url, engine_options, database_query_profiling_proxy=False, trac
     if slow_query_log_threshold:
         @event.listens_for(Engine, "before_cursor_execute")
         def before_cursor_execute(conn, cursor, statement,
-                                parameters, context, executemany):
+                                  parameters, context, executemany):
             conn.info.setdefault('query_start_time', []).append(time.time())
 
         @event.listens_for(Engine, "after_cursor_execute")
         def after_cursor_execute(conn, cursor, statement,
-                                parameters, context, executemany):
+                                 parameters, context, executemany):
             total = time.time() - conn.info['query_start_time'].pop(-1)
             if total > slow_query_log_threshold:
                 log.debug("Slow query: %f(s) for %s" % (total, statement))
