48639028df5b59e7547a2e716b9c7e34e9743a41
anuprulez <anup.rulez@gmail.com>
2017-02-15 14:11:55 +0100

    Removing utils.get to fetch pdb files

--- config/plugins/visualizations/charts/static/repository/build/ngl_viewer.js
+++ config/plugins/visualizations/charts/static/repository/build/ngl_viewer.js
@@ -49,42 +49,34 @@ define(function() { return /******/ (function(modules) { // webpackBootstrap
 	        initialize: function( options ) {
 	            var dataset = options.dataset,
 	                settings = options.chart.settings,
-	                stage = new ngl.Stage( options.targets[ 0 ], { backgroundColor: settings.get( 'backcolor' ) } );
+	                stage = new ngl.Stage( options.targets[ 0 ], { backgroundColor: settings.get( 'backcolor' ) } ),
+	                viewer_options = {},
+	                representation_parameters = {},
+	                stage_parameters = {};
 	
-	            Utils.get( {
-	                url     : dataset.download_url,
-	                cache   : true,
-	                success : function( response ) {
-	                    var viewer_options = {},
-	                        representation_parameters = {};
-	                    _.each( settings.attributes, function( value, key ) {
-	                        if ( key.startsWith( 'viewer|' ) ) {
-	                            viewer_options[ key.replace( 'viewer|', '' ) ] = value;
-	                        }
-	                    } );
-	                    representation_parameters = { 
-	                        radius: settings.get( 'radius' ),
-	                        scale: settings.get( 'scale' ),
-	                        assembly: settings.get( 'assembly' ),
-	                        color: settings.get( 'colorscheme' ),
-	                        opacity: settings.get( 'opacity' )
-	                    };
-	                    stage.loadFile( dataset.download_url, {ext: dataset.extension, name: dataset.name, defaultRepresentation: true} ).then( function( o ) {
-	                        o.addRepresentation( viewer_options.mode, representation_parameters );
-	                        o.centerView();
-	                    } );
-	                    stage.setQuality( settings.get( 'quality' ) );
-	                    if( settings.get( 'spin' ) === true || settings.get( 'spin' ) === 'true' ) {
-	                        stage.setSpin( [ 0, 1, 0 ], 0.01 );
-	                    }
-	                    options.chart.state( 'ok', 'Chart drawn.' );
-	                    options.process.resolve();
-	                },
-	                error   : function() {
-	                    options.chart.state( 'ok', 'Failed to load pdb file.' );
-	                    options.process.resolve();
+	            _.each( settings.attributes, function( value, key ) {
+	                if ( key.startsWith( 'viewer|' ) ) {
+	                    viewer_options[ key.replace( 'viewer|', '' ) ] = value;
 	                }
-	            });
+	            } );
+	            representation_parameters = { 
+	                radius: settings.get( 'radius' ),
+	                scale: settings.get( 'scale' ),
+	                assembly: settings.get( 'assembly' ),
+	                color: settings.get( 'colorscheme' ),
+	                opacity: settings.get( 'opacity' )
+	            };
+	            stage_parameters = { ext: dataset.extension, name: dataset.name, defaultRepresentation: true };
+	            stage.loadFile( dataset.download_url, stage_parameters ).then( function( component ) {
+	                component.addRepresentation( viewer_options.mode, representation_parameters );
+	                component.centerView();
+	                options.chart.state( 'ok', 'Chart drawn.' );
+	                options.process.resolve();
+	            } );
+	            stage.setQuality( settings.get( 'quality' ) );
+	            if( settings.get( 'spin' ) === true || settings.get( 'spin' ) === 'true' ) {
+	                stage.setSpin( [ 0, 1, 0 ], 0.01 );
+	            }
 	            // Re-renders the molecule view when window is resized
 	            $( window ).resize( function() { stage.viewer.handleResize() } );
 	        }
