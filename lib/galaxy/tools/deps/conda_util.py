f53b6474828d1f8ab1170630ce5851bb913ebec7
John Chilton <jmchilton@gmail.com>
2017-02-17 16:12:06 -0500

    WIP: Fix for conda=4.3

--- lib/galaxy/tools/deps/conda_util.py
+++ lib/galaxy/tools/deps/conda_util.py
@@ -82,6 +82,39 @@ class CondaContext(installable.InstallableContext):
             ensure_channels = None
         self.ensure_channels = ensure_channels
         self.ensured_channels = False
+        self._conda_version = None
+        self._miniconda_version = None
+
+    @property
+    def conda_version(self):
+        if self._conda_version is None:
+            self._guess_conda_version()
+        return self._conda_version
+
+    def _guess_conda_version(self):
+        conda_meta_path = self._conda_meta_path
+        # Perhaps we should call "conda info --json" and parse it but for now we are going
+        # to assume the default.
+        conda_version = LooseVersion(CONDA_VERSION)
+        miniconda_version = "3"
+
+        if os.path.exists(conda_meta_path):
+            for package in os.listdir(conda_meta_path):
+                package_parts = package.split("-", 2)
+                if len(package_parts) < 3:
+                    continue
+                package, version, build = package_parts
+                if package == "conda":
+                    conda_version = LooseVersion(version)
+                if package == "python" and version.startswith("2"):
+                    miniconda_version = "2"
+
+        self._conda_version = conda_version
+        self._miniconda_version = miniconda_version
+
+    @property
+    def _conda_meta_path(self):
+        return os.path.join(self.conda_prefix, "conda-meta")
 
     def ensure_channels_configured(self):
         if not self.ensured_channels:
@@ -495,7 +528,11 @@ def build_isolated_environment(
                 export_path
             )
             export_paths.append(export_path)
-        create_args = ["--unknown", "--offline"]
+        create_args = ["--unknown"]
+        if conda_context.conda_version < LooseVersion("4.3"):
+            create_args.append("--offline")
+        else:
+            create_args.extend(["--use-index-cache"])
         if path is None:
             create_args.extend(["--name", tempdir_name])
         else:
