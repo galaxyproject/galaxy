b91680a5b2dfc83b8ee974fdc9ed6f2ab405829c
guerler <aysam.guerler@gmail.com>
2017-02-21 16:26:46 -0500

    Use shared label/annotation input parameter building process

--- client/galaxy/scripts/mvc/workflow/workflow-view.js
+++ client/galaxy/scripts/mvc/workflow/workflow-view.js
@@ -662,58 +662,58 @@ define([
             if ( content && $container.find( '#' + id ).length == 0 ) {
                 var $el = $( '<div id="' + id + '" class="' + cls + '"/>' );
                 var form = null;
+                content.node = node;
+                content.workflow = this.workflow;
+                content.datatypes = this.datatypes;
                 content.icon = WorkflowIcons[ node.type ];
                 content.cls = 'ui-portlet-narrow';
-                if ( node.type == 'tool' ) {
-                    content.node = node;
-                    content.workflow = this.workflow;
-                    content.datatypes = this.datatypes;
-                    form = new ToolForm.View( content );
-                } else {
-                    content.inputs.unshift({
-                        type    : 'text',
-                        name    : '__label',
-                        label   : 'Label',
-                        value   : node.label,
-                        help    : 'Add a step label.',
-                        onchange: function( new_label ) {
-                            var duplicate = false;
-                            for ( var i in self.workflow.nodes ) {
-                                var n = self.workflow.nodes[ i ];
-                                if ( n.label && n.label == new_label && n.id != node.id ) {
-                                    duplicate = true;
-                                    break;
-                                }
+                content.inputs.unshift({
+                    type    : 'text',
+                    name    : '__label',
+                    label   : 'Label',
+                    value   : node.label,
+                    help    : 'Add a step label.',
+                    onchange: function( new_label ) {
+                        var duplicate = false;
+                        for ( var i in self.workflow.nodes ) {
+                            var n = self.workflow.nodes[ i ];
+                            if ( n.label && n.label == new_label && n.id != node.id ) {
+                                duplicate = true;
+                                break;
                             }
-                            var input_id = form.data.match( '__label' );
-                            var input_element = form.element_list[ input_id ];
-                            input_element.model.set( 'error_text', duplicate && 'Duplicate label. Please fix this before saving the workflow.' );
-                            form.trigger( 'change' );
+                        }
+                        var input_id = form.data.match( '__label' );
+                        var input_element = form.element_list[ input_id ];
+                        input_element.model.set( 'error_text', duplicate && 'Duplicate label. Please fix this before saving the workflow.' );
+                        form.trigger( 'change' );
+                    }
+                });
+                content.inputs.push({
+                    type    : 'text',
+                    name    : '__annotation',
+                    label   : 'Annotation',
+                    value   : node.annotation,
+                    area    : true,
+                    help    : 'Add an annotation or notes to this step. Annotations are available when a workflow is viewed.'
+                });
+                content.onchange = function() {
+                    Utils.request({
+                        type    : 'POST',
+                        url     :  Galaxy.root + 'api/workflows/build_module',
+                        data    : {
+                            id          : node.id,
+                            type        : node.type,
+                            content_id  : node.content_id,
+                            inputs      : form.data.create()
+                        },
+                        success : function( data ) {
+                            node.update_field_data( data );
                         }
                     });
-                    content.inputs.push({
-                        type    : 'text',
-                        name    : '__annotation',
-                        label   : 'Annotation',
-                        value   : node.annotation,
-                        area    : true,
-                        help    : 'Add an annotation or notes to this step. Annotations are available when a workflow is viewed.'
-                    });
-                    content.onchange = function() {
-                        Utils.request({
-                            type    : 'POST',
-                            url     :  Galaxy.root + 'api/workflows/build_module',
-                            data    : {
-                                id          : node.id,
-                                type        : node.type,
-                                content_id  : node.content_id,
-                                inputs      : form.data.create()
-                            },
-                            success : function( data ) {
-                                node.update_field_data( data );
-                            }
-                        });
-                    };
+                };
+                if ( node.type == 'tool' ) {
+                    form = new ToolForm.View( content );
+                } else {
                     form = new Form( content );
                 }
                 $el.append( form.$el );
