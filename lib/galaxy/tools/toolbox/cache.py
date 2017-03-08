7bf6a581249ec409d33ccc060816b9f505185165
mvdbeek <m.vandenbeek@gmail.com>
2017-03-08 20:48:21 +0100

    Remove outdated items from tool cache

--- lib/galaxy/tools/toolbox/cache.py
+++ lib/galaxy/tools/toolbox/cache.py
@@ -1,3 +1,6 @@
+import os
+
+from galaxy.util.hash_util import md5_hash_file
 
 
 class ToolCache(object):
@@ -7,8 +10,19 @@ class ToolCache(object):
     """
 
     def __init__(self):
+        self._hash_by_tool_paths = {}
         self._tools_by_path = {}
         self._tool_paths_by_id = {}
+
+    def cleanup(self):
+        """Remove uninstalled tools from tool cache if they are not on disk anymore or if their content has changed."""
+        clean_paths = {path: tool.all_ids for path, tool in self._tools_by_path.items() if not os.path.exists(path) or md5_hash_file(path) != self._hash_by_tool_paths[path]}
+        for path, tool_ids in clean_paths.items():
+            del self._hash_by_tool_paths[path]
+            del self._tools_by_path[path]
+            for tool_id in tool_ids:
+                if tool_id in self._tool_paths_by_id:
+                    del self._tool_paths_by_id[tool_id]
 
     def get_tool(self, config_filename):
         """ Get the tool from the cache if the tool is up to date.
@@ -22,6 +36,8 @@ class ToolCache(object):
             del self._tools_by_path[config_filename]
 
     def cache_tool(self, config_filename, tool):
+        tool_hash = md5_hash_file(config_filename)
         tool_id = str( tool.id )
+        self._hash_by_tool_paths[config_filename] = tool_hash
         self._tool_paths_by_id[tool_id] = config_filename
         self._tools_by_path[config_filename] = tool
