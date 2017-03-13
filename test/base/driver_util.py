8d8bf5eaffeda0f229bf56cb45e4adbb05b12d57
John Chilton <jmchilton@gmail.com>
2017-03-12 23:40:45 -0400

    Dockerized job integration testing.
    Add simple test cases for both mulled containers and explicit Docker image resolution.

--- test/base/driver_util.py
+++ test/base/driver_util.py
@@ -131,6 +131,8 @@ def setup_galaxy_config(
     log_format=None,
 ):
     """Setup environment and build config for test Galaxy instance."""
+    # For certain docker operations this needs to be evaluated out - e.g. for cwltool.
+    tmpdir = os.path.realpath(tmpdir)
     if not os.path.exists(tmpdir):
         os.makedirs(tmpdir)
     file_path = os.path.join(tmpdir, 'files')
