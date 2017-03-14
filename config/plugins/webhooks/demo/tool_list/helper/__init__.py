90ae42c3f1d0efc4f8b461e43a158eb5b7b95d4f
Clemens BlanK <blankclemens@gmail.com>
2017-03-14 17:04:48 +0000

    Reduced API calls to speed it up.

--- config/plugins/webhooks/demo/tool_list/helper/__init__.py
+++ config/plugins/webhooks/demo/tool_list/helper/__init__.py
@@ -10,23 +10,22 @@ def main(trans, webhook):
     for tool in tools:
         if tool[1].tool_shed_repository is not None:
 
-            if (str(tool[1].tool_shed_repository.name) +
-                    str(tool[1].tool_shed_repository.installed_changeset_revision)
-                    not in unique_tools):
+            name = str(tool[1].tool_shed_repository.name)
+            revision = str(tool[1].tool_shed_repository.installed_changeset_revision)
 
-                unique_tools.append(
-                    str(tool[1].tool_shed_repository.name) +
-                    str(tool[1].tool_shed_repository.installed_changeset_revision))
+            if (name + revision) not in unique_tools:
+
+                unique_tools.append(name + revision)
 
                 data['tools'].append({
-                    'name': tool[1].tool_shed_repository.name,
+                    'name': name,
                     'owner': tool[1].tool_shed_repository.owner,
                     'tool_panel_section_label': tool[1].get_panel_section()[1],
                     'tool_shed_url': tool[1].tool_shed_repository.tool_shed,
                     'install_tool_dependencies': True,
                     'install_repository_dependencies': True,
                     'install_resolver_dependencies': True,
-                    'revisions': [str(tool[1].tool_shed_repository.installed_changeset_revision)]
+                    'revisions': [revision]
                 })
                 if tool[1].get_panel_section()[0]:
                     data['tools'][-1]['tool_panel_section_id'] = \
