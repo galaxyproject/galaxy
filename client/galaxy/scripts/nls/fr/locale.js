90de3671f132848a9eaae9048bf091f1969d0caf
Yvan Le Bras <yvan.le_bras@irisa.fr>
2017-03-17 15:56:57 +0100

    pass 2

--- client/galaxy/scripts/nls/fr/locale.js
+++ client/galaxy/scripts/nls/fr/locale.js
@@ -6,61 +6,61 @@ define({
 "Workflow":
     "Workflow",
 "Shared Data":
-    "Données partagées",
+    "Partage de données",
 "Data Libraries":
-    "Bibliothèque de données",
+    false,
 "Histories":
-    "Historiques",
+    false,
 "Workflows":
-    "Workflows",
+    false,
 "Visualizations":
-    "Visualisations",
+    false,
 "Pages":
-    "Pages",
+    false,
 "Visualization":
-    "Visualisation",
+    "Représentation des données",
 "New Track Browser":
-    "Nouveau Navigateur de Tracks/Pistes",
+    false,
 "Saved Visualizations":
-    "Visualisations sauvegardés",
+    false,
 "Interactive Environments":
-    "Environnements interactifs",
+    false,
 "Admin":
-    "Admin",
+    false,
 "Help":
     "Aide",
 "Support":
-    "Assistance",
+    false,
 "Search":
-    "Recherche",
+    false,
 "Mailing Lists":
-    "Liste de diffusion",
+    false,
 "Videos":
-    "Vidéos",
+    false,
 "Wiki":
-    "Documentation",
+    false,
 "How to Cite Galaxy":
-    "Comment citer Galaxy",
+    false,
 "Interactive Tours":
-    "Guides interactifs",
+    false,
 "User":
     "Utilisateur",
 "Login":
-    "Authentification",
+    false,
 "Register":
-    "Enregistrement",
+    false,
 "Logged in as":
-    "Authentification en tant que",
+    false,
 "Custom Builds":
-    "Genome Builds personalisés",
+    false,
 "Logout":
-    "Déconnection",
+    false,
 "Saved Histories":
-    "Historiques sauvegardés",
+    false,
 "Saved Datasets":
-    "Données sauvegardés",
+    false,
 "Saved Pages":
-    "Pages sauvegardées",
+    false,
 // ---------------------------------------------------------------------------- histories
 // ---- history-model
 // ---- history-view
@@ -69,18 +69,18 @@ define({
 "No matching datasets found" :
     "Aucunes données correspondantes trouvées",
 "An error occurred while getting updates from the server" :
-    "Une erreur s'est produite lors de la réception des données depuis le serveur",
+    "Une erreur s'est produite lors de l'envoi des mises à jour depuis le serveur",
 "Please contact a Galaxy administrator if the problem persists" :
-    "Veuillez contacter un administrateur de l'instance Galaxy si ce problème persiste.",
+    "Veuillez contacter un administrateur Galaxy si ce problème persiste.",
 //TODO:
 //"An error was encountered while <% where %>" :
 //false,
 "Search datasets" :
-    "Rechercher des données",
+    "Recherche dans l'ensemble des données",
 "You are currently viewing a deleted history!" :
     "Vous consultez actuellement un historique supprimé!",
 "You are over your disk quota" :
-    "Vous avez dépassé votre quota d'espace disque",
+    "Vous avez dépassé votre quota disque",
 "Tool execution is on hold until your disk usage drops below your allocated quota" :
     "L'exécution de l'outil est en attente tant que votre utilisation d'espace disque n'est pas inférieur à votre quota attribué",
 "All" :
@@ -88,79 +88,79 @@ define({
 "None" :
     "Aucun",
 "For all selected" :
-    "Pour tous les selectionnés",
+    "Pour tous les",
 
 // // ---- history-view-edit
 // "Edit history tags" :
-// false,
+// "Editer les mots-clés de l'historique",
 // "Edit history Annotation" :
-// false,
+// "Editer l'annotation de l'historique",
 // "Click to rename history" :
-// false,
+// "Cliquer pour renommer l'historique",
 // // multi operations
 // "Operations on multiple datasets" :
-// false,
+// "Opérer sur plusieurs jeux de données en même temps",
 // "Hide datasets" :
-// false,
+// "Cacher les jeux de données",
 // "Unhide datasets" :
-// false,
+// "Restaurer les jeux de données cachés",
 // "Delete datasets" :
-// false,
+// "Supprimer les jeux de données",
 // "Undelete datasets" :
-// false,
+// "Restaurer les jeux de données supprimés",
 // "Permanently delete datasets" :
-// false,
+// "Supprimer définitivement les jeux de données",
 // "This will permanently remove the data in your datasets. Are you sure?" :
-// false,
+// "Cela supprimera de manière permanente les données de votre historique. Êtes-vous certains?",
 
 // // ---- history-view-annotated
 // "Dataset" :
-// false,
+// "Jeu de données",
 // "Annotation" :
-// false,
-//
+// "Annotation",
+// 
 // // ---- history-view-edit-current
 // "This history is empty. Click 'Get Data' on the left tool menu to start" :
-// false,
+// "Cet historique est vide. Cliquer sur 'Téléverser des données' au niveau du menu d'outils à gauche pour démarrer",
 // "You must be logged in to create histories" :
-// false,
+// "Vous devez être connecté pour créer un historique",
 //TODO:
 //"You can <% loadYourOwn %> or <% externalSource %>" :
-//false,
+//"Vous pouvez <% loadYourOwn %> ou <% externalSource %>",
 //"load your own data" :
-//false,
+//"Charger vos propres données",
 //"get data from an external source" :
-//false,
+//"Charger des données depuis une source externe",
 
 // these aren't in zh/ginga.po and the template doesn't localize
 //"Include Deleted Datasets" :
-//false,
+//"Inclure les jeux de données supprimés",
 //"Include Hidden Datasets" :
-//false,
+//"Inclure les jeux de données cachés",
 
 
 // ---------------------------------------------------------------------------- datasets
 // ---- hda-model
 // "Unable to purge dataset" :
-// false,
+// "Impossible de purger le jeu de données",
 
 // ---- hda-base
 // display button
 // "Cannot display datasets removed from disk" :
-// false,
+// "Impossible de visualiser les jeux de données supprimés du disque",
 // "This dataset must finish uploading before it can be viewed" :
-// false,
+// "Le jeu de données doit être totalement téléversé avant de pouvoir être visualiser",
 // "This dataset is not yet viewable" :
-// false,
+// "Ce jeu de données n'est pas visualisable",
 // "View data" :
-// false,
+// "Voir les données,
 // download button
 "Download" :
     "Télécharger",
 "Download dataset" :
     "Télécharger le jeu de données",
 // "Additional files" :
-// false,
+// "Fichiers additionnels",
 // info/show_params
 "View details" :
     "Voir les détails",
@@ -168,105 +168,105 @@ define({
 // // dataset states
 // // state: new
 // "This is a new dataset and not all of its data are available yet" :
-// false,
+// "Il s'agit d'un nouveau jeu de données et seule une partie des données est accessible pour le moment",
 // // state: noPermission
 // "You do not have permission to view this dataset" :
-// false,
+// "Vous n'avez pas la permission de voir ce jeu de données",
 // // state: discarded
 // "The job creating this dataset was cancelled before completion" :
-// false,
+// "Le calcul ayant créé ce jeu de données a été annulé prématurément",
 // // state: queued
 // "This job is waiting to run" :
-// false,
+// "Ce calcul est en attente de traitement",
 // // state: upload
 // "This dataset is currently uploading" :
-// false,
+// "Ce jeu de données est en cours de téléversement",
 // // state: setting_metadata
 // "Metadata is being auto-detected" :
-// false,
+// "Les métadonnées sont auto-détectées",
 // // state: running
 // "This job is currently running" :
-// false,
+// "Le traitement est en cours",
 // // state: paused
 // "This job is paused. Use the \"Resume Paused Jobs\" in the history menu to resume" :
-// false,
+// "Ce traitement est en pause. Utilisez le \"Relancer les traitements en pause\" dans le menu d'historique pour le relancer",
 // // state: error
 // "An error occurred with this dataset" :
-// false,
+// "Un erreur est survenue avec ce jeu de données",
 // // state: empty
 // "No data" :
-// false,
+// "Aucune donnée",
 // // state: failed_metadata
 // "An error occurred setting the metadata for this dataset" :
-// false,
-//
+// "Une erreur est survenue pendant la récupération des métadonnées de ce jeu de données",
+// 
 // // ajax error prefix
 // "There was an error getting the data for this dataset" :
-// false,
-//
+// "Il est survenu une erreur durant la récupération du contenu de ce jeu de données",
+// 
 // // purged'd/del'd msg
 // "This dataset has been deleted and removed from disk" :
-// false,
+// "Ce jeu de données a été supprimé et effacé du disque",
 // "This dataset has been deleted" :
-// false,
+// "Ce jeu de données a été supprimé",
 // "This dataset has been hidden" :
-// false,
-//
+// "Ce jeu de données a été caché",
+// 
 // "format" :
-// false,
+// "format",
 // "database" :
-// false,
-//
+// "génome de référence",
+// 
 // // ---- hda-edit
 // "Edit attributes" :
-// false,
+// "Editer les attributs",
 // "Cannot edit attributes of datasets removed from disk" :
-// false,
+// "Impossible d'éditer les attributs de jeux de données effacés du disque",
 // "Undelete dataset to edit attributes" :
-// false,
+// "Restaurer le jeu de données pour en éditer les attributs",
 // "This dataset must finish uploading before it can be edited" :
-// false,
+// "Ce jeu de données doit être entièrement téléversé avant avant toute modification",
 // "This dataset is not yet editable" :
-// false,
-//
+// "Ce jeu de données n'est pas encore éditable",
+// 
 // "Delete" :
-// false,
+// "Supprimer",
 // "Dataset is already deleted" :
-// false,
-//
+// "Pov' tâche, le jeu de données est déjà supprimé ;)",
+// 
 // "View or report this error" :
-// false,
-//
+// "Voir ou remonter cette erreur",
+// 
 // "Run this job again" :
-// false,
-//
+// "Exécuter ce traitement à nouveau",
+// 
 // "Visualize" :
-// false,
+// "Visualiser",
 // "Visualize in" :
-// false,
-//
+// "Visualiser via",
+// 
 // "Undelete it" :
-// false,
+// "Restaurer",
 // "Permanently remove it from disk" :
-// false,
+// "Supprimer définitivement du disque",
 // "Unhide it" :
-// false,
-//
+// "Rendre visible",
+// 
 // "You may be able to" :
-// false,
+// "Vous devriez être en mesure de",
 // "set it manually or retry auto-detection" :
-// false,
-//
+// "Traitez le manuellement ou retenter la détection automatique",
+// 
 // "Edit dataset tags" :
-// false,
+// "Editer les mots-clés du jeu de données",
 // "Edit dataset annotation" :
-// false,
-//
-//
+// "Editer les annotations du jeu de données",
+// 
+// 
 // // ---------------------------------------------------------------------------- misc. MVC
 // "Tags" :
-// false,
+// "Mots-clés",
 // "Edit annotation" :
-// false,
+// "Editer les annotations",
 
 });
