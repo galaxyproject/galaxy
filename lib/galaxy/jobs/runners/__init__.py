galaxyproject/galaxy

commit b78287f1508db2c06f0c309ed8d3747adb4d17fa

Integration test for advanced job runner stuff.
Set of test cases that cover job resource handlers and resubmit job destinations.

lib/galaxy/jobs/runners/__init__.py
@@ -368,6 +368,17 @@ class BaseJobRunner( object ):
         except:
             log.exception('Caught exception in runner state handler:')
 
+    def fail_job( self, job_state, exception=False ):
+        if getattr( job_state, 'stop_job', True ):
+            self.stop_job( self.sa_session.query( self.app.model.Job ).get( job_state.job_wrapper.job_id ) )
+        self._handle_runner_state( 'failure', job_state )
+        # Not convinced this is the best way to indicate this state, but
+        # something necessary
+        if not job_state.runner_state_handled:
+            job_state.job_wrapper.fail( getattr( job_state, 'fail_message', 'Job failed' ), exception=exception )
+            if job_state.job_wrapper.cleanup_job == "always":
+                job_state.cleanup()
+
     def mark_as_resubmitted( self, job_state, info=None ):
         job_state.job_wrapper.mark_as_resubmitted( info=info )
         if not self.app.config.track_jobs_in_database:
@@ -386,8 +397,10 @@ class JobState( object ):
         OUTPUT_SIZE_LIMIT='output_size_limit'
     )
 
-    def __init__( self ):
+    def __init__( self, job_wrapper, job_destination ):
         self.runner_state_handled = False
+        self.job_wrapper = job_wrapper
+        self.job_destination = job_destination
 
     def set_defaults( self, files_dir ):
         if self.job_wrapper is not None:
@@ -421,16 +434,14 @@ class AsynchronousJobState( JobState ):
     """
 
     def __init__( self, files_dir=None, job_wrapper=None, job_id=None, job_file=None, output_file=None, error_file=None, exit_code_file=None, job_name=None, job_destination=None  ):
-        super( AsynchronousJobState, self ).__init__()
+        super( AsynchronousJobState, self ).__init__( job_wrapper, job_destination )
         self.old_state = None
         self._running = False
         self.check_count = 0
         self.start_time = None
 
-        self.job_wrapper = job_wrapper
         # job_id is the DRM's job id, not the Galaxy job id
         self.job_id = job_id
-        self.job_destination = job_destination
 
         self.job_file = job_file
         self.output_file = output_file
@@ -612,17 +623,6 @@ class AsynchronousJobRunner( BaseJobRunner ):
         except:
             log.exception( "(%s/%s) Job wrapper finish method failed" % ( galaxy_id_tag, external_job_id ) )
             job_state.job_wrapper.fail( "Unable to finish job", exception=True )
-
-    def fail_job( self, job_state ):
-        if getattr( job_state, 'stop_job', True ):
-            self.stop_job( self.sa_session.query( self.app.model.Job ).get( job_state.job_wrapper.job_id ) )
-        self._handle_runner_state( 'failure', job_state )
-        # Not convinced this is the best way to indicate this state, but
-        # something necessary
-        if not job_state.runner_state_handled:
-            job_state.job_wrapper.fail( getattr( job_state, 'fail_message', 'Job failed' ) )
-            if job_state.job_wrapper.cleanup_job == "always":
-                job_state.cleanup()
 
     def mark_as_finished(self, job_state):
         self.work_queue.put( ( self.finish_job, job_state ) )

lib/galaxy/jobs/__init__.py
@@ -764,7 +764,30 @@ class JobConfiguration( object ):
                     log.warning("Legacy destination with id '%s' could not be converted: Unknown runner plugin: %s" % (id, destination.runner))
 
 
-class JobWrapper( object ):
+class HasResourceParameters:
+
+    def get_resource_parameters( self, job=None ):
+        # Find the dymically inserted resource parameters and give them
+        # to rule.
+
+        if job is None:
+            job = self.get_job()
+
+        app = self.app
+        param_values = job.get_param_values( app, ignore_errors=True )
+        resource_params = {}
+        try:
+            resource_params_raw = param_values[ "__job_resource" ]
+            if resource_params_raw[ "__job_resource__select" ].lower() in [ "1", "yes", "true" ]:
+                for key, value in resource_params_raw.items():
+                    resource_params[ key ] = value
+        except KeyError:
+            pass
+
+        return resource_params
+
+
+class JobWrapper( object, HasResourceParameters ):
     """
     Wraps a 'model.Job' with convenience methods for running processes and
     state management.

lib/galaxy/jobs/mapper.py
@@ -130,18 +130,7 @@ class JobRunnerMapper( object ):
                 actual_args[ "user_email" ] = user_email
 
             if "resource_params" in function_arg_names:
-                # Find the dymically inserted resource parameters and give them
-                # to rule.
-                param_values = self.__job_params( job )
-                resource_params = {}
-                try:
-                    resource_params_raw = param_values[ "__job_resource" ]
-                    if resource_params_raw[ "__job_resource__select" ].lower() in [ "1", "yes", "true" ]:
-                        for key, value in resource_params_raw.iteritems():
-                            resource_params[ key ] = value
-                except KeyError:
-                    pass
-                actual_args[ "resource_params" ] = resource_params
+                actual_args[ "resource_params" ] = self.job_wrapper.get_resource_parameters( job )
 
             if "workflow_invocation_uuid" in function_arg_names:
                 param_values = job.raw_param_dict( )

lib/galaxy/jobs/runners/local.py
@@ -78,9 +78,16 @@ class LocalJobRunner( BaseJobRunner ):
 
     def queue_job( self, job_wrapper ):
         # prepare the job
-        include_metadata = asbool( job_wrapper.job_destination.params.get( "embed_metadata_in_job", DEFAULT_EMBED_METADATA_IN_JOB ) )
+        job_destination = job_wrapper.job_destination
+        include_metadata = asbool( job_destination.params.get( "embed_metadata_in_job", DEFAULT_EMBED_METADATA_IN_JOB ) )
         if not self.prepare_job( job_wrapper, include_metadata=include_metadata ):
             return
+
+        def fail(message):
+            job_state = JobState(job_wrapper, job_destination)
+            job_state.fail_message = message
+            job_state.stop_job = False
+            self.fail_job(job_state, exception=True)
 
         stderr = stdout = ''
         exit_code = 0
@@ -100,7 +107,7 @@ class LocalJobRunner( BaseJobRunner ):
                                      stderr=stderr_file,
                                      env=self._environ,
                                      preexec_fn=os.setpgrp )
-            job_wrapper.set_job_destination(job_wrapper.job_destination, proc.pid)
+            job_wrapper.set_job_destination(job_destination, proc.pid)
             job_wrapper.change_state( model.Job.states.RUNNING )
 
             terminated = self.__poll_if_needed( proc, job_wrapper, job_id )
@@ -123,17 +130,17 @@ class LocalJobRunner( BaseJobRunner ):
             log.debug('execution finished: %s' % command_line)
         except Exception:
             log.exception("failure running job %d" % job_wrapper.job_id)
-            job_wrapper.fail( "failure running job", exception=True )
+            fail("failure running job")
             return
-        external_metadata = not asbool( job_wrapper.job_destination.params.get( "embed_metadata_in_job", DEFAULT_EMBED_METADATA_IN_JOB ) )
-        if external_metadata:
+
+        if not include_metadata:
             self._handle_metadata_externally( job_wrapper, resolve_requirements=True )
         # Finish the job!
         try:
             job_wrapper.finish( stdout, stderr, exit_code )
         except:
             log.exception("Job wrapper finish method failed")
-            job_wrapper.fail("Unable to finish job", exception=True)
+            fail("Unable to finish job")
 
     def stop_job( self, job ):
         # if our local job has JobExternalOutputMetadata associated, then our primary job has to have already finished

lib/galaxy/jobs/runners/state_handlers/resubmit.py
@@ -25,10 +25,14 @@ def failure(app, job_runner, job_state):
             # There is a resubmit defined for the destination but
             # its condition is not for the encountered state
             continue
-        log.info("(%s/%s) Job will be resubmitted to '%s' because %s at "
+        external_id = getattr(job_state, "job_id", None)
+        if external_id:
+            job_log_prefix = "(%s/%s)" % (job_state.job_wrapper.job_id, job_state.job_id)
+        else:
+            job_log_prefix = "(%s)" % (job_state.job_wrapper.job_id)
+        log.info("%s Job will be resubmitted to '%s' because %s at "
                  "the '%s' destination",
-                 job_state.job_wrapper.job_id,
-                 job_state.job_id,
+                 job_log_prefix,
                  resubmit['destination'],
                  MESSAGES[job_state.runner_state],
                  job_state.job_wrapper.job_destination.id )
@@ -43,8 +47,8 @@ def failure(app, job_runner, job_state):
         job_state.job_wrapper.invalidate_external_metadata()
         job = job_state.job_wrapper.get_job()
         if resubmit.get('handler', None):
-            log.debug('(%s/%s) Job reassigned to handler %s',
-                      job_state.job_wrapper.job_id, job_state.job_id,
+            log.debug('%s Job reassigned to handler %s',
+                      job_log_prefix,
                       resubmit['handler'])
             job.set_handler(resubmit['handler'])
             job_runner.sa_session.add( job )

test/base/driver_util.py
@@ -707,7 +707,7 @@ class GalaxyTestDriver(TestDriver):
         )
         return functional.test_toolbox
 
-    def run_tool_test(self, tool_id, index=0):
+    def run_tool_test(self, tool_id, index=0, resource_parameters={}):
         import functional.test_toolbox
         functional.test_toolbox.toolbox = self.app.toolbox
         tool = self.app.toolbox.get_tool(tool_id)
@@ -718,7 +718,7 @@ class GalaxyTestDriver(TestDriver):
         test_case.master_api_key = get_master_api_key()
         test_case.user_api_key = get_user_api_key()
         test_case.setUp()
-        test_case.do_it(testdef)
+        test_case.do_it(testdef, resource_parameters=resource_parameters)
 
 
 def drive_test(test_driver_class):

test/base/interactor.py
@@ -218,7 +218,7 @@ class GalaxyInteractorApi( object ):
         self.uploads[ os.path.basename(fname) ] = self.uploads[ fname ] = self.uploads[ name ] = {"src": "hda", "id": hid}
         return self.__wait_for_history( history_id )
 
-    def run_tool( self, testdef, history_id ):
+    def run_tool( self, testdef, history_id, resource_parameters={} ):
         # We need to handle the case where we've uploaded a valid compressed file since the upload
         # tool will have uncompressed it on the fly.
 
@@ -235,6 +235,11 @@ class GalaxyInteractorApi( object ):
                 else:
                     new_values.append( value )
             inputs_tree[ key ] = new_values
+
+        if resource_parameters:
+            inputs_tree["__job_resource|__job_resource__select"] = "yes"
+            for key, value in resource_parameters.items():
+                inputs_tree["__job_resource|%s" % key] = value
 
         # HACK: Flatten single-value lists. Required when using expand_grouping
         for key, value in inputs_tree.items():

test/functional/test_toolbox.py
@@ -28,7 +28,7 @@ TOOL_TYPES_NO_TEST = ( DataManagerTool, )
 class ToolTestCase( TwillTestCase ):
     """Abstract test case that runs tests based on a `galaxy.tools.test.ToolTest`"""
 
-    def do_it( self, testdef ):
+    def do_it( self, testdef, resource_parameters={} ):
         """
         Run through a tool test case.
         """
@@ -53,7 +53,7 @@ class ToolTestCase( TwillTestCase ):
         expected_failure_occurred = False
         try:
             try:
-                tool_response = galaxy_interactor.run_tool( testdef, test_history )
+                tool_response = galaxy_interactor.run_tool( testdef, test_history, resource_parameters=resource_parameters )
                 data_list, jobs, tool_inputs = tool_response.outputs, tool_response.jobs, tool_response.inputs
                 data_collection_list = tool_response.output_collections
             except RunToolException as e:

commit 15aa0bd615726ed46fa7beec9a364bb3c11df9f2

Fix problem with cleaning up jobs in local runner.
Fixes #3801 (broken in b78287f as part of #3291).

lib/galaxy/jobs/runners/__init__.py
@@ -404,6 +404,8 @@ class JobState( object ):
         self.job_wrapper = job_wrapper
         self.job_destination = job_destination
 
+        self.cleanup_file_attributes = [ 'job_file', 'output_file', 'error_file', 'exit_code_file' ]
+
     def set_defaults( self, files_dir ):
         if self.job_wrapper is not None:
             id_tag = self.job_wrapper.get_id_tag()
@@ -426,6 +428,19 @@ class JobState( object ):
     @staticmethod
     def default_exit_code_file( files_dir, id_tag ):
         return os.path.join( files_dir, 'galaxy_%s.ec' % id_tag )
+
+    def cleanup( self ):
+        for file in [ getattr( self, a ) for a in self.cleanup_file_attributes if hasattr( self, a ) ]:
+            try:
+                os.unlink( file )
+            except Exception as e:
+                # TODO: Move this prefix stuff to a method so we don't have dispatch on attributes we may or may
+                # not have.
+                if not hasattr( self, "job_id" ):
+                    prefix = "(%s)" % self.job_wrapper.get_id_tag()
+                else:
+                    prefix = "(%s/%s)" % ( self.job_wrapper.get_id_tag(), self.job_id )
+                log.debug( "%s Unable to cleanup %s: %s" % (prefix, file, str( e ) ) )
 
 
 class AsynchronousJobState( JobState ):
@@ -453,8 +468,6 @@ class AsynchronousJobState( JobState ):
 
         self.set_defaults( files_dir )
 
-        self.cleanup_file_attributes = [ 'job_file', 'output_file', 'error_file', 'exit_code_file' ]
-
     @property
     def running( self ):
         return self._running
@@ -481,13 +494,6 @@ class AsynchronousJobState( JobState ):
             self.stop_job = True
             return True
         return False
-
-    def cleanup( self ):
-        for file in [ getattr( self, a ) for a in self.cleanup_file_attributes if hasattr( self, a ) ]:
-            try:
-                os.unlink( file )
-            except Exception as e:
-                log.debug( "(%s/%s) Unable to cleanup %s: %s" % ( self.job_wrapper.get_id_tag(), self.job_id, file, str( e ) ) )
 
     def register_cleanup_file_attribute( self, attribute ):
         if attribute not in self.cleanup_file_attributes: