26822859c13d19cc86185f68fe5145439d04af97
anuprulez <anup.rulez@gmail.com>
2017-04-26 00:02:25 +0200

    Adding shared workflow and congifure workflow options

--- client/galaxy/scripts/mvc/workflow/workflow.js
+++ client/galaxy/scripts/mvc/workflow/workflow.js
@@ -11,39 +11,51 @@ define( [ 'mvc/form/form-view', 'mvc/ui/ui-misc', 'mvc/ui/ui-select' ], function
 
         render: function() {
             var self = this,
-                workflows = [];
+                workflows = [],
+                shared_workflows = [],
+                url = Galaxy.root + 'api/workflows/',
+                username = Galaxy.user.attributes.username;
 
-            $.getJSON( Galaxy.root + 'api/workflows/', function( response ) {
+            $.getJSON( url, function( response ) {
                 self.$el.empty().append( self._templateHeader() );
                 // Update the workflows collection with different attributes names
                 // to be shown as a dropdown list
-                for(var i = 0; i < response.length; i++) {
-                    var wf_obj = {},
-                        wf = response[ i ];
+                _.each( response, function( wf ) {
+                    var wf_obj = {};
                     wf_obj.id = wf.id;
                     wf_obj.text = wf.name;
                     wf_obj.update_time = wf.update_time;
                     wf_obj.workflow_steps = wf.latest_workflow_steps;
-                    workflows.push( wf_obj );
-                }
+                    wf.owner === username ? workflows.push( wf_obj ) : shared_workflows.push( wf_obj );
+                });
                 // Button for new workflow
                 self.btnNewWorkflow = new Ui.Button( {
                     title: 'New Workflow',
                     tooltip: 'Create a workflow',
                     icon: 'fa-plus-circle',
-                    onclick: function() { self.new_workflow(); }
+                    onclick: function() { window.location.href = Galaxy.root + 'workflow/create'; }
                 } );
                 // Button for importing a workflow
                 self.btnImportWorkflow = new Ui.Button( {
                     title: 'Import Workflow',
                     tooltip: 'Import a workflow',
                     icon: 'fa-upload',
-                    onclick: function() { self.import_workflow(); }
+                    cls: 'btn-wf',
+                    onclick: function() { window.location.href = Galaxy.root + 'workflow/import_workflow'; }
+                } );
+                // Button for configuring workflow menu
+                self.btnCongifureWorkflowMenu = new Ui.Button( {
+                    title: 'Configure menu',
+                    tooltip: 'Configure your workflow menu',
+                    icon: 'fa-cog',
+                    cls: 'btn-wf',
+                    onclick: function() { window.location.href = Galaxy.root + 'workflow/configure_menu'; }
                 } );
 
-                // Make new and import workflow buttons
+                // Make new, import workflow and configure workflow menu buttons
                 self.$el.append( self.btnNewWorkflow.$el );
                 self.$el.append( self.btnImportWorkflow.$el );
+                self.$el.append( self.btnCongifureWorkflowMenu.$el );
 
                 // Make workflows select list only where there is at least
                 // one workflow present
@@ -54,44 +66,49 @@ define( [ 'mvc/form/form-view', 'mvc/ui/ui-misc', 'mvc/ui/ui-select' ], function
                         container   : self.$( '.user-workflows' ),
                         data        : workflows,
                         value       : workflows[0].id, // Defaults to the first value
-                        onchange    : function( value ) { self.select_workflow( self, value, workflows ) }
+                        onchange    : function( value ) { self.select_workflow( self, value, workflows, 'user-workflows' ) }
                     });
                     self.$el.append( self.selectWorkflows.$el );
                     // Make table to show default selected workflow
-                    self.select_workflow( self, workflows[0].id, workflows );
+                    self.select_workflow( self, workflows[0].id, workflows, 'user-workflows' );
                 }
                 else {
                     self.$el.append( '<div class="wf-nodata"> You have no workflows. </div>' );
                 }
+
+                // Make shared workflows select list only where there is at least
+                // one shared workflow present
+                if( shared_workflows.length > 0 ) {
+                    // Workflows select box
+                    self.selectSharedWorkflows = new Select.View({
+                        css         : 'workflow-list',
+                        container   : self.$( '.shared-workflows' ),
+                        data        : shared_workflows,
+                        value       : shared_workflows[0].id, // Defaults to the first value
+                        onchange    : function( value ) { self.select_workflow( self, value, shared_workflows, 'shared-workflows' ) }
+                    });
+                    self.$el.append( self.selectSharedWorkflows.$el );
+                    // Make table to show default selected workflow
+                    self.select_workflow( self, shared_workflows[0].id, shared_workflows, 'shared-workflows' );
+                }
+                else {
+                    self.$el.append( '<div class="wf-nodata"> No workflows have been shared with you. </div>' );
+                }
             });
         },
 
-        /* Select a workflow */
-        select_workflow: function( self, value, collection ) {
-            var template = "",
-                $el = $( '.user-workflows' );
-            $el.find( '.manage-table' ).remove();
-
-            for(var i = 0; i < collection.length; i++) {
-                var item = collection[i];
+        /** Select a workflow */
+        select_workflow: function( self, value, collection, cssClass ) {
+            _.each( collection, function( item ) {
                 if( value.toString() === item.id.toString() ) {
-                    item.count = i;
-                    template = self._templateWorkflowInfoTable( item );
-                    $el.append( template );
-                    break;
+                    var $el = $( '.' + cssClass );
+                    $el.find( '.manage-table' ).remove();
+                    $el.append( self._templateWorkflowInfoTable( item ) );
                 }
-            }
+            });
         },
 
-        /* Open a url for creating new workflow */
-        new_workflow: function() {
-            window.location.href = Galaxy.root + 'workflow/create';
-        },
-
-        import_workflow: function() {
-            // TODO: Add logic for importing workflow
-        },
-
+        /** Template for workflow table */
         _templateWorkflowInfoTable: function( options ) {
             return '<table class="manage-table colored">' +
                     '<tr class="header">' +
@@ -122,10 +139,16 @@ define( [ 'mvc/form/form-view', 'mvc/ui/ui-misc', 'mvc/ui/ui-select' ], function
                 '</table>';
         },
        
+        /** Main template */
         _templateHeader: function( options ) {
-            return  '<div class="user-workflows">' +
+            return  '<div class="user-workflows wf">' +
                         '<div class="page-container">' +
                             '<h2>Your workflows</h2>' +
+                        '</div>' +
+                    '</div>'+
+                    '<div class="shared-workflows wf">' +
+                        '<div class="page-container">' +
+                            '<h2>Workflows shared with you by others</h2>' +
                         '</div>' +
                     '</div>';
         }
