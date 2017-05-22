9db1cc455d48e0b74e3db6b1f39b623297f9b453
mvdbeek <m.vandenbeek@gmail.com>
2017-05-22 21:40:57 +0200

    Fix the API tests for collection downloads
    by waiting for the datasets to become ready.

--- test/api/test_dataset_collections.py
+++ test/api/test_dataset_collections.py
@@ -99,17 +99,22 @@ class DatasetCollectionApiTestCase( api.ApiTestCase ):
         dataset_collection = self.dataset_collection_populator.create_list_in_history(self.history_id).json()
         returned_datasets = dataset_collection["elements"]
         assert len(returned_datasets) == 3, dataset_collection
+        for element in returned_datasets:
+            self.dataset_populator.wait_for_dataset(history_id=self.history_id, dataset_id=element['id'])
         create_response = self._download_dataset_collection(history_id=self.history_id, hdca_id=dataset_collection['id'])
         self._assert_status_code_is(create_response, 200)
         tar_contents = tarfile.open(fileobj=StringIO(create_response.content))
         namelist = tar_contents.getnames()
         assert len(namelist) == 3
         collection_name = dataset_collection['name']
-        for element, zip_path in zip(dataset_collection['elements'], namelist):
+        for element, zip_path in zip(returned_datasets, namelist):
             assert "%s/%s.%s" % (collection_name, element['element_identifier'], element['object']['file_ext']) == zip_path
 
     def test_pair_download(self):
         dataset_collection = self.dataset_collection_populator.create_pair_in_history(self.history_id).json()
+        returned_datasets = dataset_collection["elements"]
+        for element in returned_datasets:
+            self.dataset_populator.wait_for_dataset(history_id=self.history_id, dataset_id=element['id'])
         hdca_id = dataset_collection['id']
         create_response = self._download_dataset_collection(history_id=self.history_id, hdca_id=hdca_id)
         self._assert_status_code_is(create_response, 200)
@@ -117,20 +122,22 @@ class DatasetCollectionApiTestCase( api.ApiTestCase ):
         namelist = tar_contents.getnames()
         assert len(namelist) == 2
         collection_name = dataset_collection['name']
-        for element, zip_path in zip(dataset_collection['elements'], namelist):
+        for element, zip_path in zip(returned_datasets, namelist):
             assert "%s/%s.%s" % (collection_name, element['element_identifier'], element['object']['file_ext']) == zip_path
 
     def test_list_pair_download(self):
         dataset_collection = self.dataset_collection_populator.create_list_of_pairs_in_history(self.history_id).json()
         returned_datasets = dataset_collection["elements"]
         assert len(returned_datasets) == 1, dataset_collection
+        list_collection_name = dataset_collection['name']
+        pair = returned_datasets[0]
+        for element in pair['object']['elements']:
+            self.dataset_populator.wait_for_dataset(history_id=self.history_id, dataset_id=element['id'])
         create_response = self._download_dataset_collection(history_id=self.history_id, hdca_id=dataset_collection['id'])
         self._assert_status_code_is(create_response, 200)
         tar_contents = tarfile.open(fileobj=StringIO(create_response.content))
         namelist = tar_contents.getnames()
         assert len(namelist) == 2
-        list_collection_name = dataset_collection['name']
-        pair = dataset_collection['elements'][0]
         pair_collection_name = pair['element_identifier']
         for element, zip_path in zip(pair['object']['elements'], namelist):
             assert "%s/%s/%s.%s" % (list_collection_name, pair_collection_name, element['element_identifier'], element['object']['file_ext']) == zip_path
