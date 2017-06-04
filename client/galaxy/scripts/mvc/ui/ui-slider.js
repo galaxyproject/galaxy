6fe034f23dbe49ecddaf9dea571c401d7c41e2d1
guerler <aysam.guerler@gmail.com>
2017-06-04 12:39:50 -0400

    Add value to model

--- client/galaxy/scripts/mvc/ui/ui-slider.js
+++ client/galaxy/scripts/mvc/ui/ui-slider.js
@@ -8,7 +8,8 @@ define([ 'utils/utils' ], function( Utils ) {
                 max     : null,
                 step    : null,
                 precise : false,
-                split   : 10000
+                split   : 10000,
+                value   : null
             }).set( options );
 
             // create new element
@@ -17,24 +18,21 @@ define([ 'utils/utils' ], function( Utils ) {
             this.$slider        = this.$( '#slider' );
             this.$slider_text   = this.$( '.ui-form-slider-text' );
 
-            // set initial value
-            this.options.value !== undefined && ( this.value( this.options.value ) );
-
             // add text field event
             var pressed = [];
             this.$text.on( 'change', function () {
                 self.value( $( this ).val() );
             }).on( 'keyup', function( e ) {
                 pressed[e.which] = false;
-                self.options.onchange && self.options.onchange( $( this ).val() );
+                self.model.get( 'onchange' ) && self.model.get( 'onchange' )( $( this ).val() );
             }).on( 'keydown', function ( e ) {
                 var v = e.which;
                 pressed[ v ] = true;
-                if ( self.options.is_workflow && pressed[ 16 ] && v == 52 ) {
+                if ( self.model.get( 'is_workflow' ) && pressed[ 16 ] && v == 52 ) {
                     self.value( '$' )
                     event.preventDefault();
                 } else if (!( v == 8 || v == 9 || v == 13 || v == 37 || v == 39 || ( v >= 48 && v <= 57 && !pressed[ 16 ] ) || ( v >= 96 && v <= 105 )
-                    || ( ( v == 190 || v == 110 ) && $( this ).val().indexOf( '.' ) == -1 && self.options.precise )
+                    || ( ( v == 190 || v == 110 ) && $( this ).val().indexOf( '.' ) == -1 && self.model.get( 'precise' ) )
                     || ( ( v == 189 || v == 109 ) && $( this ).val().indexOf( '-' ) == -1 )
                     || self._isParameter( $( this ).val() )
                     || pressed[ 91 ] || pressed[ 17 ] ) ) {
@@ -42,6 +40,7 @@ define([ 'utils/utils' ], function( Utils ) {
                 }
             });
 
+            // add change listener
             this.listenTo( this.model, 'change', this.render, this );
             this.render();
         },
@@ -58,11 +57,11 @@ define([ 'utils/utils' ], function( Utils ) {
                 }
             }
             if ( useslider ) {
-                this.$slider.show();
-                this.$slider.slider( { min: options.min, max: options.max, step: step } );
-                this.$slider.on( 'slide', function ( event, ui ) {
-                    self.value( ui.value );
-                });
+                this.$slider.slider( { min: options.min, max: options.max, step: step } )
+                            .on( 'slide', function ( event, ui ) {
+                                self.value( ui.value );
+                            }).show();
+                this.$slide_text.css( 'width', 'auto' );
             } else {
                 this.$slider.hide();
                 this.$slide_text.css( 'width', '100%' );
@@ -71,22 +70,23 @@ define([ 'utils/utils' ], function( Utils ) {
 
         /** Set and Return the current value */
         value : function ( new_val ) {
+            var options = this.model.attributes;
             if ( new_val !== undefined ) {
                 if ( new_val !== null && new_val !== '' && !this._isParameter( new_val ) ) {
                     isNaN( new_val ) && ( new_val = 0 );
-                    this.options.max !== null && ( new_val = Math.min( new_val, this.options.max ) );
-                    this.options.min !== null && ( new_val = Math.max( new_val, this.options.min ) );
+                    options.max !== null && ( new_val = Math.min( new_val, options.max ) );
+                    options.min !== null && ( new_val = Math.max( new_val, options.min ) );
                 }
                 this.$slider && this.$slider.slider( 'value', new_val );
                 this.$text.val( new_val );
-                this.options.onchange && this.options.onchange( new_val );
+                options.onchange && options.onchange( new_val );
             }
             return this.$text.val();
         },
 
         /** Return true if the field contains a workflow parameter i.e. $('name') */
         _isParameter: function( value ) {
-            return this.options.is_workflow && String( value ).substring( 0, 1 ) === '$';
+            return this.model.get( 'is_workflow' ) && String( value ).substring( 0, 1 ) === '$';
         },
 
         /** Slider template */
