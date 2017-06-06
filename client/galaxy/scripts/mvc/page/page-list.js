26fdf8b8fd200af439f4ba8b2138d3209c617ba4
guerler <aysam.guerler@gmail.com>
2017-06-06 12:49:28 -0400

    Display only link for shared pages since viewing is the only operation

--- client/galaxy/scripts/mvc/page/page-list.js
+++ client/galaxy/scripts/mvc/page/page-list.js
@@ -30,21 +30,15 @@ define( [ 'utils/utils', 'mvc/grid/grid-view' ], function( Utils, GridView ) {
                                     '<tr class="header">' +
                                         '<th>Title</th>' +
                                         '<th>Owner</th>' +
-                                        '<th></th>' +
                                     '</tr>' +
                                 '</table>' );
                 _.each( options.shared_by_others, function( page, index ) {
                     var display_url = Galaxy.root + 'page/display_by_username_and_slug?username=' + page.username + '&slug=' + page.slug;
                     $table.append(  '<tr>' +
                                         '<td>' +
-                                            '<a class="menubutton" id="shared-' + index + '-popup" href="' + display_url + '">' + _.escape( page.title ) + '</a>' +
+                                            '<a href="' + display_url + '">' + _.escape( page.title ) + '</a>' +
                                         '</td>' +
                                         '<td>' + _.escape( page.username ) + '</td>' +
-                                        '<td>' +
-                                            '<div popupmenu="shared-' + index + '-popup">' +
-                                                '<a class="action-button" target="_top" href="' + display_url + '">View</a>' +
-                                            '</div>' +
-                                        '</td>' +
                                     '</tr>' );
                 });
                 $tmpl.append( $table );
@@ -53,29 +47,6 @@ define( [ 'utils/utils', 'mvc/grid/grid-view' ], function( Utils, GridView ) {
             }
             return $tmpl;
         }
-                    /*%for i, association in enumerate( shared_by_others ):
-                        <% page = association.page %>
-                        <tr>
-                            <td>
-                                <a class="menubutton" id="shared-${i}-popup" href="${h.url_for(controller='page', action='display_by_username_and_slug', username=page.user.username, slug=page.slug)}">${page.title | h}</a>
-                            </td>
-                            <td>${page.user.username}</td>
-                            <td>
-                                <div popupmenu="shared-${i}-popup">
-                                    <a class="action-button" href="${h.url_for(controller='page', action='display_by_username_and_slug', username=page.user.username, slug=page.slug)}" >View</a>
-                                </div>
-                            </td>
-                        </tr>    
-                    %endfor
-                </table>
-            %else:
-
-                No pages have been shared with you.
-
-            %endif*/
-
-
-
     });
 
     return {
--- static/scripts/bundled/analysis.bundled.js
+++ static/scripts/bundled/analysis.bundled.js
@@ -23640,10 +23640,10 @@ webpackJsonp([4,1],[
 	            $tmpl.append('<h2>Pages shared with you by others</h2>');
 	            var options = this.model.attributes;
 	            if (options.shared_by_others && options.shared_by_others.length > 0) {
-	                var $table = $('<table class="colored" border="0" cellspacing="0" cellpadding="0" width="100%">' + '<tr class="header">' + '<th>Title</th>' + '<th>Owner</th>' + '<th></th>' + '</tr>' + '</table>');
+	                var $table = $('<table class="colored" border="0" cellspacing="0" cellpadding="0" width="100%">' + '<tr class="header">' + '<th>Title</th>' + '<th>Owner</th>' + '</tr>' + '</table>');
 	                _.each(options.shared_by_others, function (page, index) {
 	                    var display_url = Galaxy.root + 'page/display_by_username_and_slug?username=' + page.username + '&slug=' + page.slug;
-	                    $table.append('<tr>' + '<td>' + '<a class="menubutton" id="shared-' + index + '-popup" href="' + display_url + '">' + _.escape(page.title) + '</a>' + '</td>' + '<td>' + _.escape(page.username) + '</td>' + '<td>' + '<div popupmenu="shared-' + index + '-popup">' + '<a class="action-button" target="_top" href="' + display_url + '">View</a>' + '</div>' + '</td>' + '</tr>');
+	                    $table.append('<tr>' + '<td>' + '<a href="' + display_url + '">' + _.escape(page.title) + '</a>' + '</td>' + '<td>' + _.escape(page.username) + '</td>' + '</tr>');
 	                });
 	                $tmpl.append($table);
 	            } else {
@@ -23651,25 +23651,6 @@ webpackJsonp([4,1],[
 	            }
 	            return $tmpl;
 	        }
-	        /*%for i, association in enumerate( shared_by_others ):
-	            <% page = association.page %>
-	            <tr>
-	                <td>
-	                    <a class="menubutton" id="shared-${i}-popup" href="${h.url_for(controller='page', action='display_by_username_and_slug', username=page.user.username, slug=page.slug)}">${page.title | h}</a>
-	                </td>
-	                <td>${page.user.username}</td>
-	                <td>
-	                    <div popupmenu="shared-${i}-popup">
-	                        <a class="action-button" href="${h.url_for(controller='page', action='display_by_username_and_slug', username=page.user.username, slug=page.slug)}" >View</a>
-	                    </div>
-	                </td>
-	            </tr>    
-	        %endfor
-	        </table>
-	        %else:
-	        No pages have been shared with you.
-	        %endif*/
-	
 	    });
 	
 	    return {
