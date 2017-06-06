aa85d00615edcc28b94e00ac95efe803d90ee009
guerler <aysam.guerler@gmail.com>
2017-06-06 12:43:17 -0400

    Add shared pages table

--- client/galaxy/scripts/mvc/page/page-list.js
+++ client/galaxy/scripts/mvc/page/page-list.js
@@ -16,9 +16,66 @@ define( [ 'utils/utils', 'mvc/grid/grid-view' ], function( Utils, GridView ) {
         },
 
         render: function() {
-            grid = new GridView( this.model.attributes );
+            var grid = new GridView( this.model.attributes );
             this.$el.empty().append( grid.$el );
+            this.$el.append( this._templateShared() );
+        },
+
+        _templateShared: function() {
+            var $tmpl = $( '<div/>' );
+            $tmpl.append( '<h2>Pages shared with you by others</h2>' );
+            var options = this.model.attributes;
+            if ( options.shared_by_others && options.shared_by_others.length > 0 ) {
+                var $table = $( '<table class="colored" border="0" cellspacing="0" cellpadding="0" width="100%">' +
+                                    '<tr class="header">' +
+                                        '<th>Title</th>' +
+                                        '<th>Owner</th>' +
+                                        '<th></th>' +
+                                    '</tr>' +
+                                '</table>' );
+                _.each( options.shared_by_others, function( page, index ) {
+                    var display_url = Galaxy.root + 'page/display_by_username_and_slug?username=' + page.username + '&slug=' + page.slug;
+                    $table.append(  '<tr>' +
+                                        '<td>' +
+                                            '<a class="menubutton" id="shared-' + index + '-popup" href="' + display_url + '">' + _.escape( page.title ) + '</a>' +
+                                        '</td>' +
+                                        '<td>' + _.escape( page.username ) + '</td>' +
+                                        '<td>' +
+                                            '<div popupmenu="shared-' + index + '-popup">' +
+                                                '<a class="action-button" target="_top" href="' + display_url + '">View</a>' +
+                                            '</div>' +
+                                        '</td>' +
+                                    '</tr>' );
+                });
+                $tmpl.append( $table );
+            } else {
+                $tmpl.append( 'No pages have been shared with you.' );
+            }
+            return $tmpl;
         }
+                    /*%for i, association in enumerate( shared_by_others ):
+                        <% page = association.page %>
+                        <tr>
+                            <td>
+                                <a class="menubutton" id="shared-${i}-popup" href="${h.url_for(controller='page', action='display_by_username_and_slug', username=page.user.username, slug=page.slug)}">${page.title | h}</a>
+                            </td>
+                            <td>${page.user.username}</td>
+                            <td>
+                                <div popupmenu="shared-${i}-popup">
+                                    <a class="action-button" href="${h.url_for(controller='page', action='display_by_username_and_slug', username=page.user.username, slug=page.slug)}" >View</a>
+                                </div>
+                            </td>
+                        </tr>    
+                    %endfor
+                </table>
+            %else:
+
+                No pages have been shared with you.
+
+            %endif*/
+
+
+
     });
 
     return {
--- static/scripts/bundled/analysis.bundled.js
+++ static/scripts/bundled/analysis.bundled.js
@@ -23612,7 +23612,7 @@ webpackJsonp([4,1],[
 /* 141 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/* WEBPACK VAR INJECTION */(function(Backbone, $) {/** This class renders the page list. */
+	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/* WEBPACK VAR INJECTION */(function(Backbone, $, _) {/** This class renders the page list. */
 	!(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__(14), __webpack_require__(138)], __WEBPACK_AMD_DEFINE_RESULT__ = function (Utils, GridView) {
 	    var View = Backbone.View.extend({
 	        initialize: function (options) {
@@ -23630,16 +23630,53 @@ webpackJsonp([4,1],[
 	        },
 	
 	        render: function () {
-	            grid = new GridView(this.model.attributes);
+	            var grid = new GridView(this.model.attributes);
 	            this.$el.empty().append(grid.$el);
+	            this.$el.append(this._templateShared());
+	        },
+	
+	        _templateShared: function () {
+	            var $tmpl = $('<div/>');
+	            $tmpl.append('<h2>Pages shared with you by others</h2>');
+	            var options = this.model.attributes;
+	            if (options.shared_by_others && options.shared_by_others.length > 0) {
+	                var $table = $('<table class="colored" border="0" cellspacing="0" cellpadding="0" width="100%">' + '<tr class="header">' + '<th>Title</th>' + '<th>Owner</th>' + '<th></th>' + '</tr>' + '</table>');
+	                _.each(options.shared_by_others, function (page, index) {
+	                    var display_url = Galaxy.root + 'page/display_by_username_and_slug?username=' + page.username + '&slug=' + page.slug;
+	                    $table.append('<tr>' + '<td>' + '<a class="menubutton" id="shared-' + index + '-popup" href="' + display_url + '">' + _.escape(page.title) + '</a>' + '</td>' + '<td>' + _.escape(page.username) + '</td>' + '<td>' + '<div popupmenu="shared-' + index + '-popup">' + '<a class="action-button" target="_top" href="' + display_url + '">View</a>' + '</div>' + '</td>' + '</tr>');
+	                });
+	                $tmpl.append($table);
+	            } else {
+	                $tmpl.append('No pages have been shared with you.');
+	            }
+	            return $tmpl;
 	        }
+	        /*%for i, association in enumerate( shared_by_others ):
+	            <% page = association.page %>
+	            <tr>
+	                <td>
+	                    <a class="menubutton" id="shared-${i}-popup" href="${h.url_for(controller='page', action='display_by_username_and_slug', username=page.user.username, slug=page.slug)}">${page.title | h}</a>
+	                </td>
+	                <td>${page.user.username}</td>
+	                <td>
+	                    <div popupmenu="shared-${i}-popup">
+	                        <a class="action-button" href="${h.url_for(controller='page', action='display_by_username_and_slug', username=page.user.username, slug=page.slug)}" >View</a>
+	                    </div>
+	                </td>
+	            </tr>    
+	        %endfor
+	        </table>
+	        %else:
+	        No pages have been shared with you.
+	        %endif*/
+	
 	    });
 	
 	    return {
 	        View: View
 	    };
 	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(4), __webpack_require__(2)))
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(4), __webpack_require__(2), __webpack_require__(1)))
 
 /***/ },
 /* 142 */
