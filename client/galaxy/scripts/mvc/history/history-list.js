d931df2ca9d107d07af1c97a3d5de326f8ef378b
guerler <aysam.guerler@gmail.com>
2017-06-07 14:34:35 -0400

    Reimplement copy operation

--- client/galaxy/scripts/mvc/history/history-list.js
+++ client/galaxy/scripts/mvc/history/history-list.js
@@ -1,93 +1,5 @@
-/** This class renders the page list. */
-define( [ 'utils/utils', 'mvc/grid/grid-view' ], function( Utils, GridView ) {
-
-    /*/ define a module that has:
-    // an hack for which to override the url based history copy function in the popupmenus
-    // and replace it with a dialog that uses the API instead
-    define( 'copy-dialog-hack', [
-        'mvc/history/history-model',
-        'mvc/history/copy-dialog'
-    ], function( mHistory, historyCopyDialog ){
-
-        // callbacks
-        function tellTheUserItFailed(){
-            // history failed to copy, put the relevant techy crap in the console and alert the user
-            console.error( arguments );
-            alert( "${_('History could not be fetched. Please contact an administrator')}" );
-        }
-        function refreshEverything(){
-            // history was copied
-            // if we're in a frame, check for the parent Galaxy and try to refresh the history
-            if( window.parent && window.parent.Galaxy && window.parent.Galaxy.currHistoryPanel ){
-                window.parent.Galaxy.currHistoryPanel.loadCurrentHistory();
-            }
-            // in any case reload the save history panel
-            window.location.reload( true );
-        }
-
-        // get the id from the dom somehow (the two doms (saved/shared) are different...)
-        function findHistoryId( $menuButton ){
-            var title = '${grid.title}';
-            if( title === 'Saved Histories' ){
-                var $link = $menuButton.children( '.menubutton-label' );
-                // TODO: ohdearlord. stahp.
-                return ( $link.attr( 'href' ).match( /id=(\w+)/ ) || [] )[1];
-            }
-            // Histories shared with you
-            var $label = $menuButton.children( 'label' );
-            return $label.attr( 'id' );
-        }
-
-        // for each popupmenu, (this == a popup activator button), remove the link and add a click function
-        // that fetches the history and shows a copy dialog for it
-        // pass in a fn for extracting the id from the dom and an (optional) object with dialog options
-        function copyDialogHack( i, historyIdFindFn, dialogOptions ){
-            dialogOptions = dialogOptions || {};
-            var $this = $( this ),
-                historyId = historyIdFindFn( this ),
-                menuOptions = $this.data( 'popupmenu' ).options,
-                copyOption  = menuOptions.filter( function( o ){ return o.html === 'Copy' })[0];
-
-            copyOption.href = 'javascript:void(0)';
-            copyOption.func = function copyOptionClicked( ev ){
-                ev.preventDefault();
-                var history = new mHistory.History({ id : historyId });
-                history.fetch()
-                    .fail( tellTheUserItFailed )
-                    .done( function(){
-                        historyCopyDialog( history, dialogOptions ).done( refreshEverything );
-                    });
-            }
-        }
-        return copyDialogHack;
-    });
-
-    function findHistoryId( menuButton ){
-                var $link = $( menuButton ).children( '.menubutton-label' );
-                // TODO: ohdearlord. stahp.
-                return ( $link.attr( 'href' ).match( /id=(\w+)/ ) || [] )[1];
-            }
-
-            // wait for page ready and set it all up, do it again when the grid refreshes
-            $(function(){
-                if( !gridView ){
-                    console.warn( 'no grid' );
-                    return;
-                }
-
-                function replaceCopyFunction(){
-                    gridView.$( '.popup.menubutton' ).each( function( i ){
-                        copyDialogHack.call( this, i, findHistoryId );
-                    });
-                }
-                replaceCopyFunction();
-
-                var originalInitGrid = gridView.init_grid;
-                gridView.init_grid = function __patched_init_grid( json ){
-                    originalInitGrid.call( gridView, json );
-                    replaceCopyFunction();
-                };
-            });*/
+/** This class renders the grid list. */
+define( [ 'utils/utils', 'mvc/grid/grid-view', 'mvc/history/history-model', 'mvc/history/copy-dialog' ], function( Utils, GridView, HistoryModel, historyCopyDialog ) {
 
     var View = Backbone.View.extend({
         initialize: function( options ) {
@@ -98,12 +10,9 @@ define( [ 'utils/utils', 'mvc/grid/grid-view' ], function( Utils, GridView ) {
                 url     : Galaxy.root + 'history/list',
                 success : function( response ) {
                     response[ 'dict_format' ] = true;
-                    window.console.log( response );
                     _.each( response[ 'operations' ], function( operation ) {
                         if ( operation.label == 'Copy' ) {
-                            operation.onclick = function( id ) {
-                                alert( id );
-                            }
+                            operation.onclick = function( id ) { self._showCopyDialog( id ) };
                         }
                     });
                     self.model.set( response );
@@ -115,6 +24,22 @@ define( [ 'utils/utils', 'mvc/grid/grid-view' ], function( Utils, GridView ) {
         render: function() {
             var grid = new GridView( this.model.attributes );
             this.$el.empty().append( grid.$el );
+        },
+
+        _showCopyDialog: function( id ) {
+            var history = new HistoryModel.History( { id : id } );
+            history.fetch()
+                   .fail( function() {
+                       alert( 'History could not be fetched. Please contact an administrator' );
+                   })
+                   .done( function(){
+                       historyCopyDialog( history, {} ).done( function() {
+                           if( window.parent && window.parent.Galaxy && window.parent.Galaxy.currHistoryPanel ) {
+                               window.parent.Galaxy.currHistoryPanel.loadCurrentHistory();
+                           }
+                           window.location.reload( true );
+                       } );
+                   });
         }
     });
 
