f83871d0707dea0256a5f409f285eec16f7bcef7
bernt@informatik.uni-leipzig.de <bernt@informatik.uni-leipzig.de>
2017-06-13 22:12:48 +0200

    moved assign_all_groups parameter to config

--- lib/galaxy/jobs/runners/drmaa.py
+++ lib/galaxy/jobs/runners/drmaa.py
@@ -364,7 +364,6 @@ class DRMAAJobRunner( AsynchronousJobRunner ):
         """
         command = shlex.split( external_runjob_script )
         command.extend( [ str(username), jobtemplate_filename ] )
-        command.append( "--assign_all_groups"  )
         log.info("Running command %s" % command)
         p = subprocess.Popen(command,
                              shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
