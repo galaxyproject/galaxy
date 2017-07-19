2671f2ae01a53d67c9f38f0cc77c12926b78055b
guerler <aysam.guerler@gmail.com>
2017-07-19 16:03:43 -0400

    Add shared base router

--- client/galaxy/scripts/apps/admin.js
+++ client/galaxy/scripts/apps/admin.js
@@ -1,10 +1,10 @@
 var jQuery = require( 'jquery' ),
     $ = jQuery,
     GalaxyApp = require( 'galaxy' ).GalaxyApp,
-    QUERY_STRING = require( 'utils/query-string-parsing' ),
     AdminPanel = require( './panels/admin-panel' ),
     GridView = require( 'mvc/grid/grid-view' ),
     Ui = require( 'mvc/ui/ui-misc' ),
+    Router = require( 'layout/router' ),
     Page = require( 'layout/page' );
 
 window.app = function app( options, bootstrapped ){
@@ -13,39 +13,7 @@ window.app = function app( options, bootstrapped ){
     Galaxy.params = Galaxy.config.params;
 
     /** Routes */
-    var Router = Backbone.Router.extend({
-        // TODO: not many client routes at this point - fill and remove from server.
-        // since we're at root here, this may be the last to be routed entirely on the client.
-        initialize : function( page, options ){
-            this.page = page;
-            this.options = options;
-        },
-
-        /** helper to push a new navigation state */
-        push: function( url, data ) {
-            data = data || {};
-            data.__identifer = Math.random().toString( 36 ).substr( 2 );
-            if ( !$.isEmptyObject( data ) ) {
-                url += url.indexOf( '?' ) == -1 ? '?' : '&';
-                url += $.param( data , true );
-            }
-            this.navigate( url, { 'trigger': true } );
-        },
-
-        /** override to parse query string into obj and send to each route */
-        execute: function( callback, args, name ){
-            Galaxy.debug( 'router execute:', callback, args, name );
-            var queryObj = QUERY_STRING.parse( args.pop() );
-            args.push( queryObj );
-            if( callback ){
-                if ( this.authenticate( args, name ) ) {
-                    callback.apply( this, args );
-                } else {
-                    this.page.display( new Ui.Message( { status: 'danger', message: 'You must be logged in as admin user to make this request.', persistent: true } ) );
-                }
-            }
-        },
-
+    var AdminRouter = Router.extend({
         routes: {
             '(/)admin(/)user' : 'show_user'
         },
@@ -63,7 +31,7 @@ window.app = function app( options, bootstrapped ){
         _.extend( options.config, { active_view : 'admin' } );
         Galaxy.page = new Page.View( _.extend( options, {
             Left    : AdminPanel,
-            Router  : Router
+            Router  : AdminRouter
         } ) );
 
         // start the router - which will call any of the routes above
