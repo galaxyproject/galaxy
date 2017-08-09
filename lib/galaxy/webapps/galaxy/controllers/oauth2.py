57f5a5f645345508f7aac856f7d34e0e3974003d
vahid <jalili.vahid@gmail.com>
2017-08-09 16:27:01 -0700

    Update OAuth2.0 controller.

--- lib/galaxy/webapps/galaxy/controllers/oauth2.py
+++ lib/galaxy/webapps/galaxy/controllers/oauth2.py
@@ -7,16 +7,17 @@ import logging
 log = logging.getLogger(__name__)
 from galaxy import web
 from galaxy.web.base.controller import BaseUIController
-from oauth2client import client
-import requests
-import hashlib
 
 class OAuth2( BaseUIController ):
 
     @web.expose
-    def authenticate(self, trans, **kwargs):
-        return
+    @web.require_login( "authenticate against Google identity provider" )
+    def google_authn(self, trans, **kwargs):
+        if trans.user is None:
+            # Only logged in users are allowed here.
+            return
+        return trans.response.send_redirect( web.url_for( trans.app.authnz_manager.authenticate( "Google", trans ) ) )
 
     @web.expose
-    def callback(self, trans, **kwargs):
-        return
+    def google_callback(self, trans, **kwargs):
+        trans.app.authnz_manager.callback( "Google", kwargs[ 'state' ], kwargs[ 'code' ], trans  )
