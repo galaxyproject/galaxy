64d1cd8cab341512c74a47bf92930c17f67bc72a
vahid <jalili.vahid@gmail.com>
2017-08-14 11:55:48 -0700

    Remove a `TODO` on Google callback check.

--- lib/galaxy/webapps/galaxy/controllers/oauth2.py
+++ lib/galaxy/webapps/galaxy/controllers/oauth2.py
@@ -19,7 +19,7 @@ class OAuth2( BaseUIController ):
         return trans.response.send_redirect( web.url_for( trans.app.authnz_manager.authenticate( "Google", trans ) ) )
 
     @web.expose
-    def google_callback(self, trans, **kwargs):  # TODO: to be tested.
+    def google_callback(self, trans, **kwargs):
         if trans.app.authnz_manager.callback( "Google", kwargs[ 'state' ], kwargs[ 'code' ], trans ) is False:
             # TODO: inform the user why he/she is being re-authenticated.
             self.google_authn( trans )
