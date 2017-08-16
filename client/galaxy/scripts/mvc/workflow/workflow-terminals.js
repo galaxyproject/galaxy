8950d4423ff7d02bcf95b417676b2ef1c311333a
Max Schaefer <max@semmle.com>
2017-08-16 15:23:42 +0100

    Move two variable definitions to combine them with their declarations.

--- client/galaxy/scripts/mvc/workflow/workflow-terminals.js
+++ client/galaxy/scripts/mvc/workflow/workflow-terminals.js
@@ -5,7 +5,39 @@ define(['mvc/workflow/workflow-globals'], function( Globals ) {
         this.rank = collectionType.split(":").length;
     }
 
-    var ANY_COLLECTION_TYPE_DESCRIPTION, NULL_COLLECTION_TYPE_DESCRIPTION;
+    var NULL_COLLECTION_TYPE_DESCRIPTION = {
+        isCollection: false,
+        canMatch: function( other ) { return false; },
+        canMapOver: function( other ) {
+            return false;
+        },
+        toString: function() {
+            return "NullCollectionType[]";
+        },
+        append: function( otherCollectionType ) {
+            return otherCollectionType;
+        },
+        equal: function( other ) {
+            return other === this;
+        }
+    };
+
+    var ANY_COLLECTION_TYPE_DESCRIPTION = {
+        isCollection: true,
+        canMatch: function( other ) { return NULL_COLLECTION_TYPE_DESCRIPTION !== other; },
+        canMapOver: function( other ) {
+            return false;
+        },
+        toString: function() {
+            return "AnyCollectionType[]";
+        },
+        append: function( otherCollectionType ) {
+            throw "Cannot append to ANY_COLLECTION_TYPE_DESCRIPTION";
+        },
+        equal: function( other ) {
+            return other === this;
+        }
+    };
 
     $.extend( CollectionTypeDescription.prototype, {
         append: function( otherCollectionTypeDescription ) {
@@ -55,40 +87,6 @@ define(['mvc/workflow/workflow-globals'], function( Globals ) {
             return str.indexOf(suffix, str.length - suffix.length) !== -1;
         }
     } );
-
-    NULL_COLLECTION_TYPE_DESCRIPTION = {
-        isCollection: false,
-        canMatch: function( other ) { return false; },
-        canMapOver: function( other ) {
-            return false;
-        },
-        toString: function() {
-            return "NullCollectionType[]";
-        },
-        append: function( otherCollectionType ) {
-            return otherCollectionType;
-        },
-        equal: function( other ) {
-            return other === this;
-        }
-    };
-
-    ANY_COLLECTION_TYPE_DESCRIPTION = {
-        isCollection: true,
-        canMatch: function( other ) { return NULL_COLLECTION_TYPE_DESCRIPTION !== other; },
-        canMapOver: function( other ) {
-            return false;
-        },
-        toString: function() {
-            return "AnyCollectionType[]";
-        },
-        append: function( otherCollectionType ) {
-            throw "Cannot append to ANY_COLLECTION_TYPE_DESCRIPTION";
-        },
-        equal: function( other ) {
-            return other === this;
-        }
-    };
 
     var TerminalMapping = Backbone.Model.extend( {
         initialize: function( attr ) {
