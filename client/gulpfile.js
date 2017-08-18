394e4bd07708e1eae7e567974779d9637d12c77d
Dannon Baker <dannon.baker@gmail.com>
2017-08-17 22:47:40 -0400

    Swap jquery from bower-installed to yarn, as a trial.

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,16 +1,21 @@
-var gulp = require('gulp');
-
-var babel = require('gulp-babel');
+var path = require('path');
+var fs = require('fs');
 var del = require('del');
+var _ = require('underscore');
+
+var gulp = require('gulp');
+var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var cached = require('gulp-cached');
 
 var paths = {
+    node_modules: './node_modules',
     scripts: ['galaxy/scripts/**/*.js',
               '!galaxy/scripts/apps/**/*.js',
                '!galaxy/scripts/libs/**/*.js'
     ],
+    lib_locs: {'jquery':['dist/jquery.js', 'jquery/jquery.js']},
     libs: ['galaxy/scripts/libs/**/*.js']
 };
 
@@ -22,6 +27,18 @@ gulp.task('scripts', function() {
     .pipe(uglify())
     .pipe(sourcemaps.write('../maps/'))
     .pipe(gulp.dest('../static/scripts/'));
+});
+
+gulp.task('stage-libs', function(){
+    _.each(_.keys(paths.lib_locs), function(lib){
+        var p1 = path.resolve(path.join(paths.node_modules, lib, paths.lib_locs[lib][0]));
+        var p2 = path.resolve(path.join('galaxy', 'scripts', 'libs', paths.lib_locs[lib][1]));
+        if (fs.existsSync(p1)) {
+            console.log(p1 + " -> " + p2);
+            del.sync(p2);
+            fs.createReadStream(p1).pipe(fs.createWriteStream(p2));
+        }
+    });
 });
 
 gulp.task('libs', function() {
