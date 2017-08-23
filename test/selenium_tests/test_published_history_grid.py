0e1cbc09f8a604c1b45127faf30170ba1b67bb1b
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2017-08-23 21:11:22 +0200

    add 'sort by name/owner' tests

--- test/selenium_tests/test_published_history_grid.py
+++ test/selenium_tests/test_published_history_grid.py
@@ -1,13 +1,17 @@
 import time
 
-import requests
+# import requests
 
 from .framework import SeleniumTestCase, selenium_test
 
+# Test case data
+USER1_EMAIL = 'test_user1@test.test'
+USER2_EMAIL = 'test_user2@test.test'
+HISTORY1_NAME = 'First'
+HISTORY2_NAME = 'Second'
+
 
 class HistoryGridTestCase(SeleniumTestCase):
-    _user1_email = None
-    _user2_email = None
 
     # def setUp(self):
     #     super(HistoryGridTestCase, self).setUp()
@@ -22,8 +26,8 @@ class HistoryGridTestCase(SeleniumTestCase):
     def test_history_grid_histories(self):
         self.setup_users_and_histories()
         self.navigate_to_published_histories_page()
-        histories = self.get_histories_names()
-        assert histories == ['Second', 'First']
+        histories = self.get_histories()
+        assert histories == [HISTORY2_NAME, HISTORY1_NAME]
 
     @selenium_test
     def test_history_grid_standard_search(self):
@@ -31,46 +35,49 @@ class HistoryGridTestCase(SeleniumTestCase):
 
         input_selector = '#input-free-text-search-filter'
         search_input = self.wait_for_selector(input_selector)
-        search_input.send_keys('first')
+        search_input.send_keys(HISTORY1_NAME)
         self.send_enter(search_input)
-        time.sleep(1)  # search delay
 
-        histories = self.get_histories_names()
-        assert histories == ['First']
+        histories = self.get_histories()
+        assert histories == [HISTORY1_NAME]
 
     # @selenium_test
     # def test_history_grid_advanced_search(self):
     #     pass
 
-    # @selenium_test
-    # def test_history_grid_sort_by_name(self):
-    #     pass
+    @selenium_test
+    def test_history_grid_sort_by_name(self):
+        self.navigate_to_published_histories_page()
+        sort_link = self.wait_for_selector('th#name-header > a')
+        sort_link.click()
+        histories = self.get_histories()
+        assert histories == [HISTORY1_NAME, HISTORY2_NAME]
 
-    # @selenium_test
-    # def test_history_grid_sort_by_owner(self):
-    #     pass
+    @selenium_test
+    def test_history_grid_sort_by_owner(self):
+        self.navigate_to_published_histories_page()
+        sort_link = self.wait_for_selector('th#username-header > a')
+        sort_link.click()
+        histories = self.get_histories()
+        assert histories == [HISTORY1_NAME, HISTORY2_NAME]
 
-    # @selenium_test
-    # def test_history_grid_sort_by_rating(self):
-    #     pass
+    def get_histories(self):
+        time.sleep(1)
 
-    def get_histories_names(self):
         names = []
         grid = self.wait_for_selector('#grid-table-body')
         for row in grid.find_elements_by_tag_name('tr'):
             cell = row.find_elements_by_tag_name('td')[0]  # Name
             names.append(cell.text)
+
         return names
 
     def setup_users_and_histories(self):
-        self._user1_email = self._get_random_email()
-        self.register(self._user1_email)
-        self.create_and_publish_history('First')
+        self.register(USER1_EMAIL)
+        self.create_and_publish_history(HISTORY1_NAME)
         self.logout_if_needed()
-
-        self._user2_email = self._get_random_email()
-        self.register(self._user2_email)
-        self.create_and_publish_history('Second')
+        self.register(USER2_EMAIL)
+        self.create_and_publish_history(HISTORY2_NAME)
 
     def create_and_publish_history(self, name):
         self.click_history_option('Create New')
