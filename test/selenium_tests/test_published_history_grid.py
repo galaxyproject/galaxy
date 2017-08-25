72195e14baa5e5f3131f2364858e71c4e4fb960c
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2017-08-25 22:51:58 +0200

    [WIP]: add advanced search tests

--- test/selenium_tests/test_published_history_grid.py
+++ test/selenium_tests/test_published_history_grid.py
@@ -9,12 +9,13 @@ USER1_EMAIL = 'test_user1@test.test'
 USER2_EMAIL = 'test_user2@test.test'
 HISTORY1_NAME = 'First'
 HISTORY2_NAME = 'Second'
+HISTORY3_NAME = 'Third'
+HISTORY1_TAGS = ['tag1', 'tag2']
+HISTORY2_TAGS = ['tag3']
+HISTORY3_ANNOT = 'some description'
 
 
 class HistoryGridTestCase(SeleniumTestCase):
-
-    # def setUp(self):
-    #     super(HistoryGridTestCase, self).setUp()
 
     # @selenium_test
     # def test_history_grid_accessible(self):
@@ -27,10 +28,10 @@ class HistoryGridTestCase(SeleniumTestCase):
         self.setup_users_and_histories()
         self.navigate_to_published_histories_page()
         histories = self.get_histories()
-        assert histories == [HISTORY2_NAME, HISTORY1_NAME]
+        assert histories == [HISTORY2_NAME, HISTORY3_NAME, HISTORY1_NAME]
 
     @selenium_test
-    def test_history_grid_standard_search(self):
+    def test_history_grid_search_standard(self):
         self.navigate_to_published_histories_page()
 
         input_selector = '#input-free-text-search-filter'
@@ -41,9 +42,44 @@ class HistoryGridTestCase(SeleniumTestCase):
         histories = self.get_histories()
         assert histories == [HISTORY1_NAME]
 
-    # @selenium_test
-    # def test_history_grid_advanced_search(self):
-    #     pass
+    @selenium_test
+    def test_history_grid_search_advanced(self):
+        self.navigate_to_published_histories_page()
+
+        advanced_search_selector = '#standard-search .advanced-search-toggle'
+        advanced_search_link = self.wait_for_selector(advanced_search_selector)
+        advanced_search_link.click()
+
+        name_filter_selector = '#input-name-filter'
+        annot_filter_selector = '#input-annotation-filter'
+        owner_filter_selector = '#input-username-filter'
+        tags_filter_selector = '#input-tags-filter'
+
+        # Search by name
+        self.set_filter(name_filter_selector, HISTORY1_NAME)
+        histories = self.get_histories()
+        assert histories == [HISTORY1_NAME]
+        self.unset_filter('name', HISTORY1_NAME)
+
+        # TODO: Search by annotation
+        # annotation = HISTORY3_ANNOT.split(' ')[0]
+        # self.set_filter(annot_filter_selector, annotation)
+        # histories = self.get_histories()
+        # assert histories == [HISTORY3_NAME]
+        # self.unset_filter('annotation', annotation)
+
+        # Search by owner
+        owner = USER2_EMAIL.split('@')[0]
+        self.set_filter(owner_filter_selector, owner)
+        histories = self.get_histories()
+        assert histories == [HISTORY2_NAME]
+        self.unset_filter('username', owner)
+
+        # Search by tags
+        self.set_filter(tags_filter_selector, HISTORY1_TAGS[0])
+        histories = self.get_histories()
+        assert histories == [HISTORY1_NAME]
+        self.unset_filter('tags', HISTORY1_TAGS[0])
 
     @selenium_test
     def test_history_grid_sort_by_name(self):
@@ -51,7 +87,7 @@ class HistoryGridTestCase(SeleniumTestCase):
         sort_link = self.wait_for_selector('th#name-header > a')
         sort_link.click()
         histories = self.get_histories()
-        assert histories == [HISTORY1_NAME, HISTORY2_NAME]
+        assert histories == [HISTORY1_NAME, HISTORY2_NAME, HISTORY3_NAME]
 
     @selenium_test
     def test_history_grid_sort_by_owner(self):
@@ -59,10 +95,10 @@ class HistoryGridTestCase(SeleniumTestCase):
         sort_link = self.wait_for_selector('th#username-header > a')
         sort_link.click()
         histories = self.get_histories()
-        assert histories == [HISTORY1_NAME, HISTORY2_NAME]
+        assert histories == [HISTORY1_NAME, HISTORY3_NAME, HISTORY2_NAME]
 
     def get_histories(self):
-        time.sleep(1)
+        time.sleep(1.5)
 
         names = []
         grid = self.wait_for_selector('#grid-table-body')
@@ -72,21 +108,89 @@ class HistoryGridTestCase(SeleniumTestCase):
 
         return names
 
+    def set_filter(self, selector, value):
+        filter_input = self.wait_for_selector(selector)
+        time.sleep(.5)
+        filter_input.click()
+        filter_input.send_keys(value)
+        self.send_enter(filter_input)
+
+    def unset_filter(self, filter_key, filter_value):
+        close_link_selector = 'a[filter_key="%s"][filter_val="%s"]' % \
+            (filter_key, filter_value)
+        close_link = self.wait_for_selector(close_link_selector)
+        close_link.click()
+        time.sleep(.5)
+
+    def set_tags(self, tags):
+        tag_icon_selector = self.test_data['historyPanel']['selectors'] \
+            ['history']['tagIcon']
+        tag_area_selector = self.test_data['historyPanel']['selectors'] \
+            ['history']['tagArea']
+
+        tag_area = self.driver.find_element_by_css_selector(tag_area_selector)
+        if not tag_area.is_displayed():
+            tag_icon = self.wait_for_selector(tag_icon_selector)
+            tag_icon.click()
+            time.sleep(.5)
+
+        tag_area_selector += ' .tags-input input'
+        tag_area = self.wait_for_selector(tag_area_selector)
+        tag_area.click()
+
+        for tag in tags:
+            tag_area.send_keys(tag)
+            self.send_enter(tag_area)
+            time.sleep(.5)
+
+    def set_annotation(self, annotation):
+        anno_icon_selector = self.test_data['historyPanel']['selectors'] \
+            ['history']['annoIcon']
+        anno_area_selector = self.test_data['historyPanel']['selectors'] \
+            ['history']['annoArea']
+
+        annon_icon = self.wait_for_selector(anno_icon_selector)
+        annon_icon.click()
+
+        annon_area = self.wait_for_selector(anno_area_selector)
+
+        # TODO: complete the function
+
+        # annon_area.click()
+
+        # anno_area_editable_selector = anno_area_selector + ' textarea'
+        # # anno_done_button_selector = anno_area_selector + ' button'
+        # annon_area_editable = self.wait_for_selector(
+        #     anno_area_editable_selector)
+        # # anno_done_button = self.wait_for_selector(anno_done_button_selector)
+
+        # annon_area_editable.click()
+        # annon_area_editable.send_keys('some annotation')
+        # # anno_done_button.click()
+
     def setup_users_and_histories(self):
         self.register(USER1_EMAIL)
-        self.create_and_publish_history(HISTORY1_NAME)
-        self.logout_if_needed()
-        self.register(USER2_EMAIL)
-        self.create_and_publish_history(HISTORY2_NAME)
+        self.create_history(HISTORY1_NAME)
+        self.set_tags(HISTORY1_TAGS)
+        self.publish_current_history()
 
-    def create_and_publish_history(self, name):
+        self.create_history(HISTORY3_NAME)
+        # self.set_annotation(HISTORY3_ANNOT)
+        self.publish_current_history()
+        self.logout_if_needed()
+
+        self.register(USER2_EMAIL)
+        self.create_history(HISTORY2_NAME)
+        self.set_tags(HISTORY2_TAGS)
+        self.publish_current_history()
+
+    def create_history(self, name):
         self.click_history_option('Create New')
 
         # Rename the history
         editable_text_input_element = self.click_to_rename_history()
         editable_text_input_element.send_keys(name)
         self.send_enter(editable_text_input_element)
-        self.publish_current_history()
 
     def publish_current_history(self):
         self.click_history_option('Share or Publish')
@@ -114,7 +218,6 @@ class HistoryGridTestCase(SeleniumTestCase):
 
     def click_to_rename_history(self):
         self.history_panel_name_element().click()
-        return self.wait_for_selector(self.edit_title_input_selector())
-
-    def edit_title_input_selector(self):
-        return self.test_data['historyPanel']['selectors']['history']['nameEditableTextInput']
+        edit_title_input_selector = self.test_data['historyPanel'] \
+            ['selectors']['history']['nameEditableTextInput']
+        return self.wait_for_selector(edit_title_input_selector)
