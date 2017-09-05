8c4716e71d600711021041fc31bc8b02972c24c4
mvdbeek <m.vandenbeek@gmail.com>
2017-09-05 19:30:01 +0200

    Use environment markers to select bz2file when python is < 3.3

--- lib/galaxy/dependencies/__init__.py
+++ lib/galaxy/dependencies/__init__.py
@@ -1,7 +1,6 @@
 """
 Determine what optional dependencies are needed.
 """
-import sys
 from os.path import dirname, join
 from xml.etree import ElementTree
 
@@ -112,9 +111,6 @@ class ConditionalDependencies(object):
 
     def check_kamaki(self):
         return 'pithos' in self.object_stores
-
-    def check_bz2file(self):
-        return sys.version < (3, 3)
 
 
 def optional(config_file):
