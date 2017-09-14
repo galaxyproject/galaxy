2c128b097a07057f4d5a6f9c622eab0f5dabcf3b
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2017-09-14 18:30:41 +0200

    add custom builds tests

---
+++ test/selenium_tests/test_custom_builds.py
@@ -0,0 +1,122 @@
+import time
+
+from .framework import (
+    retry_assertion_during_transitions,
+    selenium_test,
+    SeleniumTestCase,
+)
+
+
+class CustomBuildsTestcase(SeleniumTestCase):
+
+    def setUp(self):
+        super(CustomBuildsTestcase, self).setUp()
+        self.ensure_user()
+
+    @selenium_test
+    def test_build_add(self):
+        self.navigate_to_custom_builds_page()
+
+        # self.add_custom_build(self.build_name1, self.build_key1, 'fasta')
+        # self.assert_custom_builds_in_grid([self.build_name1])
+
+        self.add_custom_build(self.build_name2, self.build_key2, 'len_file_c_p')
+        self.assert_custom_builds_in_grid([self.build_name2])
+
+    @selenium_test
+    def test_build_delete(self):
+        self.navigate_to_custom_builds_page()
+
+        self.add_custom_build(self.build_name3, self.build_key3, 'len_file_c_p')
+        self.assert_custom_builds_in_grid([self.build_name3])
+
+        self.delete_custom_build(self.build_name3)
+        self.assert_custom_builds_in_grid([self.build_name3], False)
+
+    @retry_assertion_during_transitions
+    def assert_custom_builds_in_grid(self, expected_builds, present=True):
+        actual_builds = self.get_custom_builds()
+        intersection = set(actual_builds).intersection(expected_builds)
+        if present:
+            self.assertEqual(intersection, set(expected_builds))
+        else:
+            self.assertEqual(intersection, set())
+
+    def add_custom_build(self, build_name, build_key, definition):
+        name_div = self.wait_for_selector('div[tour_id="name"')
+        name_input = name_div.find_element_by_css_selector('.ui-form-field input')
+        name_input.click()
+        name_input.send_keys(build_name)
+
+        key_div = self.wait_for_selector('div[tour_id="id"')
+        key_input = key_div.find_element_by_css_selector('.ui-form-field input')
+        key_input.click()
+        key_input.send_keys(build_key)
+
+        len_type_div = self.wait_for_selector('div[tour_id="len|type')
+        len_type_select = len_type_div.find_element_by_css_selector('.ui-form-field .ui-select')
+        len_type_select.click()
+
+        if definition == 'fasta':
+            option = self.wait_for_sizzle_selector_clickable('div[role="option"]:contains("FASTA-file from history")')
+            option.click()
+
+        elif definition == 'len_file_c_p':
+            option = self.wait_for_sizzle_selector_clickable('div[role="option"]:contains("Len-file by copy/paste")')
+            option.click()
+            content_area = self.wait_for_and_click_selector('.ui-form-field > textarea')
+            content_area.send_keys('content')
+
+        self.wait_for_and_click_selector('button#save')
+
+    def delete_custom_build(self, build_name):
+        delete_button = None
+        grid = self.wait_for_selector('table.grid > tbody')
+        for row in grid.find_elements_by_tag_name('tr'):
+            td = row.find_elements_by_tag_name('td')
+            name = td[1].text  # Bug in Galaxy, replace to td[0] after the fix
+            if name == build_name:
+                delete_button = td[3].find_element_by_css_selector('.ui-button-icon-plain')
+                break
+
+        if delete_button is None:
+            raise AssertionError('Failed to find custom build with name [%s]' % build_name)
+
+        delete_button.click()
+
+    def get_custom_builds(self):
+        time.sleep(1)
+        builds = []
+        grid = self.wait_for_selector('table.grid > tbody')
+        for row in grid.find_elements_by_tag_name('tr'):
+            td = row.find_elements_by_tag_name('td')
+            name = td[1].text  # Bug in Galaxy, replace to td[0] after the fix
+            builds.append(name)
+        return builds
+
+    def navigate_to_custom_builds_page(self):
+        self.home()
+        self.click_masthead_user()  # Open masthead menu
+        label = self.navigation_data['labels']['masthead']['menus']['user']
+        self.click_label(label)
+        self.wait_for_and_click_selector('a[href="/custom_builds"]')
+
+    def ensure_user(self):
+        if getattr(CustomBuildsTestcase, 'user_email', None):
+            self.home()  # ensure Galaxy is loaded
+            self.submit_login(self.user_email)
+            return
+
+        CustomBuildsTestcase.user_email = self._get_random_email()
+        self.register(self.user_email)
+
+        CustomBuildsTestcase.build_name1 = self._get_random_name()
+        CustomBuildsTestcase.build_name2 = self._get_random_name()
+        CustomBuildsTestcase.build_name3 = self._get_random_name()
+        CustomBuildsTestcase.build_key1 = self._get_random_name(len=5)
+        CustomBuildsTestcase.build_key2 = self._get_random_name(len=5)
+        CustomBuildsTestcase.build_key3 = self._get_random_name(len=5)
+
+        # Upload a FASTA file
+        # self.perform_upload(self.get_filename('2.fasta'))
+        # self.history_panel_wait_for_hid_ok(1)
