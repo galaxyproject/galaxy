2669ab022ed2f90faac54c510014f87783efeaa0
Nate Coraor <nate@bx.psu.edu>
2017-09-20 14:52:24 -0400

    Pass job output file unqualified names to Pulsar so that it can create them before running the job.

--- lib/galaxy/jobs/__init__.py
+++ lib/galaxy/jobs/__init__.py
@@ -1571,6 +1571,9 @@ class JobWrapper(object, HasResourceParameters):
                     paths.append(DatasetPath(da.id, real_path=real_path, false_path=false_path, mutable=False))
         return paths
 
+    def get_output_basenames(self):
+        return map(os.path.basename, map(str, self.get_output_fnames()))
+
     def get_output_fnames(self):
         if self.output_paths is None:
             self.compute_outputs()
@@ -1998,6 +2001,10 @@ class ComputeEnvironment(object):
     """
 
     @abstractmethod
+    def output_names(self):
+        """ Output unqualified filenames defined by job. """
+
+    @abstractmethod
     def output_paths(self):
         """ Output DatasetPaths defined by job. """
 
@@ -2061,6 +2068,9 @@ class SharedComputeEnvironment(SimpleComputeEnvironment):
         self.app = job_wrapper.app
         self.job_wrapper = job_wrapper
         self.job = job
+
+    def output_names(self):
+        return self.job_wrapper.get_output_basenames()
 
     def output_paths(self):
         return self.job_wrapper.get_output_fnames()
