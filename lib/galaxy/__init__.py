1bc56442859fa5e5ef0d998f63169427e1bcad15
Dannon Baker <dannon.baker@gmail.com>
2017-09-21 14:38:19 -0400

    Add a custom "TRACE" log level for even more verbosity. Import logging as usual and within the galaxy namespace you should be able to use it with log.trace().

--- lib/galaxy/__init__.py
+++ lib/galaxy/__init__.py
@@ -18,3 +18,16 @@ if not hasattr(logging, 'NullHandler'):
         def emit(self, record):
             pass
     logging.NullHandler = NullHandler
+
+
+# Add custom "TRACE" log level for ludicrous verbosity.
+LOGLV_TRACE = 5
+logging.addLevelName(LOGLV_TRACE, "TRACE")
+
+
+def trace(self, message, *args, **kws):
+    if self.isEnabledFor(LOGLV_TRACE):
+        self._log(LOGLV_TRACE, message, args, **kws)
+
+
+logging.Logger.trace = trace
