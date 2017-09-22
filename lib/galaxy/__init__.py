75e5aa12e487fe4015d552bfd4c8a5a35550fd5a
Dannon Baker <dannon.baker@gmail.com>
2017-09-22 09:02:56 -0400

    Remove BadZipFile patch, Python 2.6 is no longer supported

--- lib/galaxy/__init__.py
+++ lib/galaxy/__init__.py
@@ -4,9 +4,3 @@ Galaxy root package -- this is a namespace package.
 
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
-
-
-# compat: BadZipFile introduced in Python 2.7
-import zipfile
-if not hasattr(zipfile, 'BadZipFile'):
-    zipfile.BadZipFile = zipfile.error
