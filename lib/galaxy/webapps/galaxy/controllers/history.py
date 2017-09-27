d61db29f29f99598577467a90690ce3deef09844
guerler <aysam.guerler@gmail.com>
2017-09-27 14:23:58 -0400

    Disable multi operation for dataset permissions view

--- lib/galaxy/webapps/galaxy/controllers/history.py
+++ lib/galaxy/webapps/galaxy/controllers/history.py
@@ -109,7 +109,7 @@ class HistoryListGrid(grids.Grid):
         grids.GridOperation("Switch", allow_multiple=False, condition=(lambda item: not item.deleted), async_compatible=True),
         grids.GridOperation("View", allow_multiple=False, url_args=dict(action='view')),
         grids.GridOperation("Share or Publish", allow_multiple=False, condition=(lambda item: not item.deleted), url_args=dict(action='sharing')),
-        grids.GridOperation("Change Permissions", condition=(lambda item: not item.deleted), url_args=dict(controller="", action="histories/permissions")),
+        grids.GridOperation("Change Permissions", allow_multiple=False, condition=(lambda item: not item.deleted), url_args=dict(controller="", action="histories/permissions")),
         grids.GridOperation("Copy", allow_multiple=False, condition=(lambda item: not item.deleted), async_compatible=False),
         grids.GridOperation("Rename", condition=(lambda item: not item.deleted), url_args=dict(controller="", action="histories/rename"), target="top"),
         grids.GridOperation("Delete", condition=(lambda item: not item.deleted), async_compatible=True),
