deb282c7b421e785917e753b70cd0fd14afadcbf
Dannon Baker <dannon.baker@gmail.com>
2017-10-13 12:02:50 -0400

    webpack config formatting

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,107 +1,104 @@
-var webpack = require( 'webpack' ),
+var webpack = require("webpack"),
     // paths
-    path = require( 'path' ),
-    scriptsBase = path.join( __dirname, 'galaxy/scripts' ),
-    libsBase = path.join( scriptsBase, 'libs' ),
-
+    path = require("path"),
+    scriptsBase = path.join(__dirname, "galaxy/scripts"),
+    libsBase = path.join(scriptsBase, "libs"),
     // libraries used on almost every page
     // TODO: reduce
     commonLibs = [
-        'polyfills',
+        "polyfills",
         // jquery et al
-        'jquery',
-        'libs/jquery/jquery.migrate',
+        "jquery",
+        "libs/jquery/jquery.migrate",
         // jquery plugins
-        'libs/jquery/jquery.autocomplete',
-        'libs/jquery/jquery.event.hover',
-        'libs/jquery/jquery.event.drag',
-        'libs/jquery/jquery.mousewheel',
-        'libs/jquery/jquery.form',
-        'libs/jquery/jquery.rating',
-        'libs/jquery/select2',
-        'libs/jquery.sparklines',
-        'libs/jquery/jquery-ui',
-        'libs/jquery/jstorage',
-        'libs/jquery/jquery.complexify',
-        'libs/farbtastic',
-        'libs/bootstrap',
-        'libs/bootstrap-tour',
+        "libs/jquery/jquery.autocomplete",
+        "libs/jquery/jquery.event.hover",
+        "libs/jquery/jquery.event.drag",
+        "libs/jquery/jquery.mousewheel",
+        "libs/jquery/jquery.form",
+        "libs/jquery/jquery.rating",
+        "libs/jquery/select2",
+        "libs/jquery.sparklines",
+        "libs/jquery/jquery-ui",
+        "libs/jquery/jstorage",
+        "libs/jquery/jquery.complexify",
+        "libs/farbtastic",
+        "libs/bootstrap",
+        "libs/bootstrap-tour",
         // mvc
-        'libs/underscore',
-        'libs/backbone',
+        "libs/underscore",
+        "libs/backbone",
         // all pages get these
-        'ui/autocom_tagging',
-        'layout/modal',
-        'layout/panel',
-        'onload',
+        "ui/autocom_tagging",
+        "layout/modal",
+        "layout/panel",
+        "onload"
     ];
 
-
 module.exports = {
-    devtool : 'source-map',
-    entry   : {
-        libs    : commonLibs,
-        login   : './galaxy/scripts/apps/login.js',
-        analysis: './galaxy/scripts/apps/analysis.js',
-        admin   : './galaxy/scripts/apps/admin.js',
+    devtool: "source-map",
+    entry: {
+        libs: commonLibs,
+        login: "./galaxy/scripts/apps/login.js",
+        analysis: "./galaxy/scripts/apps/analysis.js",
+        admin: "./galaxy/scripts/apps/admin.js"
     },
-    output  : {
-        path        : path.join(__dirname, '../', 'static/scripts/bundled'),
-        filename    : '[name].bundled.js'
+    output: {
+        path: path.join(__dirname, "../", "static/scripts/bundled"),
+        filename: "[name].bundled.js"
     },
-    resolve : {
-        modules: [
-            scriptsBase,
-        ],
-        alias : {
+    resolve: {
+        modules: [scriptsBase],
+        alias: {
             //TODO: correct our imports and remove these rules
             // Backbone looks for these in the same root directory
-            jquery      : path.join( libsBase, 'jquery/jquery' ),
-            underscore  : path.join( libsBase, 'underscore.js' ),
+            jquery: path.join(libsBase, "jquery/jquery"),
+            underscore: path.join(libsBase, "underscore.js")
         }
     },
-    module : {
+    module: {
         rules: [
-            { test: /\.js$/,
-              exclude: [
-                  /(node_modules|bower_components)/,
-                  libsBase
-              ],
-              loader: 'babel-loader'
+            {
+                test: /\.js$/,
+                exclude: [/(node_modules|bower_components)/, libsBase],
+                loader: "babel-loader"
             },
-            { test: require.resolve('jquery'),
-              use: [{
-                  loader: 'expose-loader',
-                  options: 'jQuery'
-              },{
-                  loader: 'expose-loader',
-                  options: '$'
-              }]
+            {
+                test: require.resolve("jquery"),
+                use: [
+                    {
+                        loader: "expose-loader",
+                        options: "jQuery"
+                    },
+                    {
+                        loader: "expose-loader",
+                        options: "$"
+                    }
+                ]
             }
-        ],
+        ]
     },
-    resolveLoader : {
-        alias : {
+    resolveLoader: {
+        alias: {
             // since we support both requirejs i18n and non-requirejs and both use a similar syntax,
             // use an alias so we can just use one file
-            i18n : 'amdi18n-loader'
+            i18n: "amdi18n-loader"
         }
     },
-    plugins : [
+    plugins: [
         new webpack.optimize.CommonsChunkPlugin({
-            name: 'libs',
-            filename: 'libs.bundled.js'
+            name: "libs",
+            filename: "libs.bundled.js"
         }),
         // this plugin allows using the following keys/globals in scripts (w/o req'ing them first)
         // and webpack will automagically require them in the bundle for you
         new webpack.ProvidePlugin({
-            $:                  'jquery',
-            jQuery:             'jquery',
-            'window.jQuery':    'jquery',
-            _:                  "underscore",
-            Backbone:           'libs/backbone',
-
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+            _: "underscore",
+            Backbone: "libs/backbone"
         })
         // new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 })
-    ],
+    ]
 };
