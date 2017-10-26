d01802c314fa084fd672d70b3bf8186e7cac655a
Dannon Baker <dannon.baker@gmail.com>
2017-10-26 16:20:39 -0400

    Don't mangle the i18n plugin and localization -- require w/ these plugins is a fickle beast.

--- static/scripts/utils/localization.js
+++ static/scripts/utils/localization.js
@@ -1,2 +1,2 @@
-define("utils/localization",[],function(){"use strict";define(["i18n!nls/locale"],function(o){if(o.hasOwnProperty("__root")){var n="undefined"==typeof navigator?"__root":(navigator.language||navigator.userLanguage||"__root").toLowerCase();o=o["__"+n]||o["__"+n.split("-")[0]]||o.__root}var t=function(n){return o[n]||n};return t.cacheNonLocalized=!1,t})});
+define(["i18n!nls/locale"],function(o){if(o.hasOwnProperty("__root")){var n="undefined"==typeof navigator?"__root":(navigator.language||navigator.userLanguage||"__root").toLowerCase();o=o["__"+n]||o["__"+n.split("-")[0]]||o.__root}var a=function(n){return o[n]||n};return a.cacheNonLocalized=!1,a});
 //# sourceMappingURL=../../maps/utils/localization.js.map
