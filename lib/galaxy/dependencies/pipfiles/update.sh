7b2df5a881d1cb78d7b5411754cca31865c36086
John Chilton <jmchilton@gmail.com>
2017-11-02 07:47:23 -0400

    Rearrange dependency update cd's as requested by @nsoranzo.

--- lib/galaxy/dependencies/pipfiles/update.sh
+++ lib/galaxy/dependencies/pipfiles/update.sh
@@ -30,24 +30,22 @@ while getopts ":hc" opt; do
 done
 
 THIS_DIRECTORY="$(cd "$(dirname "$0")" > /dev/null && pwd)"
-cd "$THIS_DIRECTORY"
 ENVS="develop
 flake8
 flake8_imports"
 
 for env in $ENVS
 do
-        cd "$env"
+        cd "$THIS_DIRECTORY/$env"
         pipenv lock
         pipenv lock -r > pinned-hashed-requirements.txt
         # Strip out hashes and trailing whitespace for unhashed version
         # of this requirements file.
         sed 's/--hash[^[:space:]]*//g' pinned-hashed-requirements.txt | sed 's/[[:space:]]*$//' > pinned-requirements.txt
-        cd ".."
 done
 
 if [ "$commit" -eq "1" ];
 then
-	git add -u .
+	git add -u "$THIS_DIRECTORY"
 	git commit -m "Rev and re-lock Galaxy dependencies."
 fi
