ad26832461bb27aced41f0530ba5f528412b576a
Dannon Baker <dannon.baker@gmail.com>
2017-11-10 12:52:38 -0500

    More cleanup.

--- client/galaxy/scripts/components/user_api_keys.vue
+++ client/galaxy/scripts/components/user_api_keys.vue
@@ -1,32 +1,32 @@
 <template>
-<div id="form-userkeys" class="toolForm" v-cloak>
-    <div class="toolFormTitle">User Information</div>
-    <div v-if="users && users.length > 0">
-        <table class="grid">
-            <thead><th>Encoded UID</th><th>Email</th><th>API Key</th><th>Actions</th></thead>
-            <tbody>
-                <tr v-for="user in users">
-                    <td>
-                        {{ user.uid }}
-                    </td>
-                    <td>
-                        {{ user.email }}
-                    </td>
-                    <td>
-                        {{ user.key }}
-                    </td>
-                    <td>
-                        <input type="button" value="Generate a new key now" v-on:click="generateKey( user.uid )" />
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <div id="form-userkeys" class="toolForm" v-cloak>
+        <div class="toolFormTitle">User Information</div>
+        <div v-if="users && users.length > 0">
+            <table class="grid">
+                <thead><th>Encoded UID</th><th>Email</th><th>API Key</th><th>Actions</th></thead>
+                <tbody>
+                    <tr v-for="user in users">
+                        <td>
+                            {{ user.uid }}
+                        </td>
+                        <td>
+                            {{ user.email }}
+                        </td>
+                        <td>
+                            {{ user.key }}
+                        </td>
+                        <td>
+                            <input type="button" value="Generate a new key now" v-on:click="generateKey( user.uid )" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div v-else>
+            <div>No user information available</div>
+        </div>
+        <div style="clear: both"></div>
     </div>
-    <div v-else>
-        <div>No user information available</div>
-    </div>
-    <div style="clear: both"></div>
-</div>
 </template>
 
 <script>
