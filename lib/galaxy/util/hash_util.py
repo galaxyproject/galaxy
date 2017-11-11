b6a0a1b084fe09de394aa6d0fb3caabc9fdf98a3
mvdbeek <m.vandenbeek@gmail.com>
2017-11-11 10:15:39 +0100

    Tolerate IOError in tool and data table watcher
    Without these changes the watcher thread may die when manually deleting loc files:
    ```
    Exception in thread Thread-7:
    Traceback (most recent call last):
      File "/usr/local/Cellar/python/2.7.13_1/Frameworks/Python.framework/Versions/2.7/lib/python2.7/threading.py", line 801, in __bootstrap_inner
        self.run()
      File "/Users/mvandenb/src/galaxy/.venv/lib/python2.7/site-packages/watchdog/observers/api.py", line 199, in run
        self.dispatch_events(self.event_queue, self.timeout)
      File "/Users/mvandenb/src/galaxy/.venv/lib/python2.7/site-packages/watchdog/observers/api.py", line 368, in dispatch_events
        handler.dispatch(event)
      File "/Users/mvandenb/src/galaxy/.venv/lib/python2.7/site-packages/watchdog/events.py", line 322, in dispatch
        self.on_any_event(event)
      File "/Users/mvandenb/src/galaxy/lib/galaxy/tools/toolbox/watcher.py", line 226, in on_any_event
        self._handle(event)
      File "/Users/mvandenb/src/galaxy/lib/galaxy/tools/toolbox/watcher.py", line 235, in _handle
        cur_hash = md5_hash_file(path)
      File "/Users/mvandenb/src/galaxy/lib/galaxy/util/hash_util.py", line 23, in md5_hash_file
        with open(path, 'rb') as afile:
    IOError: [Errno 2] No such file or directory: '/Users/mvandenb/src/galaxy/tool-data/toolshed.g2.bx.psu.edu/repos/lparsons/htseq_count/620d5603d1a8/sam_fa_indices.loc'
    ```
    Similar problems could occur when deleting tool xml files.

--- lib/galaxy/util/hash_util.py
+++ lib/galaxy/util/hash_util.py
@@ -15,13 +15,17 @@ md5 = hashlib.md5
 
 def md5_hash_file(path):
     """
-    Return a md5 hashdigest for a file.
+    Return a md5 hashdigest for a file or None if path could not be read.
     """
     hasher = hashlib.md5()
-    with open(path, 'rb') as afile:
-        buf = afile.read()
-        hasher.update(buf)
-        return hasher.hexdigest()
+    try:
+        with open(path, 'rb') as afile:
+            buf = afile.read()
+            hasher.update(buf)
+            return hasher.hexdigest()
+    except IOError:
+        # This may happen if path has been deleted
+        return None
 
 
 def new_secure_hash( text_type=None ):
