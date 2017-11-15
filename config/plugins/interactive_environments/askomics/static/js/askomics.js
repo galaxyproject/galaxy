7d674e1de0d23b48d3f6a8cf0fc4c5a2d883d9e7
Dannon Baker <dannon.baker@gmail.com>
2017-11-15 09:40:52 -0500

    Left off semicolon.

--- config/plugins/interactive_environments/askomics/static/js/askomics.js
+++ config/plugins/interactive_environments/askomics/static/js/askomics.js
@@ -3,7 +3,7 @@ var IES = window.IES;
 function load_askomics(url){
     $( document ).ready(function() {
         IES.test_ie_availability(url, function(){
-            IES.append_notebook(url)
+            IES.append_notebook(url);
         });
     });
 }
