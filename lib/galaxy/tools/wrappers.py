197333857c6ce1af61b4b9ad148d465600074e8b
John Chilton <jmchilton@gmail.com>
2017-11-20 12:24:27 -0500

    Bug fix - expose proper element_identifier in tool action template expansions.
    xref https://github.com/galaxyproject/tools-iuc/commit/f57f1bf4816f212b83c08e0e3c3782ebba8b7d9a

--- lib/galaxy/tools/wrappers.py
+++ lib/galaxy/tools/wrappers.py
@@ -430,3 +430,21 @@ class DatasetCollectionWrapper(ToolParameterValueWrapper, HasDatasets):
         # not specified or if resulting collection is empty.
         return self.__input_supplied and bool(self.__element_instance_list)
     __nonzero__ = __bool__
+
+
+class ElementIdentifierMapper(object):
+    """Track mapping of dataset collection elements datasets to element identifiers."""
+
+    def __init__(self, input_datasets=None):
+        if input_datasets is not None:
+            self.identifier_key_dict = dict((v, "%s|__identifier__" % k) for k, v in input_datasets.items())
+        else:
+            self.identifier_key_dict = {}
+
+    def identifier(self, dataset_value, input_values):
+        identifier_key = self.identifier_key_dict.get(dataset_value, None)
+        element_identifier = None
+        if identifier_key:
+            element_identifier = input_values.get(identifier_key, None)
+
+        return element_identifier
