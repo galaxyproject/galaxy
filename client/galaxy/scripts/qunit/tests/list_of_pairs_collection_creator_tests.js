930ac1ab796a7d4414d6c573c752d9dcc07f4de2
John Chilton <jmchilton@gmail.com>
2017-11-24 23:16:35 -0500

    Convert qunit tests to ES6.
    ... and make client-format them.

--- client/galaxy/scripts/qunit/tests/list_of_pairs_collection_creator_tests.js
+++ client/galaxy/scripts/qunit/tests/list_of_pairs_collection_creator_tests.js
@@ -1,108 +1,101 @@
 /* global define */
-define(
-    [
-        "qunit/test-app",
-        "mvc/collection/list-of-pairs-collection-creator",
-        "qunit/test-data/paired-collection-creator.data",
-        "jquery",
-        "sinon"
-    ],
-    function(testApp, PAIRED_COLLECTION_CREATOR, DATA, $, sinon) {
-        "use strict";
-        PAIRED_COLLECTION_CREATOR = PAIRED_COLLECTION_CREATOR.default;
-        var PCC = PAIRED_COLLECTION_CREATOR.PairedCollectionCreator;
+import testApp from "qunit/test-app";
+import PAIRED_COLLECTION_CREATOR from "mvc/collection/list-of-pairs-collection-creator";
+import DATA from "qunit/test-data/paired-collection-creator.data";
+import $ from "jquery";
+import sinon from "sinon";
 
-        QUnit.module("Galaxy client app tests");
+var PCC = PAIRED_COLLECTION_CREATOR.PairedCollectionCreator;
 
-        QUnit.test("Collection creation", function(assert) {
-            var pcc = new PCC({
-                    datasets: DATA._1,
-                    historyId: "fakeHistoryId"
-                }),
-                server = sinon.fakeServer.create();
+QUnit.module("Galaxy client app tests");
 
-            var requestJSON;
-            server.respondWith("POST", "/api/histories/fakeHistoryId/contents/dataset_collections", function(request) {
-                requestJSON = JSON.parse(request.requestBody);
-                request.respond(
-                    200,
-                    { "Content-Type": "application/json" },
-                    JSON.stringify({
-                        fakeResponse: "yes"
-                    })
-                );
-            });
+QUnit.test("Collection creation", function(assert) {
+    var pcc = new PCC({
+            datasets: DATA._1,
+            historyId: "fakeHistoryId"
+        }),
+        server = sinon.fakeServer.create();
 
-            //console.debug( 'requestBody:', JSON.stringify( requestJSON, null, '  ' ) );
-            pcc.createList("Heres a collection");
-            server.respond();
-            assert.deepEqual(requestJSON, DATA._1requestJSON);
-            server.restore();
-        });
+    var requestJSON;
+    server.respondWith("POST", "/api/histories/fakeHistoryId/contents/dataset_collections", function(request) {
+        requestJSON = JSON.parse(request.requestBody);
+        request.respond(
+            200,
+            { "Content-Type": "application/json" },
+            JSON.stringify({
+                fakeResponse: "yes"
+            })
+        );
+    });
 
-        QUnit.test("Creator base/empty construction/initializiation defaults", function(assert) {
-            var pcc = new PCC([]);
-            assert.ok(pcc instanceof PCC);
-            assert.deepEqual(pcc.filters, pcc.commonFilters[pcc.DEFAULT_FILTERS]);
-            assert.ok(pcc.automaticallyPair);
-            assert.equal(pcc.matchPercentage, 0.9);
-            assert.equal(pcc.strategy, "autopairLCS");
-        });
+    //console.debug( 'requestBody:', JSON.stringify( requestJSON, null, '  ' ) );
+    pcc.createList("Heres a collection");
+    server.respond();
+    assert.deepEqual(requestJSON, DATA._1requestJSON);
+    server.restore();
+});
 
-        QUnit.test("Creator construction/initializiation with datasets", function(assert) {
-            var pcc = new PCC({
-                datasets: DATA._1
-            });
-            //pcc.initialList.forEach( function( dataset, i ){
-            //    console.log( i + ':\n' + JSON.stringify( dataset ) );
-            //});
-            // pcc maintains the original list - which, in this case, is already sorted
-            assert.deepEqual(pcc.initialList, DATA._1);
-            // datasets 1 has no ids, so the pcc will create them
-            assert.ok(
-                _.every(pcc.initialList, function(dataset) {
-                    return dataset.id;
-                })
-            );
-            // datasets 1 is very easy to auto pair
-            assert.equal(pcc.unpaired.length, 0);
-            assert.equal(pcc.paired.length, pcc.initialList.length / 2);
-        });
+QUnit.test("Creator base/empty construction/initializiation defaults", function(assert) {
+    var pcc = new PCC([]);
+    assert.ok(pcc instanceof PCC);
+    assert.deepEqual(pcc.filters, pcc.commonFilters[pcc.DEFAULT_FILTERS]);
+    assert.ok(pcc.automaticallyPair);
+    assert.equal(pcc.matchPercentage, 0.9);
+    assert.equal(pcc.strategy, "autopairLCS");
+});
 
-        QUnit.test("Try easy autopairing with simple exact matching", function(assert) {
-            var pcc = new PCC({
-                datasets: DATA._1,
-                strategy: "simple",
-                twoPassAutopairing: false
-            });
-            assert.equal(pcc.unpaired.length, 0);
-            assert.equal(pcc.paired.length, pcc.initialList.length / 2);
-        });
+QUnit.test("Creator construction/initializiation with datasets", function(assert) {
+    var pcc = new PCC({
+        datasets: DATA._1
+    });
+    //pcc.initialList.forEach( function( dataset, i ){
+    //    console.log( i + ':\n' + JSON.stringify( dataset ) );
+    //});
+    // pcc maintains the original list - which, in this case, is already sorted
+    assert.deepEqual(pcc.initialList, DATA._1);
+    // datasets 1 has no ids, so the pcc will create them
+    assert.ok(
+        _.every(pcc.initialList, function(dataset) {
+            return dataset.id;
+        })
+    );
+    // datasets 1 is very easy to auto pair
+    assert.equal(pcc.unpaired.length, 0);
+    assert.equal(pcc.paired.length, pcc.initialList.length / 2);
+});
 
-        QUnit.test("Try easy autopairing with LCS", function(assert) {
-            var pcc = new PCC({
-                datasets: DATA._1,
-                strategy: "lcs",
-                twoPassAutopairing: false
-            });
-            assert.equal(pcc.unpaired.length, 0);
-            assert.equal(pcc.paired.length, pcc.initialList.length / 2);
-        });
+QUnit.test("Try easy autopairing with simple exact matching", function(assert) {
+    var pcc = new PCC({
+        datasets: DATA._1,
+        strategy: "simple",
+        twoPassAutopairing: false
+    });
+    assert.equal(pcc.unpaired.length, 0);
+    assert.equal(pcc.paired.length, pcc.initialList.length / 2);
+});
 
-        QUnit.test("Try easy autopairing with Levenshtein", function(assert) {
-            var pcc = new PCC({
-                datasets: DATA._1,
-                strategy: "levenshtein",
-                twoPassAutopairing: false
-            });
-            assert.equal(pcc.unpaired.length, 0);
-            assert.equal(pcc.paired.length, pcc.initialList.length / 2);
-        });
+QUnit.test("Try easy autopairing with LCS", function(assert) {
+    var pcc = new PCC({
+        datasets: DATA._1,
+        strategy: "lcs",
+        twoPassAutopairing: false
+    });
+    assert.equal(pcc.unpaired.length, 0);
+    assert.equal(pcc.paired.length, pcc.initialList.length / 2);
+});
 
-        //TODO:
-        //  filters: clearing, setting via popover, regex
-        //  partition: maximize paired, maximize unpaired, split evenly
-        //  pairing: manually pairing and unpairing
-        //  misc: renaming pairs, removing file extensions
-    }
-);
+QUnit.test("Try easy autopairing with Levenshtein", function(assert) {
+    var pcc = new PCC({
+        datasets: DATA._1,
+        strategy: "levenshtein",
+        twoPassAutopairing: false
+    });
+    assert.equal(pcc.unpaired.length, 0);
+    assert.equal(pcc.paired.length, pcc.initialList.length / 2);
+});
+
+//TODO:
+//  filters: clearing, setting via popover, regex
+//  partition: maximize paired, maximize unpaired, split evenly
+//  pairing: manually pairing and unpairing
+//  misc: renaming pairs, removing file extensions
