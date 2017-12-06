07158e03308e026ee013bd6a50708167918c2d27
mvdbeek <m.vandenbeek@gmail.com>
2017-12-06 09:31:22 +0100

    Need force=True in pysam.tabix_index because the index file path exists in the object store

--- lib/galaxy/datatypes/tabular.py
+++ lib/galaxy/datatypes/tabular.py
@@ -763,7 +763,7 @@ class VcfGz(BaseVcf, binary.Binary):
             index_file = dataset.metadata.spec['tabix_index'].param.new_file(dataset=dataset)
 
         try:
-            pysam.tabix_index(dataset.file_name, index=index_file.file_name, preset='vcf')
+            pysam.tabix_index(dataset.file_name, index=index_file.file_name, preset='vcf', force=True)
         except Exception as e:
             raise Exception('Error setting VCF.gz metadata: %s' % (str(e)))
         dataset.metadata.tabix_index = index_file
