6f722861cfd4c36f93348048fe6e6c83305fcc9f
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2017-12-15 01:02:40 +0000

    Python3: fix moved imports
    Add `.ci/check_py3_compatibility.sh` script to find library modules
    that have been moved in Python3. Fix errors found by the script.

---
+++ .ci/check_py3_compatibility.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+export ACK_OPTIONS=" --type python \
+--ignore-dir=.git/ \
+--ignore-dir=.tox/ \
+--ignore-dir=.venv/ \
+--ignore-dir=client/node_modules/ \
+--ignore-dir=database/ \
+--ignore-dir=doc/build/ \
+--ignore-dir=eggs/ \
+--ignore-dir=static/maps/ \
+--ignore-dir=static/scripts/"
+
+PYTHON2_ONLY_MODULES="__builtin__ _winreg BaseHTTPServer CGIHTTPServer \
+ConfigParser Cookie cookielib copy_reg cPickle cStringIO Dialog dummy_thread \
+FileDialog gdbm htmlentitydefs HTMLParser httplib Queue robotparser \
+ScrolledText SimpleDialog SimpleHTTPServer SimpleXMLRPCServer SocketServer \
+StringIO thread Tix tkColorChooser tkCommonDialog Tkconstants Tkdnd tkFont \
+Tkinter tkFileDialog tkMessageBox tkSimpleDialog ttk urllib urllib2 urlparse \
+xmlrpclib"
+
+ret=0
+for mod in $PYTHON2_ONLY_MODULES; do
+    ack "^import $mod(\n|\.)|^from $mod import "
+    if [ $? -eq 0 ]; then ret=1; fi
+done
+
+exit $ret
