cd9783e9f15d1e476bcc2e5462644d331d8ecead
vahid <jalili.vahid@gmail.com>
2017-12-17 22:43:39 -0800

    Removed the unused function `create_user`.

--- lib/galaxy/authnz/psa_authnz.py
+++ lib/galaxy/authnz/psa_authnz.py
@@ -231,22 +231,6 @@ class Storage:
         return exception.__class__ is IntegrityError
 
 
-def create_user(strategy, details, backend, user=None, *args, **kwargs):
-    print '\n\n', '@' * 50
-    if user:
-        return {'is_new': False}
-
-    fields = dict((name, kwargs.get(name, details.get(name)))
-                  for name in backend.setting('USER_FIELDS', USER_FIELDS))
-    if not fields:
-        return
-
-    return {
-        'is_new': True,
-        'user': strategy.create_user(**fields)
-    }
-
-
 def allowed_to_disconnect(name=None, user=None, user_storage=None, strategy=None,
                           backend=None, request=None, details=None, **kwargs):
     """
