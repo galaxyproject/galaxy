759014e15e2a5a383c8f280c18eba4554b3e7a1d
Dannon Baker <dannon.baker@gmail.com>
2017-12-18 14:44:18 -0500

    Consolidate to the one grunt task.

--- client/GruntFile.js
+++ client/GruntFile.js
@@ -1,26 +1,13 @@
 module.exports = function(grunt) {
-    "use strict";
-
     var GALAXY_PATHS = {
-            dist        : '../static/scripts',
-            maps        : '../static/maps',
-            // this symlink allows us to serve uncompressed scripts in DEV_PATH for use with sourcemaps
-            srcSymlink  : '../static/src',
-        },
-        TOOLSHED_PATHS = {
-            dist        : '../static/toolshed/scripts',
-            maps        : '../static/toolshed/maps',
-            srcSymlink  : '../static/toolshed/src',
-        };
-
-    grunt.config.set( 'app', 'galaxy' );
-    grunt.config.set( 'paths', GALAXY_PATHS );
-    if( grunt.option( 'app' ) === 'toolshed' ){
-	    grunt.config.set( 'app', grunt.option( 'app' ) );
-	    grunt.config.set( 'paths', TOOLSHED_PATHS );
-    }
-
+        dist: "../static/scripts",
+        maps: "../static/maps",
+        // this symlink allows us to serve uncompressed scripts in DEV_PATH for use with sourcemaps
+        srcSymlink: "../static/src"
+    };
+    grunt.config.set("app", "galaxy");
+    grunt.config.set("paths", GALAXY_PATHS);
     // see the sub directory grunt-tasks/ for individual task definitions
-    grunt.loadTasks( 'grunt-tasks' );
-    grunt.registerTask( 'default', [ 'uglify' ] );
+    grunt.loadTasks("grunt-tasks");
+    grunt.registerTask("default", ["style"]);
 };
