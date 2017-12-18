cb4645fd198073fc408b6513cac2caba5ab6ad66
vahid <jalili.vahid@gmail.com>
2017-12-17 22:15:04 -0800

    Some cleanup.

--- lib/galaxy/authnz/psa_authnz.py
+++ lib/galaxy/authnz/psa_authnz.py
@@ -1,8 +1,6 @@
 from ..authnz import IdentityProvider
 from ..model import UserAuthnzToken, PSANonce, PSAAssociation, PSAPartial, PSACode
 
-from galaxy.web import url_for
-
 from social_core.actions import do_auth, do_complete, do_disconnect
 from social_core.backends.utils import get_backend
 from social_core.strategy import BaseStrategy
@@ -10,12 +8,12 @@ from social_core.utils import setting_name, module_member
 from sqlalchemy.exc import IntegrityError
 
 
+# key: a component name which PSA requests.
+# value: is the name of a class associated with that key.
 DEFAULTS = {
     'STRATEGY': 'Strategy',
     'STORAGE': 'Storage'
 }
-# key: a component name which PSA requests.
-# value: is the name of a class associated with that key.
 
 BACKENDS = {
     'google': 'social_core.backends.google_openidconnect.GoogleOpenIdConnect'
@@ -25,38 +23,54 @@ BACKENDS_NAME = {
     'google': 'google-openidconnect'
 }
 
-# NOTE: a PSA backend is not initialized at the time of initializing PSAAuthnz because PSA backends have the
-# following line in the initialization which obviously requires session data, and given that PSAAuthnz is initialized
-# when loading Galaxy application, and at this point session does not exist, hence we can not pass session data
-# at initialization to the backend, which results in backend crash.
-# self.data = self.strategy.request_data()
+AUTH_PIPELINE = (
+    # Get the information we can about the user and return it in a simple
+    # format to create the user instance later. On some cases the details are
+    # already part of the auth response from the provider, but sometimes this
+    # could hit a provider API.
+    'social_core.pipeline.social_auth.social_details',
 
+    # Get the social uid from whichever service we're authing thru. The uid is
+    # the unique identifier of the given user in the provider.
+    'social_core.pipeline.social_auth.social_uid',
 
-# 'SOCIAL_AUTH_GOOGLE_OPENIDCONNECT_SECRET': 'nx3j4Z-IkIW051dJ1DObAmUs',
-# 'SOCIAL_AUTH_LOGIN_REDIRECT_URL': '/done/'
-# 'SOCIAL_AUTH_AUTHENTICATION_BACKENDS': ('social_core.backends.google_openidconnect.GoogleOpenIdConnect',
-#                                         'social_core.backends.instagram.InstagramOAuth2'),
-# 'SOCIAL_AUTH_USER_MODEL': 'models.User'
-# 'SOCIAL_AUTH_GOOGLE_OPENIDCONNECT_KEY': '893677542423-5g3s0n6m1bj6fnbo739irsk7bt5os83j.apps.googleusercontent.com',
-# 'SOCIAL_AUTH_PIPELINE': ('social_core.pipeline.social_auth.social_details',
-#                          'social_core.pipeline.social_auth.social_uid',
-#                          'social_core.pipeline.social_auth.auth_allowed',
-#                          'social_core.pipeline.social_auth.social_user',
-#                          'social_core.pipeline.user.get_username',
-#                          'common.pipeline.require_email',
-#                          'social_core.pipeline.mail.mail_validation',
-#                          'social_core.pipeline.user.create_user',
-#                          'social_core.pipeline.social_auth.associate_user',
-#                          'social_core.pipeline.debug.debug',
-#                          'social_core.pipeline.social_auth.load_extra_data',
-#                          'social_core.pipeline.user.user_details',
-#                          'social_core.pipeline.debug.debug')
-#
-#
-# names:
-# STRATEGY
-# STORAGE
-# AUTHENTICATION_BACKENDS
+    # Verifies that the current auth process is valid within the current
+    # project, this is where emails and domains whitelists are applied (if
+    # defined).
+    'social_core.pipeline.social_auth.auth_allowed',
+
+    # Checks if the current social-account is already associated in the site.
+    'social_core.pipeline.social_auth.social_user',
+
+    # Make up a username for this person, appends a random string at the end if
+    # there's any collision.
+    'social_core.pipeline.user.get_username',
+
+    # Send a validation email to the user to verify its email address.
+    # 'social_core.pipeline.mail.mail_validation',
+
+    # Associates the current social details with another user account with
+    # a similar email address.
+    # 'social_core.pipeline.social_auth.associate_by_email',
+
+    # Create a user account if we haven't found one yet.
+    'social_core.pipeline.user.create_user',
+
+    # Create the record that associated the social account with this user.
+    'social_core.pipeline.social_auth.associate_user',
+
+    # Populate the extra_data field in the social record with the values
+    # specified by settings (and the default ones like access_token, etc).
+    'social_core.pipeline.social_auth.load_extra_data',
+
+    # Update the user record with any changed info from the auth service.
+    'social_core.pipeline.user.user_details'
+)
+
+DISCONNECT_PIPELINE = (
+    'galaxy.authnz.psa_authnz.allowed_to_disconnect',
+    'galaxy.authnz.psa_authnz.disconnect'
+)
 
 
 class PSAAuthnz(IdentityProvider):
@@ -147,30 +161,19 @@ class Strategy(BaseStrategy):
         self.config['SOCIAL_AUTH_REDIRECT_IS_HTTPS'] = True if self.trans.request.host.startswith('https:') else False
         super(Strategy, self).__init__(storage, tpl)
 
-    # Settings
     def get_setting(self, name):
-        """Return value for given setting name"""
-        print '\n', ('>' * 50)
-        print '\tasking for: ', name
-        print '\treturning : ', self.config[name]
         return self.config[name]
 
-    # Session
     def session_get(self, name, default=None):
-        """Return session value for given key"""
         return self.session.get(name, default)
 
     def session_set(self, name, value):
-        """Set session value for given key"""
         self.session[name] = value
 
     def session_pop(self, name):
-        """Pop session value for given key"""
-        raise NotImplementedError('Not implemented D')
+        raise NotImplementedError('Not implemented.')
 
-    # Requests
     def request_data(self, merge=True):
-        """Return current request data (POST or GET)"""
         if not self.request:
             return {}
         if merge:
@@ -183,33 +186,26 @@ class Strategy(BaseStrategy):
         return data
 
     def request_host(self):
-        """Return current host value"""
         if self.request:
             return self.request.host
 
     def build_absolute_uri(self, path=None):
-        """Build absolute URI with given (optional) path"""
         path = path or ''
         if path.startswith('http://') or path.startswith('https://'):
             return path
-        return self.trans.request.host + '/authn' +\
+        return self.trans.request.host + '/authn' + \
                ('/' + self.config.get('provider')) if self.config.get('provider', None) is not None else ''
 
-    # Response
-    def html(self, content):
-        """Return HTTP response with given content"""
-        raise NotImplementedError('Implement in subclass HH')
-
     def redirect(self, url):
-        """Return a response redirect to the given URL"""
         return url
 
+    def html(self, content):
+        raise NotImplementedError('Not implemented.')
+
     def render_html(self, tpl=None, html=None, context=None):
-        """Render given template or raw html with given context"""
-        raise NotImplementedError('Implement in subclass JJ')
+        raise NotImplementedError('Not implemented.')
 
     def start(self):
-        # Clean any partial pipeline info before starting the process
         self.clean_partial_pipeline()
         if self.backend.uses_redirect():
             return self.redirect(self.backend.auth_url())
@@ -222,71 +218,6 @@ class Strategy(BaseStrategy):
     def continue_pipeline(self, *args, **kwargs):
         return self.backend.continue_pipeline(*args, **kwargs)
 
-    # TODO: a function very similar to this exists in the basestrategy, check if I need this implementation or that.
-    # def authenticate(self, *args, **kwargs):
-    #     kwargs['strategy'] = self
-    #     kwargs['storage'] = self.storage
-    #     kwargs['backend'] = self.backend
-    #     return self.backend.authenticate(*args, **kwargs)
-
-    # TODO: basestrategy implements these function as the following, update them to get galaxy user instead.
-    # def create_user(self, *args, **kwargs):
-    #     return self.storage.user.create_user(*args, **kwargs)
-    #
-    # def get_user(self, *args, **kwargs):
-    #     return self.storage.user.get_user(*args, **kwargs)
-
-    # TODO: not sure if I need these three functions in my strategy.
-    # def partial_to_session(self, next, backend, request=None, *args, **kwargs):
-    #     user = kwargs.get('user')
-    #     social = kwargs.get('social')
-    #     clean_kwargs = {
-    #         'response': kwargs.get('response') or {},
-    #         'details': kwargs.get('details') or {},
-    #         'username': kwargs.get('username'),
-    #         'uid': kwargs.get('uid'),
-    #         'is_new': kwargs.get('is_new') or False,
-    #         'new_association': kwargs.get('new_association') or False,
-    #         'user': user and user.id or None,
-    #         'social': social and {
-    #             'provider': social.provider,
-    #             'uid': social.uid
-    #         } or None
-    #     }
-    #     clean_kwargs.update(kwargs)
-    #     # Clean any MergeDict data type from the values
-    #     clean_kwargs.update((name, dict(value))
-    #                             for name, value in clean_kwargs.items()
-    #                                 if isinstance(value, dict))
-    #     return {
-    #         'next': next,
-    #         'backend': backend.name,
-    #         'args': tuple(map(self.to_session_value, args)),
-    #         'kwargs': dict((key, self.to_session_value(val))
-    #                             for key, val in clean_kwargs.items()
-    #                                if isinstance(val, self.SERIALIZABLE_TYPES))
-    #     }
-    #
-    # def partial_from_session(self, session):
-    #     kwargs = session['kwargs'].copy()
-    #     user = kwargs.get('user')
-    #     social = kwargs.get('social')
-    #     if isinstance(social, dict):
-    #         kwargs['social'] = self.storage.user.get_social_auth(**social)
-    #     if user:
-    #         kwargs['user'] = self.storage.user.get_user(user)
-    #     return (
-    #         session['next'],
-    #         session['backend'],
-    #         list(map(self.from_session_value, session['args'])),
-    #         dict((key, self.from_session_value(val))
-    #                 for key, val in kwargs.items())
-    #     )
-    #
-    # def clean_partial_pipeline(self, name='partial_pipeline'):
-    #     self.session_pop(name)
-
-
 
 class Storage:
     user = UserAuthnzToken
@@ -298,57 +229,6 @@ class Storage:
     @classmethod
     def is_integrity_error(cls, exception):
         return exception.__class__ is IntegrityError
-
-
-AUTH_PIPELINE = (
-    # Get the information we can about the user and return it in a simple
-    # format to create the user instance later. On some cases the details are
-    # already part of the auth response from the provider, but sometimes this
-    # could hit a provider API.
-    'social_core.pipeline.social_auth.social_details',
-
-    # Get the social uid from whichever service we're authing thru. The uid is
-    # the unique identifier of the given user in the provider.
-    'social_core.pipeline.social_auth.social_uid',
-
-    # Verifies that the current auth process is valid within the current
-    # project, this is where emails and domains whitelists are applied (if
-    # defined).
-    'social_core.pipeline.social_auth.auth_allowed',
-
-    # Checks if the current social-account is already associated in the site.
-    'social_core.pipeline.social_auth.social_user',
-
-    # Make up a username for this person, appends a random string at the end if
-    # there's any collision.
-    'social_core.pipeline.user.get_username',
-
-    # Send a validation email to the user to verify its email address.
-    # 'social_core.pipeline.mail.mail_validation',
-
-    # Associates the current social details with another user account with
-    # a similar email address.
-    # 'social_core.pipeline.social_auth.associate_by_email',
-
-    # Create a user account if we haven't found one yet.
-    'social_core.pipeline.user.create_user',
-
-    # Create the record that associated the social account with this user.
-    'social_core.pipeline.social_auth.associate_user',
-
-    # Populate the extra_data field in the social record with the values
-    # specified by settings (and the default ones like access_token, etc).
-    'social_core.pipeline.social_auth.load_extra_data',
-
-    # Update the user record with any changed info from the auth service.
-    'social_core.pipeline.user.user_details'
-)
-
-
-DISCONNECT_PIPELINE = (
-    'galaxy.authnz.psa_authnz.allowed_to_disconnect',
-    'galaxy.authnz.psa_authnz.disconnect'
-)
 
 
 def create_user(strategy, details, backend, user=None, *args, **kwargs):
