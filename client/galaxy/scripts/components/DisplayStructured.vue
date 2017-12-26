53394dcd5ccab9fa6086c1170de86f43556a6f90
anup kumar <anup.rulez@gmail.com>
2017-12-26 17:13:59 +0100

    Add vue component for display structure

---
+++ client/galaxy/scripts/components/DisplayStructured.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>Display history structure</div>
+</template>
+
+<script>
+
+
+
+export default {
+    data() {
+        return {
+        };
+    }
+}    
+
+</script>
+
+<style>
+
+</style>
+
