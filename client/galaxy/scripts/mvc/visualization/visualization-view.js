b6db962284d06918a1ff1dc61b73b534712f2821
guerler <aysam.guerler@gmail.com>
2017-12-28 14:39:33 -0500

    Revise initial viewer, fix logo display

--- client/galaxy/scripts/mvc/visualization/visualization-view.js
+++ client/galaxy/scripts/mvc/visualization/visualization-view.js
@@ -10,16 +10,16 @@ var View = Backbone.View.extend({
         var self = this;
         this.setElement( $( '<div/>' ) );
         this.model = new Backbone.Model();
-        Utils.get({
-            url     : Galaxy.root + 'api/datasets/' + options.dataset_id,
-            success : function( dataset ) {
-                if ( dataset.visualizations.length === 0 ) {
-                    self.$el.append( $( '<div/>' ).addClass( 'errormessagelarge' )
-                            .append( $( '<p/>' ).text( 'Unfortunately we could not identify a suitable plugin. Feel free to contact us if you are aware of visualizations for this datatype.' )  ) );
-                } else {
-                    self.model.set( { 'dataset': dataset } );
-                    self.render();
-                }
+        $.ajax({
+            url: `${Galaxy.root}api/datasets/${options.dataset_id}`
+        })
+        .done(dataset => {
+            if ( dataset.visualizations.length === 0 ) {
+                self.$el.append( $( '<div/>' ).addClass( 'errormessagelarge' )
+                        .append( $( '<p/>' ).text( 'Unfortunately we could not identify a suitable plugin. Feel free to contact us if you are aware of visualizations for this datatype.' )  ) );
+            } else {
+                self.model.set( { 'dataset': dataset } );
+                self.render();
             }
         });
     },
@@ -29,18 +29,19 @@ var View = Backbone.View.extend({
         var dataset = this.model.get( 'dataset' );
         this.vis_index = {};
         this.vis_array = [];
-        _.each( dataset.visualizations, function( v ) {
-            if ( v.types ) {
-                _.each( v.types, function( type ) {
-                    var dict = self._getTypeDictionary( v, type );
-                    self.vis_index[ dict.id ] = dict;
-                    self.vis_array.push( dict );
-                });
-            } else {
-                var dict = self._getTypeDictionary( v );
-                self.vis_index[ dict.id ] = dict;
-                self.vis_array.push( dict );
+        _.each( dataset.visualizations, function( v, id ) {
+            var dict = {
+                id              : id,
+                name            : v.name,
+                keywords        : v.keywords || [],
+                title           : v.title ? type.title + ' (' + v.html + ')' : v.html,
+                image_src       : v.logo ? Galaxy.root + v.logo : null,
+                description     : v.description || 'No description available.',
+                regular         : v.regular,
+                visualization   : v
             }
+            self.vis_index[ dict.id ] = dict;
+            self.vis_array.push( dict );
         });
         this.types = new Thumbnails.View({
             title_default   : 'Suggested plugins',
@@ -52,21 +53,6 @@ var View = Backbone.View.extend({
             }
         });
         this.$el.empty().append( this.types.$el );
-    },
-
-    /** Build a dictionary for the thumbnail ui element */
-    _getTypeDictionary: function( visualization, type ) {
-        type = type || visualization;
-        return {
-            id              : visualization.name + '_' + type.name,
-            name            : type.name,
-            keywords        : type.keywords || [],
-            title           : type.title ? type.title + ' (' + visualization.html + ')' : visualization.html,
-            image_src       : type.image ? Galaxy.root + type.image : null,
-            description     : type.description || 'No description available.',
-            regular         : type.regular,
-            visualization   : visualization
-        }
     }
 });
 export default { View: View }
