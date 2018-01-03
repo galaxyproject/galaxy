4b2bf122b627d4dfbd87e9491cbf97755f40a9bf
Matthias Bernt <m.bernt@ufz.de>
2018-01-03 11:26:04 +0100

    fix for microbial import tool
    There seems to be no UnvalidatedValue class anymore. So I removed these
    checks which seems to make the tool functional again.
    
    Note that there is one more instance of such a check in the Galaxy
    sources (in tools/parameters/basic.py). I guess this can also be
    removed?

--- tools/data_source/microbial_import_code.py
+++ tools/data_source/microbial_import_code.py
@@ -101,11 +101,6 @@ def exec_after_process(app, inp_data, out_data, param_dict, tool, stdout, stderr
     # if not (kingdom or group or org):
     if not (kingdom or org):
         print("Parameters are not available.")
-    # workflow passes galaxy.tools.parameters.basic.UnvalidatedValue instead of values
-    if isinstance(kingdom, tools.parameters.basic.UnvalidatedValue):
-        kingdom = kingdom.value
-    if isinstance(org, tools.parameters.basic.UnvalidatedValue):
-        org = org.value
 
     GALAXY_DATA_INDEX_DIR = app.config.tool_data_path
     microbe_info = load_microbial_data(GALAXY_DATA_INDEX_DIR, sep='\t')
