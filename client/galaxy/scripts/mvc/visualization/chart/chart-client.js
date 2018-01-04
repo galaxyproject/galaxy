e0e48527c758353a1f9fffa79c5aadfad2fa2dbc
guerler <aysam.guerler@gmail.com>
2018-01-04 11:55:20 -0500

    Load dataset for chart visualization plugin

--- client/galaxy/scripts/mvc/visualization/chart/chart-client.js
+++ client/galaxy/scripts/mvc/visualization/chart/chart-client.js
@@ -1,30 +1,30 @@
+import Deferred from "utils/deferred";
 import Modal from "mvc/ui/ui-modal";
 import Portlet from "mvc/ui/ui-portlet";
 import Ui from "mvc/ui/ui-misc";
 import Utils from "utils/utils";
 import Chart from "mvc/visualization/chart/components/model";
-import Deferred from "utils/deferred";
-import Viewer from "mvc/visualization/chart/views/viewer";
 import Editor from "mvc/visualization/chart/views/editor";
+import Viewer from "mvc/visualization/chart/views/viewer";
 var View = Backbone.View.extend({
     initialize: function(options) {
         this.options = options;
+        window.console.log(options);
         this.modal = (parent.Galaxy && parent.Galaxy.modal) || new Modal.View();
         this.setElement("<div/>")
         $.ajax({
-            url: `${Galaxy.root}api/visualizations/${options.id}`
+            url: `${Galaxy.root}api/datasets/${options.dataset_id}`
         })
-        .done(visualization => {
-            window.console.log(visualization);
-            this.visualization = visualization;
-            this.chart = new Chart({}, visualization.latest_revision.config.chart_dict);
+        .done(dataset => {
+            this.dataset = dataset;
+            this.chart = new Chart({}, options);
             this.deferred = new Deferred();
             this.viewer = new Viewer(this);
             this.editor = new Editor(this);
             this.$el.append(this.viewer.$el);
             this.$el.append(this.editor.$el);
+            this.go(this.chart.load() ? "viewer" : "editor");
             this.render();
-
         })
         .fail(response => {
             let message = response.responseJSON && response.responseJSON.err_msg;
@@ -34,10 +34,9 @@ var View = Backbone.View.extend({
 
     /** Build client ui */
     render: function() {
-        let plugin_path = this.visualization.plugin.static_url + '/' + this.visualization.type;
+        //let plugin_path = this.visualization.plugin.static_url + '/' + this.visualization.type;
         //import Plugin from plugin_path;
-        /*this.go(this.chart.load() ? "viewer" : "editor");
-        require(["repository/build/" + chart.get("type")], function(ChartView) {
+        /*require(["repository/build/" + chart.get("type")], function(ChartView) {
             new ChartView({ process: process, chart: chart, dataset: self.app.dataset, targets: self.targets });
         }, function(err) {
             chart.state(
