2b0fbe80ebc8814d2d6bba8bae0dcab9922a418d
Dannon Baker <dannon.baker@gmail.com>
2018-01-12 08:29:22 -0500

    Client formatting.

--- client/galaxy/scripts/mvc/visualization/visualization-view.js
+++ client/galaxy/scripts/mvc/visualization/visualization-view.js
@@ -6,19 +6,25 @@ import Thumbnails from "mvc/ui/ui-thumbnails";
 import Portlet from "mvc/ui/ui-portlet";
 import Ui from "mvc/ui/ui-misc";
 var View = Backbone.View.extend({
-    initialize: function( options ) {
+    initialize: function(options) {
         var self = this;
-        this.setElement( $( '<div/>' ) );
+        this.setElement($("<div/>"));
         this.model = new Backbone.Model();
         $.ajax({
             url: `${Galaxy.root}api/datasets/${options.dataset_id}`
-        })
-        .done(dataset => {
-            if ( dataset.visualizations.length === 0 ) {
-                self.$el.append( $( '<div/>' ).addClass( 'errormessagelarge' )
-                        .append( $( '<p/>' ).text( 'Unfortunately we could not identify a suitable plugin. Feel free to contact us if you are aware of visualizations for this datatype.' )  ) );
+        }).done(dataset => {
+            if (dataset.visualizations.length === 0) {
+                self.$el.append(
+                    $("<div/>")
+                        .addClass("errormessagelarge")
+                        .append(
+                            $("<p/>").text(
+                                "Unfortunately we could not identify a suitable plugin. Feel free to contact us if you are aware of visualizations for this datatype."
+                            )
+                        )
+                );
             } else {
-                self.model.set( { 'dataset': dataset } );
+                self.model.set({ dataset: dataset });
                 self.render();
             }
         });
@@ -26,33 +32,33 @@ var View = Backbone.View.extend({
 
     render: function() {
         var self = this;
-        var dataset = this.model.get( 'dataset' );
+        var dataset = this.model.get("dataset");
         this.vis_index = {};
         this.vis_array = [];
-        _.each( dataset.visualizations, function( v, id ) {
+        _.each(dataset.visualizations, function(v, id) {
             var dict = {
-                id              : id,
-                name            : v.name,
-                keywords        : v.keywords || [],
-                title           : v.html,
-                image_src       : v.logo ? Galaxy.root + v.logo : null,
-                description     : v.description || 'No description available.',
-                regular         : v.regular,
-                visualization   : v
-            }
-            self.vis_index[ dict.id ] = dict;
-            self.vis_array.push( dict );
+                id: id,
+                name: v.name,
+                keywords: v.keywords || [],
+                title: v.html,
+                image_src: v.logo ? Galaxy.root + v.logo : null,
+                description: v.description || "No description available.",
+                regular: v.regular,
+                visualization: v
+            };
+            self.vis_index[dict.id] = dict;
+            self.vis_array.push(dict);
         });
         this.types = new Thumbnails.View({
-            title_default   : 'Suggested plugins',
-            title_list      : 'List of available plugins',
-            collection      : this.vis_array,
-            ondblclick      : function( id ) {
-                var vis_type = self.vis_index[ id ];
-                $('#galaxy_main').attr('src', vis_type.visualization.href);
+            title_default: "Suggested plugins",
+            title_list: "List of available plugins",
+            collection: this.vis_array,
+            ondblclick: function(id) {
+                var vis_type = self.vis_index[id];
+                $("#galaxy_main").attr("src", vis_type.visualization.href);
             }
         });
-        this.$el.empty().append( this.types.$el );
+        this.$el.empty().append(this.types.$el);
     }
 });
-export default { View: View }
+export default { View: View };
