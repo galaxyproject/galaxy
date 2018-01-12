1e9c8cf57a7f9a3978416bc0ba706f0cd7a858fd
Dannon Baker <dannon.baker@gmail.com>
2018-01-12 09:14:21 -0500

    Extend instead of overwrite window.bundleEntries

--- client/galaxy/scripts/apps/chart.js
+++ client/galaxy/scripts/apps/chart.js
@@ -1,7 +1,12 @@
+import * as _ from "libs/underscore";
 import ChartClient from "mvc/visualization/chart/chart-client";
+
+window.bundleEntries = window.bundleEntries || {};
+
 export const bundleEntries = {
     chart: function chartEntry(options) {
         return new ChartClient.View(options);
     }
 };
-window.bundleEntries = bundleEntries;
+
+_.extend(window.bundleEntries, bundleEntries);
