161467513b3f911cde15b890c404ed373f4571f4
mvdbeek <m.vandenbeek@gmail.com>
2018-01-18 20:22:07 +0100

    Use a for loop when checking size in object store
    and adjust `empty()` to use the new code. Thanks @nsoranzo.

--- lib/galaxy/objectstore/__init__.py
+++ lib/galaxy/objectstore/__init__.py
@@ -359,7 +359,7 @@ class DiskObjectStore(ObjectStore):
 
     def empty(self, obj, **kwargs):
         """Override `ObjectStore`'s stub by checking file size on disk."""
-        return os.path.getsize(self.get_filename(obj, **kwargs)) == 0
+        return self.size(obj, **kwargs) == 0
 
     def size(self, obj, **kwargs):
         """Override `ObjectStore`'s stub by return file size on disk.
@@ -368,11 +368,13 @@ class DiskObjectStore(ObjectStore):
         """
         if self.exists(obj, **kwargs):
             try:
-                size = os.path.getsize(self.get_filename(obj, **kwargs))
-                if size == 0:
+                filepath = self.get_filename(obj, **kwargs)
+                for _ in range(0, 2):
+                    size = os.path.getsize(filepath)
+                    if size != 0:
+                        break
                     # May be legitimately 0, or there may be an issue with the FS / kernel, so we try again
                     time.sleep(0.01)
-                    size = os.path.getsize(self.get_filename(obj, **kwargs))
                 return size
             except OSError:
                 return 0
