5e5b8040b4d44a0628ecef6904e0959834c3409f
guerler <aysam.guerler@gmail.com>
2018-01-29 10:16:26 -0500

    Remove unused components from config parser

--- lib/galaxy/visualization/plugins/config_parser.py
+++ lib/galaxy/visualization/plugins/config_parser.py
@@ -1,9 +1,7 @@
 import logging
 from six import string_types
-from xml.etree import cElementTree as ElementTree
 
 import galaxy.model
-from galaxy import util
 from galaxy.util.xml_macros import load
 
 log = logging.getLogger(__name__)
@@ -48,38 +46,6 @@ class VisualizationsConfigParser(object):
         xml_tree = load(xml_filepath)
         visualization = self.parse_visualization(xml_tree.getroot())
         return visualization
-
-    def xml_to_json(self, xml_input, json_output):
-        '''Converts an xml file to json.'''
-        self.dict_to_json(self.etree_to_dict(self.xml_to_etree(xml_input), True), json_output)
-
-    def xml_to_etree(self, xml_input):
-        '''Converts xml to a lxml etree.'''
-        f = open(xml_input, 'r')
-        xml = f.read()
-        f.close()
-        return etree.HTML(xml)
-
-    def etree_to_dict(self, tree, only_child):
-        '''Converts an lxml etree into a dictionary.'''
-        mydict = dict([(item[0], item[1]) for item in tree.items()])
-        children = tree.getchildren()
-        if children:
-            if len(children) > 1:
-                mydict['children'] = [self.etree_to_dict(child, False) for child in children]
-            else:
-                child = children[0]
-                mydict[child.tag] = self.etree_to_dict(child, True)
-        if only_child:
-            return mydict
-        else:
-            return {tree.tag: mydict}
-
-    def dict_to_json(self, dictionary, json_output):
-        '''Coverts a dictionary into a json file.'''
-        f = open(json_output, 'w')
-        f.write(json.dumps(dictionary, sort_keys=True, indent=4))
-        f.close()
 
     def parse_visualization(self, xml_tree):
         """
