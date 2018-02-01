269be966a9da235d94d3058b7786c2326697b8c6
guerler <aysam.guerler@gmail.com>
2018-02-01 11:06:07 -0500

    Add editor buttons

--- client/galaxy/scripts/mvc/visualization/chart/chart-client.js
+++ client/galaxy/scripts/mvc/visualization/chart/chart-client.js
@@ -1,5 +1,7 @@
 import Deferred from "utils/deferred";
+import Utils from "utils/utils";
 import Modal from "mvc/ui/ui-modal";
+import Ui from "mvc/ui/ui-misc";
 import Chart from "mvc/visualization/chart/components/model";
 import Editor from "mvc/visualization/chart/views/editor";
 import Viewer from "mvc/visualization/chart/views/viewport";
@@ -7,8 +9,13 @@ import Viewer from "mvc/visualization/chart/views/viewport";
 export default Backbone.View.extend({
     initialize: function(options) {
         this.modal = (window.parent.Galaxy && window.parent.Galaxy.modal) || new Modal.View();
-        this.setElement($("<div/>").append($("<div/>").addClass("charts-center"))
+        this.setElement($("<div/>").addClass("charts-client")
+                                   .append($("<div/>").addClass("charts-buttons"))
+                                   .append($("<div/>").addClass("charts-center"))
                                    .append($("<div/>").addClass("charts-right")));
+        this.$center = this.$(".charts-center");
+        this.$right = this.$(".charts-right");
+        this.$buttons = this.$(".charts-buttons");
         this.chart = new Chart({}, options);
         this.chart.plugin = options.visualization_plugin;
         this.chart.plugin.specs = this.chart.plugin.specs || {};
@@ -16,8 +23,37 @@ export default Backbone.View.extend({
         this.deferred = new Deferred();
         this.viewer = new Viewer(this);
         this.editor = new Editor(this);
-        this.$(".charts-center").append(this.viewer.$el);
-        this.$(".charts-right").append(this.editor.$el);
+        this.buttons =[{
+            icon: "fa-caret-left",
+            tooltip: "Show Settings",
+            cls: "ui-button-icon charts-fullscreen-button",
+            onclick: () => {
+                this.$el.removeClass("charts-fullscreen");
+                window.dispatchEvent(new Event("resize"));
+            }
+        },{
+            icon: "fa-caret-right",
+            tooltip: "Hide Settings",
+            onclick: () => {
+                this.$el.addClass("charts-fullscreen");
+                window.dispatchEvent(new Event("resize"));
+            }
+        },{
+            icon: "fa-line-chart",
+            tooltip: "Render Visualization",
+            title: "Visualize",
+            onclick: () => {
+                this.chart.set({
+                    date: Utils.time()
+                });
+                this.chart.trigger("redraw");
+            }
+        }];
+        for(let b of this.buttons) {
+            this.$buttons.append((new Ui.ButtonIcon(b)).$el);
+        }
+        this.$center.append(this.viewer.$el);
+        this.$right.append(this.editor.$el);
         $.ajax({
             url: `${Galaxy.root}api/datasets/${options.dataset_id}`
         }).done(dataset => {
