galaxyproject/galaxy

commit 78babab6455f4cd1c51aed0b1ddf6b4b29ad6d03

Pre-build collections during mapping to be more recoverable.
Create mapped over collections ahead of time instead of at the end only if they are valid.

- This way if there is a problem that problem can be reported in the state of the mapped collection.
- This way datasets can just be hidden instead of displayed and then hidden (fixes #1790).
- I think this should improve performance and prevent locks because we no longer have conflicting threads trying to write dataset state.
- We populate the collection as we go so we can recover and continue scheduling the mapped step (needed for #3883)

lib/galaxy/dataset_collections/structure.py
@@ -1,12 +1,17 @@
 """ Module for reasoning about structure of and matching hierarchical collections of data.
 """
 import logging
-log = logging.getLogger(__name__)
+
+import six
 
 from .type_description import map_over_collection_type
 
+log = logging.getLogger(__name__)
 
+
+@six.python_2_unicode_compatible
 class Leaf(object):
+    children_known = True
 
     def __len__(self):
         return 1
@@ -18,18 +23,60 @@ class Leaf(object):
     def clone(self):
         return self
 
-    def multiply(self, other_structure):
-        return other_structure.clone()
+    def multiply(self, other_structure, uninitialized=False):
+        if not uninitialized:
+            return other_structure.clone()
+        else:
+            return UnitializedTree(other_structure.collection_type_description)
+
+    def sliced_collection_type(self, collection):
+        return input
+
+    def __str__(self):
+        return "Leaf[]"
 
 
 leaf = Leaf()
 
 
-class Tree(object):
+class BaseTree(object):
+
+    def __init__(self, collection_type_description):
+        self.collection_type_description = collection_type_description
+
+
+@six.python_2_unicode_compatible
+class UnitializedTree(BaseTree):
+    children_known = False
+
+    def clone(self):
+        return self
+
+    @property
+    def is_leaf(self):
+        return False
+
+    def __len__(self):
+        raise Exception("Unknown length")
+
+    def multiply(self, other_structure, uninitialized=False):
+        if other_structure.is_leaf:
+            return self.clone()
+
+        new_collection_type = self.collection_type_description.multiply(other_structure.collection_type_description)
+        return UnitializedTree(new_collection_type)
+
+    def __str__(self):
+        return "UnitializedTree[collection_type=%s]" % self.collection_type_description
+
+
+@six.python_2_unicode_compatible
+class Tree(BaseTree):
+    children_known = True
 
     def __init__(self, children, collection_type_description):
+        super(Tree, self).__init__(collection_type_description)
         self.children = children
-        self.collection_type_description = collection_type_description
 
     @staticmethod
     def for_dataset_collection(dataset_collection, collection_type_description):
@@ -107,20 +154,44 @@ class Tree(object):
             element_identifiers=element_identifiers,
         )
 
-    def multiply(self, other_structure):
+    def multiply(self, other_structure, uninitialized=False):
         if other_structure.is_leaf:
             return self.clone()
 
         new_collection_type = self.collection_type_description.multiply(other_structure.collection_type_description)
         new_children = []
         for (identifier, structure) in self.children:
-            new_children.append((identifier, structure.multiply(other_structure)))
+            new_children.append((identifier, structure.multiply(other_structure, uninitialized=uninitialized)))
 
         return Tree(new_children, new_collection_type)
 
     def clone(self):
         cloned_children = [(_[0], _[1].clone()) for _ in self.children]
         return Tree(cloned_children, self.collection_type_description)
+
+    def __str__(self):
+        return "Tree[collection_type=%s,children=%s]" % (self.collection_type_description, ",".join(map(lambda identifier_and_element: "%s=%s" % (identifier_and_element[0], identifier_and_element[1]), self.children)))
+
+
+def tool_output_to_structure(get_sliced_input_collection_type, tool_output, collections_manager):
+    if not tool_output.collection:
+        tree = leaf
+    else:
+        collection_type_descriptions = collections_manager.collection_type_descriptions
+        # Okay this is ToolCollectionOutputStructure not a Structure - different
+        # concepts of structure.
+        if tool_output.dynamic_structure:
+            # Two cases collection_type_source and collection_type right?
+            tree = UnitializedTree(collection_type_descriptions.for_type_description("list"))  # list is obviously wrong...
+        else:
+            structured_like = tool_output.structure.structured_like
+            if structured_like:
+                collection_type = get_sliced_input_collection_type(structured_like)
+            else:
+                collection_type = tool_output.structure.collection_type
+            tree = UnitializedTree(collection_type)
+
+    return tree
 
 
 def dict_map(func, input_dict):
@@ -131,4 +202,5 @@ def get_structure(dataset_collection_instance, collection_type_description, leaf
     if leaf_subcollection_type:
         collection_type_description = collection_type_description.effective_collection_type_description(leaf_subcollection_type)
 
-    return Tree.for_dataset_collection(dataset_collection_instance.collection, collection_type_description)
+    collection = dataset_collection_instance.collection
+    return Tree.for_dataset_collection(collection, collection_type_description)

lib/galaxy/dataset_collections/matching.py
@@ -44,23 +44,29 @@ class MatchingCollections(object):
         self.linked_structure = None
         self.unlinked_structures = []
         self.collections = {}
+        self.subcollection_types = {}
 
     def __attempt_add_to_linked_match(self, input_name, hdca, collection_type_description, subcollection_type):
         structure = get_structure(hdca, collection_type_description, leaf_subcollection_type=subcollection_type)
         if not self.linked_structure:
             self.linked_structure = structure
             self.collections[input_name] = hdca
+            self.subcollection_types[input_name] = subcollection_type
         else:
             if not self.linked_structure.can_match(structure):
                 raise exceptions.MessageException(CANNOT_MATCH_ERROR_MESSAGE)
             self.collections[input_name] = hdca
+            self.subcollection_types[input_name] = subcollection_type
 
     def slice_collections(self):
         return self.linked_structure.walk_collections(self.collections)
 
+    def subcollection_mapping_type(self, input_name):
+        return self.subcollection_types[input_name]
+
     @property
     def structure(self):
-        """Yield cross product of all unlinked datasets to linked dataset."""
+        """Yield cross product of all unlinked collections structures to linked collection structure."""
         effective_structure = leaf
         for unlinked_structure in self.unlinked_structures:
             effective_structure = effective_structure.multiply(unlinked_structure)

lib/galaxy/dataset_collections/type_description.py
@@ -8,6 +8,7 @@ class CollectionTypeDescriptionFactory(object):
         self.type_registry = type_registry
 
     def for_collection_type(self, collection_type):
+        assert collection_type is not None
         return CollectionTypeDescription(collection_type, self)
 
 

lib/galaxy/managers/collections.py
@@ -46,6 +46,37 @@ class DatasetCollectionManager(object):
         self.tag_manager = tags.GalaxyTagManager(app.model.context)
         self.ldda_manager = lddas.LDDAManager(app)
 
+    def precreate_dataset_collection_instance(self, trans, parent, name, implicit_inputs, implicit_output_name, structure):
+        dataset_collection = self.precreate_dataset_collection(structure)
+        return self._create_instance_for_collection(
+            trans, parent, name, dataset_collection, implicit_inputs=implicit_inputs, implicit_output_name=implicit_output_name,
+        )
+
+    def precreate_dataset_collection(self, structure):
+        if structure.is_leaf or not structure.children_known:
+            return model.DatasetCollectionElement.UNINITIALIZED_ELEMENT
+        else:
+            collection_type_description = structure.collection_type_description
+            dataset_collection = model.DatasetCollection(populated=False)
+            dataset_collection.collection_type = collection_type_description.collection_type
+            elements = []
+            for index, (identifier, substructure) in enumerate(structure.children):
+                # TODO: Open question - populate these now or later?
+                if substructure.is_leaf:
+                    element = model.DatasetCollectionElement.UNINITIALIZED_ELEMENT
+                else:
+                    element = self.precreate_dataset_collection(substructure)
+
+                element = model.DatasetCollectionElement(
+                    element=element,
+                    element_identifier=identifier,
+                    element_index=index,
+                )
+                elements.append(element)
+            dataset_collection.elements = elements
+
+            return dataset_collection
+
     def create(self, trans, parent, name, collection_type, element_identifiers=None,
                elements=None, implicit_collection_info=None, trusted_identifiers=None,
                hide_source_items=False, tags=None):
@@ -68,27 +99,30 @@ class DatasetCollectionManager(object):
             hide_source_items=hide_source_items,
         )
 
+        implicit_inputs = []
+        if implicit_collection_info:
+            implicit_inputs = implicit_collection_info.get('implicit_inputs', [])
+
+        implicit_output_name = None
+        if implicit_collection_info:
+            implicit_output_name = implicit_collection_info["implicit_output_name"]
+
+        return self._create_instance_for_collection(
+            trans, parent, name, dataset_collection, implicit_inputs=implicit_inputs, implicit_output_name=implicit_output_name, tags=tags
+        )
+
+    def _create_instance_for_collection(self, trans, parent, name, dataset_collection, implicit_output_name=None, implicit_inputs=None, tags=None):
         if isinstance(parent, model.History):
             dataset_collection_instance = self.model.HistoryDatasetCollectionAssociation(
                 collection=dataset_collection,
                 name=name,
             )
-            if implicit_collection_info:
-                for input_name, input_collection in implicit_collection_info["implicit_inputs"]:
+            if implicit_inputs:
+                for input_name, input_collection in implicit_inputs:
                     dataset_collection_instance.add_implicit_input_collection(input_name, input_collection)
-                for output_dataset in implicit_collection_info.get("outputs"):
-                    if output_dataset not in trans.sa_session:
-                        output_dataset = trans.sa_session.query(type(output_dataset)).get(output_dataset.id)
-                    if isinstance(output_dataset, model.HistoryDatasetAssociation):
-                        output_dataset.hidden_beneath_collection_instance = dataset_collection_instance
-                    elif isinstance(output_dataset, model.HistoryDatasetCollectionAssociation):
-                        dataset_collection_instance.add_implicit_input_collection(input_name, input_collection)
-                    else:
-                        # dataset collection, don't need to do anything...
-                        pass
-                    trans.sa_session.add(output_dataset)
 
-                dataset_collection_instance.implicit_output_name = implicit_collection_info["implicit_output_name"]
+            if implicit_output_name:
+                dataset_collection_instance.implicit_output_name = implicit_output_name
 
             log.debug("Created collection with %d elements" % (len(dataset_collection_instance.collection.elements)))
             # Handle setting hid
@@ -106,9 +140,6 @@ class DatasetCollectionManager(object):
             log.exception(message)
             raise MessageException(message)
 
-        implicit_inputs = []
-        if implicit_collection_info:
-            implicit_inputs = implicit_collection_info.get('implicit_inputs', [])
         tags = self._append_tags(dataset_collection_instance, implicit_inputs, tags)
         return self.__persist(dataset_collection_instance)
 

lib/galaxy/managers/collections_util.py
@@ -122,12 +122,16 @@ def dictify_dataset_collection_instance(dataset_collection_instance, parent, sec
 
 def dictify_element(element):
     dictified = element.to_dict(view="element")
-    object_detials = element.element_object.to_dict()
-    if element.child_collection:
-        # Recursively yield elements for each nested collection...
-        child_collection = element.child_collection
-        object_detials["elements"] = [dictify_element(_) for _ in child_collection.elements]
-        object_detials["populated"] = child_collection.populated
+    element_object = element.element_object
+    if element_object is not None:
+        object_detials = element.element_object.to_dict()
+        if element.child_collection:
+            # Recursively yield elements for each nested collection...
+            child_collection = element.child_collection
+            object_detials["elements"] = [dictify_element(_) for _ in child_collection.elements]
+            object_detials["populated"] = child_collection.populated
+    else:
+        object_detials = None
 
     dictified["object"] = object_detials
     return dictified

lib/galaxy/model/__init__.py
@@ -3218,6 +3218,15 @@ class DatasetCollection(object, Dictifiable, UsesAnnotations):
         self.populated_state = DatasetCollection.populated_states.FAILED
         self.populated_state_message = message
 
+    def finalize(self):
+        # All jobs have written out their elements - everything should be populated
+        # but might not be - check that second case! (TODO)
+        self.mark_as_populated()
+        if self.has_subcollections:
+            # THIS IS WRONG - SHOULD ONLY BE TO THE DEPTH OF THE MAP OVER.
+            for element in self.elements:
+                element.child_collection.finalize()
+
     @property
     def dataset_instances(self):
         instances = []
@@ -3485,6 +3494,8 @@ class DatasetCollectionElement(object, Dictifiable):
     dict_collection_visible_keys = ['id', 'element_type', 'element_index', 'element_identifier']
     dict_element_visible_keys = ['id', 'element_type', 'element_index', 'element_identifier']
 
+    UNINITIALIZED_ELEMENT = object()
+
     def __init__(
         self,
         id=None,
@@ -3499,7 +3510,7 @@ class DatasetCollectionElement(object, Dictifiable):
             self.ldda = element
         elif isinstance(element, DatasetCollection):
             self.child_collection = element
-        else:
+        elif element != self.UNINITIALIZED_ELEMENT:
             raise AttributeError('Unknown element type provided: %s' % type(element))
 
         self.id = id
@@ -3517,7 +3528,7 @@ class DatasetCollectionElement(object, Dictifiable):
             # TOOD: Rename element_type to element_type.
             return "dataset_collection"
         else:
-            raise Exception("Unknown element instance type")
+            return None
 
     @property
     def is_collection(self):
@@ -3532,7 +3543,7 @@ class DatasetCollectionElement(object, Dictifiable):
         elif self.child_collection:
             return self.child_collection
         else:
-            raise Exception("Unknown element instance type")
+            return None
 
     @property
     def dataset_instance(self):
@@ -4193,6 +4204,10 @@ class WorkflowInvocationStep(object, Dictifiable):
 
     def update(self):
         self.workflow_invocation.update()
+
+    @property
+    def is_new(self):
+        return self.state == self.states.NEW
 
     def add_output(self, output_name, output_object):
         if output_object.history_content_type == "dataset":

lib/galaxy/tools/__init__.py
@@ -1314,13 +1314,20 @@ class Tool(object, Dictifiable):
                         output_collections=execution_tracker.output_collections,
                         implicit_collections=execution_tracker.implicit_collections)
 
-    def handle_single_execution(self, trans, rerun_remap_job_id, params, history, mapping_over_collection, execution_cache=None):
+    def handle_single_execution(self, trans, rerun_remap_job_id, execution_slice, history, execution_cache=None):
         """
         Return a pair with whether execution is successful as well as either
         resulting output data or an error message indicating the problem.
         """
         try:
-            job, out_data = self.execute(trans, incoming=params, history=history, rerun_remap_job_id=rerun_remap_job_id, mapping_over_collection=mapping_over_collection, execution_cache=execution_cache)
+            job, out_data = self.execute(
+                trans,
+                incoming=execution_slice.param_combination,
+                history=history,
+                rerun_remap_job_id=rerun_remap_job_id,
+                execution_cache=execution_cache,
+                dataset_collection_elements=execution_slice.dataset_collection_elements,
+            )
         except httpexceptions.HTTPFound as e:
             # if it's a paste redirect exception, pass it up the stack
             raise e
@@ -2263,7 +2270,7 @@ class DatabaseOperationTool(Tool):
     def check_inputs_ready(self, input_datasets, input_dataset_collections):
         def check_dataset_instance(input_dataset):
             if input_dataset.is_pending:
-                raise ToolInputsNotReadyException()
+                raise ToolInputsNotReadyException("An input dataset is pending.")
 
             if self.require_dataset_ok:
                 if input_dataset.state != input_dataset.dataset.states.OK:
@@ -2275,7 +2282,7 @@ class DatabaseOperationTool(Tool):
         for input_dataset_collection_pairs in input_dataset_collections.values():
             for input_dataset_collection, is_mapped in input_dataset_collection_pairs:
                 if not input_dataset_collection.collection.populated:
-                    raise ToolInputsNotReadyException()
+                    raise ToolInputsNotReadyException("An input collection is not populated.")
 
             map(check_dataset_instance, input_dataset_collection.dataset_instances)
 

commit f99299f8332a75a6075f49176770078be9cda571

Pre-compute more collection state during mapping.
Still dealing with fallout from 78babab6455f4cd1c51aed0b1ddf6b4b29ad6d03. After that commit we were treating structured_like collections like collections with unknown structure and not dealing with collection_type_source information at all when building backbones for new collections during mapping.

lib/galaxy/dataset_collections/structure.py
@@ -23,11 +23,8 @@ class Leaf(object):
     def clone(self):
         return self
 
-    def multiply(self, other_structure, uninitialized=False):
-        if not uninitialized:
-            return other_structure.clone()
-        else:
-            return UnitializedTree(other_structure.collection_type_description)
+    def multiply(self, other_structure):
+        return other_structure.clone()
 
     def sliced_collection_type(self, collection):
         return input
@@ -59,7 +56,7 @@ class UnitializedTree(BaseTree):
     def __len__(self):
         raise Exception("Unknown length")
 
-    def multiply(self, other_structure, uninitialized=False):
+    def multiply(self, other_structure):
         if other_structure.is_leaf:
             return self.clone()
 
@@ -154,14 +151,14 @@ class Tree(BaseTree):
             element_identifiers=element_identifiers,
         )
 
-    def multiply(self, other_structure, uninitialized=False):
+    def multiply(self, other_structure):
         if other_structure.is_leaf:
             return self.clone()
 
         new_collection_type = self.collection_type_description.multiply(other_structure.collection_type_description)
         new_children = []
         for (identifier, structure) in self.children:
-            new_children.append((identifier, structure.multiply(other_structure, uninitialized=uninitialized)))
+            new_children.append((identifier, structure.multiply(other_structure)))
 
         return Tree(new_children, new_collection_type)
 
@@ -173,19 +170,35 @@ class Tree(BaseTree):
         return "Tree[collection_type=%s,children=%s]" % (self.collection_type_description, ",".join(map(lambda identifier_and_element: "%s=%s" % (identifier_and_element[0], identifier_and_element[1]), self.children)))
 
 
-def tool_output_to_structure(get_sliced_input_collection_type, tool_output, collections_manager):
+def tool_output_to_structure(get_sliced_input_collection_structure, tool_output, collections_manager):
     if not tool_output.collection:
         tree = leaf
     else:
+        collection_type_descriptions = collections_manager.collection_type_descriptions
         # Okay this is ToolCollectionOutputStructure not a Structure - different
         # concepts of structure.
         structured_like = tool_output.structure.structured_like
+        collection_type = tool_output.structure.collection_type
         if structured_like:
-            collection_type = get_sliced_input_collection_type(structured_like)
+            tree = get_sliced_input_collection_structure(structured_like)
+            if collection_type and tree.collection_type_description.collection_type != collection_type:
+                # See tool paired_collection_map_over_structured_like - type should
+                # override structured_like if they disagree.
+                tree = UnitializedTree(collection_type_descriptions.for_collection_type(collection_type))
         else:
-            collection_type = tool_output.structure.collection_type
-        tree = UnitializedTree(collection_type)
+            # Can't pre-compute the structure in this case, see if we can find a collection type.
+            if collection_type is None and tool_output.structure.collection_type_source:
+                collection_type = get_sliced_input_collection_structure(tool_output.structure.collection_type_source).collection_type_description.collection_type
 
+            if not collection_type:
+                raise Exception("Failed to determine collection type for mapping over output %s" % tool_output.name)
+
+            tree = UnitializedTree(collection_type_descriptions.for_collection_type(collection_type))
+
+    if not tree.children_known and tree.collection_type_description.collection_type == "paired":
+        # TODO: We don't need to return unitializedtree for pairs I think, we should build
+        # a paired tree for the known structure here.
+        pass
     return tree
 
 

lib/galaxy/tools/execute.py
@@ -12,7 +12,7 @@ import six
 from six.moves.queue import Queue
 
 from galaxy import model
-from galaxy.dataset_collections.structure import tool_output_to_structure
+from galaxy.dataset_collections.structure import get_structure, tool_output_to_structure
 from galaxy.tools.actions import filter_output, on_text_for_names, ToolExecutionCache
 from galaxy.tools.parser import ToolOutputCollectionPart
 from galaxy.util import ExecutionTimer
@@ -210,13 +210,14 @@ class ExecutionTracker(object):
 
         return output_collection_name
 
-    def sliced_input_collection_type(self, input_name):
+    def sliced_input_collection_structure(self, input_name):
+        input_collection = self.example_params[input_name]
+        collection_type_description = self.trans.app.dataset_collections_service.collection_type_descriptions.for_collection_type(input_collection.collection.collection_type)
+        subcollection_mapping_type = None
         if self.is_implicit_input(input_name):
             subcollection_mapping_type = self.collection_info.subcollection_mapping_type(input_name)
-            return subcollection_mapping_type
-            # return self.collection_info.structure.sliced_input_collection_type(self.implicit_inputs[input_name])
-        else:
-            return self.mapping_params.param_template[input_name].collection.collection_type
+
+        return get_structure(input_collection, collection_type_description, leaf_subcollection_type=subcollection_mapping_type)
 
     def _structure_for_output(self, trans, tool_output):
         structure = self.collection_info.structure
@@ -240,12 +241,14 @@ class ExecutionTracker(object):
 
     def _mapped_output_structure(self, trans, tool_output):
         collections_manager = trans.app.dataset_collections_service
-        output_structure = tool_output_to_structure(self.sliced_input_collection_type, tool_output, collections_manager)
+        output_structure = tool_output_to_structure(self.sliced_input_collection_structure, tool_output, collections_manager)
+        # self.collection_info.structure - the mapping structure with default_identifier_source
+        # used to determine the identifiers to use.
         mapping_structure = self._structure_for_output(trans, tool_output)
         # Output structure may not be known, but input structure must be,
         # otherwise this step of the workflow shouldn't have been scheduled
         # or the tool should not have been executable on this input.
-        mapped_output_structure = mapping_structure.multiply(output_structure, uninitialized=True)
+        mapped_output_structure = mapping_structure.multiply(output_structure)
         return mapped_output_structure
 
     def ensure_implicit_collections_populated(self, history, params):