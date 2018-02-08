40bbcc144832442da7e162a788e9744b1777a43d
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2018-02-08 09:30:32 +0100

    fix lint issue

--- lib/galaxy/webapps/galaxy/api/dynamic_actions.py
+++ lib/galaxy/webapps/galaxy/api/dynamic_actions.py
@@ -1,5 +1,5 @@
-from galaxy.web import _future_expose_api_anonymous_and_sessionless as expose_api_anonymous_and_sessionless
 from galaxy.jobs.actions.post import get_dynamic_post_processing_actions
+from galaxy.web import _future_expose_api_anonymous_and_sessionless as expose_api_anonymous_and_sessionless
 from galaxy.web.base.controller import BaseAPIController
 
 
