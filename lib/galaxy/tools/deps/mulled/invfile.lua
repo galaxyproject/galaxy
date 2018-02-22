22fef3433eea291e9ca5c8796374fd56edc31826
John Chilton <jmchilton@gmail.com>
2018-02-21 23:28:32 -0500

    Update to latest galaxy-lib.
    Includes:
    
    - Fixes for multi-requirement container building via mulled via https://github.com/galaxyproject/galaxy-lib/pull/84.
    - Fixes for Conda search problems https://github.com/galaxyproject/galaxy-lib/pull/83.
    - Updates to CWL library support https://github.com/galaxyproject/galaxy-lib/pull/78.
    - Make bz2file dependency optional (xref https://github.com/galaxyproject/galaxy-lib/pull/79)
    - Python 3 fixes for path stuff - runtime fixes caught because galaxy-lib runs tests using Python 3 https://github.com/galaxyproject/galaxy-lib/pull/85/commits/c056e39451ca8b4b9ab872b7ae12e74de5c98d48 and https://github.com/galaxyproject/galaxy-lib/pull/85/commits/c7cc9caba389770feb2913a7f0c789a4c8dec53d)
    - Python 3 fix for linter (https://github.com/galaxyproject/galaxy-lib/pull/88/commits/17d167662cda2ae4a2cccfbe0f0e9d151cc57ced).

--- lib/galaxy/tools/deps/mulled/invfile.lua
+++ lib/galaxy/tools/deps/mulled/invfile.lua
@@ -104,8 +104,8 @@ if VAR.SINGULARITY ~= '' then
         .withHostConfig({binds = {"build:/data",singularity_image_dir .. ":/import"}, privileged = true})
         .withConfig({entrypoint = {'/bin/sh', '-c'}})
         -- for small containers (less than 7MB), double the size otherwise, add a little bit more as half the conda size
-        .run("size=$(du -sc  /data/dist/ | tail -n 1 | cut -f 1 | awk '{print int($1/1024)}' ) && if [ $size -lt '7' ]; then echo $(($size*2)); else  echo $(($size+$size*7/10)); fi")
-        .run("singularity create --size `size=$(du -sc /data/dist/ | tail -n 1 | cut -f 1 | awk '{print int($1/1024)}' ) && if [ $size -lt '7' ]; then echo $(($size*2)); else  echo $(($size+$size*7/10)); fi` /import/" .. VAR.SINGULARITY_IMAGE_NAME)
+        .run("size=$(du -sc  /data/dist/ | tail -n 1 | cut -f 1 | awk '{print int($1/1024)}' ) && if [ $size -lt '10' ]; then echo 20; else  echo $(($size+$size*7/10)); fi")
+        .run("singularity create --size `size=$(du -sc /data/dist/ | tail -n 1 | cut -f 1 | awk '{print int($1/1024)}' ) && if [ $size -lt '10' ]; then echo 20; else  echo $(($size+$size*7/10)); fi` /import/" .. VAR.SINGULARITY_IMAGE_NAME)
         .run('mkdir -p /usr/local/var/singularity/mnt/container && singularity bootstrap /import/' .. VAR.SINGULARITY_IMAGE_NAME .. ' /import/Singularity')
         .run('chown ' .. VAR.USER_ID .. ' /import/' .. VAR.SINGULARITY_IMAGE_NAME)
 end
