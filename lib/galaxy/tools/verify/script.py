ec15086b679f62e6839a76166dd6797f2847eac8
John Chilton <jmchilton@gmail.com>
2018-02-23 15:23:35 -0500

    Add structured output json example to galaxy.tools.verify.script.
    Capture more information about the test.

--- lib/galaxy/tools/verify/script.py
+++ lib/galaxy/tools/verify/script.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import json
 import sys
 
 from galaxy.tools.verify.interactor import GalaxyInteractorApi, verify_tool
@@ -23,7 +24,30 @@ def main(argv=None):
     tool_version = args.tool_version
 
     galaxy_interactor = GalaxyInteractorApi(**galaxy_interactor_kwds)
-    verify_tool(tool_id, galaxy_interactor, test_index=test_index, tool_version=tool_version)
+
+    test_results = []
+
+    def _register_job_data(job_data):
+        test_results.append({
+            'id': tool_id + "-" + str(test_index),
+            'has_data': True,
+            'data': job_data,
+        })
+
+    try:
+        verify_tool(
+            tool_id, galaxy_interactor, test_index=test_index, tool_version=tool_version, register_job_data=_register_job_data
+        )
+    finally:
+        report_obj = {
+            'version': '0.1',
+            'tests': test_results,
+        }
+        output_json = args.output_json
+        if output_json:
+            with open(args.output_json, "w") as f:
+                print(report_obj)
+                json.dump(report_obj, f)
 
 
 def _arg_parser():
@@ -35,6 +59,7 @@ def _arg_parser():
     parser.add_argument('--tool-version', default=None, help='Tool Version')
     parser.add_argument('-i', '--test-index', default=0, help='Tool Test Index (starting at 0)')
     parser.add_argument('-o', '--output', default=None, help='directory to dump outputs to')
+    parser.add_argument('-j', '--output_json', default=None, help='output metadata json')
     return parser
 
 
