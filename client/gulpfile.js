f4f45c99f43545987e6ce0e35986d438c7aaacab
Dannon Baker <dannon.baker@gmail.com>
2018-03-06 15:26:19 -0500

    Drop jquery-form from bower; minor upgrade to latest V3.

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -29,6 +29,7 @@ var paths = {
         'bibtex-parse-js': [ 'bibtexParse.js', 'bibtexParse.js' ],
         'jquery': ['dist/jquery.js', 'jquery/jquery.js'],
         'jquery.complexify':     [ 'jquery.complexify.js', 'jquery/jquery.complexify.js' ],
+        'jquery-form':     [ 'jquery.form.js', 'jquery/jquery.form.js' ],
         'jquery.cookie': [ 'jquery.cookie.js', 'jquery/jquery.cookie.js' ],
         'jquery-migrate': [ 'dist/jquery-migrate.js', 'jquery/jquery.migrate.js' ],
         'jquery-mousewheel': [ 'jquery.mousewheel.js', 'jquery/jquery.mousewheel.js' ],
