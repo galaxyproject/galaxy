galaxyproject/galaxy

commit 66944ba7de178cc45ffa7a4aa9253ec4512cb6fd

Lint with Python 3 serveral modules.
Namely the modules I'd like to be shared with planemo, pulsar, etc....

lib/galaxy/util/sanitize_html.py
@@ -5,6 +5,10 @@ HTML Sanitizer (ripped from feedparser)
 import re
 import sgmllib
 
+from six import unichr
+from six import text_type as unicode
+
+
 # reversable htmlentitydefs mappings for Python 2.2
 try:
     from htmlentitydefs import name2codepoint, codepoint2name
@@ -370,7 +374,7 @@ class _HTMLSanitizer(_BaseHTMLProcessor):
 
         # declare xlink namespace, if needed
         if self.mathmlOK or self.svgOK:
-            if filter(lambda (n, v): n.startswith('xlink:'), attrs):
+            if filter(lambda n, v: n.startswith('xlink:'), attrs):
                 if not ('xmlns:xlink', 'http://www.w3.org/1999/xlink') in attrs:
                     attrs.append(('xmlns:xlink', 'http://www.w3.org/1999/xlink'))
 

.ci/flake8_py3_wrapper.sh
@@ -1,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+flake8 --exclude `paste -sd, .ci/flake8_blacklist.txt` `paste -s .ci/py3_sources.txt`

lib/galaxy/jobs/metrics/collectl/processes.py
@@ -3,11 +3,15 @@ statistics for a given pid's process and process ancestors.
 """
 import collections
 import csv
+import sys
 import tempfile
 
 from galaxy import util
 
 from ..collectl import stats
+
+if sys.version_info > (3,):
+    long = int
 
 import logging
 log = logging.getLogger( __name__ )

lib/galaxy/jobs/metrics/instrumenters/meminfo.py
@@ -1,9 +1,13 @@
 import re
-
+import sys
 from galaxy import util
 
 from ..instrumenters import InstrumentPlugin
 from ...metrics import formatting
+
+if sys.version_info > (3,):
+    long = int
+
 
 MEMINFO_LINE = re.compile(r"(\w+)\s*\:\s*(\d+) kB")
 

lib/galaxy/objectstore/s3_multipart_upload.py
@@ -24,7 +24,7 @@ except ImportError:
 def map_wrap(f):
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
-        return apply(f, *args, **kwargs)
+        return f(*args, **kwargs)
     return wrapper
 
 

lib/galaxy/tools/toolbox/base.py
@@ -798,7 +798,7 @@ class AbstractToolBox( object, Dictifiable, ManagesIntegratedToolPanelMixin ):
             tool = self._tools_by_id[ tool_id ]
             tarball_files = []
             temp_files = []
-            tool_xml = file( os.path.abspath( tool.config_file ), 'r' ).read()
+            tool_xml = open( os.path.abspath( tool.config_file ), 'r' ).read()
             # Retrieve tool help images and rewrite the tool's xml into a temporary file with the path
             # modified to be relative to the repository root.
             image_found = False
@@ -820,7 +820,7 @@ class AbstractToolBox( object, Dictifiable, ManagesIntegratedToolPanelMixin ):
             if image_found:
                 fd, new_tool_config = tempfile.mkstemp( suffix='.xml' )
                 os.close( fd )
-                file( new_tool_config, 'w' ).write( tool_xml )
+                open( new_tool_config, 'w' ).write( tool_xml )
                 tool_tup = ( os.path.abspath( new_tool_config ), os.path.split( tool.config_file )[-1]  )
                 temp_files.append( os.path.abspath( new_tool_config ) )
             else:
@@ -880,7 +880,7 @@ class AbstractToolBox( object, Dictifiable, ManagesIntegratedToolPanelMixin ):
                                 table_definition = table_definition % '\n'.join( data_table_definitions )
                                 fd, table_conf = tempfile.mkstemp()
                                 os.close( fd )
-                                file( table_conf, 'w' ).write( table_definition )
+                                open( table_conf, 'w' ).write( table_definition )
                                 tarball_files.append( ( table_conf, os.path.join( 'tool-data', 'tool_data_table_conf.xml.sample' ) ) )
                                 temp_files.append( table_conf )
             # Create the tarball.

lib/galaxy/tools/toolbox/integrated_panel.py
@@ -101,4 +101,4 @@ class ManagesIntegratedToolPanelMixin:
             shutil.copy( filename, filename + ".copy" )
             filename = filename + ".copy"
         shutil.move( filename, destination )
-        os.chmod( self._integrated_tool_panel_config, 0644 )
+        os.chmod( self._integrated_tool_panel_config, 0o644 )

lib/galaxy/util/__init__.py
@@ -27,6 +27,9 @@ import urlparse
 from galaxy.util import json
 from datetime import datetime
 
+from six import string_types, text_type
+from six.moves import xrange
+
 from email.MIMEText import MIMEText
 
 from os.path import relpath
@@ -120,7 +123,7 @@ def file_iter(fname, sep=None):
     >>> len(lines) !=  0
     True
     """
-    for line in file(fname):
+    for line in open(fname):
         if line and line[0] != '#':
             yield line.split(sep)
 
@@ -169,7 +172,7 @@ def xml_to_string( elem, pretty=False ):
         elem = pretty_print_xml( elem )
     try:
         return ElementTree.tostring( elem )
-    except TypeError, e:
+    except TypeError as e:
         # we assume this is a comment
         if hasattr( elem, 'text' ):
             return "<!-- %s -->\n" % ( elem.text )
@@ -329,7 +332,7 @@ def pretty_print_time_interval( time=False, precise=False ):
         diff = now - datetime.fromtimestamp( time )
     elif isinstance( time, datetime ):
         diff = now - time
-    elif isinstance( time, basestring ):
+    elif isinstance( time, string_types ):
         time = datetime.strptime( time, "%Y-%m-%dT%H:%M:%S.%f" )
         diff = now - time
     else:
@@ -417,7 +420,7 @@ def sanitize_text( text, valid_characters=valid_chars, character_map=mapped_char
     """
     if isinstance( text, list ):
         return map( lambda x: sanitize_text( x, valid_characters=valid_characters, character_map=character_map, invalid_character=invalid_character ), text )
-    if not isinstance( text, basestring ):
+    if not isinstance( text, string_types ):
         text = smart_str( text )
     return _sanitize_text_helper( text, valid_characters=valid_characters, character_map=character_map )
 
@@ -452,7 +455,7 @@ def sanitize_lists_to_string( values, valid_characters=valid_chars, character_ma
 
 def sanitize_param( value, valid_characters=valid_chars, character_map=mapped_chars, invalid_character='X' ):
     """Clean incoming parameters (strings or lists)"""
-    if isinstance( value, basestring ):
+    if isinstance( value, string_types ):
         return sanitize_text( value, valid_characters=valid_characters, character_map=character_map, invalid_character=invalid_character )
     elif isinstance( value, list ):
         return map( lambda x: sanitize_text( x, valid_characters=valid_characters, character_map=character_map, invalid_character=invalid_character ), value )
@@ -712,7 +715,7 @@ falsy = frozenset(['false', 'no', 'off', 'n', 'f', '0'])
 
 
 def asbool(obj):
-    if isinstance(obj, basestring):
+    if isinstance(obj, string_types):
         obj = obj.strip().lower()
         if obj in truthy:
             return True
@@ -758,7 +761,7 @@ def listify( item, do_strip=False ):
         return []
     elif isinstance( item, list ):
         return item
-    elif isinstance( item, basestring ) and item.count( ',' ):
+    elif isinstance( item, string_types ) and item.count( ',' ):
         if do_strip:
             return [token.strip() for token in item.split( ',' )]
         else:
@@ -791,10 +794,10 @@ def unicodify( value, encoding=DEFAULT_ENCODING, error='replace', default=None )
     Returns a unicode string or None
     """
 
-    if isinstance( value, unicode ):
+    if isinstance( value, text_type ):
         return value
     try:
-        return unicode( str( value ), encoding, error )
+        return text_type( str( value ), encoding, error )
     except:
         return default
 
@@ -809,12 +812,12 @@ def smart_str(s, encoding='utf-8', strings_only=False, errors='strict'):
     """
     if strings_only and isinstance(s, (type(None), int)):
         return s
-    if not isinstance(s, basestring):
+    if not isinstance(s, string_types):
         try:
             return str(s)
         except UnicodeEncodeError:
-            return unicode(s).encode(encoding, errors)
-    elif isinstance(s, unicode):
+            return text_type(s).encode(encoding, errors)
+    elif isinstance(s, text_type):
         return s.encode(encoding, errors)
     elif s and encoding != 'utf-8':
         return s.decode('utf-8', errors).encode(encoding, errors)
@@ -931,7 +934,7 @@ def read_dbnames(filename):
         man_builds.sort()
         man_builds = [(build, name) for name, build in man_builds]
         db_names = DBNames( db_names + man_builds )
-    except Exception, e:
+    except Exception as e:
         log.error( "ERROR: Unable to read builds file: %s", e )
     if len(db_names) < 1:
         db_names = DBNames( [( db_names.default_value, db_names.default_name )] )
@@ -1012,7 +1015,7 @@ def mkstemp_ln( src, prefix='mkstemp_ln_' ):
         try:
             os.link( src, file )
             return (os.path.abspath(file))
-        except OSError, e:
+        except OSError as e:
             if e.errno == errno.EEXIST:
                 continue  # try again
             raise
@@ -1026,14 +1029,14 @@ def umask_fix_perms( path, umask, unmasked_perms, gid=None ):
     perms = unmasked_perms & ~umask
     try:
         st = os.stat( path )
-    except OSError, e:
+    except OSError as e:
         log.exception( 'Unable to set permissions or group on %s' % path )
         return
     # fix modes
     if stat.S_IMODE( st.st_mode ) != perms:
         try:
             os.chmod( path, perms )
-        except Exception, e:
+        except Exception as e:
             log.warning( 'Unable to honor umask (%s) for %s, tried to set: %s but mode remains %s, error was: %s' % ( oct( umask ),
                                                                                                                       path,
                                                                                                                       oct( perms ),
@@ -1043,7 +1046,7 @@ def umask_fix_perms( path, umask, unmasked_perms, gid=None ):
     if gid is not None and st.st_gid != gid:
         try:
             os.chown( path, -1, gid )
-        except Exception, e:
+        except Exception as e:
             try:
                 desired_group = grp.getgrgid( gid )
                 current_group = grp.getgrgid( st.st_gid )
@@ -1165,26 +1168,26 @@ def send_mail( frm, to, subject, body, config ):
         try:
             s.starttls()
             log.debug( 'Initiated SSL/TLS connection to SMTP server: %s' % config.smtp_server )
-        except RuntimeError, e:
+        except RuntimeError as e:
             log.warning( 'SSL/TLS support is not available to your Python interpreter: %s' % e )
-        except smtplib.SMTPHeloError, e:
+        except smtplib.SMTPHeloError as e:
             log.error( "The server didn't reply properly to the HELO greeting: %s" % e )
             s.close()
             raise
-        except smtplib.SMTPException, e:
+        except smtplib.SMTPException as e:
             log.warning( 'The server does not support the STARTTLS extension: %s' % e )
     if config.smtp_username and config.smtp_password:
         try:
             s.login( config.smtp_username, config.smtp_password )
-        except smtplib.SMTPHeloError, e:
+        except smtplib.SMTPHeloError as e:
             log.error( "The server didn't reply properly to the HELO greeting: %s" % e )
             s.close()
             raise
-        except smtplib.SMTPAuthenticationError, e:
+        except smtplib.SMTPAuthenticationError as e:
             log.error( "The server didn't accept the username/password combination: %s" % e )
             s.close()
             raise
-        except smtplib.SMTPException, e:
+        except smtplib.SMTPException as e:
             log.error( "No suitable authentication method was found: %s" % e )
             s.close()
             raise
@@ -1195,7 +1198,7 @@ def send_mail( frm, to, subject, body, config ):
 def force_symlink( source, link_name ):
     try:
         os.symlink( source, link_name )
-    except OSError, e:
+    except OSError as e:
         if e.errno == errno.EEXIST:
             os.remove( link_name )
             os.symlink( source, link_name )

commit 4adaad4f0ee5444c6ef099f90681845c81901b82

Fix sanitazion of MathML and SVG
Broken in commit 66944ba7de178cc45ffa7a4aa9253ec4512cb6fd .

lib/galaxy/util/sanitize_html.py
@@ -349,7 +349,7 @@ class _HTMLSanitizer(_BaseHTMLProcessor):
 
         # declare xlink namespace, if needed
         if self.mathmlOK or self.svgOK:
-            if any(map(lambda n, v: n.startswith('xlink:'), attrs)):
+            if any(n_v[0].startswith('xlink:') for n_v in attrs):
                 if not ('xmlns:xlink', 'http://www.w3.org/1999/xlink') in attrs:
                     attrs.append(('xmlns:xlink', 'http://www.w3.org/1999/xlink'))
 