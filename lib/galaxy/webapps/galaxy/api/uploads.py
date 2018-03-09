d6a3964c5b41f0a9a268a1c5182fa23798526a55
guerler <aysam.guerler@gmail.com>
2018-03-09 13:29:53 -0500

    Add additional checks, use join

--- lib/galaxy/webapps/galaxy/api/uploads.py
+++ lib/galaxy/webapps/galaxy/api/uploads.py
@@ -4,7 +4,7 @@ API operations for uploaded files in storage.
 import logging
 import os
 
-from galaxy import exceptions
+from galaxy.exceptions import MessageException, NotImplemented
 from galaxy.web import expose_api_anonymous
 from galaxy.web.base.controller import BaseAPIController
 
@@ -15,7 +15,7 @@ class UploadsAPIController(BaseAPIController):
 
     @expose_api_anonymous
     def index(self, trans, **kwd):
-        raise exceptions.NotImplemented("Listing uploads is not implemented.")
+        raise NotImplemented("Listing uploads is not implemented.")
 
     @expose_api_anonymous
     def create(self, trans, payload, **kwd):
@@ -24,19 +24,24 @@ class UploadsAPIController(BaseAPIController):
         """
         session_id = payload.get("session_id")
         session_start = payload.get("session_start")
+        session_chunk = payload.get("session_chunk")
         if session_id is None:
-            raise exceptions.MessageException("Requires a session id.")
+            raise MessageException("Requires a session id.")
         if session_start is None:
-            raise exceptions.MessageException("Requires a session start.")
-        target_file = "%s/%s" % (trans.app.config.new_file_path, session_id)
+            raise MessageException("Requires a session start.")
+        if not hasattr(session_chunk, "file"):
+            raise MessageException("Requires a session chunk.")
+        target_file = os.path.join(trans.app.config.new_file_path, session_id)
         target_size = 0
         if os.path.exists(target_file):
             target_size = os.path.getsize(target_file)
         if session_start != target_size:
-            raise exceptions.MessageException("Incorrect session start.")
-        source = payload.get("session_chunk")
+            raise MessageException("Incorrect session start.")
+        chunk_size = os.fstat(session_chunk.file.fileno()).st_size / 1048576
+        if chunk_size > trans.app.config.chunk_upload_size:
+            raise MessageException("Invalid chunk size.")
         with open(target_file, "a") as f:
-            f.write(source.file.read())
+            f.write(session_chunk.file.read())
             f.close()
-        source.file.close()
+        session_chunk.file.close()
         return {"message": "Successful."}
