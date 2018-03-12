a4287f1212050c7ff068ad161ce2d17cd65bd7cb
guerler <aysam.guerler@gmail.com>
2018-03-12 17:36:32 -0400

    Test chunked uploads

--- test/base/driver_util.py
+++ test/base/driver_util.py
@@ -191,6 +191,7 @@ def setup_galaxy_config(
         api_allow_run_as='test@bx.psu.edu',
         auto_configure_logging=logging_config_file is None,
         check_migrate_tools=False,
+        chunk_upload_size=0.001,
         conda_auto_init=conda_auto_init,
         conda_auto_install=conda_auto_install,
         cleanup_job='onsuccess',
