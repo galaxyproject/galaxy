cb5e81b6a1209ef89dd7edc9bdd870c8953a96a5
Martin Cech <marten@bx.psu.edu>
2018-03-12 14:37:22 -0400

    replace incorrect list add syntax

--- lib/galaxy/managers/libraries.py
+++ lib/galaxy/managers/libraries.py
@@ -145,13 +145,13 @@ class LibraryManager(object):
             allowed_library_manage_ids = set([])
             for action in all_actions:
                 if action.action == library_access_action:
-                    accessible_restricted_library_ids.add(action.library_id)
+                    accessible_restricted_library_ids.append(action.library_id)
                 if action.action == library_add_action:
-                    allowed_library_add_ids.add(action.library_id)
+                    allowed_library_add_ids.append(action.library_id)
                 if action.action == library_modify_action:
-                    allowed_library_modify_ids.add(action.library_id)
+                    allowed_library_modify_ids.append(action.library_id)
                 if action.action == library_manage_action:
-                    allowed_library_manage_ids.add(action.library_id)
+                    allowed_library_manage_ids.append(action.library_id)
             query = query.filter(or_(
                 not_(trans.model.Library.table.c.id.in_(restricted_library_ids)),
                 trans.model.Library.table.c.id.in_(accessible_restricted_library_ids)
