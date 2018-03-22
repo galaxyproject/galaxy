galaxyproject/galaxy

commit a45cbfeb8d7d7cc3df5e2561b422023986e805d0

Stronger assertion about link_data_only option in upload.py.

tools/data_source/upload.py
@@ -83,7 +83,10 @@ def add_file(dataset, registry, json_file, output_path):
     line_count = None
     converted_path = None
     stdout = None
-    link_data_only = dataset.get('link_data_only', 'copy_files') != 'copy_files'
+    link_data_only_str = dataset.get('link_data_only', 'copy_files')
+    if link_data_only_str not in ['link_data_only', 'copy_files']:
+        raise UploadProblemException("Invalid setting for option link_data_only - upload request misconfigured.")
+    link_data_only = link_data_only_str == 'link_data_only'
 
     # run_as_real_user is estimated from galaxy config (external chmod indicated of inputs executed)
     # If this is True we always purge supplied upload inputs so they are cleaned up and we reuse their

commit 6e36af5c0ec47a3ae14edd99f1044c8fb8b26d9a

Fix linking option for library dataset uploads
Broken in commit a45cbfeb8d7d7cc3df5e2561b422023986e805d0 .

Found by BioBlend tests:
https://travis-ci.org/galaxyproject/bioblend/jobs/356761960

tools/data_source/upload.py
@@ -68,9 +68,9 @@ def add_file(dataset, registry, output_path):
     converted_path = None
     stdout = None
     link_data_only_str = dataset.get('link_data_only', 'copy_files')
-    if link_data_only_str not in ['link_data_only', 'copy_files']:
-        raise UploadProblemException("Invalid setting for option link_data_only - upload request misconfigured.")
-    link_data_only = link_data_only_str == 'link_data_only'
+    if link_data_only_str not in ['link_to_files', 'copy_files']:
+        raise UploadProblemException("Invalid setting '%s' for option link_data_only - upload request misconfigured" % link_data_only_str)
+    link_data_only = link_data_only_str == 'link_to_files'
 
     # run_as_real_user is estimated from galaxy config (external chmod indicated of inputs executed)
     # If this is True we always purge supplied upload inputs so they are cleaned up and we reuse their