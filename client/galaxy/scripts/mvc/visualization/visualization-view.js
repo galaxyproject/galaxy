b147abe9e58d21ee29722cedbdd12c354a6b095b
Dannon Baker <dannon.baker@gmail.com>
2018-03-23 09:07:29 -0400

    Minor es6'ing in vizview

--- client/galaxy/scripts/mvc/visualization/visualization-view.js
+++ client/galaxy/scripts/mvc/visualization/visualization-view.js
@@ -1,10 +1,15 @@
 /**
  *  Visualization selection grid
  */
+import * as Backbone from "backbone";
+import * as _ from "underscore";
 import Thumbnails from "mvc/ui/ui-thumbnails";
+
+/* global $ */
+/* global Galaxy */
+
 var View = Backbone.View.extend({
     initialize: function(options) {
-        var self = this;
         this.setElement($("<div/>"));
         this.model = new Backbone.Model();
         this.active_tab = "visualization";
@@ -12,7 +17,7 @@ var View = Backbone.View.extend({
             url: `${Galaxy.root}api/datasets/${options.dataset_id}`
         }).done(dataset => {
             if (dataset.visualizations.length === 0) {
-                self.$el.append(
+                this.$el.append(
                     $("<div/>")
                         .addClass("errormessagelarge")
                         .append(
@@ -22,22 +27,21 @@ var View = Backbone.View.extend({
                         )
                 );
             } else {
-                self.model.set({ dataset: dataset });
-                self.render();
+                this.model.set({ dataset: dataset });
+                this.render();
             }
         });
     },
 
     render: function() {
-        var self = this;
         var dataset = this.model.get("dataset");
         this.vis_index = {};
         this.vis_array = [];
         _.each(
-            dataset.visualizations.sort(function(a, b) {
+            dataset.visualizations.sort((a, b) => {
                 return a.html < b.html ? -1 : 1;
             }),
-            function(v, id) {
+            (v, id) => {
                 var dict = {
                     id: id,
                     name: v.name,
@@ -48,16 +52,16 @@ var View = Backbone.View.extend({
                     regular: v.regular,
                     visualization: v
                 };
-                self.vis_index[dict.id] = dict;
-                self.vis_array.push(dict);
+                this.vis_index[dict.id] = dict;
+                this.vis_array.push(dict);
             }
         );
         this.types = new Thumbnails.View({
             title_default: "Suggested plugins",
             title_list: "List of available plugins",
             collection: this.vis_array,
-            onclick: function(id) {
-                var vis_type = self.vis_index[id];
+            onclick: id => {
+                var vis_type = this.vis_index[id];
                 $("#galaxy_main").attr("src", vis_type.visualization.href);
             }
         });
