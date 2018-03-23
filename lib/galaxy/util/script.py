a177173672b86f4da8f83c3068dcb63f5f49cc40
John Chilton <jmchilton@gmail.com>
2018-03-23 12:28:02 -0400

    Fix missing ENV information in config parameter args.

--- lib/galaxy/util/script.py
+++ lib/galaxy/util/script.py
@@ -11,6 +11,19 @@ ACTIONS = None
 ARGUMENTS = None
 DEFAULT_ACTION = None
 
+ARG_HELP_CONFIG_FILE = """
+Galaxy config file (defaults to $GALAXY_ROOT/config/galaxy.yml if that file exists
+or else to $GALAXY_ROOT/config/galaxy.ini if that exists). If this isn't set on the
+command line it can be set with the environment variable GALAXY_CONFIG_FILE.
+"""
+
+ARG_HELP_CONFIG_SECTION = """
+Section containing application configuration in the target config file specified with
+-c/--config-file. This defaults to 'galaxy' for YAML/JSON configuration files and 'main'
+with 'app:' prepended for INI. If this isn't set on the command line it can be set with
+the environment variable GALAXY_CONFIG_SECTION.
+"""
+
 
 def main_factory(description=None, actions=None, arguments=None, default_action=None):
     global DESCRIPTION, ACTIONS, ARGUMENTS, DEFAULT_ACTION
@@ -47,12 +60,17 @@ def config_file_from_args(args, legacy_config_override=None):
 def populate_config_args(parser):
     # config and config-file respected because we have used different arguments at different
     # time for scripts.
+
+    # Options (e.g. option_name) not found in this file can have their defaults overridden
+    # set setting GALAXY_CONFIG_OPTION_NAME where OPTION_NAME is option_name converted to upper case.
+    # Options specified in that file can be overridden for this program set setting
+    # GALAXY_CONFIG_OVERRIDE_OPTION_NAME to a new value.
     parser.add_argument("-c", "--config-file", "--config",
                         default=os.environ.get('GALAXY_CONFIG_FILE', None),
-                        help="Galaxy config file (defaults to config/galaxy.ini or config/galaxy.yml)")
+                        help=ARG_HELP_CONFIG_FILE)
     parser.add_argument("--config-section",
                         default=os.environ.get('GALAXY_CONFIG_SECTION', None),
-                        help="app section in config file (defaults to 'galaxy' for YAML/JSON, 'main' (w/ 'app:' prepended) for INI")
+                        help=ARG_HELP_CONFIG_SECTION)
 
 
 def _arg_parser():
