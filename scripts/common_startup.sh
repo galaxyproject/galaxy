60ca4d4cc94966598c102ea5a473270ea15e9b22
Dannon Baker <dannon.baker@gmail.com>
2018-03-28 12:42:37 -0400

    Instead of using -q, send to /dev/null, since the early pipe exit blows up pip

--- scripts/common_startup.sh
+++ scripts/common_startup.sh
@@ -187,7 +187,7 @@ if [ $FETCH_WHEELS -eq 1 ]; then
     pip install $requirement_args --index-url "${GALAXY_WHEELS_INDEX_URL}" --extra-index-url "${PYPI_INDEX_URL}"
     GALAXY_CONDITIONAL_DEPENDENCIES=$(PYTHONPATH=lib python -c "from __future__ import print_function; import galaxy.dependencies; print('\n'.join(galaxy.dependencies.optional('$GALAXY_CONFIG_FILE')))")
     if [ -n "$GALAXY_CONDITIONAL_DEPENDENCIES" ]; then
-        if pip list --format=columns | grep -q "psycopg2[\(\ ]*2.7.3"; then
+        if pip list --format=columns | grep "psycopg2[\(\ ]*2.7.3" > /dev/null; then
             echo "An older version of psycopg2 (non-binary, version 2.7.3) has been detected.  Galaxy now uses psycopg2-binary, which will be installed after removing psycopg2."
             pip uninstall -y psycopg2==2.7.3
         fi
