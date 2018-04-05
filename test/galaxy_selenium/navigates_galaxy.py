c27434bcf380fb2267dcf12c4c35b80742838ec8
John Chilton <jmchilton@gmail.com>
2018-04-04 21:47:26 -0400

    Fix workflow rename test case for BS4 branch.

--- test/galaxy_selenium/navigates_galaxy.py
+++ test/galaxy_selenium/navigates_galaxy.py
@@ -938,7 +938,7 @@ class NavigatesGalaxy(HasDriver):
     def workflow_index_name(self, workflow_index=0):
         """Get workflow name for workflow_index'th row."""
         row_element = self.workflow_index_table_row(workflow_index=workflow_index)
-        workflow_button = row_element.find_element_by_css_selector(".menubutton")
+        workflow_button = row_element.find_element_by_css_selector(".dropdown-toggle")
         return workflow_button.text
 
     def workflow_index_click_option(self, option_title, workflow_index=0):
@@ -951,7 +951,7 @@ class NavigatesGalaxy(HasDriver):
 
         click_option()
 
-        menu_element = self.wait_for_selector_visible(".dropdown.show")
+        menu_element = self.wait_for_selector_visible(".dropdown-menu.show")
         menu_options = menu_element.find_elements_by_css_selector("a.dropdown-item")
         found_option = False
         for menu_option in menu_options:
