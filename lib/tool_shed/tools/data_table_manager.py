galaxyproject/galaxy

commit 7df9dce63d635d66a0a9ce5045135ab49b7ef506

Rename the ToolDataTableManager that deals with repository data to ShedToolDataTableManager to avoid confusion

lib/tool_shed/tools/data_table_manager.py
@@ -9,7 +9,7 @@ from tool_shed.util import hg_util, xml_util
 log = logging.getLogger(__name__)
 
 
-class ToolDataTableManager(object):
+class ShedToolDataTableManager(object):
 
     def __init__(self, app):
         self.tdtm = tdtm(app.config.shed_tool_data_path)

lib/galaxy/webapps/tool_shed/controllers/upload.py
@@ -17,7 +17,6 @@ from tool_shed.dependencies import attribute_handlers
 from tool_shed.galaxy_install import dependency_display
 from tool_shed.metadata import repository_metadata_manager
 from tool_shed.repository_types import util as rt_util
-from tool_shed.tools import data_table_manager
 from tool_shed.util import (
     basic_util,
     commit_util,
@@ -28,6 +27,7 @@ from tool_shed.util import (
     xml_util
 )
 from tool_shed.util.web_util import escape
+from tool_shed.tools.data_table_manager import ShedToolDataTableManager
 
 log = logging.getLogger(__name__)
 
@@ -99,7 +99,7 @@ class UploadController(BaseUIController):
             if uploaded_file or uploaded_directory:
                 rdah = attribute_handlers.RepositoryDependencyAttributeHandler(trans.app, unpopulate=False)
                 tdah = attribute_handlers.ToolDependencyAttributeHandler(trans.app, unpopulate=False)
-                tdtm = data_table_manager.ToolDataTableManager(trans.app)
+                stdtm = ShedToolDataTableManager(trans.app)
                 ok = True
                 isgzip = False
                 isbz2 = False
@@ -223,7 +223,7 @@ class UploadController(BaseUIController):
                                 # Handle the special case where a tool_data_table_conf.xml.sample file is being uploaded
                                 # by parsing the file and adding new entries to the in-memory trans.app.tool_data_tables
                                 # dictionary.
-                                error, error_message = tdtm.handle_sample_tool_data_table_conf_file(full_path, persist=False)
+                                error, error_message = stdtm.handle_sample_tool_data_table_conf_file(full_path, persist=False)
                                 if error:
                                     message = '%s<br/>%s' % (message, error_message)
                             # See if the content of the change set was valid.
@@ -312,7 +312,7 @@ class UploadController(BaseUIController):
                         message += invalid_repository_dependencies_message
                         status = 'error'
                     # Reset the tool_data_tables by loading the empty tool_data_table_conf.xml file.
-                    tdtm.reset_tool_data_tables()
+                    stdtm.reset_tool_data_tables()
                     if uploaded_directory:
                         basic_util.remove_dir(uploaded_directory)
                     trans.response.send_redirect(web.url_for(controller='repository',
@@ -326,7 +326,7 @@ class UploadController(BaseUIController):
                         basic_util.remove_dir(uploaded_directory)
                     status = 'error'
                 # Reset the tool_data_tables by loading the empty tool_data_table_conf.xml file.
-                tdtm.reset_tool_data_tables()
+                stdtm.reset_tool_data_tables()
         return trans.fill_template('/webapps/tool_shed/repository/upload.mako',
                                    repository=repository,
                                    changeset_revision=tip,

lib/tool_shed/galaxy_install/install_manager.py
@@ -19,7 +19,7 @@ from tool_shed.galaxy_install.tool_dependencies.recipe.install_environment impor
 from tool_shed.galaxy_install.tool_dependencies.recipe.recipe_manager import StepManager
 from tool_shed.galaxy_install.tool_dependencies.recipe.recipe_manager import TagManager
 from tool_shed.galaxy_install.tools import data_manager, tool_panel_manager
-from tool_shed.tools import data_table_manager
+from tool_shed.tools.data_table_manager import ShedToolDataTableManager
 from tool_shed.util import basic_util, encoding_util, hg_util, repository_util
 from tool_shed.util import shed_util_common as suc, tool_dependency_util
 from tool_shed.util import tool_util, xml_util
@@ -499,7 +499,7 @@ class InstallRepositoryManager(object):
         reinstalling an uninstalled repository.
         """
         shed_config_dict = self.app.toolbox.get_shed_config_dict_by_filename(shed_tool_conf)
-        tdtm = data_table_manager.ToolDataTableManager(self.app)
+        stdtm = ShedToolDataTableManager(self.app)
         irmm = InstalledRepositoryMetadataManager(app=self.app,
                                                   tpm=self.tpm,
                                                   repository=tool_shed_repository,
@@ -527,9 +527,9 @@ class InstallRepositoryManager(object):
                                                                 set_status=True)
         if 'sample_files' in irmm_metadata_dict:
             sample_files = irmm_metadata_dict.get('sample_files', [])
-            tool_index_sample_files = tdtm.get_tool_index_sample_files(sample_files)
+            tool_index_sample_files = stdtm.get_tool_index_sample_files(sample_files)
             tool_data_table_conf_filename, tool_data_table_elems = \
-                tdtm.install_tool_data_tables(tool_shed_repository, tool_index_sample_files)
+                stdtm.install_tool_data_tables(tool_shed_repository, tool_index_sample_files)
             if tool_data_table_elems:
                 self.app.tool_data_tables.add_new_entries_from_config_file(tool_data_table_conf_filename,
                                                                            self.app.config.shed_tool_data_path,
@@ -538,13 +538,13 @@ class InstallRepositoryManager(object):
         if 'tools' in irmm_metadata_dict:
             tool_panel_dict = self.tpm.generate_tool_panel_dict_for_new_install(irmm_metadata_dict['tools'], tool_section)
             sample_files = irmm_metadata_dict.get('sample_files', [])
-            tool_index_sample_files = tdtm.get_tool_index_sample_files(sample_files)
+            tool_index_sample_files = stdtm.get_tool_index_sample_files(sample_files)
             tool_util.copy_sample_files(self.app, tool_index_sample_files, tool_path=tool_path)
             sample_files_copied = [str(s) for s in tool_index_sample_files]
             repository_tools_tups = irmm.get_repository_tools_tups()
             if repository_tools_tups:
                 # Handle missing data table entries for tool parameters that are dynamically generated select lists.
-                repository_tools_tups = tdtm.handle_missing_data_table_entry(relative_install_dir,
+                repository_tools_tups = stdtm.handle_missing_data_table_entry(relative_install_dir,
                                                                              tool_path,
                                                                              repository_tools_tups)
                 # Handle missing index files for tool parameters that are dynamically generated select lists.

lib/tool_shed/tools/tool_validator.py
@@ -7,8 +7,10 @@ from galaxy.tools import (
     parameters,
     Tool
 )
+from galaxy.tools.data import ToolDataTableManager
+from galaxy.tools.fetcher import ToolLocationFetcher
 from galaxy.tools.parameters import dynamic_options
-from tool_shed.tools import data_table_manager
+from tool_shed.tools.data_table_manager import ShedToolDataTableManager
 from tool_shed.util import (
     basic_util,
     hg_util,
@@ -24,7 +26,7 @@ class ToolValidator(object):
 
     def __init__(self, app):
         self.app = app
-        self.tdtm = data_table_manager.ToolDataTableManager(self.app)
+        self.stdtm = ShedToolDataTableManager(self.app)
 
     def can_use_tool_config_disk_file(self, repository, repo, file_path, changeset_revision):
         """
@@ -67,7 +69,7 @@ class ToolValidator(object):
                         sample_tool_data_table_conf = hg_util.get_config_from_disk('tool_data_table_conf.xml.sample', repo_dir)
                         if sample_tool_data_table_conf:
                             error, correction_msg = \
-                                self.tdtm.handle_sample_tool_data_table_conf_file(sample_tool_data_table_conf,
+                                self.stdtm.handle_sample_tool_data_table_conf_file(sample_tool_data_table_conf,
                                                                                   persist=False)
                             if error:
                                 invalid_files_and_errors_tups.append(('tool_data_table_conf.xml.sample', correction_msg))
@@ -204,7 +206,7 @@ class ToolValidator(object):
             if 'tool_data_table_conf.xml.sample' in sample_files:
                 # Load entries into the tool_data_tables if the tool requires them.
                 tool_data_table_config = os.path.join(work_dir, 'tool_data_table_conf.xml')
-                error, message = self.tdtm.handle_sample_tool_data_table_conf_file(tool_data_table_config,
+                error, message = self.stdtm.handle_sample_tool_data_table_conf_file(tool_data_table_config,
                                                                                    persist=False)
         tool, valid, message2 = self.load_tool_from_config(repository_id, tool_config_filepath)
         message = self.concat_messages(message, message2)
@@ -224,7 +226,7 @@ class ToolValidator(object):
                 # Load entries into the tool_data_tables if the tool requires them.
                 tool_data_table_config = os.path.join(work_dir, 'tool_data_table_conf.xml')
                 if tool_data_table_config:
-                    error, message = self.tdtm.handle_sample_tool_data_table_conf_file(tool_data_table_config,
+                    error, message = self.stdtm.handle_sample_tool_data_table_conf_file(tool_data_table_config,
                                                                                        persist=False)
                     if error:
                         log.debug(message)
@@ -286,7 +288,7 @@ class ToolValidator(object):
         basic_util.remove_dir(work_dir)
         self.app.config.tool_data_path = original_tool_data_path
         # Reset the tool_data_tables by loading the empty tool_data_table_conf.xml file.
-        self.tdtm.reset_tool_data_tables()
+        self.stdtm.reset_tool_data_tables()
         return repository, tool, message
 
     def load_tool_from_config(self, repository_id, full_path):

lib/tool_shed/util/commit_util.py
@@ -12,7 +12,7 @@ from sqlalchemy.sql.expression import null
 import tool_shed.repository_types.util as rt_util
 from galaxy.util import checkers
 from galaxy.util.path import safe_relpath
-from tool_shed.tools import data_table_manager
+from tool_shed.tools.data_table_manager import ShedToolDataTableManager
 from tool_shed.util import basic_util, hg_util, shed_util_common as suc
 
 if sys.version_info < (3, 3):
@@ -214,8 +214,8 @@ def handle_directory_changes(app, host, username, repository, full_path, filenam
             # Handle the special case where a tool_data_table_conf.xml.sample file is being uploaded
             # by parsing the file and adding new entries to the in-memory app.tool_data_tables
             # dictionary.
-            tdtm = data_table_manager.ToolDataTableManager(app)
-            error, message = tdtm.handle_sample_tool_data_table_conf_file(filename_in_archive, persist=False)
+            stdtm = ShedToolDataTableManager(app)
+            error, message = stdtm.handle_sample_tool_data_table_conf_file(filename_in_archive, persist=False)
             if error:
                 return False, message, files_to_remove, content_alert_str, undesirable_dirs_removed, undesirable_files_removed
     hg_util.commit_changeset(repo.ui,

commit cd9db0bffa68cec4891f38db77af578757d3b63d

Provide ToolDataTableManager for backwards compatibility
Broken in 7df9dce63d635d66a0a9ce5045135ab49b7ef506

lib/tool_shed/tools/data_table_manager.py
@@ -169,3 +169,7 @@ class ShedToolDataTableManager(object):
     def reset_tool_data_tables(self):
         # Reset the tool_data_tables to an empty dictionary.
         self.app.tool_data_tables.data_tables = {}
+
+
+# For backwards compatibility with exisiting data managers
+ToolDataTableManager = ShedToolDataTableManager