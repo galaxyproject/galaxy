cd1326c925c59c0071bd1360c5e7e98ed8f2549f
John Chilton <jmchilton@gmail.com>
2018-04-20 07:10:52 -0400

    WIP: Work toward Galaxy-flavored CWL tools.
    gx:Interface.
    
    Co-authored-by: John Chilton <jmchilton@gmail.com>
    Co-authored-by: jra001

--- lib/galaxy_test/api/test_tools_cwl.py
+++ lib/galaxy_test/api/test_tools_cwl.py
@@ -93,6 +93,34 @@ class TestCwlTools(ApiTestCase):
         stdout = self._get_job_stdout(run_object.job_id)
         assert stdout == "Hello world!\n"
 
+    @skip_without_tool("galactic_cat.cwl#galactic_cat")
+    def test_galactic_cat_1(self, history_id: str) -> None:
+        hda_id = self.dataset_populator.new_dataset(history_id, name="test_dataset.txt")["id"]
+        self.dataset_populator.wait_for_history(history_id, assert_ok=True)
+        inputs = {"input1": {"src": "hda", "id": hda_id}}
+        run_response = self._run("galactic_cat.cwl#galactic_cat", history_id, inputs, assert_ok=True)
+        dataset = run_response["outputs"][0]
+        content = self.dataset_populator.get_history_dataset_content(history_id, dataset=dataset)
+        assert content.strip() == "TestData123", content
+
+    @skip_without_tool("galactic_record_input.cwl#galactic_record_input")
+    def test_galactic_record_input(self, history_id: str) -> None:
+        hda1_id = self.dataset_populator.new_dataset(history_id, content="moo", name="test_dataset.txt")["id"]
+        hda2_id = self.dataset_populator.new_dataset(history_id, content="cow dog foo", name="test_dataset.txt")["id"]
+        self.dataset_populator.wait_for_history(history_id, assert_ok=True)
+        inputs = {
+            "input1": {"src": "hda", "id": hda1_id},
+            "input2": {"src": "hda", "id": hda2_id},
+        }
+        run_response = self._run("galactic_record_input.cwl#galactic_record_input", history_id, inputs, assert_ok=True)
+        dataset = run_response["outputs"][0]
+        content = self.dataset_populator.get_history_dataset_content(history_id, dataset=dataset)
+        assert content.strip() == "moo", content
+
+        dataset = run_response["outputs"][1]
+        content = self.dataset_populator.get_history_dataset_content(history_id, dataset=dataset)
+        assert content.strip() == "cow dog foo", content
+
     def _run_and_get_stdout(self, tool_id: str, history_id: str, inputs: Dict[str, Any], **kwds) -> str:
         response = self._run(tool_id, history_id, inputs, **kwds)
         assert "jobs" in response
