eeb1dae5b71200ffe9f2056b2a7fd8ed560033c1
Nate Coraor <nate@bx.psu.edu>
2018-05-10 06:31:00 -0400

    Make JobWrapper/TaskWrapper's dataset_path_rewriter a property

--- lib/galaxy/jobs/__init__.py
+++ lib/galaxy/jobs/__init__.py
@@ -714,7 +714,7 @@ class JobWrapper(HasResourceParameters):
         self._create_working_directory()
         # the path rewriter needs destination params, so it cannot be set up until after the destination has been
         # resolved
-        self.dataset_path_rewriter = None
+        self.__dataset_path_rewriter = None
         self.output_paths = None
         self.output_hdas_and_paths = None
         self.tool_provided_job_metadata = None
@@ -731,13 +731,19 @@ class JobWrapper(HasResourceParameters):
         self.__user_system_pwent = None
         self.__galaxy_system_pwent = None
 
-    def _job_dataset_path_rewriter(self, working_directory):
-        outputs_to_working_directory = util.asbool(self.get_destination_configuration("outputs_to_working_directory", False))
-        if outputs_to_working_directory:
-            dataset_path_rewriter = OutputsToWorkingDirectoryPathRewriter(working_directory)
-        else:
-            dataset_path_rewriter = NullDatasetPathRewriter()
-        return dataset_path_rewriter
+    @property
+    def _job_dataset_path_rewriter(self):
+        if self.__dataset_path_rewriter is None:
+            outputs_to_working_directory = util.asbool(self.get_destination_configuration("outputs_to_working_directory", False))
+            if outputs_to_working_directory:
+                self.__dataset_path_rewriter = OutputsToWorkingDirectoryPathRewriter(self.working_directory)
+            else:
+                self.__dataset_path_rewriter = NullDatasetPathRewriter()
+        return self.__dataset_path_rewriter
+
+    @property
+    def dataset_path_rewriter(self):
+        return self._job_dataset_path_rewriter
 
     @property
     def cleanup_job(self):
@@ -803,10 +809,7 @@ class JobWrapper(HasResourceParameters):
 
         :returns: ``JobDestination``
         """
-        dest = self.job_runner_mapper.get_job_destination(self.params)
-        if self.dataset_path_rewriter is None:
-            self.dataset_path_rewriter = self._job_dataset_path_rewriter(self.working_directory)
-        return dest
+        return self.job_runner_mapper.get_job_destination(self.params)
 
     def get_job(self):
         return self.sa_session.query(model.Job).get(self.job_id)
@@ -1872,15 +1875,18 @@ class TaskWrapper(JobWrapper):
     def __init__(self, task, queue):
         super(TaskWrapper, self).__init__(task.job, queue)
         self.task_id = task.id
-        working_directory = task.working_directory
-        self.working_directory = working_directory
-        job_dataset_path_rewriter = self._job_dataset_path_rewriter(self.working_directory)
-        self.dataset_path_rewriter = TaskPathRewriter(working_directory, job_dataset_path_rewriter)
+        self.working_directory = task.working_directory
         if task.prepare_input_files_cmd is not None:
             self.prepare_input_files_cmds = [task.prepare_input_files_cmd]
         else:
             self.prepare_input_files_cmds = None
         self.status = task.states.NEW
+
+    @property
+    def dataset_path_rewriter(self):
+        if self.__dataset_path_rewriter is None:
+            self.__dataset_path_rewriter = TaskPathRewriter(self.working_directory, self._job_dataset_path_rewriter)
+        return self.__dataset_path_rewriter
 
     def can_split(self):
         # Should the job handler split this job up? TaskWrapper should
