3cfe8040b6f239a53bf1e048104c7d9af7958149
Helena Rasche <hxr@hx42.org>
2018-05-10 18:36:59 +0200

    allow tests to pass

--- test/unit/unittest_utils/galaxy_mock.py
+++ test/unit/unittest_utils/galaxy_mock.py
@@ -129,6 +129,7 @@ class MockAppConfig(Bunch):
 
         self.migrated_tools_config = "/tmp/migrated_tools_conf.xml"
         self.preserve_python_environment = "always"
+        self.gdpr_compliance_mode = False
 
         # set by MockDir
         self.root = root
