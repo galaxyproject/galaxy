148d56a9c9d12df6d3d8fbe48a713f75cca6a468
John Chilton <jmchilton@gmail.com>
2018-05-10 14:17:25 -0400

    Many more matches_any tests, better categorization of them with comments.
    Some of these suggested by @nsoranzo.

--- test/unit/datatypes/test_datatypes_registry.py
+++ test/unit/datatypes/test_datatypes_registry.py
@@ -34,18 +34,49 @@ def test_matches_any():
     # Test inheritance.
     assert fastqsanger_datatype.matches_any([fastq_datatype])
     assert fastqsanger_datatype.matches_any([fastq_datatype.__class__])
+    assert not fastq_datatype.matches_any([fastqsanger_datatype])
+    assert not fastq_datatype.matches_any([fastqsanger_datatype.__class__])
 
     # Test dynamic subclass handling.
     assert not h5_datatype.matches_any([mz5_datatype])
     assert mz5_datatype.matches_any([h5_datatype])
 
-    # Test compressed datatype handling.
+    # Test compressed datatype handling - matching direct.
+    assert fastqsangergz_datatype.matches_any([fastqsangergz_datatype])
+    assert fastqsangergz_datatype.matches_any([fastqsangergz_datatype.__class__])
+    assert fastqsangerbz2_datatype.matches_any([fastqsangerbz2_datatype])
+    assert fastqsangerbz2_datatype.matches_any([fastqsangerbz2_datatype.__class__])
+
+    # Test compressed datatype handling - matching in compressed hierarchy.
+    assert fastqsangergz_datatype.matches_any([fastqgz_datatype])
+    assert fastqsangergz_datatype.matches_any([fastqgz_datatype.__class__])
+    assert fastqsangerbz2_datatype.matches_any([fastqbz2_datatype])
+    assert fastqsangerbz2_datatype.matches_any([fastqbz2_datatype.__class__])
+
+    # Test compressed datatype handling - matching as raw compressed data.
+    assert fastqsangergz_datatype.matches_any([data_datatype])
+    assert fastqsangergz_datatype.matches_any([data_datatype.__class__])
+    assert fastqsangerbz2_datatype.matches_any([data_datatype])
+    assert fastqsangerbz2_datatype.matches_any([data_datatype.__class__])
+
+    # Test compressed datatype handling - everything else mismatches.
     assert not data_datatype.matches_any([fastqsangergz_datatype])
     assert not fastq_datatype.matches_any([fastqsangergz_datatype])
     assert not fastqsanger_datatype.matches_any([fastqsangergz_datatype])
-    assert fastqsangergz_datatype.matches_any([data_datatype])
+    assert not fastqgz_datatype.matches_any([fastqsangergz_datatype])
+    assert not fastqgz_datatype.matches_any([fastqsangergz_datatype.__class__])
     assert not fastqsangergz_datatype.matches_any([fastqsanger_datatype])
-    assert fastqsangergz_datatype.matches_any([fastqgz_datatype])
     assert not fastqsangerbz2_datatype.matches_any([fastqgz_datatype])
+    assert not fastqsangerbz2_datatype.matches_any([fastqgz_datatype.__class__])
     assert not fastqsangerbz2_datatype.matches_any([fastqsangergz_datatype])
-    assert fastqsangerbz2_datatype.matches_any([fastqbz2_datatype])
+    assert not fastqsangerbz2_datatype.matches_any([fastqsangergz_datatype.__class__])
+
+    # Test matches after first arg.
+    assert fasta_datatype.matches_any([fastq_datatype, data_datatype])
+    assert fasta_datatype.matches_any([fastq_datatype, h5_datatype, data_datatype])
+    assert fastqsangerbz2_datatype.matches_any([fastqsangergz_datatype.__class__, fastqsangerbz2_datatype])
+    assert fastqsangerbz2_datatype.matches_any([fastqsangergz_datatype.__class__, fastqsangerbz2_datatype.__class__])
+
+    # Test mismatches in multiple args.
+    assert not fasta_datatype.matches_any([fastq_datatype, h5_datatype])
+    assert not fasta_datatype.matches_any([fastq_datatype.__class__, h5_datatype.__class__])
