e814b75dc27db739dcfe7d3b43f9c17b6510c1eb
Dannon Baker <dannon.baker@gmail.com>
2018-05-16 15:16:44 -0400

    Floating this card approach for dialogs like this -- if we design components for display in cards, they'll be portable and usable everywhere.

--- client/galaxy/scripts/components/HistoryImport.vue
+++ client/galaxy/scripts/components/HistoryImport.vue
@@ -1,28 +1,17 @@
 <template>
-    <div class="ui-portlet-limited">
-        <div class="portlet-header">
-            <div class="portlet-title">
-                <i class="portlet-title-icon fa fa-upload"></i>
-                <span class="portlet-title-text"><b>Import a History from an Archive</b></span>
-            </div>
-        </div>
-        <div class="portlet-content">
-            <b-form @submit="submit">
-                <b-alert class="ui-message visible d-block" :show="hasErrorMessage" variant="danger">{{ errorMessage }}</b-alert>
-                <div class="ui-form-element">
-                    <div class="ui-form-title">Archived History URL</div>
-                    <b-form-input type="url" v-model="sourceURL"/>
-                </div>
-                <div class="ui-form-element">
-                    <div class="ui-form-title">Archived History file</div>
-                    <b-form-file v-model="sourceFile"/>
-                </div>
-                <div class="portlet-buttons">
-                    <b-button type="submit">Import history</b-button>
-                </div>
-            </b-form>
-        </div>
-    </div>
+    <b-form @submit="submit">
+        <b-card title="Import a history from an archive">
+            <b-alert :show="hasErrorMessage" variant="danger">{{ errorMessage }}</b-alert>
+            <p>Please provide a Galaxy history export URL or a history file.</p>
+            <b-form-group label="Archived History URL">
+                <b-form-input type="url" v-model="sourceURL"/>
+            </b-form-group>
+            <b-form-group label="Archived History File">
+                <b-form-file v-model="sourceFile"/>
+            </b-form-group>
+            <b-button type="submit">Import history</b-button>
+        </b-card>
+    </b-form>
 </template>
 <script>
 import axios from "axios";
