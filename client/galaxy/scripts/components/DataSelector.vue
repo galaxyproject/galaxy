8a55c4fd5467ed67650f543c4a1955316b569d04
guerler <aysam.guerler@gmail.com>
2018-05-18 14:59:41 -0400

    Add dataselector component

---
+++ client/galaxy/scripts/components/DataSelector.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <b-btn v-b-modal.modal1>Launch demo modal</b-btn>
+
+      <!-- Modal Component -->
+      <b-modal id="modal1" title="Bootstrap-Vue">
+        <p class="my-4">Hello from modal!</p>
+      </b-modal>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+
+Vue.use(BootstrapVue);
+
+export default {
+    props: {
+    },
+    computed: {
+    },
+    data() {
+        return {
+        };
+    },
+    created: function() {
+    },
+    updated: function() {
+    },
+    methods: {
+    }
+};
+</script>
