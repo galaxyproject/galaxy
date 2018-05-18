7cf17001efb8b9491b0e34c8fe063e31d16e1729
guerler <aysam.guerler@gmail.com>
2018-05-18 15:03:07 -0400

    Use methods to show and hide dialog

--- client/galaxy/scripts/components/DataSelector.vue
+++ client/galaxy/scripts/components/DataSelector.vue
@@ -1,10 +1,7 @@
 <template>
     <div>
-      <b-btn v-b-modal.modal1>Launch demo modal</b-btn>
-
-      <!-- Modal Component -->
-      <b-modal id="modal1" title="Bootstrap-Vue">
-        <p class="my-4">Hello from modal!</p>
+      <b-modal ref="modal" title="Dataset Selector">
+        <p class="my-4">Content</p>
       </b-modal>
     </div>
 </template>
@@ -30,6 +27,12 @@ export default {
     updated: function() {
     },
     methods: {
+        show() {
+          this.$refs.modal.show()
+        },
+        hide() {
+          this.$refs.modal.hide()
+        }
     }
 };
 </script>
