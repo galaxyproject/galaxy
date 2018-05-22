galaxyproject/galaxy

commit ef6f7f62ab679928045c816a3cb719fb3bcea507

Memory usage bug fixes and code cleanup/simplification for upload tool

lib/galaxy/datatypes/sniff.py
@@ -14,15 +14,21 @@ import tempfile
 import zipfile
 
 from six import text_type
+from six.moves import filter
 from six.moves.urllib.request import urlopen
 
 from galaxy import util
 from galaxy.util import compression_utils
 from galaxy.util.checkers import (
     check_binary,
+    check_bz2,
+    check_gzip,
     check_html,
+    check_zip,
     is_bz2,
-    is_gzip
+    is_gzip,
+    is_tar,
+    is_zip,
 )
 
 if sys.version_info < (3, 3):
@@ -278,7 +284,7 @@ def is_column_based(fname, sep='\t', skip=0):
     return True
 
 
-def guess_ext(fname, sniff_order):
+def guess_ext(fname, sniff_order, is_binary=False):
     """
     Returns an extension that can be used in the datatype factory to
     generate a data for the 'fname' file
@@ -402,7 +408,8 @@ def guess_ext(fname, sniff_order):
         successfully discovered.
         """
         try:
-            if datatype.sniff(fname):
+            if ((is_binary and datatype.is_binary) or
+                    (not is_binary)) and datatype.sniff(fname):
                 file_ext = datatype.file_ext
                 break
         except Exception:
@@ -416,46 +423,78 @@ def guess_ext(fname, sniff_order):
     if file_ext is not None:
         return file_ext
 
+    # skip header check if data is already known to be binary
+    if is_binary:
+        return file_ext or 'binary'
     try:
         get_headers(fname, None)
     except UnicodeDecodeError:
-        return 'data'  # default binary data type file extension
+        return 'data'  # default data type file extension
     if is_column_based(fname, '\t', 1):
         return 'tabular'  # default tabular data type file extension
     return 'txt'  # default text data type file extension
 
 
-def handle_compressed_file(filename, datatypes_registry, ext='auto'):
+def zip_single_fileobj(path):
+    z = zipfile.ZipFile(path)
+    for name in z.namelist():
+        if not name.endswith('/'):
+            return z.open(name)
+
+
+def handle_compressed_file(
+        filename,
+        datatypes_registry,
+        ext='auto',
+        tmp_prefix='sniff_uncompress_',
+        tmp_dir=None,
+        in_place=False,
+        check_content=True,
+        auto_decompress=True,
+):
+    """
+    Check uploaded files for compression, check compressed file contents, and uncompress if necessary.
+
+    Supports GZip, BZip2, and the first file in a Zip file.
+
+    For performance reasons, the temporary file used for uncompression is located in the same directory as the
+    input/output file. This behavior can be changed with the `tmp_dir` param.
+
+    ``ext`` as returned will only be changed from the ``ext`` input param if the param was an autodetect type (``auto``)
+    and the file was sniffed as a keep-compressed datatype.
+
+    ``is_valid`` as returned will only be set if the file is compressed and contains invalid contents (or the first file
+    in the case of a zip file), this is so lengthy decompression can be bypassed if there is invalid content in the
+    first 32KB. Otherwise the caller should be checking content.
+    """
     CHUNK_SIZE = 2 ** 20  # 1Mb
     is_compressed = False
     compressed_type = None
     keep_compressed = False
     is_valid = False
+    uncompressed = filename
+    tmp_dir = tmp_dir or os.path.dirname(filename)
     for compressed_type, check_compressed_function in COMPRESSION_CHECK_FUNCTIONS:
-        is_compressed = check_compressed_function(filename)
+        is_compressed, is_valid = check_compressed_function(filename, check_content=check_content)
         if is_compressed:
             break  # found compression type
-    if is_compressed:
+    if is_compressed and is_valid:
         if ext in AUTO_DETECT_EXTENSIONS:
-            check_exts = COMPRESSION_DATATYPES[compressed_type]
-        elif ext in COMPRESSED_EXTENSIONS:
-            check_exts = [ext]
-        else:
-            check_exts = []
-        for compressed_ext in check_exts:
-            compressed_datatype = datatypes_registry.get_datatype_by_extension(compressed_ext)
-            if compressed_datatype.sniff(filename):
-                ext = compressed_ext
-                keep_compressed = True
-                is_valid = True
-                break
-
-    if not is_compressed:
-        is_valid = True
-    elif not keep_compressed:
-        is_valid = True
-        fd, uncompressed = tempfile.mkstemp()
+            # attempt to sniff for a keep-compressed datatype (observing the sniff order)
+            sniff_datatypes = filter(lambda d: d.file_ext in COMPRESSION_DATATYPES[compressed_type],
+                                     datatypes_registry.sniff_order)
+            for compressed_datatype in sniff_datatypes:
+                if compressed_datatype.sniff(filename):
+                    ext = compressed_datatype.file_ext
+                    break
+        if ext in COMPRESSED_EXTENSIONS:
+            # if a keep-compressed datatype is explicitly selected (or autodetected), honor the selection
+            keep_compressed = True
+    # don't waste time decompressing if we sniff invalid contents
+    if is_compressed and is_valid and auto_decompress and not keep_compressed:
+        fd, uncompressed = tempfile.mkstemp(prefix=tmp_prefix, dir=tmp_dir)
         compressed_file = DECOMPRESSION_FUNCTIONS[compressed_type](filename)
+        # TODO: it'd be ideal to convert to posix newlines and space-to-tab here as well
         while True:
             try:
                 chunk = compressed_file.read(CHUNK_SIZE)
@@ -469,32 +508,81 @@ def handle_compressed_file(filename, datatypes_registry, ext='auto'):
             os.write(fd, chunk)
         os.close(fd)
         compressed_file.close()
-        # Replace the compressed file with the uncompressed file
-        shutil.move(uncompressed, filename)
-    return is_valid, ext
+        if in_place:
+            # Replace the compressed file with the uncompressed file
+            shutil.move(uncompressed, filename)
+            uncompressed = filename
+    elif not is_compressed:
+        is_valid = True
+    return is_valid, ext, uncompressed, compressed_type
 
 
-def handle_uploaded_dataset_file(filename, datatypes_registry, ext='auto'):
-    is_valid, ext = handle_compressed_file(filename, datatypes_registry, ext=ext)
+def handle_uploaded_dataset_file(
+        filename,
+        datatypes_registry,
+        ext='auto',
+        tmp_prefix='sniff_upload_',
+        tmp_dir=None,
+        in_place=False,
+        check_content=True,
+        is_binary=None,
+        auto_decompress=True,
+        uploaded_file_ext=None,
+        convert_to_posix_lines=None,
+        convert_spaces_to_tabs=None,
+):
+    is_valid, ext, converted_path, compressed_type = handle_compressed_file(
+        filename,
+        datatypes_registry,
+        ext=ext,
+        tmp_prefix=tmp_prefix,
+        tmp_dir=tmp_dir,
+        in_place=in_place,
+        check_content=check_content,
+        auto_decompress=auto_decompress,
+    )
+    try:
+        if not is_valid:
+            if is_tar(converted_path):
+                raise InappropriateDatasetContentError('TAR file uploads are not supported')
+            raise InappropriateDatasetContentError('The uploaded compressed file contains invalid content')
 
-    if not is_valid:
-        raise InappropriateDatasetContentError('The compressed uploaded file contains inappropriate content.')
+        # This needs to be checked again after decompression
+        is_binary = check_binary(converted_path)
 
-    if ext in AUTO_DETECT_EXTENSIONS:
-        ext = guess_ext(filename, sniff_order=datatypes_registry.sniff_order)
+        if not is_binary and convert_to_posix_lines:
+            # Convert universal line endings to Posix line endings, spaces to tabs (if desired)
+            if convert_spaces_to_tabs:
+                convert_fxn = convert_newlines_sep2tabs
+            else:
+                convert_fxn = convert_newlines
+            line_count, _converted_path = convert_fxn(converted_path, in_place=in_place, tmp_dir=tmp_dir, tmp_prefix=tmp_prefix)
+            if not in_place:
+                if converted_path and filename != converted_path:
+                    os.unlink(converted_path)
+                converted_path = _converted_path
 
-    if check_binary(filename):
-        if not datatypes_registry.is_extension_unsniffable_binary(ext) and not datatypes_registry.get_datatype_by_extension(ext).sniff(filename):
-            raise InappropriateDatasetContentError('The binary uploaded file contains inappropriate content.')
-    elif check_html(filename):
-        raise InappropriateDatasetContentError('The uploaded file contains inappropriate HTML content.')
-    return ext
+        if ext in AUTO_DETECT_EXTENSIONS:
+            ext = guess_ext(converted_path, sniff_order=datatypes_registry.sniff_order, is_binary=is_binary)
+
+        if not is_binary and check_content and check_html(converted_path):
+            raise InappropriateDatasetContentError('The uploaded file contains invalid HTML content')
+    except Exception:
+        if filename != converted_path:
+            os.unlink(converted_path)
+        raise
+    return ext, converted_path, compressed_type
 
 
 AUTO_DETECT_EXTENSIONS = ['auto']  # should 'data' also cause auto detect?
-DECOMPRESSION_FUNCTIONS = dict(gzip=gzip.GzipFile, bz2=bz2.BZ2File)
-COMPRESSION_CHECK_FUNCTIONS = [('gzip', is_gzip), ('bz2', is_bz2)]
-COMPRESSION_DATATYPES = dict(gzip=['bam', 'fasta.gz', 'fastq.gz', 'fastqsanger.gz', 'fastqillumina.gz', 'fastqsolexa.gz', 'fastqcssanger.gz'], bz2=['fastq.bz2', 'fastqsanger.bz2', 'fastqillumina.bz2', 'fastqsolexa.bz2', 'fastqcssanger.bz2'])
+DECOMPRESSION_FUNCTIONS = dict(gz=gzip.GzipFile, bz2=bz2.BZ2File, zip=zip_single_fileobj)
+COMPRESSION_CHECK_FUNCTIONS = [('gz', check_gzip), ('bz2', check_bz2), ('zip', check_zip)]
+COMPRESSION_IS_FUNCTIONS = [('gz', is_gzip), ('bz2', is_bz2), ('zip', is_zip)]
+COMPRESSION_DATATYPES = dict(
+    gz=['bam', 'fasta.gz', 'fastq.gz', 'fastqsanger.gz', 'fastqillumina.gz', 'fastqsolexa.gz', 'fastqcssanger.gz'],
+    bz2=['fastq.bz2', 'fastqsanger.bz2', 'fastqillumina.bz2', 'fastqsolexa.bz2', 'fastqcssanger.bz2'],
+    zip=[],
+)
 COMPRESSED_EXTENSIONS = []
 for exts in COMPRESSION_DATATYPES.values():
     COMPRESSED_EXTENSIONS.extend(exts)

lib/galaxy/jobs/__init__.py
@@ -3,6 +3,7 @@ Support for running a tool in Galaxy via an internal job management system
 """
 import copy
 import datetime
+import errno
 import logging
 import os
 import pwd
@@ -831,6 +832,18 @@ class JobWrapper(HasResourceParameters):
     def get_version_string_path(self):
         return os.path.abspath(os.path.join(self.app.config.new_file_path, "GALAXY_VERSION_STRING_%s" % self.job_id))
 
+    def __prepare_upload_paramfile(self, tool_evaluator):
+        """Special case paramfile handling for the upload tool. Moves the paramfile to the working directory
+        """
+        new = os.path.join(self.working_directory, 'upload_params.json')
+        try:
+            shutil.move(tool_evaluator.param_dict['paramfile'], new)
+        except (OSError, IOError) as exc:
+            # It won't exist at the old path if setup was interrupted and tried again later
+            if exc.errno != errno.ENOENT or not os.path.exists(new):
+                raise
+        tool_evaluator.param_dict['paramfile'] = new
+
     def prepare(self, compute_environment=None):
         """
         Prepare the job to run by creating the working directory and the
@@ -854,6 +867,10 @@ class JobWrapper(HasResourceParameters):
         tool_evaluator.set_compute_environment(compute_environment, get_special=get_special)
 
         self.sa_session.flush()
+
+        # TODO: The upload tool actions that create the paramfile can probably be turned in to a configfile to remove this special casing
+        if job.tool_id == 'upload1':
+            self.__prepare_upload_paramfile(tool_evaluator)
 
         self.command_line, self.extra_filenames, self.environment_variables = tool_evaluator.build()
         # Ensure galaxy_lib_dir is set in case there are any later chdirs
@@ -1273,7 +1290,7 @@ class JobWrapper(HasResourceParameters):
                     if retry_internally and not self.external_output_metadata.external_metadata_set_successfully(dataset, self.sa_session):
                         # If Galaxy was expected to sniff type and didn't - do so.
                         if dataset.ext == "_sniff_":
-                            extension = sniff.handle_uploaded_dataset_file(dataset.dataset.file_name, self.app.datatypes_registry)
+                            extension = sniff.handle_uploaded_dataset_file(dataset.dataset.file_name, self.app.datatypes_registry)[0]
                             dataset.extension = extension
 
                         # call datatype.set_meta directly for the initial set_meta call during dataset creation

lib/galaxy_ext/metadata/set_metadata.py
@@ -44,7 +44,7 @@ def set_meta_with_tool_provided(dataset_instance, file_dict, set_meta_kwds, data
     if extension == "_sniff_":
         try:
             from galaxy.datatypes import sniff
-            extension = sniff.handle_uploaded_dataset_file(dataset_instance.dataset.external_filename, datatypes_registry)
+            extension = sniff.handle_uploaded_dataset_file(dataset_instance.dataset.external_filename, datatypes_registry)[0]
             # We need to both set the extension so it is available to set_meta
             # and record it in the metadata so it can be reloaded on the server
             # side and the model updated (see MetadataCollection.{from,to}_JSON_dict)

config/datatypes_conf.xml.sample
@@ -74,6 +74,7 @@
     <datatype extension="bowtie_base_index" type="galaxy.datatypes.ngsindex:BowtieBaseIndex" mimetype="text/html" display_in_upload="false"/>
     <datatype extension="csfasta" type="galaxy.datatypes.sequence:csFasta" display_in_upload="true"/>
     <datatype extension="data" type="galaxy.datatypes.data:Data" mimetype="application/octet-stream" max_optional_metadata_filesize="1048576" />
+    <datatype extension="binary" type="galaxy.datatypes.binary:Binary" mimetype="application/octet-stream" max_optional_metadata_filesize="1048576" />
     <datatype extension="d3_hierarchy" type="galaxy.datatypes.text:Json" mimetype="application/json" subclass="true" display_in_upload="true"/>
     <datatype extension="data_manager_json" type="galaxy.datatypes.text:Json" mimetype="application/json" subclass="true" display_in_upload="false"/>
     <datatype extension="dbn" type="galaxy.datatypes.sequence:DotBracket" display_in_upload="true" description="Dot-Bracket format is a text-based format for storing both an RNA sequence and its corresponding 2D structure." description_url="https://wiki.galaxyproject.org/Learn/Datatypes#Dbn"/>

lib/galaxy/datatypes/data.py
@@ -116,6 +116,15 @@ class Data(object):
         self.composite_files = self.composite_files.copy()
         self.display_applications = odict()
 
+    @property
+    def validate_mode(self):
+        """Indicate that a sniffer should run, even if disabled.
+
+        Some sniffers (e.g. fastq.gz) work but are not enabled for certain reasons, but when running a sniffer to
+        "validate" a selected filetype, those sniffers should be enabled.
+        """
+        return os.environ.get('GALAXY_SNIFFER_VALIDATE_MODE', '0') == '1'
+
     def get_raw_data(self, dataset):
         """Returns the full data. To stream it open the file_name and read/write as needed"""
         try:
@@ -751,6 +760,8 @@ class Text(Data):
     edam_format = "format_2330"
     file_ext = 'txt'
     line_class = 'line'
+
+    is_binary = False
 
     # Add metadata elements
     MetadataElement(name="data_lines", default=0, desc="Number of data lines", readonly=True, optional=True, visible=False, no_value=0)

lib/galaxy/datatypes/genetics.py
@@ -606,7 +606,6 @@ class RexpBase(Html):
     MetadataElement(name="pheno_path", desc="Path to phenotype data for this experiment", default="rexpression.pheno", visible=True)
     file_ext = 'rexpbase'
     html_table = None
-    is_binary = True
     composite_type = 'auto_primary_file'
     allow_datatype_change = False
 

lib/galaxy/datatypes/ngsindex.py
@@ -18,7 +18,6 @@ class BowtieIndex(Html):
     MetadataElement(name="base_name", desc="base name for this index set", default='galaxy_generated_bowtie_index', set_in_upload=True, readonly=True)
     MetadataElement(name="sequence_space", desc="sequence_space for this index set", default='unknown', set_in_upload=True, readonly=True)
 
-    is_binary = True
     composite_type = 'auto_primary_file'
     allow_datatype_change = False
 

lib/galaxy/datatypes/sequence.py
@@ -319,7 +319,7 @@ class FastaGz(Sequence, Binary):
 
     def sniff(self, filename):
         """Determines whether the file is in gzip-compressed FASTA format"""
-        if not SNIFF_COMPRESSED_FASTAS:
+        if not SNIFF_COMPRESSED_FASTAS and not self.validate_mode:
             return False
         if not is_gzip(filename):
             return False
@@ -746,7 +746,7 @@ class FastqGz(BaseFastq, Binary):
 
     def sniff(self, filename):
         """Determines whether the file is in gzip-compressed FASTQ format"""
-        if not SNIFF_COMPRESSED_FASTQS:
+        if not SNIFF_COMPRESSED_FASTQS and not self.validate_mode:
             return False
         if not is_gzip(filename):
             return False
@@ -784,7 +784,7 @@ class FastqBz2(BaseFastq, Binary):
 
     def sniff(self, filename):
         """Determine whether the file is in bzip2-compressed FASTQ format"""
-        if not SNIFF_COMPRESSED_FASTQS:
+        if not SNIFF_COMPRESSED_FASTQS and not self.validate_mode:
             return False
         if not is_bz2(filename):
             return False

commit 65c1cd761c48ba121a847d4d501eb8a97a911e70

Fix data_source.py for upload changes in 18.05 release cycle.
handle_uploaded_dataset_file was used by tools external to ``lib/`` include data_source.py and potentially tools in the tool shed. This restores the interface as a wrapper around a new method handle_uploaded_dataset_file_internal.

Broken in https://github.com/galaxyproject/galaxy/commit/ef6f7f62ab679928045c816a3cb719fb3bcea507 I believe.

lib/galaxy/datatypes/sniff.py
@@ -706,7 +706,12 @@ def handle_compressed_file(
     return is_valid, ext, uncompressed, compressed_type
 
 
-def handle_uploaded_dataset_file(
+def handle_uploaded_dataset_file(*args, **kwds):
+    """Legacy wrapper about handle_uploaded_dataset_file_internal for tools using it."""
+    return handle_uploaded_dataset_file_internal(*args, **kwds)[0]
+
+
+def handle_uploaded_dataset_file_internal(
         filename,
         datatypes_registry,
         ext='auto',

lib/galaxy/datatypes/upload_util.py
@@ -40,7 +40,7 @@ def handle_upload(
         if auto_decompress and is_zip(path) and not is_single_file_zip(path):
             stdout = 'ZIP file contained more than one file, only the first file was added to Galaxy.'
         try:
-            ext, converted_path, compression_type = sniff.handle_uploaded_dataset_file(
+            ext, converted_path, compression_type = sniff.handle_uploaded_dataset_file_internal(
                 path,
                 registry,
                 ext=requested_ext,

lib/galaxy/jobs/__init__.py
@@ -1294,7 +1294,7 @@ class JobWrapper(HasResourceParameters):
                     if retry_internally and not self.external_output_metadata.external_metadata_set_successfully(dataset, self.sa_session):
                         # If Galaxy was expected to sniff type and didn't - do so.
                         if dataset.ext == "_sniff_":
-                            extension = sniff.handle_uploaded_dataset_file(dataset.dataset.file_name, self.app.datatypes_registry)[0]
+                            extension = sniff.handle_uploaded_dataset_file(dataset.dataset.file_name, self.app.datatypes_registry)
                             dataset.extension = extension
 
                         # call datatype.set_meta directly for the initial set_meta call during dataset creation

lib/galaxy_ext/metadata/set_metadata.py
@@ -44,7 +44,7 @@ def set_meta_with_tool_provided(dataset_instance, file_dict, set_meta_kwds, data
     if extension == "_sniff_":
         try:
             from galaxy.datatypes import sniff
-            extension = sniff.handle_uploaded_dataset_file(dataset_instance.dataset.external_filename, datatypes_registry)[0]
+            extension = sniff.handle_uploaded_dataset_file(dataset_instance.dataset.external_filename, datatypes_registry)
             # We need to both set the extension so it is available to set_meta
             # and record it in the metadata so it can be reloaded on the server
             # side and the model updated (see MetadataCollection.{from,to}_JSON_dict)