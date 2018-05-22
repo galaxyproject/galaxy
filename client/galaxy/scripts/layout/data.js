648caa48e49f7a086b20a39fa39cf438f103b9f8
guerler <aysam.guerler@gmail.com>
2018-05-22 10:21:11 -0400

    Add comments

--- client/galaxy/scripts/layout/data.js
+++ client/galaxy/scripts/layout/data.js
@@ -2,6 +2,10 @@ import DataDialog from "components/DataDialog.vue";
 import Vue from "vue";
 
 export default class Data {
+    /**
+     * Opens a modal dialog for data selection
+     * @param {function} callback - Result function called with selection
+     */
     dialog(callback) {
         var instance = Vue.extend(DataDialog);
         var vm = document.createElement("div");
