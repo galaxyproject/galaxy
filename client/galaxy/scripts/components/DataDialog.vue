8a34978decbe8fb70be9eece744aba1b3de03383
guerler <aysam.guerler@gmail.com>
2018-05-22 01:20:06 -0400

    Improve error handling, add spinner

--- client/galaxy/scripts/components/DataDialog.vue
+++ client/galaxy/scripts/components/DataDialog.vue
@@ -1,9 +1,15 @@
 <template>
-    <b-modal v-model="modalShow" ref="modal" title="Select a dataset" @ok="handleOk">
-        <b-alert variant="danger" :show="errMsg">
+    <b-modal v-model="modalShow" :title="modalTitle" @ok="handleOk" ok-only="!errMsg">
+        <b-alert v-if="errMsg" variant="danger" :show="errMsg">
             {{ errMsg }}
         </b-alert>
-        <b-form-select v-model="selected" :options="options"/>
+        <div v-else>
+            <b-form-select v-if="optionsShow" v-model="selected" :options="options"/>
+            <div v-if="!optionsShow">
+                <span class="fa fa-spinner fa-spin"/>
+                <span>Please wait...</span>
+            </div>
+        </div>
     </b-modal>
 </template>
 
@@ -21,8 +27,20 @@ export default {
             required: true
         }
     },
+    computed: {
+        modalTitle() {
+            if (this.errMsg) {
+                return "Failed to load datasets";
+            }
+            if (this.optionsShow) {
+                return "Select a dataset";
+            }
+            return "Loading datasets";
+        }
+    },
     data() {
         return {
+            optionsShow: false,
             modalShow: true,
             historyId: null,
             selected: null,
@@ -33,20 +51,29 @@ export default {
     created: function() {this.loadOptions()},
     methods: {
         handleOk: function() {
-            let host = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
-            this.callback(`${host}${this.selected}/display`);
+            if (this.selected) {
+                let host = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+                this.callback(`${host}${this.selected}/display`);
+            }
         },
         loadOptions: function() {
             this.historyId = Galaxy.currHistoryPanel && Galaxy.currHistoryPanel.model.id;
+            this.selected = null;
             if (this.historyId) {
                 axios
                     .get(`${Galaxy.root}api/histories/${this.historyId}/contents`)
                     .then(response => {
                         for(let item of response.data) {
                             this.options.push({value: item.url, text: item.name});
+                            if (!this.selected) {
+                                this.selected = item.url;
+                            }
                         }
+                        this.optionsShow = true;
                     })
-                    .catch(e => (this.errMsg = e.response.data.err_msg));
+                    .catch(e => {
+                        this.errMsg = e.response.data.err_msg;
+                    });
             } else {
                 this.errMsg = "History not accessible.";
             }
