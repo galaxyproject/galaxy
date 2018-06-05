galaxyproject/galaxy

commit 7cc01380afd017c5cac2af2b2d735b2265774a68

Update bs4 style dropdowns, minor cleanup

client/galaxy/scripts/mvc/history/hdca-li-edit.js
@@ -1,7 +1,9 @@
+import * as _ from "underscore";
 import HDCA_LI from "mvc/history/hdca-li";
 import DC_VIEW_EDIT from "mvc/collection/collection-view-edit";
-import faIconButton from "ui/fa-icon-button";
 import _l from "utils/localization";
+
+/* global $ */
 
 //==============================================================================
 var _super = HDCA_LI.HDCAListItemView;
@@ -38,23 +40,19 @@ var HDCAListItemEdit = _super.extend(
                     <a class="delete-btn icon-btn" title="${_l("Delete")}" data-toggle="dropdown">
                         <span class="fa fa-times"></span>
                     </a>
-                    <ul class="dropdown-menu float-right" role="menu">
-                        <li>
-                            <a href="#" class="delete-collection">
+                    <div class="dropdown-menu" role="menu">
+                            <a href="#" class="dropdown-item" delete-collection">
                                 ${_l("Collection Only")}
                             </a>
-                        </li>
-                        <li>
-                            <a href="#" class="delete-collection-and-datasets">
+                            <a href="#" class="dropdown-item delete-collection-and-datasets">
                                 ${_l("Delete Datasets")}
                             </a>
-                        </li>
-                        <li style="display: ${this.purgeAllowed ? "inherit" : "none"}">
-                            <a href="#" class="delete-collection-and-purge-datasets">
+                            <a href="#" style="display: ${
+                                this.purgeAllowed ? "inherit" : "none"
+                            }" class="dropdown-item delete-collection-and-purge-datasets">
                                 ${_l("Permanently Delete Datasets")}
                             </a>
-                        </li>
-                    </ul>
+                    </div>
                 </div>`);
         },
 

commit 031ce67e996edc3c363bca1fbaf8572e92b5cbc7

Fix delete "Collection Only" option in history menu.
Broken with https://github.com/galaxyproject/galaxy/commit/7cc01380afd017c5cac2af2b2d735b2265774a68.

client/galaxy/scripts/mvc/history/hdca-li-edit.js
@@ -41,7 +41,7 @@ var HDCAListItemEdit = _super.extend(
                         <span class="fa fa-times"></span>
                     </a>
                     <div class="dropdown-menu" role="menu">
-                            <a href="#" class="dropdown-item" delete-collection">
+                            <a href="#" class="dropdown-item delete-collection">
                                 ${_l("Collection Only")}
                             </a>
                             <a href="#" class="dropdown-item delete-collection-and-datasets">