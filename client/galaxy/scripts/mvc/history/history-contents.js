54ec854c130f92f49d2c3f42e2e8971d6676a79a
Martin Cech <marten@bx.psu.edu>
2018-06-08 13:19:13 -0400

    remove outdated comment

--- client/galaxy/scripts/mvc/history/history-contents.js
+++ client/galaxy/scripts/mvc/history/history-contents.js
@@ -35,8 +35,6 @@ var HistoryContents = _super.extend(BASE_MVC.LoggableMixin).extend({
     /** @type {String} order used here and when fetching from server */
     order: "hid",
 
-    /** root api url */
-
     /** complete api url */
     url: function() {
         return `${this.urlRoot}/${this.historyId}/contents`;
