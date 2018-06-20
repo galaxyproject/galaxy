7ca8657d139dde3f626cf612817cd72d594ee7da
John Chilton <jmchilton@gmail.com>
2018-06-20 15:33:26 -0400

    Optimize permission loading and checking collection parameters.
    We were checking permissions twice it seems like to me, only do it once. Avoid loading dataset permissions for each individual dataset in a collection - work with summary sets of role ids.

--- lib/galaxy/security/__init__.py
+++ lib/galaxy/security/__init__.py
@@ -607,6 +607,17 @@ class GalaxyRBACAgent(RBACAgent):
         retval = self.dataset_is_public(dataset) or self.allow_action(user_roles, self.permitted_actions.DATASET_ACCESS, dataset)
         return retval
 
+    def can_access_datasets(self, user_roles, action_tuples):
+        user_role_ids = [r.id for r in user_roles]
+
+        # For DATASET_ACCESS, user must have ALL associated roles
+        for action, user_role_id in action_tuples:
+            if action == self.permitted_actions.DATASET_ACCESS:
+                if user_role_id not in user_role_ids:
+                    return False
+
+        return True
+
     def can_manage_dataset(self, roles, dataset):
         return self.allow_action(roles, self.permitted_actions.DATASET_MANAGE_PERMISSIONS, dataset)
 
@@ -715,6 +726,29 @@ class GalaxyRBACAgent(RBACAgent):
                     elif action.model == 'restrict':
                         # join existing roles with new roles
                         perms[action].extend([_ for _ in roles if _ not in perms[action]])
+        return perms
+
+    def guess_derived_permissions(self, all_input_permissions):
+        """Returns a dict of { action : [ role_id, role_id, ... ] } for the output dataset based upon input dataset permissions.
+
+        all_input_permissions should be of the form {action_name: set(role_ids)}
+        """
+        perms = {}
+        for action in self.get_actions():
+            perms[action] = []
+
+        for action_name, role_ids in all_input_permissions.items():
+            action = self.get_action(action_name)
+            if action not in perms.keys():
+                perms[action] = list(role_ids)
+            else:
+                if action.model == 'grant':
+                    # intersect existing roles with new roles
+                    perms[action] = [role_id for role_id in role_ids if role_id in perms[action]]
+                elif action.model == 'restrict':
+                    # join existing roles with new roles
+                    perms[action].extend([role_id for role_id in role_ids if role_id not in perms[action]])
+
         return perms
 
     def associate_components(self, **kwd):
@@ -922,7 +956,11 @@ class GalaxyRBACAgent(RBACAgent):
             if isinstance(action, Action):
                 action = action.action
             for role in roles:
-                dp = self.model.DatasetPermissions(action, dataset, role_id=role.id)
+                if hasattr(role, "id"):
+                    role_id = role.id
+                else:
+                    role_id = role
+                dp = self.model.DatasetPermissions(action, dataset, role_id=role_id)
                 self.sa_session.add(dp)
                 flush_needed = True
         if flush_needed and flush:
