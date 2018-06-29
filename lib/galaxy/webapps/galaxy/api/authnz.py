d03f187eb7e964de71c06df5d6b1ffddb62f65bc
vjalili <jalili.vahid@gmail.com>
2018-06-29 10:50:18 -0700

    Use Galaxy exceptions class to raise/handle errors in authnz.

--- lib/galaxy/webapps/galaxy/api/authnz.py
+++ lib/galaxy/webapps/galaxy/api/authnz.py
@@ -7,6 +7,7 @@ from __future__ import absolute_import
 import logging
 
 from galaxy import web
+from galaxy.exceptions import MessageException
 from galaxy.web import url_for
 from galaxy.web.base.controller import BaseAPIController
 
@@ -38,9 +39,8 @@ class OIDC(BaseAPIController):
     @web.expose
     def login(self, trans, provider):
         if not trans.app.config.enable_oidc:
-            msg = "Login to Galaxy using third-party identities is not enabled on this Galaxy instance."
-            log.debug(msg)
-            return trans.show_error_message(msg)
+            raise MessageException(
+                err_msg = "Login to Galaxy using third-party identities is not enabled on this Galaxy instance.")
         success, message, redirect_uri = trans.app.authnz_manager.authenticate(provider, trans)
         return redirect_uri
 
@@ -48,30 +48,30 @@ class OIDC(BaseAPIController):
     def callback(self, trans, provider, **kwargs):
         user = trans.user.username if trans.user is not None else 'anonymous'
         if not bool(kwargs):
-            log.error("OIDC callback received no data for provider `{}` and user `{}`".format(provider, user))
-            return trans.show_error_message(
-                'Did not receive any information from the `{}` identity provider to complete user `{}` authentication '
-                'flow. Please try again, and if the problem persists, contact the Galaxy instance admin. Also note '
-                'that this endpoint is to receive authentication callbacks only, and should not be called/reached by '
-                'a user.'.format(provider, user))
+            raise MessageException(
+                err_msg = "Did not receive any information from the `{}` identity provider to complete user `{}` "
+                          "authentication flow. Please try again, and if the problem persists, contact the Galaxy "
+                          "instance admin. Also note that this endpoint is to receive authentication callbacks only, "
+                          "and should not be called/reached by a user.".format(provider, user))
         if 'error' in kwargs:
-            log.error("Error handling authentication callback from `{}` identity provider for user `{}` login request."
-                      " Error message: {}".format(provider, user, kwargs.get('error', 'None')))
-            return trans.show_error_message('Failed to handle authentication callback from {}. '
-                                            'Please try again, and if the problem persists, contact '
-                                            'the Galaxy instance admin'.format(provider))
+            raise MessageException(
+                err_msg = "Failed to handle authentication callback from {}. Please try again, and if the problem "
+                          "persists, contact the Galaxy instance admin".format(provider),
+                provider_returned_error = kwargs.get('error', 'None'))
+
         success, message, (redirect_url, user) = trans.app.authnz_manager.callback(provider,
                                                                                    kwargs['state'],
                                                                                    kwargs['code'],
                                                                                    trans,
                                                                                    login_redirect_url=url_for('/'))
         if success is False:
-            return trans.show_error_message(message)
+            raise MessageException(err_msg = message)
         user = user if user is not None else trans.user
         if user is None:
-            return trans.show_error_message("An unknown error occurred when handling the callback from `{}` "
-                                            "identity provider. Please try again, and if the problem persists, "
-                                            "contact the Galaxy instance admin.".format(provider))
+            raise MessageException(
+                err_msg = "An unknown error occurred when handling the callback from `{}` identity provider. "
+                          "Please try again, and if the problem persists, contact the admin of this Galaxy "
+                          "instance.".format(provider))
         trans.handle_user_login(user)
         return {"message": message}
 
@@ -85,7 +85,7 @@ class OIDC(BaseAPIController):
                                                                              trans,
                                                                              disconnect_redirect_url=url_for('/'))
         if success is False:
-            return trans.show_error_message(message)
+            raise MessageException(err_msg = message)
         if redirect_url is None:
             redirect_url = url_for('/')
         return redirect_url
