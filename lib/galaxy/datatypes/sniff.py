82028d95d0d58532821816d526e2b932ea8ee5ec
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2018-07-03 00:15:07 +0100

    Remove needless check
    The `filename` file is the destination file and is always empty at this
    point.
    
    Introduced in commit 85ea42b79dcfc30622a55f1b9fc507bc822ae325 .

--- lib/galaxy/datatypes/sniff.py
+++ lib/galaxy/datatypes/sniff.py
@@ -73,17 +73,14 @@ def stream_to_open_named_file(stream, fd, filename, source_encoding=None, source
             break
         if not data_checked:
             # See if we're uploading a compressed file
-            if zipfile.is_zipfile(filename):
-                is_compressed = True
-            else:
-                try:
-                    # Convert chunk to a bytestring if it is not already.
-                    # Check if the first 2 bytes of the chunk are equal to the
-                    # gzip magic number.
-                    if smart_str(chunk)[:2] == util.gzip_magic:
-                        is_compressed = True
-                except Exception:
-                    pass
+            try:
+                # Convert chunk to a bytestring if it is not already.
+                # Check if the first 2 bytes of the chunk are equal to the
+                # gzip magic number.
+                if smart_str(chunk)[:2] == util.gzip_magic:
+                    is_compressed = True
+            except Exception:
+                pass
             if not is_compressed:
                 is_binary = util.is_binary(chunk)
             data_checked = True
