5745df2318d4907fb0fa40bc9032b44db853bca0
thobalose <lose.thoba@gmail.com>
2018-07-03 14:31:38 +0200

    GIE static url; https://github.com/galaxyproject/galaxy/issues/6459

--- lib/galaxy/visualization/plugins/interactive_environments.py
+++ lib/galaxy/visualization/plugins/interactive_environments.py
@@ -49,7 +49,7 @@ class InteractiveEnvironmentRequest(object):
         self.attr.redact_username_in_logs = trans.app.config.redact_username_in_logs
         self.attr.galaxy_root_dir = os.path.abspath(self.attr.galaxy_config.root)
         self.attr.root = web.url_for("/")
-        self.attr.app_root = self.attr.root + "plugins/interactive_environments/" + self.attr.viz_id + "/static/"
+        self.attr.app_root = self.attr.root + "static/plugins/interactive_environments/" + self.attr.viz_id + "/static/"
         self.attr.import_volume = True
 
         plugin_path = os.path.abspath(plugin.path)
