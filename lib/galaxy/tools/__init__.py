fddc7899c1f09bf20f36369883f091199db0edd0
rhpvorderman <r.h.p.vorderman@lumc.nl>
2018-07-05 12:18:09 +0200

    Let data managers also use conda python.
    Since data managers only return a json object, there is no need for them to use the galaxy python.

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -508,7 +508,7 @@ class Tool(Dictifiable):
         """Indicates this tool's runtime requires Galaxy's Python environment."""
         # All special tool types (data source, history import/export, etc...)
         # seem to require Galaxy's Python.
-        if self.tool_type != "default":
+        if self.tool_type not in ["default","manage_data"]:
             return True
 
         config = self.app.config
