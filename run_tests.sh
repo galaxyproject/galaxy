2527038ffce1f7f1b987f9318671bc4fec567ec0
mvdbeek <m.vandenbeek@gmail.com>
2018-08-10 18:23:28 +0200

    Mount in run_tests script at container runtime
    This allows changing variables in the script without rebuilding the
    container

--- run_tests.sh
+++ run_tests.sh
@@ -301,7 +301,13 @@ then
     # locally before testing - you will need to do this also if using this script for Selenium testing.
     DOCKER_RUN_EXTRA_ARGS="-e GALAXY_TEST_UID=${MY_UID} -e GALAXY_SKIP_CLIENT_BUILD=1 ${DOCKER_RUN_EXTRA_ARGS}"
     echo "Launching docker container for testing with extra args ${DOCKER_RUN_EXTRA_ARGS}..."
-    docker $DOCKER_EXTRA_ARGS run $DOCKER_RUN_EXTRA_ARGS -e "BUILD_NUMBER=$BUILD_NUMBER" -e "GALAXY_TEST_DATABASE_TYPE=$db_type" --rm -v `pwd`:/galaxy $DOCKER_IMAGE "$@"
+    docker $DOCKER_EXTRA_ARGS run $DOCKER_RUN_EXTRA_ARGS \
+        -e "BUILD_NUMBER=$BUILD_NUMBER" \
+        -e "GALAXY_TEST_DATABASE_TYPE=$db_type" \
+        -e "LC_ALL=C" \
+        --rm \
+        -v `pwd`:/galaxy \
+        -v `pwd`/test/docker/base/run_test_wrapper.sh:/usr/local/bin/run_test_wrapper.sh $DOCKER_IMAGE "$@"
     exit $?
 fi
 
--- test/docker/base/run_test_wrapper.sh
+++ test/docker/base/run_test_wrapper.sh
@@ -29,7 +29,7 @@ then
     GALAXY_TEST_INSTALL_DB_MERGED="true"
     GALAXY_TEST_DBURI="postgres://root@localhost:5930/galaxy?client_encoding=utf8"
     TOOL_SHED_TEST_DBURI="postgres://root@localhost:5930/toolshed?client_encoding=utf8"
-    export GALAXY_CONFIG_OVERRIDE_DATABASE_ENCODING="SQL_ASCII"
+    export GALAXY_CONFIG_OVERRIDE_DATABASE_ENCODING="UTF-8"
 elif [ "$GALAXY_TEST_DATABASE_TYPE" = "mysql" ];
 then
     sh /opt/galaxy/start_mysql.sh
