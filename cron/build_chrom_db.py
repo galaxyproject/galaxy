galaxyproject/galaxy

commit 9b8e76f9d9df0ae0ba6238d44a8f07b97fe34d6f

Convert urllib to requests
Which has built-in protection against urls like "file:///tmp/a"

cron/build_chrom_db.py
@@ -15,10 +15,10 @@ from __future__ import print_function
 
 import fileinput
 import os
+import requests
 import sys
 
 from six.moves.urllib.parse import urlencode
-from six.moves.urllib.request import urlopen
 
 import parse_builds
 
@@ -36,8 +36,8 @@ def getchrominfo(url, db):
         "hgta_regionType": "",
         "position": "",
         "hgta_doTopSubmit": "get info"})
-    page = urlopen(URL)
-    for line in page:
+    page = requests.get(URL).text
+    for line in page.split('\n'):
         line = line.rstrip("\r\n")
         if line.startswith("#"):
             continue

cron/parse_builds.py
@@ -6,21 +6,20 @@ build   description
 """
 from __future__ import print_function
 
+import requests
 import sys
 import xml.etree.ElementTree as ElementTree
 
-from six.moves.urllib.request import urlopen
 
 
 def getbuilds(url):
     try:
-        page = urlopen(url)
+        text = requests.get(url).text
     except:
         print("#Unable to open " + url)
         print("?\tunspecified (?)")
         sys.exit(1)
 
-    text = page.read()
     try:
         tree = ElementTree.fromstring(text)
     except:

cron/parse_builds_3_sites.py
@@ -4,9 +4,8 @@ Connects to sites and determines which builds are available at each.
 """
 from __future__ import print_function
 
+import requests
 import xml.etree.ElementTree as ElementTree
-
-from six.moves.urllib.request import urlopen
 
 sites = ['http://genome.ucsc.edu/cgi-bin/',
          'http://archaea.ucsc.edu/cgi-bin/',
@@ -20,11 +19,11 @@ def main():
         trackurl = sites[i] + "hgTracks?"
         builds = []
         try:
-            page = urlopen(site)
+            page = requests.get(site)
         except:
             print("#Unable to connect to " + site)
             continue
-        text = page.read()
+        text = page.text
         try:
             tree = ElementTree.fromstring(text)
         except:

lib/galaxy/external_services/actions.py
@@ -1,6 +1,6 @@
 # Contains actions that are used in External Services
 import logging
-from urllib import urlopen
+import requests
 from galaxy.web import url_for
 from galaxy.util.template import fill_template
 from result_handlers.basic import ExternalServiceActionResultHandler
@@ -104,7 +104,7 @@ class ExternalServiceWebAPIActionResult(ExternalServiceResult):
     @property
     def content(self):
         if self._content is None:
-            self._content = urlopen(self.url).read()
+            self._content = requests.get(self.url).read()
         return self._content
 
 

lib/galaxy/jobs/deferred/pacific_biosciences_smrt_portal.py
@@ -4,9 +4,8 @@ produced by SMRT Portal.
 """
 import json
 import logging
+import requests
 from string import Template
-
-from six.moves.urllib.request import urlopen
 
 from .data_transfer import DataTransfer
 
@@ -88,8 +87,8 @@ class SMRTPortalPlugin(DataTransfer):
             if self._missing_params(job.params, ['smrt_host', 'smrt_job_id']):
                 return self.job_states.INVALID
             url = 'http://' + job.params['smrt_host'] + self.api_path + '/Jobs/' + job.params['smrt_job_id'] + '/Status'
-            r = urlopen(url)
-            status = json.loads(r.read())
+            r = requests.get(url)
+            status = r.json()
             # TODO: error handling: unexpected json or bad response, bad url, etc.
             if status['Code'] == 'Completed':
                 log.debug("SMRT Portal job '%s' is Completed.  Initiating transfer." % job.params['smrt_job_id'])

lib/galaxy/managers/citations.py
@@ -1,6 +1,6 @@
 import functools
 import os
-import urllib2
+import requests
 
 from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
@@ -47,10 +47,8 @@ class DoiCache(object):
     def _raw_get_bibtex(self, doi):
         dx_url = "http://dx.doi.org/" + doi
         headers = {'Accept': 'text/bibliography; style=bibtex, application/x-bibtex'}
-        req = urllib2.Request(dx_url, data="", headers=headers)
-        response = urllib2.urlopen(req)
-        bibtex = response.read()
-        return bibtex
+        req = requests.get(dx_url, headers=headers)
+        return req.text
 
     def get_bibtex(self, doi):
         createfunc = functools.partial(self._raw_get_bibtex, doi)

lib/galaxy/tools/data/__init__.py
@@ -11,12 +11,11 @@ import logging
 import os
 import os.path
 import re
+import requests
 import string
 import time
 from glob import glob
 from tempfile import NamedTemporaryFile
-
-from six.moves.urllib.request import urlopen
 
 from galaxy import util
 from galaxy.util.dictifiable import Dictifiable
@@ -340,7 +339,7 @@ class TabularToolDataTable(ToolDataTable, Dictifiable):
                 if filename:
                     tmp_file = NamedTemporaryFile(prefix='TTDT_URL_%s-' % self.name)
                     try:
-                        tmp_file.write(urlopen(filename, timeout=url_timeout).read())
+                        tmp_file.write(requests.get(filename, timeout=url_timeout).text)
                     except Exception as e:
                         log.error('Error loading Data Table URL "%s": %s', filename, e)
                         continue

lib/galaxy/tools/imp_exp/unpack_tar_gz_archive.py
@@ -10,12 +10,11 @@ from __future__ import print_function
 import math
 import optparse
 import os
+import requests
 import sys
 import tarfile
 import tempfile
 from base64 import b64decode
-
-from six.moves.urllib.request import urlopen
 
 # Set max size of archive/file that will be handled to be 100 GB. This is
 # arbitrary and should be adjusted as needed.
@@ -27,18 +26,16 @@ def url_to_file(url, dest_file):
     Transfer a file from a remote URL to a temporary file.
     """
     try:
-        url_reader = urlopen(url)
+        url_reader = requests.get(url, stream=True)
         CHUNK = 10 * 1024  # 10k
         total = 0
         fp = open(dest_file, 'wb')
-        while True:
-            chunk = url_reader.read(CHUNK)
-            if not chunk:
-                break
-            fp.write(chunk)
-            total += CHUNK
-            if total > MAX_SIZE:
-                break
+        for chunk in url_reader.iter_content(chunk_size=CHUNK):
+            if chunk:
+                fp.write(chunk)
+                total += CHUNK
+                if total > MAX_SIZE:
+                    break
         fp.close()
         return dest_file
     except Exception as e:

commit c852de734dbc8d95ac925c103f4cad563d3f9e53

Fix cron/build_chrom_db.py
Fix the following errors:

```
$ python cron/build_chrom_db.py tool-data/shared/ucsc/chrom/ tool-data/shared/ucsc/builds.txt
Retrieving hg38
Failed to retrieve hg38: Problem parsing line ''
Retrieving hg19
Failed to retrieve hg19: Problem parsing line ''
...
```

Broken in commit 9b8e76f9d9df0ae0ba6238d44a8f07b97fe34d6f .

cron/build_chrom_db.py
@@ -37,7 +37,7 @@ def getchrominfo(url, db):
         "position": "",
         "hgta_doTopSubmit": "get info"})
     page = requests.get(URL).text
-    for line in page.split('\n'):
+    for i, line in enumerate(page.splitlines()):
         line = line.rstrip("\r\n")
         if line.startswith("#"):
             continue
@@ -45,7 +45,7 @@ def getchrominfo(url, db):
         if len(fields) > 1 and len(fields[0]) > 0 and int(fields[1]) > 0:
             yield [fields[0], fields[1]]
         else:
-            raise Exception("Problem parsing line '%s'" % line)
+            raise Exception("Problem parsing line %d '%s' in page '%s'" % (i, line, page))
 
 
 if __name__ == "__main__":