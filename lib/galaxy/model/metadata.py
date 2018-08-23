galaxyproject/galaxy

commit 9780d069c8646fb4f02de848d62159680d95ae79

Optimized dataset copying during histories.
Use only one unqualified flush to prevent constant repeated invalidation off all SA just to get a single ID for a new object. For a simple history with 10 datasets - this reduces the copy() time in testing from 260 ms to 70ms.

Some random times:

Before optimization:

10 datasets - with a MetadataFile metadata parameter
history copied (390.457 ms)
history copied (398.501 ms)
history copied (360.997 ms)

10 datasets  - without a MetadataFile metadata parameter
history copied (291.972 ms)
history copied (276.479 ms)
history copied (272.323 ms)

10 datasets - with a MetadataFile metadata parameter
history copied (230.171 ms)
history copied (218.215 ms)
history copied (226.205 ms)

10 datasets - without a MetadataFile metadata parameter
history copied (66.360 ms)
history copied (70.126 ms)
history copied (65.166 ms)

lib/galaxy/model/metadata.py
@@ -513,7 +513,7 @@ class FileParameter(MetadataParameter):
         if value:
             new_value = galaxy.model.MetadataFile(dataset=target_context.parent, name=self.spec.name)
             object_session(target_context.parent).add(new_value)
-            object_session(target_context.parent).flush()
+            object_session(target_context.parent).flush([new_value])
             shutil.copy(value.file_name, new_value.file_name)
             return self.unwrap(new_value)
         return None
@@ -563,7 +563,7 @@ class FileParameter(MetadataParameter):
         if object_session(dataset):
             mf = galaxy.model.MetadataFile(name=self.spec.name, dataset=dataset, **kwds)
             object_session(dataset).add(mf)
-            object_session(dataset).flush()  # flush to assign id
+            object_session(dataset).flush([mf])  # flush to assign id
             return mf
         else:
             # we need to make a tmp file that is accessable to the head node,
@@ -766,7 +766,7 @@ class JobExternalOutputMetadataWrapper(object):
                 json.dump(override_metadata, open(metadata_files.filename_override_metadata, 'wt+'))
                 # add to session and flush
                 sa_session.add(metadata_files)
-                sa_session.flush()
+                sa_session.flush([metadata_files])
             metadata_files_list.append(metadata_files)
         args = '"%s" "%s" %s %s' % (metadata_path_on_compute(datatypes_config),
                                     job_metadata,

lib/galaxy/model/__init__.py
@@ -1458,7 +1458,7 @@ class History(HasTags, Dictifiable, UsesAnnotations, HasName):
         new_history = History(name=name, user=target_user)
         db_session = object_session(self)
         db_session.add(new_history)
-        db_session.flush()
+        db_session.flush([new_history])
 
         # copy history tags and annotations (if copying user is not anonymous)
         if target_user:
@@ -1474,10 +1474,8 @@ class History(HasTags, Dictifiable, UsesAnnotations, HasName):
             hdas = self.active_datasets
         for hda in hdas:
             # Copy HDA.
-            new_hda = hda.copy()
+            new_hda = hda.copy(force_flush=False)
             new_history.add_dataset(new_hda, set_hid=False, quota=applies_to_quota)
-            db_session.add(new_hda)
-            db_session.flush()
 
             if target_user:
                 new_hda.copy_item_annotation(db_session, self.user, hda, target_user, new_hda)
@@ -1499,7 +1497,6 @@ class History(HasTags, Dictifiable, UsesAnnotations, HasName):
                 new_hdca.copy_tags_from(target_user, hdca)
 
         new_history.hid_counter = self.hid_counter
-        db_session.add(new_history)
         db_session.flush()
 
         return new_history
@@ -2580,23 +2577,22 @@ class HistoryDatasetAssociation(DatasetInstance, HasTags, Dictifiable, UsesAnnot
                                         visible=self.visible,
                                         deleted=self.deleted,
                                         parent_id=parent_id,
-                                        copied_from_history_dataset_association=self)
+                                        copied_from_history_dataset_association=self,
+                                        flush=False)
         # update init non-keywords as well
         hda.purged = self.purged
         hda.copy_tags_to(copy_tags)
-        # This next line seems unneeded. -John
-        hda.set_size()
         object_session(self).add(hda)
         flushed = False
         # May need to set after flushed, as MetadataFiles require dataset.id
         if hda.set_metadata_requires_flush:
-            object_session(self).flush()
+            object_session(self).flush([self])
             flushed = True
         hda.metadata = self.metadata
         # In some instances peek relies on dataset_id, i.e. gmaj.zip for viewing MAFs
         if not self.datatype.copy_safe_peek:
             if not flushed:
-                object_session(self).flush()
+                object_session(self).flush([self])
 
             hda.set_peek()
         if force_flush:

commit ac29abb6266fb9b83c985b27804db607c503ef51

Fix metadata elements reset when creating Metadata files
This is a fallout out from 9780d069c8646fb4f02de848d62159680d95ae79

lib/galaxy/model/metadata.py
@@ -167,6 +167,13 @@ class MetadataCollection(object):
                 raise ValueError("json_dict must be either a dictionary or a string, got %s." % (type(json_dict)))
         else:
             raise ValueError("You must provide either a filename or a json_dict")
+
+        # We build a dictionary for metadata name / value pairs
+        # because when we copy MetadatTempfile objects we flush the datasets'
+        # session, but only include the newly created MetadataFile object.
+        # If we were to set the metadata elements in the first for loop we'd
+        # lose all previously set metadata elements
+        metadata_name_value = {}
         for name, spec in self.spec.items():
             if name in JSONified_dict:
                 from_ext_kwds = {}
@@ -174,11 +181,14 @@ class MetadataCollection(object):
                 param = spec.param
                 if isinstance(param, FileParameter):
                     from_ext_kwds['path_rewriter'] = path_rewriter
-                dataset._metadata[name] = param.from_external_value(external_value, dataset, **from_ext_kwds)
+                value = param.from_external_value(external_value, dataset, **from_ext_kwds)
+                metadata_name_value[name] = value
             elif name in dataset._metadata:
                 # if the metadata value is not found in our externally set metadata but it has a value in the 'old'
                 # metadata associated with our dataset, we'll delete it from our dataset's metadata dict
                 del dataset._metadata[name]
+        for name, value in metadata_name_value.items():
+            dataset._metadata[name] = value
         if '__extension__' in JSONified_dict:
             dataset.extension = JSONified_dict['__extension__']
 