55d350d015f20afc1c350130caaa0aa9858b7aa0
mvdbeek <m.vandenbeek@gmail.com>
2018-08-26 20:36:51 +0200

    Add static for 2a148ff1ec37c68e280c431fceee88ee0510e017

--- static/scripts/mocha/test.js
+++ static/scripts/mocha/test.js
@@ -1,1 +1,2 @@
 define("mocha/test",["mocha/tests/rules_tests"],function(t){"use strict";!function(t){t&&t.__esModule}(t)});
+//# sourceMappingURL=../../maps/mocha/test.js.map
--- static/scripts/utils/localization.js
+++ static/scripts/utils/localization.js
@@ -1,1 +1,2 @@
 define(["i18n!nls/locale"],function(e){e.hasOwnProperty("__root")&&(locale=sessionStorage.getItem("currentLocale"),locale&&(e=e["__"+locale]||e["__"+locale.split("-")[0]]||e.__root));var o=function(o){return e[o]||o};return o.cacheNonLocalized=!1,o});
+//# sourceMappingURL=../../maps/utils/localization.js.map
