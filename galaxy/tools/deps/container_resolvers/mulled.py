4db7f3990f8ed8593c770fef1521e916ba4a75ef
John Chilton <jmchilton@gmail.com>
2018-09-13 13:00:06 -0400

    Unicodify only once in mulled cmd handling.
    Version of this patch that was applied to galaxy.

--- galaxy/tools/deps/container_resolvers/mulled.py
+++ galaxy/tools/deps/container_resolvers/mulled.py
@@ -55,8 +55,8 @@ CachedV2MulledImageMultiTarget.package_hash = _package_hash
 
 def list_docker_cached_mulled_images(namespace=None, hash_func="v2"):
     command = build_docker_images_command(truncate=True, sudo=False, to_str=False)
-    images_and_versions = subprocess.check_output(command).strip().splitlines()
-    images_and_versions = [unicodify(l).split()[0:2] for l in images_and_versions[1:]]
+    images_and_versions = unicodify(subprocess.check_output(command)).strip().splitlines()
+    images_and_versions = [l.split()[0:2] for l in images_and_versions[1:]]
     name_filter = get_filter(namespace)
 
     def output_line_to_image(line):
