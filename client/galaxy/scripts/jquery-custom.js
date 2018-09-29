70efe90dcdc66d85cf0713b44300624ba661ef1a
Mason <nerdinacan@gmail.com>
2018-09-29 15:07:22 -0700

    Added commentary, placeholder for more jquery plugins

--- client/galaxy/scripts/jquery-custom.js
+++ client/galaxy/scripts/jquery-custom.js
@@ -1,4 +1,7 @@
 // Having a hard time making this work with standard es6 import/exports
 var $ = require("jqueryVendor");
+
 require("libs/jquery/select2");
+// PLACE MORE PLUGINS HERE
+
 module.exports = $;
