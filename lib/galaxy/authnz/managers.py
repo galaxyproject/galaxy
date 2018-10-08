d15263329ee98470b54c36106ad5f3a352e64af0
vahid <jalili.vahid@gmail.com>
2018-10-08 11:08:19 -0700

    Remove trans from _extend_cloudauthz_config.

--- lib/galaxy/authnz/managers.py
+++ lib/galaxy/authnz/managers.py
@@ -124,12 +124,12 @@ class AuthnzManager(object):
             log.debug(msg)
             return False, msg, None
 
-    def _extend_cloudauthz_config(self, trans, cloudauthz):
+    def _extend_cloudauthz_config(self, cloudauthz, request, session, sa_session, user_id):
         config = copy.deepcopy(cloudauthz.config)
         if cloudauthz.provider == "aws":
             success, message, backend = self._get_authnz_backend(cloudauthz.authn.provider)
-            strategy = Strategy(trans.request, trans.session, Storage, backend.config)
-            on_the_fly_config(trans.sa_session)
+            strategy = Strategy(request, session, Storage, backend.config)
+            on_the_fly_config(sa_session)
             try:
                 config['id_token'] = cloudauthz.authn.get_id_token(strategy)
             except requests.exceptions.HTTPError as e:
@@ -138,7 +138,7 @@ class AuthnzManager(object):
                 log.debug("Failed to get/refresh ID token for user with ID `{}` for assuming authz_id `{}`. "
                           "User may not have a refresh token. If the problem persists, set the `prompt` key to "
                           "`consent` in `oidc_backends_config.xml`, then restart Galaxy and ask user to: {}"
-                          "Error Message: `{}`".format(trans.user.id, cloudauthz.id, msg, e.response.text))
+                          "Error Message: `{}`".format(user_id, cloudauthz.id, msg, e.response.text))
                 raise exceptions.AuthenticationFailed(
                     err_msg="An error occurred getting your ID token. {}. If the problem persists, please "
                             "contact Galaxy admin.".format(msg))
@@ -254,7 +254,7 @@ class AuthnzManager(object):
                             resource provider. See CloudAuthz (https://github.com/galaxyproject/cloudauthz)
                             for details on the content of this dictionary.
         """
-        config = self._extend_cloudauthz_config(trans, cloudauthz)
+        config = self._extend_cloudauthz_config(cloudauthz, trans.request, trans.session, trans.sa_session, trans.user.id)
         try:
             ca = CloudAuthz()
             return ca.authorize(cloudauthz.provider, config)
