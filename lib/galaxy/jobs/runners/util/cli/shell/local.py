galaxyproject/galaxy

commit 57a1f19b069597bde97852b3e0d0a3e1c0068bc4

Fix slurm cli plugin by avoiding `shell=True` in subprocess
Fixes the remaining issue in https://github.com/galaxyproject/galaxy/issues/6831#issuecomment-428097520

lib/galaxy/jobs/runners/util/cli/shell/local.py
@@ -44,7 +44,7 @@ class LocalShell(BaseShellExec):
 
     def execute(self, cmd, persist=False, timeout=DEFAULT_TIMEOUT, timeout_check_interval=DEFAULT_TIMEOUT_CHECK_INTERVAL, **kwds):
         outf = TemporaryFile()
-        p = Popen(cmd, shell=True, stdin=None, stdout=outf, stderr=PIPE)
+        p = Popen(cmd, stdin=None, stdout=outf, stderr=PIPE)
         # poll until timeout
 
         for i in range(int(timeout / timeout_check_interval)):

lib/galaxy/jobs/runners/util/cli/shell/rsh.py
@@ -15,20 +15,23 @@ __all__ = ('RemoteShell', 'SecureShell', 'GlobusSecureShell', 'ParamikoShell')
 
 class RemoteShell(LocalShell):
 
-    def __init__(self, rsh='rsh', rcp='rcp', hostname='localhost', username=None, **kwargs):
+    def __init__(self, rsh='rsh', rcp='rcp', hostname='localhost', username=None, options=None, **kwargs):
         super(RemoteShell, self).__init__(**kwargs)
         self.rsh = rsh
         self.rcp = rcp
         self.hostname = hostname
         self.username = username
+        self.options = options
         self.sessions = {}
 
     def execute(self, cmd, persist=False, timeout=60):
         # TODO: implement persistence
-        if self.username is None:
-            fullcmd = '%s %s %s' % (self.rsh, self.hostname, cmd)
-        else:
-            fullcmd = '%s -l %s %s %s' % (self.rsh, self.username, self.hostname, cmd)
+        fullcmd = [self.rsh]
+        if self.options:
+            fullcmd.extend(self.options)
+        if self.username:
+            fullcmd.extend(["-l", self.username])
+        fullcmd.extend([self.hostname, cmd])
         return super(RemoteShell, self).execute(fullcmd, persist, timeout)
 
 
@@ -37,15 +40,13 @@ class SecureShell(RemoteShell):
 
     def __init__(self, rsh='ssh', rcp='scp', private_key=None, port=None, strict_host_key_checking=True, **kwargs):
         strict_host_key_checking = "yes" if strict_host_key_checking else "no"
-        rsh += " -oStrictHostKeyChecking=%s -oConnectTimeout=60" % strict_host_key_checking
-        rcp += " -oStrictHostKeyChecking=%s -oConnectTimeout=60" % strict_host_key_checking
+        options = ["-oStrictHostKeyChecking=%s" % strict_host_key_checking]
+        options.append("-oConnectTimeout=60")
         if private_key:
-            rsh += " -i %s" % private_key
-            rcp += " -i %s" % private_key
+            options.extend(['-i', private_key])
         if port:
-            rsh += " -p %s" % port
-            rcp += " -p %s" % port
-        super(SecureShell, self).__init__(rsh=rsh, rcp=rcp, **kwargs)
+            options.extend(['-p', str(port)])
+        super(SecureShell, self).__init__(rsh=rsh, rcp=rcp, options=options, **kwargs)
 
 
 class ParamikoShell(object):

commit 19f716de9141080ecf3043fe3aeac391c55b8158

Fix ``LocalShell`` doctest
Broken in commit 57a1f19b069597bde97852b3e0d0a3e1c0068bc4 .

lib/galaxy/jobs/runners/util/cli/shell/local.py
@@ -24,7 +24,7 @@ class LocalShell(BaseShellExec):
     """
 
     >>> shell = LocalShell()
-    >>> def exec_python(script, **kwds): return shell.execute('python -c "%s"' % script, **kwds)
+    >>> def exec_python(script, **kwds): return shell.execute(['python', '-c', script], **kwds)
     >>> exec_result = exec_python("from __future__ import print_function; print('Hello World')")
     >>> exec_result.stderr == u''
     True