116ce0ddf51654cfbb0923e7afc1ae5b035bd602
vahid <jalili.vahid@gmail.com>
2018-10-15 12:21:57 -0700

    Specify `GET` action for cloudauthz index.

--- lib/galaxy/webapps/galaxy/buildapp.py
+++ lib/galaxy/webapps/galaxy/buildapp.py
@@ -295,7 +295,7 @@ def populate_api_routes(webapp, app):
     webapp.mapper.resource('group', 'groups', path_prefix='/api')
     webapp.mapper.resource_with_deleted('quota', 'quotas', path_prefix='/api')
 
-    webapp.mapper.connect('/api/cloud/authz/', action='index', controller='cloudauthz')
+    webapp.mapper.connect('/api/cloud/authz/', action='index', controller='cloudauthz', conditions=dict(method=["GET"]))
     webapp.mapper.connect('/api/cloud/authz/create', action='create', controller='cloudauthz')
 
     webapp.mapper.connect('get_custom_builds_metadata',
