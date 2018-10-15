4bd6d71ed71643a9f28db9e41a6cf43e1af9a942
guerler <aysam.guerler@gmail.com>
2018-10-15 15:12:03 -0400

    Add fix for rule editor element

--- client/galaxy/scripts/mvc/ui/ui-rules-edit.js
+++ client/galaxy/scripts/mvc/ui/ui-rules-edit.js
@@ -96,7 +96,7 @@ var View = Backbone.View.extend({
     /** Main Template */
     _template: function(options) {
         return `
-            <div class="ui-rules-edit">
+            <div class="ui-rules-edit clearfix">
                 <span class="ui-rules-preview" />
                 <span class="ui-rules-edit-button" />
             </div>
