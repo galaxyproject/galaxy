efa375e238d2808639c95f511e7242de0c15453f
Martin Cech <marten@bx.psu.edu>
2018-10-18 12:41:22 -0400

    remove unneeded import

--- lib/galaxy/webapps/galaxy/controllers/history.py
+++ lib/galaxy/webapps/galaxy/controllers/history.py
@@ -17,7 +17,6 @@ from galaxy.model.item_attrs import (
 from galaxy.util import listify, Params, parse_int, sanitize_text
 from galaxy.util.create_history_template import render_item
 from galaxy.util.odict import odict
-from galaxy.util.sanitize_html import sanitize_html
 from galaxy.web import url_for
 from galaxy.web.base.controller import (
     BaseUIController,
