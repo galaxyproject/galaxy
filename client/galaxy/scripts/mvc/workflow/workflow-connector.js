58a1b4589250866d2de6ffdd2213232cb0d7c9cb
guerler <aysam.guerler@gmail.com>
2018-11-05 15:40:59 -0500

    Style revision workflow editor

--- client/galaxy/scripts/mvc/workflow/workflow-connector.js
+++ client/galaxy/scripts/mvc/workflow/workflow-connector.js
@@ -4,7 +4,7 @@ function Connector(handle1, handle2) {
     this.canvas = null;
     this.dragging = false;
     this.inner_color = "#FFFFFF";
-    this.outer_color = "#D8B365";
+    this.outer_color = "#6c6";
     if (handle1 && handle2) {
         this.connect(
             handle1,
