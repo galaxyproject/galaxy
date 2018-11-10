4c33c8ebef7c47eecb743e6cda8c59090318e777
Mason <nerdinacan@gmail.com>
2018-11-09 16:58:11 -0800

    Updated comments in monitor

--- client/galaxy/scripts/app/singleton.js
+++ client/galaxy/scripts/app/singleton.js
@@ -1,15 +1,10 @@
 /**
  * TODO: Make this part of galaxy.js later, once the global refs are gone
- *
- * DO NOT import GalaxyApp into this file or you will create a catch-22 circular
- * dependency situation with the galaxy.monitor as window.Galaxy refers to the
- * instance and the instance refers to hard-coded Galaxy config variables which
- * refer back to window.Galaxy
  */
 
 import addLogging from "utils/add-logging";
 import { GalaxyApp } from "./galaxy";
-import { serverPath } from "utils/serverPath";
+// import { serverPath } from "utils/serverPath";
 
 export function setGalaxyInstance(factory, atTop = true) {
     // console.warn("setGalaxyInstance", serverPath());
