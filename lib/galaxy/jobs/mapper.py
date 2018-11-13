f46ab5a5dc21c1e8dd115ec3d9a37e0e3ee26a56
Nuwan Goonasekera <nuwan.ag@gmail.com>
2018-11-13 23:56:39 +0530

    Fix debug logging

--- lib/galaxy/jobs/mapper.py
+++ lib/galaxy/jobs/mapper.py
@@ -210,12 +210,13 @@ class JobRunnerMapper(object):
             raw_job_destination = self.job_wrapper.tool.get_job_destination(params)
         if raw_job_destination.runner == DYNAMIC_RUNNER_NAME:
             job_destination = self.__handle_dynamic_job_destination(raw_job_destination)
+            log.debug("(%s) Mapped job to destination id: %s", self.job_wrapper.job_id, job_destination.id)
             # Recursively handle chained dynamic destinations
-            job_destination = self.__determine_job_destination(
-                params, raw_job_destination=job_destination)
+            if job_destination.runner == DYNAMIC_RUNNER_NAME:
+                return self.__determine_job_destination(params, raw_job_destination=job_destination)
         else:
             job_destination = raw_job_destination
-        log.debug("(%s) Mapped job to destination id: %s", self.job_wrapper.job_id, job_destination.id)
+            log.debug("(%s) Mapped job to destination id: %s", self.job_wrapper.job_id, job_destination.id)
         return job_destination
 
     def __cache_job_destination(self, params, raw_job_destination=None):
