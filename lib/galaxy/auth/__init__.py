4a3fb8df710c11158aac045fb858eb35717c8bd4
Julien Van Braekel <juvanbraekel@bioit-galaxy-test.sciensano.be>
2018-12-19 16:18:45 +0100

    Fix the not logged exeption during AD authentication, now log and reraise it properly

--- lib/galaxy/auth/__init__.py
+++ lib/galaxy/auth/__init__.py
@@ -66,7 +66,8 @@ class AuthManager(object):
                     try:
                         auth_return = parse_auth_results(trans, auth_results, options)
                     except Conflict:
-                        break
+                        log.exception(Conflict)
+                        raise
                     return auth_return
                 elif auth_results[0] is None:
                     auto_email = str(auth_results[1]).lower()
