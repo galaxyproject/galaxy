a38eec9c063f17aa189d7509507264f9205b62f1
guerler <aysam.guerler@gmail.com>
2019-01-09 17:07:32 -0500

    Use urlroot and galaxy instance in password component

--- client/galaxy/scripts/components/login/Password.vue
+++ client/galaxy/scripts/components/login/Password.vue
@@ -1,31 +1,31 @@
 <template>
-    <div>
-        <b-form @submit="submit">
-            <b-card header="Change your password">
-                <b-alert :show="messageShow" :variant="messageVariant" v-html="messageText"/>
-                <b-form-group v-if="user" label="Current Password">
-                    <b-form-input type="password" v-model="current"/>
-                </b-form-group>
-                <b-form-group label="New Password">
-                    <b-form-input type="password" v-model="password"/>
-                </b-form-group>
-                <b-form-group label="Confirm password">
-                    <b-form-input type="password" v-model="confirm"/>
-                </b-form-group>
-                <b-button type="submit">Save new password</b-button>
-            </b-card>
-        </b-form>
-    </div>
+    <b-form @submit.prevent="submit">
+        <b-card header="Change your password">
+            <b-alert :show="messageShow" :variant="messageVariant" v-html="messageText"/>
+            <b-form-group v-if="user" label="Current Password">
+                <b-form-input type="password" v-model="current"/>
+            </b-form-group>
+            <b-form-group label="New Password">
+                <b-form-input type="password" v-model="password"/>
+            </b-form-group>
+            <b-form-group label="Confirm password">
+                <b-form-input type="password" v-model="confirm"/>
+            </b-form-group>
+            <b-button type="submit">Save new password</b-button>
+        </b-card>
+    </b-form>
 </template>
 <script>
 import axios from "axios";
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
 import { getGalaxyInstance } from "app";
-
-let Galaxy = getGalaxyInstance();
+import { getAppRoot } from "onload";
 
 Vue.use(BootstrapVue);
+
+let Galaxy = getGalaxyInstance();
+let urlRoot = getAppRoot();
 
 export default {
     data() {
@@ -48,7 +48,7 @@ export default {
         submit: function(ev) {
             ev.preventDefault();
             axios
-                .post(`${Galaxy.root}user/change_password`, {
+                .post(`${urlRoot}user/change_password`, {
                     token: this.token,
                     id: this.user,
                     current: this.current,
@@ -56,12 +56,12 @@ export default {
                     confirm: this.confirm
                 })
                 .then(response => {
-                    window.location = `${Galaxy.root}`;
+                    window.location = `${urlRoot}`;
                 })
                 .catch(error => {
                     this.messageVariant = "danger";
-                    let message = error.response && error.response.data && error.response.data.err_msg;
-                    this.messageText = message || "Login failed for an unknown reason.";
+                    let message = error.response.data && error.response.data.err_msg;
+                    this.messageText = message || "Password change failed for an unknown reason.";
                 });
         }
     }
