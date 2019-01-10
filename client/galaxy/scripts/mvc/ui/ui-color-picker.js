8220705766dc3e23ddfbac45d887f82f8c36c85e
Dannon Baker <dannon.baker@gmail.com>
2019-01-10 12:24:06 -0500

    Fix color picker template (was missing a trailing div before, see previous commit), also fix inner definition of hex.

--- client/galaxy/scripts/mvc/ui/ui-color-picker.js
+++ client/galaxy/scripts/mvc/ui/ui-color-picker.js
@@ -2,6 +2,11 @@
 import $ from "jquery";
 import Backbone from "backbone";
 import Utils from "utils/utils";
+
+function hex(x) {
+    return `0${parseInt(x).toString(16)}`.slice(-2);
+}
+
 export default Backbone.View.extend({
     colors: {
         standard: ["c00000", "ff0000", "ffc000", "ffff00", "92d050", "00b050", "00b0f0", "0070c0", "002060", "7030a0"],
@@ -52,14 +57,12 @@ export default Backbone.View.extend({
         return this._getValue();
     },
 
+
     /** Get value from dom */
     _getValue: function() {
         var rgb = this.$value.css("background-color");
         rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
         if (rgb) {
-            function hex(x) {
-                return `0${parseInt(x).toString(16)}`.slice(-2);
-            }
             return `#${hex(rgb[1])}${hex(rgb[2])}${hex(rgb[3])}`;
         } else {
             return null;
@@ -126,14 +129,17 @@ export default Backbone.View.extend({
     /** Main template */
     _template: function() {
         return (
-            '<div class="ui-color-picker">' +
-            '<div class="ui-color-picker-header">' +
-            '<div class="ui-color-picker-value"/>' +
-            '<div class="ui-color-picker-label">Select a color</div>' +
-            "</div>" +
-            '<div class="ui-color-picker-view ui-input">' +
-            '<div class="ui-color-picker-panel"/>' +
-            "</div>"
+            `
+            <div class="ui-color-picker">
+                <div class="ui-color-picker-header">
+                    <div class="ui-color-picker-value"/>
+                    <div class="ui-color-picker-label">Select a color</div>
+                </div>
+                <div class="ui-color-picker-view ui-input">
+                    <div class="ui-color-picker-panel"/>
+                </div>
+            </div>
+            `
         );
     }
 });
