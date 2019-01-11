4bb6f18b30d850019230cd4e347b574ac0593052
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2019-01-11 17:20:00 +0000

    Do not build rst files starting with '_'

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -103,10 +103,9 @@ release = VERSION
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
+exclude_patterns = ['**/_*.rst']
 if SKIP_SOURCE:
-    exclude_patterns = ['lib', 'releases']
-else:
-    exclude_patterns = []
+    exclude_patterns.extend(['lib', 'releases'])
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
