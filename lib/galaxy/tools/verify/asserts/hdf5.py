d36cf51f6b63407fd96e49918fe6092d993275a9
Bjoern Gruening <bjoern.gruening@gmail.com>
2019-02-06 16:31:15 +0100

    change "name" to "key"

--- lib/galaxy/tools/verify/asserts/hdf5.py
+++ lib/galaxy/tools/verify/asserts/hdf5.py
@@ -13,14 +13,14 @@ def _assert_h5py():
         raise Exception(IMPORT_MISSING_MESSAGE)
 
 
-def assert_has_h5_attribute(output_bytes, name, value):
+def assert_has_h5_attribute(output_bytes, key, value):
     """Asserts the specified HDF5 output has a given key-value pair as HDF5
     attribute"""
     _assert_h5py()
     output_temp = io.BytesIO(output_bytes)
     local_attrs = h5py.File(output_temp, 'r').attrs
-    assert name in local_attrs and str(local_attrs[name]) == value, (
-        "Not a HDF5 file or H5 attributes do not match:\n\t%s\n\n\t(%s : %s)" % (local_attrs.items(), name, value))
+    assert key in local_attrs and str(local_attrs[key]) == value, (
+        "Not a HDF5 file or H5 attributes do not match:\n\t%s\n\n\t(%s : %s)" % (local_attrs.items(), key, value))
 
 
 def assert_has_h5_keys(output_bytes, keys):
