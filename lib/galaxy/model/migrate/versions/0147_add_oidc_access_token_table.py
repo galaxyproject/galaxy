29d55e4349a46e3f40f768054b5c2125aad4fcb5
Marcus Christie <machrist@iu.edu>
2019-02-08 20:44:08 -0500

    Removed Keycloak from data model

--- lib/galaxy/model/migrate/versions/0147_add_oidc_access_token_table.py
+++ lib/galaxy/model/migrate/versions/0147_add_oidc_access_token_table.py
@@ -1,29 +1,32 @@
 """
-Migration for adding keycloak_access_token table.
+Migration for adding oidc_access_token table.
 """
 from __future__ import print_function
 
 import logging
 
 from sqlalchemy import (Column, DateTime, ForeignKey, Integer, MetaData,
-                        String, Table, Text)
+                        String, Table, Text, UniqueConstraint)
 
 from galaxy.model.custom_types import JSONType
 
 log = logging.getLogger(__name__)
 metadata = MetaData()
 
-KeycloakAccessToken_table = Table(
-    "keycloak_access_token", metadata,
+OIDCAccessToken_table = Table(
+    "oidc_access_token", metadata,
     Column('id', Integer, primary_key=True),
-    Column('user_id', Integer, ForeignKey("galaxy_user.id"), unique=True, index=True),
-    Column('keycloak_user_id', String(64), unique=True, index=True),
+    Column('user_id', Integer, ForeignKey("galaxy_user.id")),
+    Column('external_user_id', String(64)),
+    Column('provider', String(255)),
     Column('access_token', Text),
     Column('id_token', Text),
     Column('refresh_token', Text),
     Column("expiration_time", DateTime),
     Column("refresh_expiration_time", DateTime),
     Column('raw_token', JSONType, nullable=True),
+    UniqueConstraint("user_id", "external_user_id", "provider"),
+    UniqueConstraint("external_user_id", "provider"),
 )
 
 
@@ -33,9 +36,9 @@ def upgrade(migrate_engine):
     metadata.reflect()
 
     try:
-        KeycloakAccessToken_table.create()
+        OIDCAccessToken_table.create()
     except Exception:
-        log.exception("Failed to create Keycloak auth tables")
+        log.exception("Failed to create oidc_access_token table")
 
 
 def downgrade(migrate_engine):
@@ -43,6 +46,6 @@ def downgrade(migrate_engine):
     metadata.reflect()
 
     try:
-        KeycloakAccessToken_table.drop()
+        OIDCAccessToken_table.drop()
     except Exception:
-        log.exception("Failed to drop Keycloak auth tables")
+        log.exception("Failed to drop oidc_access_token table")
