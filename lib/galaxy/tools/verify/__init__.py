f105745889fd6c5b956eac3d822fdf8307050599
M Bernt <m.bernt@ufz.de>
2019-02-15 15:44:33 +0100

    verify: revert to always convert BAM->SAM
    added exception handling

--- lib/galaxy/tools/verify/__init__.py
+++ lib/galaxy/tools/verify/__init__.py
@@ -103,9 +103,11 @@ def verify(
         try:
             compare = attributes.get('compare', 'diff')
             if attributes.get('ftype', None) in ['bam', 'qname_sorted.bam', 'qname_input_sorted.bam', 'unsorted.bam']:
-                if attributes.get("decompress", None):
+                try:
                     local_fh, temp_name = _bam_to_sam(local_name, temp_name)
                     local_name = local_fh.name
+                except Exception as e:
+                    log.warning( str(e), "will compare BAM files")
             if compare == 'diff':
                 files_diff(local_name, temp_name, attributes=attributes)
             elif compare == 're_match':
