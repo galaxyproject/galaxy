ac3910a691ad9ca04355854a6e9c9a58c69c95fb
Marius van den Beek <m.vandenbeek@gmail.com>
2019-02-20 14:16:41 +0100

    Update lib/galaxy/tools/verify/__init__.py
    one more space
    
    Co-Authored-By: bernt-matthias <m.bernt@ufz.de>

--- lib/galaxy/tools/verify/__init__.py
+++ lib/galaxy/tools/verify/__init__.py
@@ -107,7 +107,7 @@ def verify(
                     local_fh, temp_name = _bam_to_sam(local_name, temp_name)
                     local_name = local_fh.name
                 except Exception as e:
-                    log.warning("%s. Will compare BAM files" %e)
+                    log.warning("%s. Will compare BAM files" % e)
             if compare == 'diff':
                 files_diff(local_name, temp_name, attributes=attributes)
             elif compare == 're_match':
