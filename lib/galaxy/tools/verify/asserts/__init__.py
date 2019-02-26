091fe8121a520673fb9feeeb1884488a613b93f7
Matthew Dillon <matthewrdillon@gmail.com>
2019-02-26 15:46:45 -0700

    SQUASH: Adding in other archive containers
    Per discussion with @bgruening

--- lib/galaxy/tools/verify/asserts/__init__.py
+++ lib/galaxy/tools/verify/asserts/__init__.py
@@ -6,7 +6,7 @@ from galaxy.util import unicodify
 
 log = logging.getLogger(__name__)
 
-assertion_module_names = ['text', 'tabular', 'xml', 'hdf5', 'zip']
+assertion_module_names = ['text', 'tabular', 'xml', 'hdf5', 'archive']
 
 # Code for loading modules containing assertion checking functions, to
 # create a new module of assertion functions, create the needed python
