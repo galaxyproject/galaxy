galaxyproject/galaxy

commit 2d3ab4ce1205aec24664225d74b2a851cb94b2b0

Allow specifying resolver index and resolver_type when installing dependencies via tools API

lib/galaxy/tools/deps/views.py
@@ -99,8 +99,8 @@ class DependencyResolversView(object):
                     envs_to_remove = envs_to_remove.difference(can_remove)
         return list(removed_environments)
 
-    def install_dependencies(self, requirements):
-        return self._dependency_manager._requirements_to_dependencies_dict(requirements, **{'install': True})
+    def install_dependencies(self, requirements, **kwds):
+        return self._dependency_manager._requirements_to_dependencies_dict(requirements, **kwds)
 
     def install_dependency(self, index=None, **payload):
         """

lib/galaxy/tools/deps/__init__.py
@@ -156,10 +156,12 @@ class DependencyManager(object):
 
         return requirement_to_dependency
 
-    def _requirements_to_dependencies_dict(self, requirements, **kwds):
+    def _requirements_to_dependencies_dict(self, requirements, search=False, **kwds):
         """Build simple requirements to dependencies dict for resolution."""
         requirement_to_dependency = OrderedDict()
-        index = kwds.get('index', None)
+        index = kwds.get('index')
+        install = kwds.get('install', False)
+        resolver_type = kwds.get('resolver_type')
         require_exact = kwds.get('exact', False)
         return_null_dependencies = kwds.get('return_null', False)
 
@@ -171,6 +173,9 @@ class DependencyManager(object):
             _requirement_to_dependency = OrderedDict([(k, v) for k, v in requirement_to_dependency.items() if not isinstance(v, NullDependency)])
 
             if index is not None and i != index:
+                continue
+
+            if resolver_type is not None and resolver.resolver_type != resolver_type:
                 continue
 
             if len(_requirement_to_dependency) == len(resolvable_requirements):
@@ -186,9 +191,9 @@ class DependencyManager(object):
             if hasattr(resolver, "resolve_all"):
                 resolve = resolver.resolve_all
             elif isinstance(resolver, ContainerResolver):
-                if not resolver.resolver_type.startswith(('cached', 'explicit')):
+                if not resolver.resolver_type.startswith(('cached', 'explicit')) and not (search or install):
                     # These would look up available containers using the quay API,
-                    # we only want ot do this if we search for containers
+                    # we only want to do this if we search for containers
                     continue
                 resolve = resolver.resolve
             else:

lib/galaxy/webapps/galaxy/api/tools.py
@@ -253,7 +253,8 @@ class ToolsController(BaseAPIController, UsesVisualizationMixin):
             force_rebuild:           If true and cache dir exists, attempts to delete cache dir
         """
         tool = self._get_tool(id)
-        tool._view.install_dependencies(tool.requirements)
+        kwds['install'] = True
+        tool._view.install_dependencies(tool.requirements, **kwds)
         if kwds.get('build_dependency_cache'):
             tool.build_dependency_cache(**kwds)
         # TODO: rework resolver install system to log and report what has been done.

commit 51d167b85d218c3181ebd6f6f67c7333f981746a

Fix dependency installation in UI
We need to pass the install kwarg here.
Broken in https://github.com/galaxyproject/galaxy/commit/2d3ab4ce1205aec24664225d74b2a851cb94b2b0

lib/galaxy/tools/deps/views.py
@@ -104,6 +104,7 @@ class DependencyResolversView(object):
         return list(removed_environments)
 
     def install_dependencies(self, requirements, **kwds):
+        kwds['install'] = True
         return self._dependency_manager._requirements_to_dependencies_dict(requirements, **kwds)
 
     def install_dependency(self, index=None, **payload):