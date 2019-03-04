galaxyproject/galaxy

commit dd8d74f364770a1272555a3753f5d1e1288315e1

Unify data/non-data input/output handling for workflow editor.
Don't treat data and non-data inputs/outputs different at the API boundary or at the workflow-manager.js level. Keep differences at the level of walking inputs/outputs in the workflow module on the backend and at the terminal definition (models and views) level on the frontend. Make the code and interfaces more consistent in my opinion and is more compatible with future directions I'd like to take things with mix-mode inputs/outputs for CWL.

lib/galaxy/workflow/modules.py
@@ -35,7 +35,7 @@ from galaxy.tools.parameters.basic import (
     DataToolParameter,
     is_runtime_value,
     parameter_types,
-    RuntimeValue,
+    runtime_to_json,
     SelectToolParameter,
     TextToolParameter,
     workflow_building_modes
@@ -152,15 +152,18 @@ class WorkflowModule(object):
         """ This returns inputs displayed in the workflow editor """
         return {}
 
+    def get_all_inputs(self, data_only=False, connectable_only=False):
+        return []
+
     def get_data_inputs(self):
         """ Get configure time data input descriptions. """
+        return self.get_all_inputs(data_only=True)
+
+    def get_all_outputs(self, data_only=False):
         return []
 
     def get_data_outputs(self):
-        return []
-
-    def get_input_parameters(self):
-        return []
+        return self.get_all_outputs(data_only=True)
 
     def get_post_job_actions(self, incoming):
         return []
@@ -365,7 +368,7 @@ class SubWorkflowModule(WorkflowModule):
             return self.subworkflow.name
         return self.name
 
-    def get_all_inputs(self, data_only=False):
+    def get_all_inputs(self, data_only=False, connectable_only=False):
         """ Get configure time data input descriptions. """
         # Filter subworkflow steps and get inputs
         step_to_input_type = {
@@ -393,10 +396,7 @@ class SubWorkflowModule(WorkflowModule):
                 inputs.append(input)
         return inputs
 
-    def get_data_inputs(self):
-        return self.get_all_inputs(data_only=True)
-
-    def get_data_outputs(self):
+    def get_all_outputs(self, data_only=False):
         outputs = []
         if hasattr(self.subworkflow, 'workflow_outputs'):
             from galaxy.managers.workflows import WorkflowContentsManager
@@ -407,7 +407,7 @@ class SubWorkflowModule(WorkflowModule):
                                                                                   tooltip=False)
             for order_index in sorted(subworkflow_dict['steps']):
                 step = subworkflow_dict['steps'][order_index]
-                data_outputs = subworkflow_dict['steps'][order_index]['data_outputs']
+                data_outputs = subworkflow_dict['steps'][order_index]['outputs']
                 for workflow_output in step['workflow_outputs']:
                     label = workflow_output['label']
                     if not label:
@@ -502,7 +502,7 @@ class InputModule(WorkflowModule):
         state.inputs = dict(input=None)
         return state
 
-    def get_data_inputs(self):
+    def get_all_inputs(self, data_only=False, connectable_only=False):
         return []
 
     def execute(self, trans, progress, invocation_step, use_cached_job=False):
@@ -542,7 +542,7 @@ class InputDataModule(InputModule):
     type = "data_input"
     name = "Input dataset"
 
-    def get_data_outputs(self):
+    def get_all_outputs(self, data_only=False):
         return [dict(name='output', extensions=['input'])]
 
     def get_filter_set(self, connections=None):
@@ -583,7 +583,7 @@ class InputDataCollectionModule(InputModule):
         input_element = Element("param", name="input", label=self.label, type="data_collection", collection_type=collection_type)
         return dict(input=DataCollectionToolParameter(None, input_element, self.trans))
 
-    def get_data_outputs(self):
+    def get_all_outputs(self, data_only=False):
         return [
             dict(
                 name='output',
@@ -645,11 +645,16 @@ class InputParameterModule(WorkflowModule):
         state.inputs = dict(input=None)
         return state
 
-    def get_data_inputs(self):
-        return [self.state.inputs]
+    def get_all_outputs(self, data_only=False):
+        if data_only:
+            return []
 
-    def get_input_parameters(self):
-        return [dict(name=self.name, label=self.label, type=self.state.inputs.get('parameter_type', self.parameter_type), optional=self.state.inputs.get('optional', self.optional))]
+        return [dict(
+            name='output',
+            label=self.label,
+            type=self.state.inputs.get('parameter_type', self.parameter_type),
+            parameter=True,
+        )]
 
     def execute(self, trans, progress, invocation_step, use_cached_job=False):
         step = invocation_step.workflow_step
@@ -664,7 +669,7 @@ class PauseModule(WorkflowModule):
     type = "pause"
     name = "Pause for dataset review"
 
-    def get_data_inputs(self):
+    def get_all_inputs(self, data_only=False, connectable_only=False):
         input = dict(
             name="input",
             label="Dataset for Review",
@@ -672,9 +677,9 @@ class PauseModule(WorkflowModule):
             extensions='input',
             input_type="dataset",
         )
-        return [input]
+        return [input] if not data_only else []
 
-    def get_data_outputs(self):
+    def get_all_outputs(self, data_only=False):
         return [dict(name="output", label="Reviewed Dataset", extensions=['input'])]
 
     def get_runtime_state(self):
@@ -816,14 +821,24 @@ class ToolModule(WorkflowModule):
     def get_inputs(self):
         return self.tool.inputs if self.tool else {}
 
-    def get_all_inputs(self, data_only=False):
+    def get_all_inputs(self, data_only=False, connectable_only=False):
+        if data_only and connectable_only:
+            raise Exception("Must specify at most one of data_only and connectable_only as True.")
+
         inputs = []
         if self.tool:
 
-            def callback(input, prefixed_name, prefixed_label, **kwargs):
+            def callback(input, prefixed_name, prefixed_label, value=None, **kwargs):
                 visible = not hasattr(input, 'hidden') or not input.hidden
+                input_type = input.type
                 is_data = isinstance(input, DataToolParameter) or isinstance(input, DataCollectionToolParameter)
-                skip = data_only and (not visible or not is_data)
+                is_connectable = is_runtime_value(value) and runtime_to_json(value)["__class__"] == "ConnectedValue"
+                if data_only:
+                    skip = not visible or not is_data
+                elif connectable_only:
+                    skip = not visible or not (is_data or is_connectable)
+                else:
+                    skip = not visible
                 if not skip:
                     if isinstance(input, DataToolParameter):
                         inputs.append(dict(
@@ -848,16 +863,14 @@ class ToolModule(WorkflowModule):
                                 label=prefixed_label,
                                 multiple=False,
                                 input_type="parameter",
+                                type=input_type,
                             )
                         )
 
             visit_input_values(self.tool.inputs, self.state.inputs, callback)
         return inputs
 
-    def get_data_inputs(self):
-        return self.get_all_inputs(data_only=True)
-
-    def get_data_outputs(self):
+    def get_all_outputs(self, data_only=False):
         data_outputs = []
         if self.tool:
             for name, tool_output in self.tool.outputs.items():

client/galaxy/scripts/mvc/workflow/workflow-node.js
@@ -200,19 +200,6 @@ var Node = Backbone.Model.extend({
             this.content_id = this.config_form.id;
         }
     },
-    add_output_parameters: function(input_parameters) {
-        var self = this;
-        $.each(input_parameters, (i, input_parameter) => {
-            self.nodeView.addParameterOutput(input_parameter);
-        });
-    },
-    add_input_parameters: function() {
-        $.each(this.config_form.inputs, (i, input) => {
-            if (input.value && input.value.__class__ == 'ConnectedValue' && StepParameterTypes.includes(input.type)){
-                this.nodeView.addParameterInput(input);
-            }
-        });
-    },
 
     init_field_data: function(data) {
         //console.debug("init_field_data: ", data);
@@ -236,27 +223,23 @@ var Node = Backbone.Model.extend({
             node: node
         });
         node.nodeView = nodeView;
-        $.each(data.data_inputs, (i, input) => {
-            if (nodeView.node.type != 'parameter_input') {
-                nodeView.addDataInput(input);
-            }
+        $.each(data.inputs, (i, input) => {
+            nodeView.addDataInput(input);
         });
 
-        if (data.data_inputs.length > 0 && data.data_outputs.length > 0) {
+        if (data.inputs.length > 0 && data.outputs.length > 0) {
             nodeView.addRule();
         }
-        $.each(data.data_outputs, (i, output) => {
+        $.each(data.outputs, (i, output) => {
             nodeView.addDataOutput(output);
         });
-        this.add_input_parameters();
-        this.add_output_parameters(data.input_parameters);
         nodeView.render();
         this.app.workflow.node_changed(this, true);
     },
     update_field_data: function(data) {
         var node = this;
         var nodeView = node.nodeView;
-        // remove unused output views and remove pre-existing output views from data.data_outputs,
+        // remove unused output views and remove pre-existing output views from data.outputs,
         // so that these are not added twice.
         var unused_outputs = [];
         // nodeView.outputViews contains pre-existing outputs,
@@ -264,7 +247,7 @@ var Node = Backbone.Model.extend({
         // Now we gather the unused outputs
         $.each(nodeView.outputViews, (i, output_view) => {
             var cur_name = output_view.output.name;
-            var data_names = data.data_outputs;
+            var data_names = data.outputs;
             var cur_name_in_data_outputs = false;
             _.each(data_names, data_name => {
                 if (data_name.name == cur_name) {
@@ -292,7 +275,7 @@ var Node = Backbone.Model.extend({
                 node.workflow_outputs.splice(i, 1); // removes output from list of workflow outputs
             }
         });
-        $.each(data.data_outputs, (i, output) => {
+        $.each(data.outputs, (i, output) => {
             if (!nodeView.outputViews[output.name]) {
                 nodeView.addDataOutput(output); // add data output if it does not yet exist
             } else {
@@ -318,11 +301,9 @@ var Node = Backbone.Model.extend({
         var old_body = nodeView.$("div.inputs");
         var new_body = nodeView.newInputsDiv();
         var newTerminalViews = {};
-        _.each(data.data_inputs, input => {
-            if (nodeView.node.type != 'parameter_input') {
-                var terminalView = node.nodeView.addDataInput(input, new_body);
-                newTerminalViews[input.name] = terminalView;
-            }
+        _.each(data.inputs, input => {
+            var terminalView = node.nodeView.addDataInput(input, new_body);
+            newTerminalViews[input.name] = terminalView;
         });
         // Cleanup any leftover terminals
         _.each(_.difference(_.values(nodeView.terminalViews), _.values(newTerminalViews)), unusedView => {
@@ -334,16 +315,15 @@ var Node = Backbone.Model.extend({
         // type (not really valid right?) but adding special logic here for
         // data collection input parameters that can have their collection
         // change.
-        if (data.data_outputs.length == 1 && "collection_type" in data.data_outputs[0]) {
-            nodeView.updateDataOutput(data.data_outputs[0]);
+        var data_outputs = data.outputs;
+        if (data_outputs.length == 1 && "collection_type" in data_outputs[0]) {
+            nodeView.updateDataOutput(data_outputs[0]);
         }
         old_body.replaceWith(new_body);
         if ("workflow_outputs" in data) {
             // Won't be present in response for data inputs
             this.workflow_outputs = data.workflow_outputs ? data.workflow_outputs : [];
         }
-        this.add_input_parameters();
-        this.add_output_parameters(data.input_parameters);
         // If active, reactivate with new config_form
         this.markChanged();
         this.redraw();

client/galaxy/scripts/mvc/workflow/workflow-view-node.js
@@ -48,47 +48,12 @@ export default Backbone.View.extend({
             skipResize = false;
         }
         var terminalView = this.terminalViews[input.name];
-        var terminalViewClass =
-            input.input_type == "dataset_collection"
-                ? TerminalViews.InputCollectionTerminalView
-                : TerminalViews.InputTerminalView;
-        if (terminalView && !(terminalView instanceof terminalViewClass)) {
-            terminalView.el.terminal.destroy();
-            terminalView = null;
+        var terminalViewClass = TerminalViews.InputTerminalView;
+        if (input.input_type == "dataset_collection") {
+            terminalViewClass = TerminalViews.InputCollectionTerminalView;
+        } else if (input.input_type == "parameter") {
+            terminalViewClass = TerminalViews.InputParameterTerminalView;
         }
-        if (!terminalView) {
-            terminalView = new terminalViewClass({
-                node: this.node,
-                input: input
-            });
-        } else {
-            var terminal = terminalView.el.terminal;
-            terminal.update(input);
-            terminal.destroyInvalidConnections();
-        }
-        this.terminalViews[input.name] = terminalView;
-        var terminalElement = terminalView.el;
-        var inputView = new DataViews.DataInputView({
-            terminalElement: terminalElement,
-            input: input,
-            nodeView: this,
-            skipResize: skipResize
-        });
-        var ib = inputView.$el;
-        body.append(ib.prepend(terminalView.terminalElements()));
-        return terminalView;
-    },
-
-    addParameterInput: function(input, body) {
-        var skipResize = true;
-        if (!body) {
-            body = this.$(".inputs");
-            // initial addition to node - resize input to help calculate node
-            // width.
-            skipResize = false;
-        }
-        var terminalView = this.terminalViews[input.name];
-        var terminalViewClass = TerminalViews.InputParameterTerminalView;
         if (terminalView && !(terminalView instanceof terminalViewClass)) {
             terminalView.el.terminal.destroy();
             terminalView = null;
@@ -117,35 +82,25 @@ export default Backbone.View.extend({
     },
 
     addDataOutput: function(output) {
-        var terminalViewClass = output.collection
-            ? TerminalViews.OutputCollectionTerminalView
-            : TerminalViews.OutputTerminalView;
+        var terminalViewClass = TerminalViews.OutputTerminalView;
+        var outputViewClass = DataViews.DataOutputView;
+        if (output.collection) {
+            terminalViewClass = TerminalViews.OutputCollectionTerminalView;
+        } else if (output.parameter) {
+            terminalViewClass = TerminalViews.OutputParameterTerminalView;
+            outputViewClass = DataViews.ParameterOutputView;
+        }
         var terminalView = new terminalViewClass({
             node: this.node,
             output: output
         });
-        var outputView = new DataViews.DataOutputView({
+        var outputView = new outputViewClass({
             output: output,
             terminalElement: terminalView.el,
             nodeView: this
         });
         this.outputViews[output.name] = outputView;
         this.tool_body.append(outputView.$el.append(terminalView.terminalElements()));
-    },
-
-    addParameterOutput: function(input_parameter) {
-        var terminalViewClass = TerminalViews.OutputParameterTerminalView;
-        var terminalView = new terminalViewClass({
-            node: this.node,
-            output: input_parameter,
-        });
-        var parameterView = new DataViews.ParameterOutputView({
-            output: input_parameter,
-            terminalElement: terminalView.el,
-            nodeView: this
-        });
-        this.outputViews[input_parameter.name] = parameterView;
-        this.tool_body.append(parameterView.$el.append(terminalView.terminalElements()));
     },
 
     redrawWorkflowOutputs: function() {

client/galaxy/scripts/qunit/tests/workflow_editor_tests.js
@@ -357,16 +357,16 @@ QUnit.module("Node unit test", {
     },
     init_field_data_simple: function(option_overrides) {
         var data = Utils.merge(option_overrides, {
-            data_inputs: [{ name: "input1", extensions: ["data"] }],
-            data_outputs: [{ name: "output1", extensions: ["data"] }],
+            inputs: [{ name: "input1", extensions: ["data"] }],
+            outputs: [{ name: "output1", extensions: ["data"] }],
             label: null
         });
         this.node.init_field_data(data);
     },
     update_field_data_with_new_input: function(option_overrides) {
         var new_data = Utils.merge(option_overrides, {
-            data_inputs: [{ name: "input1", extensions: ["data"] }, { name: "extra_0|input1", extensions: ["data"] }],
-            data_outputs: [{ name: "output1", extensions: ["data"] }],
+            inputs: [{ name: "input1", extensions: ["data"] }, { name: "extra_0|input1", extensions: ["data"] }],
+            outputs: [{ name: "output1", extensions: ["data"] }],
             post_job_actions: "{}",
             label: "New Label"
         });
@@ -401,8 +401,8 @@ QUnit.test("init_field_data properties", function(assert) {
     var node = this.node;
     this.expect_workflow_node_changed(assert, function() {
         var data = {
-            data_inputs: [],
-            data_outputs: [],
+            inputs: [],
+            outputs: [],
             type: "tool",
             name: "cat1",
             config_form: "{}",
@@ -507,8 +507,8 @@ QUnit.test("update_field_data destroys old terminals", function(assert) {
     var node = this.node;
     this.expect_workflow_node_changed(assert, function() {
         var data = {
-            data_inputs: [{ name: "input1", extensions: ["data"] }, { name: "willDisappear", extensions: ["data"] }],
-            data_outputs: [{ name: "output1", extensions: ["data"] }]
+            inputs: [{ name: "input1", extensions: ["data"] }, { name: "willDisappear", extensions: ["data"] }],
+            outputs: [{ name: "output1", extensions: ["data"] }]
         };
         node.init_field_data(data);
         var old_input_terminal = node.input_terminals.willDisappear;

lib/galaxy/managers/workflows.py
@@ -571,9 +571,8 @@ class WorkflowContentsManager(UsesAnnotations):
                 'name': module.get_name(),
                 'tool_state': module.get_state(),
                 'errors': module.get_errors(),
-                'data_inputs': module.get_data_inputs(),
-                'data_outputs': module.get_data_outputs(),
-                'input_parameters': module.get_input_parameters(),
+                'inputs': module.get_all_inputs(connectable_only=True),
+                'outputs': module.get_all_outputs(),
                 'config_form': config_form,
                 'annotation': annotation_str,
                 'post_job_actions': {},
@@ -654,12 +653,12 @@ class WorkflowContentsManager(UsesAnnotations):
         """
         for order_index in sorted(steps):
             step = steps[order_index]
-            for i, step_data_output in enumerate(step['data_outputs']):
+            for i, step_data_output in enumerate(step['outputs']):
                 if step_data_output.get('collection_type_source') and step_data_output['collection_type'] is None:
                     collection_type_source = step_data_output['collection_type_source']
                     for input_connection in step['input_connections'].get(collection_type_source, []):
                         input_step = steps[input_connection['id']]
-                        for input_step_data_output in input_step['data_outputs']:
+                        for input_step_data_output in input_step['outputs']:
                             if input_step_data_output['name'] == input_connection['output_name']:
                                 step_data_output['collection_type'] = input_step_data_output.get('collection_type')
         return steps

lib/galaxy/webapps/galaxy/api/workflows.py
@@ -557,9 +557,8 @@ class WorkflowsAPIController(BaseAPIController, UsesStoredWorkflowMixin, UsesAnn
             'annotation'        : inputs.get('__annotation', ''),
             'name'              : module.get_name(),
             'tool_state'        : module.get_state(),
-            'data_inputs'       : module.get_data_inputs(),
-            'input_parameters'  : module.get_input_parameters(),
-            'data_outputs'      : module.get_data_outputs(),
+            'inputs'            : module.get_all_inputs(connectable_only=True),
+            'outputs'           : module.get_all_outputs(),
             'config_form'       : module.get_config_form(),
             'post_job_actions'  : module.get_post_job_actions(inputs)
         }

lib/galaxy/workflow/run.py
@@ -352,13 +352,10 @@ class WorkflowProgress(object):
         try:
             replacement = step_outputs[output_name]
         except KeyError:
-            if is_data:
-                # Must resolve.
-                template = "Workflow evaluation problem - failed to find output_name %s in step_outputs %s"
-                message = template % (output_name, step_outputs)
-                raise Exception(message)
-            else:
-                replacement = modules.NO_REPLACEMENT
+            # Must resolve.
+            template = "Workflow evaluation problem - failed to find output_name %s in step_outputs %s"
+            message = template % (output_name, step_outputs)
+            raise Exception(message)
         if isinstance(replacement, model.HistoryDatasetCollectionAssociation):
             if not replacement.collection.populated:
                 if not replacement.collection.waiting_for_elements:

test/api/test_workflows.py
@@ -468,8 +468,8 @@ class WorkflowsApiTestCase(BaseWorkflowsApiTestCase):
                 'name',
                 'tool_state',
                 'tooltip',
-                'data_inputs',
-                'data_outputs',
+                'inputs',
+                'outputs',
                 'config_form',
                 'annotation',
                 'post_job_actions',
@@ -494,7 +494,7 @@ steps:
         downloaded_workflow = self._download_workflow(workflow_id, style="editor")
         steps = downloaded_workflow['steps']
         assert len(steps) == 2
-        assert steps['1']['data_outputs'][0]['collection_type'] == 'list:paired'
+        assert steps['1']['outputs'][0]['collection_type'] == 'list:paired'
 
     @skip_without_tool('collection_type_source')
     def test_export_editor_subworkflow_collection_type_source(self):
@@ -525,7 +525,7 @@ steps:
         steps = downloaded_workflow['steps']
         assert len(steps) == 2
         assert steps['1']['type'] == 'subworkflow'
-        assert steps['1']['data_outputs'][0]['collection_type'] == 'list:paired'
+        assert steps['1']['outputs'][0]['collection_type'] == 'list:paired'
 
     def test_import_missing_tool(self):
         workflow = self.workflow_populator.load_workflow_from_resource(name="test_workflow_missing_tool")

commit 1a636c376fe7fbdaa0ead866989ddfbf0ed7e943

Fix HiddenParameter use
Broken in
https://github.com/galaxyproject/galaxy/commit/dd8d74f364770a1272555a3753f5d1e1288315e1
Fixes https://github.com/galaxyproject/galaxy/issues/7454

lib/galaxy/workflow/modules.py
@@ -33,6 +33,7 @@ from galaxy.tools.parameters.basic import (
     ConnectedValue,
     DataCollectionToolParameter,
     DataToolParameter,
+    HiddenToolParameter,
     is_runtime_value,
     parameter_types,
     runtime_to_json,
@@ -889,6 +890,8 @@ class ToolModule(WorkflowModule):
                     skip = not visible or not is_data
                 elif connectable_only:
                     skip = not visible or not (is_data or is_connectable)
+                elif isinstance(input, HiddenToolParameter):
+                    skip = False
                 else:
                     skip = not visible
                 if not skip: