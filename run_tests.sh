d5f3efbf08b07bdc2891c7b1c713f6ba2abe7c6e
mvdbeek <m.vandenbeek@gmail.com>
2019-03-04 11:38:11 +0100

    Use new image from galaxy namespace

--- run_tests.sh
+++ run_tests.sh
@@ -270,7 +270,7 @@ exists() {
     type "$1" >/dev/null 2>/dev/null
 }
 
-DOCKER_DEFAULT_IMAGE='mvdbeek/testing-base:19.05.2'
+DOCKER_DEFAULT_IMAGE='galaxy/testing-base:19.05.0'
 
 test_script="./scripts/functional_tests.py"
 report_file="run_functional_tests.html"
