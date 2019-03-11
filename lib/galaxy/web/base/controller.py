galaxyproject/galaxy

commit e4399021520440a8f58e33dbae161f031f8da9eb

show warning to user if some of the items within were not published

lib/galaxy/web/base/controller.py
@@ -1459,9 +1459,10 @@ class SharableMixin(object):
                     try:
                         trans.app.security_agent.make_dataset_public(hda.dataset)
                     except Exception:
-                        log.warning("Unable to make dataset with id: %s public.".format(dataset.id)
+                        log.warning("Unable to make dataset with id: %s public.").format(dataset.id)
                         skipped = True
                 else:
+                    log.warning("User without permissions tried to make dataset with id: %s public.").format(dataset.id)
                     skipped = True
         return item, skipped
 

client/galaxy/scripts/components/Sharing.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="ready">
         <h2>Share or Publish {{model_class}} `{{item.title}}`</h2>
-        <b-alert variant="danger" :show="err_msg">
+        <b-alert :show="show_danger" variant="danger" dismissible>
             {{ err_msg }}
         </b-alert>
         </br>
@@ -141,7 +141,10 @@ export default {
             return `${Galaxy.root}${this.model_class_lc}/set_slug_async/?id=${this.id}`;
         },
         has_possible_members(){
-            return ["history"].indexOf(this.model_class_lc) > -1;;
+            return ["history"].indexOf(this.model_class_lc) > -1;
+        },
+        show_danger(){
+            return this.err_msg !== null;
         }
     },
     data() {
@@ -159,7 +162,7 @@ export default {
                 users_shared_with: []
             },
             share_fields: ["email", { key: "id", label: "" }],
-            make_members_public: false,
+            make_members_public: false
         };
     },
     created: function() {
@@ -203,6 +206,9 @@ export default {
                 .post(`${Galaxy.root}api/${this.plural_name_lc}/${this.id}/sharing`, data)
                 .then(response => {
                     Object.assign(this.item, response.data);
+                    if (response.data.skipped){
+                       this.err_msg = "Some of the items within this object were not published due to an error.";
+                    }
                 })
                 .catch(error => (this.err_msg = error.response.data.err_msg));
         },

commit 98a189ceff8e0fd329acfbb4fdec585d85317e95

Fix warning message formatting in sharing controller
Reported by @erasche in https://github.com/galaxyproject/galaxy/issues/7503#issuecomment-471496500,
broken in https://github.com/galaxyproject/galaxy/commit/e4399021520440a8f58e33dbae161f031f8da9eb

lib/galaxy/web/base/controller.py
@@ -1459,10 +1459,10 @@ class SharableMixin(object):
                     try:
                         trans.app.security_agent.make_dataset_public(hda.dataset)
                     except Exception:
-                        log.warning("Unable to make dataset with id: %s public.").format(dataset.id)
+                        log.warning("Unable to make dataset with id: %s public", dataset.id)
                         skipped = True
                 else:
-                    log.warning("User without permissions tried to make dataset with id: %s public.").format(dataset.id)
+                    log.warning("User without permissions tried to make dataset with id: %s public", dataset.id)
                     skipped = True
         return item, skipped
 