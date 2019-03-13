9964753b0bbee90f3c0b9440476a336d2a51e885
John Chilton <jmchilton@gmail.com>
2019-03-13 17:31:24 -0400

    Decompose Galaxy into Python libraries in-tree.
    Alternative to galaxy-lib codebase sychronziation.

---
+++ projects/data/tests/test_galaxy_mapping.py
@@ -0,0 +1,1 @@
+../../../test/unit/test_galaxy_mapping.py
---
+++ projects/objectstore/galaxy/project_galaxy_objectstore.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '19.5.0.dev0'
+
+PROJECT_NAME = "galaxy-objectstore"
+PROJECT_OWNER = PROJECT_USERAME = "galaxyproject"
+PROJECT_URL = "https://github.com/galaxyproject/galaxy"
+PROJECT_AUTHOR = 'Galaxy Project and Community'
+PROJECT_EMAIL = 'jmchilton@gmail.com'
+RAW_CONTENT_URL = "https://raw.github.com/%s/%s/master/" % (
+    PROJECT_USERAME, PROJECT_NAME
+)
