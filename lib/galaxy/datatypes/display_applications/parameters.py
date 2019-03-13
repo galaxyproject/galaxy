1af3deda17a44b7401fb5a1972cba5f0a7bac4f0
John Chilton <jmchilton@gmail.com>
2019-03-13 11:23:18 -0400

    Eliminate import dependency of datatypes on galaxy.web (url_for).
    Use app to find url_for at runtime. app objects created outside a web context can stub this out and throw an exception if display applications are used in that context (which wouldn't make sense anyway).
    
    This is a step toward being able to break datatypes out into their own package and toward faster loading of datatype modules.

--- lib/galaxy/datatypes/display_applications/parameters.py
+++ lib/galaxy/datatypes/display_applications/parameters.py
@@ -6,7 +6,6 @@ from six.moves.urllib.parse import quote_plus
 from galaxy.util import string_as_bool
 from galaxy.util.bunch import Bunch
 from galaxy.util.template import fill_template
-from galaxy.web import url_for
 
 DEFAULT_DATASET_NAME = 'dataset'
 
@@ -191,14 +190,14 @@ class DisplayParameterValueWrapper(object):
         if self.parameter.strip_https and base_url[: 5].lower() == 'https':
             base_url = "http%s" % base_url[5:]
         return "%s%s" % (base_url,
-                         url_for(controller='dataset',
-                                 action="display_application",
-                                 dataset_id=self._dataset_hash,
-                                 user_id=self._user_hash,
-                                 app_name=quote_plus(self.parameter.link.display_application.id),
-                                 link_name=quote_plus(self.parameter.link.id),
-                                 app_action=self.action_name,
-                                 action_param=self._url))
+                         self.trans.app.url_for(controller='dataset',
+                                                action="display_application",
+                                                dataset_id=self._dataset_hash,
+                                                user_id=self._user_hash,
+                                                app_name=quote_plus(self.parameter.link.display_application.id),
+                                                link_name=quote_plus(self.parameter.link.id),
+                                                app_action=self.action_name,
+                                                action_param=self._url))
 
     @property
     def action_name(self):
