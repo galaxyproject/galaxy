976f5ad3677fbf4f9850e6bf43d9455a81cd5f42
John Chilton <jmchilton@gmail.com>
2019-03-14 08:53:51 -0400

    Refactor galaxy.web.form_builder into galaxy.util.form_builder.
    We have other utilities for web content in galaxy.util like sanitize_html, so while I'm not eager to put web stuff in there it is not bringing any dependencies into galaxy's util package. The upshot here is that now model doesn't need to have dependencies on galaxy.web (other than implicitly through galaxy.managers.tags - but that really has to be refactored either way).
    
    Contains some more cleanup related to url_for (see #7521 for context).

--- lib/galaxy/model/metadata.py
+++ lib/galaxy/model/metadata.py
@@ -17,12 +17,11 @@ from six import string_types
 from sqlalchemy.orm import object_session
 
 import galaxy.model
-from galaxy.util import (listify, string_as_bool,
+from galaxy.util import (form_builder, listify, string_as_bool,
                          stringify_dictionary_keys)
 from galaxy.util.json import safe_dumps
 from galaxy.util.object_wrapper import sanitize_lists_to_string
 from galaxy.util.odict import odict
-from galaxy.web import form_builder
 
 log = logging.getLogger(__name__)
 
