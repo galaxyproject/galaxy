f579066655c7ce3bd7198446f97e3214a08228b2
John Chilton <jmchilton@gmail.com>
2019-03-19 17:16:35 -0400

    Python 3 fixes for galaxy.tools.expressions.evaluation.

--- lib/galaxy/tools/expressions/evaluation.py
+++ lib/galaxy/tools/expressions/evaluation.py
@@ -27,11 +27,12 @@ def evaluate(config, input):
                           close_fds=True,
                           stdin=subprocess.PIPE,
                           stdout=subprocess.PIPE)
-
-    (stdoutdata, stderrdata) = sp.communicate(json.dumps(new_input) + "\n\n")
+    input_str = json.dumps(new_input) + "\n\n"
+    input_bytes = input_str.encode("utf-8")
+    (stdoutdata, stderrdata) = sp.communicate(input_bytes)
     if sp.returncode != 0:
         args = (json.dumps(new_input, indent=4), stdoutdata, stderrdata)
         message = "Expression engine returned non-zero exit code on evaluation of\n%s%s%s" % args
         raise Exception(message)
 
-    return json.loads(stdoutdata)
+    return json.loads(stdoutdata.decode("utf-8"))
