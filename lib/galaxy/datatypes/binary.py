da7d58bf38c19a24f25ecd8e2f1f1684b76df50a
bensellak <bensellak@ensat.ac.ma>
2019-03-25 15:55:49 +0000

    Update binary.py

--- lib/galaxy/datatypes/binary.py
+++ lib/galaxy/datatypes/binary.py
@@ -22,11 +22,11 @@ from bx.seq.twobit import TWOBIT_MAGIC_NUMBER, TWOBIT_MAGIC_NUMBER_SWAP, TWOBIT_
 
 from galaxy import util
 from galaxy.datatypes import metadata
+from galaxy.datatypes.data import get_file_peek
 from galaxy.datatypes.metadata import DictParameter, ListParameter, MetadataElement, MetadataParameter
 from galaxy.util import nice_size, sqlite
 from galaxy.util.checkers import is_bz2, is_gzip
 from . import data, dataproviders
-from galaxy.datatypes.data import get_file_peek
 
 log = logging.getLogger(__name__)
 
