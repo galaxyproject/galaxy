galaxyproject/galaxy

commit fce2dd3ae801cd13a542388746b84f9cca2a4c48

Eliminate compressed datatype boiler plate.
Just annotate auto_compressed_types="gz|bz2|gz,bz2" in datatypes_conf.xml and get for free:

- The compressed datatype with correct extension, no need to have a class definition.
- Converters from the compressed datatype to the uncompressed datatype.
- A sniffer in the correct order.

Additionally, the hacky way we were controlling whether types would be sniffed or not has been improved. This can now be set in the datatypes_conf.xml file or read from a global Galaxy configuration option for options that haven't been explicitly set on a per-datatype basis in the configuration (sniff_compressed_dynamic_datatypes_default).

This should simplify all future datatypes moving in this direction and introduces the possibility of also just loading them in dynamically as needed based on what the tools in the tool panel annotate for inputs and outputs. See a tool with an input of type "genbank.gz" should be enough to trigger this someday I hope.

lib/galaxy/datatypes/sequence.py
@@ -17,8 +17,7 @@ import bx.align.maf
 from galaxy import util
 from galaxy.datatypes import metadata
 from galaxy.datatypes.binary import (
-    Binary,
-    CompressedArchive
+    Binary
 )
 from galaxy.datatypes.metadata import MetadataElement
 from galaxy.datatypes.sniff import (
@@ -31,7 +30,6 @@ from galaxy.util import (
     nice_size
 )
 from galaxy.util.checkers import (
-    is_bz2,
     is_gzip
 )
 from galaxy.util.image_util import check_image_type
@@ -41,10 +39,6 @@ if sys.version_info > (3,):
     long = int
 
 log = logging.getLogger(__name__)
-
-SNIFF_COMPRESSED_FASTQS = os.environ.get("GALAXY_ENABLE_BETA_COMPRESSED_FASTQ_SNIFFING", "0") == "1"
-SNIFF_COMPRESSED_FASTAS = os.environ.get("GALAXY_ENABLE_BETA_COMPRESSED_FASTA_SNIFFING", "0") == "1"
-SNIFF_COMPRESSED_GENBANKS = os.environ.get("GALAXY_ENABLE_BETA_COMPRESSED_GENBANK_SNIFFING", "0") == "1"
 
 
 @build_sniff_from_prefix
@@ -315,21 +309,6 @@ class Alignment(data.Text):
         if split_params is None:
             return None
         raise NotImplementedError("Can't split generic alignment files")
-
-
-class FastaGz(Sequence, CompressedArchive):
-    """Class representing a generic compressed FASTA sequence"""
-    edam_format = "format_1929"
-    file_ext = "fasta.gz"
-    compressed = True
-
-    def sniff(self, filename):
-        """Determines whether the file is in gzip-compressed FASTA format"""
-        if not SNIFF_COMPRESSED_FASTAS and not self.validate_mode:
-            return False
-        if not is_gzip(filename):
-            return False
-        return Sequence.sniff(self, filename)
 
 
 @build_sniff_from_prefix
@@ -630,7 +609,7 @@ class BaseFastq(Sequence):
             return False
         headers = iter_headers(file_prefix, None, count=1000)
         # If this is a FastqSanger-derived class, then check to see if the base qualities match
-        if isinstance(self, FastqSanger) or isinstance(self, FastqSangerGz) or isinstance(self, FastqSangerBz2):
+        if isinstance(self, FastqSanger):
             if not self.sangerQualities(headers):
                 return False
 
@@ -744,82 +723,6 @@ class FastqIllumina(Fastq):
 class FastqCSSanger(Fastq):
     """Class representing a Color Space FASTQ sequence ( e.g a SOLiD variant )"""
     file_ext = "fastqcssanger"
-
-
-class FastqGz(BaseFastq, CompressedArchive):
-    """Class representing a generic compressed FASTQ sequence"""
-    edam_format = "format_1930"
-    file_ext = "fastq.gz"
-    compressed = True
-
-    def sniff(self, filename):
-        """Determines whether the file is in gzip-compressed FASTQ format"""
-        if not SNIFF_COMPRESSED_FASTQS and not self.validate_mode:
-            return False
-        if not is_gzip(filename):
-            return False
-        return BaseFastq.sniff(self, filename)
-
-
-class FastqSangerGz(FastqGz):
-    """Class representing a compressed FASTQ sequence ( the Sanger variant )"""
-    edam_format = "format_1932"
-    file_ext = "fastqsanger.gz"
-
-
-class FastqSolexaGz(FastqGz):
-    """Class representing a compressed FASTQ sequence ( the Solexa variant )"""
-    edam_format = "format_1933"
-    file_ext = "fastqsolexa.gz"
-
-
-class FastqIlluminaGz(FastqGz):
-    """Class representing a compressed FASTQ sequence ( the Illumina 1.3+ variant )"""
-    edam_format = "format_1931"
-    file_ext = "fastqillumina.gz"
-
-
-class FastqCSSangerGz(FastqGz):
-    """Class representing a Color Space compressed FASTQ sequence ( e.g a SOLiD variant )"""
-    file_ext = "fastqcssanger.gz"
-
-
-class FastqBz2(BaseFastq, CompressedArchive):
-    """Class representing a generic compressed FASTQ sequence"""
-    edam_format = "format_1930"
-    file_ext = "fastq.bz2"
-    compressed = True
-
-    def sniff(self, filename):
-        """Determine whether the file is in bzip2-compressed FASTQ format"""
-        if not SNIFF_COMPRESSED_FASTQS and not self.validate_mode:
-            return False
-        if not is_bz2(filename):
-            return False
-        return BaseFastq.sniff(self, filename)
-
-
-class FastqSangerBz2(FastqBz2):
-    """Class representing a compressed FASTQ sequence ( the Sanger variant )"""
-    edam_format = "format_1932"
-    file_ext = "fastqsanger.bz2"
-
-
-class FastqSolexaBz2(FastqBz2):
-    """Class representing a compressed FASTQ sequence ( the Solexa variant )"""
-    edam_format = "format_1933"
-    file_ext = "fastqsolexa.bz2"
-
-
-class FastqIlluminaBz2(FastqBz2):
-    """Class representing a compressed FASTQ sequence ( the Illumina 1.3+ variant )"""
-    edam_format = "format_1931"
-    file_ext = "fastqillumina.bz2"
-
-
-class FastqCSSangerBz2(FastqBz2):
-    """Class representing a Color Space compressed FASTQ sequence ( e.g a SOLiD variant )"""
-    file_ext = "fastqcssanger.bz2"
 
 
 @build_sniff_from_prefix
@@ -1222,29 +1125,6 @@ class Genbank(data.Text):
         if compressed and not isinstance(self, Binary):
             return False
         return 'LOCUS ' == file_prefix.contents_header[0:6]
-
-
-class GenbankGz(Genbank, CompressedArchive):
-    """Class representing a compressed Genbank sequence"""
-    edam_format = "format_1936"
-    edam_data = "data_0849"
-    file_ext = "genbank.gz"
-    compressed = True
-
-    def sniff(self, filename):
-        """
-        Determines whether the file is in gzip-compressed Genbank format
-
-        >>> from galaxy.datatypes.sniff import get_test_fname
-        >>> fname = get_test_fname( '1.genbank.gz' )
-        >>> GenbankGz().sniff( fname )
-        True
-        """
-        if not SNIFF_COMPRESSED_GENBANKS and not self.validate_mode:
-            return False
-        if not is_gzip(filename):
-            return False
-        return Genbank.sniff(self, filename)
 
 
 @build_sniff_from_prefix

test/functional/tools/sample_datatypes_conf.xml
@@ -11,16 +11,10 @@
         <converter file="fasta_to_tabular_converter.xml" target_datatype="tabular"/>
     </datatype>
     <datatype extension="fastq" type="galaxy.datatypes.sequence:Fastq" display_in_upload="true" />
-    <datatype extension="fastqsanger" type="galaxy.datatypes.sequence:FastqSanger" display_in_upload="true" />
+    <datatype extension="fastqsanger" auto_compressed_types="gz,bz2" type="galaxy.datatypes.sequence:FastqSanger" display_in_upload="true" />
     <datatype extension="fastqsolexa" type="galaxy.datatypes.sequence:FastqSolexa" display_in_upload="true" />
     <datatype extension="fastqcssanger" type="galaxy.datatypes.sequence:FastqCSSanger" display_in_upload="true" />
     <datatype extension="fastqillumina" type="galaxy.datatypes.sequence:FastqIllumina" display_in_upload="true" />
-    <datatype extension="fastqsanger.gz" type="galaxy.datatypes.sequence:FastqSangerGz" display_in_upload="true">
-        <converter file="fastqsangergz_to_fastqsanger.xml" target_datatype="fastqsanger"/>
-    </datatype>
-    <datatype extension="fastqsanger.bz2" type="galaxy.datatypes.sequence:FastqSangerBz2" display_in_upload="true">
-        <converter file="fastqsangerbz2_to_fastqsanger.xml" target_datatype="fastqsanger"/>
-    </datatype>
     <datatype extension="sam" type="galaxy.datatypes.tabular:Sam" display_in_upload="true">
         <converter file="sam_to_unsorted_bam.xml" target_datatype="unsorted.bam"/>
         <converter file="to_qname_sorted_bam.xml" target_datatype="qname_sorted.bam"/>

config/datatypes_conf.xml.sample
@@ -79,7 +79,7 @@
     <datatype extension="data_manager_json" type="galaxy.datatypes.text:Json" mimetype="application/json" subclass="true" display_in_upload="false"/>
     <datatype extension="dbn" type="galaxy.datatypes.sequence:DotBracket" display_in_upload="true" description="Dot-Bracket format is a text-based format for storing both an RNA sequence and its corresponding 2D structure." description_url="https://wiki.galaxyproject.org/Learn/Datatypes#Dbn"/>
     <datatype extension="fai" type="galaxy.datatypes.tabular:Tabular" display_in_upload="true" subclass="true" description="A Fasta Index File is a text file consisting of lines each with five TAB-delimited columns : Name, Length, offset, linebases, Linewidth" description_url="http://www.htslib.org/doc/faidx.html"/>
-    <datatype extension="fasta" type="galaxy.datatypes.sequence:Fasta" display_in_upload="true" description="A sequence in FASTA format consists of a single-line description, followed by lines of sequence data. The first character of the description line is a greater-than ('&gt;') symbol in the first column. All lines should be shorter than 80 characters." description_url="https://wiki.galaxyproject.org/Learn/Datatypes#Fasta">
+    <datatype extension="fasta" auto_compressed_types="gz" type="galaxy.datatypes.sequence:Fasta" display_in_upload="true" description="A sequence in FASTA format consists of a single-line description, followed by lines of sequence data. The first character of the description line is a greater-than ('&gt;') symbol in the first column. All lines should be shorter than 80 characters." description_url="https://wiki.galaxyproject.org/Learn/Datatypes#Fasta">
       <converter file="fasta_to_tabular_converter.xml" target_datatype="tabular"/>
       <converter file="fasta_to_bowtie_base_index_converter.xml" target_datatype="bowtie_base_index"/>
       <converter file="fasta_to_bowtie_color_index_converter.xml" target_datatype="bowtie_color_index"/>
@@ -88,53 +88,20 @@
       <converter file="fasta_to_fai.xml" target_datatype="fai"/>
       <display file="igv/genome_fasta.xml" inherit="true"/>
     </datatype>
-    <datatype extension="fasta.gz" type="galaxy.datatypes.sequence:FastaGz" display_in_upload="true">
-      <converter file="fastagz_to_fasta.xml" target_datatype="fasta"/>
-    </datatype>
     <datatype extension="fastq" type="galaxy.datatypes.sequence:Fastq" display_in_upload="true" description="FASTQ format is a text-based format for storing both a biological sequence (usually nucleotide sequence) and its corresponding quality scores." description_url="https://wiki.galaxyproject.org/Learn/Datatypes#Fastq">
       <converter file="fastq_to_fqtoc.xml" target_datatype="fqtoc"/>
     </datatype>
-    <datatype extension="fastqsanger" type="galaxy.datatypes.sequence:FastqSanger" display_in_upload="true">
+    <datatype extension="fastqsanger" auto_compressed_types="gz,bz2" type="galaxy.datatypes.sequence:FastqSanger" display_in_upload="true">
       <converter file="fastq_to_fqtoc.xml" target_datatype="fqtoc"/>
     </datatype>
-    <datatype extension="fastqsolexa" type="galaxy.datatypes.sequence:FastqSolexa" display_in_upload="true" description="FastqSolexa is the Illumina (Solexa) variant of the Fastq format, which stores sequences and quality scores in a single file." description_url="https://wiki.galaxyproject.org/Learn/Datatypes#FastqSolexa">
+    <datatype extension="fastqsolexa" auto_compressed_types="gz,bz2" type="galaxy.datatypes.sequence:FastqSolexa" display_in_upload="true" description="FastqSolexa is the Illumina (Solexa) variant of the Fastq format, which stores sequences and quality scores in a single file." description_url="https://wiki.galaxyproject.org/Learn/Datatypes#FastqSolexa">
       <converter file="fastq_to_fqtoc.xml" target_datatype="fqtoc"/>
     </datatype>
-    <datatype extension="fastqcssanger" type="galaxy.datatypes.sequence:FastqCSSanger" display_in_upload="true">
+    <datatype extension="fastqcssanger" auto_compressed_types="gz,bz2" type="galaxy.datatypes.sequence:FastqCSSanger" display_in_upload="true">
       <converter file="fastq_to_fqtoc.xml" target_datatype="fqtoc"/>
     </datatype>
-    <datatype extension="fastqillumina" type="galaxy.datatypes.sequence:FastqIllumina" display_in_upload="true">
+    <datatype extension="fastqillumina" auto_compressed_types="gz,bz2" type="galaxy.datatypes.sequence:FastqIllumina" display_in_upload="true">
       <converter file="fastq_to_fqtoc.xml" target_datatype="fqtoc"/>
-    </datatype>
-    <datatype extension="fastq.gz" type="galaxy.datatypes.sequence:FastqGz" display_in_upload="true">
-      <converter file="fastqgz_to_fastq.xml" target_datatype="fastq"/>
-    </datatype>
-    <datatype extension="fastqsanger.gz" type="galaxy.datatypes.sequence:FastqSangerGz" display_in_upload="true">
-      <converter file="fastqsangergz_to_fastqsanger.xml" target_datatype="fastqsanger"/>
-    </datatype>
-    <datatype extension="fastqsolexa.gz" type="galaxy.datatypes.sequence:FastqSolexaGz" display_in_upload="true">
-      <converter file="fastqsolexagz_to_fastqsolexa.xml" target_datatype="fastqsolexa"/>
-    </datatype>
-    <datatype extension="fastqcssanger.gz" type="galaxy.datatypes.sequence:FastqCSSangerGz" display_in_upload="true">
-      <converter file="fastqcssangergz_to_fastqcssanger.xml" target_datatype="fastqcssanger"/>
-    </datatype>
-    <datatype extension="fastqillumina.gz" type="galaxy.datatypes.sequence:FastqIlluminaGz" display_in_upload="true">
-      <converter file="fastqilluminagz_to_fastqillumina.xml" target_datatype="fastqillumina"/>
-    </datatype>
-    <datatype extension="fastq.bz2" type="galaxy.datatypes.sequence:FastqBz2" display_in_upload="true">
-      <converter file="fastqbz2_to_fastq.xml" target_datatype="fastq"/>
-    </datatype>
-    <datatype extension="fastqsanger.bz2" type="galaxy.datatypes.sequence:FastqSangerBz2" display_in_upload="true">
-      <converter file="fastqsangerbz2_to_fastqsanger.xml" target_datatype="fastqsanger"/>
-    </datatype>
-    <datatype extension="fastqsolexa.bz2" type="galaxy.datatypes.sequence:FastqSolexaBz2" display_in_upload="true">
-      <converter file="fastqsolexabz2_to_fastqsolexa.xml" target_datatype="fastqsolexa"/>
-    </datatype>
-    <datatype extension="fastqcssanger.bz2" type="galaxy.datatypes.sequence:FastqCSSangerBz2" display_in_upload="true">
-      <converter file="fastqcssangerbz2_to_fastqcssanger.xml" target_datatype="fastqcssanger"/>
-    </datatype>
-    <datatype extension="fastqillumina.bz2" type="galaxy.datatypes.sequence:FastqIlluminaBz2" display_in_upload="true">
-      <converter file="fastqilluminabz2_to_fastqillumina.xml" target_datatype="fastqillumina"/>
     </datatype>
     <datatype extension="fqtoc" type="galaxy.datatypes.sequence:SequenceSplitLocations" display_in_upload="true"/>
     <datatype extension="eland" type="galaxy.datatypes.tabular:Eland" display_in_upload="true"/>
@@ -487,10 +454,7 @@
     <datatype extension="embl" type="galaxy.datatypes.data:Text" subclass="true"/>
     <datatype extension="fitch" type="galaxy.datatypes.data:Text" subclass="true"/>
     <datatype extension="gcg" type="galaxy.datatypes.data:Text" subclass="true"/>
-    <datatype extension="genbank" type="galaxy.datatypes.sequence:Genbank" display_in_upload="True"/>
-    <datatype extension="genbank.gz" type="galaxy.datatypes.sequence:GenbankGz" display_in_upload="true">
-      <converter file="genbankgz_to_genbank.xml" target_datatype="genbank"/>
-    </datatype>
+    <datatype extension="genbank" auto_compressed_types="gz" sniff_compressed_types="true" type="galaxy.datatypes.sequence:Genbank" display_in_upload="True"/>
     <datatype extension="hennig86" type="galaxy.datatypes.data:Text" subclass="true"/>
     <datatype extension="ig" type="galaxy.datatypes.data:Text" subclass="true"/>
     <datatype extension="jackknifer" type="galaxy.datatypes.data:Text" subclass="true"/>
@@ -781,7 +745,6 @@
     <!-- TODO: see molecules.py <sniffer type="galaxy.datatypes.molecules:SMILES"/>-->
     <sniffer type="galaxy.datatypes.phylip:Phylip"/>
     <sniffer type="galaxy.datatypes.sequence:Fasta"/>
-    <sniffer type="galaxy.datatypes.sequence:FastaGz"/>
     <sniffer type="galaxy.datatypes.sequence:FastqSanger"/>
     <sniffer type="galaxy.datatypes.sequence:FastqSangerGz"/>
     <sniffer type="galaxy.datatypes.sequence:FastqSangerBz2"/>
@@ -793,7 +756,6 @@
     <sniffer type="galaxy.datatypes.images:Pdf"/>
     <sniffer type="galaxy.datatypes.sequence:Axt"/>
     <sniffer type="galaxy.datatypes.sequence:Genbank"/>
-    <sniffer type="galaxy.datatypes.sequence:GenbankGz"/>
     <sniffer type="galaxy.datatypes.interval:Bed"/>
     <sniffer type="galaxy.datatypes.interval:CustomTrack"/>
     <sniffer type="galaxy.datatypes.interval:Gtf"/>

lib/galaxy/config.py
@@ -462,6 +462,7 @@ class Configuration(object):
         # allow_path_paste value.
         self.allow_library_path_paste = string_as_bool(kwargs.get('allow_library_path_paste', self.allow_path_paste))
         self.disable_library_comptypes = kwargs.get('disable_library_comptypes', '').lower().split(',')
+        self.sniff_compressed_dynamic_datatypes_default = string_as_bool(kwargs.get("sniff_compressed_dynamic_datatypes_default", False))
         self.check_upload_content = string_as_bool(kwargs.get('check_upload_content', True))
         self.watch_tools = kwargs.get('watch_tools', 'false')
         self.watch_tool_data_dir = kwargs.get('watch_tool_data_dir', 'false')

lib/galaxy/datatypes/converters/fastagz_to_fasta.xml
@@ -1,11 +1,0 @@
-<tool id="CONVERTER_fastagz_to_fasta" name="Convert fasta.gz files to fasta" version="1.0.0" hidden="true">
-  <command>gzip -dcf '$input1' > '$output1'</command>
-  <inputs>
-    <param format="fasta.gz" name="input1" type="data" label="Choose FASTA file"/>
-  </inputs>
-  <outputs>
-    <data format="fasta" name="output1"/>
-  </outputs>
-  <help>
-  </help>
-</tool>

lib/galaxy/datatypes/converters/fastqbz2_to_fastq.xml
@@ -1,11 +1,0 @@
-<tool id="CONVERTER_fastqbz2_to_fastq" name="Convert fastq.bz2 files to fastq" version="1.0.0" hidden="true">
-  <command>bzip2 -dcf '$input1' > '$output1'</command>
-  <inputs>
-    <param format="fastq.bz2" name="input1" type="data" label="Choose FASTQ file"/>
-  </inputs>
-  <outputs>
-    <data format="fastq" name="output1"/>
-  </outputs>
-  <help>
-  </help>
-</tool>

lib/galaxy/datatypes/converters/fastqcssangerbz2_to_fastqcssanger.xml
@@ -1,11 +1,0 @@
-<tool id="CONVERTER_fastqcssangerbz2_to_fastqcssanger" name="Convert fastqcssanger.bz2 files to fastqcssanger" version="1.0.0" hidden="true">
-  <command>bzip2 -dcf '$input1' > '$output1'</command>
-  <inputs>
-    <param format="fastqcssanger.bz2" name="input1" type="data" label="Choose FASTQ file"/>
-  </inputs>
-  <outputs>
-    <data format="fastqcssanger" name="output1"/>
-  </outputs>
-  <help>
-  </help>
-</tool>

lib/galaxy/datatypes/converters/fastqcssangergz_to_fastqcssanger.xml
@@ -1,11 +1,0 @@
-<tool id="CONVERTER_fastqcssangergz_to_fastqcssanger" name="Convert fastqcssanger.gz files to fastqcssanger" version="1.0.0" hidden="true">
-  <command>gzip -dcf '$input1' > '$output1'</command>
-  <inputs>
-    <param format="fastqcssanger.gz" name="input1" type="data" label="Choose FASTQ file"/>
-  </inputs>
-  <outputs>
-    <data format="fastqcssanger" name="output1"/>
-  </outputs>
-  <help>
-  </help>
-</tool>

commit 966cad890ef82c2365193831fa87e2605542b9e4

Fix fasta.gz uploads
This looks like it may probably broke with https://github.com/galaxyproject/galaxy/commit/fce2dd3ae801cd13a542388746b84f9cca2a4c48

lib/galaxy/datatypes/sequence.py
@@ -98,18 +98,19 @@ class Sequence(data.Text):
         """
         data_lines = 0
         sequences = 0
-        for line in open(dataset.file_name):
-            line = line.strip()
-            if line and line.startswith('#'):
-                # We don't count comment lines for sequence data types
-                continue
-            if line and line.startswith('>'):
-                sequences += 1
-                data_lines += 1
-            else:
-                data_lines += 1
-        dataset.metadata.data_lines = data_lines
-        dataset.metadata.sequences = sequences
+        with compression_utils.get_fileobj(dataset.file_name) as fh:
+            for line in fh:
+                line = line.strip()
+                if line and line.startswith('#'):
+                    # We don't count comment lines for sequence data types
+                    continue
+                if line and line.startswith('>'):
+                    sequences += 1
+                    data_lines += 1
+                else:
+                    data_lines += 1
+            dataset.metadata.data_lines = data_lines
+            dataset.metadata.sequences = sequences
 
     def set_peek(self, dataset, is_multi_byte=False):
         if not dataset.dataset.purged:

test/functional/tools/sample_datatypes_conf.xml
@@ -7,7 +7,7 @@
     <datatype extension="csv" type="galaxy.datatypes.tabular:CSV" display_in_upload="true" />
     <datatype extension="interval" type="galaxy.datatypes.interval:Interval" display_in_upload="true" description="File must start with definition line in the following format (columns may be in any order)." >
     </datatype>
-    <datatype extension="fasta" type="galaxy.datatypes.sequence:Fasta" auto_compressed_types="gz,bz2" display_in_upload="true">
+    <datatype extension="fasta" type="galaxy.datatypes.sequence:Fasta" auto_compressed_types="gz" display_in_upload="true">
         <converter file="fasta_to_tabular_converter.xml" target_datatype="tabular"/>
     </datatype>
     <datatype extension="fastq" type="galaxy.datatypes.sequence:Fastq" display_in_upload="true" />