cbd9c9502264cd6efb733923e184e121458cfe70
Nate Coraor <nate@bx.psu.edu>
2019-04-10 13:44:33 -0400

    Occasionally poll for status updates of non-terminal jobs that have been dispatched to Pulsar

--- lib/galaxy/util/specs.py
+++ lib/galaxy/util/specs.py
@@ -1,3 +1,4 @@
+import datetime
 import functools
 import operator
 
@@ -25,6 +26,14 @@ def to_float_or_none(value):
         return None
     else:
         return float(value)
+
+
+def to_datetime_interval_or_none(value):
+    if value is None:
+        return None
+    else:
+        h, m, s = [int(v) for v in value.split(':')]
+        return datetime.timedelta(seconds=s, minutes=m, hours=h)
 
 
 # Utility methods for specifing valid...
