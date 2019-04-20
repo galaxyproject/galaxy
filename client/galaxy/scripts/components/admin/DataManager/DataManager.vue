18823797447eb38ccd5237f283faa14f99f715f2
Dannon Baker <dannon.baker@gmail.com>
2019-04-19 20:03:29 -0400

    use standard card header styles without extra markup (which gets escaped anyway)

--- client/galaxy/scripts/components/admin/DataManager/DataManager.vue
+++ client/galaxy/scripts/components/admin/DataManager/DataManager.vue
@@ -37,7 +37,7 @@
                 </b-row>
             </b-container>
             <b-card-group columns>
-                <b-card no-body header="<h4>Data Managers</h4>" id="data-managers-card">
+                <b-card no-body header="Data Managers" id="data-managers-card">
                     <b-list-group flush>
                         <b-list-group-item v-for="(dataManager, index) in dataManagersFiltered" :key="index">
                             <b-button-group vertical>
@@ -65,7 +65,7 @@
                         </b-list-group-item>
                     </b-list-group>
                 </b-card>
-                <b-card no-body header="<h4>Tool Data Tables</h4>">
+                <b-card no-body header="Tool Data Tables">
                     <b-list-group flush>
                         <b-list-group-item
                             v-for="(dataTable, index) in dataTablesFiltered"
