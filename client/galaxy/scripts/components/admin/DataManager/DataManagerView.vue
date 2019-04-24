a551150f2fa8f7d92f65a53130ac26d6dc6fbd3c
Dannon Baker <dannon.baker@gmail.com>
2019-04-24 15:32:01 -0400

    Thanks, prettier.

--- client/galaxy/scripts/components/admin/DataManager/DataManagerView.vue
+++ client/galaxy/scripts/components/admin/DataManager/DataManagerView.vue
@@ -3,9 +3,11 @@
         <span class="mb-3">
             <h2 id="data-managers-title">Local Data</h2>
             <p>
-                Data Managers are used to manage Galaxy's local data.  They can be installed manually, or via the
-                <b-link href="/admin_toolshed/browse_tool_sheds" target="galaxy_main">ToolShed</b-link>.
-                For more comprehensive information <b-link href="https://galaxyproject.org/admin/tools/data-managers/" target="_blank">See the Wiki</b-link>.
+                Data Managers are used to manage Galaxy's local data. They can be installed manually, or via the
+                <b-link href="/admin_toolshed/browse_tool_sheds" target="galaxy_main">ToolShed</b-link>. For more
+                comprehensive information
+                <b-link href="https://galaxyproject.org/admin/tools/data-managers/" target="_blank">See the Wiki</b-link
+                >.
             </p>
         </span>
         <router-view />
