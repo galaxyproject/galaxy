645e1110b56d5fe24bd7b8510534a18dc1153163
guerler <aysam.guerler@gmail.com>
2019-05-13 13:52:58 -0400

    Continue listening to history changes in tool form

--- client/galaxy/scripts/mvc/tool/tool-form-base.js
+++ client/galaxy/scripts/mvc/tool/tool-form-base.js
@@ -79,11 +79,6 @@ export default FormBase.extend({
                 self.deferred.reset();
                 self.deferred.execute(process => {
                     self.model.get("postchange")(process, self);
-                    if (self.model.get("listen_to_history")) {
-                        process.then(() => {
-                            self.stopListening(Galaxy.currHistoryPanel.collection);
-                        });
-                    }
                 });
             }
         });
