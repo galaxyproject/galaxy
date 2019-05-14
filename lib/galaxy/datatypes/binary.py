3cecb466c65b230f5e0ad6c83e2a73482ce695a0
Alexander OSTROVSKY <alexanderostrovsky@Caroline.local>
2019-05-14 16:50:51 -0400

    tensorflow checkpoint file support

--- lib/galaxy/datatypes/binary.py
+++ lib/galaxy/datatypes/binary.py
@@ -2407,6 +2407,13 @@ class WiffTar(BafTar):
         return "Sciex WIFF/SCAN archive"
 
 
+class Tensorflow(Binary):
+    """
+    Checkpoint files for tensorflow models
+    """
+    file_ext = "ckpt"
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod(sys.modules[__name__])
