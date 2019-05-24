8efaf565a04cd05a382664454c3bf964fdaf16ac
Dannon Baker <dannon.baker@gmail.com>
2019-05-24 07:57:24 -0400

    Use default function params, dot notation for attribute access.  Fix module comment.

--- client/galaxy/scripts/components/JobMetrics/mount.js
+++ client/galaxy/scripts/components/JobMetrics/mount.js
@@ -5,16 +5,15 @@ import $ from "jquery";
 import Vue from "vue";
 import JobMetrics from "./JobMetrics.vue";
 
-export const mountJobMetrics = propsData => {
+export const mountJobMetrics = (propsData = {}) => {
     $(".job-metrics").each((index, el) => {
         const jobId = $(el).attr("job_id");
         const datasetId = $(el).attr("dataset_id");
         const datasetType = $(el).attr("dataset_type") || "hda";
         const component = Vue.extend(JobMetrics);
-        propsData = propsData || {};
-        propsData["jobId"] = jobId;
-        propsData["datasetId"] = datasetId;
-        propsData["datasetType"] = datasetType;
+        propsData.jobId = jobId;
+        propsData.datasetId = datasetId;
+        propsData.datasetType = datasetType;
         return new component({ propsData: propsData }).$mount(el);
     });
 };
