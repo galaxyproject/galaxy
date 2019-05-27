d53c09669677722f9af3cff0d0d7b9a5dd44b26b
John Chilton <jmchilton@gmail.com>
2019-05-27 16:07:08 -0400

    JobMetrics to use a Vuex Store, cache API requests using vuex-cache.

--- client/galaxy/scripts/store/index.js
+++ client/galaxy/scripts/store/index.js
@@ -4,14 +4,19 @@
 
 import Vue from "vue";
 import Vuex from "vuex";
+import createCache from "vuex-cache";
+
 import { gridSearchStore } from "./gridSearchStore";
 import { tagStore } from "./tagStore";
+import { jobMetricsStore } from "./jobMetricsStore";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+    plugins: [createCache()],
     modules: {
         gridSearch: gridSearchStore,
-        tags: tagStore
+        tags: tagStore,
+        jobMetrics: jobMetricsStore
     }
 });
