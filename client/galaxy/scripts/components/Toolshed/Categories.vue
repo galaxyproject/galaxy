a3cc8936efb88e9932b54f1099ea34ed32421b53
guerler <aysam.guerler@gmail.com>
2019-05-29 01:03:09 -0400

    Add spinner to categories view, add repository count, prep switching toolshed servers

--- client/galaxy/scripts/components/Toolshed/Categories.vue
+++ client/galaxy/scripts/components/Toolshed/Categories.vue
@@ -1,11 +1,19 @@
 <template>
-    <b-table striped :items="categories" :fields="fields">
-        <template slot="name" slot-scope="data">
-            <b-link href="#" class="font-weight-bold" @click="onCategory(data.value)">
-                {{ data.value }}
-            </b-link>
-        </template>
-    </b-table>
+    <div>
+        <div v-if="loading">
+            <span class="fa fa-spinner fa-spin mb-4" /> <span>Loading categories...</span>
+        </div>
+        <div v-else>
+            <div class="m-1 text-muted">{{ total }} repositories available at <b-link :href="toolshedUrl" target="_blank">{{ toolshedUrl }}</b-link>.</div>
+            <b-table striped :items="categories" :fields="fields">
+                <template slot="name" slot-scope="data">
+                    <b-link href="#" class="font-weight-bold" @click="onCategory(data.value)">
+                        {{ data.value }}
+                    </b-link>
+                </template>
+            </b-table>
+        </div>
+    </div>
 </template>
 <script>
 import Vue from "vue";
@@ -16,6 +24,8 @@ export default {
     data() {
         return {
             categories: [],
+            total: 0,
+            loading: true,
             fields: {
                 name: {
                     sortable: true
@@ -35,6 +45,8 @@ export default {
             .getCategories(this.toolshedUrl)
             .then(categories => {
                 this.categories = categories;
+                this.total = this.categories.reduce( (value, entry) => value + entry.repositories, 0);
+                this.loading = false;
             })
             .catch(errorMessage => {
                 alert(errorMessage);
