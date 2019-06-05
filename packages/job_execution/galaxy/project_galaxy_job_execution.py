f9baa0b4839c70e14d5e66f988214c3b05abebdd
John Chilton <jmchilton@gmail.com>
2019-06-05 17:47:59 -0400

    Stub out job_exection package for remote job finish code.

---
+++ packages/job_execution/galaxy/project_galaxy_job_execution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '19.9.0.dev0'
+
+PROJECT_NAME = "galaxy-job-execution"
+PROJECT_OWNER = PROJECT_USERAME = "galaxyproject"
+PROJECT_URL = "https://github.com/galaxyproject/galaxy"
+PROJECT_AUTHOR = 'Galaxy Project and Community'
+PROJECT_DESCRIPTION = 'Galaxy Job Execution Runtime Utilities'
+PROJECT_EMAIL = 'jmchilton@gmail.com'
+RAW_CONTENT_URL = "https://raw.github.com/%s/%s/master/" % (
+    PROJECT_USERAME, PROJECT_NAME
+)
--- packages/test.sh
+++ packages/test.sh
@@ -20,10 +20,11 @@ PACKAGE_DIRS=(
     containers
     tool_util
     data
+    job_execution
 )
 # containers has no tests, tool_util not yet working 100%,
 # data has many problems quota, tool shed install database, etc..
-RUN_TESTS=(1 1 1 0 0 0 0)
+RUN_TESTS=(1 1 1 0 0 0 0 0)
 
 for ((i=0; i<${#PACKAGE_DIRS[@]}; i++)); do
     package_dir=${PACKAGE_DIRS[$i]}
