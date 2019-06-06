b0e5eac7ca662b119e24d03f08fcfafef08da2bc
guerler <aysam.guerler@gmail.com>
2019-06-06 17:33:48 -0400

    Use async functions instead of returning promises

--- client/galaxy/scripts/components/Toolshed/services.js
+++ client/galaxy/scripts/components/Toolshed/services.js
@@ -3,115 +3,91 @@ import { getAppRoot } from "onload/loadConfig";
 
 /** Request repositories, categories etc from toolshed server **/
 export class Services {
-    getCategories(toolshedUrl) {
+    async getCategories(toolshedUrl) {
         const paramString = `tool_shed_url=${toolshedUrl}&controller=categories`;
         const url = `${getAppRoot()}api/tool_shed/request?${paramString}`;
-        return new Promise((resolve, reject) => {
-            axios
-                .get(url)
-                .then(response => {
-                    resolve(response.data);
-                })
-                .catch(e => {
-                    reject(this._errorMessage(e));
-                });
-        });
+        try {
+            let response = await axios.get(url);
+            return response.data;
+        } catch (e) {
+            return this._errorMessage(e);
+        }
     }
-    getRepositories(params) {
+    async getRepositories(params) {
         params["controller"] = "repositories";
         const paramString = this._getParamString(params);
         const url = `${getAppRoot()}api/tool_shed/request?${paramString}`;
-        return new Promise((resolve, reject) => {
-            axios
-                .get(url)
-                .then(response => {
-                    const data = response.data;
-                    const incoming = data.hits.map(x => x.repository);
-                    incoming.forEach(x => {
-                        x.times_downloaded = this._formatCount(x.times_downloaded);
-                        x.repository_url = `${data.hostname}repository?repository_id=${x.id}`;
-                    });
-                    resolve(incoming);
-                })
-                .catch(e => {
-                    reject(this._errorMessage(e));
-                });
-        });
+        try {
+            let response = await axios.get(url);
+            const data = response.data;
+            const incoming = data.hits.map(x => x.repository);
+            incoming.forEach(x => {
+                x.times_downloaded = this._formatCount(x.times_downloaded);
+                x.repository_url = `${data.hostname}repository?repository_id=${x.id}`;
+            });
+            return incoming;
+        } catch (e) {
+            return this._errorMessage(e);
+        }
     }
-    getDetails(toolshedUrl, repository_id) {
+    async getDetails(toolshedUrl, repository_id) {
         const paramString = `tool_shed_url=${toolshedUrl}&id=${repository_id}&controller=repositories&action=metadata`;
         const url = `${getAppRoot()}api/tool_shed/request?${paramString}`;
-        return new Promise((resolve, reject) => {
-            axios
-                .get(url)
-                .then(response => {
-                    const data = response.data;
-                    const table = Object.keys(data).map(key => data[key]);
-                    table.sort((a, b) => b.numeric_revision - a.numeric_revision);
-                    table.forEach(x => {
-                        if (Array.isArray(x.tools)) {
-                            x.profile = x.tools.reduce((value, current) =>
-                                current.profile > value ? current.profile : value, null);
-                        }
-                    });
-                    resolve(table);
-                })
-                .catch(e => {
-                    reject(`${this._errorMessage(e)}, ${url}`);
-                });
-        });
+        try {
+            const response = await axios.get(url);
+            const data = response.data;
+            const table = Object.keys(data).map(key => data[key]);
+            table.sort((a, b) => b.numeric_revision - a.numeric_revision);
+            table.forEach(x => {
+                if (Array.isArray(x.tools)) {
+                    x.profile = x.tools.reduce((value, current) =>
+                        current.profile > value ? current.profile : value, null);
+                }
+            });
+            return table;
+        } catch (e) {
+            return `${this._errorMessage(e)}, ${url}`;
+        }
     }
-    getInstalledRepositories(repo) {
+    async getInstalledRepositories(repo) {
         const paramsString = `name=${repo.name}&owner=${repo.repo_owner_username}`;
         const url = `${getAppRoot()}api/tool_shed_repositories?${paramsString}`;
-        return new Promise((resolve, reject) => {
-            axios
-                .get(url)
-                .then(response => {
-                    const data = response.data;
-                    const result = {};
-                    data.forEach(x => {
-                        const d = {
-                            status: x.status,
-                            installed: !x.deleted && !x.uninstalled
-                        };
-                        result[x.changeset_revision] = result[x.installed_changeset_revision] = d;
-                    });
-                    resolve(result);
-                })
-                .catch(e => {
-                    reject(this._errorMessage(e));
-                });
-        });
+        try {
+            const response = await axios.get(url);
+            const data = response.data;
+            const result = {};
+            data.forEach(x => {
+                const d = {
+                    status: x.status,
+                    installed: !x.deleted && !x.uninstalled
+                };
+                result[x.changeset_revision] = result[x.installed_changeset_revision] = d;
+            });
+            return result;
+        } catch (e) {
+            this._errorMessage(e);
+        }
     }
-    installRepository(payload) {
+    async installRepository(payload) {
         const url = `${getAppRoot()}api/tool_shed_repositories`;
-        return new Promise((resolve, reject) => {
-            axios
-                .post(url, payload)
-                .then(response => {
-                    resolve(response.data);
-                })
-                .catch(e => {
-                    reject(this._errorMessage(e));
-                });
-        });
+        try {
+            const response = await axios.post(url, payload);
+            return response.data;
+        } catch(e) {
+            return this._errorMessage(e);
+        }
     }
-    uninstallRepository(params) {
+    async uninstallRepository(params) {
         const paramsString = Object.keys(params).reduce(function(previous, key) {
             return `${previous}${key}=${params[key]}&`;
         }, "");
         const url = `${getAppRoot()}api/tool_shed_repositories?${paramsString}`;
-        return new Promise((resolve, reject) => {
-            axios
-                .delete(url)
-                .then(response => {
-                    resolve(response.data);
-                })
-                .catch(e => {
-                    reject(this._errorMessage(e));
-                });
-        });
+        try {
+            const response = await axios.delete(url);
+            return response.data;
+        } catch(e) {
+            return this._errorMessage(e);
+        }
     }
     _formatCount(value) {
         if (value > 1000) return `>${Math.floor(value / 1000)}k`;
