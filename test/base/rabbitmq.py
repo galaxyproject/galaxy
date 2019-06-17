47f2637c9b2c0e217a89d353849dd654fbfcadd2
vahid <jalili.vahid@gmail.com>
2019-06-17 14:31:23 -0700

    Wrap k8s API calls & del & retry calling api for conflicting resources.

--- test/base/rabbitmq.py
+++ test/base/rabbitmq.py
@@ -10,6 +10,12 @@ RABBITMQ_NAME = "rabbitmq"
 RABBITMQ_MT_NAME = "rabbitmq-management"
 APP_TAG = "rabbitmq"
 
+# In case the service exists (conflict), this property sets for how
+# many times it will attempt to create the service:
+# deletes for MAX_RETRIES -1 times and creates for MAX_RETRIES times.
+MAX_RETRIES = 3
+
+
 class RabbitMQK8s(object):
     def __init__(self):
         # Configs can be set in Configuration class directly or using helper
@@ -19,21 +25,19 @@ class RabbitMQK8s(object):
         self.api_instance = client.CoreV1Api()
         self.namespace = "default"
 
-    def __create_service(self, namespace, manifest):
+    def __call_api(self, method, namespace, manifest, retries=MAX_RETRIES):
         try:
-            api_response = self.api_instance.create_namespaced_service(namespace, manifest)
+            return method(namespace, manifest)
         except ApiException as e:
-            print("Exception when calling CoreV1Api->create_namespaced_endpoints: %s\n" % e)
-        return api_response
-
-    def __create_statefulset(self, namespace, manifest):
-        try:
-            # create an instance of the API class
-            api_instance = client.AppsV1beta1Api()
-            api_response = api_instance.create_namespaced_stateful_set(namespace, manifest)
-        except ApiException as e:
-            print("Exception when calling AppsV1beta1Api->create_namespaced_stateful_set: %s\n" % e)
-        return api_response
+            if (e.reason == "Conflict" or e.status == 409) and retries > 1:
+                # Do not try recalling API if it fails deleting the service.
+                try:
+                    self.delete_service(manifest["metadata"]["name"])
+                except ApiException:
+                    raise ApiException
+                else:
+                    return self.__call_api(method, namespace, manifest, retries=retries-1)
+            print("Exception when calling the method {0}; error: {1}\n".format(method, e))
 
     def create_rabbitmq(self):
         manifest = {
@@ -66,7 +70,8 @@ class RabbitMQK8s(object):
                 }
             }
         }
-        return self.__create_service(self.namespace, manifest)
+        api_response = self.__call_api(self.api_instance.create_namespaced_service, self.namespace, manifest)
+        return api_response
 
     def create_rabbitmq_management(self):
         manifest = {
@@ -91,13 +96,16 @@ class RabbitMQK8s(object):
                 "type": "NodePort"
             }
         }
-        return self.__create_service(self.namespace, manifest)
+        api_response = self.__call_api(self.api_instance.create_namespaced_service, self.namespace, manifest)
+        return api_response
 
-    def create_rabbitmq_statefulset(self):
+    def create_rabbitmq_stateful_set(self):
         manifest = {
             # TODO
         }
-        return self.__create_statefulset(self.namespace, manifest)
+        api_instance = client.AppsV1beta1Api()
+        api_response = self.__call_api(api_instance.create_namespaced_stateful_set, self.namespace, manifest)
+        return api_response
 
     def create_deployment(self):
         # TODO: authorization and cookies.
@@ -108,10 +116,23 @@ class RabbitMQK8s(object):
         # configuration.api_key_prefix['authorization'] = 'Bearer'
         self.create_rabbitmq()
         self.create_rabbitmq_management()
-        self.create_rabbitmq_statefulset()
+        self.create_rabbitmq_stateful_set()
 
     def update_deployment(self, api_instance, deployment):
         pass
 
-    def delete_deployment(self, api_instance):
+    def delete_deployment(self, name):
         pass
+
+    def delete_service(self, name):
+        try:
+            api_response = self.api_instance.delete_namespaced_service(
+                name=name,
+                namespace=self.namespace,
+                grace_period_seconds=0,
+                async_req=False
+            )
+            return api_response
+        except ApiException as e:
+            print("Exception when calling CoreV1Api->delete_namespaced_service: %s\n" % e)
+            raise
