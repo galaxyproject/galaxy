3e38b6e6962cde487afe1c47e1f8dc5570311c2c
mvdbeek <m.vandenbeek@gmail.com>
2019-06-26 09:59:49 +0200

    Fix deleting jobs
    Discussed this with @slugger90 and @erasche on gitter.
    The traceback was:
    ```
    galaxy.jobs.handler ERROR 2019-06-26 13:09:12,754 [p:32491,w:0,m:2] [JobHandlerStopQueue.monitor_thread] Exception in monitor_step
    Traceback (most recent call last):
      File "lib/galaxy/jobs/handler.py", line 866, in monitor
        self.monitor_step()
      File "lib/galaxy/jobs/handler.py", line 886, in monitor_step
        jobs_to_check.append((job, job.stderr))
      File "lib/galaxy/model/__init__.py", line 337, in get_stderr
        stderr += "\n" + self.job_stderr
    TypeError: unsupported operand type(s) for +=: 'NoneType' and 'unicode'
    ```

--- lib/galaxy/model/__init__.py
+++ lib/galaxy/model/__init__.py
@@ -331,17 +331,16 @@ class JobLike(object):
     def set_stdout(self, stdout):
         raise NotImplementedError("Attempt to set stdout, must set tool_stdout or job_stdout")
 
-    def get_stderr(self):
-        stderr = self.tool_stderr
+    @property
+    def stderr(self):
+        stderr = self.tool_stderr or ''
         if self.job_stderr:
             stderr += "\n" + self.job_stderr
         return stderr
 
-    def set_stderr(self, stderr):
+    @stderr.setter
+    def stderr(self, stderr):
         raise NotImplementedError("Attempt to set stdout, must set tool_stderr or job_stderr")
-
-    stdout = property(get_stdout, set_stdout)
-    stderr = property(get_stderr, set_stderr)
 
 
 class User(Dictifiable, RepresentById):
