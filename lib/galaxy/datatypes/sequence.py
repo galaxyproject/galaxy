80b9be3cf51d132f1156a80083251187fd641c46
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2019-07-11 21:14:05 +0200

    Clean up code

--- lib/galaxy/datatypes/sequence.py
+++ lib/galaxy/datatypes/sequence.py
@@ -822,7 +822,7 @@ class FastqSanger(Fastq):
     def quality_check(lines):
         """Presuming lines are lines from a fastq file, return True if the qualities are compatible with sanger encoding"""
         for line in islice(lines, 3, None, 4):
-            if not all(_ >= '!' and _ <= 'S' for _ in line[0]) or ' ' in line:
+            if not all(_ >= '!' and _ <= 'S' for _ in line[0]):
                 return False
         return True
 
