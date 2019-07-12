galaxyproject/galaxy

commit 891daf4f82eb45c692f3fc5542cea35f94e10118

When running under uWSGI, sys.executable is the path to uwsgi. Attempt to locate python.

lib/galaxy/util/__init__.py
@@ -39,6 +39,11 @@ except ImportError:
     docutils_core = None
     docutils_html4css1 = None
 
+try:
+    import uwsgi
+except ImportError:
+    uwsgi = None
+
 from .inflection import English, Inflector
 inflector = Inflector(English)
 
@@ -1463,6 +1468,29 @@ def safe_relpath(path):
     if path.startswith(os.sep) or normpath(path).startswith(os.pardir):
         return False
     return True
+
+
+def _executable():
+    exe = sys.executable
+    if exe.endswith('uwsgi'):
+        virtualenv = None
+        if uwsgi is not None:
+            for name in ('home', 'virtualenv', 'venv', 'pyhome'):
+                if name in uwsgi.opt:
+                    virtualenv = uwsgi.opt[name]
+                    break
+        if virtualenv is None and 'VIRTUAL_ENV' in os.environ:
+            virtualenv = os.environ['VIRTUAL_ENV']
+        if virtualenv is not None:
+            exe = os.path.join(virtualenv, 'bin', 'python')
+        else:
+            exe = os.path.join(os.path.dirname(exe), 'python')
+            if not os.path.exists(exe):
+                exe = 'python'
+    return exe
+
+
+executable = _executable()
 
 
 class ExecutionTimer(object):

lib/tool_shed/galaxy_install/migrate/check.py
@@ -6,6 +6,7 @@ import sys
 from migrate.versioning import repository, schema
 from sqlalchemy import create_engine, MetaData, Table
 
+from galaxy.util import executable
 from galaxy.util.odict import odict
 from tool_shed.util import common_util
 
@@ -60,7 +61,7 @@ def verify_tools( app, url, galaxy_config_file=None, engine_options={} ):
                     config_arg = " -c %s" % galaxy_config_file
                 # FIXME: this doesn't seem right.
                 manage_tools = os.path.abspath( os.path.join( os.path.dirname( __file__ ), 'scripts', 'manage_tools.py' ) )
-                cmd = '%s %s%s upgrade' % (sys.executable, manage_tools, config_arg)
+                cmd = '%s %s%s upgrade' % (executable, manage_tools, config_arg)
                 proc = subprocess.Popen( args=cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
                 return_code = proc.wait()
                 output = proc.stdout.read( 32768 )

commit 9574de30f6380b694b285daafa104ee8cebf553a

Fix startup on python 3 if run through uwsgi
uwsgi options are returned as bytestring, so this avoids:
```
Traceback (most recent call last):
  File "lib/galaxy/webapps/galaxy/buildapp.py", line 12, in <module>
    import galaxy.app
  File "lib/galaxy/app.py", line 8, in <module>
    import galaxy.model
  File "lib/galaxy/model/__init__.py", line 45, in <module>
    import galaxy.exceptions
  File "lib/galaxy/exceptions/__init__.py", line 19, in <module>
    from ..exceptions import error_codes
  File "lib/galaxy/exceptions/error_codes.py", line 9, in <module>
    from galaxy.util import unicodify
  File "lib/galaxy/util/__init__.py", line 1708, in <module>
    executable = _executable()
  File "lib/galaxy/util/__init__.py", line 1700, in _executable
    exe = os.path.join(virtualenv, 'bin', 'python')
  File "/Users/mvandenb/src/galaxy/.venv3/lib/python3.6/posixpath.py", line 94, in join
    genericpath._check_arg_types('join', a, *p)
  File "/Users/mvandenb/src/galaxy/.venv3/lib/python3.6/genericpath.py", line 151, in _check_arg_types
    raise TypeError("Can't mix strings and bytes in path components") from None
TypeError: Can't mix strings and bytes in path components
```

Broken in https://github.com/galaxyproject/galaxy/commit/891daf4f82eb45c692f3fc5542cea35f94e10118

lib/galaxy/util/__init__.py
@@ -1692,7 +1692,7 @@ def _executable():
         if uwsgi is not None:
             for name in ('home', 'virtualenv', 'venv', 'pyhome'):
                 if name in uwsgi.opt:
-                    virtualenv = uwsgi.opt[name]
+                    virtualenv = unicodify(uwsgi.opt[name])
                     break
         if virtualenv is None and 'VIRTUAL_ENV' in os.environ:
             virtualenv = os.environ['VIRTUAL_ENV']