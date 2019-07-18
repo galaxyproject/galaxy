efb7b131b1b40f566fa6fcf3e61c150e5f8e879d
Martin Cech <marten@bx.psu.edu>
2019-07-18 12:07:18 -0400

    include the sample in package and distr

--- packages/data/setup.py
+++ packages/data/setup.py
@@ -54,6 +54,7 @@ PACKAGE_DATA = {
     # Be sure to update MANIFEST.in for source dist.
     'galaxy': [
         'datatypes/test/*',
+        'config/datatypes_conf.xml.sample'
     ],
 }
 PACKAGE_DIR = {
