2c20eeba3fbd9509e741fa40e0ba18e55a1cf44e
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2019-07-21 00:09:17 +0200

    Add unit test for get_fileobj_raw

--- test/unit/test_compression_util.py
+++ test/unit/test_compression_util.py
@@ -2,19 +2,32 @@ import shutil
 import tempfile
 import unittest
 
-from galaxy.util.compression_utils import CompressedFile
+from galaxy.util.compression_utils import CompressedFile, get_fileobj_raw
 
 
 class CompressionUtilTestCase(unittest.TestCase):
 
     def test_compression_safety(self):
-        self.assert_safety("test-data/unsafe.tar", False)
         self.assert_safety("test-data/unsafe_relative_symlink.tar", False)
         self.assert_safety("test-data/unsafe.zip", False)
         self.assert_safety("test-data/4.bed.zip", True)
         self.assert_safety("test-data/testdir.tar", True)
         self.assert_safety("test-data/safetar_with_symlink.tar", True)
         self.assert_safety("test-data/safe_relative_symlink.tar", True)
+
+    def test_get_fileobj_raw(self):
+        self.assert_format_detected("test-data/4.bed.zip", "zip")
+        self.assert_format_detected(
+            "test-data/4.bed.zip", None, ["bz2", "gzip"]
+        )
+        self.assert_format_detected("test-data/4.bed.gz", "gzip")
+        self.assert_format_detected(
+            "test-data/4.bed.gz", None, ["bz2", "zip"]
+        )
+        self.assert_format_detected("test-data/4.bed.bz2", "bz2")
+        self.assert_format_detected(
+            "test-data/4.bed.bz2", None, ["gzip", "zip"]
+        )
 
     def assert_safety(self, path, expected_to_be_safe):
         temp_dir = tempfile.mkdtemp()
@@ -26,3 +39,13 @@ class CompressionUtilTestCase(unittest.TestCase):
                     CompressedFile(path).extract(temp_dir)
         finally:
             shutil.rmtree(temp_dir, ignore_errors=True)
+
+    def assert_format_detected(self, path, expected_fmt, allowed_fmts=None):
+        fmt1 = get_fileobj_raw(path, "r", allowed_fmts)[0]
+        fmt2 = get_fileobj_raw(path, "rb", allowed_fmts)[0]
+        fmt3 = get_fileobj_raw(path, "rt", allowed_fmts)[0]
+        fmt4 = get_fileobj_raw(path, "U", allowed_fmts)[0]
+        self.assertEqual(fmt1, fmt2)
+        self.assertEqual(fmt1, fmt3)
+        self.assertEqual(fmt1, fmt4)
+        self.assertEqual(fmt1, expected_fmt)
