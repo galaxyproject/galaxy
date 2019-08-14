393132a965ad01d420bfcdf9fb14e0c838541c2c
Ata Roudgar <aroudgar@sham.its.sfu.ca>
2019-08-14 11:52:38 -0700

    User gid instead of uid when creating files

--- lib/galaxy/util/__init__.py
+++ lib/galaxy/util/__init__.py
@@ -1291,32 +1291,6 @@ def umask_fix_perms(path, umask, unmasked_perms, gid=None):
     except OSError:
         log.exception('Unable to set permissions or group on %s', path)
         return
-    # fix modes
-    if stat.S_IMODE(st.st_mode) != perms:
-        try:
-            os.chmod(path, perms)
-        except Exception as e:
-            log.warning('Unable to honor umask (%s) for %s, tried to set: %s but mode remains %s, error was: %s' % (oct(umask),
-                                                                                                                    path,
-                                                                                                                    oct(perms),
-                                                                                                                    oct(stat.S_IMODE(st.st_mode)),
-                                                                                                                    unicodify(e)))
-    # fix group
-    if gid is not None and st.st_gid != gid:
-        try:
-            os.chown(path, -1, gid)
-        except Exception as e:
-            try:
-                desired_group = grp.getgrgid(gid)
-                current_group = grp.getgrgid(st.st_gid)
-            except Exception:
-                desired_group = gid
-                current_group = st.st_gid
-            log.warning('Unable to honor primary group (%s) for %s, group remains %s, error was: %s' % (desired_group,
-                                                                                                        path,
-                                                                                                        current_group,
-                                                                                                        unicodify(e)))
-
 
 def docstring_trim(docstring):
     """Trimming python doc strings. Taken from: http://www.python.org/dev/peps/pep-0257/"""
