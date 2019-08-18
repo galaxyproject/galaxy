galaxyproject/galaxy

commit d34142e4203880393431d471a43b87e95980579e

Skip migrations on entirely clean new databases
This should be a very nice speedup for ansible tutorials,
tests, planemo etc.

lib/galaxy/model/migrate/check.py
@@ -11,6 +11,8 @@ from sqlalchemy import (
 from sqlalchemy.exc import NoSuchTableError
 from sqlalchemy_utils import create_database, database_exists
 
+from galaxy.model import mapping
+
 log = logging.getLogger(__name__)
 
 # path relative to galaxy
@@ -60,7 +62,16 @@ def create_or_verify_database(url, galaxy_config_file, engine_options={}, app=No
         migrate_to_current_version(engine, db_schema)
 
     meta = MetaData(bind=engine)
-    if new_database or (app and getattr(app.config, 'database_auto_migrate', False)):
+    if new_database and app:
+        log.info("Creating new database from scratch, skipping migrations")
+        current_version = migrate_repository.version().version
+        mapping.init(file_path=app.config.file_path, url=url, create_tables=True)
+        schema.ControlledSchema.create(engine, migrate_repository, version=current_version)
+        db_schema = schema.ControlledSchema(engine, migrate_repository)
+        assert db_schema.version == current_version
+        migrate()
+        return
+    elif app and getattr(app.config, 'database_auto_migrate', False):
         migrate()
         return
 

lib/galaxy/model/tool_shed_install/migrate/check.py
@@ -13,6 +13,9 @@ from sqlalchemy import (
 )
 from sqlalchemy.exc import NoSuchTableError
 
+from galaxy.model.tool_shed_install import mapping
+
+
 log = logging.getLogger(__name__)
 
 # path relative to galaxy
@@ -48,6 +51,12 @@ def create_or_verify_database(url, engine_options={}, app=None):
         # No table means a completely uninitialized database.  If we
         # have an app, we'll set its new_installation setting to True
         # so the tool migration process will be skipped.
+        log.info("Creating install database from scratch, skipping migrations")
+        mapping.init(url=url, create_tables=True)
+        current_version = migrate_repository.version().version
+        schema.ControlledSchema.create(engine, migrate_repository, version=current_version)
+        db_schema = schema.ControlledSchema(engine, migrate_repository)
+        assert db_schema.version == current_version
         migrate()
         return
 

lib/galaxy/config/__init__.py
@@ -26,6 +26,8 @@ from six.moves import configparser
 
 from galaxy.containers import parse_containers_config
 from galaxy.exceptions import ConfigurationError
+from galaxy.model import mapping
+from galaxy.model.tool_shed_install.migrate.check import create_or_verify_database as tsi_create_or_verify_database
 from galaxy.tool_util.deps.container_resolvers.mulled import DEFAULT_CHANNELS
 from galaxy.util import (
     ExecutionTimer,
@@ -1014,7 +1016,6 @@ def get_database_url(config):
 
 def init_models_from_config(config, map_install_models=False, object_store=None, trace_logger=None):
     db_url = get_database_url(config)
-    from galaxy.model import mapping
     model = mapping.init(
         config.file_path,
         db_url,
@@ -1199,6 +1200,7 @@ class ConfiguresGalaxyMixin(object):
         # database file under the hood.
         combined_install_database = not(install_db_url and install_db_url != db_url)
         install_db_url = install_db_url or db_url
+        install_database_options = self.config.database_engine_options if combined_install_database else self.config.install_database_engine_options
 
         if self.config.database_wait:
             self._wait_for_database(db_url)
@@ -1212,17 +1214,11 @@ class ConfiguresGalaxyMixin(object):
             # is a new installation, we'll restrict the tool migration messaging.
             from galaxy.model.migrate.check import create_or_verify_database
             create_or_verify_database(db_url, config_file, self.config.database_engine_options, app=self)
-            if not combined_install_database:
-                from galaxy.model.tool_shed_install.migrate.check import create_or_verify_database as tsi_create_or_verify_database
-                tsi_create_or_verify_database(install_db_url, self.config.install_database_engine_options, app=self)
+            tsi_create_or_verify_database(install_db_url, install_database_options, app=self)
 
         if check_migrate_tools:
             # Alert the Galaxy admin to tools that have been moved from the distribution to the tool shed.
             from tool_shed.galaxy_install.migrate.check import verify_tools
-            if combined_install_database:
-                install_database_options = self.config.database_engine_options
-            else:
-                install_database_options = self.config.install_database_engine_options
             verify_tools(self, install_db_url, config_file, install_database_options)
 
         self.model = init_models_from_config(
@@ -1238,9 +1234,8 @@ class ConfiguresGalaxyMixin(object):
             from galaxy.model.tool_shed_install import mapping as install_mapping
             install_db_url = self.config.install_database_connection
             log.info("Install database using its own connection %s" % install_db_url)
-            install_db_engine_options = self.config.install_database_engine_options
             self.install_model = install_mapping.init(install_db_url,
-                                                      install_db_engine_options)
+                                                      install_database_options)
 
     def _configure_signal_handlers(self, handlers):
         for sig, handler in handlers.items():

commit 6de82ed5311d2c656f0f9f86ca52fee2b427db2a

Fix create_db.sh
Broken in commit d34142e4203880393431d471a43b87e95980579e .

Traceback:

```
$ rm -f database/universe.sqlite && sqlite3 database/universe.sqlite 'VACUUM;' && ./create_db.sh
Activating virtualenv at .venv
Traceback (most recent call last):
  File "./scripts/create_db.py", line 44, in <module>
    invoke_create()
  File "./scripts/create_db.py", line 36, in invoke_create
    create_db(config['db_url'], config['config_file'])
  File "/opt/galaxy/lib/galaxy/model/migrate/check.py", line 74, in create_or_verify_database
    elif getattr(app.config, 'database_auto_migrate', False):
AttributeError: 'NoneType' object has no attribute 'config'
```

lib/galaxy/model/migrate/check.py
@@ -62,16 +62,16 @@ def create_or_verify_database(url, galaxy_config_file, engine_options={}, app=No
         migrate_to_current_version(engine, db_schema)
 
     meta = MetaData(bind=engine)
-    if new_database and app:
+    if new_database:
         log.info("Creating new database from scratch, skipping migrations")
         current_version = migrate_repository.version().version
-        mapping.init(file_path=app.config.file_path, url=url, map_install_models=map_install_models, create_tables=True)
+        mapping.init(file_path='/tmp', url=url, map_install_models=map_install_models, create_tables=True)
         schema.ControlledSchema.create(engine, migrate_repository, version=current_version)
         db_schema = schema.ControlledSchema(engine, migrate_repository)
         assert db_schema.version == current_version
         migrate()
         return
-    elif getattr(app.config, 'database_auto_migrate', False):
+    elif app and getattr(app.config, 'database_auto_migrate', False):
         migrate()
         return
 