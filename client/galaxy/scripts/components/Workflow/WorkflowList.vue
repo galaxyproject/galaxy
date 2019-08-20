fd47ce8d15038559d75a23caa3cef8e253f7bf70
guerler <aysam.guerler@gmail.com>
2019-08-20 14:49:54 -0400

    Add create and import buttons for workflow vue component

--- client/galaxy/scripts/components/Workflow/WorkflowList.vue
+++ client/galaxy/scripts/components/Workflow/WorkflowList.vue
@@ -1,35 +1,59 @@
 <template>
-    <div class="ui-thumbnails">
+    <div>
         <div v-if="error" class="alert alert-danger">{{ error }}</div>
         <div v-else>
-            <div class="search-input">
-                <input
-                    class="search-query parent-width"
-                    name="query"
-                    placeholder="search workflows"
-                    autocomplete="off"
-                    type="text"
-                    v-model="search"
-                />
+            <span v-if="loading">
+                <span class="fa fa-spinner fa-spin" />
+                Loading workflows...
+            </span>
+            <div v-else>
+                <b-row class="mb-3">
+                    <b-col cols="8">
+                        <b-input
+                            class="m-1"
+                            name="query"
+                            placeholder="search workflows"
+                            autocomplete="off"
+                            type="text"
+                            v-model="filter"
+                        />
+                    </b-col>
+                    <b-col>
+                        <span class="float-right">
+                            <b-button class="m-1" @click="createWorkflow">
+                                <span class="fa fa-plus"/>
+                                Create
+                            </b-button>
+                            <b-button class="m-1" @click="importWorkflow">
+                                <span class="fa fa-upload"/>
+                                Import
+                            </b-button>
+                        </span>
+                    </b-col>
+                </b-row>
+                <b-table striped :fields="fields" :items="workflows" :filter="filter" @filtered="filtered">
+                    <template slot="name" slot-scope="row">
+                        <workflowdropdown :workflow="row.item"/>
+                    </template>
+                    <template slot="execute" slot-scope="row">
+                        <b-button
+                          class="btn-sm btn-primary fa fa-play"
+                          @click.stop="executeWorkflow(row.item)"
+                        />
+                    </template>
+                </b-table>
+                <div v-if="showNotFound">
+                    No matching entries found for: <span class="font-weight-bold">{{ this.filter }}</span>.
+                </div>
+                <div v-if="showNotAvailable">
+                    No workflows found. You may create or import new workflows.
+                </div>
             </div>
-            <b-table striped :fields="fields" :items="workflows">
-                <template slot="name" slot-scope="row">
-                    <workflowdropdown :workflow="row.item"/>
-                </template>
-                <template slot="execute" slot-scope="row">
-                    <b-button
-                      class="btn-sm btn-primary fa fa-play"
-                      @click.stop="clickExecute(row.item)"
-                    />
-                </template>
-            </b-table>
         </div>
     </div>
 </template>
 <script>
-import $ from "jquery";
 import { getAppRoot } from "onload/loadConfig";
-import { getGalaxyInstance } from "app";
 import axios from "axios";
 import WorkflowDropdown from "./WorkflowDropdown.vue";
 
@@ -42,7 +66,9 @@ export default {
             classChecked: "fa fa-check text-success",
             classUnchecked: "fa fa-times text-danger",
             workflows: [],
-            search: "",
+            loading: true,
+            nWorkflows: 0,
+            filter: "",
             error: null,
             fields: {
                 name: {
@@ -60,13 +86,23 @@ export default {
             }
         };
     },
+    computed: {
+        showNotFound() {
+            return this.nWorkflows === 0 && this.filter;
+        },
+        showNotAvailable() {
+            return this.nWorkflows === 0 && !this.filter;
+        }
+    },
     created() {
-        const Galaxy = getGalaxyInstance();
         let url = `${getAppRoot()}api/workflows`;
+        this.loading = true;
+        this.filter = "";
         axios
             .get(url)
             .then(response => {
                 this.workflows = response.data;
+                this.nWorkflows = this.workflows.length;
                 for (workflow of this.workflows) {
                     workflow.create_time = workflow.create_time.substring(0, 10);
                     if (workflow.annotations && workflow.annotations.length > 0) {
@@ -76,25 +112,24 @@ export default {
                         workflow.annotations = "Not available.";
                     }
                 }
+                this.loading = false;
             })
             .catch(e => {
                 this.error = this._errorMessage(e);
             });
     },
     methods: {
-        select: function(workflow) {
+        filtered: function(items) {
+            this.nWorkflows = items.length;
         },
-        create: function(workflow) {
+        createWorkflow: function(workflow) {
+            window.location = `${getAppRoot()}workflows/create`;
         },
-        clickExecute: function(workflow) {
+        importWorkflow: function(workflow) {
+            window.location = `${getAppRoot()}workflows/import`;
+        },
+        executeWorkflow: function(workflow) {
             window.location = `${getAppRoot()}workflows/run?id=${workflow.id}`;
-        },
-        match: function(workflow) {
-            return (
-                !this.search ||
-                workflow.name.indexOf(this.search) != -1 ||
-                (workflow.description && workflow.description.indexOf(this.search) != -1)
-            );
         },
         _errorMessage: function(e) {
             const message = e && e.response && e.response.data && e.response.data.err_msg;
