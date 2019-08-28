51265d734b9ad128278ec7d790eaf49de8bcf98f
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2019-08-28 17:48:20 +0200

    return as a comma-separated str to be consistent

--- client/galaxy/scripts/mvc/library/library-folderrow-view.js
+++ client/galaxy/scripts/mvc/library/library-folderrow-view.js
@@ -79,6 +79,7 @@ var FolderRowView = Backbone.View.extend({
     _mountNametags(context) {
         const container = this.$el.find(".nametags")[0];
         if (container) {
+            this.model.set({ tags: this.model.get("tags").split(', ') });
             const { id, model_class, tags } = this.model.attributes;
             const storeKey = `${model_class}-${id}`;
             mountNametags({ storeKey, tags }, container);
