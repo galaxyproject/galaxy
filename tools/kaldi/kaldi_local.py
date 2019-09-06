7fb4063fe903da986191cfca2becaab21965d8a9
Brian Wheeler <bdwheele@indiana.edu>
2019-09-06 10:18:05 -0400

    Add local kaldi/singularity  tool

---
+++ tools/kaldi/kaldi_local.py
@@ -0,0 +1,37 @@
+#!/bin/env python3
+
+import os.path
+import shutil
+import subprocess
+import sys
+import tempfile
+
+# The run_kaldi.sh script is assumed to be in a directory
+# called kaldi-pua-singularity, which is a peer to the
+# galaxy install.  It can either be a check out of that
+# repo, or just the script and the appropriate .sif file.
+# by default the cwd is somewhere near: 
+#    galaxy/database/jobs_directory/000/4/working
+LOCAL_KALDI = "../../../../../../kaldi-pua-singularity/run_kaldi.sh"
+MODE = "cpu"
+
+def main():
+    (input_file, json_file, text_file) = sys.argv[1:4]
+    print(os.getcwd())
+    # copy the input file to a temporary directory
+    with tempfile.TemporaryDirectory() as tmpdir:
+        shutil.copy(input_file, f"{tmpdir}/xxx.wav")
+        subprocess.run([LOCAL_KALDI, MODE, tmpdir], check=True)
+        # assuming the local kaldi ran, it's time to 
+        # find our output files and copy them 
+        print(os.listdir(tmpdir))
+        print(os.listdir(f"{tmpdir}/transcripts/txt"))
+        
+        shutil.copy(f"{tmpdir}/transcripts/txt/xxx_16kHz.txt", text_file)
+        shutil.copy(f"{tmpdir}/transcripts/json/xxx_16kHz.json", json_file)
+        print(os.listdir(os.path.dirname(json_file)))
+        print(os.listdir(os.path.dirname(text_file)))
+    exit(0)
+
+if __name__ == "__main__":
+    main()
