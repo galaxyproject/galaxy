f2a5f0a4ae749af457f1ea8ef11d3018258f73a6
Dannon Baker <dannon.baker@gmail.com>
2019-09-17 14:42:01 -0400

    No reason for this to be wrapped up in a class.  Export individual, unrelated utility functions.

--- client/galaxy/scripts/layout/data.js
+++ client/galaxy/scripts/layout/data.js
@@ -1,9 +1,9 @@
 import $ from "jquery";
-import DataDialog from "components/DataDialog/DataDialog.vue";
+import axios from "axios";
 import Vue from "vue";
+import DataDialog from "components/DataDialog/DataDialog.vue";
 import { getGalaxyInstance } from "app";
 import { getAppRoot } from "onload/loadConfig";
-import axios from "axios";
 
 // This should be moved more centrally (though still hanging off Galaxy for
 // external use?), and populated from the store; just using this as a temporary
@@ -27,64 +27,62 @@ function getCurrentGalaxyHistory() {
     }
 }
 
-export default class Data {
-    /**
-     * Opens a modal dialog for data selection
-     * @param {function} callback - Result function called with selection
-     */
-    dialog(callback, options = {}) {
-        const galaxy = getGalaxyInstance();
-        const host = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
-        Object.assign(options, {
-            callback: callback,
-            history: getCurrentGalaxyHistory(),
-            root: galaxy.root,
-            host: host
-        });
-        const instance = Vue.extend(DataDialog);
-        const vm = document.createElement("div");
-        $("body").append(vm);
-        new instance({
-            propsData: options
-        }).$mount(vm);
-    }
+/**
+ * Opens a modal dialog for data selection
+ * @param {function} callback - Result function called with selection
+ */
+export function dialog(callback, options = {}) {
+    const galaxy = getGalaxyInstance();
+    const host = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+    Object.assign(options, {
+        callback: callback,
+        history: getCurrentGalaxyHistory(),
+        root: galaxy.root,
+        host: host
+    });
+    const instance = Vue.extend(DataDialog);
+    const vm = document.createElement("div");
+    $("body").append(vm);
+    new instance({
+        propsData: options
+    }).$mount(vm);
+}
 
-    /**
-     * Creates a history dataset by submitting an upload request
-     */
-    create(options) {
-        const Galaxy = getGalaxyInstance();
-        const history_panel = Galaxy.currHistoryPanel;
-        let history_id = options.history_id;
-        if (!history_id) {
-            history_id = getCurrentGalaxyHistory();
-        }
-        $.uploadpost({
-            url: `${getAppRoot()}api/tools`,
-            success: response => {
-                if (history_panel) {
-                    history_panel.refreshContents();
-                }
-                if (options.success) {
-                    options.success(response);
-                }
-            },
-            error: options.error,
-            data: {
-                payload: {
-                    tool_id: "upload1",
-                    history_id: history_id,
-                    inputs: JSON.stringify({
-                        "files_0|type": "upload_dataset",
-                        "files_0|NAME": options.file_name,
-                        "files_0|space_to_tab": options.space_to_tab ? "Yes" : null,
-                        "files_0|to_posix_lines": options.to_posix_lines ? "Yes" : null,
-                        "files_0|dbkey": options.genome || "?",
-                        "files_0|file_type": options.extension || "auto",
-                        "files_0|url_paste": options.url_paste
-                    })
-                }
-            }
-        });
+/**
+ * Creates a history dataset by submitting an upload request
+ */
+export function create(options) {
+    const Galaxy = getGalaxyInstance();
+    const history_panel = Galaxy.currHistoryPanel;
+    let history_id = options.history_id;
+    if (!history_id) {
+        history_id = getCurrentGalaxyHistory();
     }
+    $.uploadpost({
+        url: `${getAppRoot()}api/tools`,
+        success: response => {
+            if (history_panel) {
+                history_panel.refreshContents();
+            }
+            if (options.success) {
+                options.success(response);
+            }
+        },
+        error: options.error,
+        data: {
+            payload: {
+                tool_id: "upload1",
+                history_id: history_id,
+                inputs: JSON.stringify({
+                    "files_0|type": "upload_dataset",
+                    "files_0|NAME": options.file_name,
+                    "files_0|space_to_tab": options.space_to_tab ? "Yes" : null,
+                    "files_0|to_posix_lines": options.to_posix_lines ? "Yes" : null,
+                    "files_0|dbkey": options.genome || "?",
+                    "files_0|file_type": options.extension || "auto",
+                    "files_0|url_paste": options.url_paste
+                })
+            }
+        }
+    });
 }
