7d6844bba4b25e3458887b3bc2ff67421a767961
guerler <aysam.guerler@gmail.com>
2019-09-18 01:43:38 -0400

    Add test cases for repository tools sub component in toolshed component

--- client/galaxy/scripts/components/Toolshed/RepositoryDetails/InstallationButton.test.js
+++ client/galaxy/scripts/components/Toolshed/RepositoryDetails/InstallationButton.test.js
@@ -3,17 +3,13 @@ import InstallationButton from "./InstallationButton";
 import Vue from "vue";
 
 describe("InstallationButton", () => {
-    let wrapper;
-    let emitted;
-
     it("test installed repository revision", () => {
-        wrapper = mount(InstallationButton, {
+        const wrapper = mount(InstallationButton, {
             propsData: {
                 installed: true,
                 status: "Installed"
             }
         });
-        emitted = wrapper.emitted();
         const $el = wrapper.find("button");
         expect($el.classes()).to.include("btn-danger");
         $el.trigger("click");
@@ -21,13 +17,12 @@ describe("InstallationButton", () => {
     });
 
     it("test uninstalled repository revision", () => {
-        wrapper = mount(InstallationButton, {
+        const wrapper = mount(InstallationButton, {
             propsData: {
                 installed: false,
                 status: "Uninstalled"
             }
         });
-        emitted = wrapper.emitted();
         const $el = wrapper.find("button");
         expect($el.classes()).to.include("btn-primary");
         $el.trigger("click");
