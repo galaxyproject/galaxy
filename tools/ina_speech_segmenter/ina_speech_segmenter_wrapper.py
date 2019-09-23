01356fa441ecd0b0f492c900ab3a4faa9c880154
Dan Fischer <31411749+dfische3@users.noreply.github.com>
2019-09-23 13:48:04 -0500

    Amp 290 Adds Ina Speech Segmenter (#9)
    * Adding Ina Speech Segmenter and initial python wrapper.
    
    * Cleaning up output and files
    
    * More cleanup
    
    * Updating description
    
    * Style changes to address CircleCI linting.
    
    * Tabs to spaces for consistency

---
+++ tools/ina_speech_segmenter/ina_speech_segmenter_wrapper.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+from inaSpeechSegmenter import Segmenter
+from segmentation_schema import SegmentationSchema
+
+def main():
+    (input_file, json_file) = sys.argv[1:3]
+    
+    # Run ina_speech_segmenter on input file
+    # the result is a list of tuples
+    # each tuple contains:
+    # * label in 'Male', 'Female', 'Music', 'NOACTIVITY'
+    # * start time of the segment
+    # * end time of the segment
+    seg = Segmenter()
+    segmentation = seg(input_file)
+
+    # Convert the resulting list of tuples to an object for serialization
+    seg_schema = convert_to_segmentation_schema(input_file, segmentation)
+
+    # Serialize the json and write it to destination file
+    write_output_json(seg_schema, json_file)
+    exit(0)
+
+def convert_to_segmentation_schema(filename, segmentation):
+    # Create a segmentation object to serialize
+    seg_schema = SegmentationSchema(filename)
+
+    # For each segment returned by the ina_speech_segmenter, add 
+    # a corresponding segment formatted to json spec
+    for segment in segmentation:
+        label = get_label(segment[0])
+        gender = get_gender(segment[0])
+        start = segment[1]
+        end = segment[2]
+        seg_schema.addSegment(label, gender, start, end)
+
+    return seg_schema
+
+# Recode label values to {speech, music, silence}
+def get_label(value):
+    if value == "Male" or value == "Female":
+        return "speech"
+    elif value == "Music":
+        return "music"
+    elif value == "NOACTIVITY":
+        return "silence"
+
+    return value
+
+# Recode gender to {male, female, ""}
+def get_gender(value):
+    if value == "Male" or value == "Female":
+        return value.lower()
+
+    return ""
+
+# Serialize schema obj and write it to output file
+def write_output_json(seg_schema, json_file):
+    # Serialize the segmentation object
+    with open(json_file, 'w') as outfile:
+        json.dump(seg_schema, outfile, default=lambda x: x.__dict__)
+
+if __name__ == "__main__":
+    main()
---
+++ tools/ina_speech_segmenter/segmentation_schema.py
@@ -0,0 +1,26 @@
+class SegmentationSchema:
+	def addSegment(self, label, gender, start, end):
+		self.segments.append(SegmentationSchemaSegment(label, gender, start, end))
+		return
+
+	def __init__(self, filename):
+		self.media = SegmentationSchemaMedia(filename)
+		self.segments = []
+
+
+class SegmentationSchemaMedia:
+	filename = ""
+	def __init__(self, filename):
+		self.filename = filename
+
+class SegmentationSchemaSegment:
+	label = ""
+	start = 0
+	end = 0
+	def __init__(self, label, gender, start, end):
+		self.label = label
+		if len(gender) > 0:
+			self.gender = gender
+		self.start = start
+		self.end = end
+        
