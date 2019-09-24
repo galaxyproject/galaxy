aae9ce1665e0aa4db138c42f23d0fc7f340dbb68
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2019-09-24 16:14:00 -0400

    Do not check len_file_path for None
    See discussion in code review
    
    Co-Authored-By: Nicola Soranzo <nicola.soranzo@gmail.com>

--- lib/galaxy/config/__init__.py
+++ lib/galaxy/config/__init__.py
@@ -267,7 +267,7 @@ class GalaxyAppConfiguration(BaseAppConfiguration):
         if not running_from_source and kwargs.get("tool_data_path", None) is None:
             self.tool_data_path = os.path.join(self.data_dir, "tool-data")
         self.builds_file_path = os.path.join(self.tool_data_path, self.builds_file_path)
-        if self.len_file_path is None:
+        self.len_file_path = os.path.join(self.tool_data_path, self.len_file_path)
             self.len_file_path = os.path.join(self.root, self.tool_data_path, 'shared', 'ucsc', 'chrom')
         # Galaxy OIDC settings.
         self.oidc_config = kwargs.get("oidc_config_file", self.oidc_config_file)
