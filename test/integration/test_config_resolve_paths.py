c58358d1d010f6eeb5e74c4abed4b849ec69ec09
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2019-09-30 11:20:13 -0400

    Refactor config path resolution integration test

--- test/integration/test_config_resolve_paths.py
+++ test/integration/test_config_resolve_paths.py
@@ -4,21 +4,28 @@ from base import integration_util
 
 
 class ConfigResolvePathsTestCase(integration_util.IntegrationTestCase):
+    """
+    Test automatic path resolution of configuration properties that use the
+    'path_resolves_to' attribute.
+    """
+    def setUp(self):
+        super(ConfigResolvePathsTestCase, self).setUp()
+        self.data_dir = self._app.config.data_dir
+
+    def get_default(self, key):
+        return self._app.config.appschema[key]['default']
 
     def test_resolve_openid_consumer_cache_path(self):
-        data_dir = self._app.config.data_dir
-        schema_default = self._app.config.appschema['openid_consumer_cache_path']['default']
-        expected = os.path.join(data_dir, schema_default)
-        assert expected == self._app.config.openid_consumer_cache_path
+        schema_default = self.get_default('openid_consumer_cache_path')
+        expect = os.path.join(self.data_dir, schema_default)
+        assert expect == self._app.config.openid_consumer_cache_path
 
     def test_resolve_citation_cache_data_dir(self):
-        data_dir = self._app.config.data_dir
-        schema_default = self._app.config.appschema['citation_cache_data_dir']['default']
-        expected = os.path.join(data_dir, schema_default)
-        assert expected == self._app.config.citation_cache_data_dir
+        schema_default = self.get_default('citation_cache_data_dir')
+        expect = os.path.join(self.data_dir, schema_default)
+        assert expect == self._app.config.citation_cache_data_dir
 
     def test_resolve_citation_cache_lock_dir(self):
-        data_dir = self._app.config.data_dir
-        schema_default = self._app.config.appschema['citation_cache_lock_dir']['default']
-        expected = os.path.join(data_dir, schema_default)
-        assert expected == self._app.config.citation_cache_lock_dir
+        schema_default = self.get_default('citation_cache_lock_dir')
+        expect = os.path.join(self.data_dir, schema_default)
+        assert expect == self._app.config.citation_cache_lock_dir
