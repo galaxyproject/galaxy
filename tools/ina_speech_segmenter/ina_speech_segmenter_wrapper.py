3e9ce39b8bc0cf9715e2c94a635301c030a3604e
dan <dan@portalmedia.com>
2019-10-10 14:03:31 -0500

    More singularity bug fixes

--- tools/ina_speech_segmenter/ina_speech_segmenter_wrapper.py
+++ tools/ina_speech_segmenter/ina_speech_segmenter_wrapper.py
@@ -11,20 +11,20 @@ import tempfile
 def main():
 	(input_file, json_file) = sys.argv[1:3]
 	
-	with tempfile.TemporaryDirectory() as tmpdir:
-		temp_input_file = f"{tmpdir}/xxx.wav"
-		temp_output_file = f"{tmpdir}/xxx.json"
-		shutil.copy(input_file, temp_input_file)
+	tmpdir = "/tmp"
+	temp_input_file = f"{tmpdir}/xxx.wav"
+	temp_output_file = f"{tmpdir}/xxx.json"
+	shutil.copy(input_file, temp_input_file)
 
-		r = subprocess.run(["singularity", "run", "/srv/amp/ina-speech-tools-singularity/ina-speech-tools-singularity.sif", temp_input_file, temp_output_file])
-        
-		shutil.copy(temp_output_file, json_file)
+	r = subprocess.run(["singularity", "run", "/srv/amp/ina-speech-tools-singularity/ina-speech-tools-singularity.sif", temp_input_file, temp_output_file])
+	
+	shutil.copy(temp_output_file, json_file)
 
-		if os.path.exists(temp_input_file):
-			os.remove(temp_input_file)
+	if os.path.exists(temp_input_file):
+		os.remove(temp_input_file)
 
-		if os.path.exists(temp_output_file):
-			os.remove(temp_output_file)
+	if os.path.exists(temp_output_file):
+		os.remove(temp_output_file)
 		
 	exit(r.returncode)
 		
