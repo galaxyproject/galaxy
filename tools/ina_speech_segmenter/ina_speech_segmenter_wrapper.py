da2ff434f2676b2fe07a4bcdad0846a726c725b3
dan <dan@portalmedia.com>
2019-10-10 13:40:10 -0500

    Moving files to temporary directory

--- tools/ina_speech_segmenter/ina_speech_segmenter_wrapper.py
+++ tools/ina_speech_segmenter/ina_speech_segmenter_wrapper.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 
 import os
+import os.path
+import shutil
 import subprocess
 import sys
+import tempfile
 
 
 def main():
 	(input_file, json_file) = sys.argv[1:3]
 	
-	r = subprocess.run(["singularity", "run", "/srv/amp/ina-speech-tools-singularity/ina-speech-tools-singularity.sif", input_file, json_file])
-	
+	with tempfile.TemporaryDirectory() as tmpdir:
+		temp_input_file = f"{tmpdir}/xxx.wav"
+		temp_output_file = f"{tmpdir}/xxx.json"
+		shutil.copy(input_file, temp_input_file)
+
+		r = subprocess.run(["singularity run", "/srv/amp/ina-speech-tools-singularity/ina-speech-tools-singularity.sif", temp_input_file, temp_output_file])
+        
+		shutil.copy(temp_output_file, json_file)
+
+		if os.path.exists(temp_input_file):
+			os.remove(temp_input_file)
+
+		if os.path.exists(temp_output_file):
+			os.remove(temp_output_file)
+		
 	exit(r.returncode)
 		
 if __name__ == "__main__":
