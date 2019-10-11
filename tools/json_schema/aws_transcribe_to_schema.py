e8e643b02ab0e60370d007041a658386916bc921
Dan Fischer <31411749+dfische3@users.noreply.github.com>
2019-10-11 10:14:55 -0500

    Amp 403 convert stt to json (#14)
    * Adding remove sielence and speech options.  Schema cleanup
    
    * Renaming tool to reflect both speech and music possibilities.
    
    * Minor algorithm changes.  Code cleanup.
    
    * Adding speech to text schema
    
    * Createing schema converters for transcribe and kaldi
    
    * Minor cleanup
    
    * Changing output type to .wav

---
+++ tools/json_schema/aws_transcribe_to_schema.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+
+from speech_to_text_schema import SpeechToText, SpeechToTextMedia, SpeechToTextResult, SpeechToTextScore, SpeechToTextWord
+
+def main():
+
+	(media_file, transcribe_file, output_json_file) = sys.argv[1:4]
+	
+	# Open the transcribe output
+	with open(transcribe_file) as json_file:
+		data = json.load(json_file)
+		
+	result = SpeechToTextResult()
+
+	# Parse transcript
+	transcripts = data["results"]["transcripts"]
+	for t in transcripts:
+		result.transcript = result.transcript + t["transcript"]
+
+	# Parse items (words)
+	items = data["results"]["items"]
+	duration = 0.00
+	
+	# For each item, get the necessary parts and store as a word
+	for i in items:
+		alternatives = i["alternatives"]
+		# Choose an alternative
+		max_confidence = 0.00
+		text = ""
+
+		# Each word is stored as an "alternative".  Get the one with the maximum confidence
+		for a in alternatives:
+			if float(a["confidence"]) > max_confidence:
+				max_confidence = float(a["confidence"])
+				text = a["content"]
+
+		end_time = -1
+		start_time = -1
+
+		# Two types (punctionation, pronunciation).  Only keep times for pronunciation
+		if i["type"] == "pronunciation":
+			end_time = float(i["end_time"])
+			start_time = float(i["start_time"])
+
+			# If this is the greatest end time, store it as duration
+			if end_time > duration:
+				duration = end_time
+		# Add the word to the result
+		result.addWord(i["type"], start_time, end_time, text, "confidence", max_confidence)
+	
+	# Create the media object
+	media = SpeechToTextMedia(duration, media_file)
+
+	# Create the final object
+	outputFile = SpeechToText(media, result)
+
+	# Write the output
+	write_output_json(outputFile, output_json_file)
+
+
+# Serialize schema obj and write it to output file
+def write_output_json(transcribe_schema, json_file):
+	# Serialize the segmentation object
+	with open(json_file, 'w') as outfile:
+		json.dump(transcribe_schema, outfile, default=lambda x: x.__dict__)
+
+if __name__ == "__main__":
+	main()
---
+++ tools/json_schema/kaldi_local_to_schema.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+from speech_to_text_schema import SpeechToText, SpeechToTextMedia, SpeechToTextResult, SpeechToTextScore, SpeechToTextWord
+
+# Convert kaldi output to standardized json
+def main():
+	(media_file, kaldi_file, output_json_file) = sys.argv[1:4]
+
+	# Open the kaldi json
+	with open(kaldi_file) as json_file:
+		data = json.load(json_file)
+
+	result = SpeechToTextResult()
+
+	# Get a list of words
+	words = data["words"]
+	duration = 0.00
+
+	# For each word, add a word to our result
+	for w in words:
+		time = float(w["time"])
+		end = time + float(w["duration"])
+		# Keep track of the last time and use it as the duration
+		if end > duration:
+			duration = end
+		result.addWord("", time, end, w["word"], None, None)
+
+	# Create the media objeect
+	media = SpeechToTextMedia(duration, media_file)
+
+	# Create the final object
+	outputFile = SpeechToText(media, result)
+
+	#write the output
+	write_output_json(outputFile, output_json_file)
+
+
+# Serialize schema obj and write it to output file
+def write_output_json(transcribe_schema, json_file):
+	# Serialize the segmentation object
+	with open(json_file, 'w') as outfile:
+		json.dump(transcribe_schema, outfile, default=lambda x: x.__dict__)
+
+if __name__ == "__main__":
+	main()
