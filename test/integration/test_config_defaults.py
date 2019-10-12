e5f60028d2eccfd77793e5c54ceda2b34e5fd233
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2019-10-12 01:30:41 -0400

    Remove tests for options with overridden defaults
    Options database_engine_option_max_overflow and
    database_engine_option_pool_size are overridden in base/driver_util.py
    for testing (for non-sqlite databases).  While it is easy to assert that
    a value is x or y, that defeats the goal of the test.

--- test/integration/test_config_defaults.py
+++ test/integration/test_config_defaults.py
@@ -9,14 +9,6 @@ class ConfigDefaultsTestCase(integration_util.IntegrationTestCase):
     def get_default(self, key):
         return self._app.config.appschema[key]['default']
 
-    def test_default_database_engine_option_pool_size(self):
-        expect = self.get_default('database_engine_option_pool_size')
-        assert expect == self._app.config.database_engine_option_pool_size
-
-    def test_default_database_engine_option_max_overflow(self):
-        expect = self.get_default('database_engine_option_max_overflow')
-        assert expect == self._app.config.database_engine_option_max_overflow
-
     def test_default_database_engine_option_pool_recycle(self):
         expect = self.get_default('database_engine_option_pool_recycle')
         assert expect == self._app.config.database_engine_option_pool_recycle
