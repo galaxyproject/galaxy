b63a9b9b5d5f4b2651493e155ce834dbb08eb322
will <theappdev@protonmail.com>
2019-10-15 03:41:26 -0400

    remove data not used in this view

--- client/galaxy/scripts/components/User/UserPreferences.vue
+++ client/galaxy/scripts/components/User/UserPreferences.vue
@@ -49,9 +49,7 @@
                         action: `information`,
                         title: _l("Manage information"),
                         description: "Edit your email, addresses and custom parameters or change your public name.",
-                        url: `api/users/${Galaxy.user.id}/information/inputs`,
                         icon: "fa-user",
-                        redirect: "user",
                         shouldRender: !config.use_remote_user,
                     },
                     {
@@ -59,18 +57,13 @@
                         title: _l("Change password"),
                         description: _l("Allows you to change your login credentials."),
                         icon: "fa-unlock-alt",
-                        url: `api/users/${Galaxy.user.id}/password/inputs`,
-                        submit_title: "Save password",
-                        redirect: "user",
                         shouldRender: !config.use_remote_user,
                     },
                     {
                         action: "communication",
                         title: _l("Change communication settings"),
                         description: _l("Enable or disable the communication feature to chat with other users."),
-                        url: `api/users/${Galaxy.user.id}/communication/inputs`,
                         icon: "fa-comments-o",
-                        redirect: "user",
                         shouldRender: !!config.enable_communication_server,
                     },
                     {
@@ -78,13 +71,9 @@
                         title: _l("Set dataset permissions for new histories"),
                         description:
                             "Grant others default access to newly created histories. Changes made here will only affect histories created after these settings have been stored.",
-                        url: `api/users/${Galaxy.user.id}/permissions/inputs`,
                         icon: "fa-users",
-                        submit_title: "Save permissions",
-                        redirect: "user"
                     },
                     {
-                        action: "make_data_private",
                         title: _l("Make all data private"),
                         description: _l("Click here to make all data private."),
                         icon: "fa-lock",
@@ -94,10 +83,7 @@
                         action: "api_key",
                         title: _l("Manage API key"),
                         description: _l("Access your current API key or create a new one."),
-                        url: `api/users/${Galaxy.user.id}/api_key/inputs`,
                         icon: "fa-key",
-                        submit_title: "Create a new key",
-                        submit_icon: "fa-check"
                     },
                     {
                         action: "cloud_auth",
@@ -106,35 +92,27 @@
                             "Add or modify the configuration that grants Galaxy to access your cloud-based resources."
                         ),
                         icon: "fa-cloud",
-                        submit_title: "Create a new key",
-                        submit_icon: "fa-check"
                     },
                     {
                         action: "toolbox_filters",
                         title: _l("Manage Toolbox filters"),
                         description: _l("Customize your Toolbox by displaying or omitting sets of Tools."),
-                        url: `api/users/${Galaxy.user.id}/toolbox_filters/inputs`,
                         icon: "fa-filter",
-                        submit_title: "Save filters",
-                        redirect: "user",
                         shouldRender: !!config.has_user_tool_filters,
                     },
                     {
-                        action: "custom_builds",
                         title: _l("Manage custom builds"),
                         description: _l("Add or remove custom builds using history datasets."),
                         icon: "fa-cubes",
                         onclick: this.openManageCustomBuilds,
                     },
                     {
-                        action: "genomespace",
                         title: _l("Request GenomeSpace token"),
                         description: _l("Requests token through OpenID."),
                         icon: "fa-openid",
                         onclick: this.requestGenomeSpace,
                     },
                     {
-                        action: "logout",
                         title: _l("Sign out"),
                         description: _l("Click here to sign out of all sessions."),
                         icon: "fa-sign-out",
