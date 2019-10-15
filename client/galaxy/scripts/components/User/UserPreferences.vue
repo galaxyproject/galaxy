9ee4699e62e8e73a08d7727324d532f7918ec620
will <theappdev@protonmail.com>
2019-10-15 18:26:55 -0400

    add bootstrap style fixes

--- client/galaxy/scripts/components/User/UserPreferences.vue
+++ client/galaxy/scripts/components/User/UserPreferences.vue
@@ -2,13 +2,13 @@
     <div>
         <h2>User preferences</h2>
         <p>You are logged in as <strong>{{ email }}</strong>.</p>
-        <b-container>
-            <b-row class= "w-75" v-for="link in activeLinks" >
-                <b-col cols="1" >
-                    <i :class="`ml-3 mr-3 fa fa-lg ${link.icon}`">
+        <b-container fluid>
+            <b-row v-for="link in activeLinks" >
+                <b-col cols="5" md="2" lg="1" class="pl-0 pr-0" >
+                    <i :class="`fa fa-lg ${link.icon}`">
                     </i>
                 </b-col>
-                <b-col cols="11">
+                <b-col cols="7" md="10" lg="11" class="pl-0 pr-0">
                     <div>
                         <a v-if="link.onclick" @click="link.onclick" href="javascript:void(0)"><b>{{link.title}}</b></a>
                         <a v-else :href="`${baseUrl}/${link.action}`"><b>{{link.title}}</b></a>
