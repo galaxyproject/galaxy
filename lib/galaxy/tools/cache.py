65e0d1cf21ca340fdd9bc26be751a2f5db7fe5cc
mvdbeek <m.vandenbeek@gmail.com>
2019-10-17 11:34:53 +0200

    Don't use a thread local ToolShedRepositoryCache
    this may have been a memory leak.Every thread would need to build the
    cache again when first accessing it. Now that we eagerload the
    relationships (#8791 only went into dev) we shouldn't need to keep the
    cache thread local. This should be faster and more memory efficient, and
    actually it looks like the thread-local cache didn't work as intended
    before (writing tests now to verify it does work now).

--- lib/galaxy/tools/cache.py
+++ lib/galaxy/tools/cache.py
@@ -1,13 +1,7 @@
 import logging
 import os
 from collections import defaultdict
-from threading import (
-    local,
-    Lock,
-)
-
-from sqlalchemy import inspect
-from sqlalchemy.orm.exc import DetachedInstanceError
+from threading import Lock
 
 from galaxy.util import unicodify
 from galaxy.util.hash_util import md5_hash_file
@@ -138,66 +132,29 @@ class ToolShedRepositoryCache(object):
 
     def __init__(self, app):
         self.app = app
-        self.cache = local()
+        self.local_repositories = []
+        self.repositories = []
+        self.repos_by_tuple = defaultdict(list)
 
     def add_local_repository(self, repository):
-        self.cache.repositories.append(repository)
-        self.cache.repos_by_tuple[(repository.tool_shed, repository.owner, repository.name)].append(repository)
-
-    @property
-    def tool_shed_repositories(self):
-        try:
-            repositories = self.cache.repositories
-        except AttributeError:
-            self.rebuild()
-            repositories = self.cache.repositories
-        tool_shed_repositories = [repo for repo in repositories if isinstance(repo, self.app.install_model.ToolShedRepository)]
-        if tool_shed_repositories and inspect(tool_shed_repositories[0]).detached:
-            self.rebuild()
-            repositories = self.cache.repositories
-        return repositories
-
-    @property
-    def tool_shed_repos_by_tuple(self):
-        try:
-            return self.cache.repos_by_tuple
-        except AttributeError:
-            self.rebuild()
-            return self.cache.repos_by_tuple
+        self.local_repositories.append(repository)
+        self.repos_by_tuple[(repository.tool_shed, repository.owner, repository.name)].append(repository)
 
     def rebuild(self):
-        repositories = self.app.install_model.context.current.query(self.app.install_model.ToolShedRepository).all()
-        self.cache.repositories = repositories
+        self.repositories = self.app.install_model.context.current.query(self.app.install_model.ToolShedRepository).all()
         repos_by_tuple = defaultdict(list)
-        for repository in repositories:
+        for repository in self.repositories + self.local_repositories:
             repos_by_tuple[(repository.tool_shed, repository.owner, repository.name)].append(repository)
-        self.cache.repos_by_tuple = repos_by_tuple
+        self.repos_by_tuple = repos_by_tuple
 
     def get_installed_repository(self, tool_shed=None, name=None, owner=None, installed_changeset_revision=None, changeset_revision=None, repository_id=None):
-        try:
-            return self._get_installed_repository(tool_shed=tool_shed,
-                                                  name=name,
-                                                  owner=owner,
-                                                  installed_changeset_revision=installed_changeset_revision,
-                                                  changeset_revision=changeset_revision,
-                                                  repository_id=repository_id)
-        except DetachedInstanceError:
-            self.rebuild()
-            return self._get_installed_repository(tool_shed=tool_shed,
-                                                  name=name,
-                                                  owner=owner,
-                                                  installed_changeset_revision=installed_changeset_revision,
-                                                  changeset_revision=changeset_revision,
-                                                  repository_id=repository_id)
-
-    def _get_installed_repository(self, tool_shed=None, name=None, owner=None, installed_changeset_revision=None, changeset_revision=None, repository_id=None):
         if repository_id:
-            repos = [repo for repo in self.tool_shed_repositories if repo.id == repository_id]
+            repos = [repo for repo in self.repositories if repo.id == repository_id]
             if repos:
                 return repos[0]
             else:
                 return None
-        repos = self.tool_shed_repos_by_tuple[(tool_shed, owner, name)]
+        repos = self.repos_by_tuple[(tool_shed, owner, name)]
         for repo in repos:
             if installed_changeset_revision and repo.installed_changeset_revision != installed_changeset_revision:
                 continue
