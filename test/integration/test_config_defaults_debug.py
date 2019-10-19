acd89aedd4a4e43ba4d1f440da8333fae64640fd
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2019-10-19 14:07:36 -0400

    Simulate failure

--- test/integration/test_config_defaults_debug.py
+++ test/integration/test_config_defaults_debug.py
@@ -34,53 +34,54 @@ class ConfigDefaultsTestCase(integration_util.IntegrationTestCase):
         return os.path.join(parent, child) if child else parent
 
     def test_default_shed_tool_config_file(self):
-        expect = self.get_default_in_mutable_config_dir('shed_tool_config_file')
+        expect = self.get_default_in_mutable_config_dir('shed_tool_config_file') + 'FAILME'
+        print('shed_tool_config_file', expect, self._app.config.shed_tool_config_file)
         assert expect == self._app.config.shed_tool_config_file
     
-    def test_default_dependency_resolvers_config_file(self):
-        expect = self.get_default_in_config_dir('dependency_resolvers_config_file')
-        assert expect == self._app.config.dependency_resolvers_config_file
-    
-    def test_default_tool_sheds_config_file(self):
-        expect = self.get_default_in_config_dir('tool_sheds_config_file')
-        assert expect == self._app.config.tool_sheds_config_file
-    
-    def test_default_shed_tool_data_table_config(self):
-        expect = self.get_default_in_mutable_config_dir('shed_tool_data_table_config')
-        assert expect == self._app.config.shed_tool_data_table_config
-    
-    def test_default_object_store_config_file(self):
-        expect = self.get_default_in_config_dir('object_store_config_file')
-        assert expect == self._app.config.object_store_config_file
-    
-    def test_default_user_preferences_extra_conf_path(self):
-        expect = self.get_default_in_config_dir('user_preferences_extra_conf_path')
-        assert expect == self._app.config.user_preferences_extra_conf_path
-    
-    def test_default_oidc_config_file(self):
-        expect = self.get_default_in_config_dir('oidc_config_file')
-        assert expect == self._app.config.oidc_config_file
-    
-    def test_default_oidc_backends_config_file(self):
-        expect = self.get_default_in_config_dir('oidc_backends_config_file')
-        assert expect == self._app.config.oidc_backends_config_file
-    
-    def test_default_auth_config_file(self):
-        expect = self.get_default_in_config_dir('auth_config_file')
-        assert expect == self._app.config.auth_config_file
-    
-    def test_default_shed_data_manager_config_file(self):
-        expect = self.get_default_in_mutable_config_dir('shed_data_manager_config_file')
-        assert expect == self._app.config.shed_data_manager_config_file
-    
-    def test_default_job_resource_params_file(self):
-        expect = self.get_default_in_config_dir('job_resource_params_file')
-        assert expect == self._app.config.job_resource_params_file
-    
-    def test_default_workflow_resource_params_file(self):
-        expect = self.get_default_in_config_dir('workflow_resource_params_file')
-        assert expect == self._app.config.workflow_resource_params_file
-    
-    def test_default_workflow_schedulers_config_file(self):
-        expect = self.get_default_in_config_dir('workflow_schedulers_config_file')
-        assert expect == self._app.config.workflow_schedulers_config_file
+#    def test_default_dependency_resolvers_config_file(self):
+#        expect = self.get_default_in_config_dir('dependency_resolvers_config_file')
+#        assert expect == self._app.config.dependency_resolvers_config_file
+#    
+#    def test_default_tool_sheds_config_file(self):
+#        expect = self.get_default_in_config_dir('tool_sheds_config_file')
+#        assert expect == self._app.config.tool_sheds_config_file
+#    
+#    def test_default_shed_tool_data_table_config(self):
+#        expect = self.get_default_in_mutable_config_dir('shed_tool_data_table_config')
+#        assert expect == self._app.config.shed_tool_data_table_config
+#    
+#    def test_default_object_store_config_file(self):
+#        expect = self.get_default_in_config_dir('object_store_config_file')
+#        assert expect == self._app.config.object_store_config_file
+#    
+#    def test_default_user_preferences_extra_conf_path(self):
+#        expect = self.get_default_in_config_dir('user_preferences_extra_conf_path')
+#        assert expect == self._app.config.user_preferences_extra_conf_path
+#    
+#    def test_default_oidc_config_file(self):
+#        expect = self.get_default_in_config_dir('oidc_config_file')
+#        assert expect == self._app.config.oidc_config_file
+#    
+#    def test_default_oidc_backends_config_file(self):
+#        expect = self.get_default_in_config_dir('oidc_backends_config_file')
+#        assert expect == self._app.config.oidc_backends_config_file
+#    
+#    def test_default_auth_config_file(self):
+#        expect = self.get_default_in_config_dir('auth_config_file')
+#        assert expect == self._app.config.auth_config_file
+#    
+#    def test_default_shed_data_manager_config_file(self):
+#        expect = self.get_default_in_mutable_config_dir('shed_data_manager_config_file')
+#        assert expect == self._app.config.shed_data_manager_config_file
+#    
+#    def test_default_job_resource_params_file(self):
+#        expect = self.get_default_in_config_dir('job_resource_params_file')
+#        assert expect == self._app.config.job_resource_params_file
+#    
+#    def test_default_workflow_resource_params_file(self):
+#        expect = self.get_default_in_config_dir('workflow_resource_params_file')
+#        assert expect == self._app.config.workflow_resource_params_file
+#    
+#    def test_default_workflow_schedulers_config_file(self):
+#        expect = self.get_default_in_config_dir('workflow_schedulers_config_file')
+#        assert expect == self._app.config.workflow_schedulers_config_file
