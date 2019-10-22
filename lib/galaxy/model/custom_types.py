632cbc8c39628d2fba0e496ac0fe57eddf12ef5e
mvdbeek <m.vandenbeek@gmail.com>
2019-10-22 20:13:53 +0200

    Cast to unicode string in TrimmedString
    This fixes https://github.com/galaxyproject/galaxy/issues/8680

--- lib/galaxy/model/custom_types.py
+++ lib/galaxy/model/custom_types.py
@@ -372,5 +372,5 @@ class TrimmedString(TypeDecorator):
     def process_bind_param(self, value, dialect):
         """Automatically truncate string values"""
         if self.impl.length and value is not None:
-            value = value[0:self.impl.length]
+            value = unicodify(value)[0:self.impl.length]
         return value
