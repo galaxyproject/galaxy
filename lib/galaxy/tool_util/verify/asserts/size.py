9926ea50e1813cde0c4c1a201fe4ba58798e2b80
Simon Bray <sbray1371@gmail.com>
2019-10-23 12:07:40 +0200

    add test, len(output_bytes)

--- lib/galaxy/tool_util/verify/asserts/size.py
+++ lib/galaxy/tool_util/verify/asserts/size.py
@@ -4,7 +4,6 @@ import io
 
 def assert_has_size(output_bytes, value, delta=0):
     """Asserts the specified output has a size of the specified value"""
-    output_temp = io.BytesIO(output_bytes)
-    output_size = sys.getsizeof(output_temp)
+    output_size = len(output_bytes)
     assert abs(output_size - int(value)) <= int(delta), "Expected file size was %s, actual file size was %s (difference of %s accepted)" % (value, output_size, delta)
 
