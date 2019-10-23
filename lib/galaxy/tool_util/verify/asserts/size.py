1dee121459ed7e3b43b6bc75cda52e8dc0b8c196
Simon Bray <sbray1371@gmail.com>
2019-10-23 09:42:56 +0200

    finish adding has_size

--- lib/galaxy/tool_util/verify/asserts/size.py
+++ lib/galaxy/tool_util/verify/asserts/size.py
@@ -1,17 +1,10 @@
 import sys
 import io
 
-def assert_has_line(output, line):
-    """ Asserts the specified output contains the line specified the
-    argument line."""
-    match = re.search("^%s$" % re.escape(line), output, flags=re.MULTILINE)
-    assert match is not None, "No line of output file was '%s' (output was '%s') " % (line, output)
 
-
-def assert_has_size(output_bytes, value):
+def assert_has_size(output_bytes, value, delta=0):
     """Asserts the specified output has a size of the specified value"""
-    
     output_temp = io.BytesIO(output_bytes)
     output_size = sys.getsizeof(output_temp)
-    assert output_size == value, "Expected file size was %s, actual file size was" % (value, output_size)
+    assert abs(output_size - int(value)) <= int(delta), "Expected file size was %s, actual file size was %s (difference of %s accepted)" % (value, output_size, delta)
 
