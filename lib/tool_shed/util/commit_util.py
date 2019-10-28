1ff15d057090377abe0068b74da9f8c210cfaf6d
guerler <aysam.guerler@gmail.com>
2019-10-28 00:51:08 -0400

    Remove unused repository types handler

--- lib/tool_shed/util/commit_util.py
+++ lib/tool_shed/util/commit_util.py
@@ -9,7 +9,7 @@ from collections import namedtuple
 
 from sqlalchemy.sql.expression import null
 
-import tool_shed.repository_types.util as rt_util
+from tool_shed import repository_types as rt_util
 from galaxy.util import checkers
 from galaxy.util.path import safe_relpath
 from tool_shed.tools.data_table_manager import ShedToolDataTableManager
