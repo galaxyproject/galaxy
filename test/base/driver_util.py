8038babd2e58d4639aa0ece5b25104d6aafafa87
John Chilton <jmchilton@gmail.com>
2019-11-10 20:45:19 -0500

    Fix ts tests on Mac OS X.

--- test/base/driver_util.py
+++ test/base/driver_util.py
@@ -87,7 +87,7 @@ install_context = None
 def setup_tool_shed_tmp_dir():
     tool_shed_test_tmp_dir = os.environ.get('TOOL_SHED_TEST_TMP_DIR', None)
     if tool_shed_test_tmp_dir is None:
-        tool_shed_test_tmp_dir = tempfile.mkdtemp()
+        tool_shed_test_tmp_dir = os.path.realpath(tempfile.mkdtemp())
     # Here's the directory where everything happens.  Temporary directories are created within this directory to contain
     # the hgweb.config file, the database, new repositories, etc.  Since the tool shed browses repository contents via HTTP,
     # the full path to the temporary directroy wher eht repositories are located cannot contain invalid url characters.
