35336683363d3260cf17ccc1215bda7446af7e44
mvdbeek <m.vandenbeek@gmail.com>
2019-11-11 14:50:22 +0100

    Try harder to get shed_config_dict
    The path based approach might be less desirable, but works if the
    repository metadata isn't correct and lists a wrong shed_config_filename.
    And default to returning a dict instead of `None` if that doesn't
    succeed.

--- lib/galaxy/model/tool_shed_install/__init__.py
+++ lib/galaxy/model/tool_shed_install/__init__.py
@@ -92,6 +92,8 @@ class ToolShedRepository(object):
         Return the in-memory version of the shed_tool_conf file, which is stored in the config_elems entry
         in the shed_tool_conf_dict.
         """
+        if default is None:
+            default = {}
 
         def _is_valid_shed_config_filename(filename):
             for shed_tool_conf_dict in app.toolbox.dynamic_confs(include_migrated_tool_conf=True):
@@ -137,12 +139,12 @@ class ToolShedRepository(object):
                             if tool_id in tool_ids:
                                 self.shed_config_filename = name
                                 return shed_tool_conf_dict
-        if self.includes_datatypes or self.includes_data_managers:
+        if self.includes_datatypes or self.includes_data_managers or not default:
             # We need to search by file paths here, which is less desirable.
             tool_shed = common_util.remove_protocol_and_port_from_tool_shed_url(self.tool_shed)
             for shed_tool_conf_dict in app.toolbox.dynamic_confs(include_migrated_tool_conf=True):
                 tool_path = shed_tool_conf_dict['tool_path']
-                relative_path = os.path.join(tool_path, tool_shed, 'repos', self.owner, self.name, self.installed_changeset_revision)
+                relative_path = os.path.join(tool_path, tool_shed, 'repos', self.owner, self.name)
                 if os.path.exists(relative_path):
                     self.shed_config_filename = shed_tool_conf_dict['config_filename']
                     return shed_tool_conf_dict
