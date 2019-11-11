fa4cb22f8071a994cc3ac4dfe5ea5f47d7bdd711
mvdbeek <m.vandenbeek@gmail.com>
2019-11-11 16:39:44 +0100

    Try path based approach if no luck getting shed_tool_conf dict via toolbox

--- lib/galaxy/model/tool_shed_install/__init__.py
+++ lib/galaxy/model/tool_shed_install/__init__.py
@@ -136,15 +136,14 @@ class ToolShedRepository(object):
                             if tool_id in tool_ids:
                                 self.shed_config_filename = name
                                 return shed_tool_conf_dict
-        if self.includes_datatypes or self.includes_data_managers or not default:
-            # We need to search by file paths here, which is less desirable.
-            tool_shed = common_util.remove_protocol_and_port_from_tool_shed_url(self.tool_shed)
-            for shed_tool_conf_dict in app.toolbox.dynamic_confs(include_migrated_tool_conf=True):
-                tool_path = shed_tool_conf_dict['tool_path']
-                relative_path = os.path.join(tool_path, tool_shed, 'repos', self.owner, self.name)
-                if os.path.exists(relative_path):
-                    self.shed_config_filename = shed_tool_conf_dict['config_filename']
-                    return shed_tool_conf_dict
+        # We need to search by file paths here, which is less desirable.
+        tool_shed = common_util.remove_protocol_and_port_from_tool_shed_url(self.tool_shed)
+        for shed_tool_conf_dict in app.toolbox.dynamic_confs(include_migrated_tool_conf=True):
+            tool_path = shed_tool_conf_dict['tool_path']
+            relative_path = os.path.join(tool_path, tool_shed, 'repos', self.owner, self.name)
+            if os.path.exists(relative_path):
+                self.shed_config_filename = shed_tool_conf_dict['config_filename']
+                return shed_tool_conf_dict
         return {}
 
     @property
