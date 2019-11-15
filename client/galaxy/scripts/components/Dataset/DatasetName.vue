5600843bfa7842793209fb4617da043ace26e3f1
guerler <aysam.guerler@gmail.com>
2019-11-14 23:42:16 -0500

    Add serialization view option to datasets index api endpoint

--- client/galaxy/scripts/components/Dataset/DatasetName.vue
+++ client/galaxy/scripts/components/Dataset/DatasetName.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <span v-if="isError" class="fa fa-exclamation-triangle text-danger" />
-        <span v-if="isPaused" class="fa fa-pause text-info" />
-        <b-link :href="item.url" class="ml-1"><b>{{ item.name }}</b></b-link>
+        <span v-if="isError" class="fa fa-exclamation-triangle text-danger mr-1" />
+        <span v-if="isPaused" class="fa fa-pause text-info mr-1" />
+        <b-link :href="item.url"><b>{{ item.name }}</b></b-link>
     </div>
 </template>
 <script>
