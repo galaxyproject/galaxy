d953d71d41ff3a4f92da7e9454ca31ee3f9f878e
Martin Cech <marten@bx.psu.edu>
2019-12-05 11:44:26 -0500

    switch temporal manipulations to momentjs
    use exact same version as handsontable

--- client/galaxy/scripts/components/UtcDate.vue
+++ client/galaxy/scripts/components/UtcDate.vue
@@ -1,13 +1,15 @@
 <template>
     <span class="utc-time" v-if="mode == 'date'" :title="elapsedTime">
-        {{ date }}
+        {{ full_date }}
     </span>
-    <span class="utc-time utc-time-elapsed" v-else :title="date">
+    <span class="utc-time utc-time-elapsed" v-else :title="full_date">
         {{ elapsedTime }}
     </span>
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
     props: {
         date: {
@@ -21,30 +23,10 @@ export default {
     },
     computed: {
         elapsedTime: function() {
-            var utcUpdate = new Date(this.date);
-            var now = new Date();
-            var utcNow = new Date(
-                now.getUTCFullYear(),
-                now.getUTCMonth(),
-                now.getUTCDate(),
-                now.getUTCHours(),
-                now.getUTCMinutes(),
-                now.getUTCSeconds(),
-                now.getUTCMilliseconds()
-            );
-            const timeDiff = utcNow - utcUpdate;
-            const daysDiff = timeDiff / (1000 * 3600 * 24);
-            const hoursDiff = timeDiff / (1000 * 3600);
-            const minutesDiff = timeDiff / (1000 * 60);
-            let diffStr;
-            if (daysDiff >= 1) {
-                diffStr = `${Math.floor(daysDiff)} days ago`;
-            } else if (hoursDiff >= 1) {
-                diffStr = `${Math.floor(hoursDiff)} hours ago`;
-            } else {
-                diffStr = `${Math.floor(minutesDiff)} minutes ago`;
-            }
-            return diffStr;
+            return moment(moment.utc(this.date)).from(moment().utc());
+        },
+        full_date: function() {
+            return moment.utc(this.date).format();
         }
     }
 };
