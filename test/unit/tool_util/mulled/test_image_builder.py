464d83a9ad37b437fdaea2d1de1a7f433a68433f
mvdbeek <m.vandenbeek@gmail.com>
2019-12-12 10:59:13 +0100

    Skip mulled tests in regular unit tests

--- test/unit/tool_util/mulled/test_image_builder.py
+++ test/unit/tool_util/mulled/test_image_builder.py
@@ -8,6 +8,7 @@ from galaxy.tool_util.deps.mulled.image_builder import (
     DockerContainerBuilder,
     SingularityContainerBuilder,
 )
+from ..test_conda_resolution import external_dependency_management
 
 
 def test_build_stage1_info():
@@ -38,51 +39,58 @@ def test_build_stage1_info_singularity():
     assert info.contents == 'Bootstrap: docker\nFrom: continuumio/miniconda3:latest\nStage: build\n%post\n    \n    /opt/conda/bin/conda install -c conda-forge -c bioconda transtermhp -p /usr/local --copy --yes \n    \n%test\n    true\n'
 
 
+@pytest.mark.parametrize("builder_class", [DockerContainerBuilder, SingularityContainerBuilder])
 @pytest.mark.parametrize('repo,target_args,channels,preinstall,postinstall,verbose', [
     ('docker_repo', 'package_a package_b', ['channel_a', 'channel_b'], 'echo "preinstall"', 'echo "postinstall"', False),
     ('docker_repo', 'package_a package_b', ['channel_a', 'channel_b'], 'echo "preinstall"', 'echo "postinstall"', True),
 ])
-def test_build_stage1_variants(repo, target_args, channels, preinstall, postinstall, verbose):
-    builder = DockerContainerBuilder(repo=repo, target_args=target_args, channels=channels, preinstall=preinstall, postinstall=postinstall, verbose=verbose)
+def test_build_stage1_variants(builder_class, repo, target_args, channels, preinstall, postinstall, verbose):
+    builder = builder_class(repo=repo, target_args=target_args, channels=channels, preinstall=preinstall, postinstall=postinstall, verbose=verbose)
     info = builder.build_info(builder.template_stage1)
     assert info.repo == repo
     assert target_args in info.contents
     for channel in channels:
         assert "-c %s" % channel in info.contents
-    assert "RUN %s" % preinstall in info.contents
-    assert "RUN %s" % postinstall in info.contents
+    assert "%s%s" % (builder.run_prefix, preinstall) in info.contents
+    assert "%s%s" % (builder.run_prefix, postinstall) in info.contents
     if verbose:
         assert '--verbose' in info.contents
     else:
         assert '--verbose' not in info.contents
 
 
-def test_collect_env_vars():
+@pytest.mark.parametrize("builder_class", [DockerContainerBuilder, SingularityContainerBuilder])
+@external_dependency_management
+def test_collect_env_vars(builder_class):
     repo = 'transtermhp'
-    builder = DockerContainerBuilder(repo=repo, target_args=repo)
+    builder = builder_class(repo=repo, target_args=repo)
     info = builder.build_info(builder.template_stage1)
     builder.build_stage(info)
     conda_env_vars = builder.get_conda_env_vars()
     assert conda_env_vars == {'TRANSTERMHP': '/usr/local/data/expterm.dat'}
 
 
+@external_dependency_management
+@pytest.mark.parametrize("builder_class", [DockerContainerBuilder, SingularityContainerBuilder])
 @pytest.mark.parametrize('image,expect_true', [
     ('transtermhp', False),
     ('multiqc', True)
 ])
-def test_image_requires_extended_base(image, expect_true):
-    builder = DockerContainerBuilder(repo=image, target_args=image)
+def test_image_requires_extended_base(builder_class, image, expect_true):
+    builder = builder_class(repo=image, target_args=image)
     info = builder.build_info(builder.template_stage1)
     builder.build_stage(info)
     assert builder.image_requires_extended_base() == expect_true
 
 
+@external_dependency_management
+@pytest.mark.parametrize("builder_class", [DockerContainerBuilder, SingularityContainerBuilder])
 @pytest.mark.parametrize('target_args,destination_image,env_statements', [
     ('transtermhp', DEFAULT_DESTINATION_IMAGE, 'ENV TRANSTERMHP /usr/local/data/expterm.dat'),
     ('multiqc', DEFAULT_EXTENDED_BASE_IMAGE, ''),
 ])
-def test_build_image(target_args, destination_image, env_statements):
-    builder = DockerContainerBuilder(repo=target_args, target_args=target_args)
+def test_build_image(builder_class, target_args, destination_image, env_statements):
+    builder = builder_class(repo=target_args, target_args=target_args)
     builder.build_image()
     assert "FROM %s" % destination_image in builder.recipe_stage2.contents
     assert env_statements in builder.recipe_stage2.contents
