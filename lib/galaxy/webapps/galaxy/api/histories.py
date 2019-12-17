4852ce2fc521039f7ad223c91b905ad056f28816
tinker <thom@hometreelab.com>
2019-12-17 12:49:25 +1000

    Remove potentially unnecessary lookup

--- lib/galaxy/webapps/galaxy/api/histories.py
+++ lib/galaxy/webapps/galaxy/api/histories.py
@@ -146,13 +146,12 @@ class HistoriesController(BaseAPIController, ExportsHistoryMixin, ImportsHistory
         filters = []
         # support the old default of not-returning/filtering-out deleted histories
         filters += self._get_deleted_filter(deleted, filter_params)
-        # check if user is admin and get optional parameter 'all'
-        admin = trans.user_is_admin
+        # get optional parameter 'all'
         all_histories = util.string_as_bool(kwd.get('all', False))
         # if parameter 'all' is true, throw exception if not admin
         # else add current user filter to query (default behaviour)
         if all_histories:
-            if not admin:
+            if not trans.user_is_admin:
                 message = "Only admins can query all histories"
                 raise exceptions.AdminRequiredException(message)
         else:
