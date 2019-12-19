78ba001576b9755420d4f34abbaefcd2fc0ef9b6
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2019-12-19 11:46:46 -0500

    Check if directory exists instead of try/except
    As per code review discussion.

--- lib/galaxy/tools/toolbox/base.py
+++ lib/galaxy/tools/toolbox/base.py
@@ -828,19 +828,19 @@ class AbstractToolBox(Dictifiable, ManagesIntegratedToolPanelMixin):
                 return None
 
         tool_loaded = False
-        try:
-            for name in os.listdir(directory):
-                if name.startswith(('.', '_')):
-                    # Very unlikely that we want to load tools from a hidden or private folder
-                    continue
-                child_path = os.path.join(directory, name)
-                if os.path.isdir(child_path) and recursive:
-                    self.__watch_directory(child_path, elems, integrated_elems, load_panel_dict, recursive)
-                elif self._looks_like_a_tool(child_path):
-                    quick_load(child_path, async_load=False)
-                    tool_loaded = True
-        except Exception:
-            log.exception("Failed to read tool directory %s.", directory)
+        if not os.path.isdir(directory):
+            log.error("Failed to read tool directory %s.", directory)
+            return
+        for name in os.listdir(directory):
+            if name.startswith(('.', '_')):
+                # Very unlikely that we want to load tools from a hidden or private folder
+                continue
+            child_path = os.path.join(directory, name)
+            if os.path.isdir(child_path) and recursive:
+                self.__watch_directory(child_path, elems, integrated_elems, load_panel_dict, recursive)
+            elif self._looks_like_a_tool(child_path):
+                quick_load(child_path, async_load=False)
+                tool_loaded = True
         if (tool_loaded or force_watch) and self._tool_watcher:
             self._tool_watcher.watch_directory(directory, quick_load)
 
