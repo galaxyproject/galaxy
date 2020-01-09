05ca856d3ab35eee77c7561aceb2043e6438139b
guerler <aysam.guerler@gmail.com>
2020-01-09 14:09:35 -0500

    Consolidate tool box parameterization in tool-panel wrapper

--- client/galaxy/scripts/entry/panels/tool-panel.js
+++ client/galaxy/scripts/entry/panels/tool-panel.js
@@ -11,35 +11,17 @@ import { getToolsLayout } from "../../components/Panels/utilities.js";
 import { mountVueComponent } from "../../utils/mountVueComponent";
 
 const ToolPanel = Backbone.View.extend({
-    initialize: function(page, options) {
+    initialize: function() {
         const Galaxy = getGalaxyInstance();
         const appRoot = getAppRoot();
 
-        // access configuration options
-        const config = options.config;
-        this.root = options.root;
-
-        /** @type {Object[]} descriptions of user's workflows to be shown in the tool menu */
-        this.stored_workflow_menu_entries = config.stored_workflow_menu_entries || [];
-
-        // create tool search, tool panel, and tool panel view.
-        const tool_search = new Tools.ToolSearch({
-            hidden: false
-        });
-        const tools = new Tools.ToolCollection(config.toolbox);
-        this.tool_panel = new Tools.ToolPanel({
-            tool_search: tool_search,
-            tools: tools,
-            layout: config.toolbox_in_panel
-        });
-
         // add uploader button to Galaxy object
         Galaxy.upload = new Upload({
-            upload_path: config.nginx_upload_path || `${appRoot}api/tools`,
-            chunk_upload_size: config.chunk_upload_size,
-            ftp_upload_site: config.ftp_upload_site,
-            default_genome: config.default_genome,
-            default_extension: config.default_extension
+            upload_path: Galaxy.config.nginx_upload_path || `${appRoot}api/tools`,
+            chunk_upload_size: Galaxy.config.chunk_upload_size,
+            ftp_upload_site: Galaxy.config.ftp_upload_site,
+            default_genome: Galaxy.config.default_genome,
+            default_extension: Galaxy.config.default_extension
         });
 
         // components for panel definition
@@ -62,36 +44,47 @@ const ToolPanel = Backbone.View.extend({
     },
 
     getPropsData: function() {
+        const Galaxy = getGalaxyInstance();
+        const appRoot = getAppRoot();
+
+        /** @type {Object[]} descriptions of user's workflows to be shown in the tool menu */
+        const storedWorkflowMenuEntries = Galaxy.config.stored_workflow_menu_entries || [];
+
+        // create tool search, tool panel, and tool panel view.
+        const tool_search = new Tools.ToolSearch({
+            hidden: false
+        });
+        const tools = new Tools.ToolCollection(Galaxy.config.toolbox);
+        const toolPanel = new Tools.ToolPanel({
+            tool_search: tool_search,
+            tools: tools,
+            layout: Galaxy.config.toolbox_in_panel
+        });
+
         return {
             side: "left",
             currentPanel: ToolBox,
-            currentPanelProperties: this.getProperties()
-        };
-    },
-
-    getProperties: function() {
-        const Galaxy = getGalaxyInstance();
-        const appRoot = getAppRoot();
-        return {
-            appRoot: getAppRoot(),
-            toolsTitle: _l("Tools"),
-            toolsLayout: getToolsLayout(this.tool_panel),
-            isUser: !!(Galaxy.user && Galaxy.user.id),
-            workflowsTitle: _l("Workflows"),
-            workflows: [
-                {
-                    title: _l("All workflows"),
-                    href: `${appRoot}workflows/list`,
-                    id: "list"
-                },
-                ...this.stored_workflow_menu_entries.map(menuEntry => {
-                    return {
-                        title: menuEntry["stored_workflow"]["name"],
-                        href: `${appRoot}workflows/run?id=${menuEntry["encoded_stored_workflow_id"]}`,
-                        id: menuEntry["encoded_stored_workflow_id"]
-                    };
-                })
-            ]
+            currentPanelProperties: {
+                appRoot: getAppRoot(),
+                toolsTitle: _l("Tools"),
+                toolsLayout: getToolsLayout(toolPanel),
+                isUser: !!(Galaxy.user && Galaxy.user.id),
+                workflowsTitle: _l("Workflows"),
+                workflows: [
+                    {
+                        title: _l("All workflows"),
+                        href: `${appRoot}workflows/list`,
+                        id: "list"
+                    },
+                    ...storedWorkflowMenuEntries.map(menuEntry => {
+                        return {
+                            title: menuEntry["stored_workflow"]["name"],
+                            href: `${appRoot}workflows/run?id=${menuEntry["encoded_stored_workflow_id"]}`,
+                            id: menuEntry["encoded_stored_workflow_id"]
+                        };
+                    })
+                ]
+            }
         };
     },
 
