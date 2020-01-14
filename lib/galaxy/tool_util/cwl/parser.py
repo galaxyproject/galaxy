8e6c263c924de5aef3a322463a958ddc2cbee9ac
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2020-01-14 01:34:00 +0000

    Remove code presently unused

--- lib/galaxy/tool_util/cwl/parser.py
+++ lib/galaxy/tool_util/cwl/parser.py
@@ -48,10 +48,7 @@ from .schema import (
     non_strict_non_validating_schema_loader,
     schema_loader,
 )
-from .util import (
-    guess_artifact_type,
-    SECONDARY_FILES_EXTRA_PREFIX,
-)
+from .util import SECONDARY_FILES_EXTRA_PREFIX
 
 log = logging.getLogger(__name__)
 
@@ -1303,38 +1300,6 @@ class OutputInstance(object):
         self.output_type = output_type
         self.path = path
         self.fields = fields
-
-
-def get_outputs(path):
-    tool_or_workflow = guess_artifact_type(path)
-    if tool_or_workflow == "tool":
-        from galaxy.tool_util.parser import get_tool_source
-        tool_source = get_tool_source(path)
-        output_datasets, _ = tool_source.parse_outputs(None)
-        outputs = [ToolOutput(o) for o in output_datasets.values()]
-        return outputs
-    else:
-        workflow = workflow_proxy(path, strict_cwl_validation=False)
-        return [CwlWorkflowOutput(label) for label in workflow.output_labels]
-
-
-# Lighter-weight variant of Planemo runnable outputs.
-class CwlWorkflowOutput(object):
-
-    def __init__(self, label):
-        self._label = label
-
-    def get_id(self):
-        return self._label
-
-
-class ToolOutput(object):
-
-    def __init__(self, tool_output):
-        self._tool_output = tool_output
-
-    def get_id(self):
-        return self._tool_output.name
 
 
 __all__ = (
