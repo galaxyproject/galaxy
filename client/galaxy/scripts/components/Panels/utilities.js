d6d7f37dd36060479d8bcca83a6e7b76a01207d5
guerler <aysam.guerler@gmail.com>
2020-01-15 18:23:06 -0500

    Format client

--- client/galaxy/scripts/components/Panels/utilities.js
+++ client/galaxy/scripts/components/Panels/utilities.js
@@ -3,18 +3,18 @@ export function filterToolSections(layout, results) {
         const filteredLayout = layout.map(category => {
             return {
                 ...category,
-                elems: category.elems && category.elems.filter(el => {
-                    return !el.text && results.includes(el.id);
-                })
+                elems:
+                    category.elems &&
+                    category.elems.filter(el => {
+                        return !el.text && results.includes(el.id);
+                    })
             };
         });
-        return filteredLayout.filter(
-            category => {
-                const isSection = category.elems && category.elems.length > 0;
-                const isMatchedTool = !category.text && results.includes(category.id);
-                return isSection || isMatchedTool;
-            }
-        );
+        return filteredLayout.filter(category => {
+            const isSection = category.elems && category.elems.length > 0;
+            const isMatchedTool = !category.text && results.includes(category.id);
+            return isSection || isMatchedTool;
+        });
     } else {
         return layout;
     }
