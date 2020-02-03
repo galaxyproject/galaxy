4b8acdf99c77776668de1d4acd24c6a102c8d6a1
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2020-02-03 18:07:39 -0500

    Log message instead of raising an exception
    This commit drops the exception that guarded against accidental
    overwriting of existing attributes, and instead ignores the setattr
    call, logging a debug message (which was initialy proposed in #8728).
    
    Rationale: with the previous version, base configuration properties
    (config_dir, data_dir, etc.) had to be set *after* this method was
    called (otherwise an exeption was raised), but *before* any path
    resolving code (because these properties are used for path
    resolution). As a result, config loading required a rigid sequence of steps:
    1: load defaults and kwargs, create all config attributes.
    2: set base configuration properties [CANNOT MOVE THIS]
    3: resolve paths, do the rest: load individual properties, etc.
    
    However, setting base configuration properties is a special case that is
    *independent of schema loading*, is handled separately (in
    set_config_base), and takes presendence over schema loading procedures.
    
    This modification makes config loading more flexible, and logical:
    1. set base configuration properties
    2. do the rest (now any refactoring can go here)

--- lib/galaxy/config/__init__.py
+++ lib/galaxy/config/__init__.py
@@ -311,9 +311,10 @@ class GalaxyAppConfiguration(BaseAppConfiguration):
 
     def _create_attributes_from_raw_config(self):
         for key, value in self._raw_config.items():
-            if hasattr(self, key):
-                raise ConfigurationError("Attempting to override existing attribute '%s'" % key)
-            setattr(self, key, value)
+            if not hasattr(self, key):
+                setattr(self, key, value)
+            else:
+                log.debug("Attribute '%s' is set and cannot be overwritten with value '%s'" % (key, value))
 
     def _resolve_paths(self, kwargs):
 
