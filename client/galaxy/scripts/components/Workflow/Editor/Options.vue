cb6ca5c22be265b6d4f1ef101de7364c72daba50
guerler <aysam.guerler@gmail.com>
2020-02-05 15:33:37 -0500

    Replace custom toolbar in workflow editor with bootstrap vue components

--- client/galaxy/scripts/components/Workflow/Editor/Options.vue
+++ client/galaxy/scripts/components/Workflow/Editor/Options.vue
@@ -1,52 +1,41 @@
 <template>
     <div class="panel-header-buttons">
         <span v-if="canvas">
-            <a
-                id="workflow-run-button"
-                class="panel-header-button"
-                href="javascript:void(0)"
-                role="button"
-                title="Run"
-                aria-label="Run"
-                @click="$emit('onRun')"
-            >
-                <span class="fa fa-play" />
-            </a>
-            <a
+            <b-button
                 id="workflow-save-button"
-                class="panel-header-button"
-                href="javascript:void(0)"
                 role="button"
-                title="Save"
-                aria-label="Save"
+                title="Save Workflow"
+                variant="link"
+                aria-label="Save Workflow"
+                v-b-tooltip.hover
+                v-b-tooltip.bottom
                 @click="$emit('onSave')"
             >
                 <span class="fa fa-floppy-o" />
-            </a>
-            <a
+            </b-button>
+            <b-button
                 id="workflow-report-button"
-                class="panel-header-button"
-                href="javascript:void(0)"
                 role="button"
                 title="Edit Report"
+                variant="link"
                 aria-label="Edit Report"
+                v-b-tooltip.hover
+                v-b-tooltip.bottom
                 @click="$emit('onReport')"
             >
                 <span class="fa fa-edit" />
-            </a>
-
-            <a
+            </b-button>
+            <b-dropdown
+                no-caret
+                right
                 id="workflow-options-button"
-                class="panel-header-button"
-                href="javascript:void(0)"
                 role="button"
-                title="Workflow options"
-                aria-label="Workflow options"
-                ref="save-button"
+                title="Workflow Options"
+                variant="link"
+                aria-label="Workflow Options"
+                v-b-tooltip.hover
+                v-b-tooltip.bottom
             >
-                <span class="fa fa-cog" />
-            </a>
-            <b-dropdown no-caret right class="panel-header-button">
                 <template v-slot:button-content>
                     <span class="fa fa-cog" />
                 </template>
@@ -55,30 +44,44 @@
                 <b-dropdown-item href="#" @click="$emit('onLayout')">Auto Layout</b-dropdown-item>
                 <b-dropdown-item href="#" @click="$emit('onDownload')">Download</b-dropdown-item>
             </b-dropdown>
+            <b-button
+                id="workflow-run-button"
+                role="button"
+                title="Run Workflow"
+                variant="link"
+                aria-label="Run Workflow"
+                v-b-tooltip.hover
+                v-b-tooltip.bottom
+                @click="$emit('onRun')"
+            >
+                <span class="fa fa-play" />
+            </b-button>
         </span>
         <span v-else>
-            <a
+            <b-button
                 id="workflow-report-help-button"
-                class="panel-header-button"
-                href="javascript:void(0)"
                 role="button"
                 title="Report Syntax Help"
+                sm="size"
+                variant="link"
                 aria-label="Report Syntax Help"
+                v-b-tooltip.hover
+                v-b-tooltip.bottom
                 @click="$emit('onReportHelp')"
             >
                 <span class="fa fa-question" />
-            </a>
-            <a
+            </b-button>
+            <b-button
                 id="workflow-canvas-button"
-                class="panel-header-button"
-                href="javascript:void(0)"
                 role="button"
-                title="Edit Workflow"
-                aria-label="Edit Workflow"
+                title="Return to Workflow"
+                sm="size"
+                variant="link"
+                aria-label="Return to Workflow"
                 @click="$emit('onEdit')"
             >
-                <span class="fa fa-sitemap fa-rotate-270" />
-            </a>
+                <span class="fa fa-times" />
+            </b-button>
         </span>
     </div>
 </template>
