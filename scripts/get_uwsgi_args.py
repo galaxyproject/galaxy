4834b7535f6cc21f93b0035c3b5819549a26262c
Dannon Baker <dannon.baker@gmail.com>
2020-02-06 13:27:33 -0500

    Drop static-safe from DEFAULT_ARGS, relying on an explicit add now.

--- scripts/get_uwsgi_args.py
+++ scripts/get_uwsgi_args.py
@@ -23,7 +23,7 @@ ALIASES = {
     'module': ('mount',),   # mount is not actually an alias for module, but we don't want to set module if mount is set
 }
 DEFAULT_ARGS = {
-    '_all_': ('pythonpath', 'threads', 'buffer-size', 'http', 'static-map', 'static-safe', 'die-on-term', 'hook-master-start', 'enable-threads', 'umask'),
+    '_all_': ('pythonpath', 'threads', 'buffer-size', 'http', 'static-map', 'die-on-term', 'hook-master-start', 'enable-threads', 'umask'),
     'galaxy': ('py-call-osafterfork',),
     'reports': (),
     'tool_shed': ('cron',),
