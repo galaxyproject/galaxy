bc339be5ca6308ddfb272a09357eca58fd4a353f
Nate Coraor <nate@bx.psu.edu>
2020-02-06 12:37:36 -0500

    Fix use of next() as a meethod instead of a function

--- lib/galaxy/containers/docker.py
+++ lib/galaxy/containers/docker.py
@@ -99,7 +99,7 @@ class DockerInterface(ContainerInterface):
 
     @property
     def host(self):
-        return self.__host_iter.next()
+        return next(self.__host_iter)
 
     @property
     def host_iter(self):
@@ -255,7 +255,7 @@ class DockerAPIClient(object):
     def _init_client():
         kwargs = DockerAPIClient._client_kwargs.copy()
         if DockerAPIClient._host_iter is not None and 'base_url' not in kwargs:
-            kwargs['base_url'] = DockerAPIClient._host_iter.next()
+            kwargs['base_url'] = next(DockerAPIClient._host_iter)
         DockerAPIClient._client = docker.APIClient(*DockerAPIClient._client_args, **kwargs)
         log.info('Initialized Docker API client for server: %s', kwargs.get('base_url', 'localhost'))
 
