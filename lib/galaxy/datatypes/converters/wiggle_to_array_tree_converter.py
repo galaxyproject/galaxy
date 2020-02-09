f3ef378d182d363e85df166087e44ef326f1e287
mvdbeek <m.vandenbeek@gmail.com>
2020-02-09 16:29:32 +0100

    Add bx-python dependency for CONVERTER_Wiggle_0

--- lib/galaxy/datatypes/converters/wiggle_to_array_tree_converter.py
+++ lib/galaxy/datatypes/converters/wiggle_to_array_tree_converter.py
@@ -15,15 +15,16 @@ def main():
     input_fname = sys.argv[1]
     out_fname = sys.argv[2]
 
-    reader = WiggleReader(open(input_fname))
+    with input(input_fname) as in_fh, open(out_fname, 'w') as out:
+        reader = WiggleReader(in_fh)
 
-    # Fill array from reader
-    d = array_tree_dict_from_reader(reader, {}, block_size=BLOCK_SIZE)
+        # Fill array from reader
+        d = array_tree_dict_from_reader(reader, {}, block_size=BLOCK_SIZE)
 
-    for array_tree in d.values():
-        array_tree.root.build_summary()
+        for array_tree in d.values():
+            array_tree.root.build_summary()
 
-    FileArrayTreeDict.dict_to_file(d, open(out_fname, "w"))
+        FileArrayTreeDict.dict_to_file(d, out)
 
 
 if __name__ == "__main__":
