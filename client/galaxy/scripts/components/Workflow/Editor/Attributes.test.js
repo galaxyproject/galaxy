69f58a9f8dc1f12910c883d136b54a6bd1679b71
guerler <aysam.guerler@gmail.com>
2020-02-17 23:39:35 -0500

    Resolve vue test warnings

--- client/galaxy/scripts/components/Workflow/Editor/Attributes.test.js
+++ client/galaxy/scripts/components/Workflow/Editor/Attributes.test.js
@@ -1,7 +1,19 @@
+import Vue from "vue";
 import { mount } from "@vue/test-utils";
 import Attributes from "./Attributes";
+import { __RewireAPI__ as rewire } from "./Attributes";
 
 describe("Attributes", () => {
+    beforeEach(() => {
+        rewire.__Rewire__(
+            "Services",
+            class {
+                async updateWorkflow() {
+                    return {};
+                }
+            }
+        );
+    });
     it("test attributes", async () => {
         const wrapper = mount(Attributes, {
             propsData: {
@@ -14,6 +26,9 @@ describe("Attributes", () => {
         });
         const name = wrapper.find("#workflow-name");
         expect(name.element.value).to.equal("workflow_name");
+        wrapper.setProps({ name: "new_workflow_name" });
+        await Vue.nextTick();
+        expect(name.element.value).to.equal("new_workflow_name");
         const parameters = wrapper.findAll(".list-group-item");
         expect(parameters.length).to.equal(2);
         expect(parameters.at(0).text()).to.equal("1: workflow_parameter_0");
