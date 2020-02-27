5d4d4c6707da0c75171bcce3ba53cadb31807a15
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2020-02-27 11:50:42 -0500

    If managed_config_dir is set by user, ensure path is absolute.
    Co-Authored-By: Nicola Soranzo <nicola.soranzo@gmail.com>

--- lib/galaxy/config/__init__.py
+++ lib/galaxy/config/__init__.py
@@ -135,6 +135,8 @@ class BaseAppConfiguration(object):
             self.data_dir = config_kwargs.get('data_dir')
             self.sample_config_dir = os.path.join(os.path.dirname(__file__), 'sample')
             self.managed_config_dir = config_kwargs.get('managed_config_dir')
+            if self.managed_config_dir:
+                self.managed_config_dir = os.path.abspath(self.managed_config_dir)
 
             if running_from_source:
                 if not self.config_dir:
