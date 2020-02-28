13f906738a11c8ee7122e15cb4871445a01a580b
Kaivan Kamali <kxk302@psu.edu>
2020-02-28 15:44:59 -0500

    Added some input validation. Added first integration test.

--- test/integration/objectstore/test_irods.py
+++ test/integration/objectstore/test_irods.py
@@ -9,169 +9,64 @@ from irods.exception import CollectionDoesNotExist
 from irods.exception import DataObjectDoesNotExist
 from irods.session import iRODSSession
 
-from galaxy_test.driver import integration_util
+from galaxy.config import GalaxyAppConfiguration
+from galaxy.objectstore.irods import IRODSObjectStore
 
 SCRIPT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 
-class IrodsObjectStoreIntegrationTestCase(integration_util.IntegrationTestCase):
+CONFIG_DICT = {
+    'auth': {
+        'username': 'rods',
+        'password': 'rods'
+    },
+    'resource': {
+        'name': 'demoResc'
+    },
+    'zone': {
+        'name': 'tempZone'
+    },
+    'connection': {
+        'host': 'localhost',
+        'port': '1247'
+    },
+    'cache': {
+        'size': 1000.0, 
+        'path': 'database/object_store_cache'
+    },
+    'extra_dirs': [{'type': 'job_work', 'path': 'database/job_working_directory_irods'}, {'type': 'temp', 'path': 'database/tmp_irods'}]
+}
 
-    container_name = "irods_server"
+#class IrodsObjectStoreIntegrationTestCase(object):
 
-    @classmethod
-    def handle_galaxy_config_kwds(cls, config):
-        config["object_store_config_file"] = "object_store_conf.xml" 
+#config = None 
+#config_dict = None
+#irods_os = None
 
-    @classmethod
-    def setUpClass(cls):
-        super(IrodsObjectStoreIntegrationTestCase, cls).setUpClass()
-        IrodsObjectStoreIntegrationTestCase.start_irods_server(container_name=IrodsObjectStoreIntegrationTestCase.container_name)
+@pytest.fixture(scope='module')
+def setup_module():
+    config = GalaxyAppConfiguration()
 
-        # Let the server start before running the test
-        time.sleep(10)
+    config.object_store_cache_path = '/private/var/folders/pk/jwrvp5115zg81729h2x46f080000gn/T/tmpkffyrtx1/tmpb5xoh_67/tmp_ry_69z8/database/object_store_cache'
+    config.file_path = '/private/var/folders/pk/jwrvp5115zg81729h2x46f080000gn/T/tmpkffyrtx1/tmpb5xoh_67/tmp_ry_69z8/database/objects'
+    config.object_store_store_by = 'uuid'
+    config.object_store_check_old_style = False
+    config.jobs_directory = '/private/var/folders/pk/jwrvp5115zg81729h2x46f080000gn/T/tmpkffyrtx1/tmpb5xoh_67/tmp_ry_69z8/database/job_working_directory_k5dk3021'
+    config.new_file_path = '/private/var/folders/pk/jwrvp5115zg81729h2x46f080000gn/T/tmpkffyrtx1/tmpb5xoh_67/tmp_ry_69z8/database/new_files_path_sej_tqh7'
 
-    @classmethod
-    def tearDownClass(cls): 
-        super(IrodsObjectStoreIntegrationTestCase, cls).tearDownClass()
-        IrodsObjectStoreIntegrationTestCase.stop_irods_server(container_name=IrodsObjectStoreIntegrationTestCase.container_name)       
+    config_dict = CONFIG_DICT
 
-    @classmethod
-    def start_irods_server(cls, container_name, irods_port=1247, docker_image='kxk302/irods-server:0.1'):
-        start_server = ['docker', 'run', '-h', 'localhost', '-p', '{port}:1247'.format(port=irods_port), '-d', '--name', container_name, docker_image]
-        subprocess.check_call(start_server) 
-        
-    @classmethod
-    def stop_irods_server(cls, container_name):
-        subprocess.check_call(['docker', 'rm', '-f', container_name])
+    irods_os = IRODSObjectStore(config, CONFIG_DICT)
+    return irods_os
 
-    def test_get_existing_collection(self):
-        with iRODSSession(host='localhost', 
-            port='1247', 
-            user='rods',  
-            password='rods', 
-            zone='tempZone') as session:
+#    @classmethod
+#def teardown_module(module):
+#     del irods_os
+#     del config_dict
+#     del config
 
-            zone='tempZone'
-            user='rods'
-        
-            aCollection = "/" + zone + "/home/" + user
-
-            coll = session.collections.get(aCollection)
-            assert coll.path == aCollection, "Error! Got wrong collection path"
-            assert len(coll.subcollections) == 0, "Error! Collection should not have any subcollections!"
-            assert len(coll.data_objects) == 0, "Error! Collection should not have any data objects!"
-
-    def test_create_delete_new_collection(self):
-        with iRODSSession(host='localhost', 
-            port='1247', 
-            user='rods',  
-            password='rods', 
-            zone='tempZone') as session:
-
-            zone='tempZone'
-            user='rods'
-
-            newCollection = "/" + zone + "/home/" + user + "/testdir"
-    
-            coll = session.collections.create(newCollection)
-            assert coll.path == newCollection, "Error! Got wrong collection path"
-            assert len(coll.subcollections) == 0, "Error! Collection should not have any subcollections!"
-            assert len(coll.data_objects) == 0, "Error! Collection should not have any data objects!" 
-
-            coll.remove(recurse=True, force=True)
-
-            with pytest.raises(CollectionDoesNotExist):
-                coll = session.collections.get(newCollection)
-
-    def test_create_delete_new_data_object(self):
-        with iRODSSession(host='localhost', 
-            port='1247', 
-            user='rods',  
-            password='rods', 
-            zone='tempZone') as session:
-
-            zone='tempZone'
-            user='rods'
-     
-            newCollection = "/" + zone + "/home/" + user + "/testdir"
-            newDataObject = newCollection + "/testDataObject"
-
-            # Create new collection
-            coll = session.collections.create(newCollection)
-            assert coll.path == newCollection, "Error! Got wrong collection path"
-            assert len(coll.subcollections) == 0, "Error! Collection should not have any subcollections!"
-            assert len(coll.data_objects) == 0, "Error! Collection should not have any data objects!"
-
-            # Create data object
-            obj = session.data_objects.create(newDataObject)
-            assert obj.name == "testDataObject", "Error! Got wrong data object"
-
-            # Delete data object
-            obj.unlink(force=True)
-
-            # Get data object
-            with pytest.raises(DataObjectDoesNotExist):
-                obj = session.data_objects.get(newDataObject)
-
-            # Delete collection
-            coll.remove(recurse=True, force=True)
-
-            with pytest.raises(CollectionDoesNotExist):
-                coll = session.collections.get(newCollection)
- 
-    def test_populate_fetch_new_data_object(self):
-        with iRODSSession(host='localhost', 
-            port='1247', 
-            user='rods',  
-            password='rods', 
-            zone='tempZone') as session:
-
-            zone='tempZone'
-            user='rods'
-
-            newCollection = "/" + zone + "/home/" + user + "/testdir"
-            fileName = "irods_beginner_training_2016.pdf"
-            fileNamePath = os.path.join(SCRIPT_DIRECTORY, fileName)
-            backupFileName = "irods_beginner_training_2016_backup.pdf"
-            backupFileNamePath = os.path.join(SCRIPT_DIRECTORY, backupFileName)
- 
-            # Copy file from backup
-            shutil.copyfile(fileNamePath, backupFileNamePath)
-
-            # Create new collection 
-            coll = session.collections.create(newCollection)
-            assert coll.path == newCollection, "Error! Got wrong collection path"
-            assert len(coll.subcollections) == 0, "Error! Collection should not have any subcollections!"
-            assert len(coll.data_objects) == 0, "Error! Collection should not have any data objects!" 
-
-            # Create new data object
-            session.data_objects.put(backupFileNamePath, newCollection + "/" + backupFileName)
-    
-            # Delete local file
-            os.remove(backupFileNamePath)         
-    
-            # Get the data object back
-            obj = session.data_objects.get(newCollection + "/" + backupFileName)
-    
-            # Write the data object to the file system
-            with obj.open("r") as f_in, open(backupFileNamePath, "wb") as f_out:
-                f_out.write(f_in.read())
-    
-            # Assert the size of file fetched from iRODS matches the local backup copy
-            assert os.path.getsize(fileNamePath) == os.path.getsize(backupFileNamePath), "Error! Size of file fetched from iRODS does not match the size of the original file" 
-    
-            # Delete local file
-            os.remove(backupFileNamePath)         
-    
-            # Delete data object
-            obj.unlink(force=True)
-    
-            # Get data object
-            with pytest.raises(DataObjectDoesNotExist):
-                obj = session.data_objects.get(newCollection + "/" + backupFileName)
-    
-            # Delete collection
-            coll.remove(recurse=True, force=True)
-    
-            # Get collection
-            with pytest.raises(CollectionDoesNotExist):
-                coll = session.collections.get(newCollection)
+def test_instantiate_irods(setup_module):
+    print('**********************')
+    print('setup_module.to_dict()')
+    print(setup_module.to_dict())
+    print('**********************')
+    assert 1 == 1
