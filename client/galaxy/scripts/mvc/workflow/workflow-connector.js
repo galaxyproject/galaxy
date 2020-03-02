078e50c6e7ab858580d1323e35ef1b0fdaf6d4ba
guerler <aysam.guerler@gmail.com>
2020-03-02 11:39:25 -0500

    Use const/let in connector, restore curve loop, use d3

--- client/galaxy/scripts/mvc/workflow/workflow-connector.js
+++ client/galaxy/scripts/mvc/workflow/workflow-connector.js
@@ -46,9 +46,8 @@ $.extend(Connector.prototype, {
         const handle2 = this.handle2;
         const startRibbon = handle1 && handle1.isMappedOver();
         const endRibbon = handle2 && handle2.isMappedOver();
-        const canvasClass = `${startRibbon ? "start-ribbon" : ""} ${endRibbon ? "end-ribbon" : ""}`;
-        var canvas_container = $("#canvas-container");
-        var canvasZoom = this.manager.canvasZoom;
+        const canvas_container = $("#canvas-container");
+        const canvasZoom = this.manager.canvasZoom;
         if (this.dragging) {
             this.canvas.style.zIndex = 1000;
         }
@@ -60,35 +59,33 @@ $.extend(Connector.prototype, {
             "handle2-id",
             handle2 && handle2.element.getAttribute ? handle2.element.getAttribute("id") : ""
         );
-        var relativeLeft = e => ($(e).offset().left - canvas_container.offset().left) / canvasZoom;
-        var relativeTop = e => ($(e).offset().top - canvas_container.offset().top) / canvasZoom;
+        const relativeLeft = e => ($(e).offset().left - canvas_container.offset().left) / canvasZoom;
+        const relativeTop = e => ($(e).offset().top - canvas_container.offset().top) / canvasZoom;
         if (!handle1 || !handle2) {
             return;
         }
         // Find the position of each handle
-        var start_x = relativeLeft(handle1.element) + 5;
-        var start_y = relativeTop(handle1.element) + 5;
-        var end_x = relativeLeft(handle2.element) + 5;
-        var end_y = relativeTop(handle2.element) + 5;
+        let start_x = relativeLeft(handle1.element) + 5;
+        let start_y = relativeTop(handle1.element) + 5;
+        let end_x = relativeLeft(handle2.element) + 5;
+        let end_y = relativeTop(handle2.element) + 5;
 
         // Calculate canvas area
-        var canvas_extra = 100;
-        var canvas_min_x = Math.min(start_x, end_x);
-        var canvas_max_x = Math.max(start_x, end_x);
-        var canvas_min_y = Math.min(start_y, end_y);
-        var canvas_max_y = Math.max(start_y, end_y);
-        var cp_shift = Math.min(Math.max(Math.abs(canvas_max_y - canvas_min_y) / 2, 100), 300);
-        var canvas_left = canvas_min_x - canvas_extra;
-        var canvas_top = canvas_min_y - canvas_extra;
-        var canvas_width = canvas_max_x - canvas_min_x + 2 * canvas_extra;
-        var canvas_height = canvas_max_y - canvas_min_y + 2 * canvas_extra;
+        const canvas_extra = 100;
+        const canvas_min_x = Math.min(start_x, end_x);
+        const canvas_max_x = Math.max(start_x, end_x);
+        const canvas_min_y = Math.min(start_y, end_y);
+        const canvas_max_y = Math.max(start_y, end_y);
+        const canvas_left = canvas_min_x - canvas_extra;
+        const canvas_top = canvas_min_y - canvas_extra;
+        const canvas_width = canvas_max_x - canvas_min_x + 2 * canvas_extra;
+        const canvas_height = canvas_max_y - canvas_min_y + 2 * canvas_extra;
 
         // Place the canvas
         this.canvas.style.left = `${canvas_left}px`;
         this.canvas.style.top = `${canvas_top}px`;
         this.svg.style("width", `${canvas_width}px`);
         this.svg.style("height", `${canvas_height}px`);
-        this.canvas.setAttribute("class", canvasClass);
 
         // Adjust points to be relative to the canvas
         start_x -= canvas_left;
@@ -96,21 +93,14 @@ $.extend(Connector.prototype, {
         end_x -= canvas_left;
         end_y -= canvas_top;
 
-        this.draw_outlined_curve(
-            start_x,
-            start_y,
-            end_x,
-            end_y,
-            0,
-            0,
-            0,
-            0
-        );
+        // Determine line shift
+        const cp_factor = 10;
+        const cp_shift = Math.min(Math.max(Math.abs(canvas_max_y - canvas_min_y) / 2, cp_factor), 300 / cp_factor);
 
-        /*/ Draw the line
-        var start_offsets = null;
-        var end_offsets = null;
-        var num_offsets = 1;
+        // Draw the line
+        let start_offsets = null;
+        let end_offsets = null;
+        let num_offsets = 1;
         if (startRibbon) {
             start_offsets = [-6, -3, 0, 3, 6];
             num_offsets = 5;
@@ -123,16 +113,15 @@ $.extend(Connector.prototype, {
         } else {
             end_offsets = [0];
         }
-        var connector = this;
-        for (var i = 0; i < num_offsets; i++) {
-            var inner_width = 5;
-            var outer_width = 7;
+        for (let i = 0; i < num_offsets; i++) {
+            let inner_width = 5;
+            let outer_width = 7;
             if (start_offsets.length > 1 || end_offsets.length > 1) {
                 // We have a multi-run, using many lines, make them small.
                 inner_width = 1;
                 outer_width = 3;
             }
-            connector.draw_outlined_curve(
+            this.draw_outlined_curve(
                 start_x,
                 start_y,
                 end_x,
@@ -143,7 +132,7 @@ $.extend(Connector.prototype, {
                 start_offsets[i % start_offsets.length],
                 end_offsets[i % end_offsets.length]
             );
-        }*/
+        }
     },
     draw_outlined_curve: function(
         start_x,
@@ -158,21 +147,27 @@ $.extend(Connector.prototype, {
     ) {
         offset_start = offset_start || 0;
         offset_end = offset_end || 0;
-        var lineData = [
+        const lineData = [
             { x: start_x, y: start_y + offset_start },
-            //{ x: start_x + cp_shift,   y: start_y + offset_start},
-            //{ x: end_x - cp_shift,  y: end_y + offset_end },
+            { x: start_x + cp_shift,   y: start_y + offset_start},
+            { x: end_x - cp_shift,  y: end_y + offset_end },
             { x: end_x,  y: end_y + offset_end } ];
-        var lineFunction = d3.svg.line()
-                            .x(function(d) { return d.x; })
-                            .y(function(d) { return d.y; })
-                            .interpolate("linear");
+        const lineFunction = d3.svg.line()
+                                   .x(function(d) { return d.x; })
+                                   .y(function(d) { return d.y; })
+                                   .interpolate("basis");
         // The line SVG Path we draw
         this.svg.selectAll("*").remove();
         this.svg.append("path")
                 .attr("d", lineFunction(lineData))
-                .attr("stroke", "blue")
-                .attr("stroke-width", 2);
+                .attr("stroke", this.inner_color)
+                .attr("stroke-width", inner_width)
+                .attr("fill", "none");
+        this.svg.append("path")
+                .attr("d", lineFunction(lineData))
+                .attr("stroke", this.outer_color)
+                .attr("stroke-width", outer_width)
+                .attr("fill", "none");
     }
 });
 export default Connector;
