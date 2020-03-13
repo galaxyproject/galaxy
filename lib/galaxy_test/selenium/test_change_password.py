40be7e409959d4317643c33142e33e62e0e39387
Juan David Mendez Solano <mendezj@tf.uni-freiburg.de>
2020-03-13 17:30:09 +0100

    Condense the code

--- lib/galaxy_test/selenium/test_change_password.py
+++ lib/galaxy_test/selenium/test_change_password.py
@@ -9,75 +9,47 @@ class ChangePasswordTestCase(SeleniumTestCase):
     def test_change_password(self):
         self.home()
         email = self._get_random_email()
-        password = self.default_password
-        confirm = password
-        username = email.split("@")[0]
-        self.register(email, password, username, confirm)
-        self.navigate_to_password()
-        self.fill_input_fields(password, "123123", "123123")
+        self.register(email)
+        self.click_masthead_user()
+        self.components.masthead.preferences.wait_for_and_click()
+        self.components.preferences.change_password.wait_for_and_click()
+        self.fill_input_fields(self.default_password, "123123", "123123")
         self.logout_if_needed()
         self.submit_login(email, "123123")
 
     @selenium_test
     def test_new_password_password(self):
-        self.home()
-        email = self._get_random_email()
-        password = self.default_password
-        confirm = password
-        username = email.split("@")[0]
-        self.register(email, password, username, confirm)
-        self.navigate_to_password()
-        self.fill_input_fields(password, "", "123123")
+        self.register_and_change_password()
+        self.fill_input_fields(self.default_password, "", "123123")
         self.assert_error_message(contains='Please provide a new password.')
 
     @selenium_test
     def test_no_password_confirmation(self):
-        self.home()
-        email = self._get_random_email()
-        password = self.default_password
-        confirm = password
-        username = email.split("@")[0]
-        self.register(email, password, username, confirm)
-        self.navigate_to_password()
-        self.fill_input_fields(password, "123123", "")
+        self.register_and_change_password()
+        self.fill_input_fields(self.default_password, "123123", "5444484")
         self.assert_error_message(contains='Passwords do not match.')
 
     @selenium_test
     def test_currect_password_incorrect(self):
-        self.home()
-        email = self._get_random_email()
-        password = self.default_password
-        confirm = password
-        username = email.split("@")[0]
-        self.register(email, password, username, confirm)
-        self.navigate_to_password()
+        self.register_and_change_password()
         self.fill_input_fields("4444444", "123123", "123123")
         self.assert_error_message(contains='Invalid current password.')
 
     @selenium_test
     def test_new_password_short(self):
-        self.home()
-        email = self._get_random_email()
-        password = self.default_password
-        confirm = password
-        username = email.split("@")[0]
-        self.register(email, password, username, confirm)
-        self.navigate_to_password()
-        self.fill_input_fields(password, "123", "123")
+        self.register_and_change_password()
+        self.fill_input_fields(self.default_password, "123", "123")
         self.assert_error_message(contains='Use a password of at least 6 characters.')
 
     @selenium_test
     def test_new_password_same_password(self):
-        self.home()
-        email = self._get_random_email()
+        self.register_and_change_password()
         password = self.default_password
-        confirm = password
-        username = email.split("@")[0]
-        self.register(email, password, username, confirm)
-        self.navigate_to_password()
         self.fill_input_fields(password, password, password)
 
-    def navigate_to_password(self):
+    def register_and_change_password(self):
+        self.home()
+        self.register()
         self.click_masthead_user()
         self.components.masthead.preferences.wait_for_and_click()
         self.components.preferences.change_password.wait_for_and_click()
