711e0e1de158b7aa12fde9d72b307f7756e10989
Ying Feng <yingfeng-iu@users.noreply.github.com>
2020-03-13 15:32:31 -0400

    AMP-562 / AMP-538 hmgm jira (#47)
    * AMP-562/AMP-538: initial tmp code
    
    * AMP-562/538: add tool wrapper
    
    * AMP-562/538
    - task manager
    - hmgm main
    
    * AMP-562/AMP-538:
    - task manager base class
    - Jira impl
    - HMGM wrapper
    
    * AMP-562/538
    - create/close jira
    - refactor
    -
    
    * AMP-562/538:
    - HMGM config
    - BBC transcript link
    
    * AMP-562: update for handling editor input/output
    
    * AMP-562: complete the extra logic for handling input/output files
    
    * AMP-562/AMP-538: a few corrections
    
    * AMP-562/AMP-538: fix various bugs
    
    * AMP-562: more bug fix
    
    * AMP-562: change param order for context_json
    
    * AMP-538: minor tool cmd change
    
    * AMP-538:
    - add '' around context in tool wraper xml to make sure it's passed to
    python script as one string
    - disable param sanitizer for context param.
    
    * AMP-562/AMP-538: fix logic for checking task_json created
    
    * AMP-562: fix editor input file not unique issue
    
    * AMP-562: minor fix on log
    
    * AMP-562/AMP-538: code review changes
    
    * AMP-562/AMP-538: fix url encode
    
    * AMP-562: add log
    
    * AMP-562: add flush to stdout
    
    * AMP-562: remove commented code

---
+++ tools/hmgm/task_jira.py
@@ -0,0 +1,67 @@
+import json
+from jira import JIRA
+
+from task_manager import TaskManager
+
+ 
+class TaskJira (TaskManager):
+     """Subclass of TaskManager implementing HMGM task management with Jira platforms."""
+     
+     
+     # Set up jira instance with properties in the given configuration instance.
+     def __init__(self, config):
+         super().__init__(config)
+         
+         # get Jira server info from the config 
+         jira_server = config["jira"]["server"]
+         jira_username = config["jira"]["username"]
+         jira_password = config["jira"]["password"]         
+         self.jira = JIRA(server = jira_server, basic_auth = (jira_username, jira_password))
+         
+         
+     # Create a jira issue given the task_type, context, input/output json, 
+     # save information about the created issue into a JSON file, and return the issue.
+     def create_task(self, task_type, context, editor_input, task_json):
+         # populate the jira fields into a dictionary with information from task_type and context etc
+         project = {"key": "HMGM"}
+         issuetype = {"name": "Task"}
+         labels = [task_type]
+         summary = context["primaryfileName"] + " - " + context["workflowName"] 
+         description = self.get_task_description(task_type, context, editor_input)         
+         jira_fields = {"project" : project, "issuetype": issuetype, "labels": labels, "summary": summary, "description": description}
+         
+         # create a new task jira using jira module
+         issue = self.jira.create_issue(fields = jira_fields)
+         
+         # extract important information (ID, key, and URL) of the created issue into a dictionary, which is essentially the response returned by Jira server
+         issue_dict = {"id": issue.id, "key": issue.key, "url": issue.permalink()} 
+
+         # write jira issue into task_json file to indicate successful creation of the task
+         with open(task_json, "w") as task_file:
+             json.dump(issue_dict, task_file)
+                  
+         return issue
+     
+          
+     # Close the jira issue specified in task_json by updating its status and relevant fields, and return the issue.          
+     def close_task(self, task_json):
+         # read jira issue info from task_json into a dictionary
+         with open(task_json, 'r') as task_file:
+             issue_dict = json.load(task_file)
+         
+         # get the jira issue using id
+         issue = self.jira.issue(issue_dict["id"])
+         
+         # retrieve transition ID based on name = Done instead of hard coding it, if the ID might be different
+         transitions = self.jira.transitions(issue)
+         for t in transitions:
+             if t["name"] == "Done":    # Done is the status when an issue is closed
+                 id = t["id"]
+                 break
+             
+         # update the jira status to Done via transition
+         self.jira.transition_issue(issue, id)
+                  
+         return issue
+     
+     
---
+++ tools/hmgm/task_manager.py
@@ -0,0 +1,64 @@
+import urllib.parse
+
+
+class TaskManager:
+     """Abstract base class defining the API for all HMGM task management implementations, which are based on task management platforms."""
+     
+     TRANSCRIPT = "Transcript"
+     NER = "NER"
+     SEGMENTATION = "Segmentation"
+     OCR = "OCR"
+     
+     # Set up HMGM properties with the given configuration instance.
+     def __init__(self, config):
+         self.config = config    
+         self.amppd_server = config["amppd"]["server"] 
+         self.transcript_api = config["amppd"]["transcript_api"] 
+         self.transcript_input = config["amppd"]["transcript_input"] 
+         self.transcript_media = config["amppd"]["transcript_media"] 
+         
+         # TODO set up properties for other HMGM tools
+         
+     
+     # Return description of an HMGM task based on the given task_type, context, input file etc.
+     def get_task_description(self, task_type, context, editor_input):
+         description = "Submitted By: " + context["submittedBy"] + "\n"
+         description += "Unit " + context["unitId"] + ": " + context["unitName"] + "\n"
+         description += "Collection  " + context["collectionId"] + ": " + context["collectionName"] + "\n"
+         description += "Item " + context["itemId"] + ": " + context["itemName"] + "\n"
+         description += "Primary File " + context["primaryfileId"] + ": " + context["primaryfileName"] + "\n"
+         description += "Workflow " + context["workflowId"] + ": " + context["workflowName"] + "\n"              
+         description += "Editor URL: " + self.get_editor_url(task_type, context["primaryfileUrl"], editor_input)          
+         return description
+     
+     
+     # Return the URL link to the editor tool for an HMGM task based on the given task_type, media, input file etc.
+     def get_editor_url(self, task_type, media, editor_input):
+         assert task_type in (self.TRANSCRIPT, self.NER, self.SEGMENTATION, self.OCR)
+         
+         if task_type == self.TRANSCRIPT:
+             api_url = self.amppd_server + self.transcript_api 
+             params = {self.transcript_input: editor_input, self.transcript_media: media}
+             url = api_url + "?" + urllib.parse.urlencode(params)
+         elif task_type == self.NER:
+             # TODO url for NER
+             url = self.amppd_server;
+         elif task_type == self.SEGMENTATION:
+             # TODO url for SEGMENTATION
+             url = self.amppd_server
+         elif task_type == self.OCR:
+             # TODO url for OCR
+             url = self.amppd_server
+
+         return url
+     
+
+     # Abstract method to create a task in the designated task management platform, given the task_type, context, input file etc. 
+     # save information about the created task into a JSON file, and return the task instance.
+     def create_task(self, task_type, context, editor_input, task_json):
+         raise Exception("Method TaskManager.create_task is an abstract method, please call an implemented version from a subclass.")
+     
+     
+     # Abstract method to close the task specified in task_json by updating its status and relevant fields, and return the task instance.          
+     def close_task(self, task_json):
+         raise Exception("Method TaskManager.close_task is an abstract method, please call an implemented version from a subclass.")
