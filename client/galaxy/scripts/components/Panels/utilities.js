7dbd0bb0a9af0bf97c34f3f3ac84f31360fe2b1f
Juleen Graham <juleengrah@gmail.com>
2020-03-19 13:06:27 -0400

    Allow tools search results to show in ranked order

--- client/galaxy/scripts/components/Panels/utilities.js
+++ client/galaxy/scripts/components/Panels/utilities.js
@@ -1,5 +1,6 @@
 export function filterToolSections(layout, results) {
     if (results) {
+        //Goes through each category and filters out tools that were not in results
         const filteredLayout = layout.map((category) => {
             return {
                 ...category,
@@ -10,11 +11,29 @@ export function filterToolSections(layout, results) {
                     }),
             };
         });
+        //Filters out to only display categories that have tools that were in results
         return filteredLayout.filter((category) => {
             const isSection = category.elems && category.elems.length > 0;
             const isMatchedTool = !category.text && results.includes(category.id);
             return isSection || isMatchedTool;
         });
+    } else {
+        return layout;
+    }
+}
+
+export function filterTools(layout, results) {
+    if (results) {
+        var toolsResults = [results.length];
+
+        layout.map((category) => {
+            category.elems.forEach((el) => {
+                if (!el.text && results.includes(el.id)) {
+                    toolsResults[results.indexOf(el.id)] = el;
+                }
+            });
+        });
+        return toolsResults;
     } else {
         return layout;
     }
