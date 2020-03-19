1d4e511d324aef4322364170de1042b5f888cdad
Anup Kumar <anup.rulez@gmail.com>
2020-03-19 15:47:12 +0100

    Fix v-else

--- client/galaxy/scripts/components/ToolRecommendation.vue
+++ client/galaxy/scripts/components/ToolRecommendation.vue
@@ -8,7 +8,7 @@
             tool than a tool with a lower score. Please click on one of the following/recommended tools to open its
             definition.
         </div>
-        <div v-else-if="deprecated" class="warningmessagelarge">
+        <div v-else class="warningmessagelarge">
             You have used {{ getToolId }} tool. {{ deprecatedMessage }}
         </div>
     </div>
