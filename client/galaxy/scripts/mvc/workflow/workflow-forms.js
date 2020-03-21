e4cd1f2cd17480002806b5afdb62c9c0f491a9d6
guerler <aysam.guerler@gmail.com>
2020-03-21 08:59:39 -0400

    Strip jquery from workflow forms helper class, use axios

--- client/galaxy/scripts/mvc/workflow/workflow-forms.js
+++ client/galaxy/scripts/mvc/workflow/workflow-forms.js
@@ -1,4 +1,4 @@
-import $ from "jquery";
+import axios from "axios";
 import { getAppRoot } from "onload/loadConfig";
 import { getGalaxyInstance } from "app";
 import _l from "utils/localization";
@@ -13,78 +13,71 @@ export class DefaultForm {
         var node = options.node;
         this.workflow = options.workflow;
         _addLabelAnnotation(this, node);
-        this.form = new Form(
-            Object.assign(options, {
-                onchange: function() {
-                    Utils.request({
-                        type: "POST",
-                        url: `${getAppRoot()}api/workflows/build_module`,
-                        data: {
-                            id: node.id,
-                            type: node.type,
-                            content_id: node.content_id,
-                            inputs: self.form.data.create(),
-                        },
-                        success: function (data) {
-                            node.update_field_data(data);
-                        },
-                    });
-                },
-            })
-        );
+        this.form = new Form({
+            ...options,
+            onchange() {
+                axios.post(`${getAppRoot()}api/workflows/build_module`, {
+                    id: node.id,
+                    type: node.type,
+                    content_id: node.content_id,
+                    inputs: self.form.data.create()
+                })
+                .then(response => {
+                    const data = response.data;
+                    node.update_field_data(data);
+                });
+            }
+        });
     }
 }
 
 /** Tool form wrapper for the workflow editor. */
 export class ToolForm {
     constructor(options) {
-        var self = this;
-        var node = options.node;
+        const self = this;
+        const node = options.node;
         this.workflow = options.workflow;
         this.datatypes = options.datatypes;
         this._customize(node);
-        this.form = new ToolFormBase(
-            Object.assign(node.config_form, {
-                text_enable: "Set in Advance",
-                text_disable: "Set at Runtime",
-                narrow: true,
-                initial_errors: true,
-                cls: "ui-portlet-section",
-                postchange: function(process, form) {
-                    const Galaxy = getGalaxyInstance();
-                    var options = form.model.attributes;
-                    var current_state = {
-                        tool_id: options.id,
-                        tool_version: options.version,
-                        type: "tool",
-                        inputs: $.extend(true, {}, form.data.create()),
-                    };
-                    Galaxy.emit.debug("tool-form-workflow::postchange()", "Sending current state.", current_state);
-                    Utils.request({
-                        type: "POST",
-                        url: `${getAppRoot()}api/workflows/build_module`,
-                        data: current_state,
-                        success: function(data) {
-                            self._customize(data);
-                            form.model.set(data.config_form);
-                            form.update(data.config_form);
-                            form.errors(data.config_form);
-                            // This hasn't modified the workflow, just returned
-                            // module information for the tool to update the workflow
-                            // state stored on the client with. User needs to save
-                            // for this to take effect.
-                            node.update_field_data(data);
-                            Galaxy.emit.debug("tool-form-workflow::postchange()", "Received new model.", data);
-                            process.resolve();
-                        },
-                        error: function (response) {
-                            Galaxy.emit.debug("tool-form-workflow::postchange()", "Refresh request failed.", response);
-                            process.reject();
-                        },
+        this.form = new ToolFormBase({
+            ...node.config_form,
+            text_enable: "Set in Advance",
+            text_disable: "Set at Runtime",
+            narrow: true,
+            initial_errors: true,
+            cls: "ui-portlet-section",
+            postchange(process, form) {
+                const Galaxy = getGalaxyInstance();
+                const options = form.model.attributes;
+                const current_state = {
+                    tool_id: options.id,
+                    tool_version: options.version,
+                    type: "tool",
+                    inputs: Object.assign({}, form.data.create())
+                };
+                Galaxy.emit.debug("tool-form-workflow::postchange()", "Sending current state.", current_state);
+                axios
+                    .post(`${getAppRoot()}api/workflows/build_module`, current_state)
+                    .then(response => {
+                        const data = response.data;
+                        self._customize(data);
+                        self.form.model.set(data.config_form);
+                        self.form.update(data.config_form);
+                        self.form.errors(data.config_form);
+                        // This hasn't modified the workflow, just returned
+                        // module information for the tool to update the workflow
+                        // state stored on the client with. User needs to save
+                        // for this to take effect.
+                        node.update_field_data(data);
+                        Galaxy.emit.debug("tool-form-workflow::postchange()", "Received new model.", data);
+                        process.resolve();
+                    })
+                    .catch(response => {
+                        Galaxy.emit.debug("tool-form-workflow::postchange()", "Refresh request failed.", response);
+                        process.reject();
                     });
-                },
-            })
-        );
+            }
+        });
     }
     _customize(node) {
         const inputs = node.config_form.inputs;
