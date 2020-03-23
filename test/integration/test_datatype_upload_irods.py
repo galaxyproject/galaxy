406662135859d565010b3e75fb154f994196f29b
Kaivan Kamali <kxk302@psu.edu>
2020-03-23 11:54:23 -0400

    Ran flake8 linter

--- test/integration/test_datatype_upload_irods.py
+++ test/integration/test_datatype_upload_irods.py
@@ -5,9 +5,10 @@ import pytest
 
 from galaxy.datatypes.registry import Registry
 from galaxy_test.driver import integration_util
-from .test_upload_configuration_options import BaseUploadContentConfigurationInstance
-from .test_datatype_upload import TEST_CASES
-from .test_datatype_upload import upload_datatype_helper
+
+from .test_datatype_upload import TEST_CASES, upload_datatype_helper
+from .test_upload_configuration_options import \
+    BaseUploadContentConfigurationInstance
 
 SCRIPT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 GALAXY_ROOT = os.path.abspath('%s/../../' % SCRIPT_DIRECTORY)
@@ -17,6 +18,7 @@ OBJECT_STORE_CONFIG_FILE = os.path.join(SCRIPT_DIRECTORY, "irods_object_store_co
 # Run test for only the first 10 test files
 IRODS_TEST_CASES = dict(list(TEST_CASES.items())[0:10])
 
+
 class UploadTestDatatypeDataTestCase(BaseUploadContentConfigurationInstance):
     framework_tool_and_types = False
     datatypes_conf_override = DATATYPES_CONFIG
@@ -25,15 +27,19 @@ class UploadTestDatatypeDataTestCase(BaseUploadContentConfigurationInstance):
     def handle_galaxy_config_kwds(cls, config):
         config["object_store_config_file"] = OBJECT_STORE_CONFIG_FILE
 
+
 instance = integration_util.integration_module_instance(UploadTestDatatypeDataTestCase)
+
 
 @pytest.fixture
 def temp_file():
     with tempfile.NamedTemporaryFile(delete=True, mode='wb') as fh:
         yield fh
 
+
 registry = Registry()
 registry.load_datatypes(root_dir=GALAXY_ROOT, config=DATATYPES_CONFIG)
+
 
 @pytest.mark.parametrize('test_data', IRODS_TEST_CASES.values(), ids=list(IRODS_TEST_CASES.keys()))
 def test_upload_datatype_auto(instance, test_data, temp_file):
