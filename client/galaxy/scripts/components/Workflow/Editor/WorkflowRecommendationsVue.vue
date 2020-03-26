8fd03451c4300cfaac310c6f0f56d69f03838934
Anup Kumar <anup.rulez@gmail.com>
2020-03-26 12:00:20 +0100

    Load data via ajax

--- client/galaxy/scripts/components/Workflow/Editor/WorkflowRecommendationsVue.vue
+++ client/galaxy/scripts/components/Workflow/Editor/WorkflowRecommendationsVue.vue
@@ -1,22 +1,31 @@
 <template>
-    <div class="modal ui-modal modal-tool-recommendation" style="display: block;">
+    <div class="modal ui-modal tool-recommendation-view" style="display: block;">
         <div class="modal-backdrop fade in">
         </div>
         <div class="modal-dialog" style="width: 260px;">
             <div class="modal-content">
-                <div class="modal-header" title="The recommended tools are shown in the decreasing order of their scores predicted using machine learning analysis on workflows. A tool with a higher score (closer to 100%) may fit better as the following tool than a tool with a lower score. Please click on one of the following/recommended tools to have it on the workflow editor.">
-                    <h4 class="title" tabindex="0">Recommended tools</h4>
+                <div class="modal-header" :title="modalHeaderToolTip">
+                    <h4 class="title" tabindex="0"> {{ modalHeaderTitle }}</h4>
                 </div>
                 <div class="modal-body" style="height: 280px; overflow: auto;">
-                    <i class="fa mr-1 fa-wrench"></i>
-                    <a href="#" title="Click to open the tool" class="pred-tool panel-header-button" id="Grep1">Select (100%)</a>
-                        
-                    <i class="fa mr-1 fa-wrench"></i>
-                    <a href="#" title="Click to open the tool" class="pred-tool panel-header-button" id="Grep1">Select (100%)</a>
+                    <div v-if="compatibleTools.length > 0 && !isDeprecated" >
+                        <span v-for="tool in compatibleTools">
+                            <i class="fa mr-1 fa-wrench"></i>
+                            <a href="#" title="Open tool" :id="tool.id" v-on:click="openTool(tool.id)"> {{ tool.name }} </a>
+                        </span>
+                        <br>
+                    </div>
+                    <div v-else-if="isDeprecated">
+                        {{ deprecatedMessage }}
+                    </div>
+                    <div v-else>
+                        {{ noRecommendationsMessage }}
+                    </div>
+                    
                 </div>
                 <div class="modal-footer">
                     <div class="buttons">
-                        <button id="button-0">Close</button>&nbsp;
+                        <button v-on:click="closeModal" title="Close">Close</button>
                     </div>
                 </div>
             </div>
@@ -41,6 +50,12 @@ export default {
     },
     data() {
         return {
+            modalHeaderTitle: _l("Tool Recommendations"),
+            modalHeaderToolTip: _l("The recommended tools are shown in the decreasing order of their scores predicted using machine learning analysis on workflows. A tool with a higher score (closer to 100%) may fit better as the following tool than a tool with a lower score. Please click on one of the following/recommended tools to have it on the workflow editor."),
+            compatibleTools: [],
+            isDeprecated: false,
+            noRecommendationsMessage: "",
+            deprecatedMessage: "",
         };
     },
     created() {
@@ -49,18 +64,53 @@ export default {
     computed: {
     },
     methods: {
-        loadRecommendations() {
+        async loadRecommendations() {
             console.log("loaded");
             console.log(this.workflowManager);
             console.log(this.toolSequence);
-            axios
-                .post(`${getAppRoot()}api/workflows/get_tool_predictions`, {
-                    tool_sequence: this.toolSequence,
-                })
-                .then((data) => {
-                    console.log(data);
-                });
+            const response = await axios.post(`${getAppRoot()}api/workflows/get_tool_predictions`, {tool_sequence: this.toolSequence});
+            const predictedData = response.data.predicted_data;
+            const outputDatatypes = predictedData.o_extensions;
+            const predictedDataChildren = predictedData.children;
+            const app = this.workflowManager.node.app;
+            this.isDeprecated = predictedData.is_deprecated;
+            this.deprecatedMessage = predictedData.message;
+            console.log(predictedData);
+            if (predictedDataChildren.length > 0) {
+                let cTools = [];
+                // filter results based on datatype compatibility
+                for (const [index, name_obj] of predictedDataChildren.entries()) {
+                    let t = {};
+                    let inputDatatypes = name_obj.i_extensions;
+                    for (const out_t of outputDatatypes.entries()) {
+                        for(const in_t of inputDatatypes.entries()) {
+                            if ((app.isSubType(out_t[1], in_t[1]) === true) ||
+                                 out_t[1] === "input" ||
+                                 out_t[1] === "_sniff_" ||
+                                 out_t[1] === "input_collection") {
+                                t.id = name_obj.tool_id
+                                t.name = name_obj.name;
+                                cTools.push(t);
+                                    break
+                            }
+                        }
+                    }
+                }
+                console.log(cTools);
+                console.log(this);
+                this.compatibleTools = cTools;
+            }
+            else {
+                this.noRecommendationsMessage = "No tool recommendations";
+            }
         },
+        closeModal () {
+            this.$el.remove();
+        },
+        openTool (tId) {
+            console.log(tId);
+            this.closeModal();
+        }
     },
 };
 </script>
