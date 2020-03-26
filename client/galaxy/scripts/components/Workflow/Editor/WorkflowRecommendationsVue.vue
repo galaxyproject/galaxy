b309ac896607874cc279ce93114e1911ed8d5464
Anup Kumar <anup.rulez@gmail.com>
2020-03-26 18:25:09 +0100

    Fix prettier

--- client/galaxy/scripts/components/Workflow/Editor/WorkflowRecommendationsVue.vue
+++ client/galaxy/scripts/components/Workflow/Editor/WorkflowRecommendationsVue.vue
@@ -1,22 +1,23 @@
 <template>
     <div class="modal ui-modal wf-recommendation-view">
-        <div class="modal-backdrop fade in">
-        </div>
+        <div class="modal-backdrop fade in"></div>
         <div class="modal-dialog wf-tools-dialog">
             <div class="modal-content wf-tool-content">
                 <div class="modal-header wf-tools-background" :title="modalHeaderToolTip">
-                    <h4> {{ modalHeaderTitle }}</h4>
+                    <h4>{{ modalHeaderTitle }}</h4>
                 </div>
                 <div class="modal-body wf-tools-body">
                     <div>
                         {{ loadingMessage }}
                     </div>
-                    <div v-if="compatibleTools.length > 0 && !isDeprecated" >
+                    <div v-if="compatibleTools.length > 0 && !isDeprecated">
                         <div v-for="tool in compatibleTools">
                             <i class="fa mr-1 fa-wrench"></i>
-                            <a href="#" title="Open tool" :id="tool.id" v-on:click="createTool(tool.id)"> {{ tool.name }} </a>
+                            <a href="#" title="Open tool" :id="tool.id" v-on:click="createTool(tool.id)">
+                                {{ tool.name }}
+                            </a>
                         </div>
-                        <br>
+                        <br />
                     </div>
                     <div v-else-if="isDeprecated">
                         {{ deprecatedMessage }}
@@ -24,7 +25,6 @@
                     <div v-else>
                         {{ noRecommendationsMessage }}
                     </div>
-                    
                 </div>
                 <div class="modal-footer">
                     <div class="buttons">
@@ -37,7 +37,6 @@
 </template>
 
 <script>
-
 import { getAppRoot } from "onload/loadConfig";
 import axios from "axios";
 import { getModule } from "./services";
@@ -46,7 +45,7 @@ import _l from "utils/localization";
 export default {
     props: {
         workflowManager: {
-            type: Object,
+            type: Object
         },
         toolSequence: {
             type: String
@@ -55,7 +54,9 @@ export default {
     data() {
         return {
             modalHeaderTitle: _l("Tool Recommendations"),
-            modalHeaderToolTip: _l("The recommended tools are shown in the decreasing order of their scores predicted using machine learning analysis on workflows. A tool with a higher score (closer to 100%) may fit better as the following tool than a tool with a lower score. Please click on one of the following/recommended tools to have it on the workflow editor."),
+            modalHeaderToolTip: _l(
+                "The recommended tools are shown in the decreasing order of their scores predicted using machine learning analysis on workflows. A tool with a higher score (closer to 100%) may fit better as the following tool than a tool with a lower score. Please click on one of the following/recommended tools to have it on the workflow editor."
+            ),
             compatibleTools: [],
             isDeprecated: false,
             noRecommendationsMessage: "",
@@ -66,11 +67,12 @@ export default {
     created() {
         this.loadRecommendations();
     },
-    computed: {
-    },
+    computed: {},
     methods: {
         async loadRecommendations() {
-            const response = await axios.post(`${getAppRoot()}api/workflows/get_tool_predictions`, {tool_sequence: this.toolSequence});
+            const response = await axios.post(`${getAppRoot()}api/workflows/get_tool_predictions`, {
+                tool_sequence: this.toolSequence
+            });
             const predictedData = response.data.predicted_data;
             const outputDatatypes = predictedData.o_extensions;
             const predictedDataChildren = predictedData.children;
@@ -83,12 +85,14 @@ export default {
                     let t = {};
                     const inputDatatypes = name_obj.i_extensions;
                     for (const out_t of outputDatatypes.entries()) {
-                        for(const in_t of inputDatatypes.entries()) {
-                            if ((app.isSubType(out_t[1], in_t[1]) === true) ||
-                                 out_t[1] === "input" ||
-                                 out_t[1] === "_sniff_" ||
-                                 out_t[1] === "input_collection") {
-                                t.id = name_obj.tool_id
+                        for (const in_t of inputDatatypes.entries()) {
+                            if (
+                                app.isSubType(out_t[1], in_t[1]) === true ||
+                                out_t[1] === "input" ||
+                                out_t[1] === "_sniff_" ||
+                                out_t[1] === "input_collection"
+                            ) {
+                                t.id = name_obj.tool_id;
                                 t.name = name_obj.name;
                                 cTools.push(t);
                                 break;
@@ -98,27 +102,26 @@ export default {
                 }
                 this.loadingMessage = "";
                 this.compatibleTools = cTools;
-            }
-            else {
+            } else {
                 this.noRecommendationsMessage = "No tool recommendations";
             }
         },
-        closeModal () {
+        closeModal() {
             this.$el.remove();
         },
-        createTool (tId) {
-            const app = this.workflowManager.node.app;         
+        createTool(tId) {
+            const app = this.workflowManager.node.app;
             const requestData = {
                 type: "tool",
                 tool_id: tId,
-                _: "true",
+                _: "true"
             };
-            getModule(requestData).then((response) => {
+            getModule(requestData).then(response => {
                 const node = app.create_node("tool", response.name, tId);
                 app.set_node(node, response);
                 this.closeModal();
             });
-        },
-    },
+        }
+    }
 };
 </script>
--- client/galaxy/scripts/components/Workflow/Editor/workflowRecommendations.js
+++ client/galaxy/scripts/components/Workflow/Editor/workflowRecommendations.js
@@ -11,64 +11,64 @@ function getToolId(toolId) {
 }
 
 function getWorkflowPath(wfSteps, currentNodeId) {
-        let steps = {};
-        let stepNames = {};
-        for (const stpIdx in wfSteps.steps) {
-            const step = wfSteps.steps[stpIdx];
-            const inputConnections = step.inputConnections;
-            stepNames[step.id] = getToolId(step.content_id);
-            for (const icIdx in inputConnections) {
-                const ic = inputConnections[icIdx];
-                if(ic !== null && ic !== undefined) {
-                    let prevConn = [];
-                    for (const conn of ic) {
-                        prevConn.push(conn.id.toString());
+    let steps = {};
+    let stepNames = {};
+    for (const stpIdx in wfSteps.steps) {
+        const step = wfSteps.steps[stpIdx];
+        const inputConnections = step.inputConnections;
+        stepNames[step.id] = getToolId(step.content_id);
+        for (const icIdx in inputConnections) {
+            const ic = inputConnections[icIdx];
+            if (ic !== null && ic !== undefined) {
+                let prevConn = [];
+                for (const conn of ic) {
+                    prevConn.push(conn.id.toString());
+                }
+                steps[step.id.toString()] = prevConn;
+            }
+        }
+    }
+    // recursive call to determine path
+    function readPaths(nodeId, ph) {
+        for (const st in steps) {
+            if (parseInt(st) === parseInt(nodeId)) {
+                const parentId = parseInt(steps[st][0]);
+                if (parentId !== undefined && parentId !== null) {
+                    ph.push(parentId);
+                    if (steps[parentId] !== undefined && steps[parentId] !== null) {
+                        readPaths(parentId, ph);
                     }
-                    steps[step.id.toString()] = prevConn;
                 }
             }
         }
-        // recursive call to determine path
-        function readPaths(nodeId, ph) {
-            for (const st in steps) {
-                if (parseInt(st) === parseInt(nodeId)) {
-                    const parentId = parseInt(steps[st][0]);
-                    if (parentId !== undefined && parentId !== null) {
-                        ph.push(parentId);
-                        if (steps[parentId] !== undefined && steps[parentId] !== null) {
-                            readPaths(parentId, ph);
-                        }
-                    }
-                }
-            }
-            return ph;
+        return ph;
+    }
+    let ph = [];
+    let stepNamesList = [];
+    ph.push(currentNodeId);
+    ph = readPaths(currentNodeId, ph);
+    for (const sIdx of ph) {
+        const sName = stepNames[sIdx.toString()];
+        if (sName !== undefined && sName !== null) {
+            stepNamesList.push(sName);
         }
-        let ph = [];
-        let stepNamesList = [];
-        ph.push(currentNodeId);
-        ph = readPaths(currentNodeId, ph);
-        for (const sIdx of ph) {
-            const sName = stepNames[sIdx.toString()];
-            if (sName !== undefined && sName !== null) {
-                stepNamesList.push(sName);
-            }
-        }
-        return stepNamesList.join(",");
+    }
+    return stepNamesList.join(",");
 }
 
 export function getToolRecommendations(props) {
-        const workflowSimple = props.node.app.to_simple();
-        const node = props.node;
-        const toolId = getToolId(node.content_id);
-        const toolSequence = getWorkflowPath(workflowSimple, node.id);
-        const ToolRecommendationInstance = Vue.extend(WorkflowToolRecommendations);
-        const vm = document.createElement("div");
-        $("body").append(vm);
-        const instance = new ToolRecommendationInstance({
-             propsData: {
-                 workflowManager: props,
-                 toolSequence: toolSequence
-             },
-        });
-        instance.$mount(vm);
-    }
+    const workflowSimple = props.node.app.to_simple();
+    const node = props.node;
+    const toolId = getToolId(node.content_id);
+    const toolSequence = getWorkflowPath(workflowSimple, node.id);
+    const ToolRecommendationInstance = Vue.extend(WorkflowToolRecommendations);
+    const vm = document.createElement("div");
+    $("body").append(vm);
+    const instance = new ToolRecommendationInstance({
+        propsData: {
+            workflowManager: props,
+            toolSequence: toolSequence
+        }
+    });
+    instance.$mount(vm);
+}
