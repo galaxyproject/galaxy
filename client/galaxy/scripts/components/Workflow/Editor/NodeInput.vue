b1570851cad1ba1057fe26f6bbd8c2eb379d4894
guerler <aysam.guerler@gmail.com>
2020-03-30 17:45:13 -0400

    Add node input vue component

---
+++ client/galaxy/scripts/components/Workflow/Editor/NodeInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="form-row dataRow input-data-row">
+        {{ label }}
+        <div ref="terminal" class="terminal input-terminal"/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        input: {
+            type: Object,
+            required: true,
+        },
+        manager: {
+            type: Object,
+            required: true,
+        },
+    },
+    computed: {
+        label() {
+            return this.input.label || this.input.name;
+        },
+    }
+};
+</script>
