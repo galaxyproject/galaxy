375be4d8512e4ac84c265d6f4658fa4e4bca3805
Kaivan Kamali <kxk302@psu.edu>
2020-04-03 11:35:31 -0400

    Removed config file and put configuration in test.

--- test/integration/test_datatype_upload_irods.py
+++ test/integration/test_datatype_upload_irods.py
@@ -1,5 +1,6 @@
 
 import os
+import string
 import subprocess
 import tempfile
 import time
@@ -10,9 +11,25 @@ from galaxy_test.driver import integration_util
 from .test_datatype_upload import (TEST_CASES, upload_datatype_helper, UploadTestDatatypeDataTestCase)
 
 SCRIPT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
-OBJECT_STORE_CONFIG_FILE = os.path.join(SCRIPT_DIRECTORY, "irods_object_store_conf.xml")
 # Run test for only the first 10 test files
 IRODS_TEST_CASES = dict(list(TEST_CASES.items())[0:10])
+OBJECT_STORE_HOST = os.environ.get('GALAXY_INTEGRATION_IRODS_OBJECT_STORE_HOST', 'localhost')
+OBJECT_STORE_PORT = int(os.environ.get('GALAXY_INTEGRATION_IRODS_OBJECT_STORE_PORT', 1247))
+OBJECT_STORE_USERNAME = os.environ.get('GALAXY_INTEGRATION_IRODS_OBJECT_STORE_USERNAME', 'rods')
+OBJECT_STORE_PASSWORD = os.environ.get('GALAXY_INTEGRATION_IRODS_OBJECT_STORE_PASSWORD', 'rods')
+OBJECT_STORE_RESOURCE = os.environ.get('GALAXY_INTEGRATION_IRODS_OBJECT_STORE_RESOURCE', 'demoResc')
+OBJECT_STORE_ZONE = os.environ.get('GALAXY_INTEGRATION_IRODS_OBJECT_STORE_ZONE', 'tempZone')
+OBJECT_STORE_CONFIG = string.Template("""
+<object_store type="irods">
+    <auth username="${username}" password="${password}"/>
+    <resource name="${resource}"/>
+    <zone name="${zone}"/>
+    <connection host="${host}" port="${port}"/>
+    <cache path="${temp_directory}/object_store_cache" size="1000"/>
+    <extra_dir type="job_work" path="${temp_directory}/job_working_directory_irods"/>
+    <extra_dir type="temp" path="${temp_directory}/tmp_irods"/>
+</object_store>
+""")
 
 
 def start_irods(container_name):
@@ -50,7 +67,28 @@ class UploadTestDatatypeDataTestCase(UploadTestDatatypeDataTestCase):
 
     @classmethod
     def handle_galaxy_config_kwds(cls, config):
-        config["object_store_config_file"] = OBJECT_STORE_CONFIG_FILE
+        temp_directory = cls._test_driver.mkdtemp()
+        cls.object_stores_parent = temp_directory
+        config_path = os.path.join(temp_directory, "object_store_conf.xml")
+        # This doesn't quite work yet, fails with extra_files_path
+        # config["metadata_strategy"] = "extended"
+        config["outpus_to_working_dir"] = True
+        config["retry_metadata_internally"] = False
+        with open(config_path, "w") as f:
+            f.write(
+                OBJECT_STORE_CONFIG.safe_substitute(
+                    {
+                        "temp_directory": temp_directory,
+                        "host": OBJECT_STORE_HOST,
+                        "port": OBJECT_STORE_PORT,
+                        "username": OBJECT_STORE_USERNAME,
+                        "password": OBJECT_STORE_PASSWORD,
+                        "resource": OBJECT_STORE_RESOURCE,
+                        "zone": OBJECT_STORE_ZONE,
+                    }
+                )
+            )
+        config["object_store_config_file"] = config_path
 
 
 instance = integration_util.integration_module_instance(UploadTestDatatypeDataTestCase)
