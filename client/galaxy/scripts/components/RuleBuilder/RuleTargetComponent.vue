fd1a5c21eb7babc0f81b136691afda46fa87c450
Assunta DeSanto <adesanto@lhup.edu>
2020-04-08 17:39:48 -0400

    refactored components of Rule Collection into separate vue files

---
+++ client/galaxy/scripts/components/RuleBuilder/RuleTargetComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    template: `<a class="rule-link dropdown-item" href="javascript:void(0)" :class="linkClassName" @click="builder.addNewRule(ruleType)">{{title}}</a>`,
+</template>
+
+<script>
+export default {
+    props: {
+        ruleType: {
+            type: String,
+            required: true,
+        },
+        builder: {
+            required: true,
+        },
+    },
+    computed: {
+        linkClassName() {
+            return "rule-link-" + this.ruleType.replace(/_/g, "-");
+        },
+        title() {
+            return RULES[this.ruleType].title;
+        },
+    },
+}
+</script>
