galaxyproject/galaxy

commit 8d9a56f68b77bf7b37224cc5cd7f081adcec8701

Integrate both CILogon and Custos using one list of idps
- run prettier
- remove commented code
- other minor changes (remove authenticationDuplicate exception class)

lib/galaxy/authnz/custos_authnz.py
@@ -7,11 +7,13 @@ from datetime import datetime, timedelta
 
 import jwt
 import requests
+import base64
 from oauthlib.common import generate_nonce
 from requests_oauthlib import OAuth2Session
 from six.moves.urllib.parse import quote
 
 from galaxy import util
+from galaxy import exceptions
 from galaxy.model import CustosAuthnzToken, User
 from ..authnz import IdentityProvider
 
@@ -27,6 +29,9 @@ class CustosAuthnz(IdentityProvider):
         self.config['url'] = oidc_backend_config['url']
         self.config['client_id'] = oidc_backend_config['client_id']
         self.config['client_secret'] = oidc_backend_config['client_secret']
+        if provider == 'custos':
+            #Keycloak client secret used to get token
+            self.config['iam_client_secret'] = oidc_backend_config['iam_client_secret']
         self.config['redirect_uri'] = oidc_backend_config['redirect_uri']
         self.config['ca_bundle'] = oidc_backend_config.get('ca_bundle', None)
         self.config['extra_params'] = {
@@ -41,14 +46,17 @@ class CustosAuthnz(IdentityProvider):
             self.config['token_endpoint'] = well_known_oidc_config['token_endpoint']
             self.config['userinfo_endpoint'] = well_known_oidc_config['userinfo_endpoint']
             self.config['end_session_endpoint'] = well_known_oidc_config['end_session_endpoint']
+        elif provider == 'cilogon':
+            self._load_config_for_cilogon()
+        elif provider == 'custos':
+            self._load_config_for_custos()
         else:
             realm = oidc_backend_config['realm']
             self._load_config_for_provider_and_realm(self.config['provider'], realm)
 
-    #edit here Juleen for cilogon state?
     def authenticate(self, trans, idphint=None):
-        base_authorize_url = self._create_cilogon_authorize_url(idphint)
-        oauth2_session = self._create_oauth2_session(scope=('openid', 'email', 'profile', 'org.cilogon.userinfo')) #maybe set state to be state_cookie like in callback
+        base_authorize_url = self._create_authorize_url(trans)
+        oauth2_session = self._create_oauth2_session(scope=('openid', 'email', 'profile', 'org.cilogon.userinfo'))
         nonce = generate_nonce()
         nonce_hash = self._hash_nonce(nonce)
         extra_params = {"nonce": nonce_hash}
@@ -73,8 +81,7 @@ class CustosAuthnz(IdentityProvider):
         access_token = token['access_token']
         id_token = token['id_token']
         refresh_token = token['refresh_token'] if 'refresh_token' in token else None
-        #expiration_time = datetime.now() + timedelta(seconds=token['expires_in'])
-        expiration_time = datetime.now() + timedelta(seconds=token.get('expires_in', 3600)) #revisit later Juleen, once CILogon bug fixed
+        expiration_time = datetime.now() + timedelta(seconds=token.get('expires_in', 3600))
         refresh_expiration_time = (datetime.now() + timedelta(seconds=token['refresh_expires_in'])) if 'refresh_expires_in' in token else None
 
         # Get nonce from token['id_token'] and validate. 'nonce' in the
@@ -85,11 +92,14 @@ class CustosAuthnz(IdentityProvider):
         self._validate_nonce(trans, nonce_hash)
 
         # Get userinfo and lookup/create Galaxy user record
-        userinfo = self._get_userinfo(oauth2_session)
+        if id_token_decoded['email']:
+            userinfo = id_token_decoded
+        else:
+            userinfo = self._get_userinfo(oauth2_session)
         log.debug("userinfo={}".format(json.dumps(userinfo, indent=True)))
         email = userinfo['email']
-        username = userinfo.get('preferred_username', email.split('@')[0]) #TBD handle is username already exists
-        #username = userinfo.get('preferred_username', self.generate_username(email)) #TBD handle is username already exists
+        #Check if username if already taken
+        username = userinfo.get('preferred_username', self._generate_username(trans, email))
         user_id = userinfo['sub']
 
         # Create or update custos_authnz_token record
@@ -112,7 +122,7 @@ class CustosAuthnz(IdentityProvider):
                     else:
                         message = 'There already exists a user this email.  To associate this external login, you must first be logged in as that existing account.'
                         log.exception(message)
-                        raise exceptions.AuthenticationDuplicate(message)
+                        raise exceptions.AuthenticationFailed(message)
                 else:
                     user = trans.app.user_manager.create(email=email, username=username)
                     user.set_random_password()
@@ -134,7 +144,7 @@ class CustosAuthnz(IdentityProvider):
             custos_authnz_token.refresh_expiration_time = refresh_expiration_time
         trans.sa_session.add(custos_authnz_token)
         trans.sa_session.flush()
-        return login_redirect_url, custos_authnz_token.user
+        return True, "", (login_redirect_url, custos_authnz_token.user)
 
     def disconnect(self, provider, trans, disconnect_redirect_url=None):
         try:
@@ -176,21 +186,24 @@ class CustosAuthnz(IdentityProvider):
         return session
 
     def _fetch_token(self, oauth2_session, trans):
-        client_secret = self.config['client_secret']
-
-        idsec = self.config['client_id'] + ":" + self.config['client_secret']
-        encodedIdSec = base64.b64encode(idsec)
+        if self.config.get('iam_client_secret'):
+            #Custos uses the Keycloak client secret to get the token
+            client_secret = self.config['iam_client_secret']
+        else:
+            client_secret = self.config['client_secret']
         token_endpoint = self.config['token_endpoint']
+        clientIdAndSec = self.config['client_id'] + ":" + self.config['client_secret'] #for custos
         return oauth2_session.fetch_token(
             token_endpoint,
             client_secret=client_secret,
-            authorization_response=trans.request.url,
+            authorization_response=trans.request.url, 
+            headers={"Authorization": "Basic %s" % base64.b64encode(clientIdAndSec)}, #for custos
             verify=self._get_verify_param())
 
     def _get_userinfo(self, oauth2_session):
         userinfo_endpoint = self.config['userinfo_endpoint']
         return oauth2_session.get(userinfo_endpoint,
-                                  verify=self._get_verify_param()).json()                        
+                                  verify=self._get_verify_param()).json()
 
     def _get_custos_authnz_token(self, sa_session, user_id, provider):
         return sa_session.query(CustosAuthnzToken).filter_by(
@@ -206,16 +219,28 @@ class CustosAuthnz(IdentityProvider):
         nonce_cookie_hash = self._hash_nonce(nonce_cookie)
         if nonce_hash != nonce_cookie_hash:
             raise Exception("Nonce mismatch!")
-
+    
     def _load_config_for_cilogon (self):
         #set cilogon endpoints
         self.config['authorization_endpoint'] = "https://cilogon.org/authorize"
         self.config['token_endpoint'] = "https://cilogon.org/oauth2/token"
         self.config['userinfo_endpoint'] = "https://cilogon.org/oauth2/userinfo"
 
-    def _create_cilogon_authorize_url (self, idphint):
-        return "{}/?idphint={}&response_type=code&client_id={}&redirect_uri={}".format(
-            self.config['authorization_endpoint'], self.config['url'], idphint, self.config['client_id'], self.config['redirect_uri'])
+    def _load_config_for_custos (self):
+        #set custos endpoints
+        clientIdAndSec = self.config['client_id'] + ":" + self.config['client_secret']
+        eps = requests.get("https://custos.scigap.org:32036/identity-management/v1.0.0/.well-known/openid-configuration", 
+                            headers={"Authorization": "Basic %s" % base64.b64encode(clientIdAndSec)},
+                            verify=False, params = {'client_id': self.config['client_id']})
+        endpoints = eps.json()
+        self.config['authorization_endpoint'] = endpoints['authorization_endpoint']
+        self.config['token_endpoint'] = endpoints['token_endpoint']
+        self.config['userinfo_endpoint'] = "https://custos.scigap.org:32036/user-management/v1.0.0/user"
+        self.config['end_session_endpoint'] = endpoints['end_session_endpoint']
+
+    def _create_authorize_url (self, trans):
+        return "{}/?kc_idp_hint=oidc&response_type=code&client_id={}&redirect_uri={}".format(
+                self.config['authorization_endpoint'], self.config['client_id'], self.config['redirect_uri'])
 
     def _load_config_for_provider_and_realm(self, provider, realm):
         self.config['well_known_oidc_config_uri'] = self._get_well_known_uri_for_provider_and_realm(provider, realm)
@@ -251,10 +276,15 @@ class CustosAuthnz(IdentityProvider):
         else:
             return self.config['verify_ssl']
 
-    #def generate_username(email):
-    #    temp_username = email.split('@')[0]
-    #    count = 0
+    def _generate_username(self, trans, email):
+        temp_username = email.split('@')[0] #username created from username portion of email
+        count = 0
+        existing_usernames = trans.sa_session.query(trans.app.model.User).filter_by(username=(temp_username)).first()
 
-    #    while (temp_username in database):
-    #        count += 1
-    #    return temp_username if count == 0 else temp_username + str(count) #do I have 
+        if (trans.sa_session.query(trans.app.model.User).filter_by(username=temp_username).first()):
+            #if username already exists in database, append integer and iterate until unique username found
+            while (trans.sa_session.query(trans.app.model.User).filter_by(username=(temp_username+str(count))).first()):
+                count += 1
+        else:
+            return temp_username
+        return temp_username + str(count)

client/galaxy/scripts/components/User/ExternalIdentities/ExternalIdentities.vue
@@ -7,7 +7,8 @@
                 variant="warning"
                 :show="errorMessage !== null"
                 @dismissed="errorMessage = null"
-            >{{ errorMessage }}</b-alert>
+                >{{ errorMessage }}</b-alert
+            >
 
             <hgroup class="external-id-title">
                 <h1>Manage External Identities</h1>
@@ -23,9 +24,7 @@
 
             <p>
                 See more information, including a list of supported identity providers,
-                <a
-                    href="https://galaxyproject.org/authnz/use/oidc/"
-                >here</a>.
+                <a href="https://galaxyproject.org/authnz/use/oidc/">here</a>.
             </p>
         </header>
 
@@ -67,8 +66,8 @@
                     @cancel="doomedItem = null"
                 >
                     <p>
-                        If you delete this identity, you will be logged out and need to reset your Galaxy password the next time you log in
-                        or reconnect to this third party identity.
+                        If you delete this identity, you will be logged out and need to reset your Galaxy password the
+                        next time you log in or reconnect to this third party identity.
                     </p>
                 </b-modal>
             </div>
@@ -79,17 +78,13 @@
                 variant="warning"
                 :show="errorMessage !== null"
                 @dismissed="errorMessage = null"
-            >{{ errorMessage }}</b-alert>
+                >{{ errorMessage }}</b-alert
+            >
         </div>
 
         <div class="external-subheading" v-if="enable_oidc">
             <h3>Connect Other External Identities</h3>
-            <b-button
-                v-for="idp in oidc_idps"
-                :key="idp"
-                class="d-block mt-3"
-                @click="submitOIDCLogin(idp)"
-            >
+            <b-button v-for="idp in oidc_idps" :key="idp" class="d-block mt-3" @click="submitOIDCLogin(idp)">
                 <i v-bind:class="oidc_idps_icons[idp]" />
                 Sign in with
                 {{ idp.charAt(0).toUpperCase() + idp.slice(1) }}
@@ -101,14 +96,19 @@
 <script>
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
+import Selectize from "vue2-selectize";
 import { getGalaxyInstance } from "app";
 import { getAppRoot } from "onload";
 import svc from "./service";
+import axios from "axios";
 import { logoutClick } from "layout/menu";
 
 Vue.use(BootstrapVue);
 
 export default {
+    components: {
+        Selectize
+    },
     data() {
         const galaxy = getGalaxyInstance();
         const oidc_idps = galaxy.config.oidc;
@@ -130,7 +130,10 @@ export default {
             errorMessage: null,
             enable_oidc: galaxy.config.enable_oidc,
             oidc_idps: oidc_idps,
-            oidc_idps_icons: oidc_idps_icons
+            oidc_idps_icons: oidc_idps_icons,
+            cilogon_idps: [],
+            selected: "",
+            cilogonSelected: false
         };
     },
     computed: {
@@ -147,6 +150,12 @@ export default {
             // This setter is here because vue-bootstrap modal
             // tries to set this property for unfathomable reasons
             set() {}
+        },
+        filtered_oidc_idps() {
+            return this.oidc_idps.filter(idp => idp != "cilogon");
+        },
+        cilogonShow() {
+            return this.oidc_idps.includes("cilogon") && this.cilogonSelected; //&& cilogon button clicked
         }
     },
     watch: {
@@ -157,16 +166,12 @@ export default {
     methods: {
         loadIdentities() {
             this.loading = true;
-            console.log("before loading");
-            console.log(this.items);
             svc.getIdentityProviders()
                 .then(results => {
                     this.items = results;
                 })
                 .catch(this.setError("Unable to load connected external identities."))
                 .finally(() => (this.loading = false));
-            console.log("after loading");
-            console.log(this.items);
         },
         onDisconnect(doomed) {
             this.doomedItem = doomed;
@@ -183,8 +188,10 @@ export default {
                     );
                 }
             } else {
-                this.setError("Before disconnecting this identity, you need to set your account password, "
-                + "in order to avoid being locked out of your account.");
+                this.setError(
+                    "Before disconnecting this identity, you need to set your account password, " +
+                        "in order to avoid being locked out of your account."
+                );
             }
         },
         disconnectID() {
@@ -204,8 +211,6 @@ export default {
                     this.removeItem(this.doomedItem);
                     this.doomedItem = null;
                 });
-            console.log("after disconnectID");
-            console.log(this.items);
         },
         disconnectAndReset() {
             // Disconnects the user's final ext id and logouts of current session
@@ -228,55 +233,22 @@ export default {
                     this.messageText = message || "Login failed for an unknown reason.";
                 });
         },
-        setError(msg) {
-            return err => {
-                this.errorMessage = msg;
-                console.warn(err);
-            };
-        }
-    },
-    created() {
-        this.loadIdentities();
-    }
-};
-
-/*
-    data() {
-
-        return {
-            login: null,
-            password: null,
-            url: null,
-            provider: null,
-            messageText: null,
-            messageVariant: null,
-            
-
-            allowUserCreation: galaxy.config.allow_user_creation,
-            session_csrf_token: galaxy.session_csrf_token,
-            
-        };
-    },
-    methods: {
-        toggleLogin: function() {
-            if (this.$root.toggleLogin) {
-                this.$root.toggleLogin();
-            }
-        },
-        submitGalaxyLogin: function(method) {
+        getCILogonIdps: function() {
             const rootUrl = getAppRoot();
+            axios.get(`${rootUrl}authnz/get_cilogon_idps`).then(response => {
+                this.cilogon_idps = response.data;
+                //List is originally sorted by OrganizationName which can be different from DisplayName
+                this.cilogon_idps.sort((a, b) => (a.DisplayName > b.DisplayName ? 1 : -1));
+            });
+        },
+        submitCILogon: function() {
+            const rootUrl = getAppRoot();
+
             axios
-                .post(`${rootUrl}user/login`, this.$data)
+                .post(`${rootUrl}authnz/cilogon/login/?idphint=${this.selected}`)
                 .then(response => {
-                    if (response.data.message && response.data.status) {
-                        alert(response.data.message);
-                    }
-                    if (response.data.expired_user) {
-                        window.location = `${rootUrl}root/login?expired_user=${response.data.expired_user}`;
-                    } else if (response.data.redirect) {
-                        window.location = encodeURI(response.data.redirect);
-                    } else {
-                        window.location = `${rootUrl}`;
+                    if (response.data.redirect_uri) {
+                        window.location = response.data.redirect_uri;
                     }
                 })
                 .catch(error => {
@@ -285,11 +257,17 @@ export default {
                     this.messageText = message || "Login failed for an unknown reason.";
                 });
         },
-        
-        */
-
-
-
+        setError(msg) {
+            return err => {
+                this.errorMessage = msg;
+                console.warn(err);
+            };
+        }
+    },
+    created() {
+        this.loadIdentities(), this.getCILogonIdps();
+    }
+};
 </script>
 
 <style lang="scss">
@@ -297,7 +275,7 @@ export default {
 @import "~bootstrap/scss/variables";
 @import "~bootstrap/scss/mixins";
 @import "~bootstrap/scss/utilities/spacing";
-
+@import "~selectize/dist/css/selectize.bootstrap3.css";
 @import "scss/theme/blue.scss";
 @import "scss/mixins";
 
@@ -339,11 +317,10 @@ export default {
                     color: $brand-primary;
                 }
             }
-
-            .cloudKeyHelp a {
-                @include fontawesome($fa-var-info-circle);
-            }
         }
+    }
+    .external-subheading {
+        margin-top: 1rem;
     }
 }
 
@@ -366,13 +343,11 @@ export default {
     form {
         @extend .my-3;
         @extend .pt-3;
-
-        // removes wierd double arrows on select
+        // removes weird double arrows on select
         .custom-select {
             background: none;
         }
-
-        // Undo butchering from base.css so side-by-side labels work
+        // Allow side-by-side labels to work
         .form-row {
             display: flex;
             input,
@@ -398,7 +373,6 @@ export default {
     }
 
     // icon menu
-
     .operations {
         list-style-type: none;
 
@@ -410,7 +384,6 @@ export default {
 }
 
 // Transitions
-
 .fade-enter-active,
 .fade-leave-active {
     transition: opacity 0.5s;
@@ -422,7 +395,6 @@ export default {
 }
 
 // Delete modal
-
 #disconnectIDModal {
     .modal-body {
         display: none;

client/galaxy/scripts/components/User/ExternalIdentities/service.js
@@ -1,12 +1,7 @@
-/**
- * Data retrieval/storage for the auth keys
- */
-
 import axios from "axios";
-import { /*Credential, IdentityProvider*/ } from "./model/index";
 import { getRootFromIndexLink } from "onload";
 
-const getUrl = path => getRootFromIndexLink() + path;
+const getUrl = (path) => getRootFromIndexLink() + path;
 
 export async function disconnectIdentity(doomed) {
     if (doomed) {
@@ -16,8 +11,6 @@ export async function disconnectIdentity(doomed) {
             throw new Error("Delete failure.");
         }
     }
-    
-    return;
 }
 
 // Memoize results (basically never changes)
@@ -29,7 +22,7 @@ export async function getIdentityProviders() {
     if (response.status != 200) {
         throw new Error("Unable to load connected external identities");
     }
-    identityProviders = response.data;//.map(IdentityProvider.create);
+    identityProviders = response.data;
     return identityProviders;
 }
 
@@ -46,7 +39,6 @@ export async function hasUsername() {
     const result = getCurrentUser();
     console.log(result.username);
     return getCurrentUser().username;
-    //return true;
 }
 
 export async function getCurrentUser() {
@@ -58,26 +50,9 @@ export async function getCurrentUser() {
     return response.data;
 }
 
-/*export async function getCredential(id) {
-    const url = getUrl("api/cloud/authz/${id}");
-    const response = await axios.get(url);
-    if (response.status != 200) {
-        throw new Error("Unexpected response loading key.");
-    }
-    return Credential.create(response.data);
-}
-
-async function saveOrUpdate(model) {
-    return model.id
-        ? axios.put(getUrl(`api/cloud/authz/${model.id}`), model)
-        : axios.post(getUrl("api/cloud/authz"), model);
-}*/
-
 export default {
-    //listIdentities,
-    //getCredential,
     saveIdentity,
     disconnectIdentity,
     getIdentityProviders,
-    hasUsername
+    hasUsername,
 };

commit 2231a1065ec4e600cc5868c3db840c524819d574

Fix custos auth bug introduced in 8d9a56f68b77bf7b37224cc5cd7f081adcec8701

lib/galaxy/authnz/custos_authnz.py
@@ -92,7 +92,7 @@ class CustosAuthnz(IdentityProvider):
         self._validate_nonce(trans, nonce_hash)
 
         # Get userinfo and lookup/create Galaxy user record
-        if id_token_decoded['email']:
+        if id_token_decoded.get('email', None):
             userinfo = id_token_decoded
         else:
             userinfo = self._get_userinfo(oauth2_session)