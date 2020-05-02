a3fb91b650e89deac8485aa7b17cc0c80b30da71
lawrence_nic <lawrence.nicastro1@gmail.com>
2020-05-02 19:08:59 -0400

    update to use arrow function and create variable for readability Co-authored-by: William Holden <theappdev@protonmail.com>

--- client/src/ui/editable-text.js
+++ client/src/ui/editable-text.js
@@ -75,15 +75,16 @@ $.fn.make_text_editable = function (config_dict) {
             input_elt = $("<input type='text'/>")
                 .attr({ value: $.trim(cur_text), size: num_cols })
                 .blur(e => {
-                     set_text($(e.currentTarget).val());
+                    const new_text = $(e.currentTarget).val(); 
+                    set_text(new_text);
                 })
-                .keyup(function(e){
+                .keyup((e) => {
                     if (e.keyCode === 27) {
                         // Escape key.
                         set_text(cur_text);
                     } else if (e.keyCode === 13) {
                         // Enter key.
-                        set_text($(this).val());
+                         $(e.currentTarget).trigger("blur");
                     }
 
                     // Do not propogate event to avoid unwanted side effects.
