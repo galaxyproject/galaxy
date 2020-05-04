dbf6b51d4a13254361af8e072ef660f899c8b8c9
M Bernt <m.bernt@ufz.de>
2020-05-04 11:38:46 +0200

    Apply suggestions from code review
    Co-authored-by: Marius van den Beek <m.vandenbeek@gmail.com>

--- lib/galaxy/tool_util/deps/commands.py
+++ lib/galaxy/tool_util/deps/commands.py
@@ -1,7 +1,6 @@
-import logging
+import warnings
 
 import galaxy.util.commands as commands
 
-log = logging.getLogger(__name__)
 
-log.warning("importing galaxy.tool_util.deps.commands is deprecated use galaxy.util.commands instead")
+warnings.warn("Importing galaxy.tool_util.deps.commands is deprecated, use galaxy.util.commands instead", DeprecationWarning)
