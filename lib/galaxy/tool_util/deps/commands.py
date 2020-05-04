a9d2c6f64632aaf74e10a8baf19e73f0f070d9e7
Matthias Bernt <m.bernt@ufz.de>
2020-05-04 11:22:46 +0200

    restore tool_util.deps.commands
    In https://github.com/galaxyproject/galaxy/pull/9544 tool_util.deps.commands
    was moved to utils.commands
    
    since this probaly breaks planemo we restore it such that it just
    imports from the new location logs a deprecation warning.

---
+++ lib/galaxy/tool_util/deps/commands.py
@@ -0,0 +1,7 @@
+import logging
+
+import galaxy.util.commands as commands
+
+log = logging.getLogger(__name__)
+
+log.warning("importing galaxy.tool_util.deps.commands is deprecated use galaxy.util.commands instead")
