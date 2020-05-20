9e9e7ffd88595d9050880aa06989a19d927a4d7c
Dannon Baker <dannon.baker@gmail.com>
2020-05-20 16:51:22 -0400

    Drop unused flushpromises here

--- client/galaxy/scripts/components/Toolshed/SearchList/Repositories.test.js
+++ client/galaxy/scripts/components/Toolshed/SearchList/Repositories.test.js
@@ -2,7 +2,6 @@ import { mount } from "@vue/test-utils";
 import Repositories from "./Repositories";
 import { __RewireAPI__ as rewire } from "./Repositories";
 import Vue from "vue";
-import flushPromises from "flush-promises";
 
 describe("Repositories", () => {
     beforeEach(() => {
