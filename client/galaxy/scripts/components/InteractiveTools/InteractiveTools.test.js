233e23c9b3b77bb3e7dad42b7b65c8a7e6860a5d
Dannon Baker <dannon.baker@gmail.com>
2020-05-20 15:39:35 -0400

    IT tool test cleanup, phrasing.

--- client/galaxy/scripts/components/InteractiveTools/InteractiveTools.test.js
+++ client/galaxy/scripts/components/InteractiveTools/InteractiveTools.test.js
@@ -2,7 +2,6 @@ import InteractiveTools from "./InteractiveTools";
 import { mount, createLocalVue } from "@vue/test-utils";
 import flushPromises from "flush-promises";
 import _l from "utils/localization";
-import Vue from "vue";
 import testInteractiveToolsResponse from "./testData/testInteractiveToolsResponse";
 
 import MockAdapter from "axios-mock-adapter";
@@ -12,7 +11,6 @@ describe("ToolsView/ToolsView.vue", () => {
     const localVue = createLocalVue();
     localVue.filter("localize", (value) => _l(value));
     let wrapper;
-    let emitted;
     let axiosMock;
 
     beforeEach(async () => {
@@ -26,7 +24,6 @@ describe("ToolsView/ToolsView.vue", () => {
                 },
             },
         });
-        emitted = wrapper.emitted();
         axiosMock.onGet("/api/entry_points?running=true").reply(200, testInteractiveToolsResponse);
         axiosMock.onPost("/interactivetool/list").reply(200, { status: "ok", message: "ok" });
         await flushPromises();
@@ -36,27 +33,24 @@ describe("ToolsView/ToolsView.vue", () => {
         axiosMock.restore();
     });
 
-    it("table should exist", async () => {
+    it("Interactive Tool Table renders", async () => {
         const table = wrapper.find("#workflow-table");
-        assert(table.exists() === true, "Interactive Tools table doesn't exist!");
+        assert(table.exists() === true, "Interactive Tools table doesn't exist");
     });
 
-    it("selected tool should disappear after stop button pressed", async () => {
+    it("IT should disappear after stop button pressed", async () => {
         function checkIfExists(tag, toolId) {
             return wrapper.find(tag + toolId).exists();
         }
         const toolId = testInteractiveToolsResponse[0].id;
         const tool = wrapper.vm.activeInteractiveTools.find((tool) => tool.id === toolId);
+        assert(checkIfExists("#link-", toolId) === true, "tool is not rendered");
+        assert(tool.marked === undefined || false, "tool was selected before click");
 
-        assert(checkIfExists("#link-", toolId) === true, "tool is not rendered!");
-        // ensure that the tool is not marked
-        assert(tool.marked === undefined || false, "tool was marked before click!");
-        // mark & delete tool
         const checkbox = wrapper.find("#checkbox-" + toolId);
         checkbox.trigger("click");
         await flushPromises();
-        // ensure that the tool is marked
-        assert(tool.marked === true, "clicking on checkbox did not mark corresponding tool!");
+        assert(tool.marked === true, "Clicking on checkbox did not select corresponding tool");
 
         const stopBtn = wrapper.find("#stopInteractiveTool");
         stopBtn.trigger("click");
@@ -64,8 +58,7 @@ describe("ToolsView/ToolsView.vue", () => {
         await flushPromises();
 
         const toolExists = wrapper.vm.activeInteractiveTools.includes((tool) => tool.id === toolId);
-        // ensure that the tool was deleted from the list
-        assert(toolExists === false, "tool was not deleted from the list!");
-        assert(checkIfExists("#link-", toolId) === false, "tool is rendered after deletion!");
+        assert(toolExists === false, "Interactive Tool was not deleted from the list");
+        assert(checkIfExists("#link-", toolId) === false, "Interactive Tool is still rendered after deletion");
     });
 });
