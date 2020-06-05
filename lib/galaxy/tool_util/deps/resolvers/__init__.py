5c10449c1409bd8491c19e95fcf93abe83bd25e2
Rupert Nash <r.nash@epcc.ed.ac.uk>
2020-06-05 20:09:24 +0100

    Add support for relative paths in dependency resolver mappings

--- lib/galaxy/tool_util/deps/resolvers/__init__.py
+++ lib/galaxy/tool_util/deps/resolvers/__init__.py
@@ -5,7 +5,7 @@ from abc import (
     abstractmethod,
     abstractproperty,
 )
-
+import os.path
 import six
 import yaml
 
@@ -89,9 +89,23 @@ class MappableDependencyResolver(object):
         mapping_files = dependency_manager.get_resolver_option(self, "mapping_files", explicit_resolver_options=kwds)
         mappings = []
         if mapping_files:
+            search_dirs = [os.getcwd()]
+            if isinstance(dependency_manager.default_base_path, str):
+                search_dirs.append(dependency_manager.default_base_path)
+
+            def candidates(path):
+                if os.path.isabs(path):
+                    yield path
+                else:
+                    for search_dir in search_dirs:
+                        yield os.path.join(search_dir, path)
+
             mapping_files = listify(mapping_files)
             for mapping_file in mapping_files:
-                mappings.extend(MappableDependencyResolver._mapping_file_to_list(mapping_file))
+                for full_path in candidates(mapping_file):
+                    if os.path.exists(full_path):
+                        mappings.extend(MappableDependencyResolver._mapping_file_to_list(full_path))
+                        break
         self._mappings = mappings
 
     @staticmethod
