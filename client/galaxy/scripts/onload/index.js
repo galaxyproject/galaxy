bf2df2ac0fb2107ae9b8b15f2da055e34ed2823f
Dannon Baker <dannon.baker@gmail.com>
2020-06-20 16:19:19 -0400

    Move publicPath setting to its own module

--- client/galaxy/scripts/onload/index.js
+++ client/galaxy/scripts/onload/index.js
@@ -6,15 +6,9 @@ import "bootstrap-tour";
 // Galaxy core styles
 import "scss/base.scss";
 
-// Set dynamically loaded webpack public path.
-// This is, by default, just `/static/dist`, but if galaxy is being served at a
-// prefix (e.g.  `<server>/galaxy/`), this must happen before any dynamic
-// bundle imports load, otherwise they will fail.
-import { getRootFromIndexLink } from "./getRootFromIndexLink";
-__webpack_public_path__ = `${getRootFromIndexLink().replace(/\/+$/, "")}/${__webpack_public_path__.replace(
-    /^\/+/,
-    ""
-)}`;
+// Set up webpack's public path; nothing to import but the module has side
+// effects fixing webpack globals.
+import "./publicPath";
 
 // Module exports appear as objects on window.config in the browser
 export { standardInit } from "./standardInit";
