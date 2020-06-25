11cef6de13c1f85a8c9ab5d2af77886f3bc1138b
Kaivan Kamali <kxk302@psu.edu>
2020-06-25 10:49:25 -0400

    Added a locust file to load test

---
+++ lib/galaxy_test/loadtest/locust.py
@@ -0,0 +1,24 @@
+from locust import HttpUser, task, between
+import subprocess
+
+# In order to run the load tests, the following environment variables must be set:
+#
+# GALAXY_TEST_EXTERNAL
+#   - This is the URL of server that is being load tested. Tests run against this server.
+#   - Example value: "http://127.0.0.1:8081"
+# GALAXY_CONFIG_MASTER_API_KEY
+#   - The value comes from 'master_api_key' in 'galaxy.yml' configuration file of the server that is being load tested
+#   - Example value = "abcdefg"
+
+
+class ObjectStoreLoadTest(HttpUser):
+    wait_time = between(1, 2)
+
+    # Relative path to Galaxy root folder
+    GALAXY_HOME = "../../.."
+    run_tests_script = "%s/run_tests.sh" % (GALAXY_HOME)
+
+    @task
+    def upload_download_delete_random_file(self):
+        command = subprocess.run(["sh", self.run_tests_script, "-api", "./lib/galaxy_test/api/test_tools_upload.py::ToolsUploadTestCase::test_upload_download_delete_of_randomly_generated_file"])
+        print("The exit code was %d" % command.returncode)
