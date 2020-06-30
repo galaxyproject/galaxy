7a00126ca331d4d45b938a800629a18f9c4ec377
dan <dan@portalmedia.com>
2020-06-29 22:03:21 -0500

    Testing length

--- tools/amp_json_schema/amp_stt_to_draftjs.py
+++ tools/amp_json_schema/amp_stt_to_draftjs.py
@@ -141,7 +141,7 @@ def main():
 					}
 
 					# Find the offset in the paragraph, starting with the last offset
-					lastOffset = len(this_transcript) + 1
+					lastOffset = len(this_transcript)
 
 					# Create the entity map listing
 					out_json['entityMap'][key] = {
