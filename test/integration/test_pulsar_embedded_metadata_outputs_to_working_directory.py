4907811f36b74a59f10d33e71b0825edd3e6a68c
Bjoern Gruening <bjoern.gruening@gmail.com>
2020-07-09 19:12:30 +0200

    add new test for pulsar, external-metadata and outputs_to_working_directory

---
+++ test/integration/test_pulsar_embedded_metadata_outputs_to_working_directory.py
@@ -0,0 +1,26 @@
+"""Integration tests for the Pulsar embedded runner with remote metadata and outputs_to_working_directory enabled."""
+
+import os
+
+from galaxy_test.driver import integration_util
+
+SCRIPT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
+EMBEDDED_PULSAR_JOB_CONFIG_FILE = os.path.join(SCRIPT_DIRECTORY, "embedded_pulsar_metadata_job_conf.yml")
+
+
+class EmbeddedMetadataPulsarIntegrationInstance(integration_util.IntegrationInstance):
+    """Describe a Galaxy test instance with embedded pulsar configured."""
+
+    framework_tool_and_types = True
+
+    @classmethod
+    def handle_galaxy_config_kwds(cls, config):
+        config["job_config_file"] = EMBEDDED_PULSAR_JOB_CONFIG_FILE
+        config['object_store_store_by'] = 'uuid'
+        config['retry_metadata_internally'] = False
+        config["outputs_to_working_directory"] = True
+
+
+instance = integration_util.integration_module_instance(EmbeddedMetadataPulsarIntegrationInstance)
+
+test_tools = integration_util.integration_tool_runner(["simple_constructs"])
