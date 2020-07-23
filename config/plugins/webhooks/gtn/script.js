c401de7114a892bec99f544bd2c3042e3d5de9b6
Dannon Baker <dannon.baker@gmail.com>
2020-07-23 15:22:16 -0400

    Formatting prior to edits

--- config/plugins/webhooks/gtn/script.js
+++ config/plugins/webhooks/gtn/script.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
-	function showOverlay() {
-		$(".gtn-screen-overlay").show();
-		$(".gtn-screen").show();
-	}
+$(document).ready(function () {
+    function showOverlay() {
+        $(".gtn-screen-overlay").show();
+        $(".gtn-screen").show();
+    }
 
-	function removeOverlay() {
-		$(".gtn-screen-overlay").hide();
-		$(".gtn-screen").hide();
-	}
+    function removeOverlay() {
+        $(".gtn-screen-overlay").hide();
+        $(".gtn-screen").hide();
+    }
 
-	function addIframe(proxied){
-		var url, message;
-		if(proxied){
-			url = '/training-material/';
-			message = ''
-		}
-		else {
-			url = 'https://training.galaxyproject.org/training-material/';
-			message = '<span><a href="https://docs.galaxyproject.org/en/master/admin/special_topics/gtn.html">Click to run</a> unavailable.</span>'
-		}
+    function addIframe(proxied) {
+        var url, message;
+        if (proxied) {
+            url = "/training-material/";
+            message = "";
+        } else {
+            url = "https://training.galaxyproject.org/training-material/";
+            message =
+                '<span><a href="https://docs.galaxyproject.org/en/master/admin/special_topics/gtn.html">Click to run</a> unavailable.</span>';
+        }
 
-		parentElement.prepend(`
+        parentElement.prepend(`
 			<div id="gtn_screen_overlay" class="gtn-screen-overlay"></div>
 			<div id="gtn_screen" class="gtn-screen">
 				<div class="gtn-header">
@@ -30,75 +30,70 @@ $(document).ready(function() {
 			</div>
 	   `);
 
-		// Clicking outside of GTN closes it
-		$("#gtn_screen").click(function() {
-			self.removeOverlay();
-		});
+        // Clicking outside of GTN closes it
+        $("#gtn_screen").click(function () {
+            self.removeOverlay();
+        });
 
-		// Depends on the iframe being present
-	$("#gtn-embed").on("load", function() {
-		//new_url = document.getElementById("gtn-embed").contentWindow.location.href;
-		var gtn_tools = $("#gtn-embed")
-			.contents()
-			.find("span[data-tool]");
-		// Buttonify
-		gtn_tools.addClass("btn btn-primary");
+        // Depends on the iframe being present
+        $("#gtn-embed").on("load", function () {
+            //new_url = document.getElementById("gtn-embed").contentWindow.location.href;
+            var gtn_tools = $("#gtn-embed").contents().find("span[data-tool]");
+            // Buttonify
+            gtn_tools.addClass("btn btn-primary");
 
-		gtn_tools.click(function(e) {
-			var target = e.target;
+            gtn_tools.click(function (e) {
+                var target = e.target;
 
-			// Sometimes we get the i or the strong, not the parent.
-			if (e.target.tagName.toLowerCase() !== "span") {
-				target = e.target.parentElement;
-			}
+                // Sometimes we get the i or the strong, not the parent.
+                if (e.target.tagName.toLowerCase() !== "span") {
+                    target = e.target.parentElement;
+                }
 
-			tool_id = $(target).data("tool");
-			tool_version = $(target).data("version");
+                tool_id = $(target).data("tool");
+                tool_version = $(target).data("version");
 
-			if (tool_id === "upload1" || tool_id === "upload") {
-				Galaxy.upload.show();
-			} else {
-				Galaxy.router.push(`/?tool_id=${tool_id}&version=${tool_version}`);
-			}
-			self.removeOverlay();
-		});
-	});
+                if (tool_id === "upload1" || tool_id === "upload") {
+                    Galaxy.upload.show();
+                } else {
+                    Galaxy.router.push(`/?tool_id=${tool_id}&version=${tool_version}`);
+                }
+                self.removeOverlay();
+            });
+        });
+    }
 
-	}
+    var self = this;
+    var parentElement = $(".full-content");
+    self.showOverlay = showOverlay;
+    self.removeOverlay = removeOverlay;
 
-	var self = this;
-	var parentElement = $(".full-content");
-	self.showOverlay = showOverlay;
-	self.removeOverlay = removeOverlay;
+    // Test for the precense of /training-material/. If that is available we
+    // can opt in the fancy click-to-run features. Otherwise we fallback to
+    // displaying the real GTN.
 
-	// Test for the precense of /training-material/. If that is available we
-	// can opt in the fancy click-to-run features. Otherwise we fallback to
-	// displaying the real GTN.
-	var jqxhr = $.get( "/training-material/", function() {
-		// proxy is available
-		addIframe(true)
-	})
-	.fail(function() {
-		addIframe(false)
-	})
+    var jqxhr = $.get("/training-material/", function () {
+        // proxy is available
+        addIframe(true);
+    }).fail(function () {
+        addIframe(false);
+    });
 
+    parentElement.find("ul #gtn a").on("click", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        if ($(".gtn-screen-overlay").is(":visible")) {
+            self.removeOverlay();
+        } else {
+            self.showOverlay();
+        }
+    });
 
-	parentElement.find("ul #gtn a").on("click", function(e) {
-		e.preventDefault();
-		e.stopPropagation();
-		if ($(".gtn-screen-overlay").is(":visible")) {
-			self.removeOverlay();
-		} else {
-			self.showOverlay();
-		}
-	});
-
-	// Remove the overlay on escape button click
-	parentElement.on("keydown", function(e) {
-		// Check for escape button - "27"
-		if (e.which === 27 || e.keyCode === 27) {
-			self.removeOverlay();
-		}
-	});
-
+    // Remove the overlay on escape button click
+    parentElement.on("keydown", function (e) {
+        // Check for escape button - "27"
+        if (e.which === 27 || e.keyCode === 27) {
+            self.removeOverlay();
+        }
+    });
 });
