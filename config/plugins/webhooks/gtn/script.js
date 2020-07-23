9d7a108d44b9a933147939888738924491a77740
Helena Rasche <hxr@hx42.org>
2020-07-23 11:53:32 +0200

    Rewrite to test for presence

--- config/plugins/webhooks/gtn/script.js
+++ config/plugins/webhooks/gtn/script.js
@@ -9,42 +9,33 @@ $(document).ready(function() {
 		$(".gtn-screen").hide();
 	}
 
-	var self = this;
-	var parentElement = $(".full-content");
-	self.showOverlay = showOverlay;
-	self.removeOverlay = removeOverlay;
+	function addIframe(proxied){
+		var url, message;
+		if(proxied){
+			url = '/training-material/';
+			message = ''
+		}
+		else {
+			url = 'https://training.galaxyproject.org/training-material/';
+			message = '<span><a href="https://docs.galaxyproject.org/en/master/admin/special_topics/gtn.html">Click to run</a> unavailable.</span>'
+		}
 
-	parentElement.prepend(`
-		<div id="gtn_screen_overlay" class="gtn-screen-overlay"></div>
-		<div id="gtn_screen" class="gtn-screen">
-			<div class="gtn-header">
-				<iframe id="gtn-embed" src="/training-material/" width="80%" height="80%"></iframe>
+		parentElement.prepend(`
+			<div id="gtn_screen_overlay" class="gtn-screen-overlay"></div>
+			<div id="gtn_screen" class="gtn-screen">
+				<div class="gtn-header">
+					<iframe id="gtn-embed" src="${url}" width="80%" height="80%"></iframe>
+					${message}
+				</div>
 			</div>
-		</div>
-   `);
+	   `);
 
-	parentElement.find("ul #gtn a").on("click", function(e) {
-		e.preventDefault();
-		e.stopPropagation();
-		if ($(".gtn-screen-overlay").is(":visible")) {
+		// Clicking outside of GTN closes it
+		$("#gtn_screen").click(function() {
 			self.removeOverlay();
-		} else {
-			self.showOverlay();
-		}
-	});
+		});
 
-	// Remove the overlay on escape button click
-	parentElement.on("keydown", function(e) {
-		// Check for escape button - "27"
-		if (e.which === 27 || e.keyCode === 27) {
-			self.removeOverlay();
-		}
-	});
-
-	$("#gtn-header").click(function() {
-		self.removeOverlay();
-	});
-
+		// Depends on the iframe being present
 	$("#gtn-embed").on("load", function() {
 		//new_url = document.getElementById("gtn-embed").contentWindow.location.href;
 		var gtn_tools = $("#gtn-embed")
@@ -72,4 +63,42 @@ $(document).ready(function() {
 			self.removeOverlay();
 		});
 	});
+
+	}
+
+	var self = this;
+	var parentElement = $(".full-content");
+	self.showOverlay = showOverlay;
+	self.removeOverlay = removeOverlay;
+
+	// Test for the precense of /training-material/. If that is available we
+	// can opt in the fancy click-to-run features. Otherwise we fallback to
+	// displaying the real GTN.
+	var jqxhr = $.get( "/training-material/", function() {
+		// proxy is available
+		addIframe(true)
+	})
+	.fail(function() {
+		addIframe(false)
+	})
+
+
+	parentElement.find("ul #gtn a").on("click", function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		if ($(".gtn-screen-overlay").is(":visible")) {
+			self.removeOverlay();
+		} else {
+			self.showOverlay();
+		}
+	});
+
+	// Remove the overlay on escape button click
+	parentElement.on("keydown", function(e) {
+		// Check for escape button - "27"
+		if (e.which === 27 || e.keyCode === 27) {
+			self.removeOverlay();
+		}
+	});
+
 });
