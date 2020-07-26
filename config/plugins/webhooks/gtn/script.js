e6a34afd7e4e156c87306bac9a33347c6a9bc307
Dannon Baker <dannon.baker@gmail.com>
2020-07-25 22:00:48 -0400

    Use fetch api and chained promises to test/load inline.  Still need to fix the style, but this is mostly there.

--- config/plugins/webhooks/gtn/script.js
+++ config/plugins/webhooks/gtn/script.js
@@ -10,66 +10,72 @@ $(document).ready(() => {
     }
 
     function addIframe() {
+        let url, message;
         gtnWebhookLoaded = true;
-        var url = "https://training.galaxyproject.org/training-material/";
-        var message = `
-            <span>
-                <a href="https://docs.galaxyproject.org/en/master/admin/special_topics/gtn.html">Click to run</a> unavailable.
-            </span>
-        `;
 
         // Test for the presence of /training-material/. If that is available we
         // can opt in the fancy click-to-run features. Otherwise we fallback to
         // displaying the real GTN.
-        var jqxhr = $.get("/training-material/", function () {
-            url = "/training-material/";
-            message = "";
-        });
-
-        document.querySelector("body.full-content").insertAdjacentHTML(
-            "afterbegin",
-            `
-            <div id="gtn-container">
-                <div id="gtn-screen-overlay"></div>
-                <div id="gtn-screen">
-                    <div class="gtn-header">
-                        <iframe id="gtn-embed" src="${url}" width="80%" height="80%"></iframe>
-                        ${message}
-                    </div>
-                </div>
-            </div>`
-        );
-
-        // Clicking outside of GTN closes it
-        document.getElementById("gtn-screen").addEventListener("click", () => {
-            removeOverlay();
-        });
-
-        // Depends on the iframe being present
-        document.getElementById("gtn-embed").addEventListener("load", () => {
-            var gtn_tools = $("#gtn-embed").contents().find("span[data-tool]");
-            // Buttonify
-            gtn_tools.addClass("btn btn-primary");
-
-            gtn_tools.click((e) => {
-                var target = e.target;
-
-                // Sometimes we get the i or the strong, not the parent.
-                if (e.target.tagName.toLowerCase() !== "span") {
-                    target = e.target.parentElement;
-                }
-
-                tool_id = $(target).data("tool");
-                tool_version = $(target).data("version");
-
-                if (tool_id === "upload1" || tool_id === "upload") {
-                    Galaxy.upload.show();
+        fetch("/training-material/")
+            .then((response) => {
+                if (!response.ok) {
+                    url = "https://training.galaxyproject.org/training-material/";
+                    message = `
+                        <span>
+                            <a href="https://docs.galaxyproject.org/en/master/admin/special_topics/gtn.html">Click to run</a> unavailable.
+                        </span>`;
                 } else {
-                    Galaxy.router.push(`/?tool_id=${tool_id}&version=${tool_version}`);
+                    url = "/training-material/";
+                    message = "";
                 }
-                removeOverlay();
+            })
+            .then(() => {
+                console.debug("DONE!", url);
+                document.querySelector("body.full-content").insertAdjacentHTML(
+                    "afterbegin",
+                    `
+                    <div id="gtn-container">
+                        <div id="gtn-screen-overlay"></div>
+                        <div id="gtn-screen">
+                            <div class="gtn-header">
+                                <iframe id="gtn-embed" src="${url}" width="80%" height="80%"></iframe>
+                                ${message}
+                            </div>
+                        </div>
+                    </div>`
+                );
+
+                // Clicking outside of GTN closes it
+                document.getElementById("gtn-screen").addEventListener("click", () => {
+                    removeOverlay();
+                });
+
+                // Depends on the iframe being present
+                document.getElementById("gtn-embed").addEventListener("load", () => {
+                    var gtn_tools = $("#gtn-embed").contents().find("span[data-tool]");
+                    // Buttonify
+                    gtn_tools.addClass("btn btn-primary");
+
+                    gtn_tools.click((e) => {
+                        var target = e.target;
+
+                        // Sometimes we get the i or the strong, not the parent.
+                        if (e.target.tagName.toLowerCase() !== "span") {
+                            target = e.target.parentElement;
+                        }
+
+                        tool_id = $(target).data("tool");
+                        tool_version = $(target).data("version");
+
+                        if (tool_id === "upload1" || tool_id === "upload") {
+                            Galaxy.upload.show();
+                        } else {
+                            Galaxy.router.push(`/?tool_id=${tool_id}&version=${tool_version}`);
+                        }
+                        removeOverlay();
+                    });
+                });
             });
-        });
     }
     /* The masthead icon may not exist yet when this webhook executes; we need this to wait for that to happen.
      * elementReady function from gist:
