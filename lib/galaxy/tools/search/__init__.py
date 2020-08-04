galaxyproject/galaxy

commit 19c4ff42823638d51792a98486cb28e9b92a6923

Store tool search index
This means we also need to remove indexed documents that don't exist
anymore.

lib/galaxy/tools/search/__init__.py
@@ -5,29 +5,42 @@ or searching related parts it is deeply recommended to read
 through the library docs at https://whoosh.readthedocs.io.
 """
 import logging
+import os
 import re
-import tempfile
 
-from whoosh import analysis
+from whoosh import (
+    analysis,
+    index,
+)
 from whoosh.analysis import StandardAnalyzer
 from whoosh.fields import (
+    ID,
     KEYWORD,
     Schema,
-    STORED,
     TEXT
-)
-from whoosh.filedb.filestore import (
-    FileStorage,
-    RamStorage
 )
 from whoosh.qparser import MultifieldParser
 from whoosh.qparser import OrGroup
 from whoosh.scoring import BM25F
+from whoosh.writing import AsyncWriter
 
 from galaxy.util import ExecutionTimer
 from galaxy.web.framework.helpers import to_unicode
 
 log = logging.getLogger(__name__)
+
+
+def get_or_create_index(index_dir, schema):
+    if not os.path.exists(index_dir):
+        os.makedirs(index_dir)
+    if index.exists_in(index_dir):
+        idx = index.open_dir(index_dir)
+        try:
+            assert idx.schema == schema
+            return idx
+        except AssertionError:
+            log.warning("Index at '%s' uses outdated schema, creating new index", index_dir)
+    return index.create_in(index_dir, schema=schema)
 
 
 class ToolBoxSearch(object):
@@ -36,8 +49,8 @@ class ToolBoxSearch(object):
     the Whoosh search library.
     """
 
-    def __init__(self, toolbox, index_help=True):
-        self.schema = Schema(id=STORED,
+    def __init__(self, toolbox, index_dir=None, index_help=True):
+        self.schema = Schema(id=ID(stored=True),
                              stub=KEYWORD,
                              name=TEXT(analyzer=analysis.SimpleAnalyzer()),
                              description=TEXT,
@@ -45,8 +58,9 @@ class ToolBoxSearch(object):
                              help=TEXT,
                              labels=KEYWORD)
         self.rex = analysis.RegexTokenizer()
+        self.index_dir = index_dir
         self.toolbox = toolbox
-        self.storage, self.index = self._index_setup()
+        self.index = self._index_setup()
         # We keep track of how many times the tool index has been rebuilt.
         # We start at -1, so that after the first index the count is at 0,
         # which is the same as the toolbox reload count. This way we can skip
@@ -54,11 +68,7 @@ class ToolBoxSearch(object):
         self.index_count = -1
 
     def _index_setup(self):
-        RamStorage.temp_storage = _temp_storage
-        # Works around https://bitbucket.org/mchaput/whoosh/issues/391/race-conditions-with-temp-storage
-        storage = RamStorage()
-        index = storage.create_index(self.schema)
-        return storage, index
+        return get_or_create_index(index_dir=self.index_dir, schema=self.schema)
 
     def build_index(self, tool_cache, index_help=True):
         """
@@ -68,10 +78,13 @@ class ToolBoxSearch(object):
         log.debug('Starting to build toolbox index.')
         self.index_count += 1
         execution_timer = ExecutionTimer()
-        writer = self.index.writer()
-        for tool_id in tool_cache._removed_tool_ids:
+        with self.index.searcher() as searcher:
+            indexed_tool_ids = {f.get('id') for f in searcher.all_stored_fields()}
+        tool_ids_to_remove = (indexed_tool_ids - set(tool_cache._tool_paths_by_id.keys())).union(tool_cache._removed_tool_ids)
+        writer = AsyncWriter(self.index)
+        for tool_id in tool_ids_to_remove:
             writer.delete_by_term('id', tool_id)
-        for tool_id in tool_cache._new_tool_ids:
+        for tool_id in tool_cache._new_tool_ids - indexed_tool_ids:
             tool = tool_cache.get_tool_by_id(tool_id)
             if tool and tool.is_latest_version:
                 add_doc_kwds = self._create_doc(tool_id=tool_id, tool=tool, index_help=index_help)
@@ -181,9 +194,3 @@ class ToolBoxSearch(object):
         hits_with_score = sorted(hits_with_score.items(), key=lambda x: x[1], reverse=True)
         # Return the tool ids
         return [item[0] for item in hits_with_score[0:int(tool_search_limit)]]
-
-
-def _temp_storage(self, name=None):
-    path = tempfile.mkdtemp()
-    tempstore = FileStorage(path)
-    return tempstore.create()

lib/galaxy/config/__init__.py
@@ -1031,7 +1031,7 @@ class ConfiguresGalaxyMixin(object):
         self.container_finder = containers.ContainerFinder(app_info, mulled_resolution_cache=mulled_resolution_cache)
         self._set_enabled_container_types()
         index_help = getattr(self.config, "index_tool_help", True)
-        self.toolbox_search = galaxy.tools.search.ToolBoxSearch(self.toolbox, index_help)
+        self.toolbox_search = galaxy.tools.search.ToolBoxSearch(self.toolbox, index_dir=self.config.tool_search_index_dir, index_help=index_help)
 
     def reindex_tool_search(self):
         # Call this when tools are added or removed.

lib/galaxy/webapps/galaxy/config_schema.yml
@@ -759,6 +759,14 @@ mapping:
           Per tool_conf cache locations can be configured in (shed_)tool_conf.xml files using
           the tool_cache_data_dir attribute.
 
+      tool_search_index_dir:
+        type: str
+        default: tool_search_index
+        path_resolves_to: data_dir
+        required: false
+        desc:
+          Directory in which the toolbox search index is stored.
+
       delay_tool_initialization:
         type: bool
         default: false

lib/tool_shed/util/shed_index.py
@@ -2,11 +2,11 @@ import logging
 import os
 
 from mercurial import hg, ui
-from whoosh import index
 from whoosh.writing import AsyncWriter
 
 import tool_shed.webapp.model.mapping as ts_mapping
 from galaxy.tool_util.loader_directory import load_tool_elements_from_path
+from galaxy.tools.search import get_or_create_index
 from galaxy.util import (
     directory_hash_id,
     ExecutionTimer,
@@ -21,24 +21,13 @@ from tool_shed.webapp.search.tool_search import schema as tool_schema
 log = logging.getLogger(__name__)
 
 
-def get_or_create_index(whoosh_index_dir):
+def _get_or_create_index(whoosh_index_dir):
     tool_index_dir = os.path.join(whoosh_index_dir, 'tools')
     if not os.path.exists(whoosh_index_dir):
         os.makedirs(whoosh_index_dir)
     if not os.path.exists(tool_index_dir):
         os.makedirs(tool_index_dir)
-    return _get_or_create_index(whoosh_index_dir, repo_schema), _get_or_create_index(tool_index_dir, tool_schema)
-
-
-def _get_or_create_index(index_dir, schema):
-    if index.exists_in(index_dir):
-        idx = index.open_dir(index_dir)
-        try:
-            assert idx.schema == schema
-            return idx
-        except AssertionError:
-            log.warning("Index at '%s' uses outdated schema, creating new index", index_dir)
-    return index.create_in(index_dir, schema=schema)
+    return get_or_create_index(whoosh_index_dir, repo_schema), get_or_create_index(tool_index_dir, tool_schema)
 
 
 def build_index(whoosh_index_dir, file_path, hgweb_config_dir, dburi, **kwargs):
@@ -50,7 +39,7 @@ def build_index(whoosh_index_dir, file_path, hgweb_config_dir, dburi, **kwargs):
     """
     model = ts_mapping.init(file_path, dburi, engine_options={}, create_tables=False)
     sa_session = model.context.current
-    repo_index, tool_index = get_or_create_index(whoosh_index_dir)
+    repo_index, tool_index = _get_or_create_index(whoosh_index_dir)
 
     repo_index_writer = AsyncWriter(repo_index)
     tool_index_writer = AsyncWriter(tool_index)

commit 409c53895e6822a9c04684a9ef65521208008154

Fix non-unique entries in tool search index
Fixes https://github.com/galaxyproject/galaxy/issues/10030,
broken in 19c4ff42823638d51792a98486cb28e9b92a6923, where I made
the assumption that we always start with a fully-populated index.

This resulted in somewhere between 1 and N (for N web handlers)
entries in the search index.

This ensures that we don't end up indexing tool documents multiple
times by using `update_document` and making ID unique in the Whoosh
schema.

`update_document` behaves like `add_document` if the document id is not
in the index:
https://whoosh.readthedocs.io/en/latest/indexing.html?highlight=unique#updating-documents

lib/galaxy/tools/search/__init__.py
@@ -50,7 +50,7 @@ class ToolBoxSearch(object):
     """
 
     def __init__(self, toolbox, index_dir=None, index_help=True):
-        self.schema = Schema(id=ID(stored=True),
+        self.schema = Schema(id=ID(stored=True, unique=True),
                              stub=KEYWORD,
                              name=TEXT(analyzer=analysis.SimpleAnalyzer()),
                              description=TEXT,
@@ -89,7 +89,7 @@ class ToolBoxSearch(object):
                 tool = tool_cache.get_tool_by_id(tool_id)
                 if tool and tool.is_latest_version:
                     add_doc_kwds = self._create_doc(tool_id=tool_id, tool=tool, index_help=index_help)
-                    writer.add_document(**add_doc_kwds)
+                    writer.update_document(**add_doc_kwds)
         log.debug("Toolbox index finished %s", execution_timer)
 
     def _create_doc(self, tool_id, tool, index_help=True):