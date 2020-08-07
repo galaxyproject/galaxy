7f82e3751397328df1063a277a80d8882ef21092
Dannon Baker <dannon.baker@gmail.com>
2020-08-06 22:21:39 -0400

    Set access-control-max-age, more detailed comments.

--- lib/galaxy/webapps/galaxy/api/authenticate.py
+++ lib/galaxy/webapps/galaxy/api/authenticate.py
@@ -36,13 +36,16 @@ class AuthenticationController(BaseAPIController):
     @expose_api_anonymous_and_sessionless
     def options(self, trans, **kwd):
         """
-        A no-op endpoint to return generic OPTIONS for the API.  Right now this is solely to inform preflight CORS checks, which are API wide.
+        A no-op endpoint to return generic OPTIONS for the API.
+        Any OPTIONS request to /api/* maps here.
+        Right now this is solely to inform preflight CORS checks, which are API wide.
+        Might be better placed elsewhere, but for now this is the initial entrypoint for relevant consumers.
         """
         trans.response.headers['Access-Control-Allow-Headers'] = '*'
-        #Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE
-        #Access-Control-Max-Age: 86400
-        print(trans.response.headers)
-        return
+        trans.response.headers['Access-Control-Max-Age'] = 600
+        # No need to set allow-methods for preflight cors check, I don't think.
+        # When this is actually granular, endpoints should *probably* respond appropriately.
+        # trans.response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS, DELETE'
 
     @expose_api_anonymous_and_sessionless
     def get_api_key(self, trans, **kwd):
