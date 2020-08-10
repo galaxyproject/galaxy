bea440270ef0a486cb0a3ec3df43967bf832bcb9
Dannon Baker <dannon.baker@gmail.com>
2020-08-10 16:38:25 -0400

    Format; apparently config* wasn't under monitoring before.

--- client/src/config/development.js
+++ client/src/config/development.js
@@ -1,3 +1,3 @@
 export default {
-    name: "development configs"
-}
+    name: "development configs",
+};
--- client/src/config/production.js
+++ client/src/config/production.js
@@ -1,3 +1,3 @@
 export default {
-    name: "production configs"
-}
+    name: "production configs",
+};
