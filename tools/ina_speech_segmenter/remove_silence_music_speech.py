f342ccd171f86112782439b2d774f254697abc11
dan <dan@portalmedia.com>
2020-08-10 22:01:31 -0500

    Floor and ceiling to kept segments

--- tools/ina_speech_segmenter/remove_silence_music_speech.py
+++ tools/ina_speech_segmenter/remove_silence_music_speech.py
@@ -79,13 +79,13 @@ def get_start_with_buffer(start):
 	if start <= buffer:
 		return 0
 	else:
-		return start - buffer
+		return math.floor(start - buffer)
 
 def get_end_with_buffer(end, file_duration):
 	if end + buffer > file_duration:
 		return file_duration
 	else:
-		return end + buffer
+		return math.ceil(end + buffer)
 
 # Given a start and end offset, create a segment of audio 
 def create_audio_part(input_file, start, end, segment, file_duration):
