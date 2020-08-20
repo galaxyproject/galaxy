29bd705dd08221d255b6b8b03acefd04609a5f19
mvdbeek <m.vandenbeek@gmail.com>
2020-08-20 15:44:16 +0200

    Track DatasetUIWrapper.vue

---
+++ client/src/components/History/ContentItem/Dataset/DatasetUIWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+    <DatasetUI v-if="dataset" v-bind="$attrs" v-on="$listeners" :dataset="dataset" />
+</template>
+<script>
+import DatasetUI from "./DatasetUI";
+import { Dataset } from "../../model/Dataset";
+export default {
+    components: {
+        DatasetUI,
+    },
+    props: {
+        item: { type: Object, required: true },
+    },
+    computed: {
+        dataset() {
+            return new Dataset(this.item);
+        },
+    },
+};
+</script>
