1dec4af804b9fdef6492e537497d4ff985fb7de5
Kaivan Kamali <kxk302@psu.edu>
2020-08-20 11:16:23 -0400

    python irods client successfully puts filer in irods. Added partial capability to parse input parameters.

--- scripts/objectstore/copy_files_to_irods.py
+++ scripts/objectstore/copy_files_to_irods.py
@@ -1,12 +1,21 @@
+import getopt
 import os
 import uuid
+import sys
 
+import irods.keywords as kw
+from irods.session import iRODSSession
 from psycopg2 import connect, sql
+
 
 from galaxy.util import directory_hash_id
 
 
-def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info={"files1" : "/Users/kxk302/workspace/galaxy/database/files1", "files2" : "/Users/kxk302/workspace/galaxy/database/files2"}, irods_home="/tempZone/home/rods", connection_info={"dbname" : "galaxy", "user" : "postgres", "host" : "localhost", "password" : "password"}):
+def copy_files_to_irods(start_dataset_id,
+                        end_dataset_id,
+                        object_store_info={"files1" : "/Users/kxk302/workspace/galaxy/database/files1", "files2" : "/Users/kxk302/workspace/galaxy/database/files2"},
+                        irods_info={"home" : "/tempZone/home/rods", "host" : "localhost", "port" : "1247", "user" : "rods", "password" : "rods", "zone" : "tempZone", "timeout" : "30"},
+                        connection_info={"dbname" : "galaxy", "user" : "postgres", "host" : "localhost", "password" : "password"}):
     conn = None
     cursor = None
     sql_statement = None
@@ -19,8 +28,7 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info={"fi
     disk_file_path = None
     irods_sub_folder = None
     irods_file_path = None
-    icommand_mkdir = None
-    icommand_put = None
+    session = None
 
     if start_dataset_id > end_dataset_id:
         print("start_dataset_id %d cannot be larger than end_dataset_id %d", start_dataset_id, end_dataset_id)
@@ -39,6 +47,9 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info={"fi
     except Exception as e:
         print(e)
         return
+
+    session = iRODSSession(host=irods_info["host"], port=irods_info["port"], user=irods_info["user"], password=irods_info["password"], zone=irods_info["zone"])
+    session.connection_timeout = int(irods_info["timeout"])
 
     osi_keys = list(object_store_info.keys())
     osi_keys_quoted = ["'" + key + "'" for key in osi_keys]
@@ -92,21 +103,52 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info={"fi
 
             disk_file_path = os.path.join(object_store_path, disk_sub_folder, "dataset_" + str(objectid) + ".dat")
             print("disk_file_path: ", disk_file_path)
-            irods_file_path = os.path.join(irods_home, irods_sub_folder, "dataset_" + str(uuid_with_dash) + ".dat")
+            irods_file_path = os.path.join(irods_info["home"], irods_sub_folder, "dataset_" + str(uuid_with_dash) + ".dat")
             print("irods_file_path: ", irods_file_path)
+            irods_collection_path = os.path.join(irods_info["home"], irods_sub_folder)
+            print("irods_collection_path: ", irods_collection_path)
 
-            icommand_mkdir = "imkdir -p " + irods_sub_folder
-            print("icommand_mkdir: ", icommand_mkdir)
-            icommand_put = "iput " + disk_file_path + " " + irods_file_path
-            print("icommand_put: ", icommand_put)
+            # Create the collection
+            session.collections.create(irods_collection_path)
+
+            # Add disk file to collection
+            options = {kw.DEST_RESC_NAME_KW: 'demoResc'}
+            session.data_objects.put(disk_file_path, irods_file_path, **options)
 
     except Exception as e:
         print(e)
+        session.cleanup()
         conn.rollback()
         cursor.close()
         conn.close()
         return
 
+    session.cleanup()
     conn.commit()
     cursor.close()
     conn.close()
+
+
+if __name__ == '__main__':
+    print("Number of arguments: " + str(len(sys.argv)))
+    print("Arguments list: " + str(sys.argv))
+
+    start_dataset_id = None
+    end_dataset_id = None
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hs:e:", ["start-dataset-id=", "end-dataset-id="])
+    except getopt.GetoptError:
+        print("copy_files_to_irods -s 2 -e 3")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print("copy_files_to_irods -s 2 -e 3")
+            sys.exit(2)
+        elif opt in ("-s", "--start-dataset-id"):
+            start_dataset_id = arg
+        elif opt in ("-e", "--end-datset-id"):
+            end_dataset_id = arg
+
+    copy_files_to_irods(start_dataset_id=start_dataset_id, end_dataset_id=end_dataset_id)
