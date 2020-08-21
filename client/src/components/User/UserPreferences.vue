12ff01cbb1ded2ef1ea8e26c22a33220f6ae5798
Dannon Baker <dannon.baker@gmail.com>
2020-08-21 09:44:47 -0400

    Verbiage and styling tweaks.

--- client/src/components/User/UserPreferences.vue
+++ client/src/components/User/UserPreferences.vue
@@ -37,32 +37,34 @@
             <div class="pref-content pr-1">
                 <a href="javascript:void(0)"><b v-b-modal.modal-prevent-closing>Delete Account</b></a>
                 <div class="form-text text-muted">
-                    Delete your user account. This will not purge your account immediately.
+                    Delete your account on this Galaxy server.
                 </div>
                 <b-modal
                     id="modal-prevent-closing"
+                    centered
                     ref="modal"
-                    title="Type your user email for this account as confirmation."
+                    title="Account Deletion"
+                    title-tag="h2"
                     @show="resetModal"
                     @hidden="resetModal"
                     @ok="handleOk"
                 >
                     <p>
-                        <b
-                            >This action cannot be undone. Your account will be permanently deleted, along with the data
-                            contained in it.</b
-                        >
+                        <b>
+                            This action cannot be undone. Your account will be permanently deleted, along with the data
+                            contained in it.
+                        </b>
                     </p>
-                    <form ref="form" @submit.stop.prevent="handleSubmit">
+                    <b-form ref="form" @submit.prevent="handleSubmit">
                         <b-form-group
                             :state="nameState"
-                            label="User email"
+                            label="Enter your user email for this account as confirmation."
                             label-for="Email"
                             invalid-feedback="Incorrect email"
                         >
                             <b-form-input id="name-input" v-model="name" :state="nameState" required></b-form-input>
                         </b-form-group>
-                    </form>
+                    </b-form>
                 </b-modal>
             </div>
         </b-row>
