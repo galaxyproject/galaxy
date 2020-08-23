9204e7f80c48e6089cae509265cb191112703dbc
mvdbeek <m.vandenbeek@gmail.com>
2020-08-23 17:59:13 +0200

    Label data inputs and outputs

--- client/src/components/WorkflowInvocationState/WorkflowInvocationDetails.vue
+++ client/src/components/WorkflowInvocationState/WorkflowInvocationDetails.vue
@@ -9,24 +9,25 @@
         <div v-if="Object.keys(invocation.inputs).length > 0">
             <details
                 ><summary><b>Invocation Inputs</b></summary>
-                <div v-for="input in invocation.inputs" v-bind:key="input.id">
-                    <WorkflowInvocationOutputs v-bind:data_item="input" />
+                <div v-for="(input, key) in invocation.inputs" v-bind:key="input.id">
+                    <b>{{dataInputStepLabel(key, input)}}</b>
+                    <workflow-invocation-data-contents v-bind:data_item="input" />
                 </div>
             </details>
         </div>
         <div v-if="Object.keys(invocation.outputs).length > 0">
             <details
                 ><summary><b>Invocation Outputs</b></summary>
-                <div v-for="output in invocation.outputs" v-bind:key="output.id">
-                    <WorkflowInvocationOutputs v-bind:data_item="output" />
+                <div v-for="(output, key) in invocation.outputs" v-bind:key="output.id">
+                    <b>{{key}}:</b> <workflow-invocation-data-contents v-bind:data_item="output" />
                 </div>
             </details>
         </div>
         <div v-if="Object.keys(invocation.output_collections).length > 0">
             <details
                 ><summary><b>Invocation Output Collections</b></summary>
-                <div v-for="output in invocation.output_collections" v-bind:key="output.id">
-                    <WorkflowInvocationOutputs v-bind:data_item="output" />
+                <div v-for="(output, key) in invocation.output_collections" v-bind:key="output.id">
+                    <b>{{key}}:</b> <workflow-invocation-data-contents v-bind:data_item="output" />
                 </div>
             </details>
         </div>
@@ -45,11 +46,12 @@ import BootstrapVue from "bootstrap-vue";
 import Vue from "vue";
 
 import { Dataset } from "components/History/ContentItem/Dataset";
-import WorkflowInvocationOutputs from "./WorkflowInvocationOutputs";
+import WorkflowInvocationDataContents from "./WorkflowInvocationDataContents";
 import ListMixin from "components/History/ListMixin";
 
 import { getAppRoot } from "onload/loadConfig";
 import axios from "axios";
+import _ from 'lodash';
 
 import { getRootFromIndexLink } from "onload";
 import { mapGetters, mapActions } from "vuex";
@@ -61,12 +63,30 @@ Vue.use(BootstrapVue);
 export default {
     mixins: [ListMixin],
     components: {
-        WorkflowInvocationOutputs,
+        WorkflowInvocationDataContents,
     },
     props: {
         invocation: {
             required: true,
         },
+    },
+    computed: {
+        orderedSteps() {
+            return _.orderBy(this.invocation.steps, ['order_index']);
+        }
+    },
+    methods: {
+        dataInputStepLabel(key, input) {
+            let label = this.orderedSteps[key].workflow_step_label;
+            if (!label) {
+                if (input.src === 'hda' || input.src === 'ldda') {
+                    label = 'Input dataset'
+                } else if (input.src === 'hdca') {
+                    label = 'Input dataset collection'
+                }
+            }
+            return label
+        }
     },
 };
 </script>
