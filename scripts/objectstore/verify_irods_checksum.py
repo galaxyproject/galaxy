aced6baee3adadcc584416632a25328482a85875
Kaivan Kamali <kxk302@psu.edu>
2020-08-25 09:17:59 -0400

    Broke the script into two scripts: One script copies files from disk to irods, and another script verifies the checksum, then updated the object store id in dataset table and deletes the file on disk.

--- scripts/objectstore/verify_irods_checksum.py
+++ scripts/objectstore/verify_irods_checksum.py
@@ -5,7 +5,9 @@ import uuid
 import subprocess
 import sys
 
-import irods.keywords as kw
+from irods.exception import CollectionDoesNotExist
+from irods.exception import DataObjectDoesNotExist
+from irods.exception import NetworkException
 from irods.session import iRODSSession
 from psycopg2 import connect
 
@@ -13,11 +15,12 @@ from psycopg2 import connect
 from galaxy.util import directory_hash_id
 
 
-def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file, irods_info_file, db_connection_info_file):
+def verify_irods_checksum(start_dataset_id, end_dataset_id, object_store_info_file, irods_info_file, db_connection_info_file):
     conn = None
     session = None
     osi_keys = None
     read_sql_statement = None
+    update_sql_statement = None
     read_cursor = None
     args = None
     table_data = None
@@ -33,8 +36,6 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
     irods_file_path = None
     irods_file_collection_path = None
     irods_folder_collection_path = None
-    options = None
-    iput_command = None
     object_store_info = None
     irods_info = None
     db_connection_info = None
@@ -83,6 +84,9 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
                             AND id >= %s
                             AND id <= %s
                             AND object_store_id IN %s"""
+    update_sql_statement = """UPDATE dataset
+                              SET object_store_id = %s
+                              WHERE id = %s"""
     try:
         read_cursor = conn.cursor()
         args = ('ok', start_dataset_id, end_dataset_id, osi_keys)
@@ -132,22 +136,38 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
             irods_folder_collection_path = os.path.join(irods_file_collection_path, "dataset_" + str(uuid_with_dash) + "_files")
             print("irods_folder_collection_path: ", irods_folder_collection_path)
 
-            # Create the collection
-            session.collections.create(irods_file_collection_path)
+            # Calculate disk file checksum before uploading it to irods
+            # After disk file is uploaded to irods, we get the file checksum from irods and compare it with the calculated disk file checksum
+            # Note that disk file checksum is ASCII, whereas irods file checksum is Unicode
+            disk_file_checksum = get_file_checksum(disk_file_path)
+            print("disk_file_checksum: ", disk_file_checksum)
+            # Now get the file from irods
+            try:
+                obj = session.data_objects.get(irods_file_path)
+                print("obj.checksum: ", obj.checksum)
+                # obj.checksum is prepended with 'sha2:'. Remove that so we can compare it to disk file checksum
+                irods_file_checksum = obj.checksum[5:]
+                print("irods_file_checksum: ", irods_file_checksum)
+                if irods_file_checksum != disk_file_checksum:
+                    print("irods file checksum {} does not match disk file checksum {}".format(irods_file_checksum, disk_file_checksum))
+                    continue
+            except (DataObjectDoesNotExist, CollectionDoesNotExist) as e:
+                print(e)
+                continue
+            except NetworkException as e:
+                print(e)
+                continue
 
-            # Add disk file to collection
-            options = {kw.DEST_RESC_NAME_KW: 'demoResc', kw.REG_CHKSUM_KW : ''}
-            session.data_objects.put(disk_file_path, irods_file_path, **options)
+            # Update object store id
+            update_cursor = conn.cursor()
+            print("irods object store id: ", irods_info["object_store_id"])
+            update_cursor.execute(update_sql_statement, (irods_info["object_store_id"], objectid))
+            updated_rows = update_cursor.rowcount
+            print("updated_rows: ", updated_rows)
+            update_cursor.close()
 
-            if os.path.isdir(disk_folder_path):
-                disk_folder_path_all_files = disk_folder_path + "/*"
-                print("disk_folder_path_all_files: ", disk_folder_path_all_files)
-
-                # Create the collection
-                session.collections.create(irods_folder_collection_path)
-
-                iput_command = "iput -r " + disk_folder_path_all_files + " " + irods_folder_collection_path
-                subprocess.call(iput_command, shell=True)
+            # Delete file on disk
+            os.remove(disk_file_path)
 
     except Exception as e:
         print(e)
@@ -163,12 +183,26 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
     conn.close()
 
 
+def get_file_checksum(disk_file_path):
+    checksum_cmd = "shasum -a 256 {} | xxd -r -p | base64".format(disk_file_path)
+    print("checksum_cmd: ", checksum_cmd)
+    disk_file_checksum = subprocess.check_output(checksum_cmd, shell=True)
+    # remove '\n' from the end of disk_file_checksum
+    print("disk_file_checksum: ", disk_file_checksum)
+    disk_file_checksum_len = len(disk_file_checksum)
+    print("disk_file_checksum_len: ", disk_file_checksum_len)
+    disk_file_checksum_trimmed = disk_file_checksum[0:(disk_file_checksum_len - 1)]
+    print("disk_file_checksum_trimmed: ", disk_file_checksum_trimmed)
+    # Return Unicode string
+    return disk_file_checksum_trimmed.decode("utf-8")
+
+
 def print_help_msg():
     print("\nLong form input parameter specification:")
-    print("copy_files_to_irods --start_dataset_id=2 --end_dataset_id=3 --object_store_info_file=object_store_info.json --irods_info_file=irods_info_file.json --db_connection_info_file=db_connection_info_file.json")
+    print("verify_irods_checksum --start_dataset_id=2 --end_dataset_id=3 --object_store_info_file=object_store_info.json --irods_info_file=irods_info_file.json --db_connection_info_file=db_connection_info_file.json")
     print("\nOR")
     print("\nShort form input parameter specification:")
-    print("copy_files_to_irods -s 2 -e 3 -o object_store_info.json -i irods_info_file.json -d db_connection_info_file.json")
+    print("verify_irods_checksum -s 2 -e 3 -o object_store_info.json -i irods_info_file.json -d db_connection_info_file.json")
     print("\n")
 
 
@@ -205,4 +239,4 @@ if __name__ == '__main__':
         print_help_msg()
         sys.exit(2)
 
-    copy_files_to_irods(start_dataset_id=start_dataset_id, end_dataset_id=end_dataset_id, object_store_info_file=object_store_info_file, irods_info_file=irods_info_file, db_connection_info_file=db_connection_info_file)
+    verify_irods_checksum(start_dataset_id=start_dataset_id, end_dataset_id=end_dataset_id, object_store_info_file=object_store_info_file, irods_info_file=irods_info_file, db_connection_info_file=db_connection_info_file)
