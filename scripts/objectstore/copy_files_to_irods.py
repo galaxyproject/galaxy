220c97850eff5355f267e76772d3e5778b0c40c1
Kaivan Kamali <kxk302@psu.edu>
2020-08-26 10:47:12 -0400

    Cleaned up script, added documentation to help.

--- scripts/objectstore/copy_files_to_irods.py
+++ scripts/objectstore/copy_files_to_irods.py
@@ -44,7 +44,7 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
     db_connection_info = None
 
     if start_dataset_id > end_dataset_id:
-        print("start_dataset_id %d cannot be larger than end_dataset_id %d", start_dataset_id, end_dataset_id)
+        print("Error: start_dataset_id {} cannot be larger than end_dataset_id {}".format(start_dataset_id, end_dataset_id))
         return
 
     # read object_store_info file
@@ -78,7 +78,6 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
     session.connection_timeout = int(irods_info["timeout"])
 
     osi_keys = tuple(object_store_info.keys())
-    print("osi_keys(): ", osi_keys)
     read_sql_statement = """SELECT id, object_store_id, uuid
                             FROM dataset
                             WHERE state = %s
@@ -94,51 +93,29 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
     try:
         read_cursor = conn.cursor()
         args = ('ok', start_dataset_id, end_dataset_id, osi_keys)
-        print(read_cursor.mogrify(read_sql_statement, args))
-        print(read_sql_statement)
+        read_cursor.mogrify(read_sql_statement, args)
 
         read_cursor.execute(read_sql_statement, args)
-        print("read_cursor.execute() completed")
 
         table_data = read_cursor.fetchall()
-        print("read_cursor.fetchall() completed")
         for num, row in enumerate(table_data):
-            print("row: ", row)
-            print("\n")
-
             objectid = row[0]
             object_store_id = row[1]
             uuid_without_dash = row[2]
-
-            print("Object ID: ", objectid)
-            print("\n")
-            print("Object Store ID: ", object_store_id)
-            print("\n")
-            print("uuid_without_dash: ", uuid_without_dash)
-            print("\n")
             uuid_with_dash = str(uuid.UUID(uuid_without_dash))
-            print("uuid_with_dash: ", uuid_with_dash)
-            print("\n")
 
-            object_store_path = object_store_info[object_store_id]
-            print("object_store_path:", object_store_path)
-            print("\n")
+            object_store_path = object_store_info.get(object_store_id)
+            if object_store_path is None:
+                print("Error: object_store_info_file does not have a value for {}".format(object_store_id))
+                raise Exception
 
             disk_sub_folder = os.path.join(*directory_hash_id(objectid))
-            print("disk_sub_folder: ", disk_sub_folder)
             irods_sub_folder = os.path.join(*directory_hash_id(uuid_with_dash))
-            print("irods_sub_folder: ", irods_sub_folder)
-
             disk_file_path = os.path.join(object_store_path, disk_sub_folder, "dataset_" + str(objectid) + ".dat")
-            print("disk_file_path: ", disk_file_path)
             disk_folder_path = os.path.join(object_store_path, disk_sub_folder, "dataset_" + str(objectid) + "_files")
-            print("disk_folder_path: ", disk_folder_path)
             irods_file_path = os.path.join(irods_info["home"], irods_sub_folder, "dataset_" + str(uuid_with_dash) + ".dat")
-            print("irods_file_path: ", irods_file_path)
             irods_file_collection_path = os.path.join(irods_info["home"], irods_sub_folder)
-            print("irods_file_collection_path: ", irods_file_collection_path)
             irods_folder_collection_path = os.path.join(irods_file_collection_path, "dataset_" + str(uuid_with_dash) + "_files")
-            print("irods_folder_collection_path: ", irods_folder_collection_path)
 
             if copy_or_checksum == "copy":
                 # Create the collection
@@ -147,32 +124,29 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
                 # Add disk file to collection
                 options = {kw.REG_CHKSUM_KW : ''}
                 session.data_objects.put(disk_file_path, irods_file_path, **options)
+                print("Copied disk file {} to irods {}".format(disk_file_path, irods_file_path))
 
                 if os.path.isdir(disk_folder_path):
                     disk_folder_path_all_files = disk_folder_path + "/*"
-                    print("disk_folder_path_all_files: ", disk_folder_path_all_files)
 
                     # Create the collection
                     session.collections.create(irods_folder_collection_path)
 
                     iput_command = "iput -rk " + disk_folder_path_all_files + " " + irods_folder_collection_path
                     subprocess.call(iput_command, shell=True)
+                    print("Copied disk folder {} to irods {}".format(disk_folder_path, irods_folder_collection_path))
 
             if copy_or_checksum == "checksum":
-                # Calculate disk file checksum before uploading it to irods
-                # After disk file is uploaded to irods, we get the file checksum from irods and compare it with the calculated disk file checksum
-                # Note that disk file checksum is ASCII, whereas irods file checksum is Unicode
+                # Calculate disk file checksum. Then get the file checksum from irods and compare it with the calculated disk file checksum
+                # Note that disk file checksum is ASCII, whereas irods file checksum is Unicode. get_file_checksum() converts checksum to unicode
                 disk_file_checksum = get_file_checksum(disk_file_path)
-                print("disk_file_checksum: ", disk_file_checksum)
                 # Now get the file from irods
                 try:
                     obj = session.data_objects.get(irods_file_path)
-                    print("obj.checksum: ", obj.checksum)
                     # obj.checksum is prepended with 'sha2:'. Remove that so we can compare it to disk file checksum
                     irods_file_checksum = obj.checksum[5:]
-                    print("irods_file_checksum: ", irods_file_checksum)
                     if irods_file_checksum != disk_file_checksum:
-                        print("irods file checksum {} does not match disk file checksum {}".format(irods_file_checksum, disk_file_checksum))
+                        print("Error: irods file checksum {} does not match disk file checksum {} for irods file {} and disk file {}".format(irods_file_checksum, disk_file_checksum, irods_file_path, disk_file_path))
                         continue
                 except (DataObjectDoesNotExist, CollectionDoesNotExist) as e:
                     print(e)
@@ -187,27 +161,22 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
                     for root, dirs, files in os.walk(disk_folder_path):
                         for file_name in files:
                             a_disk_file_path = os.path.join(root, file_name)
-                            print(disk_file_path)
                             # Get checksum for disk file
                             a_disk_file_checksum = get_file_checksum(a_disk_file_path)
-                            print("a_disk_file_checksum: ", a_disk_file_checksum)
 
                             # Construct iords path for this disk file, so can get the file from irods, and compare its checksum with disk file checksum
                             # This is to extract the subfoler name for irods from the full disk path
-                            sub_folder = root.replace(disk_folder_path + "/", "")
-                            print("sub_folder: ", sub_folder)
-                            an_irods_file_path = irods_folder_collection_path + "/" + sub_folder + "/" + file_name
-                            print("an_irods_file_path: ", an_irods_file_path)
+                            irods_sub_folder = root.replace(disk_folder_path + "/", "")
+                            # For some reason, join would not work here. I used string concatenation instead
+                            an_irods_file_path = irods_folder_collection_path + "/" + irods_sub_folder + "/" + file_name
 
                             # Now get the file from irods
                             try:
                                 obj = session.data_objects.get(an_irods_file_path)
-                                print("obj.checksum: ", obj.checksum)
                                 # obj.checksum is prepended with 'sha2:'. Remove that so we can compare it to disk file checksum
                                 an_irods_file_checksum = obj.checksum[5:]
-                                print("an_irods_file_checksum: ", an_irods_file_checksum)
                                 if an_irods_file_checksum != a_disk_file_checksum:
-                                    print("irods file checksum {} does not match disk file checksum {}".format(an_irods_file_checksum, a_disk_file_checksum))
+                                    print("Error: irods file checksum {} does not match disk file checksum {} for irods file {} and disk file {}".format(an_irods_file_checksum, a_disk_file_checksum, an_irods_file_path, a_disk_file_path))
                                     continue
                             except (DataObjectDoesNotExist, CollectionDoesNotExist) as e:
                                 print(e)
@@ -217,18 +186,21 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
                                 continue
 
                     # Delete file on disk
-                    print("Removing directory " + disk_folder_path)
+                    print("Removing directory {}".format(disk_folder_path))
                     shutil.rmtree(disk_folder_path)
 
                 # Update object store id
                 update_cursor = conn.cursor()
-                print("irods object store id: ", irods_info["object_store_id"])
                 update_cursor.execute(update_sql_statement, (irods_info["object_store_id"], objectid))
                 updated_rows = update_cursor.rowcount
-                print("updated_rows: ", updated_rows)
+                if updated_rows == 1:
+                    print("Updated object store ID to {} in dataset table for object ID {}".format(irods_info["object_store_id"], objectid))
+                else:
+                    print("Error: Failed to update object store ID to {} in dataset table for object ID {}".format(irods_info["object_store_id"], objectid))
                 update_cursor.close()
 
                 # Delete file on disk
+                print("Removing file {}".format(disk_file_path))
                 os.remove(disk_file_path)
 
     except Exception as e:
@@ -256,10 +228,20 @@ def get_file_checksum(disk_file_path):
 
 
 def print_help_msg():
-    print("\nLong form input parameter specification:")
+    print("This script copies datasets from Disk to Irods. It operates on a range of dataset IDs (start_dataset_id and end_dtatset_id, both inclusive).")
+    print("It also takes in the name of 3 JSON configuration files (A sample JSON configuration file is provided in the Python script folder).")
+    print("1) object_store_info_file: Has Disk object store info (e.g. files info).")
+    print("2) irods_info_file: irods connection info, and misc. info (zone, home, obejct store ID, etc.).")
+    print("3) db_connection_info: Galaxy DB connection info.")
+    print("It also takes a flag copy_or_checksum.")
+    print("If copy: for all datasets from start_dataset_it to end_dataset_id, it copies disk files to irods. This includes extra folders that come with the output of commands like fastqc.")
+    print("If checksum: for all datasets from start_dataset_id to end_dataset_id, it calculates the disk file checksum, gets the irods file, and compares the disk file checksum to irods file checksum.")
+    print("If checksums match, it updates object_store_id dataset table (so Galaxy starts using the irods version of the file), then DELETES the file from disk.")
+    print("If the checksumdoes NOT match: it prints and error message, does NOT update the dataset table or delete the disk file, and continues with the next dataset in dataset ID range.")
+    print("Long-form input parameter specification:")
     print("copy_files_to_irods --start_dataset_id=2 --end_dataset_id=3 --object_store_info_file=object_store_info.json --irods_info_file=irods_info_file.json --db_connection_info_file=db_connection_info_file.json --cop_or_checksum=<copy|checksum>")
     print("\nOR")
-    print("\nShort form input parameter specification:")
+    print("\nShort-form input parameter specification:")
     print("copy_files_to_irods -s 2 -e 3 -o object_store_info.json -i irods_info_file.json -d db_connection_info_file.json -c <copy|checksum>")
     print("\n")
 
@@ -296,12 +278,12 @@ if __name__ == '__main__':
             copy_or_checksum = arg
 
     if start_dataset_id is None or end_dataset_id is None or object_store_info_file is None or irods_info_file is None or db_connection_info_file is None:
-        print("Did not specify one of the required input parameters!")
+        print("Error: Did not specify one of the required input parameters!")
         print_help_msg()
         sys.exit(2)
 
     if copy_or_checksum != "copy" and copy_or_checksum != "checksum":
-        print("Did not specify correct value for copy_or_checksum input parameter!")
+        print("Error: Invalid value specified for copy_or_checksum input parameter {}".format(copy_or_checksum))
         print_help_msg()
         sys.exit(2)
 
