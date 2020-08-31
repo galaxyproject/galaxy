12f4529f707ad3107dd928725b8036cbe26118b2
Kaivan Kamali <kxk302@psu.edu>
2020-08-31 14:17:24 -0400

    irods resource selected based on the last time the dataset was used in a job as input or output. Any dataset used prior to a cuttoff date will be sent to tape resource. Otherewise, the dataset is sent to the regular (non-tape) resource.

--- scripts/objectstore/copy_files_to_irods.py
+++ scripts/objectstore/copy_files_to_irods.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import getopt
 import json
 import os
@@ -15,6 +16,39 @@ from psycopg2 import connect
 
 
 from galaxy.util import directory_hash_id
+
+last_accessed_sql_statement = """SELECT iq.dataset_id, MAX(iq.create_time) AS max_create_time
+                                 FROM (SELECT ds.id as dataset_id, j.create_time
+                                 FROM dataset ds
+                                 INNER JOIN history_dataset_association hda
+                                 ON ds.id = hda.dataset_id
+                                 INNER JOIN job_to_input_dataset jtid
+                                 ON hda.id = jtid.dataset_id
+                                 INNER JOIN job j
+                                 ON jtid.job_id = j.id
+                                 WHERE ds.state = %s
+                                 AND ds.deleted = False
+                                 AND ds.purged = False
+                                 AND ds.id >= %s
+                                 AND ds.id <= %s
+                                 AND ds.object_store_id IN %s
+                                 UNION
+                                 SELECT ds.id as dataset_id, j.create_time
+                                 FROM dataset ds
+                                 INNER JOIN history_dataset_association hda
+                                 ON ds.id = hda.dataset_id
+                                 INNER JOIN job_to_output_dataset jtod
+                                 ON hda.id = jtod.dataset_id
+                                 INNER JOIN job j
+                                 ON jtod.job_id = j.id
+                                 WHERE ds.state = %s
+                                 AND ds.deleted = False
+                                 AND ds.purged = False
+                                 AND ds.id >= %s
+                                 AND ds.id <= %s
+                                 AND ds.object_store_id IN %s) AS iq
+                                 GROUP BY iq.dataset_id
+                                 """
 
 
 def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file, irods_info_file, db_connection_info_file, copy_or_checksum):
@@ -91,39 +125,6 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
                               SET object_store_id = %s
                               WHERE id = %s"""
 
-    last_accessed_sql_statement = """SELECT iq.dataset_id, MAX(iq.create_time)
-                                     FROM (SELECT ds.id as dataset_id, j.create_time
-                                     FROM dataset ds
-                                     INNER JOIN history_dataset_association hda
-                                     ON ds.id = hda.dataset_id
-                                     INNER JOIN job_to_input_dataset jtid
-                                     ON hda.id = jtid.dataset_id
-                                     INNER JOIN job j
-                                     ON jtid.job_id = j.id
-                                     WHERE ds.state = %s
-                                     AND ds.deleted = False
-                                     AND ds.purged = False
-                                     AND ds.id >= %s
-                                     AND ds.id <= %s
-                                     AND ds.object_store_id IN %s
-                                     UNION
-                                     SELECT ds.id as dataset_id, j.create_time
-                                     FROM dataset ds
-                                     INNER JOIN history_dataset_association hda
-                                     ON ds.id = hda.dataset_id
-                                     INNER JOIN job_to_output_dataset jtod
-                                     ON hda.id = jtod.dataset_id
-                                     INNER JOIN job j
-                                     ON jtod.job_id = j.id
-                                     WHERE ds.state = %s
-                                     AND ds.deleted = False
-                                     AND ds.purged = False
-                                     AND ds.id >= %s
-                                     AND ds.id <= %s
-                                     AND ds.object_store_id IN %s) AS iq
-                                     GROUP BY iq.dataset_id
-                                     """
-
     try:
         read_cursor = conn.cursor()
         args = ('ok', start_dataset_id, end_dataset_id, osi_keys)
@@ -143,6 +144,8 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
                 print("Error: object_store_info_file does not have a value for {}".format(object_store_id))
                 raise Exception
 
+            irods_resc = get_irods_resource(conn, objectid, object_store_id, irods_info["tape_resc_cuttoff"])
+
             disk_sub_folder = os.path.join(*directory_hash_id(objectid))
             irods_sub_folder = os.path.join(*directory_hash_id(uuid_with_dash))
             disk_file_path = os.path.join(object_store_path, disk_sub_folder, "dataset_" + str(objectid) + ".dat")
@@ -156,7 +159,7 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
                 session.collections.create(irods_file_collection_path)
 
                 # Add disk file to collection
-                options = {kw.REG_CHKSUM_KW : ''}
+                options = {kw.REG_CHKSUM_KW : '', kw.RESC_NAME_KW: irods_resc}
                 session.data_objects.put(disk_file_path, irods_file_path, **options)
                 print("Copied disk file {} to irods {}".format(disk_file_path, irods_file_path))
 
@@ -166,7 +169,7 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
                     # Create the collection
                     session.collections.create(irods_folder_collection_path)
 
-                    iput_command = "iput -rk " + disk_folder_path_all_files + " " + irods_folder_collection_path
+                    iput_command = "iput -R " + irods_resc + " -rk " + disk_folder_path_all_files + irods_folder_collection_path
                     subprocess.call(iput_command, shell=True)
                     print("Copied disk folder {} to irods {}".format(disk_folder_path, irods_folder_collection_path))
 
@@ -249,6 +252,48 @@ def copy_files_to_irods(start_dataset_id, end_dataset_id, object_store_info_file
     conn.commit()
     read_cursor.close()
     conn.close()
+
+
+def get_irods_resource(conn, objectid, object_store_id, irods_info):
+    try:
+        irods_resc = irods_info["resc"]
+        irods_tape_resc = irods_info["tape_resc"]
+        irods_tape_resc_cuttoff = irods_info["tape_resc_cuttoff"]
+        # Convert string to datetime
+        irods_tape_resc_cuttoff_dt = datetime.strptime(irods_tape_resc_cuttoff, "%m/%d/%Y")
+
+        print("irods_resc: ", irods_resc)
+        print("irods_tape_resc: ", irods_tape_resc)
+        print("irods_tape_resc_cuttoff: ", irods_tape_resc_cuttoff)
+        print("irods_tape_resc_cuttoff_dt: ", irods_tape_resc_cuttoff_dt)
+
+        read_cursor = conn.cursor()
+        args = ('ok', objectid, objectid, tuple([object_store_id]))
+        print(read_cursor.mogrify(last_accessed_sql_statement, args))
+
+        read_cursor.execute(last_accessed_sql_statement, args)
+        row = read_cursor.fetchone()
+        if row is None:
+            print("Could not find the last access time for dataset with id {}. Returning the default resc {}.".format(objectid, irods_resc))
+            return irods_resc
+
+        dataset_id = row[0]
+        if int(dataset_id) != objectid:
+            print("The returned dataset id {} does not match the passed in datsetid {}. Returning the default resc {}.".format(dataset_id, objectid, irods_resc))
+            return irods_resc
+
+        max_create_time = row[1]
+        # Convert string to datetime
+        max_create_time_dt = datetime.strptime(max_create_time, "%Y-%m-%d %H:%M:%S.%f")
+        # If the last time a dataset was accessed was prior to a cuttoff date, use the tape resource. Otherwise, use the regular (non-tape) resource
+        if max_create_time_dt < irods_tape_resc_cuttoff_dt:
+            return irods_tape_resc_cuttoff
+        return irods_resc
+
+    except Exception as e:
+        print(e)
+        read_cursor.close()
+        return
 
 
 def get_file_checksum(disk_file_path):
