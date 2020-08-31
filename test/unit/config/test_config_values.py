4c1f4b7d2e20a71f57f1643a356b2c2b491c07df
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2020-08-31 16:28:19 -0400

    Turn off non-generated config tests

--- test/unit/config/test_config_values.py
+++ test/unit/config/test_config_values.py
@@ -21,49 +21,49 @@ DO_NOT_TEST = {
 }
 
 
-@pytest.fixture(scope='module')
-def appconfig():
-    return config.GalaxyAppConfiguration()
-
-
-@pytest.fixture
-def mock_config_file(monkeypatch):
-    # Patch this; otherwise tempfile.tempdir will be set, which is a global variable that
-    # defines the value of the default `dir` argument to the functions in Python's
-    # tempfile module - which breaks multiple tests.
-    monkeypatch.setattr(config.GalaxyAppConfiguration, '_override_tempdir', lambda a, b: None)
-    # Set this to return None to force the creation of base config directories
-    # in _set_config_directories(). Used to test the values of these directories only.
-    monkeypatch.setattr(config, 'find_config_file', lambda x: None)
-
-
-def test_root(appconfig):
-    assert appconfig.root == os.path.abspath('.')
-
-
-def test_common_base_config(appconfig):
-    assert appconfig.shed_tools_dir == os.path.join(appconfig.data_dir, 'shed_tools')
-    assert appconfig.sample_config_dir == os.path.join(appconfig.root, 'lib', 'galaxy', 'config', 'sample')
-
-
-def test_base_config_if_running_from_source(monkeypatch, mock_config_file):
-    # Simulated condition: running from source, config_file is None.
-    monkeypatch.setattr(config, 'running_from_source', True)
-    appconfig = config.GalaxyAppConfiguration()
-    assert not appconfig.config_file
-    assert appconfig.config_dir == os.path.join(appconfig.root, 'config')
-    assert appconfig.data_dir == os.path.join(appconfig.root, 'database')
-    assert appconfig.managed_config_dir == appconfig.config_dir
-
-
-def test_base_config_if_running_not_from_source(monkeypatch, mock_config_file):
-    # Simulated condition: running not from source, config_file is None.
-    monkeypatch.setattr(config, 'running_from_source', False)
-    appconfig = config.GalaxyAppConfiguration()
-    assert not appconfig.config_file
-    assert appconfig.config_dir == os.getcwd()
-    assert appconfig.data_dir == os.path.join(appconfig.config_dir, 'data')
-    assert appconfig.managed_config_dir == os.path.join(appconfig.data_dir, 'config')
+#@pytest.fixture(scope='module')
+#def appconfig():
+#    return config.GalaxyAppConfiguration()
+#
+#
+#@pytest.fixture
+#def mock_config_file(monkeypatch):
+#    # Patch this; otherwise tempfile.tempdir will be set, which is a global variable that
+#    # defines the value of the default `dir` argument to the functions in Python's
+#    # tempfile module - which breaks multiple tests.
+#    monkeypatch.setattr(config.GalaxyAppConfiguration, '_override_tempdir', lambda a, b: None)
+#    # Set this to return None to force the creation of base config directories
+#    # in _set_config_directories(). Used to test the values of these directories only.
+#    monkeypatch.setattr(config, 'find_config_file', lambda x: None)
+#
+#
+#def test_root(appconfig):
+#    assert appconfig.root == os.path.abspath('.')
+#
+#
+#def test_common_base_config(appconfig):
+#    assert appconfig.shed_tools_dir == os.path.join(appconfig.data_dir, 'shed_tools')
+#    assert appconfig.sample_config_dir == os.path.join(appconfig.root, 'lib', 'galaxy', 'config', 'sample')
+#
+#
+#def test_base_config_if_running_from_source(monkeypatch, mock_config_file):
+#    # Simulated condition: running from source, config_file is None.
+#    monkeypatch.setattr(config, 'running_from_source', True)
+#    appconfig = config.GalaxyAppConfiguration()
+#    assert not appconfig.config_file
+#    assert appconfig.config_dir == os.path.join(appconfig.root, 'config')
+#    assert appconfig.data_dir == os.path.join(appconfig.root, 'database')
+#    assert appconfig.managed_config_dir == appconfig.config_dir
+#
+#
+#def test_base_config_if_running_not_from_source(monkeypatch, mock_config_file):
+#    # Simulated condition: running not from source, config_file is None.
+#    monkeypatch.setattr(config, 'running_from_source', False)
+#    appconfig = config.GalaxyAppConfiguration()
+#    assert not appconfig.config_file
+#    assert appconfig.config_dir == os.getcwd()
+#    assert appconfig.data_dir == os.path.join(appconfig.config_dir, 'data')
+#    assert appconfig.managed_config_dir == os.path.join(appconfig.data_dir, 'config')
 
 
 def listify_strip(value):
