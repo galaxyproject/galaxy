1f12c7e42c8d0e4c0bc462743c2f606026f24af6
Kaivan Kamali <kxk302@psu.edu>
2020-09-08 12:17:17 -0400

    Made some changes based on code review feedback.

--- scripts/objectstore/copy_files_to_irods.py
+++ scripts/objectstore/copy_files_to_irods.py
@@ -1,11 +1,10 @@
+import argparse
 from datetime import datetime
-import getopt
 import json
 import os
 import uuid
 import shutil
 import subprocess
-import sys
 
 import irods.keywords as kw
 from irods.exception import CollectionDoesNotExist
@@ -300,67 +299,63 @@ def get_file_checksum(disk_file_path):
 
 
 def print_help_msg():
-    print("\nThis script copies datasets from Disk to Irods. It operates on a range of dataset IDs (start_dataset_id and end_dtatset_id, both inclusive).")
-    print("It also takes in the name of 3 JSON configuration files (A sample JSON configuration file is provided in the Python script folder).")
-    print("\n1) object_store_info_file: Has Disk object store info (e.g. files info).")
-    print("2) irods_info_file: irods connection info, and misc. info (zone, home, obejct store ID, etc.).")
-    print("3) db_connection_info: Galaxy DB connection info.")
-    print("\nIt also takes a flag copy_or_checksum.")
-    print("If copy: for all datasets from start_dataset_it to end_dataset_id, it copies disk files to irods. This includes extra folders that come with the output of commands like fastqc.")
-    print("If checksum: for all datasets from start_dataset_id to end_dataset_id, it calculates the disk file checksum, gets the irods file, and compares the disk file checksum to irods file checksum.")
-    print("If checksums match, it updates object_store_id dataset table (so Galaxy starts using the irods version of the file), then DELETES the file from disk.")
-    print("If the checksumdoes NOT match: it prints and error message, does NOT update the dataset table or delete the disk file, and continues with the next dataset in dataset ID range.")
-    print("\nIn order to run this Python3 script, you need to install the following packages via pip3:")
-    print("\tpip3 install galaxy.util")
-    print("\tpip3 install psycopg2")
-    print("\tpip3 install python_irodsclient")
-    print("\nLong-form input parameter specification:")
-    print("copy_files_to_irods --start_dataset_id=2 --end_dataset_id=3 --object_store_info_file=object_store_info.json --irods_info_file=irods_info.json --db_connection_info_file=db_connection_info.json --copy_or_checksum=<copy|checksum>")
-    print("\nOR")
-    print("\nShort-form input parameter specification:")
-    print("copy_files_to_irods -s 2 -e 3 -o object_store_info.json -i irods_info.json -d db_connection_info.json -c <copy|checksum>")
-    print("\n")
+    help_msg = """
+               This script copies datasets from Disk to Irods.
+               It operates on a range of dataset IDs (start_dataset_id and end_dtatset_id, both inclusive).
+               It also takes in the name of 3 JSON configuration files.
+               A sample JSON configuration file is provided in the Python script folder.
+
+               1) object_store_info_file: Has Disk object store info (e.g. files info).
+               2) irods_info_file: irods connection info, and misc. info (zone, home, obejct store ID, etc.).
+               3) db_connection_info: Galaxy DB connection info.
+
+               It also takes a flag copy_or_checksum.
+               If copy: for all datasets from start_dataset_it to end_dataset_id, it copies disk files to irods.
+                   This includes extra folders that come with the output of commands like fastqc.
+               If checksum: for all datasets from start_dataset_id to end_dataset_id, it calculates the disk file checksum.
+                   Then gets the irods file, and compares the disk file checksum to irods file checksum.
+                   If checksums match:
+                       It updates object_store_id dataset table (so Galaxy starts using the irods version of the file),
+                       then DELETES the file from disk.
+                   If the checksumdoes NOT match:
+                       it prints and error message, does NOT update the dataset table or delete the disk file,
+                       and continues with the next dataset in dataset ID range.
+
+               In order to run this Python3 script, you need to install the following packages via pip3:
+                   pip3 install galaxy.util
+                   pip3 install psycopg2
+                   pip3 install python_irodsclient
+
+               Long-form input parameter specification:
+               copy_files_to_irods --start_dataset_id=2 --end_dataset_id=3 --object_store_info_file=object_store_info.json
+                   --irods_info_file=irods_info.json --db_connection_info_file=db_connection_info.json --copy_or_checksum=<copy|checksum>
+
+               OR
+
+               Short-form input parameter specification:
+               copy_files_to_irods -s 2 -e 3 -o object_store_info.json -i irods_info.json -d db_connection_info.json -c <copy|checksum>
+               """
+    print(help_msg)
 
 
 if __name__ == '__main__':
-    start_dataset_id = None
-    end_dataset_id = None
-    object_store_info_file = None
-    irods_info_file = None
-    db_connection_info_file = None
-    copy_or_checksum = None
+    parser = argparse.ArgumentParser()
 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hs:e:o:i:d:c:", ["start_dataset_id=", "end_dataset_id=", "object_store_info_file=", "irods_info_file=", "db_connection_info_file=", "copy_or_checksum="])
-    except getopt.GetoptError:
-        print_help_msg()
-        sys.exit(2)
+    parser.add_argument('-s', '--start_dataset_id', type=int, required=True)
+    parser.add_argument('-e', '--end_dataset_id', type=int, required=True)
+    parser.add_argument('-o', '--object_store_info_file', type=str, required=True)
+    parser.add_argument('-i', '--irods_info_file', type=str, required=True)
+    parser.add_argument('-d', '--db_connection_info_file', type=str, required=True)
+    parser.add_argument('-c', '--copy_or_checksum', type=str, required=True, choices=['copy', 'checksum'])
 
-    for opt, arg in opts:
-        if opt == '-h':
-            print_help_msg()
-            sys.exit(2)
-        elif opt in ("-s", "--start_dataset_id"):
-            start_dataset_id = arg
-        elif opt in ("-e", "--end_datset_id"):
-            end_dataset_id = arg
-        elif opt in ("-o", "--object_store_info_file"):
-            object_store_info_file = arg
-        elif opt in ("-i", "--irods_info_file"):
-            irods_info_file = arg
-        elif opt in ("-d", "--db_connection-info-file"):
-            db_connection_info_file = arg
-        elif opt in ("-c", "--copy_or_checksum"):
-            copy_or_checksum = arg
+    args = parser.parse_args()
+    print(args)
 
-    if start_dataset_id is None or end_dataset_id is None or object_store_info_file is None or irods_info_file is None or db_connection_info_file is None or copy_or_checksum is None:
-        print("Error: Did not specify one of the required input parameters!")
-        print_help_msg()
-        sys.exit(2)
-
-    if copy_or_checksum != "copy" and copy_or_checksum != "checksum":
-        print("Error: Invalid value specified for copy_or_checksum input parameter {}".format(copy_or_checksum))
-        print_help_msg()
-        sys.exit(2)
+    start_dataset_id = args.start_dataset_id
+    end_dataset_id = args.end_dataset_id
+    object_store_info_file = args.object_store_info_file
+    irods_info_file = args.irods_info_file
+    db_connection_info_file = args.db_connection_info_file
+    copy_or_checksum = args.copy_or_checksum
 
     copy_files_to_irods(start_dataset_id=start_dataset_id, end_dataset_id=end_dataset_id, object_store_info_file=object_store_info_file, irods_info_file=irods_info_file, db_connection_info_file=db_connection_info_file, copy_or_checksum=copy_or_checksum)
