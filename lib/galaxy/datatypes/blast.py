71aa04c7361e8f5952687cb3c198a397e9b36a5c
RomainDallet <romain.dallet@sb-roscoff.fr>
2020-09-09 08:58:44 +0200

    Change description

--- lib/galaxy/datatypes/blast.py
+++ lib/galaxy/datatypes/blast.py
@@ -334,8 +334,8 @@ class LastDb(Data):
     def __init__(self, **kwd):
         Data.__init__(self, **kwd)
         self.add_composite_file('lastdb.bck', is_binary=True)
-        self.add_composite_file('lastdb.des', is_binary=False)  # description file
-        self.add_composite_file('lastdb.prj', is_binary=False)  # project resume
+        self.add_composite_file('lastdb.des', description="Description file", is_binary=False)
+        self.add_composite_file('lastdb.prj', description="Project resume file", is_binary=False)
         self.add_composite_file('lastdb.sds', is_binary=True)
         self.add_composite_file('lastdb.ssp', is_binary=True)
         self.add_composite_file('lastdb.suf', is_binary=True)
