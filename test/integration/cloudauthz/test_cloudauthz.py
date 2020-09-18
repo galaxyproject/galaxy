29aad8212365c56ddec947adb46eac6d9a89762b
mvdbeek <m.vandenbeek@gmail.com>
2020-09-18 12:23:56 +0200

    Minor test cleanup

--- test/integration/cloudauthz/test_cloudauthz.py
+++ test/integration/cloudauthz/test_cloudauthz.py
@@ -1,5 +1,4 @@
 """
-
 You may run this test using the following command:
 ./run_tests.sh test/integration/cloudauthz/test_cloudauthz.py:DefineCloudAuthzTestCase.test_post_cloudauthz_without_authn -s
 """
@@ -9,17 +8,8 @@ import json
 from galaxy_test.driver import integration_util
 
 
-class BaseCloudAuthzTestCase(integration_util.IntegrationTestCase):
+class DefineCloudAuthzTestCase(integration_util.IntegrationTestCase):
     framework_tool_and_types = True
-
-    def setUp(self):
-        super(BaseCloudAuthzTestCase, self).setUp()
-
-
-class DefineCloudAuthzTestCase(BaseCloudAuthzTestCase):
-
-    def setUp(self):
-        super(DefineCloudAuthzTestCase, self).setUp()
 
     def test_post_cloudauthz_without_authn(self):
         """
