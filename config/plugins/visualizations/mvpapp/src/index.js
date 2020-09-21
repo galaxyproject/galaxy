016dd80412bdeac5cdc5fbeade84f2aec65821c2
Dannon Baker <dannon.baker@gmail.com>
2020-09-21 13:55:06 -0400

    Swap bootstrap to npm-installed

--- config/plugins/visualizations/mvpapp/src/index.js
+++ config/plugins/visualizations/mvpapp/src/index.js
@@ -1,19 +1,19 @@
 import "./css/jquery-ui.min.css";
 import "./css/lorikeet.css";
-// TODO: this previously lumped in bootstrap3 styling; include explicitly.
 import "./css/datatables.min.css";
 import "./css/msi.css";
 import "./css/app.css";
 import "./css/igv.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "script-loader!./js/jquery.min.js";
 import "script-loader!./js/jquery-ui.min.js";
+
+import "bootstrap";
 import "script-loader!./js/popper.min.js";
-import "script-loader!./js/bootstrap.min.js";
 import "script-loader!./js/plotly-latest.min.js";
 import "script-loader!./js/datatables.min.js";
 import "script-loader!./js/d3.min.js";
 import "script-loader!./js/igv.js";
 import "script-loader!./js/msms_graphs.js";
 import "script-loader!./js/mvpapp.js";
-
