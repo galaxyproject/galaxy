fb63bf87e16abc326b0b10cc46f17be0c75ac0bb
Dannon Baker <dannon.baker@gmail.com>
2020-09-21 09:40:36 -0400

    Include base datatables style in place for now.

--- config/plugins/visualizations/mvpapp/src/index.js
+++ config/plugins/visualizations/mvpapp/src/index.js
@@ -1,7 +1,7 @@
 import "./css/jquery-ui.min.css";
 import "./css/lorikeet.css";
-// TODO: this lumps in bootstrap3 styling; use manual styling.
-//import "./css/datatables.min.css";
+// TODO: this previously lumped in bootstrap3 styling; include explicitly.
+import "./css/datatables.min.css";
 import "./css/msi.css";
 import "./css/app.css";
 import "./css/igv.css";
