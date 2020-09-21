0a3c70913f3272dd28b8b78308e01f44d592c266
Nicola Soranzo <nicola.soranzo@gmail.com>
2020-09-21 22:54:20 +0100

    Fix use of imported `exceptions` module
    It was working by chance because of the presence of another import (`import galaxy.datatypes.metadata`).

--- lib/galaxy/managers/datasets.py
+++ lib/galaxy/managers/datasets.py
@@ -55,7 +55,7 @@ class DatasetManager(base.ModelManager, secured.AccessibleManagerMixin, deletabl
         return dataset
 
     def copy(self, dataset, **kwargs):
-        raise galaxy.exceptions.NotImplemented('Datasets cannot be copied')
+        raise exceptions.NotImplemented('Datasets cannot be copied')
 
     def purge(self, dataset, flush=True):
         """
@@ -271,7 +271,7 @@ class DatasetAssociationManager(base.ModelManager,
         return dataset_assoc
 
     def by_user(self, user):
-        raise galaxy.exceptions.NotImplemented('Abstract Method')
+        raise exceptions.NotImplemented('Abstract Method')
 
     # .... associated job
     def creating_job(self, dataset_assoc):
@@ -442,7 +442,7 @@ class DatasetAssociationManager(base.ModelManager,
             self._set_permissions(trans, dataset_assoc, role_ids_dict)
 
     def _set_permissions(self, trans, dataset_assoc, roles_dict):
-        raise galaxy.exceptions.NotImplemented()
+        raise exceptions.NotImplemented()
 
 
 class _UnflattenedMetadataDatasetAssociationSerializer(base.ModelSerializer,
@@ -687,13 +687,13 @@ class DatasetAssociationDeserializer(base.ModelDeserializer, deletable.PurgableD
 
     def deserialize_datatype(self, item, key, val, **context):
         if not item.datatype.allow_datatype_change:
-            raise galaxy.exceptions.RequestParameterInvalidException("The current datatype does not allow datatype changes.")
+            raise exceptions.RequestParameterInvalidException("The current datatype does not allow datatype changes.")
         if not self.app.datatypes_registry.get_datatype_by_extension(val):
-            raise galaxy.exceptions.RequestParameterInvalidException("The target datatype does not exist.")
+            raise exceptions.RequestParameterInvalidException("The target datatype does not exist.")
         if not self.app.datatypes_registry.get_datatype_by_extension(val).allow_datatype_change:
-            raise galaxy.exceptions.RequestParameterInvalidException("The target datatype does not allow datatype changes.")
+            raise exceptions.RequestParameterInvalidException("The target datatype does not allow datatype changes.")
         if not DatasetAssociationManager(self.app).ok_to_edit_metadata(item.dataset_id):
-            raise galaxy.exceptions.RequestParameterInvalidException("Dataset metadata could not be updated, probably because it is not in an 'ok' state.")
+            raise exceptions.RequestParameterInvalidException("Dataset metadata could not be updated, probably because it is not in an 'ok' state.")
         item.change_datatype(val)
         context['trans'].sa_session.flush()
         self.app.datatypes_registry.set_external_metadata_tool.tool_action.execute(self.app.datatypes_registry.set_external_metadata_tool, context['trans'], incoming={'input1': item}, overwrite=False)  # overwrite is False as per existing behavior
