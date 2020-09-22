fa5bc510310db4dd87b59313772b33ded3b019ba
Assunta DeSanto <assuntad23@gmail.com>
2020-09-22 11:12:24 -0400

    Converting Collections to Vue WIP

--- client/src/mvc/collection/mixins/CollectionCreatorModalMixin.vue
+++ client/src/mvc/collection/mixins/CollectionCreatorModalMixin.vue
@@ -14,7 +14,7 @@ export default {
             const creatorOptions = _.defaults(options || {}, {
                 oncancel: function () {
                     modal.hide();
-                    this.$destroy();
+                    deferred.reject("cancelled");
                 },
                 oncreate: function (creator, response) {
                     modal.hide();
@@ -36,5 +36,3 @@ export default {
     },
 };
 </script>
-
-<style></style>
