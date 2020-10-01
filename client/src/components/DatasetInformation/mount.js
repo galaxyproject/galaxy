857c99efe92d54810b0effd8508c2edd3440db37
Oleg Zharkov <oleg.zharkov22@gmail.com>
2020-10-01 14:17:51 +0200

    get rid of jquery

--- client/src/components/DatasetInformation/mount.js
+++ client/src/components/DatasetInformation/mount.js
@@ -1,13 +1,12 @@
 /**
  * Endpoint for mounting job metrics from non-Vue environment.
  */
-import $ from "jquery";
 import DatasetInformation from "./DatasetInformation";
 import { mountVueComponent } from "utils/mountVueComponent";
 
 export const mountDatasetInformation = (propsData = {}) => {
-    $(".dataset-information").each((index, el) => {
-        propsData.hda_id = $(el).attr("hda_id");
-        mountVueComponent(DatasetInformation)(propsData, el);
+    document.querySelectorAll(".dataset-information").forEach((element) => {
+        propsData.hda_id = element.getAttribute("hda_id");
+        mountVueComponent(DatasetInformation)(propsData, element);
     });
 };
