b9f179a91c693ee70e79a70300062a029c6d7be0
Dannon Baker <dannon.baker@gmail.com>
2020-10-07 13:29:41 -0400

    Minor cleanup anvil.py

--- lib/galaxy/files/sources/anvil.py
+++ lib/galaxy/files/sources/anvil.py
@@ -3,12 +3,17 @@ try:
 except ImportError as e:
     AnVILFS = None
 from ._pyfilesystem2 import PyFilesystem2FilesSource
+
+
 class AnVILFilesSource(PyFilesystem2FilesSource):
     plugin_type = 'anvil'
     required_module = AnVILFS
     required_package = "fs.anvilfs"
+
     def _open_fs(self, user_context):
         props = self._serialization_props(user_context)
         handle = AnVILFS(**props)
         return handle
-__all__ = ('AnVILFilesSource',)
+
+
+__all__ = ('AnVILFilesSource', )
