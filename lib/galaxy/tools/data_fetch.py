4b7a974a2bd99257515b72de5122d159fcb1b382
mvdbeek <m.vandenbeek@gmail.com>
2020-10-15 16:31:37 +0200

    Quickfix for workflow testing

--- lib/galaxy/tools/data_fetch.py
+++ lib/galaxy/tools/data_fetch.py
@@ -352,7 +352,8 @@ def _has_src_to_path(upload_config, item, is_dataset=False):
             name = "Pasted Entry"
     else:
         assert src == "path"
-        path = item["path"]
+        path = item.get("path", item.get("name"))
+        assert path
         if name is None:
             name = os.path.basename(path)
     return name, path
