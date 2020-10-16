794ab8dc5c5fbba743d95c761b1a01bc17c0f9c3
John Chilton <jmchilton@gmail.com>
2020-10-16 14:32:13 -0400

    Citations: Add service layer to reuse citation fetching and parsing.
    Also make it easier to unit test in the future.

---
+++ client/src/components/Citation/Citation.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-html="citationHtml"></div>
+</template>
+
+<script>
+export default {
+    props: {
+        citation: {
+            type: Object,
+        },
+        outputFormat: {
+            type: String,
+        },
+    },
+    computed: {
+        citationHtml() {
+            const citation = this.citation;
+            const cite = citation.cite;
+            let link = "";
+            if (cite.data && cite.data[0] && cite.data[0].URL) {
+                link = `&nbsp<a href="${cite.data[0].URL}" target="_blank">[Link]</a>`;
+            }
+            const formattedCitation = cite.format(this.outputFormat, {
+                format: "html",
+                template: "apa",
+                lang: "en-US",
+                append: link,
+            });
+            return formattedCitation;
+        },
+    },
+};
+</script>
