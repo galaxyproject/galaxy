24f001fcd0308aaf31fe749f2a3b7ed2983269ec
Kaivan Kamali <kxk302@psu.edu>
2020-10-22 16:16:45 -0400

    Use singular and plural for minutes and seconds in job metrics formatting

--- lib/galaxy/job_metrics/formatting.py
+++ lib/galaxy/job_metrics/formatting.py
@@ -12,9 +12,25 @@ def seconds_to_str(value):
     """Convert seconds to a simple simple string describing the amount of time."""
     mins, secs = divmod(value, 60)
     hours, mins = divmod(mins, 60)
-    if value < 60:
+
+    if value == 0:
+        return "0 seconds"
+    elif value == 1:
+        return "1 second"
+    elif value < 60:
         return f"{secs} seconds"
-    elif value < 3600:
+    elif value >= 60 and value <= 119:
+        return "1 minute"
+    elif value >= 120 and value < 3600:
         return f"{mins} minutes"
+    elif value >= 3600 and value < 7200:
+        if mins == 1:
+            return "1 hour and 1 minute"
+        else:
+            return f"1 hour and {mins} minutes"
     else:
-        return f"{hours} hours and {mins} minutes"
+        if mins == 1:
+            return f"{hours} hours and 1 minute"
+        else:
+            return f"{hours} hours and {mins} minutes"
+
