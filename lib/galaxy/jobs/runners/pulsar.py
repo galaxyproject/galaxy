1ca30af5d836921c2811b3b1586ea2ed5ec09b51
Nate Coraor <nate@bx.psu.edu>
2020-10-28 15:30:30 -0400

    Don't check Pulsar MQ job state in DB if it doesn't have any entry points, and stop checking once the job is running and ports are set.

--- lib/galaxy/jobs/runners/pulsar.py
+++ lib/galaxy/jobs/runners/pulsar.py
@@ -265,19 +265,25 @@ class PulsarJobRunner(AsynchronousJobRunner):
         if self.use_mq:
             # Might still need to check pod IPs.
             job_wrapper = job_state.job_wrapper
-            persisted_state = job_wrapper.get_state()
-            if persisted_state in model.Job.terminal_states + [model.Job.states.DELETED_NEW]:
-                log.debug("(%s) Watched job in terminal state, will stop monitoring: %s", job_state.job_id, persisted_state)
-                return None
             guest_ports = job_wrapper.guest_ports
             if len(guest_ports) > 0:
-                client = self.get_client_from_state(job_state)
-                job_ip = client.job_ip()
-                if job_ip:
-                    ports_dict = {}
-                    for guest_port in guest_ports:
-                        ports_dict[str(guest_port)] = dict(host=job_ip, port=guest_port, protocol="http")
-                    self.app.interactivetool_manager.configure_entry_points(job_wrapper.get_job(), ports_dict)
+                persisted_state = job_wrapper.get_state()
+                if persisted_state in model.Job.terminal_states + [model.Job.states.DELETED_NEW]:
+                    log.debug("(%s) Watched job in terminal state, will stop monitoring: %s", job_state.job_id, persisted_state)
+                    job_state = None
+                elif persisted_state == model.Job.states.RUNNING:
+                    client = self.get_client_from_state(job_state)
+                    job_ip = client.job_ip()
+                    if job_ip:
+                        ports_dict = {}
+                        for guest_port in guest_ports:
+                            ports_dict[str(guest_port)] = dict(host=job_ip, port=guest_port, protocol="http")
+                        self.app.interactivetool_manager.configure_entry_points(job_wrapper.get_job(), ports_dict)
+                        log.debug("(%s) Got ports for entry point: %s", job_state.job_id, str(ports_dict))
+                        job_state = None
+            else:
+                # No need to monitor MQ jobs that have no entry points
+                job_state = None
             return job_state
         else:
             return self.check_watched_item_state(job_state)
