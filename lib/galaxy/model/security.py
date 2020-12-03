6c4c0ddad620a5443e389676b93b5c011db48dfc
John Chilton <jmchilton@gmail.com>
2020-12-03 09:43:40 -0500

    Some extra debugging for API tests?

--- lib/galaxy/model/security.py
+++ lib/galaxy/model/security.py
@@ -677,7 +677,7 @@ class GalaxyRBACAgent(RBACAgent):
                               .filter(and_(self.model.UserRoleAssociation.table.c.user_id == user.id,
                                            self.model.Role.table.c.id == self.model.UserRoleAssociation.table.c.role_id,
                                            self.model.Role.table.c.type == self.model.Role.types.PRIVATE)) \
-                              .first()
+                              .one_or_none()
         if not role:
             if auto_create:
                 return self.create_private_user_role(user)
