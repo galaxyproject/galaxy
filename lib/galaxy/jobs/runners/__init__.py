281971703e7457e29d3535d2ac907f0a93b96f12
dan <dan@portalmedia.com>
2020-12-09 14:30:23 -0600

    Add error handling and debugging info to queue

--- lib/galaxy/jobs/runners/__init__.py
+++ lib/galaxy/jobs/runners/__init__.py
@@ -650,9 +650,21 @@ class AsynchronousJobRunner(BaseJobRunner, Monitors):
         """
         new_watched = []
         for async_job_state in self.watched:
-            new_async_job_state = self.check_watched_item(async_job_state)
-            if new_async_job_state:
-                new_watched.append(new_async_job_state)
+            # AMPPD - don't fail the whole thing if we have a single error. 
+            try:
+                new_async_job_state = self.check_watched_item(async_job_state)
+                if new_async_job_state:
+                    new_watched.append(new_async_job_state)
+            except Exception as e:
+                log.exception('AMPPD: Unhandled exception checking watched item')
+                log.debug(str(e))
+                try:
+                    log.debug("Async Job Id: " + str(async_job_state.job_wrapper.job_id)
+                    log.debug("*** Async Job State: ****")
+                    log.debug(repr(async_job_state))
+                    log.debug("*** End Async Job State: ****")
+                except Exception:
+                    log.debug("Could not print job details");
         self.watched = new_watched
 
     # Subclasses should implement this unless they override check_watched_items all together.
