a8d195c2ea9de6d94603f28bb57deea14f1eaae5
Mason <nerdinacan@gmail.com>
2020-12-17 08:41:53 -0800

    suppress bootstrap warnings during unit testing

--- client/tests/jest/jest.setup.js
+++ client/tests/jest/jest.setup.js
@@ -3,3 +3,7 @@ import "@testing-library/jest-dom";
 /* still don't understand what was invoking the following, but nothing should,
 and this makes the tag tests work correctly */
 global.XMLHttpRequest = undefined;
+
+// stops Bootstrap's warnings when components can't find a non-existent doc
+// https://github.com/bootstrap-vue/bootstrap-vue/issues/3303
+process.env.BOOTSTRAP_VUE_NO_WARN = true;
