852edbd17dafdab0d04a888113e852df93116914
mvdbeek <m.vandenbeek@gmail.com>
2021-01-05 11:51:00 +0100

    Add Invocations unit tests

--- client/src/components/Workflow/Invocations.vue
+++ client/src/components/Workflow/Invocations.vue
@@ -8,7 +8,7 @@
         </b-alert>
         <loading-span v-if="loading" message="Loading workflow invocations" />
         <div v-else>
-            <b-alert v-if="!invocationItemsComputed.length" variant="info" show>
+            <b-alert id="no-invocations" v-if="!invocationItemsComputed.length" variant="info" show>
                 {{ noInvocationsMessage }}
             </b-alert>
             <b-table
@@ -34,12 +34,19 @@
                     </b-card>
                 </template>
                 <template v-slot:cell(workflow_id)="data">
-                    <b-link v-b-tooltip title="Show Invocation details" href="#" @click.stop="swapRowDetails(data)">
+                    <b-link
+                        id="toggle-invocation-details"
+                        v-b-tooltip
+                        title="Show Invocation details"
+                        href="#"
+                        @click.stop="swapRowDetails(data)"
+                    >
                         <b>{{ getWorkflowNameByInstanceId(data.item.workflow_id) }}</b>
                     </b-link>
                 </template>
                 <template v-slot:cell(history_id)="data">
                     <b-link
+                        id="switch-to-history"
                         v-b-tooltip
                         title="Switch to History"
                         href="#"
@@ -57,6 +64,7 @@
                 <template v-slot:cell(execute)="data">
                     <b-button
                         v-b-tooltip.hover.bottom
+                        id="run-workflow"
                         title="Run Workflow"
                         class="workflow-run btn-sm btn-primary fa fa-play"
                         @click.stop="executeWorkflow(getWorkflowByInstanceId(data.item.workflow_id).id)"
@@ -85,7 +93,7 @@ export default {
     props: {
         invocationItems: { type: Array, default: () => [] },
         loading: { type: Boolean, default: true },
-        noInvocationsMessage: { type: String, default: "" },
+        noInvocationsMessage: { type: String, default: "No Workflow Invocations to display" },
         headerMessage: { type: String, default: "" },
         ownerGrid: { type: Boolean, default: true },
     },
