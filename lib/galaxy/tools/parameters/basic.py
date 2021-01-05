865736aeca7855df6bc6b4700216cd2f19b5d144
Kaivan Kamali <kxk302@psu.edu>
2021-01-05 08:24:59 -0500

    Handled case where we have a dataset collection

--- lib/galaxy/tools/parameters/basic.py
+++ lib/galaxy/tools/parameters/basic.py
@@ -1339,10 +1339,22 @@ class ColumnListParameter(SelectToolParameter):
         legal_values = self.get_column_list(trans, other_values)
 
         hda = other_values.get(self.data_ref)
-        if hda is not None and hda.dataset.get_size() == 0:
-            value = other_values.get(self.name)
-            if value is not None:
-                legal_values.append(value)
+        if isinstance(hda, list):
+            # hda is a list of HistoryDatasetCollectionAssociation
+            for idx1 in range(len(hda)):
+                # dataset_instances is a list of HistoryDatasetAssociation
+                dataset_instances = hda[idx1].dataset_instances
+                for idx2 in range(len(dataset_instances)):
+                    if dataset_instances[idx2] is not None and dataset_instances[idx2].get_size() == 0:
+                        value = other_values.get(self.name)
+                        if value is not None:
+                            legal_values.append(value)
+        else:
+            # hda is a HistoryDatasetAssociation
+            if hda.dataset.get_size() == 0:
+                value = other_values.get(self.name)
+                if value is not None:
+                    legal_values.append(value)
 
         return set(legal_values)
 
