bba7b8e0f4f0ee8a2f73e1cf3c39db991fb0be36
Simon Bray <sbray1371@gmail.com>
2021-01-07 11:07:49 +0100

    add empty preferences attribute to BootstrapAdminUser

--- lib/galaxy/schema/__init__.py
+++ lib/galaxy/schema/__init__.py
@@ -3,9 +3,10 @@ import typing
 from pydantic import BaseModel
 
 
-class BoostrapAdminUser(BaseModel):
+class BootstrapAdminUser(BaseModel):
     id = 0
     email: typing.Optional[str] = None
+    preferences: typing.Dict[str, str] = {}
     bootstrap_admin_user = True
 
     def all_roles(*args) -> list:
