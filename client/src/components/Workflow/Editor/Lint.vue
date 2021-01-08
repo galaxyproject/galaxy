312ef4ec005697fcd5e37be52c5ebea88acd431d
guerler <aysam.guerler@gmail.com>
2021-01-08 19:44:59 +0100

    Create linear lint result listing

--- client/src/components/Workflow/Editor/Lint.vue
+++ client/src/components/Workflow/Editor/Lint.vue
@@ -1,215 +1,218 @@
 <template>
-    <b-container>
-        <h4>Validation</h4>
-        <div v-if="showFixAll">
-            <a href="#" @click="autoFixAll">Attempt to fix any issue that can be automatically fixed.</a>
-        </div>
-        
-        <div v-if="parametersOkay">
-            <p>All workflow parameters (if any are defined) are using formal inputs.{{ whyInputs }}</p>
-        </div>
-        <div else>
-            <ul>
-                <li v-for="(item, idx) in legacyParametersArray" :key="idx">
-                    {{ item.name }}
-                    <font-awesome-icon
-                        v-if="item.canExtract"
-                        icon="cog"
-                        v-b-tooltip.hover
-                        title="Attempt to create a workflow input connected to this legacy parameter."
-                        @click="extractLegacyParameter(item)"
-                    />
-                </li>
-            </ul>
-            <em class="small">
-                <p>
-                    This workflow uses older-style implicit Galaxy workflow parameters (e.g. defining
-                    <tt>${parameter}</tt> on tool steps in the workflows). These should be replaced with
-                    formal workflow inputs. {{ whyInputs }}
-                </p>
-                <p>
-                    Many such legacy workflow parameters can be automatically converted to formal input
-                    steps by Galaxy, click the <font-awesome-icon icon="cog" /> to have Galaxy attempt
-                    this.
-                </p>
-            </em>
-        </div> 
-        <div v-if="disconnectInputsOkay">
-            All non-optional inputs to workflow steps are connected to formal workflow inputs.
-            {{ whyInputs }}
-        </div>
-        <div else>
-            <ul>
-                <li
-                    v-for="(input, idx) in disconnectedInputs"
-                    :key="idx"
-                    @mouseover="highlight(input.stepId)"
-                    @mouseleave="unhighlight(input.stepId)"
-                >
-                    <b @click="scrollTo(input.stepId)" class="scrolls"
-                        ><i :class="input.stepIconClass" />{{ input.stepLabel }}</b
-                    >
-                    <span class="disconnected-input-name">
-                        {{ input.inputLabel }}
+    <b-card header-tag="header" body-class="p-0">
+        <template #header>
+            <div class="mb-0 font-weight-bold">Validation Results</div>
+            <div v-if="showFixAll">
+                <a href="#" @click="autoFixAll">Attempt to fix any issue that can be automatically fixed.</a>
+            </div>
+        </template>
+        <b-card-body>
+            <div v-if="parametersOkay">
+                <p>All workflow parameters (if any are defined) are using formal inputs.{{ whyInputs }}</p>
+            </div>
+            <div else>
+                <ul>
+                    <li v-for="(item, idx) in legacyParametersArray" :key="idx">
+                        {{ item.name }}
                         <font-awesome-icon
-                            v-if="input.canExtract"
+                            v-if="item.canExtract"
                             icon="cog"
                             v-b-tooltip.hover
-                            title="Attempt to create a workflow input connected to this step input."
-                            @click="extractWorkflowInput(input)"
+                            title="Attempt to create a workflow input connected to this legacy parameter."
+                            @click="extractLegacyParameter(item)"
                         />
-                    </span>
-                </li>
-            </ul>
-            <em class="small">
-                <p>
-                    Inputs to tool and subworkflow steps should be connected to formal workflow inputs.
-                    {{ whyInputs }}
-                </p>
-                <p>
-                    Most such step inputs can be automatically connected via Galaxy, click the
-                    <font-awesome-icon icon="cog" /> for inputs to have Galaxy attempt this.
-                </p>
-            </em>
-        </div>
-        <div v-if="inputsMetadataOkay">
-            All workflow inputs have labels and annotations.
-        </div>
-        <div v-else>
-            <p>
-                Some workflow inputs are missing labels and/or annotations.
-            </p>
-            <ul>
-                <li
-                    v-for="(input, idx) in inputsMissingMetadata"
-                    :key="idx"
-                    @mouseover="highlight(input.stepId)"
-                    @mouseleave="unhighlight(input.stepId)"
-                >
-                    <b @click="scrollTo(input.stepId)" class="scrolls"
-                        ><i :class="input.stepIconClass" />{{ input.stepLabel }}</b
+                    </li>
+                </ul>
+                <em class="small">
+                    <p>
+                        This workflow uses older-style implicit Galaxy workflow parameters (e.g. defining
+                        <tt>${parameter}</tt> on tool steps in the workflows). These should be replaced with
+                        formal workflow inputs. {{ whyInputs }}
+                    </p>
+                    <p>
+                        Many such legacy workflow parameters can be automatically converted to formal input
+                        steps by Galaxy, click the <font-awesome-icon icon="cog" /> to have Galaxy attempt
+                        this.
+                    </p>
+                </em>
+            </div> 
+            <div v-if="disconnectInputsOkay">
+                All non-optional inputs to workflow steps are connected to formal workflow inputs.
+                {{ whyInputs }}
+            </div>
+            <div else>
+                <ul>
+                    <li
+                        v-for="(input, idx) in disconnectedInputs"
+                        :key="idx"
+                        @mouseover="highlight(input.stepId)"
+                        @mouseleave="unhighlight(input.stepId)"
                     >
-                    <em class="small">
-                        <span class="input-missing-metadata-summary">
-                            This input is {{ missingSummary(input) }}.
+                        <b @click="scrollTo(input.stepId)" class="scrolls"
+                            ><i :class="input.stepIconClass" />{{ input.stepLabel }}</b
+                        >
+                        <span class="disconnected-input-name">
+                            {{ input.inputLabel }}
+                            <font-awesome-icon
+                                v-if="input.canExtract"
+                                icon="cog"
+                                v-b-tooltip.hover
+                                title="Attempt to create a workflow input connected to this step input."
+                                @click="extractWorkflowInput(input)"
+                            />
                         </span>
-                    </em>
-                </li>
-            </ul>
-        </div>
-        <div v-if="outputsOkay">
-            This workflow has outputs and they all have valid labels.
-        </div>
-        <div v-else-if="outputs.length == 0">
-            This workflow has no labeled outputs, please select and label at least one output.
+                    </li>
+                </ul>
+                <em class="small">
+                    <p>
+                        Inputs to tool and subworkflow steps should be connected to formal workflow inputs.
+                        {{ whyInputs }}
+                    </p>
+                    <p>
+                        Most such step inputs can be automatically connected via Galaxy, click the
+                        <font-awesome-icon icon="cog" /> for inputs to have Galaxy attempt this.
+                    </p>
+                </em>
+            </div>
+            <div v-if="inputsMetadataOkay">
+                All workflow inputs have labels and annotations.
+            </div>
+            <div v-else>
+                <p>
+                    Some workflow inputs are missing labels and/or annotations.
+                </p>
+                <ul>
+                    <li
+                        v-for="(input, idx) in inputsMissingMetadata"
+                        :key="idx"
+                        @mouseover="highlight(input.stepId)"
+                        @mouseleave="unhighlight(input.stepId)"
+                    >
+                        <b @click="scrollTo(input.stepId)" class="scrolls"
+                            ><i :class="input.stepIconClass" />{{ input.stepLabel }}</b
+                        >
+                        <em class="small">
+                            <span class="input-missing-metadata-summary">
+                                This input is {{ missingSummary(input) }}.
+                            </span>
+                        </em>
+                    </li>
+                </ul>
+            </div>
+            <div v-if="outputsOkay">
+                This workflow has outputs and they all have valid labels.
+            </div>
+            <div v-else-if="outputs.length == 0">
+                This workflow has no labeled outputs, please select and label at least one output.
 
-            <em class="small">
-                <p>
-                    Formal, labeled outputs make tracking workflow provenance, usage within
-                    subworkflows, and executing the workflow via the Galaxy API all more robust.
-                </p>
-            </em>
-        </div>
-        <div v-else>
-            <p>
-                The following workflow outputs have no labels, they should be assigned a useful label or
-                unchecked in the workflow editor to mark them as no longer being a workflow output.
-                Alternatively,
-                <a href="#" @click="removeUnlabeledWorkflowOutputs"
-                    >click here <font-awesome-icon icon="cog"
-                /></a>
-                to remove these all as workflow outputs.
-            </p>
-            <ul>
-                <li
-                    v-for="(output, idx) in unlabeledOutputs"
-                    :key="idx"
-                    @mouseover="highlight(output.stepId)"
-                    @mouseleave="unhighlight(output.stepId)"
-                >
-                    <b @click="scrollTo(output.stepId)" class="scrolls"
-                        ><i :class="output.stepIconClass" />{{ output.stepLabel }}</b
-                    >
-                    <span class="unlabeled-output-name">
-                        {{ output.outputName }}
-                        <!--
-                    <font-awesome-icon icon="cog"
-                                        v-b-tooltip.hover
-                                        title="Unmark this output as a workflow output."
-                                        @click="extractWorkflowInput(output)"
-                    />
-                    -->
-                    </span>
-                </li>
-            </ul>
-        </div>
-        <span>
-            <div v-if="annotationOkay">
-                <p>
-                    This workflow defines an annotation. Ideally, this helps the executors of the workflow
-                    understand the purpose and usage of the workflow.
-                </p>
-            </div>
-            <div v-else>
-                <p>
-                    <a href="#" @click="onAttributes"
-                        ><font-awesome-icon icon="pencil-alt" />Edit workflow attributes</a
-                    >
-                    to add an annotation.
-                </p>
                 <em class="small">
                     <p>
-                        This workflow does not define an annotation. This should provided to help the
-                        executors of the workflow understand the purpose and usage of the workflow.
+                        Formal, labeled outputs make tracking workflow provenance, usage within
+                        subworkflows, and executing the workflow via the Galaxy API all more robust.
                     </p>
                 </em>
             </div>
-        </span>
-        <span>
-            <div v-if="licenseOkay">
-                <p>
-                    This workflow defines a license.
-                </p>
-            </div>
             <div v-else>
                 <p>
-                    <a href="#" @click="onAttributes"
-                        ><font-awesome-icon icon="pencil-alt" />Edit workflow attributes</a
+                    The following workflow outputs have no labels, they should be assigned a useful label or
+                    unchecked in the workflow editor to mark them as no longer being a workflow output.
+                    Alternatively,
+                    <a href="#" @click="removeUnlabeledWorkflowOutputs"
+                        >click here <font-awesome-icon icon="cog"
+                    /></a>
+                    to remove these all as workflow outputs.
+                </p>
+                <ul>
+                    <li
+                        v-for="(output, idx) in unlabeledOutputs"
+                        :key="idx"
+                        @mouseover="highlight(output.stepId)"
+                        @mouseleave="unhighlight(output.stepId)"
                     >
-                    to specify a license.
-                </p>
-                <em class="small">
+                        <b @click="scrollTo(output.stepId)" class="scrolls"
+                            ><i :class="output.stepIconClass" />{{ output.stepLabel }}</b
+                        >
+                        <span class="unlabeled-output-name">
+                            {{ output.outputName }}
+                            <!--
+                        <font-awesome-icon icon="cog"
+                                            v-b-tooltip.hover
+                                            title="Unmark this output as a workflow output."
+                                            @click="extractWorkflowInput(output)"
+                        />
+                        -->
+                        </span>
+                    </li>
+                </ul>
+            </div>
+            <span>
+                <div v-if="annotationOkay">
                     <p>
-                        This workflow does not specify a license. This is important metadata for workflows
-                        that will be published and/or shared to help workflow executors understand how it
-                        may be used.
+                        This workflow defines an annotation. Ideally, this helps the executors of the workflow
+                        understand the purpose and usage of the workflow.
                     </p>
-                </em>
-            </div>
-        </span>
-        <span>
-            <div v-if="creatorOkay">
-                <p>
-                    This workflow defines creator information.
-                </p>
-            </div>
-            <div v-else>
-                <p>
-                    <a href="#" @click="onAttributes"
-                        ><font-awesome-icon icon="pencil-alt" />Edit workflow attributes</a
-                    >
-                    to specify creator(s).
-                </p>
-                <em class="small">
+                </div>
+                <div v-else>
                     <p>
-                        This workflow does not specify creator(s). This is important metadata for workflows
-                        that will be published and/or shared to help workflow executors know how to cite the
-                        workflow authors.
+                        <a href="#" @click="onAttributes"
+                            ><font-awesome-icon icon="pencil-alt" />Edit workflow attributes</a
+                        >
+                        to add an annotation.
                     </p>
-                </em>
-            </div>
-        </span>
-    </b-container>
+                    <em class="small">
+                        <p>
+                            This workflow does not define an annotation. This should provided to help the
+                            executors of the workflow understand the purpose and usage of the workflow.
+                        </p>
+                    </em>
+                </div>
+            </span>
+            <span>
+                <div v-if="licenseOkay">
+                    <p>
+                        This workflow defines a license.
+                    </p>
+                </div>
+                <div v-else>
+                    <p>
+                        <a href="#" @click="onAttributes"
+                            ><font-awesome-icon icon="pencil-alt" />Edit workflow attributes</a
+                        >
+                        to specify a license.
+                    </p>
+                    <em class="small">
+                        <p>
+                            This workflow does not specify a license. This is important metadata for workflows
+                            that will be published and/or shared to help workflow executors understand how it
+                            may be used.
+                        </p>
+                    </em>
+                </div>
+            </span>
+            <span>
+                <div v-if="creatorOkay">
+                    <p>
+                        This workflow defines creator information.
+                    </p>
+                </div>
+                <div v-else>
+                    <p>
+                        <a href="#" @click="onAttributes"
+                            ><font-awesome-icon icon="pencil-alt" />Edit workflow attributes</a
+                        >
+                        to specify creator(s).
+                    </p>
+                    <em class="small">
+                        <p>
+                            This workflow does not specify creator(s). This is important metadata for workflows
+                            that will be published and/or shared to help workflow executors know how to cite the
+                            workflow authors.
+                        </p>
+                    </em>
+                </div>
+            </span>
+        </b-card-body>
+    </b-card>
 </template>
 
 <script>
