54719c783d0d50c6ab67e7517d091e4255cd43e4
David López <46503462+davelopez@users.noreply.github.com>
2021-01-08 12:23:28 +0100

    Show OpenAPI documentation for API tags

--- lib/galaxy/webapps/galaxy/fast_app.py
+++ lib/galaxy/webapps/galaxy/fast_app.py
@@ -42,7 +42,7 @@ def add_exception_handler(
 
 
 def initialize_fast_app(gx_app):
-    app = FastAPI()
+    app = FastAPI(openapi_tags=api_tags_metadata)
 
     add_exception_handler(app)
     wsgi_handler = WSGIMiddleware(gx_app)
