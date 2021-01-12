e3f6332d7f6e7922c2d4d7fd4ebd711f9c084758
John Chilton <jmchilton@gmail.com>
2021-01-12 09:24:21 -0500

    Update lib/galaxy/jobs/runners/kubernetes.py
    Co-authored-by: Marius van den Beek <m.vandenbeek@gmail.com>

--- lib/galaxy/jobs/runners/kubernetes.py
+++ lib/galaxy/jobs/runners/kubernetes.py
@@ -243,7 +243,9 @@ class KubernetesJobRunner(AsynchronousJobRunner):
         # TODO include other relevant elements that people might want to use from
         # TODO http://kubernetes.io/docs/api-reference/v1/definitions/#_v1_podspec
         k8s_spec_template["spec"]["securityContext"] = self.__get_k8s_security_context()
-        extra_metadata = yaml.safe_load(self.runner_params['k8s_job_metadata'] or "{}")
+        extra_metadata = self.runner_params['k8s_job_metadata'] or '{}'
+        if isinstance(k8s_job_metadata, str):
+            extra_metadata = yaml.safe_load(extra_metadata)
         k8s_spec_template["metadata"]["labels"].update(extra_metadata.get('labels', {}))
         k8s_spec_template["metadata"]["annotations"].update(extra_metadata.get('annotations', {}))
         return k8s_spec_template
