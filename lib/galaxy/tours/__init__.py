2a2559812c0a240cf03cfaeb56c784cdcab0315e
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2021-01-12 20:45:19 -0500

    Define __all__ list for tours package

--- lib/galaxy/tours/__init__.py
+++ lib/galaxy/tours/__init__.py
@@ -1,7 +1,17 @@
-from ._impl import build_tours_registry  # noqa
-from ._interface import ToursRegistry  # noqa
-from ._schema import Tour  # noqa
-from ._schema import TourCore  # noqa
-from ._schema import TourStep  # noqa
-from ._schema import TourDetails  # noqa
-from ._schema import TourList  # noqa
+from ._impl import build_tours_registry
+from ._interface import ToursRegistry
+from ._schema import Tour
+from ._schema import TourCore
+from ._schema import TourDetails
+from ._schema import TourList
+from ._schema import TourStep
+
+__all__ = [
+    'build_tours_registry',
+    'ToursRegistry',
+    'Tour',
+    'TourCore',
+    'TourDetails',
+    'TourList',
+    'TourStep'
+]
