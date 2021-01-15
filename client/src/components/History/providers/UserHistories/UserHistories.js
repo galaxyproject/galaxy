33d91fa63152c7e5571ef0605a480860aa66734f
Mason Houtz <nerdinacan@gmail.com>
2021-01-14 23:05:58 -0800

    added explicit current history id to userhistories provider output

--- client/src/components/History/providers/UserHistories/UserHistories.js
+++ client/src/components/History/providers/UserHistories/UserHistories.js
@@ -60,6 +60,8 @@ export default {
             // currently selected history object, should be a full object not just a summary
             currentHistory: this.currentHistoryModel,
 
+            currentHistoryId: this.currentHistoryModel?.id || null,
+
             handlers: {
                 // Updates the history in the store without a trip to the server, in the event that a
                 // downstream component does the ajax update itself.
