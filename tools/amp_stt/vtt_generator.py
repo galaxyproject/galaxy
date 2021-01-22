9769e2a4a8a10e221354af54a285828a31924827
yingfeng-iu <yingfeng@iu.edu>
2021-01-22 13:00:24 -0500

    AMP-1013: fix issues: - update criteria on switching speakers, to use a combination of switching segment plus time gap. - fix space before punctuation - fix a bug relating to find current word segment

--- tools/amp_stt/vtt_generator.py
+++ tools/amp_stt/vtt_generator.py
@@ -2,8 +2,10 @@
 import json
 import sys
 import os
-from _ast import Or
 
+MIN_WORD_COUNT = 6	# minimum number of words per line
+MAX_WORD_COUNT = 10	# maximum number of words per line
+MIN_SEGMENT_GAP = 5.00	# minimum gap in seconds between segment for speaker switch
 
 # Reads the AMP Transcript and Segment inputs and convert them to Web VTT output.
 def main():
@@ -35,24 +37,24 @@ def main():
 				
 	# process all words which are in time order
 	for i, word in enumerate(words):
+		# find the next segment the current word belongs to
+		# note that if segments list is empty, nextseg/curseg will stay at 0
+		nextseg = findWordSegment(word, segments, curseg)
+
 		# the current word should be the start of a new line if it's pronunciation and  
 		if word['type'] == 'pronunciation':
 			# no previous line, this is the very first word
 			if (nword == 0):
 				newline = True
 			# there are already 10 words in current line
-			elif nword == 10:
+			elif nword == MAX_WORD_COUNT:
 				newline = True
 			# or there are at least 6 words plus punctuation in current line	
-			elif nword >= 6 and words[i-1]['type'] == 'punctuation':
+			elif nword >= MIN_WORD_COUNT and words[i-1]['type'] == 'punctuation':
 				newline = True
 			# or it's from a new speaker
-			else:
-				# note that if segments is empty, iseg/curseg will stay at 0
-				iseg = findWordSegmentIndex(word, segments, curseg)
-				if iseg > curseg:
-					newline = True
-					curseg = iseg
+			elif speakSwitched(segments, curseg, nextseg, MIN_SEGMENT_GAP):
+				newline = True
 		
 		# starting a new line	
 		if newline:
@@ -60,7 +62,7 @@ def main():
 			# note that punctuation words before the very first pronunciation word (in which case nword = 0) will be ignored 
 			if (nword > 0):
 				out_file.write(writeTime(start, end))
-				out_file.write(writeLine(getSpeaker(segments, curseg), line))
+				out_file.write(writeLine(getSegmentSpeaker(segments, curseg), line))
 				out_file.write(writeEmptyLine())
 			# reset status for the new line
 			nword = 0 
@@ -68,34 +70,40 @@ def main():
 			start = word['start']	 
 			line = ''	
 			newline = False
+			
+		# now we can move current segment pointer to next if they are different	
+		if nextseg > curseg:
+			curseg = nextseg
 		
 		# update current line with current word
 		if word['type'] == 'pronunciation':
 			nword += 1
 			# record potential line end time from current pronunciation (can't end time of last word on a line since it may be punctuation)
 			end = word['end']
-		line += ' ' + word['text']
-		
+			line += ' ' + word['text']	# space before pronunciation
+		else:
+			line += word['text']	# no space before punctuation
+				
 	# write the last line to file if any word in it; note that 
 	# the last line should always contain some pronunciation words unless the whole words list contains no pronunciation
 	if nword > 0:
 		out_file.write(writeTime(start, end))
-		out_file.write(writeLine(getSpeaker(segments, curseg), line))
+		out_file.write(writeLine(getSegmentSpeaker(segments, curseg), line))
 		out_file.write(writeEmptyLine())
 	out_file.close()
 		
 # Find the index of the next segment among the given segments to which the given word belongs to, starting at the given (current) 
 # segment index (always >= 0). A word belongs to a segment if its timestamp is within the segment's time range,  
-def findWordSegmentIndex(word, segments, istart):
+def findWordSegment(word, segments, curseg):
 	# non pronunciation word always stays with the current segment
 	if word['type'] != 'pronunciation':
-		return istart		
+		return curseg		
 	# if we are already beyond segments index bound, stay with current segment
-	if istart >= len(segments):
-		return istart
+	if curseg >= len(segments):
+		return curseg
 	# if word start time is within current segment end time, stay with current segment 
-	if word['start'] < segments[istart]['end']:
-		return istart
+	if word['start'] < segments[curseg]['end']:
+		return curseg
 	
 	# otherwise, scan the following segments for the next containing one, based on following assumptions: 
 	# need to scan as some segments may not contain any word (otherwise we can just return the next segment); 
@@ -103,20 +111,41 @@ def findWordSegmentIndex(word, segments, istart):
 	# instead of requiring word's [start, end] time within segment's [start, end] time,
 	# only check word's start time against segment's end time (i.e. consider segments continuous in time), 
 	# to ensure each word fall into some segment, and handle well even when word crosses segment boundary (unlikely) 
-	iseg = istart + 1
-	while iseg < len(segments) and word['start'] >= segments[iseg]['end']:
-		iseg += 1		
-	return iseg
+	nextseg = curseg + 1
+	while nextseg < len(segments) and word['start'] >= segments[nextseg]['end']:
+		nextseg += 1		
+	return nextseg
 	
+# Returns true if the given current segment and next segment have different speakers, 
+# or having same speakers but the time gap between the segments are greater than the given threshold.
+def speakSwitched(segments, curseg, nextseg, threshold):	
+	curspeaker = getSegmentSpeaker(segments, curseg)
+	nextspeaker = getSegmentSpeaker(segments, nextseg)
+	curend = getSegmentTime(segments, curseg, False)
+	nextstart = getSegmentTime(segments, nextseg, True) 
+	if curspeaker != nextspeaker or (nextstart - curend > threshold):
+		return True
+	else:
+		return False
+		
 # Gets the speaker of the given segments[iseg]. 	
-def getSpeaker(segments, iseg):
+def getSegmentSpeaker(segments, iseg):
 	if iseg < 0 or iseg >= len(segments):
 		return "No Speaker"
 	if 'speakerLabel' in segments[iseg]:
 		return segments[iseg]['speakerLabel']
 	else:
 		return "Unlabeled Speaker"
-		
+	
+# Gets the start/end timestamp of the given segments[iseg]. 	
+def getSegmentTime(segments, iseg,  forstart):
+	if iseg < 0 or iseg >= len(segments):
+		return 0
+	if forstart:		
+		return segments[iseg]['start']
+	else:
+		return segments[iseg]['end']
+	
 def writeHeader():
 	#this fuction writes the header of the vtt file
 	return "WEBVTT"+"\n"
