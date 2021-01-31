affcbd3f7226d29152ee313c2a14ebc7c591dc51
mvdbeek <m.vandenbeek@gmail.com>
2021-01-31 16:37:45 +0100

    Allow None as parameter dump

--- lib/galaxy/tools/parameters/__init__.py
+++ lib/galaxy/tools/parameters/__init__.py
@@ -206,7 +206,7 @@ def params_to_strings(params, param_values, app, nested=False, use_security=Fals
     for key, value in param_values.items():
         if key in params:
             value = params[key].value_to_basic(value, app, use_security=use_security)
-        rval[key] = value if nested else str(dumps(value, sort_keys=True))
+        rval[key] = value if nested or value is None else str(dumps(value, sort_keys=True))
     return rval
 
 
