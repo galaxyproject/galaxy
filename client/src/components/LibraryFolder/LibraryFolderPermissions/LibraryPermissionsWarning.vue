c39e22f128e69b916e5b79b6265608e8777ec215
Oleg Zharkov <oleg.zharkov22@gmail.com>
2021-02-12 17:50:37 +0100

    prettier

--- client/src/components/LibraryFolder/LibraryFolderPermissions/LibraryPermissionsWarning.vue
+++ client/src/components/LibraryFolder/LibraryFolderPermissions/LibraryPermissionsWarning.vue
@@ -2,8 +2,8 @@
     <div>
         <div class="text-center">
             <b-alert show variant="warning" v-if="is_admin">
-                You are logged in as an <strong>administrator</strong> therefore you can manage any folder on
-                this Galaxy instance. Please make sure you understand the consequences.
+                You are logged in as an <strong>administrator</strong> therefore you can manage any folder on this
+                Galaxy instance. Please make sure you understand the consequences.
             </b-alert>
             <b-alert show variant="warning" v-else>
                 You can assign any number of roles to any of the following permission types. However please read
@@ -14,12 +14,12 @@
 </template>
 
 <script>
-    export default {
-        props: {
-            is_admin: {
-                type: Boolean,
-                required: true,
-            },
+export default {
+    props: {
+        is_admin: {
+            type: Boolean,
+            required: true,
         },
-    }
+    },
+};
 </script>
