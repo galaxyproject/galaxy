c5e288dbd253cc781dd574d665c44d8ac935b8c0
mvdbeek <m.vandenbeek@gmail.com>
2021-02-13 10:35:25 +0100

    Change log level for sqlalchemy_json

--- lib/galaxy/config/__init__.py
+++ lib/galaxy/config/__init__.py
@@ -66,6 +66,10 @@ LOGGING_CONFIG_DEFAULT = {
             'level': 'WARN',
             'qualname': 'paste.httpserver.ThreadPool',
         },
+        'sqlalchemy_json.track': {
+            'level': 'WARN',
+            'qualname': 'sqlalchemy_json.track',
+        },
         'urllib3.connectionpool': {
             'level': 'WARN',
             'qualname': 'urllib3.connectionpool',
