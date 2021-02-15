80c423b68408d4c76cf9b17f13f262aac658f3b3
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2021-02-15 19:53:32 +0000

    Fix MetadataCollection attribute access for tools
    Follow-up on https://github.com/galaxyproject/galaxy/pull/11353 .

--- test/unit/data/test_galaxy_mapping.py
+++ test/unit/data/test_galaxy_mapping.py
@@ -384,6 +384,7 @@ class MappingTests(BaseModelTestCase):
         d = self.model.HistoryDatasetAssociation(extension="interval", metadata=metadata, sa_session=self.model.session)
         assert d.metadata.chromCol == 1
         assert d.metadata.anyAttribute is None
+        assert 'items' not in d.metadata
 
     def test_jobs(self):
         model = self.model
