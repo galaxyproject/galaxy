3609598fa097d2ed52b8f1c67b7d8ea0852910cc
Matthias Bernt <m.bernt@ufz.de>
2021-02-16 14:32:54 +0100

    add test for new lints

--- test/unit/tool_util/test_tool_linters.py
+++ test/unit/tool_util/test_tool_linters.py
@@ -26,13 +26,24 @@ NO_WHEN_IN_CONDITIONAL_XML = """
 </tool>
 """
 
+RADIO_SELECT_INCOMPATIBILITIES = """
+<tool name="BWA Mapper" id="bwa" version="1.0.1" is_multi_byte="true" display_interface="true" require_login="true" hidden="true">
+    <description>The BWA Mapper</description>
+    <version_command interpreter="python">bwa.py --version</version_command>
+    <inputs>
+        <param name="select" display="radio" multiple="true" optional="true"/>
+    </inputs>
+</tool>
+"""
+
 TESTS = [
     (NO_SECTIONS_XML, inputs.lint_inputs, lambda x: 'Found no input parameters.' in x.warn_messages),
     (NO_WHEN_IN_CONDITIONAL_XML, inputs.lint_inputs, lambda x: 'No <when /> block found for select option \'none\' inside conditional \'labels\'' in x.warn_messages),
+    (RADIO_SELECT_INCOMPATIBILITIES, inputs.lint_inputs, lambda x: 'Select [select] display="radio" is incompatible with optional="true"\nSelect [%s] display="radio" is incompatible with multiple="true"' in x.error_messages),
 ]
 
 
-@pytest.mark.parametrize('tool_xml,lint_func,assert_func', TESTS, ids=['Lint no sections', 'lint no when'])
+@pytest.mark.parametrize('tool_xml,lint_func,assert_func', TESTS, ids=['Lint no sections', 'lint no when', 'radio select incompatibilities'])
 def test_tool_xml(tool_xml, lint_func, assert_func):
     lint_ctx = LintContext('all')
     tree = etree.ElementTree(element=etree.fromstring(tool_xml))
