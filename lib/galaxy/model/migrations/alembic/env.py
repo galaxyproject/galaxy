75906df5f16705f0d7138d2636c7fcdda8e8b4f7
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2021-02-17 20:07:35 -0500

    Fix lint errors

--- lib/galaxy/model/migrations/alembic/env.py
+++ lib/galaxy/model/migrations/alembic/env.py
@@ -1,8 +1,6 @@
-from logging.config import fileConfig
-
+# from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 
 # this is the Alembic Config object, which provides
