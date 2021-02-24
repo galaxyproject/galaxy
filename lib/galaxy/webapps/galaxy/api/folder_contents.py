3ebb57430cdc660fa4d3863637fb8ca7b60ebecd
Oleg Zharkov <oleg.zharkov22@gmail.com>
2021-02-24 15:11:26 +0200

    fix https://github.com/galaxyproject/galaxy/issues/11461

--- lib/galaxy/webapps/galaxy/api/folder_contents.py
+++ lib/galaxy/webapps/galaxy/api/folder_contents.py
@@ -330,7 +330,7 @@ class FolderContentsController(BaseAPIController, UsesLibraryMixin, UsesLibraryM
         folders = check_deleted(folder.folders, include_deleted)
 
         if search_text is not None:
-            folders = [item for item in folders if item.description and search_text in item.name or search_text in item.description]
+            folders = [item for item in folders if search_text in item.name or search_text in item.description]
             datasets = list(filter(filter_searched_datasets, datasets))
 
         return folders, datasets
