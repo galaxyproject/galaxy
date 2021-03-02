galaxyproject/galaxy

commit 88d7c4bb798d884b410213738f1bf21ae219070b

Fix tool version switch in workflow editor
Fixes https://github.com/galaxyproject/galaxy/issues/10914

client/src/components/Workflow/Editor/Node.vue
@@ -294,6 +294,7 @@ export default {
         },
         setData(data) {
             this.config_form = data.config_form;
+            this.content_id = this.config_form?.id;
             this.tool_state = data.tool_state;
             this.errors = data.errors;
             this.annotation = data.annotation;

commit 6cfd858e134ebebcbdb5aff2db58a0602a2ac258

Fix embedding subworkflow
This broke in 88d7c4bb798d884b410213738f1bf21ae219070b.

client/src/components/Workflow/Editor/Node.vue
@@ -294,7 +294,7 @@ export default {
         },
         setData(data) {
             this.config_form = data.config_form;
-            this.content_id = this.config_form?.id;
+            this.content_id = data.config_form?.id || data.content_id;
             this.tool_state = data.tool_state;
             this.errors = data.errors;
             this.annotation = data.annotation;