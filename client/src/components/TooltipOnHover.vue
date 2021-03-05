802cd279ecb97eb6a1336820186839707006b1ff
Oleg Zharkov <oleg.zharkov22@gmail.com>
2021-03-05 18:17:30 +0200

    tooltip wrapper

---
+++ client/src/components/TooltipOnHover.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :disabled="disabled" v-b-tooltip.hover.d50 :title="title"><slot /></div>
+</template>
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        disabled: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+    },
+};
+</script>
