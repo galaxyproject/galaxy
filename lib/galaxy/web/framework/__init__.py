d33eac078c5344065b1cdc722569515a40cdb4e8
davelopez <46503462+davelopez@users.noreply.github.com>
2021-03-15 12:31:30 +0100

    Fix BaseURLToolParameter doctest.
    If the url_for fails this will return as fallback value the first argument.
    The reason for this is that the doctest for BaseURLToolParameter is assuming this value will be returned when an error is raised.

--- lib/galaxy/web/framework/__init__.py
+++ lib/galaxy/web/framework/__init__.py
@@ -9,6 +9,8 @@ def handle_url_for(*args, **kargs) -> str:
     try:
         return base.routes.url_for(*args, **kargs)
     except AttributeError:
+        if len(args) > 0:
+            return args[0]
         return "*deprecated attribute not filled in by FastAPI server*"
 
 
