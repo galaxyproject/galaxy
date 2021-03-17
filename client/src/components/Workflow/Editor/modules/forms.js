galaxyproject/galaxy

commit 526fb05d5c7d0e154f7df2f622045059105451e5

Fix data inputs becoming runtime inputs when cloning step

client/src/components/Workflow/Editor/modules/forms.js
@@ -108,7 +108,7 @@ export class ToolForm {
         const inputs = node.config_form.inputs;
         Utils.deepeach(inputs, (input) => {
             if (input.type) {
-                if (["data", "data_collection"].indexOf(input.type) != -1) {
+                if (["data", "data_collection", "hidden"].indexOf(input.type) != -1) {
                     input.type = "hidden";
                     input.info = `Data input '${input.name}' (${Utils.textify(input.extensions)})`;
                     input.value = { __class__: "RuntimeValue" };

commit 1a1621d00c259ba9c94618e82b1a3965b6659540

Fix conditional and section parameters becoming runtime parameters
This broke in 526fb05d5c7d0e154f7df2f622045059105451e5, which fixed
cloned data inputs missing from the tool form.
The problem with this commit is that we now add the runtime value state
to non-data hidden inputs, which is not correct and breaks execution
of workflows with these inputs.

This fix is better since it does not change the input type, and instead
adds a new attribute. The tool form and its variants need to be
rewritten in vue ASAP, so we can stop these hacks.

client/src/components/Workflow/Editor/modules/forms.js
@@ -108,8 +108,8 @@ export class ToolForm {
         const inputs = node.config_form.inputs;
         Utils.deepeach(inputs, (input) => {
             if (input.type) {
-                if (["data", "data_collection", "hidden"].indexOf(input.type) != -1) {
-                    input.type = "hidden";
+                if (["data", "data_collection"].indexOf(input.type) != -1) {
+                    input.hiddenInWorkflow = true;
                     input.info = `Data input '${input.name}' (${Utils.textify(input.extensions)})`;
                     input.value = { __class__: "RuntimeValue" };
                 } else if (!input.fixed) {

client/src/mvc/form/form-parameters.js
@@ -45,7 +45,7 @@ export default Backbone.Model.extend({
     /** Returns an input field for a given field type */
     create: function (input_def) {
         const Galaxy = getGalaxyInstance();
-        var fieldClass = this.types[input_def.type];
+        var fieldClass = this.types[input_def.hiddenInWorkflow ? "hidden" : input_def.type];
         var field = typeof this[fieldClass] === "function" ? this[fieldClass].call(this, input_def) : null;
         if (!field) {
             field = input_def.options ? this._fieldSelect(input_def) : this._fieldText(input_def);