galaxyproject/galaxy

commit e49d6a3c461bae843bb40877a321d15922306842

Make NodeInput/NodeOutput reactive

client/src/components/Workflow/Editor/NodeInput.vue
@@ -11,6 +11,7 @@
 <script>
 import Terminals from "./modules/terminals";
 import { InputDragging } from "./modules/dragging";
+import Connector from "./modules/connector";
 export default {
     props: {
         input: {
@@ -36,6 +37,25 @@ export default {
             isMultiple: false,
         };
     },
+    watch: {
+        input: function (newInput) {
+            const oldTerminal = this.terminal;
+            if (oldTerminal instanceof this.terminalClassForInput(newInput)) {
+                oldTerminal.update(newInput);
+                oldTerminal.destroyInvalidConnections();
+            } else {
+                // create new terminal, connect like old terminal, destroy old terminal
+                this.$emit("onRemove", this.input);
+                const newTerminal = this.createTerminal(newInput);
+                newTerminal.connectors = this.terminal.connectors.map((c) => {
+                    return new Connector(this.getManager(), c.outputHandle, newTerminal);
+                });
+                newTerminal.destroyInvalidConnections();
+                this.terminal = newTerminal;
+                oldTerminal.destroy();
+            }
+        },
+    },
     computed: {
         id() {
             const node = this.getNode();
@@ -54,16 +74,19 @@ export default {
     },
     mounted() {
         this.terminal = this.createTerminal(this.input);
-        this.$emit("onAdd", this.input, this.terminal);
     },
     methods: {
-        createTerminal(input) {
+        terminalClassForInput(input) {
             let terminalClass = Terminals.InputTerminal;
             if (input.input_type == "dataset_collection") {
                 terminalClass = Terminals.InputCollectionTerminal;
             } else if (input.input_type == "parameter") {
                 terminalClass = Terminals.InputParameterTerminal;
             }
+            return terminalClass;
+        },
+        createTerminal(input) {
+            const terminalClass = this.terminalClassForInput(input);
             const terminal = new terminalClass({
                 node: this.getNode(),
                 datatypesMapper: this.datatypesMapper,
@@ -76,6 +99,7 @@ export default {
                 el: this.$refs.terminal,
                 terminal: terminal,
             });
+            this.$emit("onAdd", this.input, terminal);
             return terminal;
         },
         onChange() {
@@ -83,14 +107,8 @@ export default {
             this.$emit("onChange");
         },
         onRemove() {
-            this.$emit("onRemove");
-            if (this.terminal.connectors.length > 0) {
-                this.terminal.connectors.forEach((x) => {
-                    if (x) {
-                        x.destroy();
-                    }
-                });
-            }
+            this.$emit("onRemove", this.input);
+            this.terminal.destroy();
         },
         mouseOver(e) {
             if (this.terminal.connectors.length > 0) {
@@ -100,6 +118,9 @@ export default {
         mouseLeave() {
             this.showRemove = false;
         },
+    },
+    beforeDestroy() {
+        this.onRemove();
     },
 };
 </script>

client/src/components/Workflow/Editor/Node.vue
@@ -63,6 +63,7 @@
                 :get-manager="getManager"
                 :datatypes-mapper="datatypesMapper"
                 @onAdd="onAddInput"
+                @onRemove="onRemoveInput"
                 @onChange="onChange"
             />
             <div v-if="showRule" class="rule" />
@@ -73,6 +74,7 @@
                 :get-node="getNode"
                 :get-manager="getManager"
                 @onAdd="onAddOutput"
+                @onRemove="onRemoveOutput"
                 @onToggle="onToggleOutput"
                 @onChange="onChange"
             />
@@ -241,22 +243,16 @@ export default {
             this.$emit("onChange");
         },
         onAddInput(input, terminal) {
-            const existingTerminal = this.inputTerminals[input.name];
-            if (existingTerminal) {
-                existingTerminal.update(input);
-                existingTerminal.destroyInvalidConnections();
-            } else {
-                this.inputTerminals[input.name] = terminal;
-            }
+            this.inputTerminals[input.name] = terminal;
+        },
+        onRemoveInput(input) {
+            delete this.inputTerminals[input.name];
         },
         onAddOutput(output, terminal) {
-            const existingTerminal = this.outputTerminals[output.name];
-            if (existingTerminal) {
-                existingTerminal.update(output);
-                existingTerminal.destroyInvalidConnections();
-            } else {
-                this.outputTerminals[output.name] = terminal;
-            }
+            this.outputTerminals[output.name] = terminal;
+        },
+        onRemoveOutput(output) {
+            delete this.outputTerminals[output.name];
         },
         onToggleOutput(name) {
             this.activeOutputs.toggle(name);
@@ -339,99 +335,11 @@ export default {
         },
         updateData(data) {
             this.setData(data);
-
-            // Create a dictionary of all incoming outputs
-            const outputNames = {};
-            const outputIndex = {};
-            data.outputs.forEach((output) => {
-                const name = output.name;
-                outputNames[name] = 1;
-                outputIndex[name] = output;
-            });
-
-            // Identify unused outputs which existed previously
-            for (let i = this.outputs.length - 1; i >= 0; i--) {
-                const output = this.outputs[i];
-                const name = output.name;
-                if (!outputNames[name]) {
-                    // Remove the noodle connectors
-                    this.outputTerminals[name].connectors.forEach((x) => {
-                        if (x) {
-                            x.destroy();
-                        }
-                    });
-                    // Remove the reference to the output and output terminal
-                    delete this.outputTerminals[name];
-                    this.outputs.splice(i, 1);
-                } else {
-                    // Output exists in both sources
-                    outputNames[name] = 2;
-                    // Update existing outputs with incoming output attributes
-                    const outputIncoming = outputIndex[name];
-                    Object.entries(outputIncoming).forEach(([key, newValue]) => {
-                        Vue.set(output, key, newValue);
-                    });
-                }
-            }
-
-            // Add or update remaining outputs
-            data.outputs.forEach((output) => {
-                const terminal = this.outputTerminals[output.name];
-                if (terminal) {
-                    terminal.update(output);
-                    terminal.destroyInvalidConnections();
-                }
-                if (outputNames[output.name] == 1) {
-                    this.outputs.push(output);
-                }
-            });
-
-            // Create a list of all current input names
-            const inputNames = {};
-            const inputIndex = {};
-            data.inputs.forEach((input) => {
-                const name = input.name;
-                inputNames[name] = 1;
-                inputIndex[name] = input;
-            });
-
-            // Identify unused inputs which existed previously
-            for (let i = this.inputs.length - 1; i >= 0; i--) {
-                const name = this.inputs[i].name;
-                if (!inputNames[name] || inputIndex[name].input_type !== this.inputs[i].input_type) {
-                    this.inputTerminals[name].destroy();
-                    delete this.inputTerminals[name];
-                    this.inputs.splice(i, 1);
-                } else {
-                    inputNames[name] = 2;
-                }
-            }
-
-            // Add and update input rows
-            data.inputs.forEach((input) => {
-                const terminal = this.inputTerminals[input.name];
-                if (terminal) {
-                    terminal.update(input);
-                    terminal.destroyInvalidConnections();
-                }
-                if (inputNames[input.name] == 1) {
-                    this.inputs.push(input);
-                }
-            });
-
-            this.inputs.forEach((input) => {
-                // Update input terminal labels
-                input.label = inputIndex[input.name].label;
-            });
-
-            // removes output from list of workflow outputs
+            // Create array of new output names
+            const outputNames = data.outputs.map((output) => output.name);
             this.activeOutputs.filterOutputs(outputNames);
-
-            // trigger legacy events
-            Vue.nextTick(() => {
-                this.onRedraw();
-            });
-
+            this.inputs = data.inputs;
+            this.outputs = data.outputs;
             // emit change completion event
             this.showLoading = false;
             this.$emit("onChange");

client/src/components/Workflow/Editor/NodeOutput.vue
@@ -19,6 +19,7 @@
 <script>
 import Terminals from "./modules/terminals";
 import { OutputDragging } from "./modules/dragging";
+import Connector from "./modules/connector";
 
 export default {
     props: {
@@ -68,45 +69,75 @@ export default {
             return cls;
         },
     },
+    watch: {
+        output: function (newOutput) {
+            const oldTerminal = this.terminal;
+            if (oldTerminal instanceof this.terminalClassForOutput(newOutput)) {
+                oldTerminal.update(newOutput);
+                oldTerminal.destroyInvalidConnections();
+            } else {
+                // create new terminal, connect like old terminal, destroy old terminal
+                this.$emit("onRemove", this.output);
+                const newTerminal = this.createTerminal(newOutput);
+                newTerminal.connectors = this.terminal.connectors.map((c) => {
+                    return new Connector(this.getManager(), newTerminal, c.inputHandle);
+                });
+                newTerminal.destroyInvalidConnections();
+                this.terminal = newTerminal;
+                oldTerminal.destroy();
+            }
+        },
+    },
     mounted() {
-        const output = this.output;
-        if (output.collection) {
-            const collection_type = output.collection_type;
-            const collection_type_source = output.collection_type_source;
-            this.terminal = new Terminals.OutputCollectionTerminal({
-                node: this.getNode(),
-                name: output.name,
-                element: this.$refs.terminal,
-                collection_type: collection_type,
-                collection_type_source: collection_type_source,
-                datatypes: output.extensions,
-                optional: output.optional,
-            });
-        } else if (output.parameter) {
-            this.terminal = new Terminals.OutputParameterTerminal({
-                node: this.getNode(),
-                name: output.name,
-                element: this.$refs.terminal,
-                type: output.type,
-                optional: output.optional,
-            });
-        } else {
-            this.terminal = new Terminals.OutputTerminal({
-                node: this.getNode(),
-                name: output.name,
-                element: this.$refs.terminal,
-                datatypes: output.extensions,
-                optional: output.optional,
-            });
-        }
-        this.terminal.on("change", this.onChange.bind(this));
-        new OutputDragging(this.getManager(), {
-            el: this.$refs.terminal,
-            terminal: this.terminal,
-        });
-        this.$emit("onAdd", this.output, this.terminal);
+        this.terminal = this.createTerminal(this.output);
     },
     methods: {
+        terminalClassForOutput(output) {
+            let terminalClass = Terminals.OutputTerminal;
+            if (output.collection) {
+                terminalClass = Terminals.OutputCollectionTerminal;
+            } else if (output.parameter) {
+                terminalClass = Terminals.OutputParameterTerminal;
+            }
+            return terminalClass;
+        },
+        createTerminal(output) {
+            let terminal;
+            const terminalClass = this.terminalClassForOutput(output);
+            const parameters = {
+                node: this.getNode(),
+                name: output.name,
+                element: this.$refs.terminal,
+                optional: output.optional,
+            };
+            if (output.collection) {
+                const collection_type = output.collection_type;
+                const collection_type_source = output.collection_type_source;
+                terminal = new terminalClass({
+                    ...parameters,
+                    collection_type: collection_type,
+                    collection_type_source: collection_type_source,
+                    datatypes: output.extensions,
+                });
+            } else if (output.parameter) {
+                terminal = new terminalClass({
+                    ...parameters,
+                    type: output.type,
+                });
+            } else {
+                terminal = new terminalClass({
+                    ...parameters,
+                    datatypes: output.extensions,
+                });
+            }
+            terminal.on("change", this.onChange.bind(this));
+            new OutputDragging(this.getManager(), {
+                el: this.$refs.terminal,
+                terminal: terminal,
+            });
+            this.$emit("onAdd", this.output, terminal);
+            return terminal;
+        },
         onChange() {
             this.isMultiple = this.terminal.mapOver && this.terminal.mapOver.isCollection;
             this.$emit("onChange");
@@ -114,6 +145,10 @@ export default {
         onToggle() {
             this.$emit("onToggle", this.output.name);
         },
+    },
+    beforeDestroy() {
+        this.$emit("onRemove", this.output);
+        this.terminal.destroy();
     },
 };
 </script>

client/src/components/Workflow/Editor/modules/connector.js
@@ -30,21 +30,13 @@ class Connector {
     }
     connect(t1, t2) {
         this.outputHandle = t1;
-        if (this.outputHandle) {
-            this.outputHandle.connect(this);
-        }
+        this.outputHandle?.connect(this);
         this.inputHandle = t2;
-        if (this.inputHandle) {
-            this.inputHandle.connect(this);
-        }
+        this.inputHandle?.connect(this);
     }
     destroy() {
-        if (this.outputHandle) {
-            this.outputHandle.disconnect(this);
-        }
-        if (this.inputHandle) {
-            this.inputHandle.disconnect(this);
-        }
+        this.outputHandle?.disconnect(this);
+        this.inputHandle?.disconnect(this);
         this.canvas.remove();
     }
     destroyIfInvalid(warn) {

client/tests/qunit/tests/workflow_editor_tests.js
@@ -502,7 +502,6 @@ QUnit.test("update_field_data preserves connectors", function (assert) {
 
 QUnit.test("update_field_data destroys old terminals", function (assert) {
     const node = this.node;
-    const node_changed_spy = sinon.spy(this.node, "onChange");
     const data = {
         inputs: [
             { name: "input1", extensions: ["data"] },
@@ -515,8 +514,9 @@ QUnit.test("update_field_data destroys old terminals", function (assert) {
         const old_input_terminal = node.inputTerminals.willDisappear;
         const destroy_spy = sinon.spy(old_input_terminal, "destroy");
         this.update_field_data_with_new_input();
-        assert.ok(destroy_spy.called);
-        assert.ok(node_changed_spy.called);
+        Vue.nextTick(() => {
+            assert.ok(destroy_spy.called);
+        })
     });
 });
 
@@ -696,7 +696,7 @@ QUnit.test("replacing terminal on data collection input with simple input change
     assert
 ) {
     const node = this.node;
-    node.inputs.push({ name: "TestName", extensions: ["txt"] });
+    node.inputs.push({ name: "TestName", extensions: ["txt"], input_type: "parameter" });
     this.connectAttachedMappedOutput((connector) => {
         const connector_destroy_spy = sinon.spy(connector, "destroy");
         const data = {

commit b0a29186434d76f419ccc270da437467b5b386d4

Fix reconnecting terminals
Fixes https://github.com/galaxyproject/galaxy/issues/11658.
Broken in e49d6a3c461bae843bb40877a321d15922306842.

We only need to remove the inputTerminal from the Node when the
NodeInput is removed, not when the connection is destroyed.
This mirrors what we do for the NodeOutout now.
https://github.com/mvdbeek/galaxy/blame/d03c7af02a0ac456a3e22d3fbc90e434debb43fe/client/src/components/Workflow/Editor/NodeOutput.vue#L153
now.

client/src/components/Workflow/Editor/NodeInput.vue
@@ -107,7 +107,6 @@ export default {
             this.$emit("onChange");
         },
         onRemove() {
-            this.$emit("onRemove", this.input);
             this.terminal.destroy();
         },
         mouseOver(e) {
@@ -120,6 +119,7 @@ export default {
         },
     },
     beforeDestroy() {
+        this.$emit("onRemove", this.input);
         this.onRemove();
     },
 };