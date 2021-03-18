b1992680a0f0f3d50402738f573d8f564c9a0cd5
Dannon Baker <dannon.baker@gmail.com>
2021-03-18 09:13:26 -0400

    Disable history permissions link in single user mode

--- client/src/mvc/history/options-menu.js
+++ client/src/mvc/history/options-menu.js
@@ -56,6 +56,7 @@ var menu = [
                 Galaxy.router.push(`/histories/permissions?id=${Galaxy.currHistoryPanel.model.id}`);
             }
         },
+        singleUserDisable: true,
     },
     {
         html: _l("Make Private"),
