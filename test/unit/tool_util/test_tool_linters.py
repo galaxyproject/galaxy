558d346de2b474af028ec7a64fdddf3a04f31ce1
Matthias Bernt <m.bernt@ufz.de>
2021-03-18 10:37:45 +0100

    add test

--- test/unit/tool_util/test_tool_linters.py
+++ test/unit/tool_util/test_tool_linters.py
@@ -36,10 +36,24 @@ RADIO_SELECT_INCOMPATIBILITIES = """
 </tool>
 """
 
+SELECT_DUPLICATED_OPTIONS = """
+<tool name="BWA Mapper" id="bwa" version="1.0.1" is_multi_byte="true" display_interface="true" require_login="true" hidden="true">
+    <description>The BWA Mapper</description>
+    <version_command interpreter="python">bwa.py --version</version_command>
+    <inputs>
+        <param name="select" type="select" display="radio" optional="true" multiple="true"/>
+            <option value="a">x</option>
+            <option value="b">x</option>
+        </param>
+    </inputs>
+</tool>
+"""
+
 TESTS = [
     (NO_SECTIONS_XML, inputs.lint_inputs, lambda x: 'Found no input parameters.' in x.warn_messages),
     (NO_WHEN_IN_CONDITIONAL_XML, inputs.lint_inputs, lambda x: 'No <when /> block found for select option \'none\' inside conditional \'labels\'' in x.warn_messages),
     (RADIO_SELECT_INCOMPATIBILITIES, inputs.lint_inputs, lambda x: 'Select [radio_select] display="radio" is incompatible with optional="true"' in x.error_messages and 'Select [radio_select] display="radio" is incompatible with multiple="true"' in x.error_messages),
+    (SELECT_DUPLICATED_OPTIONS, inputs.lint_inputs, lambda x: 'Select [radio_select]  has duplicated options' in x.error_messages),
 ]
 
 
