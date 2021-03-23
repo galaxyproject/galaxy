3b38f72c67e050d7e308abc7b147cd23656f30c8
Oleg Zharkov <oleg.zharkov22@gmail.com>
2021-03-24 00:41:19 +0200

    implement library dataset export as collection

--- lib/galaxy_test/selenium/test_library_to_collections.py
+++ lib/galaxy_test/selenium/test_library_to_collections.py
@@ -10,7 +10,22 @@ class LibraryToCollectionsTestCase(SeleniumTestCase, UsesLibraryAssertions):
     requires_admin = True
 
     @selenium_test
-    def test_list_creation(self):
+    def test_collection_export(self):
+        self.prepare_library_for_data_export()
+        self.screenshot("libraries_to_collection_landing")
+        self.components.libraries.folder.import_datasets_ok_button.wait_for_and_click()
+        self.build_collection_and_assert()
+
+    @selenium_test
+    def test_pair_export(self):
+        self.prepare_library_for_data_export()
+        self.components.libraries.folder.export_to_history_options.wait_for_and_click()
+        self.components.libraries.folder.export_to_history_paired_option(collection_option="paired").wait_for_and_click()
+        self.screenshot("libraries_to_pared_collection_landing")
+        self.components.libraries.folder.import_datasets_ok_button.wait_for_and_click()
+        self.build_collection_and_assert()
+
+    def prepare_library_for_data_export(self):
         self.navigate_to_new_library()
         self.assert_num_displayed_items_is(0)
         self.populate_library_folder_from_import_dir(self.name, ["1.bam", "1.bed"])
@@ -20,10 +35,11 @@ class LibraryToCollectionsTestCase(SeleniumTestCase, UsesLibraryAssertions):
         self.components.libraries.folder.add_to_history.wait_for_and_click()
         self.screenshot("libraries_add_to_history_menu")
         self.components.libraries.folder.add_to_history_collection.wait_for_and_click()
-        self.screenshot("libraries_to_collection_landing")
-        self.components.libraries.folder.import_datasets_ok_button.wait_for_and_click()
 
-        self.collection_builder_set_name("my cool list")
+    def build_collection_and_assert(self):
+        self.collection_builder_set_name(self._get_random_name())
         self.collection_builder_create()
         self.home()
         self.history_panel_wait_for_hid_ok(3)
+
+
