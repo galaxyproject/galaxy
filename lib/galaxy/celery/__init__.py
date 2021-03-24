aa473447d9ea0796b7f2fa255f51ec9e26ef9121
mvdbeek <m.vandenbeek@gmail.com>
2021-03-24 15:20:11 +0100

    Hack in dynamic reading of broker from galaxy.yml

--- lib/galaxy/celery/__init__.py
+++ lib/galaxy/celery/__init__.py
@@ -1,29 +1,32 @@
 import os
 from celery import Celery
 from galaxy.util.properties import load_app_properties
-
+from galaxy.config import Configuration
 from galaxy.util.custom_logging import get_logger
 
 log = get_logger(__name__)
-
-# This is a complete hack for now, todo: configure celery dynamically with the same galaxy internal amqp stuff.
-celery_app = Celery('galaxy', broker="redis://localhost", include=['galaxy.celery.tasks'])
 
 
 def get_galaxy_app():
     import galaxy.app
     if galaxy.app.app:
         return galaxy.app.app
-    config_file = os.path.abspath(os.environ["GALAXY_CONFIG_FILE"])
-    kwargs = load_app_properties(
-        config_file=config_file,
-        config_section='galaxy',
-    )
-    galaxy_app = galaxy.app.UniverseApplication(**kwargs)
+    galaxy_app = galaxy.app.UniverseApplication(**get_galaxy_config())
     return galaxy_app
 
 
+def get_galaxy_config():
+    config_file = os.path.abspath(os.environ["GALAXY_CONFIG_FILE"])
+    return load_app_properties(
+        config_file=config_file,
+        config_section='galaxy',
+    )
+broker = Configuration(**get_galaxy_config()).amqp_internal_connection
+# This is a complete hack for now, todo: configure celery dynamically with the same galaxy internal amqp stuff.
+celery_app = Celery('galaxy', broker=broker, include=['galaxy.celery.tasks'])
+log.warning(f"BROKER IS {broker}")
+
+
 if __name__ == '__main__':
-    #galaxy_app = get_galaxy_app()
     #celery_app.conf.update(broker=galaxy_app.config.amqp_internal_connection)
     celery_app.start()
