bab45f11498ebaaa4f4140e59f139ceebbc948fe
Dannon Baker <dannon.baker@gmail.com>
2021-03-24 09:35:32 -0400

    Trim down and clean up required context, add a log message to recalc task (it works!)

--- lib/galaxy/celery/tasks.py
+++ lib/galaxy/celery/tasks.py
@@ -1,19 +1,11 @@
-from galaxy.celery import celery_app, get_galaxy_app
-from galaxy.util.custom_logging import get_logger
-
-import datetime
-import decimal
-import os.path
 import sys
 
-# Setup DB scripting environment
-from sqlalchemy import *  # noqa
-from sqlalchemy.orm import *  # noqa
-from sqlalchemy.exc import *  # noqa
-from sqlalchemy.sql import label  # noqa
-
+from galaxy.celery import celery_app
 from galaxy.datatypes.registry import Registry
-from galaxy.model import *  # noqa
+from galaxy.model import (set_datatypes_registry, User)
+from galaxy.model.mapping import init
+from galaxy.model.orm.scripts import get_config
+from galaxy.util.custom_logging import get_logger
 
 log = get_logger(__name__)
 
@@ -22,10 +14,6 @@ def get_galaxy_context():
     # This insanity is because get_config (I think) is not cooperating with celery arg handling
     _preserved_argv = list(sys.argv[1:])
     del sys.argv[1:]
-
-    from galaxy.model import set_datatypes_registry
-    from galaxy.model.mapping import init
-    from galaxy.model.orm.scripts import get_config
 
     registry = Registry()
     registry.load_datatypes()
@@ -40,13 +28,12 @@ def get_galaxy_context():
 
 @celery_app.task
 def recalculate_user_disk_usage(user_id=None):
-    # This is not initializing like I want it to I don't think.
     sa_session = get_galaxy_context()
-    from galaxy import model
     if user_id:
-        user = sa_session.query(model.User).get(user_id)
+        user = sa_session.query(User).get(user_id)
         if user:
             user.calculate_and_set_disk_usage()
+            log.info(f"New user disk usage is {user.disk_usage}")
         else:
             log.error("Recalculate user disk usage task failed, user %s not found" % user_id)
     else:
