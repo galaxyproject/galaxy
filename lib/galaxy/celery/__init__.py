92b48e63d06f4eddbc88b068c6e870d92b8ee126
mvdbeek <m.vandenbeek@gmail.com>
2021-03-26 15:36:21 +0100

    Restructure so test collection should work

--- lib/galaxy/celery/__init__.py
+++ lib/galaxy/celery/__init__.py
@@ -10,26 +10,43 @@ from galaxy.util.properties import load_app_properties
 log = get_logger(__name__)
 
 
-@lru_cache
+@lru_cache(maxsize=1)
 def get_galaxy_app():
     import galaxy.app
     if galaxy.app.app:
         return galaxy.app.app
-    galaxy_app = galaxy.app.MinimalGalaxyApplication(configure_logging=False, **get_galaxy_config())
+    kwargs = get_app_properties()
+    kwargs['check_migrate_tools'] = False
+    kwargs['check_migrate_databases'] = False
+    galaxy_app = galaxy.app.MinimalGalaxyApplication(configure_logging=False, **kwargs)
     return galaxy_app
 
 
-def get_galaxy_config():
-    config_file = os.path.abspath(os.environ.get("GALAXY_CONFIG_FILE"))
-    return load_app_properties(
-        config_file=config_file,
-        config_section='galaxy',
-    )
+@lru_cache(maxsize=1)
+def get_app_properties():
+    config_file = os.environ.get("GALAXY_CONFIG_FILE")
+    if config_file:
+        return load_app_properties(
+            config_file=os.path.abspath(config_file),
+            config_section='galaxy',
+        )
 
 
-broker = Configuration(**get_galaxy_config()).amqp_internal_connection
+@lru_cache(maxsize=1)
+def get_config():
+    kwargs = get_app_properties()
+    if kwargs:
+        return Configuration(**kwargs)
+
+
+def get_broker():
+    config = get_config()
+    if config:
+        return config.amqp_internal_connection
+
+
+broker = get_broker()
 celery_app = Celery('galaxy', broker=broker, include=['galaxy.celery.tasks'])
-log.warning(f"BROKER IS {broker}")
 
 
 if __name__ == '__main__':
