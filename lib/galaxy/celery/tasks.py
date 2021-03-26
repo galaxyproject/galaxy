659651e3d86a1f8b807ce2413a2669217c8a40a3
mvdbeek <m.vandenbeek@gmail.com>
2021-03-26 17:26:50 +0100

    Use DI in celery task

--- lib/galaxy/celery/tasks.py
+++ lib/galaxy/celery/tasks.py
@@ -1,4 +1,10 @@
+from lagom import magic_bind_to_container
+from sqlalchemy.orm.scoping import (
+    scoped_session,
+)
+
 from galaxy.celery import celery_app
+from galaxy.model import User
 from galaxy.util.custom_logging import get_logger
 from . import get_galaxy_app
 
@@ -11,11 +17,10 @@ def get_galaxy_context():
 
 
 @celery_app.task
-def recalculate_user_disk_usage(user_id=None):
-    app = get_galaxy_context()
-    sa_session = app.model.session
+@magic_bind_to_container(get_galaxy_context())
+def recalculate_user_disk_usage(session: scoped_session, user_id=None):
     if user_id:
-        user = sa_session.query(app.model.User).get(user_id)
+        user = session.query(User).get(user_id)
         if user:
             user.calculate_and_set_disk_usage()
             log.info(f"New user disk usage is {user.disk_usage}")
