1f5fefb262b0e88472603051056604582fde22f2
Oleg Zharkov <oleg.zharkov22@gmail.com>
2021-03-27 15:50:56 +0200

    fix history name

--- lib/galaxy_test/selenium/test_library_to_collections.py
+++ lib/galaxy_test/selenium/test_library_to_collections.py
@@ -10,28 +10,28 @@ class LibraryToCollectionsTestCase(SeleniumTestCase, UsesLibraryAssertions):
     requires_admin = True
 
     @selenium_test
-    def test_library_collection_export(self):
-        self.collection_export()
-
-    @selenium_test
     def test_library_collection_export_new_history(self):
         self.collection_export(is_new_history=True)
 
     @selenium_test
-    def test_library_pair_export(self):
-        self.collection_export(collection_option="paired")
+    def test_library_collection_export(self):
+        self.collection_export()
 
     @selenium_test
     def test_library_pair_export_new_history(self):
         self.collection_export(is_new_history=True, collection_option="paired")
 
     @selenium_test
-    def test_export_pairs_list(self):
-        self.list_of_pairs_export()
+    def test_library_pair_export(self):
+        self.collection_export(collection_option="paired")
 
     @selenium_test
     def test_export_pairs_list_new_history(self):
         self.list_of_pairs_export(is_new_history=True)
+
+    @selenium_test
+    def test_export_pairs_list(self):
+        self.list_of_pairs_export()
 
     def prepare_library_for_data_export(self, files_to_import, history_name=None):
         self.navigate_to_new_library()
@@ -42,8 +42,7 @@ class LibraryToCollectionsTestCase(SeleniumTestCase, UsesLibraryAssertions):
         self.components.libraries.folder.add_to_history.wait_for_and_click()
         self.components.libraries.folder.add_to_history_collection.wait_for_and_click()
         if history_name is not None:
-            random_name = self._get_random_name()
-            self.components.libraries.folder.export_to_history_new_history.wait_for_and_send_keys(random_name)
+            self.components.libraries.folder.export_to_history_new_history.wait_for_and_send_keys(history_name)
 
     def build_collection_and_assert(self):
         self.collection_builder_set_name(self._get_random_name())
@@ -52,10 +51,8 @@ class LibraryToCollectionsTestCase(SeleniumTestCase, UsesLibraryAssertions):
         self.history_panel_wait_for_hid_ok(3)
 
     def collection_export(self, is_new_history=False, collection_option=None):
-        random_name = None
-        if is_new_history:
-            random_name = self._get_random_name()
-        self.prepare_library_for_data_export(["1.bam", "1.bed"], random_name)
+        random_name = self._get_random_name()
+        self.prepare_library_for_data_export(["1.bam", "1.bed"], random_name if is_new_history else None)
         self.components.libraries.folder.export_to_history_options.wait_for_and_click()
 
         if collection_option is not None:
@@ -68,7 +65,9 @@ class LibraryToCollectionsTestCase(SeleniumTestCase, UsesLibraryAssertions):
             assert self.history_panel_name_element().text == random_name
 
     def list_of_pairs_export(self, is_new_history=False):
-        self.prepare_library_for_data_export(["bam_from_sam.bam", "asian_chars_1.txt", "1.bam", "1.bed"])
+        history_name = self._get_random_name()
+        self.prepare_library_for_data_export(["bam_from_sam.bam", "asian_chars_1.txt", "1.bam", "1.bed"],
+                                             history_name if is_new_history else None)
         self.components.libraries.folder.export_to_history_options.wait_for_and_click()
         self.components.libraries.folder.export_to_history_paired_option(collection_option="list:paired").wait_for_and_click()
         self.screenshot(f'test_export_pairs_list={is_new_history}')
@@ -82,3 +81,5 @@ class LibraryToCollectionsTestCase(SeleniumTestCase, UsesLibraryAssertions):
         self.history_panel_wait_for_hid_ok(3)
         self.history_panel_wait_for_hid_hidden(1)
         self.history_panel_wait_for_hid_hidden(2)
+        if is_new_history:
+            assert self.history_panel_name_element().text == history_name
