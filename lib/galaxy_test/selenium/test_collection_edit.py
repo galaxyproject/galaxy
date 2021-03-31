a63111a324de1030047c1ea371109dfff831358b
Assunta DeSanto <assuntad23@gmail.com>
2021-03-31 10:41:07 -0400

    sql error in terminal, test fails waiting on upload

--- lib/galaxy_test/selenium/test_collection_edit.py
+++ lib/galaxy_test/selenium/test_collection_edit.py
@@ -10,8 +10,14 @@ class CollectionEditTestCase(SeleniumTestCase):
 
     @selenium_test
     def test_change_dbkey_simple_list(self):
+        self.click_history_options()
+        self.components.history_panel.options_use_beta_history.wait_for_and_click()
+
         self.perform_upload(self.get_filename("1.fasta"))
+        # self.wait(60000)
+        # self.history_panel_wait_for_hid_ok(1, allowed_force_refreshes=2)
         self.history_panel_wait_for_hid_ok(1)
+
         self.history_panel_multi_operations_show()
         self.history_panel_muli_operation_select_hid(1)
         self.history_panel_multi_operation_action_click(self.navigation.history_panel.multi_operations.labels.build_list)
@@ -21,6 +27,7 @@ class CollectionEditTestCase(SeleniumTestCase):
         self.collection_builder_create()
         self.history_panel_wait_for_hid_ok(2)
         
+
         # assert dbkey is hg 17 (?)
 
         # open edit collection view
