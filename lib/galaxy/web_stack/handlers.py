546935a5fb32440e72d9a6571f953b0c77d10ccb
mvdbeek <m.vandenbeek@gmail.com>
2021-04-06 15:20:13 +0200

    Add log statements when determining best handler assignment method

--- lib/galaxy/web_stack/handlers.py
+++ lib/galaxy/web_stack/handlers.py
@@ -77,8 +77,10 @@ class ConfiguresHandlers:
 
     def get_preferred_handler_assignment_method(self):
         if not self.supports_returning(self.app.model.session):
+            log.debug("Database does not support RETURNING statement, cannot use DB-SKIP-LOCKED or DB-TRANSACTION-ISOLATION handler assignment")
             return None
         if self.supports_skip_locked(self.app.model.session):
+            log.debug("Database does not support WITH FOR UPDATE statement, cannot use DB-SKIP-LOCKED handler assignment")
             return HANDLER_ASSIGNMENT_METHODS.DB_SKIP_LOCKED
         return HANDLER_ASSIGNMENT_METHODS.DB_TRANSACTION_ISOLATION
 
