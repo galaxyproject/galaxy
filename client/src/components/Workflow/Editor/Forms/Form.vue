546e981ed561bb40afda5ab897e0dc13d2d5f702
guerler <aysam.guerler@gmail.com>
2021-04-12 12:49:55 -0400

    Add form components for editor

---
+++ client/src/components/Workflow/Editor/Forms/Form.vue
@@ -0,0 +1,28 @@
+<template>
+    <Form inputs="inputs" />
+</template>
+
+<script>
+import Form from "mvc/form/form-view";
+
+export default {
+    props: {
+        getNode: () => {},
+    },
+    mounted() {
+        this.$nextTick(() => {
+            const el = this.$refs["inputs"];
+            this.form = new Form({
+                el,
+                inputs: this.inputs,
+            }).on("change", this.onChange);
+            this.onChange();
+        });
+    },
+    methods: {
+        onChange() {
+            this.$emit("onChange", this.form.data.create());
+        },
+    },
+};
+</script>
---
+++ client/src/components/Workflow/Editor/Forms/FormDefault.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>Test</div>
+</template>
+
+<script>
+
+export default {
+    props: {
+    },
+    mounted() {
+    },
+    methods: {
+    },
+};
+</script>
