999052ad0de62b14a89908967bf8335049b673ad
davelopez <46503462+davelopez@users.noreply.github.com>
2021-04-14 17:25:32 +0200

    Add pydantic models for HDCAs and related entities

--- lib/galaxy/schema/schema.py
+++ lib/galaxy/schema/schema.py
@@ -4,6 +4,7 @@ from typing import (
     Any,
     List,
     Optional,
+    Union,
 )
 
 from pydantic import (
@@ -15,7 +16,11 @@ from pydantic import (
     UUID4,
 )
 
-from galaxy.model import Dataset, DatasetInstance
+from galaxy.model import (
+    Dataset,
+    DatasetCollection,
+    DatasetInstance,
+)
 from galaxy.schema.fields import (
     AccessibleField,
     AnnotationField,
@@ -27,11 +32,79 @@ from galaxy.schema.fields import (
 
 USER_MODEL_CLASS_NAME = "User"
 HDA_MODEL_CLASS_NAME = "HistoryDatasetAssociation"
+DC_MODEL_CLASS_NAME = "DatasetCollection"
+DCE_MODEL_CLASS_NAME = "DatasetCollectionElement"
+HDCA_MODEL_CLASS_NAME = "HistoryDatasetCollectionAssociation"
+
+EncodedEntityIdField: EncodedDatabaseIdField = Field(
+    ...,
+    title="ID",
+    description="The encoded ID of this entity.",
+)
 
 DatasetStateField: Dataset.states = Field(
     ...,
     title="State",
     description="The current state of this dataset.",
+)
+
+CreateTimeField: datetime = Field(
+    ...,
+    title="Create Time",
+    description="The time and date this item was created.",
+)
+
+UpdateTimeField: datetime = Field(
+    ...,
+    title="Update Time",
+    description="The last time and date this item was updated.",
+)
+
+CollectionTypeField: str = Field(
+    ...,
+    title="Collection Type",
+    description=(
+        "The type of the collection, can be `list`, `paired`, or define subcollections using `:` "
+        "as separator like `list:paired` or `list:list`."
+    ),
+)
+
+PopulatedStateField: DatasetCollection.populated_states = Field(
+    ...,
+    title="Populated State",
+    description=(
+        "Indicates the general state of the elements in the dataset collection:"
+        "- 'new': new dataset collection, unpopulated elements."
+        "- 'ok': collection elements populated (HDAs may or may not have errors)."
+        "- 'failed': some problem populating, won't be populated."
+    ),
+)
+
+PopulatedStateMessageField: Optional[str] = Field(
+    None,
+    title="Populated State Message",
+    description="Optional message with further information in case the population of the dataset collection failed.",
+)
+
+ElementCountField: Optional[int] = Field(
+    None,
+    title="Element Count",
+    description=(
+        "The number of elements contained in the dataset collection. "
+        "It may be None or undefined if the collection could not be populated."
+    ),
+)
+
+PopulatedField: bool = Field(
+    ...,
+    title="Populated",
+    description="Whether the dataset collection elements (and any subcollections elements) were successfully populated.",
+)
+
+ElementsField: List['DCESummary'] = Field(
+    [],
+    title="Elements",
+    description="The summary information of each of the elements inside the dataset collection.",
 )
 
 
@@ -43,15 +116,30 @@ class UserModel(BaseModel):
     active: bool = Field(title='Active', description='User is active')
     deleted: bool = Field(title='Deleted', description='User is deleted')
     last_password_change: datetime = Field(title='Last password change', description='')
-    model_class: str = ModelClassField(USER_MODEL_CLASS_NAME)
+    model_class = ModelClassField(USER_MODEL_CLASS_NAME)
+
+
+class JobSourceType(str, Enum):
+    """Available types of job sources (model classes) that produce dataset collections."""
+    Job = "Job"
+    ImplicitCollectionJobs = "ImplicitCollectionJobs"
+    WorkflowInvocation = "WorkflowInvocation"
 
 
 class HistoryContentType(str, Enum):
+    """Available types of History contents."""
     dataset = "dataset"
     dataset_collection = "dataset_collection"
 
 
+class DCEType(str, Enum):  # TODO: suspiciously similar to HistoryContentType
+    """Available types of dataset collection elements."""
+    hda = "hda"
+    dataset_collection = "dataset_collection"
+
+
 class TagCollection(BaseModel):
+    """Represents the collection of tags associated with an item."""
     __root__: List[str] = Field(
         [],
         title="Tags",
@@ -60,15 +148,17 @@ class TagCollection(BaseModel):
 
 
 class MetadataFile(BaseModel):
+    """Metadata file associated with a dataset."""
     file_type: str = Field(
         ...,
         title="File Type",
         description="TODO",
     )
-    download_url: AnyUrl = DownloadUrlField
+    download_url = DownloadUrlField
 
 
 class DatasetPermissions(BaseModel):
+    """Role-based permissions for accessing and managing a dataset."""
     manage: List[EncodedDatabaseIdField] = Field(
         [],
         title="Management",
@@ -82,6 +172,7 @@ class DatasetPermissions(BaseModel):
 
 
 class Hyperlink(BaseModel):
+    """Represents some text with an Hyperlink."""
     target: str = Field(
         ...,
         title="Target",
@@ -101,6 +192,7 @@ class Hyperlink(BaseModel):
 
 
 class DisplayApp(BaseModel):
+    """Basic linked information about an application that can display certain datatypes."""
     label: str = Field(
         ...,
         title="Label",
@@ -119,11 +211,8 @@ class Visualization(BaseModel):  # TODO annotate this model
 
 
 class HistoryItemBase(BaseModel):
-    id: EncodedDatabaseIdField = Field(
-        ...,
-        title="ID",
-        description="The encoded ID of the item.",
-    )
+    """Common basic information provided by items contained in a History."""
+    id = EncodedEntityIdField
     name: str = Field(
         ...,
         title="Name",
@@ -157,6 +246,7 @@ class HistoryItemBase(BaseModel):
 
 
 class HistoryItemSummary(HistoryItemBase):
+    """Common summary information provided by items contained in a History."""
     type_id: str = Field(
         ...,
         title="Type - ID",
@@ -168,27 +258,20 @@ class HistoryItemSummary(HistoryItemBase):
         title="Type",
         description="The type of this item.",
     )
-    create_time: datetime = Field(
-        ...,
-        title="Create Time",
-        description="The time and date this item was created.",
-    )
-    update_time: datetime = Field(
-        ...,
-        title="Update Time",
-        description="The last time and date this item was updated.",
-    )
+    create_time = CreateTimeField
+    update_time = UpdateTimeField
     url: AnyUrl = UrlField
     tags: TagCollection
 
 
 class HDASummary(HistoryItemSummary):
+    """History Dataset Association summary information."""
     dataset_id: EncodedDatabaseIdField = Field(
         ...,
         title="Dataset ID",
         description="The encoded ID of the dataset associated with this item.",
     )
-    state: Dataset.states = DatasetStateField
+    state = DatasetStateField
     extension: str = Field(
         ...,
         title="Extension",
@@ -203,12 +286,14 @@ class HDASummary(HistoryItemSummary):
 
 
 class HDAInaccessible(HistoryItemBase):
-    accessible: bool = AccessibleField
-    state: Dataset.states = DatasetStateField
+    """History Dataset Association information when is inaccessible to the user."""
+    accessible = AccessibleField
+    state = DatasetStateField
 
 
 class HDADetailed(HDASummary):
-    model_class: str = ModelClassField(HDA_MODEL_CLASS_NAME)
+    """History Dataset Association detailed information."""
+    model_class = ModelClassField(HDA_MODEL_CLASS_NAME)
     hda_ldda: str = Field(
         "hda",
         const=True,
@@ -216,7 +301,7 @@ class HDADetailed(HDASummary):
         description="Whether this dataset belongs to a history (HDA) or a library (LDDA).",
         deprecated=False  # TODO Should this field be deprecated in favor of model_class?
     )
-    accessible: bool = AccessibleField
+    accessible = AccessibleField
     genome_build: str = Field(
         "?",
         title="Genome Build",
@@ -319,8 +404,8 @@ class HDADetailed(HDASummary):
         title="Validated State Message",
         description="The message with details about the datatype validation result for this dataset.",
     )
-    annotation: Optional[str] = AnnotationField
-    download_url: AnyUrl = DownloadUrlField
+    annotation = AnnotationField
+    download_url = DownloadUrlField
     type: str = Field(
         "file",
         const=True,
@@ -342,6 +427,7 @@ class HDADetailed(HDASummary):
 
 
 class HDAExtended(HDADetailed):
+    """History Dataset Association extended information."""
     tool_version: str = Field(
         ...,
         title="Tool Version",
@@ -357,3 +443,83 @@ class HDAExtended(HDADetailed):
         title="Designation",
         description="TODO",
     )
+
+
+class DCSummary(BaseModel):
+    """Dataset Collection summary information."""
+    model_class = ModelClassField(DC_MODEL_CLASS_NAME)
+    id = EncodedEntityIdField
+    create_time = CreateTimeField
+    update_time = UpdateTimeField
+    collection_type = CollectionTypeField
+    populated_state = PopulatedStateField
+    populated_state_message = PopulatedStateMessageField
+    element_count = ElementCountField
+
+
+class DCDetailed(DCSummary):
+    """Dataset Collection detailed information."""
+    populated = PopulatedField
+    elements = ElementsField
+
+
+class DCESummary(BaseModel):
+    """Dataset Collection Element summary information."""
+    id = EncodedEntityIdField
+    model_class = ModelClassField(DCE_MODEL_CLASS_NAME)
+    element_index: int = Field(
+        ...,
+        title="Element Index",
+        description="The position index of this element inside the collection.",
+    )
+    element_identifier: str = Field(
+        ...,
+        title="Element Identifier",
+        description="The actual name of this element.",
+    )
+    element_type: DCEType = Field(
+        ...,
+        title="Element Type",
+        description="The type of the element. Used to interpret the `object` field.",
+    )
+    object: Union[HDASummary, DCSummary] = Field(
+        ...,
+        title="Object",
+        description="The element's specific data depending on the value of `element_type`.",
+    )
+
+
+class HDCASummary(HistoryItemSummary):
+    """History Dataset Collection Association summary information."""
+    model_class = ModelClassField(HDCA_MODEL_CLASS_NAME)  # TODO: inconsistency? HDASummary does not have model_class only the detailed view has it...
+    type: str = Field(
+        "collection",
+        const=True,
+        title="Type",
+        description="This is always `collection` for dataset collections.",
+    )
+    collection_type = CollectionTypeField
+    populated_state = PopulatedStateField
+    populated_state_message = PopulatedStateMessageField
+    element_count = ElementCountField
+    job_source_id: Optional[EncodedDatabaseIdField] = Field(
+        None,
+        title="Job Source ID",
+        description="The encoded ID of the Job that produced this dataset collection. Used to track the state of the job.",
+    )
+    job_source_type: Optional[JobSourceType] = Field(
+        None,
+        title="Job Source Type",
+        description="The type of job (model class) that produced this dataset collection. Used to track the state of the job.",
+    )
+    contents_url: AnyUrl = Field(
+        ...,
+        title="Contents URL",
+        description="The relative URL to access the contents of this dataset collection.",
+    )
+
+
+class HDCADetailed(HDCASummary):
+    """History Dataset Collection Association detailed information."""
+    populated = PopulatedField
+    elements = ElementsField
