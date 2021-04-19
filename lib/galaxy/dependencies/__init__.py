4361e1001d3dba6173e45de7023b7b9a405c9d85
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-04-19 14:12:02 +0200

    Also check for boolean True, not just string 'True' when evaluating conditional dependencies.
    When people know how actual yaml works they might have
    
        galaxy:
            watch_tools: true
    
    as their config. Which signifies an actual Boolean value. This should be considered by the script as well.

--- lib/galaxy/dependencies/__init__.py
+++ lib/galaxy/dependencies/__init__.py
@@ -232,7 +232,7 @@ class ConditionalDependencies:
         return 's3fs' in self.file_sources
 
     def check_watchdog(self):
-        install_set = {'auto', 'True', 'true', 'polling'}
+        install_set = {'auto', 'True', 'true', 'polling', True}
         return (self.config['watch_tools'] in install_set
                 or self.config['watch_tool_data_dir'] in install_set)
 
