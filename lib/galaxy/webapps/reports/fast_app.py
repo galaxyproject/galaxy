4d6f9358f7c16d4e2ff0ccb13f3cc26a735b8914
davelopez <46503462+davelopez@users.noreply.github.com>
2021-04-20 11:33:22 +0200

    Add basic FastAPI metatada to entry points

--- lib/galaxy/webapps/reports/fast_app.py
+++ lib/galaxy/webapps/reports/fast_app.py
@@ -3,7 +3,14 @@ from fastapi.middleware.wsgi import WSGIMiddleware
 
 
 def initialize_fast_app(gx_webapp):
-    app = FastAPI()
+    app = FastAPI(
+        title="Galaxy Reports API",
+        description=(
+            "This API will give you insights into the Galaxy instance's usage and load. "
+            "It aims to provide data about users, jobs, workflows, disk space, and much more."
+        ),
+        docs_url="/api/docs",
+    )
     wsgi_handler = WSGIMiddleware(gx_webapp)
     app.mount('/', wsgi_handler)
     return app
--- lib/tool_shed/webapp/fast_app.py
+++ lib/tool_shed/webapp/fast_app.py
@@ -3,7 +3,13 @@ from fastapi.middleware.wsgi import WSGIMiddleware
 
 
 def initialize_fast_app(gx_webapp):
-    app = FastAPI()
+    app = FastAPI(
+        title="Galaxy Tool Shed API",
+        description=(
+            "This API allows you to manage the Tool Shed repositories."
+        ),
+        docs_url="/api/docs",
+    )
     wsgi_handler = WSGIMiddleware(gx_webapp)
     app.mount('/', wsgi_handler)
     return app
