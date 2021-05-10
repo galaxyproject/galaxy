a1ececdcf8a3706124851983db74864ab9c7a720
mvdbeek <m.vandenbeek@gmail.com>
2021-05-10 13:51:50 +0200

    Fix contianer port parsing if ::: in port line
    :: is an unspecified ipv6 address and just recently appeared in the
    docker port output.

--- lib/galaxy/tool_util/deps/docker_util.py
+++ lib/galaxy/tool_util/deps/docker_util.py
@@ -230,8 +230,12 @@ def parse_port_text(port_text):
         for line in port_text.strip().split('\n'):
             if " -> " not in line:
                 raise Exception("Cannot parse host and port from line [%s]" % line)
+            if ':::' in line:
+                # Skip unspecified ipv6 address.
+                # This is brittle of course, but so is parsing the container ports like this.
+                continue
             tool, host = line.split(" -> ", 1)
-            hostname, port = host.split(':')
+            hostname, port = host.rsplit(':')
             port = int(port)
             tool_p, tool_prot = tool.split("/")
             tool_p = int(tool_p)
