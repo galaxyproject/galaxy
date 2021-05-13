94c4615817051e890970c77231208c3a5b2af7d9
John Chilton <jmchilton@gmail.com>
2021-05-13 14:06:31 -0400

    Rebase into state decorating.

--- client/src/mvc/history/hdca-li.js
+++ client/src/mvc/history/hdca-li.js
@@ -75,6 +75,7 @@ var HDCAListItemView = _super.extend(
                 state = this.model.get("populated_state") ? STATES.OK : STATES.RUNNING;
             }
             this.$el.addClass(`state-${state}`);
+            this.$el.attr("data-state", state);
             const collection = this.model;
             const stateContainer = this.$el.find(".state-description")[0];
             mountCollectionJobStates({ jobStatesSummary, collection }, stateContainer);
