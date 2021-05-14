galaxyproject/galaxy

commit 71a70ea1f12cca48c33ee5569bd74035f96376f7

Don't execute involucro commands via shell
Move parameter quoting to invfile.lua

lib/galaxy/tool_util/deps/mulled/invfile.lua
@@ -19,13 +19,13 @@ local repo = VAR.REPO
 local channel_args = ''
 local channels = VAR.CHANNELS:split(",")
 for i = 1, #channels do
-    channel_args = channel_args .. " -c " .. channels[i]
+    channel_args = channel_args .. " -c '" .. channels[i] .. "'"
 end
 
 local target_args = ''
 local targets = VAR.TARGETS:split(",")
 for i = 1, #targets do
-    target_args = target_args .. " " .. targets[i]
+    target_args = target_args .. " '" .. targets[i] .. "'"
 end
 
 local bind_args = {}
@@ -101,10 +101,11 @@ inv.task('build')
 if VAR.SINGULARITY ~= '' then
     inv.task('singularity')
         .using(singularity_image)
-        .withHostConfig({binds = {"build:/data",singularity_image_dir .. ":/import"}, privileged = true})
+        .withHostConfig({binds = {"build:/data", "'" .. singularity_image_dir .. "':/import"}, privileged = true})
         .withConfig({entrypoint = {'/bin/sh', '-c'}})
-        .run('mkdir -p /usr/local/var/singularity/mnt/container && singularity build /import/' .. VAR.SINGULARITY_IMAGE_NAME .. ' /import/Singularity.def')
-        .run('chown ' .. VAR.USER_ID .. ' /import/' .. VAR.SINGULARITY_IMAGE_NAME)
+        .run('mkdir', '-p', '/usr/local/var/singularity/mnt/container')
+        .run('singularity', 'build', '/import/' .. VAR.SINGULARITY_IMAGE_NAME, '/import/Singularity.def')
+        .run('chown', VAR.USER_ID, '/import/' .. VAR.SINGULARITY_IMAGE_NAME)
 end
 
 inv.task('cleanup')

lib/galaxy/tool_util/deps/commands.py
@@ -118,16 +118,12 @@ def _wait(cmds, **popen_kwds):
     return stdout
 
 
-def download_command(url, to=STDOUT_INDICATOR, quote_url=False):
+def download_command(url, to=STDOUT_INDICATOR):
     """Build a command line to download a URL.
 
     By default the URL will be downloaded to standard output but a specific
     file can be specified with the `to` argument.
     """
-    if quote_url:
-        url = "'%s'" % url
-        if to != STDOUT_INDICATOR:
-            to = "'%s'" % to
     if which("wget"):
         download_cmd = ["wget", "-q"]
         if to == STDOUT_INDICATOR:

lib/galaxy/tool_util/deps/conda_util.py
@@ -435,7 +435,7 @@ def hash_conda_packages(conda_packages, conda_target=None):
 def install_conda(conda_context, force_conda_build=False):
     f, script_path = tempfile.mkstemp(suffix=".sh", prefix="conda_install")
     os.close(f)
-    download_cmd = commands.download_command(conda_link(), to=script_path, quote_url=False)
+    download_cmd = commands.download_command(conda_link(), to=script_path)
     install_cmd = ['bash', script_path, '-b', '-p', conda_context.conda_prefix]
     package_targets = [
         "conda=%s" % CONDA_VERSION,

lib/galaxy/tool_util/deps/mulled/mulled_build.py
@@ -14,6 +14,7 @@ import json
 import logging
 import os
 import shutil
+import stat
 import string
 import subprocess
 import sys
@@ -227,10 +228,10 @@ def mull_targets(
     bind_str = ",".join(binds)
     involucro_args = [
         '-f', '%s/invfile.lua' % DIRNAME,
-        '-set', "CHANNELS='%s'" % channels,
-        '-set', "TARGETS='%s'" % target_str,
-        '-set', "REPO='%s'" % repo,
-        '-set', "BINDS='%s'" % bind_str,
+        '-set', "CHANNELS=%s" % channels,
+        '-set', "TARGETS=%s" % target_str,
+        '-set', "REPO=%s" % repo,
+        '-set', "BINDS=%s" % bind_str,
     ]
     dest_base_image = None
     if base_image:
@@ -241,22 +242,22 @@ def mull_targets(
         dest_base_image = base_image_for_targets(targets)
 
     if dest_base_image:
-        involucro_args.extend(["-set", "DEST_BASE_IMAGE='%s'" % dest_base_image])
+        involucro_args.extend(["-set", "DEST_BASE_IMAGE=%s" % dest_base_image])
     if CONDA_IMAGE:
-        involucro_args.extend(["-set", "CONDA_IMAGE='%s'" % CONDA_IMAGE])
+        involucro_args.extend(["-set", "CONDA_IMAGE=%s" % CONDA_IMAGE])
     if verbose:
-        involucro_args.extend(["-set", "VERBOSE='1'"])
+        involucro_args.extend(["-set", "VERBOSE=1"])
     if singularity:
         singularity_image_name = repo_template_kwds['image']
-        involucro_args.extend(["-set", "SINGULARITY='1'"])
-        involucro_args.extend(["-set", "SINGULARITY_IMAGE_NAME='%s'" % singularity_image_name])
-        involucro_args.extend(["-set", "SINGULARITY_IMAGE_DIR='%s'" % singularity_image_dir])
-        involucro_args.extend(["-set", "USER_ID='%s:%s'" % (os.getuid(), os.getgid())])
+        involucro_args.extend(["-set", "SINGULARITY=1"])
+        involucro_args.extend(["-set", "SINGULARITY_IMAGE_NAME=%s" % singularity_image_name])
+        involucro_args.extend(["-set", "SINGULARITY_IMAGE_DIR=%s" % singularity_image_dir])
+        involucro_args.extend(["-set", "USER_ID=%s:%s" % (os.getuid(), os.getgid())])
     if test:
-        involucro_args.extend(["-set", "TEST=%s" % shlex_quote(test)])
+        involucro_args.extend(["-set", "TEST=%s" % test])
     if conda_version is not None:
         verbose = "--verbose" if verbose else "--quiet"
-        involucro_args.extend(["-set", "PREINSTALL='conda install %s --yes conda=%s'" % (verbose, conda_version)])
+        involucro_args.extend(["-set", "PREINSTALL=conda install %s --yes conda=%s" % (verbose, conda_version)])
     involucro_args.append(command)
     if test_files:
         test_bind = []
@@ -269,24 +270,25 @@ def mull_targets(
                     test_bind.append(test_file)
         if test_bind:
             involucro_args.insert(6, '-set')
-            involucro_args.insert(7, "TEST_BINDS='%s'" % ",".join(test_bind))
-    print(" ".join(involucro_context.build_command(involucro_args)))
-    if not dry_run:
-        ensure_installed(involucro_context, True)
-        if singularity:
-            if not os.path.exists(singularity_image_dir):
-                safe_makedirs(singularity_image_dir)
-            with open(os.path.join(singularity_image_dir, 'Singularity.def'), 'w+') as sin_def:
-                fill_template = SINGULARITY_TEMPLATE % {'container_test': test, 'base_image': dest_base_image or DEFAULT_BASE_IMAGE}
-                sin_def.write(fill_template)
-        with PrintProgress():
-            ret = involucro_context.exec_command(involucro_args)
-        if singularity:
-            # we can not remove this folder as it contains the image wich is owned by root
-            pass
-            # shutil.rmtree('./singularity_import')
-        return ret
-    return 0
+            involucro_args.insert(7, "TEST_BINDS=%s" % ",".join(test_bind))
+    cmd = involucro_context.build_command(involucro_args)
+    print('Executing: ' + ' '.join(shlex_quote(_) for _ in cmd))
+    if dry_run:
+        return 0
+    ensure_installed(involucro_context, True)
+    if singularity:
+        if not os.path.exists(singularity_image_dir):
+            safe_makedirs(singularity_image_dir)
+        with open(os.path.join(singularity_image_dir, 'Singularity.def'), 'w+') as sin_def:
+            fill_template = SINGULARITY_TEMPLATE % {'container_test': test, 'base_image': dest_base_image or DEFAULT_BASE_IMAGE}
+            sin_def.write(fill_template)
+    with PrintProgress():
+        ret = involucro_context.exec_command(involucro_args)
+    if singularity:
+        # we can not remove this folder as it contains the image wich is owned by root
+        pass
+        # shutil.rmtree('./singularity_import')
+    return ret
 
 
 def context_from_args(args):
@@ -335,7 +337,7 @@ class InvolucroContext(installable.InstallableContext):
             created_build_dir = True
             os.mkdir('./build')
         try:
-            res = self.shell_exec(" ".join(cmd))
+            res = self.shell_exec(cmd)
         finally:
             # delete build directory in any case
             if created_build_dir:
@@ -357,12 +359,19 @@ def ensure_installed(involucro_context, auto_init):
     return installable.ensure_installed(involucro_context, install_involucro, auto_init)
 
 
-def install_involucro(involucro_context=None, to_path=None):
+def install_involucro(involucro_context):
     install_path = os.path.abspath(involucro_context.involucro_bin)
     involucro_context.involucro_bin = install_path
-    download_cmd = " ".join(commands.download_command(involucro_link(), to=install_path, quote_url=True))
-    full_cmd = "%s && chmod +x %s" % (download_cmd, install_path)
-    return involucro_context.shell_exec(full_cmd)
+    download_cmd = commands.download_command(involucro_link(), to=install_path)
+    exit_code = involucro_context.shell_exec(download_cmd)
+    if exit_code:
+        return exit_code
+    try:
+        os.chmod(install_path, os.stat(install_path).st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+        return 0
+    except Exception:
+        log.exception("Failed to make file '%s' executable", install_path)
+        return 1
 
 
 def add_build_arguments(parser):

commit fd03c446c0347517fb483fc4c65700ca9b342411

Fix mulled singularity building
This used to fail with
```
[May 13 12:53:06] DEBU Container [8a432293fb2f step-adb59c6b2d] started, waiting for completion
[May 13 12:53:06] SERR mkdir: missing operand
[May 13 12:53:06] SERR Try 'mkdir --help' for more information.
[May 13 12:53:06] ERRO Task processing failed: Unexpected exit code [1] of container [8a432293fb2f step-adb59c6b2d], container preserved
```
Involucro interpretes comma-separated arguments in `.run` as individual
commands. You can see the full error in https://github.com/BioContainers/multi-package-containers/pull/1713/checks?check_run_id=2575472382

Broke in https://github.com/mvdbeek/galaxy/commit/71a70ea1f12cca48c33ee5569bd74035f96376f7

lib/galaxy/tool_util/deps/mulled/invfile.lua
@@ -101,11 +101,11 @@ inv.task('build')
 if VAR.SINGULARITY ~= '' then
     inv.task('singularity')
         .using(singularity_image)
-        .withHostConfig({binds = {"build:/data", "'" .. singularity_image_dir .. "':/import"}, privileged = true})
+        .withHostConfig({binds = {"build:/data", singularity_image_dir .. ":/import"}, privileged = true})
         .withConfig({entrypoint = {'/bin/sh', '-c'}})
-        .run('mkdir', '-p', '/usr/local/var/singularity/mnt/container')
-        .run('singularity', 'build', '/import/' .. VAR.SINGULARITY_IMAGE_NAME, '/import/Singularity.def')
-        .run('chown', VAR.USER_ID, '/import/' .. VAR.SINGULARITY_IMAGE_NAME)
+        .run('mkdir -p /usr/local/var/singularity/mnt/container && '
+            .. 'singularity build /import/' .. VAR.SINGULARITY_IMAGE_NAME .. ' /import/Singularity.def && '
+            .. 'chown ' .. VAR.USER_ID .. ' /import/' .. VAR.SINGULARITY_IMAGE_NAME)
 end
 
 inv.task('cleanup')

test/unit/tool_util/mulled/test_mulled_build.py
@@ -5,6 +5,7 @@ from galaxy.tool_util.deps.mulled.mulled_build import (
     build_target,
     DEFAULT_BASE_IMAGE,
     DEFAULT_EXTENDED_BASE_IMAGE,
+    mull_targets,
 )
 from ..util import external_dependency_management
 
@@ -18,3 +19,11 @@ from ..util import external_dependency_management
 def test_base_image_for_targets(target, version, base_image):
     target = build_target(target, version=version)
     assert base_image_for_targets([target]) == base_image
+
+
+@external_dependency_management
+def test_mulled_build_files_cli(tmpdir):
+    singularity_image_dir = tmpdir.mkdir('singularity_image_dir')
+    target = build_target('zlib')
+    mull_targets([target], command='build-and-test', singularity=True, singularity_image_dir=singularity_image_dir)
+    assert singularity_image_dir.join('zlib').exists()