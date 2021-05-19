30a527a1c9e2b7a1923c1b3dcd12f14164c71ee0
Assunta DeSanto <assuntad23@gmail.com>
2021-05-18 23:50:29 -0400

    fixing some styling issues with the menu; adding a small modal for deleting collections

--- client/src/components/History/ContentItem/DatasetCollection/DscMenu.vue
+++ client/src/components/History/ContentItem/DatasetCollection/DscMenu.vue
@@ -14,24 +14,24 @@
                 v-if="notIn(STATES.DISCARDED)"
                 key="edit-collection"
                 :title="deleteCollectionButtonTitle"
-                @click.stop="$emit('delete')"
+                @click.stop="deleteCollectionModalShow = !deleteCollectionModalShow"
                 icon="fas fa-trash"
-            />
-            <PriorityMenuItem
-                v-if="notIn(STATES.DISCARDED)"
-                key="edit-collection"
-                :title="deleteDatasetsButtonTitle"
-                @click.stop="$emit('delete', { recursive: true })"
-                icon="fas fa-trash"
-            />
-            <PriorityMenuItem
-                v-if="notIn(STATES.DISCARDED)"
-                key="edit-collection"
-                :title="purgeCollectionButtonTitle"
-                @click.stop="$emit('delete', { recursive: true, purge: true })"
-                icon="fas fa-trash"
-            />
+            >
+            </PriorityMenuItem>
         </PriorityMenu>
+        <b-modal v-model="deleteCollectionModalShow" @ok="runDelete(deleteSelected)">
+            <b-form-group label="Select a method to delete the collection">
+                <b-form-radio v-model="deleteSelected" name="delete-select" :value="null"
+                    >Delete Collection Only</b-form-radio
+                >
+                <b-form-radio v-model="deleteSelected" name="delete-select" :value="{ recursive: true }"
+                    >Delete Contained Datasets</b-form-radio
+                >
+                <b-form-radio v-model="deleteSelected" name="delete-select" :value="{ recursive: true, purge: true }"
+                    >Purge Contained Datasets</b-form-radio
+                >
+            </b-form-group>
+        </b-modal>
     </div>
 </template>
 
@@ -39,7 +39,17 @@
 import { DatasetCollection } from "../../model";
 import { PriorityMenu, PriorityMenuItem } from "components/PriorityMenu";
 import { legacyNavigationMixin } from "components/plugins/legacyNavigation";
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+
+Vue.use(BootstrapVue);
 export default {
+    data() {
+        return {
+            deleteSelected: null,
+            deleteCollectionModalShow: false,
+        };
+    },
     components: {
         PriorityMenu,
         PriorityMenuItem,
@@ -64,16 +74,17 @@ export default {
             return "Edit attributes";
         },
         deleteCollectionButtonTitle() {
-            return "Delete Collection Only";
-        },
-        deleteDatasetsButtonTitle() {
-            return "Delete Contained Datasets";
-        },
-        purgeCollectionButtonTitle() {
-            return "Purge Contained Datasets";
+            return "Delete Collection";
         },
     },
     methods: {
+        runDelete: function (kind) {
+            if (kind) {
+                this.$emit("delete", kind);
+            } else {
+                this.$emit("delete");
+            }
+        },
         collectionEditURL: function () {
             return "/collection/edit/" + this.collection.hdca_id;
         },
