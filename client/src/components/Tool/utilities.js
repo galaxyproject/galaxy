a2025333baee69a74b6468892104bcd40915090e
guerler <aysam.guerler@gmail.com>
2021-05-27 16:43:41 -0400

    Move utilities to tool card location

--- client/src/components/Tool/utilities.js
+++ client/src/components/Tool/utilities.js
@@ -1,4 +1,18 @@
 import $ from "jquery";
+import { getAppRoot } from "onload/loadConfig";
+import { copy } from "utils/clipboard";
+
+export function copyLink(toolId, message) {
+    copy(`${window.location.origin + getAppRoot()}root?tool_id=${toolId}`, message);
+}
+
+export function downloadTool(toolId) {
+    window.location.href = `${getAppRoot()}api/tools/${toolId}/download`;
+}
+
+export function openLink(url) {
+    window.open(url);
+}
 
 export function send(options, job_def) {
     const $f = $("<form/>").attr({
