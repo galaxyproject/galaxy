69341998ff9d54e0ae21ab8320b2195354024902
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2021-05-28 22:21:16 -0400

    Refactor tests; ensure db cleanup after each test

--- test/unit/model/test_model.py
+++ test/unit/model/test_model.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 import pytest
 from sqlalchemy import (
     delete,
@@ -6,6 +8,213 @@ from sqlalchemy import (
 )
 
 import galaxy.model.mapping as mapping
+
+
+def test_Group(model, session):
+    cls = model.Group
+    assert cls.__tablename__ == 'galaxy_group'
+    with dbcleanup(session, cls):
+        name = 'a'
+        obj = cls(name)
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.create_time
+        assert stored_obj.update_time
+        assert stored_obj.name == name
+        assert stored_obj.deleted is False
+
+
+def test_PageRevision(model, session, page):
+    cls = model.PageRevision
+    assert cls.__tablename__ == 'page_revision'
+    with dbcleanup(session, cls):
+        obj = cls()
+        obj.page = page
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.create_time
+        assert stored_obj.update_time
+        assert stored_obj.page_id
+        assert stored_obj.title is None
+        assert stored_obj.content is None
+        assert stored_obj.content_format == model.PageRevision.DEFAULT_CONTENT_FORMAT
+
+
+def test_Quota(model, session):
+    cls = model.Quota
+    assert cls.__tablename__ == 'quota'
+    with dbcleanup(session, cls):
+        name, description = 'a', 'b'
+        obj = cls(name, description)
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.create_time
+        assert stored_obj.update_time
+        assert stored_obj.name == name
+        assert stored_obj.description == description
+        assert stored_obj.bytes == 0
+        assert stored_obj.operation == '='
+        assert stored_obj.deleted is False
+
+
+def test_Role(model, session):
+    cls = model.Role
+    assert cls.__tablename__ == 'role'
+    with dbcleanup(session, cls):
+        name, description = 'a', 'b'
+        obj = cls(name, description)
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.create_time
+        assert stored_obj.update_time
+        assert stored_obj.name == name
+        assert stored_obj.description == description
+        assert stored_obj.type == model.Role.types.SYSTEM
+        assert stored_obj.deleted is False
+
+
+def test_WorkerProcess(model, session):
+    cls = model.WorkerProcess
+    assert cls.__tablename__ == 'worker_process'
+    with dbcleanup(session, cls):
+        server_name, hostname = 'a', 'b'
+        obj = cls(server_name, hostname)
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.server_name == server_name
+        assert stored_obj.hostname == hostname
+        assert stored_obj.pid is None
+        assert stored_obj.update_time
+
+
+def test_PSAAssociation(model, session):
+    cls = model.PSAAssociation
+    assert cls.__tablename__ == 'psa_association'
+    with dbcleanup(session, cls):
+        server_url, handle, secret, issued, lifetime, assoc_type = 'a', 'b', 'c', 1, 2, 'd'
+        obj = cls(server_url, handle, secret, issued, lifetime, assoc_type)
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.server_url == server_url
+        assert stored_obj.handle == handle
+        assert stored_obj.secret == secret
+        assert stored_obj.issued == issued
+        assert stored_obj.lifetime == lifetime
+        assert stored_obj.assoc_type == assoc_type
+
+
+def test_PSACode(model, session):
+    cls = model.PSACode
+    assert cls.__tablename__ == 'psa_code'
+    with dbcleanup(session, cls):
+        email, code = 'a', 'b'
+        obj = cls(email, code)
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.email == email
+        assert stored_obj.code == code
+
+
+def test_PSANonce(model, session):
+    cls = model.PSANonce
+    assert cls.__tablename__ == 'psa_nonce'
+    with dbcleanup(session, cls):
+        server_url, timestamp, salt = 'a', 1, 'b'
+        obj = cls(server_url, timestamp, salt)
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.server_url
+        assert stored_obj.timestamp == timestamp
+        assert stored_obj.salt == salt
+
+
+def test_PSAPartial(model, session):
+    cls = model.PSAPartial
+    assert cls.__tablename__ == 'psa_partial'
+    with dbcleanup(session, cls):
+        token, data, next_step, backend = 'a', 'b', 1, 'c'
+        obj = cls(token, data, next_step, backend)
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.token == token
+        assert stored_obj.data == data
+        assert stored_obj.next_step == next_step
+        assert stored_obj.backend == backend
+
+
+def test_UserAddress(model, session, user):
+    cls = model.UserAddress
+    assert cls.__tablename__ == 'user_address'
+    with dbcleanup(session, cls):
+        desc, name, institution, address, city, state, postal_code, country, phone = \
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'
+        obj = cls(user, desc, name, institution, address, city, state, postal_code, country, phone)
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.create_time
+        assert stored_obj.update_time
+        assert stored_obj.user_id == user.id
+        assert stored_obj.desc == desc
+        assert stored_obj.name == name
+        assert stored_obj.institution == institution
+        assert stored_obj.address == address
+        assert stored_obj.city == city
+        assert stored_obj.state == state
+        assert stored_obj.postal_code == postal_code
+        assert stored_obj.country == country
+        assert stored_obj.phone == phone
+        assert stored_obj.deleted is False
+        assert stored_obj.purged is False
+        assert stored_obj.user == user
+
+
+def test_VisualizationRevision(model, session, visualization):
+    cls = model.VisualizationRevision
+    assert cls.__tablename__ == 'visualization_revision'
+    with dbcleanup(session, cls):
+        obj = cls()
+        obj.visualization = visualization
+        persist(session, obj)
+
+        stmt = select(cls)
+        stored_obj = session.execute(stmt).scalar_one()
+        assert stored_obj.id
+        assert stored_obj.create_time
+        assert stored_obj.update_time
+        assert stored_obj.visualization_id
+        assert stored_obj.title is None
+        assert stored_obj.dbkey is None
+        assert stored_obj.config is None
 
 
 @pytest.fixture(scope='module')
@@ -22,287 +231,39 @@ def session(model):
 
 
 @pytest.fixture
-def user(model):
-    return model.User(email='test@example.com', password='password')
+def user(model, session):
+    u = model.User(email='test@example.com', password='password')
+    yield from dbcleanup_wrapper(session, u)
 
 
 @pytest.fixture
-def page(model, user):
+def page(model, session, user):
     p = model.Page()
     p.user = user
-    return p
+    yield from dbcleanup_wrapper(session, p)
 
 
 @pytest.fixture
-def visualization(model, user):
+def visualization(model, session, user):
     v = model.Visualization()
     v.user = user
-    return v
-
-
-def test_Group_table(model):
-    tbl = model.Group.__table__
-    assert tbl.name == 'galaxy_group'
-
-
-def test_Group(model, session):
-    cls = model.Group
-    name = 'a'
-    obj = cls(name)
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.create_time
-    assert stored_obj.update_time
-    assert stored_obj.name == name
-    assert stored_obj.deleted is False
-
-    cleanup(session, cls)
-
-
-def test_PageRevision_table(model):
-    tbl = model.PageRevision.__table__
-    assert tbl.name == 'page_revision'
-
-
-def test_PageRevision(model, session, page):
-    cls = model.PageRevision
-    obj = cls()
-    obj.page = page
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.create_time
-    assert stored_obj.update_time
-    assert stored_obj.page_id
-    assert stored_obj.title is None
-    assert stored_obj.content is None
-    assert stored_obj.content_format == model.PageRevision.DEFAULT_CONTENT_FORMAT
-
-    cleanup(session, cls)
-
-
-def test_Quota_table(model):
-    tbl = model.Quota.__table__
-    assert tbl.name == 'quota'
-
-
-def test_Quota(model, session):
-    cls = model.Quota
-    name, description = 'a', 'b'
-    obj = cls(name, description)
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.create_time
-    assert stored_obj.update_time
-    assert stored_obj.name == name
-    assert stored_obj.description == description
-    assert stored_obj.bytes == 0
-    assert stored_obj.operation == '='
-    assert stored_obj.deleted is False
-
-    cleanup(session, cls)
-
-
-def test_Role_table(model):
-    tbl = model.Role.__table__
-    assert tbl.name == 'role'
-
-
-def test_Role(model, session):
-    cls = model.Role
-    name, description = 'a', 'b'
-    obj = cls(name, description)
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.create_time
-    assert stored_obj.update_time
-    assert stored_obj.name == name
-    assert stored_obj.description == description
-    assert stored_obj.type == model.Role.types.SYSTEM
-    assert stored_obj.deleted is False
-
-    cleanup(session, cls)
-
-
-def test_WorkerProcess_table(model):
-    tbl = model.WorkerProcess.__table__
-    assert tbl.name == 'worker_process'
-    assert has_unique_constraint(tbl, ('server_name', 'hostname'))
-
-
-def test_WorkerProcess(model, session):
-    cls = model.WorkerProcess
-    server_name, hostname = 'a', 'b'
-    obj = cls(server_name, hostname)
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.server_name == server_name
-    assert stored_obj.hostname == hostname
-    assert stored_obj.pid is None
-    assert stored_obj.update_time
-
-    cleanup(session, cls)
-
-
-def test_PSAAssociation_table(model):
-    tbl = model.PSAAssociation.__table__
-    assert tbl.name == 'psa_association'
-
-
-def test_PSAAssociation(model, session):
-    cls = model.PSAAssociation
-    server_url, handle, secret, issued, lifetime, assoc_type = 'a', 'b', 'c', 1, 2, 'd'
-    obj = cls(server_url, handle, secret, issued, lifetime, assoc_type)
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.server_url == server_url
-    assert stored_obj.handle == handle
-    assert stored_obj.secret == secret
-    assert stored_obj.issued == issued
-    assert stored_obj.lifetime == lifetime
-    assert stored_obj.assoc_type == assoc_type
-
-    cleanup(session, cls)
-
-
-def test_PSACode_table(model):
-    tbl = model.PSACode.__table__
-    assert tbl.name == 'psa_code'
-    assert has_unique_constraint(tbl, ('code', 'email'))
-
-
-def test_PSACode(model, session):
-    cls = model.PSACode
-    email, code = 'a', 'b'
-    obj = cls(email, code)
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.email == email
-    assert stored_obj.code == code
-
-    cleanup(session, cls)
-
-
-def test_PSANonce_table(model):
-    tbl = model.PSANonce.__table__
-    assert tbl.name == 'psa_nonce'
-
-
-def test_PSANonce(model, session):
-    cls = model.PSANonce
-    server_url, timestamp, salt = 'a', 1, 'b'
-    obj = cls(server_url, timestamp, salt)
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.server_url
-    assert stored_obj.timestamp == timestamp
-    assert stored_obj.salt == salt
-
-    cleanup(session, cls)
-
-
-def test_PSAPartial_table(model):
-    tbl = model.PSAPartial.__table__
-    assert tbl.name == 'psa_partial'
-
-
-def test_PSAPartial(model, session):
-    cls = model.PSAPartial
-    token, data, next_step, backend = 'a', 'b', 1, 'c'
-    obj = cls(token, data, next_step, backend)
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.token == token
-    assert stored_obj.data == data
-    assert stored_obj.next_step == next_step
-    assert stored_obj.backend == backend
-
-    cleanup(session, cls)
-
-
-def test_UserAddress_table(model):
-    tbl = model.UserAddress.__table__
-    assert tbl.name == 'user_address'
-
-
-def test_UserAddress(model, session, user):
-    cls = model.UserAddress
-    desc, name, institution, address, city, state, postal_code, country, phone = \
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'
-    obj = cls(user, desc, name, institution, address, city, state, postal_code, country, phone)
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.create_time
-    assert stored_obj.update_time
-    assert stored_obj.user_id == user.id
-    assert stored_obj.desc == desc
-    assert stored_obj.name == name
-    assert stored_obj.institution == institution
-    assert stored_obj.address == address
-    assert stored_obj.city == city
-    assert stored_obj.state == state
-    assert stored_obj.postal_code == postal_code
-    assert stored_obj.country == country
-    assert stored_obj.phone == phone
-    assert stored_obj.deleted is False
-    assert stored_obj.purged is False
-    assert stored_obj.user == user
-
-    cleanup(session, cls)
-
-
-def test_VisualizationRevision_table(model):
-    tbl = model.VisualizationRevision.__table__
-    assert tbl.name == 'visualization_revision'
-    assert has_index(tbl, ('dbkey',))
-
-
-def test_VisualizationRevision(model, session, visualization):
-    cls = model.VisualizationRevision
-    obj = cls()
-    obj.visualization = visualization
-    persist(session, obj)
-
-    stmt = select(cls)
-    stored_obj = session.execute(stmt).scalar_one()
-    assert stored_obj.id
-    assert stored_obj.create_time
-    assert stored_obj.update_time
-    assert stored_obj.visualization_id
-    assert stored_obj.title is None
-    assert stored_obj.dbkey is None
-    assert stored_obj.config is None
-
-    cleanup(session, cls)
+    yield from dbcleanup_wrapper(session, v)
+
+
+@contextmanager
+def dbcleanup(session, cls):
+    """
+    Ensure all records of cls are deleted from the database on exit.
+    """
+    try:
+        yield
+    finally:
+        session.execute(delete(cls))
+
+
+def dbcleanup_wrapper(session, obj):
+    with dbcleanup(session, type(obj)):
+        yield obj
 
 
 def persist(session, obj):
@@ -310,10 +271,6 @@ def persist(session, obj):
     session.flush()
     # Remove from session, so that on a subsequent load we get a *new* obj from the db
     session.expunge(obj)
-
-
-def cleanup(session, cls):
-    session.execute(delete(cls))
 
 
 def has_unique_constraint(table, fields):
