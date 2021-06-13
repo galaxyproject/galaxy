a8ab3ed94f97f286abe5807fefb91059df599155
hadleyking <hadley_king@gwu.edu>
2021-06-13 19:44:19 -0400

    BCODB form Updated to add form for submission of BCO to BCODB Changes to be committed: 	new file:   client/src/components/WorkflowInvocationState/WorkflowInvocationBiocompute.vue 	modified:   client/src/components/WorkflowInvocationState/WorkflowInvocationState.vue

--- client/src/components/WorkflowInvocationState/WorkflowInvocationState.vue
+++ client/src/components/WorkflowInvocationState/WorkflowInvocationState.vue
@@ -49,9 +49,11 @@
             :error-count="errorCount"
             :loading="!invocationAndJobTerminal"
             class="jobs-progress" />
-        <span v-if="invocationAndJobTerminal">
-            <a class="bco-json" :href="bcoJSON"><b>Download BioCompute Object</b></a>
-        </span>
+        <workflow-invocation-biocompute
+            v-if="invocation"
+            :invocation="invocation.id"
+            :invocation-and-job-terminal="invocationAndJobTerminal"
+        />
         <workflow-invocation-details
             v-if="invocation"
             :invocation="invocation"
@@ -62,6 +64,7 @@
 import { cancelWorkflowScheduling } from "./services";
 import { getRootFromIndexLink } from "onload";
 import WorkflowInvocationDetails from "./WorkflowInvocationDetails";
+import WorkflowInvocationBiocompute from "./WorkflowInvocationBiocompute"
 
 import JOB_STATES_MODEL from "mvc/history/job-states-model";
 import mixin from "components/JobStates/mixin";
@@ -75,6 +78,7 @@ export default {
     components: {
         ProgressBar,
         WorkflowInvocationDetails,
+        WorkflowInvocationBiocompute,
     },
     mixins: [mixin],
     props: {
