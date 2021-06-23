2b76c4e9b6df734e190ecd65289b5e44165e745f
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2021-06-23 18:19:46 -0400

    Add unit test for HDCTagAssociation

--- test/unit/model/test_model.py
+++ test/unit/model/test_model.py
@@ -291,6 +291,26 @@ def test_HistoryDatasetCollectionRatingAssociation(
         assert stored_obj.rating == rating
 
 
+def test_HistoryDatasetCollectionTagAssociation(model, session, history_dataset_collection_association, tag, user):
+    cls = model.HistoryDatasetCollectionTagAssociation
+    # TODO assert cls.__tablename__ == ''
+    with dbcleanup(session, cls):
+        user_tname, value, user_value = 'a', 'b', 'c'
+        obj = cls(user=user, tag_id=tag.id, user_tname=user_tname, value=value)
+        obj.user_value = user_value
+        obj.history_dataset_collection_id = history_dataset_collection_association.id
+        obj_id = persist(session, obj)
+
+        stored_obj = get_stored_obj(session, cls, obj_id)
+        assert stored_obj.id == obj_id
+        assert stored_obj.history_dataset_collection_id == history_dataset_collection_association.id
+        assert stored_obj.tag_id == tag.id
+        assert stored_obj.user == user
+        assert stored_obj.user_tname == user_tname
+        assert stored_obj.value == value
+        assert stored_obj.user_value == user_value
+
+
 def test_HistoryRatingAssociation(model, session, history, user):
     cls = model.HistoryRatingAssociation
     assert cls.__tablename__ == 'history_rating_association'
