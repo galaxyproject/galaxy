galaxyproject/galaxy

commit 60efa8e094b4b2e70f59b8edda7c87998e6ecd99

history load fixes, rxjs provider debugging, upload init isolation

client/src/components/WorkflowInvocationState/DatasetCollectionUIWrapper.vue
@@ -5,12 +5,12 @@
             v-bind="$attrs"
             v-on="$listeners"
             :dsc="datasetCollection"
-            :showTags="true"
+            :show-tags="true"
             @update:expanded="toggleExpand"
-            @hideCollection="onHide(datasetCollection)"
-            @unhideCollection="onUnhide(datasetCollection)"
-            @deleteCollection="onDelete(datasetCollection, $event)"
-            @undeleteCollection="onUndelete(datasetCollection)"
+            @hide="onHide(datasetCollection)"
+            @unhide="onUnhide(datasetCollection)"
+            @delete="onDelete(datasetCollection, $event)"
+            @undelete="onUndelete(datasetCollection)"
         />
         <DatasetCollectionContentProvider v-if="expand" :id="datasetCollection.contents_url" v-slot="{ item, loading }">
             <b-spinner v-if="loading">Loading Dataset Collection...</b-spinner>

client/src/components/WorkflowInvocationState/DatasetCollectionUIWrapper.test.js
@@ -36,13 +36,13 @@ describe("DatasetUIWrapper.vue with Dataset", () => {
         expect(onUnhide).not.toHaveBeenCalled();
         expect(onDelete).not.toHaveBeenCalled();
         expect(onUndelete).not.toHaveBeenCalled();
-        wrapper.findComponent(DscUI).vm.$emit("hideCollection");
+        wrapper.findComponent(DscUI).vm.$emit("hide");
         expect(onHide).toHaveBeenCalled();
-        wrapper.findComponent(DscUI).vm.$emit("unhideCollection");
+        wrapper.findComponent(DscUI).vm.$emit("unhide");
         expect(onUnhide).toHaveBeenCalled();
-        wrapper.findComponent(DscUI).vm.$emit("deleteCollection");
+        wrapper.findComponent(DscUI).vm.$emit("delete");
         expect(onDelete).toHaveBeenCalled();
-        wrapper.findComponent(DscUI).vm.$emit("undeleteCollection");
+        wrapper.findComponent(DscUI).vm.$emit("undelete");
         expect(onUndelete).toHaveBeenCalled();
     });
     it("creates DatasetCollection", async () => {

commit fffb3c2e04447531ab596860961683624887beb9

Fix expanding collections in invocation view
Broke in
https://github.com/galaxyproject/galaxy/commit/60efa8e094b4b2e70f59b8edda7c87998e6ecd99.

client/src/components/WorkflowInvocationState/DatasetCollectionContents.vue
@@ -24,13 +24,12 @@
 <script>
 import { DatasetProvider } from "./providers";
 import DatasetUIWrapper from "./DatasetUIWrapper";
-import DatasetCollectionUIWrapper from "./DatasetCollectionUIWrapper";
 import LoadingSpan from "components/LoadingSpan";
 
 export default {
     components: {
         DatasetProvider,
-        DatasetCollectionUIWrapper,
+        DatasetCollectionUIWrapper: () => import("components/WorkflowInvocationState/DatasetCollectionUIWrapper"),
         DatasetUIWrapper,
         LoadingSpan,
     },

client/src/components/WorkflowInvocationState/DatasetCollectionUIWrapper.vue
@@ -6,7 +6,7 @@
             v-on="$listeners"
             :dsc="datasetCollection"
             :show-tags="true"
-            @update:expanded="toggleExpand"
+            @viewCollection="toggleExpand"
             @hide="onHide(datasetCollection)"
             @unhide="onUnhide(datasetCollection)"
             @delete="onDelete(datasetCollection, $event)"

lib/galaxy/selenium/navigation.yml
@@ -509,6 +509,12 @@ invocations:
     steps_details: '.workflow-invocation-state-component .invocation-steps-details'
     step_title: '.workflow-invocation-state-component .invocation-steps-details [data-step="${order_index}"] .step-title'
     step_details: '.workflow-invocation-state-component .invocation-steps-details [data-step="${order_index}"] .portlet-operations'
+    step_output_collection: '.workflow-invocation-state-component .invocation-steps-details [data-step="${order_index}"] .invocation-step-output-collection-details'
+    step_output_collection_toggle: '.workflow-invocation-state-component .invocation-steps-details [data-step="${order_index}"] .invocation-step-output-collection-details .name'
+    step_output_collection_element_identifier:
+      type: xpath
+      selector: '//span[@class="name"][text()="${element_identifier}"]'
+    step_output_collection_element_datatype: '.workflow-invocation-state-component .invocation-steps-details [data-step="${order_index}"] .invocation-step-output-collection-details .datatype .value'
     step_job_details: '.workflow-invocation-state-component .invocation-steps-details [data-step="${order_index}"] .invocation-step-job-details'
     step_job_table: '.workflow-invocation-state-component .invocation-steps-details [data-step="${order_index}"] .invocation-step-job-details table'
     step_job_table_rows: '.workflow-invocation-state-component .invocation-steps-details [data-step="${order_index}"] .invocation-step-job-details table tbody tr'

client/src/components/WorkflowInvocationState/DatasetCollectionUIWrapper.test.js
@@ -51,7 +51,7 @@ describe("DatasetUIWrapper.vue with Dataset", () => {
     });
     it("manages collection expansion", async () => {
         expect(wrapper.vm.expand).toBeFalsy();
-        wrapper.findComponent(DscUI).vm.$emit("update:expanded", wrapper.vm.datasetCollection);
+        wrapper.findComponent(DscUI).vm.$emit("viewCollection", wrapper.vm.datasetCollection);
         expect(wrapper.vm.expand).toBeTruthy();
     });
     it("build dsc from collection content", async () => {

lib/galaxy_test/selenium/test_workflow_invocation_details.py
@@ -66,3 +66,9 @@ class WorkflowInvocationDetailsTestCase(SeleniumTestCase):
 
         invocations.step_job_information(order_index="1").wait_for_visible()
         assert "collection_creates_pair" in invocations.step_job_information_tool_id(order_index="1").wait_for_visible().text
+
+        invocations.step_output_collection(order_index="1").wait_for_and_click()
+        invocations.step_output_collection_toggle(order_index="1").wait_for_and_click()
+        invocations.step_output_collection_element_identifier(element_identifier="forward").wait_for_and_click()
+        datatype = invocations.step_output_collection_element_datatype(order_index="1").wait_for_text()
+        assert datatype == 'txt'