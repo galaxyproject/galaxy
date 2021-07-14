3f485b6ceecb7f6b170471f3084a13c00a07f803
yingfeng-iu <yingfeng@iu.edu>
2021-07-14 19:20:35 -0400

    AMP-1314: - add python to MGM command - improve naming

--- tools/amp_stt/gentle_forced_alignment.py
+++ tools/amp_stt/gentle_forced_alignment.py
@@ -7,7 +7,6 @@ import shutil
 import subprocess
 import sys
 import tempfile
-
 import uuid
 
 import mgm_utils
@@ -17,66 +16,67 @@ def main():
 	(speech_audio, amp_transcript_unaligned, gentle_transcript, amp_transcript_aligned) = sys.argv[1:5]
 	exception = False
 	try:
-		# Create random temp names
+		# prefix random id to original filenames to ensure uniqueness for the tmp Gentle singularity input files 
 		id = str(uuid.uuid4())
-		tmpAudioName = os.path.basename(speech_audio) + "_" + id
-		tmpTranscriptName = os.path.basename(amp_transcript_unaligned) + "_" + id
-		tmpOutputName = os.path.basename(gentle_transcript) + "_" + id
+		tmp_speech_audio_name = "gentle-" + id + "-" + os.path.basename(speech_audio)
+		tmp_amp_transcript_unaligned_name = "gentle-" + id + "-" + os.path.basename(amp_transcript_unaligned)
+		tmp_gentle_transcript_name = "gentle-" + id + "-" + os.path.basename(gentle_transcript)
 
-		# Define directory accessible to singularity container
+		# define directory accessible to singularity container
 		tmpdir = '/tmp'
 
-		# Create temp file paths
-		temp_audio_file = f"{tmpdir}/{tmpAudioName}.dat"
-		temp_transcript_file = f"{tmpdir}/{tmpTranscriptName}.dat"
-		temp_output_file = f"{tmpdir}/{tmpOutputName}.json"
+		# define tmp filepaths
+		tmp_speech_audio = f"{tmpdir}/{tmp_speech_audio_name}"
+		tmp_amp_transcript_unaligned = f"{tmpdir}/{tmp_amp_transcript_unaligned_name}"
+		tmp_gentle_transcript = f"{tmpdir}/{tmp_gentle_transcript_name}"
 
-		# Load the original transcript as input into gentle
-		with open(amp_transcript_unaligned, "r") as input_ts_file:
-			orig_transcript = json.load(input_ts_file)
+		# Load the amp_transcript_unaligned as input into gentle
+		with open(amp_transcript_unaligned, "r") as amp_transcript_unaligned_file:
+			amp_transcript_unaligned_json = json.load(amp_transcript_unaligned_file)
 
 			# Write the transcript to an input file into gentle
-			with open(temp_transcript_file, "w") as text_file:
-				text_file.write(orig_transcript["results"]["transcript"])
+			with open(tmp_amp_transcript_unaligned, "w") as tmp_amp_transcript_unaligned_file:
+				tmp_amp_transcript_unaligned_file.write(amp_transcript_unaligned_json["results"]["transcript"])
 
 			# Copy the audio file to a location accessible to the singularity container
-			shutil.copy(speech_audio, temp_audio_file)
+			shutil.copy(speech_audio, tmp_speech_audio)
 
 			# Run gentle
-			print(f"Running gentle... input audio: {temp_audio_file}, input transcript: {temp_transcript_file}, output json: {temp_output_file}")
-			r = subprocess.run(["singularity", "run", "/srv/amp/gentle-singularity/gentle-singularity.sif", temp_audio_file, temp_transcript_file, "-o", temp_output_file], stdout=subprocess.PIPE)
-			print("Finished running gentle")
+			print(f"Running Gentle... tmp_speech_audio: {tmp_speech_audio}, input tmp_amp_transcript_unaligned: {tmp_amp_transcript_unaligned}, tmp_gentle_transcript: {tmp_gentle_transcript}")
+			r = subprocess.run(["singularity", "run", "/srv/amp/gentle-singularity/gentle-singularity.sif", tmp_speech_audio, tmp_amp_transcript_unaligned, "-o", tmp_gentle_transcript], stdout=subprocess.PIPE)
+			print("Finished running Gentle")
 
 			# Copy the tmp Gentle output file to gentle_transcript
-			shutil.copy(temp_output_file, gentle_transcript)
+			shutil.copy(tmp_gentle_transcript, gentle_transcript)
 
 			print("Creating amp transcript aligned...")
-			gentle_to_amp_transcript(gentle_transcript, orig_transcript, amp_transcript_aligned)
+			gentle_transcript_to_amp_transcript(gentle_transcript, amp_transcript_unaligned_json, amp_transcript_aligned)
+			
+			print(f"Gentle return Code: {r.returncode}")
+			exit(r.returncode)
 	except Exception as e:
-		print("Exception")
-		print(e)
-		exception = True
-
-	if os.path.exists(temp_audio_file):
-	 	os.remove(temp_audio_file)
-
-	if os.path.exists(temp_transcript_file):
-		os.remove(temp_transcript_file)
-
-	if os.path.exists(temp_output_file):
-		os.remove(temp_output_file)
-	
-	if exception == True:
-		print("Existing due to exception")
+		print("Exception while running Gentle:")
+		traceback.print_exc()
 		exit(1)
 
-	print("Return Code: " + str(r.returncode))
-	exit(r.returncode)
+# 	if os.path.exists(tmp_speech_audio):
+# 	 	os.remove(tmp_speech_audio)
+# 
+# 	if os.path.exists(tmp_amp_transcript_unaligned):
+# 		os.remove(tmp_amp_transcript_unaligned)
+# 
+# 	if os.path.exists(tmp_gentle_transcript):
+# 		os.remove(tmp_gentle_transcript)
+# 	
+# 	if exception == True:
+# 		print("Existing due to exception")
+# 		exit(1)
+
 	
 
-def find_next_success(gentle_transcript, current_index):
-	for word_index in range(current_index, len(gentle_transcript["words"])):
-		word = gentle_output["words"][word_index]
+def find_next_success(gentle_transcript_json, current_index):
+	for word_index in range(current_index, len(gentle_transcript_json["words"])):
+		word = gentle_transcript_json["words"][word_index]
 		# Make sure we have all the data
 		if word["case"] == 'success':
 			print("Found success at " + str(word_index))
@@ -84,23 +84,23 @@ def find_next_success(gentle_transcript, current_index):
 	return None
 
 
-def gentle_to_amp_transcript(gentle_transcript, original_transcript, amp_transcript_output):
+def gentle_transcript_to_amp_transcript(gentle_transcript, amp_transcript_unaligned_json, amp_transcript_aligned):
 	# Create the amp transcript
-	output = dict()
-	with open(gentle_transcript, "r") as gentle_output_file:
-		gentle_output = json.load(gentle_output_file)
-		output["media"] = original_transcript["media"]
-		output["results"] = dict()
-		output["results"]["transcript"] = original_transcript["results"]["transcript"]
-		output["results"]["words"] = list()
+	amp_transcript_aligned_json = dict()
+	with open(gentle_transcript, "r") as gentle_transcript_file:
+		gentle_transcript_json = json.load(gentle_transcript_file)
+		amp_transcript_aligned_json["media"] = amp_transcript_unaligned_json["media"]
+		amp_transcript_aligned_json["results"] = dict()
+		amp_transcript_aligned_json["results"]["transcript"] = amp_transcript_unaligned_json["results"]["transcript"]
+		amp_transcript_aligned_json["results"]["words"] = list()
 		previous_end = 0
 		last_success_index = 0
-		if "words" in gentle_output.keys():
-			for word in gentle_output["words"]:
+		if "words" in gentle_transcript_json.keys():
+			for word in gentle_transcript_json["words"]:
 				# Make sure we have all the data
 				if word["case"] == 'success':
 					previous_end = word["end"]
-					output["results"]["words"].append(
+					amp_transcript_aligned_json["results"]["words"].append(
 							{
 								"type": "pronunciation", 
 								"start": word["start"], 
@@ -113,19 +113,19 @@ def gentle_to_amp_transcript(gentle_transcript, original_transcript, amp_transcr
 							}
 						)
 				else:
-					word_index = gentle_output["words"].index(word)
-					next_success_index = find_next_success(gentle_output, word_index)
+					word_index = gentle_transcript_json["words"].index(word)
+					next_success_index = find_next_success(gentle_transcript_json, word_index)
 					avg_time = 0
 					# If we found another success
 					if(next_success_index is not None and next_success_index > word_index):
 						# Average the times based on how many words in between
-						next_success_word = gentle_output["words"][next_success_index]
+						next_success_word = gentle_transcript_json["words"][next_success_index]
 						skips_ahead = (next_success_index - last_success_index)
 						avg_time = (next_success_word["start"] - previous_end)/skips_ahead
 						print("Averaging time from next success")
 					else:
-						duration = original_transcript["media"]["duration"]
-						skips_ahead = (len(gentle_output["words"]) - word_index) + 1
+						duration = amp_transcript_unaligned_json["media"]["duration"]
+						skips_ahead = (len(gentle_transcript_json["words"]) - word_index) + 1
 						avg_time = (duration - previous_end)/skips_ahead
 						print("Averaging time from end of file")
 
@@ -139,7 +139,7 @@ def gentle_to_amp_transcript(gentle_transcript, original_transcript, amp_transcr
 					print("Avg_time " + str(avg_time)  + " Skips ahead " + str(skips_ahead))
 
 					# Add the word to the results
-					output["results"]["words"].append(
+					amp_transcript_aligned_json["results"]["words"].append(
 						{
 							"type": "pronunciation", 
 							"start": time, 
@@ -151,9 +151,9 @@ def gentle_to_amp_transcript(gentle_transcript, original_transcript, amp_transcr
 							} 
 						}
 					)
-				last_success_index = gentle_output["words"].index(word)
+				last_success_index = gentle_transcript_json["words"].index(word)
 								
-		mgm_utils.write_json_file(output, amp_transcript_output)
+		mgm_utils.write_json_file(amp_transcript_aligned_json, amp_transcript_aligned)
 		
 		
 if __name__ == "__main__":
