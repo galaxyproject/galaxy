8c94430ae47f621ee024f6205e14afb9952e9c52
Dave Bouvier <dave@bx.psu.edu>
2021-07-19 12:05:59 -0400

    Revert to functional code.

--- client/src/components/admin/SanitizeAllow.vue
+++ client/src/components/admin/SanitizeAllow.vue
@@ -58,7 +58,6 @@ export default {
                 this.blockList = response.data.data.sanitize;
                 this.message = response.data.message;
                 this.status = response.data.status;
-                console.log(this.allowList);
             })
             .catch((error) => {
                 console.error(error);
