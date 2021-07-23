69d381ddc43be6e460de334350e387f36335ee9d
yingfeng-iu <yingfeng@iu.edu>
2021-07-23 17:02:23 -0400

    AMP-1333/1291: - remove commented code - fix typos

--- tools/amp_schema/video_ocr.py
+++ tools/amp_schema/video_ocr.py
@@ -127,15 +127,5 @@ class VideoOcrObjectVertices:
         return cls(**json_data)
     
      
-# if __name__ == "__main__":
-#     import sys
-#  
-#     (amp_vocr, amp_vocr_csv) = sys.argv[1:3]
-# 
-#     # Open the file and create the vocr object
-#     with open(amp_vocr, 'r') as file:
-#         vocr = VideoOcr.from_json(json.load(file))
-# 
-#     # Write the csv file
-#     vocr.toCsv(amp_vocr_csv)
+
      
