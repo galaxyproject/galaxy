55ba08e9e55ffd483c0c33a8ac3f8b99c9f332c3
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2021-07-24 18:50:38 +0530

    Refactor k8s conditional
    Co-authored-by: Marius van den Beek <m.vandenbeek@gmail.com>

--- lib/galaxy/jobs/runners/util/pykube_util.py
+++ lib/galaxy/jobs/runners/util/pykube_util.py
@@ -89,7 +89,7 @@ def find_pod_object_by_name(pykube_api, job_name, namespace=None):
 
 
 def is_pod_unschedulable(pykube_api, pod, namespace=None):
-    is_unschedulable = bool(len([c for c in pod.obj['status'].get('conditions', []) if c.get("reason") == "Unschedulable"]))
+    is_unschedulable = any(c.get("reason") == "Unschedulable" for c in pod.obj['status'].get('conditions', []))
     if pod.obj['status'].get('phase') == "Pending" and is_unschedulable:
         return True
 
