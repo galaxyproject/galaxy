7dd797d8ce86b8fd3d2ee2787d235146a6ff40cf
guerler <aysam.guerler@gmail.com>
2021-07-27 18:02:05 -0400

    Remove legacy dataset error backbone module, replace with Vue component

--- client/src/components/DatasetInformation/DatasetError.vue
+++ client/src/components/DatasetInformation/DatasetError.vue
@@ -1,75 +1,98 @@
 <template>
-    <DatasetProvider :id="datasetId" v-slot="{ item: dataset, loading }">
-        <div v-if="!loading">
-            <JobDetailsProvider :jobid="dataset.creating_job" v-slot="{ result, loading }">
-                <div v-if="!loading">
-                    <div class="page-container edit-attr">
-                        <div class="response-message"></div>
-                    </div>
-                    <h2>Dataset Error Report</h2>
-                    <p>
-                        An error occurred while running the tool <b>{{ result.tool_id }}</b
-                        >.
-                    </p>
-                    <div v-if="result.tool_stderr || result.job_stderr || result.job_messages">
-                        <h3>Details</h3>
-                    </div>
-                    <div v-if="result.job_messages">
-                        <p>Execution resulted in the following messages:</p>
-                        <div v-for="job_message in result.job_messages">
-                            <pre class="rounded code">{{ job_message["desc"] }}</pre>
+    <CurrentUser v-slot="{ user }">
+        <DatasetProvider :id="datasetId" v-slot="{ item: dataset, loading }">
+            <div v-if="!loading">
+                <JobDetailsProvider :jobid="dataset.creating_job" v-slot="{ result, loading }">
+                    <div v-if="!loading">
+                        <div class="page-container edit-attr">
+                            <div class="response-message"></div>
+                        </div>
+                        <h2>Dataset Error Report</h2>
+                        <p>
+                            An error occurred while running the tool <b>{{ result.tool_id }}</b
+                            >.
+                        </p>
+                        <div v-if="result.tool_stderr || result.job_stderr || result.job_messages">
+                            <h3>Details</h3>
+                        </div>
+                        <div v-if="result.job_messages">
+                            <p>Execution resulted in the following messages:</p>
+                            <div v-for="job_message in result.job_messages">
+                                <pre class="rounded code">{{ job_message["desc"] }}</pre>
+                            </div>
+                        </div>
+                        <div v-if="result.tool_stderr">
+                            <p>Tool generated the following standard error:</p>
+                            <pre class="rounded code">{{ result.tool_stderr }}</pre>
+                        </div>
+                        <div v-if="result.job_stderr">
+                            <p>Galaxy job runner generated the following standard error:</p>
+                            <pre class="rounded code">{{ result.job_stderr }}</pre>
                         </div>
                     </div>
-                    <div v-if="result.tool_stderr">
-                        <p>Tool generated the following standard error:</p>
-                        <pre class="rounded code">{{ result.tool_stderr }}</pre>
+                </JobDetailsProvider>
+                <JobProblemProvider :jobid="dataset.creating_job" v-slot="{ result, loading }">
+                    <div v-if="result && (result.has_duplicate_inputs || result.has_empty_inputs)">
+                        <h3 class="common_problems">Detected Common Potential Problems</h3>
+                        <p v-if="result.has_empty_inputs">
+                            The tool was executed with one or more empty input datasets. This frequently results in tool
+                            errors due to problematic input choices.
+                        </p>
+                        <p v-if="result && result.has_duplicate_inputs">
+                            The tool was executed with one or more duplicate input datasets. This frequently results in
+                            tool errors due to problematic input choices.
+                        </p>
                     </div>
-                    <div v-if="result.job_stderr">
-                        <p>Galaxy job runner generated the following standard error:</p>
-                        <pre class="rounded code">{{ result.job_stderr }}</pre>
-                    </div>
-                </div>
-            </JobDetailsProvider>
-            <JobProblemProvider :jobid="dataset.creating_job" v-slot="{ result, loading }">
-                <div v-if="result.has_duplicate_inputs || result.has_empty_inputs">
-                    <h3 class="common_problems">Detected Common Potential Problems</h3>
-                    <p v-if="result.has_empty_inputs">
-                        The tool was executed with one or more empty input datasets. This frequently
-                        results in tool errors due to problematic input choices.
-                    <p>
-                    <p v-if="result.has_duplicate_inputs">
-                        The tool was executed with one or more duplicate input datasets. This frequently
-                        results in tool errors due to problematic input choices.
-                    <p>
-                </div>
-            </JobProblemProvider>
-            <h3>Troubleshooting</h3>
-            <p>
-                There are a number of helpful resources to self diagnose and correct problems.
-                <br />
-                Start here:
-                <b>
-                    <a href="https://galaxyproject.org/support/tool-error/" target="_blank">
-                        My job ended with an error. What can I do?
-                    </a>
-                </b>
-            </p>
-            <h3>Issue Report</h3>
-        </div>
-    </DatasetProvider>
+                </JobProblemProvider>
+                <h3>Troubleshooting</h3>
+                <p>
+                    There are a number of helpful resources to self diagnose and correct problems.
+                    <br />
+                    Start here:
+                    <b>
+                        <a href="https://galaxyproject.org/support/tool-error/" target="_blank">
+                            My job ended with an error. What can I do?
+                        </a>
+                    </b>
+                </p>
+                <h3 class="mb-2">Issue Report</h3>
+                <b-alert v-if="errorMessage" variant="danger" show>{{ errorMessage }}</b-alert>
+                <b-alert v-for="resultMessage in resultMessages" :variant="resultMessage[1]" show>{{
+                    resultMessage[0]
+                }}</b-alert>
+                <FormElement v-if="!user.email" id="email" v-model="email" title="Please provide your email:" />
+                <FormElement
+                    id="message"
+                    v-model="message"
+                    :area="true"
+                    title="Please provide detailed information on the activities leading to this issue:"
+                />
+                <b-button variant="primary" @click="submit(dataset, user.email)" class="mt-2">
+                    <font-awesome-icon icon="bug" class="mr-1" />Report
+                </b-button>
+            </div>
+        </DatasetProvider>
+    </CurrentUser>
 </template>
 
 <script>
+import CurrentUser from "components/providers/CurrentUser";
+import FormElement from "components/Form/FormElement";
 import { DatasetProvider } from "components/WorkflowInvocationState/providers";
 import { JobDetailsProvider, JobProblemProvider } from "components/providers/JobProvider";
-import FormDisplay from "components/Form/FormDisplay";
-import FormCard from "components/Form/FormCard";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBug } from "@fortawesome/free-solid-svg-icons";
+import { sendErrorReport } from "./services";
+
+library.add(faBug);
 
 export default {
     components: {
+        CurrentUser,
         DatasetProvider,
-        FormDisplay,
-        FormCard,
+        FontAwesomeIcon,
+        FormElement,
         JobDetailsProvider,
         JobProblemProvider,
     },
@@ -80,15 +103,26 @@ export default {
         },
     },
     data() {
-        return {};
-    },
-    computed: {
-        inputs() {
-            return [];
-        },
+        return {
+            message: null,
+            email: null,
+            errorMessage: null,
+            resultMessages: [],
+        };
     },
     methods: {
-        onChange(data) {},
+        submit(dataset, userEmail) {
+            const email = this.email || userEmail;
+            const message = this.message;
+            sendErrorReport(dataset, message, email).then(
+                (resultMessages) => {
+                    this.resultMessages = resultMessages;
+                },
+                (errorMessage) => {
+                    this.errorMessage = errorMessage;
+                }
+            );
+        },
     },
 };
 </script>
