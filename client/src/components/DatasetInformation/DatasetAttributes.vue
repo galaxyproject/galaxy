1d0a7c6a1e672322458a26057892da4cb5fd7c94
guerler <aysam.guerler@gmail.com>
2021-08-04 15:51:59 -0400

    Add tabs, icons and titles to dataset attributes component

--- client/src/components/DatasetInformation/DatasetAttributes.vue
+++ client/src/components/DatasetInformation/DatasetAttributes.vue
@@ -1,36 +1,28 @@
 <template>
     <div>
-        <h4>Edit dataset attributes</h4>
+        <h4>Edit Dataset Attributes</h4>
         <b-alert v-if="messageText" :variant="messageVariant" show>
-            <h4 class="alert-heading">Failed to access Dataset details.</h4>
+            <h4 class="alert-heading">Failed to access dataset details.</h4>
             {{ messageText }}
         </b-alert>
         <DatasetProvider :id="datasetId" v-slot="{ item: dataset, loading: datasetLoading }">
             <div class="mt-3">
                 <b-tabs>
                     <b-tab>
-                        <template #title>
-                            <font-awesome-icon icon="bars" class="mr-1"/>Attributes
-                        </template>
+                        <template #title><font-awesome-icon icon="bars" class="mr-1" />Attributes</template>
                         <p>I'm the first tab</p>
                     </b-tab>
                     <b-tab>
-                        <template #title>
-                            <font-awesome-icon icon="fas-gear" class="mr-1"/>Convert
-                        </template>
+                        <template #title><font-awesome-icon icon="cog" class="mr-1" />Convert</template>
                         <p>I'm the second tab</p>
                     </b-tab>
                     <b-tab>
-                        <template #title>
-                            <font-awesome-icon icon="database" class="mr-1"/>Datatypes
-                        </template>
-                        <p>I'm the second tab</p>
+                        <template #title><font-awesome-icon icon="database" class="mr-1" />Datatypes</template>
+                        <p>I'm the third tab</p>
                     </b-tab>
                     <b-tab>
-                        <template #title>
-                            <font-awesome-icon icon="user" class="mr-1"/>Permissions
-                        </template>
-                        <p>I'm the second tab</p>
+                        <template #title><font-awesome-icon icon="user" class="mr-1" />Permissions</template>
+                        <p>I'm the fourth tab</p>
                     </b-tab>
                 </b-tabs>
             </div>
@@ -43,12 +35,12 @@ import FormElement from "components/Form/FormElement";
 import { DatasetProvider } from "components/WorkflowInvocationState/providers";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBars, faDatabase, faGear, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faDatabase, faCog, faUser } from "@fortawesome/free-solid-svg-icons";
 import { sendErrorReport } from "./services";
 
 library.add(faBars);
 library.add(faDatabase);
-library.add(faGear);
+library.add(faCog);
 library.add(faUser);
 
 export default {
@@ -68,7 +60,6 @@ export default {
             email: null,
             messageText: null,
             messageVariant: null,
-            resultMessages: [],
         };
     },
     methods: {
