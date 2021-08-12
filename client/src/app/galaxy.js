7742e76972e547a7f3cec4940dbb4b01c0bbf774
guerler <aysam.guerler@gmail.com>
2021-08-12 12:49:25 -0400

    Add upload dialog to global Galaxy object

--- client/src/app/galaxy.js
+++ client/src/app/galaxy.js
@@ -8,6 +8,7 @@ import addLogging from "utils/add-logging";
 import localize from "utils/localization";
 import { getGalaxyInstance } from "app";
 import { create, dialog } from "utils/data";
+import { openGlobalUploadModal } from "components/upload/mount";
 
 // ============================================================================
 /** Base galaxy client-side application.
@@ -291,6 +292,11 @@ GalaxyApp.prototype.debuggingNamespaces = function _debuggingNamespaces(namespac
     } catch (storageErr) {
         console.log(localize("localStorage not available for debug namespace retrieval"));
     }
+};
+
+/** Open Upload Dialog */
+GalaxyApp.prototype.uploadDialog = function uploadDialog() {
+    openGlobalUploadModal();
 };
 
 /** string rep */
