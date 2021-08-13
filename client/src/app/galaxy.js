fe8366e6cbf36083b86556e6342effdf76bf9d39
Dannon Baker <dannon.baker@gmail.com>
2021-08-13 11:05:33 -0400

    Fix component case-sensitivity; import from primary interface instead of mount

--- client/src/app/galaxy.js
+++ client/src/app/galaxy.js
@@ -8,7 +8,7 @@ import addLogging from "utils/add-logging";
 import localize from "utils/localization";
 import { getGalaxyInstance } from "app";
 import { create, dialog } from "utils/data";
-import { openGlobalUploadModal } from "components/upload/mount";
+import { openGlobalUploadModal } from "components/Upload";
 
 // ============================================================================
 /** Base galaxy client-side application.
