9996a5683176596c6eac6e1b56f8955730ad4109
wei <shuangjian_wei@163.com>
2021-08-18 11:32:58 +0800

    add a title in joint output files

--- tools/myTools/joint.py
+++ tools/myTools/joint.py
@@ -1,40 +1,47 @@
 # 文件拼接
 # 2021.8.4
-# 
+#
 import sys
+
 
 def stop_err(msg):
     sys.stderr.write(msg)
     sys.exit()
 
+
 def __main__():
     try:
-        infile1 = open(sys.argv[1])
-        infile2 = open(sys.argv[2])
-        outfile = open(sys.argv[3],'w')
+        in_path1 = sys.argv[1]
+        in_path2 = sys.argv[2]
+        out_path = sys.argv[3]
         has_title1 = sys.argv[4]
         has_title2 = sys.argv[5]
     except Exception:
         stop_err("无法打开或者创建文件失败！")
-    if(has_title1=='t'):
-        title1 = infile1.readline()
-    if(has_title2=='f'):
-        title2 = infile2.readline()#去标题
 
-    # 不留标题
-    # title_items = title1.strip().split()
-    # for item in title_items:
-    #     outfile.write("%s\t"%item)
-    # outfile.write('\n')
+    in_f1 = open(in_path1, 'r')
+    in_f2 = open(in_path2, 'r')
+    out_f = open(out_path, 'w')
 
-    for line in infile1:
-        print(line,file=outfile)
+    if(has_title1 == 't'):
+        title_line1 = in_f1.readline()
+    if(has_title2 == 'f'):
+        title_line2 = in_f2.readline()  # 去标题
+
+    title_itms = title_line1.strip().split()
+    for itm in title_itms:
+        out_f.write("%s\t" % itm)
+    out_f.write('\n')
+
+    for line in in_f1:
+        print(line, file=out_f)
         pass
-    for line in infile2:
-        print(line,file=outfile)
+    for line in in_f2:
+        print(line, file=out_f)
         pass
-
-
+    in_f1.close()
+    in_f2.close()
+    out_f.close()
 
 
 if __name__ == "__main__":
