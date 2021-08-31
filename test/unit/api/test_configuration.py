1de73222e2393fc8a027379c124dd1537ca51ee5
davelopez <46503462+davelopez@users.noreply.github.com>
2021-08-31 19:01:48 +0200

    Adapt test_parse_serialization_params to expect a model

--- test/unit/api/test_configuration.py
+++ test/unit/api/test_configuration.py
@@ -5,15 +5,15 @@ def test_parse_serialization_params():
     view, default_view = 'a', 'b'
     keys = 'foo'
     serialized = parse_serialization_params(view, keys, default_view)
-    assert serialized['view'] == view
-    assert serialized['default_view'] == default_view
-    assert serialized['keys'] == [keys]
+    assert serialized.view == view
+    assert serialized.default_view == default_view
+    assert serialized.keys == [keys]
 
     keys = 'foo,bar,baz'
     serialized = parse_serialization_params(view, keys, default_view)
-    assert serialized['keys'] == ['foo', 'bar', 'baz']
+    assert serialized.keys == ['foo', 'bar', 'baz']
 
     serialized = parse_serialization_params(default_view=default_view)
-    assert serialized['view'] is None
-    assert serialized['keys'] is None
-    assert serialized['default_view'] == default_view
+    assert serialized.view is None
+    assert serialized.keys is None
+    assert serialized.default_view == default_view
