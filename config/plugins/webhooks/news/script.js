33c64034dc38438402c1b3508c8e06fadbab92e9
Dannon Baker <dannon.baker@gmail.com>
2021-09-02 08:19:43 -0400

    Cleanup

--- config/plugins/webhooks/news/script.js
+++ config/plugins/webhooks/news/script.js
@@ -1,44 +1,41 @@
 function removeNewsOverlay() {
-  document.getElementById("news-container").style.visibility = "hidden";
+    document.getElementById("news-container").style.visibility = "hidden";
 }
 
 function showNewsOverlay() {
-  document.getElementById("news-container").style.visibility = "visible";
-  newsSeen();
+    document.getElementById("news-container").style.visibility = "visible";
+    newsSeen();
 }
 
 function newsSeen() {
-  // When it's seen, remove fa and far, add fas.
-  const newsIconSpan = document.getElementById("news").querySelector(".fa-bell")
-  newsIconSpan.classList.remove("fa");
-  newsIconSpan.classList.add("far");
-  window.localStorage.setItem(
-    "galaxy-news-seen-release",
-    Galaxy.config.version_major
-  );
+    // When it's seen, remove fa and far, add fas.
+    const newsIconSpan = document.getElementById("news").querySelector(".fa-bell");
+    newsIconSpan.classList.remove("fa");
+    newsIconSpan.classList.add("far");
+    window.localStorage.setItem("galaxy-news-seen-release", Galaxy.config.version_major);
 }
 
 function newsUnseen() {
-  // When there is news, set far instead of fa
-  const newsIconSpan = document.getElementById("news").querySelector(".fa-bell")
-  newsIconSpan.classList.remove("far");
-  newsIconSpan.classList.add("fa");
+    // When there is news, set far instead of fa
+    const newsIconSpan = document.getElementById("news").querySelector(".fa-bell");
+    newsIconSpan.classList.remove("far");
+    newsIconSpan.classList.add("fa");
 }
 
 function addNewsIframe() {
-  let currentGalaxyVersion = Galaxy.config.version_major;
-  let releaseNotes = `https://docs.galaxyproject.org/en/master/releases/${currentGalaxyVersion}_announce_user.html`;
-  let lastSeenVersion = window.localStorage.getItem("galaxy-news-seen-release");
-  // Check that they've seen the current version's release notes.
-  if (lastSeenVersion != currentGalaxyVersion) {
-    newsUnseen();
-  } else {
-    newsSeen();
-  }
+    const currentGalaxyVersion = Galaxy.config.version_major;
+    const releaseNotes = `https://docs.galaxyproject.org/en/master/releases/${currentGalaxyVersion}_announce_user.html`;
+    const lastSeenVersion = window.localStorage.getItem("galaxy-news-seen-release");
+    // Check that they've seen the current version's release notes.
+    if (lastSeenVersion != currentGalaxyVersion) {
+        newsUnseen();
+    } else {
+        newsSeen();
+    }
 
-  document.querySelector("body.full-content").insertAdjacentHTML(
-    "afterbegin",
-    `
+    document.querySelector("body.full-content").insertAdjacentHTML(
+        "afterbegin",
+        `
             <div id="news-container" style="visibility: hidden">
                 <div id="news-screen-overlay"></div>
                 <div id="news-screen">
@@ -47,12 +44,12 @@ function addNewsIframe() {
                     </div>
                 </div>
             </div>`
-  );
+    );
 
-  // Clicking outside of GTN closes it
-  document.getElementById("news-screen").addEventListener("click", () => {
-    removeNewsOverlay();
-  });
+    // Clicking outside of GTN closes it
+    document.getElementById("news-screen").addEventListener("click", () => {
+        removeNewsOverlay();
+    });
 }
 
 /* The masthead icon may not exist yet when this webhook executes; we need this to wait for that to happen.
@@ -60,45 +57,45 @@ function addNewsIframe() {
  * https://gist.github.com/jwilson8767/db379026efcbd932f64382db4b02853e
  */
 function elementReadyNews(selector) {
-  return new Promise((resolve, reject) => {
-    let el = document.querySelector(selector);
-    if (el) {
-      resolve(el);
-    }
-    new MutationObserver((mutationRecords, observer) => {
-      // Query for elements matching the specified selector
-      Array.from(document.querySelectorAll(selector)).forEach(element => {
-        resolve(element);
-        //Once we have resolved we don't need the observer anymore.
-        observer.disconnect();
-      });
-    }).observe(document.documentElement, {
-      childList: true,
-      subtree: true
+    return new Promise((resolve, reject) => {
+        const el = document.querySelector(selector);
+        if (el) {
+            resolve(el);
+        }
+        new MutationObserver((mutationRecords, observer) => {
+            // Query for elements matching the specified selector
+            Array.from(document.querySelectorAll(selector)).forEach((element) => {
+                resolve(element);
+                //Once we have resolved we don't need the observer anymore.
+                observer.disconnect();
+            });
+        }).observe(document.documentElement, {
+            childList: true,
+            subtree: true,
+        });
     });
-  });
 }
 
-elementReadyNews("#news a").then(el => {
-  // External stuff may also have attached a click handler here (vue-based masthead)
-  // replace with a clean copy of the node to remove all that cruft.
-  clean = el.cloneNode(true);
-  el.parentNode.replaceChild(clean, el);
+elementReadyNews("#news a").then((el) => {
+    // External stuff may also have attached a click handler here (vue-based masthead)
+    // replace with a clean copy of the node to remove all that cruft.
+    clean = el.cloneNode(true);
+    el.parentNode.replaceChild(clean, el);
 
-  // This gets added by default.
-  addNewsIframe();
+    // This gets added by default.
+    addNewsIframe();
 
-  clean.addEventListener("click", e => {
-    e.preventDefault();
-    e.stopPropagation();
-    showNewsOverlay();
-  });
+    clean.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        showNewsOverlay();
+    });
 });
 
 // Remove the overlay on escape button click
-document.addEventListener("keydown", e => {
-  // Check for escape button - "27"
-  if (e.which === 27 || e.keyCode === 27) {
-    removeNewsOverlay();
-  }
+document.addEventListener("keydown", (e) => {
+    // Check for escape button - "27"
+    if (e.which === 27 || e.keyCode === 27) {
+        removeNewsOverlay();
+    }
 });
