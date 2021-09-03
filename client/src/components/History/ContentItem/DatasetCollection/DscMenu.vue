eb115aec7369dddf0d1b3e959210b3fb21c07518
mvdbeek <m.vandenbeek@gmail.com>
2021-09-03 13:11:21 +0200

    Drop unnecessary blank lines

--- client/src/components/History/ContentItem/DatasetCollection/DscMenu.vue
+++ client/src/components/History/ContentItem/DatasetCollection/DscMenu.vue
@@ -17,11 +17,9 @@
             <b-dropdown-item @click.stop="$emit('delete')">
                 <span v-localize>Delete Collection Only </span>
             </b-dropdown-item>
-
             <b-dropdown-item @click.stop="$emit('delete', { recursive: true })">
                 <span v-localize>Delete Contained Datasets</span>
             </b-dropdown-item>
-
             <b-dropdown-item @click.stop="$emit('delete', { recursive: true, purge: true })">
                 <span v-localize>Purge Contained Datasets</span>
             </b-dropdown-item>
