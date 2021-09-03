41068ee6964eac2dc9ac1fbbacd45048c07c6362
John Chilton <jmchilton@gmail.com>
2021-09-03 11:28:50 -0400

    revert broken(?) changes to selenium abstraction

--- lib/galaxy/selenium/navigates_galaxy.py
+++ lib/galaxy/selenium/navigates_galaxy.py
@@ -1461,17 +1461,15 @@ class NavigatesGalaxy(HasDriver):
 
     def history_panel_click_item_title(self, hid, **kwds):
         item_component = self.history_panel_item_component(hid=hid)
+        details_component = item_component.details
+        details_displayed = details_component.is_displayed
         item_component.title.wait_for_and_click()
+
         if kwds.get("wait", False):
-            if self.is_beta_history():
-                item_component.details.wait_for_present()
+            if details_displayed:
+                details_component.wait_for_absent_or_hidden()
             else:
-                details_component = item_component.details
-                details_displayed = details_component.is_displayed
-                if details_displayed:
-                    details_component.wait_for_absent_or_hidden()
-                else:
-                    details_component.wait_for_visible()
+                details_component.wait_for_visible()
         return item_component
 
     def history_panel_ensure_showing_item_details(self, hid):
