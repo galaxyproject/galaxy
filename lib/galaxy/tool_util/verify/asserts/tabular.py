8c3052332f83f8231ad0a1b852c4897edc38c882
Matthias Bernt <m.bernt@ufz.de>
2021-09-22 15:03:48 +0200

    allow to specify comment char for has_n_columns

--- lib/galaxy/tool_util/verify/asserts/tabular.py
+++ lib/galaxy/tool_util/verify/asserts/tabular.py
@@ -1,23 +1,26 @@
 import re
 
 
-def get_first_line(output):
+def get_first_line(output, comment):
     """
     get the first non-comment and non-empty line
     """
-    match = re.search("^([^#\n].*)$", output, flags=re.MULTILINE)
+    if comment != "":
+        match = re.search(f"^([^{comment}].*)$", output, flags=re.MULTILINE)
+    else:
+        match = re.search("^(.+)$", output, flags=re.MULTILINE)
     if match is None:
         return None
     else:
         return match.group(1)
 
 
-def assert_has_n_columns(output, n, sep='\t'):
+def assert_has_n_columns(output, n, sep='\t', comment=""):
     """ Asserts the tabular output contains n columns. The optional
     sep argument specifies the column seperator used to determine the
     number of columns."""
     n = int(n)
-    first_line = get_first_line(output)
+    first_line = get_first_line(output, comment)
     assert first_line is not None, "Was expecting output with %d columns, but output was empty." % n
     n_columns = len(first_line.split(sep))
     assert n_columns == n, f"Expected {n} columns in output, found {n_columns} columns"
--- test/unit/tool_util/verify/test_asserts.py
+++ test/unit/tool_util/verify/test_asserts.py
@@ -4,8 +4,6 @@ from galaxy.tool_util.parser.xml import __parse_assert_list_from_elem
 from galaxy.tool_util.verify import asserts
 from galaxy.util import etree
 
-# def test_foo()
-
 TABULAR_ASSERTION = """
     <assert_contents>
         <has_n_columns n="3"/>
@@ -16,20 +14,26 @@ TABULAR_CSV_ASSERTION = """
         <has_n_columns n="3" sep=","/>
     </assert_contents>
 """
+TABULAR_ASSERTION_COMMENT = """
+    <assert_contents>
+        <has_n_columns n="3" comment="#$"/>
+    </assert_contents>
+"""
 
-TABULAR_DATA_POS = """
-# comment line
+TABULAR_DATA_POS = """1\t2\t3
+"""
+
+TABULAR_DATA_NEG = """1\t2\t3\t4
+"""
+
+TABULAR_CSV_DATA = """1,2
+"""
+
+TABULAR_DATA_COMMENT = """# comment
+$ more comment (using a char with meaning wrt regexp)
 1\t2\t3
 """
 
-TABULAR_DATA_NEG = """
-# comment line
-1\t2\t3\t4
-"""
-
-TABULAR_CSV_DATA = """
-1,2
-"""
 
 TESTS = [
     # test successful assertion
@@ -47,12 +51,18 @@ TESTS = [
         TABULAR_CSV_ASSERTION, TABULAR_CSV_DATA,
         lambda x: 'Expected 3 columns in output, found 2 columns' in x
     ),
+    # test tabular data with comments
+    (
+        TABULAR_ASSERTION_COMMENT, TABULAR_DATA_COMMENT,
+        lambda x: len(x) == 0
+    ),
 ]
 
 TEST_IDS = [
     'tabular assertion success',
     'tabular assertion failure',
     'tabular csv assertion',
+    'tabular with comments assertion',
 ]
 
 
