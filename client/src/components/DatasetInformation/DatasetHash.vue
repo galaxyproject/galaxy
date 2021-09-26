ada2784f740e022386ee5f5baeb401838d67e6e5
John Chilton <jmchilton@gmail.com>
2021-09-26 12:02:06 -0400

    Integrated hashses and sources into various APIs for UI display.

---
+++ client/src/components/DatasetInformation/DatasetHash.vue
@@ -0,0 +1,16 @@
+<template>
+    <li class="dataset-hash">
+        <i>{{ hash.hash_value }}</i>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        hash: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
