481449099b4d595ce7779d0663b2de833b4e9d3a
John Chilton <jmchilton@gmail.com>
2021-09-28 08:53:49 -0400

    Fix a small bug in Error summary Vue template.

--- client/src/components/History/ContentItem/Dataset/Summary/Error.vue
+++ client/src/components/History/ContentItem/Dataset/Summary/Error.vue
@@ -3,7 +3,7 @@
         <div v-if="!props.dataset.purged && props.dataset.misc_info">
             <span>{{ props.dataset.misc_info }}</span>
         </div>
-        <span class="help-text" v-localize>An error occurred with this props.dataset</span>
+        <span class="help-text" v-localize>An error occurred with this dataset</span>
         <div v-if="props.dataset.misc_info" class="job-error-text">
             <span>{{ props.dataset.misc_info }}</span>
         </div>
