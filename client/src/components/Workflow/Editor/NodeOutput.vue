a33de24e57b03682926aae3f2bdd2fe8e09bd85a
guerler <aysam.guerler@gmail.com>
2021-09-29 11:58:10 -0400

    Refactor terminal handling flow

--- client/src/components/Workflow/Editor/NodeOutput.vue
+++ client/src/components/Workflow/Editor/NodeOutput.vue
@@ -54,6 +54,9 @@ export default {
             const activeLabel = this.output.activeLabel || this.output.label || this.output.name;
             return `${activeLabel} (${extensions})`;
         },
+        node() {
+            return this.getNode();
+        },
         activeClass() {
             return this.output.activeOutput && "mark-terminal-active";
         },
@@ -78,18 +81,17 @@ export default {
             } else {
                 // create new terminal, connect like old terminal, destroy old terminal
                 this.$emit("onRemove", this.output);
-                const newTerminal = this.createTerminal(newOutput);
-                newTerminal.connectors = this.terminal.connectors.map((c) => {
-                    return new Connector(this.getManager(), newTerminal, c.inputHandle);
+                this.createTerminal(newOutput);
+                this.terminal.connectors = oldTerminal.connectors.map((c) => {
+                    return new Connector(this.getManager(), this.terminal, c.inputHandle);
                 });
-                newTerminal.destroyInvalidConnections();
-                this.terminal = newTerminal;
+                this.terminal.destroyInvalidConnections();
                 oldTerminal.destroy();
             }
         },
     },
     mounted() {
-        this.terminal = this.createTerminal(this.output);
+        this.createTerminal(this.output);
     },
     methods: {
         terminalClassForOutput(output) {
@@ -102,7 +104,6 @@ export default {
             return terminalClass;
         },
         createTerminal(output) {
-            let terminal;
             const terminalClass = this.terminalClassForOutput(output);
             const parameters = {
                 node: this.getNode(),
@@ -113,33 +114,35 @@ export default {
             if (output.collection) {
                 const collection_type = output.collection_type;
                 const collection_type_source = output.collection_type_source;
-                terminal = new terminalClass({
+                this.terminal = new terminalClass({
                     ...parameters,
                     collection_type: collection_type,
                     collection_type_source: collection_type_source,
                     datatypes: output.extensions,
                 });
             } else if (output.parameter) {
-                terminal = new terminalClass({
+                this.terminal = new terminalClass({
                     ...parameters,
                     type: output.type,
                 });
             } else {
-                terminal = new terminalClass({
+                this.terminal = new terminalClass({
                     ...parameters,
                     datatypes: output.extensions,
                 });
             }
-            terminal.on("change", this.onChange.bind(this));
             new OutputDragging(this.getManager(), {
                 el: this.$refs.terminal,
-                terminal: terminal,
+                terminal: this.terminal,
             });
-            this.$emit("onAdd", this.output, terminal);
-            return terminal;
+            this.terminal.on("change", this.onChange.bind(this));
+            if (this.node.mapOver) {
+                this.terminal.setMapOver(this.node.mapOver);
+            }
+            this.$emit("onAdd", this.output, this.terminal);
         },
         onChange() {
-            this.isMultiple = this.terminal.mapOver && this.terminal.mapOver.isCollection;
+            this.isMultiple = this.terminal.isMappedOver();
             this.$emit("onChange");
         },
         onToggle() {
