852b328c2d6a03b832f10b86598cd0f0a5479ba7
Oleg Zharkov <oleg.zharkov22@gmail.com>
2021-10-12 16:17:22 +0200

    fix linting

--- client/src/components/FilesDialog/FilesDialog.vue
+++ client/src/components/FilesDialog/FilesDialog.vue
@@ -56,7 +56,6 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCaretLeft } from "@fortawesome/free-solid-svg-icons";
 
-
 library.add(faCaretLeft);
 export default {
     mixins: [SelectionDialogMixin],
