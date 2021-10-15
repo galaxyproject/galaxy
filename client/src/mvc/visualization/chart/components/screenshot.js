e620959e43a31fd1f966978e5e78a6642b3ed6b7
Dannon Baker <dannon.baker@gmail.com>
2021-10-15 15:10:48 -0400

    Use libraries for more robust support of exporting images from charts

--- client/src/mvc/visualization/chart/components/screenshot.js
+++ client/src/mvc/visualization/chart/components/screenshot.js
@@ -1,107 +1,21 @@
 /** This class enables users to export/download a chart as PNG, SVG or PDF. */
-/** PNG export */
 
 import $ from "jquery";
 
+import domtoimage from "dom-to-image";
+import { saveAs } from "file-saver";
+
 function createPNG(options) {
-    if (options.$el.find("svg").length > 0) {
-        _svg2png(options);
-    } else {
-        _canvas2png(options);
-    }
-}
-
-function _downloadData(imageData, filename) {
-    const link = document.createElement("a");
-    link.download = filename;
-    link.href = imageData;
-    link.click();
-}
-function _downloadPNGData(imageData, filename) {
-    _downloadData(imageData.replace("image/png", "image/octet-stream"), `${filename || "GalaxyImage"}.png`);
-}
-
-function _toImage($el, x_offset, y_offset, newContext) {
-    var tagname = $el.prop("tagName").toLowerCase();
-    var p = $el.position();
-    var left =
-        x_offset +
-        p.left +
-        parseInt($el.css("marginLeft"), 10) +
-        parseInt($el.css("borderLeftWidth"), 10) +
-        parseInt($el.css("paddingLeft"), 10);
-    var top =
-        y_offset +
-        p.top +
-        parseInt($el.css("marginTop"), 10) +
-        parseInt($el.css("borderTopWidth"), 10) +
-        parseInt($el.css("paddingTop"), 10);
-    if (tagname == "div" || tagname == "span") {
-        $el.children().each(function () {
-            _toImage($(this), left, top);
-        });
-    } else if (tagname == "canvas") {
-        newContext.drawImage($el[0], left, top);
-    }
-}
-
-/** Convert canvas to png */
-function _canvas2png(options) {
-    const $canvas = options.$el.find(".charts-viewer-canvas");
-    try {
-        if ($canvas.width() !== 0 && $canvas.height() !== 0) {
-            var newCanvas = document.createElement("canvas");
-            newCanvas.width = $canvas.outerWidth(true);
-            newCanvas.height = $canvas.outerHeight(true);
-            var newContext = newCanvas.getContext("2d");
-            newContext.save();
-            newContext.fillStyle = "rgb(255,255,255)";
-            newContext.fillRect(0, 0, newCanvas.width, newCanvas.height);
-            newContext.restore();
-            newContext.translate(0, 0);
-            newContext.textAlign = "left";
-            newContext.textBaseline = "top";
-            $canvas.children().each(function () {
-                _toImage($(this), 0, 0, newContext);
-            });
-            const imgData = newCanvas.toDataURL("image/png");
-            if (imgData) {
-                _downloadPNGData(imgData, options.title);
-            }
-        }
-    } catch (err) {
-        console.debug("FAILED - screenshot::_canvas2png() - " + err);
-        if (options.error) {
-            options.error("Please reduce your visualization to a single panel and try again.");
-        }
-    }
-}
-
-/** Convert svg to png */
-function _svg2png(options) {
-    var scale = 5;
-    var xml = toXML(options);
-    var canvas = document.createElement("canvas");
-    var context = canvas.getContext("2d");
-    var source = new Image();
-    var $container = $('<div style="display:none;"/>').append($(canvas));
-    $("body").append($container);
-    canvas.width = xml.width * scale;
-    canvas.height = xml.height * scale;
-    source.src = "data:image/svg+xml;base64," + window.btoa(xml.string);
-    source.onload = function () {
-        context.drawImage(source, 0, 0, canvas.width, canvas.height);
-        const imageData = canvas.toDataURL("image/png");
-        _downloadPNGData(imageData, options.title);
-        $container.remove();
-    };
+    domtoimage.toBlob(options.$el[0]).then(function (blob) {
+        saveAs(blob, `${options.title || "my-chart"}.png`);
+    });
 }
 
 /** SVG export */
 function createSVG(options) {
-    const imageData = `data:none/none;base64,${window.btoa(toXML(options).string)}`;
-    const filename = `${options.title || "GalaxyImage"}.svg`;
-    _downloadData(imageData, filename);
+    domtoimage.toSvg(options.$el[0]).then(function (dataUrl) {
+        saveAs(dataUrl, `${options.title || "my-chart"}.svg`);
+    });
 }
 
 /** PDF export */
@@ -164,7 +78,6 @@ function toXML(options) {
     var offsetX = 0;
     $svg.each(function () {
         var $svg = $(this).clone();
-        _inline($svg);
         var $g = $('<g transform="translate(' + offsetX + ', 0)">').attr("xmlns", "http://www.w3.org/2000/svg");
         $g.append($svg.find("g").first());
         $composite.append($g);
@@ -175,25 +88,6 @@ function toXML(options) {
         height: height,
         width: width,
     };
-}
-
-/** inlines CSS code */
-function _inline($target) {
-    for (var sheet_id in document.styleSheets) {
-        var sheet = document.styleSheets[sheet_id];
-        var rules = sheet.cssRules;
-        if (rules) {
-            for (var idx = 0, len = rules.length; idx < len; idx++) {
-                try {
-                    $target.find(rules[idx].selectorText).each(function (i, elem) {
-                        elem.style.cssText += rules[idx].style.cssText;
-                    });
-                } catch (err) {
-                    console.error(err);
-                }
-            }
-        }
-    }
 }
 
 export default {
