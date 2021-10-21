518cc517fc68dc73f9573cc932ea060436cac7ac
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2021-10-21 18:37:53 -0400

    Add better function+test for testing collection equality

--- test/unit/data/model/test_model_mapping.py
+++ test/unit/data/model/test_model_mapping.py
@@ -7503,6 +7503,27 @@ def are_same_entity_collections(collection1, collection2):
     return True
 
 
+def collection_consists_of_objects(collection, *objects):
+    """
+    Returns True iff list(collection) == list(objects), where object equality is determined
+    by primary key equality: object1.id == object2.id.
+    """
+    if len(collection) != len(objects):  # False if lengths are different
+        return False
+    if not collection:   # True if both are empty
+        return True
+
+    # Sort, then compare each member by its 'id' attribute, which must be its primary key.
+    collection.sort(key=lambda item: item.id)
+    objects = list(objects)
+    objects.sort(key=lambda item: item.id)
+
+    for item1, item2 in zip(collection, objects):
+        if item1.id is None or item2.id is None or item1.id != item2.id:
+            return False
+    return True
+
+
 def _run_average_rating_test(session, obj, user, obj_rating_association_factory):
     # obj has been expunged; to access its deferred properties,
     # it needs to be added back to the session.
