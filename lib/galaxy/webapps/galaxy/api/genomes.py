galaxyproject/galaxy

commit 47e37cddda77a053b9283574ae444cb54834fcff

Do not use @web decorators in api controllers
@web decorator will cause json content to be sent to the client as html,
including html-formatted errors, which is incorrect.

lib/galaxy/webapps/galaxy/api/genomes.py
@@ -1,4 +1,7 @@
-from galaxy import web
+from galaxy.web import (
+    expose_api_anonymous,
+    expose_api_raw_anonymous,
+)
 from galaxy.web.framework.helpers import is_true
 from galaxy.webapps.base.controller import BaseAPIController
 
@@ -15,15 +18,14 @@ class GenomesController(BaseAPIController):
     RESTful controller for interactions with genome data.
     """
 
-    @web.legacy_expose_api_anonymous
+    @expose_api_anonymous
     def index(self, trans, **kwd):
         """
         GET /api/genomes: returns a list of installed genomes
         """
-
         return self.app.genomes.get_dbkeys(trans, **kwd)
 
-    @web.json
+    @expose_api_anonymous
     def show(self, trans, id, num=None, chrom=None, low=None, high=None, **kwd):
         """
         GET /api/genomes/{id}
@@ -44,7 +46,7 @@ class GenomesController(BaseAPIController):
             rval = self.app.genomes.chroms(trans, dbkey=id, num=num, chrom=chrom, low=low)
         return rval
 
-    @web.legacy_expose_api_raw_anonymous
+    @expose_api_raw_anonymous
     def indexes(self, trans, id, **kwd):
         """
         GET /api/genomes/{id}/indexes?type={table name}
@@ -62,7 +64,7 @@ class GenomesController(BaseAPIController):
         if_open = open(index_file_name + index_extensions[index_type], mode='r')
         return if_open.read()
 
-    @web.legacy_expose_api_raw_anonymous
+    @expose_api_raw_anonymous
     def sequences(self, trans, id, num=None, chrom=None, low=None, high=None, **kwd):
         """
         GET /api/genomes/{id}/sequences

commit 4467a27fe8c67373fb59563f2d50ab6255cefafb

Allow sessionless access to genomes API
Broken in commit 47e37cddda77a053b9283574ae444cb54834fcff

Reported by @innovate-invent

lib/galaxy/webapps/galaxy/api/genomes.py
@@ -12,8 +12,8 @@ from fastapi.responses import Response
 from galaxy.managers.context import ProvidesUserContext
 from galaxy.managers.genomes import GenomesManager
 from galaxy.web import (
-    expose_api_anonymous,
-    expose_api_raw_anonymous,
+    expose_api_anonymous_and_sessionless,
+    expose_api_raw_anonymous_and_sessionless,
 )
 from galaxy.web.framework.helpers import is_true
 from . import (
@@ -163,7 +163,7 @@ class GenomesController(BaseGalaxyAPIController):
     """
     manager: GenomesManager = depends(GenomesManager)
 
-    @expose_api_anonymous
+    @expose_api_anonymous_and_sessionless
     def index(self, trans, **kwd):
         """
         GET /api/genomes: returns a list of installed genomes
@@ -171,7 +171,7 @@ class GenomesController(BaseGalaxyAPIController):
         chrom_info = kwd.get('chrom_info')
         return self.manager.get_dbkeys(trans.user, chrom_info)
 
-    @expose_api_anonymous
+    @expose_api_anonymous_and_sessionless
     def show(self, trans, id, num=None, chrom=None, low=None, high=None, **kwd):
         """
         GET /api/genomes/{id}
@@ -182,7 +182,7 @@ class GenomesController(BaseGalaxyAPIController):
         reference = is_true(kwd.get('reference', False))
         return self.manager.get_genome(trans, id, num, chrom, low, high, reference)
 
-    @expose_api_raw_anonymous
+    @expose_api_raw_anonymous_and_sessionless
     def indexes(self, trans, id, **kwd):
         """
         GET /api/genomes/{id}/indexes?type={table name}
@@ -194,7 +194,7 @@ class GenomesController(BaseGalaxyAPIController):
         index_type = kwd.get('type')
         return self.manager.get_indexes(id, index_type)
 
-    @expose_api_raw_anonymous
+    @expose_api_raw_anonymous_and_sessionless
     def sequences(self, trans, id, chrom=None, low=None, high=None, **kwd):
         """
         GET /api/genomes/{id}/sequences