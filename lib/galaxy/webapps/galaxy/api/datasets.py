galaxyproject/galaxy

commit c92fcb91d7f455d5c3d11febe3f5d028741e178f

Drop legacy_expose_api from api/datasets
We raise MessageException inside the security checks, and these are not
handled in legacy_expose_api.
Fixes
```
MessageException: You are not allowed to access this dataset
  File "galaxy/web/framework/decorators.py", line 184, in decorator
    rval = func(self, trans, *args, **kwargs)
  File "galaxy/webapps/galaxy/api/datasets.py", line 134, in show
    dataset = self.get_hda_or_ldda(trans, hda_ldda=hda_ldda, dataset_id=id)
  File "galaxy/webapps/base/controller.py", line 987, in get_hda_or_ldda
    return self.get_hda(trans, dataset_id, check_ownership=False, check_accessible=True)
  File "galaxy/webapps/base/controller.py", line 1023, in get_hda
    error("You are not allowed to access this dataset")
  File "galaxy/web/framework/decorators.py", line 32, in error
    raise MessageException(message, type='error')
```
(https://sentry.galaxyproject.org/organizations/galaxy/issues/6012/?project=3&query=is%3Aunresolved).

lib/galaxy/webapps/galaxy/api/datasets.py
@@ -124,7 +124,7 @@ class DatasetsController(BaseGalaxyAPIController, UsesVisualizationMixin):
         )
         return [self.serializer_by_type[content.history_content_type].serialize_to_view(content, user=trans.user, trans=trans, view=view) for content in contents]
 
-    @web.legacy_expose_api_anonymous
+    @web.expose_api_anonymous
     def show(self, trans, id, hda_ldda='hda', data_type=None, provider=None, **kwd):
         """
         GET /api/datasets/{encoded_dataset_id}
@@ -420,7 +420,7 @@ class DatasetsController(BaseGalaxyAPIController, UsesVisualizationMixin):
 
         return data
 
-    @web.legacy_expose_api_anonymous
+    @web.expose_api_anonymous
     def extra_files(self, trans, history_content_id, history_id, **kwd):
         """
         GET /api/histories/{encoded_history_id}/contents/{encoded_content_id}/extra_files
@@ -495,26 +495,18 @@ class DatasetsController(BaseGalaxyAPIController, UsesVisualizationMixin):
             "item_url": item_url,
         }
 
-    @web.legacy_expose_api_raw_anonymous
+    @web.expose_api_raw_anonymous
     def get_metadata_file(self, trans, history_content_id, history_id, metadata_file=None, **kwd):
         """
         GET /api/histories/{history_id}/contents/{history_content_id}/metadata_file
         """
         decoded_content_id = self.decode_id(history_content_id)
-        rval = ''
-        try:
-            hda = self.hda_manager.get_accessible(decoded_content_id, trans.user)
-            file_ext = hda.metadata.spec.get(metadata_file).get("file_ext", metadata_file)
-            fname = ''.join(c in util.FILENAME_VALID_CHARS and c or '_' for c in hda.name)[0:150]
-            trans.response.headers["Content-Type"] = "application/octet-stream"
-            trans.response.headers["Content-Disposition"] = f'attachment; filename="Galaxy{hda.hid}-[{fname}].{file_ext}"'
-            return open(hda.metadata.get(metadata_file).file_name, 'rb')
-        except Exception as e:
-            log.exception("Error getting metadata_file (%s) for dataset (%s) from history (%s)",
-                          metadata_file, history_content_id, history_id)
-            trans.response.status = 500
-            rval = f"Could not get metadata for dataset: {util.unicodify(e)}"
-        return rval
+        hda = self.hda_manager.get_accessible(decoded_content_id, trans.user)
+        file_ext = hda.metadata.spec.get(metadata_file).get("file_ext", metadata_file)
+        fname = ''.join(c in util.FILENAME_VALID_CHARS and c or '_' for c in hda.name)[0:150]
+        trans.response.headers["Content-Type"] = "application/octet-stream"
+        trans.response.headers["Content-Disposition"] = f'attachment; filename="Galaxy{hda.hid}-[{fname}].{file_ext}"'
+        return open(hda.metadata.get(metadata_file).file_name, 'rb')
 
     @web.expose_api_anonymous
     def converted(self, trans, dataset_id, ext, **kwargs):

lib/galaxy_test/api/test_datasets.py
@@ -101,6 +101,14 @@ class DatasetsApiTestCase(ApiTestCase):
         self._assert_status_code_is(show_response, 200)
         self.__assert_matches_hda(hda1, show_response.json())
 
+    def test_show_permission_denied(self):
+        hda = self.dataset_populator.new_dataset(self.history_id)
+        self.dataset_populator.make_private(history_id=self.history_id, dataset_id=hda['id'])
+        with self._different_user():
+            show_response = self._get(f"datasets/{hda['id']}")
+            self._assert_status_code_is(show_response, 400)
+            assert show_response.json()['err_msg'] == 'You are not allowed to access this dataset'
+
     def __assert_matches_hda(self, input_hda, query_hda):
         self._assert_has_keys(query_hda, "id", "name")
         assert input_hda["name"] == query_hda["name"]

commit eafca3f68c4cf263660ea558b011b9ad674cd476

Allow sessionless access to some datasets API endpoints
Commit c92fcb91d7f455d5c3d11febe3f5d028741e178f broke a BioBlend test where
a public dataset is accessed anonymously via the API:

https://github.com/galaxyproject/bioblend/blob/main/bioblend/_tests/TestGalaxyDatasets.py#L181-L190

See the GitHub workflow failed jobs:

https://github.com/galaxyproject/bioblend/runs/4074342140?check_suite_focus=true

N.B.: in terms of access control, `legacy_expose_api_anonymous` is
equivalent to `expose_api_anonymous_and_sessionless` (not
`expose_api_anonymous`).

lib/galaxy/webapps/galaxy/api/datasets.py
@@ -124,7 +124,7 @@ class DatasetsController(BaseGalaxyAPIController, UsesVisualizationMixin):
         )
         return [self.serializer_by_type[content.history_content_type].serialize_to_view(content, user=trans.user, trans=trans, view=view) for content in contents]
 
-    @web.expose_api_anonymous
+    @web.expose_api_anonymous_and_sessionless
     def show(self, trans, id, hda_ldda='hda', data_type=None, provider=None, **kwd):
         """
         GET /api/datasets/{encoded_dataset_id}
@@ -420,7 +420,7 @@ class DatasetsController(BaseGalaxyAPIController, UsesVisualizationMixin):
 
         return data
 
-    @web.expose_api_anonymous
+    @web.expose_api_anonymous_and_sessionless
     def extra_files(self, trans, history_content_id, history_id, **kwd):
         """
         GET /api/histories/{encoded_history_id}/contents/{encoded_content_id}/extra_files
@@ -495,7 +495,7 @@ class DatasetsController(BaseGalaxyAPIController, UsesVisualizationMixin):
             "item_url": item_url,
         }
 
-    @web.expose_api_raw_anonymous
+    @web.expose_api_raw_anonymous_and_sessionless
     def get_metadata_file(self, trans, history_content_id, history_id, metadata_file=None, **kwd):
         """
         GET /api/histories/{history_id}/contents/{history_content_id}/metadata_file