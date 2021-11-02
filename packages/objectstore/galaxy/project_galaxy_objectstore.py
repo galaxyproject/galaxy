6b29202be64b68a971b51d28af668b7d67ed6f5c
mvdbeek <m.vandenbeek@gmail.com>
2021-11-02 12:08:02 +0100

    Update version to 21.09.

--- packages/objectstore/galaxy/project_galaxy_objectstore.py
+++ packages/objectstore/galaxy/project_galaxy_objectstore.py
@@ -1,4 +1,4 @@
-__version__ = "21.9.0rc1"
+__version__ = "21.9.0"
 
 PROJECT_NAME = "galaxy-objectstore"
 PROJECT_OWNER = PROJECT_USERAME = "galaxyproject"
--- packages/web_framework/galaxy/project_galaxy_web_framework.py
+++ packages/web_framework/galaxy/project_galaxy_web_framework.py
@@ -1,4 +1,4 @@
-__version__ = "21.9.0rc1"
+__version__ = "21.9.0"
 
 PROJECT_NAME = "galaxy-web-framework"
 PROJECT_OWNER = PROJECT_USERAME = "galaxyproject"
