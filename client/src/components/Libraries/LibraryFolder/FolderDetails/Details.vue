6145fcc405f9ead81aca0a8e1825f09d15e835cb
Oleg Zharkov <oleg.zharkov22@gmail.com>
2021-11-09 16:51:41 +0100

    add missing file

---
+++ client/src/components/Libraries/LibraryFolder/FolderDetails/Details.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <b-button title="Show location details" v-b-modal.details-modal>
+            <font-awesome-icon icon="info-circle" /> Details
+        </b-button>
+
+        <b-modal id="details-modal"> my amazing details! </b-modal>
+
+    </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faInfoCircle);
+
+export default {
+    components: {
+        FontAwesomeIcon,
+    },
+    props: {
+        id: {
+            type: String,
+            required: true,
+        },
+        metadata: {
+            type: Object,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            someVariable: "example"
+        };
+    },
+    methods: {},
+};
+</script>
