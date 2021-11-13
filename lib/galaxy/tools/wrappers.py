dc8ee7f0015ff74753bdc8c1c8222dd4b368a5b9
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-11-13 16:30:40 +0100

    Fix type hints

--- lib/galaxy/tools/wrappers.py
+++ lib/galaxy/tools/wrappers.py
@@ -68,7 +68,7 @@ class ToolParameterValueWrapper:
     Base class for object that Wraps a Tool Parameter and Value.
     """
 
-    value: Optional[Union[str, List[str]]]
+    value: Any
     input: "ToolParameter"
 
     def __bool__(self) -> bool:
@@ -122,7 +122,7 @@ class InputValueWrapper(ToolParameterValueWrapper):
     def __init__(
         self,
         input: "ToolParameter",
-        value: Optional[str],
+        value: Any,
         other_values: Optional[Dict[str, str]] = None,
         profile: Optional[float] = None,
     ) -> None:
