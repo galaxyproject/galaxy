1cc6905c3f124adf541d71f7518e9ecb5d77566e
davelopez <46503462+davelopez@users.noreply.github.com>
2021-11-15 20:18:31 +0100

    Add get_filter_query_params function to api/common.py module

--- lib/galaxy/webapps/galaxy/api/common.py
+++ lib/galaxy/webapps/galaxy/api/common.py
@@ -1,9 +1,17 @@
 """This module contains utility functions shared across the api package."""
-from typing import Any, Dict, Optional
+from typing import (
+    Any,
+    Dict,
+    List,
+    Optional,
+)
 
 from fastapi import Query
 
-from galaxy.schema import SerializationParams
+from galaxy.schema import (
+    FilterQueryParams,
+    SerializationParams,
+)
 from galaxy.schema.schema import UpdateDatasetPermissionsPayload
 
 SerializationViewQueryParam: Optional[str] = Query(
@@ -43,6 +51,55 @@ def query_serialization_params(
     default_view: Optional[str] = SerializationDefaultViewQueryParam,
 ) -> SerializationParams:
     return parse_serialization_params(view=view, keys=keys, default_view=default_view)
+
+
+def get_filter_query_params(
+    q: Optional[List[str]] = Query(
+        default=None,
+        title="Filter Query",
+        description="Generally a property name to filter by followed by an (often optional) hyphen and operator string.",
+        example="create_time-gt",
+    ),
+    qv: Optional[List[str]] = Query(
+        default=None,
+        title="Filter Value",
+        description="The value to filter by.",
+        example="2015-01-29",
+    ),
+    offset: Optional[int] = Query(
+        default=0,
+        ge=0,
+        title="Offset",
+        description="Starts at the beginning skip the first ( offset - 1 ) items and begin returning at the Nth item",
+    ),
+    limit: Optional[int] = Query(
+        default=None,
+        ge=1,
+        title="Limit",
+        description="The maximum number of items to return.",
+    ),
+    order: Optional[str] = Query(
+        default=None,
+        title="Order",
+        description=(
+            "String containing one of the valid ordering attributes followed (optionally) "
+            "by '-asc' or '-dsc' for ascending and descending order respectively. "
+            "Orders can be stacked as a comma-separated list of values."
+        ),
+        example="name-dsc,create_time",
+    ),
+) -> FilterQueryParams:
+    """
+    This function is meant to be used as a Dependency.
+    See https://fastapi.tiangolo.com/tutorial/dependencies/#first-steps
+    """
+    return FilterQueryParams(
+        q=q,
+        qv=qv,
+        offset=offset,
+        limit=limit,
+        order=order,
+    )
 
 
 def get_update_permission_payload(payload: Dict[str, Any]) -> UpdateDatasetPermissionsPayload:
