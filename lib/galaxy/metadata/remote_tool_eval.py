1a558dfd88de6b40d7edbbdbe25c4c51ced5a921
mvdbeek <m.vandenbeek@gmail.com>
2021-11-17 14:55:30 +0100

    More todos and fixmes

--- lib/galaxy/metadata/remote_tool_eval.py
+++ lib/galaxy/metadata/remote_tool_eval.py
@@ -38,6 +38,8 @@ class ToolAppConfig(NamedTuple):
     len_file_path: str
     builds_file_path: str
     root: str
+    # TODO: serialize user object or serialize only relevant attributes (email, is_admin) ?
+    # Do we even really need is_admin_user ?
     is_admin_user: Callable = lambda _: False
     admin_users: list = []
 
@@ -74,12 +76,14 @@ def main():
     object_store = get_object_store()
     import_store = store.imported_store_for_metadata(os.path.join(WORKING_DIRECTORY, 'metadata', 'outputs_new'))
     job = next(iter(import_store.sa_session.objects[Job].values()))
+    # TODO: clean up random places from which we read files in the working directory
     job_io = JobIO.from_json(os.path.join(WORKING_DIRECTORY, 'job_io.json'), sa_session=import_store.sa_session, job=job)
     tool_app_config = ToolAppConfig(name='tool_app', tool_data_path=job_io.tool_data_path, nginx_upload_path=TMPDIR, len_file_path=job_io.len_file_path, builds_file_path=job_io.builds_file_path, root=TMPDIR)
     with open(os.path.join(WORKING_DIRECTORY, 'tool_data_tables.json')) as data_tables_json:
         tdtm = ToolDataTableManager.from_dict(json.load(data_tables_json))
     app = ToolApp(sa_session=import_store.sa_session, tool_app_config=tool_app_config, datatypes_registry=datatypes_registry, object_store=object_store, tool_data_table_manager=tdtm, file_sources=job_io.file_sources)
     # TODO: could try to serialize just a minimal tool variant instead of the whole thing ?
+    # FIXME: enable loading all supported tool types
     tool_source = get_tool_source(os.path.join(WORKING_DIRECTORY, 'metadata', 'outputs_new', 'tool.xml'))
     tool = create_tool_from_source(app, tool_source=tool_source)
     tool_evaluator = evaluation.RemoteToolEvaluator(app=app, tool=tool, job=job, local_working_directory=WORKING_DIRECTORY, job_io=job_io)
@@ -90,4 +94,5 @@ def main():
 
 
 if __name__ == "__main__":
+    # TODO: exception handling, right now we silently fail with no clue what happened.
     main()
