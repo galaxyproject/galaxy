7451ef68156fbf8a84c0821576ceedd3ad169fa7
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2021-11-25 18:59:49 -0500

    Do not override Galaxy logging: disable in alembic.

--- lib/galaxy/model/migrations/alembic/env.py
+++ lib/galaxy/model/migrations/alembic/env.py
@@ -8,10 +8,6 @@ from sqlalchemy import pool
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-fileConfig(config.config_file_name)
-
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
