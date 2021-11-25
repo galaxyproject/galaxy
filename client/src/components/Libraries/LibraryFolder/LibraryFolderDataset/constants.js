83ea4507866a70dc10cc5d8cb4d692cf7ef45356
oleg <oleg.zharkov22@gmail.com>
2021-11-25 15:44:48 +0100

    delete unused constant

--- client/src/components/Libraries/LibraryFolder/LibraryFolderDataset/constants.js
+++ client/src/components/Libraries/LibraryFolder/LibraryFolderDataset/constants.js
@@ -16,14 +16,3 @@ export const fieldTitles = {
     tags: "Tags",
     uuid: "UUID",
 };
-export const auto = {
-    id: "auto",
-    text: "Auto-detect",
-    description:
-        "This system will try to detect the file type automatically." +
-        " If your file is not detected properly as one of the known formats," +
-        " it most likely means that it has some format problems (e.g., different" +
-        " number of columns on different rows). You can still coerce the system" +
-        " to set your data to the format you think it should be." +
-        " You can also upload compressed files, which will automatically be decompressed.",
-};
