327779c584d7f8d1f66a05cc4b9c56dc71dd418d
guerler <aysam.guerler@gmail.com>
2021-12-01 09:40:14 -0500

    Move plugin list component to visualizations subdirectory, parse id as prop

---
+++ client/src/components/Visualizations/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <PluginList :dataset-id="datasetId" />
+    </div>
+</template>
+<script>
+import DisplayApplications from "./DisplayApplications";
+import PluginList from "./PluginList";
+export default {
+    components: {
+        PluginList,
+    },
+    props: {
+        datasetId: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
