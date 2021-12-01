a76d13109e8f2f444a35b8d6347723e5a500ec4c
guerler <aysam.guerler@gmail.com>
2021-12-01 10:23:31 -0500

    Use simple dataprovider to retrieve display application links

--- client/src/components/Visualizations/DisplayApplications.vue
+++ client/src/components/Visualizations/DisplayApplications.vue
@@ -1,14 +1,27 @@
 <template>
     <div>
-        {{ datasetId }}
+        <UrlDataProvider :url="url" v-slot="{ result: dataset, loading }">
+            <div v-if="dataset && dataset.display_apps">
+                {{ dataset.display_apps }}
+            </div>
+        </UrlDataProvider>
     </div>
 </template>
 <script>
+import { UrlDataProvider } from "components/providers/UrlDataProvider";
 export default {
+    components: {
+        UrlDataProvider,
+    },
     props: {
         datasetId: {
             type: String,
             required: true,
+        },
+    },
+    computed: {
+        url() {
+            return `api/datasets/${this.datasetId}`;
         },
     },
 };
