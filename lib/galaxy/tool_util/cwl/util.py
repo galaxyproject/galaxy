5884d94df50f705fec1297117c4dfeeff4766183
mvdbeek <m.vandenbeek@gmail.com>
2021-12-09 19:34:40 +0100

    Implement rough mapping between EDAM formats and datatypes
    This adds the cwl_format parameter to the upload tool and exposes the
    cwl_formats field in the details view of the HDA API response, which
    contains a list of URIs for the format. This also provides the
    edam_format from the datatype annotation as the format field of the cwl
    job input description and uses the format field within the cwl job
    outputs to write out a gaalxy.json file that contains the cwl-format,
    which we then use to pick a datatype (if we find one).
    
    Not sure this is a good idea, in practice this will fall apart for
    compressed outputs at least, for which there is no distinction in edam.
    If that's the only problem we could probably work around the issue, but
    there could be more problems. It is kind of cool and maybe necessary for
    interoperability between cwl tool outputs and galaxy tool inputs
    (assuming users won't want to change dataypes), but maybe we should just always
    stick the edam format into the hda metadata or into a separate hda
    column and keep these two concepts separate.
    
    We should maybe extend the edam format use a bit more. This could be
    a cool way for tools to use user-defined datatypes that Galaxy doesn't
    (yet) know about.

--- lib/galaxy/tool_util/cwl/util.py
+++ lib/galaxy/tool_util/cwl/util.py
@@ -57,6 +57,7 @@ OutputPropertiesType = TypedDict(
         "secondaryFiles": List[Any],
         "checksum": str,
         "size": int,
+        "format": Optional[str],
     },
     total=False,
 )
@@ -67,6 +68,7 @@ def output_properties(
     content: Optional[bytes] = None,
     basename=None,
     pseudo_location=False,
+    cwl_formats: Optional[List[str]] = None,
 ) -> OutputPropertiesType:
     checksum = hashlib.sha1()
     properties: OutputPropertiesType = {"class": "File", "checksum": "", "size": 0}
@@ -90,6 +92,7 @@ def output_properties(
         f.close()
     properties["checksum"] = f"sha1${checksum.hexdigest()}"
     properties["size"] = filesize
+    properties["format"] = cwl_formats[0] if cwl_formats else None
     set_basename_and_derived_properties(properties, basename)
     _handle_pseudo_location(properties, pseudo_location)
     return properties
@@ -516,7 +519,9 @@ def output_to_cwl_json(
 
             if file_or_directory == "File":
                 dataset_dict = get_dataset(output_metadata)
-                properties = output_properties(pseudo_location=pseudo_location, **dataset_dict)
+                properties = output_properties(
+                    pseudo_location=pseudo_location, cwl_formats=output_metadata["cwl_formats"], **dataset_dict
+                )
                 basename = properties["basename"]
                 extra_files = get_extra_files(output_metadata)
                 found_index = False
