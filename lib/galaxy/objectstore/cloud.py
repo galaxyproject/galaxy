5391d54a72129370c492a74a67ffd8554b27aee9
Uehara.Keizo <keizou.bos@gmail.com>
2021-12-17 04:45:05 +0000

    Make access_key,secret_key for aws optional
    Access_key and secret_key are not required
    because boto3 looks at various configuration locations.

--- lib/galaxy/objectstore/cloud.py
+++ lib/galaxy/objectstore/cloud.py
@@ -186,11 +186,7 @@ class Cloud(ConcreteObjectStore, CloudConfigMixin):
             missing_config = []
             if provider == "aws":
                 akey = auth_element.get("access_key")
-                if akey is None:
-                    missing_config.append("access_key")
                 skey = auth_element.get("secret_key")
-                if skey is None:
-                    missing_config.append("secret_key")
 
                 config["auth"] = {
                     "access_key": akey,
