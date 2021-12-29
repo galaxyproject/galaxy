73de30a53cef66bf06edbd003142c7d6c77c6493
Alireza Heidari <itisalirh@gmail.com>
2021-12-29 18:18:12 +0330

    Add new slot 'helper' to SelectionDialog and other improvements
    in modal-fotter, wrap 'w-100' class to the parent div, delete showFtpHelper prop and data(), delete ftp alert

--- client/src/components/SelectionDialog/SelectionDialog.vue
+++ client/src/components/SelectionDialog/SelectionDialog.vue
@@ -3,32 +3,29 @@
         <template v-slot:modal-header>
             <slot name="search"> </slot>
         </template>
-        <b-alert v-if="showFtpHelper" variant="info" show>
-            This Galaxy server allows you to upload files via FTP. To upload some files, log in to the FTP server at
-            <strong>{{ ftpUploadSite }}</strong> using your Galaxy credentials. For help visit the
-            <a href="https://galaxyproject.org/ftp-upload/" target="_blank">tutorial</a>.
-            <span v-if="oidcEnabled"
-                ><br />If you are signed-in to Galaxy using a third-party identity and you
-                <strong>do not have a Galaxy password</strong> please use the reset password option in the login form
-                with your email to create a password for your account.</span
-            >
-        </b-alert>
+        <slot name="helper"> </slot>
         <b-alert v-if="errorMessage" variant="danger" show v-html="errorMessage" />
         <div v-else>
             <slot name="options" v-if="optionsShow"> </slot>
             <div v-else><span class="fa fa-spinner fa-spin" /> <span>Please wait...</span></div>
         </div>
         <template v-slot:modal-footer>
-            <div v-if="!errorMessage" class="w-100">
-                <slot name="buttons"> </slot>
-                <b-btn size="sm" class="float-right selection-dialog-modal-cancel" @click="hideModal"> Cancel </b-btn>
-            </div>
-            <div v-else class="w-100">
-                <b-btn id="back-btn" size="sm" class="float-left" v-if="undoShow" @click="backFunc()">
-                    <font-awesome-icon :icon="['fas', 'caret-left']" />
-                    Back
-                </b-btn>
-                <b-btn size="sm" class="float-right" variant="primary" id="close-btn" @click="hideModal"> Close </b-btn>
+            <div class="w-100">
+                <div v-if="!errorMessage">
+                    <slot name="buttons"> </slot>
+                    <b-btn size="sm" class="float-right selection-dialog-modal-cancel" @click="hideModal">
+                        Cancel
+                    </b-btn>
+                </div>
+                <div v-else>
+                    <b-btn id="back-btn" size="sm" class="float-left" v-if="undoShow" @click="backFunc()">
+                        <font-awesome-icon :icon="['fas', 'caret-left']" />
+                        Back
+                    </b-btn>
+                    <b-btn size="sm" class="float-right" variant="primary" id="close-btn" @click="hideModal">
+                        Close
+                    </b-btn>
+                </div>
             </div>
         </template>
     </b-modal>
@@ -36,7 +33,6 @@
 
 <script>
 import Vue from "vue";
-import { getGalaxyInstance } from "app";
 import BootstrapVue from "bootstrap-vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
@@ -77,19 +73,9 @@ export default {
             type: Boolean,
             required: false,
         },
-        showFtpHelper: {
-            type: Boolean,
-            required: false,
-        },
     },
     components: {
         FontAwesomeIcon,
-    },
-    data() {
-        return {
-            ftpUploadSite: getGalaxyInstance()?.config?.ftp_upload_site,
-            oidcEnabled: getGalaxyInstance()?.config?.enable_oidc,
-        };
     },
 };
 </script>
