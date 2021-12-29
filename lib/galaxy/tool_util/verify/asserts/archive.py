96de9ed5b51603580cc237438a2526a3d7a4e22b
Matthias Bernt <m.bernt@ufz.de>
2021-12-29 10:33:17 +0100

    add all attribute to archive asserts
    allowing to check sub assertions for all archive members matching path

--- lib/galaxy/tool_util/verify/asserts/archive.py
+++ lib/galaxy/tool_util/verify/asserts/archive.py
@@ -1,45 +1,56 @@
 import io
+import os
 import re
 import tarfile
+import tempfile
 import zipfile
 
 
-def _extract_from_tar(tar_temp, path):
-    for fn in tar_temp.getnames():
-        if not re.match(path, fn):
-            continue
-        # Will only match on first hit, probably fine for now
-        # if called on a dir zip.open returns a handle to an empty string
-        # in contrast tar.extractfile returns None, the following
-        # if makes the _extract... functions behave equally in this case
-        if not tar_temp.getmember(fn).isfile():
-            return io.StringIO("")
-        return tar_temp.extractfile(fn)
+def _extract_from_tar(temp, path):
+    with tarfile.open(fileobj=temp, mode='r') as tar_temp:
+        for fn in tar_temp.getnames():
+            if not re.match(path, fn):
+                continue
+            # if called on a dir zip.open returns a handle to an empty string
+            # in contrast tar.extractfile returns None, the following
+            # if makes the _extract... functions behave equally in this case
+            if not tar_temp.getmember(fn).isfile():
+                yield io.StringIO("")
+            yield tar_temp.extractfile(fn)
 
 
-def _extract_from_zip(zip_temp, path):
-    for fn in zip_temp.namelist():
-        if not re.match(path, fn):
-            continue
-        return zip_temp.open(fn)
+def _extract_from_zip(temp, path):
+    with zipfile.ZipFile(temp, mode='r') as zip_temp:
+        for fn in zip_temp.namelist():
+            if not re.match(path, fn):
+                continue
+            yield zip_temp.open(fn)
 
 
-def assert_has_archive_member(output_bytes, path, verify_assertions_function, children):
+def assert_has_archive_member(output_bytes, path, verify_assertions_function, children, all=False):
     """ Recursively checks the specified children assertions against the text of
     the first element matching the specified path found within the archive.
     Currently supported formats: .zip, .tar, .tar.gz."""
 
+    # from python 3.9 is_tarfile supports file like objects then we do not need
+    # the tempfile detour but can use io.BytesIO(output_bytes)
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        tmpname = tmp.name
+        tmp.write(output_bytes)
+    zipfoo = None
+    if zipfile.is_zipfile(tmp.name):
+        zipfoo = _extract_from_zip
+    if tarfile.is_tarfile(tmp.name):
+        zipfoo = _extract_from_tar
+    os.remove(tmpname)
+    assert zipfoo is not None, f"Expected path '{path}' to be an archive"
+
     with io.BytesIO(output_bytes) as output_temp:
-        try:  # tar / tar.gz
-            temp = tarfile.open(fileobj=output_temp, mode='r')
-            contents = _extract_from_tar(temp, path)
-        except tarfile.TarError:  # zip
-            try:
-                temp = zipfile.ZipFile(output_temp, mode='r')
-                contents = _extract_from_zip(temp, path)
-            except zipfile.BadZipFile:
-                raise AssertionError(f"Expected path '{path}' to be an archive")
-        assert contents is not None, f"Expected path '{path}' in archive"
-        verify_assertions_function(contents.read(), children)
-        contents.close()
-        temp.close()
+        haspath = False
+        for contents in zipfoo(output_temp, path):
+            haspath = True
+            verify_assertions_function(contents.read(), children)
+            contents.close()
+            if not all:
+                break
+        assert haspath, f"Expected path '{path}' in archive"
