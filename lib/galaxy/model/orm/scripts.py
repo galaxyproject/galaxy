a325964c4ee4c48232d34f9e18685b224e1a0f73
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2022-01-06 14:28:58 -0500

    Remove outdated parts from orm.scripts.py

--- lib/galaxy/model/orm/scripts.py
+++ lib/galaxy/model/orm/scripts.py
@@ -26,29 +26,25 @@ DEFAULT_CONFIG_PREFIX = ""
 DEFAULT_DATABASE = "galaxy"
 
 DATABASE = {
-    "galaxy": {
-        "repo": "galaxy/model/migrate",
-        "default_sqlite_file": "universe.sqlite",
-        "config_override": "GALAXY_CONFIG_",
-    },
-    "tools": {
-        "repo": "galaxy/model/tool_shed_install/migrate",
-        "default_sqlite_file": "universe.sqlite",
-        "config_override": "GALAXY_CONFIG_",
-    },
-    "tool_shed": {
-        "repo": "tool_shed/webapp/model/migrate",
-        "config_names": ["tool_shed", "tool_shed_wsgi"],
-        "default_sqlite_file": "community.sqlite",
-        "config_override": "TOOL_SHED_CONFIG_",
-        "config_section": "tool_shed",
-    },
-    "install": {
-        "repo": "galaxy/model/tool_shed_install/migrate",
-        "config_prefix": "install_",
-        "default_sqlite_file": "install.sqlite",
-        "config_override": "GALAXY_INSTALL_CONFIG_",
-    },
+    "galaxy":
+        {
+            'default_sqlite_file': 'universe.sqlite',
+            'config_override': 'GALAXY_CONFIG_',
+        },
+    "tool_shed":
+        {
+            'repo': 'tool_shed/webapp/model/migrate',
+            'config_names': ['tool_shed', 'tool_shed_wsgi'],
+            'default_sqlite_file': 'community.sqlite',
+            'config_override': 'TOOL_SHED_CONFIG_',
+            'config_section': 'tool_shed',
+        },
+    "install":
+        {
+            'config_prefix': 'install_',
+            'default_sqlite_file': 'install.sqlite',
+            'config_override': 'GALAXY_INSTALL_CONFIG_',
+        },
 }
 
 
@@ -111,8 +107,6 @@ def get_config(argv, use_argparse=True, cwd=None):
     >>> uri_with_env = os.getenv("GALAXY_TEST_DBURI", "sqlite:////moo/universe.sqlite?isolation_level=IMMEDIATE")
     >>> config['db_url'] == uri_with_env
     True
-    >>> config['repo'].endswith('galaxy/model/migrate')
-    True
     >>> rmtree(config_dir)
     """
     config_file, config_section, database = _read_model_arguments(argv, use_argparse=use_argparse)
@@ -125,10 +119,13 @@ def get_config(argv, use_argparse=True, cwd=None):
             cwd = [DEFAULT_CONFIG_DIR]
         config_file = find_config_file(config_names, dirs=cwd)
 
-    repo = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir, database_defaults["repo"])
-    config_prefix = database_defaults.get("config_prefix", DEFAULT_CONFIG_PREFIX)
-    config_override = database_defaults.get("config_override", "GALAXY_CONFIG_")
-    default_sqlite_file = database_defaults["default_sqlite_file"]
+    repo = database_defaults.get('repo')
+    if repo:
+        repo = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir, repo)
+
+    config_prefix = database_defaults.get('config_prefix', DEFAULT_CONFIG_PREFIX)
+    config_override = database_defaults.get('config_override', 'GALAXY_CONFIG_')
+    default_sqlite_file = database_defaults['default_sqlite_file']
     if config_section is None:
         if not config_file or get_ext(config_file, ignore="sample") == "yaml":
             config_section = database_defaults.get("config_section", None)
