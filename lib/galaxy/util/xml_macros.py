c3d3727a005a870c5b957e8a60e1d857b2fa46b8
Matthias Bernt <m.bernt@ufz.de>
2022-01-11 23:07:44 +0100

    even simpler _expand_yield_statements

--- lib/galaxy/util/xml_macros.py
+++ lib/galaxy/util/xml_macros.py
@@ -197,18 +197,10 @@ def _expand_yield_statements(macro_def, expand_el):
             <sub_of_expand_2/>
         </xml>
     """
-    yield_els = [yield_el for macro_def_el in macro_def for yield_el in macro_def_el.findall('.//yield')]
+    yield_els = [yield_el for yield_el in macro_def.findall('.//yield')]
     expand_el_children = list(expand_el)
     for yield_el in yield_els:
         _xml_replace(yield_el, expand_el_children)
-
-    # Replace yields at the top level of a macro, seems hacky approach
-    for i, macro_def_el in enumerate(macro_def):
-        if macro_def_el.tag == "yield":
-            for target in expand_el_children:
-                i += 1
-                macro_def.insert(i, deepcopy(target))
-            macro_def.remove(macro_def_el)
 
 
 def _load_macros(macros_el, xml_base_dir):
