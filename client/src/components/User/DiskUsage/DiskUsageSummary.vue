2070ef9585a412737b2c7424243d816afe28e9e0
davelopez <46503462+davelopez@users.noreply.github.com>
2022-01-11 15:37:17 +0100

    Center spinner when loading the quota usage summary

--- client/src/components/User/DiskUsage/DiskUsageSummary.vue
+++ client/src/components/User/DiskUsage/DiskUsageSummary.vue
@@ -6,17 +6,17 @@
                     <h4 class="alert-heading">{{ errorMessageTitle }}</h4>
                     {{ errorMessage }}
                 </b-alert>
-                <div v-if="user">
-                    <div v-if="config.enable_quotas">
+                <b-container v-if="user">
+                    <b-row v-if="config.enable_quotas" class="justify-content-md-center">
                         <QuotaUsageProvider v-slot="{ result: quotaUsages, loading: isLoadingUsage }">
-                            <LoadingSpan v-if="isLoadingUsage" />
+                            <b-spinner v-if="isLoadingUsage" />
                             <QuotaUsageSummary v-else-if="quotaUsages" :quota-usages="quotaUsages" />
                         </QuotaUsageProvider>
-                    </div>
+                    </b-row>
                     <h2 v-else class="text-center my-3" id="basic-disk-usage-summary">
                         You're using <b>{{ getTotalDiskUsage(user) }}</b> of disk space.
                     </h2>
-                </div>
+                </b-container>
             </div>
         </CurrentUser>
     </ConfigProvider>
@@ -25,7 +25,6 @@
 <script>
 import _l from "utils/localization";
 import { bytesToString } from "utils/utils";
-import LoadingSpan from "components/LoadingSpan";
 import CurrentUser from "components/providers/CurrentUser";
 import ConfigProvider from "components/providers/ConfigProvider";
 import QuotaUsageSummary from "components/User/DiskUsage/Quota/QuotaUsageSummary";
@@ -37,7 +36,6 @@ export default {
         ConfigProvider,
         QuotaUsageSummary,
         QuotaUsageProvider,
-        LoadingSpan,
     },
     data() {
         return {
