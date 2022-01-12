19e9e55ca3dd2a715954de701c2c5711962f2f81
mvdbeek <m.vandenbeek@gmail.com>
2022-01-12 12:09:57 +0100

    Fix uvloop message

--- lib/galaxy/webapps/galaxy/workers.py
+++ lib/galaxy/webapps/galaxy/workers.py
@@ -5,5 +5,5 @@ try:
     import uvloop  # noqa: F401
     from uvicorn.workers import UvicornWorker as Worker
 except ImportError:
-    log.warning("uvtools not available, falling back to pure python worker")
+    log.warning("uvloop not available, falling back to pure python worker")
     from uvicorn.workers import UvicornH11Worker as Worker  # noqa: F401
