171167abf26dfa2c86e2f32583424a06cacd84e2
mvdbeek <m.vandenbeek@gmail.com>
2022-01-20 12:10:44 +0100

    Split monitor test into tests that need and don't need mocking

---
+++ client/src/components/providers/monitors/index.js
@@ -0,0 +1,11 @@
+// Public exports
+export {
+    buildHistoryMonitor,
+    createContentMonitor,
+    datasetMonitor,
+    datasetCollectionMonitor,
+    getHistoryMonitor,
+    invocationStepMonitor,
+} from "./monitors";
+
+export { monitorHistoryUntilTrue } from "./monitorHistoriesUntilTrue";
