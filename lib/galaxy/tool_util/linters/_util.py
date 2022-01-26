28072009d25a1eee9efb2140be0d37a9f57ef537
mvdbeek <m.vandenbeek@gmail.com>
2022-01-26 12:40:41 +0100

    Rewrite parameterized pytests to regular test
    The .. and  ... and ... and within a lambda gives you
    no chance to debug the test without a print statement or
    a debugger.
    
    Co-authored-by: Marius van den Beek <m.vandenbeek@gmail.com>

--- lib/galaxy/tool_util/linters/_util.py
+++ lib/galaxy/tool_util/linters/_util.py
@@ -13,6 +13,7 @@ def node_props_factory(tool_xml):
 
     return node_props
 
+
 def is_datasource(tool_xml):
     """Returns true if the tool is a datasource tool"""
     return tool_xml.getroot().attrib.get('tool_type', '') == 'data_source'
