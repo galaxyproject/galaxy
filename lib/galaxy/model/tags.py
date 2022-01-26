6fe9158069e0021cde7791a0b0058a70448941c2
Alexander OSTROVSKY <a.ostrovsky@mac.com>
2022-01-25 16:49:02 -0800

    only remove lowercase from value, not name

--- lib/galaxy/model/tags.py
+++ lib/galaxy/model/tags.py
@@ -157,18 +157,20 @@ class TagHandler:
         return False
 
     def apply_item_tag(self, user, item, name, value=None, flush=True):
+        # Use lowercase name for searching/creating tag.
         if name is None:
             return
+        lc_name = name.lower()
         # Get or create item-tag association.
-        item_tag_assoc = self._get_item_tag_assoc(user, item, name)
+        item_tag_assoc = self._get_item_tag_assoc(user, item, lc_name)
         # If the association does not exist, or if it has a different value, add another.
         # We do allow multiple associations with different values.
         if not item_tag_assoc or (item_tag_assoc and item_tag_assoc.value != value):
             # Create item-tag association.
             # Create tag; if None, skip the tag (and log error).
-            tag = self._get_or_create_tag(name)
+            tag = self._get_or_create_tag(lc_name)
             if not tag:
-                log.warning(f"Failed to create tag with name {name}")
+                log.warning(f"Failed to create tag with name {lc_name}")
                 return
             # Create tag association based on item class.
             item_tag_assoc_class = self.get_tag_assoc_class(item.__class__)
@@ -214,7 +216,7 @@ class TagHandler:
     def get_tag_by_name(self, tag_name):
         """Get a Tag object from a tag name (string)."""
         if tag_name:
-            return self.sa_session.query(galaxy.model.Tag).filter_by(name=tag_name).first()
+            return self.sa_session.query(galaxy.model.Tag).filter_by(name=tag_name.lower()).first()
         return None
 
     def _create_tag(self, tag_str: str):
