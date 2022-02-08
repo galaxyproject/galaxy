b7b1c120c07a7873537573de40af613759f4892c
Assunta DeSanto <assuntad23@gmail.com>
2022-02-07 19:03:28 -0500

    tidying up mismanaged merge issue

--- lib/galaxy_test/api/test_configuration.py
+++ lib/galaxy_test/api/test_configuration.py
@@ -6,13 +6,13 @@ from galaxy_test.base.api_util import TEST_USER
 from ._framework import ApiTestCase
 
 TEST_KEYS_FOR_ALL_USERS = [
-    'enable_unique_workflow_defaults',
-    'ftp_upload_site',
-    'wiki_url',
-    'support_url',
-    'logo_url',
-    'terms_url',
-    'allow_user_dataset_purge',
+    "enable_unique_workflow_defaults",
+    "ftp_upload_site",
+    "wiki_url",
+    "support_url",
+    "logo_url",
+    "terms_url",
+    "allow_user_dataset_purge",
 ]
 TEST_KEYS_FOR_ADMIN_ONLY = [
     "library_import_dir",
