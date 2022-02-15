60d7468ddcdd59f8ca29d170fa2686e696c9f262
guerler <aysam.guerler@gmail.com>
2022-02-15 00:17:23 -0500

    Add interface to content details

---
+++ client/src/components/History/ContentItem/ContentDetails.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="p-3 details">
+        Content Details
+        <h4 data-description="dataset name">{{ item.name || "(Dataset Name)" }}</h4>
+    </div>
+</template>
+
+<script>
+import Placeholder from "./Placeholder";
+import Dataset from "./Dataset/Dataset";
+import DatasetCollection from "./DatasetCollection";
+import Subdataset from "./Subdataset";
+import Subcollection from "./Subcollection";
+
+export default {
+    components: {
+        Placeholder,
+        Dataset,
+        DatasetCollection,
+        Subdataset,
+        Subcollection,
+    },
+    props: {
+        item: { type: Object, required: true },
+    },
+};
+</script>
