b61076a57225b0c67ea595755a796f4f0daff996
guerler <aysam.guerler@gmail.com>
2022-02-15 06:14:01 -0500

    Move detail component methods to content details component

--- client/src/components/History/ContentItem/ContentDetails.vue
+++ client/src/components/History/ContentItem/ContentDetails.vue
@@ -23,5 +23,35 @@ export default {
     props: {
         item: { type: Object, required: true },
     },
+    methods: {
+        contentItemComponent() {
+            if (this.item.id === undefined) {
+                return "Placeholder";
+            }
+            return this.historyContentItem();
+        },
+        historyContentItem() {
+            const { history_content_type } = this.item;
+            switch (history_content_type) {
+                case "dataset":
+                    return "Dataset";
+                case "dataset_collection":
+                    return "DatasetCollection";
+                default:
+                    return "Placeholder";
+            }
+        },
+        collectionContentItem() {
+            const { element_type } = this.item;
+            switch (element_type) {
+                case "hda":
+                    return "Subdataset";
+                case "hdca":
+                    return "Subcollection";
+                default:
+                    return "Placeholder";
+            }
+        },
+    },
 };
 </script>
