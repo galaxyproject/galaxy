96b4be0c0d7762f752d5172d22b3cd5e15720380
guerler <aysam.guerler@gmail.com>
2022-02-17 14:44:37 -0500

    Revise collection navigation in history

--- client/src/components/History/CurrentCollection/Navigation.vue
+++ client/src/components/History/CurrentCollection/Navigation.vue
@@ -1,47 +1,35 @@
 <template>
-    <b-breadcrumb>
-        <b-breadcrumb-item href="#" @click="close">
-            <span class="fa fa-angle-double-left" />
-        </b-breadcrumb-item>
-        <b-breadcrumb-item v-for="option in breadCrumbOptions" :key="option.key" @click="reselect(option.value)">
-            <span>{{ option.text }}</span>
-        </b-breadcrumb-item>
-    </b-breadcrumb>
+    <div>
+        <b-button size="sm" variant="link" @click="close">
+            <icon icon="angle-double-left" class="mr-1" /><span>History</span>
+        </b-button>
+        <b-button size="sm" variant="link" v-if="previousName" @click="back">
+            <span class="fa fa-angle-left mr-1" /><span>{{ previousName }}</span>
+        </b-button>
+    </div>
 </template>
 
 <script>
-import { History } from "../model";
-
 export default {
     props: {
-        history: { type: History, required: true },
         selectedCollections: { type: Array, required: true, validate: (val) => val.length > 0 },
     },
     computed: {
-        // List for the dropdown. Should be the history and every parent
-        // collection excepting the current one.
-        breadCrumbOptions() {
-            const parents = this.selectedCollections.slice(0, -1);
-            const options = parents.map((bc, i) => {
-                return {
-                    key: bc.type_id,
-                    value: parents.slice(0, i + 1),
-                    text: bc.name,
-                };
-            });
-            return options;
+        previousName() {
+            const length = this.selectedCollections.length;
+            if (length > 1) {
+                const last = this.selectedCollections[length - 2];
+                return last.name || last.element_identifier;
+            }
         },
     },
     methods: {
-        reselect(newList) {
-            this.$emit("update:selectedCollections", newList);
-        },
         back() {
             const newList = this.selectedCollections.slice(0, -1);
-            this.reselect(newList);
+            this.$emit("update:selected-collections", newList);
         },
         close() {
-            this.reselect([]);
+            this.$emit("update:selected-collections", []);
         },
     },
 };
