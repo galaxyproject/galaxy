a7511c2bf507727adb920c8b508a7dda03960ca3
Assunta DeSanto <assuntad23@gmail.com>
2022-03-01 22:56:56 -0500

    adding a test case for sorting saved rules

--- client/src/components/RuleBuilder/SavedRulesSelector.test.js
+++ client/src/components/RuleBuilder/SavedRulesSelector.test.js
@@ -57,4 +57,61 @@ describe("SavedRulesSelector", () => {
         emitted = wrapper.emitted();
         expect(emitted["update-rules"]).toBeTruthy();
     });
+
+    it("should emit a click event when a session is clicked", async () => {
+        //created less recently 12/12/2021
+        const testRules1 = {
+            rule: {
+                rules: [
+                    {
+                        type: "add_filter_count",
+                        count: 1,
+                        which: "first",
+                        invert: false,
+                    },
+                ],
+                mapping: [
+                    {
+                        type: "url",
+                        columns: [0],
+                    },
+                ],
+            },
+            dateTime: "2021-12-12T12:12:12.000Z",
+        };
+        //created more recently 12/22/2021
+        const testRules2 = {
+            rule: {
+                rules: [
+                    {
+                        type: "add_filter_count",
+                        count: 1,
+                        which: "first",
+                        invert: false,
+                    },
+                ],
+                mapping: [
+                    {
+                        type: "url",
+                        columns: [1],
+                    },
+                ],
+            },
+            dateTime: "2021-12-22T12:12:12.000Z",
+        };
+        wrapper.setProps({
+            user: "test_user",
+            savedRules: [testRules1, testRules2],
+        });
+        await wrapper.vm.$nextTick();
+        const sessions = wrapper.findAll("div.dropdown-menu > a.saved-rule-item");
+        expect(sessions.length == 2).toBeTruthy();
+        //clicking the top result of the dropdown, with sorting should be most recent created rules
+        sessions.wrappers[0].trigger("click");
+        emitted = wrapper.emitted();
+        expect(emitted["update-rules"]).toBeTruthy();
+        expect(emitted["update-rules"].length).toBe(1);
+        //ensure the object sent with emit is equal to most recent rule set
+        expect(emitted["update-rules"][0]).toEqual([testRules2.rule]);
+    });
 });
