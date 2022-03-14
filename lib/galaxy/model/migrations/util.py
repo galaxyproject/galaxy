77254915a7488c0f0c896eebfef21ab558a5f3b6
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2022-03-14 17:46:21 -0400

    Fix dumb mistake
    Co-authored-by: David López <46503462+davelopez@users.noreply.github.com>

--- lib/galaxy/model/migrations/util.py
+++ lib/galaxy/model/migrations/util.py
@@ -2,5 +2,5 @@ from alembic import op
 
 
 def drop_column(table, column):
-    with op.batch_alter_table("workflow") as batch_op:
-        batch_op.drop_column("source_metadata")
+    with op.batch_alter_table(table) as batch_op:
+        batch_op.drop_column(column)
