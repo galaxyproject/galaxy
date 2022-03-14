6bb01256644f86655221bda94ea04920ee382d2a
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2022-03-14 13:24:31 -0400

    Add source_metadata column to workflow table
    The context manager in the downgrade() function is a standard workaround
    for dealing with the limitations of SQLite's alter_table statement.
    
    Ref:
    https://alembic.sqlalchemy.org/en/latest/ops.html?highlight=batch_alter_table#alembic.operations.Operations.batch_alter_table
    https://alembic.sqlalchemy.org/en/latest/batch.html#batch-migrations

---
+++ lib/galaxy/model/migrations/alembic/versions_gxy/b182f655505f_add_workflow_source_metadata_column.py
@@ -0,0 +1,27 @@
+"""add workflow.source_metadata column
+
+Revision ID: b182f655505f
+Revises: e7b6dcb09efd
+Create Date: 2022-03-14 12:56:57.067748
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy import Column
+
+from galaxy.model.custom_types import JSONType
+
+# revision identifiers, used by Alembic.
+revision = "b182f655505f"
+down_revision = "e7b6dcb09efd"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("workflow", Column("source_metadata", JSONType))
+
+
+def downgrade():
+    with op.batch_alter_table("workflow") as batch_op:
+        batch_op.drop_column("source_metadata")
