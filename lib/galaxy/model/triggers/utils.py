63e613436db9954aa5770adfce8cffbfc91b7033
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2022-03-16 12:16:43 -0400

    Fix implicit exection and autocommit (both removed in 2.0)
    Ref:
    https://docs.sqlalchemy.org/en/14/changelog/migration_20.html#library-level-but-not-driver-level-autocommit-removed-from-both-core-and-ormo
    https://docs.sqlalchemy.org/en/14/changelog/migration_20.html#implicit-and-connectionless-execution-bound-metadata-removed
    
    Exposed by the following tests:
    
    test/unit/data/model/test_model_discovery.py::test_model_create_context_persist_hdas
    test/unit/data/model/test_model_discovery.py::test_model_create_context_persist_error_hda
    test/unit/data/model/test_model_discovery.py::test_persist_target_library_dataset
    test/unit/data/model/test_model_discovery.py::test_persist_target_library_folder
    test/unit/data/model/test_model_discovery.py::test_persist_target_hdca
      /home/sergey/0dev/galaxy/_galaxy/dev/lib/galaxy/model/triggers/utils.py:8: RemovedIn20Warning: The DDLElement.execute() method is considered legacy as of the 1.x series of SQLAlchemy and will be removed in 2.0. All statement execution in SQLAlchemy 2.0 is performed by the Connection.execute() method of Connection, or in the ORM by the Session.execute() method of Session. (Background on SQLAlchemy 2.0 at: https://sqlalche.me/e/b8d9)
        cmd.execute(bind=engine)
    
    test/unit/data/model/test_model_discovery.py::test_model_create_context_persist_hdas
    test/unit/data/model/test_model_discovery.py::test_model_create_context_persist_error_hda
    test/unit/data/model/test_model_discovery.py::test_persist_target_library_dataset
    test/unit/data/model/test_model_discovery.py::test_persist_target_library_folder
    test/unit/data/model/test_model_discovery.py::test_persist_target_hdca
      /home/sergey/0dev/galaxy/_galaxy/dev/lib/galaxy/model/triggers/utils.py:8: RemovedIn20Warning: The Engine.execute() method is considered legacy as of the 1.x series of SQLAlchemy and will be removed in 2.0. All statement execution in SQLAlchemy 2.0 is performed by the Connection.execute() method of Connection, or in the ORM by the Session.execute() method of Session. (Background on SQLAlchemy 2.0 at: https://sqlalche.me/e/b8d9)
        cmd.execute(bind=engine)

--- lib/galaxy/model/triggers/utils.py
+++ lib/galaxy/model/triggers/utils.py
@@ -3,6 +3,7 @@ from sqlalchemy import DDL
 
 def execute_statements(engine, raw_sql):
     statements = raw_sql if isinstance(raw_sql, list) else [raw_sql]
-    for sql in statements:
-        cmd = DDL(sql)
-        cmd.execute(bind=engine)
+    with engine.begin() as connection:
+        for sql in statements:
+            cmd = DDL(sql)
+            connection.execute(cmd)
