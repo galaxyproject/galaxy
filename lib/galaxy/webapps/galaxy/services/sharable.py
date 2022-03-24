040040c2fa189199b0858952b3f9dde4ba996002
mvdbeek <m.vandenbeek@gmail.com>
2022-03-24 10:15:09 +0100

    Fix ShareableService._get_user

--- lib/galaxy/webapps/galaxy/services/sharable.py
+++ lib/galaxy/webapps/galaxy/services/sharable.py
@@ -119,24 +119,18 @@ class ShareableService:
         send_to_users: Set[User] = set()
         send_to_err: Set[str] = set()
         for email_or_id in set(emails_or_ids):
-            email_or_id = email_or_id.strip()
-            if not email_or_id:
-                continue
-
             send_to_user = None
-            if "@" in email_or_id:
-                email_address = email_or_id
+            if isinstance(email_or_id, int):
+                send_to_user = self.manager.user_manager.by_id(email_or_id)
+                if send_to_user.deleted:
+                    send_to_user = None
+            else:
+                email_address = email_or_id.strip()
+                if not email_address:
+                    continue
                 send_to_user = self.manager.user_manager.by_email(
                     email_address, filters=[User.table.c.deleted == false()]
                 )
-            else:
-                try:
-                    decoded_user_id = trans.security.decode_id(email_or_id)
-                    send_to_user = self.manager.user_manager.by_id(decoded_user_id)
-                    if send_to_user.deleted:
-                        send_to_user = None
-                except exceptions.MalformedId:
-                    send_to_user = None
 
             if not send_to_user:
                 send_to_err.add(f"{email_or_id} is not a valid Galaxy user.")
