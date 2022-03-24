543940685295f1caa0d6e0a7d69e51e2cdba3051
mvdbeek <m.vandenbeek@gmail.com>
2022-03-24 13:59:55 +0100

    Drop Container and ContainerPort

--- lib/galaxy/containers/__init__.py
+++ lib/galaxy/containers/__init__.py
@@ -6,23 +6,12 @@ import logging
 from abc import (
     ABCMeta,
     abstractmethod,
-    abstractproperty,
 )
-from typing import NamedTuple
 
 DEFAULT_CONTAINER_TYPE = "docker"
 DEFAULT_CONF = {"_default_": {"type": DEFAULT_CONTAINER_TYPE}}
 
 log = logging.getLogger(__name__)
-
-
-class ContainerPort(NamedTuple):
-    """Named tuple representing ports published by a container, with attributes"""
-
-    port: int  # Port number (inside the container)
-    protocol: str  # Port protocol, either ``tcp`` or ``udp``
-    hostaddr: str  # Address or hostname where the published port can be accessed
-    hostport: int  # Published port number on which the container can be accessed
 
 
 class ContainerVolume(metaclass=ABCMeta):
@@ -51,97 +40,3 @@ class ContainerVolume(metaclass=ABCMeta):
     @property
     def mode_is_valid(self):
         return self.mode in self.valid_modes
-
-
-class Container(metaclass=ABCMeta):
-    def __init__(self, interface, id, name=None, **kwargs):
-        """
-
-        :param      interface:  Container interface for the given container type
-        :type       interface:  :class:`ContainerInterface` subclass instance
-        :param      id:         Container identifier
-        :type       id:         str
-        :param      name:       Container name
-        :type       name:       str
-
-        """
-        self._interface = interface
-        self._id = id
-        self._name = name
-
-    @property
-    def id(self):
-        """The container's id"""
-        return self._id
-
-    @property
-    def name(self):
-        """The container's name"""
-        return self._name
-
-    @abstractmethod
-    def from_id(cls, interface, id):
-        """Classmethod to create an instance of Container from the container system's id for the given container type.
-
-        :param  interface:  Container insterface for the given id
-        :type   interface:  :class:`ContainerInterface` subclass instance
-        :param  id:         Container identifier
-        :type   id:         str
-        :returns:           Container object
-        :rtype:             :class:`Container` subclass instance
-        """
-
-    @abstractproperty
-    def ports(self):
-        """Attribute for accessing details of ports published by the container.
-
-        :returns:   Port details
-        :rtype:     list of :class:`ContainerPort` s
-        """
-
-    @abstractproperty
-    def address(self):
-        """Attribute for accessing the address or hostname where published ports can be accessed.
-
-        :returns:   Hostname or IP address
-        :rtype:     str
-        """
-
-    @abstractmethod
-    def is_ready(self):
-        """Indicate whether or not the container is "ready" (up, available, running).
-
-        :returns:   True if ready, else False
-        :rtpe:      bool
-        """
-
-    def map_port(self, port):
-        """Map a given container port to a host address/port.
-
-        For legacy reasons, if port is ``None``, the first port (if any) will be returned
-
-        :param  port:   Container port to map
-        :type   port:   int
-        :returns:       Mapping to host address/port for given container port
-        :rtype:         :class:`ContainerPort` instance
-        """
-        mapping = None
-        ports = self.ports or []
-        for mapping in ports:
-            if port == mapping.port:
-                return mapping
-            if port is None:
-                log.warning(
-                    "Container %s (%s): Don't know how to map ports to containers with multiple exposed ports "
-                    "when a specific port is not requested. Arbitrarily choosing first: %s",
-                    self.name,
-                    self.id,
-                    mapping,
-                )
-                return mapping
-        else:
-            if port is None:
-                log.warning("Container %s (%s): No exposed ports found!", self.name, self.id)
-            else:
-                log.warning("Container %s (%s): No mapping found for port: %s", self.name, self.id, port)
-        return None
--- lib/galaxy/containers/docker_model.py
+++ lib/galaxy/containers/docker_model.py
@@ -5,11 +5,7 @@ Model objects for docker objects
 import logging
 import shlex
 
-from galaxy.containers import (
-    Container,
-    ContainerPort,
-    ContainerVolume,
-)
+from galaxy.containers import ContainerVolume
 
 log = logging.getLogger(__name__)
 
@@ -49,73 +45,3 @@ class DockerVolume(ContainerVolume):
             # e.g. $_GALAXY_JOB_TMP_DIR:$_GALAXY_JOB_TMP_DIR:rw so don't single quote.
             volume_for_cmd_line = f'"{volume_str}"'
         return volume_for_cmd_line
-
-
-class DockerContainer(Container):
-    def __init__(self, interface, id, name=None, inspect=None):
-        super().__init__(interface, id, name=name)
-        self._inspect = inspect
-
-    @classmethod
-    def from_id(cls, interface, id):
-        inspect = interface.inspect(id)
-        return cls(interface, id, name=inspect["Name"], inspect=inspect)
-
-    @property
-    def ports(self):
-        # {
-        #     "NetworkSettings" : {
-        #         "Ports" : {
-        #             "3306/tcp" : [
-        #                 {
-        #                     "HostIp" : "127.0.0.1",
-        #                     "HostPort" : "3306"
-        #                 }
-        #             ]
-        rval = []
-        try:
-            port_mappings = self.inspect["NetworkSettings"]["Ports"]
-        except KeyError:
-            log.warning(
-                "Failed to get ports for container %s from `docker inspect` output at "
-                "['NetworkSettings']['Ports']: %s: %s",
-                self.id,
-                exc_info=True,
-            )
-            return None
-        for port_name in port_mappings:
-            for binding in port_mappings[port_name]:
-                rval.append(
-                    ContainerPort(
-                        int(port_name.split("/")[0]),
-                        port_name.split("/")[1],
-                        self.address,
-                        int(binding["HostPort"]),
-                    )
-                )
-        return rval
-
-    @property
-    def address(self):
-        if self._interface.host and self._interface.host.startswith("tcp://"):
-            return self._interface.host.replace("tcp://", "").split(":", 1)[0]
-        else:
-            return "localhost"
-
-    def is_ready(self):
-        return self.inspect["State"]["Running"]
-
-    def __eq__(self, other):
-        return self._id == other.id
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __hash__(self):
-        return hash(self._id)
-
-    @property
-    def inspect(self):
-        if not self._inspect:
-            self._inspect = self._interface.inspect(self._id)
-        return self._inspect
