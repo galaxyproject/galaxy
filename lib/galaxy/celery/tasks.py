6b134cb7a29f56e92a55a0c3ed096f2a551bb6ca
mvdbeek <m.vandenbeek@gmail.com>
2022-03-28 18:53:06 +0200

    Create set_job_metadata celery task

--- lib/galaxy/celery/tasks.py
+++ lib/galaxy/celery/tasks.py
@@ -1,12 +1,15 @@
 from galaxy import model
 from galaxy.celery import galaxy_task
 from galaxy.config import GalaxyAppConfiguration
+from galaxy.datatypes.registry import Registry as DatatypesRegistry
 from galaxy.managers.collections import DatasetCollectionManager
 from galaxy.managers.hdas import HDAManager
 from galaxy.managers.lddas import LDDAManager
 from galaxy.managers.markdown_util import generate_branded_pdf
 from galaxy.managers.model_stores import ModelStoreManager
+from galaxy.metadata.set_metadata import set_metadata_portable
 from galaxy.model.scoped_session import galaxy_scoped_session
+from galaxy.objectstore import ObjectStore
 from galaxy.schema.tasks import (
     GenerateHistoryContentDownload,
     GenerateHistoryDownload,
@@ -52,6 +55,11 @@ def materialize(
 ):
     """Materialize datasets using HDAManager."""
     hda_manager.materialize(request)
+
+
+@galaxy_task(action="Set metadata for job")
+def set_job_metadata(tool_job_working_directory, datatypes_registry: DatatypesRegistry, object_store: ObjectStore):
+    set_metadata_portable(tool_job_working_directory, datatypes_registry=datatypes_registry, object_store=object_store)
 
 
 @galaxy_task(action="set dataset association metadata")
