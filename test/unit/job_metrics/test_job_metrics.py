a6199947cd15974ea5c98dade0078bd9994b5ba4
John Chilton <jmchilton@gmail.com>
2022-03-29 12:32:32 -0400

    More tests for job metrics.

--- test/unit/job_metrics/test_job_metrics.py
+++ test/unit/job_metrics/test_job_metrics.py
@@ -1,13 +1,44 @@
+from typing import (
+    Any,
+    Optional,
+)
+
 from galaxy.job_metrics import (
     formatting,
     JobMetrics,
 )
 
+TEST_JOBS_METRICS = JobMetrics()
 
-def test_job_metrics_load():
-    # Just construct the manager to make sure all the plugin classes load fine
-    # and package is configured properly.
-    JobMetrics()
+
+def test_job_metrics_format_env():
+    _assert_format(
+        "env",
+        "moo",
+        "cow",
+        assert_title="moo",
+        assert_value="cow",
+    )
+
+
+def test_job_metrics_format_core():
+    _assert_format(
+        "core",
+        "galaxy_slots",
+        "4",
+        assert_title="Cores Allocated",
+        assert_value="4",
+    )
+
+
+def test_job_metrics_uname():
+    _assert_format(
+        "uname",
+        "moo",
+        "Ubuntu Linux 21.10",
+        assert_title="Operating System",
+        assert_value="Ubuntu Linux 21.10",
+    )
 
 
 def test_job_metric_formatting():
@@ -30,3 +61,13 @@ def test_job_metric_formatting():
     assert formatting.seconds_to_str(7260) == "2 hours and 1 minute"
     assert formatting.seconds_to_str(7320) == "2 hours and 2 minutes"
     assert formatting.seconds_to_str(36181) == "10 hours and 3 minutes"
+
+
+def _assert_format(
+    plugin: str, key: str, value: Any, assert_title: Optional[str] = None, assert_value: Optional[str] = None
+):
+    result = TEST_JOBS_METRICS.format(plugin, key, value)
+    if assert_title is not None:
+        assert result[0] == assert_title
+    if assert_value is not None:
+        assert result[1] == assert_value
