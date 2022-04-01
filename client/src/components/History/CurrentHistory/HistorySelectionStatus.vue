c8582da59cf9e2f947d8df4625dd0a03e494597c
davelopez <46503462+davelopez@users.noreply.github.com>
2022-04-01 16:03:11 +0200

    Redesign `HistorySelectionStatus` template
    And simplify logic a bit

--- client/src/components/History/CurrentHistory/HistorySelectionStatus.vue
+++ client/src/components/History/CurrentHistory/HistorySelectionStatus.vue
@@ -1,19 +1,33 @@
 <template>
-    <div class="text-center">
-        <div v-if="!hasSelection">No items selected</div>
-        <div v-else>
-            <div v-if="isQuerySelection">
-                <div v-if="hasFilters">All {{ totalItemsInQuery }} items that match the filters are selected</div>
-                <div v-else>All {{ totalItemsInQuery }} active items in history are selected</div>
+    <div class="text-center p-0">
+        <div v-if="canSelectAll || hasSelection">
+            <b-button-group size="sm" class="mb-2">
+                <b-button v-if="canSelectAll" @click="selectAllItemsInQuery">Select All</b-button>
+                <b-button v-if="hasSelection" @click="clearSelection">Clear selection</b-button>
+            </b-button-group>
+        </div>
+
+        <b-alert show :variant="selectionAlertVariant" class="mb-0 p-2">
+            <div v-if="!hasSelection">No items selected</div>
+            <div v-else>
+                <div v-if="isQuerySelection">
+                    <div v-if="hasFilters">
+                        All <b>{{ totalItemsInQuery }}</b> items that match the filters are selected
+                    </div>
+                    <div v-else>
+                        All <b>{{ totalItemsInQuery }}</b> active items in history are selected
+                    </div>
+                </div>
+                <div v-else>
+                    <div v-if="hasFilters">
+                        <b>{{ selectionSize }}</b> of {{ totalItemsInQuery }} items that match the filters are selected
+                    </div>
+                    <div v-else>
+                        <b>{{ selectionSize }}</b> of {{ totalItemsInQuery }} active items in history are selected
+                    </div>
+                </div>
             </div>
-            <div v-else>{{ selectionSize }} items selected</div>
-        </div>
-        <div v-if="canSelectAll">
-            <b-link @click="onSelectAllItemsInQuery">
-                {{ selectAllOptionText }}
-            </b-link>
-        </div>
-        <b-link v-if="hasSelection" @click="clearSelection"> Clear selection </b-link>
+        </b-alert>
     </div>
 </template>
 
@@ -32,29 +46,20 @@ export default {
         },
         /** @returns {Boolean} */
         canSelectAll() {
-            return this.queryMatchesItems && !this.selectionMatchesQuery;
-        },
-        /** @returns {Boolean} */
-        selectAllOptionText() {
-            return this.hasFilters
-                ? `Select all ${this.totalItemsInQuery} items that match the current filters`
-                : `Select all ${this.totalItemsInQuery} active items in history`;
+            return !this.selectionMatchesQuery;
         },
         /** @returns {Boolean} */
         selectionMatchesQuery() {
-            if (this.isQuerySelection) {
-                return true;
-            }
-            return this.queryMatchesItems && this.totalItemsInQuery === this.selectionSize;
+            return this.totalItemsInQuery === this.selectionSize;
         },
-        /** @returns {Boolean} */
-        queryMatchesItems() {
-            return !isNaN(this.totalItemsInQuery);
+        /** @returns {String} */
+        selectionAlertVariant() {
+            return this.hasSelection ? "info" : "secondary";
         },
     },
     methods: {
-        onSelectAllItemsInQuery() {
-            this.$emit("select-all", this.totalItemsInQuery);
+        selectAllItemsInQuery() {
+            this.$emit("select-all");
         },
         clearSelection() {
             this.$emit("clear-selection");
