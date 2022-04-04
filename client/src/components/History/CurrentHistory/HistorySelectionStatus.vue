a01b475d5a1f92c16d0d3c139c2651fcf8f79994
davelopez <46503462+davelopez@users.noreply.github.com>
2022-04-04 17:31:10 +0200

    Add tests for HistorySelectionStatus component

--- client/src/components/History/CurrentHistory/HistorySelectionStatus.vue
+++ client/src/components/History/CurrentHistory/HistorySelectionStatus.vue
@@ -2,27 +2,31 @@
     <div class="text-center p-0">
         <div v-if="canSelectAll || hasSelection">
             <b-button-group size="sm" class="mb-2">
-                <b-button v-if="canSelectAll" @click="selectAllItemsInQuery">Select All</b-button>
-                <b-button v-if="hasSelection" @click="clearSelection">Clear selection</b-button>
+                <b-button v-if="canSelectAll" @click="selectAllItemsInQuery" data-test-id="select-all-btn">
+                    Select All
+                </b-button>
+                <b-button v-if="hasSelection" @click="clearSelection" data-test-id="clear-btn">
+                    Clear selection
+                </b-button>
             </b-button-group>
         </div>
 
         <b-alert show :variant="selectionAlertVariant" class="mb-0 p-2">
-            <div v-if="!hasSelection">No items selected</div>
+            <div v-if="!hasSelection" data-test-id="empty-selection">No items selected</div>
             <div v-else>
                 <div v-if="selectionMatchesQuery">
-                    <div v-if="hasFilters">
+                    <div v-if="hasFilters" data-test-id="all-filter-selected">
                         All <b>{{ totalItemsInQuery }}</b> items that match the filters are selected
                     </div>
-                    <div v-else>
+                    <div v-else data-test-id="all-active-selected">
                         All <b>{{ totalItemsInQuery }}</b> active items in history are selected
                     </div>
                 </div>
                 <div v-else>
-                    <div v-if="hasFilters">
+                    <div v-if="hasFilters" data-test-id="num-filter-selected">
                         <b>{{ selectionSize }}</b> of {{ totalItemsInQuery }} items that match the filters are selected
                     </div>
-                    <div v-else>
+                    <div v-else data-test-id="num-active-selected">
                         <b>{{ selectionSize }}</b> of {{ totalItemsInQuery }} active items in history are selected
                     </div>
                 </div>
@@ -32,6 +36,11 @@
 </template>
 
 <script>
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+
+Vue.use(BootstrapVue);
+
 export default {
     props: {
         hasFilters: { type: Boolean, required: true },
