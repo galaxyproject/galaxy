04ed93d81fc01fe8718559022b8c4068f9d95ceb
Dannon Baker <dannon.baker@gmail.com>
2022-04-05 08:58:18 -0400

    Make sure config actually exists.  Exits silently if nothing found or no matches.

--- scripts/config_parse.py
+++ scripts/config_parse.py
@@ -11,15 +11,16 @@ from galaxy.config import GalaxyAppConfiguration
 def main(config, setting):
     # Use explicit config, then env, then guess.
     config = config or os.environ.get("GALAXY_CONFIG_FILE", "config/galaxy.yml")
-    gx_config = GalaxyAppConfiguration(**yaml.safe_load(open(config))['galaxy'])
-    if setting:
-        print(gx_config.get(setting))
-    else:
-        pprint.pprint(gx_config.config_dict)
+    if config and os.path.exists(config):
+        gx_config = GalaxyAppConfiguration(**yaml.safe_load(open(config))["galaxy"])
+        if setting:
+            print(gx_config.get(setting))
+        else:
+            pprint.pprint(gx_config.config_dict)
 
 
 if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser()
+    arg_parser = argparse.ArgumentParser(description="Fetch values from Galaxy config, or print all set values.")
     # add optional 'setting' argument
     arg_parser.add_argument("-s", "--setting", default=None, help="setting")
     arg_parser.add_argument(
