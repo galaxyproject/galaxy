0015ea9bb10559b04c1ec34920c961174f41cf10
Priyansh Singh <63330165+ps-19@users.noreply.github.com>
2022-04-10 09:39:54 +0530

    Update modal_tests.js

--- client/tests/qunit/tests/modal_tests.js
+++ client/tests/qunit/tests/modal_tests.js
@@ -27,21 +27,21 @@ QUnit.module("Modal dialog test", {
 });
 
 QUnit.test("test dialog attributes", function (assert) {
-    assert.ok(this.app.$header.find(".title").html() === "Test title", "Modal header has correct title.");
-    assert.ok(this.app.$body.html() === "Test body", "Modal header has correct body.");
+    assert.ok(this.app.$header.find(".title").html() == "Test title", "Modal header has correct title.");
+    assert.ok(this.app.$body.html() == "Test body", "Modal header has correct body.");
 });
 
 QUnit.test("test dialog visibility", function (assert) {
     this.app.hide();
-    assert.ok(this.app.$el.css("display") === "none", "Modal hidden manually");
+    assert.ok(this.app.$el.css("display") == "none", "Modal hidden manually");
     this.app.show();
-    assert.ok(this.app.$el.css("display") === "block", "Modal shown manually");
+    assert.ok(this.app.$el.css("display") == "block", "Modal shown manually");
     this.app.getButton("Ok").trigger("click");
-    assert.ok(this.app.$el.css("display") === "block", "Modal still visible after clicking Ok");
+    assert.ok(this.app.$el.css("display") == "block", "Modal still visible after clicking Ok");
     this.app.getButton("Cancel").trigger("click");
-    assert.ok(this.app.$el.css("display") === "none", "Modal hidden after clicking Cancel");
+    assert.ok(this.app.$el.css("display") == "none", "Modal hidden after clicking Cancel");
     this.app.show();
-    assert.ok(this.app.$el.css("display") === "block", "Modal manually shown again");
+    assert.ok(this.app.$el.css("display") == "block", "Modal manually shown again");
     assert.ok(!this.app.$header.hasClass("no-separator"), "Title separator tagged as visible.");
     this.app.show({ title_separator: false });
     assert.ok(this.app.$header.hasClass("no-separator"), "Title separator tagged as hidden.");
@@ -53,25 +53,25 @@ QUnit.test("test dialog visibility", function (assert) {
 QUnit.test("test dialog closing events", function (assert) {
     this.app.show();
     this.app.$backdrop.trigger("click");
-    assert.ok(this.app.$el.css("display") === "block", "Modal shown after backdrop click");
+    assert.ok(this.app.$el.css("display") == "block", "Modal shown after backdrop click");
     this.app.hide();
     this.app.show({ closing_events: true });
-    assert.ok(this.app.$el.css("display") === "block", "Modal shown with closing events");
+    assert.ok(this.app.$el.css("display") == "block", "Modal shown with closing events");
     this.app.$backdrop.trigger("click");
-    assert.ok(this.app.$el.css("display") === "none", "Modal hidden after backdrop click");
+    assert.ok(this.app.$el.css("display") == "none", "Modal hidden after backdrop click");
 });
 
 QUnit.test("test dialog rendering", function (assert) {
     var before = this.app.$el.html();
     this.app.render();
-    assert.ok(before === this.app.$el.html(), "Re-rendering successful");
+    assert.ok(before == this.app.$el.html(), "Re-rendering successful");
     this.app.options.title = "New Title";
     this.app.render();
-    assert.ok(this.app.$header.find(".title").html() === "New Title", "Modal header has correct new title.");
+    assert.ok(this.app.$header.find(".title").html() == "New Title", "Modal header has correct new title.");
 });
 
 QUnit.test("test button states", function (assert) {
-    assert.ok(this.app.getButton("Ok").html() === "Ok", "Ok has correct label");
+    assert.ok(this.app.getButton("Ok").html() == "Ok", "Ok has correct label");
     assert.ok(!this.app.getButton("Ok").prop("disabled"), "Ok is active");
     assert.ok(!this.app.getButton("Cancel").prop("disabled"), "Cancel is active");
     this.app.disableButton("Ok");
