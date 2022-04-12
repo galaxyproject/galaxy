dd055099cdca88ffc560b9e86eac3d2935913c9f
David López <46503462+davelopez@users.noreply.github.com>
2022-04-12 18:55:40 +0200

    Add tool-tip to reset selection button

--- client/src/components/History/CurrentHistory/HistoryOperations/SelectionStatus.vue
+++ client/src/components/History/CurrentHistory/HistoryOperations/SelectionStatus.vue
@@ -1,7 +1,7 @@
 <template>
     <b-button-group size="sm">
         <b-button v-if="hasSelection" variant="link" @click="resetSelection" data-test-id="clear-btn">
-            <span class="fa fa-fw fa-times" />
+            <span class="fa fa-fw fa-times" title="Clear selection" />
         </b-button>
         <b-button v-else variant="link" @click="selectAll" data-test-id="select-all-btn">
             <span>Select All</span>
