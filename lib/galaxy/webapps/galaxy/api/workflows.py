139fd44346cb384fa41a9bceb750fcd3ff1e45e8
John Chilton <jmchilton@gmail.com>
2022-04-14 16:18:52 -0400

    Fix to published workflow behavior.
    Previously untested and hence broken in #13698.

--- lib/galaxy/webapps/galaxy/api/workflows.py
+++ lib/galaxy/webapps/galaxy/api/workflows.py
@@ -190,7 +190,7 @@ class WorkflowsAPIController(BaseGalaxyAPIController, UsesStoredWorkflowMixin, U
         :param  missing_tools:       if True, include a list of missing tools per workflow
         :type   missing_tools:       boolean
         """
-        show_published = util.string_as_bool(show_published)
+        show_published = util.string_as_bool_or_none(show_published)
         show_hidden = util.string_as_bool(show_hidden)
         show_deleted = util.string_as_bool(show_deleted)
         missing_tools = util.string_as_bool(missing_tools)
