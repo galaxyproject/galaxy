9f7bad6f266926da9bebf556a56fc1e918cd0ef2
John Chilton <jmchilton@gmail.com>
2022-04-15 12:00:58 -0400

    Allow reuse of pagination parameter warping logic from InvocationsProvider.

--- client/src/components/providers/InvocationsProvider.js
+++ client/src/components/providers/InvocationsProvider.js
@@ -1,21 +1,9 @@
 import axios from "axios";
-import { snakeCase } from "snake-case";
+import { cleanPaginationParameters} from "./utils";
 
 export function invocationsProvider(ctx, callback, extraParams) {
     const { apiUrl, ...requestParams } = ctx;
-    const cleanParams = {};
-    Object.entries(requestParams).map(([key, val]) => {
-        if (key === "perPage") {
-            key = "limit";
-        }
-        if (val) {
-            cleanParams[snakeCase(key)] = val;
-        }
-    });
-    if (cleanParams.current_page && cleanParams.limit) {
-        cleanParams.offset = (cleanParams.current_page - 1) * cleanParams.limit;
-        delete cleanParams.current_page;
-    }
+    const cleanParams = cleanPaginationParameters(requestParams);
     const promise = axios.get(apiUrl, { params: { ...cleanParams, ...extraParams } });
 
     // Must return a promise that resolves to an array of items
--- client/src/components/providers/utils.js
+++ client/src/components/providers/utils.js
@@ -1,5 +1,25 @@
 import JOB_STATES_MODEL from "mvc/history/job-states-model";
+import { snakeCase } from "snake-case";
 
 export function stateIsTerminal(result) {
     return !JOB_STATES_MODEL.NON_TERMINAL_STATES.includes(result.state);
+}
+
+// Adapt bootstrap parameters to Galaxy API. Galaxy consumes snake case parameters
+// and generally uses limit instead of perPage/per_page as a name for this concept.
+export function cleanPaginationParameters(requestParams) {
+    const cleanParams = {};
+    Object.entries(requestParams).map(([key, val]) => {
+        if (key === "perPage") {
+            key = "limit";
+        }
+        if (val) {
+            cleanParams[snakeCase(key)] = val;
+        }
+    });
+    if (cleanParams.current_page && cleanParams.limit) {
+        cleanParams.offset = (cleanParams.current_page - 1) * cleanParams.limit;
+        delete cleanParams.current_page;
+    }
+    return cleanParams;
 }
