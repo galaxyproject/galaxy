469bdea2efe6f8b5a0697948176f23d624e61063
Dannon Baker <dannon.baker@gmail.com>
2022-04-22 11:07:58 -0400

    Use actual galaxy format timestamp in test

--- client/src/components/UtcDate.test.js
+++ client/src/components/UtcDate.test.js
@@ -7,14 +7,14 @@ describe("UTCDate component", () => {
 
     it("Loads a date in default mode, computeds work", () => {
         const wrapper = shallowMount(UtcDate, {
-            propsData: { date: "2015-10-21 16:29:00.000" },
+            propsData: { date: "2015-10-21T16:29:00.000000" },
             localVue,
         });
         // expect elapsed time to be years ago.
         expect(wrapper.text()).toBe("2015-10-21T16:29:00Z");
         expect(wrapper.vm.elapsedTime).toContain("years ago");
         expect(wrapper.vm.fullDate).toBe("2015-10-21T16:29:00Z");
-        expect(wrapper.vm.formattedDate).toBe("2015-10-21 16:29:00.000");
+        expect(wrapper.vm.formattedDate).toBe("2015-10-21T16:29:00.000000");
         expect(wrapper.vm.pretty).toBe("Wednesday Oct 21st 4:29:00 2015 UTC");
     });
 });
