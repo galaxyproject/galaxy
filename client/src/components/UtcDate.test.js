43fb1340a20b9a10c08de023307dc4b93cc6bdd1
Dannon Baker <dannon.baker@gmail.com>
2022-04-22 18:28:51 -0400

    Swap UtcDate component to use date-fns instead of moment.

--- client/src/components/UtcDate.test.js
+++ client/src/components/UtcDate.test.js
@@ -5,16 +5,16 @@ import UtcDate from "./UtcDate.vue";
 describe("UTCDate component", () => {
     const localVue = getLocalVue();
 
-    it("Loads a date in default mode, computeds work", () => {
+    it("Loads a date in default mode, can format outputs as expected.", () => {
         const wrapper = shallowMount(UtcDate, {
             propsData: { date: "2015-10-21T16:29:00.000000" },
             localVue,
         });
-        // expect elapsed time to be years ago.
-        expect(wrapper.text()).toBe("2015-10-21T16:29:00Z");
+        // expect elapsed time to be 'years ago.
+        expect(wrapper.text()).toBe("2015-10-21T16:29:00.000Z");
         expect(wrapper.vm.elapsedTime).toContain("years ago");
-        expect(wrapper.vm.fullDate).toBe("2015-10-21T16:29:00Z");
-        expect(wrapper.vm.formattedDate).toBe("2015-10-21T16:29:00.000000");
-        expect(wrapper.vm.pretty).toBe("Wednesday Oct 21st 4:29:00 2015 UTC");
+        expect(wrapper.vm.fullISO).toBe("2015-10-21T16:29:00.000Z");
+        expect(wrapper.vm.parsedDate).toEqual(new Date("2015-10-21T16:29:00.000Z"));
+        expect(wrapper.vm.pretty).toBe("Wednesday Oct 21st 16:29:00 2015 UTC");
     });
 });
