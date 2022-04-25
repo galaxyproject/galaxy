19a662b84c5932884ae5e72ecee3a5d67fa98891
John Chilton <jmchilton@gmail.com>
2022-04-25 15:04:39 -0400

    Selenium test case for workflow search by name filters.

--- lib/galaxy_test/selenium/test_workflow_management.py
+++ lib/galaxy_test/selenium/test_workflow_management.py
@@ -100,6 +100,32 @@ class WorkflowManagementTestCase(SeleniumTestCase, TestsGalaxyPagers):
         self._assert_showing_n_workflows(1)
 
     @selenium_test
+    def test_index_search_filters(self):
+        self.workflow_index_open()
+        self._workflow_import_from_url()
+        self.workflow_index_rename("searchforthis")
+        self._assert_showing_n_workflows(1)
+
+        self.workflow_index_search_for("name:doesnotmatch")
+        self._assert_showing_n_workflows(0)
+        self.screenshot("workflow_manage_search_no_matches")
+
+        self.workflow_index_search_for()
+        self._assert_showing_n_workflows(1)
+
+        self.workflow_index_search_for("name:searchforthis")
+        self._assert_showing_n_workflows(1)
+        self.screenshot("workflow_manage_search_name_filter")
+
+        self.workflow_index_search_for("n:searchforthis")
+        self._assert_showing_n_workflows(1)
+        self.screenshot("workflow_manage_search_name_alias")
+
+        self.workflow_index_search_for("n:doesnotmatch")
+        self._assert_showing_n_workflows(0)
+        self.screenshot("workflow_manage_search_name_alias")
+
+    @selenium_test
     def test_pagination(self):
         self.workflow_index_open()
         self._workflow_import_from_url()
