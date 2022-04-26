efb16ce91167f37751083bc7351ccd6df087b4a5
John Chilton <jmchilton@gmail.com>
2022-04-26 11:53:11 -0400

    Restructure Invocation.test.js to de-duplicate setup.

--- client/src/components/Workflow/Invocations.test.js
+++ client/src/components/Workflow/Invocations.test.js
@@ -8,146 +8,132 @@ import { parseISO, formatDistanceToNow } from "date-fns";
 
 const localVue = getLocalVue();
 
-describe("Invocations.vue without invocations", () => {
+describe("Invocations.vue", () => {
     let axiosMock;
     let wrapper;
-    let propsData;
 
     beforeEach(async () => {
         axiosMock = new MockAdapter(axios);
-        axiosMock.onAny().reply(200, [], { total_matches: "0" });
-        propsData = {
-            ownerGrid: false,
-        };
-        wrapper = mount(Invocations, {
-            propsData,
-            localVue,
-        });
     });
 
     afterEach(() => {
         axiosMock.restore();
     });
 
-    it("title should be shown", async () => {
-        expect(wrapper.find("#invocations-title").text()).toBe("Workflow Invocations");
-    });
+    describe(" with empty invocation list", () => {
+        beforeEach(async () => {
+            axiosMock.onAny().reply(200, [], { total_matches: "0" });
+            const propsData = {
+                ownerGrid: false,
+            };
+            wrapper = mount(Invocations, {
+                propsData,
+                localVue,
+            });
+        });
 
-    it("no invocations message should be shown when not loading", async () => {
-        expect(wrapper.find("#no-invocations").exists()).toBe(true);
-    });
-});
+        it("title should be shown", async () => {
+            expect(wrapper.find("#invocations-title").text()).toBe("Workflow Invocations");
+        });
 
-describe("Invocations.vue for a workflow invocations", () => {
-    let axiosMock;
-    let wrapper;
-    let propsData;
-
-    beforeEach(async () => {
-        axiosMock = new MockAdapter(axios);
-        axiosMock.onAny().reply(200, [], { total_matches: "0" });
-        propsData = {
-            ownerGrid: false,
-            storedWorkflowName: "My Workflow",
-            storedWorkflowId: "abcde145678",
-        };
-        wrapper = mount(Invocations, {
-            propsData,
-            localVue,
+        it("no invocations message should be shown when not loading", async () => {
+            expect(wrapper.find("#no-invocations").exists()).toBe(true);
         });
     });
 
-    afterEach(() => {
-        axiosMock.restore();
-    });
+    describe("for a workflow with an empty invocation list", () => {
+        beforeEach(async () => {
+            axiosMock.onAny().reply(200, [], { total_matches: "0" });
+            const propsData = {
+                ownerGrid: false,
+                storedWorkflowName: "My Workflow",
+                storedWorkflowId: "abcde145678",
+            };
+            wrapper = mount(Invocations, {
+                propsData,
+                localVue,
+            });
+        });
 
-    it("title should be shown", async () => {
-        expect(wrapper.find("#invocations-title").text()).toBe("Workflow Invocations for My Workflow");
-    });
+        it("title should be shown", async () => {
+            expect(wrapper.find("#invocations-title").text()).toBe("Workflow Invocations for My Workflow");
+        });
 
-    it("no invocations message should be shown when not loading", async () => {
-        expect(wrapper.find("#no-invocations").exists()).toBe(true);
-    });
-});
-
-describe("Invocations.vue with invocation", () => {
-    let wrapper;
-    let propsData;
-    let axiosMock;
-
-    beforeEach(async () => {
-        axiosMock = new MockAdapter(axios);
-        axiosMock
-            .onGet("/api/invocations", { params: { limit: 50, offset: 0, include_terminal: false } })
-            .reply(200, [mockInvocationData], { total_matches: "1" });
-        propsData = {
-            ownerGrid: false,
-            loading: false,
-        };
-        wrapper = mount(Invocations, {
-            propsData,
-            computed: {
-                getWorkflowNameByInstanceId: (state) => (id) => "workflow name",
-                getWorkflowByInstanceId: (state) => (id) => {
-                    return { id: "workflowId" };
-                },
-                getHistoryById: (state) => (id) => {
-                    return { id: "historyId" };
-                },
-                getHistoryNameById: () => () => "history name",
-            },
-            stubs: {
-                "workflow-invocation-state": {
-                    template: "<span/>",
-                },
-            },
-            localVue,
+        it("no invocations message should be shown when not loading", async () => {
+            expect(wrapper.find("#no-invocations").exists()).toBe(true);
         });
     });
 
-    afterEach(() => {
-        axiosMock.reset();
-    });
+    describe("with invocation", () => {
+        beforeEach(async () => {
+            axiosMock
+                .onGet("/api/invocations", { params: { limit: 50, offset: 0, include_terminal: false } })
+                .reply(200, [mockInvocationData], { total_matches: "1" });
+            const propsData = {
+                ownerGrid: false,
+                loading: false,
+            };
+            wrapper = mount(Invocations, {
+                propsData,
+                computed: {
+                    getWorkflowNameByInstanceId: (state) => (id) => "workflow name",
+                    getWorkflowByInstanceId: (state) => (id) => {
+                        return { id: "workflowId" };
+                    },
+                    getHistoryById: (state) => (id) => {
+                        return { id: "historyId" };
+                    },
+                    getHistoryNameById: () => () => "history name",
+                },
+                stubs: {
+                    "workflow-invocation-state": {
+                        template: "<span/>",
+                    },
+                },
+                localVue,
+            });
+        });
 
-    it("renders one row", async () => {
-        const rows = wrapper.findAll("tbody > tr").wrappers;
-        expect(rows.length).toBe(1);
-        const row = rows[0];
-        const columns = row.findAll("td");
-        expect(columns.at(1).text()).toBe("workflow name");
-        expect(columns.at(2).text()).toBe("history name");
-        expect(columns.at(3).text()).toBe(
-            formatDistanceToNow(parseISO(`${mockInvocationData.create_time}Z`), { addSuffix: true })
-        );
-        expect(columns.at(4).text()).toBe(
-            formatDistanceToNow(parseISO(`${mockInvocationData.update_time}Z`), { addSuffix: true })
-        );
-        expect(columns.at(5).text()).toBe("scheduled");
-        expect(columns.at(6).text()).toBe("");
-    });
+        it("renders one row", async () => {
+            const rows = wrapper.findAll("tbody > tr").wrappers;
+            expect(rows.length).toBe(1);
+            const row = rows[0];
+            const columns = row.findAll("td");
+            expect(columns.at(1).text()).toBe("workflow name");
+            expect(columns.at(2).text()).toBe("history name");
+            expect(columns.at(3).text()).toBe(
+                formatDistanceToNow(parseISO(`${mockInvocationData.create_time}Z`), { addSuffix: true })
+            );
+            expect(columns.at(4).text()).toBe(
+                formatDistanceToNow(parseISO(`${mockInvocationData.update_time}Z`), { addSuffix: true })
+            );
+            expect(columns.at(5).text()).toBe("scheduled");
+            expect(columns.at(6).text()).toBe("");
+        });
 
-    it("toggles detail rendering", async () => {
-        let rows = wrapper.findAll("tbody > tr").wrappers;
-        expect(rows.length).toBe(1);
-        await wrapper.find(".toggle-invocation-details").trigger("click");
-        rows = wrapper.findAll("tbody > tr").wrappers;
-        expect(rows.length).toBe(3);
-        await wrapper.find(".toggle-invocation-details").trigger("click");
-        rows = wrapper.findAll("tbody > tr").wrappers;
-        expect(rows.length).toBe(1);
-    });
+        it("toggles detail rendering", async () => {
+            let rows = wrapper.findAll("tbody > tr").wrappers;
+            expect(rows.length).toBe(1);
+            await wrapper.find(".toggle-invocation-details").trigger("click");
+            rows = wrapper.findAll("tbody > tr").wrappers;
+            expect(rows.length).toBe(3);
+            await wrapper.find(".toggle-invocation-details").trigger("click");
+            rows = wrapper.findAll("tbody > tr").wrappers;
+            expect(rows.length).toBe(1);
+        });
 
-    it("calls switchHistory", async () => {
-        const mockMethod = jest.fn();
-        wrapper.vm.switchHistory = mockMethod;
-        await wrapper.find("#switch-to-history").trigger("click");
-        expect(mockMethod).toHaveBeenCalled();
-    });
+        it("calls switchHistory", async () => {
+            const mockMethod = jest.fn();
+            wrapper.vm.switchHistory = mockMethod;
+            await wrapper.find("#switch-to-history").trigger("click");
+            expect(mockMethod).toHaveBeenCalled();
+        });
 
-    it("calls executeWorkflow", async () => {
-        const mockMethod = jest.fn();
-        wrapper.vm.executeWorkflow = mockMethod;
-        await wrapper.find("#run-workflow").trigger("click");
-        expect(mockMethod).toHaveBeenCalledWith("workflowId");
+        it("calls executeWorkflow", async () => {
+            const mockMethod = jest.fn();
+            wrapper.vm.executeWorkflow = mockMethod;
+            await wrapper.find("#run-workflow").trigger("click");
+            expect(mockMethod).toHaveBeenCalledWith("workflowId");
+        });
     });
 });
