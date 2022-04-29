72623271946787a7a90d1d85e1fa12407580eb13
John Chilton <jmchilton@gmail.com>
2022-04-29 09:05:15 -0400

    Use a Invocations-style provider for Jobs grid.

--- client/src/components/providers/JobProvider.js
+++ client/src/components/providers/JobProvider.js
@@ -3,6 +3,7 @@ import { getAppRoot } from "onload/loadConfig";
 import { SingleQueryProvider } from "components/providers/SingleQueryProvider";
 import { rethrowSimple } from "utils/simple-error";
 import { stateIsTerminal } from "./utils";
+import { cleanPaginationParameters } from "./utils";
 
 async function jobDetails({ jobid }) {
     const url = `${getAppRoot()}api/jobs/${jobid}?full=True`;
@@ -26,3 +27,18 @@ async function jobProblems({ jobid }) {
 
 export const JobDetailsProvider = SingleQueryProvider(jobDetails, stateIsTerminal);
 export const JobProblemProvider = SingleQueryProvider(jobProblems, stateIsTerminal);
+
+export function jobsProvider(ctx, callback, extraParams = {}) {
+    const { apiUrl, ...requestParams } = ctx;
+    const cleanParams = cleanPaginationParameters(requestParams);
+    const promise = axios.get(apiUrl, { params: { ...cleanParams, ...extraParams } });
+
+    // Must return a promise that resolves to an array of items
+    return promise.then((data) => {
+        // Pluck the array of items off our axios response
+        const items = data.data;
+        callback && callback(data);
+        // Must return an array of items or an empty array if an error occurred
+        return items || [];
+    });
+}
