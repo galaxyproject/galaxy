0843ce2d1addf5a46b9f2a5a0edfb1a2ba2f0b78
guerler <aysam.guerler@gmail.com>
2022-05-06 14:58:55 -0400

    Revise user histories providers test

--- client/src/store/historyStore/historyStore.js
+++ client/src/store/historyStore/historyStore.js
@@ -84,12 +84,12 @@ const promises = {
 const actions = {
     async copyHistory({ dispatch }, { history, name, copyAll }) {
         const newHistory = await cloneHistory(history, name, copyAll);
-        dispatch("selectHistory", newHistory);
+        await dispatch("selectHistory", newHistory);
     },
     async createNewHistory({ dispatch }) {
         // create history, then select it as current at the same time
         const newHistory = await createNewHistory();
-        dispatch("selectHistory", newHistory);
+        await dispatch("selectHistory", newHistory);
     },
     async deleteHistory({ dispatch, commit, getters }, { history, purge = false }) {
         const deletedHistory = await deleteHistoryById(history.id, purge);
