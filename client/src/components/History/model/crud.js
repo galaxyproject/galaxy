205248ae6a8c839608a8b313cb5c5601faa3fe34
guerler <aysam.guerler@gmail.com>
2022-05-08 20:28:15 -0400

    Remove remaining unused async declaration

--- client/src/components/History/model/crud.js
+++ client/src/components/History/model/crud.js
@@ -4,37 +4,37 @@ import { bulkUpdate } from "./queries";
  * Content operations
  */
 
-export async function hideSelectedContent(history, filters, items) {
+export function hideSelectedContent(history, filters, items) {
     return bulkUpdate(history, "hide", filters, items);
 }
 
-export async function unhideSelectedContent(history, filters, items) {
+export function unhideSelectedContent(history, filters, items) {
     return bulkUpdate(history, "unhide", filters, items);
 }
 
-export async function deleteSelectedContent(history, filters, items) {
+export function deleteSelectedContent(history, filters, items) {
     return bulkUpdate(history, "delete", filters, items);
 }
 
-export async function undeleteSelectedContent(history, filters, items) {
+export function undeleteSelectedContent(history, filters, items) {
     return bulkUpdate(history, "undelete", filters, items);
 }
 
-export async function purgeSelectedContent(history, filters, items) {
+export function purgeSelectedContent(history, filters, items) {
     return bulkUpdate(history, "purge", filters, items);
 }
 
-export async function unhideAllHiddenContent(history) {
+export function unhideAllHiddenContent(history) {
     const filters = { visible: false };
     return unhideSelectedContent(history, filters);
 }
 
-export async function deleteAllHiddenContent(history) {
+export function deleteAllHiddenContent(history) {
     const filters = { deleted: false, visible: false };
     return deleteSelectedContent(history, filters);
 }
 
-export async function purgeAllDeletedContent(history) {
+export function purgeAllDeletedContent(history) {
     const filters = { deleted: true };
     return purgeSelectedContent(history, filters);
 }
