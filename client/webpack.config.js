5521dc79a4028d81383e6bebbc656f2317082c4b
Dannon Baker <dannon.baker@gmail.com>
2022-05-12 11:10:13 -0400

    Allow synthetic default imports

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -90,9 +90,9 @@ module.exports = (env = {}, argv = {}) => {
                 //     test: /\.ts$/,
                 //     // exclude: /node_modules|vue\/src/,
                 //     loader: "ts-loader",
-                //     // options: {
-                //     //     appendTsSuffixTo: [/\.vue$/],
-                //     // },
+                //     options: {
+                //         appendTsSuffixTo: [/\.vue$/],
+                //     },
                 // },
                 {
                     test: /\.vue$/,
