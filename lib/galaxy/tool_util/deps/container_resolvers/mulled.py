ad2f6c56acbe0b8345bfe7d907bf852de7f4c1d2
pvanheus <pvh@sanbi.ac.za>
2022-05-20 08:21:52 +0200

    ensure that Singularity doesn't run with HOME=/root
    When Galaxy is run in Docker, it starts uwsgi and passes the `--uid` and `--gid` flags to make the daemon process drop privileges. This leaves the HOME environment variable unchanged, so typically HOME=/root. In this case, when `singularity build` runs, it looks in for the `$HOME/.docker/config.json` file which fails if HOME is inaccessible. The result is that the container does not get built and cached and rather gets rebuilt every time a tool runs.
    
    This PR addresses that case by looking for an inaccessible HOME directory and choosing an alternative - either the value in GALAXY_HOME or /tmp.

--- lib/galaxy/tool_util/deps/container_resolvers/mulled.py
+++ lib/galaxy/tool_util/deps/container_resolvers/mulled.py
@@ -633,6 +633,14 @@ class MulledSingularityContainerResolver(SingularityCliContainerResolver, Mulled
             cmds = container.build_mulled_singularity_pull_command(
                 cache_directory=self.cache_directory.path, namespace=self.namespace
             )
+            # when Galaxy runs in Docker with uswgi, HOME remains set to /root, this is a workaround for that case
+            homedir = os.environ.get('HOME')
+            if homedir is not None and not (base) ubuntu@composer2:~/docker-galaxy-stable/compose$ _OK)):
+                galaxy_homedir = os.environ.get('GALAXY_HOME')
+                if galaxy_homedir is not None:
+                    homedir = galaxy_homedir
+                else:
+                    homedir = '/tmp'
             shell(cmds=cmds)
             self.cache_directory.invalidate_cache()
 
