7d3063bd17c13a0eed0e93b805c66cbf42fb49b1
Michelle Savage <michelle@Michelles-MBP.fios-router.home>
2022-05-23 09:22:20 -0400

    secondary test removal

--- client/src/components/Workflow/Editor/Attributes.test.js
+++ client/src/components/Workflow/Editor/Attributes.test.js
@@ -7,7 +7,7 @@ jest.mock("app");
 
 const TEST_ANNOTATION = "my cool annotation";
 const TEST_NAME = "workflow_name";
-const TEST_VERSIONS = [  //TODO (high) does it make sense to pre-fill this, if this is coming from prior user actions?
+const TEST_VERSIONS = [
     { versions: 0, update_time: "2022-01-01", steps: 10 },
     { versions: 1, update_time: "2022-01-02", steps: 20 },
 ];
@@ -34,27 +34,16 @@ describe("Attributes", () => {
         });
         expect(wrapper.find(`[itemprop='description']`).attributes("content")).toBe(TEST_ANNOTATION);
         expect(wrapper.find(`[itemprop='name']`).attributes("content")).toBe(TEST_NAME);
-        expect(wrapper.find(`#workflow-version-area > select`).exists()).toBeTruthy(); //TODO (low) does an `itemprop` need to be created in Attributes.vue (and why)?
+        expect(wrapper.find(`#workflow-version-area > select`).exists()).toBeTruthy();
 
         const name = wrapper.find("#workflow-name");
         expect(name.element.value).toBe(TEST_NAME);
         await wrapper.setProps({ name: "new_workflow_name" });
         expect(name.element.value).toBe("new_workflow_name");
-
-        const version = wrapper.findAll("#workflow-version-area > select > option");
-        console.log('version', version);
-        for (const v of version) {
-            console.log('v', v);
-            console.log('v.label', v.label);
-            const versionDate = (v.label).substring(versionLabel.indexOf(":") + 1, (v.label).indexOf(",")).trim();
-            expect(isDate(new Date(versionDate))).toBe(true);
-        }
-
-        const parameters = wrapper.findAll(".list-group-item"); //TODO (nit) not part of my fix, just curious: is this supposed to this exist in the code?
+        const parameters = wrapper.findAll(".list-group-item");
         expect(parameters.length).toBe(2);
         expect(parameters.at(0).text()).toBe("1: workflow_parameter_0");
         expect(parameters.at(1).text()).toBe("2: workflow_parameter_1");
-        console.log(wrapper.html()); //TODO (low) we don't need this, right?
         expect(wrapper.find("#workflow-annotation").element.value).toBe(TEST_ANNOTATION);
     });
 });
