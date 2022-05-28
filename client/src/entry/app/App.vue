1aa173b0e1c7d825afb15c59c3169b30749980df
guerler <aysam.guerler@gmail.com>
2022-05-27 22:35:12 -0400

    Add masthead to app entry

--- client/src/entry/app/App.vue
+++ client/src/entry/app/App.vue
@@ -2,7 +2,16 @@
     <div>
         <div id="everything">
             <div id="background"/>
-            <div id="masthead"/>
+            <div id="masthead">
+                <Masthead
+                    :mastheadState="getState()"
+                    :displayGalaxyBrand="options.display_galaxy_brand"
+                    :brand="options.brand"
+                    :brandLink="staticUrlToPrefixed(options.logo_url)"
+                    :brandImage="staticUrlToPrefixed(options.logo_src)"
+                    :brandImageSecondary="staticUrlToPrefixed(options.logo_src_secondary)"
+                    :menuOptions="options"/>
+            </div>
             <small id="messagebox"/>
             <small id="inactivebox" class="alert rounded-0 m-0 p-2 alert-warning" />
             <div id="columns">
@@ -14,3 +23,28 @@
         <div id="dd-helper" />
     </div>
 </template>
+<script>
+import { MastheadState } from "layout/masthead";
+import Masthead from "components/Masthead/Masthead.vue";
+import { getGalaxyInstance } from "app";
+import { getAppRoot } from "onload";
+
+export default {
+    components: {
+        Masthead,
+    },
+    computed: {
+        options() {
+            return getGalaxyInstance().config;
+        },
+    },
+    methods: {
+        getState() {
+            return new MastheadState();
+        },
+        staticUrlToPrefixed(url) {
+            return url?.startsWith("/") ? `${getAppRoot()}${url.substring(1)}` : url;
+        },
+    },
+}
+</script>
