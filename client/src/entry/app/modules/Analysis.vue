23832f9392ec54bd33132537083f5d6117a7a4bc
guerler <aysam.guerler@gmail.com>
2022-05-28 05:49:35 -0400

    Add toolbox to analysis module

--- client/src/entry/app/modules/Analysis.vue
+++ client/src/entry/app/modules/Analysis.vue
@@ -1,15 +1,39 @@
 <template>
-    <div>
-        Test
+    <div id="columns">
+        <div id="left" class="unified-panel">
+            <SidePanel side="left" :currentPanel="getToolBox()" :currentPanelProperties="ToolBoxProperties" />
+        </div>
+        <div id="center" />
+        <div id="right" class="unified-panel" />
     </div>
 </template>
 <script>
 import store from "store";
 import { urlData } from "utils/url";
 import Query from "utils/query-string-parsing";
+import { getGalaxyInstance } from "app";
+import ToolBox from "components/Panels/ProviderAwareToolBox";
+import SidePanel from "components/Panels/SidePanel";
+
 export default {
+    components: {
+        SidePanel,
+    },
     data() {
         return {};
+    },
+    computed: {
+        ToolBoxProperties() {
+            const Galaxy = getGalaxyInstance();
+            return {
+                storedWorkflowMenuEntries: Galaxy.config.stored_workflow_menu_entries,
+            };
+        },
+    },
+    methods: {
+        getToolBox() {
+            return ToolBox;
+        },
     },
 };
 </script>
