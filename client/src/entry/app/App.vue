dc2f184e20f429c2b797994292392507e8268c23
guerler <aysam.guerler@gmail.com>
2022-05-27 22:21:38 -0400

    Add everything template

--- client/src/entry/app/App.vue
+++ client/src/entry/app/App.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
-        TEST
+        <div id="everything">
+            <div id="background"/>
+            <div id="masthead"/>
+            <small id="messagebox"/>
+            <small id="inactivebox" class="alert rounded-0 m-0 p-2 alert-warning" />
+            <div id="columns">
+                <div id="left" class="unified-panel"/>
+                <div id="center" />
+                <div id="right" class="unified-panel" />
+            </div>
+        </div>
+        <div id="dd-helper" />
     </div>
 </template>
