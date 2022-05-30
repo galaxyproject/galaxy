5a9609c6d59d3a962e391657ddf3461726bd184d
guerler <aysam.guerler@gmail.com>
2022-05-30 10:37:27 -0400

    Use legacy routing helper, remove debug statement

--- client/src/components/History/Content/Dataset/DatasetActions.vue
+++ client/src/components/History/Content/Dataset/DatasetActions.vue
@@ -110,8 +110,8 @@ export default {
             this.backboneRoute("datasets/error", { dataset_id: this.item.id });
         },
         onInfo() {
-            //this.backboneRoute(`/datasets/${this.item.id}/details`);
-            this.$router.push(`/datasets/${this.item.id}/details`);
+            this.backboneRoute(`datasets/${this.item.id}/details`);
+            //this.$router.push(`datasets/${this.item.id}/details`);
         },
         onRerun() {
             this.backboneRoute(`root?job_id=${this.item.creating_job}`);
