24c4491a0dfc5cf25cce9a5a0f71c7238a2d4499
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2022-06-06 11:50:39 -0400

    Do not instantiate MarkdownFormatHelpers
    No need for a separate instance or even a class: it's just functions;
    also instead of passing references, just import as needed.

--- lib/galaxy/datatypes/data.py
+++ lib/galaxy/datatypes/data.py
@@ -36,6 +36,10 @@ from galaxy.util import (
     unicodify,
 )
 from galaxy.util.bunch import Bunch
+from galaxy.util.markdown import (
+    indicate_data_truncated,
+    literal_via_fence,
+)
 from galaxy.util.sanitize_html import sanitize_html
 from galaxy.util.zipstream import ZipstreamWrapper
 from . import (
@@ -500,7 +504,7 @@ class Data(metaclass=DataMeta):
                 headers,
             )
 
-    def display_as_markdown(self, dataset_instance, markdown_format_helpers):
+    def display_as_markdown(self, dataset_instance: "DatasetInstance"):
         """Prepare for embedding dataset into a basic Markdown document.
 
         This is a somewhat experimental interface and should not be implemented
@@ -523,9 +527,9 @@ class Data(metaclass=DataMeta):
         else:
             with open(dataset_instance.file_name) as f:
                 contents = f.read(DEFAULT_MAX_PEEK_SIZE)
-            result = markdown_format_helpers.literal_via_fence(contents)
+            result = literal_via_fence(contents)
             if len(contents) == DEFAULT_MAX_PEEK_SIZE:
-                result += markdown_format_helpers.indicate_data_truncated()
+                result += indicate_data_truncated()
         return result
 
     def _yield_user_file_content(self, trans, from_dataset, filename, headers: Headers):
