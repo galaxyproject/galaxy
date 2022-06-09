26caab07d3947ca62b6cf59a7fd0826529c67d63
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2022-06-09 23:31:43 +0100

    Modernise build of galaxy-web-stack package

--- packages/web_stack/galaxy/project_galaxy_web_stack.py
+++
@@ -1,7 +0,0 @@
-__version__ = "22.5.0.dev0"
-
-PROJECT_NAME = "galaxy-web-stack"
-PROJECT_URL = "https://github.com/galaxyproject/galaxy"
-PROJECT_AUTHOR = "Galaxy Project and Community"
-PROJECT_DESCRIPTION = "Galaxy Web Strack Abstraction"
-PROJECT_EMAIL = "galaxy-committers@lists.galaxyproject.org"
