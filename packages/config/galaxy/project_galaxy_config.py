c2298402b5ef2d3083cff21ad411dac3e367a9ed
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2022-06-09 18:40:57 +0100

    Modernise build of galaxy-config package
    Also:
    - Add galaxy-util and remove requests requirements
    - Update version to 22.5.0.dev0

--- packages/config/galaxy/project_galaxy_config.py
+++
@@ -1,7 +0,0 @@
-__version__ = "22.1.0"
-
-PROJECT_NAME = "galaxy-config"
-PROJECT_URL = "https://github.com/galaxyproject/galaxy"
-PROJECT_AUTHOR = "Galaxy Project and Community"
-PROJECT_DESCRIPTION = "Galaxy Configuration"
-PROJECT_EMAIL = "galaxy-committers@lists.galaxyproject.org"
