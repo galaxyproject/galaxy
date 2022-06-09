c5f3d2d04c8705afa5ddeefa1c8489ff11e709f9
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2022-06-10 00:51:00 +0100

    Modernise build of galaxy-app package
    Also:
    - Add missing files to `MANIFEST.in`
    - Add missing requirements

--- packages/app/galaxy/project_galaxy_app.py
+++
@@ -1,7 +0,0 @@
-__version__ = "22.5.0.dev0"
-
-PROJECT_NAME = "galaxy-app"
-PROJECT_URL = "https://github.com/galaxyproject/galaxy"
-PROJECT_AUTHOR = "Galaxy Project and Community"
-PROJECT_DESCRIPTION = "Galaxy Application (backend)"
-PROJECT_EMAIL = "galaxy-committers@lists.galaxyproject.org"
