076f1b5170a35a40bf59d115ba849094e264285d
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2022-06-09 18:50:22 +0100

    Modernise build of galaxy-files package
    Also:
    - Renamed build_scripts symlink to scripts for consistency

--- packages/files/galaxy/project_galaxy_files.py
+++
@@ -1,7 +0,0 @@
-__version__ = "22.5.0.dev0"
-
-PROJECT_NAME = "galaxy-files"
-PROJECT_URL = "https://github.com/galaxyproject/galaxy"
-PROJECT_AUTHOR = "Galaxy Project and Community"
-PROJECT_DESCRIPTION = "Galaxy File Source Framework and Default Plugins"
-PROJECT_EMAIL = "galaxy-committers@lists.galaxyproject.org"
