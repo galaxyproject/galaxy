5de7dbfe99a2d62cf80c708c25ccaee01c91babd
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2022-06-10 01:54:55 +0100

    Modernise build of galaxy-test-base package
    Also:
    - Add missing files to `MANIFEST.in`

--- packages/test_base/galaxy/project_galaxy_test_base.py
+++
@@ -1,7 +0,0 @@
-__version__ = "22.5.0.dev0"
-
-PROJECT_NAME = "galaxy-test-base"
-PROJECT_URL = "https://github.com/galaxyproject/galaxy"
-PROJECT_AUTHOR = "Galaxy Project and Community"
-PROJECT_DESCRIPTION = "Galaxy Testing Utilities"
-PROJECT_EMAIL = "galaxy-committers@lists.galaxyproject.org"
