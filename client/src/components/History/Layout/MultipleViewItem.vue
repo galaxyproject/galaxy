24b56ed8b9512562b7ea2728dedb865d39ce9538
Alireza Heidari <itisalirh@gmail.com>
2022-06-11 01:56:39 +0430

    Create MultipleViewItem.vue

---
+++ client/src/components/History/Layout/MultipleViewItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <HistoryPanel :history="source" v-on="handlers" @view-collection="onViewCollection" />
+</template>
+
+<script>
+import HistoryPanel from "components/History/CurrentHistory/HistoryPanel";
+
+export default {
+    components: { HistoryPanel },
+    props: {
+        source: {
+            type: Object,
+            required: true,
+        },
+        handlers: {
+            type: Object,
+            required: true,
+        },
+        onViewCollection: {
+            type: Function,
+            required: true,
+        },
+    },
+};
+</script>
