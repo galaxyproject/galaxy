231fda2d222c4e8a5d3c4330686323b2820fe230
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2022-06-10 01:41:48 +0100

    Modernise build of galaxy-webapps package
    Also:
    - Add missing files to `MANIFEST.in`

--- packages/webapps/galaxy/project_galaxy_webapps.py
+++
@@ -1,7 +0,0 @@
-__version__ = "22.5.0.dev0"
-
-PROJECT_NAME = "galaxy-webapps"
-PROJECT_URL = "https://github.com/galaxyproject/galaxy"
-PROJECT_AUTHOR = "Galaxy Project and Community"
-PROJECT_DESCRIPTION = "Galaxy Web Apps"
-PROJECT_EMAIL = "jmchilton@gmail.com"
