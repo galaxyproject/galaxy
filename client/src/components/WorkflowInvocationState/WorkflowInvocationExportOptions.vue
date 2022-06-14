2d62cf1297de10cd55b78ea71ac3ba696f0f1fe3
davelopez <46503462+davelopez@users.noreply.github.com>
2022-06-14 16:14:12 +0200

    Cleaup WorkflowInvocationExportOptions

--- client/src/components/WorkflowInvocationState/WorkflowInvocationExportOptions.vue
+++ client/src/components/WorkflowInvocationState/WorkflowInvocationExportOptions.vue
@@ -1,13 +1,10 @@
 <template>
     <b-card-group deck>
-        <b-card title="BioCompute Object" class="mb-2">
-            <p>BioCompute Object description...</p>
-            <a class="bco-json" :href="bcoJSON"><b>Download</b></a>
+        <b-card title="BioCompute Object" class="export-plugin-card">
+            <p>Learn more about <a href="https://biocomputeobject.org/" target="_blank">BioCompute Objects</a>.</p>
+            <a class="bco-json" :href="bcoDownloadLink"><b>Download</b></a>
         </b-card>
-        <!-- <b-card title="RO-Crate">
-                <p>RO-Crate description...</p>
-                <a class="bco-json" :href="bcoJSON"><b>Download</b></a>
-            </b-card> -->
+        <!-- Add more export plugins here -->
     </b-card-group>
 </template>
 
@@ -21,7 +18,7 @@ export default {
         },
     },
     computed: {
-        bcoJSON: function () {
+        bcoDownloadLink: function () {
             return `${getAppRoot()}api/invocations/${this.invocationId}/biocompute/download`;
         },
     },
