07cb649aa68e3b83d4f0183dafa7948b527a6413
Dannon Baker <dannon.baker@gmail.com>
2022-07-01 12:16:20 -0400

    Minor linting

--- client/src/components/DatasetInformation/DatasetError.vue
+++ client/src/components/DatasetInformation/DatasetError.vue
@@ -24,7 +24,10 @@
                         :tool-stderr="jobDetails.tool_stderr"
                         :job-stderr="jobDetails.job_stderr"
                         :job-messages="jobDetails.job_messages" />
-                    <JobProblemProvider v-slot="{ result: jobProblems }" :job-id="dataset.creating_job" @error="onError">
+                    <JobProblemProvider
+                        v-slot="{ result: jobProblems }"
+                        :job-id="dataset.creating_job"
+                        @error="onError">
                         <div v-if="jobProblems && (jobProblems.has_duplicate_inputs || jobProblems.has_empty_inputs)">
                             <h3 class="common_problems mt-3">Detected Common Potential Problems</h3>
                             <p v-if="jobProblems.has_empty_inputs" id="dataset-error-has-empty-inputs">
