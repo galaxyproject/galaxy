aa9ba99f8400cdcf44a147f395292f56efe2ce3f
Dannon Baker <dannon.baker@gmail.com>
2022-07-05 09:48:14 -0400

    Modernization of restored msa wrapper

--- config/plugins/visualizations/msa/src/script.js
+++ config/plugins/visualizations/msa/src/script.js
@@ -1,20 +1,23 @@
-import './msa.min.js';
+import "./msa.min.js";
 
-console.debug("MSA IS", msa);
 Object.assign(window.bundleEntries || {}, {
     load: function (options) {
-        console.debug('OPTIONS ARE', options);
-        var r = options.chart,
-            i = options.dataset,
-            o = r.settings,
-            s = msa({
-                el: $("#" + options.target),
-                vis: { conserv: "true" == o.get("conserv"), overviewbox: "true" == o.get("overviewbox") },
-                menu: "small",
-                bootstrapMenu: "true" == o.get("menu"),
-            });
-        s.u.file.importURL(i.download_url, function () {
-            s.render(), r.state("ok", "Chart drawn."), options.process.resolve();
+        const chart = options.chart;
+        const dataset = options.dataset;
+        const settings = chart.settings;
+        const msaViz = msa({
+            el: $("#" + options.target),
+            vis: {
+                conserv: "true" == settings.get("conserv"),
+                overviewbox: "true" == settings.get("overviewbox"),
+            },
+            menu: "small",
+            bootstrapMenu: "true" == settings.get("menu"),
+        });
+        msaViz.u.file.importURL(dataset.download_url, () => {
+            msaViz.render();
+            chart.state("ok", "Chart drawn.");
+            options.process.resolve();
         });
     },
 });
