27b4c95f1af9afa2759e78be70bbdf1af24e2ab4
Shivani Sangwan <shivanisangwan31@gmail.com>
2022-07-14 09:33:22 +0530

    Add Notification System API Database with migration

---
+++ lib/galaxy/model/migrations/alembic/versions_gxy/f6fb225c05ab_add_notification_table.py
@@ -0,0 +1,41 @@
+"""Add Notification Table
+
+Revision ID: f6fb225c05ab
+Revises: b182f655505f
+Create Date: 2022-07-12 18:34:56.156814
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "f6fb225c05ab"
+down_revision = "b182f655505f"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "notification_push",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("create_time", sa.DateTime),
+        sa.Column("update_time", sa.DateTime),
+        sa.Column("message_text", sa.String(500), index=True, unique=True),
+        sa.Column("deleted", sa.Boolean, index=True, default=False),
+    )
+
+    op.create_table(
+        "user_notification_association",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("user_id", sa.Integer, sa.ForeignKey("galaxy_user.id"), index=True),
+        sa.Column("notification_id", sa.Integer, sa.ForeignKey("notification_push.id"), index=True),
+        sa.Column("create_time", sa.DateTime),
+        sa.Column("update_time", sa.DateTime),
+        sa.Column("status_seen", sa.Boolean, index=True, default=False),
+    )
+
+
+def downgrade():
+    op.drop_table("notification_push")
+    op.drop_table("user_notification_association")
