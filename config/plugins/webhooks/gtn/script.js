11f511feac1e790fd9b357f586de19fd941d63d2
Helena Rasche <hxr@hx42.org>
2022-07-21 12:28:45 +0200

    Fix handling + encoding of spaces in tool names

--- config/plugins/webhooks/gtn/script.js
+++ config/plugins/webhooks/gtn/script.js
@@ -138,7 +138,7 @@
                         if (tool_id === "upload1" || tool_id === "upload") {
                             document.getElementById("tool-panel-upload-button").click();
                         } else {
-                            Galaxy.router.push("/", { tool_id: tool_id });
+                            Galaxy.router.push("/", { tool_id: encodeURI(tool_id) });
                         }
                         removeOverlay();
                     });
