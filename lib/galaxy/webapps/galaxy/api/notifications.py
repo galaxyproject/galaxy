93da87c5e089625e2a331a49838753338993bdcf
Shivani Sangwan <shivanisangwan31@gmail.com>
2022-07-27 15:48:52 +0530

    Adding API module for Notification Manager

---
+++ lib/galaxy/webapps/galaxy/api/notifications.py
@@ -0,0 +1,39 @@
+"""
+API operations on Notification objects.
+"""
+
+import logging
+
+from fastapi import Path
+
+from galaxy.managers.notification import NotificationManager
+from . import (
+    Router
+)
+
+log = logging.getLogger(__name__)
+
+router = Router(tags=["notifications"])
+
+@router.cbv
+class FastAPIGroupRoles:
+    manager: GroupRolesManager = depends(NotificationManager)
+
+    @router.get("/api/notifications", summary="Displays a collection (list) of notifications.")
+    def index(self):
+        return self.manager.index(limit = 5)
+
+    @router.get(
+        "/api/notifications",
+        summary="Displays information about a notification.",
+    )
+    def show(self, notification_id):
+        return self.manager.show(notification_id)
+
+    @router.put("/api/notifications", summary="Create a notificaton message")
+    def create(self, message):
+        return self.manager.create(message)
+
+    @router.put("/api/notifications", summary="Updates a notificaton message")
+    def update(self, notification, updated_message):
+        return self.manager.update(notification, updated_message)
