5fa81610e8502968f5e369cca6b05742fc575769
Peter van Heusden <pvh@sanbi.ac.za>
2022-08-01 14:17:19 +0200

    Correctly use string_as_bool for sort_by reverse_sort_order

--- lib/galaxy/tools/parameters/dynamic_options.py
+++ lib/galaxy/tools/parameters/dynamic_options.py
@@ -516,7 +516,7 @@ class SortByColumnFilter(Filter):
         column = elem.get("column", None)
         assert column is not None, "Required 'column' attribute missing from filter"
         self.column = d_option.column_spec_to_index(column)
-        self.reverse = elem.get("reverse_sort_order", False)
+        self.reverse = string_as_bool(elem.get("reverse_sort_order", "False"))
 
     def filter_options(self, options, trans, other_values):
         return sorted(options, key=lambda x: x[self.column], reverse=self.reverse)
