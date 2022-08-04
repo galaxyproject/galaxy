df7d1aa602cdf3318c77fad11be88ec168ec8f24
Matej Trojak <trojak@mail.muni.cz>
2022-08-04 10:33:04 +0200

    Reformatted files using black

--- test/unit/data/datatypes/test_pithya.py
+++ test/unit/data/datatypes/test_pithya.py
@@ -1,19 +1,16 @@
 import pytest
-from galaxy.datatypes.text import (
-    PithyaModel,
-    PithyaProperty,
-    PithyaResult
-)
-from .util import (
-    get_input_files
-)
+from galaxy.datatypes.text import PithyaModel, PithyaProperty, PithyaResult
+from .util import get_input_files
 
 
-@pytest.mark.parametrize('pithya_loader, input_file', [
-    [PithyaModel, "test_file1.pithya.model"],
-    [PithyaProperty, "test_file1.pithya.property"],
-    [PithyaResult, "test_file1.pithya.result"]
-])
+@pytest.mark.parametrize(
+    "pithya_loader, input_file",
+    [
+        [PithyaModel, "test_file1.pithya.model"],
+        [PithyaProperty, "test_file1.pithya.property"],
+        [PithyaResult, "test_file1.pithya.result"],
+    ],
+)
 def test_pithya_sniff(pithya_loader, input_file):
     loader = pithya_loader()
     with get_input_files(input_file) as input_files:
--- test/unit/data/datatypes/test_storm.py
+++ test/unit/data/datatypes/test_storm.py
@@ -1,32 +1,30 @@
 import pytest
-from galaxy.datatypes.text import (
-    StormRegions,
-    StormSample,
-    StormCheck
-)
-from .util import (
-    get_input_files,
-    MockDataset,
-    MockDatasetDataset
-)
+from galaxy.datatypes.text import StormRegions, StormSample, StormCheck
+from .util import get_input_files, MockDataset, MockDatasetDataset
 
 
-@pytest.mark.parametrize('storm_loader, input_file', [
-    [StormRegions, "test_file2.storm.regions"],
-    [StormSample, "test_file2.storm.sample"],
-    [StormCheck, "test_file2.storm.check"]
-])
+@pytest.mark.parametrize(
+    "storm_loader, input_file",
+    [
+        [StormRegions, "test_file2.storm.regions"],
+        [StormSample, "test_file2.storm.sample"],
+        [StormCheck, "test_file2.storm.check"],
+    ],
+)
 def test_storm_sniff(storm_loader, input_file):
     loader = storm_loader()
     with get_input_files(input_file) as input_files:
         assert loader.sniff(input_files[0]) is True
 
 
-@pytest.mark.parametrize('storm_loader, input_file, expected_peek', [
-    [StormRegions, "test_file2.storm.regions", """Storm-pars region results."""],
-    [StormSample, "test_file2.storm.sample", """Storm-pars sample results."""],
-    [StormCheck, "test_file2.storm.check", """Model checking result: true"""]
-])
+@pytest.mark.parametrize(
+    "storm_loader, input_file, expected_peek",
+    [
+        [StormRegions, "test_file2.storm.regions", """Storm-pars region results."""],
+        [StormSample, "test_file2.storm.sample", """Storm-pars sample results."""],
+        [StormCheck, "test_file2.storm.check", """Model checking result: true"""],
+    ],
+)
 def test_storm_set_peek(storm_loader, input_file, expected_peek):
     loader = storm_loader()
     with get_input_files(input_file) as input_files:
