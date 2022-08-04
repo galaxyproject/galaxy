b622c25f900171f5dd91aaeb6022fd8036ad9fbd
Shivani Sangwan <shivanisangwan31@gmail.com>
2022-08-04 21:14:31 +0530

    Adding NotificationRequestModel in schema

--- lib/galaxy/schema/schema.py
+++ lib/galaxy/schema/schema.py
@@ -3279,6 +3279,12 @@ class NotificationListResponseModel(BaseModel):
     __root__: List[NotificationResponseModel]
 
 
+class NotificationRequestModel(BaseModel):
+    id: DecodedDatabaseIdField = None
+    message_text: str = None
+    updated_message: str = None
+
+
 class MaterializeDatasetInstanceAPIRequest(Model):
     source: DatasetSourceType = Field(
         title="Source",
