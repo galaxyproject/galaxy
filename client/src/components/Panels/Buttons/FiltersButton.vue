6b076d47d82e4968146c0bda7d8b9fa5131318be
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-08-08 10:56:04 +0200

    display number of active filters in tooltip

--- client/src/components/Panels/Buttons/FiltersButton.vue
+++ client/src/components/Panels/Buttons/FiltersButton.vue
@@ -16,10 +16,30 @@ import _l from "utils/localization";
 
 export default {
     name: "FiltersButton",
-    data() {
-        return {
-            tooltip: _l("Toolbox Filter Settings"),
-        };
+    computed: {
+        activeFilters() {
+            const user = this.$store.state.user.currentUser;
+
+            if (!user || !user.id) {
+                return 0;
+            }
+
+            const preferences = user.preferences;
+            const labelFilters = preferences.toolbox_label_filters?.split(",") ?? [];
+            const sectionFilters = preferences.toolbox_section_filters?.split(",") ?? [];
+            const toolFilters = preferences.toolbox_tool_filters?.split(",") ?? [];
+
+            return [...labelFilters, ...sectionFilters, ...toolFilters].filter((s) => s !== "").length;
+        },
+        tooltip() {
+            if (this.activeFilters === 0) {
+                return _l("Toolbox Filter Settings");
+            } else if (this.activeFilters === 1) {
+                return _l("1 Filter Active");
+            } else {
+                return this.activeFilters + " " + _l("Filters Active");
+            }
+        },
     },
     methods: {
         onFilter() {
