5876acc06515c5679fe3a203f94bd8f176937ce4
Dannon Baker <dannon.baker@gmail.com>
2022-08-11 16:10:24 -0400

    Do not mount vue to body, use a normal element.  Mounted element is replaced with vue completely, so body will cause issues.

--- client/src/entry/analysis/index.js
+++ client/src/entry/analysis/index.js
@@ -9,7 +9,7 @@ addInitialization((Galaxy) => {
     console.log("App setup");
     const router = getRouter(Galaxy);
     new Vue({
-        el: "body",
+        el: "#app",
         render: (h) => h(App),
         router: router,
         store: store,
