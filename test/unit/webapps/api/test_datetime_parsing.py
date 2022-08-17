973d0779b65993f0252531533717790fea759870
Marius van den Beek <m.vandenbeek@gmail.com>
2022-08-17 18:43:57 +0200

    Include timezone testing
    Co-authored-by: Nicola Soranzo <nicola.soranzo@gmail.com>

--- test/unit/webapps/api/test_datetime_parsing.py
+++ test/unit/webapps/api/test_datetime_parsing.py
@@ -8,7 +8,7 @@ class Time(BaseModel):
 
 
 def test_naive_datetime_parsing():
-    with_zulu = Time(time="2022-08-15T11:29:32.853974Z")
-    without_zulu = Time(time="2022-08-15T11:29:32.853974")
-    assert with_zulu.time == without_zulu.time
-    assert not with_zulu.time.tzinfo
+    with_tz = Time(time="2022-08-15T11:29:32.853974+02:00")
+    without_tz = Time(time="2022-08-15T09:29:32.853974")
+    assert with_tz.time == without_tz.time
+    assert with_tz.time.tzinfo is None
