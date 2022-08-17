73cc49e6a7b89d182af3b68b13b5fa7d293698d2
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-08-17 18:02:39 +0200

    add table to render datatypes

--- client/src/components/AvailableDatatypes.vue
+++ client/src/components/AvailableDatatypes.vue
@@ -4,22 +4,66 @@ import { useGetDetailedDatatypes } from "composables/datatypes";
 import { useFilterObjectArray } from "composables/filter";
 import DelayedInput from "components/Common/DelayedInput";
 
-const fields = ["extension"];
-
 const filter = ref("");
+const filterFields = ["extension"];
 
 const { datatypes } = useGetDetailedDatatypes();
-const filteredArray = useFilterObjectArray(datatypes, filter, fields);
+const filteredDatatypes = useFilterObjectArray(datatypes, filter, filterFields);
+
+const fields = [
+    {
+        key: "extension",
+    },
+    {
+        key: "edamFormatLabel",
+        label: "EDAM Format",
+    },
+    {
+        key: "edamDataLabel",
+        label: "EDAM Data",
+    },
+];
+
+const edamLink = (edamIRI) => `https://edamontology.github.io/edam-browser/#${edamIRI}`;
 </script>
 
 <template>
     <div>
         <h1>Datatypes</h1>
-        <DelayedInput :delay="200" @change="(val) => (filter = val)" />
-        <div v-for="datatype in filteredArray" :key="datatype.extension">
-            <div>{{ datatype.extension }}</div>
-        </div>
+        <p>All Datatypes supported by this Galaxy instance. Hover over an item for more information.</p>
+        <DelayedInput placeholder="filter extensions" class="mb-3" :delay="200" @change="(val) => (filter = val)" />
+        <b-table striped small :items="filteredDatatypes" :fields="fields">
+            <template v-slot:cell(extension)="row">
+                <span v-b-tooltip.hover :title="row.item.description">
+                    {{ row.item.extension }}
+                </span>
+            </template>
+
+            <template v-slot:cell(edamFormatLabel)="row">
+                <a
+                    v-b-tooltip.hover
+                    target="_blank"
+                    :href="edamLink(row.item.edamFormat)"
+                    :title="row.item.edamFormatDefinition">
+                    {{ row.item.edamFormatLabel }}
+                </a>
+            </template>
+
+            <template v-slot:cell(edamDataLabel)="row">
+                <a
+                    v-b-tooltip.hover
+                    target="_blank"
+                    :href="edamLink(row.item.edamData)"
+                    :title="row.item.edamDataDefinition">
+                    {{ row.item.edamDataLabel }}
+                </a>
+            </template>
+        </b-table>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+table {
+    cursor: default;
+}
+</style>
