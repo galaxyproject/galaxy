b577eb8806f1d9eab7ca8aaf8d5e3dcd36664abb
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-08-18 13:09:22 +0200

    restructure installation actions

--- client/src/components/Toolshed/RepositoryDetails/InstallationActions.vue
+++ client/src/components/Toolshed/RepositoryDetails/InstallationActions.vue
@@ -5,12 +5,10 @@ const props = defineProps({
     status: String,
 });
 
-const emit = defineEmits(["onInstall", "onUninstall"]);
-
-const buttonClass = "btn-sm text-nowrap";
-
 const installState = computed(() => !props.status || props.status === "Uninstalled");
 const uninstallState = computed(() => props.status === "Installed");
+
+const emit = defineEmits(["onInstall", "onUninstall"]);
 
 function onCancel() {
     if (window.confirm(`Do you want to reset this repository?`)) {
@@ -21,26 +19,20 @@ function onCancel() {
 
 <template>
     <div>
-        <div class="d-inline-block">
-            <b-button v-if="installState" variant="primary" :class="buttonClass" @click="() => emit('onInstall')">
-                Install
-            </b-button>
-            <b-button
-                v-else-if="uninstallState"
-                variant="danger"
-                :class="buttonClass"
-                @click="() => emit('onUninstall')">
-                Uninstall
-            </b-button>
-            <b-button
-                v-else
-                variant="warning"
-                :class="buttonClass"
-                :title="l('Reset Broken or Stuck Installation')"
-                @click="onCancel">
-                Reset
-            </b-button>
-        </div>
+        <b-button v-if="installState" variant="primary" class="btn-sm" @click="() => emit('onInstall')">
+            Install
+        </b-button>
+        <b-button v-else-if="uninstallState" variant="danger" class="btn-sm" @click="() => emit('onUninstall')">
+            Uninstall
+        </b-button>
+        <b-button
+            v-else
+            variant="warning"
+            class="btn-sm"
+            :title="l('Reset Broken or Stuck Installation')"
+            @click="onCancel">
+            Reset
+        </b-button>
     </div>
 </template>
 
