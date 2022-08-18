6ee735b31e835a877cbad4d1e9ec74010c04fe28
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-08-18 09:51:31 +0200

    make rows sortable add history filter hint

--- client/src/components/AvailableDatatypes.vue
+++ client/src/components/AvailableDatatypes.vue
@@ -13,14 +13,17 @@ const filteredDatatypes = useFilterObjectArray(datatypes, filter, filterFields);
 const fields = [
     {
         key: "extension",
+        sortable: true,
     },
     {
         key: "edamFormatLabel",
         label: "EDAM Format",
+        sortable: true,
     },
     {
         key: "edamDataLabel",
         label: "EDAM Data",
+        sortable: true,
     },
 ];
 
@@ -30,9 +33,12 @@ const edamLink = (edamIRI) => `https://edamontology.github.io/edam-browser/#${ed
 <template>
     <div>
         <h1>Datatypes</h1>
-        <p>All Datatypes supported by this Galaxy instance. Hover over an item for more information.</p>
+        <p>
+            All Datatypes supported by this Galaxy instance. Hover over an item for more information. These extensions
+            can be filtered by in the History, by expanding "search datasets".
+        </p>
         <DelayedInput placeholder="filter extensions" class="mb-3" :delay="200" @change="(val) => (filter = val)" />
-        <b-table striped small :items="filteredDatatypes" :fields="fields">
+        <b-table striped small sort-icon-left :items="filteredDatatypes" :fields="fields">
             <template v-slot:cell(extension)="row">
                 <span v-b-tooltip.hover :title="row.item.description">
                     {{ row.item.extension }}
