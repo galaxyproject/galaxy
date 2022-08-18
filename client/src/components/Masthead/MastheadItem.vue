28627fe3528c23a41781deb2d4a8bb001540c8d0
Dannon Baker <dannon.baker@gmail.com>
2022-08-18 17:29:31 -0400

    Masthead a11y; for icon-only tabs display the tooltip as a sr-only text placeholder.

--- client/src/components/Masthead/MastheadItem.vue
+++ client/src/components/Masthead/MastheadItem.vue
@@ -12,6 +12,8 @@
         :title="tab.tooltip"
         @click="open(tab, $event)">
         <template v-if="tab.icon">
+            <!-- If this is an icon-based tab, inject tooltip directly for screen readers -->
+            <span class="sr-only">{{ tab.tooltip || tab.id }}</span>
             <span :class="iconClasses" />
             <span v-if="toggle" class="nav-note fa fa-check" />
         </template>
