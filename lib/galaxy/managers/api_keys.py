eb6caf849a4f012db22d185d76bb49790d40fc00
Alireza Heidari <itisalirh@gmail.com>
2022-08-20 01:55:53 +0200

    Add new api_keys managers: `get_api_keys` and `delete_api_key`

--- lib/galaxy/managers/api_keys.py
+++ lib/galaxy/managers/api_keys.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from galaxy.model import APIKeys
 from galaxy.structured_app import BasicSharedApp
 
 
@@ -5,7 +8,10 @@ class ApiKeyManager:
     def __init__(self, app: BasicSharedApp):
         self.app = app
 
-    def create_api_key(self, user) -> str:
+    def get_api_keys(self, user) -> List[APIKeys]:
+        return user.api_keys
+
+    def create_api_key(self, user) -> APIKeys:
         guid = self.app.security.get_new_guid()
         new_key = self.app.model.APIKeys()
         new_key.user_id = user.id
@@ -13,7 +19,7 @@ class ApiKeyManager:
         sa_session = self.app.model.context
         sa_session.add(new_key)
         sa_session.flush()
-        return guid
+        return new_key
 
     def get_or_create_api_key(self, user) -> str:
         # Logic Galaxy has always used - but it would appear to have a race
@@ -22,5 +28,10 @@ class ApiKeyManager:
         if user.api_keys:
             key = user.api_keys[0].key
         else:
-            key = self.create_api_key(user)
+            key = self.create_api_key(user).key
         return key
+
+    def delete_api_key(self, user, key) -> None:
+        sa_session = self.app.model.context
+        sa_session.query(self.app.model.APIKeys).filter_by(user_id=user.id, key=key).delete()
+        sa_session.flush()
