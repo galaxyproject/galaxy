2ac47e96c2fb0dd02f38801d36e3866dc479aaca
Alireza Heidari <itisalirh@gmail.com>
2022-08-20 01:56:56 +0200

    Add API key services in users services

--- lib/galaxy/webapps/galaxy/services/users.py
+++ lib/galaxy/webapps/galaxy/services/users.py
@@ -1,4 +1,5 @@
 from galaxy import exceptions as glx_exceptions
+from galaxy.managers import api_keys
 from galaxy.managers.context import ProvidesUserContext
 from galaxy.managers.users import UserManager
 from galaxy.queue_worker import send_local_control_task
@@ -13,13 +14,10 @@ class UsersService(ServiceBase):
     and pydantic models to declare its parameters and return types.
     """
 
-    def __init__(
-        self,
-        security: IdEncodingHelper,
-        user_manager: UserManager,
-    ):
+    def __init__(self, security: IdEncodingHelper, user_manager: UserManager, api_key_manager: api_keys.ApiKeyManager):
         super().__init__(security)
         self.user_manager = user_manager
+        self.api_key_manager = api_key_manager
 
     def recalculate_disk_usage(
         self,
@@ -37,3 +35,22 @@ class UsersService(ServiceBase):
                 "recalculate_user_disk_usage",
                 kwargs={"user_id": trans.user.id},
             )
+
+    def get_api_keys(self, trans: ProvidesUserContext):
+        result = []
+        api_keys = self.api_key_manager.get_api_keys(trans.user)
+        for api_key in api_keys:
+            result.append({"key": api_key.key, "create_time": api_key.create_time})
+        return result
+
+    def create_api_key(self, trans: ProvidesUserContext):
+        api_key = self.api_key_manager.create_api_key(trans.user)
+        result = {"key": api_key.key, "create_time": api_key.create_time}
+        return result
+
+    def delete_api_key(
+        self,
+        api_key: str,
+        trans: ProvidesUserContext,
+    ):
+        self.api_key_manager.delete_api_key(trans.user, api_key)
