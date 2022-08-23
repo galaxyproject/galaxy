bb910fb34d5c660c4a0e3aec62d41d22f0afc3bc
guerler <aysam.guerler@gmail.com>
2022-08-23 16:07:35 -0400

    Parse version to panel properties

--- client/src/entry/analysis/modules/Admin.vue
+++ client/src/entry/analysis/modules/Admin.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="columns">
-        <SidePanel side="left" :currentPanel="getAdminPanel()" />
+        <SidePanel side="left" :currentPanel="getAdminPanel()" :current-panel-properties="panelProperties" />
         <div id="center">
             <div class="center-container">
                 <div class="center-panel" style="display: block">
@@ -18,6 +18,14 @@ import SidePanel from "components/Panels/SidePanel";
 export default {
     components: {
         SidePanel,
+    },
+    computed: {
+        panelProperties() {
+            const Galaxy = getGalaxyInstance();
+            return {
+                versionMajor: Galaxy.config.version_major,
+            };
+        },
     },
     methods: {
         getAdminPanel() {
