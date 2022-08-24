df7644299fac76c20dbf0bb8b0af8edb03cd26d0
guerler <aysam.guerler@gmail.com>
2022-08-24 14:18:42 -0400

    Restore login components

--- client/src/entry/analysis/modules/Login.vue
+++ client/src/entry/analysis/modules/Login.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        test this here today!
+    <div class="m-3">
+        <ChangePassword v-if="hasToken" />
+        <LoginIndex v-else />
     </div>
 </template>
+
+<script>
+import { getGalaxyInstance } from "app";
+import LoginIndex from "components/login/LoginIndex";
+import ChangePassword from "components/login/ChangePassword";
+
+export default {
+    components: {
+        ChangePassword,
+        LoginIndex,
+    },
+    computed: {
+        hasToken() {
+            const Galaxy = getGalaxyInstance();
+            return Galaxy.params.token || Galaxy.params.expired_user;
+        },
+    },
+};
+</script>
