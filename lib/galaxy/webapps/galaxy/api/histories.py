2141f9dd3569c0951709f973e387dff54f21cf3f
Benno Weiss <benno.weiss@gmail.com>
2022-08-25 11:51:05 +0200

    removed beacon history endpoint

--- lib/galaxy/webapps/galaxy/api/histories.py
+++ lib/galaxy/webapps/galaxy/api/histories.py
@@ -162,50 +162,6 @@ class FastAPIHistories:
         return self.service.published(trans, serialization_params, filter_query_params)
 
     @router.get(
-        "/api/histories/beacon",
-        summary="Returns the complete list of beacon histories",
-        status_code=status.HTTP_200_OK
-    )
-    def get_beacon_histories(self, trans: ProvidesUserContext = DependsOnTrans):
-        """
-        Returns IDs of all beacon histories.
-
-        A beacon history is a history that has a special name and belongs to a user that has enabled the beacon_share
-        setting.
-
-            Note:
-                 This endpoint queries histories directly. We might want to move the query to the HistoryService or
-                 HistoryManager to be more in line with the other endpoints defined by this file.
-                 However, it is not likely for any of this code to be reused anywhere else.
-        """
-
-        # Prevent non-admin users querying beacon histories
-        if not trans.user_is_admin:
-            message = "Only admins can query beacon histories"
-            raise glx_exceptions.AdminRequiredException(message)
-
-        # Query beacon histories by "beacon_enabled" setting and hard coded name "___BEACON_PICKUP___"
-        query = trans.sa_session.query(User).join(History).filter(
-            User.preferences.any(and_(UserPreference.name == "beacon_enabled", UserPreference.value == 1))).filter(
-            History.name == "___BEACON_PICKUP___")
-
-        beacon_histories = []
-        for user in query:
-            for history in user.histories:
-                # only get "user_id" and "history_id" as consumed by the beacon-import script
-                beacon_histories.append(
-                    {
-                        "user_id": trans.security.encode_id(user.id),
-                        "history_id": trans.security.encode_id(history.id)
-                    }
-                )
-                # removing this break would mean supporting an arbitrary number of beacon histories per user
-                # with this break only the first history per user is added to the response
-                break
-
-        return {"beacon_histories": beacon_histories}
-
-    @router.get(
         "/api/histories/shared_with_me",
         summary="Return all histories that are shared with the current user.",
     )
