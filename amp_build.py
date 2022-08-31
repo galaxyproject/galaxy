e760bd6ef87aaa1c9ddb3813e9d3b02510ebdf4b
Brian Wheeler <bdwheele@iu.edu>
2022-08-31 14:56:31 -0400

    converted to AMP-2150 environment

--- amp_build.py
+++ amp_build.py
@@ -2,7 +2,6 @@
 #
 # Build the amp-galaxy tarball for distribution
 #
-
 import argparse
 import logging
 import tempfile
@@ -17,37 +16,25 @@ import time
 import tarfile
 import io
 from lib.galaxy.version import VERSION
+from amp.package import *
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', default=False, action='store_true', help="Turn on debugging")
-    parser.add_argument('--version', default=None, help="Package Version")  
     parser.add_argument('--package', default=False, action='store_true', help="build a package instead of installing")
-    parser.add_argument('destdir', nargs="?", help="Output directory for the package")
+    parser.add_argument('destdir', nargs="?", help="Output directory for the package or install")
     args = parser.parse_args()
     logging.basicConfig(format="%(asctime)s [%(levelname)-8s] (%(filename)s:%(lineno)d)  %(message)s",
                         level=logging.DEBUG if args.debug else logging.INFO)
 
-    # force a download of the requirements and rebuild the client if necessary.
-    logging.info("Building environment")
+    # Building Galaxy is actually:
+    #   * run the galaxy startup so it will build the client and install any deps
+    #   * copy the source repository to a temporary directory
+    #   * remove a ton of things that we don't want to either put in the package or on the filesystem
+    sourcedir = Path(sys.path[0])
+    logging.info("Building Galaxy environment")
     subprocess.run(['scripts/common_startup.sh'], check=True)
-
-    if not args.package:
-        logging.info("Galaxy should be ready for use")
-        exit(0)
-
-    if not args.destdir:
-        logging.error("A destination directory must be specified when creating a package")
-        exit(1)
-
-    
-
-
-
-    # get our directories
-    here = Path.cwd().resolve()
-    destdir = Path(args.destdir).resolve()
-    sourcedir = Path(sys.path[0]).resolve()
 
     with tempfile.TemporaryDirectory(prefix="galaxy-build-") as builddir:
         logging.info(f"Copying instance to {builddir}")
@@ -56,7 +43,14 @@ def main():
         # building galaxy is really just a matter of removing a bunch of things:  
         logging.info("Removing non-dist directories")
         for n in ('.git', '.venv', 'static/plugins', '.ci', '.circleci', '.coveragerc', '.gitattributes',
-                  '.gitignore', '.k8s_ci.Dockerfile', '.github'):            
+                  '.gitignore', '.k8s_ci.Dockerfile', '.github', 
+                  # files we've renamed because they were doing github things our account cannot                  
+                  '.ci.upstream', '.dockerignore', '.circleci.upstream', '.github.upstream',
+                  # parts of galaxy we don't use
+                  'test', 'test-data', 'contrib', 'display_applications', 'doc', 'hooks', 'tool-data', 'run_tests.sh',
+                  # packaging system stuff
+                  'amp_build.py', 'amp_config.default', 'amp_hook_config.py'
+                  ):            
             logging.debug(f"Removing entry {n}")
             if Path(builddir, n).exists():
                 if Path(builddir, n).is_dir():
@@ -75,39 +69,54 @@ def main():
             logging.debug(f"Clearing runtime {n}")
             shutil.rmtree(builddir + "/" + n, ignore_errors=True)
             os.mkdir(builddir + "/" + n)
+   
+        # The tools directory has a bunch of tools we don't want to install
+        # it's easier to list what we want to keep...
+        tools_to_keep = ('cloud/', 'data_source/upload.', 'data_source/import.')
+        tools_dir = Path(builddir, "tools")        
+        tool_dirs = set()
+        for tfile in tools_dir.glob("**/*"):
+            tfilename = tfile.relative_to(tools_dir)
+            if tfile.is_dir():
+                tool_dirs.add(tfile)
+            for t in tools_to_keep:
+                if str(tfilename).startswith(t):
+                    logging.debug(f"Keeping tool file: {tfilename!s}")
+                    break
+            else:
+                logging.debug(f"Removing tool file {tfilename!s}")
+                if not tfile.is_dir():
+                    tfile.unlink()
+        for tooldir in sorted(tool_dirs, reverse=True):
+            if len(list(tooldir.iterdir())) == 0:
+                logging.debug(f"Removing empty tool dir: {tooldir!s}")
+                tooldir.rmdir()
 
-        # build the package.
-        buildtime = datetime.now().strftime("%Y%m%d_%H%M%S")        
-        if args.version is None:
-            args.version = VERSION
 
-        logging.info(f"Creating amp_galaxy package for with version {args.version} in {destdir}")
-        basedir= f"amp_galaxy-{args.version}"
-        pkgfile = Path(destdir, f"{basedir}.tar")
-        with tarfile.TarFile(pkgfile, "w") as tfile:
-            # create base directory
-            base_info = tarfile.TarInfo(name=basedir)
-            base_info.mtime = int(time.time())
-            base_info.type = tarfile.DIRTYPE
-            base_info.mode = 0o755
-            tfile.addfile(base_info, None)                    
 
-            # write metadata file
-            metafile = tarfile.TarInfo(name=f"{basedir}/amp_package.yaml")
-            metafile_data = yaml.safe_dump({
-                'name': 'amp_galaxy',
-                'version': args.version,
-                'build_date': buildtime,
-                'install_path': 'galaxy'
-            }).encode('utf-8')
-            metafile.size = len(metafile_data)
-            metafile.mtime = int(time.time())
-            metafile.mode = 0o644
-            tfile.addfile(metafile, io.BytesIO(metafile_data))
-            logging.debug(f"Pushing data from {builddir!s} to data in tarball")
-            tfile.add(builddir, f'{basedir}/data', recursive=True)
-        
-        logging.info(f"Package has been created {pkgfile!s}")
+        if not args.package:
+            # Installing the software
+            # Effectively, we can just copy the contents of our build directory to the 
+            # installation directory.  This won't copy the configuration bits used
+            # by amp_control.py, so during development someone would copy them manually
+            # and when a package is built it will get pulled then.        
+            logging.info(f"Copying built instance to {args.destdir}")
+            subprocess.run(["cp", "-a", f"{builddir}/." ,args.destdir], check=True)
+        else:
+            # Create the package
+            try:
+                new_package = create_package(Path(args.destdir), 
+                                             Path(builddir),
+                                             metadata={'name': 'galaxy', 
+                                                       'version': VERSION, 
+                                                       'install_path': 'galaxy'},
+                                             hooks={'config': 'amp_hook_config.py'},
+                                             defaults=Path("amp_config.default"))                                            
+                logging.info(f"New package in {new_package}")    
+            except Exception as e:
+                logging.error(f"Failed to build backage: {e}")
+                exit(1)
+
 
 if __name__ == "__main__":
     main()
