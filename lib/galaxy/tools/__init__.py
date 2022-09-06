096f9209768d30b216c1ccc7843ab8c0b54e4cbf
Matthias Bernt <m.bernt@ufz.de>
2022-09-06 15:47:11 +0200

    interval2maf: remove tool from GALAXY_LIB_TOOLS_UNVERSIONED
    the tool is now at IUC and should use a proper container
    
    xref https://github.com/galaxyproject/tools-iuc/pull/4769

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -180,7 +180,6 @@ GALAXY_LIB_TOOLS_UNVERSIONED = [
     "Interval_Maf_Merged_Fasta2",
     "GeneBed_Maf_Fasta2",
     "maf_stats1",
-    "Interval2Maf1",
     "Interval2Maf_pairwise1",
     "MAF_To_Interval1",
     "MAF_filter",
@@ -235,6 +234,7 @@ GALAXY_LIB_TOOLS_VERSIONED = {
     "Convert characters1": packaging.version.parse("1.0.1"),
     "substitutions1": packaging.version.parse("1.0.1"),
     "winSplitter": packaging.version.parse("1.0.1"),
+    "Interval2Maf1": packaging.version.parse("1.0.1+galaxy0"),
 }
 
 REQUIRE_FULL_DIRECTORY = {
