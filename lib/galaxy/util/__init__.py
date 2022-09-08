d50ec2f3b6854586be507cf00598c9181f26cb95
mvdbeek <m.vandenbeek@gmail.com>
2022-09-08 14:04:50 +0200

    Add unicodify type annotation

--- lib/galaxy/util/__init__.py
+++ lib/galaxy/util/__init__.py
@@ -32,6 +32,12 @@ from email.mime.text import MIMEText
 from hashlib import md5
 from os.path import relpath
 from pathlib import Path
+from typing import (
+    Any,
+    Optional,
+    overload,
+    Union,
+)
 from urllib.parse import (
     urlencode,
     urlparse,
@@ -46,6 +52,7 @@ from boltons.iterutils import (
 )
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from typing_extensions import Literal
 
 try:
     import grp
@@ -1083,7 +1090,35 @@ def roundify(amount, sfs=2):
         return amount[0:sfs] + "0" * (len(amount) - sfs)
 
 
-def unicodify(value, encoding=DEFAULT_ENCODING, error="replace", strip_null=False, log_exception=True):
+@overload
+def unicodify(
+    value: Literal[None],
+    encoding: str = DEFAULT_ENCODING,
+    error: str = "replace",
+    strip_null: bool = False,
+    log_exception: bool = True,
+) -> None:
+    ...
+
+
+@overload
+def unicodify(
+    value: Union[str, bytes, int, float, bool, Exception],  # Anything but None, but this seems fine for now
+    encoding: str = DEFAULT_ENCODING,
+    error: str = "replace",
+    strip_null: bool = False,
+    log_exception: bool = True,
+) -> str:
+    ...
+
+
+def unicodify(
+    value: Any,
+    encoding: str = DEFAULT_ENCODING,
+    error: str = "replace",
+    strip_null: bool = False,
+    log_exception: bool = True,
+) -> Optional[str]:
     """
     Returns a Unicode string or None.
 
