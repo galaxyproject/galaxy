1f5b0e6bba63143e3612430444f20212b632216d
John Davis <jdavcs@gmail.com>
2022-09-09 19:31:25 -0400

    Drop LegacyScripts: no longer used

--- lib/galaxy/model/migrations/scripts.py
+++ lib/galaxy/model/migrations/scripts.py
@@ -1,5 +1,4 @@
 import os
-import re
 import sys
 from typing import (
     List,
@@ -14,10 +13,7 @@ from alembic.script import ScriptDirectory
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 
-from galaxy.model.database_utils import (
-    database_exists,
-    is_one_database,
-)
+from galaxy.model.database_utils import database_exists
 from galaxy.model.migrations import (
     AlembicManager,
     DatabaseConfig,
@@ -160,120 +156,10 @@ class LegacyScriptsException(Exception):
         super().__init__(message)
 
 
-class LegacyScripts:
+class LegacyManageDb:
 
     LEGACY_CONFIG_FILE_ARG_NAMES = ["-c", "--config", "--config-file"]
-    ALEMBIC_CONFIG_FILE_ARG = "--alembic-config"  # alembic config file, set in the calling script
-    DEFAULT_DB_ARG = "default"
 
-    def __init__(self, argv: List[str], cwd: Optional[str] = None) -> None:
-        self.argv = argv
-        self.cwd = cwd or os.getcwd()
-        self._database: Optional[str] = None  # Do not assign default value: `None` means we don't know yet.
-
-    @property
-    def database(self):
-        if self._database is None:
-            raise LegacyScriptsException(
-                "Attempt to access identifier of database before processing the script arguments"
-            )
-        return self._database
-
-    def run(self) -> None:
-        """
-        Convert legacy arguments to current spec required by Alembic,
-        then add db url arguments required by Alembic
-        """
-        self.convert_args()
-        add_db_urls_to_command_arguments(self.argv, self.gxy_url, self.tsi_url)
-
-    def convert_args(self) -> None:
-        """
-        Convert legacy arguments to current spec required by Alembic.
-
-        Note: The following method calls must be done in this sequence.
-        """
-        self.pop_database_argument()
-        self.rename_config_argument()
-        self.rename_alembic_config_argument()
-        self.load_db_urls()
-        self.convert_version_argument()
-
-    def pop_database_argument(self) -> None:
-        """
-        If last argument is a valid database name, pop and assign it; otherwise assign default.
-        """
-        arg = self.argv[-1]
-        self._database = self.DEFAULT_DB_ARG
-        if arg in ["galaxy", "install"]:
-            self._database = self.argv.pop()
-
-    def rename_config_argument(self) -> None:
-        """
-        Rename the optional config argument: we can't use '-c' because that option is used by Alembic.
-        """
-        for arg in self.LEGACY_CONFIG_FILE_ARG_NAMES:
-            if arg in self.argv:
-                self._rename_arg(arg, CONFIG_FILE_ARG)
-                return
-
-    def rename_alembic_config_argument(self) -> None:
-        """
-        Rename argument name: `--alembic-config` to `-c`. There should be no `-c` argument present.
-        """
-        if "-c" in self.argv:
-            raise LegacyScriptsException("Cannot rename alembic config argument: `-c` argument present.")
-        self._rename_arg(self.ALEMBIC_CONFIG_FILE_ARG, "-c")
-
-    def convert_version_argument(self) -> None:
-        """
-        Convert legacy version argument to current spec required by Alembic.
-        """
-        if "--version" in self.argv:
-            # Just remove it: the following argument should be the version/revision identifier.
-            pos = self.argv.index("--version")
-            self.argv.pop(pos)
-        else:
-            # If we find --version=foo, extract foo and replace arg with foo (which is the revision identifier)
-            p = re.compile(r"--version=([0-9A-Fa-f]+)")
-            for i, arg in enumerate(self.argv):
-                m = p.match(arg)
-                if m:
-                    self.argv[i] = m.group(1)
-                    return
-            # No version argument found: construct argument for an upgrade operation.
-            # Raise exception otherwise.
-            if "upgrade" not in self.argv:
-                raise LegacyScriptsException("If no `--version` argument supplied, `upgrade` argument is requried")
-
-            if self._is_one_database():  # upgrade both regardless of database argument
-                self.argv.append("heads")
-            else:  # for separate databases, choose one
-                if self.database in ["galaxy", self.DEFAULT_DB_ARG]:
-                    self.argv.append(f"{GXY}@head")
-                elif self.database == "install":
-                    self.argv.append(f"{TSI}@head")
-
-    def get_db_url(self):
-        if self.database in ["galaxy", self.DEFAULT_DB_ARG]:
-            return self.gxy_url
-        elif self.database == "install":
-            return self.tsi_url
-
-    def _rename_arg(self, old_name, new_name) -> None:
-        pos = self.argv.index(old_name)
-        self.argv[pos] = new_name
-
-    def load_db_urls(self) -> None:
-        gxy_config, tsi_config, _ = get_configuration(self.argv, self.cwd)
-        self.gxy_url = gxy_config.url
-        self.tsi_url = tsi_config.url
-
-    def _is_one_database(self):
-        return is_one_database(self.gxy_url, self.tsi_url)
-
-
-class LegacyManageDb:
     def __init__(self):
         self._set_db_urls()
 
@@ -320,6 +206,19 @@ class LegacyManageDb:
         self._upgrade(gxy_db_url, GXY)
         self._upgrade(tsi_db_url, TSI)
 
+    def rename_config_argument(self, argv: List[str]) -> None:
+        """
+        Rename the optional config argument: we can't use '-c' because that option is used by Alembic.
+        """
+        for arg in self.LEGACY_CONFIG_FILE_ARG_NAMES:
+            if arg in argv:
+                self._rename_arg(argv, arg, CONFIG_FILE_ARG)
+                return
+
+    def _rename_arg(self, argv, old_name, new_name) -> None:
+        pos = argv.index(old_name)
+        argv[pos] = new_name
+
     def _upgrade(self, db_url, model):
         try:
             engine = create_engine(db_url)
@@ -329,11 +228,13 @@ class LegacyManageDb:
             engine.dispose()
 
     def _set_db_urls(self):
-        ls = LegacyScripts(sys.argv, os.getcwd())
-        ls.rename_config_argument()
-        ls.load_db_urls()
-        self.gxy_db_url = ls.gxy_url
-        self.tsi_db_url = ls.tsi_url
+        self.rename_config_argument(sys.argv)
+        self._load_db_urls()
+
+    def _load_db_urls(self):
+        gxy_config, tsi_config, _ = get_configuration(sys.argv, os.getcwd())
+        self.gxy_db_url = gxy_config.url
+        self.tsi_db_url = tsi_config.url
 
     def _get_gxy_sam_db_version(self, engine):
         dbcache = DatabaseStateCache(engine)
