1fe0adb278e1f1419752f76aea621eb53d056072
Dannon Baker <dannon.baker@gmail.com>
2022-09-13 11:37:20 -0400

    Eslint disable for unused jquery in bundleEntries, since it's now not directly used.

--- client/src/bundleEntries.js
+++ client/src/bundleEntries.js
@@ -8,7 +8,7 @@
  */
 
 /* jquery and _ are exposed via expose-loader while several external plugins rely on these */
-import $ from "jquery";
+import $ from "jquery"; // eslint-disable-line no-unused-vars
 import _ from "underscore"; // eslint-disable-line no-unused-vars
 
 export { getGalaxyInstance, setGalaxyInstance } from "app";
