03121efd8648bb15d2aa1a3a47b05e709e7f70ec
guerler <aysam.guerler@gmail.com>
2022-09-13 11:57:06 -0400

    Fix comment

--- client/src/bundleToolshed.js
+++ client/src/bundleToolshed.js
@@ -1,10 +1,5 @@
 /**
- * The the toolshed list of globals we expose on window.bundleToolshed.
- *
- * Everything that is exposed on this global variable is something that the python templates
- * require for their hardcoded initializations. These objects are going to have to continue
- * to exist until such time as we replace the overall application with a Vue component which
- * will handle initializations for components individually.
+ * The toolshed list of globals we expose in window.bundleToolshed used by Toolshed makos.
  */
 
 /* jquery and _ are exposed via expose-loader while several external plugins rely on these */
