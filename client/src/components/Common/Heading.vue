8b200f7b499ad04d56ee115d0f138a9114862b9c
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-09-14 15:56:57 +0200

    add heading component

---
+++ client/src/components/Common/Heading.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+let element = "h1";
+let sizeClass = "h-large";
+
+const props = defineProps({
+    h1: Boolean,
+    h2: Boolean,
+    h3: Boolean,
+    h4: Boolean,
+    h5: Boolean,
+    h6: Boolean,
+    bold: Boolean,
+    separator: Boolean,
+    // acceptable sizes are "large", "medium", "small", and "text"
+    size: String,
+    icon: String,
+});
+
+for (const key of ["h1", "h2", "h3", "h4", "h5", "h6"]) {
+    if (props[key]) {
+        element = key;
+        break;
+    }
+}
+
+if (props.size) {
+    sizeClass = "h-" + props.size;
+}
+</script>
+
+<template>
+    <div v-if="props.separator" class="separator heading" :class="[sizeClass, props.bold ? 'font-weight-bold' : '']">
+        <FontAwesomeIcon v-if="props.icon" :icon="props.icon" />
+        <div v-else class="stripe"></div>
+        <component :is="element" :class="sizeClass">
+            <slot />
+        </component>
+        <div class="stripe"></div>
+    </div>
+    <component v-else :is="element" class="heading" :class="[sizeClass, props.bold ? 'font-weight-bold' : '']">
+        <FontAwesomeIcon v-if="props.icon" :icon="props.icon" />
+        <slot />
+    </component>
+</template>
+
+<style lang="scss" scoped>
+@import "scss/theme/blue.scss";
+
+.heading::v-deep > svg {
+    font-size: 0.75em;
+}
+
+// prettier-ignore
+h1, h2, h3, h4, h5, h6 {
+    display: flex;
+    align-items: center;
+    gap: 0.25em;
+}
+
+.separator {
+    display: grid;
+    grid-template-columns: auto auto 1fr;
+    gap: 0.25em;
+    align-items: center;
+    margin-bottom: 0.5rem;
+
+    // prettier-ignore
+    h1, h2, h3, h4, h5, h6 {
+        margin: 0;
+    }
+
+    .stripe {
+        display: block;
+        height: 1px;
+        background-color: $brand-secondary;
+        min-width: 1rem;
+    }
+}
+</style>
