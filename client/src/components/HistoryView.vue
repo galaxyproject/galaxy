9e3a632edab339467a0ffe67bb29571a16544f91
guerler <aysam.guerler@gmail.com>
2022-09-16 11:53:43 -0400

    Use history component in history view route

--- client/src/components/HistoryView.vue
+++ client/src/components/HistoryView.vue
@@ -1,44 +1,43 @@
 <template>
-    <div id="structured-history-view">
-        <div v-if="errorMessages.length > 0">
-            <div v-for="(error, index) in errorMessages" :key="index">
-                <div class="alert alert-danger" role="alert">{{ error }}</div>
+    <CurrentUser v-slot="{ user }">
+        <UserHistories v-if="user" v-slot="{ handlers }" :user="user">
+            <div v-if="getHistory" class="d-flex flex-column h-100">
+                <CollectionPanel
+                    v-if="selectedCollections.length && selectedCollections[0].history_id == id"
+                    :history="getHistory"
+                    :selected-collections.sync="selectedCollections"
+                    :show-controls="false"
+                    @view-collection="onViewCollection" />
+                <HistoryPanel
+                    v-else
+                    v-on="handlers"
+                    :history="getHistory"
+                    :show-controls="false"
+                    @view-collection="onViewCollection" />
             </div>
-        </div>
-        <div v-else>
-            <div id="history-view-controls" class="clear">
-                <div class="float-left">
-                    <span v-if="historyHistory && historyHistory['purged'] == false">
-                        <span v-if="historyData.user_is_owner == false">
-                            <button id="import" class="btn btn-secondary">Import and start using history</button>
-                        </span>
-                        <span v-if="historyData.user_is_owner && historyData.history_is_current == false">
-                            <button id="switch-history" class="btn btn-secondary" @click="switchHistory">
-                                Switch to this history
-                            </button>
-                        </span>
-                    </span>
-                </div>
-                <div class="float-right">
-                    <button id="toggle-deleted" class="btn btn-secondary">Include deleted</button>
-                    <button id="toggle-hidden" class="btn btn-secondary">Include hidden</button>
-                </div>
-            </div>
-            <div
-                v-if="historyHistory"
-                :id="'history-' + historyHistory['id']"
-                class="history-panel unified-panel-body"
-                style="overflow: auto"></div>
-        </div>
-    </div>
+            <b-alert v-else class="m-2" variant="info" show>
+                <LoadingSpan message="Loading Histories" />
+            </b-alert>
+        </UserHistories>
+    </CurrentUser>
 </template>
 
 <script>
-import { getAppRoot } from "onload/loadConfig";
-import axios from "axios";
-import HistoryView from "mvc/history/history-view";
+import { mapGetters } from "vuex";
+import CollectionPanel from "components/History/CurrentCollection/CollectionPanel";
+import CurrentUser from "components/providers/CurrentUser";
+import HistoryPanel from "components/History/CurrentHistory/HistoryPanel";
+import LoadingSpan from "components/LoadingSpan";
+import UserHistories from "components/providers/UserHistories";
 
 export default {
+    components: {
+        LoadingSpan,
+        HistoryPanel,
+        CollectionPanel,
+        CurrentUser,
+        UserHistories,
+    },
     props: {
         id: {
             type: String,
@@ -47,73 +46,21 @@ export default {
     },
     data() {
         return {
-            historyData: {},
-            historyHistory: {},
-            // TODO: Pick a standard messaging convention and use a child component for it here.
-            errorMessages: [],
+            selectedCollections: [],
         };
     },
-    created: function () {
-        const url = getAppRoot() + "history/view/" + this.id;
-        this.ajaxCall(url);
-    },
-    updated: function () {
-        this.makeHistoryView(this.historyData);
+    computed: {
+        ...mapGetters({ getHistoryById: "history/getHistoryById" }),
+        getHistory() {
+            console.log(this.id);
+            console.log(this.getHistoryById(this.id));
+            return this.getHistoryById(this.id);
+        },
     },
     methods: {
-        ajaxCall: function (url) {
-            axios
-                .get(url)
-                .then((response) => this.updateHistoryView(response))
-                .catch((error) => this.errorMessages.push(error.response.data.err_msg));
-        },
-        updateHistoryView: function (response) {
-            this.historyData = response.data;
-            this.historyHistory = response.data.history;
-        },
-        makeHistoryView: function (history) {
-            const options = {
-                hasMasthead: history.use_panels ? "true" : "false",
-                userIsOwner: history.user_is_owner ? "true" : "false",
-                isCurrent: history.history_is_current ? "true" : "false",
-                historyJSON: history.history,
-                showDeletedJson: history.show_deleted,
-                showHiddenJson: history.show_hidden,
-                initialModeDeleted: history.show_deleted ? "showing_deleted" : "not_showing_deleted",
-                initialModeHidden: history.show_hidden ? "showing_hidden" : "not_showing_hidden",
-                allowUserDatasetPurge: history.allow_user_dataset_purge ? "true" : "false",
-            };
-            options.viewToUse = options.userIsOwner
-                ? { location: "mvc/history/history-view-edit", className: "HistoryViewEdit" }
-                : { location: "mvc/history/history-view", className: "HistoryView" };
-            return HistoryView.historyEntry(options);
-        },
-        switchHistory: function () {
-            const url = getAppRoot() + "history/switch_to_history?hist_id=" + this.historyHistory["id"];
-            this.ajaxCall(url, this.reloadPage);
-        },
-        reloadPage: function () {
-            window.location.reload();
+        onViewCollection(collection) {
+            this.selectedCollections = [...this.selectedCollections, collection];
         },
     },
 };
 </script>
-
-<style>
-#history-view-controls {
-    flex: 0 0 44px;
-    background-color: white;
-    border-bottom: 1px solid #ddd;
-    width: 100%;
-    padding: 8px;
-}
-.history-panel > .controls .title {
-    font-size: 120%;
-}
-.history-panel > .controls .title input {
-    font-size: 100%;
-}
-a.btn {
-    text-decoration: none;
-}
-</style>
