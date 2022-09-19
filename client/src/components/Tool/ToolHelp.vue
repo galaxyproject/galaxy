d0d6ea0ef2b6fbff31b5395ae4e71704ad1aa7d6
mrajani <mohit.s.rajani@gmail.com>
2022-09-19 14:44:02 +0200

    Git rid of JQuery from ToolHelp.vue

--- client/src/components/Tool/ToolHelp.vue
+++ client/src/components/Tool/ToolHelp.vue
@@ -1,29 +1,13 @@
 <template>
-    <div class="form-help form-text mt-4" v-html="formattedContent" />
+    <div id="formattedContent" class="form-help form-text mt-4" v-html="content" />
 </template>
 
 <script>
-import $ from "jquery";
-import { getAppRoot } from "onload/loadConfig";
-
 export default {
     props: {
         content: {
             type: String,
             required: true,
-        },
-    },
-    computed: {
-        formattedContent() {
-            const $tmpl = $("<div/>").append(this.content);
-            $tmpl.find("a").attr("target", "_blank");
-            $tmpl.find("img").each(function () {
-                const img_src = $(this).attr("src");
-                if (img_src.indexOf("admin_toolshed") !== -1) {
-                    $(this).attr("src", getAppRoot() + img_src);
-                }
-            });
-            return $tmpl.html();
         },
     },
 };
