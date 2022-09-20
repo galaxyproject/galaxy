b6be2b7faa56b02d6f3b7db70f260626976345df
Alireza Heidari <itisalirh@gmail.com>
2022-09-20 16:30:49 +0200

    Create HistoryDropZone.vue

---
+++ client/src/components/History/CurrentHistory/HistoryDropZone.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+    <div
+        class="dropzone d-flex align-items-center justify-content-center w-100 h-100 p-2 text-center text-secondary position-absolute rounded">
+        Drop here to copy to history
+    </div>
+</template>
+
+<style scoped>
+.dropzone {
+    z-index: 100;
+    border: dashed;
+    background-color: rgba(0, 0, 0, 0.4);
+}
+</style>
