4eb1f190048fef030be0da23b2f9abdae4f29140
hujambo-dunia <michelle.savage.1@gmail.com>
2022-09-20 14:29:50 -0400

    frontend test script of tool search filter functions to correctly match Results array-of-objects with Tools array

--- client/src/components/Panels/Common/ToolSearch.test.js
+++ client/src/components/Panels/Common/ToolSearch.test.js
@@ -1,46 +1,110 @@
-import { mount } from "@vue/test-utils";
-import { getLocalVue, mountRenderless } from "jest/helpers";
-import ToolSearch from "./ToolSearch";
-import { getAppRoot } from "onload/loadConfig";
-import DelayedInput from "components/Common/DelayedInput";
-
-jest.mock("onload/loadConfig");
-getAppRoot.mockImplementation(() => "/");
-
-jest.mock("axios");
-
-const localVue = getLocalVue();
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { toolSearch } from "../utilities";
 
 describe("ToolSearch", () => {
-    let tools;
-    let store;
-    
-    const testCase = {
-        keyword: "bed" /* examples: gff2bed1, bed2gff1, wig_to_bigWig */
-    };
+    const toolsMock = [
+        {
+            "model_class": "ToolSection",
+            "id": "group_1",
+            "name": "Group 1",
+            "version": "",
+            "description": null,
+            "links": null,
+            "elems": [
+              {
+                "model_class": "Tool1",
+                "id": "tool1",
+                "name": "Tool 1",
+                "version": "1.0.0",
+                "description": "",
+                "labels": [],
+                "edam_operations": [
+                  "operation_3359"
+                ],
+                "edam_topics": [],
+                "hidden": "",
+                "is_workflow_compatible": true,
+                "xrefs": [],
+                "link": "/tool_runner?tool_id=tool1",
+                "min_width": -1,
+                "target": "galaxy_main",
+                "panel_section_id": "group_1",
+                "panel_section_name": "Group 1",
+                "form_style": "regular"
+              },
+              {
+                "model_class": "Tool2",
+                "id": "tool2",
+                "name": "Tool 2",
+                "version": "1.0.0",
+                "description": "",
+                "labels": [],
+                "edam_operations": [
+                  "operation_3436"
+                ],
+                "edam_topics": [],
+                "hidden": "",
+                "is_workflow_compatible": true,
+                "xrefs": [],
+                "link": "/tool_runner?tool_id=tool2",
+                "min_width": -1,
+                "target": "galaxy_main",
+                "panel_section_id": "group_1",
+                "panel_section_name": "Group 1",
+                "form_style": "regular"
+              }
+            ]
+          },
+          {
+            "model_class": "ToolSection",
+            "id": "group_2",
+            "name": "Group 2",
+            "version": "",
+            "description": null,
+            "links": null,
+            "elems": [
+              {
+                "model_class": "Tool3",
+                "id": "tool3",
+                "name": "Tool 3",
+                "version": "0.1.0",
+                "description": "from dataset",
+                "labels": [],
+                "edam_operations": [],
+                "edam_topics": [],
+                "hidden": "",
+                "is_workflow_compatible": true,
+                "xrefs": [],
+                "link": "/tool_runner?tool_id=tool3",
+                "min_width": -1,
+                "target": "galaxy_main",
+                "panel_section_id": "group_2",
+                "panel_section_name": "Group 2",
+                "form_style": "special"
+              }
+            ]
+        }
+    ];
+    const resultsMock = ["tool1", "tool3"];
+
+    let axiosMock;
+    let getToolSearch;
 
     beforeEach(async () => {
+        axiosMock = new MockAdapter(axios);
+        getToolSearch = new toolSearch;
     });
 
-    test("test tool search get results", () => {
-        let wrapper = mount(ToolSearch, {
-            propsData: {
-                category: {
-                    name: "name",
-                },
-                toolbox: tools,
-                storedWorkflowMenuEntries: [],
-                currentPanelView: 'default',
-                query: testCase.keyword,
-            }, 
-            localVue,
-            stubs: {
-                icon: { template: "<div></div>" },
-            },
-            store
-        });
-    });
-
-    it("test tool search returns results", async () => {
+    it("test filter functions correctly matching: (1) Tools store array-of-objects with (2) Results array", async () => {
+        axiosMock
+            .onGet(`/api/tools`)
+            .replyOnce(200, toolsMock)
+            .onGet(/api\/tools?.*/)
+            .replyOnce(200, resultsMock);
+            let toolsResults = getToolSearch.filter(toolsMock, resultsMock);
+            let toolsResultsSection = getToolSearch.filterSections(toolsMock, resultsMock);
+        expect(toolsResults.length).toBe(2);
+        expect(toolsResultsSection.length).toBe(2);
     });
 });
