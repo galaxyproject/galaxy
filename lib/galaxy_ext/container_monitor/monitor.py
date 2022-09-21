5468ee8959cc8d8f132133c449ad81c9284a9f6b
Nate Coraor <nate@bx.psu.edu>
2022-09-21 19:01:05 -0400

    Fix isort complaint

--- lib/galaxy_ext/container_monitor/monitor.py
+++ lib/galaxy_ext/container_monitor/monitor.py
@@ -6,6 +6,5 @@ sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pa
 
 from galaxy.job_execution.container_monitor import main
 
-
 if __name__ == "__main__":
     main()
