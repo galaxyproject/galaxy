c020f494cb1ebb1bef0b583470ea661efa998477
mrajani <mohit.s.rajani@gmail.com>
2022-09-21 12:12:59 +0200

    Format and ESLINT fix

--- client/src/components/Tool/ToolHelp.vue
+++ client/src/components/Tool/ToolHelp.vue
@@ -14,18 +14,18 @@ export default {
     },
     computed: {
         formatContent() {
-            let temp = document.createElement("div")
-            temp.id = 'formattedContent'
-            temp.insertAdjacentHTML( 'beforeend', this.content );
-            temp.getElementsByTagName('a').forEach(elem => elem.setAttribute('target', '_blank'))
-            temp.getElementsByTagName('img').forEach((elem) => {
-                const imgSrc = elem.getAttribute("src")
-                if(elem.src.indexOf('admin_toolshed') !== -1) {
-                    elem.setAttribute('src', getAppRoot() + imgSrc)
+            const temp = document.createElement("div");
+            temp.id = "formattedContent";
+            temp.insertAdjacentHTML("beforeend", this.content);
+            temp.getElementsByTagName("a").forEach((elem) => elem.setAttribute("target", "_blank"));
+            temp.getElementsByTagName("img").forEach((elem) => {
+                const imgSrc = elem.getAttribute("src");
+                if (elem.src.indexOf("admin_toolshed") !== -1) {
+                    elem.setAttribute("src", getAppRoot() + imgSrc);
                 }
-            })
-            return temp.innerHTML
-        }
-    }
+            });
+            return temp.innerHTML;
+        },
+    },
 };
 </script>
