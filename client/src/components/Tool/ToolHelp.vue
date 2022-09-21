c9a9c86673449ebcc280ece39c30a0d7053b81ee
mrajani <mohit.s.rajani@gmail.com>
2022-09-21 12:06:31 +0200

    Worked on suggested changes

--- client/src/components/Tool/ToolHelp.vue
+++ client/src/components/Tool/ToolHelp.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="form-help form-text mt-4" v-html="content" />
+    <div v-html="formatContent" class="form-help form-text mt-4"></div>
 </template>
 
 <script>
+import { getAppRoot } from "onload/loadConfig";
+
 export default {
     props: {
         content: {
@@ -10,5 +12,20 @@ export default {
             required: true,
         },
     },
+    computed: {
+        formatContent() {
+            let temp = document.createElement("div")
+            temp.id = 'formattedContent'
+            temp.insertAdjacentHTML( 'beforeend', this.content );
+            temp.getElementsByTagName('a').forEach(elem => elem.setAttribute('target', '_blank'))
+            temp.getElementsByTagName('img').forEach((elem) => {
+                const imgSrc = elem.getAttribute("src")
+                if(elem.src.indexOf('admin_toolshed') !== -1) {
+                    elem.setAttribute('src', getAppRoot() + imgSrc)
+                }
+            })
+            return temp.innerHTML
+        }
+    }
 };
 </script>
