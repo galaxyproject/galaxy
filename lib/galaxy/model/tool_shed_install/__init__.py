155449648c1af80ec208b20b9498c4cc4185240d
John Chilton <jmchilton@gmail.com>
2022-09-22 11:41:20 -0400

    HasToolBox...

--- lib/galaxy/model/tool_shed_install/__init__.py
+++ lib/galaxy/model/tool_shed_install/__init__.py
@@ -1,7 +1,10 @@
 import logging
 import os
 from enum import Enum
-from typing import TYPE_CHECKING
+from typing import (
+    Optional,
+    TYPE_CHECKING,
+)
 
 from sqlalchemy import (
     Boolean,
@@ -24,6 +27,10 @@ from galaxy.model.custom_types import (
     TrimmedString,
 )
 from galaxy.model.orm.now import now
+from galaxy.tool_util.toolbox.base import (
+    AbstractToolBox,
+    DynamicToolConfDict,
+)
 from galaxy.util import asbool
 from galaxy.util.bunch import Bunch
 from galaxy.util.dictifiable import Dictifiable
@@ -40,6 +47,11 @@ if TYPE_CHECKING:
 
 else:
     _HasTable = object
+
+
+class HasToolBox(common_util.HasToolShedRegistry):
+    tool_dependency_dir: Optional[str]
+    toolbox: AbstractToolBox
 
 
 class Base(metaclass=DeclarativeMeta):
@@ -197,7 +209,7 @@ class ToolShedRepository(Base, _HasTable):
     def can_reinstall_or_activate(self):
         return self.deleted
 
-    def get_sharable_url(self, app):
+    def get_sharable_url(self, app: HasToolBox):
         return common_util.get_tool_shed_repository_url(app, self.tool_shed, self.owner, self.name)
 
     @property
@@ -208,7 +220,7 @@ class ToolShedRepository(Base, _HasTable):
     def shed_config_filename(self, value):
         self.metadata_["shed_config_filename"] = os.path.abspath(value)
 
-    def get_shed_config_dict(self, app):
+    def get_shed_config_dict(self, app: HasToolBox) -> DynamicToolConfDict:
         """
         Return the in-memory version of the shed_tool_conf file, which is stored in the config_elems entry
         in the shed_tool_conf_dict.
@@ -219,7 +231,7 @@ class ToolShedRepository(Base, _HasTable):
                 return shed_config_dict
         return self.guess_shed_config(app)
 
-    def get_tool_relative_path(self, app):
+    def get_tool_relative_path(self, app: HasToolBox):
         # This is a somewhat public function, used by data_manager_manual for instance
         shed_conf_dict = self.get_shed_config_dict(app)
         tool_path = None
@@ -231,7 +243,7 @@ class ToolShedRepository(Base, _HasTable):
             )
         return tool_path, relative_path
 
-    def guess_shed_config(self, app):
+    def guess_shed_config(self, app: HasToolBox):
         tool_ids = []
         for tool in self.metadata_.get("tools", []):
             tool_ids.append(tool.get("guid"))
@@ -389,13 +401,13 @@ class ToolShedRepository(Base, _HasTable):
                 missing_dependencies.append(tool_dependency)
         return missing_dependencies
 
-    def repo_files_directory(self, app):
+    def repo_files_directory(self, app: HasToolBox):
         repo_path = self.repo_path(app)
         if repo_path:
             return os.path.join(repo_path, self.name)
         return None
 
-    def repo_path(self, app):
+    def repo_path(self, app: HasToolBox):
         tool_shed = common_util.remove_protocol_and_port_from_tool_shed_url(self.tool_shed)
         for shed_tool_conf_dict in app.toolbox.dynamic_confs(include_migrated_tool_conf=True):
             tool_path = shed_tool_conf_dict["tool_path"]
@@ -725,8 +737,9 @@ class ToolDependency(Base, _HasTable):
     def in_error_state(self):
         return self.status == self.installation_status.ERROR
 
-    def installation_directory(self, app):
+    def installation_directory(self, app: HasToolBox) -> Optional[str]:
         if self.type == "package":
+            assert app.tool_dependency_dir
             return os.path.join(
                 app.tool_dependency_dir,
                 self.name,
@@ -736,6 +749,7 @@ class ToolDependency(Base, _HasTable):
                 self.tool_shed_repository.installed_changeset_revision,
             )
         if self.type == "set_environment":
+            assert app.tool_dependency_dir
             return os.path.join(
                 app.tool_dependency_dir,
                 "environment_settings",
