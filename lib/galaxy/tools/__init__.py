13b7fb1bc23d4ed8207a6c68a29d1870dede3a93
Matthias Bernt <m.bernt@ufz.de>
2022-09-26 11:24:56 +0200

    partially revert d674f2ba6e89e7a0894cea7a2f63b446184e7ebd
    and add comment to interval2maf tool stating that it's deprecated

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -234,6 +234,7 @@ GALAXY_LIB_TOOLS_VERSIONED = {
     "Convert characters1": packaging.version.parse("1.0.1"),
     "substitutions1": packaging.version.parse("1.0.1"),
     "winSplitter": packaging.version.parse("1.0.1"),
+    "Interval2Maf1": packaging.version.parse("1.0.1+galaxy0"),
 }
 
 REQUIRE_FULL_DIRECTORY = {
