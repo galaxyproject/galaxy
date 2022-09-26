0cc52f0cef1abadaf870588aaaf156c7fc841adc
guerler <aysam.guerler@gmail.com>
2022-09-26 16:03:22 -0400

    Add workflow published component, use markdown workflow display for now

---
+++ client/src/components/Workflow/WorkflowPublished.vue
@@ -0,0 +1,42 @@
+<template>
+    <Published :details="contentDetails">
+        <template v-slot>
+            <WorkflowDisplay :args="{ workflow_id: id }" :workflows="workflows" :expanded="true" />
+        </template>
+    </Published>
+</template>
+
+<script>
+import { urlData } from "utils/url";
+import Published from "components/Common/Published";
+import WorkflowDisplay from "components/Markdown/Elements/Workflow/WorkflowDisplay";
+
+export default {
+    components: {
+        Published,
+        WorkflowDisplay,
+    },
+    props: {
+        id: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            contentDetails: {},
+        };
+    },
+    computed: {
+        workflows() {
+            return [][this.id] = this.contentDetails;
+        },
+    },
+    created() {
+        const url = `/api/workflows/${this.id}`;
+        urlData({ url }).then((data) => {
+            this.contentDetails = { ...data };
+        });
+    },
+};
+</script>
