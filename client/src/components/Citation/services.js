25521a8288c9fe305c621972b1e73623758e0882
Dannon Baker <dannon.baker@gmail.com>
2022-09-27 08:23:48 -0400

    Update citation services to use plugin-based citationjs

--- client/src/components/Citation/services.js
+++ client/src/components/Citation/services.js
@@ -1,24 +1,30 @@
 import axios from "axios";
 import { rethrowSimple } from "utils/simple-error";
 import { getAppRoot } from "onload/loadConfig";
+import { plugins, Cite } from "@citation-js/core";
+import "@citation-js/plugin-bibtex";
+import "@citation-js/plugin-csl";
+
+plugins.config.get("@bibtex");
+plugins.config.get("@csl");
 
 export async function getCitations(source, id) {
     try {
         const request = await axios.get(`${getAppRoot()}api/${source}/${id}/citations`);
         const rawCitations = request.data;
         const citations = [];
-        import(/* webpackChunkName: "CitationJS" */ "citation-js").then(({ default: Cite }) => {
-            for (const rawCitation of rawCitations) {
-                try {
-                    const cite = new Cite(rawCitation.content);
-                    citations.push({ raw: rawCitation.content, cite: cite });
-                } catch (err) {
-                    console.warn(`Error parsing bibtex: ${err}`);
-                }
+        for (const rawCitation of rawCitations) {
+            try {
+                const cite = new Cite(rawCitation.content);
+                citations.push({ raw: rawCitation.content, cite: cite });
+            } catch (err) {
+                console.warn(`Error parsing bibtex: ${err}`);
             }
-        });
+        }
         return citations;
     } catch (e) {
         rethrowSimple(e);
     }
 }
+
+export function formattedCitation(citation) {}
