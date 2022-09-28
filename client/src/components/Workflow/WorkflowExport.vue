c281dc47014458df93e1294f23369e12fc2b2018
guerler <aysam.guerler@gmail.com>
2022-09-27 23:53:15 -0400

    Add div, fix route

--- client/src/components/Workflow/WorkflowExport.vue
+++ client/src/components/Workflow/WorkflowExport.vue
@@ -1,54 +1,56 @@
 <template>
-    <div v-if="workflow">
-        <h3 class="mb-4">Workflow Export of `{{ workflow.name }}`</h3>
-        <div v-if="workflow.importable && workflow.slug">
-            <a :href="importUrl">{{ importUrl }}</a>
+    <div>
+        <div v-if="workflow">
+            <h3 class="mb-4">Workflow Export of `{{ workflow.name }}`</h3>
+            <div v-if="workflow.importable && workflow.slug">
+                <a :href="importUrl">{{ importUrl }}</a>
+                <div>
+                    <small>
+                        Use this URL to import the workflows directly into another Galaxy server. You can copy it into
+                        the input field titled when importing a workflow.
+                    </small>
+                </div>
+                <hr />
+            </div>
+            <b-alert v-else variant="info" show>
+                This workflow is not accessible. Please use the sharing option to "Make Workflow Accessible and Publish"
+                to obtain a URL for importing to another Galaxy.
+            </b-alert>
+            <a :href="downloadUrl">Download Workflow</a>
             <div>
-                <small>
-                    Use this URL to import the workflows directly into another Galaxy server. You can copy it into the
-                    input field titled when importing a workflow.
+                <small class="text-muted">
+                    Downloads a file which can be saved or imported into another Galaxy server.
                 </small>
             </div>
             <hr />
-        </div>
-        <b-alert v-else variant="info" show>
-            This workflow is not accessible. Please use the sharing option to "Make Workflow Accessible and Publish" to
-            obtain a URL for importing to another Galaxy.
-        </b-alert>
-        <a :href="downloadUrl">Download Workflow</a>
-        <div>
-            <small class="text-muted">
-                Downloads a file which can be saved or imported into another Galaxy server.
-            </small>
-        </div>
-        <hr />
-        <a :href="svgUrl">Create Image</a>
-        <div>
-            <small class="text-muted"> Download an image of the workflow in SVG format. </small>
-        </div>
-        <hr />
-        <div class="mb-2">
-            Export to the
-            <a href="http://www.myexperiment.org/" target="_blank">www.myexperiment.org</a> site.
-        </div>
-        <form :action="myExperimentUrl" method="POST">
-            <b-form-input type="text" name="myexp_username" placeholder="Username" autocomplete="off" />
-            <small>Enter your username for myExperiment.</small>
-            <b-form-input type="password" name="myexp_password" placeholder="Password" autocomplete="off" />
-            <small>Enter your password for myExperiment.</small>
-            <div class="mt-2">
-                <b-button type="submit" variant="outline-primary"> Export to myExperiment </b-button>
+            <a :href="svgUrl">Create Image</a>
+            <div>
+                <small class="text-muted"> Download an image of the workflow in SVG format. </small>
             </div>
-        </form>
+            <hr />
+            <div class="mb-2">
+                Export to the
+                <a href="http://www.myexperiment.org/" target="_blank">www.myexperiment.org</a> site.
+            </div>
+            <form :action="myExperimentUrl" method="POST">
+                <b-form-input type="text" name="myexp_username" placeholder="Username" autocomplete="off" />
+                <small>Enter your username for myExperiment.</small>
+                <b-form-input type="password" name="myexp_password" placeholder="Password" autocomplete="off" />
+                <small>Enter your password for myExperiment.</small>
+                <div class="mt-2">
+                    <b-button type="submit" variant="outline-primary"> Export to myExperiment </b-button>
+                </div>
+            </form>
+        </div>
+        <b-alert v-else-if="!!error" variant="danger" show>
+            <span>
+                {{ error }}. Click
+                <router-link class="require-login-link" to="/login/start">here</router-link>
+                to login.
+            </span>
+        </b-alert>
+        <LoadingSpan v-else message="Loading workflow" />
     </div>
-    <b-alert v-else-if="!!error" variant="danger" show>
-        <span>
-            {{ error }}. Click
-            <router-link class="require-login-link" to="/login/start">here</router-link>
-            to login.
-        </span>
-    </b-alert>
-    <LoadingSpan v-else message="Loading workflow" />
 </template>
 <script>
 import Vue from "vue";
