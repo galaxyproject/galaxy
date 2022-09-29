649c9913f66f1b992c0c0c41cee36ed05c42c57d
John Chilton <jmchilton@gmail.com>
2022-09-29 09:19:25 -0400

    Typing for uses_shed_api.py

--- lib/galaxy_test/base/uses_shed_api.py
+++ lib/galaxy_test/base/uses_shed_api.py
@@ -1,4 +1,11 @@
+from typing import (
+    Any,
+    Callable,
+    Dict,
+)
 from unittest import SkipTest
+
+from requests import Response
 
 from galaxy.tool_util.verify.interactor import GalaxyInteractorApi
 from galaxy.util import unicodify
@@ -6,27 +13,29 @@ from galaxy_test.base.api_asserts import assert_status_code_is
 
 DEFAULT_TOOL_SHED_URL = "https://toolshed.g2.bx.psu.edu"
 
+OperationT = Callable[[Dict[str, Any]], Response]
+
 
 class UsesShedApi:
     @property
     def galaxy_interactor(self) -> GalaxyInteractorApi:
         ...
 
-    def delete_repo_request(self, payload):
+    def delete_repo_request(self, payload: Dict[str, Any]) -> Response:
         return self.galaxy_interactor._delete("tool_shed_repositories", data=payload, admin=True)
 
-    def install_repo_request(self, payload):
+    def install_repo_request(self, payload: Dict[str, Any]) -> Response:
         return self.galaxy_interactor._post(
             "tool_shed_repositories/new/install_repository_revision", data=payload, admin=True
         )
 
-    def repository_operation(self, operation, owner, name, changeset, tool_shed_url=DEFAULT_TOOL_SHED_URL):
+    def repository_operation(self, operation: OperationT, owner: str, name: str, changeset: str, tool_shed_url: str = DEFAULT_TOOL_SHED_URL) -> Dict[str, Any]:
         payload = {"tool_shed_url": tool_shed_url, "name": name, "owner": owner, "changeset_revision": changeset}
         create_response = operation(payload)
         assert_status_code_is(create_response, 200)
         return create_response.json()
 
-    def install_repository(self, owner, name, changeset, tool_shed_url=DEFAULT_TOOL_SHED_URL):
+    def install_repository(self, owner: str, name: str, changeset: str, tool_shed_url: str = DEFAULT_TOOL_SHED_URL) -> Dict[str, Any]:
         try:
             return self.repository_operation(
                 operation=self.install_repo_request,
@@ -40,7 +49,7 @@ class UsesShedApi:
                 raise SkipTest(f"Toolshed '{tool_shed_url}' unavailable")
             raise
 
-    def uninstall_repository(self, owner, name, changeset, tool_shed_url=DEFAULT_TOOL_SHED_URL):
+    def uninstall_repository(self, owner: str, name: str, changeset: str, tool_shed_url: str = DEFAULT_TOOL_SHED_URL) -> Dict[str, Any]:
         return self.repository_operation(
             operation=self.delete_repo_request, owner=owner, name=name, changeset=changeset, tool_shed_url=tool_shed_url
         )
