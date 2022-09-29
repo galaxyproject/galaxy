84005605c82b206c5573a8e5584ee002e3eba5a4
John Chilton <jmchilton@gmail.com>
2022-09-29 09:26:34 -0400

    Extend tool shed install testing to include index API.

--- lib/galaxy_test/base/uses_shed_api.py
+++ lib/galaxy_test/base/uses_shed_api.py
@@ -2,6 +2,8 @@ from typing import (
     Any,
     Callable,
     Dict,
+    List,
+    Optional,
 )
 from unittest import SkipTest
 
@@ -29,13 +31,17 @@ class UsesShedApi:
             "tool_shed_repositories/new/install_repository_revision", data=payload, admin=True
         )
 
-    def repository_operation(self, operation: OperationT, owner: str, name: str, changeset: str, tool_shed_url: str = DEFAULT_TOOL_SHED_URL) -> Dict[str, Any]:
+    def repository_operation(
+        self, operation: OperationT, owner: str, name: str, changeset: str, tool_shed_url: str = DEFAULT_TOOL_SHED_URL
+    ) -> Dict[str, Any]:
         payload = {"tool_shed_url": tool_shed_url, "name": name, "owner": owner, "changeset_revision": changeset}
         create_response = operation(payload)
         assert_status_code_is(create_response, 200)
         return create_response.json()
 
-    def install_repository(self, owner: str, name: str, changeset: str, tool_shed_url: str = DEFAULT_TOOL_SHED_URL) -> Dict[str, Any]:
+    def install_repository(
+        self, owner: str, name: str, changeset: str, tool_shed_url: str = DEFAULT_TOOL_SHED_URL
+    ) -> Dict[str, Any]:
         try:
             return self.repository_operation(
                 operation=self.install_repo_request,
@@ -49,7 +55,41 @@ class UsesShedApi:
                 raise SkipTest(f"Toolshed '{tool_shed_url}' unavailable")
             raise
 
-    def uninstall_repository(self, owner: str, name: str, changeset: str, tool_shed_url: str = DEFAULT_TOOL_SHED_URL) -> Dict[str, Any]:
+    def uninstall_repository(
+        self, owner: str, name: str, changeset: str, tool_shed_url: str = DEFAULT_TOOL_SHED_URL
+    ) -> Dict[str, Any]:
         return self.repository_operation(
             operation=self.delete_repo_request, owner=owner, name=name, changeset=changeset, tool_shed_url=tool_shed_url
         )
+
+    def index_repositories(
+        self, owner: Optional[str] = None, name: Optional[str] = None, changeset: Optional[str] = None
+    ) -> List[Dict[str, Any]]:
+        params: Dict[str, str] = {}
+        if owner is not None:
+            params["owner"] = owner
+        if name is not None:
+            params["name"] = name
+        if changeset is not None:
+            params["changeset"] = changeset
+        params["deleted"] = "false"
+        params["uninstalled"] = "false"
+        response = self.galaxy_interactor._get("tool_shed_repositories", params, admin=True)
+        response.raise_for_status()
+        return response.json()
+
+    def get_installed_repository_for(
+        self, owner: Optional[str] = None, name: Optional[str] = None, changeset: Optional[str] = None
+    ) -> Optional[Dict[str, Any]]:
+        index = self.index_repositories(owner, name, changeset)
+        if len(index) == 0:
+            return None
+        elif len(index) > 1:
+            raise AssertionError(f"expected at most one repository in response {index}")
+        else:
+            return index[0]
+
+    def get_installed_repository(self, id: str) -> Dict[str, Any]:
+        response = self.galaxy_interactor._get(f"tool_shed_repositories/{id}", admin=True)
+        response.raise_for_status()
+        return response.json()
