172a91700013d8da0c55033a80506abdd2e9c2b4
guerler <aysam.guerler@gmail.com>
2022-09-29 00:16:28 -0400

    Prep published panel view

--- client/src/components/Common/Published.vue
+++ client/src/components/Common/Published.vue
@@ -9,22 +9,23 @@
         </div>
         <div id="right">
             <div class="m-3">
-                <div v-if="modelClass">
-                    <h3>About this {{ modelClass }}</h3>
+                <div v-if="modelTitle">
+                    <h4>About this {{ modelTitle }}</h4>
+                    <h3>{{ details.title }}</h3>
                     <br />
                     <h4>Author</h4>
-                    <div>{{ details.username }}</div>
+                    <div>{{ owner }}</div>
                     <br />
+                    <hr />
                     <h4>Related Pages</h4>
                     <div>
-                        <router-link to="/pages/list_published"> All published {{ plural }} </router-link>
+                        <router-link :to="urlAll">All published {{ plural }}.</router-link>
                     </div>
                     <div>
                         <router-link :to="publishedByUser">
-                            Published {{ plural }} by {{ details.username }}
+                            Published {{ plural }} by {{ owner }}.
                         </router-link>
                     </div>
-                    <hr />
                 </div>
                 <LoadingSpan v-else message="Loading details" />
             </div>
@@ -53,18 +54,30 @@ export default {
         gravatar() {
             return `https://secure.gravatar.com/avatar/aysam.guerler@gmail.com?d=identicon`;
         },
-        publishedByUser() {
-            return `/pages/list_published?f-username=${this.details.username}`;
+        modelTitle() {
+            const modelClass = this.details ? this.details.model_class : "Item";
+            if (modelClass == "StoredWorkflow") {
+                return "Workflow";
+            }
+            return modelClass;
         },
-        modelClass() {
-            return this.details && this.details.model_class;
+        owner() {
+            return this.details.owner || this.details.username || "Unavailable";
         },
         plural() {
-            const modelLower = this.modelClass.toLowerCase();
-            if (modelLower == "history") {
-                return "histories";
+            if (this.modelTitle == "History") {
+                return "Histories";
             }
-            return `${modelLower}s`;
+            return `${this.modelTitle}s`;
+        },
+        pluralPath() {
+            return this.plural.toLowerCase();
+        },
+        publishedByUser() {
+            return `/${this.pluralPath}/list_published?f-username=${this.details.username}`;
+        },
+        urlAll() {
+            return `/${this.pluralPath}/list_published`;
         },
     },
     created() {
