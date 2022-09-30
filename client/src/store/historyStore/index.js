0f3a7d304c50d41ca2d9d3bee979f172b62f7668
Alireza Heidari <itisalirh@gmail.com>
2022-09-30 19:13:04 +0200

    export multipleViewStore in historyStore index

--- client/src/store/historyStore/index.js
+++ client/src/store/historyStore/index.js
@@ -2,3 +2,4 @@ export { collectionElementsStore } from "./collectionElementsStore";
 export { datasetStore } from "./datasetStore";
 export { historyItemsStore } from "./historyItemsStore";
 export { historyStore } from "./historyStore";
+export { multipleViewStore } from "./multipleViewStore";
