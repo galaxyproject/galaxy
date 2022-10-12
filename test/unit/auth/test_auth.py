0720dd453ccd4a87cf04b1c6a1f307d464537234
mahendrapaipuri <mahendra.paipuri@gmail.com>
2022-10-12 09:32:25 +0200

    Fix failing auth unit test

--- test/unit/auth/test_auth.py
+++ test/unit/auth/test_auth.py
@@ -5,7 +5,7 @@ from galaxy.model import User
 
 def test_alwaysreject():
     t = AlwaysReject()
-    assert t.authenticate("testmail", "testuser", "secret", dict()) == (None, "", "")
+    assert t.authenticate("testmail", "testuser", "secret", dict(), None) == (None, "", "")
 
 
 def test_localdb():
