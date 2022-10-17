dbba1b97329bf22388ca188ca2abfec14398c7e7
cat-bro <cathjb@gmail.com>
2022-10-17 19:41:40 +1100

    lint

--- lib/galaxy_test/api/test_container_resolvers.py
+++ lib/galaxy_test/api/test_container_resolvers.py
@@ -1,5 +1,6 @@
 from ._framework import ApiTestCase
 
+
 class ContainerResolversApiTestCase(ApiTestCase):
 
     def test_index(self):
@@ -8,6 +9,6 @@ class ContainerResolversApiTestCase(ApiTestCase):
         assert isinstance(response.json(), list)
 
     def test_show(self):
-        response = self._get("container_resolvers/0",  admin=True)
+        response = self._get("container_resolvers/0", admin=True)
         assert response.status_code == 200
         assert isinstance(response.json(), dict)
