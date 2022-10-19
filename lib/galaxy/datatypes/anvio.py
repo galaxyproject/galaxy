0baacdaab9a69cb1db45b383b838a90c50fdaa1a
John Davis <jdavcs@gmail.com>
2022-10-19 14:59:06 -0400

    Make dataset parameter required
    It's never None for this class, so having the parameter as optional is
    semantically incorrect. This, however, does violate the Liskov
    substitution principle (subclass method signature more strict than
    parent definition), but we can fix this (as well as many other such
    cases in datatypes) by slightly modifying the object hierarchy. Should
    be submitted in a follow-up PR.

--- lib/galaxy/datatypes/anvio.py
+++ lib/galaxy/datatypes/anvio.py
@@ -22,7 +22,7 @@ class AnvioComposite(Html):
     file_ext = "anvio_composite"
     composite_type = "auto_primary_file"
 
-    def generate_primary_file(self, dataset=None):
+    def generate_primary_file(self, dataset):
         """
         This is called only at upload to write the html file
         cannot rename the datasets here - they come with the default unfortunately
@@ -36,18 +36,17 @@ class AnvioComposite(Html):
                 if composite_file.optional:
                     opt_text = " (optional)"
                 missing_text = ""
-                if dataset and not os.path.exists(os.path.join(dataset.extra_files_path, composite_name)):
+                if not os.path.exists(os.path.join(dataset.extra_files_path, composite_name)):
                     missing_text = " (missing)"
                 rval.append(f'<li><a href="{composite_name}">{composite_name}</a>{opt_text}{missing_text}</li>')
             rval.append("</ul>")
         defined_files = map(lambda x: x[0], defined_files)
         extra_files = []
-        if dataset:
-            for dirpath, _dirnames, filenames in os.walk(dataset.extra_files_path, followlinks=True):
-                for filename in filenames:
-                    rel_path = os.path.relpath(os.path.join(dirpath, filename), dataset.extra_files_path)
-                    if rel_path not in defined_files:
-                        extra_files.append(rel_path)
+        for dirpath, _dirnames, filenames in os.walk(dataset.extra_files_path, followlinks=True):
+            for filename in filenames:
+                rel_path = os.path.relpath(os.path.join(dirpath, filename), dataset.extra_files_path)
+                if rel_path not in defined_files:
+                    extra_files.append(rel_path)
         if extra_files:
             rval.append("<p/>This composite dataset contains these undefined files:<p/><ul>")
             for rel_path in extra_files:
