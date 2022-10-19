0848fd045060fd04226a7ce206bb15a0d9933762
davelopez <46503462+davelopez@users.noreply.github.com>
2022-10-19 16:18:54 +0200

    Add placeholder client routes

---
+++ client/src/components/User/DiskUsage/Visualizations/HistoriesStorageOverview.vue
@@ -0,0 +1,14 @@
+<script setup>
+//TODO
+</script>
+<template>
+    <div>
+        <h2 class="text-center my-3">
+            <b>Histories Storage Overview</b>
+        </h2>
+        <p class="text-center">
+            TODO: This will contain an some Graphs displaying the storage taken by all the User histories (or the 'N'
+            bigger ones)
+        </p>
+    </div>
+</template>
---
+++ client/src/components/User/DiskUsage/Visualizations/HistoryStorageOverview.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+    historyId: {
+        type: String,
+        required: true,
+    },
+});
+//TODO
+</script>
+<template>
+    <div>
+        <h2 class="text-center my-3">
+            <b>History Storage Overview</b>
+        </h2>
+        <p class="text-center">
+            TODO: This will contain some Graphs displaying the storage taken by datasets in history:
+            <b>{{ props.historyId }}</b>
+        </p>
+    </div>
+</template>
