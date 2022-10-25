8c07195b5e8c46b78505db7f0cc0e6cb9cb53db6
Martin Cech <marten@bx.psu.edu>
2022-10-24 19:25:33 -0700

    update the search help box with new filtering option

--- client/src/components/Workflow/WorkflowList.vue
+++ client/src/components/Workflow/WorkflowList.vue
@@ -93,25 +93,27 @@ import SharingIndicators from "components/Indices/SharingIndicators";
 const helpHtml = `<div>
 <p>This textbox box can be used to filter the workflows displayed.
 
-<p>Text entered here will be searched against workflow names and tags. Additionally, advanced
-filtering tags can be used to refine the search more precisely. Tags are of the form
+<p>Text entered here will be searched against workflow names and workflow tags. Additionally, advanced
+filtering tags can be used to refine the search more precisely. Filtering tags are of the form
 <code>&lt;tag_name&gt;:&lt;tag_value&gt;</code> or <code>&lt;tag_name&gt;:'&lt;tag_value&gt;'</code>.
 For instance to search just for RNAseq in the workflow name, <code>name:rnsseq</code> can be used.
 Notice by default the search is not case-sensitive.
 
-If the quoted version of tag is used, the search is not case sensitive and only full matches will be
+If the quoted version of tag is used, the search is case sensitive and only full matches will be
 returned. So <code>name:'RNAseq'</code> would show only workflows named exactly <code>RNAseq</code>.
 
-<p>The available tags are:
+<p>The available filtering tags are:
 <dl>
     <dt><code>name</code></dt>
-    <dd>This filters only against the workflow name.</dd>
+    <dd>Shows workflows with given sequence of characters in their names.</dd>
     <dt><code>tag</code></dt>
-    <dd>This filters only against the workflow tag. You may also just click on a tag in your list of workflows to filter on that tag using this directly.</dd>
+    <dd>Shows workflows with the given workflow tag. You may also just click on a tag in your list of workflows to filter on that tag directly.</dd>
     <dt><code>is:published</code></dt>
-    <dd>This filters the workflows such that only published workflows are shown. You may also just click on the "published" icon of a workflow in your list to filter on this directly.</dd>
+    <dd>Shows published workflows. You may also just click on the "published" icon of a workflow in your list to filter on this directly.</dd>
     <dt><code>is:shared</code></dt>
-    <dd>This filters the workflows such that only workflows shared from another user directly with you are are shown. You may also just click on the "shared with me" icon of a workflow in your list to filter on this directly.</dd>
+    <dd>Shows workflows shared by another user directly with you. You may also just click on the "shared with me" icon of a workflow in your list to filter on this directly.</dd>
+    <dt><code>is:deleted</code></dt>
+    <dd>Shows deleted workflows.</dd>
 </dl>
 </div>
 `;
