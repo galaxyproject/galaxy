792935178fc16eb642cb001c53e4eddba3983db8
cat-bro <cathjb@gmail.com>
2022-10-26 14:45:10 +1100

    add test for /api/container_resolvers/resolve

--- lib/galaxy_test/api/test_container_resolution.py
+++ lib/galaxy_test/api/test_container_resolution.py
@@ -1,3 +1,4 @@
+from galaxy_test.base.populators import skip_without_tool
 from ._framework import ApiTestCase
 
 
@@ -11,3 +12,15 @@ class ContainerResolutionApiTestCase(ApiTestCase):
         response = self._get("container_resolvers/0", admin=True)
         assert response.status_code == 200
         assert isinstance(response.json(), dict)
+
+    @skip_without_tool("cat1")
+    def test_resolve(self):
+        tool_id = "cat1"
+
+        # no index
+        response = self._get(f"container_resolvers/resolve?tool_id={tool_id}", admin=True)
+        assert response.status_code == 200
+
+        # with index
+        response = self._get(f"container_resolvers/0/resolve?tool_id={tool_id}", admin=True)
+        assert response.status_code == 200
