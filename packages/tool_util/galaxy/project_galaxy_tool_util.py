7ff1026296332b048196a17f77dbf97717baf0d9
mvdbeek <m.vandenbeek@gmail.com>
2022-10-27 18:30:21 +0200

    Start work on galaxy-tool-util 22.1.4

--- packages/tool_util/galaxy/project_galaxy_tool_util.py
+++ packages/tool_util/galaxy/project_galaxy_tool_util.py
@@ -1,11 +1,11 @@
-__version__ = "22.1.3"
+__version__ = "22.1.4.dev0"
 
 PROJECT_NAME = "galaxy-tool-util"
 PROJECT_OWNER = PROJECT_USERAME = "galaxyproject"
 PROJECT_URL = "https://github.com/galaxyproject/galaxy"
-PROJECT_AUTHOR = 'Galaxy Project and Community'
-PROJECT_DESCRIPTION = 'Galaxy Tool and Tool Dependency Utilities'
-PROJECT_EMAIL = 'galaxy-committers@lists.galaxyproject.org'
+PROJECT_AUTHOR = "Galaxy Project and Community"
+PROJECT_DESCRIPTION = "Galaxy Tool and Tool Dependency Utilities"
+PROJECT_EMAIL = "galaxy-committers@lists.galaxyproject.org"
 RAW_CONTENT_URL = "https://raw.github.com/{}/{}/master/".format(
     PROJECT_USERAME, PROJECT_NAME
 )
