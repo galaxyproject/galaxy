3210a1961d8f40008dfe696b5ae353efe88f8f5f
Cameron Hyde <chyde@neoformit.com>
2022-11-01 09:49:26 +1000

    Refactor tests to use template activation-email.txt from codebase

--- test/unit/util/test_custom_templates.py
+++ test/unit/util/test_custom_templates.py
@@ -1,57 +1,59 @@
 """Test custom template configuration and rendering."""
 
-import os
-
 from galaxy.util.custom_templates import template
 
-TEMPLATE_FNAME = "default.html"  # This could also be a relative path e.g. mail/default.html
-TEMPLATE = """
-Here's my default template. It starts with a comment section to let admins
+TEMPLATE_RELPATH = "mail/activation-email.txt"
+CONTEXT = {
+    "name": "Jane Doe",
+    "user_email": "jane@example.com",
+    "date": "01-01-2021",
+    "hostname": "example.galaxy.org",
+    "activation_url": "https://example.galaxy.org/fake-activation-link",
+    "terms_url": "https://example.galaxy.org/terms",
+    "contact_email": "contact@example.galaxy.org",
+    "instance_resource_url": "https://example.galaxy.org/resources",
+    "expiry_days": 7,
+}
+CUSTOM_TEMPLATE = """
+Here's my custom template. It starts with a comment section to let admins
 know how to use it.
 
 >>>>>> The actual template starts from the next line
-<h1>{{ title }}</h1>
-{{ body }}
+This is my custom template!
+
+Name: {{ name }}
 
 """
-
-CONTEXT = {
-    "title": "foo",
-    "body": "bar",
-}
-
-EXPECTED_OUTPUT = "<h1>foo</h1>\nbar\n"
+DEFAULT_TEMPLATE_HEADER_SUBSTRING = "The following variables are available"
+DEFAULT_TEMPLATE_OUTPUT_SUBSTRING = f"Hello {CONTEXT['name']}"
+CUSTOM_TEMPLATE_OUTPUT_SUBSTRING = f"Name: {CONTEXT['name']}"
 
 
 def test_it_can_render_a_default_template(tmp_path):
-    try:
-        custom_templates_dir = tmp_path
-        template_path = template.DEFAULT_TEMPLATES_DIR / TEMPLATE_FNAME
-
-        with open(template_path, "w") as f:
-            f.write(TEMPLATE)
-
-        # It won't find a custom template but will fall back on the
-        # default we just created
-        output = template.render(
-            TEMPLATE_FNAME,
-            CONTEXT,
-            custom_templates_dir,
-        )
-        assert output == EXPECTED_OUTPUT
-    finally:
-        if template_path.exists():
-            os.remove(template_path)
-
-
-def test_it_can_render_a_custom_template(tmp_path):
+    """It should fall back on codebase default."""
     custom_templates_dir = tmp_path
-    with open(custom_templates_dir / TEMPLATE_FNAME, "w") as f:
-        f.write(TEMPLATE)
-    # It should find the custom template
     output = template.render(
-        TEMPLATE_FNAME,
+        TEMPLATE_RELPATH,
         CONTEXT,
         custom_templates_dir,
     )
-    assert output == EXPECTED_OUTPUT
+    # Make sure it's parsed out the doc header
+    assert DEFAULT_TEMPLATE_HEADER_SUBSTRING not in output
+    # Make sure the template content is correct
+    assert DEFAULT_TEMPLATE_OUTPUT_SUBSTRING in output
+
+
+def test_it_can_render_a_custom_template(tmp_path):
+    """It should use the custom template and ignore the default."""
+    custom_templates_dir = tmp_path
+    template_path = custom_templates_dir / TEMPLATE_RELPATH
+    template_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(template_path, "w") as f:
+        f.write(CUSTOM_TEMPLATE)
+    # It should find the custom template
+    output = template.render(
+        TEMPLATE_RELPATH,
+        CONTEXT,
+        custom_templates_dir,
+    )
+    assert CUSTOM_TEMPLATE_OUTPUT_SUBSTRING in output
