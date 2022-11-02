d079e611cf74bb822dc620abcb39d8c7575a7ecb
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-11-02 15:12:48 +0100

    fix drop event triggered outside of drop-zone

--- client/src/composables/useFileDrop.js
+++ client/src/composables/useFileDrop.js
@@ -39,7 +39,7 @@ export function useFileDrop(dropZone, onDrop, solo) {
         document.body,
         "drop",
         (event) => {
-            if (!dragBlocked.value) {
+            if (!dragBlocked.value && isFileOverDocument.value) {
                 // prevent the browser from opening the file
                 event.preventDefault();
 
