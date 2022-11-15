838c19cd6d131ce57a0e280202c9d2ad824ef218
davelopez <46503462+davelopez@users.noreply.github.com>
2022-11-15 13:59:50 +0100

    Do not force all datasets as discarded on import by default

--- lib/galaxy/managers/model_stores.py
+++ lib/galaxy/managers/model_stores.py
@@ -150,7 +150,6 @@ class ModelStoreManager:
 
     def import_model_store(self, request: ImportModelStoreTaskRequest):
         import_options = ImportOptions(
-            discarded_data=ImportDiscardedDataType.FORCE,
             allow_library_creation=request.for_library,
         )
         history_id = request.history_id
