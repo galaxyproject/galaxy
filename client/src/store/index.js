a46c7fe5b327fb3760ffeea7ae2bd378bcaeb8e8
mrajani <mohit.s.rajani@gmail.com>
2022-11-17 00:44:42 +0100

    feat: now frappe-gantt is a github dependecy fix: text-align center mistakely applied to entire app, got rid of patches and related dependency

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -70,7 +70,6 @@ export function createStore() {
             invocations: invocationStore,
             jobMetrics: jobMetricsStore,
             dbkeyStore: dbKeyStore,
-            accountingArray: jobMetricsStore,
             gridSearch: gridSearchStore,
             history: historyStore,
             historyItems: historyItemsStore,
