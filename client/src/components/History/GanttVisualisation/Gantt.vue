533ff3e320159106021453ebe41db46e11df9aca
Shubham Shubham <shubham4315@gmail.com>
2022-11-22 20:32:51 +0530

    Now the Bar colours use KeyedColorScheme which generates random colour based on the tag name hash value. The css class is generated and assigned dynamically to achieve this.

--- client/src/components/History/GanttVisualisation/Gantt.vue
+++ client/src/components/History/GanttVisualisation/Gantt.vue
@@ -8,6 +8,7 @@
         <button id="view" @click="changeMonthView">Month View</button>
         <button id="view" @click="changeHourView">Hour View</button>
         <button id="view" @click="changeMinuteView">Minute View</button>
+        <button id="view" @click="print1">Print</button>
     </div>
 </template>
 
@@ -16,6 +17,7 @@ import Gantt from "../../../../node_modules/frappe-gantt";
 import store from "../../../store";
 import { mapCacheActions } from "vuex-cache";
 import { mapGetters } from "vuex";
+import { keyedColorScheme } from "utils/color";
 
 export default {
     name: "Gantt",
@@ -61,6 +63,7 @@ export default {
             if (newArray.length > 0) {
                 var entries = [];
                 newArray.map((row, idx) => {
+                    createClassWithCSS(`.class-${row["id"]} .bar-progress`, `fill : ${keyedColorScheme(`random-${row["label"]}`)["primary"]} !important`);
                     entries.push({
                         id: idx.toString(),
                         job_id: row["id"],
@@ -68,7 +71,7 @@ export default {
                         start: row["startTime"],
                         end: row["endTime"],
                         progress: 100,
-                        custom_class: this.colors[idx % 4],
+                        custom_class: `class-${row["id"]}`,
                     });
                 });
                 this.gantt = new Gantt("#gantt", entries, {
@@ -152,8 +155,79 @@ export default {
         changeMinuteView: function () {
             this.gantt.change_view_mode("Minute");
         },
+        print1: function () {
+            console.log("Helllo",
+keyedColorScheme("test"))
+createClassWithCSS ('.hello', 'display:none')
+        },
     },
 };
+
+function createClassWithCSS (selector, style) {
+console.log(" callled with", selector);
+  if (!document.styleSheets) return;
+  if (document.getElementsByTagName('head').length == 0) return;
+
+  var styleSheet,mediaType;
+
+  if (document.styleSheets.length > 0) {
+    for (var i = 0, l = document.styleSheets.length; i < l; i++) {
+      if (document.styleSheets[i].disabled) 
+        continue;
+      var media = document.styleSheets[i].media;
+      mediaType = typeof media;
+
+      if (mediaType === 'string') {
+        if (media === '' || (media.indexOf('screen') !== -1)) {
+          styleSheet = document.styleSheets[i];
+        }
+      }
+      else if (mediaType=='object') {
+        if (media.mediaText === '' || (media.mediaText.indexOf('screen') !== -1)) {
+          styleSheet = document.styleSheets[i];
+        }
+      }
+
+      if (typeof styleSheet !== 'undefined') 
+        break;
+    }
+  }
+
+  if (typeof styleSheet === 'undefined') {
+    var styleSheetElement = document.createElement('style');
+    styleSheetElement.type = 'text/css';
+    document.getElementsByTagName('head')[0].appendChild(styleSheetElement);
+
+    for (i = 0; i < document.styleSheets.length; i++) {
+      if (document.styleSheets[i].disabled) {
+        continue;
+      }
+      styleSheet = document.styleSheets[i];
+    }
+
+    mediaType = typeof styleSheet.media;
+  }
+
+  if (mediaType === 'string') {
+    for (var i = 0, l = styleSheet.rules.length; i < l; i++) {
+      if(styleSheet.rules[i].selectorText && styleSheet.rules[i].selectorText.toLowerCase()==selector.toLowerCase()) {
+        styleSheet.rules[i].style.cssText = style;
+        return;
+      }
+    }
+    styleSheet.addRule(selector,style);
+  }
+  else if (mediaType === 'object') {
+    var styleSheetLength = (styleSheet.cssRules) ? styleSheet.cssRules.length : 0;
+    for (var i = 0; i < styleSheetLength; i++) {
+      if (styleSheet.cssRules[i].selectorText && styleSheet.cssRules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
+        styleSheet.cssRules[i].style.cssText = style;
+        return;
+      }
+    }
+    styleSheet.insertRule(selector + '{' + style + '}', styleSheetLength);
+  }
+}
 </script>
 
 <style>
@@ -183,11 +257,11 @@ export default {
 }
 
 .one .bar {
-    fill: #ff8e76 !important;
+    fill: rgb(33, 219, 92) !important;
 }
 
 .one .bar-progress {
-    fill: #d95b43 !important;
+    fill: rgb(33, 219, 92) !important;
 }
 
 .two .bar {
