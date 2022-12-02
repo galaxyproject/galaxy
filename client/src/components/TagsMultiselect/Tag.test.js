1ccf7bdf1e5fbb13ab7fe4fb1ac6c6649cb6a395
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-12-02 14:14:33 +0100

    expand and improve tag test

--- client/src/components/TagsMultiselect/Tag.test.js
+++ client/src/components/TagsMultiselect/Tag.test.js
@@ -14,72 +14,66 @@ const mountWithProps = (props) => {
 describe("Tag", () => {
     it("displays it's option", () => {
         {
-            const wrapper = mountWithProps({ option: "my_tag" });
-            const tag = wrapper.find(".tag");
+            const tag = mountWithProps({ option: "my_tag" });
             expect(tag.text()).toBe("my_tag");
         }
 
         {
-            const wrapper = mountWithProps({ option: "a_longer_tag_name" });
-            const tag = wrapper.find(".tag");
+            const tag = mountWithProps({ option: "a_longer_tag_name" });
             expect(tag.text()).toBe("a_longer_tag_name");
         }
     });
 
     it("shows it's clickable", async () => {
-        const wrapper = mountWithProps({ option: "my_tag" });
-        const tag = wrapper.find(".tag");
+        const tag = mountWithProps({ option: "my_tag" });
 
         expect(tag.classes()).not.toContain("clickable");
 
         tag.setProps({ clickable: true });
-        await wrapper.vm.$nextTick();
+        await tag.vm.$nextTick();
 
         expect(tag.classes()).toContain("clickable");
     });
 
     it("can be clicked", async () => {
-        const wrapper = mountWithProps({ option: "my_tag", clickable: true });
-        const tag = wrapper.find(".tag");
+        const tag = mountWithProps({ option: "my_tag", clickable: true });
 
         expect(tag.classes()).toContain("clickable");
 
         tag.trigger("click");
-        await wrapper.vm.$nextTick();
+        await tag.vm.$nextTick();
 
         expect(tag.emitted().click).toBeTruthy();
         expect(tag.emitted().click.length).toBe(1);
         expect(tag.emitted().click[0]).toEqual(["my_tag"]);
 
         tag.trigger("click");
-        await wrapper.vm.$nextTick();
+        await tag.vm.$nextTick();
 
         expect(tag.emitted().click.length).toBe(2);
         expect(tag.emitted().click).toStrictEqual([["my_tag"], ["my_tag"]]);
     });
 
     it("changes appearance when editable", async () => {
-        const wrapper = mountWithProps({ option: "my_tag" });
-        const tag = wrapper.find(".tag");
+        const tag = mountWithProps({ option: "my_tag" });
 
         expect(tag.classes()).not.toContain("editable");
         expect(tag.find(".tag-delete-button").exists()).not.toBe(true);
 
         tag.setProps({ editable: true });
-        await wrapper.vm.$nextTick();
+        await tag.vm.$nextTick();
 
         expect(tag.classes()).toContain("editable");
         expect(tag.find(".tag-delete-button").exists()).toBe(true);
     });
 
     it("can be deleted", async () => {
-        const wrapper = mountWithProps({ option: "my_tag", editable: true });
-        const tag = wrapper.find(".tag");
+        const tag = mountWithProps({ option: "my_tag", editable: true });
 
         expect(tag.find(".tag-delete-button").exists()).toBe(true);
 
         tag.find(".tag-delete-button").trigger("click");
-        await wrapper.vm.$nextTick();
+        await tag.vm.$nextTick();
 
         expect(tag.emitted().deleted).toBeTruthy();
         expect(tag.emitted().deleted.length).toBe(1);
@@ -100,5 +94,16 @@ describe("Tag", () => {
             const span = wrapper.find(".tag span");
             expect(span.classes()).toContain("font-weight-bold");
         }
+    });
+
+    it("highlights when searched", async () => {
+        const tag = mountWithProps({ option: "my_tag" });
+
+        expect(tag.classes()).not.toContain("searched");
+
+        tag.setProps({ search: "my_tag" });
+        await tag.vm.$nextTick();
+
+        expect(tag.classes()).toContain("searched");
     });
 });
