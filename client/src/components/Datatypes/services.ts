7cb88fa7501c5f40772b5557826620c2eaa6841f
mvdbeek <m.vandenbeek@gmail.com>
2022-12-05 13:08:34 +0100

    Move rethrowSimple into openapi-typescript-fetch middleware

--- client/src/components/Datatypes/services.ts
+++ client/src/components/Datatypes/services.ts
@@ -1,13 +1,8 @@
-import { rethrowSimple } from "utils/simple-error";
 import { fetcher } from "schema/fetcher";
 
 const getTypesAndMappings = fetcher.path("/api/datatypes/types_and_mapping").method("get").create();
 
 export async function getDatatypes(upload_only = true) {
-    try {
-        const response = await getTypesAndMappings({ upload_only });
-        return response.data;
-    } catch (e) {
-        rethrowSimple(e);
-    }
+    const response = await getTypesAndMappings({ upload_only });
+    return response.data;
 }
--- client/src/schema/fetcher.ts
+++ client/src/schema/fetcher.ts
@@ -1,6 +1,17 @@
 import { paths } from "./schema";
 import { Fetcher } from "openapi-typescript-fetch";
 import { getAppRoot } from "onload/loadConfig";
+import { Middleware } from "openapi-typescript-fetch";
+import { rethrowSimple } from "utils/simple-error";
+
+const rethrowSimpleMiddleware: Middleware = async (url, init, next) => {
+    try {
+        const response = await next(url, init);
+        return response;
+    } catch (e) {
+        rethrowSimple(e);
+    }
+};
 
 export const fetcher = Fetcher.for<paths>();
-fetcher.configure({ baseUrl: getAppRoot(undefined, true) });
+fetcher.configure({ baseUrl: getAppRoot(undefined, true), use: [rethrowSimpleMiddleware] });
