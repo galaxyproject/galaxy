e7a118425865dba2376f49eb203c543f858d72af
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-12-06 18:32:43 +0100

    use ts in script tag

--- client/src/components/TagsMultiselect/Tag.vue
+++ client/src/components/TagsMultiselect/Tag.vue
@@ -31,7 +31,7 @@ const named = computed(() => props.option?.startsWith("#"));
 const searched = computed(() => props.option?.toLowerCase() === props.search?.toLowerCase());
 </script>
 
-<script>
+<script lang="ts">
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
