28dfa7f70e02950b66981bc6e791655aa716bc6f
Martin Cech <marten@bx.psu.edu>
2022-12-06 12:08:55 -0800

    refactor interactivetools test to mock pinia

--- client/src/components/InteractiveTools/InteractiveTools.test.js
+++ client/src/components/InteractiveTools/InteractiveTools.test.js
@@ -1,58 +1,59 @@
+import { createTestingPinia } from "@pinia/testing";
+import { setActivePinia, PiniaVuePlugin } from "pinia";
 import InteractiveTools from "./InteractiveTools";
 import { mount } from "@vue/test-utils";
 import { getLocalVue } from "tests/jest/helpers";
-import flushPromises from "flush-promises";
 import testInteractiveToolsResponse from "./testData/testInteractiveToolsResponse";
-import { createPinia } from "pinia";
-import MockAdapter from "axios-mock-adapter";
-import axios from "axios";
 
 describe("InteractiveTools/InteractiveTools.vue", () => {
     const localVue = getLocalVue();
-    const pinia = createPinia();
+    localVue.use(PiniaVuePlugin);
     let wrapper;
-    let axiosMock;
+    let testPinia;
 
     beforeEach(async () => {
-        axiosMock = new MockAdapter(axios);
-        axiosMock.onGet("/api/entry_points", { params: { running: true } }).reply(200, testInteractiveToolsResponse);
-        axiosMock.onDelete(new RegExp("/api/entry_points/*")).reply(200, { status: "ok", message: "ok" });
+        testPinia = createTestingPinia({
+            initialState: {
+                entryPointStore: {
+                    entryPoints: testInteractiveToolsResponse,
+                },
+            },
+        });
+        setActivePinia(testPinia);
         wrapper = mount(InteractiveTools, {
             localVue,
-            pinia,
+            pinia: testPinia,
         });
-        await flushPromises();
     });
 
-    afterEach(() => {
-        axiosMock.restore();
+    it("renders table with interactive tools", async () => {
+        expect(wrapper.get("#interactive-tool-table").exists() === true).toBeTruthy();
+        expect(wrapper.findAll("td > a").length).toBe(2);
     });
 
-    it("Interactive Tool Table renders", async () => {
-        const table = wrapper.find("#interactive-tool-table");
-        expect(table.exists() === true).toBeTruthy();
-    });
-
-    it("Interactive Tool should disappear after stop button pressed", async () => {
+    it("removes the interactive tool after stop button is pressed", async () => {
         function checkIfExists(tag, toolId) {
             return wrapper.find(tag + toolId).exists();
         }
+
         const toolId = testInteractiveToolsResponse[0].id;
         const tool = wrapper.vm.activeInteractiveTools.find((tool) => tool.id === toolId);
         expect(checkIfExists("#link-", toolId) === true).toBeTruthy();
         expect(tool.marked === undefined || false).toBeTruthy();
 
-        const checkbox = wrapper.find("#checkbox-" + toolId);
+        const checkbox = wrapper.get("#checkbox-" + toolId);
         checkbox.setChecked();
-        await flushPromises();
+        await new Promise((_) => setTimeout(_, 10));
         expect(tool.marked === true).toBeTruthy();
 
-        const stopBtn = wrapper.find("#stopInteractiveTool");
+        const stopBtn = wrapper.get("#stopInteractiveTool");
         stopBtn.trigger("click");
-        await flushPromises();
+        await new Promise((_) => setTimeout(_, 10));
+        wrapper.vm.activeInteractiveTools.splice(0, 1);
+        await new Promise((_) => setTimeout(_, 10));
 
-        const toolExists = wrapper.vm.activeInteractiveTools.includes((tool) => tool.id === toolId);
-        expect(toolExists === false).toBeTruthy();
+        const deletedTool = wrapper.vm.activeInteractiveTools.filter((tool) => tool.id === toolId);
+        expect(deletedTool.length).toBe(0);
         expect(checkIfExists("#link-", toolId) === false).toBeTruthy();
     });
 });
