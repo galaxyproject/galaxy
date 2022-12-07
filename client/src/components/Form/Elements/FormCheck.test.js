b61454f62b0498fabf10b7b32d4e9a19917fdc40
hujambo-dunia <michelle.savage.1@gmail.com>
2022-12-07 14:50:09 -0500

    Frontend unit test for: blank values

--- client/src/components/Form/Elements/FormCheck.test.js
+++ client/src/components/Form/Elements/FormCheck.test.js
@@ -17,7 +17,7 @@ describe("FormCheck", () => {
         });
     });
 
-    it("basics", async () => {
+    it("Confirm 'n + 1' checkboxes created (eg. includes the Select-All). Confirm labels and values match. Confirm correct values emitted.", async () => {
         const noInput = wrapper.find("[type='checkbox']");
         expect(noInput.exists()).toBe(false);
         const n = 3;
@@ -35,6 +35,23 @@ describe("FormCheck", () => {
             expect(labels.at(i + 1).text()).toBe(`label_${i}`);
             expect(inputs.at(i + 1).attributes("value")).toBe(`value_${i}`);
             expectedValues.push(`value_${i}`);
+            expect(wrapper.emitted()["input"][i][0]).toEqual(expectedValues);
+        }
+    });
+    it("Confirm checkboxes are created when various 'empty values' are passed.", async () => {
+        const emptyValues = [0, null, false, true, undefined];
+        const options = [];
+        for (let i = 0; i < emptyValues.length; i++) {
+            options.push([`label_${i}`, emptyValues[i]]);
+        }
+        await wrapper.setProps({ options });
+        const inputs = wrapper.findAll("[type='checkbox']");
+        expect(inputs.length).toBe(emptyValues.length + 1);
+        const expectedValues = [];
+        for (let i = 0; i < emptyValues; i++) {
+            await inputs.at(i + 1).setChecked();
+            expect(inputs.at(i + 1).attributes("value")).toBe(emptyValues[i]);
+            expectedValues.push(expectedValues[i]);
             expect(wrapper.emitted()["input"][i][0]).toEqual(expectedValues);
         }
     });
