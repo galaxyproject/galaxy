e2c6ca5d35d91d4031b6788cb4b41236b82768ab
davelopez <46503462+davelopez@users.noreply.github.com>
2022-12-09 12:05:22 +0100

    Migrate history export services to typescript
    And use the generated OpenAPI types.

--- client/src/components/History/Export/services.ts
+++ client/src/components/History/Export/services.ts
@@ -1,13 +1,18 @@
-import axios from "axios";
-import { rethrowSimple } from "utils/simple-error";
-import { safePath } from "utils/redirect";
 import { ExportRecordModel } from "components/Common/models/exportRecordModel";
 import { DEFAULT_EXPORT_PARAMS } from "composables/shortTermStorage";
+import type { components } from "schema";
+import { fetcher } from "schema";
+
+type ModelStoreFormat = components["schemas"]["ModelStoreFormat"];
+
+const _getExportRecords = fetcher.path("/api/histories/{history_id}/exports").method("get").create();
+const _exportToFileSource = fetcher.path("/api/histories/{history_id}/write_store").method("post").create();
+const _importFromStoreAsync = fetcher.path("/api/histories/from_store_async").method("post").create();
 
 /**
  * A list of objects with the available export formats IDs and display names.
  */
-export const AVAILABLE_EXPORT_FORMATS: Array<{ id: string; name: string }> = [
+export const AVAILABLE_EXPORT_FORMATS: Array<{ id: ModelStoreFormat; name: string }> = [
     { id: "rocrate.zip", name: "RO-Crate" },
     { id: "tar.gz", name: "Compressed TGZ" },
 ];
@@ -18,20 +23,18 @@ export const AVAILABLE_EXPORT_FORMATS: Array<{ id: string; name: string }> = [
  * @param params query and pagination params
  * @returns a promise with a list of export records associated with the given history.
  */
-export async function getExportRecords(
-    historyId: string,
-    params: { offset?: number; limit?: number } = { offset: undefined, limit: undefined }
-): Promise<ExportRecordModel[]> {
-    const url = safePath(`/api/histories/${historyId}/exports`);
-    try {
-        const response = await axios.get(url, {
-            headers: { Accept: "application/vnd.galaxy.task.export+json" },
-            params: params,
-        });
-        return response.data.map((item) => new ExportRecordModel(item));
-    } catch (e) {
-        rethrowSimple(e);
-    }
+export async function getExportRecords(historyId: string) {
+    const response = await _getExportRecords(
+        {
+            history_id: historyId,
+        },
+        {
+            headers: {
+                Accept: "application/vnd.galaxy.task.export+json",
+            },
+        }
+    );
+    return response.data.map((item) => new ExportRecordModel(item));
 }
 
 /**
@@ -49,14 +52,15 @@ export async function exportToFileSource(
     exportParams = DEFAULT_EXPORT_PARAMS
 ) {
     const exportDirectoryUri = `${exportDirectory}/${fileName}.${exportParams.modelStoreFormat}`;
-    const writeStorePayload = {
+
+    return _exportToFileSource({
+        history_id: historyId,
         target_uri: exportDirectoryUri,
-        model_store_format: exportParams.modelStoreFormat,
+        model_store_format: exportParams.modelStoreFormat as ModelStoreFormat,
         include_files: exportParams.includeFiles,
         include_deleted: exportParams.includeDeleted,
         include_hidden: exportParams.includeHidden,
-    };
-    return axios.post(`/api/histories/${historyId}/write_store`, writeStorePayload);
+    });
 }
 
 /**
@@ -65,9 +69,8 @@ export async function exportToFileSource(
  * @returns A promise with the request response
  */
 export async function reimportHistoryFromRecord(record: ExportRecordModel) {
-    const writeStorePayload = {
+    return _importFromStoreAsync({
         store_content_uri: record.importUri,
         model_store_format: record.modelStoreFormat,
-    };
-    return axios.post(`/api/histories/from_store_async`, writeStorePayload);
+    });
 }
