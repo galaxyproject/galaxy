b24084319446a09afbb3629fe41c2fdc9094bbe6
John Chilton <jmchilton@gmail.com>
2022-12-12 15:51:48 -0500

    Small fixes to YAML export of OpenAPI schema.

--- scripts/dump_openapi_schema.py
+++ scripts/dump_openapi_schema.py
@@ -6,8 +6,21 @@ sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pa
 
 import click
 import yaml
+from pydantic.networks import AnyUrl, url_regex
 
 from galaxy.webapps.galaxy.fast_app import get_openapi_schema
+
+
+def _any_url_representer(dumper, data):
+    return dumper.represent_scalar("!anyurl", str(data))
+
+
+class YamlDumper(yaml.SafeDumper):
+    pass
+
+
+YamlDumper.add_representer(AnyUrl, _any_url_representer)
+YamlDumper.add_implicit_resolver("!anyurl", url_regex(), None)
 
 
 @click.command("Write openapi schema to path")
@@ -15,9 +28,9 @@ from galaxy.webapps.galaxy.fast_app import get_openapi_schema
 def write_open_api_schema(schema_path):
     openapi_schema = get_openapi_schema()
     if schema_path:
-        if schema_path.endswith((".yml", ".yml")):
+        if schema_path.endswith((".yml", ".yaml")):
             with open(schema_path, "w") as f:
-                yaml.safe_dump(openapi_schema, f)
+                yaml.dump(openapi_schema, f, YamlDumper)
         else:
             with open(schema_path, "w") as f:
                 json.dump(openapi_schema, f, sort_keys=True)
