5788be7acedd18c4bc9a447d03e00bd87ea4fa93
Dannon Baker <dannon.baker@gmail.com>
2022-12-13 12:26:52 -0500

    Convert Admin.vue to composition+ts

--- client/src/entry/analysis/modules/Admin.vue
+++ client/src/entry/analysis/modules/Admin.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { computed } from "vue";
 import { useConfig } from "@/composables/config";
 import AdminPanel from "@/components/admin/AdminPanel.vue";
