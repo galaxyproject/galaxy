8fac7bb4ddfdb5c888ace84b248876855558e1e7
davelopez <46503462+davelopez@users.noreply.github.com>
2022-12-15 10:59:45 +0100

    Migrate StorageDashboard component to composition API + TS

--- client/src/components/User/DiskUsage/StorageDashboard.vue
+++ client/src/components/User/DiskUsage/StorageDashboard.vue
@@ -1,52 +1,43 @@
+<script setup lang="ts">
+import _l from "@/utils/localization";
+import DiskUsageSummary from "./DiskUsageSummary.vue";
+import IconCard from "@/components/IconCard.vue";
+import { reactive } from "vue";
+import { useRouter } from "vue-router/composables";
+
+const router = useRouter();
+
+const texts = reactive({
+    title: _l("Storage Dashboard"),
+    subtitle: _l("Here you can see an overview of your disk usage status."),
+    freeSpace: {
+        title: _l("Is your usage more than expected?"),
+        description: _l("Find out what is eating up your space and learn how to easily and safely free up some of it."),
+        icon: "fas fa-broom fa-6x",
+        buttonText: _l("Free up disk usage"),
+    },
+});
+
+function goToStorageManager() {
+    router.push({ name: "StorageManager" });
+}
+</script>
+
 <template>
     <div>
         <header class="main-header">
             <h1 class="text-center my-3">
-                <b>{{ title }}</b>
+                <b>{{ texts.title }}</b>
             </h1>
-            <h2 class="text-center my-3 h-sm">{{ subtitle }}</h2>
+            <h2 class="text-center my-3 h-sm">{{ texts.subtitle }}</h2>
         </header>
-        <DiskUsageSummary class="m-3" />
-        <IconCard
+        <disk-usage-summary class="m-3" />
+        <icon-card
             class="mx-auto"
-            :title="freeSpaceData.title"
-            :description="freeSpaceData.description"
-            :icon="freeSpaceData.icon"
-            :button-text="freeSpaceData.buttonText"
+            :title="texts.freeSpace.title"
+            :description="texts.freeSpace.description"
+            :icon="texts.freeSpace.icon"
+            :button-text="texts.freeSpace.buttonText"
             @onButtonClick="goToStorageManager" />
     </div>
 </template>
-
-<script>
-import _l from "utils/localization";
-import DiskUsageSummary from "components/User/DiskUsage/DiskUsageSummary";
-import IconCard from "components/IconCard";
-
-export default {
-    components: {
-        DiskUsageSummary,
-        IconCard,
-    },
-    data() {
-        return {
-            title: _l("Storage Dashboard"),
-            subtitle: _l("Here you can see an overview of your disk usage status."),
-            freeSpaceData: {
-                title: _l("Is your usage more than expected?"),
-                description: _l(
-                    "Find out what is eating up your space and learn how to easily and safely free up some of it."
-                ),
-                icon: "fas fa-broom fa-6x",
-                buttonText: _l("Free up disk usage"),
-            },
-        };
-    },
-    methods: {
-        goToStorageManager() {
-            this.$router.push({
-                name: "StorageManager",
-            });
-        },
-    },
-};
-</script>
