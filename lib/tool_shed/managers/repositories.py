7ba099ab636500ac7aaa2f092fef6392ec1c6f7e
John Chilton <jmchilton@gmail.com>
2022-12-19 10:02:45 -0500

    Fix API uploads to do more work that happens in web uploads.
    Allows porting more of the tests to the API.

--- lib/tool_shed/managers/repositories.py
+++ lib/tool_shed/managers/repositories.py
@@ -7,7 +7,9 @@ from galaxy.exceptions import (
     InternalServerError,
     MalformedContents,
 )
+from galaxy.tool_shed.util import dependency_display
 from tool_shed.metadata import repository_metadata_manager
+from tool_shed.repository_types import util as rt_util
 from tool_shed.structured_app import ToolShedApp
 from tool_shed.util import hg_util
 from tool_shed.util.repository_content_util import upload_tar
@@ -47,6 +49,26 @@ def upload_tar_and_set_metadata(
             _, error_message = rmm.set_repository_metadata_due_to_new_tip(host, content_alert_str=content_alert_str)
             if error_message:
                 raise InternalServerError(error_message)
+            dd = dependency_display.DependencyDisplayer(app)
+            if str(repository.type) not in [
+                rt_util.REPOSITORY_SUITE_DEFINITION,
+                rt_util.TOOL_DEPENDENCY_DEFINITION,
+            ]:
+                # Provide a warning message if a tool_dependencies.xml file is provided, but tool dependencies
+                # weren't loaded due to a requirement tag mismatch or some other problem.  Tool dependency
+                # definitions can define orphan tool dependencies (no relationship to any tools contained in the
+                # repository), so warning messages are important because orphans are always valid.  The repository
+                # owner must be warned in case they did not intend to define an orphan dependency, but simply
+                # provided incorrect information (tool shed, name owner, changeset_revision) for the definition.
+                if repository.metadata_revisions:
+                    # A repository's metadata revisions are order descending by update_time, so the zeroth revision
+                    # will be the tip just after an upload.
+                    metadata_dict = repository.metadata_revisions[0].metadata
+                else:
+                    metadata_dict = {}
+                orphan_message = dd.generate_message_for_orphan_tool_dependencies(repository, metadata_dict)
+                if orphan_message:
+                    message += orphan_message
     else:
         raise InternalServerError(message)
     return message
