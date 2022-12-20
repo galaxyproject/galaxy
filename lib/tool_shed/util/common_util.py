e611a9566af06f064a75a6c58c25dcfb07f8b686
John Chilton <jmchilton@gmail.com>
2022-12-19 20:24:57 -0500

    Move generate_clone_url_for_repository_in_tool_shed into tool shed.

--- lib/tool_shed/util/common_util.py
+++ lib/tool_shed/util/common_util.py
@@ -1,8 +1,9 @@
+from routes import url_for
+
 from galaxy.util.tool_shed.common_util import (
     accumulate_tool_dependencies,
     check_tool_tag_set,
     generate_clone_url_for_installed_repository,
-    generate_clone_url_for_repository_in_tool_shed,
     generate_clone_url_from_repo_info_tup,
     get_protocol_from_tool_shed_url,
     get_repository_dependencies,
@@ -17,6 +18,18 @@ from galaxy.util.tool_shed.common_util import (
     remove_protocol_and_user_from_clone_url,
     remove_protocol_from_tool_shed_url,
 )
+
+
+def generate_clone_url_for_repository_in_tool_shed(user, repository) -> str:
+    """Generate the URL for cloning a repository that is in the tool shed."""
+    base_url = url_for("/", qualified=True).rstrip("/")
+    if user:
+        protocol, base = base_url.split("://")
+        username = f"{user.username}@"
+        return f"{protocol}://{username}{base}/repos/{repository.user.username}/{repository.name}"
+    else:
+        return f"{base_url}/repos/{repository.user.username}/{repository.name}"
+
 
 __all__ = (
     "accumulate_tool_dependencies",
