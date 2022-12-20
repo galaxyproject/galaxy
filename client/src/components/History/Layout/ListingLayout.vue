1c1feb8035fce75f7664c08e6b3def0cd3eabdfd
guerler <aysam.guerler@gmail.com>
2022-12-20 16:17:39 -0500

    Remove unused experimental code, already available in separate branch

--- client/src/components/History/Layout/ListingLayout.vue
+++ client/src/components/History/Layout/ListingLayout.vue
@@ -37,8 +37,6 @@ export default {
     data() {
         return {
             previousStart: undefined,
-            throttlePeriod: 20,
-            deltaMax: 20,
         };
     },
     watch: {
@@ -46,34 +44,7 @@ export default {
             this.$refs.listing.scrollToOffset(0);
         },
     },
-    created() {
-        this.onScrollThrottle = throttle((event) => {
-            this.onScroll(event);
-        }, this.throttlePeriod);
-    },
     methods: {
-        onScrollHandler(event) {
-            /* CURRENTLY UNUSED
-            // this avoids diagonal scrolling, we either scroll left/right or top/down
-            // both events are throttled and the default handler has been prevented.
-            if (Math.abs(event.deltaY) > Math.abs(event.deltaX)) {
-                // handle vertical scrolling with virtual scroller
-                const listing = this.$refs.listing;
-                const deltaMax = this.deltaMax;
-                const deltaY = Math.max(Math.min(event.deltaY, deltaMax), -deltaMax);
-                this.offset = Math.max(0, listing.getOffset() + deltaY);
-                this.$refs.listing.scrollToOffset(this.offset);
-            } else {
-                // dispatch horizontal scrolling as regular event
-                var wheelEvent = new WheelEvent("wheel", {
-                    deltaX: event.deltaX,
-                    bubbles: true,
-                    cancelable: false,
-                });
-                event.target.dispatchEvent(wheelEvent);
-            }
-            */
-        },
         onScroll() {
             const rangeStart = this.$refs.listing.range.start;
             if (this.previousStart !== rangeStart) {
