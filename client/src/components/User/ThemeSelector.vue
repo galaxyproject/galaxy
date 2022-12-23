5ef5a04b00d2072df9316f0e9ab735c0435bb6a0
guerler <aysam.guerler@gmail.com>
2022-12-23 17:23:45 -0500

    Add logo to theme selector masthead preview

--- client/src/components/User/ThemeSelector.vue
+++ client/src/components/User/ThemeSelector.vue
@@ -4,6 +4,7 @@ import { useConfig } from "@/composables/config";
 import { computed, watch, ref } from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPalette } from "@fortawesome/free-solid-svg-icons";
+import { safePath } from "@/utils/redirect";
 const { currentTheme, setCurrentTheme } = useCurrentTheme();
 const { config, isLoaded } = useConfig();
 const show = ref(false);
@@ -33,6 +34,7 @@ library.add(faPalette);
             <b-form-radio v-for="(theme, name) in config.themes" :key="name" :value="name" class="mb-2">
                 <span class="font-weight-bold mb-1">{{ name }}</span>
                 <div class="rounded p-2" :style="{ background: theme['--masthead-color'] }">
+                    <img :src="safePath(theme['--masthead-logo-img'])" alt="image" />
                     <span
                         v-localize
                         :style="{
@@ -65,3 +67,10 @@ library.add(faPalette);
         </b-form-radio-group>
     </b-card>
 </template>
+<style scoped>
+img {
+    display: inline;
+    border: none;
+    height: 2rem;
+}
+</style>
