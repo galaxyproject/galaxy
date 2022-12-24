d005d25c8dcf6e8ae6fa6d1403337366914685ea
guerler <aysam.guerler@gmail.com>
2022-12-23 20:28:58 -0500

    Avoid using getComputeStyle, use scoped classes instead

--- client/src/components/User/ThemeSelector.vue
+++ client/src/components/User/ThemeSelector.vue
@@ -8,7 +8,6 @@ import { safePath } from "@/utils/redirect";
 const { currentTheme, setCurrentTheme } = useCurrentTheme();
 const { config, isLoaded } = useConfig();
 const show = ref(false);
-const rs = getComputedStyle(document.documentElement);
 const currentValue = computed({
     get: () => {
         return currentTheme;
@@ -17,15 +16,30 @@ const currentValue = computed({
         setCurrentTheme(theme);
     },
 });
+function getBackground(themeDetails, variantKey) {
+    const background = themeDetails[`--${variantKey}`];
+    const styles = {};
+    if (background) {
+        styles["background"] = background;
+    }
+    return styles;
+}
 function getImage(themeDetails, imageKey) {
     return themeDetails[`--masthead-${imageKey}`] ?? config.value.logo_src;
 }
 function getStyle(themeDetails, variantKey) {
     const backgroundKey = `--masthead-link-${variantKey}`;
     const colorKey = `--masthead-text-${variantKey}`;
-    const background = themeDetails[backgroundKey] ?? rs.getPropertyValue(backgroundKey);
-    const color = themeDetails[colorKey] ?? rs.getPropertyValue(colorKey);
-    return { background, color };
+    const background = themeDetails[backgroundKey];
+    const color = themeDetails[colorKey];
+    const styles = {};
+    if (background) {
+        styles["background"] = background;
+    }
+    if (color) {
+        styles["color"] = color;
+    }
+    return styles;
 }
 watch(
     () => isLoaded.value,
@@ -40,21 +54,48 @@ watch(
 library.add(faPalette);
 </script>
 <template>
-    <b-card :show="show">
+    <b-card :show="show" class="overflow-auto">
         <b-form-radio-group v-model="currentValue">
             <b-form-radio v-for="(themeDetails, theme) in config.themes" :key="theme" :value="theme" class="mb-2">
                 <span class="font-weight-bold mb-1">{{ theme }}</span>
-                <div class="rounded p-1" :style="{ background: themeDetails['--masthead-color'] }">
+                <div class="default-theme-masthead" :style="getBackground(themeDetails, 'masthead-color')">
                     <img :src="safePath(getImage(themeDetails, 'logo-img'))" alt="image" />
-                    <span v-localize :style="getStyle(themeDetails, 'color')" class="rounded p-1">Text</span>
-                    <span v-localize :style="getStyle(themeDetails, 'hover')" class="rounded p-1">Hover</span>
-                    <span v-localize :style="getStyle(themeDetails, 'active')" class="rounded p-1">Active</span>
+                    <span v-localize :style="getStyle(themeDetails, 'color')" class="default-theme-color"> Text </span>
+                    <span v-localize :style="getStyle(themeDetails, 'hover')" class="default-theme-hover"> Hover </span>
+                    <span v-localize :style="getStyle(themeDetails, 'active')" class="default-theme-active">
+                        Active
+                    </span>
                 </div>
             </b-form-radio>
         </b-form-radio-group>
     </b-card>
 </template>
-<style scoped>
+<style lang="scss" scoped>
+@import "~bootstrap/scss/bootstrap.scss";
+@import "custom_theme_variables.scss";
+.default-element {
+    @extend .rounded;
+    @extend .p-1;
+}
+.default-theme-masthead {
+    @extend .default-element;
+    background: $masthead-color;
+}
+.default-theme-color {
+    @extend .default-element;
+    color: $masthead-text-color;
+    background: $masthead-link-color;
+}
+.default-theme-hover {
+    @extend .default-element;
+    color: $masthead-text-hover;
+    background: $masthead-link-hover;
+}
+.default-theme-active {
+    @extend .default-element;
+    color: $masthead-text-active;
+    background: $masthead-link-active;
+}
 img {
     cursor: pointer;
     display: inline;
