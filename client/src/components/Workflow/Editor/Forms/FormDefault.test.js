cbf5671604c7d30f4988a677580f55883884212d
mvdbeek <m.vandenbeek@gmail.com>
2023-01-03 10:20:03 +0200

    Fix FormDefault tests

--- client/src/components/Workflow/Editor/Forms/FormDefault.test.js
+++ client/src/components/Workflow/Editor/Forms/FormDefault.test.js
@@ -2,45 +2,42 @@ import { mount } from "@vue/test-utils";
 import { getLocalVue } from "tests/jest/helpers";
 import FormDefault from "./FormDefault";
 import { ActiveOutputs } from "components/Workflow/Editor/modules/outputs";
+import { createTestingPinia } from "@pinia/testing";
 
 const localVue = getLocalVue();
 
 describe("FormDefault", () => {
     let wrapper;
-    const activeOutputs = new ActiveOutputs();
     const outputs = [
         { name: "output-name", label: "output-label" },
         { name: "other-name", label: "other-label" },
     ];
 
     beforeEach(() => {
-        activeOutputs.initialize(outputs);
         wrapper = mount(FormDefault, {
             propsData: {
                 datatypes: [],
-                getManager: () => {
-                    return {
-                        nodes: [],
-                    };
-                },
-                nodeId: "id",
-                nodeContentId: "id",
-                nodeLabel: "label",
-                nodeName: "node-title",
-                nodeType: "subworkflow",
-                nodeOutputs: outputs,
-                nodeActiveOutputs: activeOutputs,
-                configForm: {
+                step: {
+                    id: 0,
+                    contentId: "id",
+                    annotation: "annotation",
+                    label: "label",
+                    type: "subworkflow",
+                    configForm: {
+                        inputs: [],
+                    },
                     inputs: [],
+                    outputs,
                 },
             },
             localVue,
+            pinia: createTestingPinia(),
         });
     });
 
     it("check initial value and value change", async () => {
         const title = wrapper.find(".portlet-title-text").text();
-        expect(title).toBe("node-title");
+        expect(title).toBe("label");
         const inputCount = wrapper.findAll("input").length;
         expect(inputCount).toBe(3);
         const outputLabelCount = wrapper.findAll("#__label__output-name").length;
