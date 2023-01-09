8bed4e4f92ccde63c39a323037f9e585f50656f4
mvdbeek <m.vandenbeek@gmail.com>
2023-01-09 15:16:45 +0100

    Limit zoom control to 5 X

--- client/src/components/Workflow/Editor/ZoomControl.vue
+++ client/src/components/Workflow/Editor/ZoomControl.vue
@@ -61,9 +61,7 @@ const props = withDefaults(
 const emit = defineEmits(["onZoom", "reset-all"]);
 
 const zoomDefault = 1;
-const zoomLevels = [
-    0.1, 0.2, 0.25, 0.33, 0.5, 0.67, 0.75, 0.8, 0.9, 1, 1.1, 1.25, 1.33, 1.5, 2, 2.5, 3, 4, 5, 6, 7, 8, 9, 10,
-];
+const zoomLevels = [0.1, 0.2, 0.25, 0.33, 0.5, 0.67, 0.75, 0.8, 0.9, 1, 1.1, 1.25, 1.33, 1.5, 2, 2.5, 3, 4, 5];
 const isMin = computed(() => Math.round(props.zoomLevel * 100) == Math.round(zoomLevels[0] * 100));
 const isMax = computed(() => Math.round(props.zoomLevel * 100) == Math.round(zoomLevels.at(-1)! * 100));
 const zoomPercentage = computed(() => Math.round(props.zoomLevel * 100));
