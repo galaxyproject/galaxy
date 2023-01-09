a3899ff8fb34678081181ee87e401806cba5460f
guerler <aysam.guerler@gmail.com>
2023-01-09 13:13:37 -0500

    Prep recursive collection drilldown in details view

--- client/src/components/History/Content/GenericElement.vue
+++ client/src/components/History/Content/GenericElement.vue
@@ -1,36 +1,51 @@
 <template>
     <div>
-        <ContentItem
-            :id="item.element_index"
-            :item="item.object"
-            :name="item.element_identifier"
-            :is-dataset="item.element_type == 'hda'"
-            :expand-dataset="expandDataset"
-            @update:expand-dataset="expandDataset = $event"
-            @view-collection="viewCollection = !viewCollection" />
-        <GenericItem v-if="viewCollection" :item-id="item.object.id" :item-src="item.object.history_content_type" />
+        <div v-for="(item, index) in dsc.elements" :key="index">
+            <ContentItem
+                :id="item.element_index"
+                :item="item.object"
+                :name="item.element_identifier"
+                :is-dataset="item.element_type == 'hda'"
+                :expand-dataset="!!expandDatasets[item.id]"
+                @update:expand-dataset="viewDataset(item.id)"
+                @view-collection="viewCollection(item.id)" />
+            <div v-if="!!expandCollections[item.id]">
+                <GenericElementNode :dsc="item.object" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import Vue from "vue";
 import ContentItem from "./ContentItem";
+import { CollectionElementsProvider } from "components/providers/storeProviders";
 
 export default {
+    name: "GenericElementNode",
     components: {
+        CollectionElementsProvider,
         ContentItem,
-        GenericItem: () => import("components/History/Content/GenericItem"),
     },
     props: {
-        item: {
+        dsc: {
             type: Object,
             required: true,
         },
     },
     data() {
         return {
-            viewCollection: false,
-            expandDataset: false,
+            expandCollections: {},
+            expandDatasets: {},
         };
+    },
+    methods: {
+        viewCollection(itemId) {
+            Vue.set(this.expandCollections, itemId, !this.expandCollections[itemId]);
+        },
+        viewDataset(itemId) {
+            Vue.set(this.expandDatasets, itemId, !this.expandDatasets[itemId]);
+        },
     },
 };
 </script>
