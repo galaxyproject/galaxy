60ece4d5efa2ca12b736df11762737aa516e90f3
Shubham Shubham <shubham4315@gmail.com>
2023-01-10 19:16:10 +0530

    Removed unused datetimemodal component and test file update

--- client/src/components/History/GanttVisualisation/Gantt.test.js
+++ client/src/components/History/GanttVisualisation/Gantt.test.js
@@ -1,11 +1,9 @@
-import { mount } from "@vue/test-utils";
 import { shallowMount } from "@vue/test-utils";
 import flushPromises from "flush-promises";
 import { createPinia } from "pinia";
 import { getLocalVue } from "tests/jest/helpers";
 import store from "@/store";
 import Gantt from "./Gantt.vue";
-import DateTimeModal from "./DateTimeModal.vue";
 import MockCurrentUser from "components/providers/MockCurrentUser";
 import MockUserHistories from "components/providers/MockUserHistories";
 
@@ -33,7 +31,6 @@ const localVue = getLocalVue();
 
 describe("gantt component", () => {
     let wrapper;
-    let Modal;
 
     beforeEach(async () => {
         wrapper = shallowMount(Gantt, {
@@ -46,30 +43,9 @@ describe("gantt component", () => {
             localVue,
         });
         await flushPromises();
-        Modal = mount(DateTimeModal);
     });
 
     it("7 change view buttons should be visible", () => {
         expect(wrapper.findAll('[data-description="change view button"]').length).toBe(7);
-    });
-
-    it("Date Time modal should be visible after clicking on Minute View", async () => {
-        expect(wrapper.vm.openModal).toBe(false);
-        await wrapper.setData({ openModal: true });
-        expect(wrapper.vm.openModal).toBe(true);
-        expect(Modal.find("#dateTimeModal").exists()).toBe(true);
-        const mod = wrapper.findComponent(DateTimeModal);
-        expect(mod.isVisible()).toBe(true);
-    });
-
-    it("Date Time modal should be closed when its open and clicked on confirm date", async () => {
-        expect(wrapper.vm.openModal).toBe(false);
-        await wrapper.setData({ openModal: true });
-        expect(wrapper.vm.openModal).toBe(true);
-        expect(Modal.find("#dateTimeModal").exists()).toBe(true);
-        const mod = wrapper.findComponent(DateTimeModal);
-        expect(mod.isVisible()).toBe(true);
-        await wrapper.setData({ openModal: false });
-        expect(mod.isVisible()).toBe(false);
     });
 });
