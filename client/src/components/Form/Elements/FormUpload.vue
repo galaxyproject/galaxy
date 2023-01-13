6d002b75be0349e5429a949cfb2fd8aa746182f3
Assunta DeSanto <assuntad23@gmail.com>
2023-01-13 17:03:25 -0500

    initial set-up, route upload type to vue component

---
+++ client/src/components/Form/Elements/FormUpload.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+const props = defineProps({
+    value: {
+        type: Object,
+    },
+    target: {
+        type: Object,
+        default: null,
+    },
+});
+
+function readFile() {
+
+}
+
+
+</script>
+
+<script></script>
+
+<template>
+    <div> HERE I AM! I'M FORMUPLOAD
+    </div>
+</template>
