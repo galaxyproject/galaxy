854177109a2331001c7d948d1a657648d29f57f8
guerler <aysam.guerler@gmail.com>
2023-01-14 11:57:55 -0500

    Switch generic element wrapper to composition api

--- client/src/components/History/Content/GenericElement.vue
+++ client/src/components/History/Content/GenericElement.vue
@@ -1,3 +1,27 @@
+<script setup>
+import Vue from "vue";
+import ContentItem from "./ContentItem";
+import { ref } from "vue";
+
+const props = defineProps({
+    dsc: {
+        type: Object,
+        required: true,
+    },
+});
+
+const expandCollections = ref({});
+const expandDatasets = ref({});
+
+function viewCollection(itemId) {
+    Vue.set(this.expandCollections, itemId, !this.expandCollections[itemId]);
+}
+
+function viewDataset(itemId) {
+    Vue.set(this.expandDatasets, itemId, !this.expandDatasets[itemId]);
+}
+</script>
+
 <template>
     <div>
         <div v-for="(item, index) in dsc.elements" :key="index">
@@ -9,43 +33,9 @@
                 :expand-dataset="!!expandDatasets[item.id]"
                 @update:expand-dataset="viewDataset(item.id)"
                 @view-collection="viewCollection(item.id)" />
-            <div v-if="!!expandCollections[item.id]">
-                <GenericElementNode :dsc="item.object" />
+            <div v-if="!!expandCollections[item.id]" class="mx-3">
+                <GenericElement :dsc="item.object" />
             </div>
         </div>
     </div>
 </template>
-
-<script>
-import Vue from "vue";
-import ContentItem from "./ContentItem";
-import { CollectionElementsProvider } from "components/providers/storeProviders";
-
-export default {
-    name: "GenericElementNode",
-    components: {
-        CollectionElementsProvider,
-        ContentItem,
-    },
-    props: {
-        dsc: {
-            type: Object,
-            required: true,
-        },
-    },
-    data() {
-        return {
-            expandCollections: {},
-            expandDatasets: {},
-        };
-    },
-    methods: {
-        viewCollection(itemId) {
-            Vue.set(this.expandCollections, itemId, !this.expandCollections[itemId]);
-        },
-        viewDataset(itemId) {
-            Vue.set(this.expandDatasets, itemId, !this.expandDatasets[itemId]);
-        },
-    },
-};
-</script>
