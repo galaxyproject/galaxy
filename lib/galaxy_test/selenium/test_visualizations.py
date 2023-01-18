7f0684c02bffed7d046e30b139698f98f3f977e2
Dannon Baker <dannon.baker@gmail.com>
2023-01-18 16:44:42 -0500

    Add basic visualization test exercising nvd3_bar.  It exercises most of the basic parts of the chart entrypoint and rendering.

---
+++ lib/galaxy_test/selenium/test_visualizations.py
@@ -0,0 +1,18 @@
+from .framework import (
+    selenium_test,
+    SeleniumTestCase,
+)
+
+
+class TestVisualizations(SeleniumTestCase):
+    @selenium_test
+    def test_charts_framework(self):
+        self.register()
+        self.perform_upload(self.get_filename("1.sam"))
+        self.history_panel_wait_for_hid_ok(1)
+        dataset_component = self.history_panel_click_item_title(1, wait=True)
+        dataset_component.visualize_button.wait_for_and_click()
+
+        self.components.visualization.plugin_item(id="nvd3_bar").wait_for_and_click()
+
+        # wait for the chart to render and look for something.
