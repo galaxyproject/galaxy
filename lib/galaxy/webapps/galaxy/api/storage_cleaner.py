3110b2dc5267be8e8ae19d75031cc7c2f45ef31e
davelopez <46503462+davelopez@users.noreply.github.com>
2023-01-23 16:27:37 +0100

    Add FastAPI routes for dataset storage management

--- lib/galaxy/webapps/galaxy/api/storage_cleaner.py
+++ lib/galaxy/webapps/galaxy/api/storage_cleaner.py
@@ -70,3 +70,35 @@ class FastAPIStorageCleaner:
         **Warning**: This operation cannot be undone. All objects will be deleted permanently from the disk.
         """
         return self.service.cleanup_histories(trans, set(payload.item_ids))
+
+    @router.get(
+        "/api/storage/datasets/discarded/summary",
+        summary="Returns information with the total storage space taken by discarded datasets owned by the given user.",
+    )
+    def discarded_datasets_summary(
+        self,
+        trans: ProvidesHistoryContext = DependsOnTrans,
+    ) -> DiscardedItemsSummary:
+        return self.service.get_discarded_datasets_summary(trans)
+
+    @router.get(
+        "/api/storage/datasets/discarded",
+        summary="Returns discarded datasets owned by the given user. The results can be paginated.",
+    )
+    def discarded_datasets(
+        self,
+        trans: ProvidesHistoryContext = DependsOnTrans,
+    ) -> List[StoredItem]:
+        return self.service.get_discarded_datasets(trans)
+
+    @router.delete(
+        "/api/storage/datasets",
+        summary="Purges a set of datasets by ID from disk. The datasets must be owned by the user.",
+    )
+    def cleanup_datasets(
+        self, trans: ProvidesHistoryContext = DependsOnTrans, payload: CleanupStorageItemsRequest = Body(...)
+    ) -> StorageItemsCleanupResult:
+        """
+        **Warning**: This operation cannot be undone. All objects will be deleted permanently from the disk.
+        """
+        return self.service.cleanup_datasets(trans, set(payload.item_ids))
--- lib/galaxy/webapps/galaxy/services/storage_cleaner.py
+++ lib/galaxy/webapps/galaxy/services/storage_cleaner.py
@@ -5,6 +5,7 @@ from typing import (
 )
 
 from galaxy.managers.context import ProvidesHistoryContext
+from galaxy.managers.hdas import HDAStorageCleanerManager
 from galaxy.managers.histories import HistoryManager
 from galaxy.managers.users import UserManager
 from galaxy.webapps.galaxy.services.base import ServiceBase
@@ -19,9 +20,11 @@ class StorageCleanerService(ServiceBase):
         self,
         user_manager: UserManager,
         history_manager: HistoryManager,
+        hda_cleaner: HDAStorageCleanerManager,
     ):
         self.user_manager = user_manager
         self.history_manager = history_manager
+        self.hda_cleaner = hda_cleaner
 
     def get_discarded_histories_summary(self, trans: ProvidesHistoryContext):
         user = self.get_authenticated_user(trans)
@@ -36,3 +39,17 @@ class StorageCleanerService(ServiceBase):
     def cleanup_histories(self, trans: ProvidesHistoryContext, item_ids: Set[int]):
         user = self.get_authenticated_user(trans)
         return self.history_manager.cleanup_items(user, item_ids)
+
+    def get_discarded_datasets_summary(self, trans: ProvidesHistoryContext):
+        user = self.get_authenticated_user(trans)
+        return self.hda_cleaner.get_discarded_summary(user)
+
+    def get_discarded_datasets(
+        self, trans: ProvidesHistoryContext, offset: Optional[int] = None, limit: Optional[int] = None
+    ):
+        user = self.get_authenticated_user(trans)
+        return self.hda_cleaner.get_discarded(user, offset, limit)
+
+    def cleanup_datasets(self, trans: ProvidesHistoryContext, item_ids: Set[int]):
+        user = self.get_authenticated_user(trans)
+        return self.hda_cleaner.cleanup_items(user, item_ids)
