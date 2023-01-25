4d25ff64fc1db21196833c310025549c93d2095e
Assunta DeSanto <assuntad23@gmail.com>
2023-01-24 23:58:04 -0500

    adding spinner while file loads

--- client/src/components/Form/Elements/FormUpload.vue
+++ client/src/components/Form/Elements/FormUpload.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, defineEmits, reactive } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 const props = defineProps({
     value: {
@@ -13,15 +14,18 @@ const emit = defineEmits(["input"]);
 const currentValue = reactive({
     value: props.value,
 });
+const waiting = ref(false);
 
 function readFile() {
     const fileContent = file.value && file.value.files[0];
     var reader = new FileReader();
     if (fileContent) {
+        waiting.value = true;
         reader.onload = () => {
             const result = reader.result;
             emit("input", result);
             currentValue.value = result;
+            waiting.value = false;
         };
         reader.readAsText(fileContent);
     }
@@ -31,6 +35,10 @@ function readFile() {
 <template>
     <div>
         <input ref="file" type="file" class="mb-1" @change="readFile" />
+        <div v-if="waiting">
+            <font-awesome-icon icon="spinner" spin />
+            Uploading File ... 
+        </div>
         <textarea v-show="currentValue.value" v-model="currentValue.value" class="ui-textarea" disabled></textarea>
     </div>
 </template>
