df1e40229a41fe052d370c58a31ce7063a61b9c3
guerler <aysam.guerler@gmail.com>
2023-01-26 22:43:03 -0500

    Remove unused awaits, check label

--- client/src/components/Form/Elements/FormUpload.test.js
+++ client/src/components/Form/Elements/FormUpload.test.js
@@ -5,25 +5,26 @@ import FormUpload from "./FormUpload";
 const localVue = getLocalVue();
 
 describe("FormUpload", () => {
-    const mountFormUpload = async (props) =>
-        await mount(FormUpload, {
+    const mountFormUpload = (props) =>
+        mount(FormUpload, {
             propsData: props,
             localVue,
         });
 
     it("should display selected file in disabled textarea", async () => {
         const v = "H1, H2, H3\nv1, v2, v3";
-        const wrapper = await mountFormUpload({ value: v });
+        const wrapper = mountFormUpload({ value: v });
         const el = wrapper.find("textarea");
         expect(el.element.value).toEqual(v);
-        const isDisabled = el.element.disabled === true;
-        expect(isDisabled).toBe(true);
+        expect(el.element.disabled).toBe(true);
     });
 
     it("should not display text box if file has not been selected", async () => {
-        const v = "";
-        const wrapper = await mountFormUpload({ value: v });
+        const wrapper = mountFormUpload({ value: null });
         const el = wrapper.find("textarea");
         expect(el.isVisible()).toBe(false);
+        expect(el.text()).toBe("");
+        const noInput = wrapper.find("label");
+        expect(noInput.text()).toBe("No file chosen");
     });
 });
