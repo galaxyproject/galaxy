e7d738569c3215ece224d567e703f9dcc6b6121d
Matthias Bernt <m.bernt@ufz.de>
2023-01-29 13:50:13 +0100

    add unit test for is_a_yaml_with_class
    as used in planemo

---
+++ test/unit/tool_util/test_loader_directory.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+
+from galaxy.tool_util.loader_directory import is_a_yaml_with_class
+
+def test_is_a_yaml_with_class():
+    with tempfile.NamedTemporaryFile("w", suffix=".yaml", delete=False) as tf:
+        fname = tf.name
+        tf.write("""class: GalaxyWorkflow
+name: "Test Workflow"
+inputs:
+  - id: input1
+outputs:
+  - id: wf_output_1
+    outputSource: first_cat/out_file1
+steps:
+  - tool_id: cat
+    label: first_cat
+    in:
+      input1: input1""")
+
+    assert is_a_yaml_with_class(fname, ["GalaxyWorkflow"])
+    os.unlink(fname)
