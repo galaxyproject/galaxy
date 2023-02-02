b7ce288e874fecb5c5b5091305d85bd5c5659c90
Ahmed Awan <qe66653@umbc.edu>
2023-02-03 01:53:26 +0500

    Remove jobId from ToolForm success page

--- client/src/components/Tool/ToolSuccess.vue
+++ client/src/components/Tool/ToolSuccess.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed, onMounted } from "vue";
-import type { ComputedRef } from "vue";
 import { useJobStore } from "@/stores/jobStore";
 import { useRouter } from "vue-router/composables";
 import { getGalaxyInstance } from "@/app";
@@ -12,44 +11,19 @@ import Webhook from "@/components/Common/Webhook.vue";
 const jobStore = useJobStore();
 const router = useRouter();
 
-/* interfaces */
-interface Job {
-    id: string;
-}
-interface JobDef {
-    tool_id: string;
-}
-interface JobResponse {
-    produces_entry_points: boolean;
-    jobs: Array<Job>;
-}
-interface ResponseVal {
-    jobDef: JobDef;
-    jobResponse: JobResponse;
-    toolName: string;
-}
-
-/* props */
-const props = defineProps({
-    jobId: {
-        type: String,
-        required: true,
-    },
-});
-
-const responseVal: ComputedRef<ResponseVal> = computed(() => jobStore.getResponseForJobId(props.jobId));
+const responseVal = computed(() => jobStore.getLatestResponse);
 
 /* lifecyle */
 onMounted(() => {
-    // no response means no tool produced this job in this session (nothing in store)
-    if (!responseVal.value) {
-        router.push(`/jobs/${props.jobId}/view`);
+    // no response means no ToolForm ran in this session (nothing in store)
+    if (Object.keys(responseVal.value).length === 0) {
+        router.push(`/`);
     }
 });
 
-const jobDef: ComputedRef<JobDef> = computed(() => responseVal.value.jobDef);
-const jobResponse: ComputedRef<JobResponse> = computed(() => responseVal.value.jobResponse);
-const toolName: ComputedRef<string> = computed(() => responseVal.value.toolName);
+const jobDef = computed(() => responseVal.value.jobDef);
+const jobResponse = computed(() => responseVal.value.jobResponse);
+const toolName = computed(() => responseVal.value.toolName);
 
 // getGalaxyInstance is not reactive
 const configEnableRecommendations = getGalaxyInstance().config.enable_tool_recommendations;
@@ -57,7 +31,7 @@ const showRecommendation: boolean = [true, "true"].includes(configEnableRecommen
 </script>
 
 <template>
-    <section v-if="responseVal">
+    <section v-if="jobResponse && jobDef">
         <div v-if="jobResponse.produces_entry_points">
             <ToolEntryPoints v-for="job in jobResponse.jobs" :key="job.id" :job-id="job.id" />
         </div>
