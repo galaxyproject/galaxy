93d4ff1646a7f1f758c817558a12e9e3aa7c2db6
nuwang <2070605+nuwang@users.noreply.github.com>
2023-02-03 10:27:41 +0530

    Remove redundant s3 implementation

--- lib/galaxy/dependencies/__init__.py
+++ lib/galaxy/dependencies/__init__.py
@@ -245,10 +245,6 @@ class ConditionalDependencies:
     def check_fs_webdavfs(self):
         return "webdav" in self.file_sources
 
-    def check_fs_s3fs(self):
-        # pyfilesystem plugin access to s3
-        return "s3" in self.file_sources
-
     def check_fs_anvilfs(self):
         # pyfilesystem plugin access to terra on anvil
         return "anvil" in self.file_sources
