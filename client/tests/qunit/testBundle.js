69b5d043f8e85d377f096bcc4cafef92f73d0b90
Dannon Baker <dannon.baker@gmail.com>
2023-02-07 10:24:40 -0500

    Drop/move remaining qunit tests and materials

--- client/tests/qunit/testBundle.js
+++
@@ -1,6 +0,0 @@
-// Load all qunit tests into a single bundle.
-//var testsContext = require.context(".", true, /_tests$/);
-//testsContext.keys().forEach(testsContext);
-
-import "./tests/modal_tests";
-import "./tests/ui_tests";
