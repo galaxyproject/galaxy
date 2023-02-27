46035c11d93e7ecb514cbbaed6c7a6d758063319
davelopez <46503462+davelopez@users.noreply.github.com>
2023-02-27 12:45:38 +0100

    Move and extend notification schema models
    - Move models to schema/notifications.py
    - Extend the models to increase flexibility and usability of the API

--- lib/galaxy/schema/schema.py
+++ lib/galaxy/schema/schema.py
@@ -3268,27 +3268,6 @@ class PageSummaryBase(Model):
     )
 
 
-class NotificationResponseModel(BaseModel):
-    id: EncodedDatabaseIdField
-    content: str
-
-    class Config:
-        orm_mode = True
-
-
-class NotificationListResponseModel(BaseModel):
-    __root__: List[NotificationResponseModel]
-
-
-class NotificationCreateRequestModel(BaseModel):
-    content: str
-    user_ids: List[DecodedDatabaseIdField]
-
-
-class NotificationUpdateRequestModel(BaseModel):
-    content: str
-
-
 class MaterializeDatasetInstanceAPIRequest(Model):
     source: DatasetSourceType = Field(
         title="Source",
