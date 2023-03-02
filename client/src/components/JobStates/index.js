9e4d9c711196a09455908c1c2a46a1501d3c3508
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-03-02 09:57:20 +0100

    remove unused legacy component

--- client/src/components/JobStates/index.js
+++
@@ -1,4 +0,0 @@
-import CollectionJobStates from "./CollectionJobStates";
-import { mountVueComponent } from "utils/mountVueComponent";
-
-export const mountCollectionJobStates = mountVueComponent(CollectionJobStates);
