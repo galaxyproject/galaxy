cf746ae35a6eefcb5042820995959616a38dc06a
Dannon Baker <dannon.baker@gmail.com>
2023-03-06 12:04:16 -0500

    Swap $.ajax out for axios in utils

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -5,6 +5,8 @@
 
 import _ from "underscore";
 import $ from "jquery";
+import axios, { type AxiosError, type AxiosResponse } from "axios";
+
 import { getAppRoot } from "@/onload/loadConfig";
 import { getGalaxyInstance } from "@/app";
 import _l from "@/utils/localization";
@@ -116,99 +118,26 @@ export function textify(lst: string[] | string): string {
  * @param{Function} error   - Callback on error
  * @param{Boolean}  cache   - Use cached data if available
  */
-export function get(options) {
-    top.__utils__get__ = top.__utils__get__ || {};
-    const cache_key = JSON.stringify(options);
-    if (options.cache && top.__utils__get__[cache_key]) {
-        if (options.success) {
-            options.success(top.__utils__get__[cache_key]);
-        }
-        window.console.debug(`utils.js::get() - Fetching from cache [${options.url}].`);
-    } else {
-        request({
-            url: options.url,
-            data: options.data,
-            success: function (response) {
-                top.__utils__get__[cache_key] = response;
-                if (options.success) {
-                    options.success(response);
-                }
-            },
-            error: function (response, status) {
-                if (options.error) {
-                    options.error(response, status);
-                }
-            },
-        });
-    }
-}
-
-/**
- * Request handler
- * @param{String}   method  - Request method ['GET', 'POST', 'DELETE', 'PUT']
- * @param{String}   url     - Url request is made to
- * @param{Object}   data    - Data send to url
- * @param{Function} success - Callback on success
- * @param{Function} error   - Callback on error
- */
-interface requestOptions {
-    type: "GET" | "POST" | "DELETE" | "PUT";
+interface getOptions {
     url: string;
-    data: any;
-    success: (response: any) => void;
-    error: (response: any, status: any) => void;
-    complete: (response: any, status: any) => void;
+    data?: any;
+    success?: (data: AxiosResponse) => void;
+    error?: (error: AxiosError) => void;
 }
 
-export function request(options: requestOptions) {
-    // prepare ajax
-    const ajaxConfig = {
-        contentType: "application/json",
-        type: options.type || "GET",
-        data: options.data || {},
-        url: options.url,
-    };
-    // encode data into url
-    if (ajaxConfig.type == "GET" || ajaxConfig.type == "DELETE") {
-        if (!$.isEmptyObject(ajaxConfig.data)) {
-            ajaxConfig.url += ajaxConfig.url.indexOf("?") == -1 ? "?" : "&";
-            ajaxConfig.url += $.param(ajaxConfig.data, true);
-        }
-        ajaxConfig.data = null;
-    } else {
-        ajaxConfig.dataType = "json";
-        ajaxConfig.data = JSON.stringify(ajaxConfig.data);
-    }
-
-    // make request
-    $.ajax(ajaxConfig)
-        .done((response) => {
-            if (typeof response === "string") {
-                try {
-                    response = response.replace("Infinity,", '"Infinity",');
-                    response = $.parseJSON(response);
-                } catch (e) {
-                    console.debug(e);
-                }
-            }
+export function get(options: getOptions): void {
+    axios
+        .get(options.url, { params: options.data })
+        .then((response) => {
             if (options.success) {
-                options.success(response);
+                options.success(response.data);
             }
         })
-        .fail((response) => {
-            let response_text = null;
-            try {
-                response_text = $.parseJSON(response.responseText);
-            } catch (e) {
-                response_text = response.responseText;
-            }
+        .catch((error) => {
+            // I don't believe any consumers actually use this, just drop it as
+            // we remove this interface
             if (options.error) {
-                options.error(response_text, response.status);
-            }
-        })
-        .always(() => {
-            if (options.complete) {
-                options.complete();
+                options.error(error);
             }
         });
 }
@@ -230,7 +159,7 @@ export function cssGetAttribute(classname: string, name: string): string {
  * Load a CSS file
  * @param{String}   url - Url of CSS file
  */
-export function cssLoadFile(url: string): void{
+export function cssLoadFile(url: string): void {
     if (!$(`link[href^="${url}"]`).length) {
         $(`<link href="${getAppRoot()}${url}" rel="stylesheet">`).appendTo("head");
     }
