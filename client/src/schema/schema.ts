1d76ce13eeeb61b24f2f9faf7fb0a97d144bbddb
Dannon Baker <dannon.baker@gmail.com>
2023-03-08 07:58:20 -0500

    Rebuild client schema

--- client/src/schema/schema.ts
+++ client/src/schema/schema.ts
@@ -6508,9 +6508,9 @@ export interface components {
              * Share Option
              * @description User choice for sharing resources which its contents may be restricted:
              *  - None: The user did not choose anything yet or no option is needed.
-             *  - SharingOptions.make_public: The contents of the resource will be made publicly accessible.
-             *  - SharingOptions.make_accessible_to_shared: This will automatically create a new `sharing role` allowing protected contents to be accessed only by the desired users.
-             *  - SharingOptions.no_changes: This won't change the current permissions for the contents. The user which this resource will be shared may not be able to access all its contents.
+             *  - make_public: The contents of the resource will be made publicly accessible.
+             *  - make_accessible_to_shared: This will automatically create a new `sharing role` allowing protected contents to be accessed only by the desired users.
+             *  - no_changes: This won't change the current permissions for the contents. The user which this resource will be shared may not be able to access all its contents.
              */
             share_option?: components["schemas"]["SharingOptions"];
             /**
@@ -8894,7 +8894,7 @@ export interface operations {
          * @description Sets the permissions to manage a library folder.
          */
         parameters: {
-            /** @description Indicates what action should be performed on the Library. Currently only `LibraryFolderPermissionAction.set_permissions` is supported. */
+            /** @description Indicates what action should be performed on the Library. Currently only `set_permissions` is supported. */
             query?: {
                 action?: components["schemas"]["LibraryFolderPermissionAction"];
             };
