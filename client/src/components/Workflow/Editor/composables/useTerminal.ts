91fdd7b7019d24aae110fcb71f122bff6b6f4bae
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-03-09 13:14:09 +0100

    make isMappedOver composable remove console.log

--- client/src/components/Workflow/Editor/composables/useTerminal.ts
+++ client/src/components/Workflow/Editor/composables/useTerminal.ts
@@ -1,24 +1,23 @@
 import type { DatatypesMapperModel } from "@/components/Datatypes/model";
 import { terminalFactory } from "@/components/Workflow/Editor/modules/terminals";
 import type { Step, TerminalSource } from "@/stores/workflowStepStore";
-import { ref, watch, type Ref } from "vue";
+import { ref, watch, type Ref, computed } from "vue";
 
 export function useTerminal(
     stepId: Ref<Step["id"]>,
     terminalSource: Ref<TerminalSource>,
     datatypesMapper: Ref<DatatypesMapperModel>
 ) {
-    const terminal = ref();
-    const isMappedOver = ref(false);
+    const terminal: Ref<ReturnType<typeof terminalFactory> | null> = ref(null);
+    const isMappedOver = computed(() => terminal.value?.isMappedOver() ?? false);
+
     watch(
         [stepId, terminalSource, datatypesMapper],
         () => {
             // rebuild terminal if any of the tracked dependencies change
-            console.log("use Terminal terminal Factory");
             const newTerminal = terminalFactory(stepId.value, terminalSource.value, datatypesMapper.value);
             newTerminal.getInvalidConnectedTerminals();
             terminal.value = newTerminal;
-            isMappedOver.value = newTerminal.isMappedOver();
         },
         {
             immediate: true,
