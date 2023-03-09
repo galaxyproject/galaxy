f5d56a3df94517689173f8451011a3c59308a06d
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-03-09 12:31:31 +0100

    remove watchEffect

--- client/src/components/Workflow/Editor/composables/useTerminal.ts
+++ client/src/components/Workflow/Editor/composables/useTerminal.ts
@@ -1,7 +1,7 @@
 import type { DatatypesMapperModel } from "@/components/Datatypes/model";
 import { terminalFactory } from "@/components/Workflow/Editor/modules/terminals";
 import type { Step, TerminalSource } from "@/stores/workflowStepStore";
-import { ref, watchEffect, type Ref } from "vue";
+import { ref, watch, type Ref } from "vue";
 
 export function useTerminal(
     stepId: Ref<Step["id"]>,
@@ -10,12 +10,19 @@ export function useTerminal(
 ) {
     const terminal = ref();
     const isMappedOver = ref(false);
-    watchEffect(() => {
-        // rebuild terminal if any of the tracked dependencies change
-        const newTerminal = terminalFactory(stepId.value, terminalSource.value, datatypesMapper.value);
-        newTerminal.getInvalidConnectedTerminals();
-        terminal.value = newTerminal;
-        isMappedOver.value = newTerminal.isMappedOver();
-    });
+    watch(
+        [stepId, terminalSource, datatypesMapper],
+        () => {
+            // rebuild terminal if any of the tracked dependencies change
+            console.log("use Terminal terminal Factory");
+            const newTerminal = terminalFactory(stepId.value, terminalSource.value, datatypesMapper.value);
+            newTerminal.getInvalidConnectedTerminals();
+            terminal.value = newTerminal;
+            isMappedOver.value = newTerminal.isMappedOver();
+        },
+        {
+            immediate: true,
+        }
+    );
     return { terminal: terminal as Ref<ReturnType<typeof terminalFactory>>, isMappedOver };
 }
