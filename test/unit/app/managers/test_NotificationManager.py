a77617ad4ad1925c82232d8151a2a25322b1ea5a
davelopez <46503462+davelopez@users.noreply.github.com>
2023-03-10 15:19:51 +0100

    Add some more basic tests

--- test/unit/app/managers/test_NotificationManager.py
+++ test/unit/app/managers/test_NotificationManager.py
@@ -1,11 +1,22 @@
 import json
+from typing import List
 
 from galaxy.managers.notification import NotificationManager
+from galaxy.model import User
 from galaxy.schema.notifications import (
+    ActionLink,
+    BroadcastNotificationContent,
+    BroadcastNotificationCreateRequest,
     MessageNotificationContent,
+    NotificationCategorySettings,
+    NotificationChannelSettings,
     NotificationCreateData,
     NotificationCreateRequest,
     NotificationRecipients,
+    PersonalNotificationCategory,
+    UpdateUserNotificationPreferencesRequest,
+    UserNotificationPreferences,
+    UserNotificationUpdateRequest,
 )
 from .base import BaseTestCase
 
@@ -22,9 +33,127 @@ class TestNotificationManager(BaseTestCase):
 
     def test_create_notification_for_users(self):
         user = self.user_manager.create(**user2_data)
+        notification, notifications_sent = self._send_notification_to_users([user])
+
+        assert notifications_sent == 1
+        assert notification.id
+        assert notification.category == "message"
+        notification_content = json.loads(notification.content)
+        assert notification_content["category"] == "message"
+        assert notification_content["subject"] == "Testing Subject"
+        assert notification_content["message"] == "Testing Message"
+
+    def test_create_notification_broadcast(self):
+        request = BroadcastNotificationCreateRequest(
+            variant="warning",
+            source="testing",
+            content=BroadcastNotificationContent(
+                subject="Testing Broadcast Subject",
+                message="Testing Broadcast Message",
+                action_links=[
+                    ActionLink(
+                        action_name="Go to Poll",
+                        link="https://link_to_anonymous.test/poll",
+                    ),
+                ],
+            ),
+        )
+        notification = self.notification_manager.create_broadcast_notification(request)
+
+        assert notification.id
+        assert notification.category == "broadcast"
+        notification_content = json.loads(notification.content)
+        assert notification_content["category"] == "broadcast"
+        assert notification_content["subject"] == "Testing Broadcast Subject"
+        assert notification_content["message"] == "Testing Broadcast Message"
+        action_links = notification_content["action_links"]
+        assert len(action_links) == 1
+        assert action_links[0]["action_name"] == "Go to Poll"
+        assert action_links[0]["link"] == "https://link_to_anonymous.test/poll"
+
+    def test_get_user_notifications(self):
+        user = self.user_manager.create(**user2_data)
+        user_notifications = self.notification_manager.get_user_notifications(user)
+        assert len(user_notifications) == 0
+
+        self._send_notification_to_users([user])
+
+        user_notifications = self.notification_manager.get_user_notifications(user)
+        assert len(user_notifications) == 1
+
+    def test_get_user_notification(self):
+        user = self.user_manager.create(**user2_data)
+        notification, _ = self._send_notification_to_users([user])
+
+        user_notification = self.notification_manager.get_user_notification(user, notification.id)
+
+        assert user_notification
+        assert user_notification.id
+        assert user_notification.source == "testing"
+        assert user_notification.variant == "info"
+        assert user_notification.category == "message"
+        assert user_notification.publication_time is not None
+        assert user_notification.expiration_time is not None
+        assert user_notification.content
+        user_notification_content = json.loads(user_notification.content)
+        assert user_notification_content["category"] == "message"
+        assert user_notification_content["subject"] == "Testing Subject"
+        assert user_notification_content["message"] == "Testing Message"
+        assert user_notification.seen_time is None
+        assert user_notification.favorite is False
+        assert user_notification.deleted is False
+
+    def test_update_user_notifications(self):
+        user = self.user_manager.create(**user2_data)
+        notification, _ = self._send_notification_to_users([user])
+        user_notification = self.notification_manager.get_user_notification(user, notification.id)
+        assert user_notification.seen_time is None
+        assert user_notification.favorite is False
+        assert user_notification.deleted is False
+        request = UserNotificationUpdateRequest(seen=True, favorite=True)
+        self.notification_manager.update_user_notifications(user, set([notification.id]), request)
+        user_notification = self.notification_manager.get_user_notification(user, notification.id)
+        assert user_notification.seen_time is not None
+        assert user_notification.favorite is True
+
+    def test_get_user_notification_preferences_returns_default_preferences(self):
+        user = self.user_manager.create(**user2_data)
+        default_preferences = UserNotificationPreferences.default()
+
+        preferences = self.notification_manager.get_user_notification_preferences(user)
+
+        assert preferences == default_preferences
+
+    def test_update_user_notification_preferences(self):
+        user = self.user_manager.create(**user2_data)
+        preferences = self.notification_manager.get_user_notification_preferences(user)
+        message_preferences = preferences.__root__.get(PersonalNotificationCategory.message)
+        assert message_preferences
+        assert message_preferences.enabled is True
+        assert message_preferences.channels.push is True
+
+        update_request = UpdateUserNotificationPreferencesRequest(
+            preferences=UserNotificationPreferences(
+                __root__={
+                    PersonalNotificationCategory.message: NotificationCategorySettings(
+                        enabled=False,
+                        channels=NotificationChannelSettings(push=False),
+                    ),
+                }
+            )
+        )
+        updated_preferences = self.notification_manager.update_user_notification_preferences(user, update_request)
+
+        updated_preferences = self.notification_manager.get_user_notification_preferences(user)
+        message_preferences = updated_preferences.__root__.get(PersonalNotificationCategory.message)
+        assert message_preferences
+        assert message_preferences.enabled is False
+        assert message_preferences.channels.push is False
+
+    def _send_notification_to_users(self, users: List[User]):
         request = NotificationCreateRequest(
             recipients=NotificationRecipients.construct(
-                user_ids=[user.id],
+                user_ids=[user.id for user in users],
             ),
             notification=NotificationCreateData(
                 source="testing",
@@ -36,12 +165,4 @@ class TestNotificationManager(BaseTestCase):
             ),
         )
         notification, notifications_sent = self.notification_manager.create_notification_for_users(request)
-        assert notifications_sent == 1
-        assert notification.id
-        assert notification.category == "message"
-        notification_content = json.loads(notification.content)
-        assert notification_content["category"] == "message"
-        assert notification_content["subject"] == "Testing Subject"
-        assert notification_content["message"] == "Testing Message"
-
-    # TODO: Fix everything and test _get_all_recipient_users
+        return notification, notifications_sent
