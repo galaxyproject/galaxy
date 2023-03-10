682cdf16781473318da83c9620a0e5d59e004bbe
John Chilton <jmchilton@gmail.com>
2023-03-10 11:47:05 -0500

    Improve SelectPreferredStore.vue.

--- client/src/components/History/CurrentHistory/SelectPreferredStore.vue
+++ client/src/components/History/CurrentHistory/SelectPreferredStore.vue
@@ -19,28 +19,26 @@ const props = defineProps({
 const error = ref<string | null>(null);
 const selectedObjectStoreId = ref(props.history.preferred_object_store_id);
 
-const newDatasetsDescription = ref("New dataset outputs from tools and workflows executed in this history");
-const galaxySelectionDefaultTitle = ref("Use Galaxy Defaults");
-const galaxySelectionDefaultDescription = ref(
-    "Selecting this will reset Galaxy to default behaviors configured by your Galaxy administrator."
-);
-const userSelectionDefaultTitle = ref("Use Your User Preference Defaults");
-const userSelectionDefaultDescription = ref(
-    "Selecting this will cause the history to not set a default and to fallback to your user preference defined default."
-);
+const newDatasetsDescription = "New dataset outputs from tools and workflows executed in this history";
+const galaxySelectionDefaultTitle = "Use Galaxy Defaults";
+const galaxySelectionDefaultDescription =
+    "Selecting this will reset Galaxy to default behaviors configured by your Galaxy administrator.";
+const userSelectionDefaultTitle = "Use Your User Preference Defaults";
+const userSelectionDefaultDescription =
+    "Selecting this will cause the history to not set a default and to fallback to your user preference defined default.";
 
 const defaultOptionTitle = computed(() => {
     if (props.userPreferredObjectStoreId) {
-        return userSelectionDefaultTitle.value;
+        return userSelectionDefaultTitle;
     } else {
-        return galaxySelectionDefaultTitle.value;
+        return galaxySelectionDefaultTitle;
     }
 });
 const defaultOptionDescription = computed(() => {
     if (props.userPreferredObjectStoreId) {
-        return userSelectionDefaultDescription.value;
+        return userSelectionDefaultDescription;
     } else {
-        return galaxySelectionDefaultDescription.value;
+        return galaxySelectionDefaultDescription;
     }
 });
 
