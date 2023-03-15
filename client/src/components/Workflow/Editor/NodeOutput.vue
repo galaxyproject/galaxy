dfe671400b296ccc1aa38e7420ed65636c6abf07
mvdbeek <m.vandenbeek@gmail.com>
2023-03-15 22:02:54 +0100

    Fix toggling output visibility

--- client/src/components/Workflow/Editor/NodeOutput.vue
+++ client/src/components/Workflow/Editor/NodeOutput.vue
@@ -120,7 +120,7 @@ function onToggleActive() {
 
 function onToggleVisible() {
     const actionKey = `HideDatasetAction${props.output.name}`;
-    const step = stepStore.getStep(stepId.value);
+    const step = { ...stepStore.getStep(stepId.value) };
     if (isVisible.value) {
         step.post_job_actions = {
             ...step.post_job_actions,
