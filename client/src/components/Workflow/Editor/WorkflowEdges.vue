e6a05cc9c2da40a14ec144b5975a9ff0135d58e8
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-03-20 13:03:08 +0100

    convert WorkflowEdges to canvas
    Signed-off-by: Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>

--- client/src/components/Workflow/Editor/WorkflowEdges.vue
+++ client/src/components/Workflow/Editor/WorkflowEdges.vue
@@ -1,50 +1,97 @@
 <script setup lang="ts">
-import RawConnector from "./Connector.vue";
-import TerminalConnector from "./TerminalConnector.vue";
-import { computed } from "vue";
-import { useConnectionStore } from "@/stores/workflowConnectionStore";
+import { computed, ref, toRef, onMounted, type Ref } from "vue";
+import { Connection, useConnectionStore, type OutputTerminal } from "@/stores/workflowConnectionStore";
 import { storeToRefs } from "pinia";
 import type { TerminalPosition } from "@/stores/workflowEditorStateStore";
 import type { OutputTerminals } from "./modules/terminals";
+import { useDrawableConnection, setDrawableConnectionStyle } from "./composables/drawableConnection";
+import { useAnimationFrame } from "@/composables/sensors/animationFrame";
 
 const props = defineProps<{
     draggingConnection: TerminalPosition | null;
     draggingTerminal: OutputTerminals | null;
+    transform: { x: number; y: number; k: number };
 }>();
+
+const canvas: Ref<HTMLCanvasElement | null> = ref(null);
+const context = computed(() => canvas.value?.getContext("2d") ?? null);
 
 const connectionStore = useConnectionStore();
 const { connections } = storeToRefs(connectionStore);
 
-const dragStyle = computed(() => {
-    // TODO: this isn't quite right, but 6 seems to work, probably because it's the outer strokewidth
-    // sync up with Connector.vue ?
-    const strokeWidth = 6;
-    const dragStyle: { [index: string]: string } = {};
-    if (props.draggingConnection) {
-        dragStyle.left = props.draggingConnection.endX - strokeWidth + "px";
-        dragStyle.top = props.draggingConnection.endY - strokeWidth + "px";
-    }
-    return dragStyle;
+const drawableConnections = computed(() => {
+    return connections.value.map((c) => useDrawableConnection(c));
 });
 
-const dragIsOptional = computed(() => {
-    return props.draggingTerminal?.optional;
+onMounted(() => {
+    const element = canvas.value!;
+    const style = getComputedStyle(element);
+
+    setDrawableConnectionStyle({
+        connectionColor: style.getPropertyValue("--connection-color"),
+        connectionColorInvalid: style.getPropertyValue("--connection-color-invalid"),
+    });
+});
+
+const drawableDraggingConnection = computed(() => {
+    if (props.draggingConnection && props.draggingTerminal) {
+        const connection = new Connection(
+            {
+                connectorType: "input",
+                name: "draggingInput",
+                stepId: -1,
+            },
+            props.draggingTerminal as unknown as OutputTerminal
+        );
+
+        return useDrawableConnection(connection, toRef(props, "draggingConnection"));
+    } else {
+        return null;
+    }
+});
+
+useAnimationFrame(() => {
+    const ctx = context.value;
+    if (!ctx) {
+        return;
+    }
+
+    ctx.canvas.width = ctx.canvas.offsetWidth;
+    ctx.canvas.height = ctx.canvas.offsetHeight;
+
+    ctx.resetTransform();
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.translate(props.transform.x, props.transform.y);
+    ctx.scale(props.transform.k, props.transform.k);
+
+    drawableConnections.value.forEach((connection) => connection.draw(ctx));
+
+    if (drawableDraggingConnection.value) {
+        drawableDraggingConnection.value.draw(ctx);
+    }
 });
 </script>
+
 <template>
-    <div>
-        <div v-if="draggingConnection" class="drag-terminal" :style="dragStyle" />
-        <svg class="canvas-svg node-area">
-            <raw-connector
-                v-if="draggingTerminal && draggingConnection"
-                :position="draggingConnection"
-                :input-is-mapped-over="false"
-                :output-is-mapped-over="draggingTerminal.mapOver.isCollection"
-                :nullable="dragIsOptional"></raw-connector>
-            <terminal-connector
-                v-for="connection in connections"
-                :key="connection.id"
-                :connection="connection"></terminal-connector>
-        </svg>
+    <div class="workflow-edges">
+        <canvas ref="canvas" class="workflow-edges-canvas"></canvas>
     </div>
 </template>
+
+<style lang="scss" scoped>
+@import "~bootstrap/scss/_functions.scss";
+@import "theme/blue.scss";
+
+.workflow-edges-canvas {
+    --connection-color: #{$brand-primary};
+    --connection-color-invalid: #{$brand-warning};
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
