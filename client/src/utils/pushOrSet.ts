0ed0a1e2e4580fe13b3ce6dc7aa99b6f79153815
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-03-20 13:21:38 +0100

    use more strict implementation of pushOrSet
    Signed-off-by: Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>

--- client/src/utils/pushOrSet.ts
+++ client/src/utils/pushOrSet.ts
@@ -1,6 +1,12 @@
-export function pushOrSet(object: { [index: string | number]: any[] }, key: string | number, value: any) {
+/**
+ * Pushes a value to an array in an object, if the array exists. Else creates a new array containing value.
+ * @param object Object which contains array
+ * @param key Key which array is in
+ * @param value Value to push
+ */
+export function pushOrSet<T, K extends string | number | symbol>(object: { [key in K]: Array<T> }, key: K, value: T) {
     if (key in object) {
-        object[key].push(value);
+        object[key]!.push(value);
     } else {
         object[key] = [value];
     }
