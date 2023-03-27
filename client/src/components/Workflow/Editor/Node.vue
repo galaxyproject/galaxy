32e8b2697757250cdfe552d92ea384986c5698c4
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-03-27 12:40:45 +0200

    add thin wrapper to Recommendations allows toggling Recommendations using v-if

--- client/src/components/Workflow/Editor/Node.vue
+++ client/src/components/Workflow/Editor/Node.vue
@@ -49,7 +49,13 @@
                     triggers="hover"
                     placement="bottom"
                     :show.sync="popoverShow">
-                    <Recommendations :step-id="id" :datatypes-mapper="datatypesMapper" @onCreate="onCreate" />
+                    <div>
+                        <Recommendations
+                            v-if="popoverShow"
+                            :step-id="id"
+                            :datatypes-mapper="datatypesMapper"
+                            @onCreate="onCreate" />
+                    </div>
                 </b-popover>
             </b-button-group>
             <i :class="iconClass" />
