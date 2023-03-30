dacc38698b42510dd9e66b572e5faa02fc646769
John Davis <jdavcs@gmail.com>
2023-03-29 23:34:14 -0400

    Add utility function for enum member values + test

--- lib/galaxy/util/__init__.py
+++ lib/galaxy/util/__init__.py
@@ -1905,3 +1905,11 @@ class StructuredExecutionTimer:
     @property
     def elapsed(self):
         return time.time() - self.begin
+
+
+def enum_values(enum_class):
+    """
+    Return a list of member values of enumeration enum_class.
+    Values are in member definition order.
+    """
+    return [item.value for key, item in enum_class.__members__.items()]
