a8afa13d70d47367a468aca9ca90804f06534243
John Davis <jdavcs@gmail.com>
2023-03-31 18:02:53 -0400

    Remove reference to model.view from revision script

--- lib/galaxy/model/migrations/alembic/versions_gxy/c39f1de47a04_add_skipped_state_to_collection_job_.py
+++ lib/galaxy/model/migrations/alembic/versions_gxy/c39f1de47a04_add_skipped_state_to_collection_job_.py
@@ -16,8 +16,6 @@ from sqlalchemy import text as sql_text
 from sqlalchemy.engine.url import make_url
 from sqlalchemy.sql.elements import TextClause
 
-from galaxy.model.view import HistoryDatasetCollectionJobStateSummary
-
 # revision identifiers, used by Alembic.
 revision = "c39f1de47a04"
 down_revision = "3100452fa030"
@@ -36,6 +34,43 @@ SELECT
     SUM(CASE WHEN state = 'error' THEN 1 ELSE 0 END) AS error,
     SUM(CASE WHEN state = 'failed' THEN 1 ELSE 0 END) AS failed,
     SUM(CASE WHEN state = 'paused' THEN 1 ELSE 0 END) AS paused,
+    SUM(CASE WHEN state = 'deleted' THEN 1 ELSE 0 END) AS deleted,
+    SUM(CASE WHEN state = 'deleted_new' THEN 1 ELSE 0 END) AS deleted_new,
+    SUM(CASE WHEN state = 'upload' THEN 1 ELSE 0 END) AS upload,
+    SUM(CASE WHEN job_id IS NOT NULL THEN 1 ELSE 0 END) AS all_jobs
+FROM (
+    SELECT hdca.id AS hdca_id, job.id AS job_id, job.state as state
+    FROM history_dataset_collection_association hdca
+    LEFT JOIN implicit_collection_jobs icj
+        ON icj.id = hdca.implicit_collection_jobs_id
+    LEFT JOIN implicit_collection_jobs_job_association icjja
+        ON icj.id = icjja.implicit_collection_jobs_id
+    LEFT JOIN job
+        ON icjja.job_id = job.id
+
+    UNION
+
+    SELECT hdca.id AS hdca_id, job.id AS job_id, job.state AS state
+    FROM history_dataset_collection_association hdca
+    LEFT JOIN job
+        ON hdca.job_id = job.id
+) jobstates
+GROUP BY jobstates.hdca_id
+"""
+
+NEW_AGGREGATE_QUERY = """
+SELECT
+    hdca_id,
+    SUM(CASE WHEN state = 'new' THEN 1 ELSE 0 END) AS new,
+    SUM(CASE WHEN state = 'resubmitted' THEN 1 ELSE 0 END) AS resubmitted,
+    SUM(CASE WHEN state = 'waiting' THEN 1 ELSE 0 END) AS waiting,
+    SUM(CASE WHEN state = 'queued' THEN 1 ELSE 0 END) AS queued,
+    SUM(CASE WHEN state = 'running' THEN 1 ELSE 0 END) AS running,
+    SUM(CASE WHEN state = 'ok' THEN 1 ELSE 0 END) AS ok,
+    SUM(CASE WHEN state = 'error' THEN 1 ELSE 0 END) AS error,
+    SUM(CASE WHEN state = 'failed' THEN 1 ELSE 0 END) AS failed,
+    SUM(CASE WHEN state = 'paused' THEN 1 ELSE 0 END) AS paused,
+    SUM(CASE WHEN state = 'skipped' THEN 1 ELSE 0 END) AS skipped,
     SUM(CASE WHEN state = 'deleted' THEN 1 ELSE 0 END) AS deleted,
     SUM(CASE WHEN state = 'deleted_new' THEN 1 ELSE 0 END) AS deleted_new,
     SUM(CASE WHEN state = 'upload' THEN 1 ELSE 0 END) AS upload,
@@ -88,7 +123,7 @@ def get_view_instance(definition: str):
 
 
 def upgrade():
-    view = get_view_instance(HistoryDatasetCollectionJobStateSummary.aggregate_state_query)
+    view = get_view_instance(NEW_AGGREGATE_QUERY)
     # op.replace_entity comes from alembic_utils plugin
     op.replace_entity(view)  # type: ignore[attr-defined]
 
