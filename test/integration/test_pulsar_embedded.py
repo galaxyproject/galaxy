2f9b7212e076a5e19c43892c099f2118e4df2a97
mvdbeek <m.vandenbeek@gmail.com>
2023-04-10 16:43:16 +0200

    Drop test tool that requires new pulsar release

--- test/integration/test_pulsar_embedded.py
+++ test/integration/test_pulsar_embedded.py
@@ -35,6 +35,5 @@ test_tools = integration_util.integration_tool_runner(
         "tool_provided_metadata_9",
         "simple_constructs_y",
         "composite_output",
-        "tool_provided_metadata_12",
     ]
 )
