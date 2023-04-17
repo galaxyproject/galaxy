8e4a7e187775a6cb8ce778ec4d9edfdc0fb4fb84
guerler <aysam.guerler@gmail.com>
2023-04-17 21:39:25 +0300

    Add margin to scroll helper to improve error highlighting

--- client/src/components/Form/FormDisplay.vue
+++ client/src/components/Form/FormDisplay.vue
@@ -195,7 +195,7 @@ export default {
                     if (element) {
                         const centerPanel = document.querySelector("#center");
                         if (centerPanel) {
-                            centerPanel.scrollTo(0, this.getOffsetTop(element));
+                            centerPanel.scrollTo(0, this.getOffsetTop(element) - 50);
                         }
                     }
                 }
