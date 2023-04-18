a543a111839881a682cc7e8b3eb259d339d57877
davelopez <46503462+davelopez@users.noreply.github.com>
2023-04-18 19:11:18 +0200

    Add basic history model type alias from schema

---
+++ client/src/components/History/model/index.ts
@@ -0,0 +1,4 @@
+import type { components } from "@/schema";
+
+export type HistorySummaryModel = components["schemas"]["HistorySummary"];
+export type HistoryDetailedModel = components["schemas"]["HistoryDetailed"];
