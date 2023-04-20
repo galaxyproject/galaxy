db34a2da042c960b9749fdac5d1bd1997a32f1b3
Martin Cech <marten@bx.psu.edu>
2023-04-20 14:02:50 -0700

    disable buttons that will fail for anons

--- client/src/components/Workflow/WorkflowIndexActions.vue
+++ client/src/components/Workflow/WorkflowIndexActions.vue
@@ -1,10 +1,10 @@
 <template>
     <span>
-        <b-button id="workflow-create" class="m-1" @click="navigateToCreate">
+        <b-button id="workflow-create" v-b-tooltip.hover aria-haspopup="true" :title="createTitle" :disabled="isAnonymous" class="m-1" @click="navigateToCreate">
             <font-awesome-icon icon="plus" />
             {{ "Create" | localize }}
         </b-button>
-        <b-button id="workflow-import" class="m-1" @click="navigateToImport">
+        <b-button id="workflow-import" v-b-tooltip.hover aria-haspopup="true" :title="importTitle" :disabled="isAnonymous" class="m-1" @click="navigateToImport">
             <font-awesome-icon icon="upload" />
             {{ "Import" | localize }}
         </b-button>
@@ -14,6 +14,7 @@
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { BButton } from "bootstrap-vue";
+import { getGalaxyInstance } from "@/app";
 
 import { faPlus, faUpload } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -23,6 +24,17 @@ export default {
     components: {
         BButton,
         FontAwesomeIcon,
+    },
+    computed: {
+        isAnonymous() {
+            return getGalaxyInstance().user?.isAnonymous();
+        },
+        createTitle() {
+            return this.isAnonymous ? "Please log in or register to use this feature" : "Create a new workflow";
+        },
+        importTitle() {
+            return this.isAnonymous ? "Please log in or register to use this feature" : "Import a workflow from URL or registry";
+        },
     },
     methods: {
         navigateToCreate: function () {
