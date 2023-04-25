2ebdc2ab534a0c01ddb60dbf246854b233cf1161
Alireza Heidari <itisalirh@gmail.com>
2023-04-25 16:45:42 +0200

    replace `UserHistories` provider in `HistoryView.vue` and some improvements

--- client/src/components/History/HistoryView.vue
+++ client/src/components/History/HistoryView.vue
@@ -1,49 +1,40 @@
 <template>
-    <UserHistories v-if="currentUser" v-slot="{ currentHistory, handlers }" :user="currentUser">
-        <div v-if="history" class="d-flex flex-column h-100">
-            <b-alert v-if="history.purged" variant="info" show>This history has been purged.</b-alert>
-            <div v-else class="flex-row flex-grow-0">
-                <b-button
-                    v-if="currentUser.id == history.user_id"
-                    size="sm"
-                    variant="outline-info"
-                    title="Switch to this history"
-                    :disabled="currentHistory.id == history.id"
-                    @click="handlers.setCurrentHistory(history)">
-                    Switch to this history
-                </b-button>
-                <b-button
-                    v-else
-                    v-b-modal:copy-history-modal
-                    size="sm"
-                    variant="outline-info"
-                    title="Import this history">
-                    Import this history
-                </b-button>
-            </div>
-            <CollectionPanel
-                v-if="selectedCollections.length && selectedCollections[0].history_id == id"
-                :history="history"
-                :selected-collections.sync="selectedCollections"
-                :show-controls="false"
-                @view-collection="onViewCollection" />
-            <HistoryPanel
-                v-else
-                :history="history"
-                :writable="currentUser.id == history.user_id"
-                :show-controls="false"
-                v-on="handlers"
-                @view-collection="onViewCollection" />
-            <CopyModal id="copy-history-modal" :history="history" />
+    <div v-if="currentUser && history" class="d-flex flex-column h-100">
+        <b-alert v-if="history.purged" variant="info" show>This history has been purged.</b-alert>
+        <div v-else class="flex-row flex-grow-0">
+            <b-button
+                v-if="currentUser.id == history.user_id"
+                size="sm"
+                variant="outline-info"
+                title="Switch to this history"
+                :disabled="currentHistory.id == history.id"
+                @click="setCurrentHistory(history)">
+                Switch to this history
+            </b-button>
+            <b-button v-else v-b-modal:copy-history-modal size="sm" variant="outline-info" title="Import this history">
+                Import this history
+            </b-button>
         </div>
-    </UserHistories>
+        <CollectionPanel
+            v-if="selectedCollections.length && selectedCollections[0].history_id == id"
+            :history="history"
+            :selected-collections.sync="selectedCollections"
+            :show-controls="false"
+            @view-collection="onViewCollection" />
+        <HistoryPanel
+            v-else
+            :history="history"
+            :writable="currentUser.id == history.user_id"
+            :show-controls="false"
+            @view-collection="onViewCollection" />
+        <CopyModal id="copy-history-modal" :history="history" />
+    </div>
 </template>
 
 <script>
 import { mapActions, mapState } from "pinia";
 import { useUserStore } from "@/stores/userStore";
 import { useHistoryStore } from "@/stores/historyStore";
-import UserHistories from "components/providers/UserHistories";
 import CollectionPanel from "./CurrentCollection/CollectionPanel";
 import HistoryPanel from "./CurrentHistory/HistoryPanel";
 import CopyModal from "./Modals/CopyModal";
@@ -52,7 +43,6 @@ export default {
     components: {
         HistoryPanel,
         CollectionPanel,
-        UserHistories,
         CopyModal,
     },
     props: {
@@ -68,7 +58,7 @@ export default {
     },
     computed: {
         ...mapState(useUserStore, ["currentUser"]),
-        ...mapState(useHistoryStore, ["getHistoryById"]),
+        ...mapState(useHistoryStore, ["getHistoryById", "currentHistory"]),
         history() {
             return this.getHistoryById(this.id);
         },
@@ -77,7 +67,7 @@ export default {
         this.loadHistoryById(this.id);
     },
     methods: {
-        ...mapActions(useHistoryStore, ["loadHistoryById"]),
+        ...mapActions(useHistoryStore, ["loadHistoryById", "setCurrentHistory"]),
         onViewCollection(collection) {
             this.selectedCollections = [...this.selectedCollections, collection];
         },
