683d8e47cc6269dd52c608d2c909211b4f7861dd
Alireza Heidari <itisalirh@gmail.com>
2023-04-25 16:47:09 +0200

    replace `UserHistories` provider in `MultipleView.vue` and some improvements

--- client/src/components/History/Multiple/MultipleView.vue
+++ client/src/components/History/Multiple/MultipleView.vue
@@ -2,12 +2,12 @@
 import { ref } from "vue";
 import { storeToRefs } from "pinia";
 import { useUserStore } from "@/stores/userStore";
+import { useHistoryStore } from "@/stores/historyStore";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import LoadingSpan from "@/components/LoadingSpan.vue";
 import DebouncedInput from "@/components/DebouncedInput";
-import UserHistories from "@/components/providers/UserHistories";
 import MultipleViewList from "./MultipleViewList.vue";
 
 const filter = ref("");
@@ -16,6 +16,7 @@ const filter = ref("");
 library.add(faTimes);
 
 const { currentUser } = storeToRefs(useUserStore());
+const { histories, historiesLoading, currentHistory } = storeToRefs(useHistoryStore());
 
 function updateFilter(newFilter: string) {
     filter.value = newFilter;
@@ -23,41 +24,32 @@ function updateFilter(newFilter: string) {
 </script>
 
 <template>
-    <UserHistories
-        v-if="currentUser"
-        v-slot="{ histories, handlers, historiesLoading, currentHistory }"
-        :user="currentUser">
-        <div>
-            <b-alert v-if="historiesLoading" class="m-2" variant="info" show>
-                <LoadingSpan message="Loading Histories" />
-            </b-alert>
-            <div v-else-if="histories.length" class="multi-history-panel d-flex flex-column h-100">
-                <b-input-group class="w-100">
-                    <DebouncedInput v-slot="{ value, input }" v-model="filter">
-                        <b-form-input
-                            size="sm"
-                            :class="filter && 'font-weight-bold'"
-                            :value="value"
-                            :placeholder="'search datasets in selected histories' | l"
-                            data-description="filter text input"
-                            @input="input"
-                            @keyup.esc="updateFilter('')" />
-                    </DebouncedInput>
-                    <b-input-group-append>
-                        <b-button size="sm" data-description="show deleted filter toggle" @click="updateFilter('')">
-                            <FontAwesomeIcon icon="fa-times" />
-                        </b-button>
-                    </b-input-group-append>
-                </b-input-group>
-                <multiple-view-list
-                    :histories="histories"
-                    :filter="filter"
-                    :current-history="currentHistory"
-                    :handlers="handlers" />
-            </div>
-            <b-alert v-else class="m-2" variant="danger" show>
-                <span v-localize class="font-weight-bold">No History found.</span>
-            </b-alert>
+    <div v-if="currentUser">
+        <b-alert v-if="historiesLoading" class="m-2" variant="info" show>
+            <LoadingSpan message="Loading Histories" />
+        </b-alert>
+        <div v-else-if="histories.length" class="multi-history-panel d-flex flex-column h-100">
+            <b-input-group class="w-100">
+                <DebouncedInput v-slot="{ value, input }" v-model="filter">
+                    <b-form-input
+                        size="sm"
+                        :class="filter && 'font-weight-bold'"
+                        :value="value"
+                        :placeholder="'search datasets in selected histories' | l"
+                        data-description="filter text input"
+                        @input="input"
+                        @keyup.esc="updateFilter('')" />
+                </DebouncedInput>
+                <b-input-group-append>
+                    <b-button size="sm" data-description="show deleted filter toggle" @click="updateFilter('')">
+                        <FontAwesomeIcon icon="fa-times" />
+                    </b-button>
+                </b-input-group-append>
+            </b-input-group>
+            <MultipleViewList :histories="histories" :filter="filter" :current-history="currentHistory" />
         </div>
-    </UserHistories>
+        <b-alert v-else class="m-2" variant="danger" show>
+            <span v-localize class="font-weight-bold">No History found.</span>
+        </b-alert>
+    </div>
 </template>
