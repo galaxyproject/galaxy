63df9be6938b1a74b8be0cd8e78dd06da977a7fc
hujambo-dunia <michelle.savage.1@gmail.com>
2023-04-26 13:43:58 -0400

    Fix for missing Href to Composables pass-through component

--- client/src/components/Toast.js
+++ client/src/components/Toast.js
@@ -1,9 +1,10 @@
 export default {
     methods: {
-        showToast(message, title, variant) {
+        showToast(message, title, variant, href) {
             this.$bvToast.toast(message, {
                 variant,
                 title,
+                href,
                 toaster: "b-toaster-bottom-right",
                 append: true,
                 solid: true,
