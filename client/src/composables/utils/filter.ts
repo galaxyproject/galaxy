d2f1f300d3d0b92f88d974d09a5cb7c7e4320832
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-05-04 12:51:22 +0200

    make filter multi-threaded to improve ui responsiveness

--- client/src/composables/utils/filter.ts
+++ client/src/composables/utils/filter.ts
@@ -1,6 +1,7 @@
-import { computed } from "vue";
+import { onScopeDispose, ref, watch, type Ref } from "vue";
 import type { MaybeComputedRef } from "@vueuse/core";
 import { resolveUnref } from "@vueuse/core";
+import type { Message, ResultMessage } from "./filter.worker";
 
 /**
  * Reactively filter an array of objects, by comparing `filter` to all `fields`.
@@ -14,32 +15,55 @@ export function useFilterObjectArray<O extends object, K extends keyof O>(
     filter: MaybeComputedRef<string>,
     objectFields: MaybeComputedRef<Array<K>>
 ) {
-    const filtered = computed(() => {
-        const f = resolveUnref(filter).toLowerCase();
-        const arr = resolveUnref(array);
-        const fields = resolveUnref(objectFields);
+    const worker = new Worker(new URL("./filter.worker.ts", import.meta.url));
 
-        if (f === "") {
-            return arr;
-        } else {
-            return arr.filter((obj) => {
-                for (const field of fields) {
-                    const val = obj[field];
+    const filtered: Ref<O[]> = ref([]);
+    filtered.value = resolveUnref(array);
 
-                    if (typeof val === "string") {
-                        if (val.toLowerCase().includes(f)) {
-                            return true;
-                        }
-                    } else if (Array.isArray(val)) {
-                        if (val.includes(f)) {
-                            return true;
-                        }
-                    }
-                }
+    const post = (message: Message<O, K>) => {
+        worker.postMessage(message);
+    };
 
-                return false;
-            });
+    watch(
+        () => resolveUnref(array),
+        (arr) => {
+            post({ type: "setArray", array: arr });
+        },
+        {
+            immediate: true,
         }
+    );
+
+    watch(
+        () => resolveUnref(filter),
+        (f) => {
+            post({ type: "setFilter", filter: f });
+        },
+        {
+            immediate: true,
+        }
+    );
+
+    watch(
+        () => resolveUnref(objectFields),
+        (fields) => {
+            post({ type: "setFields", fields });
+        },
+        {
+            immediate: true,
+        }
+    );
+
+    worker.onmessage = (e: MessageEvent<ResultMessage<O>>) => {
+        const message = e.data;
+
+        if (message.type === "result") {
+            filtered.value = message.filtered;
+        }
+    };
+
+    onScopeDispose(() => {
+        worker.terminate();
     });
 
     return filtered;
---
+++ client/src/composables/utils/filter.worker.ts
@@ -0,0 +1,63 @@
+export type Message<O extends object, K extends keyof O> =
+    | {
+          type: "setArray";
+          array: O[];
+      }
+    | {
+          type: "setFilter";
+          filter: string;
+      }
+    | {
+          type: "setFields";
+          fields: K[];
+      };
+
+export type ResultMessage<O extends object> = {
+    type: "result";
+    filtered: O[];
+};
+
+let array: Record<string, unknown>[] = [];
+let filter = "";
+let fields: string[] = [];
+
+self.addEventListener("message", (e: MessageEvent<Message<Record<string, unknown>, string>>) => {
+    const message = e.data;
+
+    if (message.type === "setArray") {
+        array = message.array;
+    } else if (message.type === "setFields") {
+        fields = message.fields;
+    } else if (message.type === "setFilter") {
+        filter = message.filter;
+    }
+
+    if (array.length > 0 && fields.length > 0) {
+        const filtered = runFilter(filter, array, fields);
+        self.postMessage({ type: "result", filtered });
+    }
+});
+
+function runFilter(f: string, arr: Record<string, unknown>[], fields: string[]) {
+    if (f === "") {
+        return arr;
+    } else {
+        return arr.filter((obj) => {
+            for (const field of fields) {
+                const val = obj[field];
+
+                if (typeof val === "string") {
+                    if (val.toLowerCase().includes(f)) {
+                        return true;
+                    }
+                } else if (Array.isArray(val)) {
+                    if (val.includes(f)) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        });
+    }
+}
