090f531b10baef613fd2bcc520e99a94849169fe
mahendrapaipuri <mahendra.paipuri@gmail.com>
2023-05-19 17:25:33 +0200

    Avoid adding galaxy prefix twice to dataset url

--- config/plugins/visualizations/openlayers/src/script.js
+++ config/plugins/visualizations/openlayers/src/script.js
@@ -215,8 +215,7 @@ var MapViewer = (function(mv) {
             const sourceVec = new Vector({ format: formatType, url: filePath, wrapX: false });
             mv.createMap(filePath, sourceVec, options, chart, styleFunction, target);
         } else if (fileType === "shp") {
-            const fileUrl = prefixedDownloadUrl(options.root, filePath);
-            axios.get(fileUrl, { responseType: "arraybuffer" }).then(shpfile => {
+            axios.get(filePath, { responseType: "arraybuffer" }).then(shpfile => {
                 console.debug(shpfile);
                 shp(shpfile.data).then(
                     geojson => {
