f515fb533699cc692d0665386fd06a4441806d14
guerler <aysam.guerler@gmail.com>
2023-06-01 12:54:39 +0200

    Remove unused div wrapper

--- client/src/components/ActivityBar/ActivityItem.vue
+++ client/src/components/ActivityBar/ActivityItem.vue
@@ -44,47 +44,45 @@ function onClick(): void {
 </script>
 
 <template>
-    <div>
-        <Popper reference-is="span" popper-is="span" placement="right">
-            <template v-slot:reference>
-                <b-nav-item
-                    :id="id"
-                    class="position-relative mb-1"
-                    :class="{ 'nav-item-active': isActive }"
-                    :aria-label="title | l"
-                    @mouseup="onClick">
-                    <span v-if="progressStatus" class="progress">
-                        <div
-                            class="progress-bar notransition"
-                            :class="{
-                                'bg-danger': progressStatus === 'danger',
-                                'bg-success': progressStatus === 'success',
-                            }"
-                            :style="{
-                                width: `${Math.round(progressPercentage)}%`,
-                            }" />
-                    </span>
-                    <span class="position-relative">
-                        <div class="nav-icon">
-                            <Icon :icon="icon" />
-                        </div>
-                        <div class="nav-title">{{ title }}</div>
-                    </span>
-                </b-nav-item>
-            </template>
-            <div class="px-2 py-1">
-                <small v-if="tooltip">{{ tooltip | l }}</small>
-                <small v-else>No tooltip available for this item</small>
-                <div v-if="options" class="nav-options p-1">
-                    <router-link v-for="(option, index) in options" :key="index" :to="option.value">
-                        <b-button size="sm" variant="outline-primary" class="w-100 my-1 text-break text-light">
-                            {{ option.name }}
-                        </b-button>
-                    </router-link>
-                </div>
+    <Popper reference-is="span" popper-is="span" placement="right">
+        <template v-slot:reference>
+            <b-nav-item
+                :id="id"
+                class="position-relative mb-1"
+                :class="{ 'nav-item-active': isActive }"
+                :aria-label="title | l"
+                @mouseup="onClick">
+                <span v-if="progressStatus" class="progress">
+                    <div
+                        class="progress-bar notransition"
+                        :class="{
+                            'bg-danger': progressStatus === 'danger',
+                            'bg-success': progressStatus === 'success',
+                        }"
+                        :style="{
+                            width: `${Math.round(progressPercentage)}%`,
+                        }" />
+                </span>
+                <span class="position-relative">
+                    <div class="nav-icon">
+                        <Icon :icon="icon" />
+                    </div>
+                    <div class="nav-title">{{ title }}</div>
+                </span>
+            </b-nav-item>
+        </template>
+        <div class="px-2 py-1">
+            <small v-if="tooltip">{{ tooltip | l }}</small>
+            <small v-else>No tooltip available for this item</small>
+            <div v-if="options" class="nav-options p-1">
+                <router-link v-for="(option, index) in options" :key="index" :to="option.value">
+                    <b-button size="sm" variant="outline-primary" class="w-100 my-1 text-break text-light">
+                        {{ option.name }}
+                    </b-button>
+                </router-link>
             </div>
-        </Popper>
-    </div>
+        </div>
+    </Popper>
 </template>
 
 <style scoped lang="scss">
