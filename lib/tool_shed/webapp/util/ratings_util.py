680348eafa6c086ab5cc44802f11b1e835de96f1
John Davis <jdavcs@gmail.com>
2023-06-01 21:22:30 -0400

    Wrap session.flush in TS webapp.util.ratings_util

--- lib/tool_shed/webapp/util/ratings_util.py
+++ lib/tool_shed/webapp/util/ratings_util.py
@@ -1,5 +1,6 @@
 import logging
 
+from galaxy.model.base import transaction
 from galaxy.model.item_attrs import UsesItemRatings
 
 log = logging.getLogger(__name__)
@@ -20,11 +21,13 @@ class ItemRatings(UsesItemRatings):
             item_rating.rating = rating
             item_rating.comment = comment
             trans.sa_session.add(item_rating)
-            trans.sa_session.flush()
+            with transaction(trans.sa_session):
+                trans.sa_session.commit()
         elif item_rating.rating != rating or item_rating.comment != comment:
             # User has previously rated item; update rating.
             item_rating.rating = rating
             item_rating.comment = comment
             trans.sa_session.add(item_rating)
-            trans.sa_session.flush()
+            with transaction(trans.sa_session):
+                trans.sa_session.commit()
         return item_rating
