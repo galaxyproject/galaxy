07cb4cf846d1a90e306f021247c4642ba4628936
guerler <aysam.guerler@gmail.com>
2023-06-03 15:56:03 +0200

    Add style support to context-menu

--- client/src/components/ActivityBar/ActivitySettings.vue
+++ client/src/components/ActivityBar/ActivitySettings.vue
@@ -10,19 +10,18 @@ const activities = ref(Activities.slice().filter((a) => !!a.to));
         <div class="font-weight-bold">Select Activities:</div>
         <div class="activity-settings-content overflow-auto">
             <div v-for="option in activities">
-                <div class="d-flex my-2">
-                    <div>
-                        <b-form-checkbox />
-                    </div>
-                    <div>
-                        <div>
-                            <icon class="mr-1" :icon="option.icon" />
-                            <span class="font-weight-bold">{{ option.title || "No title available" }}</span>
+                <div class="context-menu-checkbox p-2">
+                    <b-form-checkbox>
+                        <div class="context-menu-item">
+                            <div>
+                                <icon class="mr-1" :icon="option.icon" />
+                                <span class="font-weight-bold">{{ option.title || "No title available" }}</span>
+                            </div>
+                            <div>
+                                {{ option.description || "No description available" }}
+                            </div>
                         </div>
-                        <div>
-                            {{ option.description || "No description available" }}
-                        </div>
-                    </div>
+                    </b-form-checkbox>
                 </div>
             </div>
         </div>
@@ -39,5 +38,16 @@ const activities = ref(Activities.slice().filter((a) => !!a.to));
 
 .activity-settings-content {
     max-height: 20rem;
+}
+
+.context-menu-checkbox .custom-control-input:checked ~ .custom-control-label::before {
+    background-color: $brand-dark;
+    border-color: $white;
+}
+
+.context-menu-checkbox:hover {
+    cursor: pointer;
+    background: $brand-primary;
+    border-radius: $border-radius-base;
 }
 </style>
