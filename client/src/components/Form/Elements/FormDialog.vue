7cee1cc10528f4d7a69e77b6cbf06199209995ee
Assunta DeSanto <assuntad23@gmail.com>
2023-06-12 16:23:03 -0400

    selection updates the input box

--- client/src/components/Form/Elements/FormDialog.vue
+++ client/src/components/Form/Elements/FormDialog.vue
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faFolderOpen } from "@fortawesome/free-solid-svg-icons";
 import DataDialog from "../../DataDialog/DataDialog.vue";
-import { computed, onMounted } from "vue";
+import { onMounted } from "vue";
 import { getCurrentGalaxyHistory } from "../../../utils/data";
 import { reactive } from "vue";
 
@@ -12,63 +12,59 @@ library.add(faFolderOpen);
 interface DataDialogProps {
     id: string;
     multiple: boolean;
-    value?: string;
+    value?: Array<string>;
 }
 
 const props = withDefaults(defineProps<DataDialogProps>(), {
-    value: "",
     multiple: false,
 });
 
 const title = "Browse Datasets";
-let historyID: string = "";
+const historyID: string = "";
 const localHistoryId = reactive({
     value: historyID,
 });
 
-onMounted(() => { 
+onMounted(() => {
     getCurrentGalaxyHistory().then((historyId) => {
-        console.log("historyId", historyId);
-        //set historyID to historyId
         localHistoryId.value = historyId;
     });
-})
+});
 
-let initialODD = false;
+const initialODD = false;
 const openDataDialog = reactive({
     value: initialODD,
 });
 
+const dataSelected = reactive({
+    value: [""]
+});
+
 const emit = defineEmits<{
-    (e: "input", value: Object): void;
+    (e: "input", value: Array<string>): void;
 }>();
 
-const currentValue = computed({
-    get() {
-        return Object(props.value);
-    },
-    set(newValue) {
-        emit("input", newValue);
-    },
-});
+
+function onData(result: Array<string>) {
+    openDataDialog.value = false
+    dataSelected.value = result;
+    emit("input", result);
+}
+
 </script>
 
 <template>
     <div class="d-flex">
         <span @click="openDataDialog.value = true">
             <font-awesome-icon icon="folder-open" :title="title" />
-            <b-form-input
-                :id="id"
-                v-model="currentValue"
-                :class="['ui-text-area float-left disabled']" />
+            <input disabled class="ui-input float-left" v-model="dataSelected.value" />
         </span>
         <DataDialog
             v-if="openDataDialog.value"
-            v-model="currentValue"
             :history="localHistoryId.value"
             :multiple="multiple"
             @onCancel="openDataDialog.value = false"
             @onUpload="openDataDialog.value = false"
-             />
+            @onOk="onData" />
     </div>
 </template>
