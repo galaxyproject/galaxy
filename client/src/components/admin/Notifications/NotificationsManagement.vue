865148ce9248d76af66736ee789a00568a697ada
Alireza Heidari <itisalirh@gmail.com>
2023-06-20 17:37:17 +0200

    create NotificationsManagement component

---
+++ client/src/components/admin/Notifications/NotificationsManagement.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router/composables";
+import Heading from "@/components/Common/Heading.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import BroadcastsList from "@/components/admin/Notifications/BroadcastsList.vue";
+
+const router = useRouter();
+
+function goToCreateNewNotification() {
+    router.push("/admin/notifications/create_new_notification");
+}
+
+function goToCreateNewBroadcast() {
+    router.push("/admin/notifications/create_new_broadcast");
+}
+</script>
+
+<template>
+    <div aria-labelledby="notifications-managements">
+        <div class="d-flex justify-content-between">
+            <Heading id="notifications-title">Notifications and Broadcasts</Heading>
+            <div>
+                <BButton class="mb-2" variant="outline-primary" @click="goToCreateNewNotification">
+                    <FontAwesomeIcon icon="plus" />
+                    Send new notification
+                </BButton>
+                <BButton class="mb-2" variant="outline-primary" @click="goToCreateNewBroadcast">
+                    <FontAwesomeIcon icon="plus" />
+                    Create new broadcast
+                </BButton>
+            </div>
+        </div>
+
+        <BroadcastsList />
+    </div>
+</template>
