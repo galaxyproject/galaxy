42ec97d852ca1f6492e4c44892c4d3fdc27eae1d
mahendrapaipuri <mahendra.paipuri@gmail.com>
2023-06-21 10:27:19 +0200

    Add missing module name to js function

--- client/src/components/DatasetInformation/DatasetInformation.vue
+++ client/src/components/DatasetInformation/DatasetInformation.vue
@@ -20,7 +20,7 @@
                     </tr>
                     <tr>
                         <td>Filesize</td>
-                        <td id="filesize" v-html="bytesToString(dataset.file_size)"></td>
+                        <td id="filesize" v-html="Utils.bytesToString(dataset.file_size)"></td>
                     </tr>
                     <tr>
                         <td>Dbkey</td>
