d37b2bc171712d4e7792a6ca80abf4e10e49fd98
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-07-06 03:36:46 +0200

    remove persist from userStore

--- client/src/stores/userStore.ts
+++ client/src/stores/userStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import { computed, ref } from "vue";
 
+import { useUserLocalStorage } from "@/composables/userLocalStorage";
 import { useHistoryStore } from "@/stores/historyStore";
 import {
     addFavoriteToolQuery,
@@ -20,120 +21,110 @@ interface Preferences {
     favorites: { tools: string[] };
 }
 
-export const useUserStore = defineStore(
-    "userStore",
-    () => {
-        const toggledSideBar = ref("tools");
-        const showActivityBar = ref(false);
-        const currentUser = ref<User | null>(null);
-        const currentPreferences = ref<Preferences | null>(null);
+export const useUserStore = defineStore("userStore", () => {
+    const toggledSideBar = useUserLocalStorage("user-store-toggled-side-bar", "tools");
+    const showActivityBar = useUserLocalStorage("user-store-show-activity-bar", false);
+    const currentUser = ref<User | null>(null);
+    const currentPreferences = ref<Preferences | null>(null);
 
-        let loadPromise: Promise<void> | null = null;
+    let loadPromise: Promise<void> | null = null;
 
-        const isAnonymous = computed(() => {
-            return !currentUser.value?.email;
-        });
+    const isAnonymous = computed(() => {
+        return !currentUser.value?.email;
+    });
 
-        const currentTheme = computed(() => {
-            return currentPreferences.value?.theme ?? null;
-        });
+    const currentTheme = computed(() => {
+        return currentPreferences.value?.theme ?? null;
+    });
 
-        const currentFavorites = computed(() => {
-            if (currentPreferences.value?.favorites) {
-                return currentPreferences.value.favorites;
-            } else {
-                return { tools: [] };
-            }
-        });
-
-        function setCurrentUser(user: User) {
-            currentUser.value = user;
+    const currentFavorites = computed(() => {
+        if (currentPreferences.value?.favorites) {
+            return currentPreferences.value.favorites;
+        } else {
+            return { tools: [] };
         }
+    });
 
-        function loadUser() {
-            if (!loadPromise) {
-                loadPromise = getCurrentUser()
-                    .then(async (user) => {
-                        const historyStore = useHistoryStore();
-                        currentUser.value = { ...user, isAnonymous: !user.email };
-                        currentPreferences.value = user?.preferences ?? null;
-                        // TODO: This is a hack to get around the fact that the API returns a string
-                        if (currentPreferences.value?.favorites) {
-                            currentPreferences.value.favorites = JSON.parse(
-                                user?.preferences?.favorites ?? { tools: [] }
-                            );
-                        }
-                        await historyStore.loadCurrentHistory();
-                        // load first few histories for user to start pagination
-                        await historyStore.loadHistories();
-                    })
-                    .catch((e) => {
-                        console.error("Failed to load user", e);
-                    })
-                    .finally(() => {
-                        loadPromise = null;
-                    });
-            }
-        }
-
-        async function setCurrentTheme(theme: string) {
-            if (!currentUser.value) {
-                return;
-            }
-            const currentTheme = await setCurrentThemeQuery(currentUser.value.id, theme);
-            if (currentPreferences.value) {
-                currentPreferences.value.theme = currentTheme;
-            }
-        }
-        async function addFavoriteTool(toolId: string) {
-            if (!currentUser.value) {
-                return;
-            }
-            const tools = await addFavoriteToolQuery(currentUser.value.id, toolId);
-            setFavoriteTools(tools);
-        }
-
-        async function removeFavoriteTool(toolId: string) {
-            if (!currentUser.value) {
-                return;
-            }
-            const tools = await removeFavoriteToolQuery(currentUser.value.id, toolId);
-            setFavoriteTools(tools);
-        }
-
-        function setFavoriteTools(tools: string[]) {
-            if (currentPreferences.value) {
-                currentPreferences.value.favorites.tools = tools ?? { tools: [] };
-            }
-        }
-
-        function toggleActivityBar() {
-            showActivityBar.value = !showActivityBar.value;
-        }
-        function toggleSideBar(currentOpen = "") {
-            toggledSideBar.value = toggledSideBar.value === currentOpen ? "" : currentOpen;
-        }
-
-        return {
-            currentUser,
-            currentPreferences,
-            isAnonymous,
-            currentTheme,
-            currentFavorites,
-            showActivityBar,
-            toggledSideBar,
-            loadUser,
-            setCurrentUser,
-            setCurrentTheme,
-            addFavoriteTool,
-            removeFavoriteTool,
-            toggleActivityBar,
-            toggleSideBar,
-        };
-    },
-    {
-        persist: {
-            paths: ["showActivityBar", "toggledSideBar"],
-        },
+    function setCurrentUser(user: User) {
+        currentUser.value = user;
     }
-);
+
+    function loadUser() {
+        if (!loadPromise) {
+            loadPromise = getCurrentUser()
+                .then(async (user) => {
+                    const historyStore = useHistoryStore();
+                    currentUser.value = { ...user, isAnonymous: !user.email };
+                    currentPreferences.value = user?.preferences ?? null;
+                    // TODO: This is a hack to get around the fact that the API returns a string
+                    if (currentPreferences.value?.favorites) {
+                        currentPreferences.value.favorites = JSON.parse(user?.preferences?.favorites ?? { tools: [] });
+                    }
+                    await historyStore.loadCurrentHistory();
+                    // load first few histories for user to start pagination
+                    await historyStore.loadHistories();
+                })
+                .catch((e) => {
+                    console.error("Failed to load user", e);
+                })
+                .finally(() => {
+                    loadPromise = null;
+                });
+        }
+    }
+
+    async function setCurrentTheme(theme: string) {
+        if (!currentUser.value) {
+            return;
+        }
+        const currentTheme = await setCurrentThemeQuery(currentUser.value.id, theme);
+        if (currentPreferences.value) {
+            currentPreferences.value.theme = currentTheme;
+        }
+    }
+    async function addFavoriteTool(toolId: string) {
+        if (!currentUser.value) {
+            return;
+        }
+        const tools = await addFavoriteToolQuery(currentUser.value.id, toolId);
+        setFavoriteTools(tools);
+    }
+
+    async function removeFavoriteTool(toolId: string) {
+        if (!currentUser.value) {
+            return;
+        }
+        const tools = await removeFavoriteToolQuery(currentUser.value.id, toolId);
+        setFavoriteTools(tools);
+    }
+
+    function setFavoriteTools(tools: string[]) {
+        if (currentPreferences.value) {
+            currentPreferences.value.favorites.tools = tools ?? { tools: [] };
+        }
+    }
+
+    function toggleActivityBar() {
+        showActivityBar.value = !showActivityBar.value;
+    }
+    function toggleSideBar(currentOpen = "") {
+        toggledSideBar.value = toggledSideBar.value === currentOpen ? "" : currentOpen;
+    }
+
+    return {
+        currentUser,
+        currentPreferences,
+        isAnonymous,
+        currentTheme,
+        currentFavorites,
+        showActivityBar,
+        toggledSideBar,
+        loadUser,
+        setCurrentUser,
+        setCurrentTheme,
+        addFavoriteTool,
+        removeFavoriteTool,
+        toggleActivityBar,
+        toggleSideBar,
+    };
+});
