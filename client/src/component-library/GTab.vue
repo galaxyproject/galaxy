107e13ee696eb0705a13e172c0492dd8e7275bbe
hujambo-dunia <michelle.savage.1@gmail.com>
2023-07-15 12:09:02 +1000

    Initial commit; new component file

---
+++ client/src/component-library/GTab.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { BTab } from "bootstrap-vue";
+</script>
+
+<template>
+    <BTab v-bind="$attrs" v-on="$listeners">
+        <slot></slot>
+    </BTab>
+</template>
---
+++ client/src/component-library/GTabs.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { BTabs } from "bootstrap-vue";
+</script>
+
+<template>
+    <BTabs v-bind="$attrs" v-on="$listeners">
+        <slot></slot>
+    </BTabs>
+</template>
