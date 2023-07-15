dfe4cefe2d579c65692e628b92ea254c0f42fe6b
hujambo-dunia <michelle.savage.1@gmail.com>
2023-07-15 11:50:19 +1000

    Initial commit; new component file

---
+++ client/src/component-library/GTooltip.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { BTooltip } from "bootstrap-vue";
+</script>
+
+<template>
+    <BTooltip v-bind="$attrs" v-on="$listeners">
+        <slot></slot>
+    </BTooltip>
+</template>
