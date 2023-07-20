6db572393250a2c60d035d2cf5f2e744e58b7223
davelopez <46503462+davelopez@users.noreply.github.com>
2023-07-20 17:33:42 +0200

    Update FilesDialog services
    - Add new API parameters
    - Add new POST endpoint for creating entries
    - Add some code docs

--- client/src/components/FilesDialog/services.ts
+++ client/src/components/FilesDialog/services.ts
@@ -5,17 +5,43 @@ export type FilesSourcePlugin = components["schemas"]["FilesSourcePlugin"];
 export type RemoteFile = components["schemas"]["RemoteFile"];
 export type RemoteDirectory = components["schemas"]["RemoteDirectory"];
 export type RemoteEntry = RemoteFile | RemoteDirectory;
+export type CreatedEntry = components["schemas"]["CreatedEntryResponse"];
 
 const getRemoteFilesPlugins = fetcher.path("/api/remote_files/plugins").method("get").create();
 
-export async function getFileSources(): Promise<FilesSourcePlugin[]> {
-    const { data } = await getRemoteFilesPlugins({ browsable_only: true });
+/**
+ * Get the list of available file sources from the server that can be browsed.
+ * @param rdm_only Whether to only include Research Data Management (RDM) specific file sources.
+ * @returns The list of available file sources from the server.
+ */
+export async function getFileSources(rdm_only = false): Promise<FilesSourcePlugin[]> {
+    const { data } = await getRemoteFilesPlugins({ browsable_only: true, rdm_only: rdm_only });
     return data;
 }
 
 const getRemoteFiles = fetcher.path("/api/remote_files").method("get").create();
 
-export async function browseRemoteFiles(uri: string, isRecursive = false): Promise<RemoteEntry[]> {
-    const { data } = await getRemoteFiles({ target: uri, recursive: isRecursive });
+/**
+ * Get the list of files and directories from the server for the given file source URI.
+ * @param uri The file source URI to browse.
+ * @param isRecursive Whether to recursively retrieve all files inside subdirectories.
+ * @param writeIntent Whether to include only entries that can be written to.
+ * @returns The list of files and directories from the server for the given URI.
+ */
+export async function browseRemoteFiles(uri: string, isRecursive = false, writeIntent = false): Promise<RemoteEntry[]> {
+    const { data } = await getRemoteFiles({ target: uri, recursive: isRecursive, write_intent: writeIntent });
     return data as RemoteEntry[];
+}
+
+const createEntry = fetcher.path("/api/remote_files").method("post").create();
+
+/**
+ * Create a new entry (directory/record) on the given file source URI.
+ * @param uri The file source URI to create the entry in.
+ * @param name The name of the entry to create.
+ * @returns The created entry details.
+ */
+export async function createRemoteEntry(uri: string, name: string): Promise<CreatedEntry> {
+    const { data } = await createEntry({ target: uri, name: name });
+    return data;
 }
