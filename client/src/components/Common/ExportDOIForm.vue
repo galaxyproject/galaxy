556160ddaab9c447e9eb347fdb23984b62c603a0
davelopez <46503462+davelopez@users.noreply.github.com>
2023-07-21 14:15:08 +0200

    Add ExportDOIForm component
    This component allows to select or create new records in a DOI repository (RDM file source) for uploading exported artifacts (histories, datasets, etc.) to them.

---
+++ client/src/components/Common/ExportDOIForm.vue
@@ -0,0 +1,162 @@
+<script setup lang="ts">
+import { BButton, BCard, BFormGroup, BFormInput, BFormRadio, BFormRadioGroup } from "bootstrap-vue";
+import { computed, ref } from "vue";
+
+import { CreatedEntry, createRemoteEntry } from "@/components/FilesDialog/services";
+import localize from "@/utils/localization";
+
+import ExternalLink from "@/components/ExternalLink.vue";
+import FilesInput from "@/components/FilesDialog/FilesInput.vue";
+
+interface Props {
+    what?: string;
+    clearInputAfterExport?: boolean;
+    defaultRecordName?: string;
+    defaultFilename?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    what: "archive",
+    clearInputAfterExport: false,
+    defaultRecordName: "",
+    defaultFilename: "",
+});
+
+const emit = defineEmits<{
+    (e: "export", recordUri: string, fileName: string, newRecordName?: string): void;
+}>();
+
+type ExportChoice = "existing" | "new";
+
+const recordUri = ref<string>("");
+const sourceUri = ref<string>("");
+const fileName = ref<string>(props.defaultFilename);
+const exportChoice = ref<ExportChoice>("new");
+const recordName = ref<string>(props.defaultRecordName);
+const newEntry = ref<CreatedEntry>();
+
+const canCreateRecord = computed(() => Boolean(sourceUri.value) && Boolean(recordName.value));
+
+const canExport = computed(() => Boolean(recordUri.value) && Boolean(fileName.value));
+
+const repositoryRecordDescription = computed(() => localize(`Select a repository to export ${props.what} to.`));
+
+const nameDescription = computed(() => localize("Give the exported file a name."));
+const recordNameDescription = computed(() => localize("Give the new record a name or title."));
+
+const namePlaceholder = computed(() => localize("File name"));
+const recordNamePlaceholder = computed(() => localize("Record name"));
+
+function doExport() {
+    emit("export", recordUri.value, fileName.value);
+
+    if (props.clearInputAfterExport) {
+        clearInputs();
+    }
+}
+
+async function doCreateRecord() {
+    //TODO: handle errors
+    newEntry.value = await createRemoteEntry(sourceUri.value, recordName.value);
+    recordUri.value = newEntry.value.uri;
+}
+
+function clearInputs() {
+    recordUri.value = "";
+    sourceUri.value = "";
+    fileName.value = "";
+    newEntry.value = undefined;
+}
+</script>
+
+<template>
+    <div class="export-to-doi-repository">
+        <BFormGroup id="fieldset-name" label-for="name" :description="nameDescription" class="mt-3">
+            <BFormInput id="name" v-model="fileName" :placeholder="namePlaceholder" required />
+        </BFormGroup>
+
+        <BFormRadioGroup v-model="exportChoice" class="export-radio-group">
+            <BFormRadio v-localize name="exportChoice" value="new"> Export to new record </BFormRadio>
+            <BFormRadio v-localize name="exportChoice" value="existing"> Export to existing draft record </BFormRadio>
+        </BFormRadioGroup>
+
+        <div v-if="exportChoice === 'new'">
+            <div v-if="newEntry">
+                <BCard>
+                    <p v-localize>
+                        A new draft record with name <b>{{ newEntry.name }}</b> has been created in the repository.
+                    </p>
+                    <p v-if="newEntry.external_link">
+                        You can preview the record in the repository and further edit its metadata at
+                        <ExternalLink :href="newEntry.external_link">
+                            <b>{{ newEntry.external_link }}</b>
+                        </ExternalLink>
+                    </p>
+                    <p v-localize>Please use the button below to upload the exported {{ props.what }} to the record.</p>
+                    <BButton
+                        v-localize
+                        class="export-button"
+                        variant="primary"
+                        :disabled="!canExport"
+                        @click.prevent="doExport">
+                        Export to this record
+                    </BButton>
+                </BCard>
+            </div>
+            <div v-else>
+                <BFormGroup
+                    id="fieldset-record"
+                    label-for="source-selector"
+                    :description="repositoryRecordDescription"
+                    class="mt-3">
+                    <FilesInput
+                        id="repository-selector"
+                        v-model="sourceUri"
+                        mode="source"
+                        :require-writable="true"
+                        :rdm-only="true" />
+                </BFormGroup>
+                <BFormGroup
+                    id="fieldset-record-name"
+                    label-for="record-name"
+                    :description="recordNameDescription"
+                    class="mt-3">
+                    <BFormInput id="record-name" v-model="recordName" :placeholder="recordNamePlaceholder" required />
+                </BFormGroup>
+                <p v-localize>
+                    You need to create the new record in a repository before exporting the {{ props.what }} to it.
+                </p>
+                <BButton
+                    v-localize
+                    class="export-button"
+                    variant="primary"
+                    :disabled="!canCreateRecord"
+                    @click.prevent="doCreateRecord">
+                    Create new record
+                </BButton>
+            </div>
+        </div>
+        <div v-else>
+            <BFormGroup
+                id="fieldset-record"
+                label-for="existing-record-selector"
+                :description="repositoryRecordDescription"
+                class="mt-3">
+                <FilesInput
+                    id="existing-record-selector"
+                    v-model="recordUri"
+                    mode="directory"
+                    :require-writable="true"
+                    :rdm-only="true" />
+            </BFormGroup>
+            <BButton
+                v-localize
+                class="export-button"
+                variant="primary"
+                :disabled="!canExport"
+                @click.prevent="doExport">
+                Export to existing record
+            </BButton>
+        </div>
+    </div>
+</template>
