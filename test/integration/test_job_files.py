9db76ab147a5a3f6a2533e3bfce3f6d7920b8175
John Davis <jdavcs@gmail.com>
2023-07-21 04:43:23 -0400

    Fix SA2.0 syntax (query->select) in integration tests

--- test/integration/test_job_files.py
+++ test/integration/test_job_files.py
@@ -19,6 +19,7 @@ import os
 import tempfile
 
 import requests
+from sqlalchemy import select
 
 from galaxy import model
 from galaxy.model.base import transaction
@@ -50,10 +51,11 @@ class TestJobFilesIntegration(integration_util.IntegrationTestCase):
         if not TestJobFilesIntegration.initialized:
             history_id = self.dataset_populator.new_history()
             sa_session = self.sa_session
-            assert len(sa_session.query(model.HistoryDatasetAssociation).all()) == 0
+            stmt = select(model.HistoryDatasetAssociation)
+            assert len(sa_session.scalars(stmt).all()) == 0
             self.dataset_populator.new_dataset(history_id, content=TEST_INPUT_TEXT, wait=True)
-            assert len(sa_session.query(model.HistoryDatasetAssociation).all()) == 1
-            self.input_hda = sa_session.query(model.HistoryDatasetAssociation).all()[0]
+            assert len(sa_session.scalars(stmt).all()) == 1
+            self.input_hda = sa_session.scalars(stmt).all()[0]
             TestJobFilesIntegration.initialized = True
 
     def test_read_by_state(self):
@@ -119,11 +121,11 @@ class TestJobFilesIntegration(integration_util.IntegrationTestCase):
     def create_static_job_with_state(self, state):
         """Create a job with unknown handler so its state won't change."""
         sa_session = self.sa_session
-        hda = sa_session.query(model.HistoryDatasetAssociation).all()[0]
+        hda = sa_session.scalars(select(model.HistoryDatasetAssociation)).all()[0]
         assert hda
-        history = sa_session.query(model.History).all()[0]
+        history = sa_session.scalars(select(model.History)).all()[0]
         assert history
-        user = sa_session.query(model.User).all()[0]
+        user = sa_session.scalars(select(model.User)).all()[0]
         assert user
         output_hda = model.HistoryDatasetAssociation(history=history, create_dataset=True, flush=False)
         output_hda.hid = 2
