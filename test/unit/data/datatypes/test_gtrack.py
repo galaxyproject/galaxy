961a34e48d133e96cb7f29675c9d44975594e4e8
Sveinung Gundersen <sveinugu@gmail.com>
2023-07-23 07:12:52 +1000

    Refactored GTrack test, adding general test helper class InputFileInfo

--- test/unit/data/datatypes/test_gtrack.py
+++ test/unit/data/datatypes/test_gtrack.py
@@ -1,15 +1,48 @@
+from typing import Generator, NamedTuple
+
+import pytest
+
 from galaxy.datatypes.interval import GTrack
-from .util import get_input_files
+from .util import InputFileInfo, get_input_file_info
 
 
-def test_gtrack_sniff():
-    gtrack = GTrack()
+@pytest.fixture
+def gtrack_example_1() -> Generator[InputFileInfo, None, None]:
+    return get_input_file_info("Example_1.gtrack", 0, read_contents=True)
 
-    with get_input_files("Example_1.gtrack") as input:
-        assert gtrack.sniff(input[0]) is False
 
-    with get_input_files(
-        "Example_2.gtrack", "Example_3.gtrack", "Example_bed_direct.gtrack", "Example_mean_sd_weights.gtrack"
-    ) as input_files:
-        for input in input_files:
-            assert gtrack.sniff(input) is True
+@pytest.fixture
+def gtrack_example_2() -> Generator[InputFileInfo, None, None]:
+    return get_input_file_info("Example_2.gtrack", 1, read_contents=True)
+
+
+@pytest.fixture
+def gtrack_example_3() -> Generator[InputFileInfo, None, None]:
+    return get_input_file_info("Example_3.gtrack", 2, read_contents=True)
+
+
+@pytest.fixture
+def gtrack_example_bed_direct() -> Generator[InputFileInfo, None, None]:
+    return get_input_file_info("Example_bed_direct.gtrack", 3, read_contents=True)
+
+
+@pytest.fixture
+def gtrack_example_mean_sd_weights() -> Generator[InputFileInfo, None, None]:
+    return get_input_file_info("Example_mean_sd_weights.gtrack", 4, read_contents=True)
+
+
+@pytest.mark.parametrize(
+    "input_file_fixture_name, expected",
+    [
+        ("gtrack_example_1", False),
+        ("gtrack_example_2", True),
+        ("gtrack_example_3", True),
+        ("gtrack_example_bed_direct", True),
+        ("gtrack_example_mean_sd_weights", True),
+    ],
+)
+def test_gtrack_sniff_prefix(input_file_fixture_name, expected, request):
+    input_file = request.getfixturevalue(input_file_fixture_name)
+    with input_file as input_file_info:
+        assert isinstance(input_file_info, InputFileInfo)
+        assert GTrack().sniff_prefix(input_file_info.file_prefix) is expected
