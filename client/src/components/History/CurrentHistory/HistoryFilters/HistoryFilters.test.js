159d8ea875263ed9041bf2086ca7ad418e12cef4
Ahmed Awan <qe66653@umbc.edu>
2023-07-26 13:18:23 -0500

    Fix history items repeated search bug

--- client/src/components/History/CurrentHistory/HistoryFilters/HistoryFilters.test.js
+++ client/src/components/History/CurrentHistory/HistoryFilters/HistoryFilters.test.js
@@ -6,7 +6,9 @@ import { HistoryFilters as HistoryFiltering } from "components/History/HistoryFi
 const localVue = getLocalVue();
 
 describe("HistoryFilters", () => {
-    async function expectCorrectEmits(wrapper, showAdvanced, filterText) {
+    let wrapper;
+
+    async function expectCorrectEmits(showAdvanced, filterText) {
         // count how many times filterText and toggles are emitted
         const filterEmit = wrapper.emitted()["update:filter-text"].length - 1;
         const toggleEmit = wrapper.emitted()["update:show-advanced"].length - 1;
@@ -18,8 +20,8 @@ describe("HistoryFilters", () => {
         expect(receivedDict).toEqual(parsedDict);
     }
 
-    it("test history filter panel", async () => {
-        const wrapper = mount(HistoryFilters, {
+    beforeEach(() => {
+        wrapper = mount(HistoryFilters, {
             propsData: {
                 filterText: "",
                 showAdvanced: false,
@@ -29,6 +31,9 @@ describe("HistoryFilters", () => {
                 icon: { template: "<div></div>" },
             },
         });
+    });
+
+    it("test history items filter panel", async () => {
         expect(wrapper.find("[description='advanced filters']").exists()).toBe(false);
         await wrapper.setProps({ showAdvanced: true });
         expect(wrapper.find("[description='advanced filters']").exists()).toBe(true);
@@ -54,17 +59,17 @@ describe("HistoryFilters", () => {
 
         // Test: keyup.enter search (should toggle the view out)
         await filterName.trigger("keyup.enter");
-        await expectCorrectEmits(wrapper, false, "name:name-filter");
+        await expectCorrectEmits(false, "name:name-filter");
 
         // Test: clearing the filterText
         const clearButton = wrapper.find("[data-description='clear filters']");
         await clearButton.trigger("click");
-        await expectCorrectEmits(wrapper, false, "");
+        await expectCorrectEmits(false, "");
 
         // Test: toggling view back in
         const toggleButton = wrapper.find("[data-description='show advanced filter toggle']");
         await toggleButton.trigger("click");
-        await expectCorrectEmits(wrapper, true, "");
+        await expectCorrectEmits(true, "");
 
         // Now add filters in all input fields in the advanced menu
         Object.entries(filterInputs).forEach(([selector, value]) => {
@@ -79,7 +84,6 @@ describe("HistoryFilters", () => {
         await searchButton.trigger("click");
 
         await expectCorrectEmits(
-            wrapper,
             false,
             "create_time>'January 1, 2022' create_time<'January 1, 2023' extension:ext-filter genome_build:db-filter related:hid-related hid>hid-greater hid<hid-lower name:name-filter state:state-filter tag:'tag filter'"
         );
@@ -92,7 +96,7 @@ describe("HistoryFilters", () => {
 
         // toggle view back in
         await toggleButton.trigger("click");
-        await expectCorrectEmits(wrapper, true, "");
+        await expectCorrectEmits(true, "");
 
         // find name field again (could be destroyed beacause of toggling out)
         filterName = wrapper.find("[placeholder='any name']");
@@ -102,6 +106,62 @@ describe("HistoryFilters", () => {
 
         // press esc key from name field (should not change emitted filterText unlike enter key)
         await filterName.trigger("keyup.esc");
-        await expectCorrectEmits(wrapper, false, "");
+        await expectCorrectEmits(false, "");
+    });
+
+    it("test radio-group default filters", async () => {
+        // -------- Testing deleted filter first:  ---------
+
+        await wrapper.setProps({ showAdvanced: true });
+        const deletedFilterBtnGrp = wrapper.find("[description='filter deleted']");
+        const deletedFilterAnyBtn = deletedFilterBtnGrp.find(".btn-secondary");
+        expect(deletedFilterAnyBtn.text()).toBe("Any");
+
+        // current active button for deleted filter should be "No"
+        let deletedFilterActiveBtn = deletedFilterBtnGrp.find(".btn-secondary.active");
+        expect(deletedFilterActiveBtn.text()).toBe("No");
+
+        await deletedFilterAnyBtn.find("input").setChecked();
+
+        // now active button for deleted filter should be "Any"
+        deletedFilterActiveBtn = deletedFilterBtnGrp.find(".btn-secondary.active");
+        expect(deletedFilterActiveBtn.text()).toBe("Any");
+
+        // expect "deleted = any" filter to be applied
+        let searchButton = wrapper.find("[description='apply filters']");
+        await searchButton.trigger("click");
+        await expectCorrectEmits(false, "visible:true");
+
+        // -------- Testing visible filter now:  ---------
+
+        const toggleButton = wrapper.find("[data-description='show advanced filter toggle']");
+        await toggleButton.trigger("click");
+        await expectCorrectEmits(true, "visible:true");
+        const visibleFilterBtnGrp = wrapper.find("[description='filter visible']");
+        const visibleFilterAnyBtn = visibleFilterBtnGrp.find(".btn-secondary");
+        expect(visibleFilterAnyBtn.text()).toBe("Any");
+
+        // current active button for visible filter should be "Yes"
+        let visibleFilterActiveBtn = visibleFilterBtnGrp.find(".btn-secondary.active");
+        expect(visibleFilterActiveBtn.text()).toBe("Yes");
+
+        await visibleFilterAnyBtn.find("input").setChecked();
+
+        // now active button for visible filter should be "Any"
+        visibleFilterActiveBtn = visibleFilterBtnGrp.find(".btn-secondary.active");
+        expect(visibleFilterActiveBtn.text()).toBe("Any");
+
+        // expect "visible = any" filter to be applied
+        searchButton = wrapper.find("[description='apply filters']");
+        await searchButton.trigger("click");
+        await expectCorrectEmits(false, "deleted:any visible:any");
+
+        // -------- Testing repeated search if it prevents bug:  ---------
+        // (bug reported here: https://github.com/galaxyproject/galaxy/issues/16211)
+        await toggleButton.trigger("click");
+        await expectCorrectEmits(true, "deleted:any visible:any");
+        searchButton = wrapper.find("[description='apply filters']");
+        await searchButton.trigger("click");
+        await expectCorrectEmits(false, "deleted:any visible:any");
     });
 });
