abbe2598bbdfc7b72c7eca969a52a52e6ae91703
guerler <aysam.guerler@gmail.com>
2023-07-27 12:06:24 +0200

    Add upload settings, prep variant to align with existing appearance

--- client/src/components/Upload/UploadSettings.vue
+++ client/src/components/Upload/UploadSettings.vue
@@ -1,22 +1,51 @@
 <script setup>
-import { computed } from "vue";
 import Popper from "@/components/Popper/Popper.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
+import { faCheckSquare } from "@fortawesome/free-solid-svg-icons";
+import { faSquare } from "@fortawesome/free-regular-svg-icons";
 
-library.add(faCog);
+library.add(faCheckSquare, faCog, faSquare);
 
 const props = defineProps({
     model: Object,
 });
+
+const availableSettings = [
+    {
+        id: "space_to_tab",
+        title: "Convert spaces to tabs",
+    },
+    {
+        id: "to_posix_lines",
+        title: "Use POSIX standard",
+    },
+    {
+        id: "deferred",
+        title: "Defer dataset resolution",
+    },
+];
 </script>
 
 <template>
-    <Popper reference-is="span" popper-is="span" placement="bottom" mode="light">
+    <Popper placement="bottom" mode="light">
         <template v-slot:reference>
             <FontAwesomeIcon class="cursor-pointer" icon="fa-cog" />
         </template>
-        <div class="p-2">Test</div>
+        <div class="px-2 py-1 bg-primary text-white">Settings</div>
+        <div class="upload-settings px-2 py-2">
+            <table class="upload-settings-table grid">
+                <tbody>
+                    <tr v-for="setting in availableSettings" :key="setting.id">
+                        <td>
+                            <FontAwesomeIcon v-if="model['setting.id']" icon="fas fa-check-square" fa-fw />
+                            <FontAwesomeIcon v-else icon="far fa-square" fa-fw />
+                        </td>
+                        <td>{{ setting.title }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </Popper>
 </template>
