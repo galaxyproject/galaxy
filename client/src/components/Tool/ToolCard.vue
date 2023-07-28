d3a86b4c3ebaf5d339f7592b5ca7e9ac8856fe97
hechth <helge.hecht@recetox.muni.cz>
2023-07-28 03:49:31 +0200

    include typo fix

--- client/src/components/Tool/ToolCard.vue
+++ client/src/components/Tool/ToolCard.vue
@@ -185,7 +185,7 @@ function onUpdatePreferredObjectStoreId(selectedToolPreferredObjectStoreId) {
                 :has-citations="props.options.citations"
                 :xrefs="props.options.xrefs"
                 :license="props.options.license"
-                :creators="props.options.creators"
+                :creators="props.options.creator"
                 :funding="props.options.funding"
                 :requirements="props.options.requirements" />
         </div>
