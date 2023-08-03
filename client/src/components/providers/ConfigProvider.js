9b885a00872232a1e52ec4852d0a4a74b5b2eb33
davelopez <46503462+davelopez@users.noreply.github.com>
2023-08-03 16:19:23 +0200

    Adapt ConfigProvider (temporarily)

--- client/src/components/providers/ConfigProvider.js
+++ client/src/components/providers/ConfigProvider.js
@@ -1,24 +1,22 @@
 /**
+ * DEPRECATED: instead use 'useConfigStore'
  * ConfigProvider ensures that the configs have been loaded from the server and
- * delivers all the config values onto the slotProps. Basically it just exists
- * to house the load method in a lifecycle handler because Vuex can't do that
- * for itself.
+ * delivers all the config values onto the slotProps.
  */
 
-import Vue from "vue"; // this module's Vue is different from store/index.js' Vue
-import Vuex, { mapActions, mapGetters } from "vuex";
+import { mapActions, mapState } from "pinia";
 
-Vue.use(Vuex);
+import { useConfigStore } from "@/stores/configurationStore";
 
 export default {
     computed: {
-        ...mapGetters("config", ["config"]),
+        ...mapState(useConfigStore, ["config"]),
     },
     methods: {
-        ...mapActions("config", ["loadConfigs"]),
+        ...mapActions(useConfigStore, ["loadConfig"]),
     },
     created() {
-        this.loadConfigs();
+        this.loadConfig();
     },
     render() {
         return this.$scopedSlots.default({ config: this.config, loading: this.config === undefined });
