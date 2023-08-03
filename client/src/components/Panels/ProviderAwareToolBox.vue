eb0c26813aaddfedd0632aa082cc33ec44f2a675
davelopez <46503462+davelopez@users.noreply.github.com>
2023-08-03 17:56:04 +0200

    Replace ConfigProvider in ProviderAwareToolBox

--- client/src/components/Panels/ProviderAwareToolBox.vue
+++ client/src/components/Panels/ProviderAwareToolBox.vue
@@ -1,32 +1,35 @@
 <template>
-    <ConfigProvider v-slot="{ config }" class="d-flex flex-column">
-        <ToolPanelViewProvider
-            v-if="config.default_panel_view"
-            v-slot="{ currentPanel, currentPanelView }"
-            :panel-view="config.default_panel_view">
-            <ToolBox
-                v-if="currentPanelView"
-                :toolbox="currentPanel"
-                :panel-views="config.panel_views"
-                :current-panel-view="currentPanelView"
-                @updatePanelView="updatePanelView">
-            </ToolBox>
-        </ToolPanelViewProvider>
-    </ConfigProvider>
+    <ToolPanelViewProvider
+        v-if="isConfigLoaded && config.default_panel_view"
+        v-slot="{ currentPanel, currentPanelView }"
+        class="d-flex flex-column"
+        :panel-view="config.default_panel_view">
+        <ToolBox
+            v-if="currentPanelView"
+            :toolbox="currentPanel"
+            :panel-views="config.panel_views"
+            :current-panel-view="currentPanelView"
+            @updatePanelView="updatePanelView">
+        </ToolBox>
+    </ToolPanelViewProvider>
 </template>
 
 <script>
-import ConfigProvider from "components/providers/ConfigProvider";
 import ToolPanelViewProvider from "components/providers/ToolPanelViewProvider";
 import { mapActions } from "vuex";
+
+import { useConfig } from "@/composables/config";
 
 import ToolBox from "./ToolBox";
 
 export default {
     components: {
-        ConfigProvider,
         ToolBox,
         ToolPanelViewProvider,
+    },
+    setup() {
+        const { config, isLoaded: isConfigLoaded } = useConfig(true);
+        return { config, isConfigLoaded };
     },
     methods: {
         updatePanelView(panelView) {
