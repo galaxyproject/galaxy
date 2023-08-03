0b2fb0fdb2f41698a335c16de46dda70e7679bd8
Alireza Heidari <itisalirh@gmail.com>
2023-08-03 17:22:57 +1000

    add missing prop and model in GFormCheckboxGroup

--- client/src/component-library/GFormCheckboxGroup.vue
+++ client/src/component-library/GFormCheckboxGroup.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
 import { BFormCheckboxGroup } from "bootstrap-vue";
+import { computed } from "vue";
+
+interface Props {
+    checked?: any[];
+}
+
+const props = defineProps<Props>();
+
+const emit = defineEmits<{
+    (e: "input", value: Props["checked"]): void;
+}>();
+
+const model = computed({
+    get: () => props.checked,
+    set: (value) => {
+        emit("input", value);
+    },
+});
 </script>
 
 <template>
-    <BFormCheckboxGroup v-bind="$attrs" v-on="$listeners">
+    <BFormCheckboxGroup v-model="model" v-bind="$attrs" v-on="$listeners">
         <slot></slot>
     </BFormCheckboxGroup>
 </template>
