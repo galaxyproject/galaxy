4d8e7237d2d590e0c9b643d30a6b856022f69fa0
mvdbeek <m.vandenbeek@gmail.com>
2023-08-06 10:43:13 +0200

    Fix page response models

--- lib/galaxy/webapps/galaxy/services/pages.py
+++ lib/galaxy/webapps/galaxy/services/pages.py
@@ -76,9 +76,7 @@ class PagesService(ServiceBase):
 
         pages, total_matches = self.manager.index_query(trans, payload, include_total_count)
         return (
-            PageSummaryList.model_construct(
-                root=[trans.security.encode_all_ids(p.to_dict(), recursive=True) for p in pages]
-            ),
+            PageSummaryList(root=[p.to_dict() for p in pages]),
             total_matches,
         )
 
@@ -113,11 +111,11 @@ class PagesService(ServiceBase):
         :returns:   Dictionary return of the Page.to_dict call with the 'content' field populated by the most recent revision
         """
         page = base.get_object(trans, id, "Page", check_ownership=False, check_accessible=True)
-        rval = trans.security.encode_all_ids(page.to_dict(), recursive=True)
+        rval = page.to_dict()
         rval["content"] = page.latest_revision.content
         rval["content_format"] = page.latest_revision.content_format
         self.manager.rewrite_content_for_export(trans, rval)
-        return PageDetails.model_construct(**rval)
+        return PageDetails(**rval)
 
     def show_pdf(self, trans, id: DecodedDatabaseIdField):
         """
