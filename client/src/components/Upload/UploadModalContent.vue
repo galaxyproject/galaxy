67852c27324a62492d0d5b82af19edea44b25f47
guerler <aysam.guerler@gmail.com>
2023-08-08 09:52:46 +0200

    Convert upload modal content wrapper to composition api

--- client/src/components/Upload/UploadModalContent.vue
+++ client/src/components/Upload/UploadModalContent.vue
@@ -1,3 +1,172 @@
+<script setup>
+import { computed, ref, onMounted, onUnmounted } from "vue";
+import { BTab, BTabs } from "bootstrap-vue";
+import { getDatatypesMapper } from "components/Datatypes";
+import LoadingSpan from "components/LoadingSpan";
+import {
+    AUTO_EXTENSION,
+    DEFAULT_DBKEY,
+    DEFAULT_EXTENSION,
+    getUploadDatatypes,
+    getUploadDbKeys,
+} from "components/Upload/utils";
+
+import { uploadPayload } from "@/utils/uploadpayload.js";
+
+import Composite from "./Composite";
+import Default from "./Default";
+import RulesInput from "./RulesInput";
+
+const props = defineProps({
+    auto: {
+        type: Object,
+        default: () => AUTO_EXTENSION,
+    },
+    chunkUploadSize: {
+        type: Number,
+        default: 1024,
+    },
+    currentHistoryId: {
+        type: String,
+        required: true,
+    },
+    currentUserId: {
+        type: String,
+        default: "",
+    },
+    datatypesDisableAuto: {
+        type: Boolean,
+        default: false,
+    },
+    defaultDbKey: {
+        type: String,
+        default: DEFAULT_DBKEY,
+    },
+    defaultExtension: {
+        type: String,
+        default: DEFAULT_EXTENSION,
+    },
+    fileSourcesConfigured: {
+        type: Boolean,
+        default: false,
+    },
+    ftpUploadSite: {
+        type: String,
+        default: "",
+    },
+    formats: {
+        type: Array,
+        default: null,
+    },
+    // Return uploads when done if supplied.
+    hasCallback: {
+        type: Boolean,
+        default: false,
+    },
+    // Restrict the forms to a single dataset upload if false
+    multiple: {
+        type: Boolean,
+        default: true,
+    },
+    selectable: {
+        type: Boolean,
+        default: false,
+    },
+    uploadPath: {
+        type: String,
+        required: true,
+    },
+});
+
+const extensionsSet = ref(false);
+const datatypesMapper = ref(null);
+const datatypesMapperReady = ref(false);
+const genomesSet = ref(false);
+const listExtensions = ref([]);
+const listGenomes = ref([]);
+const regular = ref(null);
+
+const effectiveExtensions = computed(() => {
+    if (props.formats === null || !datatypesMapperReady.value) {
+        return listExtensions.value;
+    } else {
+        const result = [];
+        listExtensions.value.forEach((extension) => {
+            if (extension && extension.id == "auto") {
+                result.push(extension);
+            } else if (datatypesMapper.value.isSubTypeOfAny(extension.id, props.formats)) {
+                result.push(extension);
+            }
+        });
+        return result;
+    }
+});
+
+const hasCompositeExtension = computed(() =>
+    effectiveExtensions.value.some((extension) => !!extension.composite_files)
+);
+const hasRegularExtension = computed(() => effectiveExtensions.value.some((extension) => !extension.composite_files));
+const historyAvailable = computed(() => Boolean(props.currentHistoryId));
+const ready = computed(
+    () => genomesSet.value && extensionsSet.value && historyAvailable.value && datatypesMapperReady.value
+);
+const showCollection = computed(() => !props.formats && props.multiple);
+const showComposite = computed(() => !props.formats || hasCompositeExtension);
+const showRegular = computed(() => !props.formats || hasRegularExtension);
+const showRules = computed(() => !props.formats || props.multiple);
+
+function immediateUpload(files) {
+    regular.value?.addFiles(files);
+}
+
+function toData(items, history_id, composite = false) {
+    return uploadPayload(items, history_id, composite);
+}
+
+async function loadExtensions() {
+    listExtensions.value = await getUploadDatatypes(props.datatypesDisableAuto, props.auto);
+    extensionsSet.value = true;
+}
+
+async function loadGenomes() {
+    listGenomes.value = await getUploadDbKeys(props.defaultDbKey);
+    genomesSet.value = true;
+}
+
+async function loadMappers() {
+    if (props.formats !== null) {
+        datatypesMapper.value = await getDatatypesMapper();
+    }
+    datatypesMapperReady.value = true;
+}
+
+function progress(percentage, status = null) {
+    if (percentage !== null) {
+        //this.eventHub.$emit(`upload:${field}`, val);
+    }
+    if (status !== null) {
+        //this.eventHub.$emit(`upload:${field}`, val);
+    }
+}
+
+onMounted(() => {
+    loadExtensions();
+    loadGenomes();
+    loadMappers();
+    //this.id = String(this._uid);
+});
+
+onUnmounted(() => {
+    //const modelUnload = this.model.get("onunload");
+    //modelUnload();
+});
+
+defineExpose({
+    immediateUpload,
+    toData,
+});
+</script>
+
 <template>
     <BTabs v-if="ready">
         <BTab v-if="showRegular" id="regular" title="Regular" button-id="tab-title-link-regular">
@@ -13,6 +182,7 @@
                 :history-id="currentHistoryId"
                 :list-genomes="listGenomes"
                 :multiple="multiple"
+                @progress="progress"
                 v-on="$listeners" />
         </BTab>
         <BTab v-if="showComposite" id="composite" title="Composite" button-id="tab-title-link-composite">
@@ -38,7 +208,6 @@
                 :history-id="currentHistoryId"
                 :is-collection="true"
                 :list-genomes="listGenomes"
-                :multiple="true"
                 v-on="$listeners" />
         </BTab>
         <BTab v-if="showRules" id="rule-based" title="Rule-based" button-id="tab-title-link-rule-based">
@@ -54,168 +223,3 @@
         <LoadingSpan message="Loading required information from Galaxy server." />
     </div>
 </template>
-
-<script>
-import Backbone from "backbone";
-import { BTab, BTabs } from "bootstrap-vue";
-import { getDatatypesMapper } from "components/Datatypes";
-import LoadingSpan from "components/LoadingSpan";
-import {
-    AUTO_EXTENSION,
-    DEFAULT_DBKEY,
-    DEFAULT_EXTENSION,
-    getUploadDatatypes,
-    getUploadDbKeys,
-} from "components/Upload/utils";
-
-import { uploadPayload } from "@/utils/uploadpayload.js";
-
-import Composite from "./Composite";
-import Default from "./Default";
-import RulesInput from "./RulesInput";
-
-export default {
-    components: {
-        Composite,
-        Default,
-        RulesInput,
-        LoadingSpan,
-        BTabs,
-        BTab,
-    },
-    props: {
-        currentHistoryId: { type: String, required: true },
-        currentUserId: { type: String, default: "" },
-        uploadPath: { type: String, required: true },
-        chunkUploadSize: { type: Number, default: 1024 },
-        fileSourcesConfigured: { type: Boolean, default: false },
-        ftpUploadSite: { type: String, default: "" },
-        defaultDbKey: { type: String, default: DEFAULT_DBKEY },
-        defaultExtension: { type: String, default: DEFAULT_EXTENSION },
-        datatypesDisableAuto: { type: Boolean, default: false },
-        formats: { type: Array, default: null },
-        // Restrict the forms to a single dataset upload if false
-        multiple: {
-            type: Boolean,
-            default: true,
-        },
-        // Return uploads when done if supplied.
-        hasCallback: {
-            type: Boolean,
-            default: false,
-        },
-        selectable: { type: Boolean, default: false },
-        auto: {
-            type: Object,
-            default: () => AUTO_EXTENSION,
-        },
-    },
-    data: function () {
-        return {
-            listGenomes: [],
-            listExtensions: [],
-            genomesSet: false,
-            extensionsSet: false,
-            datatypesMapper: null,
-            datatypesMapperReady: true,
-        };
-    },
-    computed: {
-        effectiveExtensions() {
-            if (this.formats === null || !this.datatypesMapperReady) {
-                return this.listExtensions;
-            } else {
-                const result = [];
-                this.listExtensions.forEach((extension) => {
-                    if (extension && extension.id == "auto") {
-                        result.push(extension);
-                    } else if (this.datatypesMapper.isSubTypeOfAny(extension.id, this.formats)) {
-                        result.push(extension);
-                    }
-                });
-                return result;
-            }
-        },
-        hasCompositeExtension() {
-            return this.effectiveExtensions.some((extension) => !!extension.composite_files);
-        },
-        hasRegularExtension() {
-            return this.effectiveExtensions.some((extension) => !extension.composite_files);
-        },
-        historyAvailable() {
-            return Boolean(this.currentHistoryId);
-        },
-        ready() {
-            return this.genomesSet && this.extensionsSet && this.historyAvailable && this.datatypesMapperReady;
-        },
-        showCollection() {
-            return !this.formats && this.multiple;
-        },
-        showComposite() {
-            return !this.formats || this.hasCompositeExtension;
-        },
-        showRegular() {
-            return !this.formats || this.hasRegularExtension;
-        },
-        showRules() {
-            return !this.formats || this.multiple;
-        },
-    },
-    created() {
-        this.model = new Backbone.Model({
-            label: "Load Data",
-            percentage: 0,
-            status: "",
-            onunload: function () {},
-            onclick: function () {},
-        });
-
-        this.model.on("change", (model) => {
-            const { changed } = model;
-            for (const [field, val] of Object.entries(changed)) {
-                this.eventHub.$emit(`upload:${field}`, val);
-            }
-        });
-
-        this.loadExtensions();
-        this.loadGenomes();
-
-        if (this.formats !== null) {
-            this.datatypesMapperReady = false;
-            getDatatypesMapper().then((datatypesMapper) => {
-                this.datatypesMapper = datatypesMapper;
-                this.datatypesMapperReady = true;
-            });
-        } else {
-            this.datatypesMapperReady = true;
-        }
-    },
-    beforeDestroy() {
-        const modelUnload = this.model.get("onunload");
-        modelUnload();
-    },
-    mounted() {
-        this.id = String(this._uid);
-    },
-    methods: {
-        /**
-         * Package API data from array of backbone models
-         * @param{Array} items - Upload items/rows filtered from a collection
-         */
-        toData: function (items, history_id, composite = false) {
-            return uploadPayload(items, history_id, composite);
-        },
-        immediateUpload: function (files) {
-            this.$refs.regular?.addFiles(files);
-        },
-        loadExtensions: async function () {
-            this.listExtensions = await getUploadDatatypes(this.datatypesDisableAuto, this.auto);
-            this.extensionsSet = true;
-        },
-        loadGenomes: async function () {
-            this.listGenomes = await getUploadDbKeys(this.defaultDbKey);
-            this.genomesSet = true;
-        },
-    },
-};
-</script>
