a2e3c6ba0fc218102c69a21310ec007770d189ca
mvdbeek <m.vandenbeek@gmail.com>
2023-08-08 11:07:06 +0200

    Fix wrong model_dump_json argument

--- lib/galaxy/web/framework/decorators.py
+++ lib/galaxy/web/framework/decorators.py
@@ -375,7 +375,7 @@ def format_return_as_json(rval, jsonp_callback=None, pretty=False):
     """
     dumps_kwargs = dict(indent=4, sort_keys=True) if pretty else {}
     if isinstance(rval, BaseModel):
-        json = rval.model_dump_json(**dumps_kwargs)
+        json = rval.model_dump_json(indent=4)
     else:
         json = safe_dumps(rval, **dumps_kwargs)
     if jsonp_callback:
