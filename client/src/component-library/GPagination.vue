403b0e6b04167eef39eabbd003fb7b532da83a3a
Alireza Heidari <itisalirh@gmail.com>
2023-08-10 15:23:33 +1000

    add missing prop and model in GPagination

--- client/src/component-library/GPagination.vue
+++ client/src/component-library/GPagination.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
 import { BPagination } from "bootstrap-vue";
+import { computed } from "vue";
+
+interface Props {
+    value: boolean | string | number;
+}
+
+const props = defineProps<Props>();
+
+const emit = defineEmits<{
+    (e: "input", value: Props["value"]): void;
+}>();
+
+const model = computed({
+    get: () => props.value,
+    set: (value) => {
+        emit("input", value);
+    },
+});
 </script>
 
 <template>
-    <BPagination v-bind="$attrs" v-on="$listeners">
+    <BPagination v-model="model" v-bind="$attrs" v-on="$listeners">
         <slot></slot>
     </BPagination>
 </template>
