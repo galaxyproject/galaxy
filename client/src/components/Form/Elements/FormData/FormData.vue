26034b950e640a852782f61dba589ee3faba1c84
guerler <aysam.guerler@gmail.com>
2023-08-18 14:39:12 +0200

    Add comments and trigger set value when switching between select field types

--- client/src/components/Form/Elements/FormData/FormData.vue
+++ client/src/components/Form/Elements/FormData/FormData.vue
@@ -4,7 +4,7 @@ import { faCopy, faFile, faFolder } from "@fortawesome/free-regular-svg-icons";
 import { faExclamation, faLink, faUnlink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { BFormCheckbox, BFormRadio, BFormRadioGroup } from "bootstrap-vue";
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 
 import type { DataOption } from "./types";
 import { BATCH, SOURCE, VARIANTS } from "./variants";
@@ -13,24 +13,19 @@ import FormSelect from "@/components/Form/Elements/FormSelect.vue";
 
 library.add(faCopy, faExclamation, faFile, faFolder, faLink, faUnlink);
 
-interface DataValue {
-    values: Array<DataOption>;
-}
-
 interface SelectOption {
     label: string;
     value: DataOption | null;
 }
-
-/** Store options which need to be preserved **/
-const keepOptions: Record<string, SelectOption> = {};
 
 const props = withDefaults(
     defineProps<{
         multiple?: boolean;
         optional?: boolean;
         options: Record<string, Array<DataOption>>;
-        value?: DataValue;
+        value?: {
+            values: Array<DataOption>;
+        };
         extensions?: Array<string>;
         type?: string;
         flavor?: string;
@@ -49,57 +44,49 @@ const props = withDefaults(
 
 const $emit = defineEmits(["input"]);
 
+// Determines wether values should be processed as linked or unlinked
 const currentBatch = ref(true);
 
+// Indicates which of the select field from the set of variants is currently shown
 const currentField = ref(0);
 
-const currentOptions = computed(() => {
-    if (currentSource.value && currentSource.value in props.options) {
-        return props.options[currentSource.value] || [];
-    } else {
-        return [];
-    }
-});
+/** Store options which need to be preserved **/
+const keepOptions: Record<string, SelectOption> = {};
 
+/**
+ * Provides the currently shown source type
+ */
 const currentSource = computed(() => currentVariant.value && currentVariant.value.src);
 
+/**
+ * Interface between incoming input value and select field value
+ */
 const currentValue = computed({
     get: () => {
         const value: Array<DataOption> = [];
         if (props.value) {
-            props.value.values.forEach((v) => {
-                const sourceOptions = props.options[v.src];
-                if (sourceOptions) {
-                    const foundEntry = sourceOptions.find((entry) => entry.id === v.id);
-                    if (foundEntry) {
-                        value.push(foundEntry);
+            for (const v of props.value.values) {
+                const foundEntry = formattedOptions.value.find((entry) => entry.value?.id === v.id);
+                if (foundEntry && foundEntry.value) {
+                    value.push(foundEntry.value);
+                    if (!currentVariant.value?.multiple) {
+                        break;
                     }
                 }
-            });
+            }
             return value;
         } else {
             return null;
         }
     },
     set: (val) => {
-        const batchMode = currentVariant.value && currentVariant.value.batch;
-        if (val) {
-            const values = Array.isArray(val) ? val : [val];
-            $emit("input", {
-                batch: batchMode !== BATCH.DISABLED,
-                product: batchMode === BATCH.ENABLED && !currentBatch.value,
-                values: values.map((entry) => ({
-                    id: entry.id,
-                    src: entry.src,
-                    map_over_type: entry.map_over_type,
-                })),
-            });
-        } else {
-            $emit("input", null);
-        }
+        setValue(val);
     },
 });
 
+/**
+ * Returns the variant i.e. attributes, of the shown select field.
+ */
 const currentVariant = computed(() => {
     if (variant.value && currentField.value < variant.value.length) {
         return variant.value[currentField.value];
@@ -108,40 +95,76 @@ const currentVariant = computed(() => {
     }
 });
 
+/**
+ * Converts and populates options for the shown select field
+ */
 const formattedOptions = computed(() => {
     const keepSet = new Set();
-    const result: Array<SelectOption> = currentOptions.value.map((option) => {
-        const newOption = {
-            label: `${option.hid}: ${option.name}`,
-            value: option || null,
-        };
-        if (option.keep) {
-            const keepKey = `${option.id}_${option.src}`;
-            keepOptions[keepKey] = newOption;
-            keepSet.add(keepKey);
-        }
-        return newOption;
-    });
-    Object.entries(keepOptions).forEach(([key, option]) => {
-        if (!keepSet.has(key) && option.value?.src === currentSource.value) {
-            result.unshift(option);
-        }
-    });
-    if (props.optional && !props.multiple) {
-        result.unshift({
-            label: "Nothing selected",
-            value: null,
+    if (currentSource.value && currentSource.value in props.options) {
+        const options = props.options[currentSource.value] || [];
+        const result: Array<SelectOption> = options.map((option) => {
+            const newOption = {
+                label: `${option.hid}: ${option.name}`,
+                value: option || null,
+            };
+            if (option.keep) {
+                const keepKey = `${option.id}_${option.src}`;
+                keepOptions[keepKey] = newOption;
+                keepSet.add(keepKey);
+            }
+            return newOption;
         });
+        Object.entries(keepOptions).forEach(([key, option]) => {
+            if (!keepSet.has(key) && option.value?.src === currentSource.value) {
+                result.unshift(option);
+            }
+        });
+        return result;
+    } else {
+        return [];
     }
-    return result;
 });
 
+/**
+ * Provides the array of available variants associated with a specific form data type
+ */
 const variant = computed(() => {
     const flavorKey = props.flavor ? `${props.flavor}_` : "";
     const multipleKey = props.multiple ? `_${props.multiple}` : "";
     const variantKey = `${flavorKey}${props.type}${multipleKey}`;
     return VARIANTS[variantKey];
 });
+
+/**
+ * Processes and submits values from the select field
+ */
+function setValue(val: Array<DataOption> | DataOption | null) {
+    const batchMode = currentVariant.value && currentVariant.value.batch;
+    if (val) {
+        const values = Array.isArray(val) ? val : [val];
+        $emit("input", {
+            batch: batchMode !== BATCH.DISABLED,
+            product: batchMode === BATCH.ENABLED && !currentBatch.value,
+            values: values.map((entry) => ({
+                id: entry.id,
+                src: entry.src,
+                map_over_type: entry.map_over_type,
+            })),
+        });
+    } else {
+        $emit("input", null);
+    }
+}
+
+/**
+ * Watch and set current value if user switches to a different select field
+ */
+watch(
+    () => currentVariant.value,
+    () => {
+        setValue(currentValue.value);
+    }
+);
 </script>
 
 <template>
