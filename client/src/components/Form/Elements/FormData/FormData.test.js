453f2f131e4733bdd9032196dbd3ed5d15a7423c
guerler <aysam.guerler@gmail.com>
2023-08-23 10:05:32 +0200

    Add unit test for multiple data selector

--- client/src/components/Form/Elements/FormData/FormData.test.js
+++ client/src/components/Form/Elements/FormData/FormData.test.js
@@ -36,7 +36,7 @@ const SELECT_OPTIONS = ".multiselect__element";
 const SELECTED_VALUE = ".multiselect__option--selected span";
 
 describe("FormData", () => {
-    it("single dataset", async () => {
+    it("regular data", async () => {
         const wrapper = createTarget({
             value: null,
             options: defaultOptions,
@@ -69,5 +69,67 @@ describe("FormData", () => {
         expect(wrapper.emitted().input[1][0]).toEqual(value_1);
         await wrapper.setProps({ value: value_1 });
         expect(wrapper.find(SELECTED_VALUE).text()).toEqual("1: hdaName1");
+    });
+
+    it("optional dataset", async () => {
+        const wrapper = createTarget({
+            value: null,
+            optional: true,
+            options: defaultOptions,
+        });
+        expect(wrapper.emitted().input[0][0]).toEqual(null);
+        expect(wrapper.emitted().input.length).toEqual(1);
+        expect(wrapper.find(SELECTED_VALUE).text()).toEqual("Nothing selected");
+        expect(wrapper.findAll(SELECT_OPTIONS).length).toBe(4);
+    });
+
+    it("multiple datasets", async () => {
+        const wrapper = createTarget({
+            value: {
+                values: [
+                    { id: "hda2", src: "hda" },
+                    { id: "hda3", src: "hda" },
+                ],
+            },
+            multiple: true,
+            optional: true,
+            options: defaultOptions,
+        });
+        const options = wrapper.find(".btn-group").findAll("button");
+        expect(options.length).toBe(3);
+        expect(options.at(0).classes()).toContain("active");
+        expect(options.at(0).attributes("title")).toBe("Multiple datasets");
+        expect(wrapper.emitted().input[0][0]).toEqual({
+            batch: false,
+            product: false,
+            values: [
+                { id: "hda2", map_over_type: null, src: "hda" },
+                { id: "hda3", map_over_type: null, src: "hda" },
+            ],
+        });
+        expect(wrapper.emitted().input.length).toEqual(1);
+        const selectedValues = wrapper.findAll(SELECTED_VALUE);
+        expect(selectedValues.length).toBe(2);
+        expect(selectedValues.at(0).text()).toBe("3: hdaName3");
+        expect(selectedValues.at(1).text()).toBe("2: hdaName2");
+        const value_0 = {
+            batch: false,
+            product: false,
+            values: [
+                { id: "hda2", map_over_type: null, src: "hda" },
+                { id: "hda3", map_over_type: null, src: "hda" },
+            ],
+        };
+        expect(wrapper.emitted().input[0][0]).toEqual(value_0);
+        await selectedValues.at(0).trigger("click");
+        const value_1 = { batch: false, product: false, values: [{ id: "hda2", map_over_type: null, src: "hda" }] };
+        expect(wrapper.emitted().input[1][0]).toEqual(value_1);
+        await wrapper.setProps({ value: value_1 });
+        await selectedValues.at(1).trigger("click");
+        const value_2 = { batch: false, product: false, values: [{ id: "hda2", map_over_type: null, src: "hda" }] };
+        expect(wrapper.emitted().input[1][0]).toEqual(value_2);
+        await wrapper.setProps({ value: value_2 });
+        expect(wrapper.emitted().input.length).toBe(3);
+        expect(wrapper.emitted().input[2][0]).toEqual(null);
     });
 });
