f39f4b982c21edc26f0aac7dfa7eaabc4b0cc57c
Matthias Bernt <m.bernt@ufz.de>
2023-08-25 11:00:06 +0200

    make sure that TMP, TEMP, and TMPDIR are set
    seems needed for docker volume strings (an empty
    variable would lead to `-v "::rw"` which causes
    `docker: invalid spec: ::rw: empty section between colons.`)

--- lib/galaxy/jobs/runners/util/job_script/DEFAULT_JOB_FILE_TEMPLATE.sh
+++ lib/galaxy/jobs/runners/util/job_script/DEFAULT_JOB_FILE_TEMPLATE.sh
@@ -39,6 +39,29 @@ GALAXY_LIB="$galaxy_lib"
 _galaxy_setup_environment "$PRESERVE_GALAXY_ENVIRONMENT"
 export _GALAXY_JOB_HOME_DIR
 export _GALAXY_JOB_TMP_DIR
+
+TEMP="${TEMP:-$TMP}"
+TMPDIR="${TMPDIR:-$TMP}"
+
+TMP="${TMP:-$TEMP}"
+TMPDIR="${TMPDIR:-$TEMP}"
+
+TMP="${TMP:-$TMPDIR}"
+TEMP="${TEMP:-$TMPDIR}"
+
+TMP="${TMP:-_GALAXY_JOB_TMP_DIR}"
+TEMP="${TEMP:-_GALAXY_JOB_TMP_DIR}"
+TMPDIR="${TMPDIR:-_GALAXY_JOB_TMP_DIR}"
+
+export TMP
+export TEMP
+export TMPDIR
+
+echo "_GALAXY_JOB_TMP_DIR $_GALAXY_JOB_TMP_DIR" >> outputs/tmp 
+echo "TMP $TMP" >> outputs/tmp
+echo "TEMP $TEMP" >> outputs/tmp 
+echo "TMPDIR $TMPDIR" >> outputs/tmp 
+
 GALAXY_PYTHON=`command -v python`
 cd $working_directory
 $memory_statement
