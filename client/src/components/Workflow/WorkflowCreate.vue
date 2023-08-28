7d4ff73425f04a29937b8816bfb8fe9d3ed5888b
Alireza Heidari <itisalirh@gmail.com>
2023-08-28 18:31:24 +1000

    create WorkflowCreate component

---
+++ client/src/components/Workflow/WorkflowCreate.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { type AxiosError } from "axios";
+import { BFormInput } from "bootstrap-vue";
+import { ref } from "vue";
+
+import { createWorkflow } from "@/components/Workflow/workflows.services";
+import { Toast } from "@/composables/toast";
+
+import AsyncButton from "@/components/Common/AsyncButton.vue";
+import Heading from "@/components/Common/Heading.vue";
+
+library.add(faCheck);
+
+interface Pros {
+    modalView: boolean;
+}
+
+defineProps<Pros>();
+
+const workflowAnnotation = ref("");
+const workflowName = ref("Unnamed workflow");
+const workflowNameInput = ref<HTMLInputElement | null>(null);
+
+async function onCreate() {
+    try {
+        await createWorkflow(workflowName.value, workflowAnnotation.value);
+    } catch (e) {
+        const error = e as AxiosError<{ err_msg?: string }>;
+
+        Toast.error(error.response?.data.err_msg ?? "Failed to create workflow");
+    }
+}
+</script>
+
+<template>
+    <div>
+        <Heading v-if="!modalView" h1 separator size="xl">Create workflow</Heading>
+
+        <label for="workflow-name-input" class="font-weight-bold"> Workflow name </label>
+        <BFormInput
+            id="workflow-name-input"
+            ref="workflowNameInput"
+            v-model="workflowName"
+            class="mb-2"
+            type="text"
+            placeholder="Enter workflow name"
+            description="Workflow name"
+            @keyup.enter="() => onCreate()" />
+
+        <label for="workflow-annotation-input" class="font-weight-bold"> Workflow annotation </label>
+        <BFormInput
+            id="workflow-annotation-input"
+            v-model="workflowAnnotation"
+            class="mb-2"
+            type="text"
+            placeholder="Enter workflow annotation">
+        </BFormInput>
+
+        <div class="float-right">
+            <AsyncButton
+                variant="primary"
+                :icon="faCheck"
+                title="Create workflow"
+                :disabled="workflowName.length === 0"
+                :action="() => onCreate()">
+                Create
+            </AsyncButton>
+        </div>
+    </div>
+</template>
