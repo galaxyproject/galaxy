54770d13ef446431bbbc8609c6a677b690b1c7d4
Maiken Pedersen <maikenp@usit.uio.no>
2023-09-01 14:13:32 +0200

    Update __init__.py
    ArcRESTJobRunner

--- lib/galaxy/dependencies/__init__.py
+++ lib/galaxy/dependencies/__init__.py
@@ -208,6 +208,9 @@ class ConditionalDependencies:
     def check_pykube(self):
         return "galaxy.jobs.runners.kubernetes:KubernetesJobRunner" in self.job_runners or which("kubectl")
 
+    def check_pyarcrest(self):
+        return "galaxy.jobs.runners.arc:ArcRESTJobRunner" in self.job_runners
+
     def check_chronos_python(self):
         return "galaxy.jobs.runners.chronos:ChronosJobRunner" in self.job_runners
 
