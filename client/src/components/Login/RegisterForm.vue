a6b2b2f3b20d9cd8e9cda6e50f32b3f70b93f4e8
Dannon Baker <dannon.baker@gmail.com>
2023-09-06 12:20:55 -0400

    Add eslint bypass for safe v-html

--- client/src/components/Login/RegisterForm.vue
+++ client/src/components/Login/RegisterForm.vue
@@ -3,6 +3,7 @@
         <div class="row justify-content-md-center">
             <div class="col col-lg-6">
                 <b-alert :show="!!registrationWarningMessage" variant="info">
+                    <!-- eslint-disable-next-line vue/no-v-html -->
                     <span v-html="registrationWarningMessage" />
                 </b-alert>
                 <b-alert :show="!!messageText" :variant="messageVariant">
