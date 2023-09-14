4c8e526e63fdbedfc3e3c89642e6baa27b03f39c
Łukasz Opioła <ymopiola@cyfronet.pl>
2023-09-14 11:33:53 +0200

    Apply linter suggestions concerning lstrip()

--- lib/galaxy/files/sources/onedata.py
+++ lib/galaxy/files/sources/onedata.py
@@ -8,6 +8,12 @@ from . import FilesSourceOptions
 from ._pyfilesystem2 import PyFilesystem2FilesSource
 
 
+def remove_prefix(prefix, string):
+    if string.startswith(prefix):
+        string = string[len(prefix):]
+    return string
+
+
 class OnedataFilesSource(PyFilesystem2FilesSource):
     plugin_type = "onedata"
     required_module = OnedataRESTFS
@@ -16,7 +22,7 @@ class OnedataFilesSource(PyFilesystem2FilesSource):
     def _open_fs(self, user_context=None, opts: Optional[FilesSourceOptions] = None):
         props = self._serialization_props(user_context)
         onezone_domain = props.pop("onezoneDomain", {}) or ""
-        onezone_domain = onezone_domain.lstrip("https://").lstrip("http://")
+        onezone_domain = remove_prefix("http://", remove_prefix("https://", onezone_domain))
         access_token = props.pop("accessToken", {}) or ""
         handle = OnedataRESTFS(onezone_domain, access_token)
         return handle
