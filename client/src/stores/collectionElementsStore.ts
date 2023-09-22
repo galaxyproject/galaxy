25491c7759001c50b4674e9e5d4f967dec8f5f57
davelopez <46503462+davelopez@users.noreply.github.com>
2023-09-22 16:11:28 +0200

    Add new collection elements Pinia store

---
+++ client/src/stores/collectionElementsStore.ts
@@ -0,0 +1,70 @@
+import { defineStore } from "pinia";
+import Vue, { computed, ref } from "vue";
+
+import { DCESummary, HDCASummary, HistoryContentItemBase } from "./services";
+import * as Service from "./services/datasetCollection.service";
+
+export const useCollectionElementsStore = defineStore("collectionElementsStore", () => {
+    const storedCollections = ref<{ [key: string]: HDCASummary }>({});
+    const loadingCollectionElements = ref<{ [key: string]: boolean }>({});
+    const storedCollectionElements = ref<{ [key: string]: DCESummary[] }>({});
+
+    const getCollectionElements = computed(() => {
+        return (collection: HDCASummary, offset = 0, limit = 50) => {
+            const elements = storedCollectionElements.value[collection.id] ?? [];
+            fetchMissingElements({ collection, offset, limit });
+            return elements ?? null;
+        };
+    });
+
+    const isLoadingCollectionElements = computed(() => {
+        return (collection: HDCASummary) => {
+            return loadingCollectionElements.value[collection.id] ?? false;
+        };
+    });
+
+    async function fetchMissingElements(params: { collection: HDCASummary; offset: number; limit: number }) {
+        try {
+            const maxElementCountInCollection = params.collection.element_count ?? 0;
+            const storedElements = storedCollectionElements.value[params.collection.id] ?? [];
+            // Collections are immutable, so there is no need to fetch elements if the range we want is already stored
+            if (params.offset + params.limit <= storedElements.length) {
+                return;
+            }
+            // If we already have items at the offset, we can start fetching from the next offset
+            params.offset = storedElements.length;
+
+            if (params.offset >= maxElementCountInCollection) {
+                return;
+            }
+
+            Vue.set(loadingCollectionElements.value, params.collection.id, true);
+            const fetchedElements = await Service.fetchElementsFromHDCA({
+                hdca: params.collection,
+                offset: params.offset,
+                limit: params.limit,
+            });
+            const updatedElements = [...storedElements, ...fetchedElements];
+            Vue.set(storedCollectionElements.value, params.collection.id, updatedElements);
+        } finally {
+            Vue.delete(loadingCollectionElements.value, params.collection.id);
+        }
+    }
+
+    function saveCollectionObjects(historyContentsPayload: HistoryContentItemBase[]) {
+        const collectionsInHistory = historyContentsPayload.filter(
+            (entry) => entry.history_content_type === "dataset_collection"
+        ) as HDCASummary[];
+        for (const collection of collectionsInHistory) {
+            Vue.set<HDCASummary>(storedCollections.value, collection.id, collection);
+        }
+    }
+
+    return {
+        storedCollections,
+        storedCollectionElements,
+        getCollectionElements,
+        isLoadingCollectionElements,
+        saveCollectionObjects,
+    };
+});
