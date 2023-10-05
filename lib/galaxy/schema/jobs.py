ea520dfc2173ac7d67946ced4699fd43a38fdb57
Tillman <theisner@posteo.de>
2023-10-05 09:10:48 +0200

    Add pydantic model for dictify methods

--- lib/galaxy/schema/jobs.py
+++ lib/galaxy/schema/jobs.py
@@ -1,6 +1,11 @@
-from galaxy.schema.schema import Model
+from galaxy.schema.schema import EncodedDatasetSourceId, Model
 
 
 class JobInputSummary(Model):
     has_empty_inputs: bool
     has_duplicate_inputs: bool
+
+
+class JobAssociation(Model):
+    name: str
+    dataset: EncodedDatasetSourceId
