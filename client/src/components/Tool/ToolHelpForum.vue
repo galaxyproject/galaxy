a801bba26d06016d092d2e92219b258028c48325
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-10-06 10:45:29 +0200

    add helpAvailable computed

--- client/src/components/Tool/ToolHelpForum.vue
+++ client/src/components/Tool/ToolHelpForum.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { BButton } from "bootstrap-vue";
+import { computed } from "vue";
 
 import Heading from "@/components/Common/Heading.vue";
 import ExternalLink from "@/components/ExternalLink.vue";
+
+const helpAvailable = computed(() => true);
 </script>
 
 <template>
     <div class="tool-help-forum mt-2 mb-4">
         <Heading h2 separator bold size="sm">Help Forum</Heading>
 
-        <p>
+        <p v-if="helpAvailable">
+            Following questions on the <ExternalLink href="help forum link here"> Help Forum </ExternalLink> mention
+            this tool:
+        </p>
+        <p v-else>
             There are no questions on the
             <ExternalLink href="help forum link here"> Help Forum </ExternalLink>
             about this tool.
