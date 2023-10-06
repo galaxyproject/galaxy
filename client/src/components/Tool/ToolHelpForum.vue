6840ca75cd2de9988a8e8683550d9a693e1e1bd4
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-10-06 14:44:51 +0200

    include blurb add show more indicator

--- client/src/components/Tool/ToolHelpForum.vue
+++ client/src/components/Tool/ToolHelpForum.vue
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { BButton, BCard, BCardText } from "bootstrap-vue";
 import { computed, onMounted, ref } from "vue";
 
-import { type HelpForumTopic } from "./helpForumServices";
+import { type HelpForumPost, type HelpForumTopic } from "./helpForumServices";
 import testResponse from "./testResponse.json";
 
 import Heading from "@/components/Common/Heading.vue";
@@ -11,6 +11,7 @@ import ExternalLink from "@/components/ExternalLink.vue";
 
 const helpAvailable = computed(() => true);
 const topics = ref<HelpForumTopic[]>([]);
+const posts = ref<HelpForumPost[]>([]);
 
 const root = ref(null);
 
@@ -18,7 +19,18 @@ onMounted(async () => {
     //const response = await search("workflows");
 
     topics.value = testResponse.topics;
+    posts.value = testResponse.posts;
 });
+
+const displayCount = 5;
+
+const displayedTopics = computed(() => topics.value.slice(0, displayCount - 1));
+const hasMore = computed(() => topics.value.length > displayCount);
+
+function blurbForTopic(topicId: number): string {
+    const firstPost = posts.value.find((post) => post.topic_id === topicId && post.post_number === 1);
+    return firstPost?.blurb ?? "no content";
+}
 </script>
 
 <template>
@@ -35,12 +47,12 @@ onMounted(async () => {
             about this tool.
         </p>
 
-        <BCard v-for="topic in topics" :key="topic.id">
-            <template v-slot:header>
-                <Heading h3 inline size="sm">{{ topic.title }}</Heading>
-            </template>
-            <BCardText> hello world </BCardText>
+        <BCard v-for="topic in displayedTopics" :key="topic.id" class="mb-2">
+            <Heading h3 size="sm"> {{ topic.title }} </Heading>
+            <BCardText> {{ blurbForTopic(topic.id) }} </BCardText>
         </BCard>
+
+        <div v-if="hasMore">more...</div>
 
         <BButton variant="primary" class="font-weight-bold" target="blank" href="help forum new question link here">
             <FontAwesomeIcon :icon="['gxd', 'galaxyLogo']" /> Ask a new question
