6faa4b3e107b3fc59dbb1be9e71a2c9aa4a99180
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-10-06 12:00:39 +0200

    run prettier

--- client/src/components/Tool/helpForumServices.ts
+++ client/src/components/Tool/helpForumServices.ts
@@ -3,40 +3,40 @@ import { useConfigStore } from "@/stores/configurationStore";
 const configStore = useConfigStore();
 
 export interface HelpForumTopic {
-	id: number;
-	title: string;
-	fancy_title: string;
-	slug: string;
-	posts_count: number;
-	reply_count: number;
-	highest_post_number: number;
-	created_at: string;
-	last_posted_at: string;
-	bumped: boolean;
-	bumped_at: string;
-	archetype: unknown;
-	unseen: boolean;
-	pinned: boolean;
-	unpinned: unknown;
-	visible: boolean;
-	closed: boolean;
-	archived: boolean;
-	bookmarked: unknown;
-	liked: unknown;
-	tags: string[];
-	tags_descriptions: unknown;
-	category_id: number;
-	has_accepted_answer: boolean;
+    id: number;
+    title: string;
+    fancy_title: string;
+    slug: string;
+    posts_count: number;
+    reply_count: number;
+    highest_post_number: number;
+    created_at: string;
+    last_posted_at: string;
+    bumped: boolean;
+    bumped_at: string;
+    archetype: unknown;
+    unseen: boolean;
+    pinned: boolean;
+    unpinned: unknown;
+    visible: boolean;
+    closed: boolean;
+    archived: boolean;
+    bookmarked: unknown;
+    liked: unknown;
+    tags: string[];
+    tags_descriptions: unknown;
+    category_id: number;
+    has_accepted_answer: boolean;
 }
 
 export interface HelpForumSearchResponseData {
-	posts: unknown;
-	topics: HelpForumTopic[];
-	users: unknown;
-	categories: unknown;
-	tags: unknown;
-	groups: unknown;
-	grouped_search_result: unknown;
+    posts: unknown;
+    topics: HelpForumTopic[];
+    users: unknown;
+    categories: unknown;
+    tags: unknown;
+    groups: unknown;
+    grouped_search_result: unknown;
 }
 
 /**
@@ -46,18 +46,18 @@ export interface HelpForumSearchResponseData {
  * @param tags forum tags
  */
 export async function search(term: string, tags: string[] = []): Promise<HelpForumSearchResponseData> {
-	const apiBaseUrl = configStore.config.help_forum_api_url;
-	const apiUrl = new URL("/search.json", apiBaseUrl);
+    const apiBaseUrl = configStore.config.help_forum_api_url;
+    const apiUrl = new URL("/search.json", apiBaseUrl);
 
-	let query = `${term} order:latest_topic`;
+    let query = `${term} order:latest_topic`;
 
-	if (tags.length > 0) {
-		query += `tags:${tags.join(",")}`;
-	}
+    if (tags.length > 0) {
+        query += `tags:${tags.join(",")}`;
+    }
 
-	apiUrl.searchParams.append("q", encodeURIComponent(query));
+    apiUrl.searchParams.append("q", encodeURIComponent(query));
 
-	const res = await fetch(apiUrl);
+    const res = await fetch(apiUrl);
 
-	return await res.json() as HelpForumSearchResponseData;
+    return (await res.json()) as HelpForumSearchResponseData;
 }
