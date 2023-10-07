48dcf862c967f815c12fd47f3f2c28630840e370
Martin Cech <emulatorer@gmail.com>
2023-10-07 21:49:17 +0200

    fix the call arguments
    https://pysam.readthedocs.io/en/v0.21.0/api.html?highlight=AlignedSegment#pysam.AlignedSegment.tostring

--- lib/galaxy/datatypes/binary.py
+++ lib/galaxy/datatypes/binary.py
@@ -664,7 +664,7 @@ class BamNative(CompressedArchive, _BamOrSam):
                             break
 
                         offset = bamfile.tell()
-                        bamline = alignment.to_string(bamfile)
+                        bamline = alignment.to_string()
                         # With multiple tags, Galaxy would display each as a separate column
                         # because the 'to_string()' function uses tabs also between tags.
                         # Below code will turn these extra tabs into spaces.
