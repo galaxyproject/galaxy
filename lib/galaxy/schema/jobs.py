5096cbe8eed5ed2b3baafa43b86830ac5d00310b
Tillman <theisner@posteo.de>
2023-10-09 17:14:24 +0200

    Add pydantic models for error operation

--- lib/galaxy/schema/jobs.py
+++ lib/galaxy/schema/jobs.py
@@ -1,3 +1,9 @@
+from typing import (
+    List,
+    Optional,
+)
+
+from galaxy.schema.fields import DecodedDatabaseIdField
 from galaxy.schema.schema import (
     EncodedDatasetSourceId,
     Model,
@@ -9,6 +15,18 @@ class JobInputSummary(Model):
     has_duplicate_inputs: bool
 
 
+# TODO: Use Tuple again when `make update-client-api-schema` supports them
+class JobErrorSummary(Model):
+    # messages: List[Union[Tuple[str, str], List[str]]]
+    messages: List[List[str]]
+
+
 class JobAssociation(Model):
     name: str
     dataset: EncodedDatasetSourceId
+
+
+class ReportJobErrorPayload(Model):
+    dataset_id: DecodedDatabaseIdField
+    email: Optional[str] = None
+    message: Optional[str] = None
