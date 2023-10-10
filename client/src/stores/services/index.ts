9fc98f927fc7c78c105067ef9def1b6e9a633b53
davelopez <46503462+davelopez@users.noreply.github.com>
2023-10-10 14:21:26 +0200

    Refactor collection related type definitions + docs

--- client/src/stores/services/index.ts
+++ client/src/stores/services/index.ts
@@ -1,31 +1,76 @@
 import { components } from "@/schema";
 
-/** Minimal representation of a collection that can contain datasets or other collections.
- *
- * This is used as a common interface to be able to fetch the contents of a collection regardless
- * of whether it is an HDCA (top level) or a nested collection (DCO).
- *
- * To convert a DCO to a CollectionEntry we need the parent HDCA ID and the name of the
- * collection (DCE identifier). The collection_id is the ID of the DCO and collection_type as well.
+/**
+ * Contains minimal information about a HistoryContentItem.
  */
-export interface CollectionEntry {
-    /**HDCA ID */
-    id: string;
-    /**DCO ID */
-    collection_id: string;
-    /**Name of the HDCA or DatasetCollectionElement identifier */
-    name: string;
-    /**Type of the collection */
-    collection_type: string;
-}
-
-export type DatasetSummary = components["schemas"]["HDASummary"];
-export type DatasetDetails = components["schemas"]["HDADetailed"];
-export type DCESummary = components["schemas"]["DCESummary"];
-export type HDCASummary = components["schemas"]["HDCASummary"] & CollectionEntry;
-export type HDCADetailed = components["schemas"]["HDCADetailed"];
-export type DCObject = components["schemas"]["DCObject"];
-
 export type HistoryContentItemBase = components["schemas"]["EncodedHistoryContentItem"];
 
+/**
+ * Contains summary information about a HistoryDatasetAssociation.
+ */
+export type DatasetSummary = components["schemas"]["HDASummary"];
+
+/**
+ * Contains additional details about a HistoryDatasetAssociation.
+ */
+export type DatasetDetails = components["schemas"]["HDADetailed"];
+
+/**
+ * Represents a HistoryDatasetAssociation with either summary or detailed information.
+ */
 export type DatasetEntry = DatasetSummary | DatasetDetails;
+
+/**
+ * Contains summary information about a DCE (DatasetCollectionElement).
+ *
+ * DCEs associate a parent collection to its elements. Those elements can be either
+ * HDAs or other DCs (DatasetCollections).
+ * The type of the element is indicated by the `element_type` field and the element
+ * itself is contained in the `object` field.
+ */
+export type DCESummary = components["schemas"]["DCESummary"];
+
+/**
+ * Contains summary information about a HDCA (HistoryDatasetCollectionAssociation).
+ *
+ * HDCAs are (top level only) history items that contains information about the association
+ * between a History and a DatasetCollection.
+ */
+export type HDCASummary = components["schemas"]["HDCASummary"];
+
+/**
+ * Contains additional details about a HistoryDatasetCollectionAssociation.
+ */
+export type HDCADetailed = components["schemas"]["HDCADetailed"];
+
+/**
+ * Contains information about a DatasetCollection.
+ *
+ * DatasetCollections are immutable and contain one or more DCEs.
+ */
+export type DCObject = components["schemas"]["DCObject"];
+
+/**
+ * Represents a SubCollection as a DatasetCollection with additional information to simplify its handling.
+ *
+ * This is used to be able to distinguish between top level HDCAs and sub-collections.
+ * It helps simplify both, the representation of sub-collections in the UI, and fetching of elements.
+ */
+export interface SubCollection extends DCObject {
+    /** The name of the collection. Usually corresponds to the DCE identifier. */
+    name: string;
+    /** The ID of the top level HDCA that associates this collection with the History it belongs to. */
+    hdca_id: string;
+}
+
+/**
+ * Represents either a top level HDCASummary or a sub-collection.
+ */
+export type CollectionEntry = HDCASummary | SubCollection;
+
+/**
+ * Returns true if the given entry is a top level HDCA and false for sub-collections.
+ */
+export function isHDCA(entry: CollectionEntry): entry is HDCASummary {
+    return "history_content_type" in entry && entry.history_content_type === "dataset_collection";
+}
