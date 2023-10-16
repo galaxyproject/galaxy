9367a6947e7b604555ac69b7b1cfa2e168485f49
nuwang <2070605+nuwang@users.noreply.github.com>
2023-10-16 14:22:16 +0530

    Only run the cleanup if job state is available

--- lib/galaxy_test/base/api.py
+++ lib/galaxy_test/base/api.py
@@ -98,8 +98,10 @@ class UsesApiTestCaseMixin:
     def tearDown(self):
         if os.environ.get("GALAXY_TEST_EXTERNAL") is None:
             # Only kill running jobs after test for managed test instances
-            for job in self.galaxy_interactor.get("jobs?state=running").json():
-                self._delete(f"jobs/{job['id']}")
+            response = self.galaxy_interactor.get("jobs?state=running")
+            if response.ok:
+                for job in response.json():
+                    self._delete(f"jobs/{job['id']}")
 
     def _api_url(self, path, params=None, use_key=None, use_admin_key=None):
         if not params:
