4e07b35c56f8b908158c974b297743c1e7ab63b9
Dannon Baker <dannon.baker@gmail.com>
2023-10-18 16:44:01 -0400

    Swap to updated change-case from snake-case

--- client/tests/jest/jest.config.js
+++ client/tests/jest/jest.config.js
@@ -7,6 +7,7 @@ const { defaults: tsjPreset } = require("ts-jest/presets");
 const modulesToTransform = [
     "axios",
     "bootstrap-vue",
+    "change-case",
     "rxjs",
     "@hirez_io",
     "winbox",
