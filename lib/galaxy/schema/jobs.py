dd63f5afa21c5839b31b4c9d7aae8d73eea8929d
Tillman <theisner@posteo.de>
2023-10-20 19:01:44 +0200

    Add pydantic models for search operation

--- lib/galaxy/schema/jobs.py
+++ lib/galaxy/schema/jobs.py
@@ -1,14 +1,27 @@
 from typing import (
+    Any,
+    Dict,
     List,
     Optional,
 )
 
-from pydantic import Extra, Field, Required
+from pydantic import (
+    Extra,
+    Field,
+    Required,
+    UUID4,
+)
 
-from galaxy.schema.fields import DecodedDatabaseIdField
+from galaxy.schema.fields import (
+    DecodedDatabaseIdField,
+    EncodedDatabaseIdField,
+)
 from galaxy.schema.schema import (
     EncodedDatasetSourceId,
+    EntityIdField,
+    JobSummary,
     Model,
+    UuidField,
 )
 
 
@@ -73,12 +86,12 @@ class SearchJobsPayload(Model):
         title="Tool ID",
         description="The tool ID related to the job.",
     )
-    # TODO the inputs are actually a dict, but are passed as a JSON string
+    # TODO the inputs are actually a dict, but are passed as a JSON dump
     # maybe change it?
     inputs: str = Field(
         default=Required,
         title="Inputs",
-        description="The inputs of the job as a JSON string.",
+        description="The inputs of the job as a JSON dump.",
     )
     state: str = Field(
         default=Required,
@@ -88,3 +101,45 @@ class SearchJobsPayload(Model):
 
     class Config:
         extra = Extra.allow  # This is used for items named file_ and __file_
+
+
+class EncodedJobIDs(Model):
+    id: EncodedDatabaseIdField = EntityIdField
+    history_id: Optional[EncodedDatabaseIdField] = Field(
+        None,
+        title="History ID",
+        description="The encoded ID of the history associated with this item.",
+    )
+
+
+class EncodedDatasetJobInfo(EncodedDatasetSourceId):
+    uuid: UUID4 = UuidField
+
+
+class EncodedJobDetails(JobSummary, EncodedDatasetSourceId):
+    command_version: str = Field(
+        ...,
+        title="Command Version",
+        description="Tool version indicated during job execution.",
+    )
+    params: Any = Field(
+        ...,
+        title="Parameters",
+        description=(
+            "Object containing all the parameters of the tool associated with this job. "
+            "The specific parameters depend on the tool itself."
+        ),
+    )
+    inputs: Dict[str, EncodedDatasetJobInfo] = Field(
+        {},
+        title="Inputs",
+        description="Dictionary mapping all the tool inputs (by name) with the corresponding dataset information.",
+    )
+    outputs: Dict[str, EncodedDatasetJobInfo] = Field(
+        {},
+        title="Outputs",
+        description="Dictionary mapping all the tool outputs (by name) with the corresponding dataset information.",
+    )
+    # TODO add description, check type and add proper default
+    copied_from_job_id: Any = Field(default=None, title="Copied from Job-ID", description="?")
+    output_collections: Any = Field(default={}, title="Output collections", description="?")
