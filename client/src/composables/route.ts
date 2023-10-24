21c9ffb653eeca6be50e1551769c8c9f7f37e599
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-10-24 14:17:23 +0200

    fix deprecated vueuse function

--- client/src/composables/route.ts
+++ client/src/composables/route.ts
@@ -1,28 +1,28 @@
-import { MaybeComputedRef, resolveUnref } from "@vueuse/core";
+import { MaybeRefOrGetter, toValue } from "@vueuse/core";
 import { computed } from "vue";
 import { useRoute } from "vue-router/composables";
 
 /** use a route query parameter as a boolean computed */
 export function useRouteQueryBool(
-    parameter: MaybeComputedRef<string>,
-    defaultValue: MaybeComputedRef<boolean> = false
+    parameter: MaybeRefOrGetter<string>,
+    defaultValue: MaybeRefOrGetter<boolean> = false
 ) {
     const route = useRoute();
     return computed(() => {
-        const p = resolveUnref(parameter);
-        return p in route.query ? route.query[p] === "true" : resolveUnref(defaultValue);
+        const p = toValue(parameter);
+        return p in route.query ? route.query[p] === "true" : toValue(defaultValue);
     });
 }
 
-export function useRouteQueryNumber(parameter: MaybeComputedRef<string>, defaultValue: MaybeComputedRef<number> = 1) {
+export function useRouteQueryNumber(parameter: MaybeRefOrGetter<string>, defaultValue: MaybeRefOrGetter<number> = 1) {
     const route = useRoute();
     return computed(() => {
-        const p = resolveUnref(parameter);
+        const p = toValue(parameter);
         if (p in route.query) {
             const v = route.query[p];
-            return typeof v === "string" ? parseFloat(v) : resolveUnref(defaultValue);
+            return typeof v === "string" ? parseFloat(v) : toValue(defaultValue);
         } else {
-            return resolveUnref(defaultValue);
+            return toValue(defaultValue);
         }
     });
 }
