b9537a232e338792d4b6cb0edde68691cd2591d1
Alireza Heidri <itisalirh@gmail.com>
2023-11-09 16:07:12 +0000

    create `LoginRequired` component

---
+++ client/src/components/Common/LoginRequired.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { BPopover } from "bootstrap-vue";
+
+import { useUserStore } from "@/stores/userStore";
+import { withPrefix } from "@/utils/redirect";
+
+defineProps<{
+    title: string;
+    target: string;
+}>();
+
+const userStore = useUserStore();
+</script>
+
+<template>
+    <BPopover v-if="userStore.isAnonymous" :target="target" triggers="hover focus" placement="bottom">
+        <template v-slot:title> {{ title }} </template>
+        Please <a :href="withPrefix('/login')">log in or register</a> to use this feature.
+    </BPopover>
+</template>
