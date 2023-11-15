7bdf621a339506affdda9826905bb3cba53837f1
Dannon Baker <dannon.baker@gmail.com>
2023-11-15 17:24:57 -0500

    Swpa historyempty to comp+ts

--- client/src/components/History/CurrentHistory/HistoryEmpty.vue
+++ client/src/components/History/CurrentHistory/HistoryEmpty.vue
@@ -1,33 +1,36 @@
+<script setup lang="ts">
+import { useGlobalUploadModal } from "composables/globalUploadModal";
+
+import { eventHub } from "@/components/plugins/eventHub";
+import { localize } from "@/utils/localization";
+
+const props = withDefaults(
+    defineProps<{
+        message: string;
+        writable: boolean;
+    }>(),
+    {
+        message: "This history is empty.",
+        writable: true,
+    }
+);
+
+const { openGlobalUploadModal } = useGlobalUploadModal();
+function clickDataLink() {
+    eventHub.$emit("openToolSection", "getext");
+}
+</script>
+
 <template>
     <b-alert show>
         <h4 class="mb-1">
             <i class="fa fa-info-circle empty-message"></i>
-            <span>{{ message | l }}</span>
+            <span>{{ localize(message) }}</span>
         </h4>
-        <p v-if="writable">
+        <p v-if="props.writable">
             <a v-localize href="#" @click.prevent="openGlobalUploadModal">You can load your own data</a>
             <span v-localize>or</span>
             <a v-localize href="#" @click.prevent="clickDataLink">get data from an external source</a>.
         </p>
     </b-alert>
 </template>
-
-<script>
-import { useGlobalUploadModal } from "composables/globalUploadModal";
-
-export default {
-    props: {
-        message: { type: String, default: "This history is empty." },
-        writable: { type: Boolean, default: true },
-    },
-    setup() {
-        const { openGlobalUploadModal } = useGlobalUploadModal();
-        return { openGlobalUploadModal };
-    },
-    methods: {
-        clickDataLink() {
-            this.eventHub.$emit("openToolSection", "getext");
-        },
-    },
-};
-</script>
