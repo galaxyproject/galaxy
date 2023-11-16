c90dfccc688d76288cfc6fc51093cd9bb8042c03
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-11-16 17:04:48 +0100

    add tests for sorting and carousel

--- client/src/components/Notifications/Broadcasts/BroadcastsOverlay.test.ts
+++ client/src/components/Notifications/Broadcasts/BroadcastsOverlay.test.ts
@@ -13,7 +13,11 @@ const localVue = getLocalVue(true);
 const now = new Date();
 const inTwoMonths = new Date(now.setMonth(now.getMonth() + 2));
 
-function generateBroadcastNotification(id: string): BroadcastNotification {
+let idCounter = 0;
+
+function generateBroadcastNotification(overwrites: Partial<BroadcastNotification> = {}): BroadcastNotification {
+    const id = `${idCounter++}`;
+
     return {
         id: id,
         create_time: now.toISOString(),
@@ -23,15 +27,16 @@ function generateBroadcastNotification(id: string): BroadcastNotification {
         source: "testing",
         variant: "info",
         content: {
-            subject: `Test subject ${id}`,
-            message: `Test message ${id}`,
+            subject: `Test subject ${overwrites.id ?? id}`,
+            message: `Test message ${overwrites.id ?? id}`,
         },
+        ...overwrites,
     };
 }
 
 const FAKE_BROADCASTS: BroadcastNotification[] = [
-    generateBroadcastNotification("1"),
-    generateBroadcastNotification("2"),
+    generateBroadcastNotification({ id: "1" }),
+    generateBroadcastNotification({ id: "2" }),
 ];
 
 async function mountBroadcastsOverlayWith(broadcasts: BroadcastNotification[] = []) {
@@ -83,6 +88,53 @@ describe("BroadcastsOverlay.vue", () => {
         await dismissButton.trigger("click");
 
         expect(wrapper.findAll(messageCssSelector)).toHaveLength(1);
+        expect(wrapper.find(messageCssSelector).text()).toContain("Test message 2");
+    });
+
+    it("should show more important broadcasts first", async () => {
+        const broadcasts = [
+            generateBroadcastNotification({ id: "warning", variant: "warning" }),
+            generateBroadcastNotification({ id: "info", variant: "info" }),
+            generateBroadcastNotification({ id: "urgent", variant: "urgent" }),
+        ];
+
+        const wrapper = await mountBroadcastsOverlayWith(broadcasts);
+        expect(wrapper.find(messageCssSelector).text()).toContain("Test message urgent");
+
+        const dismissButton = wrapper.find(".dismiss-button");
+        await dismissButton.trigger("click");
+        expect(wrapper.find(messageCssSelector).text()).toContain("Test message warning");
+
+        await dismissButton.trigger("click");
+        expect(wrapper.find(messageCssSelector).text()).toContain("Test message info");
+    });
+
+    it("should allow cycling through broadcasts", async () => {
+        const broadcasts = [
+            generateBroadcastNotification({ id: "1" }),
+            generateBroadcastNotification({ id: "2" }),
+            generateBroadcastNotification({ id: "3" }),
+        ];
+
+        const wrapper = await mountBroadcastsOverlayWith(broadcasts);
+        expect(wrapper.findAll(messageCssSelector)).toHaveLength(1);
+        expect(wrapper.find(messageCssSelector).text()).toContain("Test message 1");
+
+        const rightButton = wrapper.find("button.right");
+        await rightButton.trigger("click");
+        expect(wrapper.find(messageCssSelector).text()).toContain("Test message 2");
+
+        await rightButton.trigger("click");
+        expect(wrapper.find(messageCssSelector).text()).toContain("Test message 3");
+
+        await rightButton.trigger("click");
+        expect(wrapper.find(messageCssSelector).text()).toContain("Test message 1");
+
+        const leftButton = wrapper.find("button.left");
+        await leftButton.trigger("click");
+        expect(wrapper.find(messageCssSelector).text()).toContain("Test message 3");
+
+        await leftButton.trigger("click");
         expect(wrapper.find(messageCssSelector).text()).toContain("Test message 2");
     });
 });
