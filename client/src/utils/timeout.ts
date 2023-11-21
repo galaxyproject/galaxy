adefcb2989483fa8890ca9108ef0701c30ed5613
guerler <aysam.guerler@gmail.com>
2023-11-21 09:47:10 +0300

    Remove custom timeout utility

--- client/src/utils/timeout.ts
+++
@@ -1,7 +0,0 @@
-let timeoutHandler: ReturnType<typeof setTimeout> | null = null;
-export const timeout = (callback: () => void, delay = 5000) => {
-    if (timeoutHandler) {
-        clearTimeout(timeoutHandler);
-    }
-    timeoutHandler = setTimeout(callback, delay);
-};
