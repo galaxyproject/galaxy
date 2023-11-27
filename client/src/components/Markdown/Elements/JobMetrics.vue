8b7a51a226dac28f9e4bc5736f3cf9be18f56ff9
John Chilton <jmchilton@gmail.com>
2023-11-27 18:07:05 -0500

    Add title/footer component to job metrics and parameters.

--- client/src/components/Markdown/Elements/JobMetrics.vue
+++ client/src/components/Markdown/Elements/JobMetrics.vue
@@ -1,26 +1,31 @@
+<script setup lang="ts">
+import JobMetrics from "@/components/JobMetrics/JobMetrics.vue";
+
+interface JobMetricsProps {
+    jobId: string;
+    title?: string;
+    footer?: string;
+}
+
+withDefaults(defineProps<JobMetricsProps>(), {
+    title: null,
+    footer: null,
+});
+</script>
+
 <template>
     <b-card nobody>
+        <b-card-title v-if="title">
+            <b>{{ title }}</b>
+        </b-card-title>
         <JobMetrics
             class="job-metrics"
-            :job-id="args.job_id"
+            :job-id="jobId"
             :should-show-aws-estimate="false"
             :should-show-carbon-emissions-estimates="false"
             :include-title="false" />
+        <b-card-footer v-if="footer">
+            {{ footer }}
+        </b-card-footer>
     </b-card>
 </template>
-
-<script>
-import JobMetrics from "components/JobMetrics/JobMetrics";
-
-export default {
-    components: {
-        JobMetrics,
-    },
-    props: {
-        args: {
-            type: Object,
-            default: null,
-        },
-    },
-};
-</script>
