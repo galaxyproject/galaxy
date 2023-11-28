7c3ca8a39a5f5baf79ba902d7fcd51c1dfa31c85
davelopez <46503462+davelopez@users.noreply.github.com>
2023-11-28 15:05:49 +0100

    Move BroadcastCard to its own component + refactor

--- client/src/components/admin/Notifications/BroadcastsList.test.ts
+++ client/src/components/admin/Notifications/BroadcastsList.test.ts
@@ -20,7 +20,7 @@ const selectors = {
     showActiveFilterButton: "#show-active-filter-button",
     showScheduledFilterButton: "#show-scheduled-filter-button",
     showExpiredFilterButton: "#show-expired-filter-button",
-    broadcastCard: ".broadcast-card",
+    broadcastItem: "[data-test-id='broadcast-item']",
 } as const;
 
 async function mountBroadcastsList(broadcasts?: BroadcastNotification[]) {
@@ -49,7 +49,7 @@ describe("BroadcastsList.vue", () => {
     it("should render empty list message when there are no broadcasts", async () => {
         const wrapper = await mountBroadcastsList();
 
-        expect(wrapper.findAll(selectors.broadcastCard).length).toBe(0);
+        expect(wrapper.findAll(selectors.broadcastItem).length).toBe(0);
         expect(wrapper.find(selectors.emptyBroadcastsListAlert).exists()).toBeTruthy();
     });
 
@@ -77,25 +77,25 @@ describe("BroadcastsList.vue", () => {
         const wrapper = await mountBroadcastsList([activeBroadcast, scheduledBroadcast, expiredBroadcast]);
 
         // All broadcasts are shown by default
-        expect(wrapper.findAll(selectors.broadcastCard).length).toBe(3);
+        expect(wrapper.findAll(selectors.broadcastItem).length).toBe(3);
 
         // Disable all filters
         await wrapper.find(selectors.showActiveFilterButton).trigger("click");
         await wrapper.find(selectors.showScheduledFilterButton).trigger("click");
         await wrapper.find(selectors.showExpiredFilterButton).trigger("click");
-        expect(wrapper.findAll(selectors.broadcastCard).length).toBe(0);
+        expect(wrapper.findAll(selectors.broadcastItem).length).toBe(0);
         expect(wrapper.find(selectors.emptyBroadcastsListAlert).exists()).toBeTruthy();
 
         // Enable active broadcasts filter
         await wrapper.find(selectors.showActiveFilterButton).trigger("click");
-        expect(wrapper.findAll(selectors.broadcastCard).length).toBe(1);
+        expect(wrapper.findAll(selectors.broadcastItem).length).toBe(1);
 
         // Enable scheduled broadcasts filter
         await wrapper.find(selectors.showScheduledFilterButton).trigger("click");
-        expect(wrapper.findAll(selectors.broadcastCard).length).toBe(2);
+        expect(wrapper.findAll(selectors.broadcastItem).length).toBe(2);
 
         // Enable expired broadcasts filter
         await wrapper.find(selectors.showExpiredFilterButton).trigger("click");
-        expect(wrapper.findAll(selectors.broadcastCard).length).toBe(3);
+        expect(wrapper.findAll(selectors.broadcastItem).length).toBe(3);
     });
 });
