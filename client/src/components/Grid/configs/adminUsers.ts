2782f88ca143096135c10ee2f566a0eb545e785b
guerler <aysam.guerler@gmail.com>
2023-12-01 10:23:18 +0300

    Fix filter labels

--- client/src/components/Grid/configs/adminUsers.ts
+++ client/src/components/Grid/configs/adminUsers.ts
@@ -305,14 +305,14 @@ const validFilters: Record<string, ValidFilter<string | boolean | undefined>> = 
     email: { placeholder: "email", type: String, handler: contains("email"), menuItem: true },
     username: { placeholder: "username", type: String, handler: contains("username"), menuItem: true },
     deleted: {
-        placeholder: "Filter on deleted visualizations",
+        placeholder: "Filter on deleted users",
         type: Boolean,
         boolType: "is",
         handler: equals("deleted", "deleted", toBool),
         menuItem: true,
     },
     purged: {
-        placeholder: "Filter on purged visualizations",
+        placeholder: "Filter on purged users",
         type: Boolean,
         boolType: "is",
         handler: equals("purged", "purged", toBool),
