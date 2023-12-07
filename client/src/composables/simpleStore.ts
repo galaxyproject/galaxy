a692a68150284a77b250df46d734b16b859d03a5
davelopez <46503462+davelopez@users.noreply.github.com>
2023-12-07 14:43:18 +0100

    Add simple store composable
    Reduces duplication for simple Pinia stores that just cache responses by item id.

---
+++ client/src/composables/simpleStore.ts
@@ -0,0 +1,51 @@
+import { computed, del, ref, set } from "vue";
+
+import type { ApiResponse } from "@/api/schema";
+
+interface FetchParams {
+    id: string;
+}
+
+interface FetchFunction<T> {
+    (params: FetchParams): Promise<ApiResponse<T>>;
+}
+
+export function useSimpleStore<T>(fetchItem: FetchFunction<T>) {
+    const storedItems = ref<{ [key: string]: T }>({});
+    const loadingItem = ref<{ [key: string]: boolean }>({});
+
+    const getItemById = computed(() => {
+        return (id: string) => {
+            const item = storedItems.value[id];
+            if (!item && !loadingItem.value[id]) {
+                fetchItemById({ id: id });
+            }
+            return item ?? null;
+        };
+    });
+
+    const isLoadingItem = computed(() => {
+        return (id: string) => {
+            return loadingItem.value[id] ?? false;
+        };
+    });
+
+    async function fetchItemById(params: FetchParams) {
+        const itemId = params.id;
+        set(loadingItem.value, itemId, true);
+        try {
+            const { data } = await fetchItem({ id: itemId });
+            set(storedItems.value, itemId, data);
+            return data;
+        } finally {
+            del(loadingItem.value, itemId);
+        }
+    }
+
+    return {
+        storedItems,
+        getItemById,
+        isLoadingItem,
+        fetchItemById,
+    };
+}
