69b9d18d65fc7596e980b14db8e1ea10b600971d
davelopez <46503462+davelopez@users.noreply.github.com>
2023-12-07 15:01:49 +0100

    Use simple store composable for datasetExtraFilesStore

--- client/src/stores/datasetExtraFilesStore.ts
+++ client/src/stores/datasetExtraFilesStore.ts
@@ -1,46 +1,17 @@
 import { defineStore } from "pinia";
-import { computed, del, ref, set } from "vue";
 
 import { DatasetExtraFiles, fetchDatasetExtraFiles } from "@/api/datasets";
+import { useSimpleStore } from "@/composables/simpleStore";
 
 export const useDatasetExtraFilesStore = defineStore("datasetExtraFilesStore", () => {
-    const storedDatasetExtraFiles = ref<{ [key: string]: DatasetExtraFiles }>({});
-    const loading = ref<{ [key: string]: boolean }>({});
-
-    const getDatasetExtraFiles = computed(() => {
-        return (datasetId: string) => {
-            const datasetExtFiles = storedDatasetExtraFiles.value[datasetId];
-            if (!datasetExtFiles && !loading.value[datasetId]) {
-                fetchDatasetExtFilesByDatasetId({ id: datasetId });
-            }
-            return datasetExtFiles ?? null;
-        };
-    });
-
-    const isLoadingDatasetExtraFiles = computed(() => {
-        return (datasetId: string) => {
-            return loading.value[datasetId] ?? false;
-        };
-    });
-
-    async function fetchDatasetExtFilesByDatasetId(params: { id: string }) {
-        const datasetId = params.id;
-        set(loading.value, datasetId, true);
-        try {
-            const { data: datasetExtFiles } = await fetchDatasetExtraFiles({
-                dataset_id: datasetId,
-            });
-            set(storedDatasetExtraFiles.value, datasetId, datasetExtFiles);
-            return datasetExtFiles;
-        } finally {
-            del(loading.value, datasetId);
-        }
-    }
+    const { storedItems, getItemById, isLoadingItem, fetchItemById } = useSimpleStore<DatasetExtraFiles>((params) =>
+        fetchDatasetExtraFiles({ dataset_id: params.id })
+    );
 
     return {
-        storedDatasetExtraFiles,
-        getDatasetExtraFiles,
-        isLoadingDatasetExtraFiles,
-        fetchDatasetExtFilesByDatasetId,
+        storedDatasetExtraFiles: storedItems,
+        getDatasetExtraFiles: getItemById,
+        isLoadingDatasetExtraFiles: isLoadingItem,
+        fetchDatasetExtFilesByDatasetId: fetchItemById,
     };
 });
