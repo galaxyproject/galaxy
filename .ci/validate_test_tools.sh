1a25ef6f58e391352bc1ebfec20444bc2db51595
Matthias Bernt <m.bernt@ufz.de>
2023-12-08 15:50:10 +0100

    skip validation of upload tool
    to allow for stricter xml schema (enforce one param child)

--- .ci/validate_test_tools.sh
+++ .ci/validate_test_tools.sh
@@ -7,5 +7,7 @@ xsd_path="lib/galaxy/tools/xsd/galaxy.xsd"
 xmllint --noout "$xsd_path"
 
 test_tools_path='test/functional/tools'
-tool_files_list=$(ls "$test_tools_path"/*.xml | grep -v '_conf.xml$')
+# test all test tools except upload.xml which uses a non-standard conditional
+# (without param) which does not survive xsd validation
+tool_files_list=$(ls "$test_tools_path"/*.xml | grep -v '_conf.xml$' | grep -v upload.xml)
 sh scripts/validate_tools.sh $tool_files_list
