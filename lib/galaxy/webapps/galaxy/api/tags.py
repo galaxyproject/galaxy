d92edb726ac25e30f6b3f101d21c9313ebc96004
John Davis <jdavcs@gmail.com>
2023-12-15 15:00:13 -0500

    Remove cbv from api.tags

--- lib/galaxy/webapps/galaxy/api/tags.py
+++ lib/galaxy/webapps/galaxy/api/tags.py
@@ -25,28 +25,25 @@ log = logging.getLogger(__name__)
 router = Router(tags=["tags"])
 
 
-@router.cbv
 class FastAPITags:
-    manager: TagsManager = depends(TagsManager)
-
     @router.put(
         "/api/tags",
         summary="Apply a new set of tags to an item.",
         status_code=status.HTTP_204_NO_CONTENT,
     )
     def update(
-        self,
         trans: ProvidesUserContext = DependsOnTrans,
         payload: ItemTagsPayload = Body(
             ...,  # Required
             title="Payload",
             description="Request body containing the item and the tags to be assigned.",
         ),
+        manager: TagsManager = depends(TagsManager),
     ):
         """Replaces the tags associated with an item with the new ones specified in the payload.
 
         - The previous tags will be __deleted__.
         - If no tags are provided in the request body, the currently associated tags will also be __deleted__.
         """
-        self.manager.update(trans, payload)
+        manager.update(trans, payload)
         return Response(status_code=status.HTTP_204_NO_CONTENT)
