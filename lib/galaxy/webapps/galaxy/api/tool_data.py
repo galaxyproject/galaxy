a645e64020cfad491da152c97049b218cffa7fd0
John Davis <jdavcs@gmail.com>
2023-12-15 15:02:55 -0500

    Remove cbv from api.tool_data

--- lib/galaxy/webapps/galaxy/api/tool_data.py
+++ lib/galaxy/webapps/galaxy/api/tool_data.py
@@ -46,19 +46,16 @@ class ImportToolDataBundle(BaseModel):
     source: ImportToolDataBundleSource = Field(..., discriminator="src")
 
 
-@router.cbv
 class FastAPIToolData:
-    tool_data_manager: ToolDataManager = depends(ToolDataManager)
-
     @router.get(
         "/api/tool_data",
         summary="Lists all available data tables",
         response_description="A list with details on individual data tables.",
         require_admin=False,
     )
-    async def index(self) -> ToolDataEntryList:
+    async def index(tool_data_manager: ToolDataManager = depends(ToolDataManager)) -> ToolDataEntryList:
         """Get the list of all available data tables."""
-        return self.tool_data_manager.index()
+        return tool_data_manager.index()
 
     @router.post(
         "/api/tool_data",
@@ -66,7 +63,7 @@ class FastAPIToolData:
         require_admin=True,
     )
     async def create(
-        self, tool_data_file_path=None, import_bundle_model: ImportToolDataBundle = Body(...)
+        tool_data_file_path=None, import_bundle_model: ImportToolDataBundle = Body(...)
     ) -> AsyncTaskResultSummary:
         source = import_bundle_model.source
         result = import_data_bundle.delay(tool_data_file_path=tool_data_file_path, **source.dict())
@@ -79,9 +76,11 @@ class FastAPIToolData:
         response_description="A description of the given data table and its content",
         require_admin=True,
     )
-    async def show(self, table_name: str = ToolDataTableName) -> ToolDataDetails:
+    async def show(
+        table_name: str = ToolDataTableName, tool_data_manager: ToolDataManager = depends(ToolDataManager)
+    ) -> ToolDataDetails:
         """Get details of a given tool data table."""
-        return self.tool_data_manager.show(table_name)
+        return tool_data_manager.show(table_name)
 
     @router.get(
         "/api/tool_data/{table_name}/reload",
@@ -89,9 +88,11 @@ class FastAPIToolData:
         response_description="A description of the reloaded data table and its content",
         require_admin=True,
     )
-    async def reload(self, table_name: str = ToolDataTableName) -> ToolDataDetails:
+    async def reload(
+        table_name: str = ToolDataTableName, tool_data_manager: ToolDataManager = depends(ToolDataManager)
+    ) -> ToolDataDetails:
         """Reloads a data table and return its details."""
-        return self.tool_data_manager.reload(table_name)
+        return tool_data_manager.reload(table_name)
 
     @router.get(
         "/api/tool_data/{table_name}/fields/{field_name}",
@@ -100,12 +101,12 @@ class FastAPIToolData:
         require_admin=True,
     )
     async def show_field(
-        self,
         table_name: str = ToolDataTableName,
         field_name: str = ToolDataTableFieldName,
+        tool_data_manager: ToolDataManager = depends(ToolDataManager),
     ) -> ToolDataField:
         """Reloads a data table and return its details."""
-        return self.tool_data_manager.show_field(table_name, field_name)
+        return tool_data_manager.show_field(table_name, field_name)
 
     @router.get(
         "/api/tool_data/{table_name}/fields/{field_name}/files/{file_name}",
@@ -115,7 +116,6 @@ class FastAPIToolData:
         require_admin=True,
     )
     async def download_field_file(
-        self,
         table_name: str = ToolDataTableName,
         field_name: str = ToolDataTableFieldName,
         file_name: str = Path(
@@ -123,9 +123,10 @@ class FastAPIToolData:
             title="File name",
             description="The name of a file associated with this data table field",
         ),
+        tool_data_manager: ToolDataManager = depends(ToolDataManager),
     ):
         """Download a file associated with the data table field."""
-        path = self.tool_data_manager.get_field_file_path(table_name, field_name, file_name)
+        path = tool_data_manager.get_field_file_path(table_name, field_name, file_name)
         return GalaxyFileResponse(str(path))
 
     @router.delete(
@@ -135,9 +136,9 @@ class FastAPIToolData:
         require_admin=True,
     )
     async def delete(
-        self,
         payload: ToolDataItem,
         table_name: str = ToolDataTableName,
+        tool_data_manager: ToolDataManager = depends(ToolDataManager),
     ) -> ToolDataDetails:
         """Removes an item from a data table and reloads it to return its updated details."""
-        return self.tool_data_manager.delete(table_name, payload.values)
+        return tool_data_manager.delete(table_name, payload.values)
