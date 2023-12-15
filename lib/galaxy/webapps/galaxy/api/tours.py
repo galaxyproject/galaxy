d4248ad6ab25587096fe30b41e1368d7c48ece09
John Davis <jdavcs@gmail.com>
2023-12-15 15:04:30 -0500

    Remove cbv from api.tours

--- lib/galaxy/webapps/galaxy/api/tours.py
+++ lib/galaxy/webapps/galaxy/api/tours.py
@@ -19,21 +19,18 @@ log = logging.getLogger(__name__)
 router = Router(tags=["tours"])
 
 
-@router.cbv
 class FastAPITours:
-    registry: ToursRegistry = depends(ToursRegistry)  # type: ignore[type-abstract]  # https://github.com/python/mypy/issues/4717
-
     @router.get("/api/tours")
-    def index(self) -> TourList:
+    def index(registry: ToursRegistry = depends(ToursRegistry)) -> TourList:  # type: ignore[type-abstract]  # https://github.com/python/mypy/issues/4717
         """Return list of available tours."""
-        return self.registry.get_tours()
+        return registry.get_tours()
 
     @router.get("/api/tours/{tour_id}")
-    def show(self, tour_id: str) -> TourDetails:
+    def show(tour_id: str, registry: ToursRegistry = depends(ToursRegistry)) -> TourDetails:  # type: ignore[type-abstract]  # https://github.com/python/mypy/issues/4717
         """Return a tour definition."""
-        return self.registry.tour_contents(tour_id)
+        return registry.tour_contents(tour_id)
 
     @router.post("/api/tours/{tour_id}", require_admin=True)
-    def update_tour(self, tour_id: str) -> TourDetails:
+    def update_tour(tour_id: str, registry: ToursRegistry = depends(ToursRegistry)) -> TourDetails:  # type: ignore[type-abstract]  # https://github.com/python/mypy/issues/4717
         """Return a tour definition."""
-        return self.registry.load_tour(tour_id)
+        return registry.load_tour(tour_id)
