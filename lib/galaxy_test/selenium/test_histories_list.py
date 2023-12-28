49937da82d7b1cbf8a249883d09e67c1c3168fc7
guerler <aysam.guerler@gmail.com>
2023-12-28 09:22:06 +0300

    Restore advanced filter history list by tag test

--- lib/galaxy_test/selenium/test_histories_list.py
+++ lib/galaxy_test/selenium/test_histories_list.py
@@ -153,9 +153,35 @@ class TestSavedHistories(SharedStateSeleniumTestCase):
         self._login()
         self.navigate_to_histories_page()
         self.sleep_for(self.wait_types.UX_RENDER)
+        # search by name
         self.components.histories.advanced_search_toggle.wait_for_and_click()
-        # search by tag and name
         self.components.histories.advanced_search_name_input.wait_for_and_send_keys(self.history3_name)
+        self.components.histories.advanced_search_submit.wait_for_and_click()
+        self.assert_histories_present([self.history3_name])
+
+        self.components.histories.reset_input.wait_for_and_click()
+
+        self.components.histories.advanced_search_toggle.wait_for_and_click()
+        self.components.histories.advanced_search_name_input.wait_for_and_send_keys(self.history2_name)
+        self.components.histories.advanced_search_submit.wait_for_and_click()
+        self.assert_histories_present([self.history2_name])
+
+        self.components.histories.reset_input.wait_for_and_click()
+
+        self.components.histories.advanced_search_toggle.wait_for_and_click()
+        self.components.histories.advanced_search_name_input.wait_for_and_send_keys(self.history4_name)
+        self.components.histories.advanced_search_submit.wait_for_and_click()
+        self.assert_histories_present([])
+
+        self.components.histories.reset_input.wait_for_and_click()
+
+        # search by tags
+        self.components.histories.advanced_search_toggle.wait_for_and_click()
+        self.components.histories.advanced_search_tag_area.wait_for_and_click()
+        input_element = self.components.histories.advanced_search_tag_input.wait_for_visible()
+        input_element.send_keys(self.history3_tags[0])
+        self.send_enter(input_element)
+        self.sleep_for(self.wait_types.UX_RENDER)
         self.components.histories.advanced_search_submit.wait_for_and_click()
         self.assert_histories_present([self.history3_name])
 
