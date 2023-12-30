50d81b888555a512fb7b7cd05e6357998423b109
mvdbeek <m.vandenbeek@gmail.com>
2023-12-30 13:44:29 +0200

    Override fixture for TS tests

--- lib/tool_shed/test/conftest.py
+++ lib/tool_shed/test/conftest.py
@@ -2,15 +2,11 @@ import os
 
 import pytest
 
-from galaxy_test.conftest import (  # noqa: F401
-    embedded_driver,
-    real_driver,
-)
 from tool_shed.test.base import driver
 
 
 @pytest.fixture(scope="session")
-def tool_shed_test_driver():
+def embedded_driver():
     if not os.environ.get("GALAXY_TEST_ENVIRONMENT_CONFIGURED"):
         ts_driver = driver.ToolShedTestDriver()
         ts_driver.setup()
