7392c6b70dc0e0e07d6a479889b1482fd49438b5
nuwang <2070605+nuwang@users.noreply.github.com>
2024-01-17 19:38:56 +0530

    Move _bucket initializer to open_fs, following GoogleCloudStorageFilesSource

--- lib/galaxy/files/sources/s3fs.py
+++ lib/galaxy/files/sources/s3fs.py
@@ -44,48 +44,56 @@ class S3FsFilesSource(BaseFilesSource):
         if s3fs is None:
             raise Exception("Package s3fs unavailable but required for this file source plugin.")
         props: S3FsFilesSourceProperties = cast(S3FsFilesSourceProperties, self._parse_common_config_opts(kwd))
-        self._bucket = props.pop("bucket", "")
+        # There is a possibility that the bucket name could be parameterized: e.g.
+        # bucket: ${user.preferences['generic_s3|bucket']}
+        # that's ok, because we evaluate the bucket name again later. The bucket property here will only
+        # be used by `score_url_match`. In the case of a parameterized bucket name, we will always return
+        # a score of 4 as the url will only match the s3:// part.
+        self._bucket = props.get("bucket", "")
         self._endpoint_url = props.pop("endpoint_url", None)
         self._props = props
         if self._endpoint_url:
             self._props.update({"client_kwargs": {"endpoint_url": self._endpoint_url}})
 
     def _list(self, path="/", recursive=True, user_context=None, opts: Optional[FilesSourceOptions] = None):
-        fs = self._open_fs(user_context=user_context, opts=opts)
+        _props = self._serialization_props(user_context)
+        fs = self._open_fs(props=_props, opts=opts)
+        _bucket_name = _props.get("bucket", "")
         if recursive:
             res: List[Dict[str, Any]] = []
-            bucket_path = self._bucket_path(path)
+            bucket_path = self._bucket_path(_bucket_name, path)
             for p, dirs, files in fs.walk(bucket_path, detail=True):
                 to_dict = functools.partial(self._resource_info_to_dict, p)
                 res.extend(map(to_dict, dirs.values()))
                 res.extend(map(to_dict, files.values()))
             return res
         else:
-            bucket_path = self._bucket_path(path)
+            bucket_path = self._bucket_path(_bucket_name, path)
             res = fs.ls(bucket_path, detail=True)
             to_dict = functools.partial(self._resource_info_to_dict, path)
             return list(map(to_dict, res))
 
     def _realize_to(self, source_path, native_path, user_context=None, opts: Optional[FilesSourceOptions] = None):
-        fs = self._open_fs(user_context=user_context, opts=opts)
-        bucket_path = self._bucket_path(source_path)
+        _props = self._serialization_props(user_context)
+        fs = self._open_fs(props=_props, opts=opts)
+        _bucket_name = _props.get("bucket", "")
+        bucket_path = self._bucket_path(_bucket_name, source_path)
         fs.download(bucket_path, native_path)
 
     def _write_from(self, target_path, native_path, user_context=None, opts: Optional[FilesSourceOptions] = None):
         bucket_path = self._bucket_path(target_path)
         self._open_fs(user_context=user_context, opts=opts).upload(native_path, bucket_path)
 
-    def _bucket_path(self, path: str):
+    def _bucket_path(self, bucket_name: str, path: str):
         if path.startswith("s3://"):
             return path.replace("s3://", "")
         elif not path.startswith("/"):
             path = f"/{path}"
-        return f"{self._bucket}{path}"
+        return f"{bucket_name}{path}"
 
-    def _open_fs(self, user_context=None, opts: Optional[FilesSourceOptions] = None):
+    def _open_fs(self, props: FilesSourceProperties, opts: Optional[FilesSourceOptions] = None):
         extra_props = opts.extra_props or {} if opts else {}
-        self._props = self._serialization_props(user_context)
-        fs = s3fs.S3FileSystem(**{**self._props, **extra_props})
+        fs = s3fs.S3FileSystem(**{**props, **extra_props})
         return fs
 
     def _resource_info_to_dict(self, dir_path: str, resource_info):
@@ -105,12 +113,11 @@ class S3FsFilesSource(BaseFilesSource):
                 "path": path,
             }
 
-    def _serialization_props(self, user_context=None):
+    def _serialization_props(self, user_context=None) -> S3FsFilesSourceProperties:
         effective_props = {}
         for key, val in self._props.items():
             effective_props[key] = self._evaluate_prop(val, user_context=user_context)
-        self._bucket = self._evaluate_prop(self._bucket, user_context=user_context)
-        return effective_props
+        return cast(S3FsFilesSourceProperties, effective_props)
 
     def score_url_match(self, url: str):
         # For security, we need to ensure that a partial match doesn't work. e.g. s3://{bucket}something/myfiles
