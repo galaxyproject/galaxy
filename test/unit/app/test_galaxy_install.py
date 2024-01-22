0e7b0eb83b4d6abf1711c268d80d1d316c1c9242
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2024-01-22 17:20:22 +0000

    Extend .venv paths to ignore
    Useful when creating virtual environments for several Python versions
    (e.g. `.venv38`, `.venv312`), which otherwise are linted by tools like
    black, isort and flake8.

--- test/unit/app/test_galaxy_install.py
+++ test/unit/app/test_galaxy_install.py
@@ -38,7 +38,16 @@ def test_against_production_shed(tmp_path: Path):
         assert tool_guid in f.read()
     repo_path = tmp_path / "tools" / "toolshed.g2.bx.psu.edu" / "repos" / repo_owner / repo_name / repo_revision
     assert repo_path.exists()
-    tool_data_table_path = tmp_path / "tool_data" / "toolshed.g2.bx.psu.edu" / "repos" / repo_owner / repo_name / repo_revision / "tool_data_table_conf.xml"
+    tool_data_table_path = (
+        tmp_path
+        / "tool_data"
+        / "toolshed.g2.bx.psu.edu"
+        / "repos"
+        / repo_owner
+        / repo_name
+        / repo_revision
+        / "tool_data_table_conf.xml"
+    )
     assert tool_data_table_path.exists()
 
     install_model_context = install_target.install_model.context
