99994a3d03b4ba005b55a7de28164ee6c6068c26
Ahmed Awan <qe66653@umbc.edu>
2024-01-30 10:17:34 -0600

    add name_eq and genome_build_eq filters to `HistoryFilters`

--- client/src/utils/filtering.test.js
+++ client/src/utils/filtering.test.js
@@ -85,6 +85,8 @@ describe("filtering", () => {
         expect(HistoryFilters.getFilterValue(filterTexts[1], "create_time_gt", true)).toBe(1609459200);
         expect(HistoryFilters.getFilterValue("", "deleted")).toBe(false);
         expect(HistoryFilters.getFilterValue("", "visible")).toBe(true);
+        expect(HistoryFilters.getFilterValue("name_eq:Select", "name")).toBe(undefined);
+        expect(HistoryFilters.getFilterValue("name_eq:Select", "name_eq")).toBe("select");
     });
     test("parse get valid filters and settings", () => {
         sampleFilters.forEach((sample) => {
@@ -115,6 +117,9 @@ describe("filtering", () => {
             expect(filters[8][1]).toBe("false");
             expect(filters[9][0]).toBe("visible");
             expect(filters[9][1]).toBe("true");
+            const filters_eq = HistoryFilters.getFiltersForText('genome_build_eq:"hg19"');
+            expect(filters_eq[0][0]).toBe("genome_build_eq");
+            expect(filters_eq[0][1]).toBe("hg19");
         });
     });
     test("parse filter text as query dictionary", () => {
@@ -131,6 +136,8 @@ describe("filtering", () => {
             expect(queryDict["deleted"]).toBe(false);
             expect(queryDict["visible"]).toBe(true);
         });
+        const queryDict = HistoryFilters.getQueryDict("name_eq:'name of item'");
+        expect(queryDict["name-eq"]).toBe("name of item");
     });
     test("apply valid filters to existing filterText", () => {
         expect(HistoryFilters.applyFiltersToText(sampleFilters[0].filters, "")).toEqual("deleted:true visible:true");
