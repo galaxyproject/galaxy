7a461f3a76f8af564a62024ea6f062e71936e671
Alireza Heidari <itisalirh@gmail.com>
2024-01-31 13:11:29 +0100

    🐛: fix import `localize` in `HistoryEmpty` and some refactors

--- client/src/components/History/CurrentHistory/HistoryEmpty.vue
+++ client/src/components/History/CurrentHistory/HistoryEmpty.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { useEventBus } from "@vueuse/core";
+import { BAlert } from "bootstrap-vue";
 
 import { useGlobalUploadModal } from "@/composables/globalUploadModal";
-import { localize } from "@/utils/localization";
+import localize from "@/utils/localization";
 
 const { emit } = useEventBus<string>("open-tool-section");
 
@@ -24,15 +25,16 @@ function clickDataLink() {
 </script>
 
 <template>
-    <b-alert show>
+    <BAlert show>
         <h4 class="mb-1">
             <i class="fa fa-info-circle empty-message"></i>
             <span>{{ localize(message) }}</span>
         </h4>
+
         <p v-if="props.writable">
             <a v-localize href="#" @click.prevent="openGlobalUploadModal">You can load your own data</a>
             <span v-localize>or</span>
             <a v-localize href="#" @click.prevent="clickDataLink">get data from an external source</a>.
         </p>
-    </b-alert>
+    </BAlert>
 </template>
