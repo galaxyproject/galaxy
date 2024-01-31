c1081c328908372afa3fae7c055aaeef77df69e2
John Davis <jdavcs@gmail.com>
2024-01-30 21:57:22 -0500

    Reverse conditional to skip empty lines

--- packages/build_packages.sh
+++ packages/build_packages.sh
@@ -10,17 +10,18 @@ cd "$(dirname "$0")"
 
 # ensure ordered by dependency dag
 while read -r package_dir; do
-	if [ -n "$package_dir" ]
-	then
-		printf "\n========= RELEASING PACKAGE %s =========\n\n" "$package_dir"
-		
-		cd "$package_dir"
-		
-		make clean
-		make commit-version
-		make dist
-		make new-version
-		
-		cd ..
-	fi
+    if [ -z "$package_dir" ]; then
+        # Skip empty lines
+        continue
+    fi
+    printf "\n========= RELEASING PACKAGE %s =========\n\n" "$package_dir"
+    
+    cd "$package_dir"
+    
+    make clean
+    make commit-version
+    make dist
+    make new-version
+    
+    cd ..
 done < packages_by_dep_dag.txt
