71237cf5cfc0390e324171e3dd648454ae24f767
davelopez <46503462+davelopez@users.noreply.github.com>
2024-02-06 14:29:37 +0100

    Add unit test for Tools copyLink utility

---
+++ client/src/components/Tool/utilities.test.ts
@@ -0,0 +1,29 @@
+import { copyLink } from "./utilities";
+
+const writeText = jest.fn();
+
+Object.assign(navigator, {
+    clipboard: {
+        writeText,
+    },
+});
+
+describe("copyLink", () => {
+    beforeEach(() => {
+        (navigator.clipboard.writeText as jest.Mock).mockResolvedValue(undefined);
+    });
+
+    it("should copy the link to the clipboard", () => {
+        const toolId = "MyToolId";
+        copyLink(toolId);
+        expect(writeText).toHaveBeenCalledTimes(1);
+        expect(writeText).toHaveBeenCalledWith(expect.stringContaining(toolId));
+    });
+
+    it("should encode the tool id", () => {
+        const toolId = "My Tool Id";
+        copyLink(toolId);
+        expect(writeText).toHaveBeenCalledTimes(1);
+        expect(writeText).toHaveBeenCalledWith(expect.stringContaining("My%20Tool%20Id"));
+    });
+});
