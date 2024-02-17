db736612e15eed80cca7d02ca03df54455286b51
guerler <aysam.guerler@gmail.com>
2024-02-17 16:30:31 -0500

    Add grid pages

---
+++ client/src/components/Grid/GridPage.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPlus, faStar, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { BNav, BNavItem } from "bootstrap-vue";
+
+import pagesGridConfig from "@/components/Grid/configs/pages";
+import pagesPublishedGridConfig from "@/components/Grid/configs/pagesPublished";
+
+import Heading from "@/components/Common/Heading.vue";
+import GridList from "@/components/Grid/GridList.vue";
+
+library.add(faPlus, faStar, faTrash);
+
+interface Props {
+    activeList?: "my" | "published";
+}
+
+withDefaults(defineProps<Props>(), {
+    activeList: "my",
+});
+</script>
+
+<template>
+    <div>
+        <div class="mb-2">
+            <div class="d-flex">
+                <Heading h1 separator inline size="xl" class="flex-grow-1 mb-2">Pages</Heading>
+                <div>
+                    <BButton size="sm" variant="outline-primary" to="/pages/create">
+                        <Icon :icon="faPlus" />
+                        <span v-localize>Create New Page</span>
+                    </BButton>
+                </div>
+            </div>
+            <BNav pills justified class="mb-2">
+                <BNavItem :active="activeList === 'my'" to="/pages/list"> My Pages </BNavItem>
+                <BNavItem :active="activeList === 'published'" to="/pages/list_published"> Public Pages </BNavItem>
+            </BNav>
+            <GridList v-if="activeList === 'my'" :grid-config="pagesGridConfig" embedded />
+            <GridList v-else :grid-config="pagesPublishedGridConfig" embedded />
+        </div>
+    </div>
+</template>
