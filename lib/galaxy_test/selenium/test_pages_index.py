0db37564cdf896456a6fdde4dff61bde0a596858
guerler <aysam.guerler@gmail.com>
2024-02-19 22:45:54 -0500

    Adjust page deletion test case

--- lib/galaxy_test/selenium/test_pages_index.py
+++ lib/galaxy_test/selenium/test_pages_index.py
@@ -11,10 +11,10 @@ class TestPagesIndex(SeleniumTestCase):
     @selenium_test
     def test_page_deletion(self):
         page_response = self.new_page()
-        page_id = page_response["id"]
+        page_title = page_response["title"]
         self.navigate_to_pages()
         self._assert_showing_n_pages(1)
-        self.select_grid_operation("MY PAGE", "Delete")
+        self.select_grid_operation(page_title, "Delete")
         alert = self.driver.switch_to.alert
         alert.accept()
         self._assert_showing_n_pages(0)
