65bb37a769b880d5497669061819db4fe5832a89
John Chilton <jmchilton@gmail.com>
2024-02-21 10:30:47 -0500

    opps.

--- lib/galaxy/model/migrations/alembic/versions_gxy/7ffd33d5d144_implement_structured_tool_state.py
+++ lib/galaxy/model/migrations/alembic/versions_gxy/7ffd33d5d144_implement_structured_tool_state.py
@@ -15,6 +15,13 @@ from sqlalchemy import (
 )
 
 from galaxy.model.custom_types import JSONType
+from galaxy.model.migrations.util import (
+    add_column,
+    create_table,
+    drop_column,
+    drop_table,
+    transaction,
+)
 
 # revision identifiers, used by Alembic.
 revision = "7ffd33d5d144"
@@ -24,25 +31,27 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table(
-        "tool_source",
-        Column("id", Integer, primary_key=True),
-        Column("hash", String(255), index=True),
-        Column("source", JSONType),
-    )
-    op.create_table(
-        "tool_request",
-        Column("id", Integer, primary_key=True),
-        Column("request", JSONType),
-        Column("state", String(32)),
-        Column("state_message", JSONType),
-        Column("tool_source_id", Integer, ForeignKey("tool_sources.id"), index=True),
-        Column("history_id", Integer, ForeignKey("history.id"), index=True),
-    )
-    op.add_column("job", Column("tool_request_id", Integer, ForeignKey("tool_source.id"), index=True))
+    with transaction():
+        create_table(
+            "tool_source",
+            Column("id", Integer, primary_key=True),
+            Column("hash", String(255), index=True),
+            Column("source", JSONType),
+        )
+        create_table(
+            "tool_request",
+            Column("id", Integer, primary_key=True),
+            Column("request", JSONType),
+            Column("state", String(32)),
+            Column("state_message", JSONType),
+            Column("tool_source_id", Integer, ForeignKey("tool_source.id"), index=True),
+            Column("history_id", Integer, ForeignKey("history.id"), index=True),
+        )
+        add_column("job", Column("tool_request_id", Integer, ForeignKey("tool_request.id"), index=True))
 
 
 def downgrade():
-    op.drop_table("tool_request")
-    op.drop_table("tool_source")
-    op.drop_column("job", "tool_request_id")
+    with transaction():
+        drop_column("job", "tool_request_id")
+        drop_table("tool_request")
+        drop_table("tool_source")
