83ca1a50c6bc5896d1fde522c734239112e7a590
Ahmed Awan <qe66653@umbc.edu>
2024-02-21 19:03:52 -0600

    if `config.require_login: true`, do not show panels

--- client/src/composables/usePanels.ts
+++ client/src/composables/usePanels.ts
@@ -3,14 +3,19 @@ import { useRoute } from "vue-router/composables";
 
 import { useUserStore } from "@/stores/userStore";
 
+import { useConfig } from "./config";
+
 export function usePanels() {
     const userStore = useUserStore();
     const route = useRoute();
+    const { config, isConfigLoaded } = useConfig();
 
     const showPanels = computed(() => {
         const panels = route.query.hide_panels;
         if (panels !== undefined && panels !== null && typeof panels === "string") {
             return panels.toLowerCase() != "true";
+        } else if (isConfigLoaded.value && config.value.require_login === true) {
+            return false;
         }
         return true;
     });
