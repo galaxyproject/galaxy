78725fb9311a3458782941a0a76aa80dcb9c35a6
heisner-tillman <theisner@posteo.de>
2024-02-23 09:20:44 +0100

    Remove models for get_tool_predictions

--- lib/galaxy/schema/workflows.py
+++ lib/galaxy/schema/workflows.py
@@ -83,32 +83,6 @@ class GetTargetHistoryPayload(Model):
     )
 
 
-class GetToolPredictionsPayload(Model):
-    tool_sequence: Optional[Any] = Field(
-        None,
-        title="Tool Sequence",
-        description="comma separated sequence of tool ids",
-    )
-    remote_model_url: Optional[Any] = Field(
-        None,
-        title="Remote Model URL",
-        description="Path to the deep learning model",
-    )
-
-
-class ToolPredictionsSummary(Model):
-    current_tool: Optional[Any] = Field(
-        None,
-        title="Current Tools",
-        description="A comma separated sequence of the current tool ids",
-    )
-    predicted_data: Optional[Any] = Field(
-        None,
-        title="Recommended Tools",
-        description="List of predictions",
-    )
-
-
 class InvokeWorkflowPayload(GetTargetHistoryPayload):
     # TODO - Are the descriptions correct?
     instance: Optional[bool] = Field(
