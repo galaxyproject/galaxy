5e27757fce03afaf3c99fe143473412a3b89288f
heisner-tillman <theisner@posteo.de>
2024-02-26 19:32:48 +0100

    Remove the mapping to the legacy route

--- lib/galaxy/webapps/galaxy/buildapp.py
+++ lib/galaxy/webapps/galaxy/buildapp.py
@@ -591,9 +591,6 @@ def populate_api_routes(webapp, app):
 
     webapp.mapper.resource("plugins", "plugins", path_prefix="/api")
     webapp.mapper.connect("/api/workflows/build_module", action="build_module", controller="workflows")
-    webapp.mapper.connect(
-        "/api/workflows/menu", action="set_workflow_menu", controller="workflows", conditions=dict(method=["PUT"])
-    )
     webapp.mapper.resource("workflow", "workflows", path_prefix="/api")
 
     # ---- visualizations registry ---- generic template renderer
