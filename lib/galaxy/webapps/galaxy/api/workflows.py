f1a281c60bac89ccf812107c4c6300d466dbb3cd
heisner-tillman <theisner@posteo.de>
2024-02-27 17:35:19 +0100

    Type payload of set_workflow_menu operation properly

--- lib/galaxy/webapps/galaxy/api/workflows.py
+++ lib/galaxy/webapps/galaxy/api/workflows.py
@@ -857,7 +857,6 @@ RefactorWorkflowBody = Annotated[
 SetWorkflowMenuBody = Annotated[
     Optional[SetWorkflowMenuPayload],
     Body(
-        default=None,
         title="Set workflow menu",
         description="The values to set a workflow menu.",
     ),
@@ -986,7 +985,7 @@ class FastAPIWorkflows:
     )
     def set_workflow_menu(
         self,
-        payload: SetWorkflowMenuBody,
+        payload: SetWorkflowMenuBody = None,
         trans: ProvidesHistoryContext = DependsOnTrans,
     ) -> SetWorkflowMenuSummary:
         return self.service.set_workflow_menu(payload, trans)
