982e12889233ea994868255735106fc2fb2cfd00
Leonid Kostrykin <void@evoid.de>
2024-02-28 12:22:29 +0100

    Add more metadata assertions

--- lib/galaxy/tool_util/verify/asserts/image.py
+++ lib/galaxy/tool_util/verify/asserts/image.py
@@ -10,11 +10,21 @@ from PIL import Image
 def assert_has_image_metadata(
     output_bytes: bytes,
     width: Optional[Union[int,str]] = None,
+    height: Optional[Union[int,str]] = None,
+    channels: Optional[Union[int,str]] = None,
 ) -> None:
-    """Asserts the specified image output has a specific width"""
+    """
+    Assert the specified image output has specific metadata.
+    """
     buf = io.BytesIO(output_bytes)
     with Image.open(buf) as im:
+
         assert width is None or im.size[0] == int(width), \
             f"Image has wrong width: {im.size[0]} (expected {int(width)})"
 
-    # len(image.getbands())
+        assert height is None or im.size[1] == int(height), \
+            f"Image has wrong height: {im.size[1]} (expected {int(height)})"
+
+        actual_channels = len(im.getbands())
+        assert channels is None or actual_channels == int(channels), \
+            f"Image has wrong number of channels: {actual_channels} (expected {int(channels)})"
