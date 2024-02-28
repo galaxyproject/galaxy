900881b0443f0cbebc9e5cc78e37e17e6007a7d3
Leonid Kostrykin <void@evoid.de>
2024-02-28 12:17:54 +0100

    Fix bug

--- lib/galaxy/tool_util/verify/asserts/image.py
+++ lib/galaxy/tool_util/verify/asserts/image.py
@@ -1,6 +1,7 @@
 import io
 from typing import (
     Optional,
+    Union,
 )
 
 from PIL import Image
@@ -8,12 +9,12 @@ from PIL import Image
 
 def assert_has_image_metadata(
     output_bytes: bytes,
-    width: Optional[int] = None,
+    width: Optional[Union[int,str]] = None,
 ) -> None:
     """Asserts the specified image output has a specific width"""
     buf = io.BytesIO(output_bytes)
     with Image.open(buf) as im:
-        assert width is None or im.size[0] == width, \
-            f"Image has wrong width: {im.size[0]} (expected {width})"
+        assert width is None or im.size[0] == int(width), \
+            f"Image has wrong width: {im.size[0]} (expected {int(width)})"
 
     # len(image.getbands())
