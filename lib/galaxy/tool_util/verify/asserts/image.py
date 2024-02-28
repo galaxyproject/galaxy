aad820f59a81fb6b0864dd9dc2daad787b93e077
Leonid Kostrykin <void@evoid.de>
2024-02-28 11:46:42 +0100

    Fix syntax error

--- lib/galaxy/tool_util/verify/asserts/image.py
+++ lib/galaxy/tool_util/verify/asserts/image.py
@@ -12,5 +12,5 @@ def assert_has_image_metadata(
     """Asserts the specified image output has a specific width"""
     buf = io.BytesIO(output_bytes)
     with Image.open(buf) as im:
-        assert width is None or im.size[0] == width,
+        assert width is None or im.size[0] == width, \
             "Image has wrong width: {im.size[0]} (expected {width})"
