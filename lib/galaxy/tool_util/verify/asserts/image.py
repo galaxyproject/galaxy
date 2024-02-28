7af279300b7a46ca9388852b903fc313e5cca434
Leonid Kostrykin <void@evoid.de>
2024-02-28 14:18:52 +0100

    Add tests for label images

--- lib/galaxy/tool_util/verify/asserts/image.py
+++ lib/galaxy/tool_util/verify/asserts/image.py
@@ -2,19 +2,21 @@ import io
 from typing import (
     Optional,
     Union,
+    List
 )
 
+import numpy
 from PIL import Image
 
 
-def assert_has_image_metadata(
+def assert_image_has_metadata(
     output_bytes: bytes,
     width: Optional[Union[int,str]] = None,
     height: Optional[Union[int,str]] = None,
     channels: Optional[Union[int,str]] = None,
 ) -> None:
     """
-    Assert the specified image output has specific metadata.
+    Assert the image output has specific metadata.
     """
     buf = io.BytesIO(output_bytes)
     with Image.open(buf) as im:
@@ -28,3 +30,40 @@ def assert_has_image_metadata(
         actual_channels = len(im.getbands())
         assert channels is None or actual_channels == int(channels), \
             f"Image has wrong number of channels: {actual_channels} (expected {int(channels)})"
+
+
+def assert_image_has_labels(
+    output_bytes: bytes,
+    number_of_objects: Optional[Union[int, str]] = None,
+    mean_object_size: Optional[Union[float, str]] = None,
+    exclude_labels: Optional[Union[str, List[int]]] = list(),
+    eps: Optional[Union[float, str]] = 1e-8,
+) -> None:
+    """
+    Assert the image output has specific label content.
+    """
+    buf = io.BytesIO(output_bytes)
+    with Image.open(buf) as im:
+        im_arr = numpy.array(im)
+    
+    # Determine labels present in the image.
+    labels = numpy.unique(im_arr)
+
+    # Apply filtering induced by `exclude_labels`.
+    if isinstance(exclude_labels, str):
+        exclude_labels = [im_arr.dtype(label) for label in exclude_labels.split(",") if len(label) > 0]
+    labels = [label for label in labels if label is not in exclude_labels]
+
+    # Perform `number_of_objects` assertion.
+    if number_of_objects is not None:
+        actual = len(labels)
+        expected = int(number_of_objects)
+        assert actual == expected, \
+            f"Wrong number of objects: {actual} (expected {expected})"
+
+    # Perform `mean_object_size` assertion.
+    if mean_object_size is not None:
+        actual = numpy.mean((im_arr == label).sum() for label in labels)
+        expected = float(mean_object_size)
+        assert abs(actual - expected) <= float(eps), \
+            f"Wrong mean object size: {actual} (expected {expected})"
