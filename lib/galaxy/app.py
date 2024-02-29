61d97a212a0fc03428e1637836785c35235612db
Stephen Von Worley <svonworl@ucsc.edu>
2024-02-29 12:11:04 -0800

    append 'galaxy' to python/requests User-Agent header value

--- lib/galaxy/app.py
+++ lib/galaxy/app.py
@@ -17,6 +17,7 @@ from typing import (
 
 from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
+import requests
 
 import galaxy.model
 import galaxy.model.security
@@ -480,6 +481,12 @@ class MinimalGalaxyApplication(BasicSharedApp, HaltableContainer, SentryClientMi
         for sig, handler in handlers.items():
             signal.signal(sig, handler)
 
+    def _configure_user_agent(self):
+        """Append 'galaxy' to the python requests http user agent string if it's not already there."""
+        original_function = requests.utils.default_user_agent
+        if not original_function().endswith(" galaxy"):
+            requests.utils.default_user_agent = lambda *args: original_function(*args) + " galaxy"
+
     def _wait_for_database(self, url):
         attempts = self.config.database_wait_attempts
         pause = self.config.database_wait_sleep
@@ -772,6 +779,9 @@ class UniverseApplication(StructuredApp, GalaxyManagerApplication):
         if self.heartbeat:
             handlers[signal.SIGUSR1] = self.heartbeat.dump_signal_handler
         self._configure_signal_handlers(handlers)
+
+        # Set HTTP User-Agent header string
+        self._configure_user_agent()
 
         self.database_heartbeat = DatabaseHeartbeat(application_stack=self.application_stack)
         self.database_heartbeat.add_change_callback(self.watchers.change_state)
