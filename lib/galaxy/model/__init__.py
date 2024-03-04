5837c32f66be01968eda452bd2797055b86164c4
mvdbeek <m.vandenbeek@gmail.com>
2024-03-03 18:15:06 -0600

    Add invocation filters to history contents
    This is a basic filter that should cover
    https://github.com/galaxyproject/galaxy/issues/17103, but we should also
    add invocation step and invocation output filters.
    
    Subworkflows and hidden datasets are still a TODO.

--- lib/galaxy/model/__init__.py
+++ lib/galaxy/model/__init__.py
@@ -87,6 +87,7 @@ from sqlalchemy import (
     tuple_,
     type_coerce,
     Unicode,
+    union,
     UniqueConstraint,
     update,
     VARCHAR,
@@ -4994,6 +4995,8 @@ class HistoryDatasetAssociation(DatasetInstance, HasTags, Dictifiable, UsesAnnot
     """
     Resource class that creates a relation between a dataset and a user history.
     """
+
+    workflow_invocation_id: column_property
 
     def __init__(
         self,
@@ -11065,6 +11068,14 @@ mapper_registry.map_imperatively(
             primaryjoin=(lambda: ImplicitlyConvertedDatasetAssociation.hda_id == HistoryDatasetAssociation.id),
             back_populates="dataset",
         ),
+        invocation_id=column_property(
+            select(WorkflowInvocation.id)
+            .join(WorkflowInvocationStep, WorkflowInvocation.id == WorkflowInvocationStep.workflow_invocation_id)
+            .join(Job, Job.id == WorkflowInvocationStep.job_id)
+            .join(Dataset, and_(Dataset.job_id == Job.id, Dataset.id == HistoryDatasetAssociation.table.c.dataset_id))
+            .scalar_subquery(),
+            deferred=True,
+        ),
     ),
 )
 
@@ -11217,6 +11228,34 @@ WorkflowInvocationStep.subworkflow_invocation_id = column_property(
     )
     .scalar_subquery(),
 )
+
+HistoryDatasetCollectionAssociation.invocation_id = column_property(
+    union(
+        select(WorkflowInvocation.id)
+        .join(WorkflowInvocationStep, WorkflowInvocation.id == WorkflowInvocationStep.workflow_invocation_id)
+        .join(Job, Job.id == WorkflowInvocationStep.job_id)
+        .join(
+            JobToOutputDatasetCollectionAssociation,
+            and_(
+                JobToOutputDatasetCollectionAssociation.job_id == Job.id,
+                JobToOutputDatasetCollectionAssociation.dataset_collection_id == HistoryDatasetCollectionAssociation.id,
+            ),
+        ),
+        select(WorkflowInvocation.id)
+        .join(WorkflowInvocationStep, WorkflowInvocation.id == WorkflowInvocationStep.workflow_invocation_id)
+        .join(
+            ImplicitCollectionJobs,
+            and_(
+                ImplicitCollectionJobs.id == WorkflowInvocationStep.implicit_collection_jobs_id,
+                HistoryDatasetCollectionAssociation.implicit_collection_jobs_id == ImplicitCollectionJobs.id,
+            ),
+        ),
+    )
+    .limit(1)
+    .scalar_subquery(),
+    deferred=True,
+)
+
 
 # Set up proxy so that this syntax is possible:
 # <user_obj>.preferences[pref_name] = pref_value
