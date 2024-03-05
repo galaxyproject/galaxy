805b675c25bc0d219d9026b27d34ef448d565016
Dannon Baker <dannon.baker@gmail.com>
2024-03-05 09:39:23 -0500

    Swap default toggle state to 'not toggled', adjust awsestimate

--- client/src/composables/persistentToggle.ts
+++ client/src/composables/persistentToggle.ts
@@ -9,7 +9,7 @@ interface ToggleStateInterface {
 
 export function usePersistentToggle(uniqueId: string): ToggleStateInterface {
     const localStorageKey = `toggle-state-${uniqueId}`;
-    const toggled = useUserLocalStorage(localStorageKey, true);
+    const toggled = useUserLocalStorage(localStorageKey, false);
 
     // Expose a function for toggling state
     const toggle = () => {
