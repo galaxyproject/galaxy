a064d056c6c9d811ef441670e892a632ff7438e7
Ahmed Awan <qe66653@umbc.edu>
2024-03-07 15:37:28 -0600

    add non-keyed filter values as entries in history items
    For `HistoryContents` filters such as `related` and `invocation_id`, we do not have keys in the api payload summary. Therefore, I have created an object in the `historyItemsStore` that is passed to `mergeArray` and an entry is created in the content items for the given "non-keyed" filters.

--- client/src/store/historyStore/model/utilities.js
+++ client/src/store/historyStore/model/utilities.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 
 /* This function merges the existing data with new incoming data. */
-export function mergeArray(id, payload, items, itemKey) {
+export function mergeArray(id, payload, items, itemKey, nonKeyedValues = {}) {
     if (!items[id]) {
         Vue.set(items, id, []);
     }
@@ -14,9 +14,19 @@ export function mergeArray(id, payload, items, itemKey) {
                 Object.keys(localItem).forEach((key) => {
                     localItem[key] = item[key];
                 });
+
+                // for each non-keyed value, create a new key in the local item
+                Object.keys(nonKeyedValues).forEach((key) => {
+                    Vue.set(localItem, key, nonKeyedValues[key]);
+                });
             }
         } else {
             Vue.set(itemArray, itemIndex, item);
+            const localItem = itemArray[itemIndex];
+            // for each non-keyed value, create a new key in the local item
+            Object.keys(nonKeyedValues).forEach((key) => {
+                Vue.set(localItem, key, nonKeyedValues[key]);
+            });
         }
     }
 }
