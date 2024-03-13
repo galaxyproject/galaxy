848177e04e14df7460cd4e935b692f4dd9270339
Martin Cech <marten@bx.psu.edu>
2024-03-12 19:01:46 -0700

    remove reference to a non-existent component

--- client/src/components/Panels/ToolBox.vue
+++ client/src/components/Panels/ToolBox.vue
@@ -252,7 +252,6 @@ function setButtonText() {
                 @onQuery="(q) => (query = q)"
                 @onResults="onResults" />
             <section v-if="!propShowAdvanced">
-                <UploadButton v-if="!props.workflow" />
                 <div v-if="hasResults && resultPanel" class="pb-2">
                     <b-button size="sm" class="w-100" @click="onToggle">
                         <FontAwesomeIcon :icon="buttonIcon" />
