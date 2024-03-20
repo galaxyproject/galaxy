1fdbb121dcfe21027fd08d07b3e27736c3a50663
Marius van den Beek <m.vandenbeek@gmail.com>
2024-03-20 08:06:41 +0100

    Create new history if user has no history AND for anonymous users

--- lib/galaxy/webapps/base/webapp.py
+++ lib/galaxy/webapps/base/webapp.py
@@ -938,6 +938,8 @@ class GalaxyWebTransaction(base.DefaultWebTransaction, context.ProvidesHistoryCo
                 self.set_history(history)
             else:
                 history = self.new_history()
+        else:
+            history = self.new_history()
 
         return history
 
