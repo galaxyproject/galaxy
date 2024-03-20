6b979a8a49515eb1b4d1704825667fe815acc43b
Alireza Heidari <itisalirh@gmail.com>
2024-03-20 12:35:30 +0100

    🛠️: refactor `DatasetHistory` to use `composition API` and `typeScript`

--- client/src/components/Dataset/DatasetHistory.vue
+++ client/src/components/Dataset/DatasetHistory.vue
@@ -1,21 +1,26 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { type components } from "@/api/schema";
+import { useHistoryStore } from "@/stores/historyStore";
+
+type HDASummary = components["schemas"]["HDASummary"];
+
+interface Props {
+    item: HDASummary;
+}
+
+const props = defineProps<Props>();
+
+const historyStore = useHistoryStore();
+
+const historyName = computed(() => {
+    return historyStore.getHistoryNameById(props.item.history_id);
+});
+</script>
+
 <template>
     <div>
         {{ historyName }}
     </div>
 </template>
-<script>
-import { mapState } from "pinia";
-import { useHistoryStore } from "stores/historyStore";
-
-export default {
-    props: {
-        item: Object,
-    },
-    computed: {
-        ...mapState(useHistoryStore, ["getHistoryNameById"]),
-        historyName() {
-            return this.getHistoryNameById(this.item.history_id);
-        },
-    },
-};
-</script>
