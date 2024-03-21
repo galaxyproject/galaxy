22bae91fa28ed920cf7dc21836d8efb9ca0a53d5
Alireza Heidari <itisalirh@gmail.com>
2024-03-21 15:20:49 +0100

    🛠️: refactor `DatasetAttributes` test to `typeScript`

--- client/src/components/DatasetInformation/DatasetAttributes.test.ts
+++ client/src/components/DatasetInformation/DatasetAttributes.test.ts
@@ -1,16 +1,22 @@
+import { createTestingPinia } from "@pinia/testing";
 import { mount } from "@vue/test-utils";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 import flushPromises from "flush-promises";
+import { setActivePinia } from "pinia";
 import { getLocalVue } from "tests/jest/helpers";
 
-import MockProvider from "../providers/MockProvider";
-import DatasetAttributes from "./DatasetAttributes";
+import MockProvider from "@/components/providers/MockProvider";
+
+import DatasetAttributes from "./DatasetAttributes.vue";
 
 const localVue = getLocalVue();
 
 async function buildWrapper(conversion_disable = false) {
-    const wrapper = mount(DatasetAttributes, {
+    const pinia = createTestingPinia();
+    setActivePinia(pinia);
+
+    const wrapper = mount(DatasetAttributes as object, {
         propsData: {
             datasetId: "dataset_id",
         },
@@ -28,8 +34,11 @@ async function buildWrapper(conversion_disable = false) {
             FontAwesomeIcon: false,
             FormElement: false,
         },
+        pinia,
     });
+
     await flushPromises();
+
     return wrapper;
 }
 
@@ -37,7 +46,9 @@ describe("DatasetAttributes", () => {
     it("check rendering", async () => {
         const axiosMock = new MockAdapter(axios);
         axiosMock.onPut(`/dataset/set_edit`).reply(200, { message: "success", status: "success" });
+
         const wrapper = await buildWrapper();
+
         expect(wrapper.findAll("button").length).toBe(6);
         expect(wrapper.findAll("#attribute_text").length).toBe(1);
         expect(wrapper.findAll("#conversion_text").length).toBe(1);
@@ -45,14 +56,18 @@ describe("DatasetAttributes", () => {
         expect(wrapper.findAll("#permission_text").length).toBe(1);
         expect(wrapper.findAll(".tab-pane").length).toBe(3);
         expect(wrapper.findAll(".ui-portlet-section").length).toBe(2);
+
         const $button = wrapper.find("#dataset-attributes-default-save");
+
         await $button.trigger("click");
         await flushPromises();
+
         expect(wrapper.find("[role=alert]").text()).toBe("success");
     });
 
     it("check rendering without conversion option", async () => {
         const wrapper = await buildWrapper(true);
+
         expect(wrapper.findAll("button").length).toBe(5);
         expect(wrapper.findAll("#attribute_text").length).toBe(1);
         expect(wrapper.findAll("#conversion_text").length).toBe(0);
