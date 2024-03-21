320012140cae85d4c735fc1ec84b2dbca47a5c21
Alireza Heidari <itisalirh@gmail.com>
2024-03-21 15:18:40 +0100

    🛠️: refactor `DatasetAttributes` to use `composition API` and `typeScript`

--- client/src/components/DatasetInformation/DatasetAttributes.vue
+++ client/src/components/DatasetInformation/DatasetAttributes.vue
@@ -1,157 +1,180 @@
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars, faCog, faDatabase, faExchangeAlt, faRedo, faSave, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { BAlert, BButton, BTab, BTabs } from "bootstrap-vue";
+import { ref } from "vue";
+
+import { DatasetAttributesProvider } from "@/components/providers/DatasetProvider";
+import { useHistoryStore } from "@/stores/historyStore";
+import localize from "@/utils/localization";
+
+import { setAttributes } from "./services";
+
+import FormDisplay from "@/components/Form/FormDisplay.vue";
+
+library.add(faBars, faCog, faDatabase, faExchangeAlt, faRedo, faSave, faUser);
+
+interface Props {
+    datasetId: string;
+}
+
+const props = defineProps<Props>();
+
+const historyStore = useHistoryStore();
+
+const messageText = ref("");
+const messageVariant = ref("danger");
+const formData = ref<Record<string, any>>({});
+
+function onAttribute(data: object) {
+    formData.value["attribute"] = data;
+}
+
+function onConversion(data: object) {
+    formData.value["conversion"] = data;
+}
+
+function onDatatype(data: object) {
+    formData.value["datatype"] = data;
+}
+
+function onPermission(data: object) {
+    formData.value["permission"] = data;
+}
+
+function onError(message: string) {
+    messageText.value = message;
+    messageVariant.value = "danger";
+}
+
+function submit(key: string, operation: string) {
+    setAttributes(props.datasetId, formData.value[key], operation).then((response) => {
+        messageText.value = response.message;
+        messageVariant.value = response.status;
+
+        historyStore.loadCurrentHistory();
+    }, onError);
+}
+</script>
+
 <template>
     <div aria-labelledby="dataset-attributes-heading">
         <h1 id="dataset-attributes-heading" v-localize class="h-lg">Edit Dataset Attributes</h1>
-        <b-alert v-if="messageText" class="dataset-attributes-alert" :variant="messageVariant" show>
-            {{ messageText | l }}
-        </b-alert>
+
+        <BAlert v-if="messageText" class="dataset-attributes-alert" :variant="messageVariant" show>
+            {{ localize(messageText) }}
+        </BAlert>
+
         <DatasetAttributesProvider :id="datasetId" v-slot="{ result, loading }" @error="onError">
             <div v-if="!loading" class="mt-3">
-                <b-tabs>
-                    <b-tab v-if="!result['attribute_disable']">
+                <BTabs>
+                    <BTab v-if="!result['attribute_disable']">
                         <template v-slot:title>
-                            <FontAwesomeIcon icon="bars" class="mr-1" />{{ "Attributes" | l }}
+                            <FontAwesomeIcon :icon="faBars" class="mr-1" />
+                            {{ localize("Attributes") }}
                         </template>
+
                         <FormDisplay :inputs="result['attribute_inputs']" @onChange="onAttribute" />
+
                         <div class="mt-2">
-                            <b-button
+                            <BButton
                                 id="dataset-attributes-default-save"
                                 variant="primary"
                                 class="mr-1"
                                 @click="submit('attribute', 'attributes')">
-                                <FontAwesomeIcon icon="save" class="mr-1" />{{ "Save" | l }}
-                            </b-button>
-                            <b-button v-if="!result['metadata_disable']" @click="submit('attribute', 'autodetect')">
-                                <FontAwesomeIcon icon="redo" class="mr-1" />{{ "Auto-detect" | l }}
-                            </b-button>
+                                <FontAwesomeIcon :icon="faSave" class="mr-1" />
+                                {{ localize("Save") }}
+                            </BButton>
+
+                            <BButton v-if="!result['metadata_disable']" @click="submit('attribute', 'autodetect')">
+                                <FontAwesomeIcon :icon="faRedo" class="mr-1" />
+                                {{ localize("Auto-detect") }}
+                            </BButton>
                         </div>
-                    </b-tab>
-                    <b-tab
+                    </BTab>
+
+                    <BTab
                         v-if="
                             (!result['conversion_disable'] || !result['datatype_disable']) &&
                             !result['metadata_disable']
                         ">
                         <template v-slot:title>
-                            <FontAwesomeIcon icon="database" class="mr-1" />{{ "Datatypes" | l }}
+                            <FontAwesomeIcon :icon="faDatabase" class="mr-1" />
+                            {{ localize("Datatypes") }}
                         </template>
+
                         <div v-if="!result['datatype_disable']" class="ui-portlet-section">
                             <div class="portlet-header">
                                 <div class="portlet-title">
-                                    <FontAwesomeIcon icon="database" class="portlet-title-icon fa-fw mr-1" />
+                                    <FontAwesomeIcon :icon="faDatabase" class="portlet-title-icon fa-fw mr-1" />
+
                                     <span class="portlet-title-text">
-                                        <b itemprop="name">{{ "Assign Datatype" | l }}</b>
+                                        <b itemprop="name">
+                                            {{ localize("Assign Datatype") }}
+                                        </b>
                                     </span>
                                 </div>
                             </div>
                             <div class="portlet-content">
                                 <FormDisplay :inputs="result['datatype_inputs']" @onChange="onDatatype" />
+
                                 <div class="mt-2">
-                                    <b-button variant="primary" class="mr-1" @click="submit('datatype', 'datatype')">
-                                        <FontAwesomeIcon icon="save" class="mr-1" />{{ "Save" | l }}
-                                    </b-button>
-                                    <b-button @click="submit('datatype', 'datatype_detect')">
-                                        <FontAwesomeIcon icon="redo" class="mr-1" />{{ "Auto-detect" | l }}
-                                    </b-button>
+                                    <BButton variant="primary" class="mr-1" @click="submit('datatype', 'datatype')">
+                                        <FontAwesomeIcon :icon="faSave" class="mr-1" />
+                                        {{ localize("Save") }}
+                                    </BButton>
+
+                                    <BButton @click="submit('datatype', 'datatype_detect')">
+                                        <FontAwesomeIcon :icon="faRedo" class="mr-1" />
+                                        {{ localize("Auto-detect") }}
+                                    </BButton>
                                 </div>
                             </div>
                         </div>
+
                         <div v-if="!result['conversion_disable']" class="ui-portlet-section">
                             <div class="portlet-header">
                                 <div class="portlet-title">
-                                    <FontAwesomeIcon icon="cog" class="portlet-title-icon fa-fw mr-1" />
+                                    <FontAwesomeIcon :icon="faCog" class="portlet-title-icon fa-fw mr-1" />
+
                                     <span class="portlet-title-text">
-                                        <b itemprop="name">{{ "Convert to Datatype" | l }}</b>
+                                        <b itemprop="name">
+                                            {{ localize("Convert to Datatype") }}
+                                        </b>
                                     </span>
                                 </div>
                             </div>
                             <div class="portlet-content">
                                 <FormDisplay :inputs="result['conversion_inputs']" @onChange="onConversion" />
+
                                 <div class="mt-2">
-                                    <b-button variant="primary" @click="submit('conversion', 'conversion')">
-                                        <FontAwesomeIcon icon="exchange-alt" class="mr-1" />{{ "Create Dataset" | l }}
-                                    </b-button>
+                                    <BButton variant="primary" @click="submit('conversion', 'conversion')">
+                                        <FontAwesomeIcon :icon="faExchangeAlt" class="mr-1" />
+                                        {{ localize("Create Dataset") }}
+                                    </BButton>
                                 </div>
                             </div>
                         </div>
-                    </b-tab>
-                    <b-tab v-if="!result['permission_disable']">
+                    </BTab>
+
+                    <BTab v-if="!result['permission_disable']">
                         <template v-slot:title>
-                            <FontAwesomeIcon icon="user" class="mr-1" />{{ "Permissions" | l }}
+                            <FontAwesomeIcon :icon="faUser" class="mr-1" />
+                            {{ localize("Permissions") }}
                         </template>
+
                         <FormDisplay :inputs="result['permission_inputs']" @onChange="onPermission" />
+
                         <div class="mt-2">
-                            <b-button variant="primary" @click="submit('permission', 'permission')">
-                                <FontAwesomeIcon icon="save" class="mr-1" />{{ "Save" | l }}
-                            </b-button>
+                            <BButton variant="primary" @click="submit('permission', 'permission')">
+                                <FontAwesomeIcon :icon="faSave" class="mr-1" />
+                                {{ localize("Save") }}
+                            </BButton>
                         </div>
-                    </b-tab>
-                </b-tabs>
+                    </BTab>
+                </BTabs>
             </div>
         </DatasetAttributesProvider>
     </div>
 </template>
-
-<script>
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBars, faCog, faDatabase, faExchangeAlt, faRedo, faSave, faUser } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { getGalaxyInstance } from "app";
-import FormDisplay from "components/Form/FormDisplay";
-import { DatasetAttributesProvider } from "components/providers/DatasetProvider";
-
-import { setAttributes } from "./services";
-
-library.add(faBars, faCog, faDatabase, faExchangeAlt, faRedo, faSave, faUser);
-
-export default {
-    components: {
-        DatasetAttributesProvider,
-        FontAwesomeIcon,
-        FormDisplay,
-    },
-    props: {
-        datasetId: {
-            type: String,
-            required: true,
-        },
-    },
-    data() {
-        return {
-            messageText: null,
-            messageVariant: null,
-            formData: {},
-        };
-    },
-    methods: {
-        onAttribute(data) {
-            this.formData["attribute"] = data;
-        },
-        onConversion(data) {
-            this.formData["conversion"] = data;
-        },
-        onDatatype(data) {
-            this.formData["datatype"] = data;
-        },
-        onPermission(data) {
-            this.formData["permission"] = data;
-        },
-        onError(messageText) {
-            this.messageText = messageText;
-            this.messageVariant = "danger";
-        },
-        submit(key, operation) {
-            setAttributes(this.datasetId, this.formData[key], operation).then((response) => {
-                this.messageText = response.message;
-                this.messageVariant = response.status;
-                this._reloadHistory();
-            }, this.onError);
-        },
-        /** reload Galaxy's history after updating dataset's attributes */
-        _reloadHistory: function () {
-            const Galaxy = getGalaxyInstance();
-            if (Galaxy) {
-                Galaxy.currHistoryPanel.loadCurrentHistory();
-            }
-        },
-    },
-};
-</script>
