351f04fd570da794d9981cb13d1577b23217fd6a
Alireza Heidari <itisalirh@gmail.com>
2024-03-21 15:30:58 +0100

    🛠️: refactor `DatasetSource` to use `composition API` and `typeScript`

--- client/src/components/DatasetInformation/DatasetSource.vue
+++ client/src/components/DatasetInformation/DatasetSource.vue
@@ -1,55 +1,56 @@
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCopy, faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { computed } from "vue";
+
+import { copy } from "@/utils/clipboard";
+import localize from "@/utils/localization";
+
+import DatasetSourceTransform from "@/components/DatasetInformation/DatasetSourceTransform.vue";
+
+library.add(faCopy, faExternalLinkAlt);
+
+interface Props {
+    source: {
+        source_uri: string;
+        transform: {
+            action: "to_posix_lines" | "spaces_to_tabs" | "datatype_groom";
+            datatype_ext: "bam" | "qname_sorted.bam" | "qname_input_sorted.bam" | "isa-tab" | "isa-json";
+        }[];
+    };
+}
+
+const props = defineProps<Props>();
+
+const sourceUri = computed(() => {
+    return props.source.source_uri;
+});
+const browserCompatUri = computed(() => {
+    return sourceUri.value && (sourceUri.value.indexOf("http") == 0 || sourceUri.value.indexOf("ftp") == 0);
+});
+
+function copyLink() {
+    copy(sourceUri.value, localize("Link copied to your clipboard"));
+}
+</script>
+
 <template>
     <li class="dataset-source">
         <a v-if="browserCompatUri" v-b-tooltip.hover title="Dataset Source URL" :href="sourceUri" target="_blank">
             {{ source.source_uri }}
-            <FontAwesomeIcon icon="external-link-alt" />
+            <FontAwesomeIcon :icon="faExternalLinkAlt" />
         </a>
         <span v-else>
             {{ source.source_uri }}
         </span>
-        <span v-b-tooltip.hover title="Copy URI"
-            ><FontAwesomeIcon icon="copy" style="cursor: pointer" @click="copyLink"
-        /></span>
+
+        <span v-b-tooltip.hover title="Copy URI">
+            <FontAwesomeIcon :icon="faCopy" style="cursor: pointer" @click="copyLink" />
+        </span>
+
         <br />
+
         <DatasetSourceTransform :transform="source.transform" />
     </li>
 </template>
-
-<script>
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCopy, faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { copy } from "utils/clipboard";
-import _l from "utils/localization";
-
-import DatasetSourceTransform from "./DatasetSourceTransform";
-
-library.add(faCopy, faExternalLinkAlt);
-
-export default {
-    components: {
-        DatasetSourceTransform,
-        FontAwesomeIcon,
-    },
-    props: {
-        source: {
-            type: Object,
-            required: true,
-        },
-    },
-    computed: {
-        browserCompatUri() {
-            const sourceUri = this.sourceUri;
-            return sourceUri && (sourceUri.indexOf("http") == 0 || sourceUri.indexOf("ftp") == 0);
-        },
-        sourceUri() {
-            return this.source.source_uri;
-        },
-    },
-    methods: {
-        copyLink() {
-            copy(this.sourceUri, _l("Link copied to your clipboard"));
-        },
-    },
-};
-</script>
