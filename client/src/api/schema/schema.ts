0bb7ad328e6074828f897d8afb50611f9ba0f988
heisner-tillman <theisner@posteo.de>
2024-03-27 15:16:54 +0100

    Regenerate the client schema

--- client/src/api/schema/schema.ts
+++ client/src/api/schema/schema.ts
@@ -14559,12 +14559,12 @@ export interface components {
              * X
              * @description Horizontal coordinate of the top right corner of the box.
              */
-            x?: number | null;
+            x?: number | number | null;
             /**
              * Y
              * @description Vertical coordinate of the top right corner of the box.
              */
-            y?: number | null;
+            y?: number | number | null;
         };
         /** WriteInvocationStoreToPayload */
         WriteInvocationStoreToPayload: {
