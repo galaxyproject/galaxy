738bb637e319e2a38021c8e8276e7889261abac2
guerler <aysam.guerler@gmail.com>
2024-04-02 10:09:58 +0300

    Remove nested div wrappers from admin center panel

--- client/src/entry/analysis/modules/Admin.vue
+++ client/src/entry/analysis/modules/Admin.vue
@@ -1,7 +1,3 @@
 <template>
-    <div id="columns" class="d-flex">
-        <div id="center" class="overflow-auto p-3 w-100">
-            <router-view :key="$route.fullPath" class="h-100" />
-        </div>
-    </div>
+    <router-view :key="$route.fullPath" />
 </template>
