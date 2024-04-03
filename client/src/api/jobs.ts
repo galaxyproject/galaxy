7435acfd30c09180af8861f27adc84cc30c5f0fd
Alireza Heidari <itisalirh@gmail.com>
2024-04-03 16:14:28 +0200

    ✨: add `fetchJobDetails` and `JobDetails` type to `api/jobs`

--- client/src/api/jobs.ts
+++ client/src/api/jobs.ts
@@ -9,4 +9,7 @@ export const fetchJobDestinationParams = fetcher.path("/api/jobs/{job_id}/destin
 
 export const jobsFetcher = fetcher.path("/api/jobs").method("get").create();
 
+export type JobDetails = components["schemas"]["ShowFullJobResponse"] | components["schemas"]["EncodedJobDetails"];
+export const fetchJobDetails = fetcher.path("/api/jobs/{job_id}").method("get").create();
+
 export const jobsReportError = fetcher.path("/api/jobs/{job_id}/error").method("post").create();
