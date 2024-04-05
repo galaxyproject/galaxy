0076ba2ee4094021e53dc4e0b1bfeee9bf18aa68
John Davis <jdavcs@gmail.com>
2024-04-05 14:08:41 -0400

    Fix typing in CustosAuthnzToken model

--- lib/galaxy/model/__init__.py
+++ lib/galaxy/model/__init__.py
@@ -10164,8 +10164,8 @@ class CustosAuthnzToken(Base, RepresentById):
     access_token: Mapped[Optional[str]] = mapped_column(Text)
     id_token: Mapped[Optional[str]] = mapped_column(Text)
     refresh_token: Mapped[Optional[str]] = mapped_column(Text)
-    expiration_time: Mapped[Optional[datetime]] = mapped_column(DateTime)
-    refresh_expiration_time: Mapped[Optional[datetime]] = mapped_column(DateTime)
+    expiration_time: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    refresh_expiration_time: Mapped[datetime] = mapped_column(DateTime, nullable=True)
     user = relationship("User", back_populates="custos_auth")
 
 
