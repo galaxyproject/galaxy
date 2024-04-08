d8ab05d4de3007747b5add4558481c323fbeb7b0
Alireza Heidari <itisalirh@gmail.com>
2024-04-08 23:23:32 +0200

    🛠️: refactor `FormOptionalText` to composition API and `typeScript`

--- client/src/components/Form/Elements/FormOptionalText.vue
+++ client/src/components/Form/Elements/FormOptionalText.vue
@@ -1,14 +1,68 @@
+<script setup lang="ts">
+import { BFormCheckbox } from "bootstrap-vue";
+import { computed, ref } from "vue";
+
+import FormText from "@/components/Form/Elements/FormText.vue";
+
+interface Props {
+    value?: string;
+    id?: string;
+    type?: string;
+    area?: boolean;
+    multiple?: boolean;
+    readonly?: boolean;
+    placeholder?: string;
+    datalist?: { value: string; label: string }[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    value: "",
+    id: "",
+    type: "text",
+    area: false,
+    multiple: false,
+    readonly: false,
+    placeholder: "",
+    datalist: () => [],
+});
+
+const emit = defineEmits<{
+    (e: "input", value: string): void;
+}>();
+
+const status = ref(false);
+
+const currentValue = computed({
+    get() {
+        return props.value;
+    },
+    set(val) {
+        emit("input", val);
+    },
+});
+const currentStatus = computed({
+    get() {
+        return status.value || props.value !== null;
+    },
+    set(val) {
+        status.value = Boolean(val);
+
+        currentValue.value = "";
+    },
+});
+</script>
+
 <template>
     <div>
-        <b-form-checkbox v-model="currentStatus" class="ui-switch" switch>
+        <BFormCheckbox v-model="currentStatus" class="ui-switch" switch>
             Set value for this optional select field?
-        </b-form-checkbox>
+        </BFormCheckbox>
+
         <FormText
             v-if="currentStatus"
             :id="id"
             v-model="currentValue"
             :readonly="readonly"
-            :value="value"
             :area="area"
             :placeholder="placeholder"
             :multiple="multiple"
@@ -16,77 +70,3 @@
             :type="type" />
     </div>
 </template>
-
-<script>
-import FormText from "./FormText";
-
-export default {
-    components: {
-        FormText,
-    },
-    props: {
-        value: {
-            default: "",
-        },
-        id: {
-            type: String,
-            default: null,
-        },
-        type: {
-            type: String,
-            default: "text",
-        },
-        area: {
-            // <textarea> instead of <input> element
-            type: Boolean,
-            default: false,
-        },
-        multiple: {
-            // Allow multiple entries to be created
-            type: Boolean,
-            default: false,
-        },
-        readonly: {
-            type: Boolean,
-            default: false,
-        },
-        placeholder: {
-            type: String,
-            default: null,
-        },
-        datalist: {
-            // Display list of suggestions in autocomplete dialog
-            type: Array,
-            default: null,
-        },
-    },
-    data() {
-        return {
-            status: false,
-        };
-    },
-    computed: {
-        currentValue: {
-            get() {
-                return this.value;
-            },
-            set(val) {
-                this.$emit("input", val);
-            },
-        },
-        currentStatus: {
-            get() {
-                return this.status || this.value !== null;
-            },
-            set(val) {
-                this.status = Boolean(val);
-                if (this.status) {
-                    this.currentValue = "";
-                } else {
-                    this.currentValue = null;
-                }
-            },
-        },
-    },
-};
-</script>
