15546954194b8a559553034b3a6bcbfb3dd26b0c
guerler <aysam.guerler@gmail.com>
2024-04-08 11:09:17 +0300

    Add plugin request to visualizations activity

--- client/src/components/Panels/VisualizationPanel.vue
+++ client/src/components/Panels/VisualizationPanel.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 import ActivityPanel from "@/components/Panels/ActivityPanel.vue";
 import PluginList from "@/components/Visualizations/PluginList.vue";
+import { urlData } from "@/utils/url";
+
+const plugins = ref([]);
+
+onMounted(() => {
+    getPlugins();
+});
+
+async function getPlugins() {
+    plugins.value = await urlData({ url: "/api/plugins" });
+}
+
 </script>
 
 <template>
     <ActivityPanel title="Visualizations" go-to-all-title="Saved Visualizations" href="/visualizations/list">
         <h3>Create Visualization</h3>
-        <PluginList />
+        <div class="card-container">
+            stuff
+        </div>
     </ActivityPanel>
 </template>
