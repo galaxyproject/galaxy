1b722212c7fdb44b83989782084cb0e5f0fdf799
Alireza Heidari <itisalirh@gmail.com>
2024-04-08 23:28:04 +0200

    🛠️: refactor `FormSelect` test to typescript

--- client/src/components/Form/Elements/FormSelect.test.ts
+++ client/src/components/Form/Elements/FormSelect.test.ts
@@ -1,15 +1,15 @@
 import { createTestingPinia } from "@pinia/testing";
-import { mount } from "@vue/test-utils";
-import { getLocalVue } from "tests/jest/helpers";
+import { getLocalVue } from "@tests/jest/helpers";
+import { mount, Wrapper } from "@vue/test-utils";
 
 import MountTarget from "./FormSelection.vue";
 
 const localVue = getLocalVue(true);
 
-function createTarget(propsData) {
+async function mountFormSelection(propsData: object) {
     const pinia = createTestingPinia();
 
-    return mount(MountTarget, {
+    return mount(MountTarget as object, {
         localVue,
         propsData,
         pinia,
@@ -23,10 +23,12 @@ const defaultOptions = [
     ["label_4", 99],
 ];
 
-function testDefaultOptions(wrapper) {
+function testDefaultOptions(wrapper: Wrapper<Vue>) {
     const target = wrapper.findComponent(MountTarget);
     const options = target.findAll("li > span > span");
+
     expect(options.length).toBe(4);
+
     for (let i = 0; i < options.length; i++) {
         expect(options.at(i).text()).toBe(`label_${i + 1}`);
     }
@@ -34,67 +36,93 @@ function testDefaultOptions(wrapper) {
 
 describe("FormSelect", () => {
     it("basics", async () => {
-        const wrapper = createTarget({
+        const wrapper = await mountFormSelection({
             options: defaultOptions,
         });
+
         testDefaultOptions(wrapper);
+
         const noValue = wrapper.find(".multiselect__option--selected");
         expect(noValue.exists()).toBe(false);
-        expect(wrapper.emitted().input[0][0]).toBe("value_1");
+        expect(wrapper.emitted().input?.[0]?.[0]).toBe("value_1");
+
         await wrapper.setProps({ value: "value_1" });
+
         const selectedValue = wrapper.find(".multiselect__option--selected");
         expect(selectedValue.text()).toBe("label_1");
     });
 
     it("optional values", async () => {
-        const wrapper = createTarget({
+        const wrapper = await mountFormSelection({
             options: defaultOptions,
             optional: true,
         });
+
         const target = wrapper.findComponent(MountTarget);
+
         const options = target.findAll("li > span > span");
         expect(options.length).toBe(5);
         expect(options.at(0).text()).toBe("Nothing selected");
+
         const selectedDefault = wrapper.find(".multiselect__option--selected");
         expect(selectedDefault.text()).toBe("Nothing selected");
+
         await wrapper.setProps({ value: "value_1" });
+
         const selectedValue = wrapper.find(".multiselect__option--selected");
         expect(selectedValue.text()).toBe("label_1");
+
         options.at(0).trigger("click");
-        const nullValue = wrapper.emitted().input[0][0];
+
+        const nullValue = wrapper.emitted().input?.[0]?.[0];
         expect(nullValue).toBe(null);
+
         await wrapper.setProps({ value: null });
+
         const unselectDefault = wrapper.find(".multiselect__option--selected");
         expect(unselectDefault.text()).toBe("Nothing selected");
     });
 
     it("multiple values", async () => {
-        const wrapper = createTarget({
+        const wrapper = await mountFormSelection({
             optional: true,
             multiple: true,
             options: defaultOptions,
             value: ["value_1", "", 99],
         });
+
         testDefaultOptions(wrapper);
+
         const selectedValue = wrapper.findAll(".multiselect__option--selected");
         expect(selectedValue.length).toBe(3);
         expect(selectedValue.at(0).text()).toBe("label_1");
         expect(selectedValue.at(1).text()).toBe("label_3");
         expect(selectedValue.at(2).text()).toBe("label_4");
+
         selectedValue.at(0).trigger("click");
-        const newValue = wrapper.emitted().input[0][0];
+
+        const newValue = wrapper.emitted().input?.[0]?.[0];
         expect(newValue).toEqual(["", 99]);
+
         await wrapper.setProps({ value: newValue });
+
         selectedValue.at(1).trigger("click");
-        const numericValue = wrapper.emitted().input[1][0];
+
+        const numericValue = wrapper.emitted().input?.[1]?.[0];
         expect(numericValue).toEqual([99]);
+
         await wrapper.setProps({ value: numericValue });
+
         selectedValue.at(2).trigger("click");
-        const nullValue = wrapper.emitted().input[2][0];
+
+        const nullValue = wrapper.emitted().input?.[2]?.[0];
         expect(nullValue).toBe(null);
+
         await wrapper.setProps({ value: nullValue });
+
         selectedValue.at(0).trigger("click");
-        const finalValue = wrapper.emitted().input[3][0];
+
+        const finalValue = wrapper.emitted().input?.[3]?.[0];
         expect(finalValue).toEqual(["value_1"]);
     });
 });
