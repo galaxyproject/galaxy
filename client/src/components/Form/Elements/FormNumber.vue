4d69ec180d6d4a0689f07ff49fb708c0acf0ff3e
Alireza Heidari <itisalirh@gmail.com>
2024-04-08 23:19:19 +0200

    🛠️: refactor `FormNumber` to composition API and `typeScript`

--- client/src/components/Form/Elements/FormNumber.vue
+++ client/src/components/Form/Elements/FormNumber.vue
@@ -1,12 +1,144 @@
+<script setup lang="ts">
+import { BAlert, BCol, BFormInput, BRow } from "bootstrap-vue";
+import { computed, ref } from "vue";
+
+interface Props {
+    value: number;
+    type: "integer" | "float";
+    min?: number;
+    max?: number;
+    workflowBuildingMode?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    min: undefined,
+    max: undefined,
+});
+
+const emit = defineEmits<{
+    (e: "input", value: string | number): void;
+}>();
+
+const dismissSecs = ref(5);
+const errorMessage = ref("");
+const dismissCountDown = ref(0);
+const defaultInputSizeWithSlider = ref(4);
+const fractionWarning = ref("This output doesn't allow fractions!");
+
+const isInteger = computed(() => {
+    return props.type.toLowerCase() === "integer";
+});
+const fieldType = computed(() => {
+    return props.workflowBuildingMode ? "text" : "number";
+});
+const isRangeValid = computed(() => {
+    return !isNaN(props.min) && !isNaN(props.max) && props.max > props.min;
+});
+const decimalPlaces = computed(() => {
+    return props.type.toLowerCase() === "integer" ? 0 : getNumberOfDecimals(props.value);
+});
+const currentValue = computed({
+    get() {
+        return props.value;
+    },
+    set(newVal: string | number) {
+        if (newVal !== props.value) {
+            emit("input", newVal);
+        }
+    },
+});
+
+/**
+ * Dynamically sets the step value depending on the
+ * current value precision when float number.
+ */
+const step = computed(() => {
+    if (isInteger.value) {
+        return 1;
+    }
+
+    if (decimalPlaces.value < 2) {
+        return 0.1;
+    } else if (decimalPlaces.value < 3) {
+        return 0.01;
+    }
+
+    return 0.001;
+});
+
+function onFloatInput(e: KeyboardEvent) {
+    if (isInteger.value) {
+        e.preventDefault();
+
+        showAlert(fractionWarning.value);
+    }
+}
+
+function onInputChange(value: number) {
+    resetAlert();
+
+    if (isOutOfRange(value)) {
+        showOutOfRangeWarning(value);
+
+        currentValue.value = value > props.max ? props.max : props.min;
+    }
+}
+
+function showAlert(error: string) {
+    if (error) {
+        errorMessage.value = error;
+        dismissCountDown.value = dismissSecs.value;
+    }
+}
+
+function isOutOfRange(value: number) {
+    return isRangeValid.value && (value > props.max || value < props.min);
+}
+
+function showOutOfRangeWarning(value: number) {
+    const warningMessage = `${value} is out of ${props.min} - ${props.max} range!`;
+
+    showAlert(warningMessage);
+}
+
+function resetAlert() {
+    dismissCountDown.value = 0;
+}
+
+/**
+ * https://stackoverflow.com/questions/10454518/javascript-how-to-retrieve-the-number-of-decimals-of-a-string-number
+ */
+function getNumberOfDecimals(value: number | string): number {
+    if (value == null) {
+        return 0;
+    }
+
+    var match = value.toString().match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+
+    if (!match) {
+        return 0;
+    }
+
+    return Math.max(
+        0,
+        // Number of digits right of decimal point.
+        (match[1] ? match[1].length : 0) -
+            // Adjust for scientific notation.
+            (match[2] ? +match[2] : 0)
+    );
+}
+</script>
+
 <template>
     <div>
-        <b-alert v-if="errorMessage" class="mt-2" :show="dismissCountDown" variant="info" @dismissed="resetAlert">
+        <BAlert v-if="errorMessage" class="mt-2" :show="dismissCountDown" variant="info" @dismissed="resetAlert">
             {{ errorMessage }}
-        </b-alert>
-        <b-row align-v="center">
-            <b-col :sm="isRangeValid ? defaultInputSizeWithSlider : false">
+        </BAlert>
+
+        <BRow align-v="center">
+            <BCol :sm="isRangeValid ? defaultInputSizeWithSlider : false">
                 <!-- regular dot and dot on numpad have different codes -->
-                <b-form-input
+                <BFormInput
                     v-model="currentValue"
                     class="ui-input"
                     :no-wheel="true"
@@ -15,143 +147,11 @@
                     @change="onInputChange"
                     @keydown.190.capture="onFloatInput"
                     @keydown.110.capture="onFloatInput" />
-            </b-col>
-            <b-col v-if="isRangeValid" class="pl-0">
-                <b-form-input v-model="currentValue" class="ui-input" :min="min" :max="max" :step="step" type="range" />
-            </b-col>
-        </b-row>
+            </BCol>
+
+            <BCol v-if="isRangeValid" class="pl-0">
+                <BFormInput v-model="currentValue" class="ui-input" :min="min" :max="max" :step="step" type="range" />
+            </BCol>
+        </BRow>
     </div>
 </template>
-
-<script>
-export default {
-    props: {
-        value: {
-            required: true,
-        },
-        type: {
-            type: String,
-            required: true,
-            validator: (prop) => ["integer", "float"].includes(prop.toLowerCase()),
-        },
-        min: {
-            type: [Number, String],
-            required: false,
-            default: undefined,
-        },
-        max: {
-            type: [Number, String],
-            required: false,
-            default: undefined,
-        },
-        workflowBuildingMode: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    data() {
-        return {
-            defaultInputSizeWithSlider: 4,
-            dismissSecs: 5,
-            dismissCountDown: 0,
-            errorMessage: "",
-            fractionWarning: "This output doesn't allow fractions!",
-            decimalPlaces: this.type.toLowerCase() === "integer" ? 0 : this.getNumberOfDecimals(this.value),
-        };
-    },
-    computed: {
-        currentValue: {
-            get() {
-                return this.value;
-            },
-            set(newVal, oldVal) {
-                if (newVal !== oldVal) {
-                    this.$emit("input", newVal);
-                }
-            },
-        },
-        fieldType() {
-            return this.workflowBuildingMode ? "text" : "number";
-        },
-        isRangeValid() {
-            return !isNaN(this.min) && !isNaN(this.max) && this.max > this.min;
-        },
-        isInteger() {
-            return this.type.toLowerCase() === "integer";
-        },
-        isFloat() {
-            return !this.isInteger;
-        },
-        /**
-         * Dynamically sets the step value depending on the
-         * current value precision when float number.
-         */
-        step() {
-            if (this.isInteger) {
-                return 1;
-            }
-            const numDecimals = this.decimalPlaces;
-            if (numDecimals < 2) {
-                return 0.1;
-            }
-            if (numDecimals < 3) {
-                return 0.01;
-            }
-            return 0.001;
-        },
-    },
-    methods: {
-        onFloatInput(e) {
-            if (this.isInteger) {
-                e.preventDefault();
-                this.showAlert(this.fractionWarning);
-            }
-        },
-        onInputChange(value) {
-            this.resetAlert();
-            if (this.isOutOfRange(value)) {
-                this.showOutOfRangeWarning(value);
-                this.currentValue = value > this.max ? this.max : this.min;
-            }
-            if (this.isFloat) {
-                this.decimalPlaces = this.getNumberOfDecimals(this.currentValue);
-            }
-        },
-        showAlert(error) {
-            if (error) {
-                this.errorMessage = error;
-                this.dismissCountDown = this.dismissSecs;
-            }
-        },
-        isOutOfRange(value) {
-            return this.isRangeValid && (value > this.max || value < this.min);
-        },
-        showOutOfRangeWarning(value) {
-            const warningMessage = `${value} is out of ${this.min} - ${this.max} range!`;
-            this.showAlert(warningMessage);
-        },
-        resetAlert() {
-            this.dismissCountDown = 0;
-        },
-        /**
-         * https://stackoverflow.com/questions/10454518/javascript-how-to-retrieve-the-number-of-decimals-of-a-string-number
-         */
-        getNumberOfDecimals(value) {
-            if (value == null) {
-                return 0;
-            }
-            var match = value.toString().match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-            if (!match) {
-                return 0;
-            }
-            return Math.max(
-                0,
-                // Number of digits right of decimal point.
-                (match[1] ? match[1].length : 0) -
-                    // Adjust for scientific notation.
-                    (match[2] ? +match[2] : 0)
-            );
-        },
-    },
-};
-</script>
