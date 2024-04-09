5e6cc7991fd1443a8f8c224c38e6e2481605777f
Alireza Heidari <itisalirh@gmail.com>
2024-04-09 13:37:36 +0200

    🛠️: move API endpoint `postJobErrorReport` and `ShowFullJobResponse` type from `DatasetInformation/services` to `api/jobs`

--- client/src/api/jobs.ts
+++ client/src/api/jobs.ts
@@ -9,10 +9,11 @@ export const fetchJobDestinationParams = fetcher.path("/api/jobs/{job_id}/destin
 
 export const jobsFetcher = fetcher.path("/api/jobs").method("get").create();
 
+export type ShowFullJobResponse = components["schemas"]["ShowFullJobResponse"];
 export type JobDetails = components["schemas"]["ShowFullJobResponse"] | components["schemas"]["EncodedJobDetails"];
 export const fetchJobDetails = fetcher.path("/api/jobs/{job_id}").method("get").create();
 
 export type JobInputSummary = components["schemas"]["JobInputSummary"];
 export const fetchJobCommonProblems = fetcher.path("/api/jobs/{job_id}/common_problems").method("get").create();
 
-export const jobsReportError = fetcher.path("/api/jobs/{job_id}/error").method("post").create();
+export const postJobErrorReport = fetcher.path("/api/jobs/{job_id}/error").method("post").create();
--- client/src/components/DatasetInformation/services.ts
+++ client/src/components/DatasetInformation/services.ts
@@ -1,27 +1,7 @@
 import axios from "axios";
 
-import { type components, fetcher } from "@/api/schema";
 import { getAppRoot } from "@/onload/loadConfig";
 import { rethrowSimple } from "@/utils/simple-error";
-
-export type ShowFullJobResponse = components["schemas"]["ShowFullJobResponse"];
-
-type ReportJobErrorPayload = components["schemas"]["ReportJobErrorPayload"];
-const postErrorReport = fetcher.path("/api/jobs/{job_id}/error").method("post").create();
-export async function sendErrorReport(job_id: string, payload: ReportJobErrorPayload) {
-    try {
-        const { data } = await postErrorReport({
-            job_id,
-            dataset_id: payload.dataset_id,
-            email: payload.email,
-            message: payload.message,
-        });
-
-        return data.messages;
-    } catch (e) {
-        rethrowSimple(e);
-    }
-}
 
 export async function setAttributes(datasetId: string, settings: object, operation: string) {
     const payload = {
