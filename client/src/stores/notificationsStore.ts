af2c2ed314265b8ff8c03bd1d202c45c74ec7b34
davelopez <46503462+davelopez@users.noreply.github.com>
2024-04-17 16:19:13 +0200

    Set active check for notifications every 30 seconds

--- client/src/stores/notificationsStore.ts
+++ client/src/stores/notificationsStore.ts
@@ -12,8 +12,8 @@ import { mergeObjectListsById } from "@/utils/utils";
 
 import { useBroadcastsStore } from "./broadcastsStore";
 
-const ACTIVE_POLLING_INTERVAL = 60000; // 1 minute
-const INACTIVE_POLLING_INTERVAL = ACTIVE_POLLING_INTERVAL * 10; // 10 minutes
+const ACTIVE_POLLING_INTERVAL = 30000; // 30 seconds
+const INACTIVE_POLLING_INTERVAL = ACTIVE_POLLING_INTERVAL * 20; // 10 minutes
 
 export const useNotificationsStore = defineStore("notificationsStore", () => {
     const { startWatchingResource: startWatchingNotifications } = useResourceWatcher(getNotificationStatus, {
