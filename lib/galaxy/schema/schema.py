60a5bbf1ea5b7b71a61f3994ed265f791a0d11b0
heisner-tillman <theisner@posteo.de>
2024-04-18 17:10:54 +0200

    Add an optional tool version field to the JobBaseModel

--- lib/galaxy/schema/schema.py
+++ lib/galaxy/schema/schema.py
@@ -1949,6 +1949,10 @@ class JobBaseModel(Model, WithModelClass):
         title="Tool ID",
         description="Identifier of the tool that generated this job.",
     )
+    tool_version: Optional[str] = Field(
+        None,
+        description="Version of the tool that generated this job.",
+    )
     state: JobState = Field(
         ...,
         title="State",
