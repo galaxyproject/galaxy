f93bf55fd461fe4595bcb24d076942cceb6626c7
guerler <aysam.guerler@gmail.com>
2024-04-18 09:39:14 +0300

    Add visualization frame container

---
+++ client/src/components/Visualizations/VisualizationFrame.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { withPrefix } from "@/utils/redirect";
+
+export interface Props {
+    visualizationId: string;
+    datasetId: string;
+}
+
+const props = defineProps<Props>();
+
+const srcWithRoot = computed(() => {
+    if (props.visualizationId === "trackster") {
+        return withPrefix(`/visualization/${props.visualizationId}?id=${props.datasetId}`);
+    } else {
+        return withPrefix(`/plugins/visualizations/${props.visualizationId}/saved?id=${props.datasetId}`);
+    }
+});
+</script>
+
+<template>
+    <iframe
+        :src="srcWithRoot"
+        class="center-frame"
+        frameborder="0"
+        title="galaxy visualization frame"
+        width="100%"
+        height="100%" />
+</template>
