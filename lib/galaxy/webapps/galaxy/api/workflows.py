a81a69d92251adaccf5c7a41322cfaf6e14d5f2b
heisner-tillman <theisner@posteo.de>
2024-05-07 17:17:22 +0200

    Remove unused imports

--- lib/galaxy/webapps/galaxy/api/workflows.py
+++ lib/galaxy/webapps/galaxy/api/workflows.py
@@ -92,13 +92,7 @@ from galaxy.tool_shed.galaxy_install.install_manager import InstallRepositoryMan
 from galaxy.tools import recommendations
 from galaxy.tools.parameters import populate_state
 from galaxy.tools.parameters.workflow_utils import workflow_building_modes
-from galaxy.util.sanitize_html import sanitize_html
-from galaxy.version import VERSION
-from galaxy.web import (
-    expose_api,
-    expose_api_raw_anonymous_and_sessionless,
-    format_return_as_json,
-)
+from galaxy.web import expose_api
 from galaxy.webapps.base.controller import (
     SharableMixin,
     url_for,
