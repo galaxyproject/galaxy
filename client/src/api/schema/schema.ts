54e256041f8bc0eb098f55f875f73ad1cea5e511
Arash <arash77.kad@gmail.com>
2024-05-07 11:49:33 +0200

    Fix type annotation in ValidationErrorModel

--- client/src/api/schema/schema.ts
+++ client/src/api/schema/schema.ts
@@ -12631,7 +12631,7 @@ export interface components {
             /** Input */
             input: Record<string, never>;
             /** Loc */
-            loc: (number | string)[];
+            loc: number[];
             /** Msg */
             msg: string;
             /** Type */
