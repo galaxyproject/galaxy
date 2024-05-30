c0de4962a38bc51c957f707b752ca7320df1b87a
guerler <aysam.guerler@gmail.com>
2024-05-31 00:56:23 +0300

    Adjust quota meter test case

--- client/src/components/Masthead/QuotaMeter.test.ts
+++ client/src/components/Masthead/QuotaMeter.test.ts
@@ -17,7 +17,6 @@ async function createQuotaMeterWrapper(config: any, userData: any) {
     const pinia = createTestingPinia();
     const userStore = useUserStore();
     userStore.currentUser = { ...userStore.currentUser, ...userData };
-
     const wrapper = mount(QuotaMeter, {
         localVue,
         pinia,
@@ -32,51 +31,34 @@ describe("QuotaMeter.vue", () => {
             total_disk_usage: 5120,
             quota_percent: 50,
         };
-
         const config = { enable_quotas: true };
         const wrapper = await createQuotaMeterWrapper(config, user);
-
         expect(wrapper.find(".quota-progress > span").text()).toBe("Using 50%");
     });
 
     it("changes appearance depending on usage", async () => {
         const config = { enable_quotas: true };
-
         {
             const user = { quota_percent: 30 };
             const wrapper = await createQuotaMeterWrapper(config, user);
-
             expect(wrapper.find(".quota-progress .progress-bar").classes()).toContain("bg-success");
         }
-
         {
             const user = { quota_percent: 80 };
             const wrapper = await createQuotaMeterWrapper(config, user);
-
             expect(wrapper.find(".quota-progress .progress-bar").classes()).toContain("bg-warning");
         }
-
         {
             const user = { quota_percent: 95 };
             const wrapper = await createQuotaMeterWrapper(config, user);
-
             expect(wrapper.find(".quota-progress .progress-bar").classes()).toContain("bg-danger");
         }
     });
 
-    it("prompts user to log in", async () => {
+    it("displays tooltip", async () => {
         const config = { enable_quotas: true };
         const wrapper = await createQuotaMeterWrapper(config, {});
-
-        expect(wrapper.find(".quota-meter > a").attributes("title")).toContain("Log in");
-    });
-
-    it("shows total usage in title", async () => {
-        const user = { total_disk_usage: 9216 };
-        const config = { enable_quotas: true };
-        const wrapper = await createQuotaMeterWrapper(config, user);
-
-        expect(wrapper.find(".quota-progress").attributes("title")).toContain("9 KB");
+        expect(wrapper.attributes("title")).toContain("Storage");
     });
 
     it("shows total usage when there is no quota", async () => {
@@ -84,22 +66,17 @@ describe("QuotaMeter.vue", () => {
             const user = { total_disk_usage: 7168 };
             const config = { enable_quotas: false };
             const wrapper = await createQuotaMeterWrapper(config, user);
-
-            expect(wrapper.find(".quota-text > a").text()).toBe("Using 7 KB");
-            expect(wrapper.find(".quota-text > a").attributes("title")).not.toContain("7 KB");
+            console.log(wrapper.html());
+            expect(wrapper.find("span").text()).toBe("Using 7 KB");
         }
-
         {
             const user = {
                 total_disk_usage: 21504,
                 quota: "unlimited",
             };
-
             const config = { enable_quotas: true };
             const wrapper = await createQuotaMeterWrapper(config, user);
-
-            expect(wrapper.find(".quota-text > a").text()).toBe("Using 21 KB");
-            expect(wrapper.find(".quota-text > a").attributes("title")).not.toContain("21 KB");
+            expect(wrapper.find("span").text()).toBe("Using 21 KB");
         }
     });
 });
