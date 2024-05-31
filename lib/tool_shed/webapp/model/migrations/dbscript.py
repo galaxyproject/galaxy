579884db75d14b2ae7cbfb67270b5d015c157b80
John Davis <jdavcs@gmail.com>
2024-05-31 12:25:40 -0400

    Add TS db revision tags for 23.2

--- lib/tool_shed/webapp/model/migrations/dbscript.py
+++ lib/tool_shed/webapp/model/migrations/dbscript.py
@@ -28,6 +28,8 @@ CONFIG_FILE_ARG = "--toolshed-config"
 REVISION_TAGS = {
     "release_23.1": "1b5bf427db25",
     "23.1": "1b5bf427db25",
+    "release_23.2": "1b5bf427db25",
+    "23.2": "1b5bf427db25",
 }
 
 
