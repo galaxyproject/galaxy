0f13941fab3b78217d42d5b4a4b2a01e7e0b13bc
heisner-tillman <theisner@posteo.de>
2024-06-04 17:48:02 +0200

    Specify valid style types for query param of workflow_dict operation

--- lib/galaxy/webapps/galaxy/api/workflows.py
+++ lib/galaxy/webapps/galaxy/api/workflows.py
@@ -10,6 +10,7 @@ from typing import (
     Any,
     Dict,
     List,
+    Literal,
     Optional,
     Union,
 )
@@ -797,7 +798,9 @@ SkipStepCountsQueryParam: bool = Query(
 )
 
 StyleQueryParam = Annotated[
-    Optional[str],
+    Optional[
+        Literal["export", "format2", "editor", "legacy", "instance", "run", "preview", "format2_wrapped_yaml", "ga"]
+    ],
     Query(
         title="Style of export",
         description="The default is 'export', which is meant to be used with workflow import endpoints. Other formats such as 'instance', 'editor', 'run' are tied to the GUI and should not be considered stable APIs. The default format for 'export' is specified by the admin with the `default_workflow_export_format` config option. Style can be specified as either 'ga' or 'format2' directly to be explicit about which format to download.",
