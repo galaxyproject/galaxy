galaxyproject/galaxy

commit 6682ca60fe64087a15d3e69c68a2054ca950d538

Allow html in input field error text, used for batch mode fields showing bold dataset counts

client/src/components/Form/FormElement.vue
@@ -2,7 +2,7 @@
     <div v-show="!hidden" :id="elementId" :class="['ui-form-element section-row', cls]" :tour_id="id">
         <div v-if="hasError" class="ui-form-error">
             <span class="fa fa-exclamation mr-1" />
-            <span class="ui-form-error-text">{{ error }}</span>
+            <span class="ui-form-error-text" v-html="error" />
         </div>
         <div class="ui-form-title">
             <div v-if="collapsible || connectable">

commit 4f5d81325eb817cbe865077222d64e82a568bab4

Sanitize FormElement error messages
v-html was introduced in https://github.com/galaxyproject/galaxy/commit/6682ca60fe64087a15d3e69c68a2054ca950d538
to show bold items. `FormElement` however is used so widely that it's
hard to keep track on whether or not user-modifiable fields are shown,
so better safe than sorry.

client/src/components/Form/FormElement.vue
@@ -3,8 +3,11 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCaretSquareDown, faCaretSquareUp } from "@fortawesome/free-regular-svg-icons";
 import { faArrowsAltH, faExclamation, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { sanitize } from "dompurify";
 import type { ComputedRef } from "vue";
 import { computed, ref, useAttrs } from "vue";
+
+import { linkify } from "@/utils/utils";
 
 import type { FormParameterAttributes, FormParameterTypes, FormParameterValue } from "./parameterTypes";
 
@@ -181,7 +184,9 @@ const isOptional = computed(() => !isRequired.value && attrs.value["optional"] !
         :class="{ alert: hasAlert, 'alert-info': hasAlert }">
         <div v-if="hasAlert" class="ui-form-error">
             <FontAwesomeIcon class="mr-1" icon="fa-exclamation" />
-            <span class="ui-form-error-text" v-html="props.error || props.warning" />
+            <span
+                class="ui-form-error-text"
+                v-html="linkify(sanitize(props.error || props.warning, { USE_PROFILES: { html: true } }))" />
         </div>
 
         <div class="ui-form-title">