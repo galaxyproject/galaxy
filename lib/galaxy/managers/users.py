e69d7b2f8b32369d754aea336102f5dfc2254f1c
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2024-06-28 08:01:25 -0400

    Prevent deleted users from reseting password

--- lib/galaxy/managers/users.py
+++ lib/galaxy/managers/users.py
@@ -613,6 +613,8 @@ class UserManager(base.ModelManager, deletable.PurgableManagerMixin):
 
     def get_reset_token(self, trans, email):
         reset_user = get_user_by_email(trans.sa_session, email, self.app.model.User)
+        if reset_user.deleted:
+            return None, None
         if not reset_user and email != email.lower():
             reset_user = self._get_user_by_email_case_insensitive(trans.sa_session, email)
         if reset_user:
