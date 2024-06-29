dc72eadab00536e35014a1e8656df952f2a842cb
guerler <aysam.guerler@gmail.com>
2024-06-29 14:03:20 +0200

    Add modal confirmation before restoring ab defaults

--- client/src/components/Panels/SettingsPanel.vue
+++ client/src/components/Panels/SettingsPanel.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import { faUndo } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { BButton, BModal } from "bootstrap-vue";
 import { ref } from "vue";
+
+import { useActivityStore } from "@/stores/activityStore";
 
 import ActivitySettings from "@/components/ActivityBar/ActivitySettings.vue";
 import DelayedInput from "@/components/Common/DelayedInput.vue";
 import ActivityPanel from "@/components/Panels/ActivityPanel.vue";
 
 const query = ref("");
+const confirmRestore = ref(false);
+const activityStore = useActivityStore();
 
 function onQuery(newQuery: string) {
     query.value = newQuery;
@@ -17,6 +24,24 @@ function onQuery(newQuery: string) {
         <template v-slot:header>
             <DelayedInput :delay="100" placeholder="Search activities" @change="onQuery" />
         </template>
+        <template v-slot:header-buttons>
+            <BButton
+                v-b-tooltip.bottom.hover
+                data-description="restore factory settings"
+                size="sm"
+                variant="link"
+                title="Restore default"
+                @click="confirmRestore = true">
+                <FontAwesomeIcon :icon="faUndo" fixed-width />
+            </BButton>
+        </template>
         <ActivitySettings :query="query" />
+        <BModal
+            v-model="confirmRestore"
+            title="Restore Activity Bar Defaults"
+            title-tag="h2"
+            @ok="activityStore.restore()">
+            <p v-localize>Are you sure you want to reset the activity bar to its default settings?</p>
+        </BModal>
     </ActivityPanel>
 </template>
