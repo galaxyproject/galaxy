d43d18f9239b620c6d15033fd88ba4c231e24907
Matthias Bernt <m.bernt@ufz.de>
2024-07-10 15:58:25 +0200

    fix mulled v2 hash generation from strings
    the build information that may be contained in requirement versions is considered
    as part of the version in some places of the code
    
    - mulled container resolvers
    - mulled-build-tool
    - [planemo](https://github.com/galaxyproject/planemo/blob/b3e12a334e3d358ce30bf2c8c3f8d5a7a7e08136/planemo/conda.py#L116)
    
    in others it isn't:
    
    - mulled-hash
    - mulled-build-files
    - mulled_build
    
    which are the files that use the `target_str_to_targets` function.
    
    With this change the build info in version strings is always considered
    as part of the version.
    
    `mulled-build-files` is used in the multi-package-containers repo which
    led to wrong hashes in up to approx 50 tools (check with `grep "=[^,]*=" combinations/* | wc -l`).

--- lib/galaxy/tool_util/deps/mulled/mulled_build_tool.py
+++ lib/galaxy/tool_util/deps/mulled/mulled_build_tool.py
@@ -27,6 +27,23 @@ if TYPE_CHECKING:
     from galaxy.tool_util.deps.conda_util import CondaTarget
 
 
+def _mulled_build_tool(tool, args):
+    """
+    >>> import doctest
+    >>> doctest.ELLIPSIS_MARKER = '-ignore-'
+    >>> import argparse
+    >>> _mulled_build_tool("test/functional/tools/mulled_example_multi_1.xml", argparse.Namespace(dry_run=True, base_image="does-not-matter-here-but-test-is-fast", command="build", verbose=True, involucro_path="./involucro"))  # doctest: +ELLIPSIS
+    -ignore- REPO=quay.io/biocontainers/mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:a6419f25efff953fc505dbd5ee734856180bb619-0 -ignore-
+    >>> _mulled_build_tool("test/functional/tools/mulled_example_multi_2.xml", argparse.Namespace(dry_run=True, base_image="does-not-matter-here-but-test-is-fast", command="build", verbose=True, involucro_path="./involucro"))  # doctest: +ELLIPSIS
+    -ignore- REPO=quay.io/biocontainers/mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:8e86df67d257ce6494ae12b2c60e1b94025ea529-0 -ignore-
+    """ 
+    tool_source = get_tool_source(tool)
+    requirements, *_ = tool_source.parse_requirements_and_containers()
+    targets = requirements_to_mulled_targets(requirements)
+    kwds = args_to_mull_targets_kwds(args)
+    mull_targets(targets, **kwds)
+
+
 def main(argv=None) -> None:
     """Main entry-point for the CLI tool."""
     parser = arg_parser(argv, globals())
@@ -35,12 +52,7 @@ def main(argv=None) -> None:
     parser.add_argument("command", metavar="COMMAND", help="Command (build-and-test, build, all)")
     parser.add_argument("tool", metavar="TOOL", default=None, help="Path to tool to build mulled image for.")
     args = parser.parse_args()
-    tool_source = get_tool_source(args.tool)
-    requirements, *_ = tool_source.parse_requirements_and_containers()
-    targets = requirements_to_mulled_targets(requirements)
-    kwds = args_to_mull_targets_kwds(args)
-    mull_targets(targets, **kwds)
-
+    _mulled_build_tool(args.tool, args)
 
 def requirements_to_mulled_targets(requirements) -> List["CondaTarget"]:
     """Convert Galaxy's representation of requirements into a list of CondaTarget objects.
