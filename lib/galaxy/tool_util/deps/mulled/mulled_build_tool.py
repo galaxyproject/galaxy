1ff21ee5a9690a218d6033e2340ff732c22cd258
Matthias Bernt <m.bernt@ufz.de>
2024-07-11 09:38:32 +0200

    fix linter errors

--- lib/galaxy/tool_util/deps/mulled/mulled_build_tool.py
+++ lib/galaxy/tool_util/deps/mulled/mulled_build_tool.py
@@ -36,7 +36,7 @@ def _mulled_build_tool(tool, args):
     -ignore- REPO=quay.io/biocontainers/mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:a6419f25efff953fc505dbd5ee734856180bb619-0 -ignore-
     >>> _mulled_build_tool("test/functional/tools/mulled_example_multi_2.xml", argparse.Namespace(dry_run=True, base_image="does-not-matter-here-but-test-is-fast", command="build", verbose=True, involucro_path="./involucro"))  # doctest: +ELLIPSIS
     -ignore- REPO=quay.io/biocontainers/mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:8e86df67d257ce6494ae12b2c60e1b94025ea529-0 -ignore-
-    """ 
+    """
     tool_source = get_tool_source(tool)
     requirements, *_ = tool_source.parse_requirements_and_containers()
     targets = requirements_to_mulled_targets(requirements)
@@ -53,6 +53,7 @@ def main(argv=None) -> None:
     parser.add_argument("tool", metavar="TOOL", default=None, help="Path to tool to build mulled image for.")
     args = parser.parse_args()
     _mulled_build_tool(args.tool, args)
+
 
 def requirements_to_mulled_targets(requirements) -> List["CondaTarget"]:
     """Convert Galaxy's representation of requirements into a list of CondaTarget objects.
