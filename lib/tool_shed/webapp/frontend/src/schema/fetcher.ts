6361fac7c2feead0d7ed90c2dfdb1a63590c5bb0
davelopez <46503462+davelopez@users.noreply.github.com>
2024-07-12 14:10:49 +0200

    Add middleware for error handling in Toolshed API client

--- lib/tool_shed/webapp/frontend/src/schema/fetcher.ts
+++ lib/tool_shed/webapp/frontend/src/schema/fetcher.ts
@@ -1,5 +1,20 @@
-import createClient from "openapi-fetch"
+import createClient, { Middleware } from "openapi-fetch"
 import type { paths as ToolShedApiPaths } from "./schema"
 
+const errorHandlingMiddleware: Middleware = {
+    async onResponse({ response }) {
+        if (!response.ok) {
+            const errorMessage = response.headers.get("content-type")?.includes("json")
+                ? await response.clone().json()
+                : await response.clone().text()
+            throw new Error(`Request failed with status ${response.status}`, {
+                cause: errorMessage,
+            })
+        }
+    },
+}
+
 const client = createClient<ToolShedApiPaths>({ baseUrl: "" })
+client.use(errorHandlingMiddleware)
+
 export { type ToolShedApiPaths, client }
