f1d63d77f6cda5ff4b8951a498c72ff2b28df065
davelopez <46503462+davelopez@users.noreply.github.com>
2024-07-17 10:55:16 +0200

    Refactor use openapi-fetch in some invocation related components

--- client/src/api/schema/index.ts
+++ client/src/api/schema/index.ts
@@ -1,2 +1,3 @@
 export { type ApiResponse, fetcher } from "./fetcher";
+export { client, GalaxyApiPaths } from "./fetcher";
 export type { components, operations, paths } from "./schema";
--- client/src/api/workflows.ts
+++ client/src/api/workflows.ts
@@ -2,7 +2,6 @@ import { type components, fetcher } from "@/api/schema";
 
 export type StoredWorkflowDetailed = components["schemas"]["StoredWorkflowDetailed"];
 
-export const workflowsFetcher = fetcher.path("/api/workflows").method("get").create();
 export const workflowFetcher = fetcher.path("/api/workflows/{workflow_id}").method("get").create();
 
 export const invocationCountsFetcher = fetcher.path("/api/workflows/{workflow_id}/counts").method("get").create();
