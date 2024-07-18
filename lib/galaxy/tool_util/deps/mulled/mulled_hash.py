ccda2bed0cd21cd55fce9588c90e103b9685014b
Matthias Bernt <m.bernt@ufz.de>
2024-07-18 14:30:22 +0200

    Revert "fix mulled v2 hash generation from strings"
    This reverts commit e7bca837f08345980f240f633921dbcce174345e.

--- lib/galaxy/tool_util/deps/mulled/mulled_hash.py
+++ lib/galaxy/tool_util/deps/mulled/mulled_hash.py
@@ -16,29 +16,17 @@ from .util import (
 )
 
 
-def _mulled_hash(hash, targets):
-    """
-    >>> _mulled_hash(hash="v2", targets="samtools=1.3.1,bedtools=2.26.0")
-    'mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:a6419f25efff953fc505dbd5ee734856180bb619'
-    >>> _mulled_hash(hash="v2", targets="samtools=1.3.1=h9071d68_10,bedtools=2.26.0=0")
-    'mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:8e86df67d257ce6494ae12b2c60e1b94025ea529'
-    """
-    targets = target_str_to_targets(targets)
-    image_name = v2_image_name if hash == "v2" else v1_image_name
-    return image_name(targets)
-
-
 def main(argv=None):
-    """
-    Main entry-point for the CLI tool.
-    """
+    """Main entry-point for the CLI tool."""
     parser = arg_parser(argv, globals())
     parser.add_argument(
         "targets", metavar="TARGETS", default=None, help="Comma-separated packages for calculating the mulled hash."
     )
     parser.add_argument("--hash", dest="hash", choices=["v1", "v2"], default="v2")
     args = parser.parse_args()
-    print(_mulled_hash(args.hash, args.targets))
+    targets = target_str_to_targets(args.targets)
+    image_name = v2_image_name if args.hash == "v2" else v1_image_name
+    print(image_name(targets))
 
 
 __all__ = ("main",)
