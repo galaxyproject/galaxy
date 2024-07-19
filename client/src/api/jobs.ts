721391fee23e6773c4c2cc66a8b38b9150b9bf53
davelopez <46503462+davelopez@users.noreply.github.com>
2024-07-19 16:53:35 +0200

    Replace fetcher in DatasetError and DatasetDetails

--- client/src/api/jobs.ts
+++ client/src/api/jobs.ts
@@ -1,11 +1,6 @@
-import { type components, fetcher } from "@/api/schema";
+import { type components } from "@/api/schema";
 
 export type JobDestinationParams = components["schemas"]["JobDestinationParams"];
 export type ShowFullJobResponse = components["schemas"]["ShowFullJobResponse"];
 export type JobDetails = components["schemas"]["ShowFullJobResponse"] | components["schemas"]["EncodedJobDetails"];
-export const fetchJobDetails = fetcher.path("/api/jobs/{job_id}").method("get").create();
-
 export type JobInputSummary = components["schemas"]["JobInputSummary"];
-export const fetchJobCommonProblems = fetcher.path("/api/jobs/{job_id}/common_problems").method("get").create();
-
-export const postJobErrorReport = fetcher.path("/api/jobs/{job_id}/error").method("post").create();
