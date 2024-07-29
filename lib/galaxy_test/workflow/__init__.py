419f538adb6225b528a59c4cb3cc2d91be531384
John Chilton <jmchilton@gmail.com>
2024-07-29 11:11:32 -0400

    Update lib/galaxy_test/workflow/__init__.py
    Co-authored-by: Marius van den Beek <m.vandenbeek@gmail.com>

--- lib/galaxy_test/workflow/__init__.py
+++ lib/galaxy_test/workflow/__init__.py
@@ -3,5 +3,5 @@
 This is meant to grow into the workflow based mirror of what the framework tests are for tools.
 api/test_workflows.py is still the place to test exceptional conditions, errors, etc... but tests
 of normal operation where semantics can be verified with simple inputs and outputs can now be
-places in here.
+placed in here.
 """
