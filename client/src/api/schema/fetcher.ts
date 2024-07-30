5df52c2ba5201d7ef5cdc55adf7abe4ac63e167d
davelopez <46503462+davelopez@users.noreply.github.com>
2024-07-30 15:59:02 +0200

    Simplify client api exports

--- client/src/api/schema/fetcher.ts
+++
@@ -1,9 +0,0 @@
-import createClient from "openapi-fetch";
-
-import { getAppRoot } from "@/onload/loadConfig";
-
-import { type paths as GalaxyApiPaths } from "./schema";
-
-export { type GalaxyApiPaths };
-
-export const client = createClient<GalaxyApiPaths>({ baseUrl: getAppRoot(undefined, true) });
