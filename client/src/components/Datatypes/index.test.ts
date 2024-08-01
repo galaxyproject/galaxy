b35cf2e4a7346f4151e6442bcc21ea8c8d05f418
davelopez <46503462+davelopez@users.noreply.github.com>
2024-08-01 14:31:08 +0200

    Refactor Datatypes/index.test.js to use TypeScript + new client mock

--- client/src/components/Datatypes/index.test.ts
+++ client/src/components/Datatypes/index.test.ts
@@ -1,10 +1,17 @@
+import { useClientApiMock } from "@/api/client/__mocks__";
+
 import { getDatatypesMapper } from "./index";
 import { typesAndMappingResponse } from "./test_fixtures";
+
+const { clientMock } = useClientApiMock();
+
+clientMock.get("/api/datatypes/types_and_mapping", ({ response }) => {
+    return response(200).json(typesAndMappingResponse);
+});
 
 describe("Datatypes/index.js", () => {
     describe("getDatatypesMapper", () => {
         it("should fetch logic from API for comparing datatypes in a hierarchy", async () => {
-            fetchDatatypesAndMappings.mockResolvedValue(typesAndMappingResponse);
             await getDatatypesMapper().then((mapper) => {
                 expect(mapper.isSubType("txt", "data")).toBe(true);
                 expect(mapper.isSubType("txt", "txt")).toBe(true);
