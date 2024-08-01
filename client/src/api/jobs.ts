197306d8913d4ca5ce1d8a2edcaa9d0cdcc76142
Ahmed Awan <qe66653@umbc.edu>
2024-08-01 10:59:47 -0500

    add a tab view for jobs under a step in invocation view

--- client/src/api/jobs.ts
+++ client/src/api/jobs.ts
@@ -2,5 +2,6 @@ import { type components } from "@/api/schema";
 
 export type JobDestinationParams = components["schemas"]["JobDestinationParams"];
 export type ShowFullJobResponse = components["schemas"]["ShowFullJobResponse"];
+export type JobBaseModel = components["schemas"]["JobBaseModel"];
 export type JobDetails = components["schemas"]["ShowFullJobResponse"] | components["schemas"]["EncodedJobDetails"];
 export type JobInputSummary = components["schemas"]["JobInputSummary"];
