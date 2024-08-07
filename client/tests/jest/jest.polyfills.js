290bab3195c8f2a8e27a678aec1085846c7cce2c
davelopez <46503462+davelopez@users.noreply.github.com>
2024-08-07 13:53:51 +0200

    Fix jest fetch polyfill by using whatwg-fetch

--- client/tests/jest/jest.polyfills.js
+++ client/tests/jest/jest.polyfills.js
@@ -25,14 +25,8 @@ Object.defineProperties(globalThis, {
 });
 
 const { Blob, File } = require("node:buffer");
-const { fetch, Headers, FormData, Request, Response } = require("undici");
 
 Object.defineProperties(globalThis, {
-    fetch: { value: fetch, writable: true },
     Blob: { value: Blob },
     File: { value: File },
-    Headers: { value: Headers },
-    FormData: { value: FormData },
-    Request: { value: Request },
-    Response: { value: Response },
 });
