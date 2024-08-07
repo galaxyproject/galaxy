a2685e1df815f3bb5702c7da850696074eb8bf7a
Dannon Baker <dannon.baker@gmail.com>
2024-08-07 08:25:27 -0400

    Drop performance polyfill, no longer necessary

--- client/tests/jest/jest.polyfills.js
+++ client/tests/jest/jest.polyfills.js
@@ -12,14 +12,12 @@
 
 // https://mswjs.io/docs/migrations/1.x-to-2.x#frequent-issues
 
-const { performance } = require("node:perf_hooks");
 const { TextDecoder, TextEncoder, ReadableStream } = require("node:util");
 const { clearImmediate } = require("node:timers");
 
 Object.defineProperties(globalThis, {
     TextDecoder: { value: TextDecoder },
     TextEncoder: { value: TextEncoder },
-    performance: { value: performance },
     ReadableStream: { value: ReadableStream },
     clearImmediate: { value: clearImmediate },
 });
