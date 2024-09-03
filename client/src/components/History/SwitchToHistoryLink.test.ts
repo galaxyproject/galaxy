987bbf9be58fa69669251b06452cc56b2d07b285
Ahmed Awan <qe66653@umbc.edu>
2024-09-03 11:59:22 -0500

    [24.1] Add error handling for `SwitchToHistoryLink`
    Yet another case where errors were not being handled; in this case if the user clicks on the `SwitchToHistoryLink` when the history isn't shared/importable, we now `Toast` an error message.
    
    Error currently reproducable if you click on the history name in this invocation: https://usegalaxy.org/workflows/invocations/a11d3f54288f2b34?from_panel=true

--- client/src/components/History/SwitchToHistoryLink.test.ts
+++ client/src/components/History/SwitchToHistoryLink.test.ts
@@ -1,27 +1,45 @@
 import { createTestingPinia } from "@pinia/testing";
 import { getLocalVue } from "@tests/jest/helpers";
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 import flushPromises from "flush-promises";
 
-import { type HistorySummary } from "@/api";
+import { type HistorySummaryExtended } from "@/api";
+import { Toast } from "@/composables/toast";
+import { useUserStore } from "@/stores/userStore";
 
 import SwitchToHistoryLink from "./SwitchToHistoryLink.vue";
+
+jest.mock("@/composables/toast", () => ({
+    Toast: {
+        error: jest.fn(),
+    },
+}));
+jest.mock("vue-router/composables", () => ({
+    useRouter: jest.fn(() => ({
+        resolve: jest.fn((route) => ({
+            href: route,
+        })),
+    })),
+}));
 
 const localVue = getLocalVue(true);
 
 const selectors = {
     historyLink: ".history-link",
+    historyLinkClick: ".history-link > a",
 } as const;
 
-function mountSwitchToHistoryLinkForHistory(history: HistorySummary) {
+function mountSwitchToHistoryLinkForHistory(history: HistorySummaryExtended) {
+    const windowSpy = jest.spyOn(window, "open");
+    windowSpy.mockImplementation(() => null);
     const pinia = createTestingPinia();
 
     const axiosMock = new MockAdapter(axios);
     axiosMock.onGet(`/api/histories/${history.id}`).reply(200, history);
 
-    const wrapper = shallowMount(SwitchToHistoryLink as object, {
+    const wrapper = mount(SwitchToHistoryLink as object, {
         propsData: {
             historyId: history.id,
         },
@@ -31,10 +49,19 @@ function mountSwitchToHistoryLinkForHistory(history: HistorySummary) {
             FontAwesomeIcon: true,
         },
     });
+
+    const userStore = useUserStore();
+    userStore.currentUser = {
+        email: "email",
+        id: "user_id",
+        tags_used: [],
+        isAnonymous: false,
+        total_disk_usage: 0,
+    };
     return wrapper;
 }
 
-async function expectOptionForHistory(option: string, history: HistorySummary) {
+async function expectOptionForHistory(option: string, history: HistorySummaryExtended) {
     const wrapper = mountSwitchToHistoryLinkForHistory(history);
 
     // Wait for the history to be loaded
@@ -42,6 +69,8 @@ async function expectOptionForHistory(option: string, history: HistorySummary) {
 
     expect(wrapper.html()).toContain(option);
     expect(wrapper.text()).toContain(history.name);
+
+    return wrapper;
 }
 
 describe("SwitchToHistoryLink", () => {
@@ -52,7 +81,7 @@ describe("SwitchToHistoryLink", () => {
             deleted: false,
             archived: false,
             purged: false,
-        } as HistorySummary;
+        } as HistorySummaryExtended;
         const wrapper = mountSwitchToHistoryLinkForHistory(history);
 
         expect(wrapper.find(selectors.historyLink).exists()).toBe(false);
@@ -72,7 +101,8 @@ describe("SwitchToHistoryLink", () => {
             deleted: false,
             purged: false,
             archived: false,
-        } as HistorySummary;
+            user_id: "user_id",
+        } as HistorySummaryExtended;
         await expectOptionForHistory("Switch", history);
     });
 
@@ -83,7 +113,8 @@ describe("SwitchToHistoryLink", () => {
             deleted: false,
             purged: true,
             archived: false,
-        } as HistorySummary;
+            user_id: "user_id",
+        } as HistorySummaryExtended;
         await expectOptionForHistory("View", history);
     });
 
@@ -94,7 +125,56 @@ describe("SwitchToHistoryLink", () => {
             deleted: false,
             purged: false,
             archived: true,
-        } as HistorySummary;
+            user_id: "user_id",
+        } as HistorySummaryExtended;
         await expectOptionForHistory("View", history);
+    });
+
+    it("should view in new tab when the history is unowned and published", async () => {
+        const history = {
+            id: "unowned-history-id",
+            name: "History Published",
+            deleted: false,
+            purged: false,
+            archived: false,
+            published: true,
+            user_id: "other_user_id",
+        } as HistorySummaryExtended;
+        const wrapper = await expectOptionForHistory("View", history);
+
+        // Wait for the history to be loaded
+        await flushPromises();
+
+        // Click on the history link
+        const link = wrapper.find(selectors.historyLinkClick);
+        await link.trigger("click");
+        await wrapper.vm.$nextTick();
+
+        // History opens in a new tab
+        expect(Toast.error).toHaveBeenCalledTimes(0);
+    });
+
+    it("should display View option and show error on click when history is unowned and not published", async () => {
+        const history = {
+            id: "published-history-id",
+            name: "History Unowned",
+            deleted: false,
+            purged: false,
+            archived: false,
+            user_id: "other_user_id",
+        } as HistorySummaryExtended;
+        const wrapper = await expectOptionForHistory("View", history);
+
+        // Wait for the history to be loaded
+        await flushPromises();
+
+        // Click on the history link
+        const link = wrapper.find(selectors.historyLinkClick);
+        await link.trigger("click");
+        await wrapper.vm.$nextTick();
+
+        // History does not open in a new tab and we get a Toast error
+        expect(Toast.error).toHaveBeenCalledTimes(1);
+        expect(Toast.error).toHaveBeenCalledWith("You do not have permission to view this history.");
     });
 });
