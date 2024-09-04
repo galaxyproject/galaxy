3365eaddd39da4b8502d24e1da6b5df6c18cb3ed
guerler <aysam.guerler@gmail.com>
2024-09-04 17:01:42 +0300

    Use innerText in example plugin

--- config/plugins/visualizations/example/static/script.js
+++ config/plugins/visualizations/example/static/script.js
@@ -1,4 +1,4 @@
 var element = document.getElementById("app");
 var div = document.createElement('div');
-div.innerHTML = JSON.stringify(JSON.parse(element.getAttribute("data-incoming")));
+div.innerText = JSON.stringify(JSON.parse(element.getAttribute("data-incoming")));
 document.body.appendChild(div);
