a24ac91671bb71c4d84b178af70d29764ac8de71
guerler <aysam.guerler@gmail.com>
2024-09-05 05:36:27 +0300

    Update minimal plugin example

--- config/plugins/visualizations/example/static/script.js
+++ config/plugins/visualizations/example/static/script.js
@@ -1,4 +1,28 @@
-var element = document.getElementById("app");
-var div = document.createElement('div');
-div.innerText = JSON.stringify(JSON.parse(element.getAttribute("data-incoming")));
+const { visualization_plugin: plugin, root } = JSON.parse(document.getElementById("app").dataset.incoming);
+
+const div = Object.assign(document.createElement("div"), {
+    style: "border: 2px solid #25537b; border-radius: 1rem; padding: 1rem"
+});
+
+const img = Object.assign(document.createElement("img"), {
+    src: root + plugin.logo,
+    style: "height: 3rem"
+});
+
+div.appendChild(img);
+
+Object.entries(plugin).forEach(([key, value]) => {
+    const row = document.createElement("div");
+    const spanKey = Object.assign(document.createElement("span"), {
+        innerText: `${key}: `,
+        style: "font-weight: bold"
+    });
+    const spanValue = Object.assign(document.createElement("span"), {
+        innerText: value
+    });
+    row.appendChild(spanKey);
+    row.appendChild(spanValue);
+    div.appendChild(row);
+});
+
 document.body.appendChild(div);
