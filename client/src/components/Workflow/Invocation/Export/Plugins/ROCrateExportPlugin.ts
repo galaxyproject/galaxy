589fcbef65be0183eb80250e625c1ed838fd9ebf
davelopez <46503462+davelopez@users.noreply.github.com>
2024-09-06 13:17:32 +0200

    Add optional image logos to invocation export plugins

--- client/src/components/Workflow/Invocation/Export/Plugins/ROCrateExportPlugin.ts
+++ client/src/components/Workflow/Invocation/Export/Plugins/ROCrateExportPlugin.ts
@@ -2,7 +2,8 @@ import { type InvocationExportPlugin } from "@/components/Workflow/Invocation/Ex
 
 export const RO_CRATE_EXPORT_PLUGIN: InvocationExportPlugin = {
     id: "ro-crate",
-    title: "Research Object Crate (RO-Crate)",
+    title: "Research Object Crate",
+    img: "https://www.researchobject.org/workflow-run-crate/img/ro-crate-workflow-run.svg",
     markdownDescription: `
 RO-Crate is a community effort to establish a lightweight approach to packaging research data with their metadata. It is based on schema.org annotations in JSON-LD, and aims to make best-practice in formal metadata description accessible and practical for use in a wider variety of situations, from an individual researcher working with a folder of data, to large data-intensive computational research environments.
 
