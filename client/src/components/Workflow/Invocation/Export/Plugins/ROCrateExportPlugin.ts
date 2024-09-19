c9bea5afcaf85764083724b4519b23d5fe627db3
davelopez <46503462+davelopez@users.noreply.github.com>
2024-09-19 11:23:13 +0200

    Change RO-Crate URL link to WF Run Crate link

--- client/src/components/Workflow/Invocation/Export/Plugins/ROCrateExportPlugin.ts
+++ client/src/components/Workflow/Invocation/Export/Plugins/ROCrateExportPlugin.ts
@@ -4,11 +4,11 @@ export const RO_CRATE_EXPORT_PLUGIN: InvocationExportPlugin = {
     id: "ro-crate",
     title: "Research Object Crate",
     img: "https://www.researchobject.org/workflow-run-crate/img/ro-crate-workflow-run.svg",
-    url: "https://www.researchobject.org/ro-crate/",
+    url: "https://www.researchobject.org/workflow-run-crate/",
     markdownDescription: `
 RO-Crate is a community effort to establish a lightweight approach to packaging research data with their metadata. It is based on schema.org annotations in JSON-LD, and aims to make best-practice in formal metadata description accessible and practical for use in a wider variety of situations, from an individual researcher working with a folder of data, to large data-intensive computational research environments.
 
-Learn more about [RO Crate](https://www.researchobject.org/ro-crate/).`,
+Learn more about it [here](https://www.researchobject.org/workflow-run-crate/).`,
     exportParams: {
         modelStoreFormat: "rocrate.zip",
         includeFiles: true,
