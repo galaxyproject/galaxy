4471c6a851dfc95c5a55e18cca440c6ad2cee333
John Davis <jdavcs@gmail.com>
2024-09-21 00:17:20 -0400

    Refactor tests to reduce duplication

--- test/unit/data/model/db/__init__.py
+++ test/unit/data/model/db/__init__.py
@@ -3,18 +3,7 @@ from collections import (
     namedtuple,
 )
 
-PRIVATE_OBJECT_STORE_ID = "my_private_data"
-
 MockTransaction = namedtuple("MockTransaction", "user")
-
-
-class MockObjectStore:
-
-    def is_private(self, object):
-        if object.object_store_id == PRIVATE_OBJECT_STORE_ID:
-            return True
-        else:
-            return False
 
 
 def verify_items(items, expected_items):
