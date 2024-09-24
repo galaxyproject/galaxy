5524ea77a7424d0d14e6ce568140adc9a066e904
John Davis <jdavcs@gmail.com>
2024-09-23 23:52:05 -0400

    Add tests for migration fixes

--- test/unit/data/model/migration_fixes/test_migrations.py
+++ test/unit/data/model/migration_fixes/test_migrations.py
@@ -1,6 +1,12 @@
 import pytest
+from sqlalchemy import select
 
-from galaxy.model import User
+from galaxy.model import (
+    GroupRoleAssociation,
+    User,
+    UserGroupAssociation,
+    UserRoleAssociation,
+)
 from galaxy.model.unittest_utils.migration_scripts_testing_utils import (  # noqa: F401 - contains fixtures we have to import explicitly
     run_command,
     tmp_directory,
@@ -152,3 +158,222 @@ def test_d619fdfa6168(monkeypatch, session, make_user):
     assert u1_fixed.deleted is True
     assert u2_fixed.deleted is True
     assert u3_fixed.deleted is False
+
+
+def test_349dd9d9aac9(monkeypatch, session, make_user, make_role, make_user_role_association):
+    # Initialize db and migration environment
+    dburl = str(session.bind.url)
+    monkeypatch.setenv("GALAXY_CONFIG_OVERRIDE_DATABASE_CONNECTION", dburl)
+    monkeypatch.setenv("GALAXY_INSTALL_CONFIG_OVERRIDE_INSTALL_DATABASE_CONNECTION", dburl)
+    run_command(f"{COMMAND} init")
+
+    # Load pre-migration state
+    run_command(f"{COMMAND} downgrade 1cf595475b58")
+
+    # Load duplicate records
+    u1, u2 = make_user(), make_user()
+    r1, r2 = make_role(), make_role()
+    make_user_role_association(user=u1, role=r1)
+    make_user_role_association(user=u1, role=r2)
+    make_user_role_association(user=u1, role=r2)  # duplicate
+    make_user_role_association(user=u2, role=r1)
+    make_user_role_association(user=u2, role=r1)  # duplicate
+
+    # Verify duplicates
+    assert len(u1.roles) == 3
+    assert len(u2.roles) == 2
+    all_associations = session.execute(select(UserRoleAssociation)).all()
+    assert len(all_associations) == 5
+
+    # Run migration
+    run_command(f"{COMMAND} upgrade 349dd9d9aac9")
+    session.expire_all()
+
+    # Verify clean data
+    assert len(u1.roles) == 2
+    assert len(u2.roles) == 1
+    all_associations = session.execute(select(UserRoleAssociation)).all()
+    assert len(all_associations) == 3
+
+
+def test_56ddf316dbd0(monkeypatch, session, make_user, make_group, make_user_group_association):
+    # Initialize db and migration environment
+    dburl = str(session.bind.url)
+    monkeypatch.setenv("GALAXY_CONFIG_OVERRIDE_DATABASE_CONNECTION", dburl)
+    monkeypatch.setenv("GALAXY_INSTALL_CONFIG_OVERRIDE_INSTALL_DATABASE_CONNECTION", dburl)
+    run_command(f"{COMMAND} init")
+
+    # Load pre-migration state
+    run_command(f"{COMMAND} downgrade 1fdd615f2cdb")
+
+    # Load duplicate records
+    u1, u2 = make_user(), make_user()
+    g1, g2 = make_group(), make_group()
+    make_user_group_association(user=u1, group=g1)
+    make_user_group_association(user=u1, group=g2)
+    make_user_group_association(user=u1, group=g2)  # duplicate
+    make_user_group_association(user=u2, group=g1)
+    make_user_group_association(user=u2, group=g1)  # duplicate
+
+    # Verify duplicates
+    assert len(u1.groups) == 3
+    assert len(u2.groups) == 2
+    all_associations = session.execute(select(UserGroupAssociation)).all()
+    assert len(all_associations) == 5
+
+    # Run migration
+    run_command(f"{COMMAND} upgrade 56ddf316dbd0")
+    session.expire_all()
+
+    # Verify clean data
+    assert len(u1.groups) == 2
+    assert len(u2.groups) == 1
+    all_associations = session.execute(select(UserGroupAssociation)).all()
+    assert len(all_associations) == 3
+
+
+def test_9ef6431f3a4e(monkeypatch, session, make_group, make_role, make_group_role_association):
+    # Initialize db and migration environment
+    dburl = str(session.bind.url)
+    monkeypatch.setenv("GALAXY_CONFIG_OVERRIDE_DATABASE_CONNECTION", dburl)
+    monkeypatch.setenv("GALAXY_INSTALL_CONFIG_OVERRIDE_INSTALL_DATABASE_CONNECTION", dburl)
+    run_command(f"{COMMAND} init")
+
+    # Load pre-migration state
+    run_command(f"{COMMAND} downgrade 13fe10b8e35b")
+
+    # Load duplicate records
+    g1, g2 = make_group(), make_group()
+    r1, r2 = make_role(), make_role()
+    make_group_role_association(group=g1, role=r1)
+    make_group_role_association(group=g1, role=r2)
+    make_group_role_association(group=g1, role=r2)  # duplicate
+    make_group_role_association(group=g2, role=r1)
+    make_group_role_association(group=g2, role=r1)  # duplicate
+
+    # Verify duplicates
+    assert len(g1.roles) == 3
+    assert len(g2.roles) == 2
+    all_associations = session.execute(select(GroupRoleAssociation)).all()
+    assert len(all_associations) == 5
+
+    # Run migration
+    run_command(f"{COMMAND} upgrade 9ef6431f3a4e")
+    session.expire_all()
+
+    # Verify clean data
+    assert len(g1.roles) == 2
+    assert len(g2.roles) == 1
+    all_associations = session.execute(select(GroupRoleAssociation)).all()
+    assert len(all_associations) == 3
+
+
+def test_1fdd615f2cdb(monkeypatch, session, make_user, make_role, make_user_role_association):
+    # Initialize db and migration environment
+    dburl = str(session.bind.url)
+    monkeypatch.setenv("GALAXY_CONFIG_OVERRIDE_DATABASE_CONNECTION", dburl)
+    monkeypatch.setenv("GALAXY_INSTALL_CONFIG_OVERRIDE_INSTALL_DATABASE_CONNECTION", dburl)
+    run_command(f"{COMMAND} init")
+
+    # Load pre-migration state
+    run_command(f"{COMMAND} downgrade 349dd9d9aac9")
+
+    # Load records w/nulls
+    ura1 = make_user_role_association(user=make_user(), role=make_role())
+    ura2 = make_user_role_association(user=make_user(), role=make_role())
+    ura3 = make_user_role_association(user=make_user(), role=make_role())
+    ura1.user_id = None
+    ura2.role_id = None
+    ura3.user_id = None
+    ura3.role_id = None
+    session.add_all([ura1, ura2, ura3])
+    session.commit()
+
+    # Load record w/o nulls
+    make_user_role_association(user=make_user(), role=make_role())
+
+    # Verify data
+    all_associations = session.execute(select(UserRoleAssociation)).all()
+    assert len(all_associations) == 4
+
+    # Run migration
+    run_command(f"{COMMAND} upgrade 1fdd615f2cdb")
+    session.expire_all()
+
+    # Verify clean data
+    all_associations = session.execute(select(UserRoleAssociation)).all()
+    assert len(all_associations) == 1
+
+
+def test_13fe10b8e35b(monkeypatch, session, make_user, make_group, make_user_group_association):
+    # Initialize db and migration environment
+    dburl = str(session.bind.url)
+    monkeypatch.setenv("GALAXY_CONFIG_OVERRIDE_DATABASE_CONNECTION", dburl)
+    monkeypatch.setenv("GALAXY_INSTALL_CONFIG_OVERRIDE_INSTALL_DATABASE_CONNECTION", dburl)
+    run_command(f"{COMMAND} init")
+
+    # Load pre-migration state
+    run_command(f"{COMMAND} downgrade 56ddf316dbd0")
+
+    # Load records w/nulls
+    uga1 = make_user_group_association(user=make_user(), group=make_group())
+    uga2 = make_user_group_association(user=make_user(), group=make_group())
+    uga3 = make_user_group_association(user=make_user(), group=make_group())
+    uga1.user_id = None
+    uga2.group_id = None
+    uga3.user_id = None
+    uga3.group_id = None
+    session.add_all([uga1, uga2, uga3])
+    session.commit()
+
+    # Load record w/o nulls
+    make_user_group_association(user=make_user(), group=make_group())
+
+    # Verify data
+    all_associations = session.execute(select(UserGroupAssociation)).all()
+    assert len(all_associations) == 4
+
+    # Run migration
+    run_command(f"{COMMAND} upgrade 13fe10b8e35b")
+    session.expire_all()
+
+    # Verify clean data
+    all_associations = session.execute(select(UserGroupAssociation)).all()
+    assert len(all_associations) == 1
+
+
+def test_25b092f7938b(monkeypatch, session, make_group, make_role, make_group_role_association):
+    # Initialize db and migration environment
+    dburl = str(session.bind.url)
+    monkeypatch.setenv("GALAXY_CONFIG_OVERRIDE_DATABASE_CONNECTION", dburl)
+    monkeypatch.setenv("GALAXY_INSTALL_CONFIG_OVERRIDE_INSTALL_DATABASE_CONNECTION", dburl)
+    run_command(f"{COMMAND} init")
+
+    # Load pre-migration state
+    run_command(f"{COMMAND} downgrade 9ef6431f3a4e")
+
+    # Load records w/nulls
+    gra1 = make_group_role_association(group=make_group(), role=make_role())
+    gra2 = make_group_role_association(group=make_group(), role=make_role())
+    gra3 = make_group_role_association(group=make_group(), role=make_role())
+    gra1.group_id = None
+    gra2.role_id = None
+    gra3.group_id = None
+    gra3.role_id = None
+    session.add_all([gra1, gra2, gra3])
+    session.commit()
+
+    # Load record w/o nulls
+    make_group_role_association(group=make_group(), role=make_role())
+
+    # Verify data
+    all_associations = session.execute(select(GroupRoleAssociation)).all()
+    assert len(all_associations) == 4
+
+    # Run migration
+    run_command(f"{COMMAND} upgrade 25b092f7938b")
+    session.expire_all()
+
+    # Verify clean data
+    all_associations = session.execute(select(GroupRoleAssociation)).all()
+    assert len(all_associations) == 1
