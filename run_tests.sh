178a1efb3048cb24f3af263fea136b96f58d3104
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2024-10-04 11:40:34 +0100

    Remove outdated fimo wrapper and galaxy-sequence-utils dependency
    The fimo tool was removed from `tool_conf.xml.sample` in 2014
    (commit fa11d10c8b490aa2628299930ff470ad3fafb0d9) and migrated to the
    tools-iuc repository since 2015
    ( https://github.com/galaxyproject/tools-iuc/pull/499 ).
    This bundled version doesn't probably work any more and is not
    available on the UseGalaxy.* servers.
    
    This allows us to remove `galaxy-sequence-utils` from the project
    dependencies, since the other 2 tools
    (`lib/galaxy/datatypes/converters/vcf_to_interval_index_converter.xml`
    and `tools/maf/vcf_to_maf_customtrack.xml`) correctly specify it as a
    requirement.

--- run_tests.sh
+++ run_tests.sh
@@ -492,14 +492,15 @@ do
       -u|-unit|--unit)
           report_file="run_unit_tests.html"
           unit_extra='--doctest-modules
-			--ignore lib/galaxy/jobs/runners/chronos.py
-			--ignore lib/galaxy/model/migrations/alembic
-			--ignore lib/galaxy/tools/bundled
-			--ignore lib/galaxy/web/proxy/js/node_modules/
-			--ignore lib/galaxy_test
-			--ignore lib/tool_shed/test
-			--ignore lib/tool_shed/webapp/controllers
-			--ignore=lib/tool_shed/webapp/model/migrations/alembic/'
+            --ignore lib/galaxy/datatypes/converters/
+            --ignore lib/galaxy/jobs/runners/chronos.py
+            --ignore lib/galaxy/model/migrations/alembic
+            --ignore lib/galaxy/tools/bundled
+            --ignore lib/galaxy/web/proxy/js/node_modules/
+            --ignore lib/galaxy_test
+            --ignore lib/tool_shed/test
+            --ignore lib/tool_shed/webapp/controllers
+            --ignore=lib/tool_shed/webapp/model/migrations/alembic/'
           generate_cwl_conformance_tests=1
           if [ $# -gt 1 ]; then
               unit_extra="$unit_extra $2"
