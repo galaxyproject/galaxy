8d3b2ae5a91706446de5653b0cce9e6970a59b53
mvdbeek <m.vandenbeek@gmail.com>
2024-10-12 09:20:57 +0200

    Redirect user to login/registration if anonymous
    Also fixes error handling.

--- client/src/components/Landing/WorkflowLanding.vue
+++ client/src/components/Landing/WorkflowLanding.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { BAlert } from "bootstrap-vue";
-import { onBeforeMount, ref } from "vue";
+import { storeToRefs } from "pinia";
+import { ref, watch } from "vue";
+import { useRouter } from "vue-router/composables";
 
 import { GalaxyApi } from "@/api";
+import { useUserStore } from "@/stores/userStore";
 import { errorMessageAsString } from "@/utils/simple-error";
 
 import LoadingSpan from "@/components/LoadingSpan.vue";
@@ -21,33 +24,45 @@ const workflowId = ref<string | null>(null);
 const errorMessage = ref<string | null>(null);
 const requestState = ref<Record<string, never> | null>(null);
 const instance = ref<boolean>(false);
+const userStore = useUserStore();
+const router = useRouter();
 
-onBeforeMount(async () => {
-    const { data, error } = await GalaxyApi().GET("/api/workflow_landings/{uuid}", {
-        params: {
-            path: { uuid: props.uuid },
-        },
-    });
-    if (data) {
-        workflowId.value = data.workflow_id;
-        instance.value = data.workflow_target_type === "workflow";
-        requestState.value = data.request_state;
-    } else {
-        errorMessage.value = errorMessageAsString(error);
-    }
-    console.log(data);
-});
+userStore.loadUser(false);
+const { isAnonymous, currentUser } = storeToRefs(userStore);
+
+watch(
+    currentUser,
+    async () => {
+        if (isAnonymous.value) {
+            router.push(`/login/start?redirect=/workflow_landings/${props.uuid}`);
+        } else if (currentUser.value) {
+            const { data, error } = await GalaxyApi().GET("/api/workflow_landings/{uuid}", {
+                params: {
+                    path: { uuid: props.uuid },
+                },
+            });
+            if (data) {
+                workflowId.value = data.workflow_id;
+                instance.value = data.workflow_target_type === "workflow";
+                requestState.value = data.request_state;
+            } else {
+                errorMessage.value = errorMessageAsString(error);
+            }
+        }
+    },
+    { immediate: true }
+);
 </script>
 
 <template>
     <div>
-        <div v-if="!workflowId">
-            <LoadingSpan message="Loading workflow parameters" />
-        </div>
-        <div v-else-if="errorMessage">
+        <div v-if="errorMessage">
             <BAlert variant="danger" show>
                 {{ errorMessage }}
             </BAlert>
+        </div>
+        <div v-else-if="!workflowId">
+            <LoadingSpan message="Loading workflow parameters" />
         </div>
         <div v-else>
             <WorkflowRun
