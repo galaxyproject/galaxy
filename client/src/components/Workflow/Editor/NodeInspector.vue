b519f992dc9d45d8d63dd2557959ccc53045093e
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2024-10-17 17:47:47 +0200

    allow for saving default sizes for tools / steps

--- client/src/components/Workflow/Editor/NodeInspector.vue
+++ client/src/components/Workflow/Editor/NodeInspector.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 import { faCog, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { BButton, BButtonGroup } from "bootstrap-vue";
+import { BButton, BButtonGroup, BDropdown, BDropdownForm, BDropdownItemButton, BFormCheckbox } from "bootstrap-vue";
 //@ts-ignore deprecated package without types (vue 2, remove this comment on vue 3 migration)
-import { ArrowLeftFromLine } from "lucide-vue";
-import { computed, ref } from "vue";
+import { ArrowLeftFromLine, ArrowRightToLine } from "lucide-vue";
+import { computed } from "vue";
 
+import { useWorkflowNodeInspectorStore } from "@/stores/workflowNodeInspectorStore";
 import type { Step } from "@/stores/workflowStepStore";
 
 import FormTool from "./Forms/FormTool.vue";
 import DraggableSeparator from "@/components/Common/DraggableSeparator.vue";
 import Heading from "@/components/Common/Heading.vue";
+import IdleLoad from "@/components/Common/IdleLoad.vue";
 
 const props = defineProps<{
     step: Step;
@@ -23,9 +25,13 @@ const emit = defineEmits<{
     (e: "labelChanged", id: string, label: string): void;
     (e: "dataChanged", id: string, data: unknown): void;
     (e: "stepUpdated", id: string, step: Step): void;
+    (e: "close"): void;
 }>();
 
-const width = ref(300);
+const inspectorStore = useWorkflowNodeInspectorStore();
+
+const maximized = computed(() => inspectorStore.maximized(props.step));
+const width = computed(() => inspectorStore.width(props.step));
 
 const cssVars = computed(() => ({
     "--width": `${width.value}px`,
@@ -34,44 +40,82 @@ const cssVars = computed(() => ({
 const isTool = computed(() => props.step.type === "tool");
 
 const title = computed(() => `${props.step.id + 1}: ${props.step.label ?? props.step.name}`);
+
+function close() {
+    inspectorStore.generalMaximized = false;
+    emit("close");
+}
 </script>
 
 <template>
-    <section class="tool-inspector" :style="cssVars">
+    <section class="tool-inspector" :class="{ maximized }" :style="cssVars">
         <DraggableSeparator
+            v-if="!maximized"
             inner
             :position="width"
             side="right"
             :min="100"
             :max="1200"
-            @positionChanged="(v) => (width = v)"></DraggableSeparator>
+            @positionChanged="(v) => inspectorStore.setWidth(props.step, v)"></DraggableSeparator>
 
         <div class="inspector-heading">
             <Heading h2 inline size="sm"> {{ title }} </Heading>
 
             <BButtonGroup>
-                <BButton variant="link" size="md">
-                    <ArrowLeftFromLine absolute-stroke-width size="17" />
+                <BButton
+                    v-if="!maximized"
+                    class="heading-button"
+                    variant="link"
+                    size="md"
+                    @click="inspectorStore.setMaximized(props.step, true)">
+                    <ArrowLeftFromLine absolute-stroke-width :size="17" />
                 </BButton>
-                <BButton variant="link" size="md">
-                    <FontAwesomeIcon :icon="faCog" fixed-width />
+                <BButton
+                    v-else
+                    class="heading-button"
+                    variant="link"
+                    size="md"
+                    @click="inspectorStore.setMaximized(props.step, false)">
+                    <ArrowRightToLine absolute-stroke-width :size="17" />
                 </BButton>
-                <BButton variant="link" size="md">
+
+                <BDropdown class="dropdown" toggle-class="heading-button" variant="link" size="md" no-caret>
+                    <template v-slot:button-content>
+                        <FontAwesomeIcon :icon="faCog" fixed-width />
+                    </template>
+
+                    <BDropdownForm form-class="px-2" title="remember size for all steps using this tool">
+                        <BFormCheckbox
+                            :checked="inspectorStore.isStored(props.step)"
+                            @input="(v) => inspectorStore.setStored(props.step, v)">
+                            remember size
+                        </BFormCheckbox>
+                    </BDropdownForm>
+
+                    <BDropdownItemButton @click="inspectorStore.clearAllStored">
+                        reset all stored sizes
+                    </BDropdownItemButton>
+                </BDropdown>
+
+                <BButton class="heading-button" variant="link" size="md" @click="close">
                     <FontAwesomeIcon :icon="faTimes" fixed-width />
                 </BButton>
             </BButtonGroup>
         </div>
 
         <div class="inspector-content">
-            <FormTool
-                v-if="isTool"
-                :step="props.step"
-                :datatypes="props.datatypes"
-                @onSetData="(id, d) => emit('dataChanged', id, d)"
-                @onUpdateStep="(id, s) => emit('stepUpdated', id, s)"
-                @onChangePostJobActions="(id, a) => emit('postJobActionsChanged', id, a)"
-                @onAnnotation="(id, a) => emit('annotationChanged', id, a)"
-                @onLabel="(id, l) => emit('labelChanged', id, l)"></FormTool>
+            <IdleLoad :key="props.step.id" spinner center class="w-100 h-50">
+                <FormTool
+                    v-if="isTool"
+                    class="w-100"
+                    :step="props.step"
+                    :datatypes="props.datatypes"
+                    @onSetData="(id, d) => emit('dataChanged', id, d)"
+                    @onUpdateStep="(id, s) => emit('stepUpdated', id, s)"
+                    @onChangePostJobActions="(id, a) => emit('postJobActionsChanged', id, a)"
+                    @onAnnotation="(id, a) => emit('annotationChanged', id, a)"
+                    @onLabel="(id, l) => emit('labelChanged', id, l)"></FormTool>
+            </IdleLoad>
         </div>
     </section>
 </template>
@@ -101,15 +145,33 @@ const title = computed(() => `${props.step.id + 1}: ${props.step.label ?? props.
     border-style: solid;
     border-radius: 0.5rem 0 0 0.5rem;
 
+    &.maximized {
+        border-radius: 0.5rem;
+        --clearance: 16px;
+        right: var(--clearance);
+        top: var(--clearance);
+        bottom: var(--clearance);
+        left: var(--clearance);
+        width: calc(100% - var(--clearance) * 2);
+    }
+
     .inspector-heading {
-        padding: 0.5rem;
+        padding: 0.25rem 0.5rem;
         display: flex;
         justify-content: space-between;
 
-        button {
+        h2 {
+            word-break: break-word;
+        }
+
+        &:deep(.heading-button) {
             padding: 0.4rem;
             display: grid;
             place-items: center;
+        }
+
+        .dropdown.show {
+            display: inline-flex;
         }
     }
 
