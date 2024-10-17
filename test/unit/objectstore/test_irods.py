9e35f149ef2278ec2be06ea750912896142ba834
pauldg <pauldegeest1@gmail.com>
2024-10-17 13:58:09 +0200

    added tests for new sections

--- test/unit/objectstore/test_irods.py
+++ test/unit/objectstore/test_irods.py
@@ -16,6 +16,12 @@ CONFIG_FILE_NO_EXTRA_DIR = os.path.join(SCRIPT_DIRECTORY, CONFIG_FILE_NAME_NO_EX
 CONFIG_FILE_NAME_NO_AUTH = "irods_object_store_conf_no_auth.xml"
 CONFIG_FILE_NO_AUTH = os.path.join(SCRIPT_DIRECTORY, CONFIG_FILE_NAME_NO_AUTH)
 
+CONFIG_FILE_NAME_SSL = "irods_object_store_conf_ssl.xml"
+CONFIG_FILE_SSL = os.path.join(SCRIPT_DIRECTORY, CONFIG_FILE_NAME_SSL)
+
+CONFIG_FILE_NAME_LOGICAL_PATH = "irods_object_store_conf_logical_path.xml"
+CONFIG_FILE_LOGICAL_PATH = os.path.join(SCRIPT_DIRECTORY, CONFIG_FILE_NAME_LOGICAL_PATH)
+
 
 def test_parse_valid_config_xml():
     tree = parse_xml(CONFIG_FILE)
@@ -37,6 +43,31 @@ def test_parse_valid_config_xml():
     assert config["extra_dirs"][0]["path"] == "database/job_working_directory_irods"
     assert config["extra_dirs"][1]["type"] == "temp"
     assert config["extra_dirs"][1]["path"] == "database/tmp_irods"
+
+
+def test_parse_config_xml_ssl():
+    tree = parse_xml(CONFIG_FILE_SSL)
+    root = tree.getroot()
+    config = parse_config_xml(root)
+
+    print(config)
+
+    assert config["ssl"]["client_server_negotiation"] == "request_server_negotiation"
+    assert config["ssl"]["client_server_policy"] == "CS_NEG_REQUIRE"
+    assert config["ssl"]["encryption_algorithm"] == "AES-256-CBC"
+    assert config["ssl"]["encryption_key_size"] == 32
+    assert config["ssl"]["encryption_num_hash_rounds"] == 16
+    assert config["ssl"]["encryption_salt_size"] == 8
+    assert config["ssl"]["ssl_verify_server"] == "cert"
+    assert config["ssl"]["ssl_ca_certificate_file"] == "/etc/irods/ssl/irods.crt"
+
+
+def test_parse_config_xml_logical_path():
+    tree = parse_xml(CONFIG_FILE_LOGICAL_PATH)
+    root = tree.getroot()
+    config = parse_config_xml(root)
+
+    assert config["logical"]["path"] == "/tempZone/home/rods"
 
 
 def test_parse_config_xml_no_extra_dir():
