c4aba56164edd8f19b80d2d17d91d7ad9eaeeccd
Matthias Bernt <m.bernt@ufz.de>
2024-10-22 09:23:12 +0200

    fix test: number of linters

--- test/unit/tool_util/test_tool_linters.py
+++ test/unit/tool_util/test_tool_linters.py
@@ -2208,7 +2208,7 @@ def test_skip_by_module(lint_ctx):
 def test_list_linters():
     linter_names = Linter.list_listers()
     # make sure to add/remove a test for new/removed linters if this number changes
-    assert len(linter_names) == 135
+    assert len(linter_names) == 134
     assert "Linter" not in linter_names
     # make sure that linters from all modules are available
     for prefix in [
