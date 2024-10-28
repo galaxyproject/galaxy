169496fb4944c4243063c978d206d3aa4323625e
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2024-10-28 15:20:23 +0100

    fxi activity store test

--- client/src/stores/activityStore.test.ts
+++ client/src/stores/activityStore.test.ts
@@ -4,7 +4,7 @@ import { useActivityStore } from "@/stores/activityStore";
 
 // mock Galaxy object
 jest.mock("./activitySetup", () => ({
-    Activities: [
+    defaultActivities: [
         {
             anonymous: false,
             description: "a-description",
@@ -55,16 +55,16 @@ describe("Activity Store", () => {
         setActivePinia(createPinia());
     });
 
-    it("initialize store", () => {
+    it("initialize store", async () => {
         const activityStore = useActivityStore("default");
         expect(activityStore.getAll().length).toBe(0);
-        activityStore.sync();
+        await activityStore.sync();
         expect(activityStore.getAll().length).toBe(1);
     });
 
-    it("add activity", () => {
+    it("add activity", async () => {
         const activityStore = useActivityStore("default");
-        activityStore.sync();
+        await activityStore.sync();
         const initialActivities = activityStore.getAll();
         expect(initialActivities[0]?.visible).toBeTruthy();
         activityStore.setAll(newActivities);
@@ -72,7 +72,7 @@ describe("Activity Store", () => {
         const currentActivities = activityStore.getAll();
         expect(currentActivities[0]).toEqual(newActivities[0]);
         expect(currentActivities[1]).toEqual(newActivities[1]);
-        activityStore.sync();
+        await activityStore.sync();
         const syncActivities = activityStore.getAll();
         expect(syncActivities.length).toEqual(2);
         expect(syncActivities[0]?.description).toEqual("a-description");
@@ -80,19 +80,19 @@ describe("Activity Store", () => {
         expect(syncActivities[1]).toEqual(newActivities[1]);
     });
 
-    it("remove activity", () => {
+    it("remove activity", async () => {
         const activityStore = useActivityStore("default");
-        activityStore.sync();
+        await activityStore.sync();
         const initialActivities = activityStore.getAll();
         expect(initialActivities.length).toEqual(1);
         activityStore.remove("a-id");
         expect(activityStore.getAll().length).toEqual(0);
-        activityStore.sync();
+        await activityStore.sync();
         expect(activityStore.getAll().length).toEqual(1);
         activityStore.setAll(newActivities);
         expect(activityStore.getAll().length).toEqual(2);
         activityStore.remove("b-id");
-        activityStore.sync();
+        await activityStore.sync();
         expect(activityStore.getAll().length).toEqual(1);
     });
 });
