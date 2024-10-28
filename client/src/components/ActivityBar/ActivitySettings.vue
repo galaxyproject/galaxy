47392d6a2c3a22976c371679afd058ef9ea436cc
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2024-10-28 15:41:31 +0100

    fix empty divs rendered

--- client/src/components/ActivityBar/ActivitySettings.vue
+++ client/src/components/ActivityBar/ActivitySettings.vue
@@ -3,7 +3,6 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSquare, faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
 import { faCheckSquare, faStar, faThumbtack, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { storeToRefs } from "pinia";
 import { computed, type ComputedRef } from "vue";
 import { useRouter } from "vue-router/composables";
 
@@ -28,12 +27,13 @@ const emit = defineEmits<{
 }>();
 
 const activityStore = useActivityStore(props.activityBarId);
-const { activities } = storeToRefs(activityStore);
+
+const optionalActivities = computed(() => activityStore.activities.filter((a) => a.optional));
 
 const filteredActivities = computed(() => {
     if (props.query?.length > 0) {
         const queryLower = props.query.toLowerCase();
-        const results = activities.value.filter((a: Activity) => {
+        const results = optionalActivities.value.filter((a: Activity) => {
             const attributeValues = [a.title, a.description];
             for (const value of attributeValues) {
                 if (value.toLowerCase().indexOf(queryLower) !== -1) {
@@ -44,7 +44,7 @@ const filteredActivities = computed(() => {
         });
         return results;
     } else {
-        return activities.value;
+        return optionalActivities.value;
     }
 });
 
@@ -82,56 +82,55 @@ function executeActivity(activity: Activity) {
 <template>
     <div class="activity-settings rounded no-highlight">
         <div v-if="foundActivities" class="activity-settings-content">
-            <div v-for="activity in filteredActivities" :key="activity.id">
-                <button
-                    v-if="activity.optional"
-                    class="activity-settings-item p-2 cursor-pointer"
-                    @click="executeActivity(activity)">
-                    <div class="d-flex justify-content-between align-items-start">
-                        <span class="d-flex justify-content-between w-100">
-                            <span>
-                                <icon class="mr-1" :icon="activity.icon" />
-                                <span v-localize class="font-weight-bold">{{
-                                    activity.title || "No title available"
-                                }}</span>
-                            </span>
-                            <div>
-                                <BButton
-                                    v-if="activity.mutable"
-                                    v-b-tooltip.hover
-                                    data-description="delete activity"
-                                    size="sm"
-                                    title="Delete Activity"
-                                    variant="link"
-                                    @click.stop="onRemove(activity)">
-                                    <FontAwesomeIcon icon="fa-trash" fa-fw />
-                                </BButton>
-                                <BButton
-                                    v-if="activity.visible"
-                                    v-b-tooltip.hover
-                                    size="sm"
-                                    title="Hide in Activity Bar"
-                                    variant="link"
-                                    @click.stop="onFavorite(activity)">
-                                    <FontAwesomeIcon icon="fas fa-star" fa-fw />
-                                </BButton>
-                                <BButton
-                                    v-else
-                                    v-b-tooltip.hover
-                                    size="sm"
-                                    title="Show in Activity Bar"
-                                    variant="link"
-                                    @click.stop="onFavorite(activity)">
-                                    <FontAwesomeIcon icon="far fa-star" fa-fw />
-                                </BButton>
-                            </div>
+            <button
+                v-for="activity in filteredActivities"
+                :key="activity.id"
+                class="activity-settings-item p-2 cursor-pointer"
+                @click="executeActivity(activity)">
+                <div class="d-flex justify-content-between align-items-start">
+                    <span class="d-flex justify-content-between w-100">
+                        <span>
+                            <icon class="mr-1" :icon="activity.icon" />
+                            <span v-localize class="font-weight-bold">{{
+                                activity.title || "No title available"
+                            }}</span>
                         </span>
-                    </div>
-                    <div v-localize class="text-muted">
-                        {{ activity.description || "No description available" }}
-                    </div>
-                </button>
-            </div>
+                        <div>
+                            <BButton
+                                v-if="activity.mutable"
+                                v-b-tooltip.hover
+                                data-description="delete activity"
+                                size="sm"
+                                title="Delete Activity"
+                                variant="link"
+                                @click.stop="onRemove(activity)">
+                                <FontAwesomeIcon icon="fa-trash" fa-fw />
+                            </BButton>
+                            <BButton
+                                v-if="activity.visible"
+                                v-b-tooltip.hover
+                                size="sm"
+                                title="Hide in Activity Bar"
+                                variant="link"
+                                @click.stop="onFavorite(activity)">
+                                <FontAwesomeIcon icon="fas fa-star" fa-fw />
+                            </BButton>
+                            <BButton
+                                v-else
+                                v-b-tooltip.hover
+                                size="sm"
+                                title="Show in Activity Bar"
+                                variant="link"
+                                @click.stop="onFavorite(activity)">
+                                <FontAwesomeIcon icon="far fa-star" fa-fw />
+                            </BButton>
+                        </div>
+                    </span>
+                </div>
+                <div v-localize class="text-muted">
+                    {{ activity.description || "No description available" }}
+                </div>
+            </button>
         </div>
         <div v-else class="activity-settings-content">
             <b-alert v-localize class="py-1 px-2" show> No matching activities found. </b-alert>
