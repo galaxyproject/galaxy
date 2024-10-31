364f20b10e1fac731c4dbaedf4cb0699f98e48ee
John Chilton <jmchilton@gmail.com>
2024-10-31 15:53:18 -0400

    Implement directory_uri workflow parameters.

--- lib/galaxy/workflow/workflow_parameter_input_definitions.py
+++ lib/galaxy/workflow/workflow_parameter_input_definitions.py
@@ -7,12 +7,13 @@ from typing import (
 from galaxy.tools.parameters.basic import (
     BooleanToolParameter,
     ColorToolParameter,
+    DirectoryUriToolParameter,
     FloatToolParameter,
     IntegerToolParameter,
     TextToolParameter,
 )
 
-param_types = Literal["text", "integer", "float", "color", "boolean"]
+param_types = Literal["text", "integer", "float", "color", "boolean", "directory_uri"]
 default_source_type = Dict[str, Union[int, float, bool, str]]
 tool_param_type = Union[
     TextToolParameter,
@@ -20,6 +21,7 @@ tool_param_type = Union[
     FloatToolParameter,
     BooleanToolParameter,
     ColorToolParameter,
+    DirectoryUriToolParameter,
 ]
 
 
@@ -36,6 +38,7 @@ def get_default_parameter(param_type: param_types) -> tool_param_type:
             FloatToolParameter,
             BooleanToolParameter,
             ColorToolParameter,
+            DirectoryUriToolParameter,
         ] = TextToolParameter(None, default_source)
     elif param_type == "integer":
         input_default_value = IntegerToolParameter(None, default_source)
@@ -45,4 +48,6 @@ def get_default_parameter(param_type: param_types) -> tool_param_type:
         input_default_value = BooleanToolParameter(None, default_source)
     elif param_type == "color":
         input_default_value = ColorToolParameter(None, default_source)
+    elif param_type == "directory_uri":
+        input_default_value = DirectoryUriToolParameter(None, default_source)
     return input_default_value
