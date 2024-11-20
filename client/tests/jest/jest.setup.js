c567829e888a88f46eb05b9b2e921c2d9e0fbb84
John Chilton <jmchilton@gmail.com>
2024-11-20 11:42:49 -0500

    Cleanup extra printings of Vue production and devtools message.
    See comments inline for how to test this works and my best shot at the explaining the problem.

--- client/tests/jest/jest.setup.js
+++ client/tests/jest/jest.setup.js
@@ -11,6 +11,18 @@ Element.prototype.scrollIntoView = jest.fn();
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
 
+// if we do jest.mock(module) where modules imports Vue, it will create a clean Vue context
+// that ignores the above tips. So pre-emptively mock out Vue in these contexts with the following
+// block of code. GridList.test.js is a good example of this, when ran (yarn test GridList.test.js)
+// it will bypass the attempt to set the Vue config values above and print the messages anyway without
+// this block.
+jest.mock("vue", () => {
+    const Vue = jest.requireActual("vue"); // Import the actual Vue instance
+    Vue.config.productionTip = false; // Disable production tip
+    Vue.config.devtools = false; // Disable production tip
+    return Vue; // Return the modified Vue instance
+});
+
 /* still don't understand what was invoking the following, but nothing should,
 and this makes the tag tests work correctly */
 global.setImmediate = global.setTimeout;
