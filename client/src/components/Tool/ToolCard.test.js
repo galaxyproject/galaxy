a2c2d236a256b11344b389c4d1b403a32b183c04
John Chilton <jmchilton@gmail.com>
2024-11-20 14:21:35 -0500

    fewer warnings around ToolCard.test.js

--- client/src/components/Tool/ToolCard.test.js
+++ client/src/components/Tool/ToolCard.test.js
@@ -1,20 +1,22 @@
 import { mount } from "@vue/test-utils";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import flushPromises from "flush-promises";
 import { createPinia } from "pinia";
 import { useUserStore } from "stores/userStore";
 import { getLocalVue } from "tests/jest/helpers";
+import { setupMockConfig } from "tests/jest/mockConfig";
+
+import { useServerMock } from "@/api/client/__mocks__";
 
 import ToolCard from "./ToolCard";
 
+const { server, http } = useServerMock();
+
 jest.mock("@/api/schema");
 
-jest.mock("@/composables/config", () => ({
-    useConfig: jest.fn(() => ({
-        config: { enable_tool_source_display: false },
-        isConfigLoaded: true,
-    })),
-}));
+const config = { enable_tool_source_display: false };
+setupMockConfig(config);
 
 const localVue = getLocalVue();
 
@@ -23,7 +25,15 @@ describe("ToolCard", () => {
     let axiosMock;
     let userStore;
 
-    beforeEach(() => {
+    beforeEach(async () => {
+        // some child component must be bypassing useConfig - so we need to explicitly
+        // stup the API endpoint also. If you can drop this without request problems in log,
+        // this hack can be removed.
+        server.use(
+            http.get("/api/configuration", ({ response }) => {
+                return response(200).json(config);
+            })
+        );
         axiosMock = new MockAdapter(axios);
         axiosMock.onGet(`/api/webhooks`).reply(200, []);
 
@@ -38,9 +48,12 @@ describe("ToolCard", () => {
                 sustainVersion: false,
                 options: {
                     id: "options.id",
+                    name: "options.name",
+                    version: "options.version",
                     versions: [],
                     sharable_url: "options.sharable_url",
                     help: "options.help",
+                    help_format: "restructuredtext",
                     citations: false,
                 },
                 messageText: "messageText",
@@ -60,6 +73,7 @@ describe("ToolCard", () => {
             is_admin: true,
             preferences: {},
         };
+        await flushPromises();
     });
 
     it("shows props", async () => {
@@ -81,5 +95,6 @@ describe("ToolCard", () => {
         await wrapper.setProps({ disabled: true });
         const backdropActive = wrapper.findAll(".portlet-backdrop");
         expect(backdropActive.length).toBe(1);
+        await flushPromises();
     });
 });
