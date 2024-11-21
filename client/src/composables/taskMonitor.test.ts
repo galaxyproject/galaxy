1cad7f759e55cbb2a41ee399af06f7d251a744fb
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2024-11-21 15:52:28 +0100

    fix pesky warning

--- client/src/composables/taskMonitor.test.ts
+++ client/src/composables/taskMonitor.test.ts
@@ -35,39 +35,39 @@ describe("useTaskMonitor", () => {
     });
 
     it("should indicate the task is running when it is still pending", async () => {
-        const { waitForTask, isRunning, status } = useTaskMonitor();
+        const { waitForTask, isRunning, taskStatus } = useTaskMonitor();
 
         expect(isRunning.value).toBe(false);
         waitForTask(PENDING_TASK_ID);
         await flushPromises();
         expect(isRunning.value).toBe(true);
-        expect(status.value).toBe("PENDING");
+        expect(taskStatus.value).toBe("PENDING");
     });
 
     it("should indicate the task is successfully completed when the state is SUCCESS", async () => {
-        const { waitForTask, isRunning, isCompleted, status } = useTaskMonitor();
+        const { waitForTask, isRunning, isCompleted, taskStatus } = useTaskMonitor();
 
         expect(isCompleted.value).toBe(false);
         waitForTask(COMPLETED_TASK_ID);
         await flushPromises();
         expect(isCompleted.value).toBe(true);
         expect(isRunning.value).toBe(false);
-        expect(status.value).toBe("SUCCESS");
+        expect(taskStatus.value).toBe("SUCCESS");
     });
 
     it("should indicate the task has failed when the state is FAILED", async () => {
-        const { waitForTask, isRunning, hasFailed, status } = useTaskMonitor();
+        const { waitForTask, isRunning, hasFailed, taskStatus } = useTaskMonitor();
 
         expect(hasFailed.value).toBe(false);
         waitForTask(FAILED_TASK_ID);
         await flushPromises();
         expect(hasFailed.value).toBe(true);
         expect(isRunning.value).toBe(false);
-        expect(status.value).toBe("FAILURE");
+        expect(taskStatus.value).toBe("FAILURE");
     });
 
     it("should indicate the task status request failed when the request failed", async () => {
-        const { waitForTask, requestHasFailed, isRunning, isCompleted, status } = useTaskMonitor();
+        const { waitForTask, requestHasFailed, isRunning, isCompleted, taskStatus } = useTaskMonitor();
 
         expect(requestHasFailed.value).toBe(false);
         waitForTask(REQUEST_FAILED_TASK_ID);
@@ -75,16 +75,16 @@ describe("useTaskMonitor", () => {
         expect(requestHasFailed.value).toBe(true);
         expect(isRunning.value).toBe(false);
         expect(isCompleted.value).toBe(false);
-        expect(status.value).toBe("Request failed");
+        expect(taskStatus.value).toBe("Request failed");
     });
 
     it("should load the status from the stored monitoring data", async () => {
-        const { loadStatus, isRunning, isCompleted, hasFailed, status } = useTaskMonitor();
+        const { loadStatus, isRunning, isCompleted, hasFailed, taskStatus } = useTaskMonitor();
         const storedStatus = "SUCCESS";
 
         loadStatus(storedStatus);
 
-        expect(status.value).toBe(storedStatus);
+        expect(taskStatus.value).toBe(storedStatus);
         expect(isRunning.value).toBe(false);
         expect(isCompleted.value).toBe(true);
         expect(hasFailed.value).toBe(false);
@@ -92,24 +92,24 @@ describe("useTaskMonitor", () => {
 
     describe("isFinalState", () => {
         it("should indicate is final state when the task is completed", async () => {
-            const { waitForTask, isFinalState, isRunning, isCompleted, hasFailed, status } = useTaskMonitor();
+            const { waitForTask, isFinalState, isRunning, isCompleted, hasFailed, taskStatus } = useTaskMonitor();
 
-            expect(isFinalState(status.value)).toBe(false);
+            expect(isFinalState(taskStatus.value)).toBe(false);
             waitForTask(COMPLETED_TASK_ID);
             await flushPromises();
-            expect(isFinalState(status.value)).toBe(true);
+            expect(isFinalState(taskStatus.value)).toBe(true);
             expect(isRunning.value).toBe(false);
             expect(isCompleted.value).toBe(true);
             expect(hasFailed.value).toBe(false);
         });
 
         it("should indicate is final state when the task has failed", async () => {
-            const { waitForTask, isFinalState, isRunning, isCompleted, hasFailed, status } = useTaskMonitor();
+            const { waitForTask, isFinalState, isRunning, isCompleted, hasFailed, taskStatus } = useTaskMonitor();
 
-            expect(isFinalState(status.value)).toBe(false);
+            expect(isFinalState(taskStatus.value)).toBe(false);
             waitForTask(FAILED_TASK_ID);
             await flushPromises();
-            expect(isFinalState(status.value)).toBe(true);
+            expect(isFinalState(taskStatus.value)).toBe(true);
             expect(isRunning.value).toBe(false);
             expect(isCompleted.value).toBe(false);
             expect(hasFailed.value).toBe(true);
