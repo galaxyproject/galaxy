7e90bcb1eeaf49361013a5d18bd4b5a980d7ed83
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2024-11-25 15:13:58 +0000

    Add type annotations

--- lib/galaxy/tool_util/deps/mulled/mulled_hash.py
+++ lib/galaxy/tool_util/deps/mulled/mulled_hash.py
@@ -7,6 +7,7 @@ Produce a mulled hash with:
 
     mulled-hash samtools=1.3.1,bedtools=2.22
 """
+from typing_extensions import Literal
 
 from ._cli import arg_parser
 from .mulled_build import target_str_to_targets
@@ -16,14 +17,14 @@ from .util import (
 )
 
 
-def _mulled_hash(hash, targets):
+def _mulled_hash(hash: Literal["v1", "v2"], targets_str: str):
     """
-    >>> _mulled_hash(hash="v2", targets="samtools=1.3.1,bedtools=2.26.0")
+    >>> _mulled_hash("v2", "samtools=1.3.1,bedtools=2.26.0")
     'mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:a6419f25efff953fc505dbd5ee734856180bb619'
-    >>> _mulled_hash(hash="v2", targets="samtools=1.3.1=h9071d68_10,bedtools=2.26.0=0")
+    >>> _mulled_hash("v2", "samtools=1.3.1=h9071d68_10,bedtools=2.26.0=0")
     'mulled-v2-8186960447c5cb2faa697666dc1e6d919ad23f3e:a6419f25efff953fc505dbd5ee734856180bb619'
     """
-    targets = target_str_to_targets(targets)
+    targets = target_str_to_targets(targets_str)
     image_name = v2_image_name if hash == "v2" else v1_image_name
     return image_name(targets)
 
