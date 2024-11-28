e66652e78423158932bd6574568bbeea21e2fb8d
Marius van den Beek <m.vandenbeek@gmail.com>
2024-11-28 14:47:36 +0100

    Drop stray print statement

--- test/unit/data/model/test_model_store.py
+++ test/unit/data/model/test_model_store.py
@@ -441,7 +441,6 @@ def test_import_export_library():
 def test_import_export_invocation():
     app = _mock_app()
     workflow_invocation = _setup_invocation(app)
-    print(workflow_invocation)
     temp_directory = mkdtemp()
     with store.DirectoryModelExportStore(temp_directory, app=app) as export_store:
         export_store.export_workflow_invocation(workflow_invocation)
