0778c768b8aefc68080903d982347ad0e7f414c1
Arash <arash77.kad@gmail.com>
2024-11-28 17:05:09 +0100

    Rename 'secrets' to 'credentials' in tool parsing and related methods and use the discussed XML format #19196

--- lib/galaxy/tool_util/parser/cwl.py
+++ lib/galaxy/tool_util/parser/cwl.py
@@ -175,12 +175,12 @@ class CwlToolSource(ToolSource):
 
         software_requirements = self.tool_proxy.software_requirements()
         resource_requirements = self.tool_proxy.resource_requirements()
-        secrets = self.tool_proxy.secrets()
+        credentials = self.tool_proxy.credentials()
         return requirements.parse_requirements_from_lists(
             software_requirements=[{"name": r[0], "version": r[1], "type": "package"} for r in software_requirements],
             containers=containers,
             resource_requirements=resource_requirements,
-            secrets=secrets,
+            credentials=credentials,
         )
 
     def parse_profile(self):
--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -1412,17 +1412,18 @@ class Tool(UsesDictVisibleKeys):
                     raise Exception(message)
 
         # Requirements (dependencies)
-        requirements, containers, resource_requirements, secrets = tool_source.parse_requirements_and_containers()
+        requirements, containers, resource_requirements, credentials = tool_source.parse_requirements_and_containers()
         self.requirements = requirements
         self.containers = containers
         self.resource_requirements = resource_requirements
-        self.secrets = secrets
-        for secret in self.secrets:
-            preferences = self.app.config.user_preferences_extra["preferences"]
-            main_key, input_key = secret.user_preferences_key.split("/")
-            preferences_input = preferences.get(main_key, {}).get("inputs", [])
-            if not any(input_item.get("name") == input_key for input_item in preferences_input):
-                raise exceptions.ConfigurationError(f"User preferences key {secret.user_preferences_key} not found")
+        self.credentials = credentials
+        # for credential in self.credentials:
+        #     pass
+        #     preferences = self.app.config.user_preferences_extra["preferences"]
+        #     main_key, input_key = credential.user_preferences_key.split("/")
+        #     preferences_input = preferences.get(main_key, {}).get("inputs", [])
+        #     if not any(input_item.get("name") == input_key for input_item in preferences_input):
+        #         raise exceptions.ConfigurationError(f"User preferences key {credential.user_preferences_key} not found")
 
         required_files = tool_source.parse_required_files()
         if required_files is None:
