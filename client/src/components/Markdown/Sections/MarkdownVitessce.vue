6904e2bd45a5f8e6375d17fe07165d3f89ca65e7
guerler <aysam.guerler@gmail.com>
2024-12-02 13:18:29 +0300

    Add plugin container

---
+++ client/src/components/Markdown/Sections/MarkdownVitessce.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import PluginContainer from "./Elements/PluginContainer.vue";
+
+const props = defineProps<{
+    content: string;
+}>();
+
+const spec = computed(() => ({
+    ...JSON.parse(props.content),
+}));
+</script>
+
+<template>
+    <div>
+        <PluginContainer plugin-src="/static/plugins/visualizations/vitessce/static/dist/index.js" />
+        {{ spec }}
+    </div>
+</template>
