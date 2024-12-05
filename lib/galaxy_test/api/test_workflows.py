874f3a2841f17faffd7fb01e16cd1268cb5b2054
mvdbeek <m.vandenbeek@gmail.com>
2024-12-05 19:29:18 +0100

    Minor fix to restore admin-style dynamic tools

--- lib/galaxy_test/api/test_workflows.py
+++ lib/galaxy_test/api/test_workflows.py
@@ -1135,6 +1135,7 @@ steps:
       outputs:
         output1:
           format: txt
+          type: data
   - tool_id: cat1
     state:
       input1:
@@ -7962,6 +7963,7 @@ steps:
       outputs:
         output1:
           format: txt
+          type: data
   - tool_id: cat1
     state:
       input1:
