e0fec62a09e99d1d17606c5d6bb71d73431c9466
davelopez <46503462+davelopez@users.noreply.github.com>
2024-12-05 15:02:59 +0100

    Add WIP user credentials store

--- client/src/components/Tool/ToolCard.vue
+++ client/src/components/Tool/ToolCard.vue
@@ -179,7 +179,7 @@ const showHelpForum = computed(() => isConfigLoaded.value && config.value.enable
                 v-if="props.options.credentials"
                 :tool-id="props.id"
                 :tool-version="props.version"
-                :tool-credentials="props.options.credentials" />
+                :tool-credentials-definition="props.options.credentials" />
 
             <FormMessage variant="danger" :message="errorText" :persistent="true" />
             <FormMessage :variant="props.messageVariant" :message="props.messageText" />
