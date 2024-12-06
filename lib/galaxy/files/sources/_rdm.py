571dc07df0c50c6286f940845838a5c12c40b235
Kai Koenig <kai.koenig@pm.me>
2024-12-06 12:02:44 +0100

    chore: renaming from "record" to "container" for coherent terminology

--- lib/galaxy/files/sources/_rdm.py
+++ lib/galaxy/files/sources/_rdm.py
@@ -36,6 +36,10 @@ class RDMRepositoryInteractor:
 
     This class is not intended to be used directly, but rather to be subclassed
     by file sources that interact with RDM repositories.
+
+    Different RDM repositories use different terminology. Also they use the same term for different things.
+    To prevent confusion, we use the term "container" in the base repository.
+    This is an abstract term for the entity that contains multiple files, i.e. what is a "Collection" in galaxy.
     """
 
     def __init__(self, repository_url: str, plugin: "RDMFilesSource"):
@@ -55,13 +59,13 @@ class RDMRepositoryInteractor:
         """
         return self._repository_url
 
-    def to_plugin_uri(self, record_id: str, filename: Optional[str] = None) -> str:
-        """Creates a valid plugin URI to reference the given record_id.
+    def to_plugin_uri(self, container_id: str, filename: Optional[str] = None) -> str:
+        """Creates a valid plugin URI to reference the given container_id.
 
-        If a filename is provided, the URI will reference the specific file in the record."""
+        If a filename is provided, the URI will reference the specific file in the container."""
         raise NotImplementedError()
 
-    def get_records(
+    def get_file_containers(
         self,
         writeable: bool,
         user_context: OptionalUserContext = None,
@@ -70,41 +74,43 @@ class RDMRepositoryInteractor:
         query: Optional[str] = None,
         sort_by: Optional[str] = None,
     ) -> Tuple[List[RemoteDirectory], int]:
-        """Returns the list of records in the repository and the total count of records.
+        """Returns the list of file containers in the repository and the total count containers.
 
-        If writeable is True, only records that the user can write to will be returned.
+        If writeable is True, only containers that the user can write to will be returned.
         The user_context might be required to authenticate the user in the repository.
         """
         raise NotImplementedError()
 
-    def get_files_in_record(
-        self, record_id: str, writeable: bool, user_context: OptionalUserContext = None
+    def get_files_in_container(
+        self, container_id: str, writeable: bool, user_context: OptionalUserContext = None
     ) -> List[RemoteFile]:
-        """Returns the list of files contained in the given record.
+        """Returns the list of files of a file container.
 
-        If writeable is True, we are signaling that the user intends to write to the record.
+        If writeable is True, we are signaling that the user intends to write to the container.
         """
         raise NotImplementedError()
 
-    def create_draft_record(
+    def create_draft_container(
+
         self, title: str, public_name: Optional[str] = None, user_context: OptionalUserContext = None
     ):
-        """Creates a draft record (directory) in the repository with basic metadata.
+        """Creates a draft container (directory) in the repository with basic metadata.
 
-        The metadata is usually just the title of the record and the user that created it.
+        The metadata is usually just the title of the container and the user that created it.
         Some plugins might also provide additional metadata defaults in the user settings."""
         raise NotImplementedError()
 
-    def upload_file_to_draft_record(
+    def upload_file_to_draft_container(
         self,
-        record_id: str,
+        container_id: str,
         filename: str,
         file_path: str,
         user_context: OptionalUserContext = None,
     ) -> None:
-        """Uploads a file with the provided filename (from file_path) to a draft record with the given record_id.
+        """Uploads a file with the provided filename (from file_path) to a draft container with the given container_id.
 
-        The draft record must have been created in advance with the `create_draft_record` method.
+        The draft container must have been created in advance with the `create_draft_container` method.
+
         The file must exist in the file system at the given file_path.
         The user_context might be required to authenticate the user in the repository.
         """
@@ -133,13 +139,11 @@ class RDMFilesSource(BaseFilesSource):
     by file sources that interact with RDM repositories.
 
     A RDM file source is similar to a regular file source, but instead of tree of
-    files and directories, it provides a (one level) list of records (representing directories)
+    files and directories, it provides a (one level) list of containers (representing directories)
     that can contain only files (no subdirectories).
 
-    In addition, RDM file sources might need to create a new record (directory) in advance in the
-    repository, and then upload a file to it. This is done by calling the `create_entry`
-    method.
-
+    In addition, RDM file sources might need to create a new container (directory) in advance in the
+    repository, and then upload a file to it. This is done by calling the `_create_entry` method.
     """
 
     plugin_kind = PluginKind.rdm
