9dfc7377c037601e670c73f700c386ddf2baedd5
Kai Koenig <kai.koenig@pm.me>
2024-12-06 12:21:46 +0100

    chore: change order of file source and repository interactor

--- lib/galaxy/files/sources/_rdm.py
+++ lib/galaxy/files/sources/_rdm.py
@@ -31,6 +31,72 @@ class ContainerAndFileIdentifier(NamedTuple):
     file_identifier: str
 
 
+class RDMFilesSource(BaseFilesSource):
+    """Base class for Research Data Management (RDM) file sources.
+
+    This class is not intended to be used directly, but rather to be subclassed
+    by file sources that interact with RDM repositories.
+
+    A RDM file source is similar to a regular file source, but instead of tree of
+    files and directories, it provides a (one level) list of containers (representing directories)
+    that can contain only files (no subdirectories).
+
+    In addition, RDM file sources might need to create a new container (directory) in advance in the
+    repository, and then upload a file to it. This is done by calling the `_create_entry` method.
+    """
+
+    plugin_kind = PluginKind.rdm
+
+    def __init__(self, **kwd: Unpack[RDMFilesSourceProperties]):
+        props = self._parse_common_config_opts(kwd)
+        self.url = props.get("url")
+        if not self.url:
+            raise Exception("URL for RDM repository must be provided in configuration")
+        self._props = props
+        self._repository_interactor = self.get_repository_interactor(self.url)
+
+    @property
+    def repository(self) -> RDMRepositoryInteractor:
+        return self._repository_interactor
+
+    def get_url(self) -> Optional[str]:
+        return self.url
+
+    def get_repository_interactor(self, repository_url: str) -> RDMRepositoryInteractor:
+        """Returns an interactor compatible with the given repository URL.
+
+        This must be implemented by subclasses."""
+        raise NotImplementedError()
+
+    def parse_path(self, source_path: str, container_id_only: bool = False) -> ContainerAndFileIdentifier:
+        """Parses the given source path and returns the container_id and filename.
+        
+        If container_id_only is True, an empty filename will be returned.
+
+        This must be implemented by subclasses."""
+        raise NotImplementedError()
+
+    def get_container_id_from_path(self, source_path: str) -> str:
+        raise NotImplementedError()
+
+    def _serialization_props(self, user_context: OptionalUserContext = None):
+        effective_props = {}
+        for key, val in self._props.items():
+            effective_props[key] = self._evaluate_prop(val, user_context=user_context)
+        return effective_props
+
+    def get_authorization_token(self, user_context: OptionalUserContext) -> Optional[str]:
+        token = None
+        if user_context:
+            effective_props = self._serialization_props(user_context)
+            token = effective_props.get("token")
+        return token
+
+    def get_public_name(self, user_context: OptionalUserContext) -> Optional[str]:
+        effective_props = self._serialization_props(user_context)
+        return effective_props.get("public_name")
+
+
 class RDMRepositoryInteractor:
     """Base class for interacting with an external RDM repository.
 
@@ -130,69 +196,3 @@ class RDMRepositoryInteractor:
         file is not publicly available.
         """
         raise NotImplementedError()
-
-
-class RDMFilesSource(BaseFilesSource):
-    """Base class for Research Data Management (RDM) file sources.
-
-    This class is not intended to be used directly, but rather to be subclassed
-    by file sources that interact with RDM repositories.
-
-    A RDM file source is similar to a regular file source, but instead of tree of
-    files and directories, it provides a (one level) list of containers (representing directories)
-    that can contain only files (no subdirectories).
-
-    In addition, RDM file sources might need to create a new container (directory) in advance in the
-    repository, and then upload a file to it. This is done by calling the `_create_entry` method.
-    """
-
-    plugin_kind = PluginKind.rdm
-
-    def __init__(self, **kwd: Unpack[RDMFilesSourceProperties]):
-        props = self._parse_common_config_opts(kwd)
-        self.url = props.get("url")
-        if not self.url:
-            raise Exception("URL for RDM repository must be provided in configuration")
-        self._props = props
-        self._repository_interactor = self.get_repository_interactor(self.url)
-
-    @property
-    def repository(self) -> RDMRepositoryInteractor:
-        return self._repository_interactor
-
-    def get_url(self) -> Optional[str]:
-        return self.url
-
-    def get_repository_interactor(self, repository_url: str) -> RDMRepositoryInteractor:
-        """Returns an interactor compatible with the given repository URL.
-
-        This must be implemented by subclasses."""
-        raise NotImplementedError()
-
-    def parse_path(self, source_path: str, container_id_only: bool = False) -> ContainerAndFileIdentifier:
-        """Parses the given source path and returns the container_id and filename.
-        
-        If container_id_only is True, an empty filename will be returned.
-
-        This must be implemented by subclasses."""
-        raise NotImplementedError()
-
-    def get_container_id_from_path(self, source_path: str) -> str:
-        raise NotImplementedError()
-
-    def _serialization_props(self, user_context: OptionalUserContext = None):
-        effective_props = {}
-        for key, val in self._props.items():
-            effective_props[key] = self._evaluate_prop(val, user_context=user_context)
-        return effective_props
-
-    def get_authorization_token(self, user_context: OptionalUserContext) -> Optional[str]:
-        token = None
-        if user_context:
-            effective_props = self._serialization_props(user_context)
-            token = effective_props.get("token")
-        return token
-
-    def get_public_name(self, user_context: OptionalUserContext) -> Optional[str]:
-        effective_props = self._serialization_props(user_context)
-        return effective_props.get("public_name")
