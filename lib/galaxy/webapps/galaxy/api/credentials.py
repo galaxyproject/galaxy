8ae668a9bca4d1b799744a2ebe077e7c7399a5ba
Arash <arash77.kad@gmail.com>
2024-12-16 11:18:07 +0100

    partial update for credentials api to match the new changes

--- lib/galaxy/webapps/galaxy/api/credentials.py
+++ lib/galaxy/webapps/galaxy/api/credentials.py
@@ -5,16 +5,19 @@ API operations on credentials (credentials and variables).
 import logging
 from typing import Optional
 
-from fastapi import Query
+from fastapi import (
+    Query,
+    Response,
+    status,
+)
 
 from galaxy.managers.context import ProvidesUserContext
 from galaxy.schema.credentials import (
-    CredentialsListResponse,
     CredentialsPayload,
-    DeleteCredentialsResponse,
+    SOURCE_TYPE,
     UpdateCredentialsPayload,
+    UserCredentialCreateResponse,
     UserCredentialsListResponse,
-    VerifyCredentialsResponse,
 )
 from galaxy.schema.fields import DecodedDatabaseIdField
 from galaxy.webapps.galaxy.api import (
@@ -42,7 +45,7 @@ class FastAPICredentials:
         self,
         user_id: UserIdPathParam,
         trans: ProvidesUserContext = DependsOnTrans,
-        source_type: Optional[str] = Query(
+        source_type: Optional[SOURCE_TYPE] = Query(
             None,
             description="The type of source to filter by.",
         ),
@@ -50,33 +53,12 @@ class FastAPICredentials:
             None,
             description="The ID of the source to filter by.",
         ),
+        group_name: Optional[str] = Query(
+            None,
+            description="The name of the group to filter by.",
+        ),
     ) -> UserCredentialsListResponse:
-        return self.service.list_user_credentials(trans, user_id, source_type, source_id)
-
-    @router.get(
-        "/api/users/{user_id}/credentials/{user_credentials_id}",
-        summary="Verifies if credentials have been provided for a specific service",
-    )
-    def verify_service_credentials(
-        self,
-        user_id: UserIdPathParam,
-        user_credentials_id: DecodedDatabaseIdField,
-        trans: ProvidesUserContext = DependsOnTrans,
-    ) -> VerifyCredentialsResponse:
-        return self.service.verify_service_credentials(trans, user_id, user_credentials_id)
-
-    @router.get(
-        "/api/users/{user_id}/credentials/{user_credentials_id}/{credentials_id}",
-        summary="Verifies if a credential have been provided",
-    )
-    def verify_credentials(
-        self,
-        user_id: UserIdPathParam,
-        user_credentials_id: DecodedDatabaseIdField,
-        credentials_id: DecodedDatabaseIdField,
-        trans: ProvidesUserContext = DependsOnTrans,
-    ) -> VerifyCredentialsResponse:
-        return self.service.verify_credentials(trans, user_credentials_id, credentials_id)
+        return self.service.list_user_credentials(trans, user_id, source_type, source_id, group_name)
 
     @router.post(
         "/api/users/{user_id}/credentials",
@@ -87,43 +69,45 @@ class FastAPICredentials:
         user_id: UserIdPathParam,
         payload: CredentialsPayload,
         trans: ProvidesUserContext = DependsOnTrans,
-    ) -> CredentialsListResponse:
+    ) -> UserCredentialCreateResponse:
         return self.service.provide_credential(trans, user_id, payload)
 
-    @router.put(
-        "/api/users/{user_id}/credentials/{user_credentials_id}",
-        summary="Updates credentials for a specific secret/variable",
-    )
-    def update_credential(
-        self,
-        user_id: UserIdPathParam,
-        user_credentials_id: DecodedDatabaseIdField,
-        payload: UpdateCredentialsPayload,
-        trans: ProvidesUserContext = DependsOnTrans,
-    ) -> CredentialsListResponse:
-        return self.service.update_credential(trans, user_id, user_credentials_id, payload)
+    # @router.put(
+    #     "/api/users/{user_id}/credentials/{user_credentials_id}",
+    #     summary="Updates credentials for a specific secret/variable",
+    # )
+    # def update_credential(
+    #     self,
+    #     user_id: UserIdPathParam,
+    #     user_credentials_id: DecodedDatabaseIdField,
+    #     payload: UpdateCredentialsPayload,
+    #     trans: ProvidesUserContext = DependsOnTrans,
+    # ) -> CredentialsListResponse:
+    #     return self.service.update_credential(trans, user_id, user_credentials_id, payload)
 
-    @router.delete(
-        "/api/users/{user_id}/credentials/{user_credentials_id}",
-        summary="Deletes all credentials for a specific service",
-    )
-    def delete_service_credentials(
-        self,
-        user_id: UserIdPathParam,
-        user_credentials_id: DecodedDatabaseIdField,
-        trans: ProvidesUserContext = DependsOnTrans,
-    ) -> DeleteCredentialsResponse:
-        return self.service.delete_service_credentials(trans, user_id, user_credentials_id)
+    # @router.delete(
+    #     "/api/users/{user_id}/credentials/{user_credentials_id}",
+    #     summary="Deletes all credentials for a specific service",
+    # )
+    # def delete_service_credentials(
+    #     self,
+    #     user_id: UserIdPathParam,
+    #     user_credentials_id: DecodedDatabaseIdField,
+    #     trans: ProvidesUserContext = DependsOnTrans,
+    # ):
+    #     self.service.delete_service_credentials(trans, user_id, user_credentials_id)
+    #     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-    @router.delete(
-        "/api/users/{user_id}/credentials/{user_credentials_id}/{credentials_id}",
-        summary="Deletes a specific credential",
-    )
-    def delete_credentials(
-        self,
-        user_id: UserIdPathParam,
-        user_credentials_id: DecodedDatabaseIdField,
-        credentials_id: DecodedDatabaseIdField,
-        trans: ProvidesUserContext = DependsOnTrans,
-    ) -> DeleteCredentialsResponse:
-        return self.service.delete_credentials(trans, user_id, user_credentials_id, credentials_id)
+    # @router.delete(
+    #     "/api/users/{user_id}/credentials/{user_credentials_id}/{group_id}",
+    #     summary="Deletes a specific credential",
+    # )
+    # def delete_credentials(
+    #     self,
+    #     user_id: UserIdPathParam,
+    #     user_credentials_id: DecodedDatabaseIdField,
+    #     group_id: DecodedDatabaseIdField,
+    #     trans: ProvidesUserContext = DependsOnTrans,
+    # ):
+    #     self.service.delete_credentials(trans, user_id, user_credentials_id, group_id)
+    #     return Response(status_code=status.HTTP_204_NO_CONTENT)
