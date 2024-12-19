db672952e915355f802e03ef03b4e0d7bf93dc59
Arash <arash77.kad@gmail.com>
2024-12-19 15:35:52 +0100

    rename variable and secret tables to credential_variable and credential_secret for clarity

--- lib/galaxy/model/__init__.py
+++ lib/galaxy/model/__init__.py
@@ -12169,7 +12169,7 @@ class Variable(Base):
     Represents a variable associated with a user for a specific service.
     """
 
-    __tablename__ = "variable"
+    __tablename__ = "credential_variable"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     user_credential_group_id: Mapped[int] = mapped_column(
@@ -12186,7 +12186,7 @@ class Secret(Base):
     Represents a secret associated with a user for a specific service.
     """
 
-    __tablename__ = "secret"
+    __tablename__ = "credential_secret"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     user_credential_group_id: Mapped[int] = mapped_column(
