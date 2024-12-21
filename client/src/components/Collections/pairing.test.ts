2921d2a044654486a671a3333d962cec08fca75e
John Chilton <jmchilton@gmail.com>
2024-12-21 11:23:17 -0500

    Simplification to paired list creator.

--- client/src/components/Collections/pairing.test.ts
+++ client/src/components/Collections/pairing.test.ts
@@ -1,8 +1,6 @@
-import { guessInitialFilterType } from "./pairing";
+import { guessInitialFilterType, guessNameForPair } from "./pairing";
 
 describe("guessInitialFilterType", () => {
-    beforeEach(() => {});
-
     test("should return 'illumina' when illumina matches are most common", () => {
         const elements = [
             { name: "sample1_1.fastq" },
@@ -17,4 +15,40 @@ describe("guessInitialFilterType", () => {
         const elements = [{ name: "mydata123.fastq" }, { name: "mydata124.fastq" }, { name: "mydata125.fastq" }];
         expect(guessInitialFilterType(elements)).toBe(null);
     });
+});
+
+function mockDataset(name: string) {
+    return { name };
+}
+
+describe("guessNameForPair", () => {
+    test("it should use the LCS and work with _1/_2 filters", () => {
+        expect(guessNameForPair(mockDataset("moo_1.fastq"), mockDataset("moo_2.fastq"), "_1", "_2", true)).toEqual(
+            "moo"
+        );
+    });
+
+    test("it should use the LCS as a name and work with .1.fastq/.2.fastq filters", () => {
+        expect(
+            guessNameForPair(mockDataset("moo.1.fastq"), mockDataset("moo.2.fastq"), ".1.fastq", ".2.fastq", true)
+        ).toEqual("moo");
+    });
+
+    test("it should use the LCS as a name and part of that is cutting out mismatched junk", () => {
+        expect(
+            guessNameForPair(
+                mockDataset("moo_middledifferent_endssame.1.fastq"),
+                mockDataset("moo_yuck_endssame.2.fastq"),
+                ".1.fastq",
+                ".2.fastq",
+                true
+            )
+        ).toEqual("moo__endssame");
+    });
+
+    /* The way the filters and extension stripping are handled is less than ideal, this should work IMO for instance. -jmchilton
+    test("it should use the LCS as a name and part of that is cutting out mismatched junk", () => {
+        expect(guessNameForPair(mockDataset("moo.middledifferent.endssame.1.fastq"), mockDataset("moo.yuck.endssame.2.fastq"), ".1.fastq", ".2.fastq", true)).toEqual("moo.endssame");
+    });
+    */
 });
