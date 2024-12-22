d6a19d00f64cc7c018234126f014de61c9eeba2d
John Chilton <jmchilton@gmail.com>
2024-12-22 14:05:25 -0500

    Refactor showing of extensions into own component.

---
+++ client/src/components/Collections/common/CollectionCreatorShowExtensions.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { BAlert } from "bootstrap-vue";
+
+import localize from "@/utils/localization";
+import { orList } from "@/utils/strings";
+
+import HelpText from "@/components/Help/HelpText.vue";
+
+interface Props {
+    extensions?: string[];
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <div class="d-flex align-items-center justify-content-between">
+        <BAlert v-if="extensions?.length" class="w-100 py-0" variant="secondary" show>
+            <HelpText
+                uri="galaxy.collections.collectionBuilder.filteredExtensions"
+                :text="localize('Filtered extensions: ')" />
+            <strong>{{ orList(extensions) }}</strong>
+        </BAlert>
+    </div>
+</template>
