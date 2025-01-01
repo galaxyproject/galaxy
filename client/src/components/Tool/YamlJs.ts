0efc059047d0d750f7661d3f75fd515c22d6d42a
mvdbeek <m.vandenbeek@gmail.com>
2025-01-01 20:24:48 +0200

    Convert yaml-with-js to typescript

--- client/src/components/Tool/YamlJs.ts
+++ client/src/components/Tool/YamlJs.ts
@@ -1,4 +1,6 @@
 import * as monaco from "monaco-editor";
+import { editor } from "monaco-editor";
+import { type IPosition, type MonacoEditor } from "monaco-types";
 import { configureMonacoYaml } from "monaco-yaml";
 
 import TOOL_SOURCE_SCHEMA from "./ToolSourceSchema.json";
@@ -8,7 +10,7 @@ const LANG = "yaml-with-js";
 
 const embeddedModelUri = monaco.Uri.parse("file://embedded-model.js");
 
-export function setupMonaco(monaco) {
+export function setupMonaco(monaco: MonacoEditor) {
     // Define the custom YAML language with embedded JavaScript
     monaco.languages.register({ id: LANG });
     monaco.languages.register({ id: "typescript" });
@@ -19,12 +21,9 @@ export function setupMonaco(monaco) {
         tokenizer: {
             root: [
                 // Multiline JavaScript block
-                [
-                    /(script|code|javascript):\s*\|/,
-                    { token: "key", nextEmbedded: "javascript", next: "@jsBlockMultiline" },
-                ],
+                [/(expressionLib):\s*\|/, { token: "key", nextEmbedded: "javascript", next: "@jsBlockMultiline" }],
                 // Inline JavaScript: Key followed by code on the same line
-                [/(script|code|javascript):\s*/, { token: "key", nextEmbedded: "javascript", next: "@inlineJs" }],
+                [/(expressionLib):\s*/, { token: "key", nextEmbedded: "javascript", next: "@inlineJs" }],
                 [/.*/, { token: "@rematch", nextEmbedded: "yaml", next: "@yamlRest" }],
             ],
             // Inline JavaScript ends at the end of the line
@@ -117,9 +116,9 @@ export function setupMonaco(monaco) {
     return { dispose, providerFunctions };
 }
 
-export async function setupEditor(editor, providerFunctions) {
+export async function setupEditor(providerFunctions: any) {
     // Virtual model for JavaScript
-    const yamlModel = editor.getModels().find((item) => item.getLanguageId() == LANG);
+    const yamlModel = editor.getModels().find((item) => item.getLanguageId() == LANG)!;
     const embeddedModel = editor.getModel(embeddedModelUri) || editor.createModel("", "typescript", embeddedModelUri);
     setupContentSync(yamlModel, embeddedModel);
     mixJsYamlProviders(providerFunctions);
@@ -137,8 +136,8 @@ export async function setupEditor(editor, providerFunctions) {
     attachDiagnosticsProvider(yamlModel, embeddedModel, providerFunctions.provideMarkerData);
 }
 
-function extractEmbeddedJavaScript(yamlContent) {
-    const scriptRegex = /(script|code|javascript):\s*(?:\|([\s\S]*?)(?=\n\s*\w+:|\n\s*$)|(.+))/g;
+function extractEmbeddedJavaScript(yamlContent: string) {
+    const scriptRegex = /(expressionLib):\s*(?:\|([\s\S]*?)(?=\n\s*\w+:|\n\s*$)|(.+))/g;
     const match = scriptRegex.exec(yamlContent);
     if (match) {
         if (match[2]) {
@@ -150,7 +149,7 @@ function extractEmbeddedJavaScript(yamlContent) {
     return "";
 }
 
-export function setupContentSync(yamlModel, embeddedModel) {
+export function setupContentSync(yamlModel: editor.ITextModel, embeddedModel: editor.ITextModel) {
     // Keep the embedded JavaScript model in sync with the YAML editor
     yamlModel.onDidChangeContent(() => {
         const yamlContent = yamlModel.getValue();
@@ -159,12 +158,12 @@ export function setupContentSync(yamlModel, embeddedModel) {
     });
 }
 
-async function mixJsYamlProviders(yamlProviderFunctions) {
+async function mixJsYamlProviders(yamlProviderFunctions: any) {
     // Complete and hover consume position and return null if not in focus,
     // so execute JS provider, then yaml as fallback
     const yamlProvideCompletionItems = yamlProviderFunctions.provideCompletionItems;
     const yamlProvideHover = yamlProviderFunctions.provideHover;
-    yamlProviderFunctions.provideCompletionItems = async (model, position) => {
+    yamlProviderFunctions.provideCompletionItems = async (model: editor.ITextModel, position: IPosition) => {
         const jsCompletions = await provideCompletionItems(model, position);
         if (jsCompletions?.suggestions?.length > 0) {
             return jsCompletions;
@@ -172,17 +171,17 @@ async function mixJsYamlProviders(yamlProviderFunctions) {
             return await yamlProvideCompletionItems(model, position);
         }
     };
-    yamlProviderFunctions.provideHover = async (model, position) =>
+    yamlProviderFunctions.provideHover = async (model: editor.ITextModel, position: IPosition) =>
         (await provideHover(model, position)) || (await yamlProvideHover(model, position));
 }
 
 // Add IntelliSense for the embedded JavaScript
-async function provideCompletionItems(model, position) {
+async function provideCompletionItems(model: editor.ITextModel, position: IPosition) {
     const yamlContent = model.getValue();
     const embeddedContent = extractEmbeddedJavaScript(yamlContent);
 
     if (embeddedContent) {
-        const embeddedModel = monaco.editor.getModel(embeddedModelUri);
+        const embeddedModel = monaco.editor.getModel(embeddedModelUri)!;
         const embeddedPosition = translateYamlPositionToEmbedded(model, position, embeddedModel, yamlContent);
         if (!embeddedPosition) {
             return null;
@@ -192,15 +191,14 @@ async function provideCompletionItems(model, position) {
 
         const completionInfo = await languageService.getCompletionsAtPosition(
             embeddedModelUri.toString(),
-            embeddedModel.getOffsetAt(embeddedPosition),
-            {}
+            embeddedModel.getOffsetAt(embeddedPosition)
         );
 
         if (completionInfo && completionInfo.entries) {
             const wordInfo = model.getWordUntilPosition(position);
 
             return {
-                suggestions: completionInfo.entries.map((entry) => ({
+                suggestions: completionInfo.entries.map((entry: any) => ({
                     label: entry.name,
                     kind: monaco.languages.CompletionItemKind[entry.kind],
                     insertText: entry.name,
@@ -218,14 +216,18 @@ async function provideCompletionItems(model, position) {
     return { suggestions: [] };
 }
 
-function attachDiagnosticsProvider(yamlModel, embeddedModel, provideMarkerData) {
+function attachDiagnosticsProvider(
+    yamlModel: editor.ITextModel,
+    embeddedModel: editor.ITextModel,
+    provideMarkerData: any
+) {
     monaco.editor.setModelMarkers(yamlModel, "owner", []); // Clear existing markers.
 
     yamlModel.onDidChangeContent(async () => {
         const yamlContent = yamlModel.getValue();
         const embeddedJavaScript = extractEmbeddedJavaScript(yamlContent);
         const yamlMarkers = await provideMarkerData(yamlModel);
-        let jsMarkers = [];
+        let jsMarkers: editor.IMarkerData[] = [];
 
         if (embeddedJavaScript) {
             const worker = await monaco.languages.typescript.getTypeScriptWorker();
@@ -236,20 +238,23 @@ function attachDiagnosticsProvider(yamlModel, embeddedModel, provideMarkerData) 
             jsMarkers = diagnostics.map((diagnostic) => {
                 const startPosition = translateEmbeddedPositionToYaml(
                     embeddedModel,
-                    diagnostic.start,
+                    diagnostic.start!,
                     yamlModel,
                     yamlContent
-                );
+                )!;
                 const endPosition = translateEmbeddedPositionToYaml(
                     embeddedModel,
-                    diagnostic.start + diagnostic.length,
+                    diagnostic.start! + diagnostic.length!,
                     yamlModel,
                     yamlContent
-                );
+                )!;
 
                 return {
                     severity: monaco.MarkerSeverity.Error, // Severity: Error, Warning, or Info
-                    message: diagnostic.messageText,
+                    message:
+                        typeof diagnostic.messageText === "string"
+                            ? diagnostic.messageText
+                            : diagnostic.messageText.messageText,
                     startLineNumber: startPosition.lineNumber,
                     startColumn: startPosition.column,
                     endLineNumber: endPosition.lineNumber,
@@ -261,9 +266,9 @@ function attachDiagnosticsProvider(yamlModel, embeddedModel, provideMarkerData) 
     });
 }
 
-async function provideHover(model, position) {
+async function provideHover(model: editor.ITextModel, position: IPosition) {
     const yamlContent = model.getValue();
-    const embeddedModel = monaco.editor.getModel(embeddedModelUri);
+    const embeddedModel = monaco.editor.getModel(embeddedModelUri)!;
     const embeddedContent = extractEmbeddedJavaScript(yamlContent);
 
     if (embeddedContent) {
@@ -290,7 +295,7 @@ async function provideHover(model, position) {
                     endColumn: position.column,
                 },
                 contents: [
-                    { value: quickInfo.displayParts.map((p) => p.text).join("") },
+                    { value: quickInfo.displayParts.map((p: any) => p.text).join("") },
                     { value: quickInfo.documentation },
                 ],
             };
@@ -299,8 +304,13 @@ async function provideHover(model, position) {
     return null;
 }
 
-function translateEmbeddedPositionToYaml(embeddedModel, embeddedOffset, yamlModel, yamlContent) {
-    const scriptRegex = /(script|code|javascript):\s*(?:\|([\s\S]*?)(?=\n\s*\w+:|\n\s*$)|(.+))/; // No g flag
+function translateEmbeddedPositionToYaml(
+    embeddedModel: editor.ITextModel,
+    embeddedOffset: number,
+    yamlModel: editor.ITextModel,
+    yamlContent: string
+) {
+    const scriptRegex = /(expressionLib):\s*(?:\|([\s\S]*?)(?=\n\s*\w+:|\n\s*$)|(.+))/; // No g flag
     const scriptMatch = yamlContent.match(scriptRegex);
 
     if (!scriptMatch) {
@@ -310,10 +320,10 @@ function translateEmbeddedPositionToYaml(embeddedModel, embeddedOffset, yamlMode
     let scriptStartIndex;
     if (scriptMatch[2]) {
         // Multiline
-        scriptStartIndex = scriptMatch.index + scriptMatch[0].indexOf("|") + 1;
+        scriptStartIndex = scriptMatch.index! + scriptMatch[0].indexOf("|") + 1;
     } else if (scriptMatch[3]) {
         // Inline
-        scriptStartIndex = scriptMatch.index + scriptMatch[0].indexOf(scriptMatch[3]); // Start of the inline code
+        scriptStartIndex = scriptMatch.index! + scriptMatch[0].indexOf(scriptMatch[3]); // Start of the inline code
     } else {
         return null; // Should not happen, but good to have
     }
@@ -322,8 +332,13 @@ function translateEmbeddedPositionToYaml(embeddedModel, embeddedOffset, yamlMode
     return yamlModel.getPositionAt(yamlOffset);
 }
 
-function translateYamlPositionToEmbedded(yamlModel, yamlPosition, embeddedModel, yamlContent) {
-    const scriptRegex = /(script|code|javascript):\s*(?:\|([\s\S]*?)(?=\n\s*\w+:|\n\s*$)|(.+))/; // No g flag
+function translateYamlPositionToEmbedded(
+    yamlModel: editor.ITextModel,
+    yamlPosition: IPosition,
+    embeddedModel: editor.ITextModel,
+    yamlContent: string
+) {
+    const scriptRegex = /(expressionLib):\s*(?:\|([\s\S]*?)(?=\n\s*\w+:|\n\s*$)|(.+))/; // No g flag
     const scriptMatch = yamlContent.match(scriptRegex);
 
     if (!scriptMatch) {
@@ -334,11 +349,11 @@ function translateYamlPositionToEmbedded(yamlModel, yamlPosition, embeddedModel,
     let scriptEndIndex;
     if (scriptMatch[2]) {
         // Multiline
-        scriptStartIndex = scriptMatch.index + scriptMatch[0].indexOf("|") + 1;
-        scriptEndIndex = scriptMatch.index + scriptMatch[0].length; // End of the matched block
+        scriptStartIndex = scriptMatch.index! + scriptMatch[0].indexOf("|") + 1;
+        scriptEndIndex = scriptMatch.index! + scriptMatch[0].length; // End of the matched block
     } else if (scriptMatch[3]) {
         // Inline
-        scriptStartIndex = scriptMatch.index + scriptMatch[0].indexOf(scriptMatch[3]);
+        scriptStartIndex = scriptMatch.index! + scriptMatch[0].indexOf(scriptMatch[3]);
         scriptEndIndex = scriptStartIndex + scriptMatch[3].length;
     } else {
         return null;
