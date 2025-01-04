b99112c3374a0fef57c9d08f0e2bf8f5a8b24a7e
mvdbeek <m.vandenbeek@gmail.com>
2025-01-04 11:27:27 +0200

    Fix DataCollectionParameterModel factory

---
+++ test/unit/tool_util/test_input_models.py
@@ -0,0 +1,12 @@
+from galaxy.tool_util.models import parse_tool
+from galaxy.tool_util.parameters.models import DataCollectionParameterModel
+from galaxy.tool_util.parser.factory import get_tool_source
+from galaxy.tool_util.unittest_utils import functional_test_tool_path
+
+
+def test_input_collection_type():
+    tool_source = get_tool_source(functional_test_tool_path("parameters/gx_data_collection_list.xml"))
+    tool = parse_tool(tool_source)
+    tool_input = tool.inputs[0]
+    assert isinstance(tool_input, DataCollectionParameterModel)
+    assert tool_input.collection_type == "list"
