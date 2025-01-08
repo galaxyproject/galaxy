galaxyproject/galaxy

commit 9695ccc3c45a14b36beccfbaac69036a5ac65c21

More typing of test case parsing - again.

lib/galaxy/tool_util/verify/interactor.py
@@ -40,6 +40,7 @@ from galaxy.tool_util.parser.interface import (
     TestCollectionDef,
     TestCollectionOutputDef,
     TestSourceTestOutputColllection,
+    ToolSourceTestOutputs,
 )
 from galaxy.util import requests
 from galaxy.util.bunch import Bunch
@@ -49,6 +50,8 @@ from galaxy.util.hash_util import (
 )
 from . import verify
 from ._types import (
+    ExpandedToolInputs,
+    ExpandedToolInputsJsonified,
     RequiredDataTablesT,
     RequiredFilesT,
     RequiredLocFileT,
@@ -64,7 +67,7 @@ log = getLogger(__name__)
 VERBOSE_ERRORS = util.asbool(os.environ.get("GALAXY_TEST_VERBOSE_ERRORS", False))
 UPLOAD_ASYNC = util.asbool(os.environ.get("GALAXY_TEST_UPLOAD_ASYNC", True))
 ERROR_MESSAGE_DATASET_SEP = "--------------------------------------"
-DEFAULT_TOOL_TEST_WAIT = int(os.environ.get("GALAXY_TEST_DEFAULT_WAIT", 86400))
+DEFAULT_TOOL_TEST_WAIT: int = int(os.environ.get("GALAXY_TEST_DEFAULT_WAIT", 86400))
 CLEANUP_TEST_HISTORIES = "GALAXY_TEST_NO_CLEANUP" not in os.environ
 DEFAULT_TARGET_HISTORY = os.environ.get("GALAXY_TEST_HISTORY_ID", None)
 
@@ -96,8 +99,8 @@ JobDataCallbackT = Callable[[JobDataT], None]
 
 
 class ValidToolTestDict(TypedDict):
-    inputs: Any
-    outputs: Any
+    inputs: ExpandedToolInputs
+    outputs: ToolSourceTestOutputs
     output_collections: List[TestSourceTestOutputColllection]
     stdout: NotRequired[AssertionList]
     stderr: NotRequired[AssertionList]
@@ -231,7 +234,7 @@ class GalaxyInteractorApi:
         assert response.status_code == 200, f"Non 200 response from tool tests available API. [{response.content}]"
         return response.json()
 
-    def get_tool_tests(self, tool_id: str, tool_version: Optional[str] = None) -> ToolTestDictsT:
+    def get_tool_tests(self, tool_id: str, tool_version: Optional[str] = None) -> List["ToolTestDescriptionDict"]:
         url = f"tools/{tool_id}/test_data"
         params = {"tool_version": tool_version} if tool_version else None
         response = self._get(url, data=params)
@@ -1344,7 +1347,7 @@ def verify_tool(
     client_test_config: Optional[TestConfig] = None,
     skip_with_reference_data: bool = False,
     skip_on_dynamic_param_errors: bool = False,
-    _tool_test_dicts: Optional[ToolTestDictsT] = None,  # extension point only for tests
+    _tool_test_dicts: Optional[List["ToolTestDescriptionDict"]] = None,  # extension point only for tests
 ):
     if resource_parameters is None:
         resource_parameters = {}
@@ -1385,7 +1388,7 @@ def verify_tool(
         return
 
     tool_test_dict.setdefault("maxseconds", maxseconds)
-    testdef = ToolTestDescription(cast(ToolTestDict, tool_test_dict))
+    testdef = ToolTestDescription(tool_test_dict)
     _handle_def_errors(testdef)
 
     created_history = False
@@ -1664,9 +1667,12 @@ class JobOutputsError(AssertionError):
 
 
 class ToolTestDescriptionDict(TypedDict):
+    tool_id: str
+    tool_version: Optional[str]
     name: str
-    inputs: Any
-    outputs: Any
+    test_index: int
+    inputs: ExpandedToolInputsJsonified
+    outputs: ToolSourceTestOutputs
     output_collections: List[TestSourceTestOutputColllection]
     stdout: Optional[AssertionList]
     stderr: Optional[AssertionList]
@@ -1680,10 +1686,139 @@ class ToolTestDescriptionDict(TypedDict):
     required_data_tables: List[Any]
     required_loc_files: List[str]
     error: bool
-    tool_id: str
-    tool_version: Optional[str]
-    test_index: int
     exception: Optional[str]
+    maxseconds: NotRequired[Optional[int]]
+
+
+DEFAULT_NUM_OUTPUTS: Optional[int] = None
+DEFAULT_OUTPUT_COLLECTIONS: List[TestSourceTestOutputColllection] = []
+DEFAULT_REQUIRED_FILES: RequiredFilesT = []
+DEFAULT_REQUIRED_DATA_TABLES: RequiredDataTablesT = []
+DEFAULT_REQUIRED_LOC_FILES: RequiredLocFileT = []
+DEFAULT_COMMAND_LINE: Optional[AssertionList] = []
+DEFAULT_COMMAND_VERSION: Optional[AssertionList] = []
+DEFAULT_STDOUT: Optional[AssertionList] = []
+DEFAULT_STDERR: Optional[AssertionList] = []
+DEFAULT_OUTPUTS: ToolSourceTestOutputs = []
+DEFAULT_EXPECT_EXIT_CODE: Optional[int] = None
+DEFAULT_EXPECT_FAILURE: bool = False
+DEFAULT_EXPECT_TEST_FAILURE: bool = False
+DEFAULT_EXCEPTION: Optional[str] = None
+
+
+def adapt_tool_source_dict(processed_dict: ToolTestDict) -> "ToolTestDescriptionDict":
+    """Convert the dictionaries parsed from tool sources (ToolTestDict) to a ToolTestDescriptionDict.
+
+    ToolTestDescription is used inside and outside of Galaxy, so convert the dictionaries to the format
+    produced by ToolTestDescription.to_dict() and then construct a ToolTestDescription from that.
+    """
+    test_index: int = _get_test_index(processed_dict)
+    name = _get_test_name(processed_dict, test_index)
+    error_in_test_definition = processed_dict["error"]
+
+    exception: Optional[str] = DEFAULT_EXCEPTION
+    output_collections: List[TestSourceTestOutputColllection] = []
+    num_outputs: Optional[int] = DEFAULT_NUM_OUTPUTS
+    required_files: RequiredFilesT = DEFAULT_REQUIRED_FILES
+    required_data_tables: RequiredDataTablesT = DEFAULT_REQUIRED_DATA_TABLES
+    required_loc_files: RequiredLocFileT = DEFAULT_REQUIRED_LOC_FILES
+    command_line: Optional[AssertionList] = DEFAULT_COMMAND_LINE
+    command_version: Optional[AssertionList] = DEFAULT_COMMAND_VERSION
+    stdout: Optional[AssertionList] = DEFAULT_STDERR
+    stderr: Optional[AssertionList] = DEFAULT_STDERR
+    outputs: ToolSourceTestOutputs = DEFAULT_OUTPUTS
+    expect_exit_code: Optional[int] = DEFAULT_EXPECT_EXIT_CODE
+    expect_failure: bool = DEFAULT_EXPECT_FAILURE
+    expect_test_failure: bool = DEFAULT_EXPECT_TEST_FAILURE
+    inputs: ExpandedToolInputsJsonified = {}
+    maxseconds: Optional[int] = None
+
+    if not error_in_test_definition:
+        processed_test_dict = cast(ValidToolTestDict, processed_dict)
+        maxseconds = _get_maxseconds(processed_test_dict)
+        output_collections = processed_test_dict.get("output_collections", [])
+        if "num_outputs" in processed_test_dict and processed_test_dict["num_outputs"]:
+            num_outputs = int(processed_test_dict["num_outputs"])
+
+        required_files = processed_test_dict.get("required_files", DEFAULT_REQUIRED_FILES)
+        required_data_tables = processed_test_dict.get("required_data_tables", DEFAULT_REQUIRED_DATA_TABLES)
+        required_loc_files = processed_test_dict.get("required_loc_files", DEFAULT_REQUIRED_LOC_FILES)
+        command_line = processed_test_dict.get("command_line", DEFAULT_COMMAND_LINE)
+        command_version = processed_test_dict.get("command_version", DEFAULT_COMMAND_VERSION)
+        stdout = processed_test_dict.get("stdout", DEFAULT_STDOUT)
+        stderr = processed_test_dict.get("stderr", DEFAULT_STDERR)
+        outputs = processed_test_dict.get("outputs", DEFAULT_OUTPUTS)
+        raw_expect_exit_code: Optional[Union[str, int]] = processed_test_dict.get(
+            "expect_exit_code", DEFAULT_EXPECT_EXIT_CODE
+        )
+        if raw_expect_exit_code is not None:
+            expect_exit_code = int(raw_expect_exit_code)
+
+        expect_failure = processed_test_dict.get("expect_failure", DEFAULT_EXPECT_FAILURE)
+        expect_test_failure = processed_test_dict.get("expect_test_failure", DEFAULT_EXPECT_TEST_FAILURE)
+        inputs = processed_test_dict.get("inputs", {})
+    else:
+        invalid_test_dict = cast(InvalidToolTestDict, processed_dict)
+        maxseconds = DEFAULT_TOOL_TEST_WAIT
+        exception = invalid_test_dict.get("exception", DEFAULT_EXCEPTION)
+
+    return ToolTestDescriptionDict(
+        test_index=test_index,
+        name=name,
+        error=error_in_test_definition,
+        maxseconds=maxseconds,
+        tool_id=processed_dict["tool_id"],
+        tool_version=processed_dict.get("tool_version"),
+        exception=exception,
+        num_outputs=num_outputs,
+        required_files=required_files,
+        required_data_tables=required_data_tables,
+        required_loc_files=required_loc_files,
+        command_line=command_line,
+        command_version=command_version,
+        stdout=stdout,
+        stderr=stderr,
+        outputs=outputs,
+        output_collections=output_collections,
+        expect_exit_code=expect_exit_code,
+        expect_failure=expect_failure,
+        expect_test_failure=expect_test_failure,
+        inputs=inputs,
+    )
+
+
+def _get_test_index(test_dict: Union[ToolTestDict, ToolTestDescriptionDict]) -> int:
+    assert "test_index" in test_dict, "Invalid processed test description, must have a 'test_index' for naming, etc.."
+    return test_dict["test_index"]
+
+
+def _get_test_name(test_dict: Union[ToolTestDict, ToolTestDescriptionDict], test_index: int) -> str:
+    name = cast(str, test_dict.get("name", f"Test-{test_index + 1}"))
+    return name
+
+
+def _get_maxseconds(test_dict: Union[ToolTestDict, ToolTestDescriptionDict]) -> int:
+    return int(cast(Union[str, int], test_dict.get("maxseconds") or DEFAULT_TOOL_TEST_WAIT or 86400))
+
+
+def expanded_inputs_from_json(expanded_inputs_json: ExpandedToolInputsJsonified) -> ExpandedToolInputs:
+    loaded_inputs: ExpandedToolInputs = {}
+    for key, value in expanded_inputs_json.items():
+        if isinstance(value, dict) and value.get("model_class"):
+            loaded_inputs[key] = TestCollectionDef.from_dict(value)
+        else:
+            loaded_inputs[key] = value
+    return loaded_inputs
+
+
+def expanded_inputs_to_json(expanded_inputs: ExpandedToolInputs) -> ExpandedToolInputsJsonified:
+    inputs_dict: ExpandedToolInputsJsonified = {}
+    for key, value in expanded_inputs.items():
+        if hasattr(value, "to_dict"):
+            inputs_dict[key] = value.to_dict()
+        else:
+            inputs_dict[key] = value
+    return inputs_dict
 
 
 class ToolTestDescription:
@@ -1694,6 +1829,9 @@ class ToolTestDescription:
     """
 
     name: str
+    tool_id: str
+    tool_version: Optional[str]
+    test_index: int
     num_outputs: Optional[int]
     stdout: Optional[AssertionList]
     stderr: Optional[AssertionList]
@@ -1706,69 +1844,38 @@ class ToolTestDescription:
     expect_failure: bool
     expect_test_failure: bool
     exception: Optional[str]
+    inputs: ExpandedToolInputs
+    outputs: ToolSourceTestOutputs
     output_collections: List[TestCollectionOutputDef]
+    maxseconds: Optional[int]
 
-    def __init__(self, processed_test_dict: ToolTestDict):
-        assert (
-            "test_index" in processed_test_dict
-        ), "Invalid processed test description, must have a 'test_index' for naming, etc.."
-        test_index = processed_test_dict["test_index"]
-        name = cast(str, processed_test_dict.get("name", f"Test-{test_index + 1}"))
-        error_in_test_definition = processed_test_dict["error"]
-        num_outputs: Optional[int] = None
-        if not error_in_test_definition:
-            processed_test_dict = cast(ValidToolTestDict, processed_test_dict)
-            maxseconds = int(processed_test_dict.get("maxseconds") or DEFAULT_TOOL_TEST_WAIT or 86400)
-            output_collections = processed_test_dict.get("output_collections", [])
-            if "num_outputs" in processed_test_dict and processed_test_dict["num_outputs"]:
-                num_outputs = int(processed_test_dict["num_outputs"])
-            self.required_files = processed_test_dict.get("required_files", [])
-            self.required_data_tables = processed_test_dict.get("required_data_tables", [])
-            self.required_loc_files = processed_test_dict.get("required_loc_files", [])
-            self.command_line = processed_test_dict.get("command_line", None)
-            self.command_version = processed_test_dict.get("command_version", None)
-            self.stdout = processed_test_dict.get("stdout", None)
-            self.stderr = processed_test_dict.get("stderr", None)
-        else:
-            processed_test_dict = cast(InvalidToolTestDict, processed_test_dict)
-            maxseconds = DEFAULT_TOOL_TEST_WAIT
-            output_collections = []
-            self.required_files = []
-            self.required_data_tables = []
-            self.required_loc_files = []
-            self.command_line = None
-            self.command_version = None
-            self.stdout = None
-            self.stderr = None
+    @staticmethod
+    def from_tool_source_dict(processed_test_dict: ToolTestDict) -> "ToolTestDescription":
+        return ToolTestDescription(adapt_tool_source_dict(processed_test_dict))
 
-        self.test_index = test_index
-        assert (
-            "tool_id" in processed_test_dict
-        ), "Invalid processed test description, must have a 'tool_id' for naming, etc.."
-        self.tool_id = processed_test_dict["tool_id"]
-        self.tool_version = processed_test_dict.get("tool_version")
-        self.name = name
-        self.maxseconds = maxseconds
-
-        inputs = processed_test_dict.get("inputs", {})
-        loaded_inputs = {}
-        for key, value in inputs.items():
-            if isinstance(value, dict) and value.get("model_class"):
-                loaded_inputs[key] = TestCollectionDef.from_dict(value)
-            else:
-                loaded_inputs[key] = value
-
-        self.inputs = loaded_inputs
-        self.outputs = processed_test_dict.get("outputs", [])
-        self.num_outputs = num_outputs
-
-        self.error = processed_test_dict.get("error", False)
-        self.exception = cast(Optional[str], processed_test_dict.get("exception", None))
-
+    def __init__(self, json_dict: ToolTestDescriptionDict):
+        self.test_index = _get_test_index(json_dict)
+        self.name = _get_test_name(json_dict, self.test_index)
+        self.error = json_dict["error"]
+        self.exception = json_dict.get("exception", DEFAULT_EXCEPTION)
+        output_collections = json_dict.get("output_collections", DEFAULT_OUTPUT_COLLECTIONS)
         self.output_collections = [TestCollectionOutputDef.from_dict(d) for d in output_collections]
-        self.expect_exit_code = cast(Optional[int], processed_test_dict.get("expect_exit_code", None))
-        self.expect_failure = cast(bool, processed_test_dict.get("expect_failure", False))
-        self.expect_test_failure = cast(bool, processed_test_dict.get("expect_test_failure", False))
+        self.num_outputs = json_dict.get("num_outputs", DEFAULT_NUM_OUTPUTS)
+        self.required_files = json_dict.get("required_files", DEFAULT_REQUIRED_FILES)
+        self.required_data_tables = json_dict.get("required_data_tables", DEFAULT_REQUIRED_DATA_TABLES)
+        self.required_loc_files = json_dict.get("required_loc_files", DEFAULT_REQUIRED_LOC_FILES)
+        self.command_line = json_dict.get("command_line", DEFAULT_COMMAND_LINE)
+        self.command_version = json_dict.get("command_version", DEFAULT_COMMAND_VERSION)
+        self.stdout = json_dict.get("stdout", DEFAULT_STDOUT)
+        self.stderr = json_dict.get("stderr", DEFAULT_STDERR)
+        self.outputs = json_dict.get("outputs", DEFAULT_OUTPUTS)
+        self.expect_exit_code = json_dict.get("expect_exit_code", DEFAULT_EXPECT_EXIT_CODE)
+        self.expect_failure = json_dict.get("expect_failure", DEFAULT_EXPECT_FAILURE)
+        self.expect_test_failure = json_dict.get("expect_test_failure", DEFAULT_EXPECT_TEST_FAILURE)
+        self.inputs = expanded_inputs_from_json(json_dict.get("inputs", {}))
+        self.tool_id = json_dict["tool_id"]
+        self.tool_version = json_dict.get("tool_version")
+        self.maxseconds = _get_maxseconds(json_dict)
 
     def test_data(self):
         """
@@ -1777,35 +1884,32 @@ class ToolTestDescription:
         return test_data_iter(self.required_files)
 
     def to_dict(self) -> ToolTestDescriptionDict:
-        inputs_dict = {}
-        for key, value in self.inputs.items():
-            if hasattr(value, "to_dict"):
-                inputs_dict[key] = value.to_dict()
-            else:
-                inputs_dict[key] = value
-
-        return {
-            "inputs": inputs_dict,
-            "outputs": self.outputs,
-            "output_collections": [_.to_dict() for _ in self.output_collections],
-            "num_outputs": self.num_outputs,
-            "command_line": self.command_line,
-            "command_version": self.command_version,
-            "stdout": self.stdout,
-            "stderr": self.stderr,
-            "expect_exit_code": self.expect_exit_code,
-            "expect_failure": self.expect_failure,
-            "expect_test_failure": self.expect_test_failure,
-            "name": self.name,
-            "test_index": self.test_index,
-            "tool_id": self.tool_id,
-            "tool_version": self.tool_version,
-            "required_files": self.required_files,
-            "required_data_tables": self.required_data_tables,
-            "required_loc_files": self.required_loc_files,
-            "error": self.error,
-            "exception": self.exception,
-        }
+        inputs = expanded_inputs_to_json(self.inputs)
+        return ToolTestDescriptionDict(
+            {
+                "inputs": inputs,
+                "outputs": self.outputs,
+                "output_collections": [_.to_dict() for _ in self.output_collections],
+                "num_outputs": self.num_outputs,
+                "command_line": self.command_line,
+                "command_version": self.command_version,
+                "stdout": self.stdout,
+                "stderr": self.stderr,
+                "expect_exit_code": self.expect_exit_code,
+                "expect_failure": self.expect_failure,
+                "expect_test_failure": self.expect_test_failure,
+                "name": self.name,
+                "test_index": self.test_index,
+                "tool_id": self.tool_id,
+                "tool_version": self.tool_version,
+                "required_files": self.required_files,
+                "required_data_tables": self.required_data_tables,
+                "required_loc_files": self.required_loc_files,
+                "error": self.error,
+                "exception": self.exception,
+                "maxseconds": self.maxseconds,
+            }
+        )
 
 
 def test_data_iter(required_files):

test/unit/tool_util/test_test_definition_parsing.py
@@ -1,5 +1,6 @@
 """Tool test parsing to dicts logic."""
 
+import json
 import os
 from typing import (
     Any,
@@ -110,6 +111,7 @@ class TestTestParsing(TestCase):
         self._verify_each(test_dicts[1].to_dict(), BIGWIG_TO_WIG_EXPECTATIONS)
 
     def _verify_each(self, target_dict: dict, expectations: List[Any]):
+        assert_json_encodable(target_dict)
         for path, expectation in expectations:
             exception = target_dict.get("exception")
             assert not exception, f"Test failed to generate with exception {exception}"
@@ -120,3 +122,7 @@ class TestTestParsing(TestCase):
         for path_part in expectation_path:
             rest = rest[path_part]
         assert rest == expectation, f"{rest} != {expectation} for {expectation_path}"
+
+
+def assert_json_encodable(as_dict: dict):
+    json.dumps(as_dict)

lib/galaxy/tool_util/parser/interface.py
@@ -18,7 +18,10 @@ from typing import (
 
 import packaging.version
 from pydantic import BaseModel
-from typing_extensions import TypedDict
+from typing_extensions import (
+    NotRequired,
+    TypedDict,
+)
 
 from galaxy.util import Element
 from galaxy.util.path import safe_walk
@@ -43,8 +46,48 @@ class AssertionDict(TypedDict):
 AssertionList = Optional[List[AssertionDict]]
 XmlInt = Union[str, int]
 
-ToolSourceTestInputs = Any
-ToolSourceTestOutputs = Any
+
+class ToolSourceTestOutputAttributes(TypedDict):
+    object: NotRequired[Optional[Any]]
+    compare: str
+    lines_diff: int
+    delta: int
+    delta_frac: Optional[float]
+    sort: bool
+    decompress: bool
+    location: NotRequired[Optional[str]]
+    ftype: NotRequired[Optional[str]]
+    eps: float
+    metric: str
+    pin_labels: Optional[Any]
+    count: Optional[int]
+    metadata: Dict[str, Any]
+    md5: Optional[str]
+    checksum: Optional[str]
+    primary_datasets: Dict[str, Any]
+    elements: Dict[str, Any]
+    assert_list: AssertionList
+    extra_files: List[Dict[str, Any]]
+
+
+class ToolSourceTestOutput(TypedDict):
+    name: str
+    value: Optional[str]
+    attributes: ToolSourceTestOutputAttributes
+
+
+# The unfortunate 'attrib = dict(param_elem.attrib)' makes this difficult to type.
+ToolSourceTestInputAttributes = Dict[str, Any]
+
+
+class ToolSourceTestInput(TypedDict):
+    name: str
+    value: Optional[Any]
+    attributes: ToolSourceTestInputAttributes
+
+
+ToolSourceTestInputs = List[ToolSourceTestInput]
+ToolSourceTestOutputs = List[ToolSourceTestOutput]
 TestSourceTestOutputColllection = Any
 
 

lib/galaxy/tool_util/parser/util.py
@@ -20,6 +20,8 @@ DEFAULT_DELTA_FRAC = None
 DEFAULT_METRIC = "mae"
 DEFAULT_EPS = 0.01
 DEFAULT_PIN_LABELS = None
+DEFAULT_SORT = False
+DEFAULT_DECOMPRESS = False
 
 
 def is_dict(item):

commit 05ede4ab0e11048a542e1ec8dd5fa4daaad40b91

Fix planemo timeout handling
Planemo calls `verify_tool` with a maxseconds value,
and then sets `tool_test_dict.setdefault("maxseconds", maxseconds)`,
which fails, since we are serializing and already setting the default
maxseconds while parsing the test dict.

Fixes the IUC tool test timeouts not working when testing against 24.2.
Broken in
https://github.com/galaxyproject/galaxy/commit/9695ccc3c45a14b36beccfbaac69036a5ac65c21#diff-06835be1d1943ebce8373e2cd1f8e86c7b7813b720398850c978b734dd29e2daR1390
(maxseconds was prviously not serialized at all in
`ToolTestDescription.to_dict`)

lib/galaxy/tool_util/verify/interactor.py
@@ -1688,7 +1688,7 @@ def adapt_tool_source_dict(processed_dict: ToolTestDict) -> ToolTestDescriptionD
 
     if not error_in_test_definition:
         processed_test_dict = cast(ValidToolTestDict, processed_dict)
-        maxseconds = _get_maxseconds(processed_test_dict)
+        maxseconds = processed_test_dict.get("maxseconds")
         output_collections = processed_test_dict.get("output_collections", [])
         if "num_outputs" in processed_test_dict and processed_test_dict["num_outputs"]:
             num_outputs = int(processed_test_dict["num_outputs"])
@@ -1748,10 +1748,6 @@ def _get_test_index(test_dict: Union[ToolTestDict, ToolTestDescriptionDict]) -> 
 def _get_test_name(test_dict: Union[ToolTestDict, ToolTestDescriptionDict], test_index: int) -> str:
     name = cast(str, test_dict.get("name", f"Test-{test_index + 1}"))
     return name
-
-
-def _get_maxseconds(test_dict: Union[ToolTestDict, ToolTestDescriptionDict]) -> int:
-    return int(cast(Union[str, int], test_dict.get("maxseconds") or DEFAULT_TOOL_TEST_WAIT or 86400))
 
 
 def expanded_inputs_from_json(expanded_inputs_json: ExpandedToolInputsJsonified) -> ExpandedToolInputs:
@@ -1829,7 +1825,7 @@ class ToolTestDescription:
         self.inputs = expanded_inputs_from_json(json_dict.get("inputs", {}))
         self.tool_id = json_dict["tool_id"]
         self.tool_version = json_dict.get("tool_version")
-        self.maxseconds = _get_maxseconds(json_dict)
+        self.maxseconds = json_dict.get("maxseconds")
 
     def test_data(self):
         """
@@ -1839,31 +1835,31 @@ class ToolTestDescription:
 
     def to_dict(self) -> ToolTestDescriptionDict:
         inputs = expanded_inputs_to_json(self.inputs)
-        return ToolTestDescriptionDict(
-            {
-                "inputs": inputs,
-                "outputs": self.outputs,
-                "output_collections": [_.to_dict() for _ in self.output_collections],
-                "num_outputs": self.num_outputs,
-                "command_line": self.command_line,
-                "command_version": self.command_version,
-                "stdout": self.stdout,
-                "stderr": self.stderr,
-                "expect_exit_code": self.expect_exit_code,
-                "expect_failure": self.expect_failure,
-                "expect_test_failure": self.expect_test_failure,
-                "name": self.name,
-                "test_index": self.test_index,
-                "tool_id": self.tool_id,
-                "tool_version": self.tool_version,
-                "required_files": self.required_files,
-                "required_data_tables": self.required_data_tables,
-                "required_loc_files": self.required_loc_files,
-                "error": self.error,
-                "exception": self.exception,
-                "maxseconds": self.maxseconds,
-            }
-        )
+        test_description_def: ToolTestDescriptionDict = {
+            "inputs": inputs,
+            "outputs": self.outputs,
+            "output_collections": [_.to_dict() for _ in self.output_collections],
+            "num_outputs": self.num_outputs,
+            "command_line": self.command_line,
+            "command_version": self.command_version,
+            "stdout": self.stdout,
+            "stderr": self.stderr,
+            "expect_exit_code": self.expect_exit_code,
+            "expect_failure": self.expect_failure,
+            "expect_test_failure": self.expect_test_failure,
+            "name": self.name,
+            "test_index": self.test_index,
+            "tool_id": self.tool_id,
+            "tool_version": self.tool_version,
+            "required_files": self.required_files,
+            "required_data_tables": self.required_data_tables,
+            "required_loc_files": self.required_loc_files,
+            "error": self.error,
+            "exception": self.exception,
+        }
+        if self.maxseconds is not None:
+            test_description_def["maxseconds"] = self.maxseconds
+        return ToolTestDescriptionDict(**test_description_def)
 
 
 def test_data_iter(required_files):

test/unit/tool_util/test_test_definition_parsing.py
@@ -68,6 +68,21 @@ class TestTestParsing(TestCase):
         tool_source = get_tool_source(path)
         self.tool_source = tool_source
 
+    def test_maxseconds_not_filled_with_default(self):
+        self._init_tool_for_path(functional_test_tool_path("simple_constructs.xml"))
+        test_dicts = list(self._parse_tests())
+        assert test_dicts[0].maxseconds is None
+        assert "maxseconds" not in test_dicts[0].to_dict()
+
+    def test_maxseconds_parsed(self):
+        self._init_tool_for_path(functional_test_tool_path("maxseconds.xml"))
+        test_def = next(iter(self._parse_tests()))
+        assert test_def.maxseconds == 5
+        test_dict = test_def.to_dict()
+        print(test_dict)
+        assert "maxseconds" in test_dict
+        assert test_dict["maxseconds"] == 5
+
     def test_simple_state_parsing(self):
         self._init_tool_for_path(functional_test_tool_path("simple_constructs.xml"))
         test_dicts = self._parse_tests()