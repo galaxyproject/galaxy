2ba17ffecc3f223848947dfcc776f4e43b6000dc
John Davis <jdavcs@gmail.com>
2025-01-08 18:28:13 -0500

    Fix spelling
    Co-authored-by: Nicola Soranzo <nicola.soranzo@gmail.com>

--- lib/galaxy/model/base.py
+++ lib/galaxy/model/base.py
@@ -52,7 +52,7 @@ def transaction(session: Union[scoped_session, Session, "SessionlessContext"]):
         yield
         return  # exit: can't use as a Session
 
-    yield  # TODO once we can verify this does not break SQLAlchemy transactions, remmove this helper completely (561 instances)
+    yield  # TODO once we can verify this does not break SQLAlchemy transactions, remove this helper completely (561 instances)
     # if not session.in_transaction():  # type:ignore[union-attr]
     #    with session.begin():  # type:ignore[union-attr]
     #        yield
