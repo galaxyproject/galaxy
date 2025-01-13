04f9d8c7f7ab41e8f6ae170b6dcce5afc82f984f
mvdbeek <m.vandenbeek@gmail.com>
2025-01-13 17:48:49 +0100

    Move monarch language config to separate file

--- client/src/components/Tool/YamlJs.ts
+++ client/src/components/Tool/YamlJs.ts
@@ -5,6 +5,7 @@ import { configureMonacoYaml } from "monaco-yaml";
 
 import es5Lib from "@/libs/es5.txt";
 
+import { monarchConfig } from "./MonarchYamlJs";
 import { fetchAndConvertSchemaToInterface } from "./runTimeModel";
 import TOOL_SOURCE_SCHEMA from "./ToolSourceSchema.json";
 import { buildProviderFunctions } from "./yaml";
@@ -22,76 +23,7 @@ export async function setupMonaco(monaco: MonacoEditor) {
     monaco.languages.register({ id: "yaml" });
     const disposables: monaco.IDisposable[] = [];
 
-    disposables.push(
-        monaco.languages.setMonarchTokensProvider(LANG, {
-            tokenizer: {
-                root: [
-                    // Multiline JavaScript block
-                    [/(expressionLib):\s*\|/, { token: "key", nextEmbedded: "javascript", next: "@jsBlockMultiline" }],
-                    // Inline JavaScript: Key followed by code on the same line
-                    [/(expressionLib):\s*/, { token: "key", nextEmbedded: "javascript", next: "@inlineJs" }],
-                    [/(shell_command):\s*/, { token: "key", next: "@shellCommand" }],
-                    [/.*/, { token: "@rematch", nextEmbedded: "yaml", next: "@yamlRest", log: "Match rest" }],
-                ],
-                shellCommand: [
-                    [
-                        /\$\(/, {
-                            token: "@rematch",
-                            bracket: "@open",
-                            next: "jsEmbedded"
-                        }
-                    ],
-                    [
-                        /./, {
-                            cases: {
-                                "@eos": {
-                                    token: "@rematch",
-                                    next: "@pop"
-                                },
-                                "default": {
-                                    token: "string"
-                                }
-                            }
-                        }
-                    ]
-                ],
-                jsEmbedded: [
-                    // Hack? `)` would normally end the JS scope without some clever
-                    // regex that counts how many times we can use `)` before we quit the embedded javascript state.
-                    // The workaround is to open another jsEmbedded state when encountering `(`.
-                    [/\(/, { token: "parens", next: "jsEmbedded" }],
-                    [/\)/, { token: "@rematch", next: "@pop", nextEmbedded: "@pop" }],
-                    [/[^(]+/, {token: "source.js", nextEmbedded: "javascript"}]
-                ],
-                // Inline JavaScript ends at the end of the line
-                inlineJs: [
-                    [
-                        /$/,
-                        {
-                            token: "@rematch",
-                            nextEmbedded: "@pop",
-                            next: "@pop",
-                        },
-                    ],
-                ],
-                jsBlockMultiline: [
-                    [
-                        /^\s*\w+:|^\s*$/,
-                        {
-                            token: "@rematch",
-                            nextEmbedded: "@pop",
-                            next: "@pop",
-                        },
-                    ], // Key or blank line (end condition)
-                ],
-                // Delegate to YAML tokenizer for the rest
-                yamlRest: [
-                    // Include YAML language tokenizer here
-                    [/$/, { token: "@rematch", nextEmbedded: "@pop", next: "@pop" }],
-                ],
-            },
-        })
-    );
+    disposables.push(monaco.languages.setMonarchTokensProvider(LANG, monarchConfig));
     disposables.push(
         monaco.languages.setLanguageConfiguration(LANG, {
             comments: { lineComment: "#" },
