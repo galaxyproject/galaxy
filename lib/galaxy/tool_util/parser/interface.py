f61603e2b29794cc919d01eab956606a729e0fb1
davelopez <46503462+davelopez@users.noreply.github.com>
2025-01-13 17:13:56 +0100

    Add parse_icon method and ToolIcon type for tool icon handling

--- lib/galaxy/tool_util/parser/interface.py
+++ lib/galaxy/tool_util/parser/interface.py
@@ -191,6 +191,10 @@ class ToolSource(metaclass=ABCMeta):
         We parse this out as "" if it isn't explicitly declared.
         """
 
+    @abstractmethod
+    def parse_icon(self) -> Optional["ToolIcon"]:
+        """Return icon for tool."""
+
     def parse_edam_operations(self) -> List[str]:
         """Parse list of edam operation codes."""
         return []
@@ -916,3 +920,8 @@ class DrillDownOptionsDict(TypedDict):
     value: str
     options: List["DrillDownOptionsDict"]
     selected: bool
+
+
+class ToolIcon(TypedDict):
+    src: str
+    alt: str
