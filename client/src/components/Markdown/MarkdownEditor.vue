1c970887e8f84faab5cf99341bf4b34314b18735
guerler <aysam.guerler@gmail.com>
2025-01-14 08:17:20 -0500

    Use markdown help modal within text editor component

--- client/src/components/Markdown/MarkdownEditor.vue
+++ client/src/components/Markdown/MarkdownEditor.vue
@@ -1,26 +1,19 @@
 <template>
-    <TextEditor :title="title" :markdown-text="markdownText" :steps="steps" :mode="mode" />
+    <TextEditor
+        :title="title"
+        :markdown-text="markdownText"
+        :steps="steps"
+        :mode="mode"
+        @update="$emit('update', $event)" />
 </template>
 
 <script setup lang="ts">
 import TextEditor from "./Editor/TextEditor.vue";
 
-defineProps({
-    markdownText: {
-        type: String,
-        default: null,
-    },
-    steps: {
-        type: Object,
-        default: null,
-    },
-    title: {
-        type: String,
-        default: null,
-    },
-    mode: {
-        type: String,
-        default: "report",
-    },
-});
+defineProps<{
+    markdownText: string;
+    steps: Record<string, any>;
+    title: string;
+    mode: "report" | "page";
+}>();
 </script>
