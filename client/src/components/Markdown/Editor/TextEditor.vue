7ad2ed98f46f7f92b16919058e9f9d3942581c9c
guerler <aysam.guerler@gmail.com>
2025-01-14 08:17:20 -0500

    Use markdown help modal within text editor component

--- client/src/components/Markdown/Editor/TextEditor.vue
+++ client/src/components/Markdown/Editor/TextEditor.vue
@@ -26,94 +26,87 @@
                 <div class="unified-panel-body d-flex">
                     <textarea
                         id="workflow-report-editor"
-                        ref="text-area"
+                        ref="textArea"
                         v-model="content"
                         class="markdown-textarea"
                         @input="onUpdate" />
                 </div>
             </div>
         </div>
-        <MarkdownHelpModal ref="help" :mode="mode" />
+        <b-modal v-model="showHelpModal" hide-footer>
+            <template v-slot:modal-title>
+                <h2 v-if="mode === 'page'" class="mb-0">Markdown Help for Pages</h2>
+                <h2 v-else class="mb-0">Markdown Help for Invocation Reports</h2>
+            </template>
+            <MarkdownHelp :mode="mode" />
+        </b-modal>
     </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faQuestion } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import BootstrapVue from "bootstrap-vue";
-import _ from "underscore";
-import Vue from "vue";
+import { debounce } from "lodash";
+import { defineEmits, defineProps, nextTick, ref, watch } from "vue";
 
-import MarkdownHelpModal from "@/components/Markdown/MarkdownHelpModal";
-import MarkdownToolBox from "@/components/Markdown/MarkdownToolBox";
-import FlexPanel from "@/components/Panels/FlexPanel";
-
-Vue.use(BootstrapVue);
+import MarkdownHelp from "@/components/Markdown/MarkdownHelp.vue";
+import MarkdownToolBox from "@/components/Markdown/MarkdownToolBox.vue";
+import FlexPanel from "@/components/Panels/FlexPanel.vue";
 
 library.add(faQuestion);
 
+const props = defineProps<{
+    markdownText: string;
+    mode: "report" | "page";
+    steps: Record<string, any>;
+    title: string;
+}>();
+
+const emit = defineEmits<{
+    (e: "update", value: string): void;
+}>();
+
+const content = ref<string>(props.markdownText);
+const showHelpModal = ref<boolean>(false);
+const textArea = ref<HTMLTextAreaElement | null>(null);
+
 const FENCE = "```";
 
-export default {
-    components: {
-        FlexPanel,
-        FontAwesomeIcon,
-        MarkdownHelpModal,
-        MarkdownToolBox,
-    },
-    props: {
-        markdownText: {
-            type: String,
-            default: null,
-        },
-        steps: {
-            type: Object,
-            default: null,
-        },
-        title: {
-            type: String,
-            default: null,
-        },
-        mode: {
-            type: String,
-            default: "report",
-        },
-    },
-    data() {
-        return {
-            content: this.markdownText,
-        };
-    },
-    watch: {
-        markdownText() {
-            const textArea = this.$refs["text-area"];
-            const textCursor = textArea.selectionEnd;
-            this.content = this.markdownText;
-            Vue.nextTick(() => {
-                textArea.selectionEnd = textCursor;
-                textArea.focus();
-            });
-        },
-    },
-    methods: {
-        insertMarkdown(markdown) {
-            markdown = markdown.replace(")(", ", ");
-            markdown = `${FENCE}galaxy\n${markdown}\n${FENCE}\n`;
-            const textArea = this.$refs["text-area"];
-            textArea.focus();
-            const cursorPosition = textArea.selectionStart;
-            let newContent = this.content.substr(0, cursorPosition);
-            newContent += `\r\n${markdown.trim()}\r\n`;
-            newContent += this.content.substr(cursorPosition);
-            this.$emit("update", newContent);
-        },
-        onUpdate: _.debounce(function (e) {
-            this.$emit("update", this.content);
-        }, 300),
-        onHelp() {
-            this.$refs.help.showMarkdownHelp();
-        },
-    },
-};
+watch(
+    () => props.markdownText,
+    (newValue) => {
+        const textCursor = textArea.value?.selectionEnd || 0;
+        content.value = newValue;
+        nextTick(() => {
+            if (textArea.value) {
+                textArea.value.selectionEnd = textCursor;
+                textArea.value.focus();
+            }
+        });
+    }
+);
+
+function insertMarkdown(markdown: string) {
+    markdown = markdown.replace(")(", ", ");
+    markdown = `${FENCE}galaxy\n${markdown}\n${FENCE}\n`;
+    if (textArea.value) {
+        textArea.value.focus();
+        const cursorPosition = textArea.value.selectionStart;
+        const newContent =
+            content.value?.substr(0, cursorPosition) +
+            `\r\n${markdown.trim()}\r\n` +
+            content.value?.substr(cursorPosition);
+
+        emit("update", newContent || "");
+    }
+}
+
+const onUpdate = debounce((e: Event) => {
+    emit("update", content.value || "");
+}, 300);
+
+function onHelp() {
+    showHelpModal.value = true;
+}
 </script>
