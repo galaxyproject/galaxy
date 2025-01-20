ec32b0eb18b7b78b47af2ab2b0d0a3416a6161f3
Ahmed Awan <qe66653@umbc.edu>
2025-01-20 17:42:38 -0600

    add `workflowRun` version of form elements
    - This drastically changes the styling of `FormElement`s as they have badges (or full colored headers) along with title sections, indicating current status of a step. Each step is in a card like style.
    - The variant for what type of `FormData` option it currently is has text now, letting the user clearly know whether they are selecting a single dataset, multiple etc.
    - After a "browse section" in the `FormData` component, we have a tabs section which shows the users the current selected file(s), as well as providing options for uploading/creating collections etc in other tabs.

---
+++ client/src/components/Form/Elements/FormError.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { faExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { BAlert } from "bootstrap-vue";
+
+const props = defineProps<{
+    alerts: string[];
+    hasAlertClass?: boolean;
+}>();
+</script>
+
+<template>
+    <component :is="props.hasAlertClass ? BAlert : 'span'" show variant="info">
+        <div v-for="(alert, index) in props.alerts" :key="index" class="ui-form-error">
+            <FontAwesomeIcon class="mr-1" :icon="faExclamation" />
+            <!-- eslint-disable-next-line vue/no-v-html -->
+            <span class="ui-form-error-text" v-html="alert" />
+        </div>
+    </component>
+</template>
