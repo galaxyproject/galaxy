776645c9254dbe54af3305c19351452a81e1efc1
Arash <arash77.kad@gmail.com>
2025-01-20 10:45:52 +0100

    Adds session instance to CredentialsManager constructor Moves access checks to CredentialsService for better separation of concerns Refactoring to get the CredentialsManager ready for unit test

--- lib/galaxy/managers/credentials.py
+++ lib/galaxy/managers/credentials.py
@@ -7,12 +7,7 @@ from typing import (
 from sqlalchemy import select
 from sqlalchemy.orm import aliased
 
-from galaxy.exceptions import (
-    AuthenticationRequired,
-    ItemOwnershipException,
-    RequestParameterInvalidException,
-)
-from galaxy.managers.context import ProvidesUserContext
+from galaxy.exceptions import RequestParameterInvalidException
 from galaxy.model import (
     CredentialsGroup,
     Secret,
@@ -23,27 +18,22 @@ from galaxy.model.base import transaction
 from galaxy.model.scoped_session import galaxy_scoped_session
 from galaxy.schema.credentials import SOURCE_TYPE
 from galaxy.schema.fields import DecodedDatabaseIdField
-from galaxy.schema.schema import FlexibleUserIdType
 
 
 class CredentialsManager:
     """Manager object shared by controllers for interacting with credentials."""
 
+    def __init__(self, session: galaxy_scoped_session) -> None:
+        self.session = session
+
     def get_user_credentials(
         self,
-        trans: ProvidesUserContext,
-        user_id: FlexibleUserIdType,
+        user_id: DecodedDatabaseIdField,
         source_type: Optional[SOURCE_TYPE] = None,
         source_id: Optional[str] = None,
         user_credentials_id: Optional[DecodedDatabaseIdField] = None,
         group_id: Optional[DecodedDatabaseIdField] = None,
     ) -> List[Tuple[UserCredentials, CredentialsGroup]]:
-        if trans.anonymous:
-            raise AuthenticationRequired("You need to be logged in to access your credentials.")
-        if user_id == "current":
-            user_id = trans.user.id
-        elif trans.user.id != user_id:
-            raise ItemOwnershipException("You can only access your own credentials.")
         user_cred_alias, group_alias = aliased(UserCredentials), aliased(CredentialsGroup)
         stmt = (
             select(user_cred_alias, group_alias)
@@ -61,27 +51,25 @@ class CredentialsManager:
         if group_id:
             stmt = stmt.where(group_alias.id == group_id)
 
-        result = trans.sa_session.execute(stmt).all()
+        result = self.session.execute(stmt).all()
         return [(row[0], row[1]) for row in result]
 
     def fetch_credentials(
         self,
-        session: galaxy_scoped_session,
         group_id: DecodedDatabaseIdField,
     ) -> Tuple[List[Variable], List[Secret]]:
         variables = list(
-            session.execute(select(Variable).where(Variable.user_credential_group_id == group_id)).scalars().all()
+            self.session.execute(select(Variable).where(Variable.user_credential_group_id == group_id)).scalars().all()
         )
 
         secrets = list(
-            session.execute(select(Secret).where(Secret.user_credential_group_id == group_id)).scalars().all()
+            self.session.execute(select(Secret).where(Secret.user_credential_group_id == group_id)).scalars().all()
         )
 
         return variables, secrets
 
     def add_user_credentials(
         self,
-        session: galaxy_scoped_session,
         db_user_credentials: List[Tuple[UserCredentials, CredentialsGroup]],
         user_id: DecodedDatabaseIdField,
         reference: str,
@@ -96,13 +84,12 @@ class CredentialsManager:
                 source_type=source_type,
                 source_id=source_id,
             )
-            session.add(user_credentials)
-            session.flush()
+            self.session.add(user_credentials)
+            self.session.flush()
         return user_credentials.id
 
     def add_group(
         self,
-        session: galaxy_scoped_session,
         db_user_credentials: List[Tuple[UserCredentials, CredentialsGroup]],
         user_credentials_id: DecodedDatabaseIdField,
         group_name: str,
@@ -114,13 +101,12 @@ class CredentialsManager:
         )
         if not credentials_group:
             credentials_group = CredentialsGroup(name=group_name, user_credentials_id=user_credentials_id)
-            session.add(credentials_group)
-            session.flush()
+            self.session.add(credentials_group)
+            self.session.flush()
         return credentials_group.id
 
     def add_variable(
         self,
-        session: galaxy_scoped_session,
         variables: List[Variable],
         user_credential_group_id: DecodedDatabaseIdField,
         variable_name: str,
@@ -138,11 +124,10 @@ class CredentialsManager:
                 name=variable_name,
                 value=variable_value,
             )
-        session.add(variable)
+        self.session.add(variable)
 
     def add_secret(
         self,
-        session: galaxy_scoped_session,
         secrets: List[Secret],
         user_credential_group_id: DecodedDatabaseIdField,
         secret_name: str,
@@ -160,36 +145,31 @@ class CredentialsManager:
                 name=secret_name,
                 already_set=True if secret_value else False,
             )
-        session.add(secret)
+        self.session.add(secret)
 
-    def commit_session(
-        self,
-        session: galaxy_scoped_session,
-    ) -> None:
-        with transaction(session):
-            session.commit()
+    def commit_session(self) -> None:
+        with transaction(self.session):
+            self.session.commit()
 
     def update_current_group(
         self,
-        trans: ProvidesUserContext,
+        user_id: DecodedDatabaseIdField,
         user_credentials_id: DecodedDatabaseIdField,
         group_name: str,
     ) -> None:
-        db_user_credentials = self.get_user_credentials(trans, trans.user.id, user_credentials_id=user_credentials_id)
+        db_user_credentials = self.get_user_credentials(user_id, user_credentials_id=user_credentials_id)
         for user_credentials, credentials_group in db_user_credentials:
             if credentials_group.name == group_name:
                 user_credentials.current_group_id = credentials_group.id
-                trans.sa_session.add(user_credentials)
+                self.session.add(user_credentials)
                 break
         else:
             raise RequestParameterInvalidException("Group not found to set as current.")
 
     def delete_rows(
         self,
-        session: galaxy_scoped_session,
         rows_to_delete: List,
     ) -> None:
         for row in rows_to_delete:
-            session.delete(row)
-        with transaction(session):
-            session.commit()
+            self.session.delete(row)
+        self.commit_session()
--- lib/galaxy/webapps/galaxy/services/credentials.py
+++ lib/galaxy/webapps/galaxy/services/credentials.py
@@ -8,6 +8,8 @@ from typing import (
 )
 
 from galaxy.exceptions import (
+    AuthenticationRequired,
+    ItemOwnershipException,
     ObjectNotFound,
     RequestParameterInvalidException,
 )
@@ -51,7 +53,8 @@ class CredentialsService:
         source_id: Optional[str] = None,
     ) -> UserCredentialsListResponse:
         """Lists all credentials the user has provided (credentials themselves are not included)."""
-        return self._list_user_credentials(trans, user_id, source_type, source_id)
+        self._check_access(trans, user_id)
+        return self._list_user_credentials(trans, source_type, source_id)
 
     def provide_credential(
         self,
@@ -60,8 +63,9 @@ class CredentialsService:
         payload: CreateSourceCredentialsPayload,
     ) -> UserCredentialsListResponse:
         """Allows users to provide credentials for a group of secrets and variables."""
-        self._create_or_update_credentials(trans, user_id, payload)
-        return self._list_user_credentials(trans, user_id, payload.source_type, payload.source_id)
+        self._check_access(trans, user_id)
+        self._create_or_update_credentials(trans, payload)
+        return self._list_user_credentials(trans, payload.source_type, payload.source_id)
 
     def delete_credentials(
         self,
@@ -71,8 +75,9 @@ class CredentialsService:
         group_id: Optional[DecodedDatabaseIdField] = None,
     ) -> None:
         """Deletes a specific credential group or all credentials for a specific service."""
+        self._check_access(trans, user_id)
         db_user_credentials = self._credentials_manager.get_user_credentials(
-            trans, user_id, user_credentials_id=user_credentials_id, group_id=group_id
+            trans.user.id, user_credentials_id=user_credentials_id, group_id=group_id
         )
         if not db_user_credentials:
             raise ObjectNotFound("No credentials found.")
@@ -84,22 +89,21 @@ class CredentialsService:
                 if credentials_group.name == "default":
                     raise RequestParameterInvalidException("Cannot delete the default group.")
                 if credentials_group.id == uc.current_group_id:
-                    self._credentials_manager.update_current_group(trans, uc.id, "default")
-            variables, secrets = self._credentials_manager.fetch_credentials(trans.sa_session, credentials_group.id)
+                    self._credentials_manager.update_current_group(trans.user.id, uc.id, "default")
+            variables, secrets = self._credentials_manager.fetch_credentials(credentials_group.id)
             rows_to_delete.extend([credentials_group, *variables, *secrets])
-        self._credentials_manager.delete_rows(trans.sa_session, rows_to_delete)
+        self._credentials_manager.delete_rows(rows_to_delete)
 
     def _list_user_credentials(
         self,
         trans: ProvidesUserContext,
-        user_id: FlexibleUserIdType,
         source_type: Optional[SOURCE_TYPE] = None,
         source_id: Optional[str] = None,
     ) -> UserCredentialsListResponse:
-        db_user_credentials = self._credentials_manager.get_user_credentials(trans, user_id, source_type, source_id)
+        db_user_credentials = self._credentials_manager.get_user_credentials(trans.user.id, source_type, source_id)
         credentials_dict = self._map_user_credentials(db_user_credentials)
         for user_credentials, credentials_group in db_user_credentials:
-            variables, secrets = self._credentials_manager.fetch_credentials(trans.sa_session, credentials_group.id)
+            variables, secrets = self._credentials_manager.fetch_credentials(credentials_group.id)
             group = credentials_dict[user_credentials.id]["groups"].get(credentials_group.name, {})
             group["variables"].extend(
                 {"id": variable.id, "name": variable.name, "value": variable.value} for variable in variables
@@ -147,33 +151,31 @@ class CredentialsService:
     def _create_or_update_credentials(
         self,
         trans: ProvidesUserContext,
-        user_id: FlexibleUserIdType,
         payload: CreateSourceCredentialsPayload,
     ) -> None:
-        session = trans.sa_session
         source_type, source_id = payload.source_type, payload.source_id
-        db_user_credentials = self._credentials_manager.get_user_credentials(trans, user_id, source_type, source_id)
+        db_user_credentials = self._credentials_manager.get_user_credentials(trans.user.id, source_type, source_id)
         for service_payload in payload.credentials:
             reference = service_payload.reference
             current_group_name = service_payload.current_group
             if not current_group_name:
                 current_group_name = "default"
             user_credentials_id = self._credentials_manager.add_user_credentials(
-                session, db_user_credentials, trans.user.id, reference, source_type, source_id
+                db_user_credentials, trans.user.id, reference, source_type, source_id
             )
             for group in service_payload.groups:
                 group_name = group.name
                 user_credential_group_id = self._credentials_manager.add_group(
-                    session, db_user_credentials, user_credentials_id, group_name, reference
+                    db_user_credentials, user_credentials_id, group_name, reference
                 )
-                variables, secrets = self._credentials_manager.fetch_credentials(session, user_credential_group_id)
+                variables, secrets = self._credentials_manager.fetch_credentials(user_credential_group_id)
                 user_vault = UserVaultWrapper(self._app.vault, trans.user)
                 for variable_payload in group.variables:
                     variable_name, variable_value = variable_payload.name, variable_payload.value
                     if variable_value is None:
                         continue
                     self._credentials_manager.add_variable(
-                        session, variables, user_credential_group_id, variable_name, variable_value
+                        variables, user_credential_group_id, variable_name, variable_value
                     )
                 for secret_payload in group.secrets:
                     secret_name, secret_value = secret_payload.name, secret_payload.value
@@ -181,9 +183,17 @@ class CredentialsService:
                         continue
                     vault_ref = f"{source_type}|{source_id}|{reference}|{group_name}|{secret_name}"
                     user_vault.write_secret(vault_ref, secret_value)
-                    self._credentials_manager.add_secret(
-                        session, secrets, user_credential_group_id, secret_name, secret_value
-                    )
+                    self._credentials_manager.add_secret(secrets, user_credential_group_id, secret_name, secret_value)
 
-            self._credentials_manager.update_current_group(trans, user_credentials_id, current_group_name)
-        self._credentials_manager.commit_session(session)
+            self._credentials_manager.update_current_group(trans.user.id, user_credentials_id, current_group_name)
+        self._credentials_manager.commit_session()
+
+    def _check_access(
+        self,
+        trans: ProvidesUserContext,
+        user_id: FlexibleUserIdType,
+    ) -> None:
+        if trans.anonymous:
+            raise AuthenticationRequired("You need to be logged in to access your credentials.")
+        if user_id != "current" and user_id != trans.user.id:
+            raise ItemOwnershipException("You can only access your own credentials.")
