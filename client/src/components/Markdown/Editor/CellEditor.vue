4355f38c545e2658587a91292e7ae3e225d282c0
guerler <aysam.guerler@gmail.com>
2025-01-20 14:40:58 -0500

    Start drafting additional cell wrapper options

--- client/src/components/Markdown/Editor/CellEditor.vue
+++ client/src/components/Markdown/Editor/CellEditor.vue
@@ -4,11 +4,38 @@
             <CellButtonAdd :cell-index="cellIndex" @click="onAdd" />
             <hr class="solid m-0" />
             <div class="d-flex my-1 mx-3">
-                <span class="cell-name small text-primary">{{ cell.name }}</span>
+                <div class="d-flex flex-column justify-content-between">
+                    <div class="cell-name small text-primary">
+                        {{ cell.name }}
+                    </div>
+                    <div
+                        v-b-tooltip.right
+                        role="button"
+                        tabindex="0"
+                        class="d-inline text-muted cursor-pointer mt-4 align-self-end"
+                        title="Expand to Edit">
+                        <FontAwesomeIcon class="text-primary" :icon="faAngleDoubleDown" />
+                        <FontAwesomeIcon class="text-primary" :icon="faAngleDoubleUp" />
+                    </div>
+                </div>
                 <div class="ml-2 w-100">
                     <MarkdownDefault v-if="cell.name === 'markdown'" :content="cell.content" />
                     <MarkdownVega v-else-if="cell.name === 'vega'" :content="cell.content" />
                     <MarkdownVitessce v-else-if="cell.name === 'vitessce'" :content="cell.content" />
+                </div>
+            </div>
+            <div class="d-flex my-1 mx-3">
+                <div class="cell-name d-flex flex-column justify-content-between">
+                    <div
+                        v-b-tooltip.right
+                        role="button"
+                        tabindex="0"
+                        class="d-inline text-muted cursor-pointer mt-4 align-self-end"
+                        title="Insert new Cell">
+                        <FontAwesomeIcon class="text-primary" :icon="faTrash" />
+                    </div>
+                </div>
+                <div class="ml-2 w-100">
                     <CellCode :value="cell.content" :mode="getMode(cell.name)" @change="onChange(cellIndex, $event)" />
                 </div>
             </div>
@@ -22,6 +49,9 @@
 import { ref } from "vue";
 
 import { parseMarkdown } from "@/components/Markdown/parse";
+
+import { faTrash, faAngleDoubleDown, faAngleDoubleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import MarkdownDefault from "../Sections/MarkdownDefault.vue";
 import MarkdownVega from "../Sections/MarkdownVega.vue";
