aaafa3e193312324c9ed68935d9ae85b4c56894a
guerler <aysam.guerler@gmail.com>
2025-01-20 10:29:23 -0500

    Add update event handler to code cell

--- client/src/components/Markdown/Editor/CellEditor.vue
+++ client/src/components/Markdown/Editor/CellEditor.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="h-100 w-75 mx-auto">
         <div v-for="(cell, cellIndex) of cells" :key="cellIndex">
-            <CellButtonAdd :cell-index="cellIndex" @click="onClick" />
+            <CellButtonAdd :cell-index="cellIndex" @click="onAdd" />
             <hr class="solid m-0" />
             <div class="d-flex my-1 mx-3">
                 <span class="cell-name small text-primary">{{ cell.name }}</span>
@@ -9,34 +9,34 @@
                     <MarkdownDefault v-if="cell.name === 'markdown'" :content="cell.content" />
                     <MarkdownVega v-else-if="cell.name === 'vega'" :content="cell.content" />
                     <MarkdownVitessce v-else-if="cell.name === 'vitessce'" :content="cell.content" />
-                    <CellCode :model-value="cell.content" :mode="getMode(cell.name)" />
+                    <CellCode :value="cell.content" :mode="getMode(cell.name)" @update="onUpdate(cellIndex, $event)" />
                 </div>
             </div>
             <hr class="solid m-0" />
         </div>
-        <CellButtonAdd :cell-index="cells.length" @click="onClick" />
+        <CellButtonAdd :cell-index="cells.length" @click="onAdd" />
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-
 import { parseMarkdown } from "@/components/Markdown/parse";
-
+import CellButtonAdd from "./CellButtonAdd.vue";
+import CellCode from "./CellCode.vue";
 import MarkdownDefault from "../Sections/MarkdownDefault.vue";
 import MarkdownVega from "../Sections/MarkdownVega.vue";
 import MarkdownVitessce from "../Sections/MarkdownVitessce.vue";
-import CellButtonAdd from "./CellButtonAdd.vue";
-import CellCode from "./CellCode.vue";
-
-const props = defineProps<{
-    markdownText: string;
-}>();
 
 interface CellType {
     name: string;
     content: string;
 }
+
+const props = defineProps<{
+    markdownText: string;
+}>();
+
+const emit = defineEmits(["update"]);
 
 const cells = ref<Array<CellType>>(parseMarkdown(props.markdownText));
 
@@ -50,8 +50,15 @@ function getMode(cellName: string) {
     return "json";
 }
 
-function onClick(cellIndex: number, cellType: string) {
+function onAdd(cellIndex: number, cellType: string) {
     console.log([cellIndex, cellType]);
+}
+
+function onUpdate(cellIndex: number, cellContent: string) {
+    const cell = cells.value?.[cellIndex];
+    if (cell) {
+        cell.content = cellContent;
+    }
 }
 </script>
 
