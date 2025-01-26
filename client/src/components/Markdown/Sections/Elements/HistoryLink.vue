8ceea04a6231a9ad0e7242b513096d786c9db62b
guerler <aysam.guerler@gmail.com>
2025-01-26 13:05:03 -0500

    Decouple history link from requiring prepopulated history summaries, should use stores

--- client/src/components/Markdown/Sections/Elements/HistoryLink.vue
+++ client/src/components/Markdown/Sections/Elements/HistoryLink.vue
@@ -1,10 +1,49 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import axios from "axios";
+//@ts-ignore
+import { errorMessageAsString } from "utils/simple-error";
+import { computed, ref } from "vue";
+
+import { getAppRoot } from "@/onload/loadConfig";
+
+interface Props {
+    historyId: string;
+}
+
+const props = defineProps<Props>();
+
+const imported = ref(false);
+const error = ref<string | null>(null);
+
+const name = ref("");
+const showLink = computed(() => !imported.value && !error.value);
+
+const onClick = async () => {
+    try {
+        await axios.post(`${getAppRoot}/api/histories/${props.historyId}`);
+        imported.value = true;
+    } catch (e) {
+        error.value = errorMessageAsString(e);
+    }
+};
+
+async function fetchName(historyId: string) {
+    try {
+        const { data } = await axios.get(`${getAppRoot()}api/histories/${historyId}`);
+        name.value = data?.name || "";
+    } catch (error) {
+        console.error("Error fetching history name:", error);
+        name.value = "";
+    }
+}
+
+fetchName(props.historyId);
+</script>
+
 <template>
     <div>
-        <b-link
-            v-if="showLink"
-            data-description="history import link"
-            :data-history-id="args.history_id"
-            @click="onClick">
+        <b-link v-if="showLink" data-description="history import link" :data-history-id="historyId" @click="onClick">
             Click to Import History: {{ name }}.
         </b-link>
         <div v-if="imported" class="text-success">
@@ -18,37 +57,3 @@
         </div>
     </div>
 </template>
-
-<script setup lang="ts">
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import axios from "axios";
-//@ts-ignore
-import { withPrefix } from "utils/redirect";
-//@ts-ignore
-import { errorMessageAsString } from "utils/simple-error";
-import { computed, ref } from "vue";
-
-interface Props {
-    args: {
-        history_id: string;
-    };
-    histories: Record<string, { name: string }>;
-}
-
-const props = defineProps<Props>();
-
-const imported = ref(false);
-const error = ref<string | null>(null);
-
-const name = computed(() => props.histories[props.args.history_id]?.name || "");
-const showLink = computed(() => !imported.value && !error.value);
-
-const onClick = async () => {
-    try {
-        await axios.post(withPrefix("/api/histories"), { history_id: props.args.history_id });
-        imported.value = true;
-    } catch (e) {
-        error.value = errorMessageAsString(e);
-    }
-};
-</script>
