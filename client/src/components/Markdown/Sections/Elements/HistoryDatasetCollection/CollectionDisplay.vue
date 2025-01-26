e6cbd85419ccd38d1986a048007c3ec0f1e16ccd
guerler <aysam.guerler@gmail.com>
2025-01-26 14:34:08 -0500

    Convert collection display component to composition api

--- client/src/components/Markdown/Sections/Elements/HistoryDatasetCollection/CollectionDisplay.vue
+++ client/src/components/Markdown/Sections/Elements/HistoryDatasetCollection/CollectionDisplay.vue
@@ -43,81 +43,65 @@
     </b-card>
 </template>
 
-<script>
+<script setup lang="ts">
 import axios from "axios";
-import LoadingSpan from "components/LoadingSpan";
-import { copyCollection } from "components/Markdown/services";
-import { getAppRoot } from "onload/loadConfig";
-import { mapState } from "pinia";
+import { computed, onMounted, ref } from "vue";
 
+import { copyCollection } from "@/components/Markdown/services";
+import { getAppRoot } from "@/onload/loadConfig";
 import { useHistoryStore } from "@/stores/historyStore";
 import { useUserStore } from "@/stores/userStore";
 
-import CollectionTree from "./CollectionTree";
+import CollectionTree from "./CollectionTree.vue";
+import LoadingSpan from "@/components/LoadingSpan.vue";
 
-export default {
-    components: {
-        CollectionTree,
-        LoadingSpan,
-    },
-    props: {
-        collectionId: {
-            type: Object,
-            default: null,
-        },
-    },
-    data() {
-        return {
-            itemContent: null,
-            loading: true,
-            messageText: null,
-            messageVariant: null,
-        };
-    },
-    computed: {
-        ...mapState(useUserStore, ["currentUser"]),
-        ...mapState(useHistoryStore, ["currentHistoryId"]),
-        itemName() {
-            return this.itemContent?.name;
-        },
-        itemUrl() {
-            return `${getAppRoot()}api/dataset_collections/${this.collectionId}`;
-        },
-        downloadUrl() {
-            return `${getAppRoot()}api/dataset_collections/${this.collectionId}/download`;
-        },
-    },
-    created() {
-        this.getContent().then((data) => {
-            this.itemContent = data;
-            this.loading = false;
-        });
-    },
-    methods: {
-        onCopyCollection(currentHistoryId) {
-            const hdcaId = this.collectionId;
-            copyCollection(hdcaId, currentHistoryId).then(
-                (response) => {
-                    this.messageVariant = "success";
-                    this.messageText = "Successfully copied to current history.";
-                },
-                (error) => {
-                    this.messageVariant = "danger";
-                    this.messageText = error;
-                }
-            );
-        },
-        async getContent() {
-            try {
-                const response = await axios.get(this.itemUrl);
-                return response.data;
-            } catch (e) {
-                return `Failed to retrieve content. ${e}`;
-            }
-        },
-    },
+const props = defineProps<{
+    collectionId: string;
+}>();
+
+const userStore = useUserStore();
+const historyStore = useHistoryStore();
+
+const currentUser = computed(() => userStore.currentUser);
+const currentHistoryId = computed(() => historyStore.currentHistoryId);
+
+const itemContent = ref<any>(null);
+const loading = ref(true);
+const messageText = ref<string | null>(null);
+const messageVariant = ref<string | null>(null);
+
+const itemName = computed(() => itemContent.value?.name || "");
+const itemUrl = computed(() => `${getAppRoot()}api/dataset_collections/${props.collectionId}`);
+const downloadUrl = computed(() => `${getAppRoot()}api/dataset_collections/${props.collectionId}/download`);
+
+const onCopyCollection = async (currentHistoryId: string) => {
+    try {
+        await copyCollection(props.collectionId, currentHistoryId);
+        messageVariant.value = "success";
+        messageText.value = "Successfully copied to current history.";
+    } catch (error) {
+        messageVariant.value = "danger";
+        messageText.value = error as string;
+    }
 };
+
+const getContent = async () => {
+    try {
+        const response = await axios.get(itemUrl.value);
+        itemContent.value = response.data;
+    } catch (e) {
+        messageVariant.value = "danger";
+        messageText.value = `Failed to retrieve content. ${e}`;
+    } finally {
+        loading.value = false;
+    }
+};
+
+onMounted(() => {
+    getContent();
+});
 </script>
+
 <style scoped>
 .content-height {
     max-height: 15rem;
