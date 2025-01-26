f4203f125f511fd83bf1ea44af801323fd2b7c70
guerler <aysam.guerler@gmail.com>
2025-01-26 12:51:52 -0500

    Convert markdown galaxy history link component to composition api

--- client/src/components/Markdown/Sections/Elements/HistoryLink.vue
+++ client/src/components/Markdown/Sections/Elements/HistoryLink.vue
@@ -4,9 +4,9 @@
             v-if="showLink"
             data-description="history import link"
             :data-history-id="args.history_id"
-            @click="onClick"
-            >Click to Import History: {{ name }}.</b-link
-        >
+            @click="onClick">
+            Click to Import History: {{ name }}.
+        </b-link>
         <div v-if="imported" class="text-success">
             <FontAwesomeIcon icon="check" class="mr-1" />
             <span>Successfully Imported History: {{ name }}!</span>
@@ -19,55 +19,36 @@
     </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import axios from "axios";
-import BootstrapVue from "bootstrap-vue";
+//@ts-ignore
 import { withPrefix } from "utils/redirect";
+//@ts-ignore
 import { errorMessageAsString } from "utils/simple-error";
-import Vue from "vue";
+import { computed, ref } from "vue";
 
-Vue.use(BootstrapVue);
+interface Props {
+    args: {
+        history_id: string;
+    };
+    histories: Record<string, { name: string }>;
+}
 
-export default {
-    components: {
-        FontAwesomeIcon,
-    },
-    props: {
-        args: {
-            type: Object,
-            default: null,
-        },
-        histories: {
-            type: Object,
-            required: true,
-        },
-    },
-    data() {
-        return {
-            imported: false,
-            error: false,
-        };
-    },
-    computed: {
-        name() {
-            return this.histories[this.args.history_id].name;
-        },
-        showLink() {
-            return !this.imported && !this.error;
-        },
-    },
-    methods: {
-        onClick() {
-            axios
-                .post(withPrefix("/api/histories"), { history_id: this.args.history_id })
-                .then(() => {
-                    this.imported = true;
-                })
-                .catch((e) => {
-                    this.error = errorMessageAsString(e);
-                });
-        },
-    },
+const props = defineProps<Props>();
+
+const imported = ref(false);
+const error = ref<string | null>(null);
+
+const name = computed(() => props.histories[props.args.history_id]?.name || "");
+const showLink = computed(() => !imported.value && !error.value);
+
+const onClick = async () => {
+    try {
+        await axios.post(withPrefix("/api/histories"), { history_id: props.args.history_id });
+        imported.value = true;
+    } catch (e) {
+        error.value = errorMessageAsString(e);
+    }
 };
 </script>
