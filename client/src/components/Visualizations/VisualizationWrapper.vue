63ab6d68d2a6668766dcc3c0d62365403094869c
guerler <aysam.guerler@gmail.com>
2025-01-28 10:45:59 -0500

    Add error handling to visualization wrapper

--- client/src/components/Visualizations/VisualizationWrapper.vue
+++ client/src/components/Visualizations/VisualizationWrapper.vue
@@ -11,61 +11,74 @@ interface Props {
 
 const props = defineProps<Props>();
 
+const errorMessage = ref("");
 const iframeRef = ref<HTMLIFrameElement | null>(null);
 
 onMounted(async () => {
     if (!props.name) {
-        console.error("Plugin name is required!");
+        errorMessage.value = "Visualization name is required!";
         return;
     }
 
-    const { data: plugin } = await axios.get(`${getAppRoot()}api/plugins/${props.name}`);
-    const pluginPath = `${getAppRoot()}static/plugins/visualizations/${props.name}/static/`;
-    const dataIncoming = {
-        root: getAppRoot(),
-        visualization_plugin: plugin,
-        visualization_config: props.config,
-    };
-    const iframe = iframeRef.value;
-    if (iframe) {
-        // Verify document existence
-        const iframeDocument = iframe.contentDocument;
-        if (!iframeDocument) {
-            console.error("Failed to access iframe document.");
-            return;
-        }
+    try {
+        const { data: plugin } = await axios.get(`${getAppRoot()}api/plugins/${props.name}`);
 
-        // Create the container for the plugin
-        const container = iframeDocument.createElement("div");
-        container.id = "app";
-        container.setAttribute("data-incoming", JSON.stringify(dataIncoming));
-        iframeDocument.body.appendChild(container);
+        const pluginPath = `${getAppRoot()}static/plugins/visualizations/${props.name}/static/`;
+        const dataIncoming = {
+            root: getAppRoot(),
+            visualization_plugin: plugin,
+            visualization_config: props.config,
+        };
 
-        // Inject the script tag for the plugin
-        if (plugin?.entry_point?.attr?.src) {
-            const script = iframeDocument.createElement("script");
-            script.type = "module";
-            script.src = `${pluginPath}${plugin.entry_point.attr.src}`;
-            iframeDocument.body.appendChild(script);
+        const iframe = iframeRef.value;
+        if (iframe) {
+            // Verify document existence
+            const iframeDocument = iframe.contentDocument;
+            if (!iframeDocument) {
+                errorMessage.value = "Failed to access iframe document.";
+                return;
+            }
+
+            // Create the container for the plugin
+            const container = iframeDocument.createElement("div");
+            container.id = "app";
+            container.setAttribute("data-incoming", JSON.stringify(dataIncoming));
+            iframeDocument.body.appendChild(container);
+
+            // Inject the script tag for the plugin
+            if (plugin?.entry_point?.attr?.src) {
+                const script = iframeDocument.createElement("script");
+                script.type = "module";
+                script.src = `${pluginPath}${plugin.entry_point.attr.src}`;
+                iframeDocument.body.appendChild(script);
+            } else {
+                const error = iframeDocument.createElement("div");
+                error.innerHTML = `Unable to locate plugin module for: ${props.name}.`;
+                iframeDocument.body.appendChild(error);
+            }
+
+            // Add a CSS link to the iframe document
+            if (plugin?.entry_point?.attr?.css) {
+                const link = iframeDocument.createElement("link");
+                link.rel = "stylesheet";
+                link.href = `${pluginPath}${plugin.entry_point.attr.css}`;
+                iframeDocument.head.appendChild(link);
+            }
+            errorMessage.value = "";
         } else {
-            const error = iframeDocument.createElement("div");
-            error.innerHTML = `Unable to locate plugin module for: ${props.name}.`;
-            iframeDocument.body.appendChild(error);
+            errorMessage.value = "Frame has been invalidated.";
         }
-
-        // Add a CSS link to the iframe document
-        if (plugin?.entry_point?.attr?.css) {
-            const link = iframeDocument.createElement("link");
-            link.rel = "stylesheet";
-            link.href = `${pluginPath}${plugin.entry_point.attr.css}`;
-            iframeDocument.head.appendChild(link);
-        }
+    } catch (e) {
+        errorMessage.value = `Visualization '${props.name}' not available.`;
     }
 });
 </script>
 
 <template>
-    <iframe ref="iframeRef" title="visualization" class="visualization-wrapper"></iframe>
+    <div v-if="errorMessage">
+        <b-alert v-if="errorMessage" variant="danger" show>{{ errorMessage }}</b-alert>
+    </div>
+    <iframe v-else ref="iframeRef" title="visualization" class="visualization-wrapper"></iframe>
 </template>
 
 <style>
