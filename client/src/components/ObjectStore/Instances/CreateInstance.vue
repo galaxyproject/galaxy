484ca1e2af2f090fd2ee982c5096bf1f258b1a61
Alireza Heidari <itisalirh@gmail.com>
2025-01-31 15:20:17 +0100

    Adds heading with navigation links to CreateInstance
    Introduces a new heading component with navigation links for better user navigation.
    Imports necessary components and updates the template structure.

--- client/src/components/ObjectStore/Instances/CreateInstance.vue
+++ client/src/components/ObjectStore/Instances/CreateInstance.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserGear } from "font-awesome-6";
 import { computed } from "vue";
+import { RouterLink } from "vue-router";
 
 import { useObjectStoreTemplatesStore } from "@/stores/objectStoreTemplatesStore";
 
@@ -7,6 +10,7 @@ import { useInstanceRouting } from "./routing";
 import type { UserConcreteObjectStore } from "./types";
 
 import CreateForm from "./CreateForm.vue";
+import Heading from "@/components/Common/Heading.vue";
 import LoadingSpan from "@/components/LoadingSpan.vue";
 
 interface Props {
@@ -28,6 +32,18 @@ async function onCreated(objectStore: UserConcreteObjectStore) {
 
 <template>
     <div>
+        <div class="d-flex">
+            <Heading h1 separator inline size="xl" class="flex-grow-1 mb-2">
+                <RouterLink to="/user">
+                    <FontAwesomeIcon v-b-tooltip.hover.noninteractive :icon="faUserGear" title="User preferences" />
+                </RouterLink>
+                /
+                <RouterLink to="/object_store_instances/index"> Storage Locations</RouterLink> /
+                <RouterLink to="/object_store_instances/create"> Templates</RouterLink>
+                / {{ template?.name || "Template" }}
+            </Heading>
+        </div>
+
         <LoadingSpan v-if="!template" message="Loading storage location templates" />
         <CreateForm v-else :template="template" @created="onCreated"></CreateForm>
     </div>
