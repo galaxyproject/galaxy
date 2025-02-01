af8501cfc5fb9e167b739e935d8d40010cb2714f
guerler <aysam.guerler@gmail.com>
2025-01-31 22:56:58 -0500

    Align guide element styles

--- client/src/components/Markdown/Editor/CellAdd.vue
+++ client/src/components/Markdown/Editor/CellAdd.vue
@@ -1,33 +1,27 @@
 <template>
     <Popper trigger="click" placement="right" mode="light">
         <template v-slot:reference>
-            <CellButton title="Insert new Cell">
-                <FontAwesomeIcon :icon="faPlusSquare" />
-            </CellButton>
+            <div class="cell-guide d-flex flex-column justify-content-between">
+                <CellButton title="Insert new Cell">
+                    <FontAwesomeIcon :icon="faPlus" />
+                </CellButton>
+            </div>
         </template>
         <div class="cursor-pointer">
             <CellOption
                 title="Markdown"
                 description="Markdown text element"
-                :icon="faPlusSquare"
+                :icon="faPlus"
                 @click="onClick('markdown')" />
-            <CellOption
-                title="Galaxy Element"
-                description="Galaxy element"
-                :icon="faPlusSquare"
-                @click="onClick('galaxy')" />
-            <CellOption title="Vega" description="Vega Graphics" :icon="faPlusSquare" @click="onClick('vega')" />
-            <CellOption
-                title="Vitessce"
-                description="Vitessce Graphics"
-                :icon="faPlusSquare"
-                @click="onClick('vitessce')" />
+            <CellOption title="Galaxy Element" description="Galaxy element" :icon="faPlus" @click="onClick('galaxy')" />
+            <CellOption title="Vega" description="Vega Graphics" :icon="faPlus" @click="onClick('vega')" />
+            <CellOption title="Vitessce" description="Vitessce Graphics" :icon="faPlus" @click="onClick('vitessce')" />
         </div>
     </Popper>
 </template>
 
 <script setup lang="ts">
-import { faPlusSquare } from "@fortawesome/free-regular-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import CellButton from "./CellButton.vue";
