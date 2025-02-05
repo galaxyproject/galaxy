6d9bad0488d6005fa742d777ce8e559c673f252a
Arash <arash77.kad@gmail.com>
2025-02-05 16:38:19 +0100

    Refactor ToolApp and evaluation logic to utilize StructuredApp attributes for improved credential management

--- lib/galaxy/structured_app.py
+++ lib/galaxy/structured_app.py
@@ -93,8 +93,6 @@ class MinimalToolApp(Protocol):
     tool_data_tables: "ToolDataTableManager"
     file_sources: ConfiguredFileSources
     security: IdEncodingHelper
-    vault: Vault
-    install_model: ModelMapping
 
 
 class MinimalApp(BasicSharedApp):
