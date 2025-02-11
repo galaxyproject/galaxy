a28f256a90ae5ff1bd2ace6c4cba2f0970d7aa10
John Chilton <jmchilton@gmail.com>
2025-02-11 12:04:35 -0500

    Workflow readme migration.

---
+++ lib/galaxy/model/migrations/alembic/versions_gxy/71eeb8d91f92_workflow_readme.py
@@ -0,0 +1,34 @@
+"""Implement fields needed for a workflow readme & help.
+
+Revision ID: 71eeb8d91f92
+Revises: a4c3ef999ab5
+Create Date: 2025-02-11 11:57:14.477337
+
+"""
+
+import sqlalchemy as sa
+
+from galaxy.model.migrations.util import (
+    add_column,
+    drop_column,
+)
+
+workflow_table_name = "workflow"
+
+# revision identifiers, used by Alembic.
+revision = "71eeb8d91f92"
+down_revision = "a4c3ef999ab5"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    add_column(workflow_table_name, sa.Column("readme", sa.TEXT, nullable=True))
+    add_column(workflow_table_name, sa.Column("help", sa.TEXT, nullable=True))
+    add_column(workflow_table_name, sa.Column("logo_url", sa.TEXT, nullable=True))
+
+
+def downgrade():
+    drop_column(workflow_table_name, "readme")
+    drop_column(workflow_table_name, "help")
+    drop_column(workflow_table_name, "logo_url")
