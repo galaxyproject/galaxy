63787972f87810f6e4b11965aa3a1f2b74072a82
guerler <aysam.guerler@gmail.com>
2025-02-13 13:41:24 -0500

    Modularize data attachment selectors

--- client/src/components/Markdown/Editor/CellConfigure.vue
+++ client/src/components/Markdown/Editor/CellConfigure.vue
@@ -9,10 +9,11 @@
                 <FontAwesomeIcon :icon="faTimes" class="fa-lg" />
             </BButton>
         </div>
+        <b-alert v-if="errorMessage" variant="danger" show>{{ errorMessage }}</b-alert>
         <ConfigureGalaxy v-if="name === 'galaxy'" :content="content" @change="$emit('change', $event)" />
         <ConfigureVisualization
             v-else-if="name === 'visualization'"
-            :content="content"
+            :content-object="contentObject"
             @change="$emit('change', $event)" />
         <b-alert v-else variant="warning" show> Data cannot be linked to this cell type. </b-alert>
     </div>
@@ -21,11 +22,12 @@
 <script setup lang="ts">
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { ref, watch } from "vue";
 
 import ConfigureGalaxy from "./configurations/ConfigureGalaxy.vue";
 import ConfigureVisualization from "./configurations/ConfigureVisualization.vue";
 
-defineProps<{
+const props = defineProps<{
     name: string;
     content: string;
 }>();
@@ -34,4 +36,22 @@ defineEmits<{
     (e: "cancel"): void;
     (e: "change", content: string): void;
 }>();
+
+const contentObject = ref();
+const errorMessage = ref("");
+
+function parseContent() {
+    try {
+        contentObject.value = JSON.parse(props.content);
+        errorMessage.value = "";
+    } catch (e) {
+        errorMessage.value = `Failed to parse: ${e}`;
+    }
+}
+
+watch(
+    () => props.content,
+    () => parseContent(),
+    { immediate: true }
+);
 </script>
--- client/src/components/Markdown/Editor/configurations/ConfigureVisualization.vue
+++ client/src/components/Markdown/Editor/configurations/ConfigureVisualization.vue
@@ -1,21 +1,14 @@
 <template>
-    <div class="my-4">
-        <b-alert v-if="errorMessage" variant="danger" show>{{ errorMessage }}</b-alert>
-        <div v-else>
-            <label class="form-label">Select a dataset:</label>
-            <Multiselect v-model="currentValue" label="name" :options="options" @search-change="search" />
-        </div>
-    </div>
+    <ConfigureSelector :content-object="contentObject" @change="onChange($event)" />
 </template>
 
 <script setup lang="ts">
-import { debounce } from "lodash";
-import { computed, type Ref, ref, watch } from "vue";
-import Multiselect from "vue-multiselect";
+import ConfigureSelector from "./ConfigureSelector.vue";
 
-import { GalaxyApi } from "@/api";
-
-const DELAY = 300;
+interface ContentType {
+    dataset_id: string;
+    dataset_name?: string;
+}
 
 interface OptionType {
     id: string | null | undefined;
@@ -23,64 +16,15 @@ interface OptionType {
 }
 
 const props = defineProps<{
-    content: string;
+    contentObject: ContentType;
 }>();
 
 const emit = defineEmits<{
     (e: "change", content: string): void;
 }>();
 
-const errorMessage = ref("");
-const options: Ref<Array<OptionType>> = ref([]);
-
-const currentContent = ref();
-
-const currentValue = computed({
-    get: () => ({
-        id: currentContent.value?.dataset_id,
-        name: currentContent.value?.dataset_name || currentContent.value?.dataset_id,
-    }),
-    set: (newValue) => {
-        const newValues = { ...currentContent.value, dataset_id: newValue.id, dataset_name: newValue.name };
-        emit("change", JSON.stringify(newValues, null, 4));
-    },
-});
-
-const search = debounce(async (query: string = "") => {
-    if (!errorMessage.value) {
-        const { data, error } = await GalaxyApi().GET("/api/datasets", {
-            params: {
-                query: {
-                    q: ["name-contains"],
-                    qv: [query],
-                    offset: 0,
-                    limit: 50,
-                },
-            },
-        });
-        if (error) {
-            errorMessage.value = error.err_msg;
-        } else {
-            errorMessage.value = "";
-            options.value = data.map((d) => ({ id: d.id, name: d.name }));
-        }
-    }
-}, DELAY);
-
-function parseContent() {
-    try {
-        currentContent.value = JSON.parse(props.content);
-        errorMessage.value = "";
-    } catch (e) {
-        errorMessage.value = `Failed to parse: ${e}`;
-    }
+function onChange(newValue: OptionType) {
+    const newValues = { ...props.contentObject, dataset_id: newValue.id, dataset_name: newValue.name };
+    emit("change", JSON.stringify(newValues, null, 4));
 }
-
-parseContent();
-search();
-
-watch(
-    () => props.content,
-    () => parseContent()
-);
 </script>
