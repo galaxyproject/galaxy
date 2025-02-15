3643b06dd8b735e8280e3018a087e7c034694cd3
guerler <aysam.guerler@gmail.com>
2025-02-15 15:39:44 -0500

    Add handler for Galaxy cells

--- client/src/components/Markdown/Editor/CellConfigure.vue
+++ client/src/components/Markdown/Editor/CellConfigure.vue
@@ -9,11 +9,10 @@
                 <FontAwesomeIcon :icon="faTimes" class="fa-lg" />
             </BButton>
         </div>
-        <b-alert v-if="errorMessage" variant="danger" show>{{ errorMessage }}</b-alert>
         <ConfigureGalaxy v-if="name === 'galaxy'" :content="content" @change="$emit('change', $event)" />
         <ConfigureVisualization
             v-else-if="name === 'visualization'"
-            :content-object="contentObject"
+            :content="content"
             @change="$emit('change', $event)" />
         <b-alert v-else variant="warning" show> Data cannot be linked to this cell type. </b-alert>
     </div>
@@ -22,12 +21,11 @@
 <script setup lang="ts">
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { ref, watch } from "vue";
 
-import ConfigureGalaxy from "./configurations/ConfigureGalaxy.vue";
-import ConfigureVisualization from "./configurations/ConfigureVisualization.vue";
+import ConfigureGalaxy from "./Configurations/ConfigureGalaxy.vue";
+import ConfigureVisualization from "./Configurations/ConfigureVisualization.vue";
 
-const props = defineProps<{
+defineProps<{
     name: string;
     content: string;
 }>();
@@ -36,22 +34,4 @@ defineEmits<{
     (e: "cancel"): void;
     (e: "change", content: string): void;
 }>();
-
-const contentObject = ref();
-const errorMessage = ref("");
-
-function parseContent() {
-    try {
-        contentObject.value = JSON.parse(props.content);
-        errorMessage.value = "";
-    } catch (e) {
-        errorMessage.value = `Failed to parse: ${e}`;
-    }
-}
-
-watch(
-    () => props.content,
-    () => parseContent(),
-    { immediate: true }
-);
 </script>
--- client/src/components/Markdown/Editor/configurations/ConfigureVisualization.vue
+++ client/src/components/Markdown/Editor/configurations/ConfigureVisualization.vue
@@ -1,30 +1,48 @@
 <template>
-    <ConfigureSelector :content-object="contentObject" @change="onChange($event)" />
+    <b-alert v-if="errorMessage" variant="danger" show>{{ errorMessage }}</b-alert>
+    <ConfigureSelector
+        v-else
+        :id="contentObject.dataset_id"
+        :name="contentObject.dataset_name"
+        type="datasets"
+        @change="onChange($event)" />
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from "vue";
+
+import type { OptionType } from "@/components/Markdown/Editor/Configurations/types";
+
 import ConfigureSelector from "./ConfigureSelector.vue";
 
-interface ContentType {
-    dataset_id: string;
-    dataset_name?: string;
-}
-
-interface OptionType {
-    id: string | null | undefined;
-    name: string | null | undefined;
-}
-
 const props = defineProps<{
-    contentObject: ContentType;
+    content: string;
 }>();
 
 const emit = defineEmits<{
     (e: "change", content: string): void;
 }>();
 
+const contentObject = ref();
+const errorMessage = ref("");
+
 function onChange(newValue: OptionType) {
-    const newValues = { ...props.contentObject, dataset_id: newValue.id, dataset_name: newValue.name };
+    const newValues = { ...contentObject.value, dataset_id: newValue.id, dataset_name: newValue.name };
     emit("change", JSON.stringify(newValues, null, 4));
 }
+
+function parseContent() {
+    try {
+        contentObject.value = JSON.parse(props.content);
+        errorMessage.value = "";
+    } catch (e) {
+        errorMessage.value = `Failed to parse: ${e}`;
+    }
+}
+
+watch(
+    () => props.content,
+    () => parseContent(),
+    { immediate: true }
+);
 </script>
