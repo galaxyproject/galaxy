b0bccc273208610580841cf0fed312b6b8acfea7
Alireza Heidari <itisalirh@gmail.com>
2025-02-19 00:12:48 +0100

    Refactors UserPreferencesElement to use TypeScript and improves UI
    Converts script section to TypeScript and updates prop definitions
    Replaces Bootstrap Vue icons with FontAwesome icons
    Enhances UI with new styles and responsiveness
    Adds click event handling for navigation

--- client/src/components/User/UserPreferencesElement.vue
+++ client/src/components/User/UserPreferencesElement.vue
@@ -1,58 +1,93 @@
-<script setup>
-import { BRow } from "bootstrap-vue";
+<script setup lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { BBadge } from "bootstrap-vue";
+import type { IconDefinition } from "font-awesome-6";
+import { useRouter } from "vue-router/composables";
 
-defineProps({
-    id: {
-        type: String,
-        default: null,
-    },
-    icon: {
-        type: String,
-        default: "fa-gear",
-    },
-    title: {
-        type: String,
-        default: "Title not available.",
-    },
-    description: {
-        type: String,
-        default: "Description not available.",
-    },
-    to: {
-        type: String,
-        default: null,
-    },
-    badge: {
-        type: String,
-        default: null,
-    },
-});
-</script>
-<template>
-    <BRow class="ml-3 mb-1">
-        <i :class="['pref-icon pt-1 fa fa-lg', icon]" />
-        <div class="pref-content pr-1">
-            <b-badge v-if="!!badge" variant="danger">
-                {{ badge }}
-            </b-badge>
-            <router-link v-if="to" :id="id" :to="to">
-                <b v-localize>{{ title }}</b>
-            </router-link>
-            <a v-else :id="id" href="#" @click="$emit('click')">
-                <b v-localize>{{ title }}</b>
-            </a>
-            <div class="form-text text-muted">
-                {{ description }}
-            </div>
-            <slot />
-        </div>
-    </BRow>
-</template>
-<style scoped>
-.pref-content {
-    width: calc(100% - 3rem);
+interface Props {
+    id: string;
+    icon: IconDefinition;
+    title: string;
+    description: string;
+    to?: string;
+    badge?: string;
+    dangerZone?: boolean;
 }
-.pref-icon {
-    width: 3rem;
+
+const props = defineProps<Props>();
+
+const emit = defineEmits<{
+    (e: "click"): void;
+}>();
+
+const router = useRouter();
+
+function onClick() {
+    if (props.to) {
+        router.push(props.to);
+    } else {
+        emit("click");
+    }
+}
+</script>
+
+<template>
+    <div class="user-preference-element" :class="{ 'danger-zone': !!dangerZone }" @click="onClick">
+        <div class="user-preference-element-title">
+            <FontAwesomeIcon :icon="props.icon" />
+
+            <b v-localize>{{ title }}</b>
+
+            <BBadge v-if="!!badge" variant="danger">
+                {{ badge }}
+            </BBadge>
+        </div>
+
+        <div>
+            {{ description }}
+        </div>
+
+        <slot />
+    </div>
+</template>
+
+<style scoped lang="scss">
+@import "theme/blue.scss";
+@import "breakpoints.scss";
+
+.user-preference-element {
+    display: flex;
+    cursor: pointer;
+    gap: 0.5rem;
+    flex-direction: column;
+    border: 1px solid $brand-secondary;
+    border-radius: 0.5rem;
+    padding: 0.75rem;
+
+    .user-preference-element-title {
+        color: $brand-primary;
+    }
+
+    width: calc(100% / 3 - 1rem);
+
+    @container user-preferences (max-width: #{$breakpoint-xl}) {
+        width: calc(100% / 2 - 1rem);
+    }
+
+    @container user-preferences (max-width: #{$breakpoint-lg}) {
+        width: 100%;
+    }
+
+    &:hover {
+        background-color: $brand-secondary;
+    }
+
+    &.danger-zone {
+        border-color: $brand-danger;
+
+        .user-preference-element-title {
+            color: $brand-danger;
+        }
+    }
 }
 </style>
