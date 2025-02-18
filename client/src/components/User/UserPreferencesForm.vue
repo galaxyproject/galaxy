b284d1ee5f58a798b8603fb44a2e5eacb4ac5d2f
Alireza Heidari <itisalirh@gmail.com>
2025-02-19 00:10:26 +0100

    Refactors UserPreferencesForm to use Composition API
    Updates UserPreferencesForm to use Vue's Composition API for better readability and maintainability.
    Adds BreadcrumbHeading component for improved navigation.
    Removes unused imports and legacy code.

--- client/src/components/User/UserPreferencesForm.vue
+++ client/src/components/User/UserPreferencesForm.vue
@@ -1,27 +1,47 @@
-<template>
-    <FormGeneric v-bind="formConfig" :trim-inputs="true" />
-</template>
-<script>
-import FormGeneric from "components/Form/FormGeneric";
-import { getUserPreferencesModel } from "components/User/UserPreferencesModel";
-import Query from "utils/query-string-parsing";
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { computed } from "vue";
 
-export default {
-    components: {
-        FormGeneric,
-    },
-    props: {
-        formId: {
-            type: String,
-            required: true,
-        },
-    },
-    computed: {
-        formConfig() {
-            const userId = Query.get("id");
-            const model = getUserPreferencesModel(userId);
-            return model[this.formId];
-        },
-    },
-};
+import {
+    getUserPreferencesModel,
+    type UserPreferencesKey,
+    type UserPreferencesModel,
+} from "@/components/User/UserPreferencesModel";
+import { useUserStore } from "@/stores/userStore";
+
+import BreadcrumbHeading from "@/components/Common/BreadcrumbHeading.vue";
+import FormGeneric from "@/components/Form/FormGeneric.vue";
+
+interface Props {
+    formId: UserPreferencesKey;
+}
+
+const props = defineProps<Props>();
+
+const title = computed(() => formConfig.value?.title || "User Preference");
+
+const breadcrumbItems = computed(() => [{ title: "User Preferences", to: "/user" }, { title: title.value }]);
+
+const userStore = useUserStore();
+const { currentUser } = storeToRefs(userStore);
+
+const model = computed<UserPreferencesModel | undefined>(() => {
+    if (!currentUser.value?.isAnonymous) {
+        return getUserPreferencesModel(currentUser.value?.id);
+    } else {
+        return undefined;
+    }
+});
+
+const formConfig = computed(() => {
+    return model.value?.[props.formId];
+});
 </script>
+
+<template>
+    <div>
+        <BreadcrumbHeading :items="breadcrumbItems" />
+
+        <FormGeneric v-if="formConfig" v-bind="formConfig" :trim-inputs="true" />
+    </div>
+</template>
