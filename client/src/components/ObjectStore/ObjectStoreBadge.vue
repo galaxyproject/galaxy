1f2ab115dd9f189a1b0acec2fddc40cf2cbfcbf2
Alireza Heidari <itisalirh@gmail.com>
2025-02-21 17:26:33 +0100

    Refactors ObjectStoreBadge message handling
    Moves badge messages to a separate file for better organization
    Simplifies template structure by removing redundant span tags

--- client/src/components/ObjectStore/ObjectStoreBadge.vue
+++ client/src/components/ObjectStore/ObjectStoreBadge.vue
@@ -16,29 +16,8 @@ import { FontAwesomeIcon, FontAwesomeLayers } from "@fortawesome/vue-fontawesome
 import { computed } from "vue";
 
 import type { ObjectStoreBadgeType } from "@/api/objectStores.templates";
+import { MESSAGES } from "@/components/ObjectStore/badgeMessages";
 import { markup } from "@/components/ObjectStore/configurationMarkdown";
-
-const MESSAGES = {
-    restricted:
-        "This dataset is stored on storage restricted to a single user. It can not be shared, published, or added to Galaxy data libraries.",
-    user_defined: "This storage was user defined and is not managed by the Galaxy administrator.",
-    quota: "A Galaxy quota is enabled for this storage location.",
-    no_quota: "No Galaxy quota is enabled for this storage location.",
-    faster: "This storage has been marked as a faster option by the Galaxy administrator.",
-    slower: "This storage has been marked as a slower option by the Galaxy administrator.",
-    short_term: "This storage has been marked as routinely purged by the Galaxy administrator.",
-    backed_up: "This storage has been marked as backed up by the Galaxy administrator.",
-    not_backed_up: "This storage has been marked as not backed up by the Galaxy administrator.",
-    more_secure:
-        "This storage has been marked as more secure by the Galaxy administrator. The Galaxy web application doesn't make any additional promises regarding security for this storage.",
-    less_secure:
-        "This storage has been marked as less secure by the Galaxy administrator. The Galaxy web application doesn't make any additional promises regarding security for this storage.",
-    more_stable:
-        "This storage has been marked as more stable by the Galaxy administrator - expect jobs to fail less because of storage issues for this storage.",
-    less_stable:
-        "This storage has been marked as less stable by the Galaxy administrator - expect jobs to fail more because of storage issues for this storage.",
-    cloud: "This is cloud storage.",
-};
 
 interface Props {
     badge: ObjectStoreBadgeType;
@@ -80,72 +59,55 @@ const title = computed(() => {
 </script>
 
 <template>
-    <span>
-        <span ref="iconTarget" v-b-tooltip.hover.noninteractive="title" class="object-store-badge-wrapper">
-            <FontAwesomeLayers :class="layerClasses" :data-badge-type="badgeType">
-                <FontAwesomeIcon v-if="badgeType == 'restricted'" :icon="faUserLock" :class="disadvantage" />
-                <FontAwesomeIcon v-if="badgeType == 'user_defined'" :icon="faPlug" :class="neutral" />
-                <FontAwesomeIcon v-if="badgeType == 'quota'" :icon="faChartLine" :class="disadvantage" />
-                <FontAwesomeIcon v-if="badgeType == 'no_quota'" :icon="faChartLine" :class="neutral" v-bind="shrink" />
-                <FontAwesomeIcon v-if="badgeType == 'no_quota'" :icon="faBan" :class="[transparent, advantage]" />
-                <FontAwesomeIcon v-if="badgeType == 'no_quota'" :icon="faCircleNotch" :class="advantage" />
-                <FontAwesomeIcon
-                    v-if="badgeType == 'no_quota'"
-                    :icon="faCircleNotch"
-                    :class="advantage"
-                    flip="vertical" />
-                <FontAwesomeIcon v-if="badgeType == 'faster'" :icon="faTachometerAlt" :class="advantage" />
-                <FontAwesomeIcon
-                    v-if="badgeType == 'slower'"
-                    :icon="faTachometerAlt"
-                    :class="disadvantage"
-                    flip="horizontal" />
-                <FontAwesomeIcon v-if="badgeType == 'short_term'" :icon="faRecycle" :class="disadvantage" />
+    <span v-b-tooltip.hover.noninteractive="title" class="object-store-badge-wrapper">
+        <FontAwesomeLayers :class="layerClasses" :data-badge-type="badgeType">
+            <FontAwesomeIcon v-if="badgeType == 'restricted'" :icon="faUserLock" :class="disadvantage" />
+            <FontAwesomeIcon v-if="badgeType == 'user_defined'" :icon="faPlug" :class="neutral" />
+            <FontAwesomeIcon v-if="badgeType == 'quota'" :icon="faChartLine" :class="disadvantage" />
+            <FontAwesomeIcon v-if="badgeType == 'no_quota'" :icon="faChartLine" :class="neutral" v-bind="shrink" />
+            <FontAwesomeIcon v-if="badgeType == 'no_quota'" :icon="faBan" :class="[transparent, advantage]" />
+            <FontAwesomeIcon v-if="badgeType == 'no_quota'" :icon="faCircleNotch" :class="advantage" />
+            <FontAwesomeIcon v-if="badgeType == 'no_quota'" :icon="faCircleNotch" :class="advantage" flip="vertical" />
+            <FontAwesomeIcon v-if="badgeType == 'faster'" :icon="faTachometerAlt" :class="advantage" />
+            <FontAwesomeIcon
+                v-if="badgeType == 'slower'"
+                :icon="faTachometerAlt"
+                :class="disadvantage"
+                flip="horizontal" />
+            <FontAwesomeIcon v-if="badgeType == 'short_term'" :icon="faRecycle" :class="disadvantage" />
 
-                <FontAwesomeIcon v-if="badgeType == 'backed_up'" :icon="faArchive" :class="advantage" />
-                <FontAwesomeIcon
-                    v-if="badgeType == 'not_backed_up'"
-                    :icon="faArchive"
-                    :class="neutral"
-                    v-bind="shrink" />
-                <FontAwesomeIcon
-                    v-if="badgeType == 'not_backed_up'"
-                    :icon="faBan"
-                    :class="[transparent, disadvantage]" />
-                <FontAwesomeIcon v-if="badgeType == 'not_backed_up'" :icon="faCircleNotch" :class="disadvantage" />
-                <FontAwesomeIcon
-                    v-if="badgeType == 'not_backed_up'"
-                    :icon="faCircleNotch"
-                    :class="disadvantage"
-                    flip="vertical" />
+            <FontAwesomeIcon v-if="badgeType == 'backed_up'" :icon="faArchive" :class="advantage" />
+            <FontAwesomeIcon v-if="badgeType == 'not_backed_up'" :icon="faArchive" :class="neutral" v-bind="shrink" />
+            <FontAwesomeIcon v-if="badgeType == 'not_backed_up'" :icon="faBan" :class="[transparent, disadvantage]" />
+            <FontAwesomeIcon v-if="badgeType == 'not_backed_up'" :icon="faCircleNotch" :class="disadvantage" />
+            <FontAwesomeIcon
+                v-if="badgeType == 'not_backed_up'"
+                :icon="faCircleNotch"
+                :class="disadvantage"
+                flip="vertical" />
 
-                <FontAwesomeIcon v-if="badgeType == 'more_secure'" :icon="faKey" :class="advantage" />
-                <FontAwesomeIcon v-if="badgeType == 'less_secure'" :icon="faKey" :class="neutral" v-bind="shrink" />
-                <FontAwesomeIcon v-if="badgeType == 'less_secure'" :icon="faBan" :class="[transparent, disadvantage]" />
-                <FontAwesomeIcon v-if="badgeType == 'less_secure'" :icon="faCircleNotch" :class="disadvantage" />
-                <FontAwesomeIcon
-                    v-if="badgeType == 'less_secure'"
-                    :icon="faCircleNotch"
-                    :class="disadvantage"
-                    flip="vertical" />
+            <FontAwesomeIcon v-if="badgeType == 'more_secure'" :icon="faKey" :class="advantage" />
+            <FontAwesomeIcon v-if="badgeType == 'less_secure'" :icon="faKey" :class="neutral" v-bind="shrink" />
+            <FontAwesomeIcon v-if="badgeType == 'less_secure'" :icon="faBan" :class="[transparent, disadvantage]" />
+            <FontAwesomeIcon v-if="badgeType == 'less_secure'" :icon="faCircleNotch" :class="disadvantage" />
+            <FontAwesomeIcon
+                v-if="badgeType == 'less_secure'"
+                :icon="faCircleNotch"
+                :class="disadvantage"
+                flip="vertical" />
 
-                <FontAwesomeIcon v-if="badgeType == 'more_stable'" :icon="faShieldAlt" :class="advantage" />
-                <FontAwesomeIcon
-                    v-if="badgeType == 'less_stable'"
-                    :icon="faShieldAlt"
-                    :class="neutral"
-                    v-bind="shrink" />
-                <FontAwesomeIcon v-if="badgeType == 'less_stable'" :icon="faBan" :class="[transparent, disadvantage]" />
-                <FontAwesomeIcon v-if="badgeType == 'less_stable'" :icon="faCircleNotch" :class="disadvantage" />
-                <FontAwesomeIcon
-                    v-if="badgeType == 'less_stable'"
-                    :icon="faCircleNotch"
-                    :class="disadvantage"
-                    flip="vertical" />
+            <FontAwesomeIcon v-if="badgeType == 'more_stable'" :icon="faShieldAlt" :class="advantage" />
+            <FontAwesomeIcon v-if="badgeType == 'less_stable'" :icon="faShieldAlt" :class="neutral" v-bind="shrink" />
+            <FontAwesomeIcon v-if="badgeType == 'less_stable'" :icon="faBan" :class="[transparent, disadvantage]" />
+            <FontAwesomeIcon v-if="badgeType == 'less_stable'" :icon="faCircleNotch" :class="disadvantage" />
+            <FontAwesomeIcon
+                v-if="badgeType == 'less_stable'"
+                :icon="faCircleNotch"
+                :class="disadvantage"
+                flip="vertical" />
 
-                <FontAwesomeIcon v-if="badgeType == 'cloud'" :icon="faCloud" :class="neutral" />
-            </FontAwesomeLayers>
-        </span>
+            <FontAwesomeIcon v-if="badgeType == 'cloud'" :icon="faCloud" :class="neutral" />
+        </FontAwesomeLayers>
     </span>
 </template>
 
