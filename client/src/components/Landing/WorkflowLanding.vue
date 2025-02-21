af905910a6ca232be487ffd2c3bc5b89f64113e9
Dannon Baker <dannon.baker@gmail.com>
2025-02-21 13:02:12 -0500

    Refactor WorkflowLanding component to load user data on mount and close sidebar, removing redundant user store logic.

--- client/src/components/Landing/WorkflowLanding.vue
+++ client/src/components/Landing/WorkflowLanding.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
 import { BAlert } from "bootstrap-vue";
-import { storeToRefs } from "pinia";
-import { ref, watch } from "vue";
-import { useRouter } from "vue-router/composables";
+import { onMounted, ref } from "vue";
 
 import { GalaxyApi } from "@/api";
 import { useActivityStore } from "@/stores/activityStore";
-import { useUserStore } from "@/stores/userStore";
 import { errorMessageAsString } from "@/utils/simple-error";
 
 import LoadingSpan from "@/components/LoadingSpan.vue";
@@ -27,56 +24,41 @@ const workflowId = ref<string | null>(null);
 const errorMessage = ref<string | null>(null);
 const requestState = ref<Record<string, never> | null>(null);
 const instance = ref<boolean>(false);
-const userStore = useUserStore();
-const router = useRouter();
-
-userStore.loadUser(false);
-const { isAnonymous, currentUser } = storeToRefs(userStore);
 
 const activityStore = useActivityStore("default");
 
-watch(
-    currentUser,
-    async () => {
-        if (isAnonymous.value) {
-            router.push(
-                `/login/start?redirect=/workflow_landings/${props.uuid}?public=${props.public}&client_secret=${props.secret}`
-            );
-        } else if (currentUser.value) {
-            let claim;
-            let claimError;
-            activityStore.closeSideBar();
-            if (props.public) {
-                const { data, error } = await GalaxyApi().GET("/api/workflow_landings/{uuid}", {
-                    params: {
-                        path: { uuid: props.uuid },
-                    },
-                });
-                claim = data;
-                claimError = error;
-            } else {
-                const { data, error } = await GalaxyApi().POST("/api/workflow_landings/{uuid}/claim", {
-                    params: {
-                        path: { uuid: props.uuid },
-                    },
-                    body: {
-                        client_secret: props.secret,
-                    },
-                });
-                claim = data;
-                claimError = error;
-            }
-            if (claim) {
-                workflowId.value = claim.workflow_id;
-                instance.value = claim.workflow_target_type === "workflow";
-                requestState.value = claim.request_state;
-            } else {
-                errorMessage.value = errorMessageAsString(claimError);
-            }
-        }
-    },
-    { immediate: true }
-);
+onMounted(async() => {
+    let claim;
+    let claimError;
+    if (props.public) {
+        const { data, error } = await GalaxyApi().GET("/api/workflow_landings/{uuid}", {
+            params: {
+                path: { uuid: props.uuid },
+            },
+        });
+        claim = data;
+        claimError = error;
+    } else {
+        const { data, error } = await GalaxyApi().POST("/api/workflow_landings/{uuid}/claim", {
+            params: {
+                path: { uuid: props.uuid },
+            },
+            body: {
+                client_secret: props.secret,
+            },
+        });
+        claim = data;
+        claimError = error;
+    }
+    if (claim) {
+        workflowId.value = claim.workflow_id;
+        instance.value = claim.workflow_target_type === "workflow";
+        requestState.value = claim.request_state;
+    } else {
+        errorMessage.value = errorMessageAsString(claimError);
+    }
+    activityStore.closeSideBar();
+});
 </script>
 
 <template>
