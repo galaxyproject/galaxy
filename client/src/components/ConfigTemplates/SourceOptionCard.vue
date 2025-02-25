fa997d56e647a15b750da3724927970b34f3a7ff
Alireza Heidari <itisalirh@gmail.com>
2025-02-25 14:38:36 +0100

    Enhances SourceOptionCard UI and logic
    Adds computed properties for uniqueId, buttonTitle, and buttonTooltip
    Introduces optional props 'selected', 'submitButtonTooltip', and 'selectionMode'
    Updates styles for selected state
    Implements emit for 'select' event
    
    Improves user interaction and flexibility of SourceOptionCard component

--- client/src/components/ConfigTemplates/SourceOptionCard.vue
+++ client/src/components/ConfigTemplates/SourceOptionCard.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import type { IconDefinition } from "font-awesome-6";
+import { computed } from "vue";
 
+import type { ConcreteObjectStoreModel } from "@/api";
 import type { FileSourceTemplateSummary } from "@/api/fileSources";
 import type { ObjectStoreTemplateSummary } from "@/api/objectStores.templates";
 import type { CardAttributes } from "@/components/Common/GCard.types";
@@ -10,6 +12,8 @@ import { markup } from "@/components/ObjectStore/configurationMarkdown";
 import GButton from "@/components/BaseComponents/GButton.vue";
 import GCard from "@/components/Common/GCard.vue";
 
+type SourceOption = FileSourceTemplateSummary | ObjectStoreTemplateSummary | ConcreteObjectStoreModel;
+
 type OptionType = {
     icon: IconDefinition;
     title: string;
@@ -17,26 +21,55 @@ type OptionType = {
 
 interface Props {
     /** The source option to display */
-    sourceOption: FileSourceTemplateSummary | ObjectStoreTemplateSummary;
+    sourceOption: SourceOption;
     /** The route to navigate to when the user selects this option */
-    selectRoute: string;
+    selectRoute?: string;
     /** The type of the source option */
-    optionType: OptionType;
+    optionType?: OptionType;
     /** Whether to display the card in a grid view */
     gridView?: boolean;
+    /** Whether the card is selected */
+    selected?: boolean;
+    /** The title of the submit button */
+    submitButtonTooltip?: string;
+    /** Whether to show the selection mode */
+    selectionMode?: boolean;
 }
 
 const props = defineProps<Props>();
+
+const emit = defineEmits<{
+    (e: "select", sourceOption: SourceOption): void;
+}>();
+
+const uniqueId = computed(() =>
+    "id" in props.sourceOption ? props.sourceOption.id : Math.random().toString(36).substring(7)
+);
+const buttonTitle = computed(() => {
+    if (props.selectionMode) {
+        return props.selected ? "Selected" : "Select as Default";
+    }
+
+    return props.submitButtonTooltip ?? "Select";
+});
+const buttonTooltip = computed(() => {
+    if (props.selectionMode) {
+        return props.selected ? "This is the preferred option" : "Select this option as the preferred one";
+    }
+
+    return props.submitButtonTooltip ?? "Select this option to create a new instance";
+});
 
 const description = markup(props.sourceOption.description ?? "", true) ?? undefined;
 
 const primaryActions: CardAttributes[] = [
     {
         id: "select",
-        label: "Select",
-        title: "Select this option to create a new instance",
+        label: buttonTitle.value,
+        title: buttonTooltip.value,
         variant: "outline-primary",
         to: props.selectRoute,
+        handler: () => emit("select", props.sourceOption),
         visible: true,
     },
 ];
@@ -44,11 +77,12 @@ const primaryActions: CardAttributes[] = [
 
 <template>
     <GCard
-        :id="props.sourceOption.id"
+        :id="uniqueId"
         :title="props.sourceOption.name ?? ''"
         :description="description"
         :primary-actions="primaryActions"
-        :grid-view="props.gridView">
+        :grid-view="props.gridView"
+        :selected="props.selected">
         <template v-slot:titleActions>
             <GButton
                 tooltip
