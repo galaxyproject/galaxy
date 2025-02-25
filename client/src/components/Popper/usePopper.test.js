e92f642db044954e414c28bf1de15d170f8df3ec
guerler <aysam.guerler@gmail.com>
2025-02-25 09:25:49 -0500

    Fix popper test case

--- client/src/components/Popper/usePopper.test.js
+++ client/src/components/Popper/usePopper.test.js
@@ -45,6 +45,12 @@ describe("usePopper", () => {
         createTestComponent();
         expect(createPopper).toHaveBeenCalledWith(referenceElement, popperElement, {
             placement: "bottom",
+            modifiers: [{
+                name: "offset",
+                options: {
+                    offset: [0, 5],
+                },
+            }],
             strategy: "absolute",
         });
     });
