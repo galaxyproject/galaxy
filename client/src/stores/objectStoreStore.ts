97631afc48daf3bd0f26748091fe6b1a524109c3
Alireza Heidari <itisalirh@gmail.com>
2025-02-25 15:37:45 +0100

    Updates object store type and loading logic
    Replaces ConcreteObjectStoreModel with UserConcreteObjectStoreModel.
    Simplifies isLoaded computed property logic.
    Typecasts loaded data to ensure correct type.

--- client/src/stores/objectStoreStore.ts
+++ client/src/stores/objectStoreStore.ts
@@ -1,22 +1,22 @@
 import { defineStore } from "pinia";
 import { computed, ref, set } from "vue";
 
-import type { ConcreteObjectStoreModel } from "@/api";
+import type { UserConcreteObjectStoreModel } from "@/api";
 import { getSelectableObjectStores } from "@/api/objectStores";
 import { errorMessageAsString } from "@/utils/simple-error";
 
 export const useObjectStoreStore = defineStore("objectStoreStore", () => {
-    const selectableObjectStores = ref<ConcreteObjectStoreModel[] | null>(null);
+    const selectableObjectStores = ref<UserConcreteObjectStoreModel[]>();
     const loadErrorMessage = ref<string | null>(null);
     const isLoading = ref(false);
-    const isLoaded = computed(() => selectableObjectStores.value != null);
+    const isLoaded = computed(() => selectableObjectStores.value);
 
     async function loadObjectStores() {
         if (!isLoaded.value && !isLoading.value) {
             isLoading.value = true;
             try {
                 const data = await getSelectableObjectStores();
-                selectableObjectStores.value = data;
+                selectableObjectStores.value = data as UserConcreteObjectStoreModel[];
             } catch (err) {
                 const errorMessage = `Error loading Galaxy object stores: ${errorMessageAsString(err)}`;
                 loadErrorMessage.value = errorMessage;
