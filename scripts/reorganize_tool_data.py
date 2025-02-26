5027e02bf33869452cd15f70f33368f93732dd8d
Nate Coraor <nate@bx.psu.edu>
2025-02-26 10:21:13 -0500

    Drop unused colors

--- scripts/reorganize_tool_data.py
+++ scripts/reorganize_tool_data.py
@@ -85,7 +85,6 @@ class Color:
     REVERSE = "\033[7m"
     HIDDEN = "\033[8m"
 
-    # Foreground colors
     BLACK = "\033[30m"
     RED = "\033[31m"
     GREEN = "\033[32m"
@@ -94,36 +93,6 @@ class Color:
     MAGENTA = "\033[35m"
     CYAN = "\033[36m"
     WHITE = "\033[37m"
-
-    # Bright foreground colors
-    BRIGHT_BLACK = "\033[90m"
-    BRIGHT_RED = "\033[91m"
-    BRIGHT_GREEN = "\033[92m"
-    BRIGHT_YELLOW = "\033[93m"
-    BRIGHT_BLUE = "\033[94m"
-    BRIGHT_MAGENTA = "\033[95m"
-    BRIGHT_CYAN = "\033[96m"
-    BRIGHT_WHITE = "\033[97m"
-
-    # Background colors
-    BG_BLACK = "\033[40m"
-    BG_RED = "\033[41m"
-    BG_GREEN = "\033[42m"
-    BG_YELLOW = "\033[43m"
-    BG_BLUE = "\033[44m"
-    BG_MAGENTA = "\033[45m"
-    BG_CYAN = "\033[46m"
-    BG_WHITE = "\033[47m"
-
-    # Bright background colors
-    BG_BRIGHT_BLACK = "\033[100m"
-    BG_BRIGHT_RED = "\033[101m"
-    BG_BRIGHT_GREEN = "\033[102m"
-    BG_BRIGHT_YELLOW = "\033[103m"
-    BG_BRIGHT_BLUE = "\033[104m"
-    BG_BRIGHT_MAGENTA = "\033[105m"
-    BG_BRIGHT_CYAN = "\033[106m"
-    BG_BRIGHT_WHITE = "\033[107m"
 
     @staticmethod
     def sprint(text, color, effect=None):
@@ -350,7 +319,7 @@ def main(argv=None):
         argv = sys.argv[1:]
     args = parse_arguments(argv)
     for tdtc in args.tool_data_table_conf:
-        Color.print(f"Processing: {tdtc}", Color.BRIGHT_MAGENTA, effect=Color.UNDERLINE)
+        Color.print(f"Processing: {tdtc}", Color.MAGENTA, effect=Color.UNDERLINE)
         for dt in parse_tdtc(tdtc, args.tool_data_path):
             Color.print(f"Data Table: {dt.name}", Color.MAGENTA)
             dt.reorganize(commit=args.commit)
