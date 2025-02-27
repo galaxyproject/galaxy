4908f9d07289090d6ad1d2c378e6a8774dacb299
guerler <aysam.guerler@gmail.com>
2025-02-27 13:52:03 -0500

    Remove unused details from Markdown Galaxy component props, remove version display

--- client/src/components/Markdown/Markdown.vue
+++ client/src/components/Markdown/Markdown.vue
@@ -4,8 +4,6 @@ import { faDownload, faEdit } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { computed, onMounted, ref, watch } from "vue";
 
-import { useWorkflowStore } from "@/stores/workflowStore";
-
 import { parseMarkdown } from "./parse";
 
 import MarkdownDefault from "./Sections/MarkdownDefault.vue";
@@ -22,11 +20,6 @@ interface MarkdownConfig {
     content?: string;
     markdown?: string;
     errors?: Array<{ error?: string; line?: string }>;
-    history_datasets?: Record<string, unknown>;
-    histories?: Record<string, unknown>;
-    history_dataset_collections?: Record<string, unknown>;
-    workflows?: Record<string, unknown>;
-    invocations?: Record<string, unknown>;
     id?: string;
     title?: string;
     model_class?: string;
@@ -43,18 +36,7 @@ const props = defineProps<{
 // Refs and data
 const markdownObjects = ref<any[]>([]);
 const markdownErrors = ref<any[]>([]);
-const datasets = ref<Record<string, unknown>>({});
-const histories = ref<Record<string, unknown>>({});
-const collections = ref<Record<string, unknown>>({});
-const workflows = ref<Record<string, unknown>>({});
-const invocations = ref<Record<string, unknown>>({});
 const loading = ref(true);
-const workflowID = ref("");
-
-// Workflow Store
-const workflowStore = useWorkflowStore();
-const fetchWorkflowForInstanceId = workflowStore.fetchWorkflowForInstanceId;
-const getStoredWorkflowByInstanceId = workflowStore.getStoredWorkflowByInstanceId;
 
 // Computed properties
 const effectiveExportLink = computed(() => (props.enable_beta_markdown_export ? props.exportLink : null));
@@ -79,8 +61,6 @@ const time = computed(() => {
 
 const version = computed(() => props.markdownConfig.generate_version || "Unknown Galaxy Version");
 
-const workflowVersions = computed(() => getStoredWorkflowByInstanceId(workflowID.value));
-
 // Methods
 function initConfig() {
     const config = props.markdownConfig;
@@ -88,12 +68,6 @@ function initConfig() {
         const markdown = config.content || config.markdown || "";
         markdownErrors.value = config.errors || [];
         markdownObjects.value = parseMarkdown(markdown);
-        datasets.value = config.history_datasets || {};
-        histories.value = config.histories || {};
-        collections.value = config.history_dataset_collections || {};
-        workflows.value = config.workflows || {};
-        invocations.value = config.invocations || {};
-        workflowID.value = Object.keys(config.workflows || {})[0] || "";
         loading.value = false;
     }
 }
@@ -104,7 +78,6 @@ watch(() => props.markdownConfig, initConfig);
 // Lifecycle hooks
 onMounted(() => {
     initConfig();
-    fetchWorkflowForInstanceId(workflowID.value);
 });
 </script>
 
@@ -136,9 +109,6 @@ onMounted(() => {
                     <h1 class="text-break align-middle">
                         Title: {{ markdownConfig.title || markdownConfig.model_class }}
                     </h1>
-                    <h2 v-if="workflowVersions" class="text-break align-middle">
-                        Workflow Checkpoint: {{ workflowVersions.version }}
-                    </h2>
                 </span>
             </div>
             <b-badge variant="info" class="w-100 rounded mb-3 white-space-normal">
@@ -155,16 +125,7 @@ onMounted(() => {
             </div>
             <div v-for="(obj, index) in markdownObjects" :key="index" class="markdown-components">
                 <MarkdownDefault v-if="obj.name === 'markdown'" :content="obj.content" />
-                <MarkdownGalaxy
-                    v-else-if="obj.name === 'galaxy'"
-                    :content="obj.content"
-                    :datasets="datasets"
-                    :collections="collections"
-                    :histories="histories"
-                    :invocations="invocations"
-                    :time="time"
-                    :version="version"
-                    :workflows="workflows" />
+                <MarkdownGalaxy v-else-if="obj.name === 'galaxy'" :content="obj.content" />
             </div>
         </div>
     </div>
