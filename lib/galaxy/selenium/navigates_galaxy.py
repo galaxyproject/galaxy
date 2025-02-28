59d1eef264bece98b65ac2211ae9a1a2786724ff
Alireza Heidari <itisalirh@gmail.com>
2025-02-28 14:39:17 +0100

    Updates storage selection method
    Replaces option_buttons with option_cards for storage selection
    Adds check and click for primary button if present

--- lib/galaxy/selenium/navigates_galaxy.py
+++ lib/galaxy/selenium/navigates_galaxy.py
@@ -1826,10 +1826,12 @@ class NavigatesGalaxy(HasDriver):
 
     def select_storage(self, storage_id: str) -> None:
         selection_component = self.components.preferences.object_store_selection
-        selection_component.option_buttons.wait_for_present()
-        button = selection_component.option_button(object_store_id=storage_id)
+        selection_component.option_cards.wait_for_present()
+        button = selection_component.option_card_select(object_store_id=storage_id)
         button.wait_for_and_click()
-        selection_component.option_buttons.wait_for_absent_or_hidden()
+        if self.find_element_by_selector(".btn-primary"):
+            self.find_element_by_selector(".btn-primary").click()
+        selection_component.option_cards.wait_for_absent_or_hidden()
 
     def create_page_and_edit(self, name=None, slug=None, screenshot_name=None):
         name = self.create_page(name=name, slug=slug, screenshot_name=screenshot_name)
