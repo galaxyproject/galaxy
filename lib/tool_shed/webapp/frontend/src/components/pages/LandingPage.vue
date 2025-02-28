6eaf9dc8ff04e909802349478e54e166aefabf87
John Chilton <jmchilton@gmail.com>
2025-02-28 08:33:19 -0500

    Adding help carousel to fill some landing page space, provide context.

--- lib/tool_shed/webapp/frontend/src/components/pages/LandingPage.vue
+++ lib/tool_shed/webapp/frontend/src/components/pages/LandingPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import RecentlyCreatedRepositories from "@/components/RecentlyCreatedRepositories.vue"
 import PageContainer from "@/components/PageContainer.vue"
+import LandingCarousel from "@/components/LandingCarousel.vue"
 import { notify } from "@/util"
 
 interface LandingPageProps {
@@ -15,8 +16,21 @@ if (props.message != null) {
 
 <template>
     <page-container>
-        <div class="row justify-left">Welcome to the Galaxy Tool Shed.</div>
         <div class="row justify-center">
+            <div class="col-8">
+                <h2>Welcome to the Galaxy Tool Shed</h2>
+                <p class="text-body1">
+                    The Galaxy Tool Shed contains thousands of published Galaxy tools to enable cutting edge research.
+                    The tool shed being constantly updated by the Galaxy community with new tools, updates to tools as
+                    new versions of software are released, and bug fixes.
+                </p>
+            </div>
+        </div>
+
+        <div class="row justify-center">
+            <div class="col-4">
+                <LandingCarousel />
+            </div>
             <recently-created-repositories class="col-4" />
         </div>
     </page-container>
