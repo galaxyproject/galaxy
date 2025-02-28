3ba0c1c4e022c016be68f6dc189922036895934e
guerler <aysam.guerler@gmail.com>
2025-02-28 16:12:55 -0500

    Complete history dataset display test

--- client/src/components/Markdown/Sections/Elements/HistoryDatasetDisplay.test.js
+++ client/src/components/Markdown/Sections/Elements/HistoryDatasetDisplay.test.js
@@ -1,6 +1,6 @@
 import { createTestingPinia } from "@pinia/testing";
 import { mount } from "@vue/test-utils";
-// import flushPromises from "flush-promises";
+import flushPromises from "flush-promises";
 import { getLocalVue } from "tests/jest/helpers";
 
 import { useServerMock } from "@/api/client/__mocks__";
@@ -27,7 +27,7 @@ describe("History Tabular Dataset Display", () => {
     const tabularMetaData = { metadata_columns: 2, metadata_data_lines: 4, file_ext: "tabular", name: "someName" };
     const tabularTableDataCounts = tabularMetaData.metadata_columns * tabularMetaData.metadata_data_lines;
 
-    function mountTarget() {
+    async function mountTarget() {
         server.use(
             http.get("/api/datasets/{dataset_id}", ({ response }) => response(200).json(tabularMetaData)),
             http.get("/api/datasets/{dataset_id}/get_content_as_text", ({ response }) => response(200).json(tabular))
@@ -37,11 +37,11 @@ describe("History Tabular Dataset Display", () => {
             propsData: { datasetId },
             pinia: setUpDatatypesStore(),
         });
-        // await flushPromises();
+        await flushPromises();
     }
 
     it("should render table", async () => {
-        mountTarget();
+        await mountTarget();
         expect(wrapper.find("table").exists()).toBe(true);
         expect(wrapper.findAll("td").length).toBe(tabularTableDataCounts);
         expect(wrapper.findAll("th").length).toBe(tabularMetaData.metadata_columns);
@@ -54,7 +54,7 @@ describe("History Text Dataset Display", () => {
     const text = { item_data: "some text" };
     const textMetaData = { file_ext: "txt", name: "someName" };
 
-    function mountTarget() {
+    async function mountTarget() {
         server.resetHandlers();
         server.use(
             http.get("/api/datasets/{dataset_id}", ({ response }) => response(200).json(textMetaData)),
@@ -65,25 +65,25 @@ describe("History Text Dataset Display", () => {
             propsData: { datasetId },
             pinia: setUpDatatypesStore(),
         });
-        // await flushPromises();
+        await flushPromises();
     }
 
-    it("should render text", () => {
-        mountTarget();
+    it("should render text", async () => {
+        await mountTarget();
         const renderedText = wrapper.find(".word-wrap-normal");
         expect(renderedText.exists()).toBe(true);
         expect(renderedText.text()).toBe(text.item_data);
     });
 
     it("should render header with embedded true", async () => {
-        mountTarget();
+        await mountTarget();
         expect(wrapper.find(".card-header").exists()).toBe(true);
         await wrapper.setProps({ embedded: true });
         expect(wrapper.find(".card-header").exists()).toBe(false);
     });
 
     it("should expand dataset", async () => {
-        mountTarget();
+        await mountTarget();
         const expandBTN = wrapper.find('.btn[title="Expand"]');
         expect(expandBTN.exists()).toBe(true);
         expect(wrapper.find(".embedded-dataset").exists()).toBe(true);
