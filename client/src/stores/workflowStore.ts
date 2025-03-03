6d31292af03a3d0cf6f4d5e663fa286fb9e75443
Ahmed Awan <qe66653@umbc.edu>
2025-03-03 15:24:11 +0500

    use `StoredWorkflowDetailed` instead of `Workflow` type from store
    We really need a less specific type to be used here, and this will be possible once routes like `api/workflows` index are modernized.

--- client/src/stores/workflowStore.ts
+++ client/src/stores/workflowStore.ts
@@ -1,16 +1,14 @@
 import { defineStore } from "pinia";
 import { computed, ref, set } from "vue";
 
+import { GalaxyApi } from "@/api";
 import type { StoredWorkflowDetailed } from "@/api/workflows";
-import { getAppRoot } from "@/onload/loadConfig";
-import { type Steps } from "@/stores/workflowStepStore";
 
-export interface Workflow extends Omit<StoredWorkflowDetailed, "steps"> {
-    steps: Steps;
-}
+// TODO: Define a less specific `Workflow` type here, and unify with the one in `workflows.services.ts`,
+//       instead of using `StoredWorkflowDetailed` directly.
 
 export const useWorkflowStore = defineStore("workflowStore", () => {
-    const workflowsByInstanceId = ref<{ [index: string]: Workflow }>({});
+    const workflowsByInstanceId = ref<{ [index: string]: StoredWorkflowDetailed }>({});
 
     const getStoredWorkflowByInstanceId = computed(() => (workflowId: string) => {
         return workflowsByInstanceId.value[workflowId];
