1b01f86cafff39e88864821bbcb9d9d51639420d
Ahmed Awan <qe66653@umbc.edu>
2025-03-05 19:20:10 +0500

    remove unneeded `v-html` from `FormError`
    Co-authored-by: mvdbeek <m.vandenbeek@gmail.com>

--- client/src/components/Form/Elements/FormError.vue
+++ client/src/components/Form/Elements/FormError.vue
@@ -13,8 +13,7 @@ const props = defineProps<{
     <component :is="props.hasAlertClass ? BAlert : 'span'" show variant="info">
         <div v-for="(alert, index) in props.alerts" :key="index" class="ui-form-error">
             <FontAwesomeIcon class="mr-1" :icon="faExclamation" />
-            <!-- eslint-disable-next-line vue/no-v-html -->
-            <span class="ui-form-error-text" v-html="alert" />
+            <span class="ui-form-error-text">{{ alert }}</span>
         </div>
     </component>
 </template>
