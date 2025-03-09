823e3bd75260885c29b22fa4d3df0830fe36a09b
Matthias Bernt <m.bernt@ufz.de>
2025-03-09 18:36:45 +0100

    fix wording of API doc string

--- lib/galaxy/webapps/galaxy/api/library_contents.py
+++ lib/galaxy/webapps/galaxy/api/library_contents.py
@@ -101,7 +101,7 @@ class FastAPILibraryContents:
         id: MaybeLibraryFolderOrDatasetID,
         trans: ProvidesUserContext = DependsOnTrans,
     ) -> AnyLibraryContentsShowResponse:
-        """This endpoint is deprecated. Please use GET /api/libraries/datasets/{library_id} instead."""
+        """This endpoint is deprecated. Please use GET /api/libraries/datasets/{id} instead."""
         return self.service.show(trans, id)
 
     @router.post(
@@ -147,7 +147,7 @@ class FastAPILibraryContents:
         payload,
         trans: ProvidesUserContext = DependsOnTrans,
     ) -> None:
-        """This endpoint is deprecated. Please use PATCH /api/libraries/datasets/{library_id} instead."""
+        """This endpoint is deprecated. Please use PATCH /api/libraries/datasets/{id} instead."""
         return self.service.update(trans, id, payload)
 
     @router.delete(
@@ -162,5 +162,5 @@ class FastAPILibraryContents:
         payload: Optional[LibraryContentsDeletePayload] = Body(None),
         trans: ProvidesHistoryContext = DependsOnTrans,
     ) -> LibraryContentsDeleteResponse:
-        """This endpoint is deprecated. Please use DELETE /api/libraries/datasets/{library_id} instead."""
+        """This endpoint is deprecated. Please use DELETE /api/libraries/datasets/{id} instead."""
         return self.service.delete(trans, id, payload or LibraryContentsDeletePayload())
