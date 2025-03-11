9a9db4ec1d92b990a732b4a184c3c34b6215ad66
John Chilton <jmchilton@gmail.com>
2025-03-11 12:12:59 -0400

    Event name: clicked-create -> on-create

--- client/src/components/Collections/PairCollectionCreator.vue
+++ client/src/components/Collections/PairCollectionCreator.vue
@@ -39,7 +39,7 @@ interface Props {
 const props = defineProps<Props>();
 
 const emit = defineEmits<{
-    (event: "clicked-create", selectedPair: DatasetPair, collectionName: string, hideSourceItems: boolean): void;
+    (event: "on-create", selectedPair: DatasetPair, collectionName: string, hideSourceItems: boolean): void;
     (event: "on-cancel"): void;
 }>();
 
@@ -263,7 +263,7 @@ function clickedCreate(collectionName: string) {
             reverse: pairElements.value.reverse as HDASummary,
             name: collectionName,
         };
-        emit("clicked-create", returnedPair, collectionName, hideSourceItems.value);
+        emit("on-create", returnedPair, collectionName, hideSourceItems.value);
     }
 }
 
