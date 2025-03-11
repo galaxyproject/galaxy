73190aeff84a71c7335aa4304c94603468659d33
Ahmed Awan <qe66653@umbc.edu>
2025-03-11 11:39:47 -0500

    make history private also makes history inaccessible
    Fixes https://github.com/galaxyproject/galaxy/issues/19645
    
    We additionally also indicate using a little badge to the user if in case the history was accessible, it is now inaccessible.

--- client/src/components/History/HistoryAccessibility.vue
+++ client/src/components/History/HistoryAccessibility.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { faLock, faShareAlt, faUserLock } from "@fortawesome/free-solid-svg-icons";
+import { faExclamation, faLock, faShareAlt, faUserLock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { BTab, BTabs } from "bootstrap-vue";
+import { BBadge, BTab, BTabs } from "bootstrap-vue";
 import { ref } from "vue";
 
 import { useHistoryStore } from "@/stores/historyStore";
@@ -18,7 +18,21 @@ const props = defineProps<{
 
 const historyStore = useHistoryStore();
 
-const tabsLazy = ref(false);
+/** This boolean is used to trigger lazy loading (refresh) of other tabs when history privacy has changed. */
+const historyPrivacyChanged = ref(false);
+
+/** Once the history is made private, this boolean is used to notify the user if sharing status has also changed or not. */
+const sharingStatusChanged = ref(false);
+
+function historyMadePrivate(hasSharingStatusChanged: boolean) {
+    sharingStatusChanged.value = hasSharingStatusChanged;
+    historyPrivacyChanged.value = true;
+}
+
+function openSharingTab() {
+    historyPrivacyChanged.value = false;
+    sharingStatusChanged.value = false;
+}
 </script>
 
 <template>
@@ -29,16 +43,24 @@ const tabsLazy = ref(false);
         </Heading>
 
         <BTabs class="mt-3">
-            <BTab :lazy="tabsLazy" @click="tabsLazy = false">
+            <BTab :lazy="historyPrivacyChanged" @click="openSharingTab">
                 <template v-slot:title>
                     <FontAwesomeIcon :icon="faShareAlt" class="mr-1" />
                     {{ localize("Share or Publish") }}
+                    <BBadge
+                        v-if="sharingStatusChanged"
+                        v-b-tooltip.hover.noninteractive
+                        class="ml-1"
+                        :title="localize('Sharing status for this history has changed.')"
+                        variant="primary">
+                        <FontAwesomeIcon :icon="faExclamation" />
+                    </BBadge>
                 </template>
 
                 <SharingPage :id="props.historyId" plural-name="histories" model-class="History" no-heading />
             </BTab>
 
-            <BTab :lazy="tabsLazy" @click="tabsLazy = false">
+            <BTab :lazy="historyPrivacyChanged" @click="historyPrivacyChanged = false">
                 <template v-slot:title>
                     <FontAwesomeIcon :icon="faUserLock" class="mr-1" />
                     {{ localize("Set Permissions") }}
@@ -53,7 +75,7 @@ const tabsLazy = ref(false);
                     {{ localize("Make Private") }}
                 </template>
 
-                <HistoryMakePrivate :history-id="props.historyId" @change="tabsLazy = true" />
+                <HistoryMakePrivate :history-id="props.historyId" @history-made-private="historyMadePrivate" />
             </BTab>
         </BTabs>
     </div>
