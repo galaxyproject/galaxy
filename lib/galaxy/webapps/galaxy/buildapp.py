3598a2a0148a1ed26d6c38978da2867a010493c1
Dannon Baker <dannon.baker@gmail.com>
2025-03-18 12:22:24 -0400

    Remove unused client route for standalone wizard interface

--- lib/galaxy/webapps/galaxy/buildapp.py
+++ lib/galaxy/webapps/galaxy/buildapp.py
@@ -284,7 +284,6 @@ def app_pair(global_conf, load_app_kwds=None, wsgi_preflight=True, **kwargs):
     webapp.add_client_route("/collection/{collection_id}/edit")
     webapp.add_client_route("/jobs/submission/success")
     webapp.add_client_route("/jobs/{job_id}/view")
-    webapp.add_client_route("/wizard")
     webapp.add_client_route("/workflows/list")
     webapp.add_client_route("/workflows/list_published")
     webapp.add_client_route("/workflows/list_shared_with_me")
