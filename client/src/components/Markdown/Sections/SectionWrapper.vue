901d5141024fcb4089205edf5dd8a8350a549bb5
guerler <aysam.guerler@gmail.com>
2025-03-18 11:10:37 -0400

    Add markdown component section wrapper

---
+++ client/src/components/Markdown/Sections/SectionWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+    <MarkdownDefault v-if="name === 'markdown'" :content="content" />
+    <MarkdownGalaxy v-else-if="name === 'galaxy'" :content="content" :labels="labels" />
+    <b-alert v-else variant="danger" show> This cell type `{{ name }}` is not available. </b-alert>
+</template>
+
+<script setup lang="ts">
+import type { WorkflowLabel } from "@/components/Markdown/Editor/types";
+
+import MarkdownDefault from "../Sections/MarkdownDefault.vue";
+import MarkdownGalaxy from "../Sections/MarkdownGalaxy.vue";
+
+defineProps<{
+    content: string;
+    labels?: Array<WorkflowLabel>;
+    name: string;
+}>();
+
+defineEmits(["change"]);
+</script>
