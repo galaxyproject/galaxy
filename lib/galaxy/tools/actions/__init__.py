galaxyproject/galaxy

commit b7591a0872c4594bc24dd5e1f1827eabf300b16b

Improve type annotations of ``ModelPersistenceContext`` and derived classes

lib/galaxy/tools/actions/__init__.py
@@ -9,6 +9,7 @@ from typing import (
     cast,
     Dict,
     List,
+    Mapping,
     MutableMapping,
     Optional,
     Set,
@@ -38,6 +39,7 @@ from galaxy.model.dataset_collections.builder import CollectionBuilder
 from galaxy.model.dataset_collections.matching import MatchingCollections
 from galaxy.model.none_like import NoneDataset
 from galaxy.objectstore import ObjectStorePopulator
+from galaxy.tool_util.parser.output_objects import tool_output_is_collection
 from galaxy.tools._types import ToolStateJobInstancePopulatedT
 from galaxy.tools.execute import (
     DatasetCollectionElementsSliceT,
@@ -88,7 +90,7 @@ class ToolAction:
     @abstractmethod
     def execute(
         self,
-        tool,
+        tool: "Tool",
         trans,
         incoming: Optional[ToolStateJobInstancePopulatedT] = None,
         history: Optional[History] = None,
@@ -416,14 +418,14 @@ class DefaultToolAction(ToolAction):
 
     def execute(
         self,
-        tool,
+        tool: "Tool",
         trans,
         incoming: Optional[ToolStateJobInstancePopulatedT] = None,
         history: Optional[History] = None,
         job_params=None,
         rerun_remap_job_id: Optional[int] = DEFAULT_RERUN_REMAP_JOB_ID,
         execution_cache: Optional[ToolExecutionCache] = None,
-        dataset_collection_elements=None,
+        dataset_collection_elements: Optional[DatasetCollectionElementsSliceT] = DEFAULT_DATASET_COLLECTION_ELEMENTS,
         completed_job: Optional[Job] = None,
         collection_info: Optional[MatchingCollections] = None,
         job_callback: Optional[JobCallbackT] = DEFAULT_JOB_CALLBACK,
@@ -523,6 +525,7 @@ class DefaultToolAction(ToolAction):
         async_tool = tool.tool_type == "data_source_async"
 
         def handle_output(name, output, hidden=None):
+            assert incoming is not None
             if async_tool and name in incoming:
                 # HACK: output data has already been created as a result of the async controller
                 dataid = incoming[name]
@@ -604,19 +607,19 @@ class DefaultToolAction(ToolAction):
             out_data[name] = data
             if output.actions:
                 # Apply pre-job tool-output-dataset actions; e.g. setting metadata, changing format
-                output_action_params = dict(out_data)
+                output_action_params: Dict[str, Any] = dict(out_data)
                 output_action_params.update(wrapped_params.params)
                 output_action_params["__python_template_version__"] = tool.python_template_version
                 output.actions.apply_action(data, output_action_params)
             # Flush all datasets at once.
             return data
 
-        child_dataset_names = set()
+        child_dataset_names: Set[str] = set()
 
         for name, output in tool.outputs.items():
             if not filter_output(tool, output, incoming):
                 handle_output_timer = ExecutionTimer()
-                if output.collection:
+                if tool_output_is_collection(output):  # Only XML tools include collections in tool.outputs
                     if completed_job and dataset_collection_elements and name in dataset_collection_elements:
                         # Output collection is mapped over and has already been copied from original job
                         continue
@@ -630,10 +633,11 @@ class DefaultToolAction(ToolAction):
                         # Add elements to top-level collection, unless nested...
                         current_element_identifiers = element_identifiers
                         current_collection_type = output.structure.collection_type
+                        assert current_collection_type
+                        # TODO: replace following line with formal abstractions for doing this.
+                        current_collection_type = ":".join(current_collection_type.split(":")[1:])
 
                         for parent_id in output_part_def.parent_ids or []:
-                            # TODO: replace following line with formal abstractions for doing this.
-                            current_collection_type = ":".join(current_collection_type.split(":")[1:])
                             name_to_index = {
                                 value["name"]: index for (index, value) in enumerate(current_element_identifiers)
                             }
@@ -958,11 +962,12 @@ class DefaultToolAction(ToolAction):
         self._record_input_datasets(trans, job, inp_data)
 
     def _record_outputs(
-        self, job: Job, out_data: Dict[str, "DatasetInstance"], output_collections: "OutputCollections"
+        self, job: Job, out_data: Mapping[str, "DatasetInstance"], output_collections: "OutputCollections"
     ):
         out_collections = output_collections.out_collections
         out_collection_instances = output_collections.out_collection_instances
         for name, dataset in out_data.items():
+            assert isinstance(dataset, HistoryDatasetAssociation)
             job.add_output_dataset(name, dataset)
         for name, dataset_collection in out_collections.items():
             job.add_implicit_output_dataset_collection(name, dataset_collection)

lib/galaxy/metadata/__init__.py
@@ -5,6 +5,12 @@ import json
 import os
 import shutil
 from logging import getLogger
+from typing import (
+    Any,
+    Dict,
+    Optional,
+    TYPE_CHECKING,
+)
 
 import galaxy.model
 from galaxy.model import store
@@ -14,6 +20,9 @@ from galaxy.model.metadata import (
 )
 from galaxy.model.store import DirectoryModelExportStore
 from galaxy.util import safe_makedirs
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm.scoping import scoped_session
 
 log = getLogger(__name__)
 
@@ -57,7 +66,7 @@ class MetadataCollectionStrategy(metaclass=abc.ABCMeta):
         self,
         datasets_dict,
         out_collections,
-        sa_session,
+        sa_session: "scoped_session",
         exec_dir=None,
         tmp_dir=None,
         dataset_files_path=None,
@@ -69,12 +78,15 @@ class MetadataCollectionStrategy(metaclass=abc.ABCMeta):
         job_metadata=None,
         provided_metadata_style=None,
         compute_tmp_dir=None,
+        compute_version_path: Optional[str] = None,
         include_command=True,
         max_metadata_value_size=0,
         max_discovered_files=None,
+        validate_outputs: bool = False,
         object_store_conf=None,
         tool=None,
-        job=None,
+        job: Optional[galaxy.model.Job] = None,
+        link_data_only: bool = False,
         kwds=None,
     ):
         """Setup files needed for external metadata collection.
@@ -133,7 +145,7 @@ class PortableDirectoryMetadataGenerator(MetadataCollectionStrategy):
         self,
         datasets_dict,
         out_collections,
-        sa_session,
+        sa_session: "scoped_session",
         exec_dir=None,
         tmp_dir=None,
         dataset_files_path=None,
@@ -145,21 +157,22 @@ class PortableDirectoryMetadataGenerator(MetadataCollectionStrategy):
         job_metadata=None,
         provided_metadata_style=None,
         compute_tmp_dir=None,
-        compute_version_path=None,
+        compute_version_path: Optional[str] = None,
         include_command=True,
         max_metadata_value_size=0,
         max_discovered_files=None,
-        validate_outputs=False,
+        validate_outputs: bool = False,
         object_store_conf=None,
         tool=None,
-        job=None,
-        link_data_only=False,
+        job: Optional[galaxy.model.Job] = None,
+        link_data_only: bool = False,
         kwds=None,
     ):
         assert job_metadata, "setup_external_metadata must be supplied with job_metadata path"
         kwds = kwds or {}
         if not job:
             job = sa_session.query(galaxy.model.Job).get(self.job_id)
+            assert job
         tmp_dir = _init_tmp_dir(tmp_dir)
 
         metadata_dir = os.path.join(tmp_dir, "metadata")
@@ -234,7 +247,7 @@ class PortableDirectoryMetadataGenerator(MetadataCollectionStrategy):
                 json.dump(object_store_conf, f)
 
             # setup tool
-            tool_as_dict = {}
+            tool_as_dict: Dict[str, Any] = {}
             tool_as_dict["stdio_exit_codes"] = [e.to_dict() for e in tool.stdio_exit_codes]
             tool_as_dict["stdio_regexes"] = [r.to_dict() for r in tool.stdio_regexes]
             tool_as_dict["outputs"] = {name: output.to_dict() for name, output in tool.outputs.items()}

lib/galaxy/model/__init__.py
@@ -1541,7 +1541,7 @@ class Job(Base, JobLike, UsesCreateAndUpdateTime, Dictifiable, Serializable):
     interactivetool_entry_points: Mapped[List["InteractiveToolEntryPoint"]] = relationship(
         back_populates="job", uselist=True
     )
-    implicit_collection_jobs_association: Mapped[List["ImplicitCollectionJobsJobAssociation"]] = relationship(
+    implicit_collection_jobs_association: Mapped["ImplicitCollectionJobsJobAssociation"] = relationship(
         back_populates="job", uselist=False
     )
     container: Mapped[Optional["JobContainerAssociation"]] = relationship(back_populates="job", uselist=False)
@@ -1801,7 +1801,7 @@ class Job(Base, JobLike, UsesCreateAndUpdateTime, Dictifiable, Serializable):
         add_object_to_object_session(self, assoc)
         self.input_datasets.append(assoc)
 
-    def add_output_dataset(self, name: str, dataset: "DatasetInstance"):
+    def add_output_dataset(self, name: str, dataset: "HistoryDatasetAssociation"):
         joda = JobToOutputDatasetAssociation(name, dataset)
         if dataset.dataset.job is None:
             # Only set job if dataset doesn't already have associated job.
@@ -2459,7 +2459,7 @@ class JobToOutputDatasetAssociation(Base, RepresentById):
     )
     job: Mapped["Job"] = relationship(back_populates="output_datasets")
 
-    def __init__(self, name, dataset):
+    def __init__(self, name: str, dataset: "HistoryDatasetAssociation"):
         self.name = name
         add_object_to_object_session(self, dataset)
         self.dataset = dataset
@@ -5101,8 +5101,8 @@ class DatasetInstance(RepresentById, UsesCreateAndUpdateTime, _HasTable):
                 fake_hda = HistoryDatasetAssociation(dataset=fake_dataset)
                 return fake_hda
 
-    def clear_associated_files(self, metadata_safe=False, purge=False):
-        raise Exception("Unimplemented")
+    @abc.abstractmethod
+    def clear_associated_files(self, metadata_safe=False, purge=False): ...
 
     def get_converter_types(self):
         return self.datatype.get_converter_types(self, _get_datatypes_registry())
@@ -5848,7 +5848,7 @@ class LibraryFolder(Base, Dictifiable, HasName, Serializable):
     name: Mapped[Optional[str]] = mapped_column(TEXT)
     description: Mapped[Optional[str]] = mapped_column(TEXT)
     order_id: Mapped[Optional[int]]  # not currently being used, but for possible future use
-    item_count: Mapped[Optional[int]]
+    item_count: Mapped[int] = mapped_column(nullable=True)
     deleted: Mapped[Optional[bool]] = mapped_column(index=True, default=False)
     purged: Mapped[Optional[bool]] = mapped_column(index=True, default=False)
     genome_build: Mapped[Optional[str]] = mapped_column(TrimmedString(40))
@@ -5901,7 +5901,7 @@ class LibraryFolder(Base, Dictifiable, HasName, Serializable):
         "deleted",
     ]
 
-    def __init__(self, name=None, description=None, item_count=0, order_id=None, genome_build=None):
+    def __init__(self, name=None, description=None, item_count: int = 0, order_id=None, genome_build=None):
         self.name = name or "Unnamed folder"
         self.description = description
         self.item_count = item_count

lib/galaxy/tools/__init__.py
@@ -45,9 +45,16 @@ from galaxy.exceptions import (
     ToolInputsNotReadyException,
 )
 from galaxy.job_execution import output_collect
+from galaxy.job_execution.output_collect import (
+    BaseJobContext,
+    MetadataSourceProvider,
+    PermissionProvider,
+)
 from galaxy.metadata import get_metadata_compute_strategy
 from galaxy.model import (
+    HistoryDatasetCollectionAssociation,
     Job,
+    JobToOutputDatasetAssociation,
     StoredWorkflow,
 )
 from galaxy.model.dataset_collections.matching import MatchingCollections
@@ -81,10 +88,6 @@ from galaxy.tool_util.parser.interface import (
     InputSource,
     PageSource,
     ToolSource,
-)
-from galaxy.tool_util.parser.output_objects import (
-    ToolOutput,
-    ToolOutputCollection,
 )
 from galaxy.tool_util.parser.util import (
     parse_profile_version,
@@ -219,9 +222,19 @@ from .execute import (
 )
 
 if TYPE_CHECKING:
+    from sqlalchemy.orm.scoping import scoped_session
+
     from galaxy.app import UniverseApplication
     from galaxy.managers.context import ProvidesUserContext
     from galaxy.managers.jobs import JobSearch
+    from galaxy.model import LibraryFolder
+    from galaxy.objectstore import ObjectStore
+    from galaxy.schema.schema import JobState
+    from galaxy.tool_util.parser.output_objects import (
+        ToolOutputBase,
+        ToolOutputCollection,
+    )
+    from galaxy.tool_util.provided_metadata import BaseToolProvidedMetadata
     from galaxy.tools.actions.metadata import SetMetadataToolAction
 
 log = logging.getLogger(__name__)
@@ -763,6 +776,186 @@ class _Options(Bunch):
     refresh: str
 
 
+class JobContext(BaseJobContext):
+    def __init__(
+        self,
+        tool: "Tool",
+        tool_provided_metadata: "BaseToolProvidedMetadata",
+        job: Job,
+        job_working_directory: str,
+        permission_provider,
+        metadata_source_provider,
+        input_dbkey,
+        object_store: "ObjectStore",
+        final_job_state: "JobState",
+        max_discovered_files: Optional[int],
+        flush_per_n_datasets=None,
+    ):
+        self.tool = tool
+        self._metadata_source_provider = metadata_source_provider
+        self._permission_provider = permission_provider
+        self._input_dbkey = input_dbkey
+        self.app = tool.app
+        self._sa_session = tool.sa_session
+        self._job = job
+        self.job_working_directory = job_working_directory
+        self.tool_provided_metadata = tool_provided_metadata
+        self._object_store = object_store
+        self.final_job_state = final_job_state
+        self._flush_per_n_datasets = flush_per_n_datasets
+        self.max_discovered_files = float("inf") if max_discovered_files is None else max_discovered_files
+        self.discovered_file_count = 0
+        self._tag_handler = None
+
+    @property
+    def change_datatype_actions(self):
+        return self.job.get_change_datatype_actions()
+
+    @property
+    def tag_handler(self):
+        if self._tag_handler is None:
+            self._tag_handler = self.app.tag_handler.create_tag_handler_session(self.job.galaxy_session)
+        return self._tag_handler
+
+    @property
+    def work_context(self):
+        return WorkRequestContext(self.app, user=self.user, galaxy_session=self.job.galaxy_session)
+
+    @property
+    def sa_session(self) -> "scoped_session":
+        return self._sa_session
+
+    @property
+    def permission_provider(self) -> PermissionProvider:
+        return self._permission_provider
+
+    @property
+    def metadata_source_provider(self) -> MetadataSourceProvider:
+        return self._metadata_source_provider
+
+    @property
+    def job(self) -> Job:
+        return self._job
+
+    @property
+    def flush_per_n_datasets(self) -> Optional[int]:
+        return self._flush_per_n_datasets
+
+    @property
+    def input_dbkey(self) -> str:
+        return self._input_dbkey
+
+    @property
+    def object_store(self) -> "ObjectStore":
+        return self._object_store
+
+    @property
+    def user(self):
+        if self.job:
+            user = self.job.user
+        else:
+            user = None
+        return user
+
+    def persist_object(self, obj):
+        self.sa_session.add(obj)
+
+    def flush(self):
+        self.sa_session.commit()
+
+    def get_library_folder(self, destination: Dict[str, Any]):
+        folder_id = destination.get("library_folder_id")
+        assert folder_id
+        decoded_folder_id = self.app.security.decode_id(folder_id) if isinstance(folder_id, str) else folder_id
+        library_folder = self.app.library_folder_manager.get(self.work_context, decoded_folder_id)
+        return library_folder
+
+    def get_hdca(self, object_id):
+        hdca = self.sa_session.query(HistoryDatasetCollectionAssociation).get(int(object_id))
+        return hdca
+
+    def create_library_folder(self, parent_folder: "LibraryFolder", name: str, description: str):
+        nested_folder = self.app.library_folder_manager.create(self.work_context, parent_folder.id, name, description)
+        return nested_folder
+
+    def create_hdca(self, name, structure):
+        history = self.job.history
+        trans = self.work_context
+        collection_manager = self.app.dataset_collection_manager
+        hdca = collection_manager.precreate_dataset_collection_instance(trans, history, name, structure=structure)
+        return hdca
+
+    def add_output_dataset_association(self, name, dataset):
+        assoc = JobToOutputDatasetAssociation(name, dataset)
+        assoc.job = self.job
+        self.sa_session.add(assoc)
+
+    def add_library_dataset_to_folder(self, library_folder, ld):
+        trans = self.work_context
+        ldda = ld.library_dataset_dataset_association
+        trans.sa_session.add(ldda)
+
+        trans = self.work_context
+        trans.app.security_agent.copy_library_permissions(trans, library_folder, ld)
+        trans.sa_session.add(ld)
+        trans.sa_session.commit()
+
+        # Permissions must be the same on the LibraryDatasetDatasetAssociation and the associated LibraryDataset
+        trans.app.security_agent.copy_library_permissions(trans, ld, ldda)
+        # Copy the current user's DefaultUserPermissions to the new LibraryDatasetDatasetAssociation.dataset
+        trans.app.security_agent.set_all_dataset_permissions(
+            ldda.dataset, trans.app.security_agent.user_get_default_permissions(trans.user), flush=False, new=True
+        )
+        library_folder.add_library_dataset(ld, genome_build=ldda.dbkey)
+        trans.sa_session.add(library_folder)
+        trans.sa_session.commit()
+
+        trans.sa_session.add(ld)
+        trans.sa_session.commit()
+
+    def add_datasets_to_history(self, datasets, for_output_dataset=None):
+        sa_session = self.sa_session
+        assert self.job.history
+        self.job.history.stage_addition(datasets)
+        pending_histories = {self.job.history}
+        if for_output_dataset is not None:
+            # Need to update all associated output hdas, i.e. history was
+            # shared with job running
+            for copied_dataset in for_output_dataset.dataset.history_associations:
+                if copied_dataset == for_output_dataset:
+                    continue
+                for dataset in datasets:
+                    new_data = dataset.copy()
+                    copied_dataset.history.stage_addition(new_data)
+                    pending_histories.add(copied_dataset.history)
+                    sa_session.add(new_data)
+        for history in pending_histories:
+            history.add_pending_items()
+
+    def output_collection_def(self, name):
+        tool = self.tool
+        if name not in tool.output_collections:
+            return None
+        output_collection_def = tool.output_collections[name]
+        return output_collection_def
+
+    def output_def(self, name):
+        tool = self.tool
+        if name not in tool.outputs:
+            return None
+        output_collection_def = tool.outputs[name]
+        return output_collection_def
+
+    def job_id(self):
+        return self.job.id
+
+    def get_job_id(self):
+        return self.job.id
+
+    def get_implicit_collection_jobs_association_id(self):
+        return self.job.implicit_collection_jobs_association and self.job.implicit_collection_jobs_association.id
+
+
 class Tool(UsesDictVisibleKeys):
     """
     Represents a computational tool that can be executed through Galaxy.
@@ -848,7 +1041,7 @@ class Tool(UsesDictVisibleKeys):
         self.tool_errors = None
         # Parse XML element containing configuration
         self.tool_source = tool_source
-        self.outputs: Dict[str, ToolOutput] = {}
+        self.outputs: Dict[str, ToolOutputBase] = {}
         self.output_collections: Dict[str, ToolOutputCollection] = {}
         self._is_workflow_compatible = None
         self.__help = None
@@ -1493,7 +1686,7 @@ class Tool(UsesDictVisibleKeys):
                 self.input_required = True
                 break
 
-    def parse_outputs(self, tool_source):
+    def parse_outputs(self, tool_source: ToolSource):
         """
         Parse <outputs> elements and fill in self.outputs (keyed by name)
         """
@@ -2379,7 +2572,7 @@ class Tool(UsesDictVisibleKeys):
         out_collections,
         tool_provided_metadata,
         tool_working_directory,
-        job,
+        job: Job,
         input_ext,
         input_dbkey,
         inp_data=None,
@@ -2392,9 +2585,9 @@ class Tool(UsesDictVisibleKeys):
         # given the job_execution import is the only one, probably makes sense to refactor this out
         # into job_wrapper.
         tool = self
-        permission_provider = output_collect.PermissionProvider(inp_data, tool.app.security_agent, job)
-        metadata_source_provider = output_collect.MetadataSourceProvider(inp_data)
-        job_context = output_collect.JobContext(
+        permission_provider = PermissionProvider(inp_data, tool.app.security_agent, job)
+        metadata_source_provider = MetadataSourceProvider(inp_data)
+        job_context = JobContext(
             tool,
             tool_provided_metadata,
             job,
@@ -2731,7 +2924,7 @@ class Tool(UsesDictVisibleKeys):
             if isinstance(value, model.HistoryDatasetAssociation):
                 id = value.dataset.id
                 source = hda_source_dict
-            elif isinstance(value, model.HistoryDatasetCollectionAssociation):
+            elif isinstance(value, HistoryDatasetCollectionAssociation):
                 id = value.collection.id
                 source = hdca_source_dict
             else:

lib/galaxy/util/__init__.py
@@ -314,7 +314,10 @@ def file_reader(fp, chunk_size=CHUNK_SIZE):
         yield data
 
 
-def chunk_iterable(it: Iterable, size: int = 1000) -> Iterator[tuple]:
+ItemType = TypeVar("ItemType")
+
+
+def chunk_iterable(it: Iterable[ItemType], size: int = 1000) -> Iterator[Tuple[ItemType, ...]]:
     """
     Break an iterable into chunks of ``size`` elements.
 
@@ -1076,9 +1079,6 @@ def string_as_bool_or_none(string):
         return None
     else:
         return False
-
-
-ItemType = TypeVar("ItemType")
 
 
 @overload

commit 19bbf7e6068dd545d1e63b81555b925be663a609

Fix broken optimisation
introduced in commit b7591a0872c4594bc24dd5e1f1827eabf300b16b
(PR https://github.com/galaxyproject/galaxy/pull/19852/ ).

Also removed 2 unnecessary asserts.

lib/galaxy/tools/actions/__init__.py
@@ -634,10 +634,11 @@ class DefaultToolAction(ToolAction):
                         current_element_identifiers = element_identifiers
                         current_collection_type = output.structure.collection_type
                         assert current_collection_type
-                        # TODO: replace following line with formal abstractions for doing this.
-                        current_collection_type = ":".join(current_collection_type.split(":")[1:])
 
                         for parent_id in output_part_def.parent_ids or []:
+                            # Get subcollection type
+                            # TODO: replace following line with formal abstractions for doing this.
+                            current_collection_type = current_collection_type.partition(":")[2]
                             name_to_index = {
                                 value["name"]: index for (index, value) in enumerate(current_element_identifiers)
                             }

lib/galaxy/workflow/run.py
@@ -239,7 +239,6 @@ class WorkflowInvoker:
 
                 if not workflow_invocation_step:
                     workflow_invocation_step = WorkflowInvocationStep()
-                    assert workflow_invocation_step
                     workflow_invocation_step.workflow_invocation = workflow_invocation
                     ensure_object_added_to_session(workflow_invocation_step, object_in_session=workflow_invocation)
                     workflow_invocation_step.workflow_step = step
@@ -247,7 +246,6 @@ class WorkflowInvoker:
 
                     workflow_invocation.steps.append(workflow_invocation_step)
 
-                assert workflow_invocation_step
                 incomplete_or_none = self._invoke_step(workflow_invocation_step)
                 if incomplete_or_none is False:
                     step_delayed = delayed_steps = True