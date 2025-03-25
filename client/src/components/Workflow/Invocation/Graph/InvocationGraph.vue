7057ff245804b355b2760e38c1f75237b77aa9d2
Ahmed Awan <qe66653@umbc.edu>
2025-03-25 16:27:35 -0500

    make currently viewed step header shorter

--- client/src/components/Workflow/Invocation/Graph/InvocationGraph.vue
+++ client/src/components/Workflow/Invocation/Graph/InvocationGraph.vue
@@ -204,7 +204,7 @@ function stepClicked(nodeId: number | null) {
             </div>
             <BCard v-if="activeNodeId !== null" ref="stepCard" class="mt-2" no-body>
                 <BCardHeader
-                    class="d-flex justify-content-between align-items-center px-3 py-2"
+                    class="d-flex justify-content-between align-items-center px-3 py-1"
                     :class="activeNodeId !== null ? steps[activeNodeId]?.headerClass : ''">
                     <WorkflowInvocationStepHeader
                         class="w-100 pr-2"
