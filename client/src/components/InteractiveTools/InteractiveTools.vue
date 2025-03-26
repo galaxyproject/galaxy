f88163bcc7c033a2c0ebccd824bba5cffaa5ae33
Dannon Baker <dannon.baker@gmail.com>
2025-03-26 09:57:55 -0400

    Reorder InteractiveTools component blocks -- no changes

--- client/src/components/InteractiveTools/InteractiveTools.vue
+++ client/src/components/InteractiveTools/InteractiveTools.vue
@@ -1,69 +1,3 @@
-<template>
-    <div aria-labelledby="interactive-tools-heading">
-        <b-alert v-for="(message, index) in messages" :key="index" :show="3" variant="danger">{{ message }}</b-alert>
-        <h1 id="interactive-tools-heading" class="h-lg">Active Interactive Tools</h1>
-        <b-row class="mb-3">
-            <b-col cols="6">
-                <b-input
-                    id="interactivetool-search"
-                    v-model="filter"
-                    class="m-1"
-                    name="query"
-                    placeholder="Search Interactive Tool"
-                    autocomplete="off"
-                    type="text" />
-            </b-col>
-        </b-row>
-        <b-table
-            id="interactive-tool-table"
-            striped
-            :fields="fields"
-            :items="activeInteractiveTools"
-            :filter="filter"
-            @filtered="filtered">
-            <template v-slot:cell(actions)="row">
-                <b-button
-                    :id="createId('stop', row.item.id)"
-                    v-b-tooltip.hover
-                    variant="link"
-                    class="p-0"
-                    title="Stop this interactive tool"
-                    @click.stop="stopInteractiveTool(row.item.id, row.item.name)">
-                    <FontAwesomeIcon :icon="['fas', 'stop-circle']" />
-                </b-button>
-            </template>
-            <template v-slot:cell(name)="row">
-                <a
-                    :id="createId('link', row.item.id)"
-                    v-b-tooltip
-                    title="Open Interactive Tool"
-                    :index="row.index"
-                    :href="row.item.target"
-                    target="_blank"
-                    :name="row.item.name"
-                    >{{ row.item.name }}
-                    <FontAwesomeIcon :icon="['fas', 'external-link-alt']" />
-                </a>
-            </template>
-            <template v-slot:cell(job_info)="row">
-                <label v-if="row.item.active"> running </label>
-                <label v-else> stopped </label>
-            </template>
-            <template v-slot:cell(created_time)="row">
-                <UtcDate :date="row.item.created_time" mode="elapsed" />
-            </template>
-            <template v-slot:cell(last_updated)="row">
-                <UtcDate :date="row.item.modified_time" mode="elapsed" />
-            </template>
-        </b-table>
-        <label v-if="isActiveToolsListEmpty">You do not have active interactive tools yet </label>
-        <div v-if="showNotFound">
-            No matching entries found for: <span class="font-weight-bold">{{ filter }}</span
-            >.
-        </div>
-    </div>
-</template>
-
 <script setup lang="ts">
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faExternalLinkAlt, faStopCircle } from "@fortawesome/free-solid-svg-icons";
@@ -154,3 +88,68 @@ onMounted(() => {
     load();
 });
 </script>
+<template>
+    <div aria-labelledby="interactive-tools-heading">
+        <b-alert v-for="(message, index) in messages" :key="index" :show="3" variant="danger">{{ message }}</b-alert>
+        <h1 id="interactive-tools-heading" class="h-lg">Active Interactive Tools</h1>
+        <b-row class="mb-3">
+            <b-col cols="6">
+                <b-input
+                    id="interactivetool-search"
+                    v-model="filter"
+                    class="m-1"
+                    name="query"
+                    placeholder="Search Interactive Tool"
+                    autocomplete="off"
+                    type="text" />
+            </b-col>
+        </b-row>
+        <b-table
+            id="interactive-tool-table"
+            striped
+            :fields="fields"
+            :items="activeInteractiveTools"
+            :filter="filter"
+            @filtered="filtered">
+            <template v-slot:cell(actions)="row">
+                <b-button
+                    :id="createId('stop', row.item.id)"
+                    v-b-tooltip.hover
+                    variant="link"
+                    class="p-0"
+                    title="Stop this interactive tool"
+                    @click.stop="stopInteractiveTool(row.item.id, row.item.name)">
+                    <FontAwesomeIcon :icon="['fas', 'stop-circle']" />
+                </b-button>
+            </template>
+            <template v-slot:cell(name)="row">
+                <a
+                    :id="createId('link', row.item.id)"
+                    v-b-tooltip
+                    title="Open Interactive Tool"
+                    :index="row.index"
+                    :href="row.item.target"
+                    target="_blank"
+                    :name="row.item.name"
+                    >{{ row.item.name }}
+                    <FontAwesomeIcon :icon="['fas', 'external-link-alt']" />
+                </a>
+            </template>
+            <template v-slot:cell(job_info)="row">
+                <label v-if="row.item.active"> running </label>
+                <label v-else> stopped </label>
+            </template>
+            <template v-slot:cell(created_time)="row">
+                <UtcDate :date="row.item.created_time" mode="elapsed" />
+            </template>
+            <template v-slot:cell(last_updated)="row">
+                <UtcDate :date="row.item.modified_time" mode="elapsed" />
+            </template>
+        </b-table>
+        <label v-if="isActiveToolsListEmpty">You do not have active interactive tools yet </label>
+        <div v-if="showNotFound">
+            No matching entries found for: <span class="font-weight-bold">{{ filter }}</span
+            >.
+        </div>
+    </div>
+</template>
