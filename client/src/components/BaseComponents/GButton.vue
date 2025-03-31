1a11037f7698e862cc904354f2d518625982b783
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2025-03-31 18:28:55 +0200

    add base styles

---
+++ client/src/components/BaseComponents/GButton.vue
@@ -0,0 +1,163 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { RouterLink } from "vue-router";
+
+import { type ComponentColor, type ComponentSize, type ComponentVariantClassList, prefix } from "./componentVariants";
+
+const props = defineProps<{
+    href?: string;
+    to?: string;
+    color?: ComponentColor;
+    outline?: boolean;
+    disabled?: boolean;
+    title?: string;
+    disabledTitle?: string;
+    size?: ComponentSize;
+}>();
+
+const emit = defineEmits<{
+    (e: "click", event: PointerEvent): void;
+}>();
+
+function onClick(event: PointerEvent) {
+    emit("click", event);
+}
+
+const variantClasses = computed(() => {
+    const classObject = {} as ComponentVariantClassList;
+    classObject[prefix(props.color ?? "grey")] = true;
+    classObject[prefix(props.size ?? "medium")] = true;
+    return classObject;
+});
+
+const styleClasses = computed(() => {
+    return {
+        outline: props.outline,
+    };
+});
+
+const baseComponent = computed(() => {
+    if (props.to) {
+        return RouterLink;
+    } else if (props.href) {
+        return "a" as const;
+    } else {
+        return "button" as const;
+    }
+});
+</script>
+
+<template>
+    <component
+        :is="baseComponent"
+        class="g-button"
+        :class="{ ...variantClasses, ...styleClasses }"
+        :to="props.to"
+        :href="props.href"
+        @click="onClick">
+        <slot></slot>
+    </component>
+</template>
+
+<style scoped lang="scss">
+.g-button {
+    display: inline-block;
+    margin: 0;
+    border: 1px solid;
+    border-radius: var(--spacing);
+    text-decoration: none;
+    vertical-align: middle;
+
+    &:focus-visible {
+        box-shadow: 0 0 0 0.2rem var(--color-blue-400);
+    }
+
+    // sizes
+    &.g-small {
+        font-size: var(--font-size-small);
+        padding: var(--spacing-1) var(--spacing-2);
+    }
+
+    &.g-medium {
+        font-size: var(--font-size-medium);
+        padding: var(--spacing-1) var(--spacing-2);
+    }
+
+    &.g-large {
+        font-size: var(--font-size-large);
+        padding: var(--spacing-2) var(--spacing-3);
+    }
+
+    // colors
+    &.g-grey {
+        background-color: var(--color-grey-200);
+        border-color: var(--color-grey-300);
+        color: var(--color-grey-800);
+
+        &:hover,
+        &:focus,
+        &:focus-visible {
+            background-color: var(--color-grey-300);
+            border-color: var(--color-grey-400);
+        }
+
+        &:active {
+            background-color: var(--color-grey-400);
+            border-color: var(--color-grey-600);
+        }
+
+        &:focus-visible {
+            border-color: var(--color-grey-600);
+        }
+    }
+
+    @each $color in "blue", "green", "red", "yellow", "orange" {
+        &.g-#{$color} {
+            background-color: var(--color-#{$color}-600);
+            border-color: var(--color-#{$color}-600);
+            color: var(--color-#{$color}-100);
+
+            &:hover,
+            &:focus,
+            &:focus-visible {
+                background-color: var(--color-#{$color}-700);
+                border-color: var(--color-#{$color}-700);
+            }
+
+            &:active {
+                background-color: var(--color-#{$color}-600);
+                color: var(--color-#{$color}-100);
+            }
+
+            &:focus-visible {
+                border-color: var(--color-#{$color}-900);
+            }
+        }
+
+        &.outline.g-#{$color} {
+            background-color: rgb(100% 100% 100% / 0);
+            border-color: var(--color-#{$color}-600);
+            color: var(--color-#{$color}-600);
+
+            &:hover,
+            &:focus,
+            &:focus-visible {
+                background-color: var(--color-#{$color}-600);
+                border-color: var(--color-#{$color}-600);
+                color: var(--color-#{$color}-100);
+            }
+
+            &:active {
+                background-color: var(--color-#{$color}-600);
+                color: var(--color-#{$color}-100);
+            }
+
+            &:focus-visible {
+                border-color: var(--color-#{$color}-900);
+                background-color: var(--color-#{$color}-200);
+                color: var(--color-#{$color}-600);
+            }
+        }
+    }
+}
+</style>
