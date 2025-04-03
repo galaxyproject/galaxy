1b170553036bf502cd5ca6d68775c1edf747e432
guerler <aysam.guerler@gmail.com>
2025-04-03 11:32:30 -0400

    Fix lint warning for cell action wrapper

--- client/src/components/Markdown/Editor/CellAction.vue
+++ client/src/components/Markdown/Editor/CellAction.vue
@@ -8,33 +8,43 @@
             trigger="click"
             placement="right"
             mode="light">
-            <div @click="popperRef.visible = false">
+            <nav
+                role="menu"
+                aria-label="cell actions"
+                tabindex="0"
+                @click="popperRef.visible = false"
+                @keydown.enter="popperRef.visible = false"
+                @keydown.space.prevent="popperRef.visible = false">
                 <span class="d-flex justify-content-between">
                     <small class="my-1 mx-3 text-info">{{ title }}</small>
                 </span>
                 <CellOption
+                    role="menuitem"
                     title="Clone"
                     description="Create a copy of this cell"
                     :icon="faClone"
                     @click="$emit('clone')" />
                 <CellOption
+                    role="menuitem"
                     title="Delete"
                     description="Delete this cell"
                     :icon="faTrash"
                     @click="confirmDelete = true" />
                 <CellOption
                     v-if="cellIndex > 0"
+                    role="menuitem"
                     title="Move Up"
                     description="Move this cell upwards"
                     :icon="faArrowUp"
                     @click="$emit('move', 'up')" />
                 <CellOption
                     v-if="cellTotal - cellIndex > 1"
+                    role="menuitem"
                     title="Move Down"
                     description="Move this cell downwards"
                     :icon="faArrowDown"
                     @click="$emit('move', 'down')" />
-            </div>
+            </nav>
         </Popper>
         <BModal v-model="confirmDelete" title="Delete Cell" title-tag="h2" @ok="$emit('delete')">
             <p v-localize>Are you sure you want to delete this cell?</p>
