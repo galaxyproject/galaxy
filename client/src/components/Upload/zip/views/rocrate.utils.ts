fbc3ba1fec4a13352d298ca03f560fc018204349
davelopez <46503462+davelopez@users.noreply.github.com>
2025-04-15 10:03:51 +0200

    Add more metadata to ROCrateSummary

--- client/src/components/Upload/zip/views/rocrate.utils.ts
+++ client/src/components/Upload/zip/views/rocrate.utils.ts
@@ -1,3 +1,4 @@
+// TODO: consider moving these types and logic to ro-crate-zip-explorer
 import { type ROCrateEntity, type ROCrateImmutableView } from "ro-crate-zip-explorer";
 
 interface Conforms {
@@ -11,18 +12,25 @@ interface Person {
     name: string;
     email?: string;
     identifier?: string;
+    type: "Person";
 }
 
 interface Organization {
     id: string;
     name: string;
+    type: "Organization";
 }
 
+type Creator = Person | Organization;
+
 export interface ROCrateSummary {
+    name: string;
+    description: string;
     publicationDate: Date;
     conformsTo: Conforms[];
     license: string;
-    creators: Person[] | Organization[];
+    creators: Creator[];
+    identifier?: string;
 }
 
 function isOfType(item: ROCrateEntity, type: string): boolean {
@@ -42,7 +50,19 @@ export async function extractROCrateSummary(crate: ROCrateImmutableView): Promis
         throw new Error("Invalid RO-Crate file");
     }
 
+    // Try to get the root dataset name, if not available, try to get the mainEntity name
+    const name =
+        root.name ??
+        (typeof root.mainEntity === "object" && root.mainEntity && "name" in root.mainEntity
+            ? String(root.mainEntity.name)
+            : "Unknown");
+
     const publicationDate = new Date(root.datePublished);
+
+    let identifier = root.identifier;
+    if (typeof identifier === "object") {
+        identifier = identifier["@id"];
+    }
 
     let conformsTo: Conforms[] = [];
     if ("conformsTo" in root && Array.isArray(root.conformsTo)) {
@@ -60,31 +80,43 @@ export async function extractROCrateSummary(crate: ROCrateImmutableView): Promis
         });
     }
 
-    const license = String(root.license);
-
-    // TODO: Handle main entity
+    let license = root.license ? String(root.license) : "Unknown";
+    if (typeof root.license === "object") {
+        license = root.license["@id"];
+        const licenseEntity = crate.getEntity(license);
+        if (licenseEntity && "name" in licenseEntity) {
+            license = String(licenseEntity.name);
+        }
+    }
 
     const creators = crate.graph
         .filter((item) => isOfType(item, "Person") || isOfType(item, "Organization"))
         .map((creator) => {
             if (isOfType(creator, "Person")) {
-                return {
+                const person: Person = {
                     id: creator["@id"],
                     name: String(creator.name),
                     email: String(creator.email),
                     identifier: String(creator.identifier),
+                    type: "Person",
                 };
+                return person;
             }
-            return {
+            const organization: Organization = {
                 id: creator["@id"],
                 name: String(creator.name),
+                type: "Organization",
             };
+            return organization;
         });
 
     return {
+        name,
+        description: root.description,
         publicationDate,
         conformsTo,
         license,
         creators,
+        identifier,
     };
 }
