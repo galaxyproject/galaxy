211ec63f41e3ff86afbd84d53585949e6fe33b6d
davelopez <46503462+davelopez@users.noreply.github.com>
2025-04-15 10:04:29 +0200

    Refactor RoCrateZipView to use GCard
    And enhance display of RO-Crate summary

--- client/src/components/Upload/zip/views/RoCrateZipView.vue
+++ client/src/components/Upload/zip/views/RoCrateZipView.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import { faBoxOpen, faUniversity, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { BBadge } from "bootstrap-vue";
+import { sanitize } from "dompurify";
 import { onMounted, ref } from "vue";
 
+import { type CardBadge } from "@/components/Common/GCard.types";
 import type { ROCrateZipExplorer } from "@/composables/zipExplorer";
 
 import { extractROCrateSummary, type ROCrateSummary } from "./rocrate.utils";
 
-import Heading from "@/components/Common/Heading.vue";
+import DOILink from "@/components/Common/DOILink.vue";
+import GCard from "@/components/Common/GCard.vue";
 import ExternalLink from "@/components/ExternalLink.vue";
 import LoadingSpan from "@/components/LoadingSpan.vue";
 import UtcDate from "@/components/UtcDate.vue";
@@ -15,6 +21,16 @@ const props = defineProps<{
 }>();
 
 const crateSummary = ref<ROCrateSummary>();
+const badges: CardBadge[] = [
+    {
+        id: "rocrate",
+        label: "RO-Crate",
+        title: "A Research Object Crate is a structured package for sharing research data.",
+        variant: "info",
+        icon: faBoxOpen,
+        visible: true,
+    },
+];
 
 onMounted(async () => {
     crateSummary.value = await extractROCrateSummary(props.explorer.crate);
@@ -22,35 +38,60 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div v-if="crateSummary" class="rocrate-explorer">
-        <Heading size="lg">RO-Crate Summary</Heading>
+    <GCard
+        v-if="crateSummary"
+        id="rocrate-summary"
+        class="mt-1"
+        :badges="badges"
+        :title="crateSummary.name"
+        title-size="md">
+        <template v-slot:titleBadges>
+            <div>
+                <strong>Publication Date:</strong>
+                <UtcDate :date="crateSummary.publicationDate.toISOString()" mode="pretty" />
+                (<UtcDate :date="crateSummary.publicationDate.toISOString()" mode="elapsed" />)
+            </div>
 
-        <div>
-            <strong>Publication Date:</strong>
-            <UtcDate :date="crateSummary.publicationDate.toISOString()" mode="pretty" />
-            (<UtcDate :date="crateSummary.publicationDate.toISOString()" mode="elapsed" />)
-        </div>
+            <div v-if="crateSummary.identifier">
+                <strong>DOI:</strong>
+                <DOILink :doi="crateSummary.identifier" />
+            </div>
+        </template>
 
-        <strong>License:</strong> {{ crateSummary.license }}
+        <template v-slot:description>
+            <div v-html="sanitize(crateSummary.description)"></div>
 
-        <div v-if="crateSummary.creators.length > 0">
+            <div><strong>License:</strong> {{ crateSummary.license }}</div>
+
             <strong>Creators</strong>
-            <ul>
-                <li v-for="creator in crateSummary.creators" :key="creator.id">
+            <div v-if="crateSummary.creators.length > 0" class="d-flex flex-wrap flex-gapx-1 flex-gapy-1 px-1">
+                <BBadge
+                    v-for="creator in crateSummary.creators"
+                    :key="creator.id"
+                    variant="outline-secondary"
+                    class="outline-badge">
+                    <span v-if="creator.type === 'Person'">
+                        <FontAwesomeIcon :icon="faUser" />
+                    </span>
+                    <span v-else-if="creator.type === 'Organization'">
+                        <FontAwesomeIcon :icon="faUniversity" />
+                    </span>
                     {{ creator.name }}
-                </li>
-            </ul>
-        </div>
+                </BBadge>
+            </div>
 
-        <div v-if="crateSummary.conformsTo.length > 0">
             <strong>Conforms To</strong>
-            <ul>
-                <li v-for="conform in crateSummary.conformsTo" :key="conform.id">
+            <div v-if="crateSummary.conformsTo.length > 0" class="d-flex flex-wrap flex-gapx-1 flex-gapy-1 px-1">
+                <BBadge
+                    v-for="conform in crateSummary.conformsTo"
+                    :key="conform.id"
+                    variant="outline-secondary"
+                    class="outline-badge">
                     <ExternalLink :href="conform.id"> {{ conform.name }} {{ conform.version }} </ExternalLink>
-                </li>
-            </ul>
-        </div>
-    </div>
+                </BBadge>
+            </div>
+        </template>
+    </GCard>
     <div v-else>
         <LoadingSpan message="Loading RO-Crate summary..." />
     </div>
