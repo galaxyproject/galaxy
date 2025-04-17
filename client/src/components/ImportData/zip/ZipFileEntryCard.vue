85d04c4a45843eb8ee4e7879101f9457bda12911
davelopez <46503462+davelopez@users.noreply.github.com>
2025-04-17 16:42:25 +0200

    Refactor ZipFileEntryCard

--- client/src/components/ImportData/zip/ZipFileEntryCard.vue
+++ client/src/components/ImportData/zip/ZipFileEntryCard.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { faDatabase, faFile } from "@fortawesome/free-solid-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
 
-import type { CardBadge } from "@/components/Common/GCard.types";
 import type { ImportableFile } from "@/composables/zipExplorer";
 import { bytesToString } from "@/utils/utils";
 
@@ -19,18 +18,6 @@ const emit = defineEmits<{
     (e: "select"): void;
 }>();
 
-const titleBadges: CardBadge[] = [
-    {
-        id: "file-path",
-        label: props.file.path,
-        title: "File Path",
-        icon: faFile,
-        type: "badge",
-        variant: "secondary",
-        visible: true,
-    },
-];
-
 const badges = [
     {
         id: "file-size",
@@ -46,9 +33,7 @@ const badges = [
     <GCard
         :id="file.path"
         :title="file.name"
-        :title-badges="titleBadges"
         :badges="badges"
-        :description="file.description"
         clickable
         :update-time="file.dateTime.toISOString()"
         selectable
@@ -56,5 +41,24 @@ const badges = [
         :selected="props.selected"
         @select="emit('select')"
         @click="emit('select')">
+        <template v-slot:description>
+            <div v-if="file.path !== file.name" class="zip-file-path text-muted">
+                {{ file.path }}
+            </div>
+
+            <div v-if="file.description">
+                {{ file.description }}
+            </div>
+        </template>
     </GCard>
 </template>
+
+<style scoped lang="scss">
+.zip-file-path {
+    font-size: 0.9rem;
+    font-style: italic;
+    margin-bottom: 0.5rem;
+    word-break: break-all;
+    overflow-wrap: break-word;
+}
+</style>
