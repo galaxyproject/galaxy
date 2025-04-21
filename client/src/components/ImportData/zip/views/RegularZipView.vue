b9d9eb404d0ff921c4f94056572089ef801ea60b
davelopez <46503462+davelopez@users.noreply.github.com>
2025-04-21 12:19:00 +0200

    Refactor RegularZipView

--- client/src/components/ImportData/zip/views/RegularZipView.vue
+++ client/src/components/ImportData/zip/views/RegularZipView.vue
@@ -3,21 +3,33 @@ import { computed } from "vue";
 
 import { getImportableFiles, type IZipExplorer } from "@/composables/zipExplorer";
 
-import Heading from "@/components/Common/Heading.vue";
-import ZipFileEntryCard from "@/components/ImportData/zip/ZipFileEntryCard.vue";
+import GCard from "@/components/Common/GCard.vue";
+import ZipFileEntrySummaryCard from "@/components/ImportData/zip/ZipFileEntrySummaryCard.vue";
 
 const props = defineProps<{
     explorer: IZipExplorer;
 }>();
 
 const files = computed(() => getImportableFiles(props.explorer));
+
+const zipFileBadges = [
+    {
+        id: "file-count",
+        label: `${files.value.length} file${files.value.length > 1 ? "s" : ""} available`,
+        title: "Number of Files available to import",
+        visible: true,
+    },
+];
 </script>
 
 <template>
-    <div class="rocrate-explorer">
-        <Heading size="lg">ZIP Contents Summary</Heading>
-
-        <Heading size="md">Files</Heading>
-        <ZipFileEntryCard v-for="file in files" :key="file.path" :file="file" />
-    </div>
+    <GCard
+        id="regular-zip-summary-card"
+        title="List of files contained in the ZIP archive"
+        title-size="md"
+        :badges="zipFileBadges">
+        <template v-slot:description>
+            <ZipFileEntrySummaryCard v-for="file in files" :key="file.path" :file="file" :selectable="false" />
+        </template>
+    </GCard>
 </template>
