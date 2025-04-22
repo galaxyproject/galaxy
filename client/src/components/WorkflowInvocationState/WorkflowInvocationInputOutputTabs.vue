e954139d34388ee142f003e620ca8c9746b481c2
Ahmed Awan <qe66653@umbc.edu>
2025-04-22 13:20:45 -0500

    Always render Outputs tab in invocation view
    As opposed to conditionally rendering it when outputs become available.
    
    Fixes https://github.com/galaxyproject/galaxy/issues/20069

--- client/src/components/WorkflowInvocationState/WorkflowInvocationInputOutputTabs.vue
+++ client/src/components/WorkflowInvocationState/WorkflowInvocationInputOutputTabs.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
-import { BTab } from "bootstrap-vue";
+import { BAlert, BTab } from "bootstrap-vue";
 import { computed } from "vue";
 
 import type { InvocationInput, WorkflowInvocationElementView } from "@/api/invocations";
 
 import Heading from "../Common/Heading.vue";
+import LoadingSpan from "../LoadingSpan.vue";
 import ParameterStep from "./ParameterStep.vue";
 import GenericHistoryItem from "components/History/Content/GenericItem.vue";
 
+const OUTPUTS_NOT_AVAILABLE_YET_MSG =
+    "Either no outputs have been produced yet, or no steps were checked to " +
+    "mark their outputs as primary workflow outputs.";
+
 const props = defineProps<{
     invocation: WorkflowInvocationElementView;
-    notLazy?: boolean;
+    terminal?: boolean;
 }>();
 
 const inputData = computed(() => Object.entries(props.invocation.inputs));
@@ -38,26 +43,49 @@ function dataInputStepLabel(key: string, input: InvocationInput) {
 }
 </script>
 <template>
-    <span v-if="invocation">
-        <BTab v-if="inputData.length || parameters.length" title="Inputs">
+    <span>
+        <BTab title="Inputs">
             <div v-if="parameters.length">
                 <Heading size="text" bold separator>Parameter Values</Heading>
                 <div class="mx-1">
                     <ParameterStep :parameters="parameters" styled-table />
                 </div>
             </div>
-            <div v-for="([key, input], index) in inputData" :key="index" :data-label="dataInputStepLabel(key, input)">
-                <Heading size="text" bold separator>
-                    {{ dataInputStepLabel(key, input) }}
-                </Heading>
-                <GenericHistoryItem :item-id="input.id" :item-src="input.src" />
+            <div v-if="inputData.length">
+                <div
+                    v-for="([key, input], index) in inputData"
+                    :key="index"
+                    :data-label="dataInputStepLabel(key, input)">
+                    <Heading size="text" bold separator>
+                        {{ dataInputStepLabel(key, input) }}
+                    </Heading>
+                    <GenericHistoryItem :item-id="input.id" :item-src="input.src" />
+                </div>
             </div>
+            <BAlert v-else show variant="info"> No input data was provided for this workflow invocation. </BAlert>
         </BTab>
-        <BTab v-if="outputs.length" title="Outputs" :lazy="!props.notLazy">
-            <div v-for="([key, output], index) in outputs" :key="index">
-                <Heading size="text" bold separator>{{ key }}</Heading>
-                <GenericHistoryItem :item-id="output.id" :item-src="output.src" />
+        <BTab title="Outputs" :lazy="!props.terminal">
+            <div v-if="outputs.length">
+                <div v-for="([key, output], index) in outputs" :key="index">
+                    <Heading size="text" bold separator>{{ key }}</Heading>
+                    <GenericHistoryItem :item-id="output.id" :item-src="output.src" />
+                </div>
             </div>
+            <BAlert v-else show variant="info">
+                <p>
+                    <LoadingSpan v-if="!props.terminal" :message="OUTPUTS_NOT_AVAILABLE_YET_MSG" />
+                    <span v-else v-localize>
+                        No steps were checked to mark their outputs as primary workflow outputs.
+                    </span>
+                </p>
+                <p>
+                    To get outputs from a workflow in this tab, you need to check the
+                    <i>
+                        "Checked outputs will become primary workflow outputs and are available as subworkflow outputs."
+                    </i>
+                    option on individual outputs on individual steps in the workflow.
+                </p>
+            </BAlert>
         </BTab>
     </span>
 </template>
