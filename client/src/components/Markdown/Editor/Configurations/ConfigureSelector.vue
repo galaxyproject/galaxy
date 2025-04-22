aad36ef2a52f6b55c13a68e7d066c228b112edcb
Aysam Guerler <aysam.guerler@gmail.com>
2025-04-22 09:50:59 -0400

    Update client/src/components/Markdown/Editor/Configurations/ConfigureSelector.vue
    Co-authored-by: Alireza Heidari <itisalirh@gmail.com>

--- client/src/components/Markdown/Editor/Configurations/ConfigureSelector.vue
+++ client/src/components/Markdown/Editor/Configurations/ConfigureSelector.vue
@@ -52,7 +52,7 @@ const mappedLabels = computed(() =>
         }))
 );
 
-async function doQuery(): Promise<any> {
+async function doQuery(): Promise<OptionType[]> {
     return mappedLabels.value;
 }
 </script>
