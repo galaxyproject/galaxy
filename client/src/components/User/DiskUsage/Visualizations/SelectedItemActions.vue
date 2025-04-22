e04432e88a655667286d862f3809cc179ebe517d
Alireza Heidari <itisalirh@gmail.com>
2025-04-22 16:50:31 +0200

    Refactors history storage management UI components
    Use the GCard component and improve the delete confirmation ui.
    Move set current history logic from HistoriesStorageOverview to the SelectedItemActions to reduce complexity

--- client/src/components/User/DiskUsage/Visualizations/SelectedItemActions.vue
+++ client/src/components/User/DiskUsage/Visualizations/SelectedItemActions.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-    faArchive,
-    faChartBar,
-    faInfoCircle,
-    faLocationArrow,
-    faTrash,
-    faUndo,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { BButton } from "bootstrap-vue";
+import { faArchive, faDatabase, faInfoCircle, faTrash, faUndo } from "@fortawesome/free-solid-svg-icons";
 import { computed } from "vue";
 
 import { useHistoryStore } from "@/stores/historyStore";
-import localize from "@/utils/localization";
 import { bytesToString } from "@/utils/utils";
 
 import type { DataValuePoint } from "./Charts";
+
+import GCard from "@/components/Common/GCard.vue";
 
 type ItemTypes = "history" | "dataset";
 
@@ -28,18 +19,15 @@ interface SelectedItemActionsProps {
     canEdit?: boolean;
 }
 
-const { currentHistoryId } = useHistoryStore();
+const { currentHistoryId, setCurrentHistory } = useHistoryStore();
 
 const props = withDefaults(defineProps<SelectedItemActionsProps>(), {
     isArchived: false,
     canEdit: false,
 });
 
-library.add(faArchive, faChartBar, faInfoCircle, faLocationArrow, faTrash, faUndo);
-
 const label = computed(() => props.data?.label ?? "No data");
 const prettySize = computed(() => bytesToString(props.data?.value ?? 0));
-const viewDetailsIcon = computed(() => (props.itemType === "history" ? "chart-bar" : "info-circle"));
 const canSetAsCurrent = computed(() => props.itemType === "history" && props.data.id !== currentHistoryId);
 
 const emit = defineEmits<{
@@ -53,8 +41,8 @@ function onUndeleteItem() {
     emit("undelete-item", props.data.id);
 }
 
-function onSetCurrentHistory() {
-    emit("set-current-history", props.data.id);
+async function onSetCurrentHistory() {
+    await setCurrentHistory(props.data.id);
 }
 
 function onViewItem() {
@@ -64,58 +52,84 @@ function onViewItem() {
 function onPermanentlyDeleteItem() {
     emit("permanently-delete-item", props.data.id);
 }
-</script>
-<template>
-    <div class="selected-item-info">
-        <div class="h-md mx-2">
-            <b>{{ label }}</b>
-        </div>
-        <div class="text-muted mx-2">
-            <span v-if="isArchived"><FontAwesomeIcon icon="archive" /> This {{ itemType }} is archived.</span><br />
-            Total storage space taken: <b>{{ prettySize }}</b
-            >.
-            <span v-if="isRecoverable">
-                This {{ itemType }} was deleted. You can <b>undelete</b> it or <b>permanently delete</b> it to free up
-                its storage space.
-            </span>
-        </div>
 
-        <div class="my-2">
-            <BButton
-                v-if="canSetAsCurrent"
-                variant="outline-primary"
-                size="sm"
-                class="mx-2"
-                :title="localize('Set this history as current')"
-                @click="onSetCurrentHistory">
-                <FontAwesomeIcon icon="location-arrow" />
-            </BButton>
-            <BButton
-                variant="outline-primary"
-                size="sm"
-                class="mx-2"
-                :title="localize(`Go to the details of this ${itemType}`)"
-                @click="onViewItem">
-                <FontAwesomeIcon :icon="viewDetailsIcon" />
-            </BButton>
-            <BButton
-                v-if="isRecoverable && canEdit"
-                variant="outline-primary"
-                size="sm"
-                class="mx-2"
-                :title="localize(`Undelete this ${itemType}`)"
-                @click="onUndeleteItem">
-                <FontAwesomeIcon icon="undo" />
-            </BButton>
-            <BButton
-                v-if="canEdit"
-                variant="outline-danger"
-                size="sm"
-                class="mx-2"
-                :title="localize(`Permanently delete this ${itemType}`)"
-                @click="onPermanentlyDeleteItem">
-                <FontAwesomeIcon icon="trash" />
-            </BButton>
-        </div>
-    </div>
+const titleIcon = computed(() => {
+    return props.isArchived
+        ? { icon: faArchive, title: "This item is archived" }
+        : { icon: faInfoCircle, title: `Details of this ${props.itemType}` };
+});
+
+const badges = computed(() => [
+    {
+        id: "count",
+        label: prettySize.value,
+        title: "Total storage space taken",
+        icon: faDatabase,
+        visible: true,
+    },
+]);
+
+const description = computed(() => {
+    let ds = "";
+
+    if (props.isArchived) {
+        ds = `This ${props.itemType} is archived.`;
+    }
+    if (props.isRecoverable) {
+        ds = `This ${props.itemType} was deleted. You can **undelete** it or **permanently delete** it to free up its storage space.`;
+    }
+
+    return ds;
+});
+
+const primaryActions = computed(() => {
+    return [
+        {
+            id: "undelete",
+            label: "Restore",
+            icon: faUndo,
+            title: `Undelete this ${props.itemType}`,
+            variant: "outline-primary",
+            handler: onUndeleteItem,
+            visible: props.isRecoverable && props.canEdit,
+        },
+        {
+            id: "delete",
+            label: "Delete",
+            icon: faTrash,
+            title: `Permanently delete this ${props.itemType}`,
+            variant: "outline-danger",
+            handler: onPermanentlyDeleteItem,
+            visible: props.canEdit,
+        },
+        {
+            id: "select",
+            label: "Set as current",
+            title: "Set this history as current",
+            variant: "outline-primary",
+            handler: onSetCurrentHistory,
+            visible: canSetAsCurrent.value,
+        },
+        {
+            id: "explore",
+            label: "Explore content",
+            icon: faInfoCircle,
+            title: `Explore the content of this ${props.itemType}`,
+            variant: "outline-primary",
+            handler: onViewItem,
+            visible: true,
+        },
+    ];
+});
+</script>
+
+<template>
+    <GCard
+        :id="`selected-item-actions-${props.itemType}`"
+        :title="label"
+        :title-icon="titleIcon"
+        :badges="badges"
+        full-description
+        :description="description"
+        :primary-actions="primaryActions" />
 </template>
