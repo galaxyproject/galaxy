341edee51af1e0d670bed4dd73d1726be5fde84d
Alireza Heidari <itisalirh@gmail.com>
2025-04-22 16:16:55 +0200

    Updates breadcrumb item 'to' type
    Replaces only string routes with typed `RawLocation` objects in breadcrumb item to accept named locations as RouterLink/vue-router expected.
    Updates related components to reflect this change.

--- client/src/components/Common/index.ts
+++ client/src/components/Common/index.ts
@@ -1,8 +1,10 @@
+import type { RawLocation } from "vue-router";
+
 // TODO: Not sure if this is the best place for this type
 export type ColorVariant = "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark";
 
 export interface BreadcrumbItem {
     title: string;
-    to?: string;
+    to?: RawLocation;
     superText?: string;
 }
