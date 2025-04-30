2c923612b1ce2742b0b4b62d8698d1774c77c36b
guerler <aysam.guerler@gmail.com>
2025-04-30 15:56:50 -0400

    Draft role creation request

--- client/src/components/admin/RoleForm.vue
+++ client/src/components/admin/RoleForm.vue
@@ -14,16 +14,16 @@
                     <FormElementLabel title="Groups">
                         <FormSelection
                             id="role-groups"
-                            v-model="groups"
-                            :data="groupsOptions"
+                            v-model="groupIds"
+                            :data="groupData"
                             :multiple="true"
                             :optional="true" />
                     </FormElementLabel>
                     <FormElementLabel title="Users">
                         <FormSelection
                             id="role-users"
-                            v-model="roles"
-                            :data="rolesOptions"
+                            v-model="userIds"
+                            :data="userData"
                             :multiple="true"
                             :optional="true" />
                     </FormElementLabel>
@@ -46,63 +46,64 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { BAlert, BButton } from "bootstrap-vue";
 import { ref } from "vue";
 
-//import { useRouter } from "vue-router/composables";
+import { useRouter } from "vue-router/composables";
 import { GalaxyApi } from "@/api";
 
-import FormBoolean from "../Form/Elements/FormBoolean.vue";
-import FormSelection from "../Form/Elements/FormSelection.vue";
-import FormInput from "@/components/Form/Elements/FormInput.vue";
+import FormBoolean from "@/components/Form/Elements/FormBoolean.vue";
 import FormCard from "@/components/Form/FormCard.vue";
 import FormElementLabel from "@/components/Form/FormElementLabel.vue";
+import FormInput from "@/components/Form/Elements/FormInput.vue";
+import FormSelection from "@/components/Form/Elements/FormSelection.vue";
 import LoadingSpan from "@/components/LoadingSpan.vue";
 
 const autoCreate = ref(false);
 const errorMessage = ref("");
 const description = ref("");
-const groups = ref();
-const groupsOptions = ref();
-const roles = ref();
-const rolesOptions = ref();
+const groupIds = ref();
+const groupData = ref();
 const loading = ref(false);
 const name = ref("");
+const userIds = ref();
+const userData = ref();
 
-//const router = useRouter();
+const router = useRouter();
 
 async function fetchData() {
     loading.value = true;
-    const { data: groupsList, error: groupsError } = await GalaxyApi().GET("/api/groups");
+    const { data: groups, error: groupsError } = await GalaxyApi().GET("/api/groups");
     if (groupsError) {
         errorMessage.value = groupsError.err_msg;
     } else {
-        const { data: rolesList, error: rolesError } = await GalaxyApi().GET("/api/roles");
+        const { data: roles, error: rolesError } = await GalaxyApi().GET("/api/roles");
         if (rolesError) {
             errorMessage.value = rolesError.err_msg;
         } else {
             errorMessage.value = "";
-            groupsOptions.value = groupsList.map((g) => ({ value: g.id, label: g.name }));
-            rolesOptions.value = rolesList.map((r) => ({ value: r.id, label: r.name }));
+            groupData.value = groups.map((g) => ({ value: g.id, label: g.name }));
+            userData.value = roles.map((r) => ({ value: r.id, label: r.name }));
         }
     }
     loading.value = false;
 }
 
 async function onSubmit() {
-    console.log("ok");
-    /*if (false) {
+    if (!name.value || !description.value) {
         errorMessage.value = "Please complete all required inputs.";
-        return;
-    }
-
-    const { data, error } = await GalaxyApi().POST("/api/roles", {
-        body: {
-            name: name.value,
-        },
-    });
-    if (error) {
-        errorMessage.value = error.err_msg;
     } else {
-        router.push("/admin/roles");
-    }*/
+        const { error } = await GalaxyApi().POST("/api/roles", {
+            body: {
+                name: name.value,
+                description: description.value,
+                group_ids: groupIds.value,
+                user_ids: userIds.value,
+            },
+        });
+        if (error) {
+            errorMessage.value = error.err_msg;
+        } else {
+            router.push("/admin/roles");
+        }
+    }
 }
 
 fetchData();
