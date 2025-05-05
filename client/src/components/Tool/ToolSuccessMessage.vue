700cb959d53894e1caed36a1692ed7860ce50665
Ahmed Awan <qe66653@umbc.edu>
2025-05-05 13:30:41 -0500

    show a link to job(s) on job success page
    Is a step towards https://github.com/galaxyproject/galaxy/issues/20014. Currently, showing multiple jobs in this view can be expensive (as mentioned here https://github.com/galaxyproject/galaxy/pull/20145#issuecomment-2851824203).

--- client/src/components/Tool/ToolSuccessMessage.vue
+++ client/src/components/Tool/ToolSuccessMessage.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { computed } from "vue";
+import { RouterLink } from "vue-router";
 
 import type { JobResponse } from "@/api/jobs";
+
+import ExternalLink from "../ExternalLink.vue";
 
 const props = defineProps<{
     jobResponse: JobResponse;
@@ -34,6 +37,18 @@ const nOutputsText = computed(() => (nOutputs.value > 1 ? `${nOutputs.value} out
             You can check the status of queued jobs and view the resulting data by refreshing the History panel. When
             the job has been run the status will change from 'running' to 'finished' if completed successfully or
             'error' if problems were encountered.
+        </p>
+        <p v-if="nJobs > 1">
+            Here is a link to each job:
+            <ExternalLink v-for="job in jobResponse.jobs" :key="job.id" :href="`/jobs/${job.id}/view`">
+                {{ job.id }}
+            </ExternalLink>
+        </p>
+        <p v-else-if="jobResponse.jobs[0]">
+            Here is a link to the job:
+            <RouterLink :to="`/jobs/${jobResponse.jobs[0].id}/view`">
+                {{ jobResponse.jobs[0]?.id }}
+            </RouterLink>
         </p>
     </div>
 </template>
