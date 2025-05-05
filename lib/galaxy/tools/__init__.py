28a7e0a0422b6741e101df5608011127851ec2b7
Polina Polunina <55543056+PlushZ@users.noreply.github.com>
2025-05-05 13:20:05 +0200

    Update lib/galaxy/tools/__init__.py
    Co-authored-by: Arash Kadkhodaei <arash77.kad@gmail.com>

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -2224,7 +2224,7 @@ class Tool(UsesDictVisibleKeys, ToolParameterBundle):
         )
 
         tags = incoming.get("tags", [])
-        if tags:
+        if isinstance(tags, list) and tags:
             tag_handler = self.app.tag_handler
             for _, hda in execution_tracker.output_datasets:
                 tag_handler.apply_item_tags(user=trans.user, item=hda, tags_str=",".join(tags))
