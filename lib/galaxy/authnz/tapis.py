385c18a297846aa2da5da4cca9c5cdbb342aaff1
davelopez <46503462+davelopez@users.noreply.github.com>
2025-05-07 13:07:53 +0200

    Remove unreachable condition
    ACCESS_TOKEN_METHOD seems to be always "POST"

--- lib/galaxy/authnz/tapis.py
+++ lib/galaxy/authnz/tapis.py
@@ -77,9 +77,6 @@ class TapisOAuth2(BaseOAuth2):
         self.process_error(self.data)
         state = self.validate_state()
         data, params = None, None
-        if self.ACCESS_TOKEN_METHOD == "GET":
-            params = self.auth_complete_params(state)
-        else:
             data = self.auth_complete_params(state)
 
         response = self.request_access_token(
