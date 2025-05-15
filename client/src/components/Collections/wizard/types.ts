db309123c60e5606d046b9d27bbabe4c7f4ef338
John Chilton <jmchilton@gmail.com>
2025-05-15 17:04:31 -0400

    Refactor types out of overloaded BuildFileSetWizard component.

--- client/src/components/Collections/wizard/types.ts
+++ client/src/components/Collections/wizard/types.ts
@@ -6,3 +6,22 @@ export type RulesSourceFrom = "remote_files" | "pasted_table" | "dataset_as_tabl
 export type ListUriResponse = components["schemas"]["ListUriResponse"];
 export type RemoteFile = components["schemas"]["RemoteFile"];
 export type RemoteDirectory = components["schemas"]["RemoteDirectory"];
+
+export type RawRowData = string[][];
+export type InitialElements = RawRowData | HDCADetailed;
+
+// types and helpers around initializing the rule builder with data
+export type RuleSelectionType = "raw" | "remote_files";
+export type RuleElementsType = RemoteFile[] | string[][];
+
+// it would be nice to have a real type from the rule builder but
+// it is older code. This is really outlining what this component can
+// produce and not what the rule builder can consume which is a wide
+// superset of this.
+export interface RuleBuilderOptions {
+    dataType: RulesCreatingWhat;
+    ftpUploadSite?: string;
+    elements?: RuleElementsType | undefined;
+    content?: string;
+    selectionType: RuleSelectionType;
+}
