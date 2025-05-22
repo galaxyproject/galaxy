0087aacf028e0d975e66b222bb83523305b5d784
Alireza Heidari <itisalirh@gmail.com>
2025-05-22 15:39:33 +0200

    Refactors ToolSelectPreferredObjectStore and streamlines event handling
    - Simplifies the props definition by removing default values and renaming the interface for clarity.
    - Streamlines event handling by reordering and grouping related declarations for improved readability.

--- client/src/components/Tool/ToolSelectPreferredObjectStore.vue
+++ client/src/components/Tool/ToolSelectPreferredObjectStore.vue
@@ -1,22 +1,20 @@
 <script setup lang="ts">
 import SelectObjectStore from "@/components/ObjectStore/SelectObjectStore.vue";
 
-interface ToolSelectProps {
-    toolPreferredObjectStoreId?: String | null;
+interface Props {
+    toolPreferredObjectStoreId?: string | null;
 }
 
-const props = withDefaults(defineProps<ToolSelectProps>(), {
-    toolPreferredObjectStoreId: null,
-});
+const props = defineProps<Props>();
+
+const emit = defineEmits<{
+    (e: "updated", id: string | null): void;
+}>();
 
 const newDatasetsDescription = "The default Galaxy storage for the outputs of this tool";
 const defaultOptionTitle = "Use Defaults";
 const defaultOptionDescription =
     "If the history has a default set, that will be used. If instead, you've set an option in your user preferences - that will be assumed to be your default selection. Finally, the Galaxy configuration will be used.";
-
-const emit = defineEmits<{
-    (e: "updated", id: string | null): void;
-}>();
 
 async function handleSubmit(preferredObjectStoreId: string | null) {
     emit("updated", preferredObjectStoreId);
