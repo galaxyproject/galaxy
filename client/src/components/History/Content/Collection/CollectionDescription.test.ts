4f0958ca14feb27e474b146a428c8c51983b1ad9
davelopez <46503462+davelopez@users.noreply.github.com>
2025-05-26 16:08:03 +0200

    Refactor CollectionDescription component props to use HDCASummary
    And update tests

---
+++ client/src/components/History/Content/Collection/CollectionDescription.test.ts
@@ -0,0 +1,164 @@
+import { mount, type Wrapper } from "@vue/test-utils";
+import { getLocalVue } from "tests/jest/helpers";
+import type Vue from "vue";
+
+import type { HDCASummary } from "@/api";
+
+import CollectionDescription from "./CollectionDescription.vue";
+
+const localVue = getLocalVue();
+
+const defaultTestHDCA: HDCASummary = {
+    id: "test_id",
+    name: "Test Collection",
+    hid: 1,
+    collection_id: "test_collection_id",
+    type: "collection",
+    collection_type: "list",
+    history_content_type: "dataset_collection",
+    element_count: null,
+    create_time: null,
+    update_time: null,
+    deleted: false,
+    visible: true,
+    elements_datatypes: [],
+    history_id: "fake_history_id",
+    model_class: "HistoryDatasetCollectionAssociation",
+    populated_state: "ok",
+    tags: [],
+    url: "fake/url",
+    contents_url: "fake/contents/url",
+};
+
+describe("CollectionDescription", () => {
+    let wrapper: Wrapper<Vue>;
+
+    beforeEach(() => {
+        wrapper = mount(CollectionDescription as object, {
+            propsData: {
+                hdca: defaultTestHDCA,
+            },
+            localVue,
+        });
+    });
+
+    it("should display expected heterogeneous descriptions", async () => {
+        const HETEROGENEOUS_DATATYPES = ["txt", "csv", "tabular"];
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                collection_type: "list",
+                element_count: 1,
+                elements_datatypes: HETEROGENEOUS_DATATYPES,
+            },
+        });
+        expect(wrapper.text()).toBe("a list with 1 dataset");
+
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 2,
+                collection_type: "paired",
+            },
+        });
+        expect(wrapper.text()).toBe("a pair with 2 datasets");
+
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 10,
+                collection_type: "list",
+            },
+        });
+        expect(wrapper.text()).toBe("a list with 10 datasets");
+
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 10,
+                collection_type: "list:paired",
+            },
+        });
+        expect(wrapper.text()).toBe("a list with 10 pairs");
+
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 10,
+                collection_type: "list:list",
+            },
+        });
+        expect(wrapper.text()).toBe("a list with 10 lists");
+
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 10,
+                collection_type: "other",
+            },
+        });
+        expect(wrapper.text()).toBe("a nested list with 10 dataset collections");
+    });
+
+    it("should display expected homogeneous descriptions", async () => {
+        const EXPECTED_HOMOGENEOUS_DATATYPE = "tabular";
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 1,
+                elements_datatypes: [EXPECTED_HOMOGENEOUS_DATATYPE],
+            },
+        });
+        expect(wrapper.text()).toBe(`a list with 1 ${EXPECTED_HOMOGENEOUS_DATATYPE} dataset`);
+
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 2,
+                collection_type: "paired",
+                elements_datatypes: [EXPECTED_HOMOGENEOUS_DATATYPE],
+            },
+        });
+        expect(wrapper.text()).toBe(`a pair with 2 ${EXPECTED_HOMOGENEOUS_DATATYPE} datasets`);
+
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 10,
+                collection_type: "list",
+                elements_datatypes: [EXPECTED_HOMOGENEOUS_DATATYPE],
+            },
+        });
+        expect(wrapper.text()).toBe(`a list with 10 ${EXPECTED_HOMOGENEOUS_DATATYPE} datasets`);
+
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 10,
+                collection_type: "list:paired",
+                elements_datatypes: [EXPECTED_HOMOGENEOUS_DATATYPE],
+            },
+        });
+        expect(wrapper.text()).toBe(`a list with 10 ${EXPECTED_HOMOGENEOUS_DATATYPE} pairs`);
+
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 10,
+                collection_type: "list:list",
+                elements_datatypes: [EXPECTED_HOMOGENEOUS_DATATYPE],
+            },
+        });
+        expect(wrapper.text()).toBe(`a list with 10 ${EXPECTED_HOMOGENEOUS_DATATYPE} lists`);
+
+        await wrapper.setProps({
+            hdca: {
+                ...defaultTestHDCA,
+                element_count: 10,
+                collection_type: "other",
+                elements_datatypes: [EXPECTED_HOMOGENEOUS_DATATYPE],
+            },
+        });
+        expect(wrapper.text()).toBe(`a nested list with 10 ${EXPECTED_HOMOGENEOUS_DATATYPE} dataset collections`);
+    });
+});
--- client/src/components/History/Content/ContentItem.vue
+++ client/src/components/History/Content/ContentItem.vue
@@ -19,7 +19,6 @@ import { useEntryPointStore } from "@/stores/entryPointStore";
 import { useEventStore } from "@/stores/eventStore";
 import { clearDrag } from "@/utils/setDrag";
 
-import { JobStateSummary } from "./Collection/JobStateSummary";
 import { getContentItemState, type StateMap, STATES } from "./model/states";
 
 import CollectionDescription from "./Collection/CollectionDescription.vue";
@@ -91,12 +90,8 @@ const eventStore = useEventStore();
 const contentItem = ref<HTMLElement | null>(null);
 const subItemsVisible = ref(false);
 
-const jobState = computed(() => {
-    return new JobStateSummary(props.item);
-});
-
 const itemIsRunningInteractiveTool = computed(() => {
-    // If our datset id is in the entrypOintStore it's a running it
+    // If our dataset id is in the entrypOintStore it's a running it
     return !isCollection.value && entryPointStore.entryPointsForHda(props.item.id).length > 0;
 });
 
@@ -445,13 +440,7 @@ function unexpandedClick(event: Event) {
         <ContentExpirationIndicator :item="item" class="ml-auto align-self-start btn-group p-1" />
         <!-- eslint-disable-next-line vuejs-accessibility/click-events-have-key-events, vuejs-accessibility/no-static-element-interactions -->
         <span @click.stop="unexpandedClick">
-            <CollectionDescription
-                v-if="!isDataset"
-                class="px-2 pb-2 cursor-pointer"
-                :job-state-summary="jobState"
-                :collection-type="item.collection_type"
-                :element-count="item.element_count"
-                :elements-datatypes="item.elements_datatypes" />
+            <CollectionDescription v-if="!isDataset" class="px-2 pb-2 cursor-pointer" :hdca="item" />
             <StatelessTags
                 v-if="!tagsDisabled || hasTags"
                 class="px-2 pb-2"
