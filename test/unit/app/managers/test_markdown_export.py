1db09a2dddb944f4820e39cd7924ce26eeae6459
davelopez <46503462+davelopez@users.noreply.github.com>
2025-05-27 19:24:10 +0200

    Patch serialize_object_store_ids in test_export_dataset_collection_paired test
    To handle mock datasets when serializing the collection during export

--- test/unit/app/managers/test_markdown_export.py
+++ test/unit/app/managers/test_markdown_export.py
@@ -379,7 +379,9 @@ history_dataset_collection_display(history_dataset_collection_id=1)
         from galaxy.managers.hdcas import HDCASerializer
 
         with mock.patch.object(HDCASerializer, "url_for", return_value="http://google.com"):
-            export, extra_data = self._ready_export(example)
+            # Patch out serialize_object_store_ids because the datasets aren't real, just mock objects.
+            with mock.patch.object(HDCASerializer, "serialize_object_store_ids", return_value=[]):
+                export, extra_data = self._ready_export(example)
         assert "history_dataset_collections" in extra_data
         assert len(extra_data.get("history_dataset_collections")) == 1
 
