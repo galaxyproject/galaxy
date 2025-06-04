12675aa4774b7927bde3d8cc51f9d48abbc5bcac
Ahmed Awan <qe66653@umbc.edu>
2025-06-04 17:38:57 -0500

    jest fix for changed error based on `COLLECTION_TYPE_TO_LABEL`

--- client/src/components/Form/Elements/FormData/FormData.test.js
+++ client/src/components/Form/Elements/FormData/FormData.test.js
@@ -405,7 +405,7 @@ describe("FormData", () => {
         dispatchEvent(wrapper, "dragenter");
         dispatchEvent(wrapper, "drop");
         expect(wrapper.emitted().alert[0][0]).toEqual(
-            "paired dataset collection is not a valid input for list type dataset collection parameter."
+            "dataset pair dataset collection is not a valid input for list type dataset collection parameter."
         );
     });
 
