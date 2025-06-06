75212d09d0aa6a75b2f99f999df5876629821682
Dannon Baker <dannon.baker@gmail.com>
2025-06-06 12:23:49 -0400

    Swap datatypes info request to fetch client

--- client/src/api/datatypes.ts
+++ client/src/api/datatypes.ts
@@ -1,7 +1,5 @@
-import axios from "axios";
-
+import { GalaxyApi } from "@/api";
 import type { components } from "@/api";
-import { withPrefix } from "@/utils/redirect";
 import { rethrowSimple } from "@/utils/simple-error";
 
 export type CompositeFileInfo = components["schemas"]["CompositeFileInfo"];
@@ -10,10 +8,18 @@ export type DatatypeDetails = components["schemas"]["DatatypeDetails"];
 /**
  * Get details about a specific datatype
  */
-export async function fetchDatatypeDetails(extension: string) {
+export async function fetchDatatypeDetails(extension: string): Promise<DatatypeDetails> {
     try {
-        const { data } = await axios.get(withPrefix(`/api/datatypes/${extension}`));
-        return data;
+        const { GET } = GalaxyApi();
+        const { data } = await GET("/api/datatypes/{datatype}", {
+            params: {
+                path: { datatype: extension },
+            },
+        });
+        if (!data) {
+            throw new Error(`Failed to fetch datatype details for ${extension}`);
+        }
+        return data as DatatypeDetails;
     } catch (error) {
         rethrowSimple(error);
     }
