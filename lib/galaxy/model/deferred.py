5d3fb63c0a3c2efa364d4579a4969c2e30e60785
mvdbeek <m.vandenbeek@gmail.com>
2025-06-12 10:50:09 +0200

    Fix datatype file_ext attribute

--- lib/galaxy/model/deferred.py
+++ lib/galaxy/model/deferred.py
@@ -253,7 +253,7 @@ class DatasetInstanceMaterializer:
             applied_transforms.append(
                 {
                     "action": "datatype_groom",
-                    "datatype_ext": datatype.ext,
+                    "datatype_ext": datatype.file_ext,
                     "datatype_class": datatype.__class__.__name__,
                 }
             )
