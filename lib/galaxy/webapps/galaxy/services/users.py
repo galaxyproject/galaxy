14ab0e22b8ed15163ed73f3337e23a2ba6a31c02
M Bernt <m.bernt@ufz.de>
2025-06-19 18:27:34 +0200

    Raise exception for unauthenicated users
    Co-authored-by: Nicola Soranzo <nicola.soranzo@gmail.com>

--- lib/galaxy/webapps/galaxy/services/users.py
+++ lib/galaxy/webapps/galaxy/services/users.py
@@ -204,7 +204,7 @@ class UsersService(ServiceBase):
     ) -> List[MaybeLimitedUserModel]:
         # never give any info to non-authenticated users
         if not trans.user:
-            return []
+            raise glx_exceptions.AuthenticationRequired("Only registered users can view the list of users")
 
         # check for early return conditions
         if deleted:
