a2f4a5b8648d8280b937a892549d8c56b9dfc329
mvdbeek <m.vandenbeek@gmail.com>
2025-06-22 16:17:57 +0200

    Add dataset collection element picker
    The placement could improvement but something's better than nothing.

--- client/src/components/Markdown/Utilities/requirements.ts
+++ client/src/components/Markdown/Utilities/requirements.ts
@@ -61,5 +61,10 @@ export function hasValidName(name: string | undefined) {
 
 export function hasValidObject(name: string | undefined, args: Record<string, string>): boolean {
     const requiredObject = getRequiredObject(name);
+    if (requiredObject === "history_dataset_id" && args.history_dataset_collection_id) {
+        // accept a collection where a dataset is expected.
+        // we don't control this anyway
+        return true;
+    }
     return !requiredObject || !!args[requiredObject];
 }
