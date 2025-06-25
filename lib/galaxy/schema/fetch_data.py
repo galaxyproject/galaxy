ea524876662e2da697f1ecbdfe0c9f953233a340
John Chilton <jmchilton@gmail.com>
2025-06-25 15:45:31 -0400

    Tool Landing API...

--- lib/galaxy/schema/fetch_data.py
+++ lib/galaxy/schema/fetch_data.py
@@ -14,6 +14,7 @@ from pydantic import (
     Field,
     field_validator,
     Json,
+    TypeAdapter,
 )
 from typing_extensions import (
     Annotated,
@@ -200,7 +201,9 @@ CompositeDataElement.model_rebuild()
 
 
 class NestedElement(BaseDataElement):
-    elements: List[Union["AnyElement", "NestedElement"]] = Field(..., validation_alias=AliasChoices("elements", "items"))
+    elements: List[Union["AnyElement", "NestedElement"]] = Field(
+        ..., validation_alias=AliasChoices("elements", "items")
+    )
 
 
 AnyElement = Annotated[
@@ -282,6 +285,9 @@ Targets = List[
 ]
 
 
+TargetsAdapter = TypeAdapter(Targets)
+
+
 class FetchDataPayload(BaseDataPayload):
     targets: Targets
 
@@ -296,10 +302,8 @@ class DataLandingRequestState(Model):
 
 # Vaguely matches the schema.schema.ToolLandingState but we don't allow data_fetch to be called directly
 # via the tool API so we have a more specific model here.
-class DataLandingPayload(Model):
-    tool_id: str
-    tool_version: Optional[str] = None
-    request_state: DataLandingRequestState = None
+class CreateDataLandingPayload(Model):
+    request_state: DataLandingRequestState
     client_secret: Optional[str] = None
     public: bool = False
 
--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -80,6 +80,10 @@ from galaxy.tool_util.ontologies.ontology_data import (
     expand_ontology_data,
 )
 from galaxy.tool_util.output_checker import DETECTED_JOB_STATE
+from galaxy.tool_util.parameters import (
+    input_models_for_pages,
+    ToolParameterBundle,
+)
 from galaxy.tool_util.parser import (
     get_tool_source,
     RequiredFiles,
@@ -1001,7 +1005,7 @@ class JobContext(BaseJobContext):
         return self.job.implicit_collection_jobs_association and self.job.implicit_collection_jobs_association.id
 
 
-class Tool(UsesDictVisibleKeys):
+class Tool(UsesDictVisibleKeys, ToolParameterBundle):
     """
     Represents a computational tool that can be executed through Galaxy.
     """
@@ -1693,6 +1697,11 @@ class Tool(UsesDictVisibleKeys):
         self.inputs: ToolInputsT = {}
         pages = tool_source.parse_input_pages()
         enctypes: Set[str] = set()
+        try:
+            parameters = input_models_for_pages(pages, self.profile)
+            self.parameters = parameters
+        except Exception:
+            pass
         if pages.inputs_defined:
             if hasattr(pages, "input_elem"):
                 input_elem = pages.input_elem
