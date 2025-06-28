29e8fc403ad58bfd3361d9626cedbd62974daffc
tcollins <tyler@nhlp.com>
2025-06-28 14:57:40 -0400

    fixed schema duplication and added vue readme area

--- client/src/components/Libraries/LibraryFolder/LibraryFolder.vue
+++ client/src/components/Libraries/LibraryFolder/LibraryFolder.vue
@@ -257,6 +257,14 @@
                 </BCol>
             </BRow>
         </BContainer>
+        <section v-if="folder_metadata.readme_rendered" class="library-readme-section">
+            <hr/>
+            <b-card
+                v-html="sanitize(folder_metadata.readme_rendered)"
+                class="readme-content"
+                v-if="folder_metadata.readme_rendered">
+            </b-card>
+        </section>
     </div>
 </template>
 
