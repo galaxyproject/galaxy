0daa17bc80f19afa12ef76f9f541ac0e2ebbc594
davelopez <46503462+davelopez@users.noreply.github.com>
2025-06-30 17:25:13 +0200

    Add Recent Downloads page and integrate with routing

---
+++ client/src/components/Downloads/DownloadItemCard.vue
@@ -0,0 +1,247 @@
+<script setup lang="ts">
+import { faDownload, faHourglassEnd, faInfoCircle, faSpinner, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { BAlert } from "bootstrap-vue";
+import { formatDistanceToNow } from "date-fns";
+import { computed } from "vue";
+
+import type { CardBadge } from "@/components/Common/GCard.types";
+import type { MonitoringData, MonitoringRequest } from "@/composables/persistentProgressMonitor";
+import { usePersistentProgressTaskMonitor } from "@/composables/persistentProgressMonitor";
+import { useShortTermStorage } from "@/composables/shortTermStorage";
+import { useShortTermStorageMonitor } from "@/composables/shortTermStorageMonitor";
+import { useTaskMonitor } from "@/composables/taskMonitor";
+import { capitalizeFirstLetter } from "@/utils/strings";
+
+import GCard from "@/components/Common/GCard.vue";
+
+const { getDownloadObjectUrl } = useShortTermStorage();
+
+interface Props {
+    monitoringData: MonitoringData;
+    /** Whether to display the card in a grid view */
+    gridView?: boolean;
+}
+
+const props = defineProps<Props>();
+
+const emit = defineEmits<{
+    (e: "onGoTo", to: string): void;
+    (e: "onDownload", url: string): void;
+    (e: "onDelete", request: MonitoringRequest): void;
+}>();
+
+const title = computed(() => {
+    return `${prettyAction.value} ${prettyObjectType.value} - ${
+        props.monitoringData.request.object.name || "No name provided"
+    }`;
+});
+
+const prettyObjectType = computed(() => {
+    const type = props.monitoringData.request.object.type;
+    switch (type) {
+        case "history":
+            return "History";
+        case "invocation":
+            return "Workflow Invocation";
+        case "collection":
+            return "Dataset Collection";
+        default:
+            return "Unknown Object Type";
+    }
+});
+
+const prettyAction = computed(() => capitalizeFirstLetter(props.monitoringData.request.action));
+
+const useMonitor = props.monitoringData.request.taskType === "task" ? useTaskMonitor() : useShortTermStorageMonitor();
+
+const {
+    isRunning,
+    isCompleted,
+    hasFailed,
+    failureReason,
+    requestHasFailed,
+    storedTaskId,
+    hasExpired,
+    expirationDate,
+    start,
+    reset,
+} = usePersistentProgressTaskMonitor(props.monitoringData.request, useMonitor);
+
+const downloadUrl = computed(() => {
+    // We can only download the result if the task type is short_term_storage.
+    if (storedTaskId && props.monitoringData.request.taskType === "short_term_storage") {
+        return getDownloadObjectUrl(storedTaskId);
+    }
+    return undefined;
+});
+
+const canDownload = computed(() => {
+    return (
+        isCompleted.value &&
+        !hasExpired.value &&
+        downloadUrl.value &&
+        !requestHasFailed.value &&
+        props.monitoringData.request.taskType === "short_term_storage"
+    );
+});
+
+const primaryActions = computed(() => {
+    const actions = [
+        {
+            id: "go-to-object",
+            label: `Go to ${prettyObjectType.value}`,
+            icon: faInfoCircle,
+            title: `View details for ${prettyObjectType.value}`,
+            variant: "outline-primary",
+            handler: onGoToObject,
+            visible: true,
+        },
+    ];
+
+    if (canDownload.value) {
+        actions.push({
+            id: "download",
+            label: "Download",
+            icon: faDownload,
+            title: "Download the result",
+            variant: "primary",
+            handler: onDownload,
+            visible: true,
+        });
+    }
+
+    if (hasFailed.value || hasExpired.value) {
+        actions.push({
+            id: "remove",
+            label: "Remove",
+            icon: faTrash,
+            title: "Remove this record from the list",
+            variant: "outline-danger",
+            handler: onRemove,
+            visible: true,
+        });
+    }
+
+    return actions;
+});
+
+const elapsedTimeToExpire = computed(() =>
+    expirationDate.value ? formatDistanceToNow(expirationDate.value, { addSuffix: true }) : undefined
+);
+
+const badges = computed<CardBadge[]>(() => {
+    const badges: CardBadge[] = [];
+    if (isRunning.value) {
+        badges.push({
+            id: "in-progress",
+            title: `${prettyObjectType.value} is being prepared for download`,
+            label: "In Progress",
+            variant: "info",
+            visible: true,
+        });
+    }
+    if (canDownload.value) {
+        badges.push({
+            id: "ready-to-download",
+            title: `Download ready for ${prettyObjectType.value}`,
+            label: "Ready",
+            variant: "success",
+            visible: true,
+        });
+    }
+    if (hasFailed.value) {
+        badges.push({
+            id: "failed-preparation",
+            label: "Failed",
+            title: `Failed to prepare ${prettyObjectType.value} for download`,
+            variant: "danger",
+            visible: true,
+        });
+    }
+    if (hasExpired.value) {
+        badges.push({
+            id: "download-request-expired",
+            label: "Expired",
+            title: `Download request for ${prettyObjectType.value} has expired`,
+            variant: "warning",
+            visible: true,
+        });
+    } else if (expirationDate.value && elapsedTimeToExpire.value) {
+        badges.push({
+            id: "expiration-date",
+            label: `Expires ${elapsedTimeToExpire.value}`,
+            title: `This download will expire on ${expirationDate.value.toLocaleString()}`,
+            variant: "secondary",
+            icon: faHourglassEnd,
+            visible: true,
+        });
+    }
+    return badges;
+});
+
+function onGoToObject() {
+    const objectType = props.monitoringData.request.object.type;
+    const objectId = props.monitoringData.request.object.id;
+    switch (objectType) {
+        case "history":
+            emit("onGoTo", `/histories/view?id=${objectId}`);
+            break;
+        case "invocation":
+            emit("onGoTo", `/workflows/invocations/${objectId}`);
+            break;
+        // case "collection": TODO use async route for collections
+        default:
+            console.warn(`No specific route defined for object type: ${objectType}`);
+            break;
+    }
+}
+
+function onDownload() {
+    if (downloadUrl.value) {
+        emit("onDownload", downloadUrl.value);
+    } else {
+        console.error("Download URL is not available.");
+    }
+}
+
+function onRemove() {
+    emit("onDelete", props.monitoringData.request);
+    reset();
+}
+
+start();
+</script>
+<template>
+    <GCard
+        :id="monitoringData.taskId"
+        class="download-item-card"
+        :title="title"
+        :badges="badges"
+        :primary-actions="primaryActions"
+        :update-time="monitoringData.startedAt.toLocaleString()"
+        :grid-view="gridView"
+        :title-icon="{
+            icon: faDownload,
+            title: `${prettyAction} ${prettyObjectType}`,
+        }">
+        <template v-slot:description>
+            <p v-if="monitoringData.request.description">
+                {{ monitoringData.request.description }}
+            </p>
+            <BAlert v-if="isRunning" variant="info" show>
+                <FontAwesomeIcon :icon="faSpinner" spin />
+                <span>Preparing {{ prettyObjectType }} for download...</span>
+            </BAlert>
+            <BAlert v-if="hasFailed" variant="danger" show>
+                The download request for {{ prettyObjectType }} has failed. <strong>Failure Reason:</strong>
+                {{ failureReason }}
+            </BAlert>
+            <BAlert v-if="hasExpired" variant="warning" show>
+                The download request has expired and the result is no longer available. You can go to the
+                <strong>{{ prettyObjectType }}</strong> {{ monitoringData.request.action }} page and try again. This
+                download record can now be removed from the list of recent downloads.
+            </BAlert>
+        </template>
+    </GCard>
+</template>
---
+++ client/src/components/Downloads/RecentDownloads.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRouter } from "vue-router/composables";
+
+import { useDownloadTracker } from "@/composables/downloadTracker";
+import type { MonitoringRequest } from "@/composables/persistentProgressMonitor";
+import { useUserStore } from "@/stores/userStore";
+
+import Heading from "@/components/Common/Heading.vue";
+import ListHeader from "@/components/Common/ListHeader.vue";
+import DownloadItemCard from "@/components/Downloads/DownloadItemCard.vue";
+
+const router = useRouter();
+
+const userStore = useUserStore();
+const { downloadMonitoringData, untrackDownloadRequest } = useDownloadTracker();
+
+const isEmpty = computed(() => {
+    return downloadMonitoringData.value.length === 0;
+});
+
+const currentListView = computed(() => userStore.currentListViewPreferences.recentDownloads || "grid");
+
+function onGoTo(to: string): void {
+    router.push(to);
+}
+
+function onDelete(request: MonitoringRequest): void {
+    untrackDownloadRequest(request);
+}
+
+function onDownload(url: string): void {
+    window.open(url, "_blank");
+}
+</script>
+<template>
+    <div class="recent-downloads">
+        <Heading h1 separator inline size="lg">Recent Exports & Downloads</Heading>
+        <ListHeader v-if="!isEmpty" list-id="recentDownloads" show-view-toggle />
+
+        <p v-if="isEmpty">
+            No recent exports or downloads found. When you start a long-running export or download, it will appear here.
+        </p>
+        <div v-else class="d-flex flex-wrap">
+            <DownloadItemCard
+                v-for="download in downloadMonitoringData"
+                :key="download.taskId"
+                :monitoring-data="download"
+                :grid-view="currentListView == 'grid'"
+                @onGoTo="onGoTo"
+                @onDelete="onDelete"
+                @onDownload="onDownload" />
+        </div>
+    </div>
+</template>
