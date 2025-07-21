40e91ee66e489721418956f26ddf7bfab69d31cd
John Chilton <jmchilton@gmail.com>
2025-07-21 10:42:43 -0400

    Implement data landing UI.

--- client/src/api/tools.ts
+++ client/src/api/tools.ts
@@ -2,9 +2,44 @@ import { type components, GalaxyApi } from "@/api";
 import { ERROR_STATES, type ShowFullJobResponse } from "@/api/jobs";
 
 export type HdcaUploadTarget = components["schemas"]["HdcaDataItemsTarget"];
+export type HdasUploadTarget = components["schemas"]["DataElementsTarget"];
 export type FetchDataPayload = components["schemas"]["FetchDataPayload"];
 export type UrlDataElement = components["schemas"]["UrlDataElement"];
 export type NestedElement = components["schemas"]["NestedElement"];
+export type NestedElementItems = NestedElement["elements"];
+export type NestedElementItem = NestedElementItems[number];
+export type FetchTargets = FetchDataPayload["targets"];
+export type AnyFetchTarget = FetchTargets[number];
+
+export function urlDataElement(identifier: string, uri: string): UrlDataElement {
+    const element: UrlDataElement = {
+        src: "url",
+        url: uri,
+        name: identifier,
+        // these shouldn't be required but the way our model -> ts stuff works it is...
+        auto_decompress: false,
+        dbkey: "?",
+        ext: "auto",
+        to_posix_lines: false,
+        deferred: false,
+        space_to_tab: false,
+    };
+    return element;
+}
+
+export function nestedElement(identifier: string, elements: NestedElementItems) {
+    const nestedElement: NestedElement = {
+        name: identifier,
+        elements: elements,
+        auto_decompress: false,
+        dbkey: "?",
+        ext: "auto",
+        to_posix_lines: false,
+        deferred: false,
+        space_to_tab: false,
+    };
+    return nestedElement;
+}
 
 export async function fetch(payload: FetchDataPayload) {
     const { data } = await GalaxyApi().POST("/api/tools/fetch", {
