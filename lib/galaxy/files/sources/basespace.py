f0e354827e6673e9d73f7b43268309eb6bf78707
davelopez <46503462+davelopez@users.noreply.github.com>
2025-07-31 18:52:03 +0200

    Refactor BaseSpace file source
    To adapt it to the new framework updates

--- lib/galaxy/files/sources/basespace.py
+++ lib/galaxy/files/sources/basespace.py
@@ -3,28 +3,41 @@ try:
 except ImportError:
     BASESPACEFS = None
 
-from typing import (
-    Optional,
-    Union,
-)
+from typing import Optional
 
-from . import (
-    FilesSourceOptions,
-    FilesSourceProperties,
-)
+from . import FilesSourceProperties
 from ._pyfilesystem2 import PyFilesystem2FilesSource
+
+
+class BaseSpaceFileSourceConfiguration(FilesSourceProperties):
+    dir_path: Optional[str] = "/"
+    client_id: Optional[str] = None
+    client_secret: Optional[str] = None
+    access_token: Optional[str] = None
+    basespace_server: Optional[str] = None
 
 
 class BaseSpaceFilesSource(PyFilesystem2FilesSource):
     plugin_type = "basespace"
     required_module = BASESPACEFS
     required_package = "fs-basespace"
+    config_class: BaseSpaceFileSourceConfiguration
+    config: BaseSpaceFileSourceConfiguration
 
-    def _open_fs(self, user_context=None, opts: Optional[FilesSourceOptions] = None):
-        props = self._serialization_props(user_context)
-        extra_props: Union[FilesSourceProperties, dict] = opts.extra_props or {} if opts else {}
-        handle = BASESPACEFS(**{**props, **extra_props})
-        return handle
+    def __init__(self, config: BaseSpaceFileSourceConfiguration):
+        super().__init__(config)
+
+    def _open_fs(self):
+        if BASESPACEFS is None:
+            raise self.required_package_exception
+
+        return BASESPACEFS(
+            dir_path=self.config.dir_path,
+            client_id=self.config.client_id,
+            client_secret=self.config.client_secret,
+            access_token=self.config.access_token,
+            basespace_server=self.config.basespace_server,
+        )
 
 
 __all__ = ("BaseSpaceFilesSource",)
