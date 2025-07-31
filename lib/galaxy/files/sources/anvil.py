3e2d862b5c16b24b5f957f108f6d21e10476340c
davelopez <46503462+davelopez@users.noreply.github.com>
2025-07-31 18:51:16 +0200

    Refactor AnVILFilesSource
    To adapt it to the new framework updates

--- lib/galaxy/files/sources/anvil.py
+++ lib/galaxy/files/sources/anvil.py
@@ -3,26 +3,43 @@ try:
 except ImportError:
     AnVILFS = None
 from typing import (
+    ClassVar,
     Optional,
-    Union,
 )
 
-from . import (
-    FilesSourceOptions,
-    FilesSourceProperties,
-)
+from . import FilesSourceProperties
 from ._pyfilesystem2 import PyFilesystem2FilesSource
+
+
+class AnVILFileSourceConfiguration(FilesSourceProperties):
+    namespace: str
+    workspace: str
+    api_url: Optional[str] = None
+    on_anvil: Optional[bool] = False
+    drs_url: Optional[str] = None
 
 
 class AnVILFilesSource(PyFilesystem2FilesSource):
     plugin_type = "anvil"
     required_module = AnVILFS
     required_package = "fs.anvilfs"
+    config_class: ClassVar[type[AnVILFileSourceConfiguration]] = AnVILFileSourceConfiguration
+    config: AnVILFileSourceConfiguration
 
-    def _open_fs(self, user_context=None, opts: Optional[FilesSourceOptions] = None):
-        props = self._serialization_props(user_context)
-        extra_props: Union[FilesSourceProperties, dict] = opts.extra_props or {} if opts else {}
-        handle = AnVILFS(**{**props, **extra_props})
+    def __init__(self, config: AnVILFileSourceConfiguration):
+        super().__init__(config)
+
+    def _open_fs(self):
+        if AnVILFS is None:
+            raise self.required_package_exception
+
+        handle = AnVILFS(
+            namespace=self.config.namespace,
+            workspace=self.config.workspace,
+            api_url=self.config.api_url,
+            on_anvil=self.config.on_anvil,
+            drs_url=self.config.drs_url,
+        )
         return handle
 
 
