a21b63c79aeac24c13a0008b9028815fabddb1c6
davelopez <46503462+davelopez@users.noreply.github.com>
2025-08-01 09:26:02 +0200

    Refactor S3 file source
    To adapt it to the new framework updates

--- lib/galaxy/files/sources/s3fs.py
+++ lib/galaxy/files/sources/s3fs.py
@@ -1,15 +1,7 @@
 import functools
 import logging
 import os
-from typing import (
-    cast,
-    Optional,
-)
-
-from typing_extensions import (
-    NotRequired,
-    Unpack,
-)
+from typing import Optional
 
 from galaxy import exceptions
 from galaxy.files import OptionalUserContext
@@ -17,6 +9,8 @@ from . import (
     AnyRemoteEntry,
     FilesSourceOptions,
     FilesSourceProperties,
+    RemoteDirectory,
+    RemoteFile,
 )
 
 try:
@@ -28,44 +22,44 @@ from . import BaseFilesSource
 
 DEFAULT_ENFORCE_SYMLINK_SECURITY = True
 DEFAULT_DELETE_ON_REALIZE = False
+FS_PLUGIN_TYPE = "s3fs"
 
 log = logging.getLogger(__name__)
 
 
-class S3FsFilesSourceProperties(FilesSourceProperties, total=False):
-    bucket: str
-    endpoint_url: int
-    user: str
-    passwd: str
-    listings_expiry_time: NotRequired[Optional[int]]
-    client_kwargs: dict  # internally computed. Should not be specified in config file
+class S3FSFileSourceConfiguration(FilesSourceProperties):
+    endpoint_url: str
+    anon: bool = False
+    bucket: Optional[str] = None
+    secret: Optional[str] = None
+    key: Optional[str] = None
 
 
 class S3FsFilesSource(BaseFilesSource):
-    plugin_type = "s3fs"
+    plugin_type = FS_PLUGIN_TYPE
+    config_class = S3FSFileSourceConfiguration
+    config: S3FSFileSourceConfiguration
 
-    def __init__(self, **kwd: Unpack[S3FsFilesSourceProperties]):
+    def __init__(self, config: S3FSFileSourceConfiguration):
+        super().__init__(config)
+
+    def _open_fs(self):
         if s3fs is None:
-            raise Exception("Package s3fs unavailable but required for this file source plugin.")
-        props: S3FsFilesSourceProperties = cast(S3FsFilesSourceProperties, self._parse_common_config_opts(kwd))
-        file_sources_config = self._file_sources_config
-        if (
-            props.get("listings_expiry_time") is None
-            and file_sources_config
-            and file_sources_config.listings_expiry_time
-        ):
-            if file_sources_config.listings_expiry_time:
-                props["listings_expiry_time"] = file_sources_config.listings_expiry_time
-        # There is a possibility that the bucket name could be parameterized: e.g.
-        # bucket: ${user.preferences['generic_s3|bucket']}
-        # that's ok, because we evaluate the bucket name again later. The bucket property here will only
-        # be used by `score_url_match`. In the case of a parameterized bucket name, we will always return
-        # a score of 4 as the url will only match the s3:// part.
-        self._bucket = props.get("bucket", "")
-        self._endpoint_url = props.pop("endpoint_url", None)
-        self._props = props
-        if self._endpoint_url:
-            self._props.update({"client_kwargs": {"endpoint_url": self._endpoint_url}})
+            raise Exception("Missing s3fs package, please install it to use S3 file sources.")
+        client_kwargs = {"endpoint_url": self.config.endpoint_url} if self.config.endpoint_url else None
+        fs = s3fs.S3FileSystem(
+            anon=self.config.anon,
+            key=self.config.key,
+            secret=self.config.secret,
+            client_kwargs=client_kwargs,
+            listings_expiry_time=self.listings_expiry_time,
+        )
+        return fs
+
+    @property
+    def listings_expiry_time(self) -> Optional[int]:
+        """Return the listings expiry time for this file source."""
+        return self.config.file_sources_config.listings_expiry_time
 
     def _list(
         self,
@@ -78,10 +72,9 @@ class S3FsFilesSource(BaseFilesSource):
         query: Optional[str] = None,
         sort_by: Optional[str] = None,
     ) -> tuple[list[AnyRemoteEntry], int]:
-        _props = self._serialization_props(user_context)
-        # we need to pop the 'bucket' here, because the argument is not recognised in a downstream function
-        _bucket_name = _props.pop("bucket", "")
-        fs = self._open_fs(props=_props, opts=opts)
+        self.update_config_from_options(opts, user_context)
+        _bucket_name = self.config.bucket or ""
+        fs = self._open_fs()
         if recursive:
             res: list[AnyRemoteEntry] = []
             bucket_path = self._bucket_path(_bucket_name, path)
@@ -106,9 +99,9 @@ class S3FsFilesSource(BaseFilesSource):
         user_context: OptionalUserContext = None,
         opts: Optional[FilesSourceOptions] = None,
     ):
-        _props = self._serialization_props(user_context)
-        _bucket_name = _props.pop("bucket", "")
-        fs = self._open_fs(props=_props, opts=opts)
+        self.update_config_from_options(opts, user_context)
+        _bucket_name = self.config.bucket or ""
+        fs = self._open_fs()
         bucket_path = self._bucket_path(_bucket_name, source_path)
         fs.download(bucket_path, native_path)
 
@@ -119,9 +112,9 @@ class S3FsFilesSource(BaseFilesSource):
         user_context: OptionalUserContext = None,
         opts: Optional[FilesSourceOptions] = None,
     ):
-        _props = self._serialization_props(user_context)
-        _bucket_name = _props.pop("bucket", "")
-        fs = self._open_fs(props=_props, opts=opts)
+        self.update_config_from_options(opts, user_context)
+        _bucket_name = self.config.bucket or ""
+        fs = self._open_fs()
         bucket_path = self._bucket_path(_bucket_name, target_path)
         fs.upload(native_path, bucket_path)
 
@@ -132,39 +125,28 @@ class S3FsFilesSource(BaseFilesSource):
             path = f"/{path}"
         return f"{bucket_name}{path}"
 
-    def _open_fs(self, props: FilesSourceProperties, opts: Optional[FilesSourceOptions] = None):
-        extra_props = opts.extra_props or {} if opts else {}
-        fs = s3fs.S3FileSystem(**{**props, **extra_props})
-        return fs
-
     def _resource_info_to_dict(self, dir_path: str, resource_info) -> AnyRemoteEntry:
-        name = os.path.basename(resource_info["name"])
+        name = str(os.path.basename(resource_info["name"]))
         path = os.path.join(dir_path, name)
         uri = self.uri_from_path(path)
         if resource_info["type"] == "directory":
-            return {"class": "Directory", "name": name, "uri": uri, "path": path}
+            return RemoteDirectory(name=name, uri=uri, path=path)
         else:
-            return {
-                "class": "File",
-                "name": name,
-                "size": resource_info["size"],
-                # should this be mtime...
-                "ctime": self.to_dict_time(resource_info["LastModified"]),
-                "uri": uri,
-                "path": path,
-            }
-
-    def _serialization_props(self, user_context: OptionalUserContext = None) -> S3FsFilesSourceProperties:
-        effective_props = {}
-        for key, val in self._props.items():
-            effective_props[key] = self._evaluate_prop(val, user_context=user_context)
-        return cast(S3FsFilesSourceProperties, effective_props)
+            return RemoteFile(
+                name=name,
+                size=resource_info["size"],
+                ctime=self.to_dict_time(resource_info["LastModified"]),
+                uri=uri,
+                path=path,
+            )
 
     def score_url_match(self, url: str):
         # For security, we need to ensure that a partial match doesn't work. e.g. s3://{bucket}something/myfiles
-        if self._bucket and (url.startswith(f"s3://{self._bucket}/") or url == f"s3://{self._bucket}"):
-            return len(f"s3://{self._bucket}")
-        elif not self._bucket and url.startswith("s3://"):
+        if self.config.bucket and (
+            url.startswith(f"s3://{self.config.bucket}/") or url == f"s3://{self.config.bucket}"
+        ):
+            return len(f"s3://{self.config.bucket}")
+        elif not self.config.bucket and url.startswith("s3://"):
             return len("s3://")
         else:
             return super().score_url_match(url)
