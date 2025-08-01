c296fb94a30aa54f46a13c897c72c345698256a5
davelopez <46503462+davelopez@users.noreply.github.com>
2025-08-01 08:33:14 +0200

    Refactor Galaxy stock file sources
    To adapt them to the new framework updates

--- lib/galaxy/files/sources/galaxy.py
+++ lib/galaxy/files/sources/galaxy.py
@@ -1,15 +1,11 @@
 """Static Galaxy file sources - ftp and libraries."""
 
-from typing import (
-    Optional,
-)
-
-from typing_extensions import Unpack
+from typing import Optional
 
 from galaxy.files.sources import PluginKind
 from .posix import (
+    PosixFileSourceConfiguration,
     PosixFilesSource,
-    PosixFilesSourceProperties,
 )
 
 
@@ -17,19 +13,18 @@ class UserFtpFilesSource(PosixFilesSource):
     plugin_type = "gxftp"
     plugin_kind = PluginKind.stock
 
-    def __init__(self, **kwd: Unpack[PosixFilesSourceProperties]):
-        posix_kwds: PosixFilesSourceProperties = dict(
+    def __init__(self, config: PosixFileSourceConfiguration):
+        overrides = dict(
             id="_ftp",
             root="${user.ftp_dir}",
             label="FTP Directory",
             doc="Galaxy User's FTP Directory",
             writable=True,
         )
-        posix_kwds.update(kwd)
-        if "delete_on_realize" not in posix_kwds:
-            file_sources_config = kwd["file_sources_config"]
-            posix_kwds["delete_on_realize"] = file_sources_config.ftp_upload_purge
-        super().__init__(**posix_kwds)
+        self.config = self.config.model_copy(update=overrides)
+        if not self.config.delete_on_realize:
+            self.config.delete_on_realize = self.config.file_sources_config.ftp_upload_purge
+        super().__init__(config)
 
     def get_prefix(self) -> Optional[str]:
         return None
@@ -42,15 +37,15 @@ class LibraryImportFilesSource(PosixFilesSource):
     plugin_type = "gximport"
     plugin_kind = PluginKind.stock
 
-    def __init__(self, **kwd: Unpack[PosixFilesSourceProperties]):
-        posix_kwds: PosixFilesSourceProperties = dict(
+    def __init__(self, config: PosixFileSourceConfiguration):
+        overrides = dict(
             id="_import",
             root="${config.library_import_dir}",
             label="Library Import Directory",
             doc="Galaxy's library import directory",
         )
-        posix_kwds.update(kwd)
-        super().__init__(**posix_kwds)
+        self.config = self.config.model_copy(update=overrides)
+        super().__init__(config)
 
     def get_prefix(self) -> Optional[str]:
         return None
@@ -63,15 +58,15 @@ class UserLibraryImportFilesSource(PosixFilesSource):
     plugin_type = "gxuserimport"
     plugin_kind = PluginKind.stock
 
-    def __init__(self, **kwd: Unpack[PosixFilesSourceProperties]):
-        posix_kwds: PosixFilesSourceProperties = dict(
+    def __init__(self, config: PosixFileSourceConfiguration):
+        overrides = dict(
             id="_userimport",
             root="${config.user_library_import_dir}/${user.email}",
             label="Library User Import Directory",
             doc="Galaxy's user library import directory",
         )
-        posix_kwds.update(kwd)
-        super().__init__(**posix_kwds)
+        self.config = self.config.model_copy(update=overrides)
+        super().__init__(config)
 
     def get_prefix(self) -> Optional[str]:
         return None
