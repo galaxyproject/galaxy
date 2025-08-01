5c4ae8fb60d3fe328a2366d5b37eb84b8ac62826
davelopez <46503462+davelopez@users.noreply.github.com>
2025-08-01 08:44:26 +0200

    Refactor SSH file source
    To adapt it to the new framework updates

--- lib/galaxy/files/sources/ssh.py
+++ lib/galaxy/files/sources/ssh.py
@@ -1,32 +1,52 @@
 try:
-    from fs.sshfs import SSHFS
+    from fs.sshfs.sshfs import SSHFS
 except ImportError:
-    SSHFS = None
+    SSHFS = None  # type: ignore[misc,assignment]
 
-from typing import (
-    Optional,
-    Union,
-)
+from typing import Optional
 
-from . import (
-    FilesSourceOptions,
-    FilesSourceProperties,
-)
+from . import FilesSourceProperties
 from ._pyfilesystem2 import PyFilesystem2FilesSource
+
+
+class SshFileSourceConfiguration(FilesSourceProperties):
+    host: str
+    user: Optional[str] = None
+    passwd: Optional[str] = None
+    pkey: Optional[str] = None
+    timeout: int = 10
+    port: int = 22
+    compress: bool = False
+    config_path: str = "~/.ssh/config"
+    path: str
 
 
 class SshFilesSource(PyFilesystem2FilesSource):
     plugin_type = "ssh"
     required_module = SSHFS
     required_package = "fs.sshfs"
+    configuration_class = SshFileSourceConfiguration
+    config: SshFileSourceConfiguration
 
-    def _open_fs(self, user_context=None, opts: Optional[FilesSourceOptions] = None):
-        props = self._serialization_props(user_context)
-        extra_props: Union[FilesSourceProperties, dict] = opts.extra_props or {} if opts else {}
-        path = props.pop("path")
-        handle = SSHFS(**{**props, **extra_props})
-        if path:
-            handle = handle.opendir(path)
+    def __init__(self, config: SshFileSourceConfiguration):
+        super().__init__(config)
+
+    def _open_fs(self):
+        if SSHFS is None:
+            raise self.required_package_exception
+
+        handle = SSHFS(
+            host=self.config.host,
+            user=self.config.user,
+            passwd=self.config.passwd,
+            pkey=self.config.pkey,
+            port=self.config.port,
+            timeout=self.config.timeout,
+            compress=self.config.compress,
+            config_path=self.config.config_path,
+        )
+        if self.config.path:
+            return handle.opendir(self.config.path)
         return handle
 
 
