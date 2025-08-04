590fb842bab0d73ec07b7d853c1bca281917fcfe
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2025-08-04 19:13:55 +0200

    display inputs in review modal

--- client/src/components/Workflow/Editor/Tools/SelectionToWorkflowReviewModal.vue
+++ client/src/components/Workflow/Editor/Tools/SelectionToWorkflowReviewModal.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 import type {
     InputReconnectionMap,
     OutputReconnectionMap,
 } from "@/components/Workflow/Editor/modules/convertOpenConnections";
 import type { PartialWorkflow } from "@/components/Workflow/Editor/modules/extractSubworkflow";
+import { onAllInputs } from "@/components/Workflow/Editor/modules/onAllInputs";
+import type { NewStep } from "@/stores/workflowStepStore";
 
 import GFormInput from "@/components/BaseComponents/Form/GFormInput.vue";
 import GFormLabel from "@/components/BaseComponents/Form/GFormLabel.vue";
+import GLink from "@/components/BaseComponents/GLink.vue";
 import GModal from "@/components/BaseComponents/GModal.vue";
 import Heading from "@/components/Common/Heading.vue";
 
@@ -23,9 +26,81 @@ const props = defineProps<{
 
 const emit = defineEmits<{
     (e: "rename", name: string): void;
-    (e: "renameInput", id: string, name: string): void;
-    (e: "renameOutput", id: string, name: string): void;
+    (e: "renameInput", id: number, to: string): void;
+    (e: "renameOutput", id: number, name: string, to: string): void;
 }>();
+
+const connections = computed(() => {
+    const connectionArray: Array<{
+        from: number;
+        to: number;
+        inputName: string;
+    }> = [];
+    Object.values(props.workflow.steps).forEach((step) =>
+        onAllInputs(step, (connection, name) =>
+            connectionArray.push({ from: connection.id, to: step.id, inputName: name })
+        )
+    );
+
+    return connectionArray;
+});
+
+const inputs = computed(() => {
+    const steps = Object.values(props.workflow.steps);
+    const inputSteps = steps.filter((step) =>
+        (["parameter_input", "data_input", "data_collection_input"] as Array<NewStep["type"]>).includes(step.type)
+    );
+
+    const inputLabels = new Set(Object.keys(props.inputMap));
+
+    const inputStepSummaries = inputSteps.map((step) => {
+        return {
+            step,
+            label: step.label ?? step.name,
+            connectedTo: connections.value.filter((connection) => connection.from === step.id),
+            generated: step.label && inputLabels.has(step.label),
+        };
+    });
+
+    return inputStepSummaries;
+});
+
+const allInputLabels = computed(() => inputs.value.map((input) => input.label));
+
+function inputLabelValid(label: string) {
+    return label.trim() !== "" && allInputLabels.value.indexOf(label) === allInputLabels.value.lastIndexOf(label);
+}
+
+const outputs = computed(() => {
+    const steps = Object.values(props.workflow.steps);
+
+    const outputLabels = new Set(props.outputMap.map((output) => output.name));
+
+    const outputSummaries = steps.flatMap((step) => {
+        if (!step.workflow_outputs || step.workflow_outputs.length === 0) {
+            return [];
+        }
+
+        const outputs = step.workflow_outputs.map((output) => {
+            return {
+                step,
+                label: output.label ?? output.output_name,
+                name: output.output_name,
+                generated: output.label && outputLabels.has(output.label),
+            };
+        });
+
+        return outputs;
+    });
+
+    return outputSummaries;
+});
+
+const allOutputLabels = computed(() => outputs.value.map((output) => output.label));
+
+function _outputLabelValid(label: string) {
+    return label.trim() !== "" && allOutputLabels.value.indexOf(label) === allOutputLabels.value.lastIndexOf(label);
+}
 
 const modal = ref<InstanceType<typeof GModal>>();
 
@@ -54,17 +129,45 @@ defineExpose({
         </GFormLabel>
 
         <div class="workflow-info-grid">
-            <div class="inputs">
+            <div class="inputs list">
                 <Heading h3 size="sm" class="mb-0"> Inputs </Heading>
 
-                <span> Converted from connections </span>
+                <div v-for="(input, index) in inputs" :key="index" class="box">
+                    <div class="box-heading">
+                        <span> Input Step {{ input.step.id + 1 }} </span>
+                        <GLink
+                            v-if="input.generated"
+                            class="generated-notice"
+                            tooltip
+                            title="converted from input connections to selection">
+                            generated
+                        </GLink>
+                    </div>
 
-                <div v-for="(inputLink, label) in props.inputMap" :key="`${inputLink.id}-${inputLink.output_name}`">
-                    {{ label }}
+                    <GFormLabel
+                        :state="inputLabelValid(input.label) ? null : false"
+                        invalid-feedback="provide a unique input label">
+                        <GFormInput
+                            :value="input.label"
+                            @input="(value) => emit('renameInput', input.step.id, value ?? '')" />
+                    </GFormLabel>
+
+                    <div>
+                        Connected To:
+                        <ul class="connection-list">
+                            <li v-for="connection in input.connectedTo" :key="connection.to">
+                                {{ connection.to + 1 }}:
+                                {{
+                                    props.workflow.steps[connection.to]?.label ??
+                                    props.workflow.steps[connection.to]?.name
+                                }}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
 
-            <div class="outputs">
+            <div class="outputs list">
                 <Heading h3 size="sm" class="mb-0"> Outputs </Heading>
 
                 <span> Converted from connections </span>
@@ -76,7 +179,7 @@ defineExpose({
                 </div>
             </div>
 
-            <div class="steps">
+            <div class="steps list">
                 <Heading h3 size="sm" class="mb-0"> Steps </Heading>
 
                 <div v-for="(step, key) in props.workflow.steps" :key="key">
@@ -106,6 +209,35 @@ defineExpose({
 
     .steps {
         grid-area: s;
+    }
+}
+
+.list {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-2);
+}
+
+.box {
+    border: 1px solid var(--color-grey-400);
+    border-radius: var(--spacing-2);
+    padding: var(--spacing-2);
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-1);
+
+    &:has(.generated-notice) {
+        border: 1px solid var(--color-green-500);
+    }
+
+    .box-heading {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .connection-list {
+        margin-bottom: 0;
+        padding-left: var(--spacing-4);
     }
 }
 </style>
