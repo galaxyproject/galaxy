b50dccf923e46368419a1fa28ebde333568674b2
davelopez <46503462+davelopez@users.noreply.github.com>
2025-08-04 15:35:41 +0200

    Set default value for config_class in BaseFilesSource

--- lib/galaxy/files/sources/__init__.py
+++ lib/galaxy/files/sources/__init__.py
@@ -488,7 +488,7 @@ class BaseFilesSource(FilesSource):
     supports_pagination: ClassVar[bool] = False
     supports_search: ClassVar[bool] = False
     supports_sorting: ClassVar[bool] = False
-    config_class: ClassVar[Type[FilesSourceProperties]]
+    config_class: ClassVar[Type[FilesSourceProperties]] = FilesSourceProperties
 
     def __init__(self, config: FilesSourceProperties):
         self._parse_common_props(config)
