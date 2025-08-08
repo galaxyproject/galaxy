ee91cd04ad845c09f1265a8d4b008a5996c5c441
mvdbeek <m.vandenbeek@gmail.com>
2025-08-08 11:41:53 +0200

    Use NamedTupe for collection state summary

--- lib/galaxy/tools/parameters/dataset_matcher.py
+++ lib/galaxy/tools/parameters/dataset_matcher.py
@@ -213,8 +213,10 @@ class SummaryDatasetCollectionMatcher:
         if not dataset_collection.populated_optimized:
             return False
 
-        (states, extensions, *_) = dataset_collection.dataset_states_and_extensions_summary
-        for state in states:
+        summary = dataset_collection.dataset_states_and_extensions_summary
+        states = summary.states
+        extensions = summary.extensions
+        for state in states.keys():
             if state not in self.dataset_matcher_factory.valid_input_states:
                 return False
 
