506e672655ac2c6907d42619a11efacf25ec83cc
John Chilton <jmchilton@gmail.com>
2025-08-08 12:55:10 -0400

    Improve comment.

--- client/src/components/admin/Notifications/NotificationForm.test.ts
+++ client/src/components/admin/Notifications/NotificationForm.test.ts
@@ -11,8 +11,8 @@ import { useServerMock } from "@/api/client/__mocks__";
 
 import NotificationForm from "./NotificationForm.vue";
 
-// Even though we don't use the API endpoints, this seems to prevent failure fetching
-// openapi during jest testing.
+// Calls roles, groups, and users APIs so we need to use a server mock
+// to prevent attempts at making real API calls.
 useServerMock();
 
 const SUBMIT_BUTTON_SELECTOR = "#notification-submit";
