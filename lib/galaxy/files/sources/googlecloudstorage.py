193f4f06de53487b5de02212f6bddaeac94603bd
davelopez <46503462+davelopez@users.noreply.github.com>
2025-08-08 14:14:11 +0200

    Add more optional fields in GCSFS file source configuration

--- lib/galaxy/files/sources/googlecloudstorage.py
+++ lib/galaxy/files/sources/googlecloudstorage.py
@@ -20,18 +20,26 @@ from ._pyfilesystem2 import PyFilesystem2FilesSource
 
 class GoogleCloudStorageFileSourceTemplateConfiguration(BaseFileSourceTemplateConfiguration):
     bucket_name: Union[str, TemplateExpansion]
-    root_path: Union[str, TemplateExpansion]
-    project: Optional[Union[str, TemplateExpansion]] = None
-    anonymous: Optional[Union[bool, TemplateExpansion]] = True
-    token: Optional[Union[str, TemplateExpansion]] = None
+    root_path: Union[str, TemplateExpansion, None] = None
+    project: Union[str, TemplateExpansion, None] = None
+    anonymous: Union[bool, TemplateExpansion, None] = True
+    token: Union[str, TemplateExpansion, None] = None
+    token_uri: Union[str, TemplateExpansion, None] = None
+    client_id: Union[str, TemplateExpansion, None] = None
+    client_secret: Union[str, TemplateExpansion, None] = None
+    refresh_token: Union[str, TemplateExpansion, None] = None
 
 
 class GoogleCloudStorageFileSourceConfiguration(BaseFileSourceConfiguration):
     bucket_name: str
-    root_path: str
+    root_path: Optional[str] = None
     project: Optional[str] = None
     anonymous: Optional[bool] = True
     token: Optional[str] = None
+    token_uri: Optional[str] = None
+    client_id: Optional[str] = None
+    client_secret: Optional[str] = None
+    refresh_token: Optional[str] = None
 
 
 class GoogleCloudStorageFilesSource(
@@ -53,7 +61,16 @@ class GoogleCloudStorageFilesSource(
         if self.config.anonymous:
             client = Client.create_anonymous_client()
         elif self.config.token:
-            client = Client(project=self.config.project, credentials=Credentials(token=self.config.token))
+            client = Client(
+                project=self.config.project,
+                credentials=Credentials(
+                    token=self.config.token,
+                    token_uri=self.config.token_uri,
+                    client_id=self.config.client_id,
+                    client_secret=self.config.client_secret,
+                    refresh_token=self.config.refresh_token,
+                ),
+            )
 
         handle = GCSFS(bucket_name=self.config.bucket_name, root_path=self.config.root_path, retry=0, client=client)
         return handle
