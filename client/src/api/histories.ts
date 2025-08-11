4e6c09fd6071acadccee6e0399715390305f4d24
Alireza Heidari <itisalirh@gmail.com>
2025-08-11 20:57:13 +0200

    Adds type for updating history payload
    Export new type to represent the payload for updating history objects.

--- client/src/api/histories.ts
+++ client/src/api/histories.ts
@@ -4,6 +4,8 @@ type HistoryDetailed = components["schemas"]["HistoryDetailed"];
 
 export type HistoryContentsResult = components["schemas"]["HistoryContentsResult"];
 
+export type UpdateHistoryPayload = components["schemas"]["UpdateHistoryPayload"];
+
 export function hasImportable(entry?: AnyHistory): entry is HistoryDetailed {
     return entry !== undefined && "importable" in entry;
 }
