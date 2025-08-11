84abd0a022e8c309922d14d110dece09b789041c
Dannon Baker <dannon.baker@gmail.com>
2025-08-11 15:38:26 -0400

    Fix jQuery plugin loading

--- client/src/libs/jquery.custom.js
+++ client/src/libs/jquery.custom.js
@@ -5,25 +5,29 @@
 
 var jQuery = require("jqueryVendor");
 
-require("imports-loader?jQuery=jqueryVendor!./jquery/jquery.autocomplete");
-require("imports-loader?jQuery=jqueryVendor!./jquery/jquery.event.hover");
-require("imports-loader?jQuery=jqueryVendor!./jquery/jquery.event.drag");
-require("imports-loader?jQuery=jqueryVendor!./jquery/jquery.event.drop");
+// Make jQuery available globally for plugins
+window.jQuery = jQuery;
+window.$ = jQuery;
+
+require("./jquery/jquery.autocomplete");
+require("./jquery/jquery.event.hover");
+require("./jquery/jquery.event.drag");
+require("./jquery/jquery.event.drop");
 // TODO: replace mousewheel events
 // require("imports-loader?imports=default|jquery|jqueryVendor,define=>false!jquery-mousewheel");
-require("imports-loader?jQuery=jqueryVendor!./jquery/jquery.form");
-require("imports-loader?jQuery=jqueryVendor!./jquery/jquery.rating");
-require("imports-loader?jQuery=jqueryVendor!./jquery/select2");
-require("imports-loader?jQuery=jqueryVendor!./jquery/jquery-ui");
-require("imports-loader?jQuery=jqueryVendor!./farbtastic");
+require("./jquery/jquery.form");
+require("./jquery/jquery.rating");
+require("./jquery/select2");
+require("./jquery/jquery-ui");
+require("./farbtastic");
 // TODO: ensure unused
 //require("imports-loader?imports=default|jquery|jqueryVendor,define=>false!jquery.cookie");
-require("imports-loader?jQuery=jqueryVendor!./jquery/jquery.dynatree");
-require("imports-loader?jQuery=jqueryVendor!jquery-migrate");
+require("./jquery/jquery.dynatree");
+require("jquery-migrate");
 
 // require("imports-loader?jQuery=jqueryVendor!../ui/autocom_tagging");
 
 // Only used in reports
-require("imports-loader?jQuery=jqueryVendor!./jquery.sparklines");
+require("./jquery.sparklines");
 
 module.exports = jQuery;
