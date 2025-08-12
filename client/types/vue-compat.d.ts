27864924bf5088953cd5cacedf5d9e0693df2701
Dannon Baker <dannon.baker@gmail.com>
2025-08-11 22:37:08 -0400

    Configure Vue 3 compat mode and fix types

---
+++ client/types/vue-compat.d.ts
@@ -0,0 +1,11 @@
+declare module "@vue/compat" {
+    export * from "@vue/runtime-dom";
+    export { default } from "@vue/runtime-dom";
+    
+    export function configureCompat(config: {
+        MODE?: 2 | 3;
+        [key: string]: any;
+    }): void;
+    
+    export function createApp(...args: any[]): any;
+}
