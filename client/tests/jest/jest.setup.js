63382a5c79fea44c9a267454fd1c453219f55a22
Dannon Baker <dannon.baker@gmail.com>
2025-08-12 18:42:35 -0400

    Update jest.setup.js for Vue 3

--- client/tests/jest/jest.setup.js
+++ client/tests/jest/jest.setup.js
@@ -2,26 +2,22 @@ import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/jest-globals";
 import "fake-indexeddb/auto";
 
+import { createApp } from "@vue/compat";
 import failOnConsole from "jest-fail-on-console";
-import Vue from "vue";
 
 // not available in jsdom, mock it out
 Element.prototype.scrollIntoView = jest.fn();
 
-// Set Vue to suppress production / devtools / etc. warnings
-Vue.config.productionTip = false;
-Vue.config.devtools = false;
+// Vue 3 with compat mode setup for tests
+// In Vue 3, app-level configurations are set on app instances
+// For Jest tests, we'll configure Vue 3 compat mode globally
 
-// if we do jest.mock(module) where modules imports Vue, it will create a clean Vue context
-// that ignores the above tips. So pre-emptively mock out Vue in these contexts with the following
-// block of code. GridList.test.js is a good example of this, when ran (yarn test GridList.test.js)
-// it will bypass the attempt to set the Vue config values above and print the messages anyway without
-// this block.
+// Mock Vue for compatibility with existing tests
 jest.mock("vue", () => {
-    const Vue = jest.requireActual("vue"); // Import the actual Vue instance
-    Vue.config.productionTip = false; // Disable production tip
-    Vue.config.devtools = false; // Disable vue devtools
-    return Vue; // Return the modified Vue instance
+    const Vue = jest.requireActual("@vue/compat");
+    // In Vue 3, there's no global config.productionTip
+    // The compat build handles most Vue 2 patterns automatically
+    return Vue;
 });
 
 /* still don't understand what was invoking the following, but nothing should,
