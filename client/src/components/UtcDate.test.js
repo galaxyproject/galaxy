fdef788ffb1d5d721ceee51d87b69f5fa3eded28
Dannon Baker <dannon.baker@gmail.com>
2025-08-12 22:05:02 -0400

    Fix UtcDate test for Vue 3

--- client/src/components/UtcDate.test.js
+++ client/src/components/UtcDate.test.js
@@ -5,12 +5,12 @@ import { getLocalVue } from "tests/jest/helpers";
 import UtcDate from "./UtcDate.vue";
 
 describe("UTCDate component", () => {
-    const localVue = getLocalVue();
+    const globalConfig = getLocalVue();
 
     it("Loads a date in default mode, can format outputs as expected.", async () => {
         const wrapper = shallowMount(UtcDate, {
-            propsData: { date: "2015-10-21T16:29:00.000000" },
-            localVue,
+            props: { date: "2015-10-21T16:29:00.000000" },
+            global: globalConfig.global,
         });
         expect(wrapper.text()).toBe("2015-10-21T16:29:00.000Z");
 
