83a5ce97c846d44e913f4e7037e7e340498bdf3b
Dannon Baker <dannon.baker@gmail.com>
2025-08-18 10:43:22 -0400

    Incremental swap of expanded/selected items to setup to simplify slot handling

--- client/src/components/History/Content/ExpandedItems.js
+++ client/src/components/History/Content/ExpandedItems.js
@@ -1,64 +1,63 @@
 import { loadSet, saveSet } from "utils/setCache";
+import { ref, computed, watch, onBeforeMount } from "vue";
 
 export default {
     props: {
         scopeKey: { type: String, required: true },
         getItemKey: { type: Function, required: true },
     },
-    data() {
-        return {
-            items: new Set(),
-        };
-    },
-    computed: {
-        expandedCount() {
-            return this.items.size;
-        },
-    },
-    methods: {
-        isExpanded(item) {
-            const key = this.getItemKey(item);
-            return this.items.has(key);
-        },
-        setExpanded(item, val) {
-            const key = this.getItemKey(item);
-            const newSet = new Set(this.items);
-            val ? newSet.add(key) : newSet.delete(key);
-            this.items = newSet;
-        },
-        reset() {
-            this.items = new Set();
-        },
-    },
-    watch: {
-        scopeKey(newKey, oldKey) {
-            if (newKey !== oldKey) {
-                this.reset();
+    setup(props, { slots }) {
+        const items = ref(new Set());
+        const key = "expanded-history-items";
+
+        const expandedCount = computed(() => items.value.size);
+
+        function isExpanded(item) {
+            const itemKey = props.getItemKey(item);
+            return items.value.has(itemKey);
+        }
+
+        function setExpanded(item, val) {
+            const itemKey = props.getItemKey(item);
+            const newSet = new Set(items.value);
+            val ? newSet.add(itemKey) : newSet.delete(itemKey);
+            items.value = newSet;
+        }
+
+        function reset() {
+            items.value = new Set();
+        }
+
+        watch(
+            () => props.scopeKey,
+            (newKey, oldKey) => {
+                if (newKey !== oldKey) {
+                    reset();
+                }
             }
-        },
-        items(newSet) {
-            saveSet(this.key, newSet);
-        },
-    },
-    created() {
-        this.key = "expanded-history-items";
-        const cachedSet = loadSet(this.key);
-        if (cachedSet) {
-            this.items = cachedSet;
-        }
-    },
-    render() {
-        // In Vue 3, this.$slots.default is always a function
-        if (!this.$slots.default) {
-            return null;
-        }
-        
-        // Call the slot function with the scoped slot props
-        return this.$slots.default({
-            isExpanded: this.isExpanded,
-            setExpanded: this.setExpanded,
-            collapseAll: this.reset,
-            expandedCount: this.expandedCount,
+        );
+
+        watch(items, (newSet) => {
+            saveSet(key, newSet);
         });
+
+        onBeforeMount(() => {
+            const cachedSet = loadSet(key);
+            if (cachedSet) {
+                items.value = cachedSet;
+            }
+        });
+
+        return () => {
+            if (!slots.default) {
+                return null;
+            }
+            return slots.default({
+                isExpanded: isExpanded,
+                setExpanded: setExpanded,
+                collapseAll: reset,
+                expandedCount: expandedCount.value,
+            });
+        };
     },
 };
