3e66e10717cf79ef4f2d8bc550d249622494e453
Dannon Baker <dannon.baker@gmail.com>
2025-08-18 14:15:39 -0400

    Fix vue-router-4 route access via router

--- client/src/entry/analysis/modules/Login.vue
+++ client/src/entry/analysis/modules/Login.vue
@@ -12,7 +12,7 @@ const router = useRouter();
 const { config, isConfigLoaded } = useConfig();
 
 const hasToken = computed(() => {
-    return router.currentRoute.query.token || router.currentRoute.query.expired_user;
+    return router.currentRoute.value?.query?.token || router.currentRoute.value?.query?.expired_user;
 });
 const sessionCsrfToken = computed(() => {
     return getGalaxyInstance().session_csrf_token;
@@ -33,17 +33,19 @@ const queryAttributeForceString = function (
         <ChangePassword
             v-if="hasToken"
             id="change-password"
-            :expired-user="queryAttributeForceString(router.currentRoute.query.expired_user)"
-            :message-text="queryAttributeForceString(router.currentRoute.query.message)"
-            :message-variant="queryAttributeForceString(router.currentRoute.query.status)"
-            :token="queryAttributeForceString(router.currentRoute.query.token)" />
+            :expired-user="queryAttributeForceString(router.currentRoute.value?.query?.expired_user)"
+            :message-text="queryAttributeForceString(router.currentRoute.value?.query?.message)"
+            :message-variant="queryAttributeForceString(router.currentRoute.value?.query?.status)"
+            :token="queryAttributeForceString(router.currentRoute.value?.query?.token)" />
         <LoginIndex
             v-else-if="isConfigLoaded"
             id="login-index"
             :allow-user-creation="config.allow_local_account_creation"
             :disable-local-accounts="config.disable_local_accounts"
             :enable-oidc="config.enable_oidc"
-            :redirect="queryAttributeForceString(router.currentRoute.query.redirect)"
+            :mailing-join-addr="config.mailing_join_addr"
+            :prefer-custos-login="config.prefer_custos_login"
+            :redirect="queryAttributeForceString(router.currentRoute.value?.query?.redirect)"
             :registration-warning-message="config.registration_warning_message"
             :session-csrf-token="sessionCsrfToken"
             :show-reset-link="config.enable_account_interface"
