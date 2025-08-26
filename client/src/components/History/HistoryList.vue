250723d68baab21db37a4be1b49062d4c835cccc
Dannon Baker <dannon.baker@gmail.com>
2025-08-26 11:57:16 -0400

    Fix vue-router and types post-rebase

--- client/src/components/History/HistoryList.vue
+++ client/src/components/History/HistoryList.vue
@@ -23,7 +23,7 @@ import { faBurn, faColumns, faPlus, faTags, faTrash, faTrashRestore } from "@for
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { BAlert, BButton, BNav, BNavItem, BOverlay, BPagination } from "bootstrap-vue";
 import { computed, onMounted, ref, watch } from "vue";
-import { useRouter } from "vue-router/composables";
+import { useRouter } from "vue-router";
 
 import {
     type AnyHistoryEntry,
