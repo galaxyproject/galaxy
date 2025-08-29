35e1310cacf0804fd2049556795104fbd230ca52
Alireza Heidari <itisalirh@gmail.com>
2025-08-29 20:53:53 +0200

    Refactors credentials management to support group selection
    Improves credentials group handling for workflow tools by introducing per-service current group selection and updating UI to clarify
    group management vs. selection.
    Enables saving group selection explicitly, enhancing user control and future extensibility.

--- client/src/components/Common/WorkflowCredentialsManagement.vue
+++ client/src/components/Common/WorkflowCredentialsManagement.vue
@@ -2,11 +2,15 @@
 import { faWrench } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { BModal } from "bootstrap-vue";
-import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
+import { computed } from "vue";
 
-import type { ServiceCredentialPayload, ServiceGroupPayload } from "@/api/users";
-import { type ToolIdentifier, useUserMultiToolCredentials } from "@/composables/userMultiToolCredentials";
+import type { ToolIdentifier } from "@/api/tools";
+import { getToolKey } from "@/api/tools";
+import type { SelectCurrentGroupPayload, ServiceCredentialsIdentifier } from "@/api/users";
+import { useUserMultiToolCredentials } from "@/composables/userMultiToolCredentials";
 import { useToolStore } from "@/stores/toolStore";
+import { useUserToolsServicesStore } from "@/stores/userToolsServicesStore";
 
 import Heading from "@/components/Common/Heading.vue";
 import ServiceCredentials from "@/components/User/Credentials/ServiceCredentials.vue";
@@ -16,72 +20,98 @@ interface Props {
 }
 
 const props = defineProps<Props>();
-const { getToolNameById } = useToolStore();
-
-const { mutableUserCredentials, mutableUserCredentialsFor, checkAllUserCredentials, saveUserCredentials } =
-    useUserMultiToolCredentials(props.toolIdentifiers);
 
 const emit = defineEmits<{
     (e: "close"): void;
 }>();
 
-function onSelectCredentials() {
-    if (!mutableUserCredentials.value) {
-        return;
-    }
+const { getToolNameById } = useToolStore();
 
-    for (const ti of props.toolIdentifiers) {
-        const credentials = mutableUserCredentialsFor.value(ti.toolId, ti.toolVersion);
-        if (credentials) {
-            saveUserCredentials(credentials);
-        }
-    }
-}
+const userToolsServicesStore = useUserToolsServicesStore();
+const { userToolsServicesCurrentGroupIds } = storeToRefs(userToolsServicesStore);
 
-function onNewCredentialsSet(credential: ServiceCredentialPayload, newSet: ServiceGroupPayload) {
-    const credentialFound = mutableUserCredentials.value.credentials.find(
-        (c) => c.name === credential.name && c.version === credential.version
-    );
+const { userServiceForTool, sourceCredentialsDefinitionFor, selectCurrentCredentialsGroupsForTool } =
+    useUserMultiToolCredentials(props.toolIdentifiers);
 
-    if (credentialFound) {
-        credentialFound.groups.push(newSet);
-    }
-}
+const okTitle = "Save Group Selection";
 
-function onCurrentSetChange(credential: ServiceCredentialPayload, newSet?: ServiceGroupPayload) {
-    if (!newSet) {
-        credential.current_group = null;
-    } else {
-        const credentialFound = mutableUserCredentials.value.credentials.find(
-            (c) => c.name === credential.name && c.version === credential.version
-        );
-        if (credentialFound) {
-            credentialFound.current_group = newSet.name;
-        }
-    }
-}
-
-onMounted(async () => {
-    await checkAllUserCredentials();
+const userToolServiceIdFor = computed(() => {
+    return (toolId: string, toolVersion: string, sd: ServiceCredentialsIdentifier): string | undefined => {
+        const userToolService = userServiceForTool.value(toolId, toolVersion, sd);
+        return userToolService?.id;
+    };
 });
+
+function onToolServiceCurrentGroupChange(
+    toolId: string,
+    toolVersion: string,
+    serviceDefinition: ServiceCredentialsIdentifier,
+    groupId?: string
+) {
+    const toolKey = getToolKey(toolId, toolVersion);
+    const userToolServiceId = userToolServiceIdFor.value(toolId, toolVersion, serviceDefinition);
+    const userToolServiceCurrentGroupIds = userToolsServicesCurrentGroupIds.value[toolKey];
+    if (userToolServiceId && userToolServiceCurrentGroupIds) {
+        userToolServiceCurrentGroupIds[userToolServiceId] = groupId;
+    }
+}
+
+function serviceCurrentGroupFor(
+    toolId: string,
+    toolVersion: string,
+    serviceDefinition: ServiceCredentialsIdentifier
+): string | undefined {
+    const toolKey = getToolKey(toolId, toolVersion);
+    const userToolServiceId = userToolServiceIdFor.value(toolId, toolVersion, serviceDefinition);
+    const userToolServiceCurrentGroupIds = userToolsServicesCurrentGroupIds.value[toolKey];
+    if (userToolServiceId && userToolServiceCurrentGroupIds) {
+        return userToolServiceCurrentGroupIds[userToolServiceId];
+    }
+    return undefined;
+}
+
+function onSelectCredentials() {
+    for (const ti of props.toolIdentifiers) {
+        const toolKey = getToolKey(ti.toolId, ti.toolVersion);
+        const userToolServiceCurrentGroupIds = userToolsServicesCurrentGroupIds.value[toolKey];
+        if (userToolServiceCurrentGroupIds) {
+            const serviceCredentials: SelectCurrentGroupPayload[] = [];
+            for (const userToolServiceId of Object.keys(userToolServiceCurrentGroupIds)) {
+                const newUserToolServiceGroupId = userToolServiceCurrentGroupIds[userToolServiceId];
+                const sc: SelectCurrentGroupPayload = {
+                    user_credentials_id: userToolServiceId,
+                    current_group_id: newUserToolServiceGroupId || null,
+                };
+                serviceCredentials.push(sc);
+            }
+            selectCurrentCredentialsGroupsForTool(ti.toolId, ti.toolVersion, serviceCredentials);
+        }
+    }
+}
 </script>
 
 <template>
     <BModal
         visible
         scrollable
-        title="Select Credentials for this workflow"
         no-close-on-backdrop
         no-close-on-esc
         button-size="md"
         size="lg"
         modal-class="manage-workflow-credentials-modal"
         body-class="manage-workflow-credentials-body"
-        ok-title="Select Credentials"
+        title="Manage & Select Credentials Groups for This Workflow"
+        :ok-title="okTitle"
         cancel-title="Close"
         cancel-variant="outline-danger"
         @ok="onSelectCredentials"
         @close="emit('close')">
+        <p class="mb-0">
+            You can manage your credentials groups for each tool used in this workflow below. Any changes to credential
+            groups will persist, but changes to the current group selection for services will only be saved when you
+            click "{{ okTitle }}".
+        </p>
+
         <div v-for="(ti, i) in props.toolIdentifiers" :key="i" class="mb-2">
             <Heading inline h6 size="sm" class="mb-2" separator>
                 <FontAwesomeIcon :icon="faWrench" fixed-width />
@@ -90,14 +120,17 @@ onMounted(async () => {
 
             <div class="px-2">
                 <ServiceCredentials
-                    v-for="credential in mutableUserCredentialsFor(ti.toolId, ti.toolVersion).credentials"
-                    :key="credential.name + credential.version"
+                    v-for="sd in sourceCredentialsDefinitionFor(ti.toolId, ti.toolVersion).services.values()"
+                    :id="`service-credentials-${sd.name}-${sd.version}`"
+                    :key="sd.name + sd.version"
                     class="mb-2"
-                    :tool-id="ti.toolId"
-                    :tool-version="ti.toolVersion"
-                    :credential-payload="credential"
-                    @new-credentials-set="(newSet) => onNewCredentialsSet(credential, newSet)"
-                    @update-current-set="(updatedSet) => onCurrentSetChange(credential, updatedSet)">
+                    :source-id="ti.toolId"
+                    :source-version="ti.toolVersion"
+                    :service-definition="sd"
+                    :service-current-group-id="serviceCurrentGroupFor(ti.toolId, ti.toolVersion, sd)"
+                    @update-current-group="
+                        (groupId) => onToolServiceCurrentGroupChange(ti.toolId, ti.toolVersion, sd, groupId)
+                    ">
                 </ServiceCredentials>
             </div>
         </div>
