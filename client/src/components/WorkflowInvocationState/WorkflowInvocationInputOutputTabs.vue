d3dd1332e4d9a85934e2cfe48e9e3c0e5c8d9573
Ahmed Awan <qe66653@umbc.edu>
2025-09-06 12:47:15 -0500

    [25.0] Make invocation input/output tabs lazy
    Addresses https://github.com/galaxyproject/galaxy/issues/20864

--- client/src/components/WorkflowInvocationState/WorkflowInvocationInputOutputTabs.vue
+++ client/src/components/WorkflowInvocationState/WorkflowInvocationInputOutputTabs.vue
@@ -64,13 +64,13 @@ const parameters = computed(() => Object.values(props.invocation.input_step_para
 </script>
 <template>
     <span>
-        <BTab title="Inputs">
+        <BTab title="Inputs" lazy>
             <div v-if="parameters.length || inputData.length">
                 <WorkflowInvocationInputs :invocation="props.invocation" />
             </div>
             <BAlert v-else show variant="info"> No input data was provided for this workflow invocation. </BAlert>
         </BTab>
-        <BTab title="Outputs">
+        <BTab title="Outputs" lazy>
             <div v-if="outputs.length">
                 <div
                     v-for="([key, output], index) in outputs"
