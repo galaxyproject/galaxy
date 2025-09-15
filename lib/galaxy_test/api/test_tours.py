355cce56f6d631ba63f8846db359784fb3982469
Ahmed Awan <qe66653@umbc.edu>
2025-09-15 11:59:05 -0500

    change tool generated tour API test's tool from `cat1` to random lines
    I did this because `cat1` does not have supported input types for TGTs.

--- lib/galaxy_test/api/test_tours.py
+++ lib/galaxy_test/api/test_tours.py
@@ -29,13 +29,13 @@ class TestToursApi(ApiTestCase):
         self._assert_status_code_is(response, 404)
         assert 'Tool "nonexistent_tool_id" version "1.0" does not exist.' in response.json()["err_msg"]
 
-    @skip_without_tool("cat1")
-    def test_generate_tour_for_cat1(self):
-        response = self._get("tours/generate?tool_id=cat1&tool_version=1.0.0&performs_upload=false")
+    @skip_without_tool("random_lines1")
+    def test_generate_tour_for_random_lines1(self):
+        response = self._get("tours/generate?tool_id=random_lines1&tool_version=1.0.0&performs_upload=false")
         self._assert_status_code_is(response, 200)
         tour = response.json()["tour"]
         self._assert_tour(tour)
-        tool_response = self._get("tools/cat1")
+        tool_response = self._get("tools/random_lines1")
         tool_name = tool_response.json()["name"]
         assert tour["name"] == f"{tool_name} Tour"
 
