ecdbcb2b384b9422cc93ab6e54aea0f3515a029a
Alireza Heidari <itisalirh@gmail.com>
2025-09-17 17:59:06 +0200

    Displays service credential groups in workflow alerts
    Enhances workflow credentials alert to show which credential groups are selected for each service.
    
    Adds visual badges that display the tool name, service name, and selected credential group for each service in the workflow. Required services show with primary styling while optional services use secondary styling.
    
    Improves user awareness of credential configuration status by clearly indicating which services have assigned groups and which still need configuration.

--- client/src/components/Common/WorkflowCredentials.vue
+++ client/src/components/Common/WorkflowCredentials.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { faCheck, faExclamation, faKey } from "@fortawesome/free-solid-svg-icons";
+import { faCaretRight, faCheck, faExclamation, faKey, faWrench } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon, FontAwesomeLayers } from "@fortawesome/vue-fontawesome";
-import { BAlert, BButton } from "bootstrap-vue";
+import { BAlert, BBadge, BButton } from "bootstrap-vue";
 import { storeToRefs } from "pinia";
 import { computed, onMounted, ref } from "vue";
 
@@ -21,10 +21,12 @@ const props = defineProps<Props>();
 
 const { isAnonymous } = storeToRefs(useUserStore());
 
-const { isBusy, busyMessage } = storeToRefs(useUserToolsServiceCredentialsStore());
+const { isBusy, busyMessage, userToolServiceCredentialsGroups } = storeToRefs(useUserToolsServiceCredentialsStore());
 
 const {
     statusVariant,
+    userServiceForTool,
+    sourceCredentialsDefinitionFor,
     someToolsHasRequiredServiceCredentials,
     hasUserProvidedAllToolsServiceCredentials,
     hasUserProvidedAllRequiredToolsServiceCredentials,
@@ -38,6 +40,43 @@ const provideCredentialsButtonTitle = computed(() => {
     return hasUserProvidedAllToolsServiceCredentials.value ? "Manage credentials" : "Provide credentials";
 });
 
+const currentWorkflowServiceGroups = computed(() => {
+    const allServiceGroups: {
+        toolName: string;
+        serviceName: string;
+        isRequired: boolean;
+        groupName?: string;
+    }[] = [];
+
+    for (const toolIdentifier of props.toolIdentifiers) {
+        const { toolId, toolVersion } = toolIdentifier;
+        const sourceCredentialsDefinition = sourceCredentialsDefinitionFor.value(toolId, toolVersion);
+
+        for (const serviceDefinition of sourceCredentialsDefinition.services.values()) {
+            const userService = userServiceForTool.value(toolId, toolVersion, serviceDefinition);
+
+            let selectedGroup;
+            if (userService?.current_group_id) {
+                const group = userToolServiceCredentialsGroups.value[userService.current_group_id];
+                selectedGroup = group?.name;
+            }
+
+            allServiceGroups.push({
+                toolName: `${toolId} (${toolVersion})`,
+                serviceName: serviceDefinition.label || serviceDefinition.name,
+                isRequired: !serviceDefinition.optional,
+                groupName: selectedGroup,
+            });
+        }
+    }
+
+    return allServiceGroups;
+});
+
+function getBadgeTitle(toolName: string, isRequired: boolean, groupName?: string) {
+    return `This service is ${isRequired ? "required" : "optional"} by tool '${toolName}' and you ${groupName ? "have selected the credentials group: " + groupName : "have not selected a credentials group for it."}`;
+}
+
 function toggleDialog() {
     showModal.value = !showModal.value;
 }
@@ -49,7 +88,7 @@ onMounted(async () => {
 
 <template>
     <div>
-        <BAlert show :variant="statusVariant">
+        <BAlert show :variant="statusVariant" class="d-flex flex-column flex-gapy-1">
             <LoadingSpan v-if="isBusy" :message="busyMessage" />
             <div v-else-if="isAnonymous">
                 <FontAwesomeIcon :icon="faKey" fixed-width />
@@ -81,7 +120,7 @@ onMounted(async () => {
 
                     <span v-if="hasUserProvidedAllToolsServiceCredentials">
                         <strong>You have already provided credentials for this workflow.</strong> You can update or
-                        delete your credentials, using the <i>{{ provideCredentialsButtonTitle }}</i> button.
+                        delete your credentials using the <i>{{ provideCredentialsButtonTitle }}</i> button.
                     </span>
                     <span v-else-if="someToolsHasRequiredServiceCredentials">
                         <span v-if="hasUserProvidedAllRequiredToolsServiceCredentials">
@@ -107,6 +146,27 @@ onMounted(async () => {
                 <BButton variant="primary" size="sm" @click="toggleDialog">
                     {{ provideCredentialsButtonTitle }}
                 </BButton>
+            </div>
+
+            <div v-if="!isAnonymous && currentWorkflowServiceGroups" class="d-flex flex-wrap flex-gapx-1 flex-gapy-1">
+                <div
+                    v-for="wsg in currentWorkflowServiceGroups"
+                    :key="`${wsg.toolName}-${wsg.serviceName}`"
+                    class="d-flex align-items-center">
+                    <BBadge
+                        v-b-tooltip.hover
+                        :title="getBadgeTitle(wsg.toolName, wsg.isRequired, wsg.groupName)"
+                        :variant="wsg.isRequired ? 'primary' : 'secondary'">
+                        <FontAwesomeIcon :icon="wsg.groupName ? faCheck : faExclamation" fixed-width />
+                        <FontAwesomeIcon :icon="faWrench" fixed-width />
+                        {{ wsg.toolName }}: {{ wsg.serviceName }}
+                        <FontAwesomeIcon :icon="faCaretRight" class="mx-1" />
+                        <span v-if="wsg.groupName">{{ wsg.groupName }}</span>
+                        <em v-else>
+                            {{ wsg.isRequired ? "Required - No group" : "Optional - No group" }}
+                        </em>
+                    </BBadge>
+                </div>
             </div>
         </BAlert>
 
