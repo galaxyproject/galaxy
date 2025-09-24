ea71deec95fafacadb0df12188585118bff3cb3c
Ahmed Awan <qe66653@umbc.edu>
2025-09-24 14:46:23 -0500

    add/adjust jests for job step jobs now shown in a table instead of tabs

--- client/src/components/WorkflowInvocationState/JobStep.test.ts
+++ client/src/components/WorkflowInvocationState/JobStep.test.ts
@@ -1,145 +1,138 @@
-import { createTestingPinia } from "@pinia/testing";
-import { createLocalVue, mount, type Wrapper } from "@vue/test-utils";
+import { mount, shallowMount } from "@vue/test-utils";
 import flushPromises from "flush-promises";
 
-import { HttpResponse, useServerMock } from "@/api/client/__mocks__";
-import type { JobDisplayParametersSummary, ShowFullJobResponse } from "@/api/jobs";
-import paramResponse from "@/components/JobParameters/parameters-response.json";
+import type { JobBaseModel } from "@/api/jobs";
+import { statePlaceholders } from "@/composables/useInvocationGraph";
 
-import jobs from "./test/json/jobs.json";
+import { TEST_JOBS_BY_STATES } from "./test/jobStepUtils";
+import TEST_JOBS_JSON from "./test/json/jobs.json";
 
 import JobStep from "./JobStep.vue";
 
-const localVue = createLocalVue();
-
-const { server, http } = useServerMock();
+const TEST_INVOCATION_ID = "test-invocation-id";
 
 const SELECTORS = {
-    JOB_TABS_LIST: ".nav-pills",
-    JOB_TAB: ".nav-link",
-    JOB_CONTENT: "[data-description='workflow invocation job'] > .card-body",
+    JOB_STATE_BUTTON_NAV: "nav",
+    JOB_STATE_BUTTON: ".g-button",
+    JOBS_TABLE: ".job-step-jobs",
+    JOB_ROW: ".job-step-jobs > tbody > tr",
+    STUBBED_JOB_DETAILS: "anonymous-stub",
 };
 
-describe("DatasetUIWrapper.vue with Dataset", () => {
-    let wrapper: Wrapper<Vue>;
-    let propsData;
-    let iteration: "base" | "state_change" = "base";
-    const pinia = createTestingPinia();
-
-    function getSummary(job_id: string) {
-        let summary = jobs.find((s) => s.id === job_id);
-        if (summary && iteration === "state_change" && job_id === "1") {
-            summary.state = "running";
-        }
-        if (job_id === "3") {
-            summary = { ...(jobs[0] as any), id: "3" };
-        }
-        return summary as ShowFullJobResponse;
-    }
-
-    beforeEach(async () => {
-        server.use(
-            http.get("/api/jobs/{job_id}", ({ response, params }) => {
-                const { job_id } = params;
-                const summary = getSummary(job_id);
-                if (!summary) {
-                    return response("4XX").json({ err_msg: "Job not found", err_code: 404 }, { status: 404 });
-                }
-                return response(200).json(summary);
-            }),
-        );
-        server.use(
-            http.get("/api/invocations", ({ response }) => {
-                return response(200).json([]);
-            }),
-        );
-        server.use(
-            http.get("/api/jobs/{job_id}/parameters_display", ({ response }) => {
-                return response(200).json(paramResponse as JobDisplayParametersSummary);
-            }),
-        );
-        server.use(
-            http.get("/api/datasets/{dataset_id}", ({ response, params }) => {
-                const { dataset_id } = params;
-                // We need to use untyped here because this endpoint is not
-                // described in the OpenAPI spec due to its complexity for now.
-                return response.untyped(
-                    HttpResponse.json({
-                        id: dataset_id,
-                        creating_job: `job-${dataset_id}`,
-                    }),
-                );
-            }),
-        );
-
-        propsData = {
-            jobs: jobs,
-        };
-        wrapper = mount(JobStep as object, {
-            localVue,
-            propsData,
-            stubs: {
-                BPopover: true,
-                ContentItem: true,
-                FontAwesomeIcon: true,
-                JobInformation: true,
-                JobParameters: true,
-                BTooltip: true,
-                LoadingSpan: true,
+describe("Job Step", () => {
+    it("shows jobs grouped by state in tables when multiple jobs", async () => {
+        const wrapper = mount(JobStep as object, {
+            propsData: {
+                jobs: TEST_JOBS_JSON,
+                invocationId: TEST_INVOCATION_ID,
             },
-            pinia,
         });
         await flushPromises();
+
+        // verify that the job state buttons are rendered
+        const nav = wrapper.find(SELECTORS.JOB_STATE_BUTTON_NAV);
+        expect(nav.exists()).toBe(true);
+
+        // there are as many buttons as there are unique job states in `jobs`
+        const buttons = nav.findAll(SELECTORS.JOB_STATE_BUTTON);
+        expect(buttons.length).toBe(Object.keys(TEST_JOBS_BY_STATES).length);
+
+        const jobStates = Object.keys(TEST_JOBS_BY_STATES);
+
+        // test each job state
+        for (let i = 0; i < jobStates.length; i++) {
+            const jobState = jobStates[i] as string;
+            const expectedCount = TEST_JOBS_BY_STATES[jobState]?.length as number;
+
+            // check that the button has the expected text
+            expect(buttons.at(i).text()).toBe(
+                `${expectedCount} job${expectedCount === 1 ? "" : "s"} ${statePlaceholders[jobState] || jobState}`,
+            );
+
+            // click the button to switch to this state
+            await buttons.at(i).trigger("click");
+            await flushPromises();
+
+            // the clicked button should be pressed
+            expect(buttons.at(i).classes()).toContain("g-pressed");
+
+            // renders a table with jobs for the current state
+            const tableRows = wrapper.find(SELECTORS.JOBS_TABLE).findAll(SELECTORS.JOB_ROW);
+            expect(tableRows.length).toBe(expectedCount);
+
+            // each row has the expected state (as the last cell in the row)
+            tableRows.wrappers.forEach((tr) => {
+                const cells = tr.findAll("td");
+                expect(cells.at(cells.length - 1).text()).toBe(jobState);
+            });
+        }
     });
-    test("shows as many tabs as jobs and clicking tab changes shown job", async () => {
-        // it renders 2 jobs in 2 tabs with states
-        const ulTabs = wrapper.find(SELECTORS.JOB_TABS_LIST);
-        const liTabs = ulTabs.findAll(SELECTORS.JOB_TAB);
-        expect(liTabs.length).toBe(jobs.length);
 
-        const jobsContents = wrapper.findAll(SELECTORS.JOB_CONTENT);
-        expect(jobsContents.length).toBe(jobs.length);
-
-        liTabs.wrappers.forEach((liTab, i) => {
-            expect(jobs[i]).toBeTruthy();
-            expect(liTab.text()).toEqual(jobs[i]?.state);
-            // expect the first job to be shown and rest to be hidden
-            expect(liTab.attributes("aria-selected")).toEqual(i === 0 ? "true" : "false");
-            expect(jobsContents.at(i).attributes("style")).toEqual(i === 0 ? "" : "display: none;");
+    it("reacts to job states changing when multiple jobs", async () => {
+        const wrapper = mount(JobStep as object, {
+            propsData: {
+                jobs: TEST_JOBS_JSON,
+                invocationId: TEST_INVOCATION_ID,
+            },
         });
-
-        // click on the second tab
-        await liTabs.at(1).trigger("click");
         await flushPromises();
 
-        // expect the second job to be shown and rest to be hidden
-        jobsContents.wrappers.forEach((jobContent, i) => {
-            expect(jobContent.attributes("style")).toEqual(i === 1 ? "" : "display: none;");
-            expect(liTabs.at(i).attributes("aria-selected")).toEqual(i === 1 ? "true" : "false");
-        });
+        // NOTE: This test assumes that the first job in TEST_JOBS_JSON has state 'new', and there is only one job with that state.
+        // That is because we will update that job to 'running' and expect the active/pressed button to change to 'running',
+        // as there are no more jobs in 'new' state.
+
+        let buttons = wrapper.find(SELECTORS.JOB_STATE_BUTTON_NAV).findAll(SELECTORS.JOB_STATE_BUTTON);
+        expect(buttons.length).toBe(Object.keys(TEST_JOBS_BY_STATES).length);
+        let firstButton = buttons.at(0);
+
+        // verify initial data is displayed for 'new' state'
+        expect(firstButton.classes()).toContain("g-pressed");
+        expect(firstButton.text()).toBe("1 job new");
+
+        // the table should show one job in 'new' state
+        expect(wrapper.findAll(SELECTORS.JOB_ROW).length).toBe(1);
+        expect(wrapper.find(SELECTORS.JOB_ROW).find("td:last-child").text()).toBe("new");
+
+        // we have one running job already
+        expect(buttons.at(1).text()).toBe("1 job running");
+
+        // we trigger the state change by updating the first job's state from 'new' to 'running'
+        const updatedJob = { ...TEST_JOBS_JSON[0], state: "running" };
+        await wrapper.setProps({ jobs: [updatedJob, ...TEST_JOBS_JSON.slice(1)] });
+        await flushPromises();
+
+        // verify buttons have been updated
+        buttons = wrapper.find(SELECTORS.JOB_STATE_BUTTON_NAV).findAll(SELECTORS.JOB_STATE_BUTTON);
+        expect(buttons.length).toBe(Object.keys(TEST_JOBS_BY_STATES).length - 1);
+        firstButton = buttons.at(0);
+
+        // first button is now 'running' with 2 jobs
+        expect(firstButton.classes()).toContain("g-pressed");
+        expect(firstButton.text()).toBe("2 jobs running");
+
+        // the table should now show 2 jobs in 'running' state
+        expect(wrapper.findAll(SELECTORS.JOB_ROW).length).toBe(2);
+        expect(wrapper.find(SELECTORS.JOB_ROW).find("td:last-child").text()).toBe("running");
     });
-    test("it reacts to prop update", async () => {
-        // verify initial data is displayed for first tab
-        expect(wrapper.find(SELECTORS.JOB_TAB).text()).toBe("new");
 
-        // update first job to change state from 'new' to 'running'
-        iteration = "state_change";
-        // technically, we only need to trigger a prop update, the state
-        // change is handled by the mock server in `getSummary`
-        const updatedJob = { ...jobs[0], state: "running" };
-        await wrapper.setProps({ jobs: [updatedJob, ...jobs.slice(1)] });
+    it("just renders the job when only one job", async () => {
+        const singleJob = TEST_JOBS_JSON.slice(0, 1)[0] as JobBaseModel;
+
+        const wrapper = shallowMount(JobStep as object, {
+            propsData: {
+                jobs: [singleJob],
+                invocationId: TEST_INVOCATION_ID,
+            },
+        });
         await flushPromises();
 
-        // verify new data is displayed
-        expect(wrapper.find(SELECTORS.JOB_TAB).text()).toBe("running");
+        expect(wrapper.find(SELECTORS.JOBS_TABLE).exists()).toBe(false);
+        expect(wrapper.find(SELECTORS.JOB_STATE_BUTTON_NAV).exists()).toBe(false);
 
-        // update data
-        const additionalJob = { ...jobs[0], id: "3" };
-        await wrapper.setProps({ jobs: [...jobs, additionalJob] });
-        await flushPromises();
-        // verify new data is displayed
-        const ulTabs = wrapper.find(SELECTORS.JOB_TABS_LIST);
-        const liTabs = ulTabs.findAll(SELECTORS.JOB_TAB);
-        expect(liTabs.length).toBe(jobs.length + 1);
+        const jobDetails = wrapper.find(SELECTORS.STUBBED_JOB_DETAILS);
+        expect(jobDetails.exists()).toBe(true);
+
+        expect(jobDetails.attributes()["jobid"]).toBe(singleJob.id);
+        expect(jobDetails.attributes()["invocationid"]).toBe(TEST_INVOCATION_ID);
     });
 });
