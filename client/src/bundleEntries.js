a381133f2cb20b4a5602e2e339bcdc65ba25025e
guerler <aysam.guerler@gmail.com>
2025-09-29 18:42:37 +0300

    Remove Trackster client code

--- client/src/bundleEntries.js
+++ client/src/bundleEntries.js
@@ -1,5 +1,5 @@
 /**
- * The big list of horrible globals we expose on window.bundleEntries.
+ * The list of horrible globals we expose on window.bundleEntries.
  *
  * Everything that is exposed on this global variable is something that the python templates
  * require for their hardcoded initializations. These objects are going to have to continue
@@ -10,18 +10,12 @@
 /* jquery and _ are exposed via expose-loader while several external plugins rely on these */
 import $ from "jquery"; // eslint-disable-line no-unused-vars
 import _ from "underscore"; // eslint-disable-line no-unused-vars
-import { TracksterUIView } from "viz/trackster";
 
 export { getGalaxyInstance, setGalaxyInstance } from "app";
 export { default as LegacyGridView } from "legacy/grid/grid-view";
 export { createTabularDatasetChunkedView } from "mvc/dataset/data";
 export { create_chart, create_histogram } from "reports/run_stats";
 export { Toast } from "ui/toast"; // TODO: remove when external consumers are updated/gone (IES right now)
-export { TracksterUI } from "viz/trackster";
-
-export function trackster(options) {
-    new TracksterUIView(options);
-}
 
 // Previously wandering around as window.thing = thing in the onload script
 export { hide_modal, Modal, show_in_overlay, show_message, show_modal } from "layout/modal";
