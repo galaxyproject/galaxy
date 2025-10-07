4f5dc8880fd64408a37267c4e4a7f9d6032c4385
Alireza Heidari <itisalirh@gmail.com>
2025-10-07 16:42:41 +0200

    Refactor FilesDialog for improved selection handling
    Streamlined the selection dialog by removing unnecessary computed fields and resetting pagination on load.

--- client/src/components/FilesDialog/FilesDialog.vue
+++ client/src/components/FilesDialog/FilesDialog.vue
@@ -89,18 +89,7 @@ const showFTPHelper = ref(false);
 const selectAllIcon = ref<SelectionState>(SELECTION_STATES.UNSELECTED);
 const urlTracker = useUrlTracker<SelectionItem & { parentPage?: number }>({ root: undefined });
 const totalItems = ref(0);
-
-const fields = computed(() => {
-    const fields = [];
-    fields.push({ key: "label" });
-    if (showDetails.value) {
-        fields.push({ key: "details" });
-    }
-    if (showTime.value) {
-        fields.push({ key: "time" });
-    }
-    return fields;
-});
+const selectionDialog = ref();
 
 const fileMode = computed(() => props.mode == "file");
 
@@ -197,7 +186,6 @@ function selectDirectoryRecursive(record: SelectionItem) {
                     selectedDirectories.value.push(subRecord);
                 }
             });
-            totalItems.value = incoming.totalMatches;
             isBusy.value = false;
         });
     }
@@ -257,6 +245,8 @@ function open(record: SelectionItem) {
 
 /** Performs server request to retrieve data records **/
 function load() {
+    selectionDialog.value.resetPagination();
+
     showFTPHelper.value = urlTracker.current.value?.url === "gxftp://";
     filter.value = undefined;
     optionsShow.value = false;
@@ -379,11 +369,10 @@ function filterByMode(results: RemoteEntry[]): RemoteEntry[] {
 }
 
 function entryToRecord(entry: RemoteEntry): SelectionItem {
-    const result = {
+    const result: SelectionItem = {
         id: entry.uri,
         label: entry.name,
-        time: entry.class === "File" ? entry.ctime : "",
-        details: entry.class === "File" ? entry.ctime : "",
+        update_time: entry.class === "File" ? entry.ctime : "",
         isLeaf: entry.class === "File",
         url: entry.uri,
         size: entry.class === "File" ? entry.size : 0,
@@ -468,7 +457,6 @@ onMounted(() => {
         :disable-ok="okButtonDisabled"
         :error-message="errorMessage"
         :file-mode="fileMode"
-        :fields="fields"
         :is-busy="isBusy"
         :items="items"
         :items-provider="itemsProvider"
