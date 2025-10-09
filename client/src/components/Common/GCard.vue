02dc7c54b9cd679b65b34cf5c42cca5bb393fa9a
guerler <aysam.guerler@gmail.com>
2025-10-09 12:11:35 +0300

    Fix misalignment of icon and title text in card header

--- client/src/components/Common/GCard.vue
+++ client/src/components/Common/GCard.vue
@@ -335,33 +335,35 @@ function onKeyDown(event: KeyboardEvent) {
                                             :id="getElementId(props.id, 'title')"
                                             bold
                                             inline
-                                            class="align-items-baseline"
                                             :size="props.titleSize">
-                                            <FontAwesomeIcon
-                                                v-if="props.titleIcon?.icon"
-                                                :icon="props.titleIcon.icon"
-                                                :class="props.titleIcon.class"
-                                                :title="props.titleIcon.title"
-                                                :size="props.titleIcon.size"
-                                                fixed-width />
-                                            <BLink
-                                                v-if="typeof title === 'object'"
-                                                :id="getElementId(props.id, 'title-link')"
-                                                v-b-tooltip.hover.noninteractive
-                                                :title="localize(title.title)"
-                                                :class="{ 'g-card-title-truncate': props.titleNLines }"
-                                                @click.stop.prevent="title.handler">
-                                                {{ title.label }}
-                                            </BLink>
-                                            <template v-else>
-                                                <span
-                                                    :id="getElementId(props.id, 'title-text')"
+                                            <div class="d-flex">
+                                                <FontAwesomeIcon
+                                                    v-if="props.titleIcon?.icon"
+                                                    class="mr-1"
+                                                    :class="props.titleIcon.class"
+                                                    :icon="props.titleIcon.icon"
+                                                    :title="props.titleIcon.title"
+                                                    :size="props.titleIcon.size"
+                                                    fixed-width />
+                                                <BLink
+                                                    v-if="typeof title === 'object'"
+                                                    :id="getElementId(props.id, 'title-link')"
                                                     v-b-tooltip.hover.noninteractive
-                                                    :title="localize(title)"
-                                                    :class="{ 'g-card-title-truncate': props.titleNLines }">
-                                                    {{ title }}
-                                                </span>
-                                            </template>
+                                                    :title="localize(title.title)"
+                                                    :class="{ 'g-card-title-truncate': props.titleNLines }"
+                                                    @click.stop.prevent="title.handler">
+                                                    {{ title.label }}
+                                                </BLink>
+                                                <template v-else>
+                                                    <span
+                                                        :id="getElementId(props.id, 'title-text')"
+                                                        v-b-tooltip.hover.noninteractive
+                                                        :title="localize(title)"
+                                                        :class="{ 'g-card-title-truncate': props.titleNLines }">
+                                                        {{ title }}
+                                                    </span>
+                                                </template>
+                                            </div>
 
                                             <slot name="titleActions">
                                                 <BButton
