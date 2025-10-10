f06818996b4387835cf1f08deffc508605ebacd2
guerler <aysam.guerler@gmail.com>
2025-10-10 11:28:06 +0300

    Rewrite modal wrapper without backbone

--- client/src/mvc/ui/ui-modal.js
+++ client/src/mvc/ui/ui-modal.js
@@ -1,210 +1,182 @@
-import Backbone from "backbone";
-import $ from "jquery";
-import _ from "underscore";
-
-export var View = Backbone.View.extend({
-    className: "ui-modal",
-
-    // defaults
-    optionsDefault: {
-        container: "body",
-        title: "ui-modal",
-        cls: "ui-modal",
-        body: "",
-        backdrop: true,
-        height: null,
-        width: null,
-        xlarge: false /* BS4 max-width already expanded in Galaxy, expand even more if true. */,
-        closing_events: false,
-        closing_callback: null,
-        title_separator: true,
-    },
-
-    // button list
-    buttonList: {},
-
-    // initialize
-    initialize: function (options) {
-        this.options = _.defaults(options || {}, this.optionsDefault);
-        $(this.options.container).prepend(this.el);
-        // optional render
+export class View {
+    constructor(options = {}) {
+        this.optionsDefault = {
+            container: "body",
+            title: "ui-modal",
+            cls: "ui-modal",
+            body: "",
+            backdrop: true,
+            height: null,
+            width: null,
+            xlarge: false,
+            closing_events: false,
+            closing_callback: null,
+            title_separator: true
+        };
+        this.buttonList = {};
+        this.options = Object.assign({}, this.optionsDefault, options);
+        this.el = document.createElement("div");
+        this.el.className = "ui-modal";
+        document.querySelector(this.options.container).prepend(this.el);
         if (options) {
             this.render();
         }
-    },
-
-    /**
-     * Displays modal
-     */
-    show: function (options) {
+    }
+    show(options) {
         if (options) {
-            this.options = _.defaults(options, this.optionsDefault);
+            this.options = Object.assign({}, this.optionsDefault, options);
             this.render();
         }
         if (!this.visible) {
             this.visible = true;
-            this.$el.fadeIn("fast");
+            this.el.style.display = "block";
+            this.el.style.opacity = 0;
+            requestAnimationFrame(() => {
+                this.el.style.transition = "opacity 0.15s";
+                this.el.style.opacity = 1;
+            });
         }
         if (this.options.closing_events) {
-            $(document).on("keyup.ui-modal", (e) => {
-                if (e.keyCode == 27) {
+            this._keyupHandler = (e) => {
+                if (e.keyCode === 27) {
                     this.hide(true);
                 }
-            });
-            this.$backdrop.on("click", () => {
+            };
+            document.addEventListener("keyup", this._keyupHandler);
+            this.$backdrop.addEventListener("click", () => {
                 this.hide(true);
             });
         }
-        this.$el.find(".title").focus();
-    },
-
-    /**
-     * Hide modal
-     */
-    hide: function (canceled) {
+        this.$header.querySelector(".title").focus();
+    }
+    hide(canceled) {
         this.visible = false;
-        this.$el.fadeOut("fast");
+        this.el.style.transition = "opacity 0.15s";
+        this.el.style.opacity = 0;
+        setTimeout(() => {
+            this.el.style.display = "none";
+        }, 150);
         if (this.options.closing_callback) {
             this.options.closing_callback(canceled);
         }
-        $(document).off("keyup.ui-modal");
-        this.$backdrop.off("click");
-    },
-
-    /**
-     * Render modal
-     */
-    render: function () {
-        var self = this;
-        this.$el.html(this._template());
-
-        // link elements
-        this.$header = this.$(".modal-header");
-        this.$dialog = this.$(".modal-dialog");
+        if (this._keyupHandler) {
+            document.removeEventListener("keyup", this._keyupHandler);
+            this._keyupHandler = null;
+        }
+        if (this.$backdrop) {
+            this.$backdrop.replaceWith(this.$backdrop.cloneNode(true));
+        }
+    }
+    render() {
+        this.el.innerHTML = this._template();
+        this.$header = this.el.querySelector(".modal-header");
+        this.$dialog = this.el.querySelector(".modal-dialog");
         if (this.options.extra_class) {
-            this.$dialog.addClass(this.options.extra_class);
+            this.$dialog.classList.add(this.options.extra_class);
         }
-        this.$body = this.$(".modal-body");
-        this.$footer = this.$(".modal-footer");
-        this.$backdrop = this.$(".modal-backdrop");
-        this.$buttons = this.$(".buttons");
-
-        if (this.options.body == "progress") {
-            this.options.body = $(
-                '<div class="progress progress-striped active">' +
-                    '<div class="progress-bar progress-bar-info" style="width:100%"/>' +
-                    "</div>",
-            );
+        this.$body = this.el.querySelector(".modal-body");
+        this.$footer = this.el.querySelector(".modal-footer");
+        this.$backdrop = this.el.querySelector(".modal-backdrop");
+        this.$buttons = this.el.querySelector(".buttons");
+        if (this.options.body === "progress") {
+            const div = document.createElement("div");
+            div.className = "progress progress-striped active";
+            div.innerHTML = '<div class="progress-bar progress-bar-info" style="width:100%"></div>';
+            this.options.body = div;
         }
-
-        // fix main content
-        this.$el.removeClass().addClass("modal").addClass(this.options.cls);
-        this.$header.find(".title").html(this.options.title);
-        this.$body.html(this.options.body);
-
-        // append buttons
-        this.$buttons.empty();
+        this.el.className = "modal " + this.options.cls;
+        this.$header.querySelector(".title").innerHTML = this.options.title;
+        if (typeof this.options.body === "string") {
+            this.$body.innerHTML = this.options.body;
+        } else {
+            this.$body.innerHTML = "";
+            this.$body.append(this.options.body);
+        }
+        this.$buttons.innerHTML = "";
         this.buttonList = {};
         if (this.options.buttons) {
-            var counter = 0;
-            $.each(this.options.buttons, (name, callback) => {
-                var $button = $("<button/>").attr("id", `button-${counter++}`).text(name).click(callback);
-                self.$buttons.append($button).append("&nbsp;");
-                self.buttonList[name] = $button;
-            });
+            let counter = 0;
+            for (const [name, callback] of Object.entries(this.options.buttons)) {
+                const button = document.createElement("button");
+                button.id = `button-${counter++}`;
+                button.textContent = name;
+                button.addEventListener("click", callback);
+                this.$buttons.append(button);
+                this.$buttons.append(document.createTextNode(" "));
+                this.buttonList[name] = button;
+            }
         } else {
-            this.$footer.hide();
+            this.$footer.style.display = "none";
         }
-
-        // configure background, separator line
-        this.$backdrop[(this.options.backdrop && "addClass") || "removeClass"]("in");
-        this.$header[(!this.options.title_separator && "addClass") || "removeClass"]("no-separator");
-
-        // fix dimensions
-        // note: because this is a singleton, we need to clear inline styles from any previous invocations
-        this.$body.removeAttr("style");
-        if (this.options.height) {
-            this.$body.css("height", this.options.height);
-            this.$body.css("overflow", "hidden");
+        if (this.options.backdrop) {
+            this.$backdrop.classList.add("in");
         } else {
-            this.$body.css("max-height", $(window).height() / 2);
+            this.$backdrop.classList.remove("in");
+        }
+        if (!this.options.title_separator) {
+            this.$header.classList.add("no-separator");
+        } else {
+            this.$header.classList.remove("no-separator");
+        }
+        this.$body.removeAttribute("style");
+        if (this.options.height) {
+            this.$body.style.height = this.options.height;
+            this.$body.style.overflow = "hidden";
+        } else {
+            this.$body.style.maxHeight = window.innerHeight / 2 + "px";
         }
         if (this.options.width) {
-            this.$dialog.css("width", this.options.width);
+            this.$dialog.style.width = this.options.width;
         }
         if (this.options.xlarge) {
-            this.$dialog.css("max-width", "3000px");
+            this.$dialog.style.maxWidth = "3000px";
         }
-    },
-
-    /**
-     * Returns the button dom
-     * @param{String}   name    - Button name/title
-     */
-    getButton: function (name) {
+    }
+    getButton(name) {
         return this.buttonList[name];
-    },
-
-    /**
-     * Enables a button
-     * @param{String}   name    - Button name/title
-     */
-    enableButton: function (name) {
-        this.getButton(name).prop("disabled", false);
-    },
-
-    /**
-     * Disables a button
-     * @param{String}   name    - Button name/title
-     */
-    disableButton: function (name) {
-        this.getButton(name).prop("disabled", true);
-    },
-
-    /**
-     * Show a button
-     * @param{String}   name    - Button name/title
-     */
-    showButton: function (name) {
-        this.getButton(name).show();
-    },
-
-    /**
-     * Hide a button
-     * @param{String}   name    - Button name/title
-     */
-    hideButton: function (name) {
-        this.getButton(name).hide();
-    },
-
-    /**
-     * Returns scroll top for body element
-     */
-    scrollTop: function () {
-        return this.$body.scrollTop();
-    },
-
-    /**
-     * Returns the modal template
-     */
-    _template: function () {
+    }
+    enableButton(name) {
+        const btn = this.getButton(name);
+        if (btn) {
+            btn.disabled = false;
+        }
+    }
+    disableButton(name) {
+        const btn = this.getButton(name);
+        if (btn) {
+            btn.disabled = true;
+        }
+    }
+    showButton(name) {
+        const btn = this.getButton(name);
+        if (btn) {
+            btn.style.display = "";
+        }
+    }
+    hideButton(name) {
+        const btn = this.getButton(name);
+        if (btn) {
+            btn.style.display = "none";
+        }
+    }
+    scrollTop() {
+        return this.$body.scrollTop;
+    }
+    _template() {
         return (
-            '<div class="modal-backdrop fade"/>' +
+            '<div class="modal-backdrop fade"></div>' +
             '<div class="modal-dialog">' +
             '<div class="modal-content">' +
             '<div class="modal-header">' +
-            '<h4 class="title" tabindex="0"/>' +
+            '<h4 class="title" tabindex="0"></h4>' +
             "</div>" +
-            '<div class="modal-body"/>' +
+            '<div class="modal-body"></div>' +
             '<div class="modal-footer">' +
-            '<div class="buttons"/>' +
+            '<div class="buttons"></div>' +
             "</div>" +
             "</div>" +
             "</div>"
         );
-    },
-});
-
-export default {
-    View: View,
-};
+    }
+}
+export default { View };
