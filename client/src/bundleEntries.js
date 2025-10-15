97af8b76ca8b8ecdd1303123125377aa689d69d2
guerler <aysam.guerler@gmail.com>
2025-10-15 13:20:19 +0300

    Remove unused bundleEntries exports, verified via grep on the templates directory

--- client/src/bundleEntries.js
+++ client/src/bundleEntries.js
@@ -6,23 +6,18 @@
  * to exist until such time as we replace the overall application with a Vue component which
  * will handle initializations for components individually.
  *
- * legacy/grid_base.mako: window.bundleEntries.LegacyGridView
- * webapps/galaxy/dataset/tabular_chunked.mako: window.bundleEntries.createTabularDatasetChunkedView
- * webapps/galaxy/dataset/display.mako: window.bundleEntries.createTabularDatasetChunkedView
- * webapps/reports/run_stats.mako: window.bundleEntries.create_chart
- * webapps/reports/run_stats.mako: window.bundleEntries.create_histogram
  */
 import { replaceChildrenWithComponent } from "utils/mountVueComponent";
 
 import TabularChunkedView from "components/Visualizations/Tabular/TabularChunkedView.vue";
 
-export { getGalaxyInstance, setGalaxyInstance } from "app";
+// legacy/grid_base.mako
 export { default as LegacyGridView } from "legacy/grid/grid-view";
+
+// webapps/reports/run_stats.mako
 export { create_chart, create_histogram } from "reports/run_stats";
 
+// webapps/galaxy/dataset/{ display | tabular_chunked }.mako
 export const createTabularDatasetChunkedView = (options) => {
     return replaceChildrenWithComponent(options.parent_elt, TabularChunkedView, { options });
 };
-
-// Used in common.mako
-export { default as store } from "storemodern";
