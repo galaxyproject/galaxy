0aa35a2c073c9dfa5cbec7c1c241e0b93df5382b
guerler <aysam.guerler@gmail.com>
2025-10-17 04:14:11 +0300

    Prevent empty selection in rule builder

--- client/src/components/RuleBuilder/SelectBasic.vue
+++ client/src/components/RuleBuilder/SelectBasic.vue
@@ -1,6 +1,7 @@
 <template>
     <VueMultiselect
         class="select-basic"
+        :allow-empty="multiple"
         :close-on-select="!multiple"
         :options="options"
         :multiple="multiple"
