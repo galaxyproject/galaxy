c17a5ac599ed3a94651b3d07bbe050341710959f
guerler <aysam.guerler@gmail.com>
2025-10-18 23:07:37 +0300

    Move template content to display component

--- client/src/components/Dataset/DatasetDisplay.vue
+++ client/src/components/Dataset/DatasetDisplay.vue
@@ -11,6 +11,7 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {});
 
 const content = ref();
+const deleted = ref(true);
 
 onMounted(async () => {
     const url = withPrefix(`/datasets/${props.datasetId}/display/?preview=True`);
@@ -25,8 +26,26 @@ onMounted(async () => {
 </script>
 
 <template>
-    <pre>
-        {{  content }}
-    </pre>
+    <div>
+        <div v-if="deleted" class="errormessagelarge" id="deleted-data-message">
+            You are viewing a deleted dataset.
+        </div>
+        <div class="warningmessagelarge">
+            This dataset is large and only the first megabyte is shown below.<br />
+            <a href="${h.url_for( controller='dataset', action='display', dataset_id=trans.security.encode_id( data.id ), filename='' )}">Show all</a> |
+            <a href="${h.url_for( controller='dataset', action='display', dataset_id=trans.security.encode_id( data.id ), to_ext=data.ext )}">Save</a>
+        </div>
+        <div class="warningmessagelarge">
+            This is a binary (or unknown to Galaxy) dataset of size ${ file_size }. Preview is not implemented for this filetype. Displaying
+            %if truncated:
+        first 100KB
+            %endif
+        as ASCII text<br/>
+            <a href="${h.url_for( controller='dataset', action='display', dataset_id=trans.security.encode_id( data.id ), to_ext=data.ext )}">Download</a>
+        </div>
+        <pre>
+            {{  content }}
+        </pre>
+    </div>
 </template>
 
