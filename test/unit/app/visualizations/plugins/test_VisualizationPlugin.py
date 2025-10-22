c26b2f1e6d9cd1966203042159fc02534f564925
guerler <aysam.guerler@gmail.com>
2025-10-22 18:37:57 +0300

    Remove unused imports

--- test/unit/app/visualizations/plugins/test_VisualizationPlugin.py
+++ test/unit/app/visualizations/plugins/test_VisualizationPlugin.py
@@ -3,17 +3,11 @@ Test lib/galaxy/visualization/plugins/plugin.
 """
 
 from galaxy.app_unittest_utils import galaxy_mock
-from galaxy.util import clean_multiline_string
-from galaxy.visualization.plugins import (
-    plugin as vis_plugin,
-    resource_parser,
-    utils as vis_utils,
-)
+from galaxy.visualization.plugins.plugin import VisualizationPlugin
 from . import VisualizationsBase_TestCase
 
 
 class TestVisualizationsPlugin(VisualizationsBase_TestCase):
-    plugin_class = vis_plugin.VisualizationPlugin
 
     def test_default_init(self):
         """
@@ -25,7 +19,7 @@ class TestVisualizationsPlugin(VisualizationsBase_TestCase):
                 "static": {},
             }
         )
-        plugin = self.plugin_class(vis_dir.root_path, "myvis", {})
+        plugin = VisualizationPlugin(vis_dir.root_path, "myvis", {})
         assert plugin.name == "myvis"
         assert plugin.path == vis_dir.root_path
         assert plugin.config == {}
