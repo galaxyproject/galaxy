07ebecbc15176867269cfafcb5444d9201c457ec
guerler <aysam.guerler@gmail.com>
2025-10-23 09:43:05 +0300

    Reuse Visualization Frame component in Markdown Wrapper

--- client/src/components/Visualizations/VisualizationWrapper.vue
+++ client/src/components/Visualizations/VisualizationWrapper.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import { faExpand, faWindowMaximize } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import axios from "axios";
 import { BAlert, BButton } from "bootstrap-vue";
-import { debounce } from "lodash";
-import { computed, onMounted, ref } from "vue";
+import { computed, ref } from "vue";
 
-import { getAppRoot } from "@/onload/loadConfig";
-
-const DELAY = 300;
+import VisualizationFrame from "./VisualizationFrame.vue";
 
 const props = withDefaults(
     defineProps<{
@@ -24,86 +20,12 @@ const props = withDefaults(
 
 const emit = defineEmits(["change", "load"]);
 
-const emitChange = debounce((newValue: string) => {
-    emit("change", newValue);
-}, DELAY);
-
 const errorMessage = ref("");
 const expand = ref(false);
-const iframeRef = ref<HTMLIFrameElement | null>(null);
 
-const iframeClass = computed(() =>
-    expand.value ? "visualization-popout-wrapper" : "visualization-wrapper"
+const fixedHeight = computed(() =>
+    expand.value ? {} : { maxHeight: `${props.height}px`, minHeight: `${props.height}px` },
 );
-
-const iframeStyle = computed(() =>
-    expand.value ? {} : { maxHeight: `${props.height}px`, minHeight: `${props.height}px` }
-);
-
-async function render() {
-    if (!props.name) {
-        errorMessage.value = "Visualization name is required!";
-        return;
-    }
-    try {
-        const { data: plugin } = await axios.get(`${getAppRoot()}api/plugins/${props.name}`);
-        const pluginPath = plugin.href;
-        const dataIncoming = {
-            root: window.location.origin + getAppRoot(),
-            visualization_config: props.config,
-            visualization_plugin: plugin,
-            visualization_title: props.title,
-        };
-
-        const iframe = iframeRef.value;
-        if (!iframe) {
-            errorMessage.value = "Frame has been invalidated.";
-            return;
-        }
-
-        const iframeDocument = iframe.contentDocument;
-        if (!iframeDocument) {
-            errorMessage.value = "Failed to access iframe document.";
-            return;
-        }
-
-        const container = iframeDocument.createElement("div");
-        container.id = "app";
-        container.setAttribute("data-incoming", JSON.stringify(dataIncoming));
-        iframeDocument.body.appendChild(container);
-
-        if (plugin?.entry_point?.attr?.src) {
-            const script = iframeDocument.createElement("script");
-            script.type = "module";
-            script.src = `${pluginPath}/${plugin.entry_point.attr.src}`;
-            iframeDocument.body.appendChild(script);
-        } else {
-            const error = iframeDocument.createElement("div");
-            error.innerHTML = `Unable to locate plugin module for: ${props.name}.`;
-            iframeDocument.body.appendChild(error);
-        }
-
-        if (plugin?.entry_point?.attr?.css) {
-            const link = iframeDocument.createElement("link");
-            link.rel = "stylesheet";
-            link.href = `${pluginPath}/${plugin.entry_point.attr.css}`;
-            iframeDocument.head.appendChild(link);
-        }
-
-        iframe.contentWindow?.addEventListener("message", (event) => {
-            if (event.data.from === "galaxy-visualization") {
-                emitChange(event.data);
-            }
-        });
-
-        emit("load");
-        errorMessage.value = "";
-    } catch (e) {
-        errorMessage.value = `Visualization '${props.name}' not available: ${e}.`;
-    }
-}
-
-onMounted(() => render());
 </script>
 
 <template>
@@ -111,13 +33,15 @@ onMounted(() => render());
         <BAlert variant="danger" show>{{ errorMessage }}</BAlert>
     </div>
     <div v-else class="position-relative h-100">
-        <iframe
-            id="galaxy_visualization"
-            ref="iframeRef"
-            :class="iframeClass"
-            title="visualization"
-            :style="iframeStyle">
-        </iframe>
+        <div :class="`visualization-pop${expand ? 'out' : 'in'}`">
+            <VisualizationFrame
+                title="visualization"
+                :config="props.config"
+                :name="props.name"
+                :style="fixedHeight"
+                @change="emit('change', $event)"
+                @load="emit('load')" />
+        </div>
         <BButton
             class="visualization-popout-expand"
             variant="link"
@@ -141,6 +65,23 @@ onMounted(() => render());
 <style lang="scss">
 @import "theme/blue.scss";
 
+.visualization-popin {
+    border: none;
+    width: 100%;
+    padding-top: 1.5rem;
+}
+.visualization-popout {
+    background: $white;
+    border: $border-default;
+    border-radius: $border-radius-base;
+    height: calc(100vh - 2rem);
+    left: 1rem;
+    padding-top: 1.5rem;
+    position: fixed;
+    top: 1rem;
+    width: calc(100vw - 2rem);
+    z-index: 1000;
+}
 .visualization-popout-close {
     left: 1rem;
     position: fixed;
@@ -154,22 +95,5 @@ onMounted(() => render());
     padding: 0 0.5rem;
     position: absolute;
     top: 0;
-}
-.visualization-popout-wrapper {
-    background: $white;
-    border: $border-default;
-    border-radius: $border-radius-base;
-    height: calc(100vh - 2rem);
-    left: 1rem;
-    padding-top: 1.5rem;
-    position: fixed;
-    top: 1rem;
-    width: calc(100vw - 2rem);
-    z-index: 1000;
-}
-.visualization-wrapper {
-    border: none;
-    width: 100%;
-    padding-top: 1.5rem;
 }
 </style>
