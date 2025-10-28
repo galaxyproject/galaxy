56a5fa934b4486de463196cd920f1c86d6953b79
guerler <aysam.guerler@gmail.com>
2025-10-28 11:13:33 +0300

    Update display applications component

--- client/src/components/Visualizations/DisplayApplications.vue
+++ client/src/components/Visualizations/DisplayApplications.vue
@@ -1,6 +1,20 @@
+<script setup>
+import { withPrefix } from "utils/redirect";
+import { DatasetProvider } from "components/providers";
+import { defineProps } from "vue";
+const props = defineProps({
+    datasetId: {
+        type: String,
+        required: true,
+    },
+});
+function getUrl(link) {
+    return withPrefix(`/display_applications/${props.datasetId}/${link.app_name}/${link.link_name}`);
+}
+</script>
 <template>
     <div>
-        <DatasetProvider :id="datasetId" v-slot="{ result: dataset }">
+        <DatasetProvider :id="props.datasetId" v-slot="{ result: dataset }">
             <b-alert
                 v-if="
                     (dataset && dataset.display_apps && dataset.display_apps.length > 0) ||
@@ -15,7 +29,7 @@
                             <span class="font-weight-bold">{{ displayApp.label }}</span>
                             <span v-for="(link, linkKey) in displayApp.links" :key="linkKey">
                                 <span v-if="linkKey == 0">(</span>
-                                <b-link :href="link.href" :target="link.target">{{ link.text }}</b-link>
+                                <router-link :to="getUrl(link)">{{ link.text }}</router-link>
                                 <span v-if="linkKey != displayApp.links.length - 1">, </span>
                                 <span v-else>)</span>
                             </span>
@@ -24,7 +38,7 @@
                             <span class="font-weight-bold">{{ displayType.label }}</span>
                             <span v-for="(link, linkKey) in displayType.links" :key="linkKey">
                                 <span v-if="linkKey == 0">(</span>
-                                <b-link :href="link.href" :target="link.target">{{ link.text }}</b-link>
+                                <router-link :to="link.href">{{ link.text }}</router-link>
                                 <span v-if="linkKey != displayType.links.length - 1">, </span>
                                 <span v-else>)</span>
                             </span>
@@ -36,18 +50,3 @@
         </DatasetProvider>
     </div>
 </template>
-<script>
-import { DatasetProvider } from "@/components/providers";
-
-export default {
-    components: {
-        DatasetProvider,
-    },
-    props: {
-        datasetId: {
-            type: String,
-            required: true,
-        },
-    },
-};
-</script>
