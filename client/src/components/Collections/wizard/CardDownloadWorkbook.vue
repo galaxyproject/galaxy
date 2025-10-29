f37e5e060c2b336f0dae231fbb860c1279fdfed2
John Chilton <jmchilton@gmail.com>
2025-10-29 11:41:39 -0400

    Fix link broken in fetch workbook workflow.

--- client/src/components/Collections/wizard/CardDownloadWorkbook.vue
+++ client/src/components/Collections/wizard/CardDownloadWorkbook.vue
@@ -3,6 +3,16 @@ import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { BCard, BCardTitle, BLink } from "bootstrap-vue";
 
+// We use this in two capacities - one where we have to do a post and do complex things
+// to generate the workbook and one where we just have an easy link. Support both.
+interface Props {
+    generateWorkbookLink?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+    generateWorkbookLink: undefined,
+});
+
 const emit = defineEmits(["download"]);
 </script>
 
@@ -11,6 +21,8 @@ const emit = defineEmits(["download"]);
         <BCardTitle>
             <b>Step 1: Download</b>
         </BCardTitle>
-        <BLink @click="emit('download')"><FontAwesomeIcon size="xl" :icon="faDownload" /> Download workbook.</BLink>
+        <BLink :href="generateWorkbookLink" @click="emit('download')"
+            ><FontAwesomeIcon size="xl" :icon="faDownload" /> Download workbook.</BLink
+        >
     </BCard>
 </template>
