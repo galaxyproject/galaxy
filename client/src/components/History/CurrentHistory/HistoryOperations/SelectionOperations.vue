9a8e6322d17b8c16d0f58727f52b06cafca0e651
Ahmed Awan <qe66653@umbc.edu>
2025-10-29 12:57:07 -0500

    force push to router so collection builder can be invoked consecutively
    Same concept as in https://github.com/galaxyproject/galaxy/blob/9821f60d0054ff16dddcf2dfe794ebd5db83714e/client/src/components/History/Content/ContentItem.vue#L236-L246.
    
    Hopefully vue-router 4 will help us replace these workarounds with its native force push.

--- client/src/components/History/CurrentHistory/HistoryOperations/SelectionOperations.vue
+++ client/src/components/History/CurrentHistory/HistoryOperations/SelectionOperations.vue
@@ -303,7 +303,10 @@ export default {
             const { setSelectedItems } = useCollectionBuilderItemSelection();
             const selection = Array.from(this.contentSelection.values());
             setSelectedItems(selection);
-            this.$router.push({ path: `/collection/new_list?advanced=${advanced}` });
+
+            // vue-router 4 supports a native force push with clean URLs, but we're using a __vkey__
+            // bit as a workaround to allow the builder to be invoked consecutively
+            this.$router.push({ path: `/collection/new_list?advanced=${advanced}` }, { force: true });
         },
         // Selected content manipulation, hide/show/delete/purge
         hideSelected() {
