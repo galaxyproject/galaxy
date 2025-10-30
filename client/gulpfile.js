41d34d3819871524f4fb567e80a84d99e791d039
Dannon Baker <dannon.baker@gmail.com>
2025-10-30 19:03:45 -0400

    Add Galaxy icon generation infrastructure
    Created a script to auto-generate TypeScript exports from the Galaxy icon JSON. The gulpfile now runs this during the icon build step, creating galaxyIcons.ts with properly typed IconDefinition exports.

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -66,6 +66,9 @@ function stageLibs(callback) {
 
 async function icons() {
     await buildIcons("./src/assets/icons.json");
+    // Generate TypeScript exports for Galaxy icons
+    const generateIconsTypeScript = require("./icons/generate_ts_icons");
+    generateIconsTypeScript();
 }
 
 function cleanPlugins(callback) {
