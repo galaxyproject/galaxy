1871971b7038ef54272874a933b39560c22b25de
Aysam Guerler <aysam.guerler@gmail.com>
2025-10-31 17:56:42 +0300

    Update lib/galaxy/visualization/plugins/registry.py
    Co-authored-by: Nicola Soranzo <nicola.soranzo@gmail.com>

--- lib/galaxy/visualization/plugins/registry.py
+++ lib/galaxy/visualization/plugins/registry.py
@@ -46,8 +46,6 @@ class VisualizationsRegistry:
 
         :type   app:        galaxy.app.UniverseApplication
         :param  app:        the application (and its configuration) using this manager
-        :type   base_url:   string
-        :param  base_url:   url to prefix all plugin urls with
         """
         self.app = weakref.ref(app)
         self.config_parser = config_parser.VisualizationsConfigParser()
