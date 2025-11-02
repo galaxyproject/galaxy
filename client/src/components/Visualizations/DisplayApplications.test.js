bc4c76f72f7586fcf9f4b9159aac038ed206b8f7
guerler <aysam.guerler@gmail.com>
2025-11-02 08:17:38 +0300

    Update display applications unit test

--- client/src/components/Visualizations/DisplayApplications.test.js
+++ client/src/components/Visualizations/DisplayApplications.test.js
@@ -7,60 +7,76 @@ import DisplayApplications from "./DisplayApplications.vue";
 
 const localVue = getLocalVue();
 
-describe("DisplayApplications", () => {
-    let wrapper;
+const raw = {
+    display_apps: [
+        {
+            label: "app-1",
+            links: [
+                {
+                    app_name: "link-app-1",
+                    link_name: "link-name-1",
+                    text: "link-text-1",
+                },
+            ],
+        },
+        {
+            label: "app-2",
+            links: [
+                {
+                    app_name: "link-app-2",
+                    link_name: "link-name-2",
+                    text: "link-text-2",
+                },
+                {
+                    app_name: "link-app-3",
+                    link_name: "link-name-3",
+                    text: "link-text-3",
+                },
+            ],
+        },
+    ],
+};
 
-    beforeEach(() => {
-        wrapper = mount(DisplayApplications, {
-            propsData: {
-                datasetId: "dataset-id",
-            },
-            stubs: {
-                DatasetProvider: MockProvider({
-                    result: {
-                        display_apps: [
-                            {
-                                label: "app-1",
-                                links: [
-                                    {
-                                        href: "link-href-1",
-                                        text: "link-text-1",
-                                        target: "link-target-1",
-                                    },
-                                ],
-                            },
-                            {
-                                label: "app-2",
-                                links: [
-                                    {
-                                        href: "link-href-2",
-                                        text: "link-text-2",
-                                        target: "link-target-2",
-                                    },
-                                    {
-                                        href: "link-href-3",
-                                        text: "link-text-3",
-                                        target: "link-target-3",
-                                    },
-                                ],
-                            },
-                        ],
-                    },
-                }),
-            },
-            localVue,
+jest.mock("../providers/DatasetProvider", () => ({
+    render() {
+        return this.$scopedSlots.default({
+            loading: false,
+            result: raw,
         });
-    });
+    },
+}));
 
+function mountTarget() {
+    return mount(DisplayApplications, {
+        propsData: {
+            datasetId: "dataset-id",
+        },
+        stubs: {
+            RouterLink: {
+                props: ["to"],
+                render(h) {
+                    return h("a", { attrs: { href: this.to } }, this.$slots.default);
+                },
+            },
+        },
+        localVue,
+    });
+}
+
+function getUrl(i) {
+    return `/display_applications/dataset-id/link-app-${i + 1}/link-name-${i + 1}`;
+}
+
+describe("DisplayApplications", () => {
     it("check props", async () => {
+        const wrapper = mountTarget();
         const labels = wrapper.findAll(".font-weight-bold");
         for (let i = 0; i < 2; i++) {
             expect(labels.at(i).text()).toBe(`app-${i + 1}`);
         }
         const links = wrapper.findAll("a");
         for (let i = 0; i < 3; i++) {
-            expect(links.at(i).attributes("href")).toBe(`link-href-${i + 1}`);
-            expect(links.at(i).attributes("target")).toBe(`link-target-${i + 1}`);
+            expect(links.at(i).attributes("href")).toBe(getUrl(i));
             expect(links.at(i).text()).toBe(`link-text-${i + 1}`);
         }
     });
