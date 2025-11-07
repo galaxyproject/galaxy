d052f73474595e767bc7b9b9120ef2956487996f
guerler <aysam.guerler@gmail.com>
2025-11-08 01:50:27 +0300

    Add more tests, remove legacy controller tests

--- lib/galaxy_test/api/test_display_applications.py
+++ lib/galaxy_test/api/test_display_applications.py
@@ -58,28 +58,52 @@ class TestDisplayApplicationsApi(ApiTestCase):
         response = self._post("display_applications/reload")
         self._assert_status_code_is(response, 403)
 
-    def test_create_link_igv_interval_as_bed(self):
-        contents = open(self.get_filename("1.interval")).read()
-        with self.dataset_populator.test_history() as history_id:
-            details = self.dataset_populator.new_dataset(history_id, content=contents, file_type="interval")
-            self.dataset_populator.wait_for_history(history_id, assert_ok=True)
-            payload = {"app_name": "igv_interval_as_bed", "link_name": "local_default", "dataset_id": details["id"]}
-            response = self._post("display_applications/create_link", data=payload, json=True)
-            self._assert_status_code_is(response, 200)
-            response_json = response.json()
-            assert response_json["refresh"]
-            assert response_json["preparable_steps"][0]["name"] == "bed_file"
-            assert "required additional datasets" in response_json["messages"][0][0]
-            for _ in range(10):
+    def test_create_link(self):
+        cases = [
+            {"file": "1.interval", "app": "igv_interval_as_bed", "step": "bed_file"},
+            {"file": "1.bam", "app": "igv_bam", "step": None},
+            {"file": "test.vcf", "app": "igv_vcf", "step": "bgzip_file"},
+            {"file": "test.vcf.gz", "app": "igv_vcf", "step": None},
+            {"file": "5.gff", "app": "igv_gff", "step": None},
+            {"file": "1.bigwig", "app": "igv_bigwig", "step": None},
+            {"file": "1.fasta", "app": "igv_fasta", "step": "fasta_file"},
+        ]
+        for case in cases:
+            case: dict[str, object]
+            test_file = self.get_filename(case["file"])
+            with self.dataset_populator.test_history() as history_id:
+                ds = self.dataset_populator.new_dataset(
+                    history_id,
+                    content=open(test_file, "rb"),
+                    file_type="auto",
+                    wait=True,
+                )
+                payload = {
+                    "app_name": case["app"],
+                    "dataset_id": ds["id"],
+                    "link_name": "local_default",
+                }
                 response = self._post("display_applications/create_link", data=payload, json=True)
                 self._assert_status_code_is(response, 200)
                 response_json = response.json()
-                if response_json["refresh"] is False:
-                    break
-                time.sleep(2)
-            assert response_json["refresh"] is False
-            assert "http://localhost:60151/load?file=http" in response_json["resource"]
-            assert "name=Test_Dataset" in response_json["resource"]
+                step = case["step"]
+                if step is not None:
+                    assert response_json["refresh"]
+                    assert response_json["preparable_steps"][0]["name"] == step
+                    assert "required additional datasets" in response_json["messages"][0][0]
+                    if step == "bed_file":
+                        for _ in range(10):
+                            response = self._post("display_applications/create_link", data=payload, json=True)
+                            self._assert_status_code_is(response, 200)
+                            response_json = response.json()
+                            if response_json["refresh"] is False:
+                                break
+                            time.sleep(2)
+                        assert response_json["refresh"] is False
+                else:
+                    assert response_json["refresh"] is False
+                    assert "http://localhost:60151/load?file=http" in response_json["resource"]
+                    assert "name=Test_Dataset" in response_json["resource"]
 
     def _get_half_random_items(self, collection: list[str]) -> list[str]:
         half_num_items = int(len(collection) / 2)
