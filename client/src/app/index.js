66b73f566d1b689d32530ddd922bbf28cd6562c5
guerler <aysam.guerler@gmail.com>
2025-11-10 12:47:25 +0300

    Expose window.Galaxy as previously but without set and monitoring overhead

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -2,4 +2,3 @@
 // order is important here for unit tests, which is unfortunate and should be
 // fixed in the future
 export { getGalaxyInstance, setGalaxyInstance } from "./singleton";
-export { default as galaxyFacade } from "./monitor";
--- client/src/onload/standardInit.js
+++ client/src/onload/standardInit.js
@@ -1,17 +1,28 @@
-// standardInit.ts
-import galaxyModule from "@/app/galaxy";
-import { setGalaxyInstance } from "@/app/singleton";
+import galaxyModule from "@/app/galaxy"
+import { getGalaxyInstance, setGalaxyInstance } from "@/app/singleton"
 
-import { globalInits } from "./globalInits";
-import { loadConfig } from "./loadConfig";
+import { globalInits } from "./globalInits"
+import { loadConfig } from "./loadConfig"
 
-const { GalaxyApp } = galaxyModule;
+const { GalaxyApp } = galaxyModule
 
 export async function standardInit(label = "Galaxy") {
-    const config = await loadConfig();
-    const app = new GalaxyApp(config, {});
-    setGalaxyInstance(() => app);
-    console.debug(`${label} app`);
-    await globalInits(app, config);
-    return app;
+    const config = await loadConfig()
+    const app = new GalaxyApp(config, {})
+
+    // Register singleton
+    setGalaxyInstance(() => app)
+
+    // Expose Galaxy object in window
+    if (!window.Galaxy) {
+        Object.defineProperty(window, "Galaxy", {
+            enumerable: true,
+            configurable: true,
+            get: () => getGalaxyInstance(),
+        })
+    }
+
+    console.debug(`${label} app`)
+    await globalInits(app, config)
+    return app
 }
