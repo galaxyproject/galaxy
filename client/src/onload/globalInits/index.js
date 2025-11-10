94df9b6e73836ceb835145462366dcb25ed50d36
guerler <aysam.guerler@gmail.com>
2025-11-10 12:41:08 +0300

    Remove legacy monitoring of Galaxy object

--- client/src/onload/globalInits/index.js
+++ client/src/onload/globalInits/index.js
@@ -1,11 +1,8 @@
 // globalInits.ts
-import { monitorInit } from "@/utils/installMonitor";
-
 import { initSentry } from "./initSentry";
 import { onloadWebhooks } from "./onloadWebhooks";
 
 export async function globalInits(galaxy, config) {
-    monitorInit(galaxy, config);
     await initSentry(galaxy, config);
     await onloadWebhooks(galaxy);
 }
