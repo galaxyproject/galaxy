e4422048cdd459a9e9d6e71ebdb01abe111fa2a5
Dannon Baker <dannon.baker@gmail.com>
2025-11-18 09:40:12 -0500

    Add getLocalVue helper to vitest helpers
    This fixes the BreadcrumbHeading test and enables component tests to use the same test setup utilities as Jest tests.

--- client/tests/vitest/helpers.js
+++ client/tests/vitest/helpers.js
@@ -1,7 +1,39 @@
 /**
  * Unit test debugging utilities for Vitest
  */
+import { createLocalVue } from "@vue/test-utils";
+import BootstrapVue from "bootstrap-vue";
+import { localizationPlugin } from "@/components/plugins/localization";
+import { vueRxShortcutPlugin } from "@/components/plugins/vueRxShortcuts";
+import { PiniaVuePlugin } from "pinia";
+import _l from "@/utils/localization";
 import { vi } from "vitest";
+
+function testLocalize(text) {
+    if (text) {
+        return `test_localized<${text}>`;
+    } else {
+        return text;
+    }
+}
+
+// Gets a localVue with custom directives
+export function getLocalVue(instrumentLocalization = false) {
+    const localVue = createLocalVue();
+    const mockedDirective = {
+        bind(el, binding) {
+            el.setAttribute("data-mock-directive", binding.value || el.title);
+        },
+    };
+    localVue.use(PiniaVuePlugin);
+    localVue.use(BootstrapVue);
+    const l = instrumentLocalization ? testLocalize : _l;
+    localVue.use(localizationPlugin, l);
+    localVue.use(vueRxShortcutPlugin);
+    localVue.directive("b-tooltip", mockedDirective);
+    localVue.directive("b-popover", mockedDirective);
+    return localVue;
+}
 
 export function suppressDebugConsole() {
     vi.spyOn(console, "debug").mockImplementation(vi.fn());
