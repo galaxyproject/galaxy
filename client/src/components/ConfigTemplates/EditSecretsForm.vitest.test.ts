6d4ba6326ad85728f47a7481ecd89f8eaa4d5ddd
Dannon Baker <dannon.baker@gmail.com>
2025-11-20 22:19:08 -0500

    Migrate Form Elements and ConfigTemplates tests to Vitest

--- client/src/components/ConfigTemplates/EditSecretsForm.vitest.test.ts
+++ client/src/components/ConfigTemplates/EditSecretsForm.vitest.test.ts
@@ -1,5 +1,5 @@
+import { getLocalVue } from "@tests/vitest/helpers";
 import { mount } from "@vue/test-utils";
-import { getLocalVue } from "tests/jest/helpers";
 
 import { STANDARD_FILE_SOURCE_TEMPLATE, STANDARD_OBJECT_STORE_TEMPLATE } from "./test_fixtures";
 
