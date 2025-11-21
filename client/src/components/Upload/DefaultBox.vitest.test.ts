1c80e64d62381bff7f2c27c100f6a5eb5a0fb28d
Dannon Baker <dannon.baker@gmail.com>
2025-11-21 14:43:32 -0500

    Migrate Upload tests to Vitest

--- client/src/components/Upload/DefaultBox.vitest.test.ts
+++ client/src/components/Upload/DefaultBox.vitest.test.ts
@@ -1,7 +1,8 @@
 import { createTestingPinia } from "@pinia/testing";
+import { getLocalVue } from "@tests/vitest/helpers";
 import { mount } from "@vue/test-utils";
 import flushPromises from "flush-promises";
-import { getLocalVue } from "tests/jest/helpers";
+import { vi } from "vitest";
 
 import DefaultBox from "./DefaultBox.vue";
 
@@ -43,9 +44,9 @@ describe("Default", () => {
         // [Vue warn]: Error in directive b-visible unbind hook: "TypeError: this.observer.disconnect is not a function"
         // I don't think there is a problem with the usage so I think this a bug in bootstrap vue, it can be worked around
         // with the following code - but just suppressing the warning is probably better?
-        const observerMock = jest.fn(function IntersectionObserver(callback: IntersectionObserverCallback) {
-            this.observe = jest.fn();
-            this.disconnect = jest.fn();
+        const observerMock = vi.fn(function IntersectionObserver(callback: IntersectionObserverCallback) {
+            this.observe = vi.fn();
+            this.disconnect = vi.fn();
             this.trigger = (mockedMutationsList: IntersectionObserverEntry[]) => {
                 callback(mockedMutationsList, this);
             };
