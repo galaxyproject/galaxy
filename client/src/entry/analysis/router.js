d880276398ffb30003acf780dce511a149d4ccea
davelopez <46503462+davelopez@users.noreply.github.com>
2025-11-24 13:57:18 +0100

    Adds upload method selection index page

--- client/src/entry/analysis/router.js
+++ client/src/entry/analysis/router.js
@@ -67,6 +67,7 @@ import CreateUserObjectStore from "@/components/ObjectStore/Templates/CreateUser
 import PageView from "@/components/Page/PageView.vue";
 import PageForm from "@/components/PageDisplay/PageForm.vue";
 import PageEditor from "@/components/PageEditor/PageEditor.vue";
+import UploadIndex from "@/components/Panels/Upload/UploadIndex.vue";
 import UploadMethodView from "@/components/Panels/Upload/UploadMethodView.vue";
 import Sharing from "@/components/Sharing/SharingPage.vue";
 import ToolReport from "@/components/Tool/ToolReport.vue";
@@ -237,6 +238,10 @@ export function getRouter(Galaxy) {
                     {
                         path: "about",
                         component: AboutGalaxy,
+                    },
+                    {
+                        path: "upload",
+                        component: UploadIndex,
                     },
                     {
                         path: "upload/:methodId",
