8d002de061f4e38815803947e048a3999681a8d6
mvdbeek <m.vandenbeek@gmail.com>
2025-12-22 15:26:16 +0100

    Fix sample_sheet landing dereference

--- lib/galaxy_test/api/test_dataset_collections.py
+++ lib/galaxy_test/api/test_dataset_collections.py
@@ -332,7 +332,6 @@ class TestDatasetCollectionsApi(ApiTestCase):
             rows={"sample1": [42]},
         )
         create_response = self._post("dataset_collections", payload, json=True)
-        print(create_response.json())
         self._check_create_response(create_response)
         dataset_collection = create_response.json()
         assert dataset_collection["collection_type"] == "sample_sheet:paired"
