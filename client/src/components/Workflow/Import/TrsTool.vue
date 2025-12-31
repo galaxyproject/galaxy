02476622b68dd08e8d793a76d517d44b4fc275cf
Cameron Hyde <chyde@neoformit.com>
2025-12-31 10:23:36 +1000

    TrsSearch result items improved styling

--- client/src/components/Workflow/Import/TrsTool.vue
+++ client/src/components/Workflow/Import/TrsTool.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { BButton } from "bootstrap-vue";
+import { BButton, BFormSelect } from "bootstrap-vue";
+import { computed, ref } from "vue";
 
 import { useMarkdown } from "@/composables/markdown";
 
@@ -19,9 +20,28 @@ const emit = defineEmits<{
 
 const { renderMarkdown } = useMarkdown({ openLinksInNewPage: true });
 
-function importVersion(version: TrsToolVersion) {
-    const version_id = version.id.includes(`:${version.name}`) ? version.name : version.id;
-    emit("onImport", version_id);
+const reversedVersions = computed(() => {
+    return [...props.trsTool.versions].reverse();
+});
+
+const selectedVersion = ref<TrsToolVersion | null>(
+    reversedVersions.value.length > 0 ? reversedVersions.value[0] : null
+);
+
+const versionOptions = computed(() => {
+    return reversedVersions.value.map((version) => ({
+        value: version,
+        text: version.name,
+    }));
+});
+
+function importSelectedVersion() {
+    if (selectedVersion.value) {
+        const version_id = selectedVersion.value.id.includes(`:${selectedVersion.value.name}`)
+            ? selectedVersion.value.name
+            : selectedVersion.value.id;
+        emit("onImport", version_id);
+    }
 }
 </script>
 
@@ -45,18 +65,22 @@ function importVersion(version: TrsToolVersion) {
         <div>
             <b>Versions</b>
 
-            <ul>
-                <li v-for="version in props.trsTool.versions" :key="version.id">
-                    <BButton
-                        class="m-1 workflow-import"
-                        :data-version-name="version.name"
-                        @click="importVersion(version)">
-                        {{ version.name }}
+            <div class="d-flex align-items-center gap-2 mt-2">
+                <BFormSelect
+                    v-model="selectedVersion"
+                    :options="versionOptions"
+                    class="workflow-version-select"
+                    style="max-width: 300px" />
 
-                        <FontAwesomeIcon :icon="faUpload" />
-                    </BButton>
-                </li>
-            </ul>
+                <BButton
+                    class="workflow-import"
+                    :disabled="!selectedVersion"
+                    @click="importSelectedVersion">
+                    Import
+
+                    <FontAwesomeIcon :icon="faUpload" />
+                </BButton>
+            </div>
         </div>
     </div>
 </template>
