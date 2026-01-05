d2ac43a62bfe918b75d1f635e10472948720e660
Bjoern Gruening <bjoern.gruening@gmail.com>
2026-01-05 22:28:47 +0100

    missing file

---
+++ client/src/components/Panels/panelViews.ts
@@ -0,0 +1,4 @@
+export const MY_PANEL_VIEW_ID = "my_panel";
+export const MY_PANEL_VIEW_NAME = "My panel";
+export const MY_PANEL_VIEW_DESCRIPTION = "Search all tools and view your favorite tools.";
+export const MY_PANEL_VIEW_TYPE = "favorites";
