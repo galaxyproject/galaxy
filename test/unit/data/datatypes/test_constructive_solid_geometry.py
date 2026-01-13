b21197b9a072fd838e25b19ddd043b29263dc71f
Thorben <stehling.t@web.de>
2026-01-13 12:50:37 +0100

    added filetypes gocadSgrid and fleflowfem

--- test/unit/data/datatypes/test_constructive_solid_geometry.py
+++ test/unit/data/datatypes/test_constructive_solid_geometry.py
@@ -1,4 +1,4 @@
-from galaxy.datatypes.constructive_solid_geometry import VtkXml
+from lib.galaxy.datatypes.constructive_solid_geometry import VtkXml, GocadSGrid, FeflowFem
 from .util import (
     get_dataset,
     MockDatasetDataset,
@@ -14,3 +14,23 @@ def test_vtkXml_set_meta():
     assert dataset.metadata.vtk_version == "0.1"
     assert dataset.metadata.file_format == "XML"
     assert dataset.metadata.dataset_type == "UnstructuredGrid"
+
+
+def test_GocadSGrid_set_meta():
+    gocad = GocadSGrid()
+    with get_dataset("testfile.sg") as dataset:
+        dataset.dataset = MockDatasetDataset(dataset.get_file_name())
+        gocad.set_meta(dataset)
+
+    assert dataset.metadata.gocad_version == "1"
+    assert dataset.metadata.name == "flow_simulation_grid_stair-stepped"
+
+
+def test_FeflowFem_set_meta():
+    feflow = FeflowFem()
+    with get_dataset("testfile.fem") as dataset:
+        dataset.dataset = MockDatasetDataset(dataset.get_file_name())
+        feflow.set_meta(dataset)
+
+    assert dataset.metadata.feflow_version == "v.6.004"
+    assert dataset.metadata.problem_type == "A new FEFLOW problem"
