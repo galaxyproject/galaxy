4c64b80cbb769e7ccfe0ec9dd311fb08d8c10f45
Alireza Heidari <itisalirh@gmail.com>
2026-02-05 16:11:37 +0100

    Add expandable row details functionality
    - Add expandedRows state to track expanded row indices
    - Implement isRowExpanded and toggleRowDetails methods
    - Add expand/collapse toggle button with chevron icon animation
    - Create row-details slot for custom expandable content
    - Restructure table body to support detail rows with template pattern

--- client/src/components/Common/GTable.vue
+++ client/src/components/Common/GTable.vue
@@ -216,6 +216,7 @@ const emit = defineEmits<{
 
 const sortBy = ref<string>(props.sortBy || "update_time");
 const sortDesc = ref<boolean>(props.sortDesc || true);
+const expandedRows = ref<Set<number>>(new Set());
 
 const localItems = computed(() => {
     const items = props.items || [];
@@ -339,6 +340,26 @@ function onSelectAll(selected: boolean) {
 function onRowSelect(item: T, index: number) {
     const isSelected = props.selectedItems.includes(index);
     emit("row-select", { item, index, selected: !isSelected });
+}
+
+/**
+ * Check if row details are expanded
+ */
+function isRowExpanded(index: number) {
+    return expandedRows.value.has(index);
+}
+
+/**
+ * Toggle row details expansion
+ */
+function toggleRowDetails(index: number) {
+    if (isRowExpanded(index)) {
+        expandedRows.value.delete(index);
+    } else {
+        expandedRows.value.add(index);
+    }
+    // Trigger reactivity
+    expandedRows.value = new Set(expandedRows.value);
 }
 
 /**
@@ -479,87 +500,106 @@ const getCellId = (tableId: string, fieldKey: string, index: number) => `g-table
                     </thead>
 
                     <tbody v-if="localItems.length > 0">
-                        <tr
-                            v-for="(item, index) in localItems"
-                            :id="getRowId(props.id, index)"
-                            :key="index"
-                            :class="{
-                                'g-table-row-clickable': clickableRows,
-                                'g-table-row-selected': isRowSelected(index),
-                            }"
-                            @click="onRowClick(item, index, $event)">
-                            <!-- Selection checkbox column -->
-                            <td v-if="selectable" class="g-table-select-column">
-                                <BFormCheckbox
-                                    :id="`${getRowId(props.id, index)}-select`"
-                                    v-b-tooltip.hover.noninteractive
-                                    :checked="isRowSelected(index)"
-                                    title="Select for bulk actions"
-                                    @click.stop
-                                    @change="onRowSelect(item, index)" />
-                            </td>
+                        <template v-for="(item, index) in localItems">
+                            <template>
+                                <tr
+                                    :id="getRowId(props.id, index)"
+                                    :key="`tr` + index"
+                                    :class="{
+                                        'g-table-row-clickable': clickableRows,
+                                        'g-table-row-selected': isRowSelected(index),
+                                    }"
+                                    @click="onRowClick(item, index, $event)">
+                                    <!-- Selection checkbox column -->
+                                    <td v-if="selectable" class="g-table-select-column">
+                                        <BFormCheckbox
+                                            :id="`${getRowId(props.id, index)}-select`"
+                                            v-b-tooltip.hover.noninteractive
+                                            :checked="isRowSelected(index)"
+                                            title="Select for bulk actions"
+                                            @click.stop
+                                            @change="onRowSelect(item, index)" />
+                                    </td>
 
-                            <!-- Data columns -->
-                            <td
-                                v-for="(field, fieldIndex) in props.fields"
-                                :id="getCellId(props.id, field.key, index)"
-                                :key="field.key"
-                                :class="[
-                                    field.cellClass,
-                                    field.class,
-                                    getAlignmentClass(field.align),
-                                    { 'hide-on-small': field.hideOnSmall },
-                                ]">
-                                <template v-if="fieldIndex === 0 && getStatusIcon(item, index)">
-                                    <FontAwesomeIcon
-                                        v-if="getStatusIcon(item, index)"
-                                        v-b-tooltip.hover.noninteractive
-                                        v-bind="getIconProps(item, index)"
-                                        fixed-width />
-                                </template>
-
-                                <slot :name="`cell(${field.key})`" :value="item[field.key]" :item="item" :index="index">
-                                    <span>{{ getCellValue(item, field) }}</span>
-                                </slot>
-                            </td>
-
-                            <!-- Actions column -->
-                            <td v-if="props.actions" class="g-table-actions-column">
-                                <slot name="actions" :item="item" :index="index">
-                                    <BDropdown
-                                        v-b-tooltip.hover.noninteractive
-                                        no-caret
-                                        right
-                                        title="More actions"
-                                        variant="link"
-                                        size="lg"
-                                        toggle-class="text-decoration-none p-0"
-                                        @click.stop>
-                                        <template v-slot:button-content>
-                                            <FontAwesomeIcon :icon="faEllipsisV" fixed-width />
+                                    <!-- Data columns -->
+                                    <td
+                                        v-for="(field, fieldIndex) in props.fields"
+                                        :id="getCellId(props.id, field.key, index)"
+                                        :key="field.key"
+                                        :class="[
+                                            field.cellClass,
+                                            field.class,
+                                            getAlignmentClass(field.align),
+                                            { 'hide-on-small': field.hideOnSmall },
+                                        ]">
+                                        <template v-if="fieldIndex === 0 && getStatusIcon(item, index)">
+                                            <FontAwesomeIcon
+                                                v-if="getStatusIcon(item, index)"
+                                                v-b-tooltip.hover.noninteractive
+                                                v-bind="getIconProps(item, index)"
+                                                fixed-width />
                                         </template>
 
-                                        <template v-for="ac in props.actions">
-                                            <BDropdownItem
-                                                v-if="ac.visible ?? true"
-                                                :id="ac.id"
-                                                :key="ac.id"
-                                                :disabled="ac.disabled"
-                                                :size="ac.size || 'sm'"
-                                                :variant="ac.variant || 'link'"
-                                                :to="ac.to"
-                                                :title="ac.title"
-                                                :href="ac.href"
-                                                :target="ac.externalLink ? '_blank' : undefined"
-                                                @click.stop="ac.handler && ac.handler(item, index)">
-                                                <FontAwesomeIcon v-if="ac.icon" :icon="ac.icon" fixed-width />
-                                                {{ ac.label }}
-                                            </BDropdownItem>
-                                        </template>
-                                    </BDropdown>
-                                </slot>
-                            </td>
-                        </tr>
+                                        <slot
+                                            :name="`cell(${field.key})`"
+                                            :value="item[field.key]"
+                                            :item="item"
+                                            :index="index"
+                                            :toggle-details="() => toggleRowDetails(index)">
+                                            <span>{{ getCellValue(item, field) }}</span>
+                                        </slot>
+                                    </td>
+
+                                    <!-- Actions column -->
+                                    <td v-if="props.actions" class="g-table-actions-column">
+                                        <slot name="actions" :item="item" :index="index">
+                                            <BDropdown
+                                                v-b-tooltip.hover.noninteractive
+                                                no-caret
+                                                right
+                                                title="More actions"
+                                                variant="link"
+                                                size="lg"
+                                                toggle-class="text-decoration-none p-0"
+                                                @click.stop>
+                                                <template v-slot:button-content>
+                                                    <FontAwesomeIcon :icon="faEllipsisV" fixed-width />
+                                                </template>
+
+                                                <template v-for="ac in props.actions">
+                                                    <BDropdownItem
+                                                        v-if="ac.visible ?? true"
+                                                        :id="ac.id"
+                                                        :key="ac.id"
+                                                        :disabled="ac.disabled"
+                                                        :size="ac.size || 'sm'"
+                                                        :variant="ac.variant || 'link'"
+                                                        :to="ac.to"
+                                                        :title="ac.title"
+                                                        :href="ac.href"
+                                                        :target="ac.externalLink ? '_blank' : undefined"
+                                                        @click.stop="ac.handler && ac.handler(item, index)">
+                                                        <FontAwesomeIcon v-if="ac.icon" :icon="ac.icon" fixed-width />
+                                                        {{ ac.label }}
+                                                    </BDropdownItem>
+                                                </template>
+                                            </BDropdown>
+                                        </slot>
+                                    </td>
+                                </tr>
+                            </template>
+
+                            <!-- Row details expansion -->
+                            <tr v-if="isRowExpanded(index)" :key="`details-${index}`" class="g-table-details-row">
+                                <td :colspan="props.fields.length + (selectable ? 1 : 0) + (props.actions ? 1 : 0)">
+                                    <slot
+                                        name="row-details"
+                                        :item="item"
+                                        :index="index"
+                                        :toggle-details="() => toggleRowDetails(index)" />
+                                </td>
+                            </tr>
+                        </template>
                     </tbody>
                 </table>
 
@@ -615,6 +655,10 @@ const getCellId = (tableId: string, fieldKey: string, index: number) => `g-table
 
             &.g-table-row-selected {
                 background-color: $brand-light;
+            }
+
+            &.g-table-details-row {
+                background-color: lighten($brand-light, 0.3);
             }
         }
 
