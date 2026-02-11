499eddcb475dff01d4615e1687ae852de54f4af2
guerler <aysam.guerler@gmail.com>
2026-02-11 17:31:46 +0300

    Adjust storage landing page

--- client/src/components/User/DiskUsage/StorageDashboard.vue
+++ client/src/components/User/DiskUsage/StorageDashboard.vue
@@ -1,58 +1,51 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { BCard, BCardGroup } from "bootstrap-vue";
 import { useRouter } from "vue-router/composables";
 
 import localize from "@/utils/localization";
 
 import DiskUsageSummary from "./DiskUsageSummary.vue";
+import GButton from "@/components/BaseComponents/GButton.vue";
 import BreadcrumbHeading from "@/components/Common/BreadcrumbHeading.vue";
 import Heading from "@/components/Common/Heading.vue";
-import IconCard from "@/components/IconCard.vue";
 
 const router = useRouter();
 
-const texts = reactive({
-    title: localize("Storage Dashboard"),
-    subtitle: localize("Here you can see an overview of your disk usage status."),
-    freeSpace: {
+const title = localize("Storage Dashboard");
+const breadcrumbItems = [{ title }];
+
+const cards = [
+    {
+        key: "free-space",
         title: localize("Is your usage more than expected?"),
         description: localize(
             "Find out what is eating up your space and learn how to easily and safely free up some of it.",
         ),
-        icon: "fas fa-broom fa-6x",
+        icon: "fas fa-broom",
         buttonText: localize("Free up disk usage"),
+        onClick: () => router.push({ name: "StorageManager" }),
     },
-    explore_by_history: {
-        title: localize("Visually explore your disk usage by history"),
+    {
+        key: "explore-history",
+        title: localize("Explore disk usage by history"),
         description: localize(
-            "Want to know what histories or datasets take up the most space in your account? Here you can explore your disk usage in a visual way by history.",
+            "Find out which histories or datasets take up the most space in your account.",
         ),
-        icon: "fas fa-chart-pie fa-6x",
+        icon: "fas fa-chart-pie",
         buttonText: localize("Explore now"),
+        onClick: () => router.push({ name: "HistoriesOverview" }),
     },
-    explore_by_objectstore: {
-        title: localize("Visually explore your disk usage by storage"),
+    {
+        key: "explore-storage",
+        title: localize("Explore disk usage by storage"),
         description: localize(
-            "Want to know how the space in your account is being distributed across storages? Here you can explore your disk usage in a visual way by where it is physically stored.",
+            "See how space in your account is distributed across storage locations.",
         ),
-        icon: "fas fa-hdd fa-6x",
+        icon: "fas fa-hdd",
         buttonText: localize("Explore now"),
+        onClick: () => router.push({ name: "ObjectStoresOverview" }),
     },
-});
-
-const breadcrumbItems = [{ title: texts.title }];
-
-function goToStorageManager() {
-    router.push({ name: "StorageManager" });
-}
-
-function goToHistoriesOverview() {
-    router.push({ name: "HistoriesOverview" });
-}
-
-function goToObjectStoresOverview() {
-    router.push({ name: "ObjectStoresOverview" });
-}
+];
 </script>
 
 <template>
@@ -60,36 +53,20 @@ function goToObjectStoresOverview() {
         <BreadcrumbHeading :items="breadcrumbItems" />
 
         <Heading h2 size="sm">
-            {{ texts.subtitle }}
+            {{ localize("Here you can see an overview of your disk usage status.") }}
         </Heading>
 
         <DiskUsageSummary class="m-3" />
 
-        <IconCard
-            class="mx-auto mb-3"
-            data-description="free space card"
-            :title="texts.freeSpace.title"
-            :description="texts.freeSpace.description"
-            :icon="texts.freeSpace.icon"
-            :button-text="texts.freeSpace.buttonText"
-            @onButtonClick="goToStorageManager" />
-
-        <IconCard
-            class="mx-auto mb-3"
-            data-description="explore usage card"
-            :title="texts.explore_by_history.title"
-            :description="texts.explore_by_history.description"
-            :icon="texts.explore_by_history.icon"
-            :button-text="texts.explore_by_history.buttonText"
-            @onButtonClick="goToHistoriesOverview" />
-
-        <IconCard
-            class="mx-auto mb-3"
-            data-description="explore object store usage card"
-            :title="texts.explore_by_objectstore.title"
-            :description="texts.explore_by_objectstore.description"
-            :icon="texts.explore_by_objectstore.icon"
-            :button-text="texts.explore_by_objectstore.buttonText"
-            @onButtonClick="goToObjectStoresOverview" />
+        <BCardGroup deck class="mx-3">
+            <BCard v-for="card in cards" :key="card.key" :data-description="`${card.key} card`" class="d-flex flex-column">
+                <h5 class="mb-2">
+                    <i :class="card.icon" class="fa-lg mr-2" />
+                    {{ card.title }}
+                </h5>
+                <p class="text-muted small flex-grow-1">{{ card.description }}</p>
+                <GButton color="blue" @click="card.onClick">{{ card.buttonText }}</GButton>
+            </BCard>
+        </BCardGroup>
     </div>
 </template>
