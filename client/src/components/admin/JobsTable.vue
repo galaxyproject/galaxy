0d9a05009920cdfad4b21ec532488df397eba756
Alireza Heidari <itisalirh@gmail.com>
2026-02-12 18:49:21 +0100

    Replace b-table with GTable in JobsTable component for improved rendering
    - Updated the JobsTable.vue to use GTable instead of b-table for better performance and consistency.
    - Adjusted the component imports and slots to accommodate the new table component.
    - Ensured that all relevant props and slots are maintained during the transition.

--- client/src/components/admin/JobsTable.vue
+++ client/src/components/admin/JobsTable.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="jobs-table-wrapper">
-        <b-table
+        <GTable
             v-model="innerValue"
             :fields="fields"
             :items="items"
@@ -16,66 +16,81 @@
             <template v-slot:table-caption>
                 {{ tableCaption }}
             </template>
+
             <template v-slot:empty>
                 <LoadingSpan v-if="loading" message="Loading jobs" />
                 <b-alert v-else class="no-jobs" variant="info" show>
                     {{ noItemsMessage }}
                 </b-alert>
             </template>
+
             <template v-slot:cell(update_time)="data">
                 <UtcDate :date="data.value" mode="elapsed" />
             </template>
+
             <template v-slot:row-details="row">
                 <b-card>
                     <JobDetails :job="row.item" />
                 </b-card>
             </template>
+
             <template v-slot:cell(user_email)="data">
-                <a class="job-filter-link-user" :data-user="data.value" @click="$emit('user-clicked', data.value)">{{
-                    data.value
-                }}</a>
+                <a class="job-filter-link-user" :data-user="data.value" @click="$emit('user-clicked', data.value)">
+                    {{ data.value }}
+                </a>
             </template>
+
             <template v-slot:cell(tool_id)="data">
                 <a
                     class="job-filter-link-tool-id"
                     :data-tool-id="data.value"
-                    @click="$emit('tool-clicked', data.value)"
-                    >{{ data.value }}</a
-                >
+                    @click="$emit('tool-clicked', data.value)">
+                    {{ data.value }}
+                </a>
             </template>
+
             <template v-slot:cell(job_runner_name)="data">
                 <a
                     class="job-filter-link-runner"
                     :data-runner="data.value"
-                    @click="$emit('runner-clicked', data.value)"
-                    >{{ data.value }}</a
-                >
+                    @click="$emit('runner-clicked', data.value)">
+                    {{ data.value }}
+                </a>
             </template>
+
             <template v-slot:cell(handler)="data">
                 <a
                     class="job-filter-link-handler"
                     :data-handler="data.handler"
-                    @click="$emit('handler-clicked', data.value)"
-                    >{{ data.value }}</a
-                >
+                    @click="$emit('handler-clicked', data.value)">
+                    {{ data.value }}
+                </a>
             </template>
+
             <template v-for="(index, name) in $slots" v-slot:[name]>
                 <slot :name="name" />
             </template>
+
             <template v-for="(index, name) in $scopedSlots" v-slot:[name]="data">
                 <slot :name="name" v-bind="data"></slot>
             </template>
-        </b-table>
+        </GTable>
     </div>
 </template>
 
 <script>
+import GTable from "@/components/Common/GTable.vue";
 import JobDetails from "@/components/JobInformation/JobDetails.vue";
 import LoadingSpan from "@/components/LoadingSpan.vue";
 import UtcDate from "@/components/UtcDate.vue";
 
 export default {
-    components: { UtcDate, JobDetails, LoadingSpan },
+    components: {
+        UtcDate,
+        JobDetails,
+        LoadingSpan,
+        GTable,
+    },
     props: {
         tableCaption: {
             type: String,
