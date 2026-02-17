556fa7c7d97ce42048096361da4ffb0c0c47e980
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2026-02-17 12:15:47 +0530

    No longer assert exact case match on emails

--- test/unit/app/managers/test_UserManager.py
+++ test/unit/app/managers/test_UserManager.py
@@ -31,7 +31,6 @@ user2_data = dict(email="user2@user2.user2", username="user2", password=default_
 user3_data = dict(email="user3@user3.user3", username="user3", password=default_password)
 user4_data = dict(email="user4@user4.user4", username="user4", password=default_password)
 uppercase_email_user = dict(email="USER5@USER5.USER5", username="USER5", password=default_password)
-lowercase_email_user = dict(email="user5@user5.user5", username="user5", password=default_password)
 
 
 # =============================================================================
@@ -261,21 +260,12 @@ class TestUserManager(BaseTestCase):
         # return None if username/email not found
         assert self.user_manager.get_user_by_identity("xyz") is None
         uppercase_user = self.user_manager.create(**uppercase_email_user)
-        assert uppercase_user.email == uppercase_email_user["email"]
+        assert uppercase_user.email == uppercase_email_user["email"].lower()
         assert uppercase_user.username == uppercase_email_user["username"]
         assert self.user_manager.get_user_by_identity(uppercase_user.email) == uppercase_user
         assert self.user_manager.get_user_by_identity(uppercase_user.username) == uppercase_user
-        # Create another user with the same email just differently capitalized.
-        # This is not normally allowed now, since registration goes through user_manager.register(),
-        # which checks for that, but was possible in earlier releases of Galaxy
-        lowercase_user = self.user_manager.create(**lowercase_email_user)
-        assert lowercase_user.email == lowercase_email_user["email"]
-        assert lowercase_user.username == lowercase_email_user["username"]
-        assert self.user_manager.get_user_by_identity(lowercase_user.email) == lowercase_user
-        assert self.user_manager.get_user_by_identity(lowercase_user.username) == lowercase_user
-        # assert uppercase user can still be retrieved
-        assert self.user_manager.get_user_by_identity(uppercase_user.email) == uppercase_user
-        assert self.user_manager.get_user_by_identity(uppercase_user.username) == uppercase_user
+        # Email lookups should remain case-insensitive.
+        assert self.user_manager.get_user_by_identity(uppercase_email_user["email"]) == uppercase_user
         # username matches need to be exact
         assert self.user_manager.get_user_by_identity(uppercase_user.username.capitalize()) is None
         # email matches can ignore capitalization
