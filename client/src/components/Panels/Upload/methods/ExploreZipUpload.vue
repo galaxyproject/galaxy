be462ee9a340b2b90401dc354ed171ab88fcdfec
davelopez <46503462+davelopez@users.noreply.github.com>
2026-02-19 18:56:55 +0100

    Refactors ZipImportWizard to handle target history validity

--- client/src/components/Panels/Upload/methods/ExploreZipUpload.vue
+++ client/src/components/Panels/Upload/methods/ExploreZipUpload.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import type { UploadMethodConfig } from "../types";
+
 import ZipImportWizard from "@/components/ImportData/zip/ZipImportWizard.vue";
+
+interface Props {
+    method: UploadMethodConfig;
+    targetHistoryId: string;
+}
+
+defineProps<Props>();
 </script>
 
 <template>
-    <ZipImportWizard :should-display-heading="false" />
+    <ZipImportWizard
+        :should-display-heading="false"
+        :target-history-id="targetHistoryId"
+        :show-target-history-warning="false" />
 </template>
