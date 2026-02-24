e3571ef9f1c3a15b352d6543925a0dd14c8f8205
Matthias Bernt <m.bernt@ufz.de>
2026-02-24 14:00:21 +0100

    add rudimentary tests

---
+++ test/unit/files/test_ssh.py
@@ -0,0 +1,18 @@
+import os
+
+import pytest
+
+from ._util import assert_simple_file_realize, assert_can_write_and_read_to_conf, write_from, configured_file_sources
+
+SCRIPT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
+FILE_SOURCES_CONF = os.path.join(SCRIPT_DIRECTORY, "ssh_file_sources_conf.yml")
+
+def test_file_source():
+    assert_simple_file_realize(FILE_SOURCES_CONF, recursive=False, contains=False)
+
+def test_file_write():
+    fs = configured_file_sources(FILE_SOURCES_CONF)
+    write_from(
+        fs,
+        uri="gxfiles://test1/aaa",
+        content="???")
