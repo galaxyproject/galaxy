9b73f50889ec40eb496a0802166ff16459066e03
John Chilton <jmchilton@gmail.com>
2026-02-24 12:29:57 -0500

    Refactor for test legibility.

--- lib/galaxy_test/base/populators.py
+++ lib/galaxy_test/base/populators.py
@@ -1216,8 +1216,12 @@ class BaseDatasetPopulator(BasePopulator):
 
         return dict(tool_id=tool_id, inputs=json.dumps(inputs), history_id=history_id, **kwds)
 
-    def build_tool_state(self, tool_id: str, history_id: str):
-        response = self._post(f"tools/{tool_id}/build?history_id={history_id}")
+    def build_tool_state(self, tool_id: str, history_id: str, inputs: Optional[dict] = None):
+        if inputs is not None:
+            payload = {"history_id": history_id, "inputs": inputs}
+            response = self._post(f"tools/{tool_id}/build", data=payload, json=True)
+        else:
+            response = self._post(f"tools/{tool_id}/build?history_id={history_id}")
         response.raise_for_status()
         return response.json()
 
