fc3bcb0476c3f4d5bfe15be21c96c22753156290
Matthias Bernt <m.bernt@ufz.de>
2026-02-26 09:39:37 +0100

    more py linter

--- test/unit/files/test_ssh.py
+++ test/unit/files/test_ssh.py
@@ -1,12 +1,14 @@
 import os
 
-from ._util import assert_simple_file_realize, assert_can_write_and_read_to_conf, write_from, configured_file_sources
+from ._util import assert_simple_file_realize, write_from, configured_file_sources
 
 SCRIPT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 FILE_SOURCES_CONF = os.path.join(SCRIPT_DIRECTORY, "ssh_file_sources_conf.yml")
 
+
 def test_file_source():
     assert_simple_file_realize(FILE_SOURCES_CONF, recursive=False, contains=False)
+
 
 def test_file_write():
     fs = configured_file_sources(FILE_SOURCES_CONF)
