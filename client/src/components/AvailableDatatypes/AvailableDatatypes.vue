354696a4a1711c6ed5733dc179ad284f87c68b0c
Alireza Heidari <itisalirh@gmail.com>
2026-03-10 16:27:13 +0100

    Refactor AvailableDatatypes component to use GLink for external links
    Replaced anchor tags with GLink component for improved consistency and tooltip support in the AvailableDatatypes component. This change enhances the user experience by providing a unified link handling approach.

--- client/src/components/AvailableDatatypes/AvailableDatatypes.vue
+++ client/src/components/AvailableDatatypes/AvailableDatatypes.vue
@@ -5,6 +5,7 @@ import type { TableField } from "@/components/Common/GTable.types";
 import { type DetailedDatatypes, useDetailedDatatypes } from "@/composables/datatypes";
 import { useFilterObjectArray } from "@/composables/filter";
 
+import GLink from "@/components/BaseComponents/GLink.vue";
 import DelayedInput from "@/components/Common/DelayedInput.vue";
 import GTable from "@/components/Common/GTable.vue";
 
@@ -42,37 +43,34 @@ const edamLink = (edamIRI: string) => `https://edamontology.github.io/edam-brows
             All Datatypes supported by this Galaxy instance. Hover over an item for more information. These extensions
             can be filtered by in the History, by expanding "search datasets".
         </p>
+
         <DelayedInput placeholder="filter extensions" class="mb-3" :delay="200" @change="(val) => (filter = val)" />
 
         <GTable compact show-empty striped sort-by="extension" :fields="fields" :items="filteredDatatypes">
             <template v-slot:cell(extension)="{ item }">
-                <a
+                <GLink
                     v-if="item.descriptionUrl"
-                    v-b-tooltip.hover
+                    tooltip
                     target="_blank"
                     :title="item.description"
                     :href="item.descriptionUrl">
                     {{ item.extension }}
-                </a>
+                </GLink>
                 <span v-else v-b-tooltip.hover :title="item.description">
                     {{ item.extension }}
                 </span>
             </template>
 
             <template v-slot:cell(edamFormatLabel)="{ item }">
-                <a
-                    v-b-tooltip.hover
-                    target="_blank"
-                    :href="edamLink(item.edamFormat)"
-                    :title="item.edamFormatDefinition">
+                <GLink tooltip target="_blank" :href="edamLink(item.edamFormat)" :title="item.edamFormatDefinition">
                     {{ item.edamFormatLabel }}
-                </a>
+                </GLink>
             </template>
 
             <template v-slot:cell(edamDataLabel)="{ item }">
-                <a v-b-tooltip.hover target="_blank" :href="edamLink(item.edamData)" :title="item.edamDataDefinition">
+                <GLink tooltip target="_blank" :href="edamLink(item.edamData)" :title="item.edamDataDefinition">
                     {{ item.edamDataLabel }}
-                </a>
+                </GLink>
             </template>
         </GTable>
     </div>
