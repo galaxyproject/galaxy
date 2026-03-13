5f8eab199c61f357ccdc0bffe827b061ca4d0fdc
Ahmed Awan <qe66653@umbc.edu>
2026-03-13 17:35:48 -0500

    just use `GCard` action in `JobError`

--- client/src/components/JobInformation/JobError.vue
+++ client/src/components/JobInformation/JobError.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { computed, ref } from "vue";
 
 import type { JobDetails } from "@/api/jobs";
 
-import GLink from "../BaseComponents/GLink.vue";
 import GModal from "../BaseComponents/GModal.vue";
 import GCard from "../Common/GCard.vue";
 import Heading from "../Common/Heading.vue";
@@ -23,6 +21,16 @@ const props = withDefaults(defineProps<Props>(), {
 const expanded = ref(false);
 const showInfo = ref(false);
 
+const openModalAction = [
+    {
+        id: "open-job-information",
+        title: "Open job information",
+        label: "See full job details",
+        icon: faInfoCircle,
+        handler: () => (showInfo.value = true),
+    },
+];
+
 const errorClasses = computed(() => {
     const classes = ["code"];
     if (!expanded.value) {
@@ -37,23 +45,18 @@ function toggleExpanded() {
 </script>
 
 <template>
-    <GCard :title="props.header" content-class="p-3">
+    <GCard :title="props.header" content-class="p-3" :primary-actions="openModalAction">
         <template v-slot:description>
             <div v-if="'stderr' in job && job.stderr">
-                <div class="d-flex align-items-center flex-gapx-1 py-2">
-                    <Heading
-                        class="unselectable w-100 mb-0"
-                        separator
-                        size="sm"
-                        :collapse="expanded ? 'open' : 'closed'"
-                        @click="toggleExpanded">
-                        Job Standard Error
-                        <i> ({{ expanded ? "click to collapse" : "click to expand" }}) </i>
-                    </Heading>
-                    <GLink class="text-nowrap" @click="showInfo = true">
-                        See full job details <FontAwesomeIcon :icon="faInfoCircle" />
-                    </GLink>
-                </div>
+                <Heading
+                    class="unselectable w-100 mb-0 py-2"
+                    separator
+                    size="sm"
+                    :collapse="expanded ? 'open' : 'closed'"
+                    @click="toggleExpanded">
+                    Job Standard Error
+                    <i> ({{ expanded ? "click to collapse" : "click to expand" }}) </i>
+                </Heading>
                 <pre :class="errorClasses">{{ job.stderr }}</pre>
             </div>
             <!-- TODO: modal for reporting error. -->
