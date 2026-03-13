cdd4a5f42637cdb2a8c30024dac31c642e709717
Ahmed Awan <qe66653@umbc.edu>
2026-03-13 17:14:21 -0500

    modernize/refactor client/src/components/JobInformation/JobError.vue

--- client/src/components/JobInformation/JobError.vue
+++ client/src/components/JobInformation/JobError.vue
@@ -1,70 +1,71 @@
-<template>
-    <b-card border-variant="danger" :header="header">
-        <b-card-text>
-            <div @click="showInfo = true">
-                <a href="#">See full job details <FontAwesomeIcon :icon="faInfoCircle" /></a>
-            </div>
-            <div v-if="job.stderr" class="error-wrapper" @click="toggleExpanded">
-                Job Standard Error
-                <pre :class="errorClasses">{{ job.stderr }}</pre>
-                <b v-if="!expanded">(Click to expand job standard error)</b>
-            </div>
-            <!-- TODO: modal for reporting error. -->
-        </b-card-text>
-        <b-modal v-model="showInfo" modal-class="job-information-modal" scrollable ok-only hide-header>
-            <JobInformation :job-id="job.id" :include-times="true" />
-        </b-modal>
-    </b-card>
-</template>
-
-<script>
+<script setup lang="ts">
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import BootstrapVue from "bootstrap-vue";
-import Vue from "vue";
+import { computed, ref } from "vue";
 
+import type { JobDetails } from "@/api/jobs";
+
+import GLink from "../BaseComponents/GLink.vue";
+import GModal from "../BaseComponents/GModal.vue";
+import GCard from "../Common/GCard.vue";
+import Heading from "../Common/Heading.vue";
 import JobInformation from "./JobInformation.vue";
 
-Vue.use(BootstrapVue);
+interface Props {
+    job: JobDetails;
+    header?: string;
+}
 
-export default {
-    components: { JobInformation, FontAwesomeIcon },
-    props: {
-        job: {
-            type: Object,
-            required: true,
-        },
-        header: {
-            type: String,
-            default: "Job ended in error",
-        },
-    },
-    data() {
-        return {
-            expanded: false,
-            showInfo: false,
-            faInfoCircle,
-        };
-    },
-    computed: {
-        errorClasses() {
-            const classes = ["code"];
-            if (!this.expanded) {
-                classes.push("preview");
-            }
-            return classes;
-        },
-    },
-    methods: {
-        toggleExpanded() {
-            this.expanded = !this.expanded;
-        },
-    },
-};
+const props = withDefaults(defineProps<Props>(), {
+    header: "Job ended in error",
+});
+
+const expanded = ref(false);
+const showInfo = ref(false);
+
+const errorClasses = computed(() => {
+    const classes = ["code"];
+    if (!expanded.value) {
+        classes.push("preview");
+    }
+    return classes;
+});
+
+function toggleExpanded() {
+    expanded.value = !expanded.value;
+}
 </script>
 
+<template>
+    <GCard :title="props.header" content-class="p-3">
+        <template v-slot:description>
+            <div v-if="'stderr' in job && job.stderr">
+                <div class="d-flex align-items-center flex-gapx-1 py-2">
+                    <Heading
+                        class="unselectable w-100 mb-0"
+                        separator
+                        size="sm"
+                        :collapse="expanded ? 'open' : 'closed'"
+                        @click="toggleExpanded">
+                        Job Standard Error
+                        <i> ({{ expanded ? "click to collapse" : "click to expand" }}) </i>
+                    </Heading>
+                    <GLink class="text-nowrap" @click="showInfo = true">
+                        See full job details <FontAwesomeIcon :icon="faInfoCircle" />
+                    </GLink>
+                </div>
+                <pre :class="errorClasses">{{ job.stderr }}</pre>
+            </div>
+            <!-- TODO: modal for reporting error. -->
+            <GModal :show.sync="showInfo" class="job-information-modal" size="medium" fixed-height>
+                <JobInformation :job-id="job.id" :include-times="true" />
+            </GModal>
+        </template>
+    </GCard>
+</template>
+
 <style scoped>
-.error-wrapper {
-    margin-top: 10px;
+.job-information-modal {
+    max-height: 80vh;
 }
 </style>
