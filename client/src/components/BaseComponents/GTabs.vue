2293eefbd5a0336185f5b4a2251c8084c4f5f08a
Dannon Baker <dannon.baker@gmail.com>
2026-03-23 14:28:32 -0400

    Add aria-controls and data-tab-title to vertical tab branch
    Upstream added these attributes to the tab <a> elements for
    accessibility and robust CSS selectors, but only the non-vertical
    template branch picked them up during rebase. Mirror them in the
    vertical branch to keep both in sync.

--- client/src/components/BaseComponents/GTabs.vue
+++ client/src/components/BaseComponents/GTabs.vue
@@ -180,7 +180,9 @@ const TabTitleContent = defineComponent({
                         role="tab"
                         href="#"
                         :aria-selected="index === activeIndex"
+                        :aria-controls="tab.id"
                         :aria-disabled="tab.disabled"
+                        :data-tab-title="tab.title"
                         v-bind="tab.titleLinkAttributes"
                         @click.prevent="!tab.disabled && setActive(index)">
                         <TabTitleContent v-if="tab.titleRenderer" :tab="tab" />
