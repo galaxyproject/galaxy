f7d03e2c5df8171a856739f56bbfdfc1ac01ef56
guerler <aysam.guerler@gmail.com>
2026-03-23 18:22:59 +0300

    Add empty string guard to capitalization utility

--- client/src/utils/strings.ts
+++ client/src/utils/strings.ts
@@ -46,6 +46,9 @@ export function snakeCaseToTitleCase(str: string): string {
  * Capitalize the first letter of a string
  */
 export function capitalizeFirstLetter(str: string): string {
+    if (!str) {
+        return "";
+    }
     str = str.trim();
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
