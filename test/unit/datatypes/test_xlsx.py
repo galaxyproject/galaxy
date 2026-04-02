1245c0c226b8731799a4bc384a4057c1440beb59
Anthony96p <anthony.pragassam@inrae.fr>
2026-04-02 09:46:09 +0200

    Fix the automated test (test_xlsx.py)

--- test/unit/datatypes/test_xlsx.py
+++ test/unit/datatypes/test_xlsx.py
@@ -1,13 +1,7 @@
 from galaxy.datatypes.binary import Xlsx
 
-from .util import MockDataset
-
 def test_sheet_names () :
-    dataset = MockDataset(id=1)
-
-    dataset.set_file_name("test-data/sheet_name.xlsx")
-
     datatype = Xlsx()
-    datatype.set_meta(dataset)
+    sheet_names = datatype.get_xlsx_sheet_names("test-data/sheet_name.xlsx")
 
-    assert dataset.metadata.sheet_names == ["Sheet1", "Sheet2"]
+    assert sheet_names == ["Sheet1", "Sheet2"]
