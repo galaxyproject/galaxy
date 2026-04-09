a31e6720ced0e9be39097dc28958ed03f6d79004
marius-mather <marius.mather@sydney.edu.au>
2026-04-09 15:28:19 +1000

    chore: linting

--- lib/galaxy/authnz/auth0.py
+++ lib/galaxy/authnz/auth0.py
@@ -1,8 +1,8 @@
-from galaxy.authnz.oidc import GalaxyOpenIdConnect
 from social_core.backends.auth0_openidconnect import Auth0OpenIdConnectAuth
+
+from galaxy.authnz.oidc import GalaxyOpenIdConnect
 
 
 class GalaxyAuth0OpenIdConnect(GalaxyOpenIdConnect, Auth0OpenIdConnectAuth):
     name = "auth0"
     EXTRA_DATA = ["id_token", "refresh_token", ("sub", "id"), "picture", "expires_in"]
-
