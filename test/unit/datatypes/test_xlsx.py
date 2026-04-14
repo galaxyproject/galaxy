93f2e932d58d48481233de67f47d8bf756e9e57f
Anthony96p <anthony.pragassam@inrae.fr>
2026-04-14 09:05:12 +0200

    Fix of 'Python linting / Test (3.10) (pull_request)' failing error

--- test/unit/datatypes/test_xlsx.py
+++ test/unit/datatypes/test_xlsx.py
@@ -1,6 +1,7 @@
 from galaxy.datatypes.binary import Xlsx
 
-def test_sheet_names () :
+
+def test_sheet_names() :
     datatype = Xlsx()
     sheet_names = datatype.get_xlsx_sheet_names("test-data/sheet_name.xlsx")
 
