10d9e95545bcf91817c687417ab616ce13148987
Matthias Bernt <m.bernt@ufz.de>
2026-04-17 11:02:22 +0200

    Just return None in any case

--- lib/galaxy/tools/parameters/basic.py
+++ lib/galaxy/tools/parameters/basic.py
@@ -973,7 +973,6 @@ class SelectToolParameter(ToolParameter):
     def __init__(self, tool: Optional["Tool"], input_source, context=None):
         input_source = ensure_input_source(input_source)
         super().__init__(tool, input_source)
-        self.profile = tool.profile if tool else None
         self.multiple = input_source.get_bool("multiple", False)
         # Multiple selects are optional by default, single selection is the inverse.
         self.optional = input_source.parse_optional(self.multiple)
@@ -1161,17 +1160,11 @@ class SelectToolParameter(ToolParameter):
             return None
         value = [option.value for option in options if option.selected]
         if len(value) == 0:
-            if (self.profile is None or Version(str(self.profile)) < Version("26.1")) and not self.optional and not self.multiple and options:
-                # Nothing selected, but not optional and not a multiple select, with some values,
-                # so we have to default to something (the HTML form will anyway)
-                value2: Optional[Union[str, list[str]]] = options[0].value
-            else:
-                value2 = None
+            return None
         elif len(value) == 1 or not self.multiple:
-            value2 = value[0]
+            return value[0]
         else:
-            value2 = value
-        return value2
+            return value
 
     def to_text(self, value):
         if not isinstance(value, list):
