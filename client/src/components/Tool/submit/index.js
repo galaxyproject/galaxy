afe1ce5e9fbf3387710fd9065f3de9c00d380653
guerler <aysam.guerler@gmail.com>
2026-04-27 12:11:35 +0300

    Add sentry message if celery is enabled, but parameters are missing

--- client/src/components/Tool/submit/index.js
+++ client/src/components/Tool/submit/index.js
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/vue";
+
 import { useConfigStore } from "@/stores/configurationStore";
 
 import { submitToolJob as submitAsync } from "./submitAsync";
@@ -5,9 +7,19 @@ import { submitToolJob as submitLegacy } from "./submitLegacy";
 
 export async function submitToolJob(params) {
     const configStore = useConfigStore();
+    const celeryEnabled = !!configStore.config?.enable_celery_tasks;
     const hasParameters = !!params.formConfig?.has_parameters;
-    if (configStore.config?.enable_celery_tasks && hasParameters) {
+    if (celeryEnabled && hasParameters) {
         return submitAsync(params);
+    }
+    if (celeryEnabled && !hasParameters) {
+        Sentry.captureMessage("tool submission fell back to /api/tools: no typed parameters", {
+            level: "info",
+            tags: {
+                fallback_reason: "no_parameters",
+                tool_id: params.jobDef?.tool_id,
+            },
+        });
     }
     return submitLegacy(params);
 }
