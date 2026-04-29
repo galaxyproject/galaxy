628fda6342f7317a40272d1e1b5ecb8dd5d96544
guerler <aysam.guerler@gmail.com>
2026-04-29 17:08:14 +0300

    Use list instead of List

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -18,7 +18,6 @@ from pathlib import Path
 from typing import (
     Any,
     cast,
-    List,
     NamedTuple,
     Optional,
     TYPE_CHECKING,
@@ -3822,7 +3821,7 @@ class DatabaseOperationTool(Tool):
                 history.stage_addition(element_object)
 
     @staticmethod
-    def _read_text_file_lines(path: str, size_hint: int = 1024 * 1000000) -> List[str]:
+    def _read_text_file_lines(path: str, size_hint: int = 1024 * 1000000) -> list[str]:
         try:
             with open(path) as fh:
                 return fh.readlines(size_hint)
