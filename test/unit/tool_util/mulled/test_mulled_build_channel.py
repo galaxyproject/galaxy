95fa19f9ed13cddf4005f99f9c50e13a0a667404
paperbenni <paperbenni@gmail.com>
2026-06-08 12:53:00 +0200

    fix: make test syntax compatible with older python versions

--- test/unit/tool_util/mulled/test_mulled_build_channel.py
+++ test/unit/tool_util/mulled/test_mulled_build_channel.py
@@ -26,11 +26,9 @@ def test_fetch_repo_data_uses_target_platform(tmp_path):
 def test_fetch_repo_data_defaults_to_host_platform(tmp_path):
     repo_data = tmp_path / "repodata.json"
     args = Namespace(channel="bioconda", repo_data=str(repo_data))
-    with (
-        mock.patch("galaxy.tool_util.deps.mulled.mulled_build.conda_platform", return_value="osx-arm64"),
-        mock.patch("galaxy.tool_util.deps.mulled.mulled_build_channel.subprocess.check_call") as check_call,
-    ):
-        assert _fetch_repo_data(args) == str(repo_data)
+    with mock.patch("galaxy.tool_util.deps.mulled.mulled_build.conda_platform", return_value="osx-arm64"):
+        with mock.patch("galaxy.tool_util.deps.mulled.mulled_build_channel.subprocess.check_call") as check_call:
+            assert _fetch_repo_data(args) == str(repo_data)
     assert check_call.call_args_list[0] == mock.call(
         [
             "wget",
