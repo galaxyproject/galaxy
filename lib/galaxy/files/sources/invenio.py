de0b297a38c4d7c281e90eeddd314f29bea611cb
Martin Cech <marten@bx.psu.edu>
2026-07-13 11:43:00 +0200

    use 1MB for default chunk size

--- lib/galaxy/files/sources/invenio.py
+++ lib/galaxy/files/sources/invenio.py
@@ -368,7 +368,7 @@ class InvenioRepositoryInteractor(RDMRepositoryInteractor):
             ) as response:
                 response.raise_for_status()
                 with open(file_path, "wb") as f:
-                    for chunk in response.iter_content(chunk_size=8192):
+                    for chunk in response.iter_content(chunk_size=2**20):
                         if chunk:
                             f.write(chunk)
         except requests.exceptions.HTTPError as e:
