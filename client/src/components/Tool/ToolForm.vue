98758790bece09916d603ded290146adf4a28914
John Chilton <jmchilton@gmail.com>
2026-08-18 15:41:35 -0400

    Restore tool submission error alert

--- client/src/components/Tool/ToolForm.vue
+++ client/src/components/Tool/ToolForm.vue
@@ -593,6 +593,9 @@ export default {
                 if (errorMsg) {
                     this.errorMessage = errorMsg;
                     this.submissionRequestFailed = true;
+                    this.showError = true;
+                    this.errorTitle = "Job submission failed.";
+                    this.errorContent = jobDef;
                     return;
                 }
 
