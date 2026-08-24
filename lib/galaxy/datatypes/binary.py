cf3b6a3041a1ba66417e3ac813b65aaa0edbdd44
paulineHans <phans@rptu.de>
2026-08-24 14:36:33 +0200

    added new datatype (mzlite) to galaxy

--- lib/galaxy/datatypes/binary.py
+++ lib/galaxy/datatypes/binary.py
@@ -3115,6 +3115,22 @@ class MzSQlite(SQlite):
             return self.sniff_table_names(filename, table_names)
         return False
 
+class Mzlite(SQlite): 
+    """Class describing a Proteomics mzlite database"""
+
+    file_ext ="mzlite"
+     def set_meta(self, dataset: DatasetProtocol, overwrite: bool = True, **kwd) -> None:
+        super().set_meta(dataset, overwrite=overwrite, **kwd)
+
+    def sniff(self, filename: str) -> bool:
+        if super().sniff(filename):
+            table_names = [
+                "Chromatogram",
+                "Model",
+                "Spectrum",
+            ]
+            return self.sniff_table_names(filename, table_names)
+        return False
 
 class PQP(SQlite):
     """
