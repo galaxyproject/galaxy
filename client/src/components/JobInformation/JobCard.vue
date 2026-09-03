591ab8377ddbb7825a1e44c887a40c840cef84a0
Ahmed Awan <qe66653@umbc.edu>
2026-09-03 12:57:28 -0500

    add rerun button to `JobCard`

--- client/src/components/JobInformation/JobCard.vue
+++ client/src/components/JobInformation/JobCard.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { faClock } from "@fortawesome/free-regular-svg-icons";
-import { faHdd, faWrench } from "@fortawesome/free-solid-svg-icons";
+import { faHdd, faRedo, faWrench } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { storeToRefs } from "pinia";
-import { useRouter } from "vue-router/composables";
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router/composables";
 
 import type { JobBaseModel } from "@/api/jobs";
 import { useHistoryStore } from "@/stores/historyStore";
@@ -17,9 +18,32 @@ const props = defineProps<{
     current?: boolean;
 }>();
 
+const route = useRoute();
 const router = useRouter();
 
 const { getHistoryNameById } = storeToRefs(useHistoryStore());
+
+/** Whether the job can be rerun; actually decided based on if the tool `is_workflow_compatible`,
+ * but that would require an extra fetch. Just going by known non-rerunnable tool ids for now.
+ */
+const jobIsRerunnable = computed(
+    () => !!props.job?.tool_id && !props.job.tool_id.startsWith("upload") && props.job.tool_id !== "__DATA_FETCH__",
+);
+
+const actions = computed(() => {
+    const actions = [];
+    if (jobIsRerunnable.value) {
+        actions.push({
+            id: "rerun-job-action",
+            icon: faRedo,
+            label: "Rerun",
+            title: "Rerun this job",
+            to: `/?job_id=${props.job.id}`,
+            disabled: route.fullPath === `/?job_id=${props.job.id}`,
+        });
+    }
+    return actions;
+});
 
 function cardClicked(job: JobBaseModel) {
     router.push(`/jobs/${job.id}/view`);
@@ -32,11 +56,11 @@ function cardClicked(job: JobBaseModel) {
         clickable
         button
         :current="props.current"
-        :active="props.current"
         :title="props.job.tool_id"
         :title-icon="{ icon: faWrench }"
         :title-n-lines="2"
         title-size="text"
+        :secondary-actions="actions"
         :update-time="props.job.update_time"
         :update-time-icon="faClock"
         @click="() => cardClicked(props.job)">
