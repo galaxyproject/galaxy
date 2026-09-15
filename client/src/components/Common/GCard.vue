4250a137c924047ae623c779fac29d4948d6b5a0
Ahmed Awan <qe66653@umbc.edu>
2026-09-15 11:07:25 -0700

    fix `GCard` external link primary action not opening in new tab

--- client/src/components/Common/GCard.vue
+++ client/src/components/Common/GCard.vue
@@ -698,6 +698,7 @@ function onKeyDown(event: KeyboardEvent) {
                                                         [String(pa.class)]: pa.class,
                                                     },
                                                 ]"
+                                                :target="pa.externalLink ? '_blank' : undefined"
                                                 @click.stop="pa.handler">
                                                 <FontAwesomeIcon
                                                     v-if="pa.icon"
