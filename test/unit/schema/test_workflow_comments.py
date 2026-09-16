b0f3b8b11a298a043125168d5221608662262023
John Chilton <jmchilton@gmail.com>
2026-09-16 08:39:14 -0400

    Keep workflow comment persistence tests out of schema package

--- test/unit/schema/test_workflow_comments.py
+++ test/unit/schema/test_workflow_comments.py
@@ -1,7 +1,6 @@
 import pytest
 from pydantic import ValidationError
 
-from galaxy.model import WorkflowComment
 from galaxy.schema.workflow.comments import WorkflowCommentModel
 
 
@@ -39,17 +38,3 @@ def test_comment_color_accepts_named_color():
 def test_comment_color_rejects_unknown_value():
     with pytest.raises(ValidationError):
         WorkflowCommentModel.model_validate(_comment_dict(color="chartreuse"))
-
-
-@pytest.mark.parametrize("comment_dict", [_comment_dict(), _comment_dict(color=None)])
-def test_comment_color_is_normalized_during_import(comment_dict):
-    comment = WorkflowComment.from_dict(comment_dict)
-
-    assert comment.color == "none"
-
-
-def test_null_database_comment_color_is_normalized_during_export():
-    comment = WorkflowComment.from_dict(_comment_dict())
-    comment.color = None
-
-    assert comment.to_dict()["color"] == "none"
