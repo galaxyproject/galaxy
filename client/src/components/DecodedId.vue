d14b08932f34527c2d29ddcae341b32052a4e1e6
Ahmed Awan <qe66653@umbc.edu>
2026-09-16 10:14:57 -0700

    ensure `DecodedId` updates when prop ID changes; migrate to Composition + TS

--- client/src/components/DecodedId.vue
+++ client/src/components/DecodedId.vue
@@ -1,45 +1,40 @@
-<template>
-    <span v-if="decoded_id">({{ decoded_id }})</span>
-</template>
-<script>
-import axios from "axios";
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { ref, watch } from "vue";
 
-import { getGalaxyInstance } from "@/app";
-import { getAppRoot } from "@/onload/loadConfig";
-import { rethrowSimple } from "@/utils/simple-error";
+import { GalaxyApi } from "@/api";
+import { useUserStore } from "@/stores/userStore";
 
-export default {
-    props: {
-        id: {
-            type: String,
-            required: true,
-        },
-    },
-    data() {
-        return { decoded_id: null };
-    },
-    computed: {
-        isAdmin() {
-            // window.parent.Galaxy is needed when instance is mounted in mako
-            const Galaxy = getGalaxyInstance() || window.parent.Galaxy;
-            return Galaxy?.user?.isAdmin() || false;
-        },
-    },
-    created: function () {
-        this.decodeId(this.id);
-    },
-    methods: {
-        decodeId: async function (id) {
-            if (this.isAdmin) {
-                const url = `${getAppRoot()}api/configuration/decode/${id}`;
-                try {
-                    const response = await axios.get(url);
-                    this.decoded_id = response.data.decoded_id;
-                } catch (e) {
-                    rethrowSimple(e);
-                }
+const props = defineProps<{
+    id: string;
+}>();
+
+const { isAdmin } = storeToRefs(useUserStore());
+
+const decodedId = ref<number | null>(null);
+
+watch(
+    () => props.id,
+    async (newId) => {
+        decodedId.value = null;
+        if (isAdmin.value) {
+            const { data, error } = await GalaxyApi().GET("/api/configuration/decode/{encoded_id}", {
+                params: {
+                    path: { encoded_id: newId },
+                },
+            });
+            if (error) {
+                console.error(error);
+                decodedId.value = null;
+            } else {
+                decodedId.value = data?.decoded_id || null;
             }
-        },
+        }
     },
-};
+    { immediate: true },
+);
 </script>
+
+<template>
+    <span v-if="decodedId">({{ decodedId }})</span>
+</template>
