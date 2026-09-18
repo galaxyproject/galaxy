ab7dca83b1a68d9b1d0a04b458855564694b2894
Ahmed Awan <qe66653@umbc.edu>
2026-09-17 21:52:16 -0700

    stub `RouterLink` in `ToolEntryPoints.test.ts` since switch to `GButton` in `GCard`

--- client/src/components/ToolEntryPoints/ToolEntryPoints.test.ts
+++ client/src/components/ToolEntryPoints/ToolEntryPoints.test.ts
@@ -78,6 +78,7 @@ function mountWithEntryPoints(entryPoints: EntryPoint[], jobId = JOB_ID) {
         pinia: testPinia,
         stubs: {
             FontAwesomeIcon: true,
+            RouterLink: true,
         },
     });
 }
