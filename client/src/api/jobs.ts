debd8b64d2c4f42a5bd60ec0b40a41504e0d1dd2
Ahmed Awan <qe66653@umbc.edu>
2026-09-17 20:51:32 -0700

    Make "stopping" and "deleting" jobs states non-terminal in client
    This came up when I was working on a live updating job-success view. If the user deleted a job, and the job went into the "deleting" or "stopping" states, we would stop polling on that job, even though the job would be yet to enter an actual terminal state ("deleted" for e.g.). Therefore, I moved "deleting" from `ERROR_STATES` and "stopping" from `TERMINAL_STATES` to `NON_TERMINAL_STATES` in `client/src/api/jobs.ts`.

--- client/src/api/jobs.ts
+++ client/src/api/jobs.ts
@@ -20,9 +20,19 @@ export type JobMessage =
     | components["schemas"]["OutputCollectionSecurityJobMessage"]
     | components["schemas"]["OutputDiscoveryJobMessage"];
 
-export const NON_TERMINAL_STATES = ["new", "queued", "running", "waiting", "paused", "resubmitted", "upload"];
-export const ERROR_STATES = ["error", "deleted", "deleting", "failed"];
-export const TERMINAL_STATES = ["ok", "skipped", "stop", "stopping"].concat(ERROR_STATES);
+export const NON_TERMINAL_STATES = [
+    "new",
+    "queued",
+    "running",
+    "waiting",
+    "paused",
+    "resubmitted",
+    "upload",
+    "stopping",
+    "deleting",
+];
+export const ERROR_STATES = ["error", "deleted", "failed"];
+export const TERMINAL_STATES = ["ok", "skipped", "stop"].concat(ERROR_STATES);
 
 interface JobDef {
     tool_id: string;
