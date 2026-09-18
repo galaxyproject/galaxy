99dfd3ca2ed72baba5f09b06cad2efc8fc1db388
Ahmed Awan <qe66653@umbc.edu>
2026-09-17 22:14:26 -0700

    ensure `JobHeader` "end" items remain on the right; add placeholders

--- client/src/components/JobInformation/JobHeader.vue
+++ client/src/components/JobInformation/JobHeader.vue
@@ -9,6 +9,7 @@ import Heading from "@/components/Common/Heading.vue";
 import SuccessIconOverlay from "@/components/Common/SuccessIndicator/SuccessIconOverlay.vue";
 import RerunJobButton from "@/components/JobInformation/RerunJobButton.vue";
 import JobState from "@/components/JobStates/JobState.vue";
+import LoadingSpan from "@/components/LoadingSpan.vue";
 
 const props = defineProps<{
     jobId: string;
@@ -25,12 +26,14 @@ const { job } = useJobDetails(toRef(props, "jobId"));
     <div>
         <div class="d-flex justify-content-between">
             <Heading
-                v-if="job && !props.noToolName"
+                v-if="!props.noToolName"
+                class="job-header-title"
                 :icon="!props.animateSuccess ? faWrench : undefined"
                 inline
                 size="md">
                 <SuccessIconOverlay v-if="props.animateSuccess" :covered-icon="faWrench" />
-                {{ toolStore.getToolNameById(job.tool_id, "Job Details") }}
+                <LoadingSpan v-if="!job" message="" />
+                <span v-else>{{ toolStore.getToolNameById(job.tool_id, "Job Details") }}</span>
             </Heading>
             <div class="job-header-end">
                 <JobState class="job-information-state-badge" :job-id="props.jobId" />
@@ -38,16 +41,23 @@ const { job } = useJobDetails(toRef(props, "jobId"));
                 <RerunJobButton :job-id="props.jobId" outline />
             </div>
         </div>
-        <hr v-if="job" />
+        <hr />
     </div>
 </template>
 
 <style lang="scss" scoped>
+.job-header-title {
+    min-width: 0;
+    flex-shrink: 1;
+}
+
 .job-header-end {
     display: flex;
     align-items: center;
     justify-content: flex-end;
     gap: 0.5rem;
     flex-wrap: wrap;
+    flex-shrink: 0;
+    margin-left: auto;
 }
 </style>
