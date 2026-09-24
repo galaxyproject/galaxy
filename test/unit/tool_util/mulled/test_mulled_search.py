c4ddb7dbe0ecab5680609899d13c8348a7d0c133
mvdbeek <m.vandenbeek@gmail.com>
2026-09-24 09:40:47 +0200

    Skip mulled tests when quay.io, depot or GitHub are unavailable

--- test/unit/tool_util/mulled/test_mulled_search.py
+++ test/unit/tool_util/mulled/test_mulled_search.py
@@ -7,11 +7,17 @@ from galaxy.tool_util.deps.mulled.mulled_search import (
     QuaySearch,
     singularity_search,
 )
-from galaxy.util.unittest_utils import skip_unless_executable
+from galaxy.util.unittest_utils import (
+    skip_if_galaxy_depot_down,
+    skip_if_github_down,
+    skip_if_quay_down,
+    skip_unless_executable,
+)
 from ..util import external_dependency_management
 
 
 @external_dependency_management
+@skip_if_quay_down
 def test_quay_search():
     t = QuaySearch("biocontainers")
     t.build_index()
@@ -32,6 +38,7 @@ def test_conda_search():
 
 
 @external_dependency_management
+@skip_if_github_down
 def test_github_recipe_present():
     t = GitHubSearch()
 
@@ -50,6 +57,7 @@ def test_github_recipe_present():
 
 
 @external_dependency_management
+@skip_if_quay_down
 def test_get_package_hash():
     package_hash1 = get_package_hash(["bamtools", "samtools"], {})
     package_hash2 = get_package_hash(["bamtools", "samtools"], {"bamtools": "2.4.0", "samtools": "1.3.1"})
@@ -62,6 +70,7 @@ def test_get_package_hash():
 
 
 @external_dependency_management
+@skip_if_galaxy_depot_down
 def test_singularity_search():
     sing1 = singularity_search("mulled-v2-0560a8046fc82aa4338588eca29ff18edab2c5aa")
     sing1_versions = {result["version"] for result in sing1}
